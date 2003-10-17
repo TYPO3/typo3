02ed6c9bd41d02e8a13c8741b6c4c86c9792012c
Robert Lemke <robert.lemke@typo3.org>
2003-10-17 09:47:02 +0000

    Added HTTP header support for charset encoding (relates to utf-8).
    The [config][metaCharset] information is no used for the HTTP header information. It's still possible to override this default by defining a addtionalHeaders entry.
    
    See thread at the dev-list archive:
    http://typo3.org/1427+M5518a97ebce.0.html
    
    
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@28 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/sysext/cms/tslib/class.tslib_fe.php
+++ typo3/sysext/cms/tslib/class.tslib_fe.php
@@ -1,22 +1,22 @@
 <?php
 /***************************************************************
 *  Copyright notice
-*  
+*
 *  (c) 1999-2003 Kasper Skårhøj (kasper@typo3.com)
 *  All rights reserved
 *
-*  This script is part of the TYPO3 project. The TYPO3 project is 
+*  This script is part of the TYPO3 project. The TYPO3 project is
 *  free software; you can redistribute it and/or modify
 *  it under the terms of the GNU General Public License as published by
 *  the Free Software Foundation; either version 2 of the License, or
 *  (at your option) any later version.
-* 
+*
 *  The GNU General Public License can be found at
 *  http://www.gnu.org/copyleft/gpl.html.
-*  A copy is found in the textfile GPL.txt and important notices to the license 
+*  A copy is found in the textfile GPL.txt and important notices to the license
 *  from the author is found in LICENSE.txt distributed with these scripts.
 *
-* 
+*
 *  This script is distributed in the hope that it will be useful,
 *  but WITHOUT ANY WARRANTY; without even the implied warranty of
 *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -24,7 +24,7 @@
 *
 *  This copyright notice MUST APPEAR in all copies of the script!
 ***************************************************************/
-/** 
+/**
  * Class for the built TypoScript based Front End
  *
  * This class has a lot of functions and internal variable which are use from index_ts.php.
@@ -2021,7 +2021,13 @@ if (version == "n3") {
 		if ($this->getMethodUrlIdToken)	{
 			$this->content = str_replace($this->getMethodUrlIdToken, $this->fe_user->get_URL_ID, $this->content);
 		}
-	
+
+			// Set header for charset-encoding if set. Added by RL 17.10.03
+		if ($this->config['config']['metaCharset'])	{
+			$headLine = 'Content-Type:text/html;charset='.trim ($this->config['config']['metaCharset']);
+			header ($headLine);
+		}
+
 			// Set headers, if any
 		if ($this->config['config']['additionalHeaders'])	{
 			$headerArray = explode('|', $this->config['config']['additionalHeaders']);
