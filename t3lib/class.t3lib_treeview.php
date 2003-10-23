e9c2f78f5e3c217c5b3ebc30a7bed6c9ed7d2c3d
Robert Lemke <robert.lemke@typo3.org>
2003-10-23 16:47:34 +0000

    Stripped the blank lines after ending php tag! (Caused "header already sent" errors)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@35 709f56b5-9817-0410-a4d7-c38de5d9e867

--- t3lib/class.t3lib_treeview.php
+++ t3lib/class.t3lib_treeview.php
@@ -878,5 +878,3 @@ if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['t3lib/class
 	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['t3lib/class.t3lib_treeview.php']);
 }
 ?>
-
-
