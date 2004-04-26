fd005392ec45dfc8e599d25e28538f28283b1c16
Kasper Skårhøj <kasper@typo3.org>
2004-04-26 21:58:46 +0000

    *** empty log message ***
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@243 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/install/index.php
+++ typo3/install/index.php
@@ -1,22 +1,22 @@
 <?php
 /***************************************************************
 *  Copyright notice
-*  
+*
 *  (c) 1999-2004 Kasper Skaarhoj (kasper@typo3.com)
 *  All rights reserved
 *
-*  This script is part of the TYPO3 project. The TYPO3 project is 
+*  This script is part of the TYPO3 project. The TYPO3 project is
 *  free software; you can redistribute it and/or modify
 *  it under the terms of the GNU General Public License as published by
 *  the Free Software Foundation; either version 2 of the License, or
 *  (at your option) any later version.
-* 
+*
 *  The GNU General Public License can be found at
 *  http://www.gnu.org/copyleft/gpl.html.
-*  A copy is found in the textfile GPL.txt and important notices to the license 
+*  A copy is found in the textfile GPL.txt and important notices to the license
 *  from the author is found in LICENSE.txt distributed with these scripts.
 *
-* 
+*
 *  This script is distributed in the hope that it will be useful,
 *  but WITHOUT ANY WARRANTY; without even the implied warranty of
 *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -37,11 +37,11 @@
 
 
 // **************************************************************************
-// Insert some security here, if you don't trust the Install Tool Password: 
+// Insert some security here, if you don't trust the Install Tool Password:
 // **************************************************************************
 
 	// This checks for my own IP at home. You can just remove the if-statement.
-if (1==0 || (substr($HTTP_SERVER_VARS['REMOTE_ADDR'],0,7)!='192.168' && $HTTP_SERVER_VARS['REMOTE_ADDR']!='127.0.0.1'))		{
+if (1==1 || (substr($HTTP_SERVER_VARS['REMOTE_ADDR'],0,7)!='192.168' && $HTTP_SERVER_VARS['REMOTE_ADDR']!='127.0.0.1'))		{
 	die("In the main source distribution of TYPO3, the install script is disabled by a die() function call.<BR>Open the file typo3/install/index.php and remove/out-comment the line that outputs this message!");
 }
 
