75820a2fd1469941cac75385e12be98bd1a87e75
Kasper Skårhøj <kasper@typo3.org>
2004-05-17 18:53:27 +0000

    * Moved "install" extension to be a sys extension in sysext/
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@306 709f56b5-9817-0410-a4d7-c38de5d9e867

---
+++ typo3/sysext/install/mod/conf.php
@@ -0,0 +1,11 @@
+<?php
+define('TYPO3_MOD_PATH', 'sysext/install/mod/');
+$BACK_PATH='../../../';
+
+$MLANG['default']['tabs_images']['tab'] = 'install.gif';
+$MLANG['default']['ll_ref']='LLL:EXT:install/mod/locallang_mod.php';
+
+$MCONF['script']=$BACK_PATH.'install/index.php';
+$MCONF['access']='admin';
+$MCONF['name']='tools_install';
+?>
---
+++ typo3/sysext/install/mod/locallang_mod.php
@@ -0,0 +1,70 @@
+<?php
+
+$LOCAL_LANG = Array (
+	"default" => Array (
+		"mlang_labels_tablabel" => "Install Tool",
+		"mlang_labels_tabdescr" => "Provides a simple link to the Install Tool. Notice the Install Tool (in typo3/install/) must be enabled.<br /><em>Access for 'admin' users only!</em>",
+		"mlang_tabs_tab" => "Install",
+	),
+	"dk" => Array (
+		"mlang_labels_tablabel" => "Install Tool",
+		"mlang_labels_tabdescr" => "Dette er et simpelt link til installations-værktøjet (Install Tool). Vær opmærksom på, at 'Install Tool' (ligger i typo3/install/) skal være slået til.<br /><em>Adgang kun for 'admin' brugere!</em>",
+		"mlang_tabs_tab" => "Installation",
+	),
+	"no" => Array (
+		"mlang_labels_tablabel" => "Install Tool",
+		"mlang_labels_tabdescr" => "Dett er en enkel link til installasjonsverktøyet (Install Tool). Vær oppmerksom på at 'Install Tool' (ligger i typo3/install/) må være aktivert.<br /><em>Adgang kun for 'admin' brukere!</em>",
+		"mlang_tabs_tab" => "Installasjon",
+	),
+	"de" => Array (
+		"mlang_labels_tablabel" => "Installations-Tool",
+		"mlang_labels_tabdescr" => "Bietet einen simplen Verweis auf das Installations-Werkzeug. Zu beachten ist, dass das Installations-Werkzeug (in typo3/install/) aktiviert seien muss.<br /><em>Zugriff nur für Administratoren!</em>",
+		"mlang_tabs_tab" => "Installation",
+	),
+	"it" => Array (
+		"mlang_labels_tablabel" => "Tool di installazione",
+		"mlang_labels_tabdescr" => "Link verso il tool di installazione (che deve essere attivato, in typo3/install/)<br /><em>Accesso riservato ai soli utenti 'admin'!</em>",
+		"mlang_tabs_tab" => "Installa",
+	),
+	"fr" => Array (
+		"mlang_labels_tablabel" => "Assistant d'installation",
+		"mlang_labels_tabdescr" => "Liaison simple avec l'assistant d'installation. Notez que cette assistant doit être activé manuellement.",
+		"mlang_tabs_tab" => "Installation",
+	),
+	"pl" => Array (
+		"mlang_labels_tablabel" => "Narzêdzie instalacyjne",
+		"mlang_labels_tabdescr" => "Udostêpnia szybkie wywo³anie narzêdzia instalacyjnego, pod warunkiem, ¿e jest w³±czone. <br /><em>Dostêp tylko dla administratorów!<em>",
+		"mlang_tabs_tab" => "Instalacja",
+	),
+	"es" => Array (
+		"mlang_labels_tablabel" => "Herramienta de Instalación",
+		"mlang_labels_tabdescr" => "Proporciona una liga sencilla a la liga de instalación. Nota: la herramienta de instalación (in typo3/install/) debe estar habilitada.<br /><em>¡Acceso solamente para usuarios 'administrador'!</em>",
+		"mlang_tabs_tab" => "Instalar",
+	),
+	"si" => Array (
+		"mlang_labels_tablabel" => "Orodje za inštalacijo",
+		"mlang_labels_tabdescr" => "Dostop do orodja za inštalacijo (typo3/install/) mora biti posebej omogoèen.<br /><em>Uporaba je dovoljena le 'admin' uporabnikom!</em>",
+		"mlang_tabs_tab" => "Inštalacija",
+	),
+	"nl" => Array (
+		"mlang_labels_tablabel" => "Installatie Werkset",
+		"mlang_labels_tabdescr" => "Biedt een link naar de Installatie Werkset. Denk er aan dat het Install Gereedschap (in/typo3/install/) geactiveerd dient te zijn.<br /><em>Alleen toegang voor 'admin' gebruikers!</em>",
+		"mlang_tabs_tab" => "Installatie",
+	),
+	"cz" => Array (
+		"mlang_labels_tablabel" => "Instalaèní pomùcka",
+		"mlang_labels_tabdescr" => "POskytuje jednoduchý odkaz na instalaèní pomùcku. Uvìdomte si, že instalaèní pomùcka (v typo3/install) musí být aktivována.<br /><em>Pøístup pouze pro 'admin' uživatele!</em>",
+		"mlang_tabs_tab" => "Instalace",
+	),
+	"fi" => Array (
+		"mlang_labels_tablabel" => "Asennus työkalu",
+		"mlang_labels_tabdescr" => "Tarkoituksen mukainen linkki TYPO3 asennukselle. Huomattavaa on että työkalu on asennettu ja käytettävissä.  <br /><em>Sallittu vain 'Admin'  käyttäjille.</em> ",
+		"mlang_tabs_tab" => "Asenna",
+	),
+	"tr" => Array (
+		"mlang_labels_tablabel" => "Kurulum Aracý",
+		"mlang_labels_tabdescr" => "Kurulum Aracýna basit link saðlar. Kurulum Aracýnýn (typo3/install un içinde/) çalýþýr durumda olduðundan mutlaka emin olun.<br /><em>Sadece kullanýcýlarýn  'yönetim' için geçiþi!</em>",
+		"mlang_tabs_tab" => "Kurulum",
+	),
+);
+?>
