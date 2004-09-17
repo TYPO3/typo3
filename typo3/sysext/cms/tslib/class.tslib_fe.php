e88b9219dd7d036cd49c026f8ae3b35a65640b99
Kasper Skårhøj <kasper@typo3.org>
2004-09-17 09:03:31 +0000

    *** empty log message ***
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/branches/TYPO3_3-7@480 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/sysext/cms/tslib/class.tslib_fe.php
+++ typo3/sysext/cms/tslib/class.tslib_fe.php
@@ -1112,7 +1112,7 @@
 		if (gettype($code)=='boolean' || !strcmp($code,1))	{
 			$this->printError('The page did not exist or was inaccessible.'.($reason ? ' Reason: '.htmlspecialchars($reason) : ''));
 			exit;
-		} else if (t3lib_div::testInt($code))	{
+		} elseif (t3lib_div::testInt($code))	{
 			$this->printError('Error '.$code.($reason ? ' Reason: '.htmlspecialchars($reason) : ''));
 			exit;
 		} elseif (t3lib_div::isFirstPartOfStr($code,'READFILE:')) {
@@ -1122,7 +1122,7 @@
 				$fileContent = str_replace('###CURRENT_URL###', t3lib_div::getIndpEnv('REQUEST_URI'), $fileContent);
 				$fileContent = str_replace('###REASON###', htmlspecialchars($reason), $fileContent);
 				echo $fileContent;
-		} else {
+			} else {
 				$this->printError('Configuration Error: 404 page "'.$readFile.'" could not be found.');
 			}
 			exit;
