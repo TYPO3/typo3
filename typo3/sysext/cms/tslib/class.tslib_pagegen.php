c658286765c131f8c06ad5bb42202e88e0e49793
Martin Kutschker <martin.t.kutschker@blackbox.net>
2005-09-19 08:16:56 +0000

    Fix bug #1287
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@798 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/sysext/cms/tslib/class.tslib_pagegen.php
+++ typo3/sysext/cms/tslib/class.tslib_pagegen.php
@@ -664,7 +664,7 @@ $GLOBALS['TSFE']->content.='
 		browserName = navigator.appName;
 		browserVer = parseInt(navigator.appVersion);
 		var msie4 = (browserName == "Microsoft Internet Explorer" && browserVer >= 4);
-		if ((browserName == "Netscape" && browserVer >= 3) || msie4 || browserName=="Konqueror") {version = "n3";} else {version = "n2";}
+		if ((browserName == "Netscape" && browserVer >= 3) || msie4 || browserName=="Konqueror" || browserName=="Opera") {version = "n3";} else {version = "n2";}
 			// Blurring links:
 		function blurLink(theObject)	{	//
 			if (msie4)	{theObject.blur();}
