2fe30b5ee74f68b0b6647e7f09e7ad17bbb155d1
Karsten Dambekalns <karsten.dambekalns@typo3.org>
2005-12-27 15:00:34 +0000

    Committed DBAL extension as a sysext. Older changes can be looked up in the TYPO3xdev CVS and are summarized in the ChangeLog file.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/dbal/trunk@23835 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ typo3/sysext/dbal/ext_tables.sql
@@ -0,0 +1,33 @@
+
+#
+# Table structure for table 'tx_dbal_debuglog'
+#
+CREATE TABLE tx_dbal_debuglog (
+	uid int(11) unsigned DEFAULT '0' NOT NULL auto_increment,
+	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
+	beuser_id int(11) unsigned DEFAULT '0' NOT NULL,
+	script tinytext NOT NULL,
+	exec_time int(11) unsigned DEFAULT '0' NOT NULL,
+	table_join tinytext NOT NULL,
+	serdata text NOT NULL,
+	query text NOT NULL,
+	errorFlag int(11) unsigned DEFAULT '0' NOT NULL,
+	
+	PRIMARY KEY (uid),
+	KEY tstamp (tstamp)
+);
+
+#
+# Table structure for table 'tx_dbal_debuglog_where'
+#
+CREATE TABLE tx_dbal_debuglog_where (
+	uid int(11) unsigned DEFAULT '0' NOT NULL auto_increment,
+	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
+	beuser_id int(11) unsigned DEFAULT '0' NOT NULL,
+	script tinytext NOT NULL,
+	tablename tinytext NOT NULL,
+	whereclause text NOT NULL,
+
+	PRIMARY KEY (uid),
+	KEY tstamp (tstamp)
+);
