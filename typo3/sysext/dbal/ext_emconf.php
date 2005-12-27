9ae275730bbbf47279db9d363d8addb729578594
Karsten Dambekalns <karsten.dambekalns@typo3.org>
2005-12-27 15:19:02 +0000

    Updated ext_emconf.php to not show changed files (caused by CVS commit).
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/dbal/trunk@23836 735d13b6-9817-0410-8766-e36946ffe9aa

--- typo3/sysext/dbal/ext_emconf.php
+++ typo3/sysext/dbal/ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "dbal"
 #
-# Auto generated 27-12-2005 15:45
+# Auto generated 27-12-2005 16:16
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write
@@ -17,6 +17,7 @@ $EM_CONF[$_EXTKEY] = Array (
 	'author_email' => 'k.dambekalns@fishfarm.de',
 	'shy' => '',
 	'dependencies' => 'adodb',
+	'TYPO3_version' => '3.9.0-',
 	'conflicts' => '',
 	'priority' => '',
 	'module' => 'mod1',
@@ -31,7 +32,7 @@ $EM_CONF[$_EXTKEY] = Array (
 	'private' => '',
 	'download_password' => '',
 	'version' => '0.5.0',	// Don't modify this! Managed automatically during upload to repository.
-	'_md5_values_when_last_written' => 'a:20:{s:9:"ChangeLog";s:4:"2614";s:21:"class.ux_t3lib_db.php";s:4:"e6f3";s:28:"class.ux_t3lib_sqlengine.php";s:4:"74b9";s:28:"class.ux_t3lib_sqlparser.php";s:4:"ed68";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"4c9c";s:15:"ext_php_api.dat";s:4:"56e0";s:14:"ext_tables.php";s:4:"427e";s:14:"ext_tables.sql";s:4:"1d3e";s:12:"doc/TODO.txt";s:4:"8ff6";s:14:"doc/manual.sxw";s:4:"d418";s:45:"handlers/class.tx_dbal_handler_openoffice.php";s:4:"fb7a";s:43:"handlers/class.tx_dbal_handler_rawmysql.php";s:4:"25f3";s:40:"handlers/class.tx_dbal_handler_xmldb.php";s:4:"df24";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"03f8";s:14:"mod1/index.php";s:4:"65cb";s:18:"mod1/locallang.xml";s:4:"af22";s:22:"mod1/locallang_mod.xml";s:4:"d1c6";s:19:"mod1/moduleicon.gif";s:4:"8074";}',
+	'_md5_values_when_last_written' => 'a:32:{s:9:"ChangeLog";s:4:"2614";s:21:"class.ux_t3lib_db.php";s:4:"033d";s:28:"class.ux_t3lib_sqlengine.php";s:4:"4f41";s:28:"class.ux_t3lib_sqlparser.php";s:4:"796d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"4c9c";s:15:"ext_php_api.dat";s:4:"56e0";s:14:"ext_tables.php";s:4:"427e";s:14:"ext_tables.sql";s:4:"1d3e";s:11:"CVS/Entries";s:4:"abb2";s:14:"CVS/Repository";s:4:"2282";s:8:"CVS/Root";s:4:"b923";s:12:"doc/TODO.txt";s:4:"8ff6";s:14:"doc/manual.sxw";s:4:"d418";s:15:"doc/CVS/Entries";s:4:"f7ec";s:18:"doc/CVS/Repository";s:4:"351a";s:12:"doc/CVS/Root";s:4:"b923";s:45:"handlers/class.tx_dbal_handler_openoffice.php";s:4:"84f3";s:43:"handlers/class.tx_dbal_handler_rawmysql.php";s:4:"160b";s:40:"handlers/class.tx_dbal_handler_xmldb.php";s:4:"6651";s:20:"handlers/CVS/Entries";s:4:"71c8";s:23:"handlers/CVS/Repository";s:4:"77d9";s:17:"handlers/CVS/Root";s:4:"b923";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"cf19";s:14:"mod1/index.php";s:4:"d895";s:18:"mod1/locallang.xml";s:4:"af22";s:22:"mod1/locallang_mod.xml";s:4:"d1c6";s:19:"mod1/moduleicon.gif";s:4:"8074";s:16:"mod1/CVS/Entries";s:4:"dbdc";s:19:"mod1/CVS/Repository";s:4:"16e3";s:13:"mod1/CVS/Root";s:4:"b923";}',
 );
 
 ?>
