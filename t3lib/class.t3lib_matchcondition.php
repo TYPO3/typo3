6e1b0f8aa419b8cb818b33756a80108a617cac81
Michael Stucki <michael.stucki@typo3.org>
2006-01-14 01:14:08 +0000

    * Reverted two changes which I committed by mistake - they need to be tested first.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/branches/TYPO3_4-0@1006 709f56b5-9817-0410-a4d7-c38de5d9e867

--- t3lib/class.t3lib_matchcondition.php
+++ t3lib/class.t3lib_matchcondition.php
@@ -341,15 +341,11 @@ class t3lib_matchCondition {
 	 */
 	function matchWild($haystack,$needle)	{
 		if ($needle && $haystack)	{
-			if (preg_match('/^\/.+\/$/', $needle))	{	// Looks like a regular expression
-				$regex = $needle;
-			} else {
-				$needle = str_replace('*', '###WILD###', $needle);
-				$regex = '/^'.preg_quote($needle,'/').'$/';
-				$regex = str_replace('###WILD###', '.*', $regex);	// Replace the marker with .* to match anything (wildcard)
-			}
+			$regex = '/^'.preg_quote($needle,'/').'$/';
+			$regex = str_replace('\\*', '.*', $regex);	// 1st fix: Replace \* with .* because * is our wildcard
+			$regex = str_replace('\\\\.*', '\\*', $regex);	// 2nd fix: Match needle if the * was escaped
 
-			if (preg_match($regex, $haystack)) return true;
+			if (preg_match($regex, $haystack, $res)) return true;
 		}
 	}
 
