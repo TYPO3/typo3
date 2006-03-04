f7310142c01894ef51ad9a0998ac8c1f050f60f9
Stanislas Rolland <typo3@sjbr.ca>
2006-03-04 05:37:20 +0000

    * Fix bugtracker issue 1736: Make TS_CASE object and ts_parser support key value 0.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/branches/TYPO3_4-0@1268 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/sysext/cms/tslib/class.tslib_content.php
+++ typo3/sysext/cms/tslib/class.tslib_content.php
@@ -1495,7 +1495,7 @@ class tslib_cObj {
 		if ($this->checkIf($conf['if.']))	{
 			if ($conf['setCurrent'] || $conf['setCurrent.']){$this->data[$this->currentValKey] = $this->stdWrap($conf['setCurrent'], $conf['setCurrent.']);}
 	 		$key = $this->stdWrap($conf['key'],$conf['key.']);
-	 		$key = $conf[$key] ? $key : 'default';
+	 		$key = isset($conf[$key]) ? $key : 'default';
 	 		$name = $conf[$key];
 	 		$theValue = $this->cObjGetSingle($name,$conf[$key.'.'], $key);
 	 		if ($conf['stdWrap.'])	{
