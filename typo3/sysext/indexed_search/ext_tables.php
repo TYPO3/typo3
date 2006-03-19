e33635094fe79a1220f19279c4a54716d6c79258
Martin Kutschker <martin.t.kutschker@blackbox.net>
2006-03-19 09:09:46 +0000

    Removed "select_key" and "pages (CODE and Startingpoint) from the plugin UI
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/branches/TYPO3_4-0@1337 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/sysext/indexed_search/ext_tables.php
+++ typo3/sysext/indexed_search/ext_tables.php
@@ -2,18 +2,19 @@
 if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 
 t3lib_extMgm::addPlugin(Array('LLL:EXT:indexed_search/locallang.php:mod_indexed_search', $_EXTKEY));
-if (TYPO3_MODE=='BE')	t3lib_extMgm::addModule('tools','isearch','after:log',t3lib_extMgm::extPath($_EXTKEY).'mod/');
+
+t3lib_div::loadTCA('tt_content');
+$TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY] = 'select_key,pages';
 
 if (TYPO3_MODE=='BE')    {
-    t3lib_extMgm::insertModuleFunction(
-        'web_info',
-        'tx_indexedsearch_modfunc1',
-        t3lib_extMgm::extPath($_EXTKEY).'modfunc1/class.tx_indexedsearch_modfunc1.php',
-        'LLL:EXT:indexed_search/locallang.php:mod_indexed_search'
-    );
-}
+	t3lib_extMgm::addModule('tools','isearch','after:log',t3lib_extMgm::extPath($_EXTKEY).'mod/');
 
-if (TYPO3_MODE=="BE")	{
+	t3lib_extMgm::insertModuleFunction(
+		'web_info',
+		'tx_indexedsearch_modfunc1',
+		t3lib_extMgm::extPath($_EXTKEY).'modfunc1/class.tx_indexedsearch_modfunc1.php',
+		'LLL:EXT:indexed_search/locallang.php:mod_indexed_search'
+	);
 	t3lib_extMgm::insertModuleFunction(
 		"web_info",
 		"tx_indexedsearch_modfunc2",
