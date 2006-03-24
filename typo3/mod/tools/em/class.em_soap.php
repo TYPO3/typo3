83f730c5d0307b33d1f4289af720081c2594f962
Karsten Dambekalns <karsten.dambekalns@typo3.org>
2006-03-24 21:50:08 +0000

    Added id keyword for CVS purposes
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/branches/TYPO3_4-0@1373 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/mod/tools/em/class.em_soap.php
+++ typo3/mod/tools/em/class.em_soap.php
@@ -25,6 +25,7 @@
 *
 *  This copyright notice MUST APPEAR in all copies of the script!
 ***************************************************************/
+/* $Id$ */
 
 /**
  * Enter description here...
