43025bdaa21056d7dfcb519f402e1395020b3f0f
Rupert Germann <rupi@gmx.li>
2006-04-14 11:20:35 +0000

    fix for the fix from bug 2868: the adminpanel didn't transfer the page id correctly when realUrl was used
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/branches/TYPO3_4-0@1438 709f56b5-9817-0410-a4d7-c38de5d9e867

--- t3lib/class.t3lib_tsfebeuserauth.php
+++ t3lib/class.t3lib_tsfebeuserauth.php
@@ -186,8 +186,9 @@ class t3lib_tsfeBeUserAuth extends t3lib_beUserAuth {
 	ADMIN PANEL
 -->
 <a name="TSFE_ADMIN"></a>
-<form name="TSFE_ADMIN_PANEL_FORM" action="'.htmlspecialchars(t3lib_div::getIndpEnv('SCRIPT_NAME')).'#TSFE_ADMIN" method="get" style="margin:0;">
-'.$query.
+<form name="TSFE_ADMIN_PANEL_FORM" action="'.htmlspecialchars(t3lib_div::getIndpEnv('SCRIPT_NAME')).'#TSFE_ADMIN" method="get" style="margin:0;">'.
+(!t3lib_div::_GET('id')?'<input type="hidden" name="id" value="'.$GLOBALS['TSFE']->id.'" />
+':'').$query.
 '	<table border="0" cellpadding="0" cellspacing="0" class="typo3-adminPanel" style="background-color:#f6f2e6; border: 1px solid black; z-index:0; position:absolute;">'.$header.$out.'
 	</table>
 </form>';
