be6e7b9596d861b940ab46cc2121e25723ebc305
Ingmar Schlecht <ingmar.schlecht@typo3.org>
2006-08-17 08:42:00 +0000

    Added text to Update Wizard informing about the change of the default values of "config.inlineStyle2TempFile" and "config.removeDefaultJS" depending on compatVersion >= 4.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/branches/TYPO3_4-0@1677 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/sysext/cms/ext_localconf.php
+++ typo3/sysext/cms/ext_localconf.php
@@ -8,12 +8,17 @@ $TYPO3_CONF_VARS['FE']['eID_include']['tx_cms_showpic'] = 'EXT:cms/tslib/showpic
 $TYPO3_CONF_VARS['SC_OPTIONS']['ext/install']['compat_version']['cms'] = array(
 	'title' => 'CMS Frontend',
 	'version' => 4000000,
-	'description' => '<p>Word separator character for simulateStaticDocument is changed from ' .
-					'underscore (_) to hyphen (-) to make URLs more friendly for search engines.' .
+	'description' => '<ul>' .
+					'<li><p>Word separator character for simulateStaticDocument is changed from ' .
+					'underscore (_) to hyphen (-) to make URLs more friendly for search engines' .
 					'URLs that are already existing (e.g. external links to your site) will still work like before.</p>' .
 					'<p>You can set the separator character back to an underscore by putting the following line into the '.
 					'<b>Setup</b> section of your Page TypoScript template:</p>' .
-					'<p style="margin-top: 5px; white-space: nowrap;"><code>config.simulateStaticDocuments_replacementChar = _</code></p>',
+					'<p style="margin-top: 5px; white-space: nowrap;"><code>config.simulateStaticDocuments_replacementChar = _</code></p></li>'.
+					'<li><p>CSS Stylesheets and JavaScript are put into an external file by default.</p>'.
+					'<p>Technically, that means that the default value of "config.inlineStyle2TempFile" is now set to "1" and that of "config.removeDefaultJS" to "external"</p></li>'. 
+					'</ul>',
 );
+
 
 ?>
