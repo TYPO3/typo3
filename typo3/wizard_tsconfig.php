9f89af79f3246789d8dc591796f267168e60ae31
Michael Stucki <michael.stucki@typo3.org>
2006-09-08 16:49:42 +0000

    * Updates to NEWS.txt         * Fixed #4165: Link to TSconfig and TSref not working. Thanks to Andreas Wolf for providing a patch!
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/branches/TYPO3_4-0@1723 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/wizard_tsconfig.php
+++ typo3/wizard_tsconfig.php
@@ -305,13 +305,13 @@ class SC_wizard_tsconfig {
 			// Adding link to TSref:
 		if ($this->mode=='tsref')	{
 			$this->content.=$this->doc->section($LANG->getLL('tsprop_TSref'),'
-			<a href="'.htmlspecialchars('http://typo3.org/documentation/document-library/doc_core_tsref/').'" target="_blank">'.$LANG->getLL('tsprop_TSref',1).'</a>
+			<a href="'.htmlspecialchars('http://typo3.org/documentation/document-library/references/doc_core_tsref/current/view/').'" target="_blank">'.$LANG->getLL('tsprop_TSref',1).'</a>
 			',0,1);
 		}
 			// Adding link to admin guides etc:
 		if ($this->mode=='page' || $this->mode=='beuser')	{
 			$this->content.=$this->doc->section($LANG->getLL('tsprop_tsconfig'),'
-			<a href="'.htmlspecialchars('http://typo3.org/documentation/document-library/doc_core_tsconfig/').'" target="_blank">'.$LANG->getLL('tsprop_tsconfig',1).'</a>
+			<a href="'.htmlspecialchars('http://typo3.org/documentation/document-library/references/doc_core_tsconfig/current/view/').'" target="_blank">'.$LANG->getLL('tsprop_tsconfig',1).'</a>
 			',0,1);
 		}
 	}
