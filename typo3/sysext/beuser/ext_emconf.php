ecd9cab2b319573ba65cbf15c65556a32676a8d4
Michael Stucki <michael.stucki@typo3.org>
2006-11-23 11:35:33 +0000

    * Removed unneccessary preg_quote() call   * Increased version number of beuser and cms system extensions
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@1807 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/sysext/beuser/ext_emconf.php
+++ typo3/sysext/beuser/ext_emconf.php
@@ -35,7 +35,7 @@ $EM_CONF[$_EXTKEY] = Array (
 	'CGLcompliance_note' => '',
 	'private' => 0,
 	'download_password' => '',
-	'version' => '0.1.2',	// Don't modify this! Managed automatically during upload to repository.
+	'version' => '0.1.3',	// Don't modify this! Managed automatically during upload to repository.
 	'_md5_values_when_last_written' => 'a:17:{s:12:"ext_icon.gif";s:4:"2804";s:14:"ext_tables.php";s:4:"c05c";s:14:"mod/beuser.gif";s:4:"2804";s:13:"mod/clear.gif";s:4:"cc11";s:12:"mod/conf.php";s:4:"8ab5";s:13:"mod/index.php";s:4:"4f6a";s:21:"mod/locallang_mod.xml";s:4:"3450";s:15:"mod/CVS/Entries";s:4:"dc4c";s:18:"mod/CVS/Repository";s:4:"fdea";s:12:"mod/CVS/Root";s:4:"0ea2";s:12:"doc/TODO.txt";s:4:"93f7";s:15:"doc/CVS/Entries";s:4:"4f35";s:18:"doc/CVS/Repository";s:4:"7ada";s:12:"doc/CVS/Root";s:4:"0ea2";s:11:"CVS/Entries";s:4:"becc";s:14:"CVS/Repository";s:4:"bfc3";s:8:"CVS/Root";s:4:"0ea2";}',
 );
 
