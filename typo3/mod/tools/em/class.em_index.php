d4dfd1655d7cf20d68423e0997cad7e0dc7dab45
Dmitry Dulepov <dmitry.dulepov@gmail.com>
2007-01-24 14:16:01 +0000

    Fixed bug 4846: EM: inconsistent line ending
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/branches/TYPO3_4-0@1937 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/mod/tools/em/class.em_index.php
+++ typo3/mod/tools/em/class.em_index.php
@@ -3791,9 +3791,9 @@ EXTENSION KEYS:
 		$extInfo['EM_CONF']['_md5_values_when_last_written'] = serialize($this->serverExtensionMD5Array($extKey,$extInfo));
 		$emConfFileContent = $this->construct_ext_emconf_file($extKey,$extInfo['EM_CONF']);
 
+		$absPath = $this->getExtPath($extKey,$extInfo['type']);
+		$emConfFileName = $absPath.'ext_emconf.php';
 		if($emConfFileContent)	{
-			$absPath = $this->getExtPath($extKey,$extInfo['type']);
-			$emConfFileName = $absPath.'ext_emconf.php';
 
 			if(@is_file($emConfFileName))	{
 				if(t3lib_div::writeFile($emConfFileName,$emConfFileContent) === true) {
@@ -3835,7 +3835,7 @@ EXTENSION KEYS:
 		$vDat = $this->renderVersion($EM_CONF['version']);
 		$EM_CONF['version']=$vDat['version'];
 
-		return '<?php
+		$code = '<?php
 
 ########################################################################
 # Extension Manager/Repository config file for ext: "'.$extKey.'"
@@ -3850,6 +3850,7 @@ EXTENSION KEYS:
 $EM_CONF[$_EXTKEY] = '.$this->arrayToCode($EM_CONF, 0).';
 
 ?>';
+		return str_replace(chr(13), '', $code);
 	}
 
 	/**
