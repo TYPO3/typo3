dda2cf1819e953c5179bf6fb7da205d4157f7186
Dmitry Dulepov <dmitry.dulepov@gmail.com>
2007-05-04 09:46:41 +0000

    Updated compatibility version for t3skin
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@2325 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/sysext/t3skin/ext_emconf.php
+++ typo3/sysext/t3skin/ext_emconf.php
@@ -37,7 +37,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'php' => '4.3.0-5.9.0',
-			'typo3' => '3.9.0-4.1.999',
+			'typo3' => '3.9.0-4.2',
 		),
 		'conflicts' => array(
 		),
