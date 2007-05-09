68409cdcca019558dfff61db914bf38b5e0eeb07
Thorsten Kahler <thorsten.kahler@dkd.de>
2007-05-09 23:00:36 +0000

    * fixed bug #209: require field URL in page-type "External URL"
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/branches/TYPO3_4-0@2333 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/sysext/cms/ext_tables.php
+++ typo3/sysext/cms/ext_tables.php
@@ -218,7 +218,7 @@ if (TYPO3_MODE=='BE')	{
 				'type' => 'input',
 				'size' => '25',
 				'max' => '256',
-				'eval' => 'trim',
+				'eval' => 'trim,required',
 				'softref' => 'url'
 			)
 		),
@@ -300,7 +300,7 @@ if (TYPO3_MODE=='BE')	{
 			'config' => Array (
 				'type' => 'group',
 				'internal_type' => 'db',
-					'allowed' => 'pages',
+				'allowed' => 'pages',
 				'size' => '3',
 				'maxitems' => '1',
 				'minitems' => '0',
