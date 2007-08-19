e98f911e6be39d24b99bdba9623496ab956e5f99
Oliver Hader <oliver.hader@typo3.org>
2007-08-19 12:52:24 +0000

    (trivial) Fixed bug #6170: CSC - File locallang.php used in static template instead of locallang.xml
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@2451 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/sysext/css_styled_content/ext_emconf.php
+++ typo3/sysext/css_styled_content/ext_emconf.php
@@ -2,14 +2,15 @@
 
 ########################################################################
 # Extension Manager/Repository config file for ext: "css_styled_content"
-# 
-# Auto generated 17-11-2004 17:47
-# 
+#
+# Auto generated 19-08-2007 14:39
+#
 # Manual updates:
-# Only the data in the array - anything else is removed by next write
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
 ########################################################################
 
-$EM_CONF[$_EXTKEY] = Array (
+$EM_CONF[$_EXTKEY] = array(
 	'title' => 'CSS styled content',
 	'description' => 'Contains configuration for CSS content-rendering of the table "tt_content". This is meant as a modern substitute for the classic "content (default)" template which was based more on <font>-tags, while this is pure CSS. It is intended to work with all modern browsers (which excludes the NS4 series).',
 	'category' => 'fe',
@@ -18,8 +19,6 @@ $EM_CONF[$_EXTKEY] = Array (
 	'conflicts' => '',
 	'priority' => 'top',
 	'loadOrder' => '',
-	'TYPO3_version' => '3.7.0-',
-	'PHP_version' => '0.0.1-0.0.1',
 	'module' => '',
 	'state' => 'beta',
 	'internal' => 1,
@@ -33,10 +32,19 @@ $EM_CONF[$_EXTKEY] = Array (
 	'author_company' => 'Curby Soft Multimedia',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
-	'private' => 0,
-	'download_password' => '',
-	'version' => '0.3.1',	// Don't modify this! Managed automatically during upload to repository.
-	'_md5_values_when_last_written' => 'a:36:{s:21:"ext_conf_template.txt";s:4:"44ff";s:12:"ext_icon.gif";s:4:"1845";s:17:"ext_localconf.php";s:4:"b9d5";s:15:"ext_php_api.dat";s:4:"f62a";s:14:"ext_tables.php";s:4:"c1f8";s:16:"pageTSconfig.txt";s:4:"a3eb";s:11:"CVS/Entries";s:4:"4fb6";s:14:"CVS/Repository";s:4:"96d6";s:8:"CVS/Root";s:4:"0ea2";s:12:"doc/TODO.txt";s:4:"6534";s:14:"doc/manual.sxw";s:4:"85e4";s:15:"doc/CVS/Entries";s:4:"0a8a";s:18:"doc/CVS/Repository";s:4:"e39c";s:12:"doc/CVS/Root";s:4:"0ea2";s:37:"pi1/class.tx_cssstyledcontent_pi1.php";s:4:"8ab0";s:17:"pi1/locallang.php";s:4:"79d0";s:15:"pi1/CVS/Entries";s:4:"43e7";s:18:"pi1/CVS/Repository";s:4:"07e9";s:12:"pi1/CVS/Root";s:4:"0ea2";s:20:"static/constants.txt";s:4:"ff7a";s:20:"static/editorcfg.txt";s:4:"8d1c";s:16:"static/setup.txt";s:4:"49b4";s:18:"static/CVS/Entries";s:4:"2f1f";s:21:"static/CVS/Repository";s:4:"64f2";s:15:"static/CVS/Root";s:4:"0ea2";s:15:"css/example.css";s:4:"9804";s:24:"css/example_outlines.css";s:4:"e656";s:14:"css/readme.txt";s:4:"af18";s:15:"css/CVS/Entries";s:4:"bc3d";s:18:"css/CVS/Repository";s:4:"1c6e";s:12:"css/CVS/Root";s:4:"0ea2";s:31:"css/img/background_gradient.gif";s:4:"45d7";s:28:"css/img/red_arrow_bullet.gif";s:4:"82d6";s:19:"css/img/CVS/Entries";s:4:"a4f6";s:22:"css/img/CVS/Repository";s:4:"3aff";s:16:"css/img/CVS/Root";s:4:"0ea2";}',
+	'version' => '0.3.2',
+	'_md5_values_when_last_written' => 'a:19:{s:21:"ext_conf_template.txt";s:4:"6bbb";s:12:"ext_icon.gif";s:4:"1845";s:17:"ext_localconf.php";s:4:"37eb";s:14:"ext_tables.php";s:4:"6ffa";s:15:"flexform_ds.xml";s:4:"6f8f";s:16:"locallang_db.xml";s:4:"0284";s:16:"pageTSconfig.txt";s:4:"5cbc";s:12:"doc/TODO.txt";s:4:"6534";s:14:"doc/manual.sxw";s:4:"0179";s:20:"static/constants.txt";s:4:"ad6c";s:20:"static/editorcfg.txt";s:4:"a3c7";s:16:"static/setup.txt";s:4:"3506";s:15:"css/example.css";s:4:"9804";s:24:"css/example_outlines.css";s:4:"e656";s:14:"css/readme.txt";s:4:"ee9d";s:31:"css/img/background_gradient.gif";s:4:"45d7";s:28:"css/img/red_arrow_bullet.gif";s:4:"82d6";s:37:"pi1/class.tx_cssstyledcontent_pi1.php";s:4:"3ce2";s:17:"pi1/locallang.xml";s:4:"974c";}',
+	'constraints' => array(
+		'depends' => array(
+			'cms' => '',
+			'php' => '4.3.0-0.0.0',
+			'typo3' => '4.1.0-0.0.0',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
 );
 
 ?>
