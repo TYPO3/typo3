61e87e9a1146c16b85803b3af1958aafd4d4dd8a
Stanislas Rolland <typo3@sjbr.ca>
2007-11-01 16:19:20 +0000

    * Fixed bug 6476: htmlArea RTE incorrectly nests tags on return key in FF
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@2649 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/sysext/rtehtmlarea/htmlarea/htmlarea.js
+++ typo3/sysext/rtehtmlarea/htmlarea/htmlarea.js
@@ -1410,9 +1410,13 @@ HTMLArea.prototype.popupURL = function(file) {
  ***************************************************/
 HTMLArea.getInnerText = function(el) {
 	var txt = '', i;
-	for(i=el.firstChild;i;i =i.nextSibling) {
-		if(i.nodeType == 3) txt += i.data;
-		else if(i.nodeType == 1) txt += HTMLArea.getInnerText(i);
+	if(el.firstChild) {
+		for(i=el.firstChild;i;i =i.nextSibling) {
+			if(i.nodeType == 3) txt += i.data;
+			else if(i.nodeType == 1) txt += HTMLArea.getInnerText(i);
+		}
+	} else {
+		if(el.nodeType == 3) txt = el.data;
 	}
 	return txt;
 };
@@ -2331,9 +2335,10 @@ HTMLArea._editorEvent = function(ev) {
 			switch (ev.keyCode) {
 				case 13	: // KEY enter
 					if (HTMLArea.is_gecko && !ev.shiftKey && !editor.config.disableEnterParagraphs) {
-						editor._checkInsertP();
-						HTMLArea._stopEvent(ev);
-						editor.updateToolbar();
+						if (editor._checkInsertP(ev)) HTMLArea._stopEvent(ev);
+							// update the toolbar state after some time
+						if (editor._timerToolbar) window.clearTimeout(editor._timerToolbar);
+						editor._timerToolbar = window.setTimeout("HTMLArea.updateToolbar(" + editor._editorNumber + ");", 50);
 					}
 					break;
 				case 8	: // KEY backspace
