bbd70a392c1215c997e6b550b0be9860bfa3600e
Ingmar Schlecht <ingmar.schlecht@typo3.org>
2007-11-14 09:46:22 +0000

    Cleanup-up to prior commit #529: "TypoScript help" module
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@2699 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/sysext/tsconfig_help/ext_tables.php
+++ typo3/sysext/tsconfig_help/ext_tables.php
@@ -6,11 +6,3 @@ if (TYPO3_MODE == 'BE')	{
 	t3lib_extMgm::addModule('help','txtsconfighelpM1','',t3lib_extMgm::extPath($_EXTKEY).'mod1/');
 }
 ?>
-<?php
-if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
-
-if (TYPO3_MODE == 'BE')	{
-		
-	t3lib_extMgm::addModule('help','txtsconfighelpM1','',t3lib_extMgm::extPath($_EXTKEY).'mod1/');
-}
-?>
