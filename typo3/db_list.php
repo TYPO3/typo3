a02b64a84223bb61abd98d2252104f3149a0cd26
Dmitry Dulepov <dmitry.dulepov@gmail.com>
2007-12-06 13:11:54 +0000

    Fixed bug #6703: $TCA[table]['ctrl']['hideTable'] not implemented
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@2803 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/db_list.php
+++ typo3/db_list.php
@@ -222,6 +222,7 @@ class SC_db_list {
 		$dblist->disableSingleTableView = $this->modTSconfig['properties']['disableSingleTableView'];
 		$dblist->listOnlyInSingleTableMode = $this->modTSconfig['properties']['listOnlyInSingleTableView'];
 		$dblist->hideTables = $this->modTSconfig['properties']['hideTables'];
+		$dblist->tableTSconfigOverTCA = $this->modTSconfig['properties']['table.'];
 		$dblist->clickTitleMode = $this->modTSconfig['properties']['clickTitleMode'];
 		$dblist->alternateBgColors=$this->modTSconfig['properties']['alternateBgColors']?1:0;
 		$dblist->allowedNewTables = t3lib_div::trimExplode(',',$this->modTSconfig['properties']['allowedNewTables'],1);
