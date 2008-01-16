7caa9e94a848cf4db134235ffec308d377b913aa
Oliver Hader <oliver.hader@typo3.org>
2008-01-16 15:16:46 +0000

    Fixed bug: Missing try/catch for actionChecks in TBE_EDITOR
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@2917 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/jsfunc.tbe_editor.js
+++ typo3/jsfunc.tbe_editor.js
@@ -1,7 +1,7 @@
 /***************************************************************
 *  Copyright notice
 *
-*  (c) 1999-2006 Kasper Skaarhoj (kasperYYYY@typo3.com)
+*  (c) 1999-2008 Kasper Skaarhoj (kasperYYYY@typo3.com)
 *  All rights reserved
 *
 *  This script is part of the TYPO3 project. The TYPO3 project is
@@ -404,7 +404,9 @@ var TBE_EDITOR = {
 		// $this->additionalJS_submit:
 		if (TBE_EDITOR.actionChecks && TBE_EDITOR.actionChecks.submit) {
 			for (funcIndex=0, funcMax=TBE_EDITOR.actionChecks.submit.length; funcIndex<funcMax; funcIndex++) {
-				eval(TBE_EDITOR.actionChecks.submit[funcIndex]);
+				try {
+					eval(TBE_EDITOR.actionChecks.submit[funcIndex]);
+				} catch(error) {}
 			}
 		}
 
