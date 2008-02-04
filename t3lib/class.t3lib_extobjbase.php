bd6435f3d87b77183e135e4e9da1f7fbd88555a1
Oliver Hader <oliver.hader@typo3.org>
2008-02-04 22:04:54 +0000

    Followup to #7350: Fixed type to match with t3lib_div::array_merge_recursive_overrule()
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@3063 709f56b5-9817-0410-a4d7-c38de5d9e867

--- t3lib/class.t3lib_extobjbase.php
+++ t3lib/class.t3lib_extobjbase.php
@@ -242,7 +242,9 @@ class t3lib_extobjbase {
 		#	include($this->thisPath.'/'.$this->localLangFile);
 		if ($this->localLangFile && (@is_file($this->thisPath.'/'.$this->localLangFile) || @is_file($this->thisPath.'/'.substr($this->localLangFile,0,-4).'.xml')))	{
 			$LOCAL_LANG = $LANG->includeLLFile($this->thisPath.'/'.$this->localLangFile, FALSE);
-			if (is_array($LOCAL_LANG))	$GLOBALS['LOCAL_LANG'] = t3lib_div::array_merge_recursive_overrule($GLOBALS['LOCAL_LANG'],$LOCAL_LANG);
+			if (is_array($LOCAL_LANG)) {
+				$GLOBALS['LOCAL_LANG'] = t3lib_div::array_merge_recursive_overrule((array)$GLOBALS['LOCAL_LANG'], $LOCAL_LANG);
+			}
 		}
 	}
 
