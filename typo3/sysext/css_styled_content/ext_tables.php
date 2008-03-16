842b080bdaa2b550e02d6dea5c7e9747cb4d374e
Ingo Renner <ingo.renner@typo3.org>
2008-03-16 16:23:51 +0000

    Code cleanup: replaced CVS with SVN
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@3438 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/sysext/css_styled_content/ext_tables.php
+++ typo3/sysext/css_styled_content/ext_tables.php
@@ -1,12 +1,12 @@
 <?php
-# TYPO3 CVS ID: $Id$
+# TYPO3 SVN ID: $Id$
 if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 
 	// add flexform
 t3lib_extMgm::addPiFlexFormValue('*', 'FILE:EXT:css_styled_content/flexform_ds.xml','table');
-$TCA['tt_content']['types']['table']['showitem']='CType;;4;button,hidden,1-1-1, header;;3;;2-2-2, linkToTop;;;;4-4-4, 
+$TCA['tt_content']['types']['table']['showitem']='CType;;4;button,hidden,1-1-1, header;;3;;2-2-2, linkToTop;;;;4-4-4,
 			--div--;LLL:EXT:cms/locallang_ttc.xml:CType.I.5, layout;;10;button;3-3-3, cols, bodytext;;9;nowrap:wizards[table], text_properties, pi_flexform,
 			--div--;LLL:EXT:cms/locallang_tca.xml:pages.tabs.access,starttime, endtime';
-													
+
 t3lib_extMgm::addStaticFile($_EXTKEY,'static/','CSS Styled Content');
 ?>
