f018b4a1b41d7d5788e78c52e1638423b8eb8299
Oliver Hader <oliver.hader@typo3.org>
2008-05-24 14:13:17 +0000

    Set TYPO3 version to 4.3-dev and cleaned up NEWS.txt
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@3714 709f56b5-9817-0410-a4d7-c38de5d9e867

--- t3lib/config_default.php
+++ t3lib/config_default.php
@@ -285,9 +285,9 @@ $TYPO3_CONF_VARS = Array(
 $T3_VAR = array();	// Initialize.
 
 	// TYPO3 version
-$TYPO_VERSION = '4.2.1-dev';	// deprecated: use the constants defined below
+$TYPO_VERSION = '4.3-dev';	// deprecated: use the constants defined below
 define('TYPO3_version', $TYPO_VERSION);
-define('TYPO3_branch', '4.2');
+define('TYPO3_branch', '4.3');
 define('TYPO3_copyright_year', '1998-2008');
 define('TYPO3_user_agent', 'User-Agent: TYPO3/'.TYPO3_version);
 
