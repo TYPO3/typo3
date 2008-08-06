9e176ff1982260581de259a40d22c6431c7ad372
Ingo Renner <ingo.renner@typo3.org>
2008-08-06 18:42:08 +0000

    * Fixed bug: PHP 5.3 throws a deprecation warning in typo3/mod/tools/em/class.em_soap.php on lines 108, 112, 115, 213, 218: Assigning the return value of new by reference is deprecated * Fixed bug: PHP 5.3 throws a warning in class.t3lib_div.php on line 3286: strcmp() expects parameter 1 to be string, array given
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/branches/TYPO3_4-2@3946 709f56b5-9817-0410-a4d7-c38de5d9e867

--- t3lib/class.t3lib_div.php
+++ t3lib/class.t3lib_div.php
@@ -3283,7 +3283,7 @@ final class t3lib_div {
 		$params = t3lib_div::_GET();
 
 		foreach($getParams as $k => $v)	{
-			if (strcmp($v,''))	{
+			if (!is_array($v) && strcmp($v,''))	{
 				$params[$k]=$v;
 			} else unset($params[$k]);
 		}
