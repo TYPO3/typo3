bed2f2e22c681b93e606d14725d52a4afe2cf3ee
Stanislas Rolland <typo3@sjbr.ca>
2008-09-29 16:30:12 +0000

    Fixed bug 9449: htmlArea RTE: Opera sometimes raises a JavaScript error when the RTE closes
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@4262 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/sysext/rtehtmlarea/htmlarea/htmlarea.js
+++ typo3/sysext/rtehtmlarea/htmlarea/htmlarea.js
@@ -3588,7 +3588,10 @@ HTMLArea.Dialog = HTMLArea.Base.extend({
 				}
 				this.dialogWindow.close();
 			}
-			this.plugin.editor.updateToolbar();
+				// Opera unload event may be triggered after the editor iframe is gone
+			if (this.plugin.editor._iframe) {
+				this.plugin.editor.updateToolbar();
+			}
 		}
 		return false;
 	},
@@ -3690,7 +3693,7 @@ HTMLArea.Dialog = HTMLArea.Base.extend({
 				}
 			} catch(e) { }
 		}
-		if (HTMLArea.is_gecko && this.plugin.editor._iframe.contentWindow) {
+		if ((HTMLArea.is_gecko && !HTMLArea.is_opera) && this.plugin.editor._iframe.contentWindow) {
 			HTMLArea._removeEvent(this.plugin.editor._iframe.contentWindow, "unload", this.unloadFunctionReference);
 		}
 	},
