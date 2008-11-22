fcd5a9757e8709a315ddf861241b958234ea6de5
Steffen Kamper <info@sk-typo3.de>
2008-11-22 14:05:11 +0000

    felogin: completed ChangeLog, raised ext-version to 1.1.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@4479 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/sysext/felogin/ext_emconf.php
+++ typo3/sysext/felogin/ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "felogin"
 #
-# Auto generated 23-04-2008 10:23
+# Auto generated 22-11-2008 15:02
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '1.0.0',
+	'version' => '1.1.0',
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.1.0-0.0.0',
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:12:{s:9:"ChangeLog";s:4:"e27a";s:12:"ext_icon.gif";s:4:"7160";s:17:"ext_localconf.php";s:4:"1dbd";s:14:"ext_tables.php";s:4:"0d82";s:14:"ext_tables.sql";s:4:"640e";s:24:"ext_typoscript_setup.txt";s:4:"9f50";s:12:"flexform.xml";s:4:"8f57";s:16:"locallang_db.xml";s:4:"a53e";s:13:"template.html";s:4:"1452";s:28:"pi1/class.tx_felogin_pi1.php";s:4:"e663";s:17:"pi1/locallang.xml";s:4:"6d7d";s:14:"doc/manual.sxw";s:4:"44e7";}',
+	'_md5_values_when_last_written' => 'a:12:{s:9:"ChangeLog";s:4:"4aa0";s:12:"ext_icon.gif";s:4:"7160";s:17:"ext_localconf.php";s:4:"4e6e";s:14:"ext_tables.php";s:4:"ca93";s:14:"ext_tables.sql";s:4:"640e";s:24:"ext_typoscript_setup.txt";s:4:"ba3c";s:12:"flexform.xml";s:4:"8f57";s:16:"locallang_db.xml";s:4:"a53e";s:13:"template.html";s:4:"1452";s:14:"doc/manual.sxw";s:4:"44e7";s:28:"pi1/class.tx_felogin_pi1.php";s:4:"b499";s:17:"pi1/locallang.xml";s:4:"6d7d";}',
 	'suggests' => array(
 	),
 );
