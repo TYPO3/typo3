ce14120e0cc9504f4118da1375af505c2a8444ad
Dmitry Dulepov <dmitry.dulepov@gmail.com>
2008-12-01 20:23:54 +0000

    Fixed bug #9860: Fileadmin: New folders: Singular and plural are the same ("folders")
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@4519 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/file_newfolder.php
+++ typo3/file_newfolder.php
@@ -202,10 +202,14 @@ class SC_file_newfolder {
 		$this->number = t3lib_div::intInRange($this->number,1,10);
 		$code .= '
 			<div id="c-select">
+				<label for="number">' .
+				$LANG->sL('LLL:EXT:lang/locallang_core.php:file_newfolder.php.number_of_folders') .
+				'</label>
 				<select name="number" onchange="reload(this.options[this.selectedIndex].value);">';
 		for ($a=1;$a<=$this->folderNumber;$a++)	{
-			$code.='
-					<option value="'.$a.'"'.($this->number==$a?' selected="selected"':'').'>'.$a.' '.$LANG->sL('LLL:EXT:lang/locallang_core.php:file_newfolder.php.folders',1).'</option>';
+			$code .= '<option value="' . $a . '"' .
+					($this->number == $a ? ' selected="selected"' : '') .
+					'>' . $a . '</option>';
 		}
 		$code.='
 				</select>
