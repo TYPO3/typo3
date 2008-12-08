039cd822757a0a0d4faef60ccaf64c5b29a6251c
Steffen Kamper <info@sk-typo3.de>
2008-12-08 21:22:24 +0000

    Fixed bug #9864: 9890: sectionIndex in Menu doesn't work
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@4535 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/sysext/cms/tslib/class.tslib_menu.php
+++ typo3/sysext/cms/tslib/class.tslib_menu.php
@@ -1587,6 +1587,10 @@ class tslib_menu {
 		if ($oTarget) {
 			$conf['target'] = $oTarget;
 		}
+		if ($page['sectionIndex_uid']) {
+			$conf['section'] = $page['sectionIndex_uid'];
+		}
+
 		$this->parent_cObj->typoLink('|', $conf);
 		$LD = $this->parent_cObj->lastTypoLinkLD;
 		$LD['totalURL'] = $this->parent_cObj->lastTypoLinkUrl;
