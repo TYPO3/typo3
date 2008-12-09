d50d08d44c09f9d4bca9b7c1f6922440f6190ad8
Steffen Kamper <info@sk-typo3.de>
2008-12-09 12:44:33 +0000

    Fixed Typo in tslib_content ($gifCreateor => $gifCreator) 2nd place
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@4542 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/sysext/cms/tslib/class.tslib_content.php
+++ typo3/sysext/cms/tslib/class.tslib_content.php
@@ -4876,7 +4876,7 @@ class tslib_cObj {
 								$GLOBALS['TSFE']->tmpl->fileCache[$hash]= $gifCreator->getImageDimensions($dest);
 							} else {		// Normal situation:
 								$GLOBALS['TSFE']->tmpl->fileCache[$hash]= $gifCreator->imageMagickConvert($theImage,$fileArray['ext'],$fileArray['width'],$fileArray['height'],$fileArray['params'],$fileArray['frame'],$options);
-								if (($fileArray['reduceColors'] || ($imgExt=='png' && !$gifCreateor->png_truecolor)) && is_file($GLOBALS['TSFE']->tmpl->fileCache[$hash][3]))	{
+								if (($fileArray['reduceColors'] || ($imgExt=='png' && !$gifCreator->png_truecolor)) && is_file($GLOBALS['TSFE']->tmpl->fileCache[$hash][3]))	{
 									$reduced = $gifCreator->IMreduceColors($GLOBALS['TSFE']->tmpl->fileCache[$hash][3], t3lib_div::intInRange($fileArray['reduceColors'], 256, $gifCreator->truecolorColors, 256));
 									if (is_file($reduced))	{
 										unlink($GLOBALS['TSFE']->tmpl->fileCache[$hash][3]);
