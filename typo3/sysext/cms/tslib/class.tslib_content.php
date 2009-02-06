79cd8552111127e6e3eb6c34adc58833e7cfc1b9
Ingo Renner <ingo.renner@typo3.org>
2009-02-06 10:18:48 +0000

    Fixed bug #10357: Add gp as an alternative to gpvar in TS getText for a more consistent naming scheme
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@4959 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/sysext/cms/tslib/class.tslib_content.php
+++ typo3/sysext/cms/tslib/class.tslib_content.php
@@ -5012,6 +5012,7 @@ class tslib_cObj {
 			$key = trim($parts[1]);
 			if ((string)$key!='')	{
 				switch(strtolower(trim($parts[0])))	{
+					case 'gp':
 					case 'gpvar':
 						list($firstKey, $rest) = explode('|', $key, 2);
 						if (strlen(trim($firstKey)))	{
@@ -5023,6 +5024,7 @@ class tslib_cObj {
 								// Check that output is not an array:
 							if (is_array($retVal))	$retVal = '';
 						}
+					t3lib_div::deprecationLog('Using gpvar in TypoScript getText is deprecated since TYPO3 4.3 - Use gp instead of gpvar.');
 					break;
 					case 'tsfe':
 						$retVal = $this->getGlobal ('TSFE|'.$key);
