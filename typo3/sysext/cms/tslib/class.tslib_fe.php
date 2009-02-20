05569bbf5185e46757aa0c814656b5e48a60ee41
Oliver Hader <oliver.hader@typo3.org>
2009-02-20 20:50:20 +0000

    Follow-up to bug #10451: Fixed flaws in PATH_INFO check for simulateStaticDocuments
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/branches/TYPO3_4-2@5043 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/sysext/cms/tslib/class.tslib_fe.php
+++ typo3/sysext/cms/tslib/class.tslib_fe.php
@@ -710,7 +710,7 @@ require_once (PATH_t3lib.'class.t3lib_lock.php');
 		}
 
 			// If PATH_INFO is defined as simulateStaticDocuments mode and has information:
-		if ($this->config['config']['simulateStaticDocuments'] === 'PATH_INFO' && t3lib_div::getIndpEnv('PATH_INFO')) {
+		if (t3lib_div::getIndpEnv('PATH_INFO') && strpos(t3lib_div::getIndpEnv('TYPO3_SITE_SCRIPT'), 'index.php/') === 0) {
 			$parts=t3lib_div::trimExplode('/',t3lib_div::getIndpEnv('PATH_INFO'),1);
 			$parts[]='html';
 			$pCount = count($parts);
