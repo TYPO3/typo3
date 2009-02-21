ec269f1c33f219b5fdb348f0cda92fe9b478e883
Ingo Renner <ingo.renner@typo3.org>
2009-02-21 15:47:08 +0000

    added unit tests for t3lib_div::trimExplode()
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@5047 709f56b5-9817-0410-a4d7-c38de5d9e867

---
+++ tests/t3lib/t3lib_div_testcase.php
@@ -0,0 +1,91 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2009 Ingo Renner <ingo@typo3.org>
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+
+/**
+ * Testcase for class t3lib_div
+ *
+ * @author	Ingo Renner <ingo@typo3.org>
+ * @package TYPO3
+ * @subpackage t3lib
+ */
+class t3lib_div_testcase extends tx_phpunit_testcase {
+
+	/**
+	 * @test
+	 */
+	public function checkTrimExplodeTrimsSpacesAtElementStartAndEnd() {
+		$testString = ' a , b , c ,d ,,  e,f,';
+		$expectedArray = array('a', 'b', 'c', 'd', '', 'e', 'f', '');
+		$actualArray = t3lib_div::trimExplode(',', $testString);
+
+		$this->assertEquals($expectedArray, $actualArray);
+	}
+
+	/**
+	 * @test
+	 */
+	public function checkTrimExplodeRemovesNewLines() {
+		$testString = ' a , b , ' . chr(10) . ' ,d ,,  e,f,';
+		$expectedArray = array('a', 'b', 'd', 'e', 'f');
+		$actualArray = t3lib_div::trimExplode(',', $testString, true);
+
+		$this->assertEquals($expectedArray, $actualArray);
+	}
+
+	/**
+	 * @test
+	 */
+	public function checkTrimExplodeRemovesEmptyElements() {
+		$testString = 'a , b , c , ,d ,, ,e,f,';
+		$expectedArray = array('a', 'b', 'c', 'd', 'e', 'f');
+		$actualArray = t3lib_div::trimExplode(',', $testString, true);
+
+		$this->assertEquals($expectedArray, $actualArray);
+	}
+
+	/**
+	 * @test
+	 */
+	public function checkTrimExplodeLimitsResults() {
+		$testString = ' a , b , c , , d,, ,e ';
+		$expectedArray = array('a', 'b', 'c , , d,, ,e'); // limiting returns the rest of the string as the last element
+		$actualArray = t3lib_div::trimExplode(',', $testString, false, 3);
+
+		$this->assertEquals($expectedArray, $actualArray);
+	}
+
+	/**
+	 * @test
+	 */
+	public function checkTrimExplodeKeepsZeroAsString() {
+		$testString = 'a , b , c , ,d ,, ,e,f, 0 ,';
+		$expectedArray = array('a', 'b', 'c', 'd', 'e', 'f', '0');
+		$actualArray = t3lib_div::trimExplode(',', $testString, true);
+
+		$this->assertEquals($expectedArray, $actualArray);
+	}
+}
+
+?>
