da7eff973f1e2b165c12444002fa5adb187adff0
Oliver Hader <oliver.hader@typo3.org>
2009-04-09 07:56:50 +0000

    Fixed bug #8882: drag & drop of section elements and IRRE records does not work if frame has to be scrolled
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@5303 709f56b5-9817-0410-a4d7-c38de5d9e867

--- t3lib/class.t3lib_tceforms.php
+++ t3lib/class.t3lib_tceforms.php
@@ -5200,6 +5200,7 @@ class t3lib_TCEforms	{
 			    }
 			}
 			function flexFormSortable(id)	{	// Create sortables for flexform sections
+				Position.includeScrollOffsets = true;
  				Sortable.create(id, {tag:\'div\',constraint: false, onChange:function(){
 					setActionStatus(id);
 				} });
