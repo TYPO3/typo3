8209a6d5cdffac480374800650f321dffd92439e
Tobias Liebig <mail@etobi.de>
2009-04-15 20:00:17 +0000

    Fixed issue #10899: bugfixes for t3editor codecompletion (credits to Stephan Petzl and Christian Kartnig) * Adjustments in the descriptionbox
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@5318 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/sysext/t3editor/jslib/ts_codecompletion/descriptionPlugin.js
+++ typo3/sysext/t3editor/jslib/ts_codecompletion/descriptionPlugin.js
@@ -68,7 +68,8 @@ var DescriptionPlugin = function() {
 
 		if (type && type.properties[proposalObj.word]) {
 			// first a container has to be built
-			descriptionBox.innerHTML = '<div class="TSREF_type_label">TSREF-type: </div><div class="TSREF_type">'+type.typeId+'->'+proposalObj.type+'</div><br/>';
+			descriptionBox.innerHTML  = '<div class="TSREF_type_label">Object-type: </div><div class="TSREF_type">'+type.typeId+'</div>';
+			descriptionBox.innerHTML += '<div class="TSREF_type_label">Property-type: </div><div class="TSREF_type">'+type.properties[proposalObj.word].value+'</div><br/>';
 			descriptionBox.innerHTML += '<div class="TSREF_description_label">TSREF-description:</div><div id="TSREF_description"><img src="../../../gfx/spinner.gif" border="0" alt="one moment please..."/></div>';
 			var prop = type.properties[proposalObj.word];
 			// if there is another request for a description in the queue -> cancel it
