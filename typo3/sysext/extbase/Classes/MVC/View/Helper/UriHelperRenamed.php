5d0ab8ad2e32f626458a79ebce7c9877ec943f72
Jochen Rau <j.rau@web.de>
2009-04-30 14:28:54 +0000

    Extbase: * Rename URIHelper to UriHelper (1st step)

--- typo3/sysext/extbase/Classes/MVC/View/Helper/UriHelperRenamed.php
+++ typo3/sysext/extbase/Classes/MVC/View/Helper/UriHelperRenamed.php
@@ -1,5 +1,4 @@
 <?php
-
 /*                                                                        *
  * This script is part of the TYPO3 project - inspiring people to share!  *
  *                                                                        *
@@ -21,7 +20,8 @@ require_once(PATH_tslib . 'class.tslib_content.php');
  * @subpackage
  * @version $Id:$
  */
-class Tx_Extbase_MVC_View_Helper_URIHelper implements t3lib_Singleton {
+// TODO Rename file name URIHelper to UriHelper
+class Tx_Extbase_MVC_View_Helper_UriHelper extends Tx_Extbase_MVC_View_Helper_AbstractHelper implements t3lib_Singleton {
 
 	/**
 	 * An instance of tslib_cObj
@@ -30,40 +30,49 @@ class Tx_Extbase_MVC_View_Helper_URIHelper implements t3lib_Singleton {
 	 */
 	protected $contentObject;
 
+	/**
+	 * Constructs this URI Helper
+	 */
 	public function __construct(array $arguments = array()) {
 		$this->contentObject = t3lib_div::makeInstance('tslib_cObj');
 	}
 
 	/**
-	 * Get an URI for a controller / action / extension on a specific page within the scope
-	 * of the current request.
-	 * @return The URI
+	 * Creates an URI by making use of the typolink mechanism.
+	 *
+	 * @param string $actionName Name of the action to be called
+	 * @param array $arguments Additional arguments
+	 * @param string $controllerName Name of the target controller. If not set, current controller is used
+	 * @param string $pageUid 
+	 * @param array $options Further options
+	 * @return string the HTML code for the generated link
+	 * @author Bastian Waidelich <bastian@typo3.org>
 	 */
-	public function URIFor($request, $actionName = '', $arguments = array(), $controllerName = '', $page = '', $extensionKey = '', $anchor = '', $useCacheHash = TRUE) {
-		$prefixedExtensionKey = 'tx_' . strtolower($request->getExtensionName()) . '_' . strtolower($request->getPluginKey());
+	public function uriFor($actionName, $arguments = array(), $controllerName = '', $parameter = 0, $anchor = '', $useCacheHash = TRUE) {
+		$prefixedExtensionKey = 'tx_' . strtolower($this->request->getControllerExtensionKey()) . '_' . strtolower($this->request->getPluginKey());
 
 		$arguments['action'] = $actionName;
-		$arguments['controller'] = ($controllerName !== '') ? $controllerName : $request->getControllerName();
+		$arguments['controller'] = ($controllerName !== '') ? $controllerName : $this->request->getControllerName();
 		$prefixedArguments = array();
 		foreach ($arguments as $argumentName => $argumentValue) {
 			$key = $prefixedExtensionKey . '[' . $argumentName . ']';
 			$prefixedArguments[$key] = $argumentValue;
 		}
 
-		return $this->typolinkURI($page, $anchor, $useCacheHash, $prefixedArguments);
+		return $this->typolinkUri($pageUid, $anchor, $useCacheHash, $prefixedArguments);
 	}
 
 	/**
 	 * Get an URI from typolink
 	 * @return The URI
 	 */
-	public function typolinkURI($page = '', $anchor = '', $useCacheHash = TRUE, $arguments = array()) {
-		if ($page === '') {
-			$page = $GLOBALS['TSFE']->id;
+	public function typolinkUri($pageUid = 0, $anchor = '', $useCacheHash = TRUE, $arguments = array()) {
+		if ($pageUid === 0) {
+			$pageUid = $GLOBALS['TSFE']->id;
 		}
 
 		$typolinkConfiguration = array(
-			'parameter' => $page
+			'parameter' => $pageUid
 		);
 
 		if (count($arguments) > 0) {
@@ -71,10 +80,10 @@ class Tx_Extbase_MVC_View_Helper_URIHelper implements t3lib_Singleton {
 				$typolinkConfiguration['additionalParams'] .= '&' . $argumentNameSpace . '=' . rawurlencode($argument);
 			}
 		}
-		if ($anchor) {
+		if (!empty($anchor)) {
 			$typolinkConfiguration['section'] = $anchor;
 		}
-		if ($useCacheHash) {
+		if ($useCacheHash === TRUE) {
 			$typolinkConfiguration['useCacheHash'] = 1;
 		} else {
 			$typolinkConfiguration['useCacheHash'] = 0;
