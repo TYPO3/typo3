9b8314c2cbbd5e8bdf05677742674c4f9a9a2b48
Oliver Hader <oliver.hader@typo3.org>
2009-07-14 10:29:35 +0000

    Added feature #11508: Integrate possibility to define lifetime of frontend session data
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@5714 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/sysext/cms/tslib/class.tslib_feuserauth.php
+++ typo3/sysext/cms/tslib/class.tslib_feuserauth.php
@@ -105,6 +105,7 @@ class tslib_feUserAuth extends t3lib_userAuth {
 	var $auth_timeout_field = 6000;				// Server session lifetime. If > 0: session-timeout in seconds. If false or <0: no timeout. If string: The string is a fieldname from the usertable where the timeout can be found.
 
 	var $lifetime = 0;				// Client session lifetime. 0 = Session-cookies. If session-cookies, the browser will stop the session when the browser is closed. Otherwise this specifies the lifetime of a cookie that keeps the session.
+	protected $sessionDataLifetime = 86400;		// Lifetime of session data in seconds.
 	var $sendNoCacheHeaders = 0;
 	var $getFallBack = 1;						// If this is set, authentication is also accepted by the _GET. Notice that the identification is NOT 128bit MD5 hash but reduced. This is done in order to minimize the size for mobile-devices, such as WAP-phones
 	var $getMethodEnabled = 1;					// Login may be supplied by url.
@@ -144,6 +145,11 @@ class tslib_feUserAuth extends t3lib_userAuth {
 		if (intval($this->auth_timeout_field)>0 && intval($this->auth_timeout_field) < $this->lifetime)	{
 				// If server session timeout is non-zero but less than client session timeout: Copy this value instead.
 			$this->auth_timeout_field = $this->lifetime;
+		}
+
+		$this->sessionDataLifetime = intval($GLOBALS['TYPO3_CONF_VARS']['FE']['sessionDataLifetime']);
+		if ($this->sessionDataLifetime <= 0) {
+			$this->sessionDataLifetime = 86400;
 		}
 
 		parent::start();
@@ -370,7 +376,8 @@ class tslib_feUserAuth extends t3lib_userAuth {
 		}
 			// delete old data:
 		if ((rand()%100) <= 1) {		// a possibility of 1 % for garbage collection.
-			$GLOBALS['TYPO3_DB']->exec_DELETEquery('fe_session_data', 'tstamp < '.intval(time()-3600*24));		// all data older than 24 hours are deleted.
+			$timeoutTimeStamp = intval($GLOBALS['EXEC_TIME'] - $this->sessionDataLifetime);
+			$GLOBALS['TYPO3_DB']->exec_DELETEquery('fe_session_data', 'tstamp < ' . $timeoutTimeStamp);
 		}
 	}
 
