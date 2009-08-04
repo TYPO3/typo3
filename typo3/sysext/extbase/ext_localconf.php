b28e9b4c11ed52d7afcc0e2e9b52b12dcd5bfc69
Steffen Kamper <info@sk-typo3.de>
2009-08-04 00:18:02 +0000

    [BUGFIX] moved cache definition for FE to ext_localconf

--- typo3/sysext/extbase/ext_localconf.php
+++ typo3/sysext/extbase/ext_localconf.php
@@ -4,6 +4,13 @@ if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 require_once(t3lib_extMgm::extPath('extbase') . 'Classes/Dispatcher.php');
 require_once(t3lib_extMgm::extPath('extbase') . 'Classes/Utility/Plugin.php');
 
+// use own cache table
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['cache_extbase_reflection'] = array(
+	'backend' => 't3lib_cache_backend_DbBackend',
+	'options' => array(
+		'cacheTable' => 'cache_extbase_reflection'
+	),
+);
 
 
 # $GLOBALS ['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = 'EXT:extbase/Classes/Persistence/Hook/TCEMainValueObjectUpdater.php:tx_Extbase_Persistence_Hook_TCEMainValueObjectUpdater';
--- typo3/sysext/extbase/ext_tables.php
+++ typo3/sysext/extbase/ext_tables.php
@@ -1,13 +1,6 @@
 <?php
 if (!defined ('TYPO3_MODE')) die ('Access denied.');
 
-// use own cache table
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['cache_extbase_reflection'] = array(
-	'backend' => 't3lib_cache_backend_DbBackend',
-	'options' => array(
-		'cacheTable' => 'cache_extbase_reflection'
-	),
-);
 
 if (TYPO3_MODE == 'BE') {
 	// register the cache in BE so it will be cleared with "clear all cahces"
