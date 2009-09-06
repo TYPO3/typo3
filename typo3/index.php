350e12cfef4e8a0ffc9623746915ceec0c0fc4d9
Rupert Germann <rupi@gmx.li>
2009-09-06 14:13:11 +0000

    Added feature #11798: Remove "Login-error or session timed out" screen (thanks to Steffen Gebert)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@5901 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/index.php
+++ typo3/index.php
@@ -695,6 +695,12 @@ class SC_index {
 				return (((which >= 65 && which <= 90) && !shift_status) || 
 					((which >= 97 && which <= 122) && shift_status));
 			}
+
+				// prevent opening the login form in the backend frameset
+			if (top.location.href != self.location.href) {
+				top.location.href = self.location.href;
+			}
+
 			');
 
 		return $JSCode;
