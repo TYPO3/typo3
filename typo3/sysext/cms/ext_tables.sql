a0eda820d448b2f4a9b4fe6e8d054735c262993e
Rupert Germann <rupi@gmx.li>
2009-09-18 14:37:20 +0000

    Follow-up to bug #11505: removed superfluous fields from caching framework tables, fixed indizes
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@5984 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/sysext/cms/ext_tables.sql
+++ typo3/sysext/cms/ext_tables.sql
@@ -43,18 +43,11 @@ CREATE TABLE cachingframework_cache_pages (
   id int(11) unsigned NOT NULL auto_increment,
   identifier varchar(250) DEFAULT '' NOT NULL,
   crdate int(11) unsigned DEFAULT '0' NOT NULL,
-  page_id int(11) unsigned DEFAULT '0' NOT NULL,
-  reg1 int(11) unsigned DEFAULT '0' NOT NULL,
   content mediumtext,
   tags mediumtext,
   lifetime int(11) unsigned DEFAULT '0' NOT NULL,
-  temp_content int(1) DEFAULT '0' NOT NULL,
-  tstamp int(11) unsigned DEFAULT '0' NOT NULL,
-  expires int(10) unsigned DEFAULT '0' NOT NULL,
-  cache_data mediumblob,
-  KEY page_id (page_id),
-  KEY sel (identifier,page_id),
-  PRIMARY KEY (id)
+  PRIMARY KEY (id),
+  KEY cache_id (identifier)
 ) ENGINE=InnoDB;
 
 
@@ -68,11 +61,8 @@ CREATE TABLE cachingframework_cache_pagesection (
   content mediumtext,
   tags mediumtext,
   lifetime int(11) unsigned DEFAULT '0' NOT NULL,
-  page_id int(11) unsigned DEFAULT '0' NOT NULL,
-  mpvar_hash int(11) unsigned DEFAULT '0' NOT NULL,
   PRIMARY KEY (id),
-  KEY cache_id (identifier),
-  KEY sel (page_id,mpvar_hash)
+  KEY cache_id (identifier)
 ) ENGINE=InnoDB;
 
 
