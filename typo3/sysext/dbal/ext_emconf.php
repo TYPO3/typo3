d2a907bad8699ab3118df8b07e81f1e7ae333abb
Oliver Hader <oliver.hader@typo3.org>
2009-10-01 10:20:08 +0000

    Cleanup: Updated ext_emconf.php MD5 sums
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/dbal/trunk@24957 735d13b6-9817-0410-8766-e36946ffe9aa

--- typo3/sysext/dbal/ext_emconf.php
+++ typo3/sysext/dbal/ext_emconf.php
@@ -1,13 +1,13 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: "dbal"
+# Extension Manager/Repository config file for ext "dbal".
 #
-# Auto generated 11-03-2009 19:01
+# Auto generated 01-10-2009 12:17
 #
 # Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
@@ -33,7 +33,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
 	'version' => '0.9.22',
-	'_md5_values_when_last_written' => 'a:20:{s:9:"ChangeLog";s:4:"0461";s:26:"class.ux_db_list_extra.php";s:4:"5cab";s:21:"class.ux_t3lib_db.php";s:4:"97c5";s:28:"class.ux_t3lib_sqlengine.php";s:4:"f650";s:28:"class.ux_t3lib_sqlparser.php";s:4:"403b";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"c22b";s:14:"ext_tables.php";s:4:"427e";s:14:"ext_tables.sql";s:4:"1f95";s:27:"doc/class.tslib_fe.php.diff";s:4:"0083";s:14:"doc/manual.sxw";s:4:"17d4";s:45:"handlers/class.tx_dbal_handler_openoffice.php";s:4:"da73";s:43:"handlers/class.tx_dbal_handler_rawmysql.php";s:4:"6e90";s:40:"handlers/class.tx_dbal_handler_xmldb.php";s:4:"6f9e";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"cf19";s:14:"mod1/index.php";s:4:"da6e";s:18:"mod1/locallang.xml";s:4:"0b57";s:22:"mod1/locallang_mod.xml";s:4:"86ef";s:19:"mod1/moduleicon.gif";s:4:"8074";}',
+	'_md5_values_when_last_written' => 'a:20:{s:9:"ChangeLog";s:4:"5e63";s:26:"class.ux_db_list_extra.php";s:4:"b6e3";s:21:"class.ux_t3lib_db.php";s:4:"d6e7";s:28:"class.ux_t3lib_sqlengine.php";s:4:"fb48";s:28:"class.ux_t3lib_sqlparser.php";s:4:"b8b8";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"c22b";s:14:"ext_tables.php";s:4:"427e";s:14:"ext_tables.sql";s:4:"1f95";s:27:"doc/class.tslib_fe.php.diff";s:4:"0083";s:14:"doc/manual.sxw";s:4:"17d4";s:45:"handlers/class.tx_dbal_handler_openoffice.php";s:4:"e939";s:43:"handlers/class.tx_dbal_handler_rawmysql.php";s:4:"ca2d";s:40:"handlers/class.tx_dbal_handler_xmldb.php";s:4:"cee9";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"e53a";s:14:"mod1/index.php";s:4:"c623";s:18:"mod1/locallang.xml";s:4:"0b57";s:22:"mod1/locallang_mod.xml";s:4:"86ef";s:19:"mod1/moduleicon.gif";s:4:"8074";}',
 	'constraints' => array(
 		'depends' => array(
 			'adodb' => '4.94.0-',
