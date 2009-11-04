3a7841325703e21ce8e25b85a0047678a7527fbd
Francois Suter <francois.suter@typo3.org>
2009-11-04 16:49:02 +0000

    Follow-up to #12381: Scheduler: BLOB database fields may not be NOT NULL
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@6330 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/sysext/scheduler/ext_tables.sql
+++ typo3/sysext/scheduler/ext_tables.sql
@@ -10,8 +10,8 @@ CREATE TABLE tx_scheduler_task (
 	lastexecution_time int(11) unsigned DEFAULT '0' NOT NULL,
 	lastexecution_failure text NOT NULL,
 	lastexecution_context char(3) DEFAULT '' NOT NULL,
-	serialized_task_object blob NOT NULL,
-	serialized_executions blob NOT NULL,
+	serialized_task_object blob,
+	serialized_executions blob,
 	PRIMARY KEY (uid),
 	KEY index_nextexecution (nextexecution)
 );
