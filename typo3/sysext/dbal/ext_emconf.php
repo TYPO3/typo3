097524763e8f3026ae6d92680aa5f880ca24701b
Xavier Perseguers <typo3@perseguers.ch>
2010-01-24 08:23:05 +0000

    Fixed bug #13090: version warning with CSS styled content
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/dbal/trunk@29247 735d13b6-9817-0410-8766-e36946ffe9aa

--- typo3/sysext/dbal/ext_emconf.php
+++ typo3/sysext/dbal/ext_emconf.php
@@ -38,7 +38,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'depends' => array(
 			'adodb' => '5.10.0-',
 			'php' => '5.2.0-0.0.0',
-			'typo3' => '4.3.0-4.3.99',
+			'typo3' => '4.3.0-0.0.0',
 		),
 		'conflicts' => array(
 		),
