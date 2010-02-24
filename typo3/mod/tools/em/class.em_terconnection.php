d870ca1d02854391f2591dd5141af93ac033200f
Steffen Kamper <info@sk-typo3.de>
2010-02-24 10:11:58 +0000

    Fixed bug #13637: [EM] Upload to TER is broken
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/branches/TYPO3_4-2@7021 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/mod/tools/em/class.em_terconnection.php
+++ typo3/mod/tools/em/class.em_terconnection.php
@@ -299,7 +299,7 @@ class SC_mod_tools_em_terconnection {
 		$filesData = array();
 		foreach ($uArr['FILES'] as $filename => $infoArr) {
 			$content = (!defined('SOAP_1_2') && class_exists('soapclient')) ? base64_encode($infoArr['content']) : $infoArr['content']; // bug in NuSOAP - no automatic encoding
-			$filesData['fileData'][] = array (
+			$filesData[] = array (
 				'name' => utf8_encode($infoArr['name']),
 				'size' => intval($infoArr['size']),
 				'modificationTime' => intval($infoArr['mtime']),
