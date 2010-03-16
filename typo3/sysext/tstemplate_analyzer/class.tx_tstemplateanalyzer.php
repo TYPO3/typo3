f0dca58daf764840b6547f8145524b369d053842
Xavier Perseguers <typo3@perseguers.ch>
2010-03-16 14:12:21 +0000

    Added feature #13825: TypoScript object browser - Show all TS (thanks to Stefan Geith)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@7120 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/sysext/tstemplate_analyzer/class.tx_tstemplateanalyzer.php
+++ typo3/sysext/tstemplate_analyzer/class.tx_tstemplateanalyzer.php
@@ -125,6 +125,10 @@ class tx_tstemplateanalyzer extends t3lib_extobjbase {
 		$theOutput.=$this->pObj->doc->spacer(5);
 		$theOutput.=$this->pObj->doc->section($GLOBALS['LANG']->getLL('templateHierarchy', true), $hierar, 0, 1);
 
+		$completeLink = '<p><a href="index.php?id=' . $GLOBALS['SOBE']->id . '&amp;template=all">' . $GLOBALS['LANG']->getLL('viewCompleteTS', TRUE) . '</a></p>';
+		$theOutput .= $this->pObj->doc->spacer(5);
+		$theOutput .= $this->pObj->doc->section($GLOBALS['LANG']->getLL('completeTS', TRUE), $completeLink, 0, 1);
+
 
 			// Output options
 		$theOutput.=$this->pObj->doc->spacer(25);
