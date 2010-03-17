4a415a4b71b5de138f642d399349d4cb98edd0ee
Andreas Otto <andreas.otto@dkd.de>
2010-03-17 20:04:28 +0000

    Fixed bug #12959: cHashParams in index_phash is too small (thanks to Dmitry Dulepov).
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@7122 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/sysext/indexed_search/ext_tables.sql
+++ typo3/sysext/indexed_search/ext_tables.sql
@@ -5,7 +5,7 @@
 CREATE TABLE index_phash (
   phash int(11) DEFAULT '0' NOT NULL,
   phash_grouping int(11) DEFAULT '0' NOT NULL,
-  cHashParams tinyblob,
+  cHashParams blob,
   data_filename varchar(255) DEFAULT '' NOT NULL,
   data_page_id int(11) unsigned DEFAULT '0' NOT NULL,
   data_page_reg1 int(11) unsigned DEFAULT '0' NOT NULL,
