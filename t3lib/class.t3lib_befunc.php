29e1ebc9652832fddd8e2dc9285a5acd1598157e
Ernesto Baschny <ernst@cron-it.de>
2010-03-18 09:33:40 +0000

    Follow-up to bug #13739: CGL false=>FALSE
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@7129 709f56b5-9817-0410-a4d7-c38de5d9e867

--- t3lib/class.t3lib_befunc.php
+++ t3lib/class.t3lib_befunc.php
@@ -2649,7 +2649,7 @@ final class t3lib_BEfunc {
 	
 			// check if we need to preview a mount point
 		$sys_page = t3lib_div::makeInstance('t3lib_pageSelect');
-		$sys_page->init(false);
+		$sys_page->init(FALSE);
 		$mountPointInfo = $sys_page->getMountPointInfo($id);
 		if ($mountPointInfo) {
 			$id = $mountPointInfo['mount_pid'];
