dda1504883adf55124e5877cac75c6a0641e59fc
Christian Kuhn <lolli@schwarzbu.ch>
2010-03-24 22:19:33 +0000

    Added feature #9289: Missing index for deleting outdated session data in fe_session_data
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@7155 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/sysext/cms/ext_tables.sql
+++ typo3/sysext/cms/ext_tables.sql
@@ -169,7 +169,8 @@ CREATE TABLE fe_session_data (
   hash varchar(32) DEFAULT '' NOT NULL,
   content mediumblob,
   tstamp int(11) unsigned DEFAULT '0' NOT NULL,
-  PRIMARY KEY (hash)
+  PRIMARY KEY (hash),
+  KEY tstamp (tstamp)
 ) ENGINE=InnoDB;
 
 
