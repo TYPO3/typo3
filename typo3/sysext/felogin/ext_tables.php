793466846ea487b24b5ee81fed86701afc9250ca
Benni Mack <benni.mack@typo3.org>
2010-03-27 08:28:48 +0000

    Fixed bug #13590: felogin adds field felogin_forgotHash to tca but does not add field to database (Thanks to Fabrizio Branca)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/branches/TYPO3_4-3@7201 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/sysext/felogin/ext_tables.php
+++ typo3/sysext/felogin/ext_tables.php
@@ -16,7 +16,27 @@ $TCA['tt_content']['types']['login']['showitem']='CType;;4;;1-1-1, hidden, heade
 							--div--;LLL:EXT:cms/locallang_ttc.xml:CType.I.14, pi_flexform;;;;1-1-1,
 							--div--;LLL:EXT:cms/locallang_tca.xml:pages.tabs.access, starttime, endtime';
 
-	// Adds the redirect-field to the fe_group- and fe_users-table
+	// Adds the redirect field to the fe_groups table
+$tempColumns = array(
+	'felogin_redirectPid' => array(
+		'exclude' => 1,
+		'label'  => 'LLL:EXT:felogin/locallang_db.xml:felogin_redirectPid',
+		'config' => array(
+			'type' => 'group',
+			'internal_type' => 'db',
+			'allowed' => 'pages',
+			'size' => 1,
+			'minitems' => 0,
+			'maxitems' => 1,
+		)
+	),
+);
+
+t3lib_div::loadTCA('fe_groups');
+t3lib_extMgm::addTCAcolumns('fe_groups', $tempColumns, 1);
+t3lib_extMgm::addToAllTCAtypes('fe_groups', 'felogin_redirectPid;;;;1-1-1');
+
+	// Adds the redirect field and the forgotHash field to the fe_users-table
 $tempColumns = array (
 	"felogin_redirectPid" => array (
 		"exclude" => 1,
@@ -38,10 +58,6 @@ $tempColumns = array (
 		)
 	),
 );
-
-t3lib_div::loadTCA("fe_groups");
-t3lib_extMgm::addTCAcolumns("fe_groups",$tempColumns,1);
-t3lib_extMgm::addToAllTCAtypes("fe_groups","felogin_redirectPid;;;;1-1-1");
 
 t3lib_div::loadTCA("fe_users");
 t3lib_extMgm::addTCAcolumns("fe_users",$tempColumns,1);
