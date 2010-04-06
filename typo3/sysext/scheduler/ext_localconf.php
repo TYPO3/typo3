18cb6be3a10999d83d2b00533d33acb9045b6a5e
Francois Suter <francois.suter@typo3.org>
2010-04-06 18:57:45 +0000

    Added feature #13296: Scheduler: Add option to disable the sample tasks
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@7251 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/sysext/scheduler/ext_localconf.php
+++ typo3/sysext/scheduler/ext_localconf.php
@@ -10,17 +10,23 @@ $TYPO3_CONF_VARS['SC_OPTIONS']['GLOBAL']['cliKeys'][$_EXTKEY] = array(
 	'EXT:' . $_EXTKEY . '/cli/scheduler_cli_dispatch.php', '_CLI_scheduler'
 );
 
-	// Register information for the test and sleep tasks
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['tx_scheduler_TestTask'] = array(
-	'extension'        => $_EXTKEY,
-	'title'            => 'LLL:EXT:' . $_EXTKEY . '/locallang.xml:testTask.name',
-	'description'      => 'LLL:EXT:' . $_EXTKEY . '/locallang.xml:testTask.description',
-	'additionalFields' => 'tx_scheduler_TestTask_AdditionalFieldProvider'
-);
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['tx_scheduler_SleepTask'] = array(
-	'extension'        => $_EXTKEY,
-	'title'            => 'LLL:EXT:' . $_EXTKEY . '/locallang.xml:sleepTask.name',
-	'description'      => 'LLL:EXT:' . $_EXTKEY . '/locallang.xml:sleepTask.description',
-	'additionalFields' => 'tx_scheduler_SleepTask_AdditionalFieldProvider'
-);
+	// Get the extensions's configuration
+$extConf = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['scheduler']);
+
+	// If sample tasks should be shown,
+	// register information for the test and sleep tasks
+if (!empty($extConf['showSampleTasks'])) {
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['tx_scheduler_TestTask'] = array(
+		'extension'        => $_EXTKEY,
+		'title'            => 'LLL:EXT:' . $_EXTKEY . '/locallang.xml:testTask.name',
+		'description'      => 'LLL:EXT:' . $_EXTKEY . '/locallang.xml:testTask.description',
+		'additionalFields' => 'tx_scheduler_TestTask_AdditionalFieldProvider'
+	);
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['tx_scheduler_SleepTask'] = array(
+		'extension'        => $_EXTKEY,
+		'title'            => 'LLL:EXT:' . $_EXTKEY . '/locallang.xml:sleepTask.name',
+		'description'      => 'LLL:EXT:' . $_EXTKEY . '/locallang.xml:sleepTask.description',
+		'additionalFields' => 'tx_scheduler_SleepTask_AdditionalFieldProvider'
+	);
+}
 ?>
