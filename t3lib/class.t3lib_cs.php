dfefa65ae278f09f0c13eb64d6e7d9547853ddc9
Xavier Perseguers <typo3@perseguers.ch>
2010-05-27 13:28:00 +0000

    Fixed bug #13934: Side-effect of substr in t3lib_cs (thanks to Rens Admiraal)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@7694 709f56b5-9817-0410-a4d7-c38de5d9e867

--- t3lib/class.t3lib_cs.php
+++ t3lib/class.t3lib_cs.php
@@ -1392,7 +1392,9 @@ class t3lib_cs {
 	 * @author	Martin Kutschker <martin.t.kutschker@blackbox.net>
 	 */
 	function substr($charset,$string,$start,$len=null)	{
-		if ($len===0)	return '';
+		if ($len === 0 || $string === '') {
+			return '';
+		}
 
 		if ($GLOBALS['TYPO3_CONF_VARS']['SYS']['t3lib_cs_utils'] == 'mbstring')	{
 				// cannot omit $len, when specifying charset
