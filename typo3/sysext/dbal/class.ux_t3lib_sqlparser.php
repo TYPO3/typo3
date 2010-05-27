fb54e2c2cf26ccb434ea8d4e529af827258c9060
Xavier Perseguers <typo3@perseguers.ch>
2010-05-27 17:31:11 +0000

    Fixed bug #14496: SQL parser does not handle ALTER TABLE with character set operation
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/dbal/trunk@33599 735d13b6-9817-0410-8766-e36946ffe9aa

--- typo3/sysext/dbal/class.ux_t3lib_sqlparser.php
+++ typo3/sysext/dbal/class.ux_t3lib_sqlparser.php
@@ -301,6 +301,7 @@ class ux_t3lib_sqlparser extends t3lib_sqlparser {
 					case 'ADDUNIQUE':
 						$query .= ' (' . implode(',', $components['fields']) . ')';
 						break;
+					case 'DEFAULTCHARACTERSET':
 					case 'ENGINE':
 						// ??? todo!
 						break;
