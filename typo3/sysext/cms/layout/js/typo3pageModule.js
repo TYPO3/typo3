5a0ee1226d4e710c2841067c0f2014daf20e033c
Christian Kuhn <lolli@schwarzbu.ch>
2010-05-28 20:55:54 +0000

    Fixed bug #14508: Fixes to the Page module: Add missing js file as follow-up to #14490
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@7733 709f56b5-9817-0410-a4d7-c38de5d9e867

---
+++ typo3/sysext/cms/layout/js/typo3pageModule.js
@@ -0,0 +1,64 @@
+var typo3pageModule = {
+	/**
+	 * Initialization
+	 */
+	init: function() {
+		if (Ext.isIE) {
+			// set all content elements active
+			// the whole functionality doesn't work in IE6-8
+			Ext.select('div.t3-page-ce').addClass('active');
+		} else {
+			typo3pageModule.enableHighlighting();
+		}
+	},
+
+	/**
+	 * This method is used to bind the higlighting function "setActive"
+	 * to the mouseenter event and the "setInactive" to the mouseleave event.
+	 */
+	enableHighlighting: function() {
+		Ext.select('div.t3-page-ce')
+			.on('mouseenter',
+				typo3pageModule.setActive,
+				typo3pageModule)
+			.on('mouseleave',
+				typo3pageModule.setInactive,
+				typo3pageModule);
+	},
+
+	/**
+	 * This method is used to unbind the higlighting function "setActive"
+	 * from the mouseenter event and the "setInactive" from the mouseleave event.
+	 */
+	disableHighlighting: function() {
+		Ext.select('div.t3-page-ce')
+			.un('mouseenter',
+				typo3pageModule.setActive,
+				typo3pageModule)
+			.un('mouseleave',
+				typo3pageModule.setInactive,
+				typo3pageModule);
+	},
+
+	/**
+	 * This method is used as an event handler when the 
+	 * user hovers the a content element.
+	 */
+	setActive: function(e, t) {
+		Ext.get(t).findParent('div.t3-page-ce', null, true).addClass('active');
+	},
+
+	/**
+	 * This method is used as event handler to unset active state of
+	 * a content element when the mouse of the user leaves the 
+	 * content element.
+	 */
+	setInactive: function(e, t) {
+		Ext.get(t).findParent('div.t3-page-ce', null, true).removeClass('active');
+		
+	}
+}
+
+Ext.onReady(function() {
+	typo3pageModule.init();
+});
