6feaf88e92a5c3d2a301049cebceb53bb9d93702
Francois Suter <francois.suter@typo3.org>
2010-06-19 19:15:22 +0000

    Fixed bug: #14184: Scheduler: Handle invalid serialized exception objects
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@7972 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/sysext/scheduler/mod1/index.php
+++ typo3/sysext/scheduler/mod1/index.php
@@ -1118,8 +1118,14 @@ class tx_scheduler_Module extends t3lib_SCbase {
 						// Check if the last run failed
 					$failureOutput = '';
 					if (!empty($schedulerRecord['lastexecution_failure'])) {
+							// Try to get the stored exception object
 						$exception = unserialize($schedulerRecord['lastexecution_failure']);
-						$failureDetail = sprintf($GLOBALS['LANG']->getLL('msg.executionFailureReport'), $exception->getCode(), $exception->getMessage());
+							// If the exception could not be unserialized, issue a default error message
+						if ($exception === FALSE) {
+							$failureDetail = $GLOBALS['LANG']->getLL('msg.executionFailureDefault');
+						} else {
+							$failureDetail = sprintf($GLOBALS['LANG']->getLL('msg.executionFailureReport'), $exception->getCode(), $exception->getMessage());
+						}
 						$failureOutput = ' <img ' . t3lib_iconWorks::skinImg(t3lib_extMgm::extRelPath('scheduler'), 'res/gfx/status_failure.png') . ' alt="' . htmlspecialchars($GLOBALS['LANG']->getLL('status.failure')) . '" title="' . htmlspecialchars($failureDetail) . '" />';
 					}
 
