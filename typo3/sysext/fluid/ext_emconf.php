b9520cc822f4001357292d27f18d0da937461c77
Bastian Waidelich <bastian@typo3.org>
2010-06-25 09:57:00 +0000

    [+TASK] Fluid: Set dependency to Extbase 1.2.x in ext_emconf.php to avoid confusions when working with different versions

--- typo3/sysext/fluid/ext_emconf.php
+++ typo3/sysext/fluid/ext_emconf.php
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '1.2.0',
 	'constraints' => array(
 		'depends' => array(
-			'extbase' => '',
+			'extbase' => '1.2.0-1.2.999',
 		),
 		'conflicts' => array(
 		),
