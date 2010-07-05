9501b4b017819ec3adef3eea8f1f2b1827585515
Steffen Kamper <info@sk-typo3.de>
2010-07-05 10:17:33 +0000

    Fixed bug #14907: Help Modules can not be disabled on a per user basis (Thanks to Georg Ringer)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@8093 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/sysext/about/mod/conf.php
+++ typo3/sysext/about/mod/conf.php
@@ -5,6 +5,7 @@ $BACK_PATH='../../../';
 $MLANG['default']['tabs_images']['tab'] = 'info.gif';
 $MLANG['default']['ll_ref']='LLL:EXT:lang/locallang_mod_help_about.php';
 
+$MCONF['access'] = 'user,group';
 $MCONF['script']='index.php';
 $MCONF['name']='help_about';
 ?>
