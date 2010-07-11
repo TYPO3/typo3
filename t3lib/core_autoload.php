e36cbcb869e7735f134b3f11f77c59cf93636152
Steffen Kamper <info@sk-typo3.de>
2010-07-11 14:22:39 +0000

    Fixed bug  #15004: Cleanup cms autoload classes
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@8158 709f56b5-9817-0410-a4d7-c38de5d9e867

--- t3lib/core_autoload.php
+++ t3lib/core_autoload.php
@@ -3,7 +3,7 @@
 // This file was generated on 2009-11-25 23:05
 
 
-return array(
+$t3libClasses = array(
 	'gzip_encode' => PATH_t3lib . 'class.gzip_encode.php',
 	't3lib_admin' => PATH_t3lib . 'class.t3lib_admin.php',
 	't3lib_ajax' => PATH_t3lib . 'class.t3lib_ajax.php',
@@ -133,38 +133,9 @@ return array(
 	't3lib_spritemanager' => PATH_t3lib . 'class.t3lib_spritemanager.php',
 	't3lib_spritemanager_spriteicongenerator' => PATH_t3lib . 'interfaces/interface.t3lib_spritemanager_spriteicongenerator.php',
 	't3lib_spritemanager_simplehandler' => PATH_t3lib . 'spritemanager/class.t3lib_spritemanager_simplehandler.php',
-	'tslib_adminpanel' => PATH_tslib . 'class.tslib_adminpanel.php',
-	'tslib_cobj' => PATH_tslib . 'class.tslib_content.php',
-	'tslib_frameset' => PATH_tslib . 'class.tslib_content.php',
-	'tslib_tableoffset' => PATH_tslib . 'class.tslib_content.php',
-	'tslib_controltable' => PATH_tslib . 'class.tslib_content.php',
-	'tslib_eidtools' => PATH_tslib . 'class.tslib_eidtools.php',
-	'tslib_fe' => PATH_tslib . 'class.tslib_fe.php',
-	'tslib_fecompression' => PATH_tslib . 'class.tslib_fecompression.php',
-	'tslib_fetce' => PATH_tslib . 'class.tslib_fetce.php',
-	'tslib_feuserauth' => PATH_tslib . 'class.tslib_feuserauth.php',
-	'tslib_gifbuilder' => PATH_tslib . 'class.tslib_gifbuilder.php',
-	'tslib_menu' => PATH_tslib . 'class.tslib_menu.php',
-	'tslib_tmenu' => PATH_tslib . 'class.tslib_menu.php',
-	'tslib_gmenu' => PATH_tslib . 'class.tslib_menu.php',
-	'tslib_imgmenu' => PATH_tslib . 'class.tslib_menu.php',
-	'tslib_jsmenu' => PATH_tslib . 'class.tslib_menu.php',
-	'tspagegen' => PATH_tslib . 'class.tslib_pagegen.php',
-	'fe_loaddbgroup' => PATH_tslib . 'class.tslib_pagegen.php',
-	'tslib_pibase' => PATH_tslib . 'class.tslib_pibase.php',
-	'tslib_search' => PATH_tslib . 'class.tslib_search.php',
-	'sc_tslib_showpic' => PATH_tslib . 'showpic.php',
-	'tx_cms_mediaitems' => PATH_tslib . 'hooks/class.tx_cms_mediaitems.php',
-	'tx_cms_treelistcacheupdate' => PATH_tslib . 'hooks/class.tx_cms_treelistcacheupdate.php',
-	'tslib_content_cobjgetsinglehook' => PATH_tslib . 'interfaces/interface.tslib_content_cobjgetsinglehook.php',
-	'tslib_menu_filterMenuPagesHook' => PATH_tslib . 'interfaces/interface.tslib_menu_filterMenuPagesHook.php',
-	'tslib_content_getdatahook' => PATH_tslib . 'interfaces/interface.tslib_content_getdatahook.php',
-	'tslib_cobj_getimgresourcehook' => PATH_tslib . 'interfaces/interface.tslib_content_getimgresourcehook.php',
-	'tslib_content_postinithook' => PATH_tslib . 'interfaces/interface.tslib_content_postinithook.php',
-	'tslib_content_stdwraphook' => PATH_tslib . 'interfaces/interface.tslib_content_stdwraphook.php',
-	'user_various' => PATH_tslib . 'media/scripts/example_callfunction.php',
-	'tslib_gmenu_foldout' => PATH_tslib . 'media/scripts/gmenu_foldout.php',
-	'tslib_gmenu_layers' => PATH_tslib . 'media/scripts/gmenu_layers.php',
-	'tslib_tmenu_layers' => PATH_tslib . 'media/scripts/tmenu_layers.php',
 );
+
+$tslibClasses = require(PATH_typo3 . 'sysext/cms/ext_autoload.php');
+
+return array_merge($t3libClasses, $tslibClasses);
 ?>
