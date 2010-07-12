80eb608376f6be8e8d0a6a2ab7000da4172e4349
Steffen Kamper <info@sk-typo3.de>
2010-07-12 19:46:51 +0000

    Fixed bug #15082: [openDocs] Exception is thrown if a record isn't found
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@8165 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/sysext/opendocs/class.tx_opendocs.php
+++ typo3/sysext/opendocs/class.tx_opendocs.php
@@ -158,6 +158,10 @@ class tx_opendocs implements backend_toolbarItem {
 		$table  = $document[3]['table'];
 		$uid    = $document[3]['uid'];
 		$record = t3lib_BEfunc::getRecordWSOL($table, $uid);
+		if (!is_array($record)) {
+				// record seems to be deleted
+			return '';
+		}
 		$label  = htmlspecialchars(strip_tags(t3lib_div::htmlspecialchars_decode($document[0])));
 		$icon   = t3lib_iconWorks::getSpriteIconForRecord($table, $record);
 		$link   = $GLOBALS['BACK_PATH'] . 'alt_doc.php?' . $document[2];
