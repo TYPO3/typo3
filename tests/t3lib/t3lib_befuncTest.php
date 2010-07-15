c0e4c0dc81839db47c95dae793a91782fbf8b283
Christian Kuhn <lolli@schwarzbu.ch>
2010-07-15 23:01:44 +0000

    Fixed bug #15142: [Unit tests] Remove unnecessary require_once() calls
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/branches/TYPO3_4-4@8195 709f56b5-9817-0410-a4d7-c38de5d9e867

--- tests/t3lib/t3lib_befuncTest.php
+++ tests/t3lib/t3lib_befuncTest.php
@@ -22,8 +22,6 @@
 * This copyright notice MUST APPEAR in all copies of the script!
 ***************************************************************/
 
-require_once(PATH_t3lib . 'class.t3lib_befunc.php');
-
 /**
  * Testcase for the t3lib_BEfunc class in the TYPO3 core.
  *
