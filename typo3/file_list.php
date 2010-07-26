8a4bc1c3257178445b0523969b77fd8b29ae2f8d
Steffen Kamper <info@sk-typo3.de>
2010-07-26 20:43:50 +0000

    Fixed bug #15172: Add wrapper files for list/filelist modules
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@8273 709f56b5-9817-0410-a4d7-c38de5d9e867

---
+++ typo3/file_list.php
@@ -0,0 +1,51 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2010 Steffen Kamper (steffen@typo3.com)
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*  A copy is found in the textfile GPL.txt and important notices to the license
+*  from the author is found in LICENSE.txt distributed with these scripts.
+*
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+/**
+ * This is a wrapper file for direct calls to filelist module.
+ * It's deprecated since 4.5, use proper link generation.
+ *
+ * @author	Steffen Kamper <steffen@typo3.com>
+ * @deprecated
+ *
+ */
+
+
+require ('init.php');
+
+$query = t3lib_div::getIndpEnv('QUERY_STRING');
+t3lib_div::deprecationLog('The filelist module is a system extension now, do not link to this file.' .
+	LF .
+	'Referer: ' . t3lib_div::getIndpEnv('HTTP_REFERER')
+);
+if (t3lib_extMgm::isLoaded('list')) {
+	t3lib_utility_Http::redirect(t3lib_extMgm::extRelPath('filelist') . 'mod1/file_list.php?' . $query);
+} else {
+	$title = sprintf($GLOBALS['LANG']->sL('LLL:EXT:lang/locallang_core.xml:extension.not.installed'), 'list');
+	$message = $GLOBALS['LANG']->sL('LLL:EXT:lang/locallang_core.xml:link.to.filelist.correctly');
+	t3lib_BEfunc::typo3PrintError($title, $message);
+}
+?>
