554b8b71a98fc036ceab5481543b9e42aba167a5
Oliver Hader <oliver.hader@typo3.org>
2010-07-28 12:47:15 +0000

    Fixed bug #15260: Use of undefined method t3lib_div::sanitizeLocalUrl()
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/branches/TYPO3_4-1@8444 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/logout.php
+++ typo3/logout.php
@@ -72,7 +72,7 @@ class SC_logout {
 		$BE_USER->writelog(255,2,0,1,'User %s logged out from TYPO3 Backend',Array($BE_USER->user['username']));	// Logout written to log
 		$BE_USER->logoff();
 
-		$redirect = t3lib_div::sanitizeLocalUrl(t3lib_div::_GP('redirect'));
+		$redirect = t3lib_div::sanitizeBackEndUrl(t3lib_div::_GP('redirect'));
 		$redirectUrl = $redirect ? $redirect : 'index.php';
 
 		header('Location: '.t3lib_div::locationHeaderUrl($redirectUrl));
