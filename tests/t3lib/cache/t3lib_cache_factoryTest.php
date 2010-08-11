4beb89ea0653b39b1ab0d7f50d3cce101eabe9f6
Steffen Kamper <info@sk-typo3.de>
2010-08-11 08:05:16 +0000

    Fixed bug #13802: Hardcoded typo3_src Folder in TestCase tests/t3lib/cache/t3lib_cache_factoryTest.php (Thanks to Stefano Kowalke)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@8561 709f56b5-9817-0410-a4d7-c38de5d9e867

--- tests/t3lib/cache/t3lib_cache_factoryTest.php
+++ tests/t3lib/cache/t3lib_cache_factoryTest.php
@@ -41,7 +41,7 @@ class t3lib_cache_FactoryTest extends tx_phpunit_testcase {
 	 * @author Ingo Renner <ingo@typo3.org>
 	 */
 	public function setUp() {
-		$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheBackends']['t3lib_cache_backend_MockBackend'] = 'typo3_src/tests/t3lib/cache/backend/class.t3lib_cache_backend_mockbackend.php:t3lib_cache_backend_MockBackend';
+		$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheBackends']['t3lib_cache_backend_MockBackend'] = 'tests/t3lib/cache/backend/class.t3lib_cache_backend_mockbackend.php:t3lib_cache_backend_MockBackend';
 	}
 
 	/**
