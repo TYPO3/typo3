f20ce0d989103a2436e8a62c16dae71bc5b92562
Steffen Kamper <info@sk-typo3.de>
2010-08-12 20:51:53 +0000

    Follow-up to #3819: t3lib_div::getIndpEnv('TYPO3_DOCUMENT_ROOT') delivers wrong value in Backend
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@8592 709f56b5-9817-0410-a4d7-c38de5d9e867

--- tests/t3lib/cache/backend/t3lib_cache_backend_filebackendTest.php
+++ tests/t3lib/cache/backend/t3lib_cache_backend_filebackendTest.php
@@ -112,7 +112,7 @@ class t3lib_cache_backend_FileBackendTest extends tx_phpunit_testcase {
 	 */
 	public function getCacheDirectoryReturnsTheCurrentCacheDirectory() {
 		$directory = $this->testingCacheDirectory;
-		$fullPathToDirectory = t3lib_div::getIndpEnv('TYPO3_DOCUMENT_ROOT') . '/' . $directory;
+		$fullPathToDirectory = PATH_site . $directory;
 
 		$this->backend->setCacheDirectory($directory);
 		$this->assertEquals($fullPathToDirectory, $this->backend->getCacheDirectory(), 'getCacheDirectory() did not return the expected value.');
