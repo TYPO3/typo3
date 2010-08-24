5d367a9acca640dbadffc5c60c2fe734606b1891
Ernesto Baschny <ernst@cron-it.de>
2010-08-24 13:05:12 +0000

    Updated md5 sum of sysexts before 4.5alpha1 release
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@8676 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/sysext/em/ext_emconf.php
+++ typo3/sysext/em/ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "em".
 #
-# Auto generated 18-07-2010 18:33
+# Auto generated 24-08-2010 14:42
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -34,7 +34,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
 	'version' => '1.1.0',
-	'_md5_values_when_last_written' => 'a:16:{s:12:"ext_icon.gif";s:4:"3a30";s:14:"ext_tables.php";s:4:"6e0d";s:23:"mod1/class.em_index.php";s:4:"0dcf";s:22:"mod1/class.em_soap.php";s:4:"5068";s:31:"mod1/class.em_terconnection.php";s:4:"630f";s:23:"mod1/class.em_unzip.php";s:4:"0d71";s:28:"mod1/class.em_xmlhandler.php";s:4:"8445";s:21:"mod1/class.nusoap.php";s:4:"e746";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"6ea9";s:17:"mod1/download.png";s:4:"c5b2";s:11:"mod1/em.gif";s:4:"3a30";s:14:"mod1/index.php";s:4:"4dca";s:16:"mod1/install.gif";s:4:"8d57";s:14:"mod1/oodoc.gif";s:4:"744b";s:18:"mod1/uninstall.gif";s:4:"a77f";}',
+	'_md5_values_when_last_written' => 'a:16:{s:12:"ext_icon.gif";s:4:"3a30";s:14:"ext_tables.php";s:4:"6e0d";s:23:"mod1/class.em_index.php";s:4:"4966";s:22:"mod1/class.em_soap.php";s:4:"67b6";s:31:"mod1/class.em_terconnection.php";s:4:"630f";s:23:"mod1/class.em_unzip.php";s:4:"e3a9";s:28:"mod1/class.em_xmlhandler.php";s:4:"8445";s:21:"mod1/class.nusoap.php";s:4:"dfa8";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"6ea9";s:17:"mod1/download.png";s:4:"c5b2";s:11:"mod1/em.gif";s:4:"3a30";s:14:"mod1/index.php";s:4:"e8e5";s:16:"mod1/install.gif";s:4:"8d57";s:14:"mod1/oodoc.gif";s:4:"744b";s:18:"mod1/uninstall.gif";s:4:"a77f";}',
 	'constraints' => array(
 		'depends' => array(
 			'cms' => '',
