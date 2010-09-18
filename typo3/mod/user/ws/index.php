4eee673f84dace4aa79b20adc3d7b15e97344995
Benni Mack <benni.mack@typo3.org>
2010-09-18 10:57:56 +0000

    Fixed bug #11415 - Misleading info: No workspaces found!
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/branches/TYPO3_4-3@8807 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/mod/user/ws/index.php
+++ typo3/mod/user/ws/index.php
@@ -436,7 +436,7 @@ class SC_mod_user_ws_index extends t3lib_SCbase {
 			default:
 				$title = t3lib_iconWorks::getIconImage('sys_workspace', $GLOBALS['BE_USER']->workspaceRec, $this->doc->backPath, ' align="top"').
 							'['.$GLOBALS['BE_USER']->workspace.'] '.t3lib_BEfunc::getRecordTitle('sys_workspace',$GLOBALS['BE_USER']->workspaceRec,TRUE);
-				$description = ($GLOBALS['BE_USER']->workspaceRec['description'] ? htmlspecialchars($GLOBALS['BE_USER']->workspaceRec['description']) : '<em>[' . $LANG->getLL('shortcut_noWSfound') . ']</em>');
+				$description = $GLOBALS['BE_USER']->workspaceRec['description'];
 			break;
 		}
 
@@ -474,10 +474,10 @@ class SC_mod_user_ws_index extends t3lib_SCbase {
 				<td class="bgColor2" nowrap="nowrap"><b>' . $LANG->getLL('label_workspace') . '</b>&nbsp;</td>
 				<td class="bgColor4" nowrap="nowrap">'.$title.'</td>
 			</tr>
-			<tr>
+			<tr>' . ($description ? '
 				<td class="bgColor2" nowrap="nowrap"><b>' . $LANG->getLL('label_description') . '</b>&nbsp;</td>
 				<td class="bgColor4">'.$description.'</td>
-			</tr>'.($GLOBALS['BE_USER']->workspace!=-99 && !$details ? '
+			</tr>' : '') . ($GLOBALS['BE_USER']->workspace!=-99 && !$details ? '
 			<tr>
 				<td class="bgColor2" nowrap="nowrap"><b>' . $LANG->getLL('label_options') . '</b>&nbsp;</td>
 				<td class="bgColor4">'.$menu.$actionLinks.'</td>
