3fdc54f45427bb2d5db80e3fdafe7431d72aab9f
Sebastian Kurfürst <sebastian@typo3.org>
2010-09-22 06:38:46 +0000

    * Raised version number to 1.3.0alpha2 and updated ChangeLog.

--- typo3/sysext/fluid/ext_emconf.php
+++ typo3/sysext/fluid/ext_emconf.php
@@ -29,10 +29,10 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '1.3.0alpha1',
+	'version' => '1.3.0alpha2',
 	'constraints' => array(
 		'depends' => array(
-			'extbase' => '1.3.0alpha1',
+			'extbase' => '1.3.0alpha2',
 		),
 		'conflicts' => array(
 		),
