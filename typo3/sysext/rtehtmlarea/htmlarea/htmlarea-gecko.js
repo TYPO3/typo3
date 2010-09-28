3756522416922266330e2726e580a382bd8a37cb
Stanislas Rolland <typo3@sjbr.ca>
2010-09-28 05:54:42 +0000

    Fixed bug #15730: htmlArea RTE: Problem editing a new list entry in list of items with inline markup
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@8910 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/sysext/rtehtmlarea/htmlarea/htmlarea-gecko.js
+++ typo3/sysext/rtehtmlarea/htmlarea/htmlarea-gecko.js
@@ -658,7 +658,7 @@ HTMLArea.Editor.prototype._checkInsertP = function() {
 		}
 		p = df.firstChild;
 		if (p) {
-			if (!/\S/.test(p.innerHTML) || (p.childNodes.length == 1 && /^br$/i.test(p.firstChild.nodeName))) {
+			if (!/\S/.test(p.innerHTML) || (!/\S/.test(p.textContent) && !/<(img|hr|table)/i.test(p.innerHTML))) {
  				if (/^h[1-6]$/i.test(p.nodeName)) {
 					p = this.convertNode(p, "p");
 				}
@@ -668,7 +668,7 @@ HTMLArea.Editor.prototype._checkInsertP = function() {
 				if (!Ext.isOpera) {
 					p.innerHTML = "<br />";
 				}
-				if(/^li$/i.test(p.nodeName) && left_empty && !block.nextSibling) {
+				if (/^li$/i.test(p.nodeName) && left_empty && (!block.nextSibling || !/^li$/i.test(block.nextSibling.nodeName))) {
 					left = block.parentNode;
 					left.removeChild(block);
 					range.setEndAfter(left);
@@ -688,8 +688,15 @@ HTMLArea.Editor.prototype._checkInsertP = function() {
 			if (a && /^a$/i.test(a.nodeName) && !/\S/.test(a.innerHTML)) {
 				this.convertNode(a, 'br');
 			}
+				// Walk inside the deepest child element (presumably inline element)
+			while (p.firstChild && p.firstChild.nodeType == 1 && !/^(br|img|hr|table)$/i.test(p.firstChild.nodeName)) {
+				p = p.firstChild;
+			}
 			if (/^br$/i.test(p.nodeName)) {
-				p = p.parentNode.insertBefore(this._doc.createTextNode("\x20"), p);
+				p = p.parentNode.insertBefore(doc.createTextNode('\x20'), p);
+			} else if (!/\S/.test(p.innerHTML)) {
+					// Need some element inside the deepest element
+				p.appendChild(doc.createElement('br'));
 			}
 			this.selectNodeContents(p, true);
 		} else {
