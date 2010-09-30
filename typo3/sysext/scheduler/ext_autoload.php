f89f9715ce0ff166f6551db63f15d8e2473ccde0
Francois Suter <francois.suter@typo3.org>
2010-09-30 16:21:09 +0000

    Fixed bug #15639: Scheduler: Add missing classes and interface to autoload registry
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@8938 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/sysext/scheduler/ext_autoload.php
+++ typo3/sysext/scheduler/ext_autoload.php
@@ -4,16 +4,19 @@
  *
  * $Id$
  */
-// TODO: document necessity of providing autoloader information
+$extensionPath = t3lib_extMgm::extPath('scheduler');
 return array(
-	'tx_scheduler'										=> t3lib_extMgm::extPath('scheduler', 'class.tx_scheduler.php'),
-	'tx_scheduler_croncmd'								=> t3lib_extMgm::extPath('scheduler', 'class.tx_scheduler_croncmd.php'),
-	'tx_scheduler_task'									=> t3lib_extMgm::extPath('scheduler', 'class.tx_scheduler_task.php'),
-	'tx_scheduler_execution'							=> t3lib_extMgm::extPath('scheduler', 'class.tx_scheduler_execution.php'),
-	'tx_scheduler_failedexecutionexception'				=> t3lib_extMgm::extPath('scheduler', 'class.tx_scheduler_failedexecutionexception.php'),
-	'tx_scheduler_testtask'								=> t3lib_extMgm::extPath('scheduler', 'examples/class.tx_scheduler_testtask.php'),
-	'tx_scheduler_testtask_additionalfieldprovider'		=> t3lib_extMgm::extPath('scheduler', 'examples/class.tx_scheduler_testtask_additionalfieldprovider.php'),
-	'tx_scheduler_sleeptask'							=> t3lib_extMgm::extPath('scheduler', 'examples/class.tx_scheduler_sleeptask.php'),
-	'tx_scheduler_sleeptask_additionalfieldprovider'	=> t3lib_extMgm::extPath('scheduler', 'examples/class.tx_scheduler_sleeptask_additionalfieldprovider.php')
+	'tx_scheduler' => $extensionPath . 'class.tx_scheduler.php',
+	'tx_scheduler_croncmd' => $extensionPath . 'class.tx_scheduler_croncmd.php',
+	'tx_scheduler_execution' => $extensionPath . 'class.tx_scheduler_execution.php',
+	'tx_scheduler_failedexecutionexception' => $extensionPath . 'class.tx_scheduler_failedexecutionexception.php',
+	'tx_scheduler_task' => $extensionPath . 'class.tx_scheduler_task.php',
+	'tx_scheduler_sleeptask' => $extensionPath . 'examples/class.tx_scheduler_sleeptask.php',
+	'tx_scheduler_sleeptask_additionalfieldprovider' => $extensionPath . 'examples/class.tx_scheduler_sleeptask_additionalfieldprovider.php',
+	'tx_scheduler_testtask' => $extensionPath . 'examples/class.tx_scheduler_testtask.php',
+	'tx_scheduler_testtask_additionalfieldprovider' => $extensionPath . 'examples/class.tx_scheduler_testtask_additionalfieldprovider.php',
+	'tx_scheduler_additionalfieldprovider' => $extensionPath . 'interfaces/interface.tx_scheduler_additionalfieldprovider.php',
+	'tx_scheduler_module' => $extensionPath . 'mod1/index.php',
+	'tx_scheduler_croncmdtest' => $extensionPath . 'tests/tx_scheduler_croncmdTest.php',
 );
 ?>
