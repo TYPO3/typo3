35a20c4ae7b4da90299649c17905cd922d81caaa
Steffen Kamper <info@sk-typo3.de>
2010-10-09 09:33:26 +0000

    Fixed bug #15230: Bug/Feature: Core only is able to load one specific IE6 CSS file (Thanks to Steffen Ritter)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@9013 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/sysext/t3skin/pngfix/class.tx_templatehook.php
+++ typo3/sysext/t3skin/pngfix/class.tx_templatehook.php
@@ -9,7 +9,17 @@ class tx_templatehook {
 		&& strpos($userAgent, 'Opera') === false
 		&& strpos($userAgent, 'MSIE 7') === false) {
 				//make sure we match IE6 but not Opera or IE7
-			$params['pageRenderer']->addCssFile($parent->backPath . 'sysext/t3skin/stylesheets/ie6/z_t3-icons-gifSprites.css');
+			$files = t3lib_div::getFilesInDir(PATH_typo3 . 'sysext/t3skin/stylesheets/ie6', 'css', 0, 1);
+			foreach($files as $fileName) {
+				$params['pageRenderer']->addCssFile($parent->backPath . 'sysext/t3skin/stylesheets/ie6/' . $fileName);
+			}
+
+				// load files of spriteGenerator for ie6
+			$files = t3lib_div::getFilesInDir(PATH_site . t3lib_SpriteManager::$tempPath . 'ie6/', 'css', 0, 1);
+			foreach($files as $fileName) {
+				$params['pageRenderer']->addCssFile($parent->backPath . '../' . t3lib_SpriteManager::$tempPath . 'ie6/' . $fileName);
+			}
+
 		}
 	}
 }
