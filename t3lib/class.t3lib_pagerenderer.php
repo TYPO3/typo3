1286949e32085460be67231d0f28bf665a3aff65
Steffen Kamper <info@sk-typo3.de>
2010-10-10 15:47:24 +0000

    Fixed bug #15727: Clean-up for PageRenderer
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/branches/TYPO3_4-4@9030 709f56b5-9817-0410-a4d7-c38de5d9e867

--- t3lib/class.t3lib_pagerenderer.php
+++ t3lib/class.t3lib_pagerenderer.php
@@ -44,6 +44,7 @@ class t3lib_PageRenderer implements t3lib_Singleton {
 
 	protected $moveJsFromHeaderToFooter = FALSE;
 
+	/* @var t3lib_cs Instance of t3lib_cs */
 	protected $csConvObj;
 	protected $lang;
 
@@ -81,6 +82,7 @@ class t3lib_PageRenderer implements t3lib_Singleton {
 
 	// static inline code blocks
 	protected $jsInline = array ();
+	protected $jsFooterInline = array ();
 	protected $extOnReadyCode = array ();
 	protected $cssInline = array ();
 
@@ -119,6 +121,9 @@ class t3lib_PageRenderer implements t3lib_Singleton {
 	protected $inlineSettings = array ();
 
 	protected $inlineJavascriptWrap = array ();
+
+	// saves error messages generated during compression
+	protected $compressError = '';
 
 	// used by BE modules
 	public $backPath;
@@ -827,7 +832,7 @@ class t3lib_PageRenderer implements t3lib_Singleton {
 	 * @param boolean $forceOnTop
 	 * @return void
 	 */
-	public function addCssInlineBlock($name, $block, $compressed = FALSE, $forceOnTop = FALSE) {
+	public function addCssInlineBlock($name, $block, $compress = FALSE, $forceOnTop = FALSE) {
 		if (!isset($this->cssInline[$name]) && !empty($block)) {
 			$this->cssInline[$name] = array (
 				'code'       => $block,
@@ -1053,7 +1058,6 @@ class t3lib_PageRenderer implements t3lib_Singleton {
 		$jsFooterInline = '';
 		$jsFooterLibs = '';
 		$jsFooterFiles = '';
-		$noJS = FALSE;
 
 		// preRenderHook for possible manuipulation
 		if (is_array($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php']['render-preProcess'])) {
@@ -1207,7 +1211,7 @@ class t3lib_PageRenderer implements t3lib_Singleton {
 		$templateFile = t3lib_div::getFileAbsFileName($this->templateFile, TRUE);
 		$template = t3lib_div::getURL($templateFile);
 
-		if ($this->removeEmptyLinesFromTemplate) {
+		if ($this->removeLineBreaksFromTemplate) {
 			$template = strtr($template, array(LF => '', CR => ''));
 		}
 		if ($part != self::PART_COMPLETE) {
@@ -1446,7 +1450,6 @@ class t3lib_PageRenderer implements t3lib_Singleton {
 			t3lib_div::callUserFunction($GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['jsCompressHandler'], $params, $this);
 		} else {
 				// traverse the arrays, compress files
-			$this->compressError = '';
 
 			if ($this->compressJavascript) {
 				if (count($this->jsInline)) {
