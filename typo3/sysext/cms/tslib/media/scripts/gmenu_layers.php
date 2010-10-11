084750252541634f2b74335350e6542461b824d7
Steffen Kamper <info@sk-typo3.de>
2010-10-11 20:47:04 +0000

    Fixed bug #12376: typo3temp got filled with thousands of javascript_* files (Thanks to Georg Ringer)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/branches/TYPO3_4-3@9036 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/sysext/cms/tslib/media/scripts/gmenu_layers.php
+++ typo3/sysext/cms/tslib/media/scripts/gmenu_layers.php
@@ -114,7 +114,7 @@ class tslib_gmenu_layers extends tslib_gmenu {
 	 * @return	void
 	 */
 	function extProc_init()	{
-		$this->WMid = trim($this->mconf['layer_menu_id'])?trim($this->mconf['layer_menu_id']).'x':substr(md5(microtime()),0,6);	// NO '_' (underscore) in the ID!!! NN4 breaks!
+		$this->WMid = trim($this->mconf['layer_menu_id']) ? trim($this->mconf['layer_menu_id']) . 'x' : substr(md5('gl' . serialize($this->mconf)), 0, 6);
 
 		$GLOBALS['TSFE']->applicationData['GMENU_LAYERS']['WMid'][]=$this->WMid;
 		$this->WMtempStore = $GLOBALS['TSFE']->applicationData['GMENU_LAYERS']['WMid'];
