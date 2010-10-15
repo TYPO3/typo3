56ac9f253e50b05548ba7d10813ef8da3f16bb6c
Steffen Kamper <info@sk-typo3.de>
2010-10-15 09:21:06 +0000

    Fixed bug #15993: Bug: Felogin shouldn't show the permalogin form controls when permalogin is set to "forced on" in install tool
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@9064 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/sysext/felogin/pi1/class.tx_felogin_pi1.php
+++ typo3/sysext/felogin/pi1/class.tx_felogin_pi1.php
@@ -524,8 +524,8 @@ class tx_felogin_pi1 extends tslib_pibase {
 		}
 
 
-			// Permanent Login is only possible if permalogin is not deactivated (-1) and lifetime is greater than 0
-		if ($this->conf['showPermaLogin'] && t3lib_div::inList('0,1,2', $GLOBALS['TYPO3_CONF_VARS']['FE']['permalogin']) && $GLOBALS['TYPO3_CONF_VARS']['FE']['lifetime'] > 0) {
+			// The permanent login checkbox should only be shown if permalogin is not deactivated (-1), not forced to be always active (2) and lifetime is greater than 0
+		if ($this->conf['showPermaLogin'] && t3lib_div::inList('0,1', $GLOBALS['TYPO3_CONF_VARS']['FE']['permalogin']) && $GLOBALS['TYPO3_CONF_VARS']['FE']['lifetime'] > 0) {
 			$markerArray['###PERMALOGIN###'] = $this->pi_getLL('permalogin', '', 1);
 			if($GLOBALS['TYPO3_CONF_VARS']['FE']['permalogin'] == 1) {
 				$markerArray['###PERMALOGIN_HIDDENFIELD_ATTRIBUTES###'] = 'disabled="disabled"';
