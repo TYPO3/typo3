30813293ac3616c165305a33c3521eacd39842ab
Benni Mack <benni.mack@typo3.org>
2010-10-15 21:38:45 +0000

    Followup to #16005 - added some more comments
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@9089 709f56b5-9817-0410-a4d7-c38de5d9e867

--- t3lib/class.t3lib_befunc.php
+++ t3lib/class.t3lib_befunc.php
@@ -4008,7 +4008,7 @@ final class t3lib_BEfunc {
 
 	/**
 	 * Will return where clause de-selecting new(/deleted)-versions from other workspaces.
-	 * If in live-workspace, don't show MOVE-TO-PLACEHOLDERS if versioningWS is 2 (allows moving)
+	 * If in live-workspace, don't show "MOVE-TO-PLACEHOLDERS" records if versioningWS is 2 (allows moving)
 	 *
 	 * @param	string		Table name
 	 * @return	string		Where clause if applicable.
@@ -4021,8 +4021,7 @@ final class t3lib_BEfunc {
 					// if in any workspace other than live
 				return ' AND (' . $table . '.t3ver_state <= 0 OR ' . $table . '.t3ver_wsid = ' . $currentWorkspace . ')';
 			} elseif ($GLOBALS['TCA'][$table]['ctrl']['versioningWS'] == 2) {
-					// if in live workspace, don't show MOVE-TO-PLACEHOLDERS
-					// but only if if versioning of this table allows moving (versioningWS=2) 
+					// if in live workspace, don't show "MOVE-TO-PLACEHOLDERS"
 				return ' AND (' . $table . '.t3ver_state != 3)';
 			}
 		}
