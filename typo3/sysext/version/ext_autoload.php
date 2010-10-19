53fcfb40f89fb1d7bbfe018693426d7a0f621e1d
Benni Mack <benni.mack@typo3.org>
2010-10-19 17:37:10 +0000

    Fixed bug #16000: Move versioning selector to the version extension
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@9167 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/sysext/version/ext_autoload.php
+++ typo3/sysext/version/ext_autoload.php
@@ -5,6 +5,7 @@
  * $Id: ext_autoload.php 6536 2009-11-25 14:07:18Z stucki $
  */
 return array(
-	'tx_version_tasks_autopublish' => t3lib_extMgm::extPath('version', 'tasks/class.tx_version_tasks_autopublish.php')
+	'tx_version_tasks_autopublish' => t3lib_extMgm::extPath('version', 'tasks/class.tx_version_tasks_autopublish.php'),
+	'tx_version_gui' => t3lib_extMgm::extPath('version', 'class.tx_version_gui.php')
 );
 ?>
