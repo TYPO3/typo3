69e259f8af0a606e87b969b9c280991310faa301
Stanislas Rolland <typo3@sjbr.ca>
2010-11-02 18:02:44 +0000

    Follow-up to feature #16079: htmlArea RTE: Register RTE scripts with the autoloader
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@9243 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/sysext/rtehtmlarea/ext_autoload.php
+++ typo3/sysext/rtehtmlarea/ext_autoload.php
@@ -13,7 +13,7 @@ return array(
 	'tx_rtehtmlarea_pi2' => $rtehtmlareaExtensionPath . 'pi2/class.tx_rtehtmlarea_pi2.php',
 	'tx_rtehtmlarea_pi3' => $rtehtmlareaExtensionPath . 'pi3/class.tx_rtehtmlarea_pi3.php',
 	'tx_rtehtmlarea_browse_links' => $rtehtmlareaExtensionPath . 'mod3/class.tx_rtehtmlarea_browse_links.php',
-	'class.tx_rtehtmlarea_select_image.php' => $rtehtmlareaExtensionPath . 'mod4/class.tx_rtehtmlarea_select_image.php',
+	'tx_rtehtmlarea_select_image' => $rtehtmlareaExtensionPath . 'mod4/class.tx_rtehtmlarea_select_image.php',
 	'tx_rtehtmlarea_user' => $rtehtmlareaExtensionPath . 'mod5/class.tx_rtehtmlarea_user.php',
 	'tx_rtehtmlarea_parse_html' => $rtehtmlareaExtensionPath . 'mod6/class.tx_rtehtmlarea_parse_html.php',
 );
