38294db404bef2750b0e42130c56fae7c7357ceb
Steffen Kamper <info@sk-typo3.de>
2010-11-04 00:27:29 +0000

    Fixed bug #16196: Optimize stdWrap usage for TypoScript content element CTABLE (Thanks to Jo Hasenau)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@9266 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/sysext/cms/tslib/content/class.tslib_content_contenttable.php
+++ typo3/sysext/cms/tslib/content/class.tslib_content_contenttable.php
@@ -42,36 +42,85 @@ class tslib_content_ContentTable extends tslib_content_Abstract {
 	 * @return	string		Output
 	 */
 	public function render($conf = array()) {
+
 		$controlTable = t3lib_div::makeInstance('tslib_controlTable');
-		if ($conf['tableParams']) {
-			$controlTable->tableParams = $conf['tableParams'];
+
+		$tableParams = isset($conf['tableParams.'])
+			? $this->cObj->stdWrap($conf['tableParams'],$conf['tableParams.'])
+			: $conf['tableParams'];
+		if ($tableParams) {
+			$controlTable->tableParams = $tableParams;
 		}
 			// loads the pagecontent
+		$conf['cWidth'] = isset($conf['cWidth.'])
+			? $this->cObj->stdWrap($conf['cWidth'],$conf['cWidth.'])
+			: $conf['cWidth'];
 		$controlTable->contentW = $conf['cWidth'];
+
 		// loads the menues if any
 		if (is_array($conf['c.'])) {
 			$controlTable->content = $this->cObj->cObjGet($conf['c.'], 'c.');
-			$controlTable->contentTDparams = isset($conf['c.']['TDParams']) ? $conf['c.']['TDParams'] : 'valign="top"';
+			$contentTDParams = isset($conf['c.']['TDParams.'])
+				? $this->cObj->stdWrap($conf['c.']['TDParams'],$conf['c.']['TDParams.'])
+				: $conf['c.']['TDParams'];
+			$controlTable->contentTDparams = isset($contentTDParams)
+				? $contentTDParams
+				: 'valign="top"';
 		}
 		if (is_array($conf['lm.'])) {
 			$controlTable->lm = $this->cObj->cObjGet($conf['lm.'], 'lm.');
-			$controlTable->lmTDparams = isset($conf['lm.']['TDParams']) ? $conf['lm.']['TDParams'] : 'valign="top"';
+			$lmTDParams = isset($conf['lm.']['TDParams.'])
+				? $this->cObj->stdWrap($conf['lm.']['TDParams'],$conf['lm.']['TDParams.'])
+				: $conf['lm.']['TDParams'];
+			$controlTable->lmTDparams = isset($lmTDParams)
+				? $lmTDParams
+				: 'valign="top"';
 		}
 		if (is_array($conf['tm.'])) {
 			$controlTable->tm = $this->cObj->cObjGet($conf['tm.'], 'tm.');
-			$controlTable->tmTDparams = isset($conf['tm.']['TDParams']) ? $conf['tm.']['TDParams'] : 'valign="top"';
+			$tmTDParams = isset($conf['tm.']['TDParams.'])
+				? $this->cObj->stdWrap($conf['tm.']['TDParams'],$conf['tm.']['TDParams.'])
+				: $conf['tm.']['TDParams'];
+			$controlTable->tmTDparams = isset($tmTDParams)
+				? $tmTDParams
+				: 'valign="top"';
 		}
 		if (is_array($conf['rm.'])) {
 			$controlTable->rm = $this->cObj->cObjGet($conf['rm.'], 'rm.');
-			$controlTable->rmTDparams = isset($conf['rm.']['TDParams']) ? $conf['rm.']['TDParams'] : 'valign="top"';
+			$rmTDParams = isset($conf['rm.']['TDParams.'])
+				? $this->cObj->stdWrap($conf['rm.']['TDParams'],$conf['rm.']['TDParams.'])
+				: $conf['rm.']['TDParams'];
+			$controlTable->rmTDparams = isset($rmTDParams)
+				? $rmTDParams
+				: 'valign="top"';
 		}
 		if (is_array($conf['bm.'])) {
 			$controlTable->bm = $this->cObj->cObjGet($conf['bm.'], 'bm.');
-			$controlTable->bmTDparams = isset($conf['bm.']['TDParams']) ? $conf['bm.']['TDParams'] : 'valign="top"';
+			$bmTDParams = isset($conf['bm.']['TDParams.'])
+				? $this->cObj->stdWrap($conf['bm.']['TDParams'],$conf['bm.']['TDParams.'])
+				: $conf['bm.']['TDParams'];
+			$controlTable->bmTDparams = isset($bmTDParams)
+				? $bmTDParams
+				: 'valign="top"';
 		}
-		return $controlTable->start($conf['offset'], $conf['cMargins']);
-	}
 
+		$conf['offset'] = isset($conf['offset.'])
+				? $this->cObj->stdWrap($conf['offset'],$conf['offset.'])
+				: $conf['offset'];
+
+		$conf['cMargins'] = isset($conf['cMargins.'])
+				? $this->cObj->stdWrap($conf['cMargins'],$conf['cMargins.'])
+				: $conf['cMargins'];
+
+		$theValue = $controlTable->start($conf['offset'], $conf['cMargins']);
+
+		if (isset($conf['stdWrap.'])) {
+			$theValue = $this->cObj->stdWrap($theValue, $conf['stdWrap.']);
+		}
+
+		return $theValue;
+
+	}
 }
 
 
