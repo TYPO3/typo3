422fd2b382303b86cf312ca762a30ccdc5db5a2f
Jeff Segars <jsegars@alumni.rice.edu>
2010-11-09 17:13:30 +0000

    Fixed bug #16262: Link browser shows wrong title for pages and tt_content elements (Thanks to Reinhard Führicht)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@9323 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/class.browse_links.php
+++ typo3/class.browse_links.php
@@ -1853,7 +1853,7 @@ class browse_links {
 			$titleLen=intval($GLOBALS['BE_USER']->uc['titleLen']);
 			$mainPageRec = t3lib_BEfunc::getRecordWSOL('pages',$expPageId);
 			$picon = t3lib_iconWorks::getSpriteIconForRecord('pages', $mainPageRec);
-			$picon.= htmlspecialchars(t3lib_div::fixed_lgd_cs($mainPageRec['title'],$titleLen));
+			$picon .= t3lib_BEfunc::getRecordTitle('pages', $mainPageRec, TRUE);
 			$out.=$picon.'<br />';
 
 				// Look up tt_content elements from the expanded page:
@@ -1883,7 +1883,7 @@ class browse_links {
 						$arrCol.
 						'<a href="#" onclick="return link_typo3Page(\''.$expPageId.'\',\'#'.$row['uid'].'\');">'.
 						$icon.
-						htmlspecialchars(t3lib_div::fixed_lgd_cs($row['header'],$titleLen)).
+						t3lib_BEfunc::getRecordTitle('tt_content', $row, TRUE) .
 						'</a><br />';
 
 					// Finding internal anchor points:
