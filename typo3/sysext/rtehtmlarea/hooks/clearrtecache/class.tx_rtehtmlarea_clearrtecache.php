c3119a1e7014e364085966e7b65b5f1eea0be398
Stanislas Rolland <typo3@sjbr.ca>
2010-11-12 15:34:58 +0000

    Added feature #16365: htmlArea RTE: Delete compressed files when clearing RTE cache
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@9344 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/sysext/rtehtmlarea/hooks/clearrtecache/class.tx_rtehtmlarea_clearrtecache.php
+++ typo3/sysext/rtehtmlarea/hooks/clearrtecache/class.tx_rtehtmlarea_clearrtecache.php
@@ -32,6 +32,7 @@
  */
 class tx_rtehtmlarea_clearrtecache {
 	public static function clearTempDir() {
+			// Delete all files in typo3temp/rtehtmlarea
 		$tempPath = t3lib_div::resolveBackPath(PATH_typo3.'../typo3temp/rtehtmlarea/');
 		$handle = @opendir($tempPath);
 		if ($handle !== FALSE) {
@@ -45,6 +46,22 @@ class tx_rtehtmlarea_clearrtecache {
 			}
 			closedir($handle);
 		}
+			// Delete all files in typo3temp/compressor with names that start with "htmlarea"
+		$tempPath = t3lib_div::resolveBackPath(PATH_typo3.'../typo3temp/compressor/');
+		$handle = @opendir($tempPath);
+		if ($handle !== FALSE) {
+			while (($file = readdir($handle)) !== FALSE) {
+				if (substr($file, 0, 8) === 'htmlarea') {
+					$tempFile = $tempPath . $file;
+					if (is_file($tempFile)) {
+						unlink($tempFile);
+					}
+				}
+			}
+			closedir($handle);
+		}
+			// Log the action
+		$GLOBALS['BE_USER']->writelog(3, 1, 0, 0, 'htmlArea RTE: User %s has cleared the RTE cache', array($GLOBALS['BE_USER']->user['username']));
 	}
 }
 if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/rtehtmlarea/hooks/clearrtecache/class.tx_rtehtmlarea_clearrtecache.php']) {
