8ea36314ebe3bbcc3c6aa02da6c4ffa94d5f6d53
Ingo Renner <ingo.renner@typo3.org>
2010-11-13 18:06:13 +0000

    fixed a typo in a variable name
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@9372 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/sysext/reports/reports/status/class.tx_reports_reports_status_warningmessagepostprocessor.php
+++ typo3/sysext/reports/reports/status/class.tx_reports_reports_status_warningmessagepostprocessor.php
@@ -41,7 +41,7 @@ class tx_reports_reports_status_WarningMessagePostProcessor {
 	 * @param	array	An array of messages related to already found issues.
 	 * @return	void
 	 */
-	public function displayWarningMessages_postProcess(array &$warningMesasges) {
+	public function displayWarningMessages_postProcess(array &$warningMessages) {
 
 			// get highest severity
 		$registry = t3lib_div::makeInstance('t3lib_Registry');
@@ -54,15 +54,15 @@ class tx_reports_reports_status_WarningMessagePostProcessor {
 		if (!is_null($highestSeverity)) {
 				// status update has run, so taking over control over the core messages
 			unset(
-				$warningMesasges['install_password'],
-				$warningMesasges['backend_admin'],
-				$warningMesasges['install_enabled'],
-				$warningMesasges['install_encryption'],
-				$warningMesasges['file_deny_pattern'],
-				$warningMesasges['file_deny_htaccess'],
-				$warningMesasges['install_update'],
-				$warningMesasges['backend_reference'],
-				$warningMesasges['memcached']
+				$warningMessages['install_password'],
+				$warningMessages['backend_admin'],
+				$warningMessages['install_enabled'],
+				$warningMessages['install_encryption'],
+				$warningMessages['file_deny_pattern'],
+				$warningMessages['file_deny_htaccess'],
+				$warningMessages['install_update'],
+				$warningMessages['backend_reference'],
+				$warningMessages['memcached']
 			);
 
 			if ($highestSeverity > tx_reports_reports_status_Status::OK) {
@@ -70,7 +70,7 @@ class tx_reports_reports_status_WarningMessagePostProcessor {
 					// the admin should take a look at the detailed status report
 				$GLOBALS['LANG']->includeLLFile('EXT:reports/reports/locallang.xml');
 
-				$warningMesasges['tx_reports_status_notification'] = sprintf(
+				$warningMessages['tx_reports_status_notification'] = sprintf(
 					$GLOBALS['LANG']->getLL('status_problemNotification'),
 					'<a href="mod.php?M=tools_txreportsM1&SET[function]=tx_reports.status">',
 					'</a>'
