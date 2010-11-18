13ea64a7bd15892f562d493ae2b9e7de996f5b22
Ernesto Baschny <ernst@cron-it.de>
2010-11-18 03:29:42 +0000

    Updating version number to  4.5-dev after release of 4.5.0beta1
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@9489 709f56b5-9817-0410-a4d7-c38de5d9e867

--- t3lib/config_default.php
+++ t3lib/config_default.php
@@ -409,7 +409,7 @@ $TYPO3_CONF_VARS = array(
 $T3_VAR = array();	// Initialize.
 
 	// TYPO3 version
-$TYPO_VERSION = '4.5.0beta1';	// deprecated: use the constants defined below
+$TYPO_VERSION = '4.5-dev';	// deprecated: use the constants defined below
 define('TYPO3_version', $TYPO_VERSION);
 define('TYPO3_branch', '4.5');
 define('TYPO3_copyright_year', '1998-2010');
