a7bca5063db7990896cef55e386473eb9e723f3c
Steffen Kamper <info@sk-typo3.de>
2010-11-24 10:27:19 +0000

    Fixed bug #14050: CleanUp - CGL format of t3lib files - t3lib_flashmessage
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@9538 709f56b5-9817-0410-a4d7-c38de5d9e867

--- t3lib/class.t3lib_flashmessage.php
+++ t3lib/class.t3lib_flashmessage.php
@@ -1,29 +1,29 @@
 <?php
 /***************************************************************
-*  Copyright notice
-*
-*  (c) 2009-2010 Ingo Renner <ingo@typo3.org>
-*  All rights reserved
-*
-*  This script is part of the TYPO3 project. The TYPO3 project is
-*  free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License, or
-*  (at your option) any later version.
-*
-*  The GNU General Public License can be found at
-*  http://www.gnu.org/copyleft/gpl.html.
-*  A copy is found in the textfile GPL.txt and important notices to the license
-*  from the author is found in LICENSE.txt distributed with these scripts.
-*
-*
-*  This script is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  This copyright notice MUST APPEAR in all copies of the script!
-***************************************************************/
+ *  Copyright notice
+ *
+ *  (c) 2009-2010 Ingo Renner <ingo@typo3.org>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *  A copy is found in the textfile GPL.txt and important notices to the license
+ *  from the author is found in LICENSE.txt distributed with these scripts.
+ *
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
 
 
 /**
@@ -48,8 +48,8 @@ class t3lib_FlashMessage extends t3lib_message_AbstractMessage {
 	 * @param	string	The message.
 	 * @param	string	Optional message title.
 	 * @param	integer	Optional severity, must be either of t3lib_FlashMessage::INFO, t3lib_FlashMessage::OK,
-	 *                  t3lib_FlashMessage::WARNING or t3lib_FlashMessage::ERROR. Default is t3lib_FlashMessage::OK.
-	 * @param	bool    Optional, defines whether the message should be stored in the session or only for one request (default)
+	 *				  t3lib_FlashMessage::WARNING or t3lib_FlashMessage::ERROR. Default is t3lib_FlashMessage::OK.
+	 * @param	bool	Optional, defines whether the message should be stored in the session or only for one request (default)
 	 * @return	void
 	 */
 	public function __construct($message, $title = '', $severity = self::OK, $storeInSession = FALSE) {
@@ -86,11 +86,11 @@ class t3lib_FlashMessage extends t3lib_message_AbstractMessage {
 	 */
 	public function render() {
 		$classes = array(
-			self::NOTICE  => 'notice',
-			self::INFO    => 'information',
-			self::OK      => 'ok',
+			self::NOTICE =>  'notice',
+			self::INFO =>    'information',
+			self::OK =>      'ok',
 			self::WARNING => 'warning',
-			self::ERROR   => 'error',
+			self::ERROR =>   'error',
 		);
 
 		$title = '';
@@ -99,9 +99,9 @@ class t3lib_FlashMessage extends t3lib_message_AbstractMessage {
 		}
 
 		$message = '<div class="typo3-message message-' . $classes[$this->severity] . '">'
-			. $title
-			. '<div class="message-body">' . $this->message . '</div>'
-			. '</div>';
+				. $title
+				. '<div class="message-body">' . $this->message . '</div>'
+				. '</div>';
 
 		return $message;
 	}
@@ -109,7 +109,7 @@ class t3lib_FlashMessage extends t3lib_message_AbstractMessage {
 }
 
 
-if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['t3lib/class.t3lib_flashmessage.php'])	{
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['t3lib/class.t3lib_flashmessage.php']) {
 	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['t3lib/class.t3lib_flashmessage.php']);
 }
 
