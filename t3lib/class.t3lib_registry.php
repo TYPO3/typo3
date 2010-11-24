eebc289a400a9c4b57b08c6fd3f08cbe7157887f
Steffen Kamper <info@sk-typo3.de>
2010-11-24 13:54:36 +0000

    Fixed bug #14050: CleanUp - CGL format of t3lib files - t3lib_registry
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@9564 709f56b5-9817-0410-a4d7-c38de5d9e867

--- t3lib/class.t3lib_registry.php
+++ t3lib/class.t3lib_registry.php
@@ -1,30 +1,30 @@
 <?php
 /***************************************************************
-*  Copyright notice
-*
-*  (c) 2009-2010 Ingo Renner <ingo@typo3.org>
-*
-*  All rights reserved
-*
-*  This script is part of the TYPO3 project. The TYPO3 project is
-*  free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License, or
-*  (at your option) any later version.
-*
-*  The GNU General Public License can be found at
-*  http://www.gnu.org/copyleft/gpl.html.
-*  A copy is found in the textfile GPL.txt and important notices to the license
-*  from the author is found in LICENSE.txt distributed with these scripts.
-*
-*
-*  This script is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  This copyright notice MUST APPEAR in all copies of the script!
-***************************************************************/
+ *  Copyright notice
+ *
+ *  (c) 2009-2010 Ingo Renner <ingo@typo3.org>
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *  A copy is found in the textfile GPL.txt and important notices to the license
+ *  from the author is found in LICENSE.txt distributed with these scripts.
+ *
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
 
 
 /**
@@ -92,9 +92,9 @@ class t3lib_Registry implements t3lib_Singleton {
 			$GLOBALS['TYPO3_DB']->exec_INSERTquery(
 				'sys_registry',
 				array(
-					'entry_namespace' => $namespace,
-					'entry_key'       => $key,
-					'entry_value'     => $serializedValue
+					 'entry_namespace' => $namespace,
+					 'entry_key' => $key,
+					 'entry_value' => $serializedValue
 				)
 			);
 		} else {
@@ -103,7 +103,7 @@ class t3lib_Registry implements t3lib_Singleton {
 				'entry_namespace = ' . $GLOBALS['TYPO3_DB']->fullQuoteStr($namespace, 'sys_registry')
 				. ' AND entry_key = ' . $GLOBALS['TYPO3_DB']->fullQuoteStr($key, 'sys_registry'),
 				array(
-					'entry_value' => $serializedValue
+					 'entry_value' => $serializedValue
 				)
 			);
 		}
@@ -196,7 +196,7 @@ class t3lib_Registry implements t3lib_Singleton {
 }
 
 
-if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['t3lib/class.t3lib_registry.php'])	{
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['t3lib/class.t3lib_registry.php']) {
 	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['t3lib/class.t3lib_registry.php']);
 }
 
