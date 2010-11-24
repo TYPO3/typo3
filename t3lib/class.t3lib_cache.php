6d9216323a0e1209cf815c37bf65cf0b36626e62
Steffen Kamper <info@sk-typo3.de>
2010-11-24 08:58:52 +0000

    Fixed bug #14050: CleanUp - CGL format of t3lib files - t3lib_cache
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@9526 709f56b5-9817-0410-a4d7-c38de5d9e867

--- t3lib/class.t3lib_cache.php
+++ t3lib/class.t3lib_cache.php
@@ -1,26 +1,26 @@
 <?php
 /***************************************************************
-*  Copyright notice
-*
-*  (c) 2008-2010 Ingo Renner <ingo@typo3.org>
-*  All rights reserved
-*
-*  This script is part of the TYPO3 project. The TYPO3 project is
-*  free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License, or
-*  (at your option) any later version.
-*
-*  The GNU General Public License can be found at
-*  http://www.gnu.org/copyleft/gpl.html.
-*
-*  This script is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  This copyright notice MUST APPEAR in all copies of the script!
-***************************************************************/
+ *  Copyright notice
+ *
+ *  (c) 2008-2010 Ingo Renner <ingo@typo3.org>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
 
 
 /**
@@ -34,7 +34,7 @@ class t3lib_cache {
 	/**
 	 * @var	boolean
 	 */
-	protected static $isCachingFrameworkInitialized = false;
+	protected static $isCachingFrameworkInitialized = FALSE;
 
 	/**
 	 * Initializes the caching framework by loading the cache manager and factory
@@ -47,7 +47,7 @@ class t3lib_cache {
 			$GLOBALS['typo3CacheManager'] = t3lib_div::makeInstance('t3lib_cache_Manager');
 			$GLOBALS['typo3CacheFactory'] = t3lib_div::makeInstance('t3lib_cache_Factory');
 			$GLOBALS['typo3CacheFactory']->setCacheManager($GLOBALS['typo3CacheManager']);
-			self::$isCachingFrameworkInitialized = true;
+			self::$isCachingFrameworkInitialized = TRUE;
 		}
 	}
 
@@ -65,8 +65,8 @@ class t3lib_cache {
 				$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['cache_pages']['backend'],
 				$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['cache_pages']['options']
 			);
-		} catch(t3lib_cache_exception_DuplicateIdentifier $e) {
-				// do nothing, a cache_pages cache already exists
+		} catch (t3lib_cache_exception_DuplicateIdentifier $e) {
+			// do nothing, a cache_pages cache already exists
 		}
 	}
 
@@ -84,8 +84,8 @@ class t3lib_cache {
 				$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['cache_pagesection']['backend'],
 				$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['cache_pagesection']['options']
 			);
-		} catch(t3lib_cache_exception_DuplicateIdentifier $e) {
-				// do nothing, a cache_pagesection cache already exists
+		} catch (t3lib_cache_exception_DuplicateIdentifier $e) {
+			// do nothing, a cache_pagesection cache already exists
 		}
 	}
 
@@ -103,8 +103,8 @@ class t3lib_cache {
 				$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['cache_hash']['backend'],
 				$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['cache_hash']['options']
 			);
-		} catch(t3lib_cache_exception_DuplicateIdentifier $e) {
-				// do nothing, a cache_hash cache already exists
+		} catch (t3lib_cache_exception_DuplicateIdentifier $e) {
+			// do nothing, a cache_hash cache already exists
 		}
 	}
 
@@ -116,10 +116,10 @@ class t3lib_cache {
 	 */
 	public function isCachingFrameworkInitialized() {
 		if (!self::$isCachingFrameworkInitialized
-			&& isset($GLOBALS['typo3CacheManager']) && $GLOBALS['typo3CacheManager'] instanceof t3lib_cache_Manager
-			&& isset($GLOBALS['typo3CacheFactory']) && $GLOBALS['typo3CacheFactory'] instanceof t3lib_cache_Factory
+				&& isset($GLOBALS['typo3CacheManager']) && $GLOBALS['typo3CacheManager'] instanceof t3lib_cache_Manager
+				&& isset($GLOBALS['typo3CacheFactory']) && $GLOBALS['typo3CacheFactory'] instanceof t3lib_cache_Factory
 		) {
-			self::$isCachingFrameworkInitialized = true;
+			self::$isCachingFrameworkInitialized = TRUE;
 		}
 
 		return self::$isCachingFrameworkInitialized;
@@ -138,7 +138,7 @@ class t3lib_cache {
 		} elseif (!TYPO3_UseCachingFramework) {
 			throw new RuntimeException(
 				'The caching framework was already defined to be disabled and cannot be changed. ' .
-				'Please put your call to t3lib_cache::enableCachingFramework() into ext_localconf.php.',
+						'Please put your call to t3lib_cache::enableCachingFramework() into ext_localconf.php.',
 				1253273131
 			);
 		}
@@ -146,7 +146,7 @@ class t3lib_cache {
 }
 
 
-if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['t3lib/class.t3lib_cache.php'])	{
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['t3lib/class.t3lib_cache.php']) {
 	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['t3lib/class.t3lib_cache.php']);
 }
 
