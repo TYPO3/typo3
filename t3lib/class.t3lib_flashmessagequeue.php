d6843a90998504ce6046fb072c4a63b9f7c561e7
Steffen Kamper <info@sk-typo3.de>
2010-11-24 10:36:51 +0000

    Fixed bug #14050: CleanUp - CGL format of t3lib files - t3lib_flashmessagequeue
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@9539 709f56b5-9817-0410-a4d7-c38de5d9e867

--- t3lib/class.t3lib_flashmessagequeue.php
+++ t3lib/class.t3lib_flashmessagequeue.php
@@ -1,29 +1,29 @@
 <?php
 /***************************************************************
-*  Copyright notice
-*
-*  (c) 2009-2010 Rupert Germann <rupi@gmx.li>
-*  All rights reserved
-*
-*  This script is part of the TYPO3 project. The TYPO3 project is
-*  free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License, or
-*  (at your option) any later version.
-*
-*  The GNU General Public License can be found at
-*  http://www.gnu.org/copyleft/gpl.html.
-*  A copy is found in the textfile GPL.txt and important notices to the license
-*  from the author is found in LICENSE.txt distributed with these scripts.
-*
-*
-*  This script is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  This copyright notice MUST APPEAR in all copies of the script!
-***************************************************************/
+ *  Copyright notice
+ *
+ *  (c) 2009-2010 Rupert Germann <rupi@gmx.li>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *  A copy is found in the textfile GPL.txt and important notices to the license
+ *  from the author is found in LICENSE.txt distributed with these scripts.
+ *
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
 
 
 /**
@@ -40,15 +40,16 @@ class t3lib_FlashMessageQueue {
 	/**
 	 * Static class, no instances allowed.
 	 */
-	protected function __construct() {}
+	protected function __construct() {
+	}
 
 
 	/**
 	 * Adds a message either to the BE_USER session (if the $message has the storeInSession flag set)
 	 * or it adds the message to self::$messages.
 	 *
-	 * @param	object 	instance of t3lib_FlashMessage, representing a message
-	 * @return 	void
+	 * @param	object	 instance of t3lib_FlashMessage, representing a message
+	 * @return	 void
 	 */
 	public static function addMessage(t3lib_FlashMessage $message) {
 		if ($message->isSessionMessage()) {
@@ -69,7 +70,7 @@ class t3lib_FlashMessageQueue {
 	 * After fetching the messages the internal queue and the message queue in the session
 	 * will be emptied.
 	 *
-	 * @return 	array 	array of t3lib_FlashMessage objects
+	 * @return	 array	 array of t3lib_FlashMessage objects
 	 */
 	public static function getAllMessagesAndFlush() {
 			// get messages from user session
@@ -122,7 +123,7 @@ class t3lib_FlashMessageQueue {
 }
 
 
-if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['t3lib/class.t3lib_flashmessagequeue.php'])	{
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['t3lib/class.t3lib_flashmessagequeue.php']) {
 	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['t3lib/class.t3lib_flashmessagequeue.php']);
 }
 ?>
