4ef4a4d2480e52b0d946709ed0f6c29cee6b22bb
Xavier Perseguers <typo3@perseguers.ch>
2010-11-30 15:22:57 +0000

    Backported changeset 9689 from trunk: Fixed bug #16501
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/branches/TYPO3_4-3@9691 709f56b5-9817-0410-a4d7-c38de5d9e867

--- t3lib/class.t3lib_sqlparser.php
+++ t3lib/class.t3lib_sqlparser.php
@@ -1488,11 +1488,13 @@ class t3lib_sqlparser {
 		foreach($components['FIELDS'] as $fN => $fCfg)	{
 			$fieldsKeys[]=$fN.' '.$this->compileFieldCfg($fCfg['definition']);
 		}
-		foreach($components['KEYS'] as $kN => $kCfg)	{
-			if ($kN == 'PRIMARYKEY')	{
-				$fieldsKeys[]='PRIMARY KEY ('.implode(',', $kCfg).')';
-			} elseif ($kN == 'UNIQUE')	{
-				$fieldsKeys[]='UNIQUE '.$kN.' ('.implode(',', $kCfg).')';
+		foreach ($components['KEYS'] as $kN => $kCfg) {
+			if ($kN === 'PRIMARYKEY') {
+				$fieldsKeys[] = 'PRIMARY KEY (' . implode(',', $kCfg) . ')';
+			} elseif ($kN === 'UNIQUE') {
+				$key = key($kCfg);
+				$fields = current($kCfg);
+				$fieldsKeys[] = 'UNIQUE KEY ' . $key . ' (' . implode(',', $fields) . ')';
 			} else {
 				$fieldsKeys[]='KEY '.$kN.' ('.implode(',', $kCfg).')';
 			}
