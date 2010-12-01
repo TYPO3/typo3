05828b068c4190012243313e1282f8f377925e1f
Steffen Kamper <info@sk-typo3.de>
2010-12-01 11:54:32 +0000

    Cleaning of pagetree em_conf
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@9714 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/sysext/pagetree/ext_emconf.php
+++ typo3/sysext/pagetree/ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "pagetree".
 #
-# Auto generated 01-12-2010 12:27
+# Auto generated 01-12-2010 12:53
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -16,7 +16,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'category' => 'be',
 	'dependencies' => 'cms',
 	'conflicts' => '',
-	'priority' => '',
+	'priority' => 'top',
 	'loadOrder' => '',
 	'module' => '',
 	'doNotLoadInFE' => 1,
@@ -33,7 +33,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
 	'version' => '1.1.0',
-	'_md5_values_when_last_written' => 'a:30:{s:16:"ext_autoload.php";s:4:"b480";s:12:"ext_icon.gif";s:4:"39bd";s:17:"ext_localconf.php";s:4:"d1c7";s:14:"ext_tables.php";s:4:"7d74";s:25:"locallang_contextmenu.xml";s:4:"72dd";s:22:"locallang_pagetree.xml";s:4:"1265";s:38:"classes/class.tx_pagetree_commands.php";s:4:"7371";s:42:"classes/class.tx_pagetree_dataprovider.php";s:4:"eb8e";s:34:"classes/class.tx_pagetree_node.php";s:4:"b0a7";s:44:"classes/class.tx_pagetree_nodecollection.php";s:4:"a52a";s:43:"classes/class.tx_pagetree_stateprovider.php";s:4:"e0ab";s:60:"classes/contextmenu/class.tx_pagetree_contextmenu_action.php";s:4:"efa8";s:66:"classes/contextmenu/class.tx_pagetree_contextmenu_dataprovider.php";s:4:"9ad1";s:58:"classes/extdirect/class.tx_pagetree_extdirect_commands.php";s:4:"eade";s:61:"classes/extdirect/class.tx_pagetree_extdirect_contextmenu.php";s:4:"c7bc";s:54:"classes/extdirect/class.tx_pagetree_extdirect_tree.php";s:4:"b483";s:43:"components/pagetree/pagetree-application.js";s:4:"46be";s:43:"components/pagetree/pagetree-contextmenu.js";s:4:"bfad";s:39:"components/pagetree/pagetree-factory.js";s:4:"d503";s:41:"components/pagetree/pagetree-overrides.js";s:4:"1552";s:36:"components/pagetree/pagetree-tree.js";s:4:"b338";s:36:"components/pagetree/css/pagetree.css";s:4:"9764";s:66:"components/pagetree/javascript/Ext.ux.plugins.TreePanelStateful.js";s:4:"c062";s:41:"components/pagetree/javascript/actions.js";s:4:"3a05";s:45:"components/pagetree/javascript/contextmenu.js";s:4:"0424";s:50:"components/pagetree/javascript/deletiondropzone.js";s:4:"c21f";s:46:"components/pagetree/javascript/featurepanel.js";s:4:"d398";s:43:"components/pagetree/javascript/overrides.js";s:4:"f231";s:42:"components/pagetree/javascript/pagetree.js";s:4:"46d5";s:38:"components/pagetree/javascript/tree.js";s:4:"9dfa";}',
+	'_md5_values_when_last_written' => 'a:25:{s:16:"ext_autoload.php";s:4:"b480";s:12:"ext_icon.gif";s:4:"39bd";s:17:"ext_localconf.php";s:4:"d1c7";s:14:"ext_tables.php";s:4:"7d74";s:25:"locallang_contextmenu.xml";s:4:"72dd";s:22:"locallang_pagetree.xml";s:4:"1265";s:38:"classes/class.tx_pagetree_commands.php";s:4:"7371";s:42:"classes/class.tx_pagetree_dataprovider.php";s:4:"eb8e";s:34:"classes/class.tx_pagetree_node.php";s:4:"b0a7";s:44:"classes/class.tx_pagetree_nodecollection.php";s:4:"a52a";s:43:"classes/class.tx_pagetree_stateprovider.php";s:4:"e0ab";s:60:"classes/contextmenu/class.tx_pagetree_contextmenu_action.php";s:4:"efa8";s:66:"classes/contextmenu/class.tx_pagetree_contextmenu_dataprovider.php";s:4:"9ad1";s:58:"classes/extdirect/class.tx_pagetree_extdirect_commands.php";s:4:"eade";s:61:"classes/extdirect/class.tx_pagetree_extdirect_contextmenu.php";s:4:"c7bc";s:54:"classes/extdirect/class.tx_pagetree_extdirect_tree.php";s:4:"b483";s:36:"components/pagetree/css/pagetree.css";s:4:"9764";s:66:"components/pagetree/javascript/Ext.ux.plugins.TreePanelStateful.js";s:4:"c062";s:41:"components/pagetree/javascript/actions.js";s:4:"3a05";s:45:"components/pagetree/javascript/contextmenu.js";s:4:"0424";s:50:"components/pagetree/javascript/deletiondropzone.js";s:4:"c21f";s:46:"components/pagetree/javascript/featurepanel.js";s:4:"d398";s:43:"components/pagetree/javascript/overrides.js";s:4:"f231";s:42:"components/pagetree/javascript/pagetree.js";s:4:"46d5";s:38:"components/pagetree/javascript/tree.js";s:4:"9dfa";}',
 	'constraints' => array(
 		'depends' => array(
 			'cms' => '',
