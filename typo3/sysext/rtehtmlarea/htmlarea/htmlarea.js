224059fe66a2a45c8882fb9964c4f316ce8022ae
Stanislas Rolland <typo3@sjbr.ca>
2010-12-01 05:47:19 +0000

    Fixed bug #16058: htmlArea RTE: Overlay to choose a color exceeds the viewport if many colors are defined
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/branches/TYPO3_4-4@9703 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/sysext/rtehtmlarea/htmlarea/htmlarea.js
+++ typo3/sysext/rtehtmlarea/htmlarea/htmlarea.js
@@ -3769,6 +3769,7 @@ Ext.ux.form.ColorPaletteField = Ext.extend(Ext.form.TriggerField, {
 		}
 		if (this.menu == null) {
 			this.menu = new Ext.ux.menu.HTMLAreaColorMenu({
+				cls: 'htmlarea-color-menu',
 				hideOnClick: false,
 				colors: this.colors,
 				colorsConfiguration: this.colorsConfiguration,
