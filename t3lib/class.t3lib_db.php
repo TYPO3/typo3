e8c32474a5571336681243465f42090cf056054f
Oliver Hader <oliver.hader@typo3.org>
2010-12-16 13:38:36 +0000

    Fixed bug #15737: quoteStrForLike does not properly escape strings in sql_mode NO_BACKSLASH_ESCAPES
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/branches/TYPO3_4-3@9777 709f56b5-9817-0410-a4d7-c38de5d9e867

--- t3lib/class.t3lib_db.php
+++ t3lib/class.t3lib_db.php
@@ -1039,9 +1039,36 @@ class t3lib_DB {
 					);
 				}
 			}
+			$this->setSqlMode();
 		}
 
 		return $this->link;
+	}
+
+	/**
+	 * Fixes the SQL mode by unsetting NO_BACKSLASH_ESCAPES if found.
+	 *
+	 * @return void
+	 */
+	protected function setSqlMode() {
+		$resource = $this->sql_query('SELECT @@SESSION.sql_mode;');
+		if (is_resource($resource)) {
+			$result = $this->sql_fetch_row($resource);
+			if (isset($result[0]) && $result[0] && strpos($result[0], 'NO_BACKSLASH_ESCAPES') !== FALSE) {
+				$modes = array_diff(
+					t3lib_div::trimExplode(',', $result[0]),
+					array('NO_BACKSLASH_ESCAPES')
+				);
+				$query = 'SET sql_mode=\'' . mysql_real_escape_string(implode(',', $modes)) . '\';';
+				$success = $this->sql_query($query);
+
+				t3lib_div::sysLog(
+					'NO_BACKSLASH_ESCAPES could not be removed from SQL mode: ' . $this->sql_error(),
+					'Core',
+					3
+				);
+			}
+		}
 	}
 
 	/**
