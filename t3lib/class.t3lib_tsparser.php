5d5ca4ef0e87d8624456b91a7c06fefb043b864c
Oliver Hader <oliver.hader@typo3.org>
2010-12-16 13:37:52 +0000

    Fixed bug #16590: t3lib_TSparser::checkIncludeLines() does not check files to be included (thanks to Fabrizio Branca)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/branches/TYPO3_4-2@9772 709f56b5-9817-0410-a4d7-c38de5d9e867

--- t3lib/class.t3lib_tsparser.php
+++ t3lib/class.t3lib_tsparser.php
@@ -527,10 +527,14 @@ class t3lib_TSparser {
 								case 'file':
 									$filename = t3lib_div::getFileAbsFileName(trim($sourceParts[1]));
 									if (strcmp($filename,''))	{	// Must exist and must not contain '..' and must be relative
-										if (@is_file($filename) && filesize($filename)<100000)	{	// Max. 100 KB include files!
-												// check for includes in included text
-											$included_text = self::checkIncludeLines(t3lib_div::getUrl($filename),$cycle_counter+1);
-											$newString.= $included_text.chr(10);
+										if (t3lib_div::verifyFilenameAgainstDenyPattern($filename)) { // Check for allowed files
+											if (@is_file($filename) && filesize($filename)<100000)	{	// Max. 100 KB include files!
+													// check for includes in included text
+												$included_text = self::checkIncludeLines(t3lib_div::getUrl($filename),$cycle_counter+1);
+												$newString.= $included_text.chr(10);
+											}
+										} else {
+											t3lib_div::sysLog('File "' . $filename . '" was not included since it is not allowed due to fileDenyPattern', 'Core', 2);
 										}
 									}
 								break;
