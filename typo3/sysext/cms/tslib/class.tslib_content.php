3c5d15233ca765fabeac21f0600d831595d31cd8
Oliver Hader <oliver.hader@typo3.org>
2010-12-16 13:40:06 +0000

    Fixed bug #15735: FORM content object is susceptible to XSS (thanks to Benjamin Mack)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/branches/TYPO3_4-3@9785 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/sysext/cms/tslib/class.tslib_content.php
+++ typo3/sysext/cms/tslib/class.tslib_content.php
@@ -1809,7 +1809,7 @@ class tslib_cObj {
 			}
 			if ($val && strcspn($val,'#/')) {
 					// label:
-				$confData['label'] = trim($parts[0]);
+				$confData['label'] = t3lib_div::removeXSS(trim($parts[0]));
 					// field:
 				$fParts = explode(',',$parts[1]);
 				$fParts[0]=trim($fParts[0]);
@@ -1835,6 +1835,7 @@ class tslib_cObj {
 				} else {
 					$confData['fieldname'] = str_replace(' ','_',trim($typeParts[0]));
 				}
+				$confData['fieldname'] = htmlspecialchars($confData['fieldname']);
 				$fieldCode='';
 
 				if ($conf['wrapFieldName'])	{
