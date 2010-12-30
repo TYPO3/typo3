003e1c34c47b4df49366dfc77cf1cc6844113612
Bastian Waidelich <bastian@typo3.org>
2010-12-30 17:18:12 +0000

    [~TASK] Fluid (Tests): Fixed some testcase class names

--- typo3/sysext/fluid/Tests/Unit/Core/Parser/Interceptor/EscapeTest.php
+++ typo3/sysext/fluid/Tests/Unit/Core/Parser/Interceptor/EscapeTest.php
@@ -25,7 +25,7 @@
  *
  * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License, version 3 or later
  */
-class Tx_Fluid_Core_Parser_Interceptor_EscapeTest extends Tx_Extbase_BaseTestCase {
+class Tx_Fluid_Tests_Unit_Core_Parser_Interceptor_EscapeTest extends Tx_Extbase_BaseTestCase {
 
 	/**
 	 * @test
--- typo3/sysext/fluid/Tests/Unit/View/StandaloneViewTest.php
+++ typo3/sysext/fluid/Tests/Unit/View/StandaloneViewTest.php
@@ -25,7 +25,7 @@
  *
  * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License, version 3 or later
  */
-class Tx_Fluid_View_StandaloneViewTest extends Tx_Extbase_BaseTestCase {
+class Tx_Fluid_Tests_Unit_View_StandaloneViewTest extends Tx_Extbase_BaseTestCase {
 
 	/**
 	 * @var Tx_Fluid_View_StandaloneView
