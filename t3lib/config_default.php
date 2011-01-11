787c4ddb8ea4944a4bdbfe9c0f1e393888bf6042
Christian Kuhn <lolli@schwarzbu.ch>
2011-01-11 15:34:33 +0000

    Fixed bug #16967: [Caching framework] Remove not exising "GlobalsBackend" from config_default.php
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/branches/TYPO3_4-3@10025 709f56b5-9817-0410-a4d7-c38de5d9e867

--- t3lib/config_default.php
+++ t3lib/config_default.php
@@ -115,7 +115,6 @@ $TYPO3_CONF_VARS = array(
 			'cacheBackends' => array(
 				't3lib_cache_backend_DbBackend'              => 't3lib/cache/backend/class.t3lib_cache_backend_dbbackend.php:t3lib_cache_backend_DbBackend',
 				't3lib_cache_backend_FileBackend'            => 't3lib/cache/backend/class.t3lib_cache_backend_filebackend.php:t3lib_cache_backend_FileBackend',
-				't3lib_cache_backend_GlobalsBackend'         => 't3lib/cache/backend/class.t3lib_cache_backend_globalsbackend.php:t3lib_cache_backend_GlobalsBackend',
 				't3lib_cache_backend_MemcachedBackend'       => 't3lib/cache/backend/class.t3lib_cache_backend_memcachedbackend.php:t3lib_cache_backend_MemcachedBackend',
 				't3lib_cache_backend_ApcBackend'             => 't3lib/cache/backend/class.t3lib_cache_backend_apcbackend.php:t3lib_cache_backend_ApcBackend',
 				't3lib_cache_backend_NullBackend'            => 't3lib/cache/backend/class.t3lib_cache_backend_nullbackend.php:t3lib_cache_backend_NullBackend',
