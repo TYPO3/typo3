23f200b24c3ce7e61696a0b5a812e0bdff09c43c
tolleiv <tolleiv@743128fe-103e-dd11-99c4-001b210b3e58>
2011-01-18 12:50:08 +0000

    Fixed bug #12347: Incorrect registration of status report
    git-svn-id: https://svn.typo3.org/TYPO3v4/CoreProjects/workspaces/workspaces/trunk@3860 743128fe-103e-dd11-99c4-001b210b3e58

--- typo3/sysext/workspaces/ext_tables.php
+++ typo3/sysext/workspaces/ext_tables.php
@@ -32,7 +32,7 @@ if (TYPO3_MODE == 'BE' && !(TYPO3_REQUESTTYPE & TYPO3_REQUESTTYPE_INSTALL)) {
 	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ExtDirect']['TYPO3.Ajax.ExtDirect.ToolbarMenu'] = t3lib_extMgm::extPath($_EXTKEY) . 'Classes/ExtDirect/ToolbarMenu.php:tx_Workspaces_ExtDirect_ToolbarMenu';
 
 		// register the reports statusprovider
-	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['reports']['tx_reports']['status']['providers']['Configuration'][] = 'Tx_Workspaces_Reports_StatusProvider';
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['reports']['tx_reports']['status']['providers']['configuration'][] = 'Tx_Workspaces_Reports_StatusProvider';
 
 
 }
