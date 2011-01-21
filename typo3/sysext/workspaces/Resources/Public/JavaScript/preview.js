723195991d83823fad4a1dc12723b7a1a8608fe4
tolleiv <tolleiv@743128fe-103e-dd11-99c4-001b210b3e58>
2011-01-21 09:27:52 +0000

    Fixed bug #11539: Split view
    git-svn-id: https://svn.typo3.org/TYPO3v4/CoreProjects/workspaces/workspaces/trunk@3909 743128fe-103e-dd11-99c4-001b210b3e58

--- typo3/sysext/workspaces/Resources/Public/JavaScript/preview.js
+++ typo3/sysext/workspaces/Resources/Public/JavaScript/preview.js
@@ -25,6 +25,8 @@
  ***************************************************************/
 
 Ext.onReady(function() {
+	var iconClsChecked = 't3-icon t3-icon-status t3-icon-status-status t3-icon-status-checked';
+	var iconClsEmpty = 't3-icon t3-icon-empty t3-icon-empty-empty t3-icon-empty';
 	var viewport = new Ext.Viewport({
 		layout: 'border',
 		items: [{
@@ -35,96 +37,222 @@ Ext.onReady(function() {
 			plugins : [{
 				ptype : 'Ext.ux.plugins.TabStripContainer',
 				id: 'controls',
-				width: 400,
-				items: [{
-					xtype: 'button',
-					id: 'sizeSliderButtonLive',
-					cls: 'sliderButton',
-					text: TYPO3.LLL.Workspaces.livePreview,
-					width: 100,
-					listeners: {
-						click: {
-							fn: function () {
-								Ext.getCmp('sizeSlider').setValue(0);
-							}
-						}
-					}
-				},
-				{
-					xtype: 'slider',
-					id: 'sizeSlider',
-					margins: '0 10 0 10',
-					maxValue: 100,
-					minValue: 0,
-					value: 100,
-					flex: 1,
-					listeners: {
-						change: {
-							fn: function resizeFromValue(slider, newValue, thumb) {
-								var height = Ext.getCmp('wsPanel').getHeight();
-								Ext.getCmp('liveContainer').setHeight(height * (100 - newValue) / 100);
-								Ext.getCmp('visualPanel').setHeight(height);
-							}
-						}
-					}
-				},
-				{
-					xtype: 'button',
-					id: 'sizeSliderButtonWorkspace',
-					cls: 'sliderButton',
-					text: TYPO3.LLL.Workspaces.workspacePreview,
-					width: 100,
-					listeners: {
-						click: {
-							fn: function () {
-								Ext.getCmp('sizeSlider').setValue(100);
-							}
-						}
-					}
-				}]
+				width: 600,
+				items: [
+						{
+							xtype: 'panel',
+							id: 'slider',
+							width: 460,
+							layout: 'hbox',
+							items: [
+								{
+									xtype: 'button',
+									id: 'sizeSliderButtonLive',
+									cls: 'sliderButton',
+									text: TYPO3.LLL.Workspaces.livePreview,
+									tooltip: TYPO3.LLL.Workspaces.livePreviewDetail,
+									width: 100,
+									listeners: {
+										click: {
+											fn: function () {
+												Ext.getCmp('sizeSlider').setValue(0);
+											}
+										}
+									}
+								},
+								{
+									xtype: 'slider',
+									id: 'sizeSlider',
+									margins: '0 10 0 10',
+									maxValue: 100,
+									minValue: 0,
+									value: 100,
+									width: 200,
+									flex: 1,
+									listeners: {
+										change: {
+											fn: function resizeFromValue(slider, newValue, thumb) {
+												var height = Ext.getCmp('wsPanel').getHeight();
+												Ext.getCmp('liveContainer').setHeight(height * (100 - newValue) / 100);
+												//Ext.getCmp('visualPanel').setHeight(height);
+											}
+										}
+									}
+								},
+								{
+									xtype: 'button',
+									id: 'sizeSliderButtonWorkspace',
+									cls: 'sliderButton',
+									text: TYPO3.LLL.Workspaces.workspacePreview,
+									tooltip: TYPO3.LLL.Workspaces.workspacePreviewDetail,
+									width: 100,
+									listeners: {
+										click: {
+											fn: function () {
+												Ext.getCmp('sizeSlider').setValue(100);
+											}
+										}
+									}
+								}
+							]
+						},
+						{
+							xtype: 'toolbar',
+							id: 'visual-mode-toolbar',
+							items: [{
+								iconCls: 'x-btn-icon t3-icon t3-icon-actions t3-icon-actions-system t3-icon-system-options-view',
+								id: 'visual-mode-options',
+								menu: {
+									id: 'visual-mode-selector',
+									items: [{
+										text: TYPO3.LLL.Workspaces.modeSlider,
+										id: 'visual-mode-selector-slider',
+										iconCls: iconClsChecked,
+										handler: function(){
+											Ext.getCmp('visualPanel-hbox').hide();
+											Ext.getCmp('visualPanel-vbox').hide();
+											Ext.getCmp('visualPanel').show();
+											Ext.getCmp('slider').show();
+											Ext.select('#visual-mode-selector ul li a img.t3-icon-status-checked').removeClass(iconClsChecked.split(" "))
+											Ext.getCmp('visual-mode-selector-slider').setIconClass(iconClsChecked);
+											Ext.getCmp('visual-mode-selector-hbox').setIconClass(iconClsEmpty);
+											Ext.getCmp('visual-mode-selector-vbox').setIconClass(iconClsEmpty);
+										}
+									},{
+										text: TYPO3.LLL.Workspaces.modeVbox,
+										id: 'visual-mode-selector-vbox',
+										iconCls: iconClsEmpty,
+										handler: function() {
+											Ext.getCmp('visualPanel-hbox').hide();
+											Ext.getCmp('visualPanel-vbox').show();
+											Ext.getCmp('visualPanel').hide();
+											Ext.getCmp('slider').hide();
+											Ext.select('#visual-mode-selector ul li a img.t3-icon-status-checked').removeClass(iconClsChecked.split(" "))
+											Ext.getCmp('visual-mode-selector-slider').setIconClass(iconClsEmpty);
+											Ext.getCmp('visual-mode-selector-vbox').setIconClass(iconClsChecked)
+											Ext.getCmp('visual-mode-selector-hbox').setIconClass(iconClsEmpty);
+										}
+									},{
+										text: TYPO3.LLL.Workspaces.modeHbox,
+										id: 'visual-mode-selector-hbox',
+										iconCls: iconClsEmpty,
+										handler: function(){
+											Ext.getCmp('visualPanel-hbox').show();
+											Ext.getCmp('visualPanel-vbox').hide();
+											Ext.getCmp('visualPanel').hide();
+											Ext.getCmp('slider').hide();
+											Ext.select('#visual-mode-selector ul li a img.t3-icon-status-checked').removeClass(iconClsChecked.split(" "))
+											Ext.getCmp('visual-mode-selector-slider').setIconClass(iconClsEmpty);
+											Ext.getCmp('visual-mode-selector-vbox').setIconClass(iconClsEmpty);
+											Ext.getCmp('visual-mode-selector-hbox').setIconClass(iconClsChecked);
+										}
+									}]
+								}
+							}]
+						}]
 			}],
 			items: [{
 				title: TYPO3.LLL.Workspaces.visualPreview,
 				id: 'wsVisual',
 				layout: 'fit',
+				anchor: '100% 100%',
 				items: [{
-					layout: 'fit',
-					x: 0, y:0,
+					layout: 'absolute',
 					anchor: '100% 100%',
-					autoScroll: true,
+					x: 0, y:0,
 					items: [{
-						layout: 'absolute',
-						id: 'visualPanel',
-						items: [{
+							layout: 'fit',
 							x: 0, y:0,
 							anchor: '100% 100%',
-							id: 'wsContainer',
-							layout: 'absolute',
-							autoScroll: false,
-							items:[{
-								xtype: 'iframePanel',
-								x: 0, y:0,
-								id: 'wsPanel',
-								doMask: false,
-								src: wsUrl,
-								autoScroll: false
+							autoScroll: true,
+							items: [{
+								layout: 'absolute',
+								id: 'visualPanel',
+								items: [{
+									x: 0, y:0,
+									anchor: '100% 100%',
+									id: 'wsContainer',
+									layout: 'absolute',
+									autoScroll: false,
+									items:[{
+										xtype: 'iframePanel',
+										x: 0, y:0,
+										id: 'wsPanel',
+										doMask: false,
+										src: wsUrl,
+										autoScroll: false
+									}]
+								},{
+									x: 0, y:0,
+									anchor: '100% 0%',
+									id: 'liveContainer',
+									layout: 'absolute',
+									bodyStyle: 'height:0px;border-bottom: 2px solid red;',
+									autoScroll: false,
+									items:[{
+										xtype: 'iframePanel',
+										x: 0, y:0,
+										id: 'livePanel',
+										doMask: false,
+										src: liveUrl,
+										autoScroll: false
+									}]
+								}]
 							}]
 						},{
+							layout: 'hbox',
+							hidden: true,
 							x: 0, y:0,
-							anchor: '100% 0%',
-							id: 'liveContainer',
-							layout: 'absolute',
-							bodyStyle: 'height:0px;border-bottom: 2px solid red;',
-							autoScroll: false,
-							items:[{
+							anchor: '100% 100%',
+							layoutConfig: {
+								align : 'stretch',
+								pack  : 'start'
+							},
+							id: 'visualPanel-hbox',
+							items: [{
 								xtype: 'iframePanel',
 								x: 0, y:0,
-								id: 'livePanel',
+								id: 'wsPanel-hbox',
+								doMask: false,
+								src: wsUrl,
+								autoScroll: false,
+								flex: 1
+							},{
+								xtype: 'iframePanel',
+								x: 0, y:0,
+								id: 'livePanel-hbox',
 								doMask: false,
 								src: liveUrl,
-								autoScroll: false
+								autoScroll: false,
+								flex: 1
 							}]
-						}]
+					},{
+							layout: 'vbox',
+							hidden: true,
+							x: 0, y:0,
+							anchor: '100% 100%',
+							layoutConfig: {
+								align : 'stretch',
+								pack  : 'start'
+							},
+							id: 'visualPanel-vbox',
+							items: [{
+								xtype: 'iframePanel',
+								x: 0, y:0,
+								id: 'wsPanel-vbox',
+								doMask: false,
+								src: wsUrl,
+								autoScroll: false,
+								flex: 1
+							},{
+								xtype: 'iframePanel',
+								x: 0, y:0,
+								id: 'livePanel-vbox',
+								doMask: false,
+								src: liveUrl,
+								autoScroll: false,
+								flex: 1
+							}]
 					}]
 				}]
 			},{
