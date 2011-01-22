a8798a10282fdb1fca328d06271cf0ee7ab0638d
Ingo Renner <ingo.renner@typo3.org>
2011-01-22 00:13:20 +0000

    [reports] Changed version to 1.1.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@10234 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/sysext/reports/ext_emconf.php
+++ typo3/sysext/reports/ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "reports".
 #
-# Auto generated 23-08-2010 16:57
+# Auto generated 22-01-2011 01:12
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '1.0.0',
+	'version' => '1.1.0',
 	'doNotLoadInFE' => 1,
 	'constraints' => array(
 		'depends' => array(
@@ -39,7 +39,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:19:{s:9:"ChangeLog";s:4:"f713";s:16:"ext_autoload.php";s:4:"ef8b";s:12:"ext_icon.gif";s:4:"691d";s:14:"ext_tables.php";s:4:"5446";s:42:"interfaces/interface.tx_reports_report.php";s:4:"6bfa";s:50:"interfaces/interface.tx_reports_statusprovider.php";s:4:"d228";s:12:"mod/conf.php";s:4:"7fd9";s:13:"mod/index.php";s:4:"f669";s:17:"mod/locallang.xml";s:4:"7782";s:18:"mod/mod_styles.css";s:4:"633e";s:21:"mod/mod_template.html";s:4:"ff97";s:18:"mod/moduleicon.gif";s:4:"2ac9";s:43:"reports/class.tx_reports_reports_status.php";s:4:"c96d";s:21:"reports/locallang.xml";s:4:"6d21";s:70:"reports/status/class.tx_reports_reports_status_configurationstatus.php";s:4:"10a5";s:65:"reports/status/class.tx_reports_reports_status_securitystatus.php";s:4:"b641";s:57:"reports/status/class.tx_reports_reports_status_status.php";s:4:"9076";s:63:"reports/status/class.tx_reports_reports_status_systemstatus.php";s:4:"4e0a";s:62:"reports/status/class.tx_reports_reports_status_typo3status.php";s:4:"098f";}',
+	'_md5_values_when_last_written' => 'a:23:{s:9:"ChangeLog";s:4:"3b22";s:16:"ext_autoload.php";s:4:"9ec3";s:12:"ext_icon.gif";s:4:"691d";s:17:"ext_localconf.php";s:4:"02ec";s:14:"ext_tables.php";s:4:"a108";s:42:"interfaces/interface.tx_reports_report.php";s:4:"bc72";s:50:"interfaces/interface.tx_reports_statusprovider.php";s:4:"2d8b";s:12:"mod/conf.php";s:4:"7fd9";s:13:"mod/index.php";s:4:"804d";s:17:"mod/locallang.xml";s:4:"7782";s:18:"mod/mod_styles.css";s:4:"905d";s:21:"mod/mod_template.html";s:4:"ff97";s:18:"mod/moduleicon.gif";s:4:"2ac9";s:43:"reports/class.tx_reports_reports_status.php";s:4:"e7d2";s:21:"reports/locallang.xml";s:4:"8f45";s:70:"reports/status/class.tx_reports_reports_status_configurationstatus.php";s:4:"cf16";s:65:"reports/status/class.tx_reports_reports_status_securitystatus.php";s:4:"48b4";s:57:"reports/status/class.tx_reports_reports_status_status.php";s:4:"d3dc";s:63:"reports/status/class.tx_reports_reports_status_systemstatus.php";s:4:"35a5";s:62:"reports/status/class.tx_reports_reports_status_typo3status.php";s:4:"99f8";s:78:"reports/status/class.tx_reports_reports_status_warningmessagepostprocessor.php";s:4:"f71c";s:55:"tasks/class.tx_reports_tasks_systemstatusupdatetask.php";s:4:"5aa8";s:77:"tasks/class.tx_reports_tasks_systemstatusupdatetasknotificationemailfield.php";s:4:"5e30";}',
 	'suggests' => array(
 	),
 );
