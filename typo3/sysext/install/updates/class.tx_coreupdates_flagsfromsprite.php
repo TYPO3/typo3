9c83693798a546e1de38f9ebbaeb125281a1b192
Ernesto Baschny <ernst@cron-it.de>
2011-01-25 22:29:04 +0000

    Fixed issue #17316: Flag sprites was not updated after renaming canada.gif back to ca.gif and "multi-language" was missing
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/trunk@10313 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/sysext/install/updates/class.tx_coreupdates_flagsfromsprite.php
+++ typo3/sysext/install/updates/class.tx_coreupdates_flagsfromsprite.php
@@ -68,7 +68,15 @@ class tx_coreupdates_flagsfromsprite extends Tx_Install_Updates_Base {
 			$sql = 'UPDATE sys_language SET flag=REPLACE(flag, \'.gif\', \'\') WHERE flag LIKE \'%.gif\'';
 			$res = $GLOBALS['TYPO3_DB']->sql_query($sql);
 			$dbQueries[] = $sql;
+			if ($GLOBALS['TYPO3_DB']->sql_error()) {
+				$customMessages = 'SQL-ERROR: ' . htmlspecialchars($GLOBALS['TYPO3_DB']->sql_error());
+			} else {
+				$result = TRUE;
+			}
 
+			$sql = 'UPDATE sys_language SET flag=\'multiple\' WHERE flag=\'multi-language\'';
+			$res = $GLOBALS['TYPO3_DB']->sql_query($sql);
+			$dbQueries[] = $sql;
 			if ($GLOBALS['TYPO3_DB']->sql_error()) {
 				$customMessages = 'SQL-ERROR: ' . htmlspecialchars($GLOBALS['TYPO3_DB']->sql_error());
 			} else {
