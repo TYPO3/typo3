aaa52b80fd15561a22e78e66f50fb09efe7b154b
tolleiv <tolleiv@743128fe-103e-dd11-99c4-001b210b3e58>
2011-01-25 00:03:43 +0000

    Fixed bug #12406: Not possible to publish change of IRRE ordering
    git-svn-id: https://svn.typo3.org/TYPO3v4/CoreProjects/workspaces/workspaces/trunk@3981 743128fe-103e-dd11-99c4-001b210b3e58

--- typo3/sysext/workspaces/ext_localconf.php
+++ typo3/sysext/workspaces/ext_localconf.php
@@ -23,12 +23,14 @@ $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['hook_eofe
 if ($GLOBALS['TYPO3_CONF_VARS']['SYS']['useCachingFramework']) {
 		// Initialize the caching framework. The caching framework is asking it self if it is initialized already before initializing.
 	t3lib_cache::initializeCachingFramework();
-	
+
 	$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['sys_workspace_cache']['backend'] = 't3lib_cache_backend_DbBackend';
 	$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['sys_workspace_cache']['options'] = array(
 		'cacheTable' => 'sys_workspace_cache',
 		'tagsTable' => 'sys_workspace_cache_tags'
 	);
 }
+
+t3lib_extMgm::addUserTSConfig('options.workspaces.considerReferences = 1');
 
 ?>
