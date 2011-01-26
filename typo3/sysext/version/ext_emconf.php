db39c4b1fc73074d93ce82eff3a77ec1e0dda299
Tolleiv Nietsch <info@tolleiv.de>
2011-01-26 14:41:22 +0000

    Raised version to 4.5.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/CoreProjects/workspaces/version/trunk@4031 743128fe-103e-dd11-99c4-001b210b3e58

--- typo3/sysext/version/ext_emconf.php
+++ typo3/sysext/version/ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "version".
 #
-# Auto generated 25-01-2011 01:17
+# Auto generated 26-01-2011 15:39
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -30,7 +30,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '4.5.0rc2',
+	'version' => '4.5.0',
 	'_md5_values_when_last_written' => 'a:31:{s:9:"ChangeLog";s:4:"fb5e";s:24:"class.tx_version_cm1.php";s:4:"d808";s:24:"class.tx_version_gui.php";s:4:"deea";s:28:"class.tx_version_tcemain.php";s:4:"4f42";s:39:"class.tx_version_tcemain_commandmap.php";s:4:"2246";s:16:"ext_autoload.php";s:4:"3497";s:21:"ext_conf_template.txt";s:4:"84d4";s:12:"ext_icon.gif";s:4:"3ca2";s:17:"ext_localconf.php";s:4:"9812";s:14:"ext_tables.php";s:4:"1196";s:13:"locallang.xml";s:4:"5ec0";s:7:"tca.php";s:4:"15fa";s:47:"Resources/Private/Language/locallang_emails.xml";s:4:"efd1";s:12:"cli/conf.php";s:4:"f8ec";s:13:"cm1/clear.gif";s:4:"cc11";s:15:"cm1/cm_icon.gif";s:4:"3ca2";s:12:"cm1/conf.php";s:4:"6ed8";s:13:"cm1/index.php";s:4:"5fa3";s:44:"tasks/class.tx_version_tasks_autopublish.php";s:4:"5b4c";s:18:"ws/class.wslib.php";s:4:"2700";s:22:"ws/class.wslib_gui.php";s:4:"34f3";s:12:"ws/clear.gif";s:4:"cc11";s:11:"ws/conf.php";s:4:"b2a8";s:12:"ws/index.php";s:4:"5f7e";s:15:"ws/progress.gif";s:4:"35af";s:14:"ws/publish.php";s:4:"e20c";s:20:"ws/sys_workspace.gif";s:4:"55bc";s:20:"ws/sys_workspace.png";s:4:"db4b";s:21:"ws/workspaceforms.php";s:4:"154c";s:19:"ws/wsol_preview.php";s:4:"2050";s:15:"ws/cli/conf.php";s:4:"328d";}',
 	'constraints' => array(
 		'depends' => array(
