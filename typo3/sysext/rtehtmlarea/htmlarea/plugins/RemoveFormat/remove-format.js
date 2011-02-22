54eeaf4304ac13fce3381ac1e555a59dd246cf83
stan <stan@709f56b5-9817-0410-a4d7-c38de5d9e867>
2011-02-22 19:40:56 +0000

    Fixed bug #17695: htmlArea RTE: Removing format may remove content
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/branches/TYPO3_4-3@10544 709f56b5-9817-0410-a4d7-c38de5d9e867

--- typo3/sysext/rtehtmlarea/htmlarea/plugins/RemoveFormat/remove-format.js
+++ typo3/sysext/rtehtmlarea/htmlarea/plugins/RemoveFormat/remove-format.js
@@ -114,7 +114,7 @@ RemoveFormat = HTMLArea.Plugin.extend({
 
 				if (param["formatting"] == true) {
 						// Remove font, b, strong, i, em, u, strike, span and other inline tags
-					html = html.replace(/<\/?(abbr|acronym|b[^a-zA-Z]|big|cite|code|em[^a-zA-Z]|font|i[^a-zA-Z]|q|s[^a-zA-Z]|samp|small|span|strike|strong|sub|sup|tt|u[^a-zA-Z]|var)[^>]*>/gi, "");
+					html = html.replace(/<\/?(abbr|acronym|b|big|cite|code|em|font|i|q|s|samp|small|span|strike|strong|sub|sup|tt|u|var)(>|[^>a-zA-Z][^>]*>)/gi, '');
 						// Keep tags, strip attributes
 					html = html.replace(/[ \t\n\r]+(style|class|align|cellpadding|cellspacing|frame|bgcolor)=\"[^>\"]*\"/gi, "");
 				}
