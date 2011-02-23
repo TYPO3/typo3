94e2e8458fee63542e6ff22c9ab23047d73dc4f7
ohader <ohader@709f56b5-9817-0410-a4d7-c38de5d9e867>
2011-02-23 14:21:01 +0000

    Updating version number to  4.4.8-dev after release of 4.4.7
    git-svn-id: https://svn.typo3.org/TYPO3v4/Core/branches/TYPO3_4-4@10610 709f56b5-9817-0410-a4d7-c38de5d9e867

--- t3lib/config_default.php
+++ t3lib/config_default.php
@@ -374,7 +374,7 @@ $TYPO3_CONF_VARS = array(
 $T3_VAR = array();	// Initialize.
 
 	// TYPO3 version
-$TYPO_VERSION = '4.4.7';	// deprecated: use the constants defined below
+$TYPO_VERSION = '4.4.8-dev';	// deprecated: use the constants defined below
 define('TYPO3_version', $TYPO_VERSION);
 define('TYPO3_branch', '4.4');
 define('TYPO3_copyright_year', '1998-2010');
