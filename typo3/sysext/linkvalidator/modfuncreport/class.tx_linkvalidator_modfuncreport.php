5a43b48f3b6a666e1b327c18862dcf4606ed6520
Chrissitopher <Chrissitopher@gmx.de>
2011-02-24 12:09:59 +0000

    Followup to #13318: Regression in linkvalidator
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/linkvalidator/trunk@44183 735d13b6-9817-0410-8766-e36946ffe9aa

--- typo3/sysext/linkvalidator/modfuncreport/class.tx_linkvalidator_modfuncreport.php
+++ typo3/sysext/linkvalidator/modfuncreport/class.tx_linkvalidator_modfuncreport.php
@@ -178,6 +178,7 @@ class tx_linkvalidator_ModFuncReport extends t3lib_extobjbase {
 		$this->initialize();
 			// Setting up the context sensitive menu:
 		$this->resPath = $this->doc->backPath . t3lib_extMgm::extRelPath('linkvalidator') . 'res/';
+		/** @var t3lib_pageRenderer $pageRenderer */
 		$this->pageRenderer = $this->doc->getPageRenderer();
 
 			// Localization
