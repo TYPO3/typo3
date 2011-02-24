0b63300cb3870380ae79d8737061f7b2930f336c
Chrissitopher <Chrissitopher@gmx.de>
2011-02-24 21:00:58 +0000

    Cleaned emconf for the release of TYPO3 4.5.2 Release linkvalidator 1.0.2
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/linkvalidator/branches/4.5@44221 735d13b6-9817-0410-8766-e36946ffe9aa

--- typo3/sysext/linkvalidator/ext_emconf.php
+++ typo3/sysext/linkvalidator/ext_emconf.php
@@ -29,11 +29,10 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => 'Connecta AG / cab services ag / Infoglobe',
-	'version' => '1.0.1',
+	'version' => '1.0.2',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '4.1.0-0.0.0',
-			'php' => '5.0.0-0.0.0',
+			'typo3' => '4.5.0-0.0.0',
 			'info' => '1.0.0-0.0.0',
 		),
 		'conflicts' => array(
@@ -41,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:26:{s:9:"ChangeLog";s:4:"7cad";s:16:"ext_autoload.php";s:4:"4efa";s:12:"ext_icon.gif";s:4:"838b";s:17:"ext_localconf.php";s:4:"5e6f";s:14:"ext_tables.php";s:4:"1679";s:14:"ext_tables.sql";s:4:"2489";s:13:"locallang.xml";s:4:"f460";s:44:"classes/class.tx_linkvalidator_processor.php";s:4:"6a5d";s:61:"classes/linktype/class.tx_linkvalidator_linktype_abstract.php";s:4:"4c43";s:61:"classes/linktype/class.tx_linkvalidator_linktype_external.php";s:4:"83ce";s:57:"classes/linktype/class.tx_linkvalidator_linktype_file.php";s:4:"0a91";s:62:"classes/linktype/class.tx_linkvalidator_linktype_interface.php";s:4:"b40b";s:61:"classes/linktype/class.tx_linkvalidator_linktype_internal.php";s:4:"cc68";s:64:"classes/linktype/class.tx_linkvalidator_linktype_linkhandler.php";s:4:"ad63";s:56:"classes/tasks/class.tx_linkvalidator_tasks_validator.php";s:4:"7302";s:79:"classes/tasks/class.tx_linkvalidator_tasks_validatoradditionalfieldprovider.php";s:4:"7681";s:14:"doc/manual.sxw";s:4:"a6b9";s:14:"doc/manual.txt";s:4:"9221";s:54:"modfuncreport/class.tx_linkvalidator_modfuncreport.php";s:4:"b203";s:27:"modfuncreport/locallang.xml";s:4:"ca3f";s:31:"modfuncreport/locallang_csh.xml";s:4:"7953";s:31:"modfuncreport/locallang_mod.xml";s:4:"c589";s:31:"modfuncreport/mod_template.html";s:4:"4c0f";s:21:"res/linkvalidator.css";s:4:"2c65";s:21:"res/mailtemplate.html";s:4:"c425";s:20:"res/pagetsconfig.txt";s:4:"fb0b";}',
+	'_md5_values_when_last_written' => 'a:26:{s:9:"ChangeLog";s:4:"6c68";s:16:"ext_autoload.php";s:4:"4efa";s:12:"ext_icon.gif";s:4:"838b";s:17:"ext_localconf.php";s:4:"5e6f";s:14:"ext_tables.php";s:4:"1679";s:14:"ext_tables.sql";s:4:"2489";s:13:"locallang.xml";s:4:"f460";s:44:"classes/class.tx_linkvalidator_processor.php";s:4:"2737";s:61:"classes/linktype/class.tx_linkvalidator_linktype_abstract.php";s:4:"4c43";s:61:"classes/linktype/class.tx_linkvalidator_linktype_external.php";s:4:"83ce";s:57:"classes/linktype/class.tx_linkvalidator_linktype_file.php";s:4:"0a91";s:62:"classes/linktype/class.tx_linkvalidator_linktype_interface.php";s:4:"b40b";s:61:"classes/linktype/class.tx_linkvalidator_linktype_internal.php";s:4:"cc68";s:64:"classes/linktype/class.tx_linkvalidator_linktype_linkhandler.php";s:4:"ad63";s:56:"classes/tasks/class.tx_linkvalidator_tasks_validator.php";s:4:"7302";s:79:"classes/tasks/class.tx_linkvalidator_tasks_validatoradditionalfieldprovider.php";s:4:"7681";s:14:"doc/manual.sxw";s:4:"a6b9";s:14:"doc/manual.txt";s:4:"9221";s:54:"modfuncreport/class.tx_linkvalidator_modfuncreport.php";s:4:"b203";s:27:"modfuncreport/locallang.xml";s:4:"ca3f";s:31:"modfuncreport/locallang_csh.xml";s:4:"7953";s:31:"modfuncreport/locallang_mod.xml";s:4:"c589";s:31:"modfuncreport/mod_template.html";s:4:"4c0f";s:21:"res/linkvalidator.css";s:4:"2c65";s:21:"res/mailtemplate.html";s:4:"c425";s:20:"res/pagetsconfig.txt";s:4:"fb0b";}',
 	'suggests' => array(
 	),
 );
