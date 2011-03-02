29ee82d6badb474a13b297fdf5f9123754224ff1
Dmitry Dulepov <dmitry@typo3.org>
2011-03-02 15:45:29 +0100

    [BUGFIX] EXT:Workspaces - TSFE hook corrupts page output
    Change-Id: I2d54fbfb3a843454aa4d3cf069f57229f4716471
    Resolves: #13525
    Related: #M17800

--- typo3/sysext/workspaces/Classes/Service/Fehooks.php
+++ typo3/sysext/workspaces/Classes/Service/Fehooks.php
@@ -41,7 +41,7 @@ class tx_Workspaces_Service_Fehooks {
 			return;
 		}
 
-		echo $GLOBALS['TSFE']->cObj->cObjGetSingle(
+		$content = $GLOBALS['TSFE']->cObj->cObjGetSingle(
 			'FLUIDTEMPLATE',
 			array(
 				'file' => 'EXT:workspaces/Resources/Private/Templates/Preview/Preview.html',
@@ -51,5 +51,6 @@ class tx_Workspaces_Service_Fehooks {
 				)
 			)
 		);
+		$GLOBALS['TSFE']->content = str_ireplace('</body>', $content . '</body>', $GLOBALS['TSFE']->content);
 	}
 }
