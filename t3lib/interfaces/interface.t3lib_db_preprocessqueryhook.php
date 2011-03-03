290ff5b75e8dea6ffabee18e187112747bec89c1
Xavier Perseguers <typo3@perseguers.ch>
2011-03-03 15:58:51 +0100

    [FEATURE] Hook Request for t3lib_db
    For the introduction of uuid within TYPO3, a few hooks need to be added to t3lib_DB.
    
    Change-Id: I3822710055d8d735e615d80f1d979d0c87e94632
    Resolves: #13579
    Reviewed-on: http://review.typo3.org/1023
    Reviewed-by: Thomas Maroschik <tmaroschik@dfau.de>
    Tested-by: Thomas Maroschik <tmaroschik@dfau.de>
    Reviewed-by: Xavier Perseguers <typo3@perseguers.ch>
    Tested-by: Xavier Perseguers <typo3@perseguers.ch>

---
+++ t3lib/interfaces/interface.t3lib_db_preprocessqueryhook.php
@@ -0,0 +1,95 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2011 Xavier Perseguers <typo3@perseguers.ch>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *  A copy is found in the textfile GPL.txt and important notices to the license
+ *  from the author is found in LICENSE.txt distributed with these scripts.
+ *
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+
+/**
+ * Interface for classes which hook into t3lib_DB and do additional processing
+ * before a query has been executed.
+ *
+ * @author	Xavier Perseguers <typo3@perseguers.ch>
+ * @package TYPO3
+ * @subpackage t3lib
+ */
+interface t3lib_DB_preProcessQueryHook {
+
+	/**
+	 * Pre-processor for the INSERTquery method.
+	 *
+	 * @param string $table Database table name
+	 * @param array $fieldsValues Field values as key => value pairs
+	 * @param string/array $noQuoteFields List/array of keys NOT to quote
+	 * @param t3lib_DB $parentObject
+	 * @return void
+	 */
+	public function INSERTquery_preProcessAction($table, array $fieldsValues, $noQuoteFields, t3lib_DB $parentObject);
+
+	/**
+	 * Pre-processor for the INSERTmultipleRows method.
+	 *
+	 * @param string $table Database table name
+	 * @param array $fields Field names
+	 * @param array $rows Table rows
+	 * @param string/array $noQuoteFields List/array of keys NOT to quote
+	 * @param t3lib_DB $parentObject
+	 * @return void
+	 */
+	public function INSERTmultipleRows_preProcessAction($table, array $fields, array $rows, $noQuoteFields, t3lib_DB $parentObject);
+
+	/**
+	 * Pre-processor for the UPDATEquery method.
+	 *
+	 * @param string $table Database table name
+	 * @param string $where WHERE clause
+	 * @param array $fieldsValues Field values as key => value pairs
+	 * @param string/array $noQuoteFields List/array of keys NOT to quote
+	 * @param t3lib_DB $parentObject
+	 * @return void
+	 */
+	public function UPDATEquery_preProcessAction($table, $where, array $fieldsValues, $noQuoteFields, t3lib_DB $parentObject);
+
+	/**
+	 * Pre-processor for the DELETEquery method.
+	 *
+	 * @param string $table Database table name
+	 * @param string $where WHERE clause
+	 * @param t3lib_DB $parentObject
+	 * @return void
+	 */
+	public function DELETEquery_preProcessAction($table, $where, t3lib_DB $parentObject);
+
+	/**
+	 * Pre-processor for the TRUNCATEquery method.
+	 *
+	 * @param string $table Database table name
+	 * @param t3lib_DB $parentObject
+	 * @return void
+	 */
+	public function TRUNCATEquery_preProcessAction($table, t3lib_DB $parentObject);
+
+}
+
+?>
