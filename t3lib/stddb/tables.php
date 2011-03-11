e5a5f5e7775cff64e9fe2fb5086e11a6f7cc3030
Stefan Galinski <stefan.galinski@gmail.com>
2011-03-11 22:57:37 +0100

    [BUGFIX] Pagetree - Missing icons for expand/collapse whole branches
    The expand/collapse context menu actions are missing icons.
    
    Change-Id: I3ae86ff0a4de225854d173a485a0a8991adccd22
    Resolves: #M17924
    Reviewed-on: http://review.typo3.org/1130
    Reviewed-by: Stefan Galinski <stefan.galinski@gmail.com>
    Tested-by: Stefan Galinski <stefan.galinski@gmail.com>

--- t3lib/stddb/tables.php
+++ t3lib/stddb/tables.php
@@ -585,6 +585,7 @@ $GLOBALS['TBE_STYLES']['spriteIconApi']['coreSpriteImageNames'] = array(
 	'apps-filetree-root',
 	'apps-pagetree-backend-user',
 	'apps-pagetree-backend-user-hideinmenu',
+	'apps-pagetree-collapse',
 	'apps-pagetree-drag-copy-above',
 	'apps-pagetree-drag-copy-below',
 	'apps-pagetree-drag-move-above',
@@ -594,6 +595,7 @@ $GLOBALS['TBE_STYLES']['spriteIconApi']['coreSpriteImageNames'] = array(
 	'apps-pagetree-drag-new-between',
 	'apps-pagetree-drag-new-inside',
 	'apps-pagetree-drag-place-denied',
+	'apps-pagetree-expand',
 	'apps-pagetree-folder-contains-approve',
 	'apps-pagetree-folder-contains-board',
 	'apps-pagetree-folder-contains-fe_users',
@@ -762,6 +764,8 @@ $GLOBALS['TBE_STYLES']['spriteIconApi']['coreSpriteImageNames'] = array(
 	'status-warning-in-use',
 	'status-warning-lock'
 );
+
+
 
 
 
