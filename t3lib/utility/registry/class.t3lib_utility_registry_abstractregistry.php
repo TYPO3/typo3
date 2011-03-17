d0969c0c33c343a376be0a40351a0e2a606680fb
Oliver Hader <oliver@typo3.org>
2011-03-17 18:58:19 +0100

    [BUGFIX] Removing child records in a workspace does not work
    These parts are not working with IRRE in workspaces:
    Removing children in TCEforms
    Removing children in the list view
    Reverting changes in the workspace module
    
    Change-Id: I91313235e57c193e7103d065c02d2005ef74ab86
    Resolves: #M17981

---
+++ t3lib/utility/registry/class.t3lib_utility_registry_abstractregistry.php
@@ -0,0 +1,121 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2009-2011 Ingo Renner <ingo@typo3.org>
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *  A copy is found in the textfile GPL.txt and important notices to the license
+ *  from the author is found in LICENSE.txt distributed with these scripts.
+ *
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+
+/**
+ * A class to store and retrieve entries in a registry.
+ *
+ * @author	Ingo Renner <ingo@typo3.org>
+ * @author	Bastian Waidelich <bastian@typo3.org>
+ * @package TYPO3
+ * @subpackage t3lib
+ */
+abstract class t3lib_utility_registry_AbstractRegistry implements t3lib_Singleton {
+
+	/**
+	 * @var	array
+	 */
+	protected $entries = array();
+
+	/**
+	 * Returns an entry.
+	 *
+	 * @param	string	Extension key for extensions starting with 'tx_' / 'user_' or 'core' for core registry entries
+	 * @param	string	The key of the entry to return.
+	 * @param	mixed	Optional default value to use if this entry has never been set. Defaults to null.
+	 * @return	mixed	The value of the entry.
+	 * @throws	InvalidArgumentException	Throws an exception if the given namespace is not valid
+	 */
+	public function get($namespace, $key, $defaultValue = NULL) {
+		return isset($this->entries[$namespace][$key]) ? $this->entries[$namespace][$key] : $defaultValue;
+	}
+
+	/**
+	 * Sets an entry.
+	 *
+	 * @param	string	Extension key for extensions starting with 'tx_' / 'user_' or 'core' for core registry entries.
+	 * @param	string	The key of the entry to set.
+	 * @param	mixed	The value to set. This can be any PHP data type; this class takes care of serialization if necessary.
+	 * @return	void
+	 * @throws	InvalidArgumentException	Throws an exception if the given namespace is not valid
+	 */
+	public function set($namespace, $key, $value) {
+		$this->validateNamespace($namespace);
+		$this->entries[$namespace][$key] = $value;
+	}
+
+	/**
+	 * Unsets an entry.
+	 *
+	 * @param	string	Namespace. extension key for extensions or 'core' for core registry entries
+	 * @param	string	The key of the entry to unset.
+	 * @return	void
+	 * @throws	InvalidArgumentException	Throws an exception if the given namespace is not valid
+	 */
+	public function remove($namespace, $key) {
+		$this->validateNamespace($namespace);
+		unset($this->entries[$namespace][$key]);
+	}
+
+	/**
+	 * Unsets all entries of the given namespace.
+	 *
+	 * @param	string	Namespace. extension key for extensions or 'core' for core registry entries
+	 * @return	void
+	 * @throws	InvalidArgumentException	Throws an exception if the given namespace is not valid
+	 */
+	public function removeAllByNamespace($namespace) {
+		$this->validateNamespace($namespace);
+		unset($this->entries[$namespace]);
+	}
+
+	/**
+	 * Checks the given namespace. If it does not have a valid format an
+	 * exception is thrown.
+	 * Allowed namespaces are 'core', 'tx_*' and 'user_*'
+	 *
+	 * @param	string	Namespace. extension key for extensions or 'core' for core registry entries
+	 * @return	void
+	 * @throws	InvalidArgumentException	Throws an exception if the given namespace is not valid
+	 */
+	protected function validateNamespace($namespace) {
+		if (t3lib_div::isFirstPartOfStr($namespace, 'tx_') || t3lib_div::isFirstPartOfStr($namespace, 'user_')) {
+			return;
+		}
+
+		if ($namespace !== 'core') {
+			throw new InvalidArgumentException(
+				'"' . $namespace . '" is no valid Namespace. The namespace has to be prefixed with "tx_", "user_" or must be equal to "core"',
+				1249755131
+			);
+		}
+	}
+
+}
+
+?>
