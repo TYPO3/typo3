ecc21f2e46ffec0757963fd128d3cb5c086fda61
Xavier Perseguers <typo3@perseguers.ch>
2011-03-25 16:53:29 +0100

    [TASK] Remove explicit checks for PHP < 5.3
    Make PHP 5.3 a requirement for TYPO3 4.6 and remove tests that check for older
    versions of PHP.
    
    Change-Id: Ib73e332a44f618e59f78c8e442d2bf27474c4964
    Resolves: #25395
    Reviewed-on: http://review.typo3.org/1277
    Reviewed-by: Susanne Moog
    Tested-by: Susanne Moog

--- index.php
+++ index.php
@@ -37,11 +37,7 @@
 // Set error reporting
 // *******************************
 
-if (defined('E_DEPRECATED')) {
-	error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED);
-} else {
-	error_reporting(E_ALL ^ E_NOTICE);
-}
+error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED);
 
 
 // ******************
