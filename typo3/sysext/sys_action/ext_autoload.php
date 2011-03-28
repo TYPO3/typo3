bf679f7a3789f7394df83be9332914c74d00bde5
Thomas Maroschik <tmaroschik@dfau.de>
2011-03-28 14:04:17 +0200

    [BUGFIX] Resolves: #M15747; sys_action: Record list page turning not working
    If a list-records-task lists more than 100 records, the singe table page turning
    functionality is shown on top of the table. Unfortunately if you want to turn
    the page (or enter a page number yourself) nothing happens or you get elsewhere in
    newer TYPO3 versions.
    
    This happens due to wrong link generation. The Listmodule has been refactored, but the
    sys_actions Module stayed the same. Link generation was broken.
    
    This patch fixes the issues with sys_actions and list link generation by subclassing
    the localRecordList class and adopting the link generation to sys_actions needs.
    
    Change-Id: I6305901f0ede0efd0cf7305cbea1733bebce6a74

--- typo3/sysext/sys_action/ext_autoload.php
+++ typo3/sysext/sys_action/ext_autoload.php
@@ -5,6 +5,7 @@
  */
 $extensionPath = t3lib_extMgm::extPath('sys_action');
 return array(
+	'tx_sysaction_list' => $extensionPath . 'task/class.tx_sysaction_list.php',
 	'tx_sysaction_task' => $extensionPath . 'task/class.tx_sysaction_task.php',
 );
 ?>
