a445833d596971bb9799df0483a9c8ea2d03f601
Christian Kuhn <lolli@schwarzbu.ch>
2011-03-31 23:44:19 +0200

    [FEATURE] Add table garbage collection scheduler task
    This feature was implemented during T3BOARD11 bug auction. It is
    kindly sponsored with 100 beers by jweiland.net and is called the
    'Schnaps Hans scheduler task'
    
    Change-Id: Id7c58d28e4f6e82a708513a3ce9761baf36c06cd
    Resolves: #25343

---
+++ typo3/sysext/scheduler/tasks/class.tx_scheduler_tablegarbagecollection.php
@@ -0,0 +1,109 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2011 Christian Kuhn <lolli@schwarzbu.ch>
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+/**
+ * Remove old entries from tables.
+ *
+ * This task deletes rows from tables older than the given number of days.
+ *
+ * Available tables must be registered in
+ * $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['tx_scheduler_TableGarbageCollection']['options']['tables']
+ * See ext_localconf.php of scheduler extension for an example
+ *
+ * @author Christian Kuhn <lolli@schwarzbu.ch>
+ * @package TYPO3
+ * @subpackage scheduler
+ */
+class tx_scheduler_TableGarbageCollection extends tx_scheduler_Task {
+
+	/**
+	 * @var integer Number of days
+	 */
+	public $numberOfDays = 180;
+
+	/**
+	 * @var string Table to clean up
+	 */
+	public $table = '';
+
+	/**
+	 * Execute garbage collection, called by scheduler.
+	 *
+	 * @return void
+	 */
+	public function execute() {
+		$deleteTimestamp = strtotime('-' . $this->numberOfDays . 'days');
+
+			// Sanitize table and determine according timestamp field
+		$tableConfigurations = $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['tx_scheduler_TableGarbageCollection']['options']['tables'];
+		foreach ($tableConfigurations as $number => $configuration) {
+			if (isset($configuration['table']) && $configuration['table'] === $this->table) {
+				if (!isset($configuration['timestampField'])) {
+					throw new Exception(
+						'tx_scheduler_TableGarbageCollection misconfiguration: No timestampField defined for table ' . $configuration['table'],
+						1308170146
+					);
+				}
+				$timestampField = $configuration['timestampField'];
+				break;
+			}
+		}
+		if (!isset($timestampField)) {
+			throw new Exception(
+				'tx_scheduler_TableGarbageCollection misconfiguration: No table configuration found for table ' . $this->table,
+				1308170346
+			);
+		}
+
+		$GLOBALS['TYPO3_DB']->exec_DELETEquery(
+			$this->table,
+			$timestampField . ' < ' . $deleteTimestamp
+		);
+
+		$error = $GLOBALS['TYPO3_DB']->sql_error();
+		if ($error) {
+			throw new Exception(
+				'tx_scheduler_TableGarbageCollection failed for table ' . $this->table . ' with error: ' . $error,
+				1308255491
+			);
+		}
+
+		return TRUE;
+	}
+
+	/**
+	 * This method returns the selected table as additional information
+	 *
+	 * @return string Information to display
+	 */
+	public function getAdditionalInformation() {
+		return $GLOBALS['LANG']->sL('LLL:EXT:scheduler/mod1/locallang.xml:label.tableGarbageCollection.additionalInformationTable') . ': ' . $this->table;
+	}
+}
+
+if (defined('TYPO3_MODE') && isset($GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/scheduler/tasks/class.tx_scheduler_tablegarbagecollection.php'])) {
+	include_once($GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/scheduler/tasks/class.tx_scheduler_tablegarbagecollection.php']);
+}
+
+?>
