d81f10163ebdb39efc79e1760f68355f1ef632a3
Markus Bucher <m.bucher@kuehlhaus.com>
2011-04-06 09:53:04 +0200

    [CONF] redirectTo field max-value 240 chars
    It is neccessary to hold more than 120 characters in redirectTo field,
    thus changes in ext_table.sql and tbl_cms.php are neccessary. This fix
    changes the max-value to 240 chars.
    
    Resolves #25780
    Releases 4.6, 4.5, 4.4
    
    Change-Id: I228f636b529ca75c9b1820a8cff622970b00cd42

--- typo3/sysext/cms/ext_tables.sql
+++ typo3/sysext/cms/ext_tables.sql
@@ -289,7 +289,7 @@ CREATE TABLE sys_domain (
   cruser_id int(11) unsigned DEFAULT '0' NOT NULL,
   hidden tinyint(4) unsigned DEFAULT '0' NOT NULL,
   domainName varchar(80) DEFAULT '' NOT NULL,
-  redirectTo varchar(120) DEFAULT '' NOT NULL,
+  redirectTo varchar(240) DEFAULT '' NOT NULL,
   redirectHttpStatusCode int(4) unsigned DEFAULT '301' NOT NULL,
   sorting int(10) unsigned DEFAULT '0' NOT NULL,
   prepend_params int(10) DEFAULT '0' NOT NULL,
