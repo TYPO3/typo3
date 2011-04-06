9e49ad5f1632460d8c4cf879420383fb4f8af035
Markus Klein <klein.t3@mfc-linz.at>
2011-04-06 11:44:24 +0200

    [BUGFIX] Configuration Tool creates invalid extTables.php file
    If extTables.php is empty or not present, this changes takes care of
    adding an extra LF after the opening <?php tag.
    
    Change-Id: I948d98e033fc67e171926faccfdb5d1a2cf78aa6
    Resolves: #25781
    Releases: 4.6, 4.5, 4.4
    Reviewed-on: http://review.typo3.org/1474
    Reviewed-by: Georg Ringer
    Tested-by: Georg Ringer
    Reviewed-by: Jan Loderhose
    Tested-by: Jan Loderhose
    Reviewed-by: Oliver Hader
    Tested-by: Oliver Hader
    Reviewed-on: http://review.typo3.org/1552
    Reviewed-by: Markus Klein
    Tested-by: Markus Klein

--- typo3/sysext/lowlevel/config/index.php
+++ typo3/sysext/lowlevel/config/index.php
@@ -285,7 +285,8 @@ class SC_mod_tools_config_index {
 				}
 
 					// insert line in extTables.php
-				$extTables = '<?php' . preg_replace('/<\?php|\?>/is', '', $extTables) . $changedLine . LF . '?>';
+				$extTables = preg_replace('/<\?php|\?>/is', '', $extTables);
+				$extTables = '<?php' . (empty($extTables) ? LF : '') . $extTables . $changedLine . LF . '?>';
 				$success = t3lib_div::writeFile(PATH_typo3conf . TYPO3_extTableDef_script, $extTables);
 				if ($success) {
 						// show flash message
