ecc6224c027158de2d2f7773087e453a232ca9d8
Andy Grunwald <andreas.grunwald@wmdb.de>
2011-04-12 22:06:39 +0200

    [FEATURE] Move class "tx_scheduler_Module" from "mod1/index.php" for unit test
    It would be nice to extract the class "tx_scheduler_Module" to a seperate file "scheduler/class.tx_scheduler_module.php".
    This change makes it possible to write unit tests for "tx_scheduler_module".
    
    Patch set 2: Move XClass-Inclusion from mod1/index.php to class.tx_scheduler_module.php
    Patch set 3: Correct "whitespace bevor tab" intention-error
    
    Change-Id: I85b977687c24966aadba11248a58ee081b8bef67
    Resolves: #25925
    Releases: 4.6

--- typo3/sysext/scheduler/ext_autoload.php
+++ typo3/sysext/scheduler/ext_autoload.php
@@ -15,7 +15,7 @@ return array(
 	'tx_scheduler_testtask' => $extensionPath . 'examples/class.tx_scheduler_testtask.php',
 	'tx_scheduler_testtask_additionalfieldprovider' => $extensionPath . 'examples/class.tx_scheduler_testtask_additionalfieldprovider.php',
 	'tx_scheduler_additionalfieldprovider' => $extensionPath . 'interfaces/interface.tx_scheduler_additionalfieldprovider.php',
-	'tx_scheduler_module' => $extensionPath . 'mod1/index.php',
+	'tx_scheduler_module' => $extensionPath . 'class.tx_scheduler_module.php',
 	'tx_scheduler_croncmdtest' => $extensionPath . 'tests/tx_scheduler_croncmdTest.php',
 	'tx_scheduler_cachingframeworkgarbagecollection' => $extensionPath . 'tasks/class.tx_scheduler_cachingframeworkgarbagecollection.php',
 	'tx_scheduler_cachingframeworkgarbagecollection_additionalfieldprovider' => $extensionPath . 'tasks/class.tx_scheduler_cachingframeworkgarbagecollection_additionalfieldprovider.php',
