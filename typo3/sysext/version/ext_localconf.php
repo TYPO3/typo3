9e452a7b689496e6c6464371d6057522d0e1e36e
Francois Suter <francois@typo3.org>
2011-04-19 13:51:27 +0200

    [BUGFIX] Force icon overlay for deleted records
    t3lib_iconWorks::mapRecordOverlayToSpriteIconName() defines the
    overlays to display for any given record icon, but it not workspace-
    aware. However a pending request adds a hook to override the
    icon overlays. This enables the "version" extension to hook into
    the process and force the overlays to "deleted" when the current
    record's t3ver_state = 2 (i.e. it is deleted in a workspace).
    
    Test in conjunction with https://review.typo3.org/#change,1459
    
    Change-Id: I2c07e3ceae70aa971e1086dd1f902a960b862d09
    Resolves: #24641
    Releases: 4.6, 4.5

--- typo3/sysext/version/ext_localconf.php
+++ typo3/sysext/version/ext_localconf.php
@@ -8,6 +8,8 @@ if (!defined ('TYPO3_MODE')) {
 	// register the hook to actually do the work within TCEmain
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processCmdmapClass']['version'] = t3lib_extMgm::extPath('version', 'class.tx_version_tcemain.php:&tx_version_tcemain');
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['moveRecordClass']['version'] = t3lib_extMgm::extPath('version', 'class.tx_version_tcemain.php:&tx_version_tcemain');
+	// Register hook for overriding the icon status overlay
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_iconworks.php']['overrideIconOverlay']['version'] = t3lib_extMgm::extPath('version', 'class.tx_version_iconworks.php:&tx_version_iconworks');
 
 if (TYPO3_MODE == 'BE') {
 
