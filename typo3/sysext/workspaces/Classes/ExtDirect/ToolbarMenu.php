dd149fb0b1e0d528c9170f86763e31efdf638de5
Oliver Hader <oliver@typo3.org>
2011-05-10 13:00:36 +0200

    [TASK] Reapply direct changes of TYPO3 Core
    Commits a95c3ca51aeee40043bcd6864dc49fa59a1c952c..5573182f6705afe645307fd8395ca7ed1b72d1ed
    
    Change-Id: Ic4a320dd921c4b18a4fddd25a80afb887a0a71fd
    Resolves: #26728

--- typo3/sysext/workspaces/Classes/ExtDirect/ToolbarMenu.php
+++ typo3/sysext/workspaces/Classes/ExtDirect/ToolbarMenu.php
@@ -62,3 +62,4 @@ class tx_Workspaces_ExtDirect_ToolbarMenu {
 if (defined('TYPO3_MODE') && isset($GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/workspaces/Classes/ExtDirect/ToolbarMenu.php'])) {
 	include_once($GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/workspaces/Classes/ExtDirect/ToolbarMenu.php']);
 }
+?>
--- typo3/sysext/workspaces/Classes/Service/Fehooks.php
+++ typo3/sysext/workspaces/Classes/Service/Fehooks.php
@@ -61,3 +61,4 @@ class tx_Workspaces_Service_Fehooks {
 
 	}
 }
+?>
