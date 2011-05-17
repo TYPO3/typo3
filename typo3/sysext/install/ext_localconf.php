05d60b27c8e246c6125700b5ec0f87d1fb2192a7
Bjoern Pedersen <bjoern.pedersen@frm2.tum.de>
2011-05-17 08:39:22 +0200

    [FEATURE] CF db backend: better indexable db layout
    This patch introduces a better dblayout that allows effective
    use of indices.
    
    The tables are now auto-created on cache flush (see bugtracker
    for details).
    
    I got the info from Christian Kuhn in typo3.dev, that the dbbackend
    is not(!) a backport from flow3, but v4 only. See
    news://lists.typo3.org:119/mailman.11208.1298057799.648.typo3-dev@lists.typo3.org
    
    An update script is included.
    
    Change-Id: Ie97563e1612d64759bcbd2ee4db005a6d5a33812
    Resolves: #25119
    Releases: 4.6

--- typo3/sysext/install/ext_localconf.php
+++ typo3/sysext/install/ext_localconf.php
@@ -50,4 +50,6 @@ $TYPO3_CONF_VARS['SC_OPTIONS']['ext/install']['update']['addFlexformsToAcl'] = '
 
 	// Version 4.5: Split tt_content image_link to newline by comma
 $TYPO3_CONF_VARS['SC_OPTIONS']['ext/install']['update']['imagelink'] = 'tx_coreupdates_imagelink';
+	// Version 4.6: Autocreate cache tables
+$TYPO3_CONF_VARS['SC_OPTIONS']['ext/install']['update']['imagelink'] = 'tx_coreupdates_cachetables';
 ?>
