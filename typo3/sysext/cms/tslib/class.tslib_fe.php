9ac4d955d3f7e1749d871ca07dc04619fe603767
Markus Klein <klein.t3@mfc-linz.at>
2011-05-21 16:52:22 +0200

    [BUGFIX] pageNotFound_handling error with relative paths
    This patch fixes the handling of relative paths of pageNotFound_handling.
    Due to a missing check, the relative path is prefixed with http_host only,
    causing a wrong Location header.
    
    Change-Id: I664250d34b05a26bdaf1e2a839027b80c1e4f607
    Resolves: #25725
    Releases: 4.6, 4.5, 4.4
    Reviewed-on: http://review.typo3.org/1712
    Reviewed-by: Kurt Kunig
    Tested-by: Kurt Kunig
    Reviewed-by: Stefan Neufeind
    Tested-by: Stefan Neufeind
    Reviewed-by: Andreas Wolf
    Tested-by: Andreas Wolf

--- typo3/sysext/cms/tslib/class.tslib_fe.php
+++ typo3/sysext/cms/tslib/class.tslib_fe.php
@@ -1521,7 +1521,11 @@
 			$url_parts = parse_url($code);
 			if ($url_parts['host'] == '')	{
 				$url_parts['host'] = t3lib_div::getIndpEnv('HTTP_HOST');
-				$code = t3lib_div::getIndpEnv('TYPO3_REQUEST_HOST') . $code;
+				if ($code{0} === '/') {
+					$code = t3lib_div::getIndpEnv('TYPO3_REQUEST_HOST') . $code;
+				} else {
+					$code = t3lib_div::getIndpEnv('TYPO3_REQUEST_DIR') . $code;
+				}
 				$checkBaseTag = FALSE;
 			} else {
 				$checkBaseTag = TRUE;
