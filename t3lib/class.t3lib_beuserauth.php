35e3225b59ce70df5a690f43ee3752bedb1290f9
Christian Jul Jensen <julle@typo3.org>
2011-05-25 16:11:00 +0200

    [API][CONF][!!!] Make the name of cookies configurable
    To avoid different scenarios with clashing cookies on same-domain
    sites, make the name of cookies used for BE and FE login
    configurable.
    
    To achieve this
    * add configuration options
    * add setting of name in relevant classes with proper fallback
    * change all uses of the names to api calls
    * introduce own adminpanel cookie since timetracking relies on
      BE-cookie.
    * introduce utility function in timetracknull now that timetrack is
      only enabled when adminpanel is.
    
    Change-Id: I8078bd284cdf5d1413ddce8bc004e25f855ba6c5
    Resolves: #23872

--- t3lib/class.t3lib_beuserauth.php
+++ t3lib/class.t3lib_beuserauth.php
@@ -66,7 +66,7 @@
  */
 class t3lib_beUserAuth extends t3lib_userAuthGroup {
 	var $session_table = 'be_sessions'; // Table to use for session data.
-	var $name = 'be_typo_user'; // Session/Cookie name
+	protected $name; // Session/Cookie name
 
 	var $user_table = 'be_users'; // Table in database with userdata
 	var $username_column = 'username'; // Column for login-name
@@ -128,6 +128,19 @@ class t3lib_beUserAuth extends t3lib_userAuthGroup {
 		'resizeTextareas_MaxHeight' => 300,
 		'resizeTextareas_Flexible' => 1,
 	);
+
+	public function __construct() {
+		$this->name = self::getCookieName();
+		$this->loginType = 'BE';
+	}
+
+	public static function getCookieName() {
+		$configuredCookieName = $GLOBALS['TYPO3_CONF_VARS']['SYS']['cookieNameBe'];
+		if(strlen($configuredCookieName) === 0) {
+			$configuredCookieName = 'be_typo_user';
+		}
+		return $configuredCookieName;
+	}
 
 
 	/**
