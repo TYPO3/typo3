304a4b7b58924d7e0e6f02ce7067a1e624a3b0ea
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2011-05-31 18:00:24 +0200

    [FEATURE] After creation of ENABLE_INSTALL_TOOL file in Backend a link is shown
    Change-Id: Ie152809aad761875614fab7b986665b2297bdd62

--- typo3/sysext/setup/mod/index.php
+++ typo3/sysext/setup/mod/index.php
@@ -874,7 +874,7 @@ class SC_mod_user_setup_index {
 					$GLOBALS['LANG']->sL('LLL:EXT:setup/mod/locallang.xml:enableInstallTool.createFile') . '" onclick="document.getElementById(\'createInstallToolEnableFile\').value=1;this.form.submit();" />
 					<input type="hidden" name="createInstallToolEnableFile" value="0" id="createInstallToolEnableFile" />
 					<br />
--					<a href="javascript:top.goToModule(\'tools_install\',1);">'.$GLOBALS['LANG']->sL('LLL:EXT:setup/mod/locallang.xml:enableInstallTool.goToInstallTool').'</a>;
+-					<a href="javascript:top.goToModule(\'tools_install\',1);">'.$GLOBALS['LANG']->sL('LLL:EXT:setup/mod/locallang.xml:enableInstallTool.goToInstallTool').'</a>';
 		}
 	}
 
