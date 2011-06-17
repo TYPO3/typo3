a6db232fae4c4ef098990d83af2f7942421b5f62
Nicole Cordes <cordes@cps-it.de>
2011-06-17 11:33:56 +0200

    [BUGFIX] FE-Error "NO entry in the $TCA-array..."
    Especially in installations with many domains, the following error
    sporadically occured in Frontend:
    > "NO entry in the $TCA-array for the table "[table]".
    > This means that the function enableFields() is called with an invalid
    > table name as argument."
    
    The problem occured when a site is opened in frontend while a backend
    user clears cache. It seems that at the beginning of the frontend
    request the temp_*_ext_tables.php file still exists but when it should
    be included it is got already deleted by the clear cache request. The
    solution is to check if file exists before to include it, if not just
    include all extensions ext_tables.php with stddb/load_ext_tables.php
    "manually".
    
    Change-Id: I4fe5e0256093df8190eb6f1a2754edf1e3aba405
    Resolves: #22928
    Releases: 4.6, 4.5, 4.4, 4.3

--- typo3/sysext/cms/tslib/class.tslib_fe.php
+++ typo3/sysext/cms/tslib/class.tslib_fe.php
@@ -2268,7 +2268,7 @@
 			$GLOBALS['TCA'] = array();
 			include (TYPO3_tables_script ? PATH_typo3conf.TYPO3_tables_script : PATH_t3lib.'stddb/tables.php');
 				// Extension additions
-			if ($GLOBALS['TYPO3_LOADED_EXT']['_CACHEFILE'])	{
+			if ($GLOBALS['TYPO3_LOADED_EXT']['_CACHEFILE'] && @file_exists(PATH_typo3conf . $GLOBALS['TYPO3_LOADED_EXT']['_CACHEFILE'] . '_ext_tables.php')) {
 				include(PATH_typo3conf.$GLOBALS['TYPO3_LOADED_EXT']['_CACHEFILE'].'_ext_tables.php');
 			} else {
 				include(PATH_t3lib.'stddb/load_ext_tables.php');
