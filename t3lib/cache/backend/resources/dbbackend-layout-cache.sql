6f21a6dec55840f5b3a26d58fa0fd175ad526f1c
Christian Kuhn <lolli@schwarzbu.ch>
2011-06-19 15:09:08 +0200

    [FEATURE] Caching framework db backend: better db layout
    Introduce a better indexable database layout that allows
    effective use of indices.
    
    Change-Id: Ie97563e1612d64759bcbd2ee4db005a6d5a33812
    Resolves: #25119
    Depends: #27506
    Depends: #27536

--- t3lib/cache/backend/resources/dbbackend-layout-cache.sql
+++ t3lib/cache/backend/resources/dbbackend-layout-cache.sql
@@ -1,9 +1,8 @@
 CREATE TABLE ###CACHE_TABLE### (
 	id int(11) unsigned NOT NULL auto_increment,
 	identifier varchar(250) DEFAULT '' NOT NULL,
-	crdate int(11) unsigned DEFAULT '0' NOT NULL,
+	expires int(11) unsigned DEFAULT '0' NOT NULL,
 	content mediumblob,
-	lifetime int(11) unsigned DEFAULT '0' NOT NULL,
 	PRIMARY KEY (id),
-	KEY cache_id (identifier)
+	KEY cache_id (identifier, expires)
 ) ENGINE=InnoDB;
