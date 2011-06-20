c2542c222bf551268b7d9706a0a000017ab9da4c
Michael Klapper <klapper@aoemedia.de>
2011-06-20 17:37:39 +0200

    [BUGFIX] LIVE workspace actions don't work
    The solution is simple to hide all the "actions" in live workspace.
    
    Change-Id: Id0b1f7231fd74d14a4353df2aa290e9fe8f77fad
    Resolves: #11535
    Releases: 4.6, 4.5

--- typo3/sysext/workspaces/Resources/Public/JavaScript/grid.js
+++ typo3/sysext/workspaces/Resources/Public/JavaScript/grid.js
@@ -91,34 +91,65 @@ TYPO3.Workspaces.SelectionModel = new Ext.grid.CheckboxSelectionModel({
 });
 
 TYPO3.Workspaces.WorkspaceGrid = new Ext.grid.GridPanel({
+	initColModel: function() {
+		if (TYPO3.settings.Workspaces.isLiveWorkspace) {
+			this.colModel = new Ext.grid.ColumnModel({
+				columns: [
+					TYPO3.Workspaces.RowExpander,
+					{id: 'uid', dataIndex : 'uid', width: 40, sortable: true, header : TYPO3.lang["column.uid"], hidden: true, filterable : true },
+					{id: 't3ver_oid', dataIndex : 't3ver_oid', width: 40, sortable: true, header : TYPO3.lang["column.oid"], hidden: true, filterable : true },
+					{id: 'workspace_Title', dataIndex : 'workspace_Title', width: 120, sortable: true, header : TYPO3.lang["column.workspaceName"], hidden: true, filter : {type : 'string'}},
+					TYPO3.Workspaces.Configuration.WsPath,
+					TYPO3.Workspaces.Configuration.LivePath,
+					TYPO3.Workspaces.Configuration.WsTitleWithIcon,
+					TYPO3.Workspaces.Configuration.TitleWithIcon,
+					TYPO3.Workspaces.Configuration.ChangeDate
+				],
+				listeners: {
+
+					columnmoved: function(colModel) {
+						TYPO3.Workspaces.Actions.updateColModel(colModel);
+					},
+					hiddenchange: function(colModel) {
+						TYPO3.Workspaces.Actions.updateColModel(colModel);
+					}
+				}
+			});
+		} else {
+				this.colModel = new Ext.grid.ColumnModel({
+				columns: [
+					TYPO3.Workspaces.SelectionModel,
+					TYPO3.Workspaces.RowExpander,
+					{id: 'uid', dataIndex : 'uid', width: 40, sortable: true, header : TYPO3.lang["column.uid"], hidden: true, filterable : true },
+					{id: 't3ver_oid', dataIndex : 't3ver_oid', width: 40, sortable: true, header : TYPO3.lang["column.oid"], hidden: true, filterable : true },
+					{id: 'workspace_Title', dataIndex : 'workspace_Title', width: 120, sortable: true, header : TYPO3.lang["column.workspaceName"], hidden: true, filter : {type : 'string'}},
+					TYPO3.Workspaces.Configuration.WsPath,
+					TYPO3.Workspaces.Configuration.LivePath,
+					TYPO3.Workspaces.Configuration.WsTitleWithIcon,
+					TYPO3.Workspaces.Configuration.SwapButton,
+					TYPO3.Workspaces.Configuration.TitleWithIcon,
+					TYPO3.Workspaces.Configuration.ChangeDate,
+					TYPO3.Workspaces.Configuration.ChangeState,
+					TYPO3.Workspaces.Configuration.Stage,
+					TYPO3.Workspaces.Configuration.RowButtons
+				],
+				listeners: {
+
+					columnmoved: function(colModel) {
+						TYPO3.Workspaces.Actions.updateColModel(colModel);
+					},
+					hiddenchange: function(colModel) {
+						TYPO3.Workspaces.Actions.updateColModel(colModel);
+					}
+				}
+			});
+		}
+
+	},
 	border : true,
 	store : TYPO3.Workspaces.MainStore,
-	colModel : new Ext.grid.ColumnModel({
-		columns: [
-			TYPO3.Workspaces.SelectionModel,
-			TYPO3.Workspaces.RowExpander,
-			{id: 'uid', dataIndex : 'uid', width: 40, sortable: true, header : TYPO3.lang["column.uid"], hidden: true, filterable : true },
-			{id: 't3ver_oid', dataIndex : 't3ver_oid', width: 40, sortable: true, header : TYPO3.lang["column.oid"], hidden: true, filterable : true },
-			{id: 'workspace_Title', dataIndex : 'workspace_Title', width: 120, sortable: true, header : TYPO3.lang["column.workspaceName"], hidden: true, filter : {type : 'string'}},
-			TYPO3.Workspaces.Configuration.WsPath,
-			TYPO3.Workspaces.Configuration.LivePath,
-			TYPO3.Workspaces.Configuration.WsTitleWithIcon,
-			TYPO3.Workspaces.Configuration.SwapButton,
-			TYPO3.Workspaces.Configuration.TitleWithIcon,
-			TYPO3.Workspaces.Configuration.ChangeDate,
-			TYPO3.Workspaces.Configuration.ChangeState,
-			TYPO3.Workspaces.Configuration.Stage,
-			TYPO3.Workspaces.Configuration.RowButtons
-		],
-		listeners: {
-			columnmoved: function(colModel) {
-				TYPO3.Workspaces.Actions.updateColModel(colModel);
-			},
-			hiddenchange: function(colModel) {
-				TYPO3.Workspaces.Actions.updateColModel(colModel);
-			}
-		}
-	}),
+	colModel : null,
+
 	sm: TYPO3.Workspaces.SelectionModel,
 	loadMask : true,
 	height: 630,
