f1523f843727313a70b9c3b6fe004c1b9f86cd04
Michael Klapper <klapper@aoemedia.de>
2011-06-22 16:34:42 +0200

    [BUGFIX] Versioning with context menu is visible if workspaces are enabled
    Change-Id: I0b1e582e631e223cec3bd5759d72fb6e1368d5d3
    Resolves: #25962
    Releases: 4.6, 4.5

--- typo3/sysext/version/ext_tables.php
+++ typo3/sysext/version/ext_tables.php
@@ -4,12 +4,12 @@ if (!defined ('TYPO3_MODE')) {
 }
 
 if (TYPO3_MODE=='BE')	{
-	$GLOBALS['TBE_MODULES_EXT']['xMOD_alt_clickmenu']['extendCMclasses'][]=array(
-		'name' => 'tx_version_cm1',
-		'path' => t3lib_extMgm::extPath($_EXTKEY).'class.tx_version_cm1.php'
-	);
-
 	if (!t3lib_extMgm::isLoaded('workspaces')) {
+		$GLOBALS['TBE_MODULES_EXT']['xMOD_alt_clickmenu']['extendCMclasses'][]=array(
+			'name' => 'tx_version_cm1',
+			'path' => t3lib_extMgm::extPath($_EXTKEY).'class.tx_version_cm1.php'
+		);
+
 		t3lib_extMgm::addModule('web', 'txversionM1', '', t3lib_extMgm::extPath($_EXTKEY) . 'cm1/');
 	}
 }
