cd0d727487689b03fee06c3ec4c9b98e3463c41b
Xavier Perseguers <typo3@perseguers.ch>
2011-06-24 09:01:41 +0200

    [TASK] Add Parser for PHP locallang array
    Implement the parser to support LLPHP file as localization file format.
    
    Change-Id: I4e4d66e73d92eae57663b8e905db33a17da5cb2a
    Resolves: #27673

--- typo3/sysext/lang/classes/parser/class.tx_lang_parser_llphp.php
+++ typo3/sysext/lang/classes/parser/class.tx_lang_parser_llphp.php
@@ -44,10 +44,79 @@ class tx_lang_parser_Llphp implements tx_lang_parser {
 	 * @return void
 	 */
 	public function getParsedData($sourcePath, $languageKey, $charset = '') {
-		throw new RuntimeException(
-			'Parser for plain old PHP array is not yet implemented.',
-			1301654458
-		);
+		/** @var $csConvObj t3lib_cs */
+		if (is_object($GLOBALS['LANG'])) {
+			$csConvObj = $GLOBALS['LANG']->csConvObj;
+		} elseif (is_object($GLOBALS['TSFE'])) {
+			$csConvObj = $GLOBALS['TSFE']->csConvObj;
+		} else {
+			$csConvObj = t3lib_div::makeInstance('t3lib_cs');
+		}
+
+		if (@is_file($sourcePath) && $languageKey) {
+
+				// Set charsets:
+			$sourceCharset = $csConvObj->parse_charset($csConvObj->charSetArray[$languageKey] ? $csConvObj->charSetArray[$languageKey] : 'iso-8859-1');
+			if ($charset) {
+				$targetCharset = $csConvObj->parse_charset($charset);
+			} elseif ($GLOBALS['TYPO3_CONF_VARS']['BE']['forceCharset']) {
+					// When forceCharset is set, we store ALL labels in this charset!!!
+				$targetCharset = $csConvObj->parse_charset($GLOBALS['TYPO3_CONF_VARS']['BE']['forceCharset']);
+			} else {
+				$targetCharset = $csConvObj->parse_charset($csConvObj->charSetArray[$languageKey] ? $csConvObj->charSetArray[$languageKey] : 'iso-8859-1');
+			}
+
+				// Cache file name:
+			$hashSource = substr($sourcePath, strlen(PATH_site)) . '|' . date('d-m-Y H:i:s', filemtime($sourcePath)) . '|version=2.3';
+			$cacheFileName = PATH_site . 'typo3temp/llxml/' .
+					substr(basename($sourcePath), 10, 15) .
+					'_' . t3lib_div::shortMD5($hashSource) . '.' . $languageKey . '.' . $targetCharset . '.cache';
+				// Check if cache file exists...
+			if (!@is_file($cacheFileName)) {
+				$LOCAL_LANG = NULL;
+					// Get PHP data
+				include($sourcePath);
+				if (!is_array($LOCAL_LANG)) {
+					$fileName = substr($fileRef, strlen(PATH_site));
+					throw new RuntimeException(
+						'TYPO3 Fatal Error: "' . $fileName . '" is no TYPO3 language file!',
+						1308898491
+					);
+				}
+
+					// Converting the default language (English)
+					// This needs to be done for a few accented loan words and extension names
+				if (is_array($LOCAL_LANG['default']) && $targetCharset !== 'iso-8859-1') {
+					foreach ($LOCAL_LANG['default'] as &$labelValue) {
+						$labelValue = $csConvObj->conv($labelValue, 'iso-8859-1', $targetCharset);
+					}
+					unset($labelValue);
+				}
+
+				if ($langKey !== 'default' && is_array($LOCAL_LANG[$langKey]) && $sourceCharset != $targetCharset) {
+					foreach ($LOCAL_LANG[$langKey] as &$labelValue) {
+						$labelValue = $csConvObj->conv($labelValue, $sourceCharset, $targetCharset);
+					}
+					unset($labelValue);
+				}
+
+					// Cache the content now:
+				$serContent = array('origFile' => $hashSource, 'LOCAL_LANG' => array('default' => $LOCAL_LANG['default'], $langKey => $LOCAL_LANG[$langKey]));
+				$res = t3lib_div::writeFileToTypo3tempDir($cacheFileName, serialize($serContent));
+				if ($res) {
+					throw new RuntimeException(
+						'TYPO3 Fatal Error: "' . $res,
+						1308898501
+					);
+				}
+			} else {
+					// Get content from cache:
+				$serContent = unserialize(t3lib_div::getUrl($cacheFileName));
+				$LOCAL_LANG = $serContent['LOCAL_LANG'];
+			}
+
+			return $LOCAL_LANG;
+		}
 	}
 
 }
