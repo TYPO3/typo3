84c836a3c38011aa6fe1380416e50c9098d056c9
Michael Stucki <michael.stucki@typo3.org>
2011-07-06 20:40:47 +0200

    [BUGFIX] Fix fatal error when using caching framework and eID scripts
    When using the caching framework and calling an eID script containing
    locallang labels, it may happen that data from cache_hash is tried to be
    accessed.
    
    As there is no caching framework initialized at this time of execution,
    a fatal error is triggered:
    A cache with identifier "cache_hash" does not exist
    
    Fix the error by checking if the caching framework is initialized,
    otherwise skip its usage.
    
    Change-Id: Idafbcf0983fc189c5f71999a10c04ec4fd340f2b
    Resolves: #25086

--- t3lib/class.t3lib_page.php
+++ t3lib/class.t3lib_page.php
@@ -977,11 +977,13 @@ class t3lib_pageSelect {
 
 		if (TYPO3_UseCachingFramework) {
 			if (is_object($GLOBALS['typo3CacheManager'])) {
-				$contentHashCache = $GLOBALS['typo3CacheManager']->getCache('cache_hash');
-				$cacheEntry = $contentHashCache->get($hash);
+				if ($GLOBALS['typo3CacheManager']->hasCache('cache_hash')) {
+					$contentHashCache = $GLOBALS['typo3CacheManager']->getCache('cache_hash');
+					$cacheEntry = $contentHashCache->get($hash);
 
-				if ($cacheEntry) {
-					$hashContent = $cacheEntry;
+					if ($cacheEntry) {
+						$hashContent = $cacheEntry;
+					}
 				}
 			}
 		} else {
@@ -1014,12 +1016,14 @@ class t3lib_pageSelect {
 	public static function storeHash($hash, $data, $ident, $lifetime = 0) {
 		if (TYPO3_UseCachingFramework) {
 			if (is_object($GLOBALS['typo3CacheManager'])) {
-				$GLOBALS['typo3CacheManager']->getCache('cache_hash')->set(
-					$hash,
-					$data,
-					array('ident_' . $ident),
-					intval($lifetime)
-				);
+				if ($GLOBALS['typo3CacheManager']->hasCache('cache_hash')) {
+					$GLOBALS['typo3CacheManager']->getCache('cache_hash')->set(
+						$hash,
+						$data,
+						array('ident_' . $ident),
+						intval($lifetime)
+					);
+				}
 			}
 		} else {
 			$insertFields = array(
