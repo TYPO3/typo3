c867e6647179aadfecc2a41085460aa8b23817f6
Xavier Perseguers <typo3@perseguers.ch>
2011-07-08 01:29:41 +0200

    [TASK] Move XLIFF handling to t3lib
    Change-Id: I2bad66f9e2c92015e9aab4a06e8c92e96762ab9a
    Resolves: #28055

--- typo3/sysext/lang/ext_localconf.php
+++ typo3/sysext/lang/ext_localconf.php
@@ -3,19 +3,19 @@ if (!defined ('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-	// Register cache lang_l10n
-if (!is_array($TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['lang_l10n'])) {
-    $TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['lang_l10n'] = array();
+	// Register cache t3lib_l10n
+if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['t3lib_l10n'])) {
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['t3lib_l10n'] = array();
 }
 
-if (!isset($TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['lang_l10n']['backend'])) {
-    $TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['lang_l10n']['backend'] = 't3lib_cache_backend_FileBackend';
+if (!isset($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['t3lib_l10n']['backend'])) {
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['t3lib_l10n']['backend'] = 't3lib_cache_backend_FileBackend';
 }
 
 	// Register Clear Cache Menu hook
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['additionalBackendItems']['cacheActions']['clearLangCache'] = 'EXT:lang/hooks/clearcache/class.tx_lang_clearcachemenu.php:&tx_lang_clearcachemenu';
 
 	// Register Ajax call
-$TYPO3_CONF_VARS['BE']['AJAX']['lang::clearCache'] = 'EXT:lang/hooks/clearcache/class.tx_lang_clearcache.php:tx_lang_clearcache->clearCache';
+$GLOBALS['TYPO3_CONF_VARS']['BE']['AJAX']['lang::clearCache'] = 'EXT:lang/hooks/clearcache/class.tx_lang_clearcache.php:tx_lang_clearcache->clearCache';
 
 ?>
