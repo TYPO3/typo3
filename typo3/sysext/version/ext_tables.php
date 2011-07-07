b01cc891429030ce6092932a470ced29c7897c8e
Tolleiv Nietsch <info@tolleiv.de>
2011-07-07 04:58:32 +0200

    [TASK] Removed unused workspace code
    The workspace related parts within EXT:version aren't used anymore
    and therefore we can clean them up and save some
    space.
    
    Change-Id: Iec2b34f99e6c2a720b495031a5482778dba1f5bb
    Resolves: #9946
    Releases: 4.6

--- typo3/sysext/version/ext_tables.php
+++ typo3/sysext/version/ext_tables.php
@@ -9,8 +9,6 @@ if (TYPO3_MODE=='BE')	{
 			'name' => 'tx_version_cm1',
 			'path' => t3lib_extMgm::extPath($_EXTKEY).'class.tx_version_cm1.php'
 		);
-
-		t3lib_extMgm::addModule('web', 'txversionM1', '', t3lib_extMgm::extPath($_EXTKEY) . 'cm1/');
 	}
 }
 ?>
