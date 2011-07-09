0f2922414a15f8dcde543a73045daaa7ec7659f4
Christian Kuhn <lolli@schwarzbu.ch>
2011-07-09 15:22:53 +0200

    [TASK] Decouple extMgm::extPath from TYPO3_LOADED_EXT
    To simplify the bootstrap it is required to make
    t3lib_extMgm::extList() work more early. This is achieved
    by removing the dependency to $GLOBALS['TYPO3_LOADED_EXT']
    
    Change-Id: I73aa8c3a72979c85bf6f34cf027565199e380920
    Resolves: #28034

--- tests/t3lib/class.t3lib_extmgmTest.php
+++ tests/t3lib/class.t3lib_extmgmTest.php
@@ -48,8 +48,27 @@ class t3lib_extmgmTest extends tx_phpunit_testcase {
 	 */
 	protected $backupGlobalsBlacklist = array('TYPO3_DB');
 
+	/**
+	 * phpunit still needs some globals that are
+	 * reconstructed before $backupGlobals is handled. Those
+	 * important globals are handled in tearDown() directly.
+	 *
+	 * @var array
+	 */
+	protected $globals = array();
+
+	public function setUp() {
+		$this->globals = array(
+			'TYPO3_LOADED_EXT' => serialize($GLOBALS['TYPO3_LOADED_EXT']),
+		);
+	}
+
 	public function tearDown() {
 		t3lib_extMgm::clearExtensionKeyMap();
+
+		foreach ($this->globals as $key => $value) {
+			$GLOBALS[$key] = unserialize($value);
+		}
 	}
 
 	///////////////////////////////
@@ -71,6 +90,43 @@ class t3lib_extmgmTest extends tx_phpunit_testcase {
 	public function extPathAppendsScriptNameToPath() {
 		$GLOBALS['TYPO3_LOADED_EXT']['foo']['siteRelPath'] = 'foo/';
 		$this->assertSame(PATH_site . 'foo/bar.txt', t3lib_extMgm::extPath('foo', 'bar.txt'));
+	}
+
+	/**
+	 * @test
+	 * @expectedException BadFunctionCallException
+	 */
+	public function extPathThrowsExceptionIfExtensionIsNotLoadedAndTypo3LoadedExtensionsIsEmpty() {
+		unset($GLOBALS['TYPO3_LOADED_EXT']);
+		$GLOBALS['TYPO3_CONF_VARS']['EXT']['requiredExt'] = '';
+		$GLOBALS['TYPO3_CONF_VARS']['EXT']['extList'] = '';
+		t3lib_extMgm::extPath('bar');
+	}
+
+	/**
+	 * @test
+	 */
+	public function extPathSearchesForPathOfExtensionInRequiredExtensionList() {
+		$this->setExpectedException('Exception', '', 1294430951);
+
+		unset($GLOBALS['TYPO3_LOADED_EXT']);
+		$GLOBALS['TYPO3_CONF_VARS']['EXT']['requiredExt'] = 'foo';
+		$GLOBALS['TYPO3_CONF_VARS']['EXT']['extList'] = '';
+
+		t3lib_extMgm::extPath('foo');
+	}
+
+	/**
+	 * @test
+	 */
+	public function extPathSearchesForPathOfExtensionInExtList() {
+		$this->setExpectedException('Exception', '', 1294430951);
+
+		unset($GLOBALS['TYPO3_LOADED_EXT']);
+		$GLOBALS['TYPO3_CONF_VARS']['EXT']['requiredExt'] = '';
+		$GLOBALS['TYPO3_CONF_VARS']['EXT']['extList'] = 'foo';
+
+		t3lib_extMgm::extPath('foo');
 	}
 
 	//////////////////////
