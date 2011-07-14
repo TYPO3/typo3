e8e26558a623642a3c8fd78daf5e2628099beaba
Oliver Hader <olly@typo3.org>
2011-07-15 00:50:41 +0200

    [FEATURE] Session tables: add key for ses_timestamp and simplify timeout-check
    * add a key for ses_timestamp (in SQL) to speedup searches
      for sessions within timeout-limit
    * simplify the overly-complex timeout-check
    
    Change-Id: I171a63a0a7ec8cdd17c59d34411b41483d672b0f
    Resolves: #20877

--- typo3/sysext/cms/ext_tables.sql
+++ typo3/sysext/cms/ext_tables.sql
@@ -97,7 +97,8 @@ CREATE TABLE fe_sessions (
   ses_tstamp int(11) unsigned DEFAULT '0' NOT NULL,
   ses_data blob,
   ses_permanent tinyint(1) unsigned DEFAULT '0' NOT NULL,
-  PRIMARY KEY (ses_id,ses_name)
+  PRIMARY KEY (ses_id,ses_name),
+  KEY ses_tstamp (ses_tstamp)
 ) ENGINE=InnoDB;
 
 
