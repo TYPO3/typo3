7b80f52e70e1fd5cc9cafa9f79648ae3c77fe520
Dmitry Dulepov <dmitry@typo3.org>
2011-07-14 15:23:32 +0200

    [API][!!!] Fix API compatibility break in tslib_pibase
    Revision 50cbf264 removed the old tslib_pibase constructor without
    deprecating it first as required by TYPO3 core rules. This breaks
    every FE plugin, which overrides the old tslib_pibase constructor.
    The change was commited to Git here: https://review.typo3.org/1797
    
    The fix: reintroduce the old constructor and deprecate it.
    
    Change-Id: I66653025b9bf9e2d1e4a0dddb6d0913c50c387c2
    Resolves: #28208
    Releases: 4.6

--- typo3/sysext/cms/tslib/class.tslib_pibase.php
+++ typo3/sysext/cms/tslib/class.tslib_pibase.php
@@ -162,6 +162,15 @@ class tslib_pibase {
 	}
 
 	/**
+	 * Compatibility constructor.
+	 *
+	 * @deprecated since TYPO3 4.6 and will be removed in TYPO3 4.7. Use __construct() instead.
+	 */
+	public function tslib_pibase() {
+		tslib_pibase::__construct();
+	}
+
+	/**
 	 * If internal TypoScript property "_DEFAULT_PI_VARS." is set then it will merge the current $this->piVars array onto these default values.
 	 *
 	 * @return	void
