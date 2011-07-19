62d9f05954b15b683d6043ab252edf9f9b22541a
Georg Schoenweger <georg.schoenweger@gmail.com>
2011-07-19 18:36:00 +0200

    [TASK] call $GLOBALS['TSFE']->includeTCA() in css_styled_content
    css_styled_content doesn't call $GLOBALS['TSFE']->includeTCA() before
    running t3lib_div::loadTCA('tt_content'). Thus only the default
    tt_content TCA definitions are loaded. Any extensions extending
    tt_content TCA is ignored.
    
    Change-Id: Icd68a4126f5633a4802822556c6e4f608e604f0f
    Resolves: #27922

--- typo3/sysext/css_styled_content/pi1/class.tx_cssstyledcontent_pi1.php
+++ typo3/sysext/css_styled_content/pi1/class.tx_cssstyledcontent_pi1.php
@@ -276,6 +276,7 @@ class tx_cssstyledcontent_pi1 extends tslib_pibase {
 					// Get the list of files from the field
 				$field = (trim($conf['field']) ? trim($conf['field']) : 'media');
 				$fileList = $this->cObj->data[$field];
+				$GLOBALS['TSFE']->includeTCA();
 				t3lib_div::loadTCA('tt_content');
 				$path = 'uploads/media/';
 				if (is_array($GLOBALS['TCA']['tt_content']['columns'][$field]) && !empty($GLOBALS['TCA']['tt_content']['columns'][$field]['config']['uploadfolder'])) {
