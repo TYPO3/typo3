368142aaed0dad7cc0e03f1049bf3118243ef37a
Popy <popy.dev@gmail.com>
2011-07-21 17:04:02 +0200

    [FEATURE] Generate cHash only if needed
    tslib_cObj->typolink() generates cHash in every case, even if not
    needed.
    
    Change-Id: Id6a015e6bd89e9baeafd9532d4a21018adf91691
    Resolves: #28352

--- typo3/sysext/cms/tslib/class.tslib_content.php
+++ typo3/sysext/cms/tslib/class.tslib_content.php
@@ -5565,7 +5565,9 @@ class tslib_cObj {
 								// Mind the order below! See http://bugs.typo3.org/view.php?id=5117
 							$params = $GLOBALS['TSFE']->linkVars . $addQueryParams;
 							if (trim($params, '& ') != '') {
-								$addQueryParams .= '&cHash=' . t3lib_div::generateCHash($params);
+								if (count($params)) {
+									$addQueryParams .= '&cHash=' . t3lib_div::generateCHash($params);
+								}
 							}
 							unset($params);
 						}
