a274247591d0c7e68ef5bc535f4e5b056ad60dfe
Kay Strobach <typo3@kay-strobach.de>
2011-07-24 10:31:39 +0200

    [FEATURE] Add ExtJS xtype:modulepanel for BE Modules
    This is a follow up change to #12664.
    It adds a new Extjs xtype called modulepanel and the required styles.
    
    This way we can have extjs modules which fit the TYPO3 look'n'feel.
    
    The CSS modifies the tbar and the bbar to look like the standard
    toolbars in TYPO3.
    
    4.5 is added to releases, as #12644 is the direct parent.
    
    Change-Id: I596962fc81e66368db06059447b2f6a09db4ff73
    Resolves: #28384
    Releases: 4.6, 4.5

---
+++ typo3/js/extjs/modulepanel.js
@@ -0,0 +1,45 @@
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2011 Kay Strobach <typo3@kay-strobach.de>
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*  A copy is found in the textfile GPL.txt and important notices to the license
+*  from the author is found in LICENSE.txt distributed with these scripts.
+*
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+TYPO3.modulePanel = Ext.extend(Ext.Panel, {
+	name: 'modulePanel',
+	maskMessage: ' ',
+	doMask: true,
+	border: false,
+		// component build
+	initComponent: function() {
+		Ext.apply(this, {
+			tbarCfg: {
+				cls: 't3skin-typo3-module-panel-toolbar'
+			},
+			bbarCfg: {
+				cls: 't3skin-typo3-module-panel-toolbar'
+			}
+		});
+		TYPO3.modulePanel.superclass.initComponent.apply(this, arguments);
+	}
+});
+Ext.reg('modulePanel', TYPO3.modulePanel);
