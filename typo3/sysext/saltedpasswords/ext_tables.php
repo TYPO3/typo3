d090b3bef48d707bb7c1d3570fc014c46b8f10e6
Laurent Cherpit <laurent.cherpit@gmail.com>
2011-07-26 18:20:42 +0200

    [FEATURE] Change the way saltedpasswords sets the "eval" in ext_tables.php.
    For the TCA password fields (BE|FE), saltedpasswords overrides the field
    used to add some evaluations method. The problem appears when another evaluation method
    is added before saltedpasswords extension is loaded.
    Change the way of the available evaluations methods are added by saltedpasswords.
    
    Change-Id: Ibcc13da492736891937fefbf9e323338229fb1dc
    Releases: 4.6

--- typo3/sysext/saltedpasswords/ext_tables.php
+++ typo3/sysext/saltedpasswords/ext_tables.php
@@ -3,18 +3,52 @@ if (!defined ('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
+
 t3lib_div::loadTCA('fe_users');
-$TCA['fe_users']['columns']['password']['config']['max'] = 60;
+$GLOBALS['TCA']['fe_users']['columns']['password']['config']['max'] = 60;
 
 if (tx_saltedpasswords_div::isUsageEnabled('FE')) {
-	$TCA['fe_users']['columns']['password']['config']['eval'] = 'trim,required,tx_saltedpasswords_eval_fe,password';
+
+		// Get eval field operations methods as array
+	$operations = t3lib_div::trimExplode(',', $GLOBALS['TCA']['fe_users']['columns']['password']['config']['eval'], TRUE);
+
+		// Remove md5 from the list of evaluated methods
+	$index = array_search('md5', $operations, TRUE);
+	unset($operations[$index]);
+
+		// Get position index of password string value
+	$index = array_search('password', $operations, TRUE);
+	$replacement = array('tx_saltedpasswords_eval_fe', 'password');
+
+		// Remove a portion of the array from index and replace it. To have "password" as last operation.
+	array_splice($operations, $index, count($replacement), $replacement);
+
+	$GLOBALS['TCA']['fe_users']['columns']['password']['config']['eval'] = implode($operations, ',');
+	unset($operations, $index, $replacement);
 }
 
+
 t3lib_div::loadTCA('be_users');
-$TCA['be_users']['columns']['password']['config']['max'] = 60;
+$GLOBALS['TCA']['be_users']['columns']['password']['config']['max'] = 60;
 
 if (tx_saltedpasswords_div::isUsageEnabled('BE')) {
-	$TCA['be_users']['columns']['password']['config']['eval'] = 'trim,required,tx_saltedpasswords_eval_be,password';
+
+		// Get eval field operations methods as array
+	$operations = t3lib_div::trimExplode(',', $GLOBALS['TCA']['be_users']['columns']['password']['config']['eval'], TRUE);
+
+		// Remove md5 from the list of evaluated methods
+	$index = array_search('md5', $operations, TRUE);
+	unset($operations[$index]);
+
+		// Get position index of password string value
+	$index = array_search('password', $operations, TRUE);
+	$replacement = array('tx_saltedpasswords_eval_be', 'password');
+
+		// Remove a portion of the array from index and replace it. To have "password" as last operation.
+	array_splice($operations, $index, count($replacement), $replacement);
+
+	$GLOBALS['TCA']['be_users']['columns']['password']['config']['eval'] = implode($operations, ',');
+	unset($operations, $index, $replacement);
 
 		// Prevent md5 hashing on client side via JS
 	$GLOBALS['TYPO3_USER_SETTINGS']['columns']['password']['eval'] = '';
