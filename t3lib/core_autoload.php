07935f0e04245bd2c60ae085b20ae7ff3e7c55fa
Xavier Perseguers <typo3@perseguers.ch>
2011-07-27 09:54:12 +0200

    [FEATURE] Backport t3lib_utility_Version
    Deprecating t3lib_div::int_from_ver() in 4.6 leads to major developer
    annoyance because this method is used to switch to compatibility code
    for older versions of TYPO3 in extensions and a hocus-pocus is needed
    to prevent calling it if using 4.6.
    
    Prepare this major deprecation by backporting
    t3lib_utility_Version::convertVersionNumberToInteger() but without
    refactoring existing code to use it.
    
    Change-Id: I72673a483e5e716b24ebb6a89aaee3e09aaa7e65
    Resolves: #28499
    Releases: 4.5
    Reviewed-on: http://review.typo3.org/3732
    Reviewed-by: Xavier Perseguers
    Tested-by: Xavier Perseguers
    Reviewed-by: Benjamin Mack
    Reviewed-by: Georg Ringer
    Tested-by: Georg Ringer

--- t3lib/core_autoload.php
+++ t3lib/core_autoload.php
@@ -160,6 +160,7 @@ $t3libClasses = array(
 	't3lib_utility_mail' => PATH_t3lib . 'utility/class.t3lib_utility_mail.php',
 	't3lib_utility_phpoptions' => PATH_t3lib . 'utility/class.t3lib_utility_phpoptions.php',
 	't3lib_utility_debug' => PATH_t3lib . 'utility/class.t3lib_utility_debug.php',
+	't3lib_utility_versionnumber' => PATH_t3lib . 'utility/class.t3lib_utility_versionnumber.php',
 	't3lib_spritemanager' => PATH_t3lib . 'class.t3lib_spritemanager.php',
 	't3lib_spritemanager_spritegenerator' => PATH_t3lib . 'spritemanager/class.t3lib_spritemanager_spritegenerator.php',
 	't3lib_spritemanager_spriteicongenerator' => PATH_t3lib . 'interfaces/interface.t3lib_spritemanager_spriteicongenerator.php',
