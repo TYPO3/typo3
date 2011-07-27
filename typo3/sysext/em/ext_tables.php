3039468c890f20002a51db40e3c73c6abd11d130
Stefan Galinski <stefan.galinski@gmail.com>
2011-07-27 12:51:23 +0200

    [BUGFIX] Ext Direct API lacks the possibility of adding access control
    Change-Id: I069b13ed5a36b60c1b1c03d027680e7bdb2989e7
    Resolves: #25307

--- typo3/sysext/em/ext_tables.php
+++ typo3/sysext/em/ext_tables.php
@@ -5,9 +5,21 @@ if (!defined ('TYPO3_MODE')) {
 
 if (TYPO3_MODE === 'BE') {
 	t3lib_extMgm::addModule('tools', 'em', '', t3lib_extMgm::extPath($_EXTKEY) . 'classes/');
+
 		// register Ext.Direct
-	$TYPO3_CONF_VARS['SC_OPTIONS']['ExtDirect']['TYPO3.EM.ExtDirect'] = t3lib_extMgm::extPath($_EXTKEY) . 'classes/connection/class.tx_em_connection_extdirectserver.php:tx_em_Connection_ExtDirectServer';
-	$TYPO3_CONF_VARS['SC_OPTIONS']['ExtDirect']['TYPO3.EMSOAP.ExtDirect'] = t3lib_extMgm::extPath($_EXTKEY) . 'classes/connection/class.tx_em_connection_extdirectsoap.php:tx_em_Connection_ExtDirectSoap';
+	t3lib_extMgm::registerExtDirectComponent(
+		'TYPO3.EM.ExtDirect',
+		t3lib_extMgm::extPath($_EXTKEY) . 'classes/connection/class.tx_em_connection_extdirectserver.php:tx_em_Connection_ExtDirectServer',
+		'tools_em',
+		'admin'
+	);
+
+	t3lib_extMgm::registerExtDirectComponent(
+		'TYPO3.EMSOAP.ExtDirect',
+		t3lib_extMgm::extPath($_EXTKEY) . 'classes/connection/class.tx_em_connection_extdirectsoap.php:tx_em_Connection_ExtDirectSoap',
+		'tools_em',
+		'admin'
+	);
 
 		// register reports check
 	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['reports']['tx_reports']['status']['providers']['Extension Manager'][] = 'tx_em_reports_ExtensionStatus';
