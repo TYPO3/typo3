ae550527fc25d426055d8b1bd26e9ae97fb1df28
Kay Strobach <typo3@kay-strobach.de>
2011-07-31 20:43:17 +0200

    [BUGFIX] Add JS Polling to TceForms based on ExtDirect
    This bugfix adds a polling algorithm to the tceforms to check,
    wether a record is changed, this is a bugfix, because it lowers the
    increased risk of a lost update with the new cardlayout.
    
    The change is detected by using the lastmod timestamp of the related
    record.
    
    Logic of this change:
    1. onReady
    2. find formElements named according tceforms convention and create
       list of edited records
    3. initial poll for lastchange
    4. periodical polling
    
    After each polling the lastchange time is checked for changes, having a
    change you get an Ext.Msg.confirm to decide, wether to reload the form
    or merge the changes manually.
    
    Change-Id: I89119ea6bed827b1cb44f0c6380a13961f328697
    Resolves: #28618
    Releases: 4.6, 4.5

---
+++ t3lib/tceforms/extdirect/class.t3lib_tceforms_extdirect_commands.php
@@ -0,0 +1,57 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2011 Kay Strobach <typo3@kay-strobach.de>
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*  A copy is found in the textfile GPL.txt and important notices to the license
+*  from the author is found in LICENSE.txt distributed with these scripts.
+*
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+/**
+ * Commands tceforms
+ *
+ * @author Kay Strobach <typo3@kay-strobach.de>
+ * @package TYPO3
+ * @subpackage t3lib
+ */
+class t3lib_tceforms_extdirect_Commands {
+	/**
+	 * Returns the tstamp of a record selected by it's table and uid
+	 * Additionally this function checks wether the records is opened for editing
+	 *
+	 * @param string $table The table where you want to search the record
+	 * @param integer $uid The uid of the searched record
+	 * 
+	 * @return	array 
+	 */	 	
+	public static function getRecordLastchange($table, $uid) {
+			// build lastchange
+		$record = t3lib_BEfunc::getRecord($table,$uid);
+		$return = array(
+			'tstamp' => $record['tstamp'],
+			'table' => $table,
+			'uid' => $uid,
+			'title' => t3lib_BEfunc::getRecordTitle($table, $record, TRUE, TRUE),
+			'tableTranslated' => $GLOBALS['LANG']->sL($GLOBALS['TCA'][$table]['ctrl']['title'], TRUE),
+		);
+		return $return;
+	}
+}
