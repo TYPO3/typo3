f8487b53a0f0ef885631b5827c8a7ff2042d624c
Xavier Perseguers <xavier@typo3.org>
2011-08-01 02:04:28 +0200

    [TASK] Update email address
    Change-Id: I65185e3157607c21ff65ab92abf1a14449c76e5a

--- typo3/sysext/dbal/class.ux_t3lib_sqlparser.php
+++ typo3/sysext/dbal/class.ux_t3lib_sqlparser.php
@@ -4,7 +4,7 @@
  *
  *  (c) 2004-2009 Kasper Skårhøj (kasperYYYY@typo3.com)
  *  (c) 2004-2009 Karsten Dambekalns <karsten@typo3.org>
- *  (c) 2009-2011 Xavier Perseguers <typo3@perseguers.ch>
+ *  (c) 2009-2011 Xavier Perseguers <xavier@typo3.org>
  *  All rights reserved
  *
  *  This script is part of the TYPO3 project. The TYPO3 project is
