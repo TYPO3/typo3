c57b1b968734a16feb5b1ddc50b3fca9619f46cb
Steffen Gebert <steffen.gebert@typo3.org>
2011-08-08 21:48:28 +0200

    [TASK] Refactor t3lib_message_AbstractMessage
    Change-Id: I5a63a5ad1d491af6bbf9ac374b9df3d3f7a4d9b8
    Resolves: #29495

--- t3lib/core_autoload.php
+++ t3lib/core_autoload.php
@@ -53,6 +53,7 @@ $t3libClasses = array(
 	't3lib_loadmodules' => PATH_t3lib . 'class.t3lib_loadmodules.php',
 	't3lib_lock' => PATH_t3lib . 'class.t3lib_lock.php',
 	't3lib_message_abstractmessage' => PATH_t3lib . 'message/class.t3lib_message_abstractmessage.php',
+	't3lib_message_abstractstandalonemessage' => PATH_t3lib . 'message/class.t3lib_message_abstractstandalonemessage.php',
 	't3lib_message_errorpagemessage' => PATH_t3lib . 'message/class.t3lib_message_errorpagemessage.php',
 	't3lib_modsettings' => PATH_t3lib . 'class.t3lib_modsettings.php',
 	't3lib_pageselect' => PATH_t3lib . 'class.t3lib_page.php',
