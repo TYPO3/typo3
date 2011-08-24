f31be62e86544d16405ec8d9f818d8c56c4b6343
Stanislas Rolland <typo3@sjbr.ca>
2011-08-24 11:15:08 -0400

    [BUGFIX] Incorrect behaviour of style selector in RTE
    When all classes allowed on an element have been assigned to a single
    element, the block/text style selector becomes disabled. It is then not
    possible to remove the assigned classes from this element.
    
    Change-Id: Ifc5ca8e4fc07c8627a07fe94854ceda056b2e557
    Resolves: #27801
    Releases: 4.4, 4.5, 4.6

--- typo3/sysext/rtehtmlarea/htmlarea/plugins/BlockStyle/block-style.js
+++ typo3/sysext/rtehtmlarea/htmlarea/plugins/BlockStyle/block-style.js
@@ -346,13 +346,15 @@ HTMLArea.BlockStyle = Ext.extend(HTMLArea.Plugin, {
 					store.getAt(0).set('text', this.localize('Remove style'));
 				}
 			}
+				// Remove already assigned classes from the dropDown box
+			var classNamesString = ',' + classNames.join(',') + ',';
 			store.each(function (option) {
-				if (store.indexOf(option) != index && (',' + classNames.join(',') + ',').indexOf(',' + option.get('value') + ',') != -1) {
+				if (classNamesString.indexOf(',' + option.get('value') + ',') != -1) {
 					store.removeAt(store.indexOf(option));
 				}
 				return true;
 			});
 		}
-		dropDown.setDisabled(!(store.getCount()>1));
+		dropDown.setDisabled(!store.getCount() || (store.getCount() == 1 && dropDown.getValue() == 'none'));
 	}
 });
