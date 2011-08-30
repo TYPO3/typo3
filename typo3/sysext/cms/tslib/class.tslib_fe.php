c46b96688907375f6b2c41eec5997393332a6aa1
Daniel Pötzinger <poetzinger@aoemedia.de>
2011-08-30 22:30:24 +0200

    [FEATURE] Flexible cHash Calculation
    Introduces new configuration options for $TYPO3_CONF_VARS['FE']
    * cHashRequiredParameters
    * cHashExcludedParameters
    * cHashOnlyForParameters
    
    Moves the adjusted cHash calculation logic to it's own class
    t3lib_cacheHash.
    
    Change-Id: I257ddd2042015622872519db18deee1b5071f950
    Resolves: #29365
    Releases: 4.7

--- typo3/sysext/cms/tslib/class.tslib_fe.php
+++ typo3/sysext/cms/tslib/class.tslib_fe.php
@@ -1732,7 +1732,10 @@
 	 *******************************************/
 
 	/**
-	 * Calculates a hash string based on additional parameters in the url. This is used to cache pages with more parameters than just id and type
+	 * Calculates a hash string based on additional parameters in the url.
+	 *
+	 * Calculated hash is stored in $this->cHash_array.
+	 * This is used to cache pages with more parameters than just id and type.
 	 *
 	 * @return	void
 	 * @see reqCHash()
@@ -1745,8 +1748,9 @@
 
 		$GET = t3lib_div::_GET();
 		if ($this->cHash && is_array($GET))	{
-			$this->cHash_array = t3lib_div::cHashParams(t3lib_div::implodeArrayForUrl('',$GET));
-			$cHash_calc = t3lib_div::calculateCHash($this->cHash_array);
+			$cacheHash = t3lib_div::makeInstance('t3lib_cacheHash');
+			$this->cHash_array = $cacheHash->getRelevantParameters(t3lib_div::implodeArrayForUrl('', $GET));
+			$cHash_calc = $cacheHash->calculateCacheHash($this->cHash_array);
 
 			if ($cHash_calc!=$this->cHash)	{
 				if ($this->TYPO3_CONF_VARS['FE']['pageNotFoundOnCHashError']) {
@@ -1755,6 +1759,12 @@
 					$this->disableCache();
 					$GLOBALS['TT']->setTSlogMessage('The incoming cHash "'.$this->cHash.'" and calculated cHash "'.$cHash_calc.'" did not match, so caching was disabled. The fieldlist used was "'.implode(',',array_keys($this->cHash_array)).'"',2);
 				}
+			}
+		} elseif (is_array($GET)) {
+				// no cHash is set, check if that is correct
+			$cacheHash = t3lib_div::makeInstance('t3lib_cacheHash');
+			if ($cacheHash->parametersRequireCacheHash(t3lib_div::implodeArrayForUrl('', $GET))) {
+				$this->reqCHash();
 			}
 		}
 	}
@@ -1789,7 +1799,8 @@
 	 * @obsolete
 	 */
 	function cHashParams($addQueryParams) {
-		return t3lib_div::cHashParams($addQueryParams);
+		$cacheHash = t3lib_div::makeInstance('t3lib_cacheHash');
+		return $cacheHash->calculateCacheHash($addQueryParams);
 	}
 
 	/**
