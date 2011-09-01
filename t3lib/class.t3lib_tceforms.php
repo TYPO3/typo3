6a9eb54bc56acf21015699d41e3861be848c3174
Andreas Wolf <andreas.wolf@ikt-werk.de>
2011-09-01 19:39:38 +0200

    [BUGFIX] Error when editing a record with additionalPreviewLanguage and an deleted l18n_parent
    The behaviour that causes the error (translated elements are not deleted
    when deleting parent) is fixed in recent TYPO3 versions; it is better to
    do that additional check anyways (for old installations).
    
    Change-Id: I4daf02f6b60daf2c1a1f7daad1683001deda40e5
    Resolves: #17910
    Releases: 4.4, 4.5, 4.6

--- t3lib/class.t3lib_tceforms.php
+++ t3lib/class.t3lib_tceforms.php
@@ -3417,7 +3417,7 @@ class t3lib_TCEforms	{
 			foreach($prLang as $prL)	{
 				$t8Tools = t3lib_div::makeInstance('t3lib_transl8tools');
 				$tInfo = $t8Tools->translationInfo($lookUpTable,intval($rec[$TCA[$table]['ctrl']['transOrigPointerField']]),$prL['uid']);
-				if (is_array($tInfo['translations'][$prL['uid']]))	{
+				if (is_array($tInfo['translations']) && is_array($tInfo['translations'][$prL['uid']]))	{
 					$this->additionalPreviewLanguageData[$table.':'.$rec['uid']][$prL['uid']] = t3lib_BEfunc::getRecordWSOL($table, intval($tInfo['translations'][$prL['uid']]['uid']));
 				}
 			}
