996c9f65da4f85f9a03399969895c29baaaeba57
Francois Suter <francois@typo3.org>
2011-09-09 14:30:45 +0200

    [TASK] EM: make update window use full screen
    The UPDATE! script pop-up window in the Extension Manager is hard-coded
    to some reduced size. Make it use the full screen instead. This also
    makes it de facto modal, otherwise it can be launched several times.
    
    Change-Id: Icd8c353c96b91155b6b3bb3bfbf161d33d9b8626
    Resolves: #29593
    Releases: 4.6, 4.5

--- typo3/sysext/em/res/js/em_locallist.js
+++ typo3/sysext/em/res/js/em_locallist.js
@@ -72,8 +72,7 @@ TYPO3.EM.LocalList = Ext.extend(Ext.grid.GridPanel, {
 												panel.getEl().unmask();
 												if (response.success) {
 													var w = new Ext.Window({
-																width: 500,
-																height: 500,
+																maximized: true,
 																layout: 'fit',
 																title: TYPO3.lang.ext_details_updateScript + ' (' + record.data.extkey + ')',
 																items: [
