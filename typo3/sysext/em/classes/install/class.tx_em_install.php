4163d01e013de9eaa2208934c61062c61078c6d2
Helmut Hummel <helmut.hummel@typo3.org>
2011-09-10 12:28:27 +0200

    [BUGFIX] Show extension configuration after extension update
    When updating an extension, updates of database tables are not
    shown and not offered to be updated. Thus they remain unnoticed
    and this leads to unexpected results.
    
    Show extension configuration and database updates after updating an extension.
    
    Resolves: #29625
    Releases: 4.5, 4.6
    
    Change-Id: I82281494b71cd9fa38235ebafaa2b2423d15436d
    Reviewed-on: http://review.typo3.org/4898
    Reviewed-by: Philipp Gampe
    Tested-by: Philipp Gampe
    Reviewed-by: Georg Ringer
    Tested-by: Georg Ringer

--- typo3/sysext/em/classes/install/class.tx_em_install.php
+++ typo3/sysext/em/classes/install/class.tx_em_install.php
@@ -276,7 +276,7 @@ class tx_em_Install {
 													$messageContent,
 													$GLOBALS['LANG']->getLL('ext_import_success')
 												);
-												$content = $flashMessage->render();
+												$content = $flashMessage->render() . $updateContent;
 											} else {
 												$content = $updateContent;
 											}
