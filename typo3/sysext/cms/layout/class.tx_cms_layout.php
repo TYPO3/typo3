9882256b9c82de6f1d1d9e013ae8236f78b579ea
Felix Kopp <felix-source@phorax.com>
2011-09-22 16:27:37 +0200

    [BUGFIX] Page module: language table width
    The table to display languages in page module next to each other
    gets the same width as page module table to display columns.
    Styles language table head cell background correctly.
    
    Change-Id: I5e67cf22a2667a4fe7a81257ac844e019c281546
    Resolves: #30206
    Releases: 4.7

--- typo3/sysext/cms/layout/class.tx_cms_layout.php
+++ typo3/sysext/cms/layout/class.tx_cms_layout.php
@@ -462,7 +462,7 @@ class tx_cms_layout extends recordList {
 						} else {
 							$out .= '
 								<td><img src="clear.gif" width="4" height="1" alt="" /></td>
-								<td bgcolor="#cfcfcf"><img src="clear.gif" width="1" height="1" alt="" /></td>
+								<td class="t3-page-border"><img src="clear.gif" width="1" height="1" alt="" /></td>
 								<td><img src="clear.gif" width="4" height="1" alt="" /></td>';
 						}
 						$out .= '
@@ -479,7 +479,7 @@ class tx_cms_layout extends recordList {
 
 					// Wrap the cells into a table row:
 					$out = '
-					<table border="0" cellpadding="0" cellspacing="0" class="t3-page-columns" width="100%">
+					<table border="0" cellpadding="0" cellspacing="0" class="t3-page-columns">
 						<tr>' . $out . '
 						</tr>
 					</table>';
@@ -565,7 +565,7 @@ class tx_cms_layout extends recordList {
 				// Separator between language columns (black thin line)
 				$midSep = '
 						<td><img src="clear.gif" width="4" height="1" alt="" /></td>
-						<td bgcolor="black"><img src="clear.gif" width="1" height="1" alt="" /></td>
+						<td class="t3-page-border"><img src="clear.gif" width="1" height="1" alt="" /></td>
 						<td><img src="clear.gif" width="4" height="1" alt="" /></td>';
 
 				// Traverse languages found on the page and build up the table displaying them side by side:
@@ -600,13 +600,10 @@ class tx_cms_layout extends recordList {
 					$sCont[$lP] = '
 						<td nowrap="nowrap">' . $lPLabel . '</td>';
 				}
+
 				// Add headers:
-				$out .= '
-					<tr class="bgColor5">' . implode($midSep, $cCont) . '
-					</tr>';
-				$out .= '
-					<tr class="bgColor5">' . implode($midSep, $sCont) . '
-					</tr>';
+				$out .= '<tr>' . implode($midSep, $cCont) . '</tr>';
+				$out .= '<tr class="bgColor5">' . implode($midSep, $sCont) . '</tr>';
 
 				// Traverse previously built content for the columns:
 				foreach ($languageColumn as $cKey => $cCont) {
@@ -646,7 +643,7 @@ class tx_cms_layout extends recordList {
 
 				// Finally, wrap it all in a table and add the language selector on top of it:
 				$out = $languageSelector . '
-					<table border="0" cellpadding="0" cellspacing="0" width="480" class="typo3-page-langMode">
+					<table cellpadding="0" cellspacing="0" class="t3-page-langMode">
 						' . $out . '
 					</table>';
 
