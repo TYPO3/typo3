78d3b50b54fdbdc0c64bc6d5a560118ceab6cb5b
Andreas Wolf <andreas.wolf@ikt-werk.de>
2011-10-10 14:05:33 +0200

    [BUGFIX] Copyright year in HTML comment is harcoded
    The copyright year in the HTML comment is hardcoded as 1998-2009. This
    should be changed to using the constant.
    
    Change-Id: I475f9a221acd4da4fe5376ee73d1b6bed2c3ccaa
    Resolves: #30725
    Releases: 4.4, 4.5, 4.6

--- typo3/template.php
+++ typo3/template.php
@@ -1282,7 +1282,7 @@ $str.=$this->docBodyTagBegin().
 	 * @return	string		<meta> tag with name "generator"
 	 */
 	function generator()	{
-		$str = 'TYPO3 '.TYPO3_branch.', ' . TYPO3_URL_GENERAL . ', &#169; Kasper Sk&#229;rh&#248;j 1998-2009, extensions are copyright of their respective owners.';
+		$str = 'TYPO3 '.TYPO3_branch.', ' . TYPO3_URL_GENERAL . ', &#169; Kasper Sk&#229;rh&#248;j ' . TYPO3_copyright_year . ', extensions are copyright of their respective owners.';
 		return '<meta name="generator" content="'.$str .'" />';
 	}
 
