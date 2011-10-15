fa2ccd25e00d806587228fb7eeaf65a684a11e83
Simon Schaufelberger <simonschaufi@googlemail.com>
2011-10-15 11:38:13 +0200

    [BUGFIX] date expects parameter 2 to be long, string given
    Any zero length string value is replaced with the current timestamp.
    (Just like the default value for the second parameter of date/gmdate.)
    
    Change-Id: I231b704c3df181486c37497e0dc6f10baf1d267e
    Fixes: #30931
    Releases: 4.6, 4.5, 4.4

--- typo3/sysext/cms/tslib/class.tslib_content.php
+++ typo3/sysext/cms/tslib/class.tslib_content.php
@@ -2547,11 +2547,13 @@ class tslib_cObj {
 	 * Will return a formatted date based on configuration given according to PHP date/gmdate properties
 	 * Will return gmdate when the property GMT returns true
 	 *
-	 * @param	string		Input value undergoing processing in this function.
-	 * @param	array		stdWrap properties for date.
-	 * @return	string		The processed input value
+	 * @param string $content Input value undergoing processing in this function.
+	 * @param array $conf stdWrap properties for date.
+	 * @return string The processed input value
 	 */
 	public function stdWrap_date($content = '', $conf = array()) {
+			// check for zero length string to mimic default case of date/gmdate.
+		$content = $content == '' ? $GLOBALS['EXEC_TIME'] : intval($content);
 		$content = ($conf['date.']['GMT'] ? gmdate($conf['date'], $content) : date($conf['date'], $content));
 		return $content;
 	}
