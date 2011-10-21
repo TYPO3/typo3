e20b97d70731c28ab399c5e45d19b7014aeee3f2
Sebastiaan de Jonge <office@sebastiaandejonge.com>
2011-10-21 15:21:51 +0200

    [BUGFIX] Enable use of "0" as a token for TypoScript split function
    Optimizes the checking condition on the return value of the stdWrap function so the value "0" can be used as a valid token for TypoScript's split function.
    
    Change-Id: I9f5110ac9c0873a04fbc8845b126cfe477977f94
    Resolves: #31062
    Releases: 4.6, 4.5, 4.4

--- typo3/sysext/cms/tslib/class.tslib_content.php
+++ typo3/sysext/cms/tslib/class.tslib_content.php
@@ -4129,7 +4129,7 @@ class tslib_cObj {
 		$conf['token'] = isset($conf['token.'])
 			? $this->stdWrap($conf['token'], $conf['token.'])
 			: $conf['token'];
-		if (!$conf['token']) {
+		if ($conf['token'] === '') {
 			return $value;
 		}
 		$conf['max'] = isset($conf['max.'])
