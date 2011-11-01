1363f07f299ee2269fded636af9bbd624b722015
Xavier Perseguers <xavier@typo3.org>
2011-11-01 12:46:31 +0100

    [TASK] Update ADOdb library to version 5.14
    Change-Id: I13e1b3457ff1efc4cf9a6b1c1cbf9ff8e63c2737
    Resolves: #31486
    Releases: 4.5, 4.6, 4.7

--- typo3/sysext/adodb/adodb/adodb-xmlschema03.inc.php
+++ typo3/sysext/adodb/adodb/adodb-xmlschema03.inc.php
@@ -2031,7 +2031,7 @@ class adoSchema {
 					   . '<table>' . "\n";
 
 		foreach( $msg as $label => $details ) {
-			$error_details .= '<tr><td><strong>' . $label . ': </strong></td><td>' . htmlentities( $details ) . '</td></tr>' . "\n";
+			$error_details .= '<tr><td><b>' . $label . ': </b></td><td>' . htmlentities( $details ) . '</td></tr>' . "\n";
 		}
 
 		$error_details .= '</table>';
--- typo3/sysext/adodb/adodb/drivers/adodb-postgres7.inc.php
+++ typo3/sysext/adodb/adodb/drivers/adodb-postgres7.inc.php
@@ -1,6 +1,6 @@
 <?php
 /*
- V5.11 5 May 2010   (c) 2000-2010 John Lim (jlim#natsoft.com). All rights reserved.
+ V5.14 8 Sept 2011  (c) 2000-2011 John Lim (jlim#natsoft.com). All rights reserved.
   Released under both BSD license and Lesser GPL library license. 
   Whenever there is any discrepancy between the two licenses, 
   the BSD license will take precedence.
@@ -146,6 +146,8 @@ class ADODB_postgres7 extends ADODB_postgres64 {
 			// We don't have native support for parameterized queries, so let's emulate it at the parent
 			return ADODB_postgres64::_query($sql, $inputarr);
 		}
+		
+		$this->_pnum = 0;
 		$this->_errorMsg = false;
 		// -- added Cristiano da Cunha Duarte
 		if ($inputarr) {
