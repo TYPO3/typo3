ef3085916c42db79dc4f1f1974d1b1e58931465d
Stanislas Rolland <typo3@sjbr.ca>
2011-11-01 20:22:15 -0400

    [BUGFIX] JS error on initialization of RTE extension plugin
    Custom plugin from user RTE extension using reference to this.base() will not initialize correctly in TYPO3 4.6.
    
    Change-Id: I059a93f74ad189833ddd93d14654c851241a1a2a
    Resolves: #31498
    Releases: 4.6, 4.7

--- typo3/sysext/rtehtmlarea/htmlarea/htmlarea.js
+++ typo3/sysext/rtehtmlarea/htmlarea/htmlarea.js
@@ -4187,8 +4187,8 @@ HTMLArea.Plugin = Ext.extend(HTMLArea.Plugin, {
 	 * Note: this.base will exclusively refer to the HTMLArea.Plugin class constructor
 	 */
 	base: function (editor, pluginName) {
-		HTMLArea.appendToLog(this.editor.editorId, 'HTMLArea.' + this.name, 'base', 'Deprecated use of base function. Use Ext superclass reference instead.', 'warn');
-		HTMLArea.Plugin.prototype.constructor.call(this, this.editor, this.name);
+		HTMLArea.appendToLog(editor.editorId, 'HTMLArea.' + pluginName, 'base', 'Deprecated use of base function. Use Ext superclass reference instead.', 'warn');
+		HTMLArea.Plugin.prototype.constructor.call(this, editor, pluginName);
 	},
 	/**
 	 * Registers the plugin "About" information
