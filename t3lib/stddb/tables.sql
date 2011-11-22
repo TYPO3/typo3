417c45b20dbfe8ac99d5e7c5d6d6b6b44a91b494
Mario Rimann <typo3-coding@rimann.org>
2011-11-22 23:46:38 +0100

    [BUGFIX] Add INDEX on sys_log table
    Adds an index on the timestamp column of the sys_log table
    to improve performance in installations with huge log tables
    
    Change-Id: I1b8d27b6ce1352c03e70d67c3df54ec827e9e21a
    Resolves: #29279
    Branches: 4.7

--- t3lib/stddb/tables.sql
+++ t3lib/stddb/tables.sql
@@ -327,7 +327,8 @@ CREATE TABLE sys_log (
   NEWid varchar(20) DEFAULT '' NOT NULL,
   PRIMARY KEY (uid),
   KEY event (userid,event_pid),
-  KEY recuidIdx (recuid,uid)
+  KEY recuidIdx (recuid,uid),
+  INDEX tstamp (tstamp)
 ) ENGINE=InnoDB;
 
 #
