08d09a8b6cdd41abb040724f90e1b5bbfe3c72f7
Stefan Galinski <stefan.galinski@gmail.com>
2011-12-01 18:52:15 +0100

    [BUGFIX] Shortcut redirect ignores linkVars syntax
    Currently the code that creates the redirect url for a shortcut
    page only respects the linkVars option as list of parameters without
    additional validation informations like it's documented in the
    TSRef. Unfortunatly such paramaters are completely dropped and
    this leads to major problems on multilanguage sites with the new
    shortcut redirect feature.
    
    Change-Id: I3966eedf284b06b7765f38ca07ea6cd7155babb3
    Fixes: #32253
    Releases: 4.7, 4.6

--- typo3/sysext/cms/tslib/class.tslib_fe.php
+++ typo3/sysext/cms/tslib/class.tslib_fe.php
@@ -2659,6 +2659,50 @@
 	}
 
 	/**
+	 * Calculates and sets the internal linkVars based upon the current
+	 * $_GET parameters and the setting "config.linkVars".
+	 *
+	 * @return void
+	 */
+	public function calculateLinkVars() {
+		$this->linkVars = '';
+		$linkVars = t3lib_div::trimExplode(',', (string) $this->config['config']['linkVars']);
+		if (empty($linkVars)) {
+			return;
+		}
+
+		$getData = t3lib_div::_GET();
+		foreach ($linkVars as $key) {
+			$test = '';
+			if (preg_match('/^(.*)\((.+)\)$/', $key, $match)) {
+				$key = trim($match[1]);
+				$test = trim($match[2]);
+			}
+
+			if ($key === '' || !isset($getData[$key])) {
+				continue;
+			}
+
+			if (!is_array($getData[$key])) {
+				$temp = rawurlencode($getData[$key]);
+
+				if ($test !== '' && !TSpagegen::isAllowedLinkVarValue($temp, $test)) {
+					continue; // Error: This value was not allowed for this key
+				}
+
+				$value = '&' . $key . '=' . $temp;
+			} else {
+				if ($test !== '' && strcmp('array', $test)) {
+					continue; // Error: This key must not be an array!
+				}
+				$value = t3lib_div::implodeArrayForUrl($key, $getData[$key]);
+			}
+
+			$this->linkVars .= $value;
+		}
+	}
+
+	/**
 	 * Redirect to target page, if the current page is a Shortcut.
 	 *
 	 * If the current page is of type shortcut and accessed directly via its URL, this function redirects to the
@@ -2667,20 +2711,8 @@
 	 * @return void If page is not a Shortcut, redirects and exits otherwise
 	 */
 	public function checkPageForShortcutRedirect() {
-
 		if (!empty($this->originalShortcutPage) && $this->originalShortcutPage['doktype'] == t3lib_pageSelect::DOKTYPE_SHORTCUT) {
-			$linkVars = t3lib_div::trimExplode(',', (string) $this->config['config']['linkVars']);
-			if (!empty($linkVars)) {
-				$getData = t3lib_div::_GET();
-				foreach ($linkVars as $key) {
-					if (isset($getData[$key])) {
-						$value = rawurlencode($getData[$key]);
-						if (!empty($value)) {
-							$this->linkVars .= '&' . $key . '=' . $value;
-						}
-					}
-				}
-			}
+			$this->calculateLinkVars();
 
 				// instantiate tslib_content to generate the correct target URL
 			$cObj = t3lib_div::makeInstance('tslib_cObj');
