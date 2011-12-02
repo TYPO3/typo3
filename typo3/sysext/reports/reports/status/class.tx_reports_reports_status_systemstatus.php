f4d19dcf0b1420c6383a49fa2b6fbfb3ec9c0815
Mario Rimann <typo3-coding@rimann.org>
2011-12-02 22:10:25 +0100

    [BUGFIX] Unlimited memory limit handling in system status report
    According to the documentation, "-1" is a valid setting for PHP's
    memory_limit and means "unlimited". So far this value has caused
    the system status report to report this value as an error.
    
    Change-Id: I79e43d2e4300bce5d673296a844b9bd3416cd3da
    Resolves: #32231
    Releases: 4.7, 4.6, 4.5

--- typo3/sysext/reports/reports/status/class.tx_reports_reports_status_systemstatus.php
+++ typo3/sysext/reports/reports/status/class.tx_reports_reports_status_systemstatus.php
@@ -87,30 +87,36 @@ class tx_reports_reports_status_SystemStatus implements tx_reports_StatusProvide
 	 * @return	tx_reports_reports_status_Status	A status of whether a minimum memory limit requirment is met
 	 */
 	protected function getPhpMemoryLimitStatus() {
-		$memoryLimit = ini_get('memory_limit');
+		$memoryLimit = t3lib_div::getBytesFromSizeMeasurement(ini_get('memory_limit'));
 		$message     = '';
 		$severity    = tx_reports_reports_status_Status::OK;
 
-		if ($memoryLimit && t3lib_div::getBytesFromSizeMeasurement($memoryLimit) < t3lib_div::getBytesFromSizeMeasurement(TYPO3_REQUIREMENTS_RECOMMENDED_PHP_MEMORY_LIMIT)) {
-			$message = sprintf($GLOBALS['LANG']->getLL('status_phpMemoryRecommendation'), $memoryLimit, TYPO3_REQUIREMENTS_RECOMMENDED_PHP_MEMORY_LIMIT);
-			$severity = tx_reports_reports_status_Status::WARNING;
-		}
+		if ($memoryLimit > 0) {
+			if ($memoryLimit < t3lib_div::getBytesFromSizeMeasurement(TYPO3_REQUIREMENTS_RECOMMENDED_PHP_MEMORY_LIMIT)) {
+				$message = sprintf($GLOBALS['LANG']->getLL('status_phpMemoryRecommendation'), $memoryLimit, TYPO3_REQUIREMENTS_RECOMMENDED_PHP_MEMORY_LIMIT);
+				$severity = tx_reports_reports_status_Status::WARNING;
+			}
 
-		if ($memoryLimit && t3lib_div::getBytesFromSizeMeasurement($memoryLimit) < t3lib_div::getBytesFromSizeMeasurement(TYPO3_REQUIREMENTS_MINIMUM_PHP_MEMORY_LIMIT)) {
-			$message = sprintf($GLOBALS['LANG']->getLL('status_phpMemoryRequirement'), $memoryLimit, TYPO3_REQUIREMENTS_MINIMUM_PHP_MEMORY_LIMIT);
-			$severity = tx_reports_reports_status_Status::ERROR;
-		}
+			if ($memoryLimit < t3lib_div::getBytesFromSizeMeasurement(TYPO3_REQUIREMENTS_MINIMUM_PHP_MEMORY_LIMIT)) {
+				$message = sprintf($GLOBALS['LANG']->getLL('status_phpMemoryRequirement'), $memoryLimit, TYPO3_REQUIREMENTS_MINIMUM_PHP_MEMORY_LIMIT);
+				$severity = tx_reports_reports_status_Status::ERROR;
+			}
 
-		if ($severity > tx_reports_reports_status_Status::OK) {
-			if ($php_ini_path = get_cfg_var('cfg_file_path')) {
-				$message .= ' ' . sprintf($GLOBALS['LANG']->getLL('status_phpMemoryEditLimit'), $php_ini_path);
-			} else {
-				$message .= ' ' . $GLOBALS['LANG']->getLL('status_phpMemoryContactAdmin');
+			if ($severity > tx_reports_reports_status_Status::OK) {
+				if ($php_ini_path = get_cfg_var('cfg_file_path')) {
+					$message .= ' ' . sprintf($GLOBALS['LANG']->getLL('status_phpMemoryEditLimit'), $php_ini_path);
+				} else {
+					$message .= ' ' . $GLOBALS['LANG']->getLL('status_phpMemoryContactAdmin');
+				}
 			}
 		}
 
-		return t3lib_div::makeInstance('tx_reports_reports_status_Status',
-			$GLOBALS['LANG']->getLL('status_phpMemory'), $memoryLimit, $message, $severity
+		return t3lib_div::makeInstance(
+			'tx_reports_reports_status_Status',
+			$GLOBALS['LANG']->getLL('status_phpMemory'),
+			($memoryLimit > 0 ? $memoryLimit : $GLOBALS['LANG']->getLL('status_phpMemoryUnlimited')),
+			$message,
+			$severity
 		);
 	}
 
