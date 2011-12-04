34dcaa30faa02f3b5d07ad5b75b97b0a274bbecf
Mario Rimann <typo3-coding@rimann.org>
2011-12-04 22:53:52 +0100

    [BUGFIX] Install Tool shows internal MySQL databases
    This patch removes "mysql" and "information_schema" from the
    list of databases that the install tool offers to choose from.
    
    Change-Id: I4f407e5571e65005f24cba0b7cdf36e3782bf1c0
    Resolves: #19278
    Releases: 4.7, 4.6, 4.5

--- typo3/sysext/install/mod/class.tx_install.php
+++ typo3/sysext/install/mod/class.tx_install.php
@@ -3960,7 +3960,17 @@ REMOTE_ADDR was '".t3lib_div::getIndpEnv('REMOTE_ADDR')."' (".t3lib_div::getIndp
 		if ($result = $GLOBALS['TYPO3_DB']->sql_pconnect(TYPO3_db_host, TYPO3_db_username, TYPO3_db_password)) {
 			$dbArr = $GLOBALS['TYPO3_DB']->admin_get_dbs();
 		}
-		return $dbArr;
+
+			// remove some database names that MySQL uses internally from the list of choosable DB names
+		$forbiddenDatabaseNames = array('mysql', 'information_schema');
+		$databaseList = array();
+		foreach ($dbArr as $databaseName) {
+			if (!in_array($databaseName, $forbiddenDatabaseNames)) {
+				$databaseList[] = $databaseName;
+			}
+		}
+
+		return $databaseList;
 	}
 
 	/**
