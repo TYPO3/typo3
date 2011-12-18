50a3cf4503d0573bf17f5ffc1685e3df05cac8b2
Steffen Ritter <info@rs-websystems.de>
2011-12-18 16:54:12 +0100

    [BUGFIX] Backend loops in Page module
    Many modules call "refreshModuleMenu" when starting.
    In fact the "renderMenu" behaves different than before the
    transition to ExtJS 4 so that the backend got reloaded
    completely.
    Change it, to just refresh the menu as requested.
    
    Change-Id: Ibe5c3693a4a9a5184d3c5d2f33ce86ee6a574669
    Fixes: #32664
    Releases: 4.7

--- typo3/js/modulemenu.js
+++ typo3/js/modulemenu.js
@@ -41,7 +41,7 @@ Ext.define('TYPO3.model.ModuleMenu', {
 	idProperty: 'index',
 	fields: [{
 			name: 'index',
-			type: 'int',
+			type: 'int'
 		},{
 			name: 'key',
 			type: 'string'
@@ -391,7 +391,7 @@ TYPO3.ModuleMenu.App = {
 			TYPO3.Backend.NavigationContainer.add(component);
 		}
 
-		component.show()
+		component.show();
 
 			// backwards compatibility
 		top.nav = component;
@@ -437,7 +437,6 @@ TYPO3.ModuleMenu.App = {
 		TYPO3.ModuleMenu.Store.load({
 			scope: this,
 			callback: function(records, operation, success) {
-				this.renderMenu(records);
 				if (this.loadedModule) {
 					this.highlightModuleMenuItem(this.loadedModule);
 				}
@@ -474,7 +473,7 @@ Ext.onReady(function() {
 
 /*******************************************************************************
 *
-* Backwards compatability handling down here
+* Backwards compatibility handling down here
 *
 ******************************************************************************/
 
@@ -485,5 +484,5 @@ var currentlyHighLightedId = '';
 var currentlyHighLighted_restoreValue = '';
 var currentlyHighLightedMain = '';
 function highlightModuleMenuItem(trId, mainModule) {
-	TYPO3.ModuleMenu.App.highlightModule(trId, mainModule);
+	TYPO3.ModuleMenu.App.highlightModuleMenuItem(trId, mainModule);
 }
