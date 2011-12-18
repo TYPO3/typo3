7088f3c74dd33fa704cdfeba2f4e90a09163e69a
Oliver Hader <oliver@typo3.org>
2011-12-18 15:58:54 +0100

    [BUGFIX] Fatal error during install process
    In the install tool there is no backend user available,
    thus this needs to be checked before calling the writelog() method.
    
    Change-Id: I16494e0ba01147a5111101e9a76cf44418c1c5fb
    Fixes: #32666

--- typo3/sysext/lang/hooks/clearcache/class.tx_lang_clearcache.php
+++ typo3/sysext/lang/hooks/clearcache/class.tx_lang_clearcache.php
@@ -60,7 +60,9 @@ class tx_lang_clearcache {
 	 * @return void
 	 */
 	public function clearCache() {
-		$GLOBALS['BE_USER']->writelog(3, 1, 0, 0, '[lang]: User %s has cleared the language cache', array($GLOBALS['BE_USER']->user['username']));
+		if (isset($GLOBALS['BE_USER'])) {
+			$GLOBALS['BE_USER']->writelog(3, 1, 0, 0, '[lang]: User %s has cleared the language cache', array($GLOBALS['BE_USER']->user['username']));
+		}
 		$this->cacheInstance->flush();
 	}
 
