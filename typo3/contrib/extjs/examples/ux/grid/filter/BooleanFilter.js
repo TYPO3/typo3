b4f9754544d4a87f8a9155aacbd25164ecadf08b
Stanislas Rolland <typo3@sjbr.ca>
2011-12-19 22:06:47 +0100

    [TASK] Integrate missing Ext JS 4 examples
    Those Ext JS 4 examples provide the functionality that some
    of the modules in the TYPO3 Core rely on.
    
    Change-Id: I59db4f7e4581a3d76c71bcc2fb9b03e0990cbc94
    Resolves: #32710

--- typo3/contrib/extjs/examples/ux/grid/filter/BooleanFilter.js
+++ typo3/contrib/extjs/examples/ux/grid/filter/BooleanFilter.js
@@ -1,16 +1,24 @@
-/*!
- * Ext JS Library 3.1.1
- * Copyright(c) 2006-2010 Ext JS, LLC
- * licensing@extjs.com
- * http://www.extjs.com/license
- */
-/** 
+/*
+
+This file is part of Ext JS 4
+
+Copyright (c) 2011 Sencha Inc
+
+Contact:  http://www.sencha.com/contact
+
+GNU General Public License Usage
+This file may be used under the terms of the GNU General Public License version 3.0 as published by the Free Software Foundation and appearing in the file LICENSE included in the packaging of this file.  Please review the following information to ensure the GNU General Public License version 3.0 requirements will be met: http://www.gnu.org/copyleft/gpl.html.
+
+If you are unsure which license is appropriate for your use, please contact the sales department at http://www.sencha.com/contact.
+
+*/
+/**
  * @class Ext.ux.grid.filter.BooleanFilter
  * @extends Ext.ux.grid.filter.Filter
  * Boolean filters use unique radio group IDs (so you can have more than one!)
  * <p><b><u>Example Usage:</u></b></p>
- * <pre><code>    
-var filters = new Ext.ux.grid.GridFilters({
+ * <pre><code>
+var filters = Ext.create('Ext.ux.grid.GridFilters', {
     ...
     filters: [{
         // required configs
@@ -25,7 +33,10 @@ var filters = new Ext.ux.grid.GridFilters({
 });
  * </code></pre>
  */
-Ext.ux.grid.filter.BooleanFilter = Ext.extend(Ext.ux.grid.filter.Filter, {
+Ext.define('Ext.ux.grid.filter.BooleanFilter', {
+    extend: 'Ext.ux.grid.filter.Filter',
+    alias: 'gridfilter.boolean',
+
 	/**
 	 * @cfg {Boolean} defaultValue
 	 * Set this to null if you do not want either option to be checked by default. Defaults to false.
@@ -42,24 +53,24 @@ Ext.ux.grid.filter.BooleanFilter = Ext.extend(Ext.ux.grid.filter.Filter, {
 	 */
 	noText : 'No',
 
-    /**  
+    /**
      * @private
      * Template method that is to initialize the filter and install required menu items.
      */
     init : function (config) {
         var gId = Ext.id();
 		this.options = [
-			new Ext.menu.CheckItem({text: this.yesText, group: gId, checked: this.defaultValue === true}),
-			new Ext.menu.CheckItem({text: this.noText, group: gId, checked: this.defaultValue === false})];
-		
+			Ext.create('Ext.menu.CheckItem', {text: this.yesText, group: gId, checked: this.defaultValue === true}),
+			Ext.create('Ext.menu.CheckItem', {text: this.noText, group: gId, checked: this.defaultValue === false})];
+
 		this.menu.add(this.options[0], this.options[1]);
-		
+
 		for(var i=0; i<this.options.length; i++){
 			this.options[i].on('click', this.fireUpdate, this);
 			this.options[i].on('checkchange', this.fireUpdate, this);
 		}
 	},
-	
+
     /**
      * @private
      * Template method that is to get and return the value of the filter.
@@ -73,7 +84,7 @@ Ext.ux.grid.filter.BooleanFilter = Ext.extend(Ext.ux.grid.filter.Filter, {
      * @private
      * Template method that is to set the value of the filter.
      * @param {Object} value The value to set the filter
-     */	
+     */
 	setValue : function (value) {
 		this.options[value ? 0 : 1].setChecked(true);
 	},
@@ -89,7 +100,7 @@ Ext.ux.grid.filter.BooleanFilter = Ext.extend(Ext.ux.grid.filter.Filter, {
 		var args = {type: 'boolean', value: this.getValue()};
 		return args;
 	},
-	
+
     /**
      * Template method that is to validate the provided Ext.data.Record
      * against the filters configuration.
@@ -101,3 +112,4 @@ Ext.ux.grid.filter.BooleanFilter = Ext.extend(Ext.ux.grid.filter.Filter, {
 		return record.get(this.dataIndex) == this.getValue();
 	}
 });
+
--- typo3/contrib/extjs/examples/ux/grid/filter/DateFilter.js
+++ typo3/contrib/extjs/examples/ux/grid/filter/DateFilter.js
@@ -1,29 +1,37 @@
-/*!
- * Ext JS Library 3.1.1
- * Copyright(c) 2006-2010 Ext JS, LLC
- * licensing@extjs.com
- * http://www.extjs.com/license
- */
-/** 
+/*
+
+This file is part of Ext JS 4
+
+Copyright (c) 2011 Sencha Inc
+
+Contact:  http://www.sencha.com/contact
+
+GNU General Public License Usage
+This file may be used under the terms of the GNU General Public License version 3.0 as published by the Free Software Foundation and appearing in the file LICENSE included in the packaging of this file.  Please review the following information to ensure the GNU General Public License version 3.0 requirements will be met: http://www.gnu.org/copyleft/gpl.html.
+
+If you are unsure which license is appropriate for your use, please contact the sales department at http://www.sencha.com/contact.
+
+*/
+/**
  * @class Ext.ux.grid.filter.DateFilter
  * @extends Ext.ux.grid.filter.Filter
- * Filter by a configurable Ext.menu.DateMenu
+ * Filter by a configurable Ext.picker.DatePicker menu
  * <p><b><u>Example Usage:</u></b></p>
- * <pre><code>    
-var filters = new Ext.ux.grid.GridFilters({
+ * <pre><code>
+var filters = Ext.create('Ext.ux.grid.GridFilters', {
     ...
     filters: [{
         // required configs
         type: 'date',
         dataIndex: 'dateAdded',
-        
+
         // optional configs
         dateFormat: 'm/d/Y',  // default
         beforeText: 'Before', // default
         afterText: 'After',   // default
         onText: 'On',         // default
         pickerOpts: {
-            // any DateMenu configs
+            // any DatePicker configs
         },
 
         active: true // default is false
@@ -31,7 +39,11 @@ var filters = new Ext.ux.grid.GridFilters({
 });
  * </code></pre>
  */
-Ext.ux.grid.filter.DateFilter = Ext.extend(Ext.ux.grid.filter.Filter, {
+Ext.define('Ext.ux.grid.filter.DateFilter', {
+    extend: 'Ext.ux.grid.filter.Filter',
+    alias: 'gridfilter.date',
+    uses: ['Ext.picker.Date', 'Ext.menu.Menu'],
+
     /**
      * @cfg {String} afterText
      * Defaults to 'After'.
@@ -91,52 +103,55 @@ Ext.ux.grid.filter.DateFilter = Ext.extend(Ext.ux.grid.filter.Filter, {
      * Defaults to 'On'.
      */
     onText : 'On',
-    
+
     /**
      * @cfg {Object} pickerOpts
      * Configuration options for the date picker associated with each field.
      */
     pickerOpts : {},
 
-    /**  
+    /**
      * @private
      * Template method that is to initialize the filter and install required menu items.
      */
     init : function (config) {
-        var menuCfg, i, len, item, cfg, Cls;
+        var me = this,
+            pickerCfg, i, len, item, cfg;
 
-        menuCfg = Ext.apply(this.pickerOpts, {
-            minDate: this.minDate, 
-            maxDate: this.maxDate, 
-            format:  this.dateFormat,
+        pickerCfg = Ext.apply(me.pickerOpts, {
+            xtype: 'datepicker',
+            minDate: me.minDate,
+            maxDate: me.maxDate,
+            format:  me.dateFormat,
             listeners: {
-                scope: this,
-                select: this.onMenuSelect
+                scope: me,
+                select: me.onMenuSelect
             }
         });
 
-        this.fields = {};
-        for (i = 0, len = this.menuItems.length; i < len; i++) {
-            item = this.menuItems[i];
+        me.fields = {};
+        for (i = 0, len = me.menuItems.length; i < len; i++) {
+            item = me.menuItems[i];
             if (item !== '-') {
                 cfg = {
                     itemId: 'range-' + item,
-                    text: this[item + 'Text'],
-                    menu: new Ext.menu.DateMenu(
-                        Ext.apply(menuCfg, {
-                            itemId: item
-                        })
-                    ),
+                    text: me[item + 'Text'],
+                    menu: Ext.create('Ext.menu.Menu', {
+                        items: [
+                            Ext.apply(pickerCfg, {
+                                itemId: item
+                            })
+                        ]
+                    }),
                     listeners: {
-                        scope: this,
-                        checkchange: this.onCheckChange
+                        scope: me,
+                        checkchange: me.onCheckChange
                     }
                 };
-                Cls = Ext.menu.CheckItem;
-                item = this.fields[item] = new Cls(cfg);
+                item = me.fields[item] = Ext.create('Ext.menu.CheckItem', cfg);
             }
-            //this.add(item);
-            this.menu.add(item);
+            //me.add(item);
+            me.menu.add(item);
         }
     },
 
@@ -145,7 +160,7 @@ Ext.ux.grid.filter.DateFilter = Ext.extend(Ext.ux.grid.filter.Filter, {
         this.fireEvent('update', this);
     },
 
-    /**  
+    /**
      * @private
      * Handler method called when there is a keyup event on an input
      * item of this menu.
@@ -154,36 +169,36 @@ Ext.ux.grid.filter.DateFilter = Ext.extend(Ext.ux.grid.filter.Filter, {
         var k = e.getKey();
         if (k == e.RETURN && field.isValid()) {
             e.stopEvent();
-            this.menu.hide(true);
-            return;
+            this.menu.hide();
         }
     },
 
     /**
-     * Handler for when the menu for a field fires the 'select' event
-     * @param {Object} date
-     * @param {Object} menuItem
-     * @param {Object} value
+     * Handler for when the DatePicker for a field fires the 'select' event
+     * @param {Ext.picker.Date} picker
      * @param {Object} picker
+     * @param {Object} date
      */
-    onMenuSelect : function (menuItem, value, picker) {
+    onMenuSelect : function (picker, date) {
         var fields = this.fields,
-            field = this.fields[menuItem.itemId];
-        
+            field = this.fields[picker.itemId];
+
         field.setChecked(true);
-        
+
         if (field == fields.on) {
             fields.before.setChecked(false, true);
             fields.after.setChecked(false, true);
         } else {
             fields.on.setChecked(false, true);
-            if (field == fields.after && fields.before.menu.picker.value < value) {
+            if (field == fields.after && this.getFieldValue('before') < date) {
                 fields.before.setChecked(false, true);
-            } else if (field == fields.before && fields.after.menu.picker.value > value) {
+            } else if (field == fields.before && this.getFieldValue('after') > date) {
                 fields.after.setChecked(false, true);
             }
         }
         this.fireEvent('update', this);
+
+        picker.up('menu').hide();
     },
 
     /**
@@ -195,7 +210,7 @@ Ext.ux.grid.filter.DateFilter = Ext.extend(Ext.ux.grid.filter.Filter, {
         var key, result = {};
         for (key in this.fields) {
             if (this.fields[key].checked) {
-                result[key] = this.fields[key].menu.picker.getValue();
+                result[key] = this.getFieldValue(key);
             }
         }
         return result;
@@ -208,12 +223,12 @@ Ext.ux.grid.filter.DateFilter = Ext.extend(Ext.ux.grid.filter.Filter, {
      * @param {Boolean} preserve true to preserve the checked status
      * of the other fields.  Defaults to false, unchecking the
      * other fields
-     */	
+     */
     setValue : function (value, preserve) {
         var key;
         for (key in this.fields) {
             if(value[key]){
-                this.fields[key].menu.picker.setValue(value[key]);
+                this.getPicker(key).setValue(value[key]);
                 this.fields[key].setChecked(true);
             } else if (!preserve) {
                 this.fields[key].setChecked(false);
@@ -252,7 +267,7 @@ Ext.ux.grid.filter.DateFilter = Ext.extend(Ext.ux.grid.filter.Filter, {
                 args.push({
                     type: 'date',
                     comparison: this.compareMap[key],
-                    value: this.getFieldValue(key).format(this.dateFormat)
+                    value: Ext.Date.format(this.getFieldValue(key), this.dateFormat)
                 });
             }
         }
@@ -265,16 +280,16 @@ Ext.ux.grid.filter.DateFilter = Ext.extend(Ext.ux.grid.filter.Filter, {
      * @return {Date} Gets the current selected value of the date field
      */
     getFieldValue : function(item){
-        return this.fields[item].menu.picker.getValue();
+        return this.getPicker(item).getValue();
     },
-    
+
     /**
      * Gets the menu picker associated with the passed field
      * @param {String} item The field identifier ('before', 'after', 'on')
      * @return {Object} The menu picker
      */
     getPicker : function(item){
-        return this.fields[item].menu.picker;
+        return this.fields[item].menu.items.first();
     },
 
     /**
@@ -287,16 +302,17 @@ Ext.ux.grid.filter.DateFilter = Ext.extend(Ext.ux.grid.filter.Filter, {
     validateRecord : function (record) {
         var key,
             pickerValue,
-            val = record.get(this.dataIndex);
-            
+            val = record.get(this.dataIndex),
+            clearTime = Ext.Date.clearTime;
+
         if(!Ext.isDate(val)){
             return false;
         }
-        val = val.clearTime(true).getTime();
-        
+        val = clearTime(val, true).getTime();
+
         for (key in this.fields) {
             if (this.fields[key].checked) {
-                pickerValue = this.getFieldValue(key).clearTime(true).getTime();
+                pickerValue = clearTime(this.getFieldValue(key), true).getTime();
                 if (key == 'before' && pickerValue <= val) {
                     return false;
                 }
@@ -311,3 +327,4 @@ Ext.ux.grid.filter.DateFilter = Ext.extend(Ext.ux.grid.filter.Filter, {
         return true;
     }
 });
+
