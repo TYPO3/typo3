d288539c3c76ed9617d54da407e63a00bb02913a
Kai Vogel <kai.vogel@speedprogs.de>
2012-01-09 21:29:48 +0100

    [BUGFIX] TSConfig for fields with a dot in the name does not work
    This patch enables the use of double quotes in TypoScript setup paths.
    Thus, for example, can Extbase field names be separated using quotes in
    TSConfig.
    
    Example:
    
    TCEFORM.tt_content.pi_flexform.myext_myplugin.sDEF {
      "settings.fieldOne".config.type = text
      "settings.fieldTwo" = foo
    }
    
    Change-Id: Idf30faff9c45c662b2c2f976aeda5cc407e9610a
    Fixes: #29461
    Releases: 6.0

---
+++ typo3/sysext/core/Tests/Unit/TypoScript/Parser/TypoScriptParserTest.php
@@ -0,0 +1,93 @@
+<?php
+namespace TYPO3\CMS\Core\Tests\Unit\TypoScript\Parser;
+
+/***********************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2012 Kai Vogel <kai.vogel@speedprogs.de>, Speedprogs.de
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ **********************************************************************/
+
+/**
+ * Testcase for \TYPO3\CMS\Core\TypoScript\Parser\TypoScriptParser
+ *
+ * @author Kai Vogel <kai.vogel@speedprogs.de>
+ */
+class TypoScriptParserTest extends \TYPO3\CMS\Core\Tests\UnitTestCase {
+
+	/**
+	 * @var \TYPO3\CMS\Core\TypoScript\Parser\TypoScriptParser|\PHPUnit_Framework_MockObject_MockObject|\TYPO3\CMS\Core\Tests\AccessibleObjectInterface
+	 */
+	protected $fixture;
+
+	/**
+	 * Set up
+	 */
+	public function setUp() {
+		$this->fixture = $this->getAccessibleMock('TYPO3\\CMS\\Core\\TypoScript\\Parser\\TypoScriptParser', array('dummy'), array(), '', FALSE);
+	}
+
+	/**
+	 * Data provider for setValHandlesDoubleQoutesInTypoScriptSetupKeys
+	 */
+	public function setValHandlesDoubleQoutesInTypoScriptSetupKeysDataProvider() {
+		return array(
+			'find qoutes in the middle of keys' => array(
+				'foo.bar."settings.key".value',
+				array('foobar'),
+				array(
+					'foo.' => array(
+						'bar.' => array(
+							'settings.key.' => array(
+								'value' => 'foobar',
+							),
+						),
+					),
+				),
+			),
+			'find quotes at the end of keys' => array(
+				'foo.bar."settings.key"',
+				array('foobar'),
+				array(
+					'foo.' => array(
+						'bar.' => array(
+							'settings.key' => 'foobar',
+						),
+					),
+				),
+			),
+		);
+	}
+
+	/**
+	 * @param string $keys The keys of the TypoScript option
+	 * @param array $value The value of the TypoScript option
+	 * @param array $expected The expected result of the test
+	 * @test
+	 * @dataProvider setValHandlesDoubleQoutesInTypoScriptSetupKeysDataProvider
+	 */
+	public function setValHandlesDoubleQoutesInTypoScriptSetupKeys($keys, array $value, array $expected) {
+		$setup = array();
+		$this->fixture->setVal($keys, $setup, $value);
+		$this->assertEquals($setup, $expected);
+	}
+
+}
+?>
