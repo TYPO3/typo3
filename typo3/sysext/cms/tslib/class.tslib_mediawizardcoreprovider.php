ae3852789fbb244df0a5771b321f717b5cbac02f
Robert Heel <rheel@1drop.de>
2012-01-17 14:57:35 +0100

    [BUGFIX] Media element does not recognize short YouTube links
    Change-Id: Id4d67ee57f72a9475abaaa7762cbc42572d38e51
    Fixes: #33089
    Releases: 4.8, 4.7, 4.6, 4.5

--- typo3/sysext/cms/tslib/class.tslib_mediawizardcoreprovider.php
+++ typo3/sysext/cms/tslib/class.tslib_mediawizardcoreprovider.php
@@ -41,6 +41,7 @@ class tslib_mediaWizardCoreProvider implements tslib_mediaWizardProvider {
 	 */
 	protected $providers = array(
 		'youtube',
+		'youtu',
 		'dailymotion',
 		'sevenload',
 		'vimeo',
@@ -119,14 +120,24 @@ class tslib_mediaWizardCoreProvider implements tslib_mediaWizardProvider {
 				// it's a channel
 			$parts = explode('/', $url);
 			$videoId = $parts[count($parts) - 1];
-		} elseif (preg_match('/v=([^(\&|$)]*)/', $url, $matches)) {
-			$videoId = $matches[1];
+		} elseif (preg_match('/(v=|v\/|.be\/)([^(\&|$)]*)/', $url, $matches)) {
+			$videoId = $matches[2];
 		}
 
 		if ($videoId) {
 			$url = 'http://www.youtube.com/v/' . $videoId . '?fs=1';
 		}
 		return $url;
+	}
+
+	/**
+	 * Parse youtube short url
+	 *
+	 * @param string $url
+	 * @return string processed url
+	 */
+	protected function process_youtu($url) {
+	    return $this->process_youtube($url);
 	}
 
 	/**
