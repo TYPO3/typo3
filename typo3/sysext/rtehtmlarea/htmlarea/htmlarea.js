cdd5430fcbc0887a18fdfc9c23401e1f55d7e93b
Stanislas Rolland <typo3@sjbr.ca>
2012-01-17 18:24:58 -0500

    [BUGFIX] HTML entity &nbsp; is inserted before and after inline element
    Problem: This is a regression caused by patch to issue #16217.
    The last two lines of the merged patch are wrong. They should replace
    only leading and trailing spaces inside block elements.
    Solution: Revert the last two replacement lines of the patch.
    
    Change-Id: I94008b281bbe13189948862eda626264d0895ab1
    Resolves: #32263
    Releases: 4.5, 4.6, 4.7

--- typo3/sysext/rtehtmlarea/htmlarea/htmlarea.js
+++ typo3/sysext/rtehtmlarea/htmlarea/htmlarea.js
@@ -2237,9 +2237,7 @@ HTMLArea.Editor = Ext.extend(Ext.util.Observable, {
 				return this.textArea.getValue().
 					replace(/[\x20]+/g, '\x20').
 					replace(/^\x20/g, '&nbsp;').
-					replace(/\x20$/g, '&nbsp;').
-					replace(/>\x20/g, '>&nbsp;').
-					replace(/\x20</g, '&nbsp;<');
+					replace(/\x20$/g, '&nbsp;');
 			default:
 				return '';
 		}
