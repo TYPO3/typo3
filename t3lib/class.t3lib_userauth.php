fa2adb14427efa8bb2c05fc96c1787f1142d3b1f
Tolleiv Nietsch <info@tolleiv.de>
2012-02-04 09:08:34 +0100

    [BUGFIX] Release locked records during logout
    The sys_lockedrecords tables isn't cleared during the users logout.
    
    Change-Id: I08fa2820b048a420d3e1a39fdb0cddf42ff98776
    Fixes: #20211
    Releases: 4.7, 4.6, 4.5
    Reviewed-on: http://review.typo3.org/8835
    Reviewed-by: Markus Klein
    Reviewed-by: Philipp Gampe
    Tested-by: Philipp Gampe
    Tested-by: Markus Klein
    Reviewed-by: Georg Ringer
    Tested-by: Georg Ringer
    Reviewed-by: Tolleiv Nietsch
    Tested-by: Tolleiv Nietsch

--- t3lib/class.t3lib_userauth.php
+++ t3lib/class.t3lib_userauth.php
@@ -816,6 +816,9 @@ abstract class t3lib_userAuth {
 			t3lib_div::devLog('logoff: ses_id = ' . $this->id, 't3lib_userAuth');
 		}
 
+			// Release the locked records
+		t3lib_BEfunc::lockRecords();
+
 			// Hook for pre-processing the logoff() method, requested and implemented by andreas.otto@dkd.de:
 		if (is_array($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_userauth.php']['logoff_pre_processing'])) {
 			$_params = array();
