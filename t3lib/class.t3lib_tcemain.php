0251f44e02f3f1fd2a61e9a93ba7d0f23ce41eec
Tolleiv Nietsch <info@tolleiv.de>
2012-02-04 09:53:31 +0100

    [BUGFIX] Properly check disabled versioning within tcemain
    Setting $GLOBALS['TCA'][$table]['ctrl']['versioningWS'] = FALSE; isn't
    fully working within tcemain because some isset() checks don't
    look at the value atm.
    
    Change-Id: I5cfca5fa73a7f30b56342052ef0193f21999bce1
    Fixes: #33625
    Releases: 4.7, 4.6, 4.5

--- t3lib/class.t3lib_tcemain.php
+++ t3lib/class.t3lib_tcemain.php
@@ -3277,7 +3277,7 @@ class t3lib_TCEmain {
 		}
 
 		$where = '';
-		if (isset($GLOBALS['TCA'][$table]['ctrl']['versioningWS'])) {
+		if (isset($GLOBALS['TCA'][$table]['ctrl']['versioningWS']) && $GLOBALS['TCA'][$table]['ctrl']['versioningWS']) {
 			$where = ' AND t3ver_oid=0';
 		}
 
@@ -3623,7 +3623,7 @@ class t3lib_TCEmain {
 		}
 
 		$where = '';
-		if (isset($GLOBALS['TCA'][$table]['ctrl']['versioningWS'])) {
+		if (isset($GLOBALS['TCA'][$table]['ctrl']['versioningWS']) && $GLOBALS['TCA'][$table]['ctrl']['versioningWS']) {
 			$where = ' AND t3ver_oid=0';
 		}
 		$l10nRecords = t3lib_BEfunc::getRecordsByField($table, $GLOBALS['TCA'][$table]['ctrl']['transOrigPointerField'], $uid, $where);
@@ -4367,7 +4367,7 @@ class t3lib_TCEmain {
 		}
 
 		$where = '';
-		if (isset($GLOBALS['TCA'][$table]['ctrl']['versioningWS'])) {
+		if (isset($GLOBALS['TCA'][$table]['ctrl']['versioningWS']) && $GLOBALS['TCA'][$table]['ctrl']['versioningWS']) {
 			$where = ' AND t3ver_oid=0';
 		}
 
