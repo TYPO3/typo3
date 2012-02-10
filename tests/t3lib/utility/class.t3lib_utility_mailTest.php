dbbeed28bd0796479e2b4013b3ae78daf56756c9
Stefan Neufeind <typo3.neufeind@speedpartner.de>
2012-02-10 19:21:29 +0100

    [TASK] Adapt unit-tests to new mail-adapter-hook  (for t3lib_utility_mail)
    Adjust the unit-tests to the updated interface
    instead of the deprecated one. The deprecated
    interface was removed with 4.7
    (commit 97a6f07f8fb990302ce3b05865aedcc77f220917).
    
    Change-Id: I76f6bb24a58ab7f4d487e596e2abedb1da9767da
    Resolves: #33850
    Releases: 4.7

--- tests/t3lib/utility/class.t3lib_utility_mailTest.php
+++ tests/t3lib/utility/class.t3lib_utility_mailTest.php
@@ -89,30 +89,29 @@ class t3lib_utility_MailTest extends tx_phpunit_testcase {
 		$messageBody = 'The hooks works!';
 		$additionalHeaders = 'Reply-to: jane@example.com';
 		$additionalParameters = '-f postmaster@example.com';
+		$fakeThis = FALSE;
 
 		$additionalHeadersExpected = $additionalHeaders;
 		if ($fromAddress !== '' && $fromName !== '') {
 			$additionalHeadersExpected .= LF . sprintf('From: "%s" <%s>', $fromName, $fromAddress);
 		}
 
-		$mockMailer = $this->getMock('mockMailer', array('mail'));
+		$mockMailer = $this->getMock('t3lib_mail_MailerAdapter', array('mail'));
+		$mockClassName = get_class($mockMailer);
+		t3lib_div::addInstance($mockClassName, $mockMailer);
 		$mockMailer->expects($this->once())->method('mail')
 			->with(
-				array(
-					'to' => $to,
-					'subject' => $subject,
-					'messageBody' => $messageBody,
-					'additionalHeaders' => $additionalHeadersExpected,
-					'additionalParameters' => $additionalParameters,
-				),
-				FALSE
+				$to,
+				$subject,
+				$messageBody,
+				$additionalHeadersExpected,
+				$additionalParameters,
+				$fakeThis
 			);
-		$GLOBALS['T3_VAR']['callUserFunction']['mockMailer->mail']
-			= array('obj' => $mockMailer, 'method' => 'mail');
 
 		$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']
 			['t3lib/utility/class.t3lib_utility_mail.php']
-			['substituteMailDelivery'] = array('mockMailer->mail');
+			['substituteMailDelivery'] = array($mockClassName);
 
 		t3lib_utility_Mail::mail(
 			$to, $subject, $messageBody, $additionalHeaders,
