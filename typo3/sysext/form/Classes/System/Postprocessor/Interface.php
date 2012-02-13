8f3592143d4b8e7cc87fa81ae9057bfb630f3b5c
Franz Geiger <mail@fx-g.de>
2012-02-13 16:19:37 +0100

    [FEATURE] Create interface and enable custom form_postProcessors
    PostProcessors now need to implement the
    tx_form_System_Postprocessor_Interface.
    All classes implementing this interface can be loaded as a
    postProcessor by passing its class-name by typoscript.
    
    Before loading the class in the namespace 'tx_form_System_Postprocessor_'
    tx_form_System_Postprocessor first tries to load the class passed by
    typoscript directly.
    
    Change-Id: I3bea8ce8287a6713f483d7e019cb5f47cdc68cb8
    Resolves: #32701
    Releases: 6.0

---
+++ typo3/sysext/form/Classes/System/Postprocessor/Interface.php
@@ -0,0 +1,49 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2012 Franz Geiger (mail@fx-g.de)
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * Interface for postprocessors
+ *
+ * @author Franz Geiger <mail@fx-g.de>
+ * @package TYPO3
+ * @subpackage form
+ */
+interface tx_form_System_Postprocessor_Interface {
+
+	/**
+	 * Constructor
+	 *
+	 * @param tx_form_Domain_Model_Form $form Form domain model
+	 * @param array $typoScript Post processor TypoScript settings
+	 */
+	public function __construct(tx_form_Domain_Model_Form $form, array $typoScript);
+
+	/**
+	 * The main method called by the post processor
+	 *
+	 * @return string The post processing HTML
+	 */
+	public function process();
+}
+?>
