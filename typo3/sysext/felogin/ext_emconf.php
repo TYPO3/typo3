0732f92555dffd1d8398f1bd857f847aafd24057
Jigal van Hemert <jigal@xs4all.nl>
2012-02-16 22:42:24 +0100

    [TASK]Update felogin manual and raise version
    Update manual with new options, new screenshots, new wraps, labels, etc. for
    TYPO3 4.7. Raised extension version and dependency.
    
    Change-Id: I070e6cc826d9d9e26f46317c53a70a5359520929
    Resolves: #33927
    Releases: 4.7
    Related: #24185, #29577, #23199

--- typo3/sysext/felogin/ext_emconf.php
+++ typo3/sysext/felogin/ext_emconf.php
@@ -33,7 +33,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.3.0-0.0.0',
-			'typo3' => '4.6.0-0.0.0',
+			'typo3' => '4.7.0-0.0.0',
 		),
 		'conflicts' => array(
 		),
