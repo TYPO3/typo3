58ce3e378dc360dfddcf6a4e10fee001edcedebc
Christian Kuhn <lolli@schwarzbu.ch>
2012-02-19 00:16:30 +0100

    [TASK] Migrate aboutmodules to extbase
    Use extbase and fluid for aboutmodule.
    Provide an install tool update wizard to load extbase and
    fluid if an extension that depends on them is installed.
    
    Change-Id: I8164c0c8afdc359a9b3dc059e14baa13915944b5
    Resolves: #34086
    Branches: 4.7

--- typo3/sysext/aboutmodules/ext_tables.php
+++ typo3/sysext/aboutmodules/ext_tables.php
@@ -1,5 +1,24 @@
 <?php
-if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
+if (!defined ('TYPO3_MODE')) {
+	die ('Access denied.');
+}
 
-if (TYPO3_MODE=='BE')	t3lib_extMgm::addModule('help','aboutmodules','after:about',t3lib_extMgm::extPath($_EXTKEY).'mod/');
+	// Avoid that this block is loaded in frontend or within upgrade wizards
+if (TYPO3_MODE === 'BE' && !(TYPO3_REQUESTTYPE & TYPO3_REQUESTTYPE_INSTALL)) {
+	Tx_Extbase_Utility_Extension::registerModule(
+		$_EXTKEY,
+		'help',
+		'aboutmodules',
+		'after:about',
+		array(
+			'Modules' => 'index',
+		),
+		array(
+			'access' => 'user,group',
+			'icon' => 'EXT:aboutmodules/ext_icon.gif',
+			'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_mod.xlf',
+		)
+	);
+}
+
 ?>
