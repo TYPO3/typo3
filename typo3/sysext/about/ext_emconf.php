fe9f35b71de0ac34ecc9ecfb753a65eae3801b7b
Christian Kuhn <lolli@schwarzbu.ch>
2012-02-19 14:56:48 +0100

    [TASK] Migrate EXT:about to Extbase
    Change-Id: If4161ce96038778776bcc654ad8a09b550568d44
    Resolves: #34096
    Releases: 4.7

--- typo3/sysext/about/ext_emconf.php
+++ typo3/sysext/about/ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "about".
 #
-# Auto generated 25-10-2011 13:10
+# Auto generated 23-02-2012 00:24
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -33,8 +33,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_company' => 'Curby Soft Multimedia',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
-	'version' => '1.1.0',
-	'_md5_values_when_last_written' => 'a:8:{s:16:"ext_autoload.php";s:4:"5a89";s:12:"ext_icon.gif";s:4:"f3ab";s:14:"ext_tables.php";s:4:"57f3";s:48:"interfaces/interface.tx_about_customsections.php";s:4:"3f4a";s:13:"mod/clear.gif";s:4:"cc11";s:12:"mod/conf.php";s:4:"e02a";s:13:"mod/index.php";s:4:"16b7";s:12:"mod/info.gif";s:4:"2723";}',
+	'version' => '2.0.0',
+	'_md5_values_when_last_written' => 'a:18:{s:16:"ext_autoload.php";s:4:"f7a4";s:12:"ext_icon.gif";s:4:"f3ab";s:14:"ext_tables.php";s:4:"002c";s:38:"Classes/Controller/AboutController.php";s:4:"2234";s:34:"Classes/Domain/Model/Extension.php";s:4:"4128";s:49:"Classes/Domain/Repository/ExtensionRepository.php";s:4:"4961";s:43:"Classes/ViewHelpers/SkinImageViewHelper.php";s:4:"e3d5";s:38:"Resources/Private/Layouts/Default.html";s:4:"440b";s:37:"Resources/Private/Partials/About.html";s:4:"b7ff";s:40:"Resources/Private/Partials/CoreTeam.html";s:4:"f320";s:39:"Resources/Private/Partials/Credits.html";s:4:"14f9";s:45:"Resources/Private/Partials/CustomContent.html";s:4:"64c0";s:40:"Resources/Private/Partials/Donation.html";s:4:"2702";s:42:"Resources/Private/Partials/Extensions.html";s:4:"2d49";s:49:"Resources/Private/Partials/ExternalLibraries.html";s:4:"0799";s:36:"Resources/Private/Partials/Logo.html";s:4:"0666";s:44:"Resources/Private/Templates/About/Index.html";s:4:"b9c8";s:48:"interfaces/interface.tx_about_customsections.php";s:4:"50aa";}',
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.3.0-0.0.0',
