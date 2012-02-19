51daf1d7c57db95513163a60b81a4e20073a251a
Christian Kuhn <lolli@schwarzbu.ch>
2012-02-19 14:56:48 +0100

    [TASK] Migrate about to extbase
    Change-Id: If4161ce96038778776bcc654ad8a09b550568d44
    Resolves: #34096
    Releases: 4.7

--- typo3/sysext/about/ext_tables.php
+++ typo3/sysext/about/ext_tables.php
@@ -1,7 +1,23 @@
 <?php
-if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
+if (!defined ('TYPO3_MODE')) {
+	die ('Access denied.');
+}
 
-if (TYPO3_MODE=='BE') {
-	t3lib_extMgm::addModule('help','about','top',t3lib_extMgm::extPath($_EXTKEY).'mod/');
+	// Avoid that this block is loaded in frontend or within upgrade wizards
+if (TYPO3_MODE === 'BE' && !(TYPO3_REQUESTTYPE & TYPO3_REQUESTTYPE_INSTALL)) {
+	Tx_Extbase_Utility_Extension::registerModule(
+		$_EXTKEY,
+		'help',
+		'about',
+		'top',
+		array(
+			'About' => 'index',
+		),
+		array(
+			'access' => 'user,group',
+			'icon' => 'EXT:about/ext_icon.gif',
+			'labels' => 'LLL:EXT:lang/locallang_mod_help_about.xlf',
+		)
+	);
 }
 ?>
