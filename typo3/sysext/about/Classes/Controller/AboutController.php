7cb35d854062059620577117dbb2979dd0a04cfb
Christian Kuhn <lolli@schwarzbu.ch>
2012-02-19 14:56:48 +0100

    [TASK] Migrate EXT:about to extbase
    Change-Id: If4161ce96038778776bcc654ad8a09b550568d44
    Resolves: #34096
    Releases: 4.7
    Reviewed-on: http://review.typo3.org/9104
    Reviewed-by: Philipp Gampe
    Tested-by: Philipp Gampe
    Reviewed-by: Steffen Ritter
    Tested-by: Steffen Ritter
    Reviewed-by: Tolleiv Nietsch
    Tested-by: Tolleiv Nietsch

---
+++ typo3/sysext/about/Classes/Controller/AboutController.php
@@ -0,0 +1,95 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2012 Christian Kuhn <lolli@schwarzbu.ch>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *  A copy is found in the textfile GPL.txt and important notices to the license
+ *  from the author is found in LICENSE.txt distributed with these scripts.
+ *
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * Module 'about' shows some standard information for TYPO3 CMS: About-text, version number and so on.
+ *
+ * @author Kasper Skårhøj <kasperYYYY@typo3.com>
+ * @author Steffen Kamper <steffen@typo3.org>
+ * @author Christian Kuhn <lolli@schwarzbu.ch>
+ * @package TYPO3
+ * @subpackage about
+ */
+class Tx_About_Controller_AboutController extends Tx_Extbase_MVC_Controller_ActionController {
+	/**
+	 * @var Tx_About_Domain_Repository_ExtensionRepository
+	 */
+	protected $extensionRepository;
+
+	/**
+	 * @param Tx_About_Domain_Repository_ExtensionRepository
+	 * @return void
+	 */
+	public function injectExtensionRepository(Tx_About_Domain_Repository_ExtensionRepository $extensionRepository) {
+		$this->extensionRepository = $extensionRepository;
+	}
+
+	/**
+	 * Main action: Show standard information
+	 *
+	 * @return void
+	 */
+	public function indexAction() {
+		$extensions = $this->extensionRepository->findAllLoaded();
+		$this->view
+			->assign('TYPO3Version', TYPO3_version)
+			->assign('TYPO3CopyrightYear', TYPO3_copyright_year)
+			->assign('TYPO3UrlDonate', TYPO3_URL_DONATE)
+			->assign('loadedExtensions', $extensions)
+			->assign('customContents', $this->getCustomContent())
+		;
+	}
+
+	/**
+	 * Hook to add custom content
+	 *
+	 * @return array with additional content sections
+	 * @deprecated Since 4.7; will be removed together with the call in indexAction and the fluid partial in 4.9
+	 */
+	protected function getCustomContent() {
+		$sections = array();
+		if (is_array($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['about/index.php']['addSection'])) {
+			t3lib_div::deprecationLog(
+				'Hook about/index.php addSection is deprecated and will be removed in TYPO3 4.9, use fluid overrides instead.'
+			);
+
+			foreach ($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['about/index.php']['addSection'] as $classRef) {
+					/** @var $hookObject tx_about_customSections */
+				$hookObject = t3lib_div::getUserObj($classRef);
+				if (!($hookObject instanceof tx_about_customSections)) {
+					throw new UnexpectedValueException(
+						'$hookObject must implement interface tx_about_customSections',
+						1298121573
+					);
+				}
+				$hookObject->addSection($sections);
+			}
+		}
+		return $sections;
+	}
+}
+?>
---
+++ typo3/sysext/about/Classes/ViewHelpers/SkinImageViewHelper.php
@@ -0,0 +1,57 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2012 Christian Kuhn <lolli@schwarzbu.ch>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *  A copy is found in the textfile GPL.txt and important notices to the license
+ *  from the author is found in LICENSE.txt distributed with these scripts.
+ *
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * Render images that can be registered in the TYPO3 backend skinning API
+ * Extends the default image view helper from fluid
+ *
+ * This view helper is a helper for ext:about only and should
+ * not be used outside of the extension for now.
+ * See the fluid templates of ext:about for a usage example.
+ */
+class Tx_About_ViewHelpers_SkinImageViewHelper extends Tx_Fluid_ViewHelpers_ImageViewHelper {
+
+	/**
+	 * Resizes a given image (if required) and renders the respective img tag
+	 * @see http://typo3.org/documentation/document-library/references/doc_core_tsref/4.2.0/view/1/5/#id4164427
+	 *
+	 * @param string $src
+	 * @param string $width width of the image. This can be a numeric value representing the fixed width of the image in pixels. But you can also perform simple calculations by adding "m" or "c" to the value. See imgResource.width for possible options.
+	 * @param string $height height of the image. This can be a numeric value representing the fixed height of the image in pixels. But you can also perform simple calculations by adding "m" or "c" to the value. See imgResource.width for possible options.
+	 * @param integer $minWidth minimum width of the image
+	 * @param integer $minHeight minimum height of the image
+	 * @param integer $maxWidth maximum width of the image
+	 * @param integer $maxHeight maximum height of the image
+	 *
+	 * @return string rendered tag.
+	 */
+	public function render($src, $width = NULL, $height = NULL, $minWidth = NULL, $minHeight = NULL, $maxWidth = NULL, $maxHeight = NULL) {
+		$src = t3lib_iconWorks::skinImg('../typo3/', $src, '', 1);
+		return parent::render(''. $src, $width, $height. $minWidth, $minHeight, $maxWidth, $maxHeight);
+	}
+}
+?>
