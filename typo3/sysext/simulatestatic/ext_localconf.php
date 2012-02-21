53c008615b9ff8777831818a4fb9b9645d11e245
Markus Klein <klein.t3@mfc-linz.at>
2012-02-21 11:44:29 +0100

    [CLEANUP] Remove remaining relicts of simulate static
    tslib_fe still contains idPartsAnalyze() which is a relict of
    simulate static methods.
    
    To maintain the backward compatibility, a new hook is introduced,
    which allows general modification of tslib_fe in an early stage.
    
    Change-Id: I3631909fe6a77fd0861e7be5acdb6f3bf82fdb42
    Resolves: #25099
    Releases: 4.7

--- typo3/sysext/simulatestatic/ext_localconf.php
+++ typo3/sysext/simulatestatic/ext_localconf.php
@@ -6,4 +6,5 @@ if (!defined('TYPO3_MODE')) {
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['configArrayPostProc'][$_EXTKEY] = 'EXT:simulatestatic/class.tx_simulatestatic.php:&tx_simulatestatic->hookInitConfig';
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tstemplate.php']['linkData-PostProc'][$_EXTKEY] = 'EXT:simulatestatic/class.tx_simulatestatic.php:&tx_simulatestatic->hookLinkDataPostProc';
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['checkAlternativeIdMethods-PostProc'][$_EXTKEY] = 'EXT:simulatestatic/class.tx_simulatestatic.php:&tx_simulatestatic->hookCheckAlternativeIDMethods';
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['modifyPageId'][$_EXTKEY] = 'EXT:simulatestatic/class.tx_simulatestatic.php:&tx_simulatestatic->hookModifyPageId';
 ?>
