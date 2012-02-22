9ae05bd520dabce32c16df3bc00feaca85dab513
Florian Weber <florian.weber@wfp2.com>
2012-02-22 10:30:32 +0100

    [BUGFIX] Error in Crop Parameter for ImageMagick convert
    This change ensures that the parameters for the crop command
    are either both positive or both negative - but never a positive and
    a negative value together.
    
    Change-Id: Ie16d0f11974e04bb2062aba502a08475733589cf
    Fixes: #33728
    Releases: 4.7, 4.6, 4.5

--- t3lib/class.t3lib_stdgraphic.php
+++ t3lib/class.t3lib_stdgraphic.php
@@ -2173,10 +2173,10 @@ class t3lib_stdGraphic {
 
 					// Cropscaling:
 				if ($data['crs']) {
-					if (!$data['origW']) {
+					if (!$data['origW'] || ($data['origW']>$data[0] && $data['origH']<$data[1])) {
 						$data['origW'] = $data[0];
 					}
-					if (!$data['origH']) {
+					if (!$data['origH'] || ($data['origW']<$data[0] && $data['origH']>$data[1])) {
 						$data['origH'] = $data[1];
 					}
 					$offsetX = intval(($data[0] - $data['origW']) * ($data['cropH'] + 100) / 200);
