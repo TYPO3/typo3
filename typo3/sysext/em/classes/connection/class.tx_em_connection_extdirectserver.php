6259f6a7a724c732f4b89f1ddaa4dd84cf268d41
Georg Ringer <mail@ringerge.org>
2012-02-24 07:19:13 +0100

    [BUGFIX] Images from TER hosters break SSL
    The logos are fetched from typo3.org and if
    backend runs in SSL mode, those recourses should also
    use SSL.
    
    Change-Id: I8d210facfe89cbdae2ebd1d07da6cb8c51e5bbdb
    Fixes: #34203
    Releases: 4.7, 4.6, 4.5

--- typo3/sysext/em/classes/connection/class.tx_em_connection_extdirectserver.php
+++ typo3/sysext/em/classes/connection/class.tx_em_connection_extdirectserver.php
@@ -1049,7 +1049,7 @@ class tx_em_Connection_ExtDirectServer {
 						'path' => $mirror['path'],
 						'sponsor' => $mirror['sponsorname'],
 						'link' => $mirror['sponsorlink'],
-						'logo' => $mirror['sponsorlogo'],
+						'logo' => (t3lib_div::getIndpEnv('TYPO3_SSL') ? str_replace('http://', 'https://', $mirror['sponsorlogo']) : $mirror['sponsorlogo']),
 					);
 				}
 			}
