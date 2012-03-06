480764d76b3e1018548d9048cdea526f6474706c
TYPO3 v4 Release Team <typo3v4@typo3.org>
2012-03-06 11:38:14 +0100

    [TASK] Set TYPO3 version to 4.6.6-dev
    Change-Id: I6a71aec9477e1ca817fc164b1a0906e3ec7aeebc

--- t3lib/config_default.php
+++ t3lib/config_default.php
@@ -711,7 +711,7 @@ if (TYPO3_MODE === 'BE') {
 $T3_VAR = array();	// Initialize.
 
 	// TYPO3 version
-$TYPO_VERSION = '4.6.5';	// @deprecated: Will be removed in TYPO3 4.8. Use the constants defined below
+$TYPO_VERSION = '4.6.6-dev';	// @deprecated: Will be removed in TYPO3 4.8. Use the constants defined below
 define('TYPO3_version', $TYPO_VERSION);
 define('TYPO3_branch', '4.6');
 define('TYPO3_copyright_year', '1998-2011');
