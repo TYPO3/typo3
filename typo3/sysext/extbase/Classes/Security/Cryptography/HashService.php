876f348f6cc79ff66bca2b91f83559e6bc2434e8
Felix Oertel <f@oer.tel>
2012-03-07 11:55:58 +0100

    [SECURITY] Protect arguments of form __referrer with HMAC
    The request arguments of the referring request are
    a serialized string written to one of the hidden
    fields in a Fluid form. This string has to be protected
    by a HMAC to protect Extbase from possible unserialize
    attacks.
    
    Note: For now there is no object known within Extbase,
    that could be used for an unserialize exploit!
    
    This change also backports some convenience hmac methods
    to the hash service from the current master, to have the
    bugfix in sync.
    
    Also this backports some of the method renamings from
    FLOW3.
    
    Change-Id: Iaba9e7556c7aeeb8a3724c84d96160a511500e98
    Security-Bulletin: TYPO3-CORE-SA-2012-001
    Fixes: #35310
    Releases: 1.4, 4.7, 6.0

--- typo3/sysext/extbase/Classes/Security/Cryptography/HashService.php
+++ typo3/sysext/extbase/Classes/Security/Cryptography/HashService.php
@@ -34,19 +34,44 @@
  * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser Public License, version 3 or later
  */
 class Tx_Extbase_Security_Cryptography_HashService implements t3lib_singleton {
+
 	/**
 	 * Generate a hash for a given string
 	 *
 	 * @param string $string The string for which a hash should be generated
 	 * @return string The hash of the string
-	 * @throws F3\FLOW3\Security\Exception\InvalidArgumentForHashGeneration if something else than a string was given as parameter
-	 * @todo Mark as API once it is more stable
+	 * @deprecated since Extbase 6.0, will be removed in Extbase 6.2
 	 */
 	public function generateHash($string) {
+		t3lib_div::logDeprecatedFunction();
+		return $this->generateHmac($string);
+	}
+
+	/**
+	 * Generate a hash (HMAC) for a given string
+	 *
+	 * @param string $string The string for which a hash should be generated
+	 * @return string The hash of the string
+	 * @throws Tx_Extbase_Security_Exception_InvalidArgumentForHashGeneration if something else than a string was given as parameter
+	 */
+	public function generateHmac($string) {
 		if (!is_string($string)) throw new Tx_Extbase_Security_Exception_InvalidArgumentForHashGeneration('A hash can only be generated for a string, but "' . gettype($string) . '" was given.', 1255069587);
 		$encryptionKey = $GLOBALS['TYPO3_CONF_VARS']['SYS']['encryptionKey'];
 		if (!$encryptionKey) throw new Tx_Extbase_Security_Exception_InvalidArgumentForHashGeneration('Encryption Key was empty!', 1255069597);
 		return hash_hmac('sha1', $string, $encryptionKey);
+	}
+
+	/**
+	 * Appends a hash (HMAC) to a given string and returns the result
+	 *
+	 * @param string $string The string for which a hash should be generated
+	 * @return string The original string with HMAC of the string appended
+	 * @see generateHmac()
+	 * @todo Mark as API once it is more stable
+	 */
+	public function appendHmac($string) {
+		$hmac = $this->generateHmac($string);
+		return $string . $hmac;
 	}
 
 	/**
@@ -55,10 +80,49 @@ class Tx_Extbase_Security_Cryptography_HashService implements t3lib_singleton {
 	 * @param string $string The string which should be validated
 	 * @param string $hash The hash of the string
 	 * @return boolean TRUE if string and hash fit together, FALSE otherwise.
-	 * @todo Mark as API once it is more stable
+	 * @deprecated since Extbase 6.0, will be removed in Extbase 6.2
 	 */
 	public function validateHash($string, $hash) {
-		return ($this->generateHash($string) === $hash);
+		t3lib_div::logDeprecatedFunction();
+		return $this->validateHmac($string, $hash);
+	}
+
+	/**
+	 * Tests if a string $string matches the HMAC given by $hash.
+	 *
+	 * @param string $string The string which should be validated
+	 * @param string $hmac The hash of the string
+	 * @return boolean TRUE if string and hash fit together, FALSE otherwise.
+	 */
+	public function validateHmac($string, $hmac) {
+		return ($this->generateHmac($string) === $hmac);
+	}
+
+	/**
+	 * Tests if the last 40 characters of a given string $string
+	 * matches the HMAC of the rest of the string and, if true,
+	 * returns the string without the HMAC. In case of a HMAC
+	 * validation error, an exception is thrown.
+	 *
+	 * @param string $string The string with the HMAC appended (in the format 'string<HMAC>')
+	 * @return string the original string without the HMAC, if validation was successful
+	 * @see validateHmac()
+	 * @throws Tx_Extbase_Security_Exception_InvalidArgumentForHashGeneration if the given string is not well-formatted
+	 * @throws Tx_Extbase_Security_Exception_InvalidHash if the hash did not fit to the data.
+	 * @todo Mark as API once it is more stable
+	 */
+	public function validateAndStripHmac($string) {
+		if (!is_string($string)) {
+			throw new Tx_Extbase_Security_Exception_InvalidArgumentForHashGeneration('A hash can only be validated for a string, but "' . gettype($string) . '" was given.', 1320829762);
+		}
+		if (strlen($string) < 40) {
+			throw new Tx_Extbase_Security_Exception_InvalidArgumentForHashGeneration('A hashed string must contain at least 40 characters, the given string was only ' . strlen($string) . ' characters long.', 1320830276);
+		}
+		$stringWithoutHmac = substr($string, 0, -40);
+		if ($this->validateHmac($stringWithoutHmac, substr($string, -40)) !== TRUE) {
+			throw new Tx_Extbase_Security_Exception_InvalidHash('The given string was not appended with a valid HMAC.', 1320830018);
+		}
+		return $stringWithoutHmac;
 	}
 }
 ?>
