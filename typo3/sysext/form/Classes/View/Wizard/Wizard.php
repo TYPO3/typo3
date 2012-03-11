e08fb989d1e52a21258a20e3aeee998f82a4e3c3
Franz Geiger <mail@fx-g.de>
2012-03-11 21:58:50 +0100

    [FEATURE] Insert hook into form wizard view
    Introduces a hook in tx_form_View_Wizard_Wizard. This hook
    allows extending the wizard interface by loading extending
    resources. The hook is executed after all other resources have
    been loaded and just before content rendering.
    
    Use it by adding your function to the array
    $TYPO3_CONF_VARS['EXTCONF']['form']['hooks']['renderWizard']
    
    Change-Id: I4fc22d5044d2808a9dbdb5ea9b256c2427987030
    Resolves: #34711
    Releases: 4.8

--- typo3/sysext/form/Classes/View/Wizard/Wizard.php
+++ typo3/sysext/form/Classes/View/Wizard/Wizard.php
@@ -108,6 +108,9 @@ class tx_form_View_Wizard_Wizard extends tx_form_View_Wizard_Abstract {
 				// Setting up the buttons and markers for docheader
 			$docHeaderButtons = $this->getButtons();
 			$markers['CSH'] = $docHeaderButtons['csh'];
+
+				// Hook
+			$this->hook_render();
 		}
 
 			// Getting the body content
@@ -330,6 +333,27 @@ class tx_form_View_Wizard_Wizard extends tx_form_View_Wizard_Abstract {
 		);
 
 		$this->pageRenderer->addInlineLanguageLabelArray($labels['default']);
+	}
+
+	/**
+	 * Hook to extend the wizard interface
+	 *
+	 * The hook is called just before content rendering.
+	 * Use it by adding your function to the array
+	 * $TYPO3_CONF_VARS['EXTCONF']['form']['hooks']['renderWizard']
+	 *
+	 */
+	protected function hook_render() {
+
+		$param = array();
+
+		if (is_array($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['form']['hooks']['renderWizard'])) {
+
+			foreach ($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['form']['hooks']['renderWizard'] as $funcRef) {
+
+				t3lib_div::callUserFunction($funcRef, $param, $this);
+			}
+		}
 	}
 
 	/**
