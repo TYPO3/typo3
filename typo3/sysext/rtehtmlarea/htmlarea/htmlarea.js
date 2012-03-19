0bd8d06bbf0d24772d4b52d73b4902ce51ff86d7
Bart Dubelaar <bartdubelaar@gmail.com>
2012-03-19 15:44:34 +0100

    [BUGFIX] Custom HTML tags no longer malformed in IE
    Custom HTML tags would get malformed while editing in IE.
    If a tag is not present in HTML4 and the document mode
    of IE is lower than IE9 Standards, then the tag is not
    present in the DOM and not parsed correctly.
    This is fixed by specifying possible custom tags in TSconfig
    and creating dummy instances of the tags before loading
    the contents in the DOM, also known as the Shiv trick.
    
    Change-Id: Ie828588b94e6ee01bd645b49a0787deca9e07a40
    Fixes: #34786
    Releases: 4.5, 4.6, 4.7, 6.0
    Reviewed-on: http://review.typo3.org/10913
    Reviewed-by: Bart Dubelaar
    Reviewed-by: Stanislas Rolland
    Tested-by: Stanislas Rolland

--- typo3/sysext/rtehtmlarea/htmlarea/htmlarea.js
+++ typo3/sysext/rtehtmlarea/htmlarea/htmlarea.js
@@ -174,8 +174,8 @@ HTMLArea.Config = function (editorId) {
 	this.htmlRemoveTagsAndContents = /none/i;
 		// Remove comments
 	this.htmlRemoveComments = false;
-		// Custom tags (must be a regular expression)
-	this.customTags = /none/i;
+		// Array of custom tags
+	this.customTags = [];
 		// BaseURL to be included in the iframe document
 	this.baseURL = document.baseURI;
 		// IE does not support document.baseURI
@@ -976,6 +976,7 @@ HTMLArea.Iframe = Ext.extend(Ext.BoxComponent, {
 			this.getEditor().document = this.document;
 			this.getEditor()._doc = this.document;
 			this.getEditor()._iframe = iframe;
+			this.initializeCustomTags();
 			this.createHead();
 				// Style the document body
 			Ext.get(this.document.body).addClass('htmlarea-content-body');
@@ -989,6 +990,19 @@ HTMLArea.Iframe = Ext.extend(Ext.BoxComponent, {
 				// Set iframe ready
 			this.ready = true;
 			this.fireEvent('HTMLAreaEventIframeReady');
+		}
+	},
+	/*
+	 * Create one of each of the configured custom tags so they are properly parsed by the walker when using IE
+	 * See: http://en.wikipedia.org/wiki/HTML5_Shiv
+	 * 
+	 * @return	void
+	 */
+	initializeCustomTags: function () {
+		if (Ext.isIE6 || Ext.isIE7 || Ext.isIE8 || (Ext.isIE && this.document.documentMode < 9)) {
+			Ext.each(this.config.customTags, function (tag) {
+				this.document.createElement(tag);
+			}, this);
 		}
 	},
 	/*
