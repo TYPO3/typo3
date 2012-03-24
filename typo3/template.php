df53f572bc56cdccbdb691a4948f5cc1ef45bf47
Felix Kopp <felix-source@phorax.com>
2012-03-24 18:32:09 +0100

    [BUGFIX] Add trailing slash to PAGEPATH
    With issue #23828 the closing slash for PAGEPATH was removed.
    If PAGEPATH in docheader is combined with PAGEINFO, there is now no
    visible separation anymore. The patch re-introduces the missing slash.
    
    Change-Id: I6f261179cfcdee7301bc8eaba47d722b973d8017
    Resolves: #35216
    Releases: 6.0, 4.7
    Reviewed-on: http://review.typo3.org/9861
    Reviewed-by: Christian Kuhn
    Tested-by: Christian Kuhn

--- typo3/template.php
+++ typo3/template.php
@@ -1893,7 +1893,7 @@ $str.=$this->docBodyTagBegin().
 				// remove current page title
 			$pos = strrpos($title, '/');
 			if ($pos !== FALSE) {
-				$title = substr($title, 0, $pos);
+				$title = substr($title, 0, $pos) . '/';
 			}
 		} else {
 			$title = '';
