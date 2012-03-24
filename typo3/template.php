d289a7a56a44afc456ae54f9105c5561eb36f1ac
Felix Kopp <felix-source@phorax.com>
2012-03-24 18:32:09 +0100

    [BUGFIX] Add closing slash to PAGEPATH
    In bug fix #23828 the closing slash for PAGEPATH was removed.
    Because of this in most backend modules have no separation
    between PAGEPATH and PAGEINFO.
    
    Adds closing slash to PAGEPATH in docheader.
    
    Change-Id: I6f261179cfcdee7301bc8eaba47d722b973d8017
    Fixes: #35216
    Releases: 4.7, 4.8

--- typo3/template.php
+++ typo3/template.php
@@ -1949,7 +1949,7 @@ $str.=$this->docBodyTagBegin().
 				// remove current page title
 			$pos = strrpos($title, '/');
 			if ($pos !== FALSE) {
-				$title = substr($title, 0, $pos);
+				$title = substr($title, 0, $pos) . '/';
 			}
 		} else {
 			$title = '';
