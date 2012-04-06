969965cb3d0c4dce53930c73b72f3b85335a9cb5
Marc Bastian Heinrichs <heinrichs@mbh-web.de>
2012-04-06 11:41:11 +0200

    [+BUGFIX] (Persistence) Make Typo3QuerySettings exchangeable
    Let objectManager create querySettings object by
    Tx_Extbase_Persistence_QuerySettingsInterface in
    Tx_Extbase_Persistence_QueryFactory and remove obsolete instanceof
    check in Typo3DbBackend
    
    Backport from 1.4-branch (#13191)
    
    Change-Id: If6776cb325918d0f24fab4d28694a09f30c18c7b
    Fixes: #35738
    Reviewed-on: http://review.typo3.org/10178
    Reviewed-by: Oliver Klee
    Reviewed-by: Marc Bastian Heinrichs
    Tested-by: Marc Bastian Heinrichs

--- typo3/sysext/extbase/ext_localconf.php
+++ typo3/sysext/extbase/ext_localconf.php
@@ -24,12 +24,13 @@ $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['cache_extb
 	),
 );
 
-// We need to set the default implementation for the Storage Backend
+// We need to set the default implementation for Storage Backend & Query Settings
 // the code below is NO PUBLIC API! It's just to make sure that
 // Extbase works correctly in the backend if the page tree is empty or no
 // template is defined.
 $extbaseObjectContainer = t3lib_div::makeInstance('Tx_Extbase_Object_Container_Container'); // Singleton
 $extbaseObjectContainer->registerImplementation('Tx_Extbase_Persistence_Storage_BackendInterface', 'Tx_Extbase_Persistence_Storage_Typo3DbBackend');
+$extbaseObjectContainer->registerImplementation('Tx_Extbase_Persistence_QuerySettingsInterface', 'Tx_Extbase_Persistence_Typo3QuerySettings');
 unset($extbaseObjectContainer);
 
 # $GLOBALS ['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = 'EXT:extbase/Classes/Persistence/Hook/TCEMainValueObjectUpdater.php:tx_Extbase_Persistence_Hook_TCEMainValueObjectUpdater';
