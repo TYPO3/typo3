1d44f031fd0a59057bea8c23399df194dd03f80c
Steffen Ritter <info@rs-websystems.de>
2012-04-11 23:46:40 +0200

    [TASK] Add update-wizard for Media Flexform
    While adding the new media stuff the flexform has been altered.
    Therefore a update wizard needs to be provided to not lose the
    old media elements.
    
    Change-Id: I60943b2f9eee84d768bce8b5a5f38b6f83c027f3
    Resolves: #35879
    Releases: 6.0,4.7

--- typo3/sysext/install/ext_localconf.php
+++ typo3/sysext/install/ext_localconf.php
@@ -50,4 +50,10 @@ $TYPO3_CONF_VARS['SC_OPTIONS']['ext/install']['update']['addFlexformsToAcl'] = '
 
 	// Version 4.5: Split tt_content image_link to newline by comma
 $TYPO3_CONF_VARS['SC_OPTIONS']['ext/install']['update']['imagelink'] = 'tx_coreupdates_imagelink';
+
+	// Version 4.7: Load fluid and extbase if extensions with new dependency to them are installed
+$TYPO3_CONF_VARS['SC_OPTIONS']['ext/install']['update']['installFluidExtbase'] = 'tx_coreupdates_installFluidExtbase';
+
+	// Version 4.7: Migrate the flexforms of MediaElement
+$TYPO3_CONF_VARS['SC_OPTIONS']['ext/install']['update']['mediaElementFlexform'] = 'tx_coreupdates_mediaFlexform';
 ?>
