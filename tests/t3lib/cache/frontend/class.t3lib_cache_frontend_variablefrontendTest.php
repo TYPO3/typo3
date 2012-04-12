d862faaccfa610aded03b82b6b8079820415cc89
Daniel Pötzinger <poetzinger@aoemedia.de>
2012-04-12 19:59:38 +0200

    [BUGFIX] VariableFrontend initializeObject not called
    The method is protected and is not called in the container then
    
    Change-Id: I03ab47aa1030e782d14304d9371fc62b9c5aed18
    Fixes: #35915
    Releases: 6.0, 4.7, 4.6, 4.5
    Reviewed-on: http://review.typo3.org/12470
    Reviewed-by: Christian Kuhn
    Tested-by: Christian Kuhn

--- tests/t3lib/cache/frontend/class.t3lib_cache_frontend_variablefrontendTest.php
+++ tests/t3lib/cache/frontend/class.t3lib_cache_frontend_variablefrontendTest.php
@@ -102,6 +102,7 @@ class t3lib_cache_frontend_VariableFrontendTest extends tx_phpunit_testcase {
 		$backend->expects($this->once())->method('set')->with($this->equalTo('VariableCacheTest'), $this->equalTo(igbinary_serialize($theString)));
 
 		$cache = new t3lib_cache_frontend_VariableFrontend('VariableFrontend', $backend);
+		$cache->initializeObject();
 		$cache->set('VariableCacheTest', $theString);
 	}
 
@@ -159,6 +160,7 @@ class t3lib_cache_frontend_VariableFrontendTest extends tx_phpunit_testcase {
 		$backend->expects($this->once())->method('get')->will($this->returnValue(igbinary_serialize($theArray)));
 
 		$cache = new t3lib_cache_frontend_VariableFrontend('VariableFrontend', $backend);
+		$cache->initializeObject();
 
 		$this->assertEquals($theArray, $cache->get('VariableCacheTest'), 'The returned value was not the expected unserialized array.');
 	}
@@ -241,6 +243,7 @@ class t3lib_cache_frontend_VariableFrontendTest extends tx_phpunit_testcase {
 		$backend->expects($this->exactly(2))->method('get')->will($this->onConsecutiveCalls(igbinary_serialize('one value'), igbinary_serialize('two value')));
 
 		$cache = new t3lib_cache_frontend_VariableFrontend('VariableFrontend', $backend);
+		$cache->initializeObject();
 		$this->assertEquals($entries, $cache->getByTag($tag), 'Did not receive the expected entries');
 	}
 }
