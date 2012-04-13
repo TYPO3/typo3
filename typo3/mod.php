18b16f66743092f8b34c7e8da50d1f07d9e11af1
Felix Oertel <f@oer.tel>
2012-04-13 11:55:05 +0200

    [BUGFIX] Extbase Bootstrap not overwritable
    In mod.php the bootstrap is instanciated without using the
    objectManager, thus it's not possible to overwrite it.
    
    Change-Id: I4720172ef4b602fdb31c3a58112c03f0d6673db2
    Fixes: #35955
    Releases: 6.0
    Reviewed-on: http://review.typo3.org/10387
    Reviewed-by: Oliver Klee
    Tested-by: Oliver Klee
    Reviewed-by: Christian Kuhn
    Tested-by: Christian Kuhn

--- typo3/mod.php
+++ typo3/mod.php
@@ -47,7 +47,8 @@ if ($temp_path = $TBE_MODULES['_PATHS'][$temp_M]) {
 } else {
 	if (is_array($TBE_MODULES['_dispatcher'])) {
 		foreach ($TBE_MODULES['_dispatcher'] as $dispatcherClassName) {
-			$dispatcher = t3lib_div::makeInstance($dispatcherClassName);
+			$dispatcher = t3lib_div::makeInstance('Tx_Extbase_Object_ObjectManager')
+				->get($dispatcherClassName);
 			if ($dispatcher->callModule($temp_M) === TRUE) {
 				$isDispatched = TRUE;
 				break;
