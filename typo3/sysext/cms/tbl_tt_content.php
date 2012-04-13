bec0de474535f862a8b422c7ec986c263afe4d8d
Marco Huber <mail@marco-huber.de>
2012-04-13 16:35:47 +0200

    [BUGFIX] Hide the field "Selected Pages" for menu type "Sitemap"
    The field "Selected Pages" is never used in the content element
    "Menu/Sitemap" with menu type "Sitemap". So it should not be
    shown in the backend.
    
    Change-Id: I9464ff29fc9fd9864a404cfb14f0545b3e186366
    Fixes: #35944
    Releases: 6.0, 4.7, 4.6, 4.5

--- typo3/sysext/cms/tbl_tt_content.php
+++ typo3/sysext/cms/tbl_tt_content.php
@@ -1881,6 +1881,10 @@ $TCA['tt_content'] = array(
 					--palette--;LLL:EXT:cms/locallang_ttc.xml:palette.visibility;visibility,
 					--palette--;LLL:EXT:cms/locallang_ttc.xml:palette.access;access,
 				--div--;LLL:EXT:cms/locallang_ttc.xml:tabs.extended',
+			'subtype_value_field' => 'menu_type',
+			'subtypes_excludelist' => array(
+				'2' => 'pages',
+			),
 		),
 		'mailform' => 	array(
 			'showitem' =>
