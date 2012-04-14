ae2bbd14ba2e085410e3588e3bd1b486ccddc22e
Max Roesch <typo3@elbebass.com>
2012-04-14 19:41:48 +0200

    [BUGFIX] Check if user is allowed to paste page to pagetree
    The user rights have checks for new / edit / delete, but paste was missing
    leading to an error. Page actions 'paste into' and 'page after' now checks
    if the user has rights for adding new pages to the current page.
    
    Change-Id: Ief80a511d61ba386d667c62263d6f5335d1c87b8
    Fixes: #33546
    Releases: 4.5, 4.6, 4.7, 6.0
    Reviewed-on: http://review.typo3.org/10483
    Reviewed-by: Kilian Hann
    Reviewed-by: Tolleiv Nietsch
    Tested-by: Tolleiv Nietsch
    Reviewed-by: Felix Kopp
    Tested-by: Felix Kopp
    Reviewed-by: Georg Ringer
    Tested-by: Georg Ringer

--- t3lib/tree/pagetree/class.t3lib_tree_pagetree_node.php
+++ t3lib/tree/pagetree/class.t3lib_tree_pagetree_node.php
@@ -276,7 +276,7 @@ class t3lib_tree_pagetree_Node extends t3lib_tree_extdirect_Node {
 	 * @return bool
 	 */
 	public function canBePastedInto() {
-		return intval($this->record['t3ver_state']) !== 2;
+		return $this->canCreate($this->record) && intval($this->record['t3ver_state']) !== 2;
 	}
 
 	/**
@@ -285,7 +285,7 @@ class t3lib_tree_pagetree_Node extends t3lib_tree_extdirect_Node {
 	 * @return bool
 	 */
 	public function canBePastedAfter() {
-		return intval($this->record['t3ver_state']) !== 2;
+		return $this->canCreate($this->record) && intval($this->record['t3ver_state']) !== 2;
 	}
 
 	/**
