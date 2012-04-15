53a1fe0564b662833a562239acf6ed9c5dcb7eda
Steffen Müller <typo3@t3node.com>
2012-04-15 12:26:57 +0200

    [BUGFIX] Exclude E_STRICT from exceptionalErrors
    From PHP 5.4, E_STRICT became part of E_ALL. This leads to lots of runtime
    notice exceptions when using PHP 5.4. To avoid that, E_STRICT needs to be
    excluded from error reporting.
    
    Change-Id: Iadddfe42f48afc902a67c157df052b4ec15ee8da
    Fixes: #35154
    Releases: 6.0, 4.7, 4.6, 4.5

--- typo3/thumbs.php
+++ typo3/thumbs.php
@@ -33,8 +33,7 @@
 // *******************************
 // Set error reporting
 // *******************************
-error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED);
-
+error_reporting(E_ALL & ~(E_STRICT | E_NOTICE | E_DEPRECATED));
 
 define('PATH_thisScript', str_replace('//', '/', str_replace('\\', '/',
 	(PHP_SAPI == 'fpm-fcgi' || PHP_SAPI == 'cgi' || PHP_SAPI == 'isapi' || PHP_SAPI == 'cgi-fcgi') &&
