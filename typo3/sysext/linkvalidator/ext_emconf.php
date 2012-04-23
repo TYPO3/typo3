8443f7346a5094bad66b444f57847d982f3ea684
Oliver Hader <oliver@typo3.org>
2012-04-23 16:03:15 +0200

    [TASK] Update version to 4.7.0
    Change-Id: If626db3e812cdd10cf250df2ab4555331ba09c19
    Releases: 4.7

--- typo3/sysext/linkvalidator/ext_emconf.php
+++ typo3/sysext/linkvalidator/ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "linkvalidator".
 #
-# Auto generated 12-04-2012 21:59
+# Auto generated 23-04-2012 16:02
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -30,7 +30,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'doNotLoadInFE' => 1,
 	'lockType' => '',
 	'author_company' => 'Connecta AG / cab services ag / Infoglobe',
-	'version' => '4.7.0rc2',
+	'version' => '4.7.0',
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.3.0-0.0.0',
