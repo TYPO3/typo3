5dc8167971fe995cbcc99185703168fa4f07a1f3
Stanislas Rolland <typo3@sjbr.ca>
2012-05-02 15:38:13 -0400

    [BUGFIX] Update sys_refindex to reflect img tags in RTE content
    Problem: sys_refindex should be updated when an image is inserted or
    removed from RTE content
    Solution: Add rtehtmlarea_images softref key
    
    Change-Id: I40d504adf3d51e55fd271d01656e9db3f285d7ad
    Releases: 6.0
    Resolves: #36789

---
+++ typo3/sysext/rtehtmlarea/hooks/softref/class.tx_rtehtmlarea_softrefproc.php
@@ -0,0 +1,113 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2012 Stanislas Rolland <stanislas.rolland@typo3.org>
+*  All rights reserved
+*
+*  This script is part of the Typo3 project. The Typo3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+/**
+ * Class for processing of the FAL soft references on img tags inserted in RTE content
+ *
+ * @author Stanislas Rolland <stanislas.rolland@typo3.org>
+ */
+class tx_rtehtmlarea_softrefproc extends t3lib_softrefproc {
+		// Token prefix
+	public $tokenID_basePrefix = '';
+
+	/**
+	 * Main function through which all processing happens
+	 *
+	 * @param string Database table name
+	 * @param string Field name for which processing occurs
+	 * @param integer UID of the record
+	 * @param string The content/value of the field
+	 * @param string The softlink parser key. This is only interesting if more than one parser is grouped in the same class. That is the case with this parser.
+	 * @param array Parameters of the softlink parser. Basically this is the content inside optional []-brackets after the softref keys. Parameters are exploded by ";"
+	 * @param string If running from inside a FlexForm structure, this is the path of the tag.
+	 * @return array Result array on positive matches. Otherwise FALSE
+	 */
+	function findRef ($table, $field, $uid, $content, $spKey, $spParams, $structurePath = '') {
+		$retVal = FALSE;
+		$this->tokenID_basePrefix = $table . ':' . $uid . ':' . $field . ':' . $structurePath . ':' . $spKey;
+
+		switch ($spKey)	{
+			case 'rtehtmlarea_images':
+				$retVal = $this->findRef_rtehtmlarea_images($content, $spParams);
+			break;
+			default:
+				$retVal = FALSE;
+			break;
+		}
+
+		return $retVal;
+	}
+
+	/**
+	 * Finding image tags with data-htmlarea-file-uid attribute in the content.
+	 * All images that have an data-htmlarea-file-uid attribute will be returned with an info text
+	 *
+	 * @param	string		The input content to analyse
+	 * @param	array		Parameters set for the softref parser key in TCA/columns
+	 * @return	array		Result array on positive matches, see description above. Otherwise FALSE
+	 */
+	function findRef_rtehtmlarea_images ($content, $spParams) {
+		$retVal = FALSE;
+			// Start HTML parser and split content by image tag
+		$htmlParser = t3lib_div::makeInstance('t3lib_parsehtml');
+		$imgTags = $htmlParser->splitTags('img', $content);
+		$elements = array();
+			// Traverse splitted parts
+		foreach ($imgTags as $k => $v) {
+			if ($k%2) {
+					// Get FAL uid reference
+				$attribs = $htmlParser->get_tag_attributes($v);
+				$fileUid = $attribs[0]['data-htmlarea-file-uid'];
+				$fileTable = $attribs[0]['data-htmlarea-file-table'];
+					// If there is a file uid, continue. Otherwise ignore this img tag.
+				if ($fileUid) {
+						// Initialize the element entry with info text here
+					$tokenID = $this->makeTokenID($k);
+					$elements[$k] = array();
+					$elements[$k]['matchString'] = $v;
+
+						// Token and substitute value
+					$imgTags[$k] = str_replace('data-htmlarea-file-uid="' . $fileUid . '"', 'data-htmlarea-file-uid="{softref:' . $tokenID . '}"', $imgTags[$k]);
+					$elements[$k]['subst'] = array(
+						'type' => 'db',
+						'recordRef' => ($fileTable ? $fileTable : 'sys_file') . ':' . $fileUid,
+						'tokenID' => $tokenID,
+						'tokenValue' => $fileUid,
+					);
+				}
+			}
+		}
+			// Assemble result array
+		if (count($elements)) {
+			$retVal = array(
+				'content' => implode('', $imgTags),
+				'elements' => $elements
+			);
+		}
+		return $retVal;
+	}
+}
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/rtehtmlarea/hooks/softref/class.tx_rtehtmlarea_softrefproc.php']) {
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/rtehtmlarea/hooks/softref/class.tx_rtehtmlarea_softrefproc.php']);
+}
+?>
