292b6d2e14fe48b03eb8d33043e7d96fc26dc115
Stefan Neufeind <typo3.neufeind@speedpartner.de>
2012-05-03 01:20:14 +0200

    [TASK] Add/drop usage of preg_quote() where needed
    In some places preg_quote() is not used or is used incorrectly.
    In other places there are preg_quote() calls which had no
    real function.
    
    Change-Id: If201394036d17a68cea41903f849c6c5ea11b069
    Releases: 4.5, 4.6, 4.7, 6.0, 6.1
    Resolves: #36793
    Reviewed-on: https://review.typo3.org/10943
    Reviewed-by: Dmitry Dulepov
    Reviewed-by: Markus Klein
    Tested-by: Markus Klein
    Tested-by: Philipp Gampe
    Reviewed-by: Philipp Gampe
    Reviewed-by: Jigal van Hemert
    Tested-by: Jigal van Hemert

--- typo3/sysext/core/Classes/Html/HtmlParser.php
+++ typo3/sysext/core/Classes/Html/HtmlParser.php
@@ -220,7 +220,7 @@ class HtmlParser {
 				if (empty($wrap)) {
 					$wrapArr = array('###', '###');
 				}
-				$content = preg_replace('/' . preg_quote($wrapArr[0]) . '([A-Z0-9_|\\-]*)' . preg_quote($wrapArr[1]) . '/is', '', $content);
+				$content = preg_replace('/' . preg_quote($wrapArr[0], '/') . '([A-Z0-9_|\\-]*)' . preg_quote($wrapArr[1], '/') . '/is', '', $content);
 			}
 		}
 		return $content;
@@ -322,6 +322,9 @@ class HtmlParser {
 	 */
 	public function splitIntoBlock($tag, $content, $eliminateExtraEndTags = FALSE) {
 		$tags = array_unique(\TYPO3\CMS\Core\Utility\GeneralUtility::trimExplode(',', $tag, 1));
+		foreach ($tags as &$tag) {
+			$tag = preg_quote($tag, '/');
+		}
 		$regexStr = '/\\<\\/?(' . implode('|', $tags) . ')(\\s*\\>|\\s[^\\>]*\\>)/si';
 		$parts = preg_split($regexStr, $content);
 		$newParts = array();
@@ -425,6 +428,9 @@ class HtmlParser {
 	 */
 	public function splitTags($tag, $content) {
 		$tags = \TYPO3\CMS\Core\Utility\GeneralUtility::trimExplode(',', $tag, 1);
+		foreach ($tags as &$tag) {
+			$tag = preg_quote($tag, '/');
+		}
 		$regexStr = '/\\<(' . implode('|', $tags) . ')(\\s[^>]*)?\\/?>/si';
 		$parts = preg_split($regexStr, $content);
 		$pointer = strlen($parts[0]);
@@ -627,8 +633,8 @@ class HtmlParser {
 		// Block tags, must have endings...
 		$blockTags = explode(',', $blockTags);
 		foreach ($blockTags as $tagName) {
-			$countBegin = count(preg_split(('/\\<' . $tagName . '(\\s|\\>)/s'), $content)) - 1;
-			$countEnd = count(preg_split(('/\\<\\/' . $tagName . '(\\s|\\>)/s'), $content)) - 1;
+			$countBegin = count(preg_split(('/\\<' . preg_quote($tagName, '/') . '(\\s|\\>)/s'), $content)) - 1;
+			$countEnd = count(preg_split(('/\\<\\/' . preg_quote($tagName, '/') . '(\\s|\\>)/s'), $content)) - 1;
 			$analyzedOutput['blocks'][$tagName] = array($countBegin, $countEnd, $countBegin - $countEnd);
 			if ($countBegin) {
 				$analyzedOutput['counts'][$tagName] = $countBegin;
@@ -644,8 +650,8 @@ class HtmlParser {
 		// Solo tags, must NOT have endings...
 		$soloTags = explode(',', $soloTags);
 		foreach ($soloTags as $tagName) {
-			$countBegin = count(preg_split(('/\\<' . $tagName . '(\\s|\\>)/s'), $content)) - 1;
-			$countEnd = count(preg_split(('/\\<\\/' . $tagName . '(\\s|\\>)/s'), $content)) - 1;
+			$countBegin = count(preg_split(('/\\<' . preg_quote($tagName, '/') . '(\\s|\\>)/s'), $content)) - 1;
+			$countEnd = count(preg_split(('/\\<\\/' . preg_quote($tagName, '/') . '(\\s|\\>)/s'), $content)) - 1;
 			$analyzedOutput['solo'][$tagName] = array($countBegin, $countEnd);
 			if ($countBegin) {
 				$analyzedOutput['counts'][$tagName] = $countBegin;
@@ -1152,7 +1158,7 @@ class HtmlParser {
 	 */
 	public function mapTags($value, $tags = array(), $ltChar = '<', $ltChar2 = '<') {
 		foreach ($tags as $from => $to) {
-			$value = preg_replace('/' . preg_quote($ltChar) . '(\\/)?' . $from . '\\s([^\\>])*(\\/)?\\>/', $ltChar2 . '$1' . $to . ' $2$3>', $value);
+			$value = preg_replace('/' . preg_quote($ltChar, '/') . '(\\/)?' . $from . '\\s([^\\>])*(\\/)?\\>/', $ltChar2 . '$1' . $to . ' $2$3>', $value);
 		}
 		return $value;
 	}
--- typo3/sysext/core/Classes/Utility/ExtensionManagementUtility.php
+++ typo3/sysext/core/Classes/Utility/ExtensionManagementUtility.php
@@ -255,7 +255,7 @@ class ExtensionManagementUtility {
 						$positionArray = \TYPO3\CMS\Core\Utility\GeneralUtility::trimExplode(':', $position);
 						if ($positionArray[0] == 'replace') {
 							foreach ($GLOBALS['TCA'][$table]['palettes'] as $palette => $paletteDetails) {
-								if (preg_match('/\\b' . $palette . '\\b/', $typeDetails['showitem']) > 0 && preg_match('/\\b' . $positionArray[1] . '\\b/', $paletteDetails['showitem']) > 0) {
+								if (preg_match('/\\b' . preg_quote($palette, '/') . '\\b/', $typeDetails['showitem']) > 0 && preg_match('/\\b' . preg_quote($positionArray[1], '/') . '\\b/', $paletteDetails['showitem']) > 0) {
 									self::addFieldsToPalette($table, $palette, $str, $position);
 									// Save that palette in case other types use it
 									$palettesChanged[] = $palette;
@@ -269,7 +269,7 @@ class ExtensionManagementUtility {
 								$fieldExists = TRUE;
 							} else {
 								foreach ($GLOBALS['TCA'][$table]['palettes'] as $palette => $paletteDetails) {
-									if (preg_match('/\\b' . $palette . '\\b/', $typeDetails['showitem']) > 0 && preg_match('/\\b' . $positionArray[1] . '\\b/', $paletteDetails['showitem']) > 0) {
+									if (preg_match('/\\b' . preg_quote($palette, '/') . '\\b/', $typeDetails['showitem']) > 0 && preg_match('/\\b' . preg_quote($positionArray[1], '/') . '\\b/', $paletteDetails['showitem']) > 0) {
 										$position = $positionArray[0] . ':--palette--;;' . $palette;
 									}
 								}
@@ -280,7 +280,7 @@ class ExtensionManagementUtility {
 							$fieldExists = TRUE;
 						} elseif (is_array($GLOBALS['TCA'][$table]['palettes'])) {
 							foreach ($GLOBALS['TCA'][$table]['palettes'] as $palette => $paletteDetails) {
-								if (preg_match('/\\b' . $palette . '\\b/', $typeDetails['showitem']) > 0 && strpos($paletteDetails['showitem'], $str) !== FALSE) {
+								if (preg_match('/\\b' . preg_quote($palette, '/') . '\\b/', $typeDetails['showitem']) > 0 && strpos($paletteDetails['showitem'], $str) !== FALSE) {
 									$fieldExists = TRUE;
 								}
 							}
