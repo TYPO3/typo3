93df49b9184b9545ce11bc2badce0bb06609ca69
Ingo Renner <ingo@typo3.org>
2012-05-02 23:03:45 +0200

    [FEATURE] Add Progress bar for task progress
    Some tasks may want to provide the user with information about how
    far progress of their jobs has come.
    
    Currently there is no way to do this in a nice way. The existing
    method getAdditionalInformation() does not allow to output HTML.
    
    Fixes: #36802
    Releases: 6.0
    Change-Id: I86877b3a3ec70e8c6ef5deae7e54442c35afada3
    Reviewed-on: http://review.typo3.org/10946
    Reviewed-by: Philipp Gampe
    Reviewed-by: Nikolas Hagelstein
    Reviewed-by: Mattias Nilsson
    Tested-by: Mattias Nilsson
    Reviewed-by: Jigal van Hemert
    Tested-by: Jigal van Hemert

---
+++ typo3/sysext/scheduler/interfaces/interface.tx_scheduler_progressprovider.php
@@ -0,0 +1,45 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2012 Ingo Renner <ingo@typo3.org>
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+
+/**
+ * Interface for tasks who can provide their progress
+ *
+ * @author Ingo Renner <ingo@typo3.org>
+ * @package TYPO3
+ * @subpackage tx_scheduler
+ */
+interface tx_scheduler_ProgressProvider {
+
+	/**
+	 * Gets the progress of a task.
+	 *
+	 * @return float Progress of the task as a two decimal precision float. f.e. 44.87
+	 */
+	public function getProgress();
+
+}
+
+
+?>
