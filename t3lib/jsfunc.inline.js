92ef8583837e3772599d3f6444f3a0107eda3834
Stefan Galinski <stefan.galinski@gmail.com>
2012-05-31 15:41:39 +0200

    [BUGFIX] IRRE records can't be expanded without an hidden field
    Change-Id: If53182bf13a9d7d50479fe00561f3decbceb1fa2
    Fixes: #37615
    Releases: 6.0, 4.7

--- t3lib/jsfunc.inline.js
+++ t3lib/jsfunc.inline.js
@@ -420,13 +420,17 @@ var inline = {
 		var elName = this.parseObjectId('full', objectId, 2, 0, true);
 
 		var formObj = document.getElementsByName(elName+'[hidden]_0')[0];
-		var tempHiddenValue = formObj.checked;
-		formObj.remove();
-		document.getElementsByName(elName+'[hidden]')[0].remove();
+		if (formObj) {
+			var tempHiddenValue = formObj.checked;
+			formObj.remove();
+			document.getElementsByName(elName+'[hidden]')[0].remove();
 
-		objectDiv.update(htmlData);
-		document.getElementsByName(elName+'[hidden]')[0].value = tempHiddenValue ? 1 : 0;
-		document.getElementsByName(elName+'[hidden]_0')[0].checked = tempHiddenValue;
+			objectDiv.update(htmlData);
+			document.getElementsByName(elName+'[hidden]')[0].value = tempHiddenValue ? 1 : 0;
+			document.getElementsByName(elName+'[hidden]_0')[0].checked = tempHiddenValue;
+		} else {
+			objectDiv.update(htmlData);
+		}
 
 			// remove loading-indicator
 		if ($(objectId + '_icon')) {
