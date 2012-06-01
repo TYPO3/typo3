40f498ce367ac838341abf081e1e33a2f9ad9c42
Roland Waldner <most.wanted@gmx.at>
2012-06-01 15:22:33 +0200

    [TASK] Fix CGL violations InlineCommentWrongStyle in ext_emconf.php files
    There are violations against the CGL rule "InlineCommentWrongStyle " in
    ext_emconf.php files.
    
    These will be fixed.
    
    Change-Id: Ic8a83bf8a12246328a4c8fe48ef27fb99c2f3396
    Resolves: #37685
    Releases: 6.0
    Reviewed-on: http://review.typo3.org/11793
    Reviewed-by: Georg Ringer
    Tested-by: Georg Ringer

--- typo3/sysext/indexed_search/ext_emconf.php
+++ typo3/sysext/indexed_search/ext_emconf.php
@@ -1,14 +1,14 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "indexed_search".
-#
-# Auto generated 30-11-2011 14:26
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
+/***************************************************************
+* Extension Manager/Repository config file for ext "indexed_search".
+*
+* Auto generated 30-11-2011 14:26
+*
+* Manual updates:
+* Only the data in the array - everything else is removed by next
+* writing. "version" and "dependencies" must not be touched!
+***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Indexed Search Engine',
--- typo3/sysext/wizard_sortpages/ext_emconf.php
+++ typo3/sysext/wizard_sortpages/ext_emconf.php
@@ -1,14 +1,14 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "wizard_sortpages".
-#
-# Auto generated 25-10-2011 13:11
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
+/***************************************************************
+* Extension Manager/Repository config file for ext "wizard_sortpages".
+*
+* Auto generated 25-10-2011 13:11
+*
+* Manual updates:
+* Only the data in the array - everything else is removed by next
+* writing. "version" and "dependencies" must not be touched!
+***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Web>Func, Wizards, Sort pages',
