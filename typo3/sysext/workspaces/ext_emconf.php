5b7f73e0975ddd012d6004a3a3b033d4a2a43106
Roland Waldner <most.wanted@gmx.at>
2012-06-03 18:00:51 +0200

    [TASK] Fix CGL "InlineCommentWrongStyle" in sysext "workspaces"
    There are violations against the CGL rule "InlineCommentWrongStyle".
    
    This will be fixed.
    
    Change-Id: Id54a7c1f71e5df89a1fad762b14c96ea5df6ebcf
    Resolves: #37717
    Releases: 6.0
    Reviewed-on: http://review.typo3.org/11813
    Reviewed-by: Oliver Klee
    Reviewed-by: Susanne Moog
    Tested-by: Susanne Moog

--- typo3/sysext/workspaces/ext_emconf.php
+++ typo3/sysext/workspaces/ext_emconf.php
@@ -1,14 +1,14 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "workspaces".
-#
-# Auto generated 13-03-2012 16:03
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
+/***************************************************************
+* Extension Manager/Repository config file for ext "workspaces".
+*
+* Auto generated 13-03-2012 16:03
+*
+* Manual updates:
+* Only the data in the array - everything else is removed by next
+* writing. "version" and "dependencies" must not be touched!
+***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Workspaces Management',
