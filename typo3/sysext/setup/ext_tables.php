dd8853250c246d5866392e6897c4e4afb74e90a1
Ludwig Rafelsberger <ludwig.rafelsberger@gmx.at>
2012-06-11 22:09:01 +0200

    CGL FunctionCallArgumentSpacingNoSpaceAfterComma in setup
    Fix CGL violations against FunctionCallArgumentSpacingNoSpaceAfterComma
    in /typo3/sysext/setup.
    
    Change-Id: I34e805a53d1e36394bc99526f1ea2567c2cf2bfa
    Resolves: #37917
    Releases: 6.0
    Reviewed-on: http://review.typo3.org/11948
    Reviewed-by: Roland Waldner
    Reviewed-by: Obadja Hauser
    Reviewed-by: Christian Kuhn
    Tested-by: Christian Kuhn

--- typo3/sysext/setup/ext_tables.php
+++ typo3/sysext/setup/ext_tables.php
@@ -1,9 +1,9 @@
 <?php
 if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 
-if (TYPO3_MODE=='BE') {
-	t3lib_extMgm::addModule('user','setup','after:task',t3lib_extMgm::extPath($_EXTKEY).'mod/');
-	t3lib_extMgm::addLLrefForTCAdescr('_MOD_user_setup','EXT:setup/locallang_csh_mod.xml');
+if (TYPO3_MODE == 'BE') {
+	t3lib_extMgm::addModule('user', 'setup', 'after:task', t3lib_extMgm::extPath($_EXTKEY) . 'mod/');
+	t3lib_extMgm::addLLrefForTCAdescr('_MOD_user_setup', 'EXT:setup/locallang_csh_mod.xml');
 }
 
 $GLOBALS['TYPO3_USER_SETTINGS'] = array(
