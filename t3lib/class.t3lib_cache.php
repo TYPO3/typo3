fceaeb76770637f3f2ad361d41431df2e24ff700
Christian Kuhn <lolli@schwarzbu.ch>
2012-06-11 21:16:21 +0200

    [FEATURE] Cache negative ux_* look up in autoloader
    For every class instance that is requested via
    t3lib_div::makeInstance(), a lookup for ux_"classname" is done
    and that class is instantiated if it exists, instead of the original
    class. If no ux_ class is found with class_exists(), the requested class
    is instantiated. This is the basic "XCLASS" handling in the core.
    
    The patch adds autoloader rows for unsuccessful (NULL) ux_* lookups to the
    autoloader cache file. The file is updated during shutdown. This way the
    information that a XCLASS does NOT exist is cached between consecutive calls.
    This leads to a performance improvement and is an advantage over the
    current makeInstance class name cache that worked only for one call.
    The old "cache for one call only" code is dropped with the patch.
    
    Change-Id: I70573146600d658d218dc9b31694748341fc7a20
    Resolves: #37915
    Releases: 6.0

--- t3lib/class.t3lib_cache.php
+++ t3lib/class.t3lib_cache.php
@@ -47,12 +47,10 @@ class t3lib_cache {
 				// New operator used on purpose, makeInstance() is not ready to be used so early in bootstrap
 			$GLOBALS['typo3CacheManager'] = new t3lib_cache_Manager();
 			t3lib_div::setSingletonInstance('t3lib_cache_Manager', $GLOBALS['typo3CacheManager']);
-			t3lib_div::addClassNameToMakeInstanceCache('t3lib_cache_Manager', 't3lib_cache_Manager');
 			$GLOBALS['typo3CacheManager']->setCacheConfigurations($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']);
 				// New operator used on purpose, makeInstance() is not ready to be used so early in bootstrap
 			$GLOBALS['typo3CacheFactory'] = new t3lib_cache_Factory('production', $GLOBALS['typo3CacheManager']);
 			t3lib_div::setSingletonInstance('t3lib_cache_Factory', $GLOBALS['typo3CacheFactory']);
-			t3lib_div::addClassNameToMakeInstanceCache('t3lib_cache_Factory', 't3lib_cache_Factory');
 			self::$isCachingFrameworkInitialized = TRUE;
 		}
 	}
