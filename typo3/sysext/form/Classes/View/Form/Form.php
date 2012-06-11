e2aef485ec368c436e367744c4ae9110f63f9343
Lorenz <lorenz.ulrich@visol.ch>
2012-06-11 16:58:28 +0200

    [FEATURE] Make CSS for sysext form configurable
    This change allows the path to the form and confirmation CSS of sysext
    form to be configured by TypoScript. The default CSS are included with
    the TypoScript template of the extension. Therefore this is a non-
    breaking change.
    
    Override Form.css:
    * tt_content.mailform.20.form.includeCss = path/to/css.css
    * tt_content.mailform.20.form.includeCss > # don't include
    
    Override Confirmation.css
    * tt_content.mailform.20.confirmation.includeCss = path/to/css.css
    * tt_content.mailform.20.confirmation.includeCss > # don't include
    
    Change-Id: Ie2b0c397124f06ec32114983e78dd60b4229ce97
    Fixes: #32480
    Releases: 6.0

--- typo3/sysext/form/Classes/View/Form/Form.php
+++ typo3/sysext/form/Classes/View/Form/Form.php
@@ -76,11 +76,13 @@ class tx_form_View_Form extends tx_form_View_Form_Element_Container {
 	 * @return void
 	 */
 	protected function setCss() {
-		$GLOBALS['TSFE']->additionalHeaderData['tx_form_css'] =
-			'<link rel="stylesheet" type="text/css" href="' .
-			t3lib_extMgm::siteRelPath('form') .
-			'Resources/Public/CSS/Form.css' .
-			'" media="all" />';
+		if (!empty($this->typoscript['form.']['includeCss'])) {
+			$cssFile = $GLOBALS['TSFE']->tmpl->getFileName($this->typoscript['form.']['includeCss']);
+			$GLOBALS['TSFE']->additionalHeaderData['tx_form_css'] =
+				'<link rel="stylesheet" type="text/css" href="' .
+				$cssFile .
+				'" media="all" />';
+		}
 	}
 }
 ?>
