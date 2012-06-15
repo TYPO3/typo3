de4d0e74e7714bae7b5d15edd652200c136b96a3
Susanne Moog <typo3@susannemoog.de>
2012-06-15 14:14:47 +0200

    [FEATURE] Use mod dispatcher for tsconfig_help
    Use the backend module dispatcher for the
    tsconfig_help extension.
    
    Change-Id: I96fbe53025318601b1db113039091be545b6036e
    Resolves: #38080
    Releases: 6.0
    Reviewed-on: http://review.typo3.org/12090
    Reviewed-by: Helmut Hummel
    Tested-by: Helmut Hummel
    Reviewed-by: Christian Kuhn
    Tested-by: Christian Kuhn

--- typo3/sysext/tsconfig_help/mod1/conf.php
+++ typo3/sysext/tsconfig_help/mod1/conf.php
@@ -1,11 +1,8 @@
 <?php
-define('TYPO3_MOD_PATH', 'sysext/tsconfig_help/mod1/');
-$BACK_PATH='../../../';
 $MCONF['name']='help_txtsconfighelpM1';
-
+$MCONF['script'] = '_DISPATCH';
 
 $MCONF['access']='user,group';
-$MCONF['script']='index.php';
 
 $MLANG['default']['tabs_images']['tab'] = 'moduleicon.gif';
 $MLANG['default']['ll_ref']='LLL:EXT:tsconfig_help/mod1/locallang_mod.xml';
