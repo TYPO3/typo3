df41b3043da0b1c1e3c075e0e58341c9465a5e0f
Helmut Hummel <helmut.hummel@typo3.org>
2012-06-15 14:30:14 +0200

    [TASK] Replace duplicate code with a bootstrap method call
    The ext_tables inclusion hook has been moved to a 
    method in the bootstrap class. Use this bootstrap method 
    instead of duplicating the hook code in several places.
    
    Branches: 6.0
    Resolves: #38078
    
    Change-Id: I0713ecf9f18c01b785055222abb3e136c876ece4

--- typo3/sysext/install/updates/class.tx_coreupdates_migrateworkspaces.php
+++ typo3/sysext/install/updates/class.tx_coreupdates_migrateworkspaces.php
@@ -389,15 +389,7 @@ class tx_coreupdates_migrateworkspaces extends tx_coreupdates_installsysexts {
 
 
 			// Hook for postprocessing values set in extTables.php
-		if (is_array($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['GLOBAL']['extTablesInclusion-PostProcessing'])) {
-			foreach ($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['GLOBAL']['extTablesInclusion-PostProcessing'] AS $_classRef) {
-				$hookObject = t3lib_div::getUserObj($_classRef);
-				if (!$hookObject instanceof t3lib_extTables_PostProcessingHook) {
-					throw new UnexpectedValueException('$hookObject must implement interface t3lib_extTables_PostProcessingHook', 1320585902);
-				}
-				$hookObject->processData();
-			}
-		}
+		Typo3_Bootstrap::runExtTablesPostProcessingHooks();
 	}
 
 	/**
