64bfd7d1acd11d7144d4aa58e83346b06bd34955
Susanne Moog <typo3@susannemoog.de>
2012-06-15 20:29:29 +0200

    [FEATURE] Use mod dispatcher for opendocs extension
    Use the backend module dispatcher for the
    opendocs extension.
    
    Added an index.php that redirects to
    alt_doc.php which contains the logic.
    
    Change-Id: I2a939cfe5f48d760718a0ae330abdc0e75cda135
    Resolves: #38102
    Releases: 6.0
    Reviewed-on: http://review.typo3.org/12109
    Reviewed-by: Helmut Hummel
    Tested-by: Helmut Hummel

--- typo3/sysext/opendocs/mod/conf.php
+++ typo3/sysext/opendocs/mod/conf.php
@@ -1,11 +1,8 @@
 <?php
-define('TYPO3_MOD_PATH', 'sysext/opendocs/mod/');
-$BACK_PATH='../../../';
-
+$MCONF['script'] = '_DISPATCH';
 $MLANG['default']['tabs_images']['tab'] = 'document.gif';
 $MLANG['default']['ll_ref'] = 'LLL:EXT:opendocs/mod/locallang_mod.xml';
 
-$MCONF['script'] = $BACK_PATH.'alt_doc.php';
 $MCONF['access'] = 'group,user';
 $MCONF['name'] = 'opendocs';
 
