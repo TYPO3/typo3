0d4b27fda28b83af585bf45807c42d4a3135f6c1
Susanne Moog <typo3@susannemoog.de>
2012-06-15 16:15:11 +0200

    [FEATURE] Use mod dispatcher for cshmanual
    Use the backend module dispatcher for the
    cshmanual extension.
    
    Change-Id: I8333b488f61c30c4c1fe75a2c2e409ce616e216b
    Resolves: #38090
    Releases: 6.0

--- typo3/sysext/cshmanual/mod/conf.php
+++ typo3/sysext/cshmanual/mod/conf.php
@@ -1,11 +1,11 @@
 <?php
 define('TYPO3_MOD_PATH', 'sysext/cshmanual/mod/');
-$BACK_PATH='../../../';
+
+$MCONF['script'] = '_DISPATCH';
 
 $MLANG['default']['tabs_images']['tab'] = 'about.gif';
-$MLANG['default']['ll_ref'] = 'LLL:EXT:lang/locallang_mod_help_cshmanual.xml';
+$MLANG['default']['ll_ref'] = 'LLL:EXT:lang/locallang_mod_help_cshmanual.xlf';
 
 $MCONF['access'] = 'user,group';
-$MCONF['script'] = $BACK_PATH . 'view_help.php';
 $MCONF['name'] = 'help_cshmanual';
 ?>
