5c1fc57840f8de326efe58dffd5807a83d7ec2f4
Susanne Moog <typo3@susannemoog.de>
2012-06-15 22:28:35 +0200

    [FEATURE] Use mod dispatcher for lowlevel extensions
    Use the backend module dispatcher for the
    two modules in the lowlevel extension -
    Configuration and DBCheck.
    
    Change-Id: I492f119b82141f6801d464b76f5de3d5ca3dbeaf
    Resolves: #38105
    Releases: 6.0

--- typo3/sysext/lowlevel/config/conf.php
+++ typo3/sysext/lowlevel/config/conf.php
@@ -1,11 +1,8 @@
 <?php
-define('TYPO3_MOD_PATH', 'sysext/lowlevel/config/');
-$BACK_PATH='../../../';
-
 $MLANG['default']['tabs_images']['tab'] = 'config.gif';
 $MLANG['default']['ll_ref']='LLL:EXT:lowlevel/config/locallang_mod.xml';
 
-$MCONF['script']='index.php';
+$MCONF['script']='_DISPATCH';
 $MCONF['access']='admin';
 $MCONF['name']='tools_config';
 $MCONF['workspaces']='online';
