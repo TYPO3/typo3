80e1daa26eb237424baeab97e8aa0fd5421762d3
Susanne Moog <typo3@susannemoog.de>
2012-06-15 18:48:45 +0200

    [FEATURE] Use mod dispatcher for the perm extension
    Use the backend module dispatcher for the
    perm (Access) extension.
    
    Change-Id: I1b9566d4c37a5ab8221f70b46447786277c98bd2
    Resolves: #38099
    Releases: 6.0
    Reviewed-on: http://review.typo3.org/12105
    Reviewed-by: Oliver Hader
    Tested-by: Oliver Hader
    Reviewed-by: Helmut Hummel
    Tested-by: Helmut Hummel

--- typo3/sysext/perm/mod1/conf.php
+++ typo3/sysext/perm/mod1/conf.php
@@ -1,11 +1,9 @@
 <?php
-define('TYPO3_MOD_PATH', 'sysext/perm/mod1/');
-$BACK_PATH='../../../';
+$MCONF['script']='_DISPATCH';
 
 $MLANG['default']['tabs_images']['tab'] = 'perm.gif';
 $MLANG['default']['ll_ref']='LLL:EXT:lang/locallang_mod_web_perm.xml';
 
-$MCONF['script']='index.php';
 $MCONF['access']='user,group';
 $MCONF['name']='web_perm';
 ?>
