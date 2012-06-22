cfe1743c24622d973599d401f979ad73354dd0e5
Wouter Wolters <typo3@wouterwolters.nl>
2012-06-22 22:54:44 +0200

    [TASK] Clean up phpDoc comments in typo3/sysext/
    Clean up provides:
    
    * Provide correct parameter names in phpDoc
    * Provide correct data types in phpDoc
    * CGLify phpDoc (remove tabs)
    
    Change-Id: Ibf7958460bfac740d74bd22a12c7b9a995d1dbbe
    Resolves: #38321
    Releases: 6.0
    Reviewed-on: http://review.typo3.org/12303
    Reviewed-by: Christian Kuhn
    Tested-by: Christian Kuhn

--- typo3/sysext/taskcenter/ext_autoload.php
+++ typo3/sysext/taskcenter/ext_autoload.php
@@ -1,8 +1,5 @@
 <?php
-/*
- * Register necessary class names with autoloader
- *
- */
+	// Register necessary class names with autoloader
 $extensionPath = t3lib_extMgm::extPath('taskcenter');
 return array (
 	'tx_taskcenter_task' => $extensionPath . 'interfaces/interface.tx_taskcenter_task.php'
