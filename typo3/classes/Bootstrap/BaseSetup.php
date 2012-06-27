f3977582286b396e85bb775faef3a076a63ba1de
TYPO3 Release Team <typo3v4@typo3.org>
2012-06-27 18:07:04 +0200

    [TASK] Set TYPO3 version to 6.0-dev
    Change-Id: I26ba2f65f6b55c03adf3fa77cc9c12e3cf8132b0
    Reviewed-on: http://review.typo3.org/12413
    Reviewed-by: TYPO3 Release Team
    Tested-by: TYPO3 Release Team

--- typo3/classes/Bootstrap/BaseSetup.php
+++ typo3/classes/Bootstrap/BaseSetup.php
@@ -93,7 +93,7 @@ class Typo3_Bootstrap_BaseSetup {
 	 */
 	protected static function defineBaseConstants() {
 			// This version, branch and copyright
-		define('TYPO3_version', '6.0.0alpha2');
+		define('TYPO3_version', '6.0-dev');
 		define('TYPO3_branch', '6.0');
 		define('TYPO3_copyright_year', '1998-2012');
 
