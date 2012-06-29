145eb2cb5d9233a32a37d5ba2ef2884fd05df079
Oliver Hader <oliver@typo3.org>
2012-06-29 18:52:52 +0200

    [BUGFIX] IRRE records can't be expanded without an hidden field
    If no hidden field in TCA for IRRE child records, this will
    lead to JavaScript errors on expanding the accordant child
    record in the TCEforms view.
    
    This regression has been introduced in issue #34303
    
    Change-Id: I9d0317381638d3674012e2d235e4a74dfb787c50
    Fixes: #37615
    Related: #34303
    Releases: 6.0, 4.7, 4.6, 4.5
    Reviewed-on: http://review.typo3.org/12453
    Tested-by: Peter Kraume
    Tested-by: Steffen Ritter
    Reviewed-by: Steffen Ritter

--- t3lib/class.t3lib_tceforms_inline.php
+++ t3lib/class.t3lib_tceforms_inline.php
@@ -394,10 +394,10 @@ class t3lib_TCEforms_inline {
 			} else {
 					// set additional field for processing for saving
 				$fields .= '<input type="hidden" name="' . $this->prependCmdFieldNames . $appendFormFieldNames . '[delete]" value="1" disabled="disabled" />';
-				if(!$isExpanded) {
-					$checked = ($rec['hidden'] == 1) ? ' checked="checked"' : '';
-					$fields .= '<input type="checkbox" name="' . $this->prependFormFieldNames . $appendFormFieldNames . '[hidden]_0" value="1"'.$checked.' />';
-					$fields .= '<input type="input" name="' . $this->prependFormFieldNames . $appendFormFieldNames . '[hidden]" value="'.$rec['hidden'].'" />';
+				if (!$isExpanded && !empty($GLOBALS['TCA'][$foreign_table]['ctrl']['enablecolumns']['disabled'])) {
+					$checked = (!empty($rec['hidden']) ? ' checked="checked"' : '');
+					$fields .= '<input type="checkbox" name="' . $this->prependFormFieldNames . $appendFormFieldNames . '[hidden]_0" value="1"' . $checked . ' />';
+					$fields .= '<input type="input" name="' . $this->prependFormFieldNames . $appendFormFieldNames . '[hidden]" value="' . $rec['hidden'] . '" />';
 				}
 			}
 				// if this record should be shown collapsed
