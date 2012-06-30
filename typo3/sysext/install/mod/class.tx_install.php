dfc12288d35447b2805811dec2f2926dfcd921bd
Philipp Gampe <philipp.gampe@typo3.org>
2012-06-30 16:24:35 +0200

    [BUGFIX] Remove HTML in RuntimeException from sysext 'install'
    It is not possible to use HTML code inside runtime exceptions. This code
    will be htmlspecialchared and printed to the user.
    
    Replace <em></em> with double quotes and remove the <br /> tags
    completely, because exception messages are single line only.
    
    Change-Id: I36fab81586daa4288f3e41c8084c954a47363621
    Fixes: #38472
    Releases: 6.0, 4.7, 4.6, 4.5

--- typo3/sysext/install/mod/class.tx_install.php
+++ typo3/sysext/install/mod/class.tx_install.php
@@ -200,9 +200,9 @@ class tx_install extends t3lib_install {
 				// Check for mandatory PHP modules
 			$missingPhpModules = $this->getMissingPhpModules();
 			if (count($missingPhpModules) > 0) {
-				throw new RuntimeException('TYPO3 Installation Error: The following PHP module(s) is/are missing: <em>' .
+				throw new RuntimeException('TYPO3 Installation Error: The following PHP module(s) is/are missing: "' .
 						implode(', ', $missingPhpModules) .
-						'</em><br /><br />You need to install and enable these modules first to be able to install TYPO3.',
+						'" You need to install and enable these modules first to be able to install TYPO3.',
 					1294587482
 				);
 			}
