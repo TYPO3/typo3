9a7f866ba14ca2af8c8cbb4c50251d2613a1da52
Oliver Hader <oliver@typo3.org>
2012-07-03 21:43:59 +0200

    [BUGFIX] Remove t3ver_swapmode code blocks
    The different ways of versioning (element, page, branch) have
    been defined to element versioning some TYPO3 version ago. With
    TYPO3 4.7, those settings, especially the handling of the field
    t3ver_swapmode has been removed from t3lib_TCEmain.
    
    Since the default value of t3ver_swapmode is not -1 anymore,
    but 0 instead, this will lead to inconsistent side-effects.
    
    The solution is to remove all occurrences.
    
    Change-Id: Iced3f3ee35165f43040d878ee24fea0fe63a3a31
    Fixes: #38607
    Releases: 6.0, 4.7
    Reviewed-on: http://review.typo3.org/12554
    Reviewed-by: Oliver Hader
    Tested-by: Oliver Hader

--- typo3/sysext/workspaces/Classes/Service/GridData.php
+++ typo3/sysext/workspaces/Classes/Service/GridData.php
@@ -74,7 +74,7 @@ class Tx_Workspaces_Service_GridData {
 	/**
 	 * Generates grid list array from given versions.
 	 *
-	 * @param array $versions All records uids etc. First key is table name, second key incremental integer. Records are associative arrays with uid, t3ver_oid and t3ver_swapmode fields. The pid of the online record is found as "livepid" the pid of the offline record is found in "wspid"
+	 * @param array $versions All records uids etc. First key is table name, second key incremental integer. Records are associative arrays with uid and t3ver_oid fields. The pid of the online record is found as "livepid" the pid of the offline record is found in "wspid"
 	 * @param object $parameter Parameters as submitted by JavaScript component
 	 * @param integer $currentWorkspace The current workspace
 	 * @return array Version record information (filtered, sorted and limited)
@@ -249,7 +249,7 @@ class Tx_Workspaces_Service_GridData {
 	/**
 	 * Puts the generated dataArray into the workspace cache.
 	 *
-	 * @param array $versions All records uids etc. First key is table name, second key incremental integer. Records are associative arrays with uid, t3ver_oid and t3ver_swapmode fields. The pid of the online record is found as "livepid" the pid of the offline record is found in "wspid"
+	 * @param array $versions All records uids etc. First key is table name, second key incremental integer. Records are associative arrays with uid and t3ver_oid fields. The pid of the online record is found as "livepid" the pid of the offline record is found in "wspid"
 	 * @param string $filterTxt The given filter text from the grid.
 	 */
 	protected function setDataArrayIntoCache(array $versions, $filterTxt) {
@@ -260,7 +260,7 @@ class Tx_Workspaces_Service_GridData {
 	/**
 	 * Checks if a cache entry is given for given versions and filter text and tries to load the data array from cache.
 	 *
-	 * @param array $versions All records uids etc. First key is table name, second key incremental integer. Records are associative arrays with uid, t3ver_oid and t3ver_swapmode fields. The pid of the online record is found as "livepid" the pid of the offline record is found in "wspid"
+	 * @param array $versions All records uids etc. First key is table name, second key incremental integer. Records are associative arrays with uid and t3ver_oid fields. The pid of the online record is found as "livepid" the pid of the offline record is found in "wspid"
 	 * @param string $filterTxt The given filter text from the grid.
 	 * @return boolean TRUE if cache entry was successfully fetched from cache and content put to $this->dataArray
 	 */
@@ -282,7 +282,7 @@ class Tx_Workspaces_Service_GridData {
 	/**
 	 * Calculates the hash value of the used workspace, the user id, the versions array, the filter text, the sorting attribute, the workspace selected in grid and the sorting direction.
 	 *
-	 * @param array $versions All records uids etc. First key is table name, second key incremental integer. Records are associative arrays with uid, t3ver_oid and t3ver_swapmode fields. The pid of the online record is found as "livepid" the pid of the offline record is found in "wspid"
+	 * @param array $versions All records uids etc. First key is table name, second key incremental integer. Records are associative arrays with uid and t3ver_oid fields. The pid of the online record is found as "livepid" the pid of the offline record is found in "wspid"
 	 * @param string $filterTxt The given filter text from the grid.
 	 * @return string
 	 */
--- typo3/sysext/workspaces/Classes/Service/Workspaces.php
+++ typo3/sysext/workspaces/Classes/Service/Workspaces.php
@@ -191,7 +191,7 @@ class Tx_Workspaces_Service_Workspaces implements t3lib_Singleton {
 	 * @param	integer		Page id: Live page for which to find versions in workspace!
 	 * @param	integer		Recursion Level - select versions recursive - parameter is only relevant if $pageId != -1
 	 * @param	string		How to collect records for "listing" or "modify" these tables. Support the permissions of each type of record (@see t3lib_userAuthGroup::check).
-	 * @return	array		Array of all records uids etc. First key is table name, second key incremental integer. Records are associative arrays with uid, t3ver_oid and t3ver_swapmode fields. The pid of the online record is found as "livepid" the pid of the offline record is found in "wspid"
+	 * @return	array		Array of all records uids etc. First key is table name, second key incremental integer. Records are associative arrays with uid and t3ver_oidfields. The pid of the online record is found as "livepid" the pid of the offline record is found in "wspid"
 	 */
 	public function selectVersionsInWorkspace($wsid, $filter = 0, $stage = -99, $pageId = -1, $recursionLevel = 0, $selectionType = 'tables_select') {
 
@@ -255,7 +255,7 @@ class Tx_Workspaces_Service_Workspaces implements t3lib_Singleton {
 	 */
 	protected function selectAllVersionsFromPages($table, $pageList, $wsid, $filter, $stage) {
 
-		$fields = 'A.uid, A.t3ver_oid, A.t3ver_stage, ' . ($table==='pages' ? ' A.t3ver_swapmode,' : '') . 'B.pid AS wspid, B.pid AS livepid';
+		$fields = 'A.uid, A.t3ver_oid, A.t3ver_stage, B.pid AS wspid, B.pid AS livepid';
 		if (t3lib_BEfunc::isTableLocalizable($table)) {
 			$fields .= ', A.' . $GLOBALS['TCA'][$table]['ctrl']['languageField'];
 		}
