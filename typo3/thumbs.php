2ee4afe243a5bdef6f6b0bcf746bfeb85ad0955e
Ivan Kartolo <ivan.kartolo@dkd.de>
2012-07-04 16:36:45 +0200

    [BUGFIX] E_DEPRECATED does not exist in PHP 5.2
    Because TYPO3 4.5.x should also work on PHP 5.2, the E_DEPRECATED constant
    must be removed, since it produces notices.
    
    Change-Id: Idb0e7ffb327a44088b966e04008d8efb03555b3b
    Fixes: #38645
    Releases: 4.5

--- typo3/thumbs.php
+++ typo3/thumbs.php
@@ -36,7 +36,11 @@
 // Set error reporting
 // *******************************
 
-error_reporting(E_ALL & ~(E_STRICT | E_NOTICE | E_DEPRECATED));
+if (defined('E_DEPRECATED')) {
+	error_reporting(E_ALL & ~(E_STRICT | E_NOTICE | E_DEPRECATED));
+} else {
+	error_reporting(E_ALL ^ E_NOTICE);
+}
 
 define('PATH_thisScript', str_replace('//', '/', str_replace('\\', '/',
 	(PHP_SAPI == 'fpm-fcgi' || PHP_SAPI == 'cgi' || PHP_SAPI == 'isapi' || PHP_SAPI == 'cgi-fcgi') &&
