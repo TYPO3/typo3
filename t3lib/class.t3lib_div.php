51c639ae2a2f5311f4ccd295745cfd31faa15245
Benjamin Mack <benni@typo3.org>
2012-07-06 10:58:54 +0200

    [BUG] t3lib_div::getFilesInDir order differs from order in File list
    Files with uppercase letters are sorted before all lowercase file
    names due to a bug in getFilesInDir(). This behavior is seen in
    the browse_links function, but not in the general file list module.
    The fix does the sorting independently from the uppercase
    characters.
    
    Change-Id: I4bdacfe1644f2ac32b5b9173fa63ab6a11c78c07
    Resolves: #18771
    Releases: 4.5, 4.6, 4.7, 6.0

--- t3lib/class.t3lib_div.php
+++ t3lib/class.t3lib_div.php
@@ -3455,7 +3455,7 @@ final class t3lib_div {
 								$sortarray[$key] = filemtime($path . '/' . $entry);
 							}
 							elseif ($order) {
-								$sortarray[$key] = $entry;
+								$sortarray[$key] = strtolower($entry);
 							}
 						}
 					}
