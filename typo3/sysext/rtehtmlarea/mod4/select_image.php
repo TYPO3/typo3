9d0dd111187ab9bea2b9275fd32f8955e7d5a628
Philipp Gampe <philipp.gampe@typo3.org>
2012-07-07 17:14:45 +0200

    [BUGFIX] Exclude E_STRICT from errors with PHP 5.4
    The main entry points have been refactored, but some places have been
    forgotten leading to E_STRICT errors flooding the log.
    
    Refactor all places to use the same code:
    error_reporting(E_ALL & ~(E_STRICT | E_NOTICE | E_DEPRECATED));
    
    Change-Id: I5ba40bcefd236d77b21cdb545685bcae40e60f1f
    Related: #35154
    Fixes: #38691
    Releases: 6.0

--- typo3/sysext/rtehtmlarea/mod4/select_image.php
+++ typo3/sysext/rtehtmlarea/mod4/select_image.php
@@ -32,7 +32,7 @@
  * @author	Stanislas Rolland <typo3(arobas)jbr.ca>
  */
 
-error_reporting(E_ALL & ~(E_STRICT | E_NOTICE));
+error_reporting(E_ALL & ~(E_STRICT | E_NOTICE | E_DEPRECATED));
 unset($MCONF);
 require('conf.php');
 require($BACK_PATH.'init.php');
