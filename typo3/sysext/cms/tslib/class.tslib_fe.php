b0e7f9b7cd26ad6c0c7bdb92bae07eeda362da3b
Ingo Renner <ingo@typo3.org>
2012-07-07 17:16:21 +0200

    [FEATURE] Add a hook to pre-process TSFE determineId()
    Just like there is a hook to post-process determineID(), there
    should be a hook to pre-process the determination of the page ID.
    
    This should be useful to extract f.e. page preview handling.
    
    Change-Id: I17a2d15c5c7488a4ead5b88cfeb7a365cb124be1
    Resolves: #38725
    Releases: 6.0

--- typo3/sysext/cms/tslib/class.tslib_fe.php
+++ typo3/sysext/cms/tslib/class.tslib_fe.php
@@ -742,6 +742,13 @@ class tslib_fe {
 	 */
 	function determineId() {
 
+			// Call pre processing function for id determination
+		if (is_array($this->TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_fe.php']['determineId-PreProcessing'])) {
+			foreach ($this->TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_fe.php']['determineId-PreProcessing'] as $functionReference) {
+				t3lib_div::callUserFunction($functionReference, $this);
+			}
+		}
+
 			// Getting ARG-v values if some
 		$this->setIDfromArgV();
 
