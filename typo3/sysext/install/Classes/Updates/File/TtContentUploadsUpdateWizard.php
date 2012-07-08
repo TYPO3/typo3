8bf92c1a2ff5ec0c39bf65b4379f41ef00d17e69
Ingmar Schlecht <ingmar@typo3.org>
2012-07-08 11:49:22 +0200

    [BUGFIX] Clean up FAL update wizards
    Various small fixes to FAL update wizards.
    
    Change-Id: I84acfedcc673064b3f7842a8409ec99bda7c7af9
    Releases: 6.0
    Resolves: #38749

--- typo3/sysext/install/Classes/Updates/File/TtContentUploadsUpdateWizard.php
+++ typo3/sysext/install/Classes/Updates/File/TtContentUploadsUpdateWizard.php
@@ -31,7 +31,7 @@
  * @license     http://www.gnu.org/copyleft/gpl.html
  */
 class Tx_Install_Updates_File_TtContentUploadsUpdateWizard extends Tx_Install_Updates_Base {
-	const FOLDER_ContentUploads = 'content_uploads';
+	const FOLDER_ContentUploads = '_migrated/content_uploads';
 
 	/**
 	 * @var string
@@ -84,7 +84,7 @@ class Tx_Install_Updates_File_TtContentUploadsUpdateWizard extends Tx_Install_Up
 		}
 
 		if (!isset($this->storage)) {
-			throw new RuntimeException('Local default storage could not be initialized - migth be due to missing sys_file* tables.');
+			throw new RuntimeException('Local default storage could not be initialized - might be due to missing sys_file* tables.');
 		}
 
 		$this->fileFactory = t3lib_div::makeInstance('t3lib_file_Factory');
@@ -110,8 +110,8 @@ class Tx_Install_Updates_File_TtContentUploadsUpdateWizard extends Tx_Install_Up
 			"media <> '' AND CAST(CAST(media AS DECIMAL) AS CHAR) <> media OR (CType = 'uploads' AND select_key != '')"	// include also deleted, as they might be undeleted
 		);
 		if ($notMigratedRowsCount > 0) {
-			$description = 'There are Content Elements of type "upload" which are referencing files,' .
-				' not using FAL. The Wizard will move the files to fileadmin/content_uploads/ and index them.';
+			$description = 'There are Content Elements of type "upload" which are referencing files that are not using ' .
+				' the File Abstraction Layer. This wizard will move the files to fileadmin/' . self::FOLDER_ContentUploads . ' and index them.';
 			$updateNeeded = TRUE;
 		}
 
@@ -184,7 +184,7 @@ class Tx_Install_Updates_File_TtContentUploadsUpdateWizard extends Tx_Install_Up
 					$this->targetDirectory . $file
 				);
 
-				$fileObject = $this->storage->getFile('content_uploads/' . $file);
+				$fileObject = $this->storage->getFile(self::FOLDER_ContentUploads . '/' . $file);
 				$this->fileRepository->addToIndex($fileObject);
 				
 				$dataArray = array(
