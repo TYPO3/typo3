443a60fbaf593c03b5f061f056ec3849e38b352c
Fabien Udriot <fabien.udriot@ecodev.ch>
2012-07-14 12:32:40 +0200

    [TASK] Add Unit Test for Static File Collection
    Add two first tests for the Static File Collection
    
    Change-Id: Ib2ecc4f93171a5033b9bf44dade9e492c7014bc8
    Resolves: #38952
    Releases: 6.0

---
+++ tests/Unit/t3lib/file/Collection/StaticFileCollectionTest.php
@@ -0,0 +1,132 @@
+<?php
+
+/**
+ * Test case for t3lib_file_Collection_StaticFileCollectionTest
+ *
+ * @author Fabien Udriot <fabien.udriot@typo3.org>
+ * @package TYPO3
+ * @subpackage t3lib
+ */
+class t3lib_file_Collection_StaticFileCollectionTest extends Tx_Phpunit_TestCase {
+
+	/**
+	 * @var int
+	 */
+	private $mockCollectionUid = 0;
+
+	/**
+	 * @var int
+	 */
+	private $mockFileUid = 0;
+
+	/**
+	 * @var Tx_Phpunit_Framework
+	 */
+	private $testingFramework;
+
+	/**
+	 * @var array
+	 */
+	private $tables = array('sys_file_collection', 'sys_file', 'sys_file_reference');
+
+	/**
+	 * @var t3lib_DB
+	 */
+	protected $database;
+
+	public function setUp() {
+		$this->database = $GLOBALS['TYPO3_DB'];
+
+		$this->prepareTables();
+		$this->testingFramework = new Tx_Phpunit_Framework('sys_file');
+	}
+
+	public function tearDown() {
+		$this->testingFramework->cleanUp();
+		unset($this->testingFramework);
+		$this->cleanUpTables();
+	}
+
+	/**
+	 * @test
+	 */
+	public function canLoadFileCollection() {
+
+		$this->createDummyRecords();
+
+		/** @var $collection t3lib_category_Collection_CategoryCollection */
+		$collection = t3lib_file_Collection_StaticFileCollection::load($this->mockCollectionUid, TRUE);
+		$this->assertEquals('1', $collection->count());
+
+	}
+
+	/**
+	 * @test
+	 */
+	public function canCreateFileCollection() {
+		$collectionRecord = array(
+			'uid' => 0,
+			'title' => uniqid('title'),
+			'description' => uniqid('description'),
+			'table_name' => 'items'
+		);
+		$collection = t3lib_file_Collection_StaticFileCollection::create($collectionRecord);
+		$this->assertInstanceOf('t3lib_file_Collection_StaticFileCollection', $collection);
+	}
+
+	/**
+	 * Create dummy records
+	 */
+	private function createDummyRecords() {
+
+		// Creating a new file collection
+		$values = array(
+			'title' => uniqid('title'),
+			'type' => 'static',
+			'files' => '1',
+		);
+		$this->mockCollectionUid = $this->testingFramework->createRecord('sys_file_collection', $values);
+
+		// Creating a new file record
+		$values = array(
+			'identifier' => uniqid('identifier'),
+			'name' => uniqid('name'),
+		);
+		$this->mockFileUid = $this->testingFramework->createRecord('sys_file', $values);
+
+		// Creating a new file reference
+		$values = array(
+			'uid_local' => $this->mockFileUid,
+			'uid_foreign' => $this->mockCollectionUid,
+			'tablenames' => 'sys_file_collection',
+			'fieldname' => 'files'
+		);
+		$this->testingFramework->createRecord('sys_file_reference', $values);
+	}
+
+	/**
+	 * Add is_dummy_record record and create dummy record
+	 */
+	private function prepareTables() {
+		$sql = 'ALTER TABLE %s ADD is_dummy_record tinyint(1) unsigned DEFAULT \'0\' NOT NULL';
+
+		foreach ($this->tables as $table) {
+			$_sql = sprintf($sql, $table);
+			$this->database->sql_query($_sql);
+		}
+	}
+
+	/**
+	 * Add is_dummy_record record and create dummy record
+	 */
+	private function cleanUpTables() {
+		$sql = 'ALTER TABLE %s DROP COLUMN is_dummy_record';
+
+		foreach ($this->tables as $table) {
+			$_sql = sprintf($sql, $table);
+			$this->database->sql_query($_sql);
+		}
+	}
+}
+
+?>
