27ca63675334a97f076a9c20499034e969724bd7
Marcus Schwemer <ms@schwemer.de>
2012-07-20 00:35:37 +0200

    [BUGFIX] Clear Language Cache Configuration broken
    The three small issues are:
    
    * Wrong option naming in UserTSconfig "options.clearCache.clearRTECache"
    => Should be "options.clearCache.clearLangCache"
    
    * "Clear cache" Icon next to the Username (Thunderstorm Icon) is not
    shown, if clearLangCache (aka clearRTECache) is the only option to
    delete a cache.
    => resolved with renaming the option
    
    * Configuration is inconsistent
    ** 2 config values need to be set to enable clear language cache
    for non-admins
    ** "Clear Lang Cache" is only included in "Clear All Caches" if it
    is not enabled in localconf.php
    => Remove $GLOBALS['TYPO3_CONF_VARS']['SYS']['lang']['cache']
    ['clear_menu'] from ext_localconf.php
    
    Fixes: #39103
    Releases: 6.0, 4.7, 4.6
    
    Change-Id: I701dc9a879de4f3681f3fb441545eaa6b3e08e3e

--- typo3/sysext/lang/ext_localconf.php
+++ typo3/sysext/lang/ext_localconf.php
@@ -12,13 +12,10 @@ if (!isset($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][
 	$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['t3lib_l10n']['backend'] = 't3lib_cache_backend_FileBackend';
 }
 
-if (isset($GLOBALS['TYPO3_CONF_VARS']['SYS']['lang']['cache']['clear_menu']) && $GLOBALS['TYPO3_CONF_VARS']['SYS']['lang']['cache']['clear_menu']) {
-		// Register Clear Cache Menu hook
-	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['additionalBackendItems']['cacheActions']['clearLangCache'] = 'EXT:lang/hooks/clearcache/class.tx_lang_clearcachemenu.php:&tx_lang_clearcachemenu';
-} else {
-		// Clear l10n cache when the user clears all caches
-	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearCachePostProc']['clearLangCache'] = 'EXT:lang/hooks/clearcache/class.tx_lang_clearcache.php:tx_lang_clearcache->clearCache';
-}
+	// Register Clear Cache Menu hook
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['additionalBackendItems']['cacheActions']['clearLangCache'] = 'EXT:lang/hooks/clearcache/class.tx_lang_clearcachemenu.php:&tx_lang_clearcachemenu';
+	// Clear l10n cache when the user clears all caches
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearCachePostProc']['clearLangCache'] = 'EXT:lang/hooks/clearcache/class.tx_lang_clearcache.php:tx_lang_clearcache->clearCache';
 
 	// Register Ajax call
 $GLOBALS['TYPO3_CONF_VARS']['BE']['AJAX']['lang::clearCache'] = 'EXT:lang/hooks/clearcache/class.tx_lang_clearcache.php:tx_lang_clearcache->clearCache';
