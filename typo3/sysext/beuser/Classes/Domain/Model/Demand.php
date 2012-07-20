bea4c51264e5dc23e4854e4ae10e344401001c70
Felix Kopp <felix-source@phorax.com>
2012-07-20 19:23:34 +0200

    [FEATURE] Migrate EXT: beuser to Extbase
    This patchset includes a new backend user administration.
    
    Based on extbase a model for BackendUsers is included.
    All users can be listed and the listing can be filtered by
    * username
    * admin/non-admin
    * active/inactive
    * ever logged in.
    
    The user listing includes the last login column also the
    limit of 1.000 users was removed and pagination integrated.
    
    Selected users can be compared in a table view.
    All active backend user sessions are listed - can be terminated.
    
    Of course the SU/change user feature was migrated.
    Clickmenu was widely integrated to allow editing right away.
    
    Change-Id: I32569c848581a49b3948e093d717dad50e973c5d
    Resolves: #39141
    Resolves: #5288
    Resolves: #37949
    Resolves: #33427
    Resolves: #18602
    Resolves: #15494
    Releases: 6.0

---
+++ typo3/sysext/beuser/Classes/Domain/Model/Demand.php
@@ -0,0 +1,150 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2012 Felix Kopp <felix-source@phorax.com>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *  A copy is found in the textfile GPL.txt and important notices to the license
+ *  from the author is found in LICENSE.txt distributed with these scripts.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * Demand filter for listings
+ *
+ * @author Felix Kopp <felix-source@phorax.com>
+ * @package TYPO3
+ * @subpackage beuser
+ */
+class Tx_Beuser_Domain_Model_Demand extends Tx_Extbase_DomainObject_AbstractEntity {
+
+	/**
+	 * @var integer
+	 */
+	const ALL = 0;
+
+	/**
+	 * @var integer
+	 */
+	const USERTYPE_ADMINONLY = 1;
+	/**
+	 * @var integer
+	 */
+	const USERTYPE_USERONLY = 2;
+
+	/**
+	 * @var integer
+	 */
+	const STATUS_ACTIVE = 1;
+	/**
+	 * @var integer
+	 */
+	const STATUS_INACTIVE = 2;
+
+	/**
+	 * @var integer
+	 */
+	const LOGIN_SOME = 1;
+	/**
+	 * @var integer
+	 */
+	const LOGIN_NONE = 2;
+
+	/**
+	 * @var string
+	 */
+	protected $username = '';
+
+	/**
+	 * @var integer
+	 */
+	protected $usertype = self::ALL;
+
+	/**
+	 * @var integer
+	 */
+	protected $status = self::ALL;
+
+	/**
+	 * @var integer
+	 */
+	protected $logins = 0;
+
+	/**
+	 * @param string $username
+	 * @return void
+	 */
+	public function setUsername($username) {
+		$this->username = $username;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getUsername() {
+		return $this->username;
+	}
+
+	/**
+	 * @param int $usertype
+	 * @return void
+	 */
+	public function setUsertype($usertype) {
+		$this->usertype = $usertype;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getUsertype() {
+		return $this->usertype;
+	}
+
+	/**
+	 * @param int $status
+	 * @return void
+	 */
+	public function setStatus($status) {
+		$this->status = $status;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getStatus() {
+		return $this->status;
+	}
+
+	/**
+	 * @param int $logins
+	 * @return void
+	 */
+	public function setLogins($logins) {
+		$this->logins = $logins;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getLogins() {
+		return $this->logins;
+	}
+
+}
+
+?>
