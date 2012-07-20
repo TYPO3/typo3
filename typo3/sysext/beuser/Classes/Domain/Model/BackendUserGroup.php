c79836938a44ff432a34591a9508c51784b8d407
Felix Kopp <felix-source@phorax.com>
2012-07-20 19:23:34 +0200

    [FEATURE] Migrate EXT: beuser to Extbase
    This patchset includes a new backend user administration.
    
    Based on extbase a model for BackendUsers is included.
    All users can be listed and the listing can be filtered by
    * username
    * usergroup
    * admin/non-admin
    * active/inactive
    * ever logged in.
    
    The user listing includes the last login column also the
    limit of 1.000 users was removed and pagination integrated.
    
    Selected users can be compared in a table view.
    All active backend user sessions are listed - can be terminated.
    
    Of course the SU/change user feature was migrated.
    Clickmenu was widely integrated to allow editing right away.
    
    Change-Id: I32569c848581a49b3948e093d717dad50e973c5d
    Resolves: #39141
    Resolves: #5288
    Resolves: #37949
    Resolves: #33427
    Resolves: #18602
    Resolves: #15494
    Releases: 6.0

---
+++ typo3/sysext/beuser/Classes/Domain/Model/BackendUserGroup.php
@@ -0,0 +1,77 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2012 Felix Kopp <felix-source@phorax.com>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *  A copy is found in the textfile GPL.txt and important notices to the license
+ *  from the author is found in LICENSE.txt distributed with these scripts.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * Model for backend user group
+ *
+ * @author Felix Kopp <felix-source@phorax.com>
+ * @package TYPO3
+ * @subpackage beuser
+ */
+class Tx_Beuser_Domain_Model_BackendUserGroup extends Tx_Extbase_DomainObject_AbstractEntity {
+
+	/**
+	 * @var string
+	 */
+	protected $title;
+
+	/**
+	 * @var Tx_Extbase_Persistence_ObjectStorage<Tx_Beuser_Domain_Model_BackendUserGroup>
+	 * @lazy
+	 */
+	protected $subGroups;
+
+	/**
+	 * @param string $title
+	 */
+	public function setTitle($title) {
+		$this->title = $title;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getTitle() {
+		return $this->title;
+	}
+
+	/**
+	 * @param \Tx_Extbase_Persistence_ObjectStorage $subGroups
+	 */
+	public function setSubGroups($subGroups) {
+		$this->subGroups = $subGroups;
+	}
+
+	/**
+	 * @return \Tx_Extbase_Persistence_ObjectStorage
+	 */
+	public function getSubGroups() {
+		return $this->subGroups;
+	}
+
+}
+
+?>
