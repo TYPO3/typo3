8f03f5c6a80a9434893f892d610bb3f6eb5ffc1c
Markus Günther <mail@markus-guenther.de>
2012-07-21 18:34:24 +0200

    [FEATURE] Add a BackendUser model
    Some extensions in the TYPO3 backend use backend users. The belog and
    the beuser module are based on extbase and it will be useful if not
    every extension has to implement an own backendUser model.
    
    This patch adds a backend user model to extbase.
    
    Change-Id: I84e8ea528c5f91151369f59db6a00acb0174544b
    Resolves: #39145
    Releases: 6.0
    Reviewed-on: http://review.typo3.org/12981
    Reviewed-by: Nicole Cordes
    Tested-by: Nicole Cordes
    Reviewed-by: Oliver Klee
    Tested-by: Oliver Klee
    Reviewed-by: Christian Kuhn
    Tested-by: Christian Kuhn

---
+++ typo3/sysext/extbase/Tests/Unit/Domain/Repository/BackendUserRepositoryTest.php
@@ -0,0 +1,72 @@
+<?php
+/***************************************************************
+ * Copyright notice
+ *
+ * (c) 2012 Felix Kopp <felix-source@phorax.com>
+ * All rights reserved
+ *
+ * This script is part of the TYPO3 project. The TYPO3 project is
+ * free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * The GNU General Public License can be found at
+ * http://www.gnu.org/copyleft/gpl.html.
+ *
+ * This script is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * Testcase for Tx_Extbase_Domain_Repository_BackendUserRepository.
+ *
+ * @author Markus Günther <mail@markus-guenther.de>
+ * @author Oliver Klee <typo3-coding@oliverklee.de>
+ *
+ * @package Extbase
+ * @subpackage Domain\Repository
+ * @api
+ */
+class Tx_Extbase_Tests_Unit_Domain_Repository_BackendUserRepositoryTest extends Tx_Extbase_Tests_Unit_BaseTestCase {
+
+	/**
+	 * @test
+	 */
+	public function initializeObjectSetsRespectStoragePidToFalse() {
+		$objectManager = $this->getMock('Tx_Extbase_Object_ObjectManagerInterface');
+		$fixture = new Tx_Extbase_Domain_Repository_BackendUserRepository($objectManager);
+
+		$querySettings = $this->getMock('Tx_Extbase_Persistence_Typo3QuerySettings');
+		$querySettings->expects($this->once())->method('setRespectStoragePage')->with(FALSE);
+		$objectManager->expects($this->once())->method('create')
+			->with('Tx_Extbase_Persistence_Typo3QuerySettings')->will($this->returnValue($querySettings));
+
+		$fixture->initializeObject();
+	}
+
+	/**
+	 * @test
+	 */
+	public function initializeObjectSetsDefaultQuerySettings() {
+		$objectManager = $this->getMock('Tx_Extbase_Object_ObjectManagerInterface');
+		/** @var $fixture Tx_Extbase_Domain_Repository_BackendUserRepository */
+		$fixture = $this->getMock(
+			'Tx_Extbase_Domain_Repository_BackendUserRepository',
+			array('setDefaultQuerySettings'), array($objectManager)
+		);
+
+		$querySettings = $this->getMock('Tx_Extbase_Persistence_Typo3QuerySettings');
+		$objectManager->expects($this->once())->method('create')
+			->with('Tx_Extbase_Persistence_Typo3QuerySettings')->will($this->returnValue($querySettings));
+
+		$fixture->expects($this->once())->method('setDefaultQuerySettings')->with($querySettings);
+
+		$fixture->initializeObject();
+	}
+}
+?>
