2a48873633762ce5a21792e9d5982b078c20c68b
Georg Ringer <mail@ringerge.org>
2012-07-24 16:30:02 +0200

    [TASK] Improve sys_note code
    Code of sys_note can be highly improved:
    * Use fluid
    * Move xlf files to Resources
    * Add kind-of repository
    
    Change-Id: Ia89acb29ddbc0919901570f6bdd1575376863c4a
    Resolves: #39221
    Releases: 6.0
    Reviewed-on: http://review.typo3.org/13021
    Reviewed-by: Wouter Wolters
    Tested-by: Wouter Wolters
    Reviewed-by: Christian Kuhn
    Tested-by: Christian Kuhn

--- typo3/sysext/sys_note/Classes/SysNote.php
+++ typo3/sysext/sys_note/Classes/SysNote.php
@@ -38,77 +38,37 @@ class Tx_SysNote_SysNote {
 	 * @return string
 	 */
 	public function renderByPid($pidList) {
-			// Create query for selecting the notes:
-		$rows = $GLOBALS['TYPO3_DB']->exec_SELECTgetRows(
-					'*',
-					'sys_note',
-					'pid IN (' . $GLOBALS['TYPO3_DB']->cleanIntList($pidList) . ')
-						AND (personal=0 OR cruser=' . intval($GLOBALS['BE_USER']->user['uid']) . ')' .
-						t3lib_BEfunc::deleteClause('sys_note'),
-					'',
-					'sorting'
-				);
+		/** @var $repository Tx_SysNote_Domain_Repository_SysNoteRepository */
+		$repository = t3lib_div::makeInstance('Tx_SysNote_Domain_Repository_SysNoteRepository');
+
+		$notes = $repository->findAllByPidList($pidList);
 
 		$out = '';
+		if ($this->notesAvailable($notes)) {
+			/** @var $fluidView Tx_Fluid_View_StandaloneView */
+			$fluidView = t3lib_div::makeInstance('Tx_Fluid_View_StandaloneView');
+			$templatePathAndFilename = t3lib_extMgm::extPath('sys_note', 'Resources/Private/Template/List.html');
 
-		if (count($rows) > 0) {
-			$categories = array();
-
-				// Load full table description:
-			t3lib_div::loadTCA('sys_note');
-
-				// Traverse categories
-			if ($GLOBALS['TCA']['sys_note'] && $GLOBALS['TCA']['sys_note']['columns']['category']
-				&& is_array($GLOBALS['TCA']['sys_note']['columns']['category']['config']['items'])
-			) {
-				foreach ($GLOBALS['TCA']['sys_note']['columns']['category']['config']['items'] as $el) {
-					$categories[$el[1]] = $GLOBALS['LANG']->sL($el[0]);
-				}
-			}
-
-				// For each note found, make rendering:
-			foreach ($rows as $row) {
-				if ($row['personal'] == 1 && (int) $row['cruser'] !== (int) $GLOBALS['BE_USER']->user['uid'] && $GLOBALS['BE_USER']->isAdmin() === FALSE) {
-					continue;
-				}
-				$author = t3lib_BEfunc::getRecord('be_users', $row['cruser']);
-				$authorInformation = $author['realName'];
-				if (empty($authorInformation)) {
-					$authorInformation = $author['username'];
-				}
-				$title = !(empty($categories[$row['category']])) ? $categories[$row['category']] . ': ' : '';
-
-				$headerParts = array();
-				if (!empty($authorInformation)) {
-					$headerParts[] = '<span>' . $GLOBALS['LANG']->sL('LLL:EXT:lang/locallang_core.xlf:labels.author', TRUE) . '</span> ' . $authorInformation;
-				}
-				$headerParts[] = '<span>' . $GLOBALS['LANG']->sL('LLL:EXT:sys_note/locallang.xlf:date', TRUE) . ':</span> ' . t3lib_BEfunc::date($row['tstamp']);
-				if ($row['personal'] == 1) {
-					$headerParts[] = '<span>' . rtrim($GLOBALS['LANG']->sL('LLL:EXT:sys_note/locallang_tca.xlf:sys_note.personal', TRUE), ':') . '</span>';
-				}
-
-					// Compile content:
-				$out .= '
-					<div class="single-note category-' . $row['category'] . '">
-						<div class="header">
-							 ' . implode(' &middot; ', $headerParts) . '
-							<div class="right">
-								<a href="#" onclick="' . htmlspecialchars(t3lib_BEfunc::editOnClick('&edit[sys_note][' . $row['uid'] . ']=edit', $GLOBALS['BACK_PATH'])) . '">' .
-									t3lib_iconWorks::getSpriteIcon('actions-document-open') .
-								'</a>
-							</div>
-						</div>
-						<div class="content">
-							<div class="title">' . htmlspecialchars($title)  . htmlspecialchars($row['subject']) . '</div>
-							' . nl2br(htmlspecialchars($row['message'])) . '
-						</div>
-					</div>';
-			}
-
-			$out = '<div id="typo3-dblist-sysnotes">' . $out . '</div>';
+			$fluidView->setTemplatePathAndFilename($templatePathAndFilename);
+			$fluidView->assign('notes', $notes);
+			$out = $fluidView->render();
 		}
 
 		return $out;
+	}
+
+	/**
+	 * Check if notes are available
+	 *
+	 * @param array $notes All notes returned from repository
+	 * @return boolean TRUE if there are one or more notes
+	 */
+	protected function notesAvailable(array $notes) {
+		$notesAvailable = FALSE;
+		if (count($notes) > 0) {
+			$notesAvailable = TRUE;
+		}
+		return $notesAvailable;
 	}
 }
 
