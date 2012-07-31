c371f7f85247b65a448146129e608ef628a3dfbd
Georg Ringer <mail@ringerge.org>
2012-07-31 18:48:29 +0200

    [TASK] Reports module based on extbase
    The reports module can be done with extbase
    
    Change-Id: I6f40dca7d3d5dc84af0788a573f1caa669d773ec
    Resolves: #39427
    Releases: 6.0

---
+++ typo3/sysext/reports/Classes/ViewHelpers/ActionMenuItemViewHelper.php
@@ -0,0 +1,71 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2012 Georg Ringer <typo3@ringerge.org>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * Render an item of the menu
+ *
+ * @package TYPO3
+ * @subpackage tx_reports
+ */
+class Tx_Reports_ViewHelpers_ActionMenuItemViewHelper extends Tx_Fluid_Core_ViewHelper_AbstractTagBasedViewHelper {
+
+	/**
+	 * @var string
+	 */
+	protected $tagName = 'option';
+
+	/**
+	 * Renders an ActionMenu option tag
+	 *
+	 * @param string $label label of the option tag
+	 * @param string $controller controller to be associated with this ActionMenuItem
+	 * @param string $action the action to be associated with this ActionMenuItem
+	 * @param array $arguments additional controller arguments to be passed to the action when this ActionMenuItem is selected
+	 * @return string the rendered option tag
+	 * @see Tx_Fluid_ViewHelpers_Be_Menus_ActionMenuViewHelper
+	 */
+	public function render($label, $controller, $action, array $arguments = array()) {
+		$uriBuilder = $this->controllerContext->getUriBuilder();
+		$uri = $uriBuilder
+			->reset()
+			->uriFor($action, $arguments, $controller);
+		$this->tag->addAttribute('value', $uri);
+
+		$currentRequest = $this->controllerContext->getRequest();
+		$currentController = $currentRequest->getControllerName();
+		$currentAction = $currentRequest->getControllerActionName();
+		$currentArguments = $currentRequest->getArguments();
+
+		unset($currentArguments['action']);
+		unset($currentArguments['controller']);
+		if ($action === $currentAction && $controller === $currentController && $currentArguments === $arguments) {
+			$this->tag->addAttribute('selected', 'selected');
+		}
+
+		$this->tag->setContent($label);
+
+		return $this->tag->render();
+	}
+}
+?>
