df931cf62c02727ae3d43eb80a1882d4ce91c483
Georg Ringer <mail@ringerge.org>
2012-07-31 18:48:29 +0200

    [TASK] Reports module based on extbase
    The reports module can be done with extbase
    
    Change-Id: I6f40dca7d3d5dc84af0788a573f1caa669d773ec
    Resolves: #39427
    Releases: 6.0
    Reviewed-on: http://review.typo3.org/13418
    Reviewed-by: Wouter Wolters
    Tested-by: Wouter Wolters
    Reviewed-by: Christian Kuhn
    Tested-by: Christian Kuhn

--- typo3/sysext/reports/ext_autoload.php
+++ typo3/sysext/reports/ext_autoload.php
@@ -6,7 +6,6 @@ $extensionPath = t3lib_extMgm::extPath('reports');
 return array(
 	'tx_reports_statusprovider' => $extensionPath . 'interfaces/interface.tx_reports_statusprovider.php',
 	'tx_reports_report' => $extensionPath . 'interfaces/interface.tx_reports_report.php',
-	'tx_reports_module' => $extensionPath . 'mod/index.php',
 	'tx_reports_reports_status' => $extensionPath . 'reports/class.tx_reports_reports_status.php',
 	'tx_reports_reports_status_typo3status' => $extensionPath . 'reports/status/class.tx_reports_reports_status_typo3status.php',
 	'tx_reports_reports_status_systemstatus' => $extensionPath . 'reports/status/class.tx_reports_reports_status_systemstatus.php',
