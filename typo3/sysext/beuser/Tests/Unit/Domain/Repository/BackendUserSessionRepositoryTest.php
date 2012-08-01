44975f67f498b2bff6b6e81c6b5c7e10a33284bd
Felix Kopp <felix-source@phorax.com>
2012-08-02 01:25:07 +0200

    [FEATURE] Migrate EXT: beuser to Extbase
    This patchset includes a new backend user administration.
    
    Based on extbase a model for BackendUsers is included.
    All users can be listed and the listing can be filtered by
    * username
    * admin/non-admin
    * active/inactive
    * ever logged in.
    
    The user listing includes the last login column also the
    limit of 1.000 users was removed and pagination integrated.
    
    Selected users can be compared in a table view.
    All active backend user sessions are listed - can be terminated.
    
    Of course the SU/change user feature was migrated.
    Clickmenu was widely integrated to allow editing right away.
    
    Change-Id: I32569c848581a49b3948e093d717dad50e973c5d
    Resolves: #39141
    Resolves: #5288
    Resolves: #37949
    Resolves: #33427
    Resolves: #18602
    Resolves: #15494
    Releases: 6.0

---
+++ typo3/sysext/beuser/Tests/Unit/Domain/Repository/BackendUserSessionRepositoryTest.php
@@ -0,0 +1,57 @@
+<?php
+/***************************************************************
+ * Copyright notice
+ *
+ * (c) 2012 Felix Kopp <felix-source@phorax.com>
+ * All rights reserved
+ *
+ * This script is part of the TYPO3 project. The TYPO3 project is
+ * free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * The GNU General Public License can be found at
+ * http://www.gnu.org/copyleft/gpl.html.
+ *
+ * This script is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * Testcase for the Tx_Beuser_Domain_Repository_BackendUserSessionRepository class.
+ *
+ * @author Felix Kopp <felix-source@phorax.com>
+ * @package TYPO3
+ * @subpackage beuser
+ */
+class Tx_Beuser_Domain_Repository_BackendUserSessionRepositoryTest extends Tx_Extbase_Tests_Unit_BaseTestCase {
+
+	/**
+	 * @var Tx_Beuser_Domain_Repository_BackendUserSessionRepository
+	 */
+	protected $fixture;
+
+	public function setUp() {
+		$this->objectManager = $this->getMock('Tx_Extbase_Object_ObjectManagerInterface');
+		$this->fixture = new Tx_Beuser_Domain_Repository_BackendUserSessionRepository($this->objectManager);
+	}
+
+	public function tearDown() {
+		unset($this->fixture);
+		unset($this->objectManager);
+	}
+
+	/**
+	 * @test
+	 */
+	public function classCanBeInstantiated() {
+	}
+
+}
+
+?>
