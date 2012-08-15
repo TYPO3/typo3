605d05f0c8e9dae4cf88f9a2efc912cbc8fbeef7
Markus Bucher <markusbucher@gmx.de>
2012-08-15 12:17:55 +0200

    [SECURITY] XSS in validateForm
    Properly quote the form name and field list
    for the JavaScript validation
    
    Fixes: #25052
    Releases: 6.0, 4.7, 4.6, 4.5
    
    Change-Id: I01527117c20e25963951502c2277b853f683fe04
    Security-Commit: 20a6486d3027f474fb2352668cdb0fbee5f251f3
    Security-Bulletin: TYPO3-CORE-SA-2012-004
    Reviewed-on: http://review.typo3.org/13742
    Reviewed-by: Oliver Hader
    Tested-by: Oliver Hader

--- typo3/sysext/cms/tslib/content/class.tslib_content_form.php
+++ typo3/sysext/cms/tslib/content/class.tslib_content_form.php
@@ -839,8 +839,8 @@ class tslib_content_Form extends tslib_content_Abstract {
 			$emailMess = isset($conf['emailMess.'])
 				? $this->cObj->stdWrap($conf['emailMess'], $conf['emailMess.'])
 				: $conf['emailMess'];
-			$validateForm = ' onsubmit="return validateForm(\'' . $formName . '\',\'' . implode(',', $fieldlist)
-				. '\',' . t3lib_div::quoteJSvalue($goodMess) . ',' .
+			$validateForm = ' onsubmit="return validateForm(' . t3lib_div::quoteJSvalue($formName) . ',' . t3lib_div::quoteJSvalue(implode(',', $fieldlist))
+				. ',' . t3lib_div::quoteJSvalue($goodMess) . ',' .
 				t3lib_div::quoteJSvalue($badMess) . ',' .
 				t3lib_div::quoteJSvalue($emailMess) . ')"';
 			$GLOBALS['TSFE']->additionalHeaderData['JSFormValidate'] = '<script type="text/javascript" src="' .
