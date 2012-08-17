e7efe03e41429e0c324e8762858471828e952e6e
Christian Kuhn <lolli@schwarzbu.ch>
2012-08-17 18:53:47 +0200

    [TASK] Remove old em
    Change-Id: Ieb3823ad72fe41875484dfc25c8f1eea1feef917
    Resolves: #39906
    Related: #39726
    Reviewed-on: http://review.typo3.org/13869
    Reviewed-by: Christian Kuhn
    Tested-by: Christian Kuhn

--- typo3/sysext/em/classes/conf.php
+++
@@ -1,9 +0,0 @@
-<?php
-$MLANG['default']['tabs_images']['tab'] = 'em.gif';
-$MLANG['default']['ll_ref'] = 'LLL:EXT:em/language/locallang.xml';
-
-$MCONF['script']='_DISPATCH';
-$MCONF['access']='admin';
-$MCONF['name']='tools_em';
-$MCONF['workspaces']='online';
-?>
