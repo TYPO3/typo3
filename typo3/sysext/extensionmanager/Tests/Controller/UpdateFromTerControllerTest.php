77abce33f917c6dae57398576cadc32d7993ca0b
Helmut Hummel <helmut.hummel@typo3.org>
2012-08-19 00:25:04 +0200

    [BUGFIX] Do not fetch extension list on every request
    Due to comparing a DateTime object with an integer 
    the extension list is fetched every time when switching 
    to the get extensions tab in the extension manager.
    
    Convert the DateTime object to a timestamp first and 
    add a test which ensure this works.
    
    Additionally clean up the doc comment of that 
    property in the model and use forceUpdateCheck 
    as action parameter in the controller.
    
    Change-Id: I71f6599a6fa41115515c1764deb26e8e56762f4f
    Resolves: #39944
    Releases: 6.0

--- typo3/sysext/extensionmanager/Tests/Controller/UpdateFromTerControllerTest.php
+++ typo3/sysext/extensionmanager/Tests/Controller/UpdateFromTerControllerTest.php
@@ -80,9 +80,14 @@ class Tx_Extensionmanager_Controller_UpdateFromTerControllerTest extends Tx_Extb
 			->method('assign')
 			->will($this->returnValue($viewMock));
 
+		$lastUpdateDate = new DateTime();
+			// Wed Jul 25 18:40:02 CEST 2012
+		$lastUpdateDate->setTimestamp(1343234402);
 		$repositoryModelMock->expects($this->once())
 			->method('getLastUpdate')
-			->will($this->returnValue(1343234402));
+			->will($this->returnValue($lastUpdateDate));
+
+
 
 		$repositoryRepositoryMock
 			->expects($this->once())
@@ -92,7 +97,70 @@ class Tx_Extensionmanager_Controller_UpdateFromTerControllerTest extends Tx_Extb
 
 		$repositoryHelperMock->expects($this->once())
 			->method('updateExtList');
+			// Sat Jul 28 18:40:02 CEST 2012
+		$GLOBALS['EXEC_TIME'] = 1343493602;
 
+		$controllerMock->_set('repositoryRepository', $repositoryRepositoryMock);
+		$controllerMock->_set('repositoryHelper', $repositoryHelperMock);
+		$controllerMock->_set('settings', array('repositoryUid' => 1));
+		$controllerMock->_set('view', $viewMock);
+		$controllerMock->_set('request', $requestMock);
+		$controllerMock->updateExtensionListFromTerAction();
+	}
+
+	/**
+	 * @test
+	 * @return void
+	 */
+	public function updateExtensionListFromTerDoesNotCallUpdateExtListIfLastUpdateIsLessThan24HoursAgo() {
+		$controllerMock = $this->getAccessibleMock(
+			'Tx_Extensionmanager_Controller_UpdateFromTerController',
+			array('dummy')
+		);
+		$repositoryRepositoryMock = $this->getAccessibleMock(
+			'Tx_Extensionmanager_Domain_Repository_RepositoryRepository',
+			array('findOneByUid')
+		);
+		$repositoryModelMock = $this->getAccessibleMock(
+			'Tx_Extensionmanager_Domain_Model_Repository',
+			array('getLastUpdate')
+		);
+		$repositoryHelperMock = $this->getAccessibleMock(
+			'Tx_Extensionmanager_Utility_Repository_Helper',
+			array('updateExtList')
+		);
+		$viewMock = $this->getAccessibleMock(
+			'Tx_Fluid_View_TemplateView',
+			array('assign')
+		);
+
+		$requestMock = $this->getAccessibleMock(
+			'Tx_Extbase_MVC_Request',
+			array('hasArgument', 'getArgument')
+		);
+
+		$viewMock->expects($this->any())
+			->method('assign')
+			->will($this->returnValue($viewMock));
+
+		$lastUpdateDate = new DateTime();
+			// Wed Jul 25 18:40:02 CEST 2012
+		$lastUpdateDate->setTimestamp(1343493602);
+		$repositoryModelMock->expects($this->once())
+			->method('getLastUpdate')
+			->will($this->returnValue($lastUpdateDate));
+
+
+
+		$repositoryRepositoryMock
+			->expects($this->once())
+			->method('findOneByUid')
+			->with(1)
+			->will($this->returnValue($repositoryModelMock));
+
+		$repositoryHelperMock->expects($this->never())
+			->method('updateExtList');
+			// Sat Jul 28 18:40:02 CEST 2012
 		$GLOBALS['EXEC_TIME'] = 1343493602;
 
 		$controllerMock->_set('repositoryRepository', $repositoryRepositoryMock);
@@ -146,9 +214,12 @@ class Tx_Extensionmanager_Controller_UpdateFromTerControllerTest extends Tx_Extb
 			->method('assign')
 			->will($this->returnValue($viewMock));
 
+		$lastUpdateDate = new DateTime();
+			// Wed Jul 25 18:40:02 CEST 2012
+		$lastUpdateDate->setTimestamp(1343234402);
 		$repositoryModelMock->expects($this->once())
 			->method('getLastUpdate')
-			->will($this->returnValue(1343493602));
+			->will($this->returnValue($lastUpdateDate));
 
 		$repositoryRepositoryMock
 			->expects($this->once())
@@ -159,6 +230,7 @@ class Tx_Extensionmanager_Controller_UpdateFromTerControllerTest extends Tx_Extb
 		$repositoryHelperMock->expects($this->once())
 			->method('updateExtList');
 
+			// Sat Jul 28 18:40:02 CEST 2012
 		$GLOBALS['EXEC_TIME'] = 1343493602;
 
 		$controllerMock->_set('repositoryRepository', $repositoryRepositoryMock);
