279cde4ec3b2e2228ad035cb50d44353be1ef4ff
Sebastian fischer <typo3@evoweb.de>
2012-08-19 14:22:52 +0200

    [FEATURE] Add translation handling module
    As the new extension manager for 6.0 does not handle translation
    handling a module is needed to select and update translations of
    extensions.
    
    This change to the sysext lang extensions ands a module with
    which it is possible to both change select available languages
    as well update the translation files for extensions.
    
    Change-Id: I2a4f53fa01a1ba157a2ef8093130f80e06b74b72
    Resolves: #39963
    Releases: 6.0

---
+++ typo3/sysext/lang/Classes/Domain/Model/UpdateTranslationForm.php
@@ -0,0 +1,84 @@
+<?php
+namespace TYPO3\CMS\Lang\Domain\Model;
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2012 Sebastian Fischer <typo3@evoweb.de>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * Model to contain all information from the form requesting to update translations
+ *
+ * @author Sebastian Fischer <typo3@evoweb.de>
+ * @package TYPO3
+ * @subpackage lang
+ */
+class UpdateTranslationForm extends \TYPO3\CMS\Extbase\DomainObject\AbstractEntity {
+
+	/**
+	 * @var array
+	 */
+	protected $selectedLanguages = array();
+
+	/**
+	 * @var array
+	 */
+	protected $extensions = array();
+
+	/**
+	 * Setter for the extensions
+	 *
+	 * @param array $extensions of extensions that were requested
+	 * @return void
+	 */
+	public function setExtensions(array $extensions) {
+		$this->extensions = $extensions;
+	}
+
+	/**
+	 * Getter for the extensions
+	 *
+	 * @return array
+	 */
+	public function getExtensions() {
+		return $this->extensions;
+	}
+
+	/**
+	 * Setter for the selected languages
+	 *
+	 * @param array $selectedLanguages selected languages that were requested
+	 * @return void
+	 */
+	public function setSelectedLanguages(array $selectedLanguages) {
+		$this->selectedLanguages = $selectedLanguages;
+	}
+
+	/**
+	 * Getter for the selected languages
+	 *
+	 * @return array
+	 */
+	public function getSelectedLanguages() {
+		return $this->selectedLanguages;
+	}
+}
+
+?>
