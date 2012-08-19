3b8399afbbf3cc8f891acf02791bebc0f9f4d7b1
Sebastian fischer <typo3@evoweb.de>
2012-08-19 14:22:52 +0200

    [FEATURE] Add translation handling module
    As the new extension manager for 6.0 does not handle translation
    handling a module is needed to select and update translations of
    extensions.
    
    This change to the sysext lang extensions ands a module with
    which it is possible to both change select available languages
    as well update the translation files for extensions.
    
    Change-Id: I2a4f53fa01a1ba157a2ef8093130f80e06b74b72
    Resolves: #39963
    Releases: 6.0

---
+++ typo3/sysext/lang/Classes/Domain/Repository/LanguageRepository.php
@@ -0,0 +1,184 @@
+<?php
+namespace TYPO3\CMS\Lang\Domain\Repository;
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2012 Sebastian Fischer <typo3@evoweb.de>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * Language repository
+ *
+ * @author Sebastian Fischer <typo3@evoweb.de>
+ * @package lang
+ * @subpackage LanguageRepository
+ */
+class LanguageRepository {
+
+	/**
+	 * @var \TYPO3\CMS\Extbase\Object\ObjectManagerInterface
+	 */
+	protected $objectManager;
+
+	/**
+	 * @var array
+	 */
+	protected $selectedLanguages = array();
+
+	/**
+	 * @var \TYPO3\CMS\Core\Localization\Locales
+	 */
+	protected $locales;
+
+	/**
+	 * @var array
+	 */
+	protected $languages = array();
+
+	/**
+	 * @var string
+	 */
+	protected $configurationPath = 'EXTCONF/lang';
+
+	/**
+	 * Constructor of the language repository
+	 */
+	public function __construct() {
+		try {
+			$globalSettings = \TYPO3\CMS\Core\Configuration\ConfigurationManager::getLocalConfigurationValueByPath($this->configurationPath);
+			$this->selectedLanguages = (array) $globalSettings['availableLanguages'];
+		} catch (\TYPO3\CMS\Core\Exception $e) {
+			\TYPO3\CMS\Core\Configuration\ConfigurationManager::setLocalConfigurationValueByPath(
+				$this->configurationPath,
+				array('availableLanguages' => array())
+			);
+		}
+	}
+
+	/**
+	 * Injects the object manager
+	 *
+	 * @param \TYPO3\CMS\Extbase\Object\ObjectManager $objectManager
+	 * @return void
+	 */
+	public function injectObjectManager(\TYPO3\CMS\Extbase\Object\ObjectManager $objectManager) {
+		$this->objectManager = $objectManager;
+	}
+
+	/**
+	 * Injects the locales
+	 *
+	 * @param \TYPO3\CMS\Core\Localization\Locales $locales
+	 * @return void
+	 */
+	public function injectLocales(\TYPO3\CMS\Core\Localization\Locales $locales) {
+		$this->locales = $locales;
+	}
+
+	/**
+	 * Returns all objects of this repository.
+	 *
+	 * @return array
+	 */
+	public function findAll() {
+		if (!count($this->languages)) {
+			$languages = $this->locales->getLanguages();
+			array_shift($languages);
+
+			foreach ($languages as $locale => $language) {
+				$label = htmlspecialchars($GLOBALS['LANG']->sL('LLL:EXT:setup/mod/locallang.xml:lang_' . $locale));
+				if ($label === '') {
+					$label = htmlspecialchars($language);
+				}
+
+				$this->languages[$locale] = $this->objectManager->create(
+					'\TYPO3\CMS\Lang\Domain\Model\Language',
+					$locale,
+					$label,
+					in_array($locale, $this->selectedLanguages)
+				);
+			}
+
+			usort($this->languages, function($a, $b) {
+				/** @var $a \TYPO3\CMS\Lang\Domain\Model\Language */
+				/** @var $b \TYPO3\CMS\Lang\Domain\Model\Language */
+				if ($a->getLanguage() == $b->getLanguage()) {
+					return 0;
+				}
+				return $a->getLanguage() < $b->getLanguage() ? -1 : 1;
+			});
+		}
+
+		return $this->languages;
+	}
+
+	/**
+	 * Find selected languages
+	 *
+	 * @return array
+	 */
+	public function findSelected() {
+		$languages = $this->findAll();
+
+		$result = array();
+		/** @var $language \TYPO3\CMS\Lang\Domain\Model\Language */
+		foreach ($languages as $language) {
+			if ($language->getSelected()) {
+				$result[] = $language;
+			}
+		}
+
+		return $result;
+	}
+
+	/**
+	 * Update selected languages
+	 *
+	 * @param array $languages
+	 * @return array
+	 */
+	public function updateSelectedLanguages($languages) {
+			// Add possible dependencies for selected languages
+		$dependencies = array();
+		foreach ($languages as $language) {
+			$dependencies = array_merge($dependencies, $this->locales->getLocaleDependencies($language));
+		}
+		if (count($dependencies)) {
+			$languages = array_unique(array_merge($languages, $dependencies));
+		}
+
+		$dir = count($languages) - count($this->selectedLanguages);
+		$diff = $dir < 0 ? array_diff($this->selectedLanguages, $languages) : array_diff($languages, $this->selectedLanguages);
+
+		\TYPO3\CMS\Core\Configuration\ConfigurationManager::setLocalConfigurationValueByPath(
+			$this->configurationPath,
+			array('availableLanguages' => $languages)
+		);
+
+		return array(
+			'success' => count($diff) > 0,
+			'dir' => $dir,
+			'diff' => array_values($diff),
+			'languages' => $languages
+		);
+	}
+}
+
+?>
