72da79e09c611bf26b9c2473d2b7e2413d772fb9
Sebastian fischer <typo3@evoweb.de>
2012-08-19 14:22:52 +0200

    [FEATURE] Add translation handling module
    As the new extension manager for 6.0 does not handle translation
    handling a module is needed to select and update translations of
    extensions.
    
    This change to the sysext lang extensions ands a module with
    which it is possible to both change select available languages
    as well update the translation files for extensions.
    
    Change-Id: I2a4f53fa01a1ba157a2ef8093130f80e06b74b72
    Fixes: #39963
    Releases: 6.0

---
+++ typo3/sysext/lang/Classes/Controller/LanguageController.php
@@ -0,0 +1,202 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2012 Sebastian fischer, <typo3@evoweb.de>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+
+/**
+ * An exception when something is wrong within the lang
+ *
+ * @author Sebastian fischer, <typo3@evoweb.de>
+ * @package lang
+ * @subpackage LanguageController
+ */
+class Tx_Lang_Controller_LanguageController extends Tx_Extbase_MVC_Controller_ActionController {
+	/**
+	 * @constant integer
+	 */
+	const TRANSLATION_CHECK_FOR_EXTENSION = 0;
+
+	/**
+	 * @constant integer
+	 */
+	const TRANSLATION_UPDATE_FOR_EXTENSION = 1;
+
+	/**
+	 * @var Tx_Lang_Domain_Repository_LanguageRepository
+	 */
+	protected $languageRepository;
+
+	/**
+	 * @var Tx_Lang_Domain_Repository_ExtensionRepository
+	 */
+	protected $extensionRepository;
+
+	/**
+	 * @var Tx_Extensionmanager_Utility_Repository_Helper
+	 */
+	protected $repositoryHelper;
+
+
+	/**
+	 * @param \Tx_Lang_Domain_Repository_LanguageRepository $repository
+	 * @return void
+	 */
+	public function injectLanguageRepository(Tx_Lang_Domain_Repository_LanguageRepository $repository) {
+		$this->languageRepository = $repository;
+	}
+
+	/**
+	 * @param \Tx_Lang_Domain_Repository_ExtensionRepository $repository
+	 * @return void
+	 */
+	public function injectExtensionRepository(Tx_Lang_Domain_Repository_ExtensionRepository $repository) {
+		$this->extensionRepository = $repository;
+	}
+
+	/**
+	 * @param Tx_Extensionmanager_Utility_Repository_Helper $repositoryHelper
+	 * @return void
+	 */
+	public function injectRepositoryHelper(Tx_Extensionmanager_Utility_Repository_Helper $repositoryHelper) {
+		$this->repositoryHelper = $repositoryHelper;
+	}
+
+
+	/**
+	 * @return void
+	 */
+	public function indexAction() {
+		$languages = $this->languageRepository->findAll();
+		$selectedLanguages = $this->languageRepository->findSelected();
+		$extensions = $this->extensionRepository->findAll();
+
+		if ($this->request->hasArgument('updateResult')) {
+			$extensions = $this->mergeUpdateResult($extensions, $this->request->getArgument('updateResult'));
+		}
+
+		$this->view->assign('languages', $languages);
+		$this->view->assign('selectedLanguages', $selectedLanguages);
+		$this->view->assign('extensions', $extensions);
+	}
+
+	/**
+	 * @param array $extensions
+	 * @param array $updateResult
+	 * @return array
+	 */
+	protected function mergeUpdateResult($extensions, $updateResult) {
+		foreach ($updateResult as $key => $messages) {
+			$extensions[$key]->setUpdateResult($messages);
+		}
+
+		return $extensions;
+	}
+
+	/**
+	 * @param Tx_Lang_Domain_Model_LanguageSelectionForm $form
+	 * @return void
+	 */
+	public function saveSelectedLocaleAction(Tx_Lang_Domain_Model_LanguageSelectionForm $form) {
+		$selectedLanguages = array();
+		foreach ($form->getLocale() as $locale => $value) {
+			if ($value) {
+				$selectedLanguages[] = $locale;
+			}
+		}
+
+		$this->languageRepository->updateSelectedLanguages($selectedLanguages);
+
+		$this->forward('index');
+	}
+
+	/**
+	 * @param Tx_Lang_Domain_Model_UpdateTranslationForm $form
+	 * @return void
+	 */
+	public function updateTranslationAction(Tx_Lang_Domain_Model_UpdateTranslationForm $form) {
+		$result = array();
+
+		if (count($form->getSelectedLanguages())) {
+			foreach ($form->getExtensions() as $extension) {
+				$result[$extension] = $this->checkTranslationForExtension($form->getSelectedLanguages(), $extension);
+			}
+		}
+
+		$this->forward('index', NULL, NULL, array('updateResult' => $result));
+	}
+
+	/**
+	 * @param array $languages
+	 * @param string $extensionKey
+	 * @return array
+	 */
+	public function checkTranslationForExtension($languages, $extensionKey) {
+		$result = array();
+
+		/** @var $terConnection Tx_Lang_Utility_Connection_Ter */
+		$terConnection = $this->objectManager->create('Tx_Lang_Utility_Connection_Ter');
+		$mirrorUrl = $this->repositoryHelper->getMirrors()->getMirrorUrl();
+		$okIcon = '<span class="t3-icon t3-icon-status t3-icon-status-status t3-icon-status-checked">&nbsp;</span>';
+
+		$fetch = $terConnection->fetchTranslationStatus($extensionKey, $mirrorUrl);
+		foreach ($languages as $lang) {
+			if (!isset($fetch[$lang])) {
+				//no translation available
+				$result[$lang] = array(
+					'icon' => '<span class="t3-icon t3-icon-actions t3-icon-actions-document t3-icon-document-info">&nbsp;</span>',
+					'message' => 'translation_n_a'
+				);
+			} else {
+				$zip = PATH_site . 'typo3temp' . DIRECTORY_SEPARATOR . $extensionKey . '-l10n-' . $lang . '.zip';
+				$md5OfTranslationFile = '';
+				if (is_file($zip)) {
+					$md5OfTranslationFile = md5_file($zip);
+				}
+
+				if ($md5OfTranslationFile !== $fetch[$lang]['md5']) {
+					$update = $terConnection->updateTranslation($extensionKey, $lang, $mirrorUrl);
+
+					$result[$lang] = $update ?
+						array(
+							'icon' => $okIcon,
+							'message' => 'translation_msg_updated'
+						) :
+						array(
+							'icon' => '<span class="t3-icon t3-icon-status t3-icon-status-status t3-icon-status-permission-denied">&nbsp;</span>',
+							'message' => 'translation_msg_failed'
+						);
+				} else {
+						//translation is up to date
+					$result[$lang] = array(
+						'icon' => $okIcon,
+						'message' => 'translation_status_uptodate'
+					);
+				}
+			}
+		}
+
+		return $result;
+	}
+}
+
+?>
