17bddb966506a0bec904cec751716ce6279f7042
Sebastian fischer <typo3@evoweb.de>
2012-08-19 14:22:52 +0200

    [FEATURE] Add translation handling module
    As the new extension manager for 6.0 does not handle translation
    handling a module is needed to select and update translations of
    extensions.
    
    This change to the sysext lang extensions ands a module with
    which it is possible to both change select available languages
    as well update the translation files for extensions.
    
    Change-Id: I2a4f53fa01a1ba157a2ef8093130f80e06b74b72
    Resolves: #39963
    Releases: 6.0

---
+++ typo3/sysext/lang/Classes/Domain/Model/Language.php
@@ -0,0 +1,120 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2012 Sebastian Fischer <typo3@evoweb.de>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * Language model
+ *
+ * @author Sebastian Fischer <typo3@evoweb.de>
+ * @package TYPO3
+ * @subpackage lang
+ */
+class Tx_Lang_Domain_Model_Language extends Tx_Extbase_DomainObject_AbstractEntity {
+
+	/**
+	 * @var string
+	 */
+	protected $locale = '';
+
+	/**
+	 * @var string
+	 */
+	protected $language = '';
+
+	/**
+	 * @var boolean
+	 */
+	protected $selected = FALSE;
+
+	/**
+	 * Constructor of the language model
+	 *
+	 * @param string $locale
+	 * @param string $language
+	 * @param boolean $selected
+	 */
+	public function __construct($locale = '', $language = '', $selected = FALSE) {
+		$this->setLocale($locale);
+		$this->setLanguage($language);
+		$this->setSelected($selected);
+	}
+
+	/**
+	 * Setter for the language
+	 *
+	 * @param string $language the label of the language
+	 * @return void
+	 */
+	public function setLanguage($language) {
+		$this->language = $language;
+	}
+
+	/**
+	 * Getter for the language
+	 *
+	 * @return string
+	 */
+	public function getLanguage() {
+		return $this->language;
+	}
+
+	/**
+	 * Setter for the locale
+	 *
+	 * @param string $locale the locale for the language like da, nl or de
+	 * @return void
+	 */
+	public function setLocale($locale) {
+		$this->locale = $locale;
+	}
+
+	/**
+	 * Getter for the locale
+	 *
+	 * @return string
+	 */
+	public function getLocale() {
+		return $this->locale;
+	}
+
+	/**
+	 * Setter for the selected
+	 *
+	 * @param boolean $selected wether the language is available or not
+	 * @return void
+	 */
+	public function setSelected($selected) {
+		$this->selected = $selected ? TRUE : FALSE;
+	}
+
+	/**
+	 * Getter for the selected
+	 *
+	 * @return boolean
+	 */
+	public function getSelected() {
+		return $this->selected;
+	}
+}
+
+?>
---
+++ typo3/sysext/lang/Classes/Domain/Model/LanguageSelectionForm.php
@@ -0,0 +1,59 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2012 Sebastian Fischer <typo3@evoweb.de>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * Model to contain all information from the form changing the selection of available languages
+ *
+ * @author Sebastian Fischer <typo3@evoweb.de>
+ * @package TYPO3
+ * @subpackage lang
+ */
+class Tx_Lang_Domain_Model_LanguageSelectionForm extends Tx_Extbase_DomainObject_AbstractEntity {
+
+	/**
+	 * @var array
+	 */
+	protected $locale = array();
+
+	/**
+	 * Setter for the choosen locale
+	 *
+	 * @param array $locale all locale that were selected in the form
+	 * @return void
+	 */
+	public function setLocale(array $locale) {
+		$this->locale = $locale;
+	}
+
+	/**
+	 * Getter for the choosen locale
+	 *
+	 * @return array
+	 */
+	public function getLocale() {
+		return $this->locale;
+	}
+}
+
+?>
