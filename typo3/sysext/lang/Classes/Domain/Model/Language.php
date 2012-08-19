43acab4ee7cda3983738affe76ab5ec7c5ae6a96
Sebastian fischer <typo3@evoweb.de>
2012-08-19 14:22:52 +0200

    Add translation handling for selection and updating translations of extensions
    Change-Id: I2a4f53fa01a1ba157a2ef8093130f80e06b74b72

---
+++ typo3/sysext/lang/Classes/Domain/Model/Language.php
@@ -0,0 +1,106 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2012 Sebastian fischer, <typo3@evoweb.de>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+
+/**
+ * An exception when something is wrong within the lang
+ *
+ * @author Sebastian fischer, <typo3@evoweb.de>
+ * @package lang
+ * @subpackage Language
+ */
+class Tx_Lang_Domain_Model_Language extends Tx_Extbase_DomainObject_AbstractEntity {
+	/**
+	 * @var string
+	 */
+	protected $locale;
+
+	/**
+	 * @var string
+	 */
+	protected $language;
+
+	/**
+	 * @var boolean
+	 */
+	protected $selected;
+
+
+	/**
+	 * @param string $locale
+	 * @param string $language
+	 * @param boolean $selected
+	 * @return Tx_Lang_Domain_Model_Language
+	 */
+	public function __construct($locale, $language, $selected) {
+		$this->setLocale($locale);
+		$this->setLanguage($language);
+		$this->setSelected($selected);
+	}
+
+
+	/**
+	 * @param string $lable
+	 */
+	public function setLanguage($language) {
+		$this->language = $language;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getLanguage() {
+		return $this->language;
+	}
+
+	/**
+	 * @param string $locale
+	 */
+	public function setLocale($locale) {
+		$this->locale = $locale;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getLocale() {
+		return $this->locale;
+	}
+
+	/**
+	 * @param boolean $selected
+	 */
+	public function setSelected($selected) {
+		$this->selected = $selected ? TRUE : FALSE;
+	}
+
+	/**
+	 * @return boolean
+	 */
+	public function getSelected() {
+		return $this->selected;
+	}
+}
+
+?>
---
+++ typo3/sysext/lang/Classes/Domain/Model/UpdateTranslationForm.php
@@ -0,0 +1,73 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2012 Sebastian fischer, <typo3@evoweb.de>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+
+/**
+ * An exception when something is wrong within the lang
+ *
+ * @author Sebastian fischer, <typo3@evoweb.de>
+ * @package lang
+ * @subpackage UpdateTranslationForm
+ */
+class Tx_Lang_Domain_Model_UpdateTranslationForm extends Tx_Extbase_DomainObject_AbstractEntity {
+	/**
+	 * @var array
+	 */
+	protected $selectedLanguages = array();
+
+	/**
+	 * @var array
+	 */
+	protected $extensions = array();
+
+	/**
+	 * @param array $extensions
+	 */
+	public function setExtensions($extensions) {
+		$this->extensions = $extensions;
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getExtensions() {
+		return $this->extensions;
+	}
+
+	/**
+	 * @param array $selectedLanguages
+	 */
+	public function setSelectedLanguages($selectedLanguages) {
+		$this->selectedLanguages = $selectedLanguages;
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getSelectedLanguages() {
+		return $this->selectedLanguages;
+	}
+}
+
+?>
