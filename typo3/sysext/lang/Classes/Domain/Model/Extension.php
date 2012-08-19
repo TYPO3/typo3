2d5c8636f076a9ad53bf2df332831f563ddf84fe
Sebastian fischer <typo3@evoweb.de>
2012-08-19 14:22:52 +0200

    [FEATURE] Add translation handling module
    As the new extension manager for 6.0 does not handle translation
    handling a module is needed to select and update translations of
    extensions.
    
    This change to the sysext lang extensions ands a module with
    which it is possible to both change select available languages
    as well update the translation files for extensions.
    
    Change-Id: I2a4f53fa01a1ba157a2ef8093130f80e06b74b72
    Resolves: #39963
    Releases: 6.0

---
+++ typo3/sysext/lang/Classes/Domain/Model/Extension.php
@@ -0,0 +1,194 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2012 Sebastian Fischer <typo3@evoweb.de>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * Extension model
+ *
+ * @author Sebastian Fischer <typo3@evoweb.de>
+ * @package TYPO3
+ * @subpackage lang
+ */
+class Tx_Lang_Domain_Model_Extension extends Tx_Extbase_DomainObject_AbstractEntity {
+
+	/**
+	 * @var string
+	 */
+	protected $key = '';
+
+	/**
+	 * @var string
+	 */
+	protected $title = '';
+
+	/**
+	 * @var string
+	 */
+	protected $icon = '';
+
+	/**
+	 * @var integer
+	 */
+	protected $version = '';
+
+	/**
+	 * @var string
+	 */
+	protected $stateCls = '';
+
+	/**
+	 * @var string
+	 */
+	protected $versionislower = '';
+
+	/**
+	 * @var string
+	 */
+	protected $maxversion = '';
+
+	/**
+	 * @var array
+	 */
+	protected $updateResult = array();
+
+	/**
+	 * Construtor of the extension model.
+	 *
+	 * @param string $key The extension key
+	 * @param string $title Title of the extension
+	 * @param string $icon Icon representing the extension
+	 */
+	public function __construct($key = '', $title= '', $icon = '') {
+		$this->setKey($key);
+		$this->setTitle($title);
+		$this->setIcon($icon);
+	}
+
+	/**
+	 * Setter for the icon
+	 *
+	 * @param string $icon ext_icon path relative to typo3 folder like ../typo3conf/ext/extensionkey/ext_icon.gif
+	 * @return void
+	 */
+	public function setIcon($icon) {
+		$this->icon = $icon;
+	}
+
+	/**
+	 * Getter for the icon
+	 *
+	 * @return string ext_icon path relative to typo3 folder
+	 */
+	public function getIcon() {
+		return $this->icon;
+	}
+
+	/**
+	 * Setter for the key
+	 *
+	 * @param string $key
+	 * @return void
+	 */
+	public function setKey($key) {
+		$this->key = $key;
+	}
+
+	/**
+	 * Getter for the key
+	 *
+	 * @return string
+	 */
+	public function getKey() {
+		return $this->key;
+	}
+
+	/**
+	 * Setter for the title
+	 *
+	 * @param string $title
+	 * @return void
+	 */
+	public function setTitle($title) {
+		$this->title = $title;
+	}
+
+	/**
+	 * Getter for the title
+	 *
+	 * @return string
+	 */
+	public function getTitle() {
+		return $this->title;
+	}
+
+	/**
+	 * Setter for the version
+	 *
+	 * @param integer $version Needs to have a valid version format like 1003007
+	 * @return void
+	 */
+	public function setVersion($version) {
+		$this->version = $version;
+	}
+
+	/**
+	 * Setter for the version from string
+	 *
+	 * @param string $version Needs to have a format like '1.3.7' and converts it into an integer like 1003007 before setting the version
+	 * @see t3lib_utility_VersionNumber::convertVersionNumberToInteger
+	 * @return void
+	 */
+	public function setVersionFromString($version) {
+		$this->version = t3lib_utility_VersionNumber::convertVersionNumberToInteger($version);
+	}
+
+	/**
+	 * Getter for the version
+	 *
+	 * @return integer interpretion of the extension version
+	 */
+	public function getVersion() {
+		return $this->version;
+	}
+
+	/**
+	 * Setter for updateResult
+	 *
+	 * @param array $updateResult Needs to be in a structure like array('icon' => '', 'message' => '')
+	 * @return void
+	 */
+	public function setUpdateResult($updateResult) {
+		$this->updateResult = (array) $updateResult;
+	}
+
+	/**
+	 * Getter for updateResult
+	 *
+	 * @return array returns the update result as an arry in the structure like array('icon' => '', 'message' => '')
+	 */
+	public function getUpdateResult() {
+		return $this->updateResult;
+	}
+}
+
+?>
---
+++ typo3/sysext/lang/ext_tables.php
@@ -0,0 +1,25 @@
+<?php
+if (!defined('TYPO3_MODE')) {
+	die('Access denied.');
+}
+
+if (TYPO3_MODE == 'BE' && !(TYPO3_REQUESTTYPE & TYPO3_REQUESTTYPE_INSTALL)) {
+		// Registers a Backend Module
+	Tx_Extbase_Utility_Extension::registerModule(
+		$_EXTKEY,
+		'tools', // Make module a submodule of 'tools'
+		'language', // Submodule key
+		'after:extensionmanager', // Position
+		array(
+				// An array holding the controller-action-combinations that are accessible
+			'Language' => 'index, saveSelectedLocale, updateTranslation'
+		),
+		array(
+			'access' => 'admin',
+			'icon' => 'EXT:' . $_EXTKEY . '/Resources/Public/Images/moduleicon.gif',
+			'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_mod_language.xml',
+		)
+	);
+}
+
+?>
