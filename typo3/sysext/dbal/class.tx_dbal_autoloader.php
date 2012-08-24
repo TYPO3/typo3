1be81a1c184ae0b6483e71c7f88fab56b8f6dac6
Nicole Cordes <n.cordes@biz-design.biz>
2012-08-24 22:45:43 +0200

    [BUGFIX] Fatal error thrown in install tool
    With disabled dbal extension it fails to
    enter the install tool. The autoloader
    is required by extension manager
    function without any check for loaded
    extension.
    
    Change-Id: I74e71bd6dbccb9aed71a4d5206191fad56c1f3ff
    Fixes: #40145
    Related: #40095
    Releases: 6.0
    Reviewed-on: http://review.typo3.org/14055
    Reviewed-by: Christian Kuhn
    Tested-by: Christian Kuhn

--- typo3/sysext/dbal/class.tx_dbal_autoloader.php
+++ typo3/sysext/dbal/class.tx_dbal_autoloader.php
@@ -4,7 +4,7 @@
  * and will be removed by 7.0. The class was renamed and is now located at:
  * typo3/sysext/dbal/Classes/Autoloader.php
  */
-require_once \TYPO3\CMS\Core\Extension\ExtensionManager::extPath('dbal') . 'Classes/Autoloader.php';
+require_once PATH_site . 'typo3/sysext/dbal/Classes/Autoloader.php';
 // Make instance:
 $SOBE = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Dbal\\Autoloader');
 $SOBE->execute($this);
