40b1a13740b8199373b35a8d364678dc23058ef9
Felix Kopp <felix-source@phorax.com>
2012-08-24 14:38:56 +0200

    [BUGFIX] Namespace: addToolbarItem on TYPO3backend
    Change GLOBALS reference to not include namespace.
    
    Change-Id: I98ef141273f5101587876e9a781dfe26d39ec50c
    Fixes: #40123
    Releases: 6.0

--- typo3/sysext/sys_action/toolbarmenu/registerToolbarItem.php
+++ typo3/sysext/sys_action/toolbarmenu/registerToolbarItem.php
@@ -6,6 +6,6 @@ if (TYPO3_MODE == 'BE') {
 	// First include the class file
 	include_once \TYPO3\CMS\Core\Extension\ExtensionManager::extPath('sys_action') . 'toolbarmenu/class.tx_sysaction_toolbarmenu.php';
 	// Now register the class as toolbar item
-	$GLOBALS['TYPO3\\CMS\\Backend\\Controller\\BackendController']->addToolbarItem('sys_action', 'TYPO3\\CMS\\SysAction\\ActionToolbarMenu');
+	$GLOBALS['TYPO3backend']->addToolbarItem('sys_action', 'TYPO3\\CMS\\SysAction\\ActionToolbarMenu');
 }
 ?>
