7c03dcc3158a684c390b99a9565da0672156d2d2
Felix Kopp <felix-source@phorax.com>
2012-08-24 14:38:56 +0200

    [BUGFIX] Namespace: addToolbarItem on TYPO3backend
    Change GLOBALS reference to not include namespace.
    
    Change-Id: I98ef141273f5101587876e9a781dfe26d39ec50c
    Fixes: #40123
    Related: #40095
    Releases: 6.0

--- typo3/sysext/opendocs/registerToolbarItem.php
+++ typo3/sysext/opendocs/registerToolbarItem.php
@@ -3,9 +3,9 @@ if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
 if (TYPO3_MODE == 'BE') {
-	// First include the class file
+		// First include the class file
 	include_once \TYPO3\CMS\Core\Extension\ExtensionManager::extPath('opendocs') . 'class.tx_opendocs.php';
-	// Now register the class as toolbar item
-	$GLOBALS['TYPO3\\CMS\\Backend\\Controller\\BackendController']->addToolbarItem('opendocs', 'TYPO3\\CMS\\Opendocs\\Controller\\OpendocsController');
+		// Now register the class as toolbar item
+	$GLOBALS['TYPO3backend']->addToolbarItem('opendocs', 'TYPO3\\CMS\\Opendocs\\Controller\\OpendocsController');
 }
 ?>
