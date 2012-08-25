a6fd57ce3a9de473894fb4a66ad391719bfe9e47
Felix Kopp <felix-source@phorax.com>
2012-08-25 14:16:57 +0200

    [BUGFIX] Namespace: Revert extension split-up: wizard_sortpages
    Reverts the split-up for the extension wizard_sortpages:
    erroneous extKey was 'wizard_sort_pages'.
    
    Change-Id: I17d48af00094707981983011eddb4777810ac2c1
    Releases: 6.0
    Fixes: #40181
    Related: #40095
    Reviewed-on: http://review.typo3.org/14075
    Reviewed-by: Christian Kuhn
    Tested-by: Christian Kuhn

--- typo3/sysext/wizard_sortpages/class.tx_wizardsortpages_webfunc_2.php
+++ typo3/sysext/wizard_sortpages/class.tx_wizardsortpages_webfunc_2.php
@@ -2,7 +2,7 @@
 /*
  * @deprecated since 6.0, the classname tx_wizardsortpages_webfunc_2 and this file is obsolete
  * and will be removed by 7.0. The class was renamed and is now located at:
- * typo3/sysext/wizard_sort_pages/Classes/View/SortPagesWizardModuleFunction.php
+ * typo3/sysext/wizard_sortpages/Classes/View/SortPagesWizardModuleFunction.php
  */
-require_once \TYPO3\CMS\Core\Extension\ExtensionManager::extPath('wizard_sort_pages') . 'Classes/View/SortPagesWizardModuleFunction.php';
+require_once \TYPO3\CMS\Core\Extension\ExtensionManager::extPath('wizard_sortpages') . 'Classes/View/SortPagesWizardModuleFunction.php';
 ?>
