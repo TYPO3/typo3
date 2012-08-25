8c6e4c8fbe5b812d5e6c08e821bd55918f7dc765
Wouter Wolters <typo3@wouterwolters.nl>
2012-08-25 16:11:04 +0200

    [TASK] Remove old unit-test files from saltedpasswords
    Change-Id: Ic889af8ff204e06ab6272d22ba650894bef2a961
    Resolves: #40188
    Related: #40095
    Releases: 6.0

--- typo3/sysext/saltedpasswords/Tests/tx_saltedpasswords_divTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*
- * @deprecated since 6.0, the classname tx_saltedpasswords_divTest and this file is obsolete
- * and will be removed by 7.0. The class was renamed and is now located at:
- * typo3/sysext/saltedpasswords/Tests/Unit/Utility/SaltedPasswordsUtilityTest.php
- */
-require_once \TYPO3\CMS\Core\Extension\ExtensionManager::extPath('saltedpasswords') . 'Tests/Unit/Utility/SaltedPasswordsUtilityTest.php';
-?>
--- typo3/sysext/saltedpasswords/Tests/tx_saltedpasswords_salts_blowfishTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*
- * @deprecated since 6.0, the classname tx_saltedpasswords_salts_blowfishTest and this file is obsolete
- * and will be removed by 7.0. The class was renamed and is now located at:
- * typo3/sysext/saltedpasswords/Tests/Unit/Salts/BlowfishSaltTest.php
- */
-require_once \TYPO3\CMS\Core\Extension\ExtensionManager::extPath('saltedpasswords') . 'Tests/Unit/Salts/BlowfishSaltTest.php';
-?>
