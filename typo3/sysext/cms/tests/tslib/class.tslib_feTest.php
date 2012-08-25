0ad203c608bb79ae6533e61e6ff0ab3d1fc01813
Wouter Wolters <typo3@wouterwolters.nl>
2012-08-25 20:57:26 +0200

    [WIP][BUGFIX] Fix cms unit tests
    Change-Id: Id3ade83bc54e3ba6ed80e435671954bb7c4f9482
    Resolves: #40209
    Related: #40095
    Releases: 6.0

--- typo3/sysext/cms/tests/tslib/class.tslib_feTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*
- * @deprecated since 6.0, the classname tslib_feTest and this file is obsolete
- * and will be removed by 7.0. The class was renamed and is now located at:
- * typo3/sysext/frontend/Tests/Unit/FrontendTest.php
- */
-require_once \TYPO3\CMS\Core\Extension\ExtensionManager::extPath('frontend') . 'Tests/Unit/FrontendTest.php';
-?>
