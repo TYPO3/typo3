d3f9564da7feb6152d733cfad64d66f96ef862f9
Wouter Wolters <typo3@wouterwolters.nl>
2012-08-25 17:10:20 +0200

    [TASK] Fix ajax scripts in taskcenter
    Change-Id: I7aa1fb4ca020ba64bb57288e8ffbbc3ddf85008c
    Resolves: #40196
    Related: #40095
    Releases: 6.0

--- typo3/sysext/taskcenter/ext_tables.php
+++ typo3/sysext/taskcenter/ext_tables.php
@@ -5,7 +5,7 @@ if (!defined('TYPO3_MODE')) {
 if (TYPO3_MODE == 'BE') {
 	\TYPO3\CMS\Core\Extension\ExtensionManager::addModulePath('tools_txtaskcenterM1', \TYPO3\CMS\Core\Extension\ExtensionManager::extPath($_EXTKEY) . 'task/');
 	\TYPO3\CMS\Core\Extension\ExtensionManager::addModule('user', 'task', 'top', \TYPO3\CMS\Core\Extension\ExtensionManager::extPath($_EXTKEY) . 'task/');
-	$GLOBALS['TYPO3_CONF_VARS']['BE']['AJAX']['Taskcenter::saveCollapseState'] = 'EXT:taskcenter/classes/class.tx_taskcenter_status.php:TYPO3\\CMS\\Taskcenter\\TaskStatus->saveCollapseState';
-	$GLOBALS['TYPO3_CONF_VARS']['BE']['AJAX']['Taskcenter::saveSortingState'] = 'EXT:taskcenter/classes/class.tx_taskcenter_status.php:TYPO3\\CMS\\Taskcenter\\TaskStatus->saveSortingState';
+	$GLOBALS['TYPO3_CONF_VARS']['BE']['AJAX']['Taskcenter::saveCollapseState'] = 'EXT:taskcenter/Classes/class.tx_taskcenter_status.php:TYPO3\\CMS\\Taskcenter\\TaskStatus->saveCollapseState';
+	$GLOBALS['TYPO3_CONF_VARS']['BE']['AJAX']['Taskcenter::saveSortingState'] = 'EXT:taskcenter/Classes/class.tx_taskcenter_status.php:TYPO3\\CMS\\Taskcenter\\TaskStatus->saveSortingState';
 }
 ?>
