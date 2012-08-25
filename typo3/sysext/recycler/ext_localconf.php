8a776a22dc21399b932fbf51dc60b60995c8085c
Wouter Wolters <typo3@wouterwolters.nl>
2012-08-25 13:35:43 +0200

    [BUGFIX] Fix ajax scripts in recycler
    Fix ajax scripts in recycler
    
    Change-Id: Idda6fbde50f300365b6659a8cb8efe6438b2de4b
    Resolves: #40175
    Related: #40095
    Releases: 6.0

--- typo3/sysext/recycler/ext_localconf.php
+++ typo3/sysext/recycler/ext_localconf.php
@@ -2,5 +2,5 @@
 if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
-$TYPO3_CONF_VARS['BE']['AJAX']['tx_recycler::controller'] = \TYPO3\CMS\Core\Extension\ExtensionManager::extPath($_EXTKEY) . 'classes/controller/class.tx_recycler_controller_ajax.php:TYPO3\\CMS\\Recycler\\Controller\\RecyclerAjaxController->init';
+$TYPO3_CONF_VARS['BE']['AJAX']['RecyclerAjaxController::init'] = \TYPO3\CMS\Core\Extension\ExtensionManager::extPath($_EXTKEY) . 'Classes/Controller/class.tx_recycler_controller_ajax.php:TYPO3\\CMS\\Recycler\\Controller\\RecyclerAjaxController->init';
 ?>
