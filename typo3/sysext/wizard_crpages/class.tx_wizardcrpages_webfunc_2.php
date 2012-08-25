1d147d80889db443d4b97ed36cda5d157f63b3e7
Wouter Wolters <typo3@wouterwolters.nl>
2012-08-25 14:17:27 +0200

    [BUGFIX] Namespace: Revert extension split-up: wizard_crpages
    Reverts the split-up for the extension wizard_crpages:
    erroneous extKey was 'wizard_create_pages'.
    
    Change-Id: I7e14463d9c2cf59db2416dba48b76b32b7904f9c
    Resolves: #40178
    Related: #40095
    Releases: 6.0

--- typo3/sysext/wizard_crpages/class.tx_wizardcrpages_webfunc_2.php
+++ typo3/sysext/wizard_crpages/class.tx_wizardcrpages_webfunc_2.php
@@ -4,5 +4,5 @@
  * and will be removed by 7.0. The class was renamed and is now located at:
  * typo3/sysext/wizard_create_pages/Classes/Controller/CreatePagesWizardModuleFunctionController.php
  */
-require_once \TYPO3\CMS\Core\Extension\ExtensionManager::extPath('wizard_create_pages') . 'Classes/Controller/CreatePagesWizardModuleFunctionController.php';
+require_once \TYPO3\CMS\Core\Extension\ExtensionManager::extPath('wizard_crpages') . 'Classes/Controller/CreatePagesWizardModuleFunctionController.php';
 ?>
