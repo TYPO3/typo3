16553738dd2b736ea9f64ec86b560259d2742b88
Wouter Wolters <typo3@wouterwolters.nl>
2012-08-25 20:57:26 +0200

    [BUGFIX] Fix frontend unit tests
    Move and stabilize EXT:frontend unit tests and add them
    to travis testsuites.
    
    Change-Id: Id3ade83bc54e3ba6ed80e435671954bb7c4f9482
    Resolves: #40209
    Related: #40095
    Releases: 6.0

--- typo3/sysext/frontend/Tests/Unit/Controller/TypoScriptFrontentControllerTest.php
+++ typo3/sysext/frontend/Tests/Unit/Controller/TypoScriptFrontentControllerTest.php
@@ -1,5 +1,5 @@
 <?php
-namespace TYPO3\CMS\Frontend\Tests\Unit;
+namespace TYPO3\CMS\Frontend\Tests\Unit\Controller;
 
 /***************************************************************
  * Copyright notice
@@ -24,13 +24,13 @@ namespace TYPO3\CMS\Frontend\Tests\Unit;
  * This copyright notice MUST APPEAR in all copies of the script!
  ***************************************************************/
 /**
- * Testcase for the "tslib_fe" class in the TYPO3 Core.
+ * Testcase for TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController
  *
  * @package TYPO3
  * @subpackage tslib
  * @author Oliver Klee <typo3-coding@oliverklee.de>
  */
-class FrontendTest extends tx_phpunit_testcase {
+class TypoScriptFrontendControllerTest extends \tx_phpunit_testcase {
 
 	/**
 	 * Enable backup of global and system variables
@@ -48,16 +48,12 @@ class FrontendTest extends tx_phpunit_testcase {
 	protected $backupGlobalsBlacklist = array('TYPO3_DB');
 
 	/**
-	 * @var \TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController
+	 * @var \PHPUnit_Framework_MockObject_MockObject|\Tx_Phpunit_Interface_AccessibleObject|\TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController
 	 */
 	private $fixture;
 
 	public function setUp() {
-		// This creates an instance of the class without calling the
-		// original constructor.
-		$className = uniqid('TYPO3\\CMS\\Frontend\\Controller\\TypoScriptFrontendController');
-		eval((((((((('class ' . $className) . ' extends TYPO3\\CMS\\Frontend\\Controller\\TypoScriptFrontendController {') . 'public function ') . $className) . '() {}') . 'public function roundTripCryptString($string) {') . 'return parent::roundTripCryptString($string);') . '}') . '}');
-		$this->fixture = new $className();
+		$this->fixture = $this->getAccessibleMock('\\TYPO3\\CMS\\Frontend\\Controller\\TypoScriptFrontendController', array('dummy'), array(), '', FALSE);
 		$this->fixture->TYPO3_CONF_VARS = $GLOBALS['TYPO3_CONF_VARS'];
 		$this->fixture->TYPO3_CONF_VARS['SYS']['encryptionKey'] = '170928423746123078941623042360abceb12341234231';
 	}
@@ -91,9 +87,10 @@ class FrontendTest extends tx_phpunit_testcase {
 	 * Setup a tslib_fe object only for testing the header and footer
 	 * replacement during USER_INT rendering
 	 *
-	 * @return PHPUnit_Framework_MockObject_MockObject
+	 * @return \PHPUnit_Framework_MockObject_MockObject
 	 */
 	protected function setupTsfeMockForHeaderFooterReplacementCheck() {
+		/** @var \PHPUnit_Framework_MockObject_MockObject|\TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController $tsfe */
 		$tsfe = $this->getMock('TYPO3\\CMS\\Frontend\\Controller\\TypoScriptFrontendController', array(
 			'INTincScript_process',
 			'INTincScript_includeLibs',
@@ -101,10 +98,10 @@ class FrontendTest extends tx_phpunit_testcase {
 			'setAbsRefPrefix'
 		), array(), '', FALSE);
 		$tsfe->expects($this->once())->method('INTincScript_process')->will($this->returnCallback(array($this, 'INTincScript_processCallback')));
-		$tsfe->content = file_get_contents(__DIR__ . '/../fixtures/renderedPage.html');
+		$tsfe->content = file_get_contents(__DIR__ . '/Fixtures/renderedPage.html');
 		$tsfe->config['INTincScript_ext']['divKey'] = '679b52796e75d474ccbbed486b6837ab';
 		$tsfe->config['INTincScript'] = array('INT_SCRIPT.679b52796e75d474ccbbed486b6837ab' => array());
-		$GLOBALS['TT'] = new \t3lib_timeTrackNull();
+		$GLOBALS['TT'] = new \TYPO3\CMS\Core\TimeTracker\NullTimeTracker();
 		return $tsfe;
 	}
 
@@ -145,7 +142,7 @@ class FrontendTest extends tx_phpunit_testcase {
 	 */
 	public function roundTripCryptStringCreatesStringWithSameLengthAsInputString() {
 		$clearText = 'Hello world!';
-		$this->assertEquals(strlen($clearText), strlen($this->fixture->roundTripCryptString($clearText)));
+		$this->assertEquals(strlen($clearText), strlen($this->fixture->_callRef('roundTripCryptString', $clearText)));
 	}
 
 	/**
@@ -153,7 +150,7 @@ class FrontendTest extends tx_phpunit_testcase {
 	 */
 	public function roundTripCryptStringCreatesResultDifferentFromInputString() {
 		$clearText = 'Hello world!';
-		$this->assertNotEquals($clearText, $this->fixture->roundTripCryptString($clearText));
+		$this->assertNotEquals($clearText, $this->fixture->_callRef('roundTripCryptString', $clearText));
 	}
 
 	/**
@@ -161,10 +158,10 @@ class FrontendTest extends tx_phpunit_testcase {
 	 */
 	public function roundTripCryptStringAppliedTwoTimesReturnsOriginalString() {
 		$clearText = 'Hello world!';
-		$this->assertEquals($clearText, $this->fixture->roundTripCryptString($this->fixture->roundTripCryptString($clearText)));
+		$refValue = $this->fixture->_callRef('roundTripCryptString', $clearText);
+		$this->assertEquals($clearText, $this->fixture->_callRef('roundTripCryptString', $refValue));
 	}
 
 }
-
 
 ?>
