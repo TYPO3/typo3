19d6d4a6c3c913c8148712000b01436917ed2600
Felix Kopp <felix-source@phorax.com>
2012-08-25 13:19:43 +0200

    [BUGFIX] Namespace: Revert extension split-up: felogin
    Reverts the split-up for the extension felogin:
    erroneous extKey was 'fe_login'.
    
    Also fixes minor CGL and sub package comments.
    
    The tx_feloginTest (pi) compatibility case was remove including
    the mapping because phpUnit will find the new case anyways.
    
    Change-Id: Ic2b1b739b86000318c099d1337eb81f72bc83fdc
    Resolves: #40173
    Related: #40095
    Releases: 6.0
    Reviewed-on: http://review.typo3.org/14068
    Reviewed-by: Christian Kuhn
    Tested-by: Christian Kuhn

--- typo3/sysext/felogin/pi1/class.tx_felogin_pi1.php
+++ typo3/sysext/felogin/pi1/class.tx_felogin_pi1.php
@@ -2,7 +2,7 @@
 /*
  * @deprecated since 6.0, the classname tx_felogin_pi1 and this file is obsolete
  * and will be removed by 7.0. The class was renamed and is now located at:
- * typo3/sysext/fe_login/Classes/Controller/FrontendLoginController.php
+ * typo3/sysext/felogin/Classes/Controller/FrontendLoginController.php
  */
-require_once \TYPO3\CMS\Core\Extension\ExtensionManager::extPath('fe_login') . 'Classes/Controller/FrontendLoginController.php';
+require_once \TYPO3\CMS\Core\Extension\ExtensionManager::extPath('felogin') . 'Classes/Controller/FrontendLoginController.php';
 ?>
