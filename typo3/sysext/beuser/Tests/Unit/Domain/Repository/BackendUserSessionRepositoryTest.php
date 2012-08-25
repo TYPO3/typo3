d6022eb01fe613ed6a9ade7c66517e87286e3f74
Felix Kopp <felix-source@phorax.com>
2012-08-25 23:08:33 +0200

    [BUGFIX] Namespace: Beuser class names
    Some test in EXT:beuser were moved to the wrong location.
    Move them back to their original location and namespace properly.
    
    Change-Id: I70f25e79ee455c31d6469c0ca7a1d1b71d7f057d
    Fixes: #40194
    Relates: 40095
    Releases: 6.0
    Reviewed-on: http://review.typo3.org/14095
    Reviewed-by: Christian Kuhn
    Tested-by: Christian Kuhn

--- typo3/sysext/beuser/Tests/Unit/Domain/Repository/BackendUserSessionRepositoryTest.php
+++ typo3/sysext/beuser/Tests/Unit/Domain/Repository/BackendUserSessionRepositoryTest.php
@@ -1,8 +1,60 @@
 <?php
-/*
- * @deprecated since 6.0, the classname Tx_Beuser_Domain_Repository_BackendUserSessionRepositoryTest and this file is obsolete
- * and will be removed by 7.0. The class was renamed and is now located at:
- * typo3/sysext/beuser/Classes/Domain/Repository/BackendUserSessionRepositoryTest.php
+namespace TYPO3\CMS\Beuser\Tests\Unit\Domain\Repository;
+
+/***************************************************************
+ * Copyright notice
+ *
+ * (c) 2012 Felix Kopp <felix-source@phorax.com>
+ * All rights reserved
+ *
+ * This script is part of the TYPO3 project. The TYPO3 project is
+ * free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * The GNU General Public License can be found at
+ * http://www.gnu.org/copyleft/gpl.html.
+ *
+ * This script is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+/**
+ * Testcase for the Tx_Beuser_Domain_Repository_BackendUserSessionRepository class.
+ *
+ * @author Felix Kopp <felix-source@phorax.com>
+ * @package TYPO3
+ * @subpackage beuser
  */
-require_once \TYPO3\CMS\Core\Extension\ExtensionManager::extPath('beuser') . 'Classes/Domain/Repository/BackendUserSessionRepositoryTest.php';
+class BackendUserSessionRepositoryTest extends \TYPO3\CMS\Extbase\Tests\Unit\BaseTestCase {
+
+	/**
+	 * @var \TYPO3\CMS\Beuser\Domain\Repository\BackendUserSessionRepository
+	 */
+	protected $fixture;
+
+	public function setUp() {
+		$this->objectManager = $this->getMock('TYPO3\\CMS\\Extbase\\Object\\ObjectManagerInterface');
+		$this->fixture = new \TYPO3\CMS\Beuser\Domain\Repository\BackendUserSessionRepository($this->objectManager);
+	}
+
+	public function tearDown() {
+		unset($this->fixture);
+		unset($this->objectManager);
+	}
+
+	/**
+	 * @test
+	 */
+	public function classCanBeInstantiated() {
+
+	}
+
+}
+
+
 ?>
