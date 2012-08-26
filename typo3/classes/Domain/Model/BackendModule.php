18dc4cf5b4508bfa6e03290de8f61cef0efddc07
Wouter Wolters <typo3@wouterwolters.nl>
2012-08-26 17:58:15 +0200

    [TASK] Remove mapping for ModuleItems
    Change-Id: I15c23cc43e7100bdd9d68aa295babd546af09199
    Resolves: #40226
    Related: #40095
    Releases: 6.0

--- typo3/classes/Domain/Model/BackendModule.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*
- * @deprecated since 6.0, the classname Typo3_Domain_Model_BackendModule and this file is obsolete
- * and will be removed by 7.0. The class was renamed and is now located at:
- * typo3/sysext/backend/Classes/Domain/Model/Module/BackendModule.php
- */
-require_once \TYPO3\CMS\Core\Extension\ExtensionManager::extPath('backend') . 'Classes/Domain/Model/Module/BackendModule.php';
-?>
--- typo3/classes/Domain/Repository/BackendModuleRepository.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*
- * @deprecated since 6.0, the classname Typo3_Domain_Repository_BackendModuleRepository and this file is obsolete
- * and will be removed by 7.0. The class was renamed and is now located at:
- * typo3/sysext/backend/Classes/Domain/Repository/Module/BackendModuleRepository.php
- */
-require_once \TYPO3\CMS\Core\Extension\ExtensionManager::extPath('backend') . 'Classes/Domain/Repository/Module/BackendModuleRepository.php';
-?>
