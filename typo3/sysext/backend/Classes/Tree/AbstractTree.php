415c0866faf137877e92c94248dd618441f5a6bd
Wouter Wolters <typo3@wouterwolters.nl>
2012-08-27 21:42:27 +0200

    [BUGFIX] Editing page is broken on wrong type
    Change-Id: If6d3dfcb0fb095063750eaeb85bdb32aef0f417e
    Resolves: #40282
    Related: #40095
    Releases: 6.0
    Reviewed-on: http://review.typo3.org/14144
    Reviewed-by: Nicole Cordes
    Reviewed-by: Helmut Hummel
    Tested-by: Helmut Hummel

--- typo3/sysext/backend/Classes/Tree/AbstractTree.php
+++ typo3/sysext/backend/Classes/Tree/AbstractTree.php
@@ -45,7 +45,7 @@ abstract class AbstractTree {
 	/**
 	 * Tree Node Decorator
 	 *
-	 * @var t3lib_tree_renderer_Abstract
+	 * @var \TYPO3\CMS\Backend\Tree\Renderer\AbstractTreeRenderer
 	 */
 	protected $nodeRenderer = NULL;
 
@@ -65,7 +65,7 @@ abstract class AbstractTree {
 	}
 
 	/**
-	 * @param t3lib_tree_renderer_Abstract $dataProvider
+	 * @param \TYPO3\CMS\Backend\Tree\Renderer\AbstractTreeRenderer $dataProvider
 	 * @return void
 	 */
 	public function setNodeRenderer(\TYPO3\CMS\Backend\Tree\Renderer\AbstractTreeRenderer $nodeRenderer) {
@@ -73,7 +73,7 @@ abstract class AbstractTree {
 	}
 
 	/**
-	 * @return t3lib_tree_renderer_Abstract
+	 * @return \TYPO3\CMS\Backend\Tree\Renderer\AbstractTreeRenderer
 	 */
 	public function getNodeRenderer() {
 		return $this->nodeRenderer;
