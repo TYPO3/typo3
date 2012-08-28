43baefaac900b822cea2968997593ca02b9a28d5
Nicole Cordes <n.cordes@biz-design.biz>
2012-08-29 00:34:35 +0200

    [TASK] Remove compat layer for about module
    Patch deletes compat layer for class CustomSectionsInterface as it's newly
    introduced in TYPO3 6.0
    
    Change-Id: I22b9ffa117099d0ca86b0e8fb50a8d52c6182b8b
    Resolves: #40360
    Related: #40095
    Related: #40240
    Releases: 6.0
    Reviewed-on: http://review.typo3.org/14173
    Reviewed-by: Thomas Maroschik
    Tested-by: Thomas Maroschik

--- typo3/sysext/about/ext_autoload.php
+++
@@ -1,6 +0,0 @@
-<?php
-$extensionPath = \TYPO3\CMS\Core\Extension\ExtensionManager::extPath('about');
-return array(
-	'tx_about_customsections' => $extensionPath . '/interfaces/interface.tx_about_customsections.php'
-);
-?>
