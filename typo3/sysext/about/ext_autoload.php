a120b5e78c617230804aa0a50f2a7c2c1dcbf81a
Thomas Maroschik <tmaroschik@dfau.de>
2012-08-29 01:27:02 +0200

    Revert "[TASK] Remove compat layer for about module"
    This reverts commit 873a2e560cb58a175da9bec44a221d2ff7d93da5

---
+++ typo3/sysext/about/ext_autoload.php
@@ -0,0 +1,6 @@
+<?php
+$extensionPath = \TYPO3\CMS\Core\Extension\ExtensionManager::extPath('about');
+return array(
+	'tx_about_customsections' => $extensionPath . '/interfaces/interface.tx_about_customsections.php'
+);
+?>
