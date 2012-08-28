6284783aac13a0bdf72f4cf3bb6d46d7098d74fc
Nicole Cordes <n.cordes@biz-design.biz>
2012-08-29 00:48:41 +0200

    [TASK] Remove compat layer for aboutmodules module
    Patch deletes compat layer for class FunctionsUtility as it's newly
    introduced in TYPO3 6.0
    
    Change-Id: I8e1737bb0e51030894be486cc1dcbc4695c39138
    Resolves: #40361
    Related: #40095
    Related: #40240
    Releases: 6.0

--- typo3/sysext/aboutmodules/ext_autoload.php
+++
@@ -1,6 +0,0 @@
-<?php
-$extensionPath = \TYPO3\CMS\Core\Extension\ExtensionManager::extPath('aboutmodules');
-return array(
-	'tx_aboutmodules_functions' => $extensionPath . 'class.tx_aboutmodules_functions.php'
-);
-?>
