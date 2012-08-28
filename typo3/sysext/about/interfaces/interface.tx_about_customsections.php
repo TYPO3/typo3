985fb05d20700ae4d84a083ab9fff3384d6369e1
Thomas Maroschik <tmaroschik@dfau.de>
2012-08-29 01:27:02 +0200

    Revert "[TASK] Remove compat layer for about module"
    This reverts commit 873a2e560cb58a175da9bec44a221d2ff7d93da5
    
    Change-Id: Ia120b5e78c617230804aa0a50f2a7c2c1dcbf81a
    Reviewed-on: http://review.typo3.org/14175
    Reviewed-by: Thomas Maroschik
    Tested-by: Thomas Maroschik

---
+++ typo3/sysext/about/interfaces/interface.tx_about_customsections.php
@@ -0,0 +1,8 @@
+<?php
+/*
+ * @deprecated since 6.0, the classname tx_about_customSections and this file is obsolete
+ * and will be removed by 7.0. The class was renamed and is now located at:
+ * typo3/sysext/about/Classes/CustomSectionsInterface.php
+ */
+require_once \TYPO3\CMS\Core\Extension\ExtensionManager::extPath('about') . 'Classes/CustomSectionsInterface.php';
+?>
