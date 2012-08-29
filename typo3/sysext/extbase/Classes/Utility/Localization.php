db310c732b4c54ab5b22626367add6d8e4b5414f
Christian Kuhn <lolli@schwarzbu.ch>
2012-08-29 12:59:02 +0200

    [BUGFIX] Declare LocalizationUtility::translateFileReference static
    protected translateFileReference() is not declared static, like the
    rest of the class methods. This leads to strict errors.
    
    Change-Id: I63396e69f4bb3d36c7784c1a884dea003c89ecc4
    Fixes: #40377
    Releases: 6.0, 4.7, 4.6, 4.5
    Reviewed-on: http://review.typo3.org/14188
    Reviewed-by: Christian Kuhn
    Tested-by: Christian Kuhn

--- typo3/sysext/extbase/Classes/Utility/Localization.php
+++ typo3/sysext/extbase/Classes/Utility/Localization.php
@@ -133,7 +133,7 @@ class Tx_Extbase_Utility_Localization {
 	 * @see tslib_fe::sL()
 	 * @author Bastian Waidelich <bastian@typo3.org>
 	 */
-	protected function translateFileReference($key) {
+	static protected function translateFileReference($key) {
 		if (TYPO3_MODE === 'FE') {
 			$value = $GLOBALS['TSFE']->sL($key);
 			return $value !== FALSE ? $value : NULL;
