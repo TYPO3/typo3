7e338c3a62565d759a57060ee608f4d020b957c5
Oliver Hader <oliver@typo3.org>
2012-08-30 22:04:21 +0200

    [BUGFIX] Invalid ClearLanguageCacheMenuItem::clearCache()
    Method clearCache() is not found in ClearLanguageCacheMenuItem(),
    but in LanguageCacheClearer.
    
    Change-Id: Ibd48cbba78182563c8decc8f09cfc41c3f75a923
    Fixes: #40451
    Releases: 6.0
    Reviewed-on: http://review.typo3.org/14232
    Reviewed-by: Christian Kuhn
    Tested-by: Christian Kuhn

--- typo3/sysext/lang/ext_localconf.php
+++ typo3/sysext/lang/ext_localconf.php
@@ -14,7 +14,7 @@ if (isset($GLOBALS['TYPO3_CONF_VARS']['SYS']['lang']['cache']['clear_menu']) && 
 	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['additionalBackendItems']['cacheActions']['clearLangCache'] = 'TYPO3\CMS\Lang\ClearLanguageCacheMenuItem';
 } else {
 	// Clear l10n cache when the user clears all caches
-	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearCachePostProc']['clearLangCache'] = 'TYPO3\CMS\Lang\ClearLanguageCacheMenuItem->clearCache';
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearCachePostProc']['clearLangCache'] = 'TYPO3\CMS\Lang\LanguageCacheClearer->clearCache';
 }
 // Register Ajax call
 $GLOBALS['TYPO3_CONF_VARS']['BE']['AJAX']['lang::clearCache'] = 'TYPO3\CMS\Lang\LanguageCacheClearer->clearCache';
