d25401bbf72391786e676fff7763a7fde5757a41
Steffen Ritter <info@rs-websystems.de>
2012-09-04 15:51:50 +0200

    [BUGFIX] ContentAdapter for FAL not capable of translations
    The ContentAdapter, which rewrites FAL references to old
    style database formats, is not capable of some dealing
    with relations - images so to speak - which need to be
    translated.
    
    Introduce the capability and fix some bugs, which prevent
    the ContentAdapter to transfer translated
    sys_file_references correctly.
    
    Change-Id: I5887511bcf1dd8fb481e45762e302e0908791bdd
    Fixes: #40609
    Releases: 6.0

--- typo3/sysext/core/Classes/Resource/Service/FrontendContentAdapterService.php
+++ typo3/sysext/core/Classes/Resource/Service/FrontendContentAdapterService.php
@@ -49,7 +49,6 @@ class FrontendContentAdapterService {
 			),
 			'media' => array(
 				'paths' => 'media',
-				'captions' => 'imagecaption'
 			)
 		),
 		'pages' => array(
@@ -71,12 +70,16 @@ class FrontendContentAdapterService {
 	 * @return 	void
 	 */
 	static public function modifyDBRow(&$row, $table) {
+		if (isset($row['_MIGRATED']) && $row['_MIGRATED'] === TRUE) {
+			return;
+		}
+
 		if (array_key_exists($table, static::$migrateFields)) {
 			foreach (static::$migrateFields[$table] as $migrateFieldName => $oldFieldNames) {
 				if ($row !== NULL && isset($row[$migrateFieldName])) {
 					/** @var $fileRepository \TYPO3\CMS\Core\Resource\FileRepository */
 					$fileRepository = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Core\\Resource\\FileRepository');
-					$files = $fileRepository->findByRelation($table, $migrateFieldName, $row['uid']);
+					$files = $fileRepository->findByRelation($table, $migrateFieldName, $row['uid'], $GLOBALS['TSFE']->sys_language_content);
 					$fileFieldContents = array(
 						'paths' => array(),
 						'titleTexts' => array(),
@@ -108,11 +111,12 @@ class FrontendContentAdapterService {
 					}
 					if (count($files) > 0) {
 
-					} elseif ($row['image'] > 0) {
-						throw new \RuntimeException((('inconsistent count field in "' . $table) . '".') . $migrateFieldName, 1333754565);
+					} elseif ($row[$migrateFieldName]) {
+						throw new \RuntimeException('inconsistent count field in "' . $table . '.' . $migrateFieldName . '".', 1333754565);
 					}
 				}
 			}
+			$row['_MIGRATED'] = TRUE;
 		}
 	}
 
