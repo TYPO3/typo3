3e6c1ee0d5b3122484661f309c202d72f57fd953
Jigal van Hemert <jigal@xs4all.nl>
2012-09-07 19:50:47 +0200

    BUGFIX] strftime expects parameter 2 to be long, string given
    Any zero length string value is replaced with the current timestamp.
    (Just like the default value for the second parameter of
    strtime/gmstrftime.)
    
    Change-Id: I7cc43a09f75d21ab8db84c0191fa0c1c5d67ddb0
    Fixes: #38717
    Releases: 6.0, 4.7, 4.6, 4.5

--- typo3/sysext/frontend/Classes/ContentObject/ContentObjectRenderer.php
+++ typo3/sysext/frontend/Classes/ContentObject/ContentObjectRenderer.php
@@ -2573,6 +2573,8 @@ class ContentObjectRenderer {
 	 * @return string The processed input value
 	 */
 	public function stdWrap_strftime($content = '', $conf = array()) {
+			// Check for zero length string to mimic default case of strtime/gmstrftime
+		$content = $content == '' ? $GLOBALS['EXEC_TIME'] : intval($content);
 		$content = $conf['strftime.']['GMT'] ? gmstrftime($conf['strftime'], $content) : strftime($conf['strftime'], $content);
 		$tmp_charset = $conf['strftime.']['charset'] ? $conf['strftime.']['charset'] : $GLOBALS['TSFE']->localeCharset;
 		if ($tmp_charset) {
