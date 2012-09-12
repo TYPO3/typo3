8bfc7a224ab5ffb11cf17b35c11d4c38f51a69b0
Helmut Hummel <helmut.hummel@typo3.org>
2012-09-12 22:10:29 +0200

    [TASK] Cleanup Bootstrap Classes
    The namespace switch removed some linefeeds that 
    were there to improve readability of the bootstrap.
    
    Add these again and also remove the compatibility 
    files as these files are new to 6.0 anyway.
    
    Resolves: #40877
    Releases: 6.0
    
    Change-Id: I1ed4e7d8710f4feb97a0331b5526336075aa7932

--- typo3/classes/Bootstrap/BaseSetup.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*
- * @deprecated since 6.0, the classname Typo3_Bootstrap_BaseSetup and this file is obsolete
- * and will be removed by 7.0. The class was renamed and is now located at:
- * typo3/sysext/core/Classes/Core/SystemEnvironmentBuilder.php
- */
-require_once __DIR__ . '/../../sysext/core/Classes/Core/SystemEnvironmentBuilder.php';
-?>
--- typo3/classes/Bootstrap/Cli.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*
- * @deprecated since 6.0, the classname Typo3_Bootstrap_Cli and this file is obsolete
- * and will be removed by 7.0. The class was renamed and is now located at:
- * typo3/sysext/cli/Classes/CliBootstrap.php
- */
-require_once __DIR__ . '/../../sysext/cli/Classes/CliBootstrap.php';
-?>
