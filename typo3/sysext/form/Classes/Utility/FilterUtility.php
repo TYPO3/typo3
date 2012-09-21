fd61095db53cb25c13b17fdebbf43701c9ac0ba0
Philipp Gampe <philipp.gampe@typo3.org>
2012-09-21 22:55:10 +0200

    [BUGFIX] Form fatals in frontend after class rename
    The classname is build with a strtolower() which is not needed, but
    breaks with a camel case class name.
    
    Change-Id: I87daf2deadca08240d48fda79fd765eaa0187a12
    Fixes: #41215
    Releases: 6.0

--- typo3/sysext/form/Classes/Utility/FilterUtility.php
+++ typo3/sysext/form/Classes/Utility/FilterUtility.php
@@ -46,7 +46,7 @@ class FilterUtility implements \TYPO3\CMS\Form\Filter\FilterInterface {
 	 * will be vulnerable for XSS attacks
 	 */
 	public function __construct() {
-		$removeXssFilter = $this->makeFilter('removexss');
+		$removeXssFilter = $this->makeFilter('removeXss');
 		$this->addFilter($removeXssFilter);
 	}
 
@@ -110,7 +110,6 @@ class FilterUtility implements \TYPO3\CMS\Form\Filter\FilterInterface {
 	 * @return \TYPO3\CMS\Form\Filter\FilterInterface The filter object
 	 */
 	static public function createFilter($class, array $arguments = NULL) {
-		$class = strtolower((string) $class);
 		$className = 'TYPO3\\CMS\\Form\\Filter\\' . ucfirst($class) . 'Filter';
 		if (is_null($arguments)) {
 			$filter = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance($className);
