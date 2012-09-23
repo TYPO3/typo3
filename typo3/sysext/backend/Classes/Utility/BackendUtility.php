bc3a855abf16839673c5175648a006acfd8da598
Georg Ringer <georg.ringer@gmail.com>
2012-09-23 13:34:08 +0200

    [BUGFIX] BEfunc getProcessedValue doesn't show record title for "group"
    In the list view relation fields using the type group (internal_type=db)
    doesn't display the related value as expected. Instead, it shows
    the UID of the relation. As these calls go through getProcessedValue,
    the issue resolves the records by displaying the record titles correctly
    in the TYPO3 backend.
    
    Change-Id: I7d9cc8ec7f02e4a02518fcd14c81cc71f69b8ea7
    Resolves: #21410
    Releases: 6.2, 6.1, 6.0
    Reviewed-on: https://review.typo3.org/13082
    Reviewed-by: Wouter Wolters
    Tested-by: Wouter Wolters

--- typo3/sysext/backend/Classes/Utility/BackendUtility.php
+++ typo3/sysext/backend/Classes/Utility/BackendUtility.php
@@ -2034,7 +2034,28 @@ class BackendUtility {
 				}
 				break;
 			case 'group':
-				$l = implode(', ', GeneralUtility::trimExplode(',', $value, 1));
+				// resolve the titles for DB records
+				if ($theColConf['internal_type'] === 'db') {
+					$finalValues = array();
+					$relationTableName = $theColConf['allowed'];
+					$explodedValues = GeneralUtility::trimExplode(',', $value, TRUE);
+
+					foreach ($explodedValues as $explodedValue) {
+
+						if (MathUtility::canBeInterpretedAsInteger($explodedValue)) {
+							$relationTableNameForField = $relationTableName;
+						} else {
+							list($relationTableNameForField, $explodedValue) = self::splitTable_Uid($explodedValue);
+						}
+
+						$relationRecord = static::getRecordWSOL($relationTableNameForField, $explodedValue);
+						$finalValues[] = static::getRecordTitle($relationTableNameForField, $relationRecord);
+					}
+
+					$l = implode(', ', $finalValues);
+				} else {
+					$l = implode(', ', GeneralUtility::trimExplode(',', $value, TRUE));
+				}
 				break;
 			case 'check':
 				if (!is_array($theColConf['items']) || count($theColConf['items']) == 1) {
