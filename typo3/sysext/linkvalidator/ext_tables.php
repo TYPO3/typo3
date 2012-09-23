12c2fd9fe41b7e9289174fa472e631610da23de4
Philipp Gampe <dev@philippgampe.info>
2012-09-23 13:27:32 +0200

    [BUGFIX] Info module path is not namespaced
    Fixes: #41287
    Releases: 6.0
    
    Change-Id: I3fc21829fd8c725b500a7f96aefccd5f44b87c0c
    Reviewed-on: http://review.typo3.org/14962
    Reviewed-by: Philipp Gampe
    Tested-by: Philipp Gampe

--- typo3/sysext/linkvalidator/ext_tables.php
+++ typo3/sysext/linkvalidator/ext_tables.php
@@ -4,7 +4,7 @@ if (!defined('TYPO3_MODE')) {
 }
 if (TYPO3_MODE == 'BE') {
 	// Add module
-	\TYPO3\CMS\Core\Extension\ExtensionManager::insertModuleFunction('web_info', 'TYPO3\\CMS\\Linkvalidator\\Report\\LinkValidatorReport', \TYPO3\CMS\Core\Extension\ExtensionManager::extPath('linkvalidator') . 'modfuncreport/class.tx_linkvalidator_modfuncreport.php', 'LLL:EXT:linkvalidator/locallang.xml:mod_linkvalidator');
+	\TYPO3\CMS\Core\Extension\ExtensionManager::insertModuleFunction('web_info', 'TYPO3\\CMS\\Linkvalidator\\Report\\LinkValidatorReport', \TYPO3\CMS\Core\Extension\ExtensionManager::extPath('linkvalidator') . 'Classes/Report/LinkValidatorReport.php', 'LLL:EXT:linkvalidator/locallang.xml:mod_linkvalidator');
 }
 // Initialize Context Sensitive Help (CSH)
 \TYPO3\CMS\Core\Extension\ExtensionManager::addLLrefForTCAdescr('linkvalidator', 'EXT:linkvalidator/modfuncreport/locallang_csh.xml');
