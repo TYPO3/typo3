313903686ec2cdbaecdb51ea8b8c06f44bf030d9
Felix Kopp <felix-source@phorax.com>
2012-09-27 00:23:09 +0200

    [TASK] Remove: Open Documents module
    Removes the Module Menu item for OpenDocs on the left.
    Also moves Css/JavaScript/Images and Icons to default folders.
    
    Change-Id: I90c3f04f5981c6b0e02b036c8b604ce03b3d7b66
    Fixes: #41240
    Releases: 6.0
    Reviewed-on: http://review.typo3.org/15107
    Reviewed-by: Helmut Hummel
    Reviewed-by: Mattias Nilsson
    Reviewed-by: Wouter Wolters
    Tested-by: Wouter Wolters
    Reviewed-by: Christian Kuhn
    Tested-by: Christian Kuhn

--- typo3/sysext/opendocs/ext_tables.php
+++ typo3/sysext/opendocs/ext_tables.php
@@ -11,10 +11,5 @@ if (TYPO3_MODE == 'BE') {
 	$GLOBALS['TYPO3_CONF_VARS']['BE']['AJAX']['OpendocsController::closeDocument'] = $opendocsPath . 'class.tx_opendocs.php:TYPO3\\CMS\\Opendocs\\Controller\\OpendocsController->closeDocument';
 	// Register update signal to update the number of open documents
 	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_befunc.php']['updateSignalHook']['OpendocsController::updateNumber'] = $opendocsPath . 'class.tx_opendocs.php:TYPO3\\CMS\\Opendocs\\Controller\\OpendocsController->updateNumberOfOpenDocsHook';
-	// Register menu module if option is wanted
-	$_EXTCONF = unserialize($_EXTCONF);
-	if ($_EXTCONF['enableModule']) {
-		\TYPO3\CMS\Core\Extension\ExtensionManager::addModule('user', 'doc', 'after:ws', $opendocsPath . 'mod/');
-	}
 }
 ?>
