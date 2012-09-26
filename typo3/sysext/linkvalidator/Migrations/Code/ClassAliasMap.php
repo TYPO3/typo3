de7f03412357008398a6ccbb89557970bf2cfd84
Thomas Maroschik <tmaroschik@dfau.de>
2012-09-26 16:38:06 +0200

    [TASK] Provide class alias file for linkvalidator
    Change-Id: Ie9e202481b9946ec72a9e8f3227d0f0ae8d6de56
    Resolves: #41387
    Related: #40095
    Releases: 6.0
    Reviewed-on: http://review.typo3.org/15089
    Reviewed-by: Philipp Gampe
    Tested-by: Philipp Gampe

---
+++ typo3/sysext/linkvalidator/Migrations/Code/ClassAliasMap.php
@@ -0,0 +1,14 @@
+<?php
+return array(
+	'tx_linkvalidator_Processor' => 'TYPO3\\CMS\\Linkvalidator\\LinkAnalyzer',
+	'tx_linkvalidator_linktype_Abstract' => 'TYPO3\\CMS\\Linkvalidator\\Linktype\\AbstractLinktype',
+	'tx_linkvalidator_linktype_External' => 'TYPO3\\CMS\\Linkvalidator\\Linktype\\ExternalLinktype',
+	'tx_linkvalidator_linktype_File' => 'TYPO3\\CMS\\Linkvalidator\\Linktype\\FileLinktype',
+	'tx_linkvalidator_linktype_Internal' => 'TYPO3\\CMS\\Linkvalidator\\Linktype\\InternalLinktype',
+	'tx_linkvalidator_linktype_LinkHandler' => 'TYPO3\\CMS\\Linkvalidator\\Linktype\\LinkHandler',
+	'tx_linkvalidator_linktype_Interface' => 'TYPO3\\CMS\\Linkvalidator\\Linktype\\LinktypeInterface',
+	'tx_linkvalidator_ModFuncReport' => 'TYPO3\\CMS\\Linkvalidator\\Report\\LinkValidatorReport',
+	'tx_linkvalidator_tasks_Validator' => 'TYPO3\\CMS\\Linkvalidator\\Task\\ValidatorTask',
+	'tx_linkvalidator_tasks_ValidatorAdditionalFieldProvider' => 'TYPO3\\CMS\\Linkvalidator\\Task\\ValidatorTaskAdditionalFieldProvider',
+);
+?>
