8522394d6b31f125ba2d8fadf31abbcd918c92b0
Thomas Maroschik <tmaroschik@dfau.de>
2012-09-26 16:46:55 +0200

    [TASK] Provide class alias file for workspaces
    Change-Id: I2b38e28115a93c3796884180c5678d544ba52d77
    Resolves: #41389
    Related: #40095
    Releases: 6.0
    Reviewed-on: http://review.typo3.org/15092
    Reviewed-by: Helmut Hummel
    Tested-by: Helmut Hummel

---
+++ typo3/sysext/workspaces/Migrations/Code/ClassAliasMap.php
@@ -0,0 +1,27 @@
+<?php
+return array(
+	'Tx_Workspaces_Controller_AbstractController' => 'TYPO3\\CMS\\Workspaces\\Controller\\AbstractController',
+	'Tx_Workspaces_Controller_PreviewController' => 'TYPO3\\CMS\\Workspaces\\Controller\\PreviewController',
+	'Tx_Workspaces_Controller_ReviewController' => 'TYPO3\\CMS\\Workspaces\\Controller\\ReviewController',
+	'Tx_Workspaces_Domain_Model_CombinedRecord' => 'TYPO3\\CMS\\Workspaces\\Domain\\Model\\CombinedRecord',
+	'Tx_Workspaces_Domain_Model_DatabaseRecord' => 'TYPO3\\CMS\\Workspaces\\Domain\\Model\\DatabaseRecord',
+	'Tx_Workspaces_ExtDirect_AbstractHandler' => 'TYPO3\\CMS\\Workspaces\\ExtDirect\\AbstractHandler',
+	'Tx_Workspaces_ExtDirect_ActionHandler' => 'TYPO3\\CMS\\Workspaces\\ExtDirect\\ActionHandler',
+	'Tx_Workspaces_ExtDirect_Server' => 'TYPO3\\CMS\\Workspaces\\ExtDirect\\ExtDirectServer',
+	'Tx_Workspaces_ExtDirect_MassActionHandler' => 'TYPO3\\CMS\\Workspaces\\ExtDirect\\MassActionHandler',
+	'Tx_Workspaces_ExtDirect_PagetreeCollectionsProcessor' => 'TYPO3\\CMS\\Workspaces\\ExtDirect\\PagetreeCollectionsProcessor',
+	'Tx_Workspaces_ExtDirect_ToolbarMenu' => 'TYPO3\\CMS\\Workspaces\\ExtDirect\\ToolbarMenu',
+	'Tx_Workspaces_ExtDirect_WorkspaceSelectorToolbarItem' => 'TYPO3\\CMS\\Workspaces\\ExtDirect\\WorkspaceSelectorToolbarItem',
+	'Tx_Workspaces_Service_Befunc' => 'TYPO3\\CMS\\Workspaces\\Hook\\BackendUtilityHook',
+	'Tx_Workspaces_Service_Tcemain' => 'TYPO3\\CMS\\Workspaces\\Hook\\DataHandlerHook',
+	'Tx_Workspaces_Service_Fehooks' => 'TYPO3\\CMS\\Workspaces\\Hook\\TypoScriptFrontendControllerHook',
+	'Tx_Workspaces_Service_AutoPublish' => 'TYPO3\\CMS\\Workspaces\\Service\\AutoPublishService',
+	'Tx_Workspaces_Service_GridData' => 'TYPO3\\CMS\\Workspaces\\Service\\GridDataService',
+	'Tx_Workspaces_Service_History' => 'TYPO3\\CMS\\Workspaces\\Service\\HistoryService',
+	'Tx_Workspaces_Service_Integrity' => 'TYPO3\\CMS\\Workspaces\\Service\\IntegrityService',
+	'Tx_Workspaces_Service_Stages' => 'TYPO3\\CMS\\Workspaces\\Service\\StagesService',
+	'Tx_Workspaces_Service_Workspaces' => 'TYPO3\\CMS\\Workspaces\\Service\\WorkspaceService',
+	'Tx_Workspaces_Service_AutoPublishTask' => 'TYPO3\\CMS\\Workspaces\\Task\\AutoPublishTask',
+	'Tx_Workspaces_Service_CleanupPreviewLinkTask' => 'TYPO3\\CMS\\Workspaces\\Task\\CleanupPreviewLinkTask',
+);
+?>
