45ec6b27097b5234bd51b9be6a3f4bacd57fea5b
Kai Vogel <kai.vogel@speedprogs.de>
2012-10-03 15:37:23 +0200

    [BUGFIX] Allow overwrite when uploading an existing extension
    This patch adds a checkbox to the upload form, which makes it possible
    to overwrite the existing directory when uploading an extension
    which is already available in the system.
    
    Resolves: #41006
    Releases: 6.0
    
    Change-Id: Ib51a10a0e557060f0260e97ff8a9129f1c9f7eba

--- typo3/sysext/extensionmanager/Resources/Public/JavaScript/upload.js
+++ typo3/sysext/extensionmanager/Resources/Public/JavaScript/upload.js
@@ -1,34 +1,36 @@
 // IIFE for faster access to $ and save $ use
 (function ($) {
 
-	$(document).ready(function() {
-		$('.t3-icon-edit-upload').parent().not('.transformed').each(function () {
-			$(this).data('href', $(this).attr('href'));
-			$(this).attr('href', '#');
+	$(function() {
+		// Show upload form
+		$('.t3-icon-edit-upload').parent().not('.transformed').on('click', function(event) {
+			event.preventDefault();
 			$(this).addClass('transformed');
-			$(this).click(function () {
-				$('.uploadForm').show();
-				$.ajax({
-					url:$(this).data('href'),
-					dataType:'html',
-					success:function (data) {
-						$('.uploadForm').html(data);
-						handleUploadForm();
-					}
-				});
+			$('.uploadForm').show();
+			$.ajax({
+				url: $(this).attr('href'),
+				dataType: 'html',
+				success: function (data) {
+					$('.uploadForm').html(data);
+					handleUploadForm();
+				}
 			});
 		});
+
+		function handleUploadForm() {
+			// Handle form submit response
+			$('#typo3-extensionmanager-upload-target').on('load', function(event) {
+				var html = $.trim($(this).contents().find('body').html());
+				if (html[0] === '{') {
+					var data = $.parseJSON(html);
+					if (data.error) {
+						TYPO3.Flashmessage.display(TYPO3.Severity.error, 'Extension Upload failed', data.error, 5);
+					} else if (data.success) {
+						TYPO3.Flashmessage.display(TYPO3.Severity.information, TYPO3.l10n.localize('extensionList.uploadFlashMessage.title'), TYPO3.l10n.localize('extensionList.uploadFlashMessage.message').replace(/\{0\}/g, data.extension), 15);
+						location.reload();
+					}
+				}
+			});
+		}
 	});
-
-	function handleUploadForm() {
-		$('#typo3-extensionmanager-upload-target').on('load', function() {
-			var ret = frames['typo3-extensionmanager-upload-target'].document.getElementsByTagName("body")[0].innerHTML;
-			var data = eval("("+ret+")");
-			if (data.success) {
-				TYPO3.Flashmessage.display(TYPO3.Severity.information, TYPO3.l10n.localize('extensionList.uploadFlashMessage.title'), TYPO3.l10n.localize('extensionList.uploadFlashMessage.message').replace(/\{0\}/g, data.extension), 15);
-				location.reload();
-			}
-		});
-	}
-
 }(jQuery));
