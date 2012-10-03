2895dc7299c09b32107527819cd661f5c0b10727
Helmut Hummel <helmut.hummel@typo3.org>
2012-10-03 20:49:19 +0200

    [TASK] Make Core\Configuration\ConfigurationManager non static
    To have the possibility to remove dependencies from code that 
    calls the configuration manager, it has to be made non static.
    
    Together with this change, all further configuration file logic
    is moved to that class, to not expose internals in other
    places. For that some methods needed to be made public, but
    have been marked private, thus are not public API.
    
    Further cleanup and simplification, especially not calling
    makeInstance several times, will go in a separate change.
    
    Change-Id: I18392f2b222309e58f1addc8b6ec8eb54f0d78f5
    Resolves: #41052
    Releases: 6.0
    Reviewed-on: http://review.typo3.org/15268
    Reviewed-by: Anja Leichsenring
    Tested-by: Anja Leichsenring
    Reviewed-by: Wouter Wolters
    Tested-by: Wouter Wolters
    Reviewed-by: Helmut Hummel
    Tested-by: Helmut Hummel

--- typo3/sysext/dbal/Classes/Hooks/InstallToolHooks.php
+++ typo3/sysext/dbal/Classes/Hooks/InstallToolHooks.php
@@ -156,7 +156,7 @@ class InstallToolHooks {
 					)
 				)
 			);
-			\TYPO3\CMS\Core\Configuration\ConfigurationManager::setLocalConfigurationValuesByPathValuePairs('EXTCONF/dbal/handlerCfg', $config);
+			\TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Core\\Configuration\\ConfigurationManager')->setLocalConfigurationValuesByPathValuePairs('EXTCONF/dbal/handlerCfg', $config);
 			break;
 		}
 	}
