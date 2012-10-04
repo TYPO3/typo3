f3222a6c5fe9d388b763b08d3f1dac6f2ea59af4
Andreas Wolf <andreas.wolf@typo3.org>
2012-10-04 10:16:04 +0200

    [BUGFIX] Info link in file context menu broken
    The link for file info in the context menu of the file list module does
    not work, as it missed the special identifier '_FILE'.
    
    Change-Id: Ie9990e3800b0d4e8b49c2594b4ad96d9ffe9839b
    Resolves: #41396
    Releases: 6.0

--- typo3/sysext/backend/Classes/ClickMenu/ClickMenu.php
+++ typo3/sysext/backend/Classes/ClickMenu/ClickMenu.php
@@ -749,7 +749,7 @@ class ClickMenu {
 			}
 			// Info
 			if (!in_array('info', $this->disabledItems)) {
-				$menuItems['info'] = $this->DB_info($identifier, '');
+				$menuItems['info'] = $this->fileInfo($identifier);
 			}
 			$menuItems[] = 'spacer';
 			// Copy:
@@ -899,6 +899,16 @@ class ClickMenu {
 		}
 		$editOnClick = 'if(' . $conf . '){' . $loc . '.location.href=top.TS.PATH_typo3+\'' . $this->clipObj->pasteUrl('_FILE', $path, 0) . '&redirect=\'+top.rawurlencode(' . $this->frameLocation(($loc . '.document')) . '.pathname+' . $this->frameLocation(($loc . '.document')) . '.search);  }hideCM();top.nav.refresh();';
 		return $this->linkItem($this->label('pasteinto'), $this->excludeIcon(\TYPO3\CMS\Backend\Utility\IconUtility::getSpriteIcon('actions-document-paste-into')), $editOnClick . 'return false;');
+	}
+
+	/**
+	 * Adding ClickMenu element for file info
+	 *
+	 * @param string $identifier The combined identifier of the file.
+	 * @return array Item array, element in $menuItems
+	 */
+	protected function fileInfo($identifier) {
+		return $this->DB_info('_FILE', $identifier);
 	}
 
 	/***************************************
