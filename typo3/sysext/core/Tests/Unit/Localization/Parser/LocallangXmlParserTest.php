579a806937d2995f8a78660759f9eb6a6bf3322c
Xavier Perseguers <xavier@typo3.org>
2012-10-15 13:35:42 +0200

    [!!!][BUGFIX] Localization fallback does not work in Backend
    The ll-XML parser internally falls-back to English when looking for a
    non-existing language translation. This prevents the localization
    fallback mechanism to detect that the translation did not exist or was
    only partially available and thus take the language dependency into
    account to return a proper label.
    
    Change-Id: I4095d20ec8fc08105dfff108c7e3865f468ddb91
    Fixes: #41996
    Releases: 4.6, 4.7, 6.0
    Reviewed-on: http://review.typo3.org/15634
    Reviewed-by: Dominique Feyer
    Tested-by: Dominique Feyer
    Reviewed-by: Steffen Ritter
    Tested-by: Steffen Ritter
    Reviewed-by: Xavier Perseguers
    Tested-by: Xavier Perseguers

--- typo3/sysext/core/Tests/Unit/Localization/Parser/LocallangXmlParserTest.php
+++ typo3/sysext/core/Tests/Unit/Localization/Parser/LocallangXmlParserTest.php
@@ -120,12 +120,12 @@ class LocallangXmlParserTest extends \TYPO3\CMS\Core\Tests\UnitTestCase {
 	/**
 	 * @test
 	 */
-	public function canParseLlxmlInFrenchAndReturnsDefaultLabelsIfNoTranslationIsFound() {
+	public function canParseLlxmlInFrenchAndReturnsNullLabelsIfNoTranslationIsFound() {
 		$LOCAL_LANG = $this->parser->getParsedData($this->llxmlFileNames['locallangOnlyDefaultLanguage'], 'fr');
 		$expectedLabels = array(
-			'label1' => 'This is label #1',
-			'label2' => 'This is label #2',
-			'label3' => 'This is label #3'
+			'label1' => NULL,
+			'label2' => NULL,
+			'label3' => NULL
 		);
 		foreach ($expectedLabels as $key => $expectedLabel) {
 			$this->assertEquals($expectedLabel, $LOCAL_LANG['fr'][$key][0]['target']);
