ef81763b8976458aa5e48695ff2aea911c8f5562
Xavier Perseguers <xavier@typo3.org>
2012-10-16 17:32:06 +0200

    [BUGFIX] PHP warning: open_basedir restriction
    Change-Id: Ie1dfe4306cad8bdc23864f31a77dac980eac3494
    Fixes: #42054
    Relates: #35212
    Releases: 4.6, 4.7, 6.0

--- typo3/sysext/core/Classes/Resource/ResourceCompressor.php
+++ typo3/sysext/core/Classes/Resource/ResourceCompressor.php
@@ -525,7 +525,7 @@ class ResourceCompressor {
 			}
 		}
 		// if the file is from a special TYPO3 internal directory, add the missing typo3/ prefix
-		if (is_file(PATH_site . TYPO3_mainDir . $filename)) {
+		if (is_file(realpath(PATH_site . TYPO3_mainDir . $filename))) {
 			$filename = TYPO3_mainDir . $filename;
 		}
 		// build the file path relatively to the PATH_site
