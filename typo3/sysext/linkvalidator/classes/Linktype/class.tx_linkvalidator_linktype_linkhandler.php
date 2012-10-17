eb755dff89fbf588593247577d52c0d1a428cc63
Thomas Maroschik <tmaroschik@dfau.de>
2012-10-17 21:43:49 +0200

    [TASK][WIP] Move non class code back to source files
    Change-Id: I1677c621640a512a36424e65bcb974a2d0630507

---
+++ typo3/sysext/linkvalidator/classes/Linktype/class.tx_linkvalidator_linktype_linkhandler.php
@@ -0,0 +1,8 @@
+<?php
+/*
+ * @deprecated since 6.0, the classname tx_linkvalidator_linktype_LinkHandler and this file is obsolete
+ * and will be removed by 7.0. The class was renamed and is now located at:
+ * typo3/sysext/linkvalidator/Classes/Linktype/LinkHandler.php
+ */
+require_once t3lib_extMgm::extPath('linkvalidator') . 'Classes/Linktype/LinkHandler.php';
+?>
---
+++ typo3/sysext/linkvalidator/classes/tasks/class.tx_linkvalidator_tasks_validatoradditionalfieldprovider.php
@@ -0,0 +1,8 @@
+<?php
+/*
+ * @deprecated since 6.0, the classname tx_linkvalidator_tasks_ValidatorAdditionalFieldProvider and this file is obsolete
+ * and will be removed by 7.0. The class was renamed and is now located at:
+ * typo3/sysext/linkvalidator/Classes/Task/ValidatorTaskAdditionalFieldProvider.php
+ */
+require_once t3lib_extMgm::extPath('linkvalidator') . 'Classes/Task/ValidatorTaskAdditionalFieldProvider.php';
+?>
