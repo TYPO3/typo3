bb57123db2c3dc2c48d0622608f30640cd0dd604
Stefan Neufeind <typo3.neufeind@speedpartner.de>
2012-10-17 21:58:02 +0200

    [TASK] t3lib: Autoloader now named ClassLoader
    Adjust occurence in t3lib which is just
    there for compatibility (deprecated).
    
    Change-Id: Ieca152e5a4f21fac4e5473747a205eead278f88e
    Resolves: #42102
    Releases: 6.0

--- t3lib/class.t3lib_autoloader.php
+++ t3lib/class.t3lib_autoloader.php
@@ -2,7 +2,7 @@
 /*
  * @deprecated since 6.0, the classname t3lib_autoloader and this file is obsolete
  * and will be removed by 7.0. The class was renamed and is now located at:
- * typo3/sysext/core/Classes/Autoloader.php
+ * typo3/sysext/core/Classes/Core/ClassLoader.php
  */
-require_once __DIR__ . '/../typo3/sysext/core/Classes/Autoloader.php';
+require_once __DIR__ . '/../typo3/sysext/core/Classes/Core/ClassLoader.php';
 ?>
