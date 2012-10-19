06e428c89d98b8d33a5560fc51a2aca7f06c87f3
Andreas Wolf <andreas.wolf@typo3.org>
2012-10-19 17:20:12 +0200

    [TASK] Refactor FAL file processing
    File processing is a central part of TYPO3's file usage, as e.g. all
    images in content elements have to be resized when they don't fit the
    requirements. However, the current implementation of file processing
    with FAL has several drawbacks and shortcomings, not to mention quite a
    few bugs.
    
    This patch brings a completely reworked infrastructure behind the
    File Processing Service, while keeping the external API unchanged.
    
    The processing to be done is described in tasks, which are part of a
    ProcessedFile's properties. The processing itself is now moved to
    processors, which could execute the tasks using different utilities,
    e.g. ImageMagick or some cloud image processing service. Currently,
    there is only a local image processor implementation, which relies on
    ImageMagick/GraphicsMagick (i.e. uses the same configuration as the old
    processing).
    
    The processed file class now also supports safe handling of unchanged
    files, i.e. files that should have been processed, but didn't need
    processing.
    
    Change-Id: I186a46f9923dacd98be655d72be2bd89b43866c2
    Fixes: #39904, #40033, #40669
    Releases: 6.0

--- t3lib/stddb/tables.sql
+++ t3lib/stddb/tables.sql
@@ -280,6 +280,7 @@ CREATE TABLE sys_file_storage (
 CREATE TABLE sys_file (
 	uid int(11) NOT NULL auto_increment,
 	pid int(11) DEFAULT '0' NOT NULL,
+	# update timestamp of the database record, not the file!
 	tstamp int(11) DEFAULT '0' NOT NULL,
 	crdate int(11) DEFAULT '0' NOT NULL,
 	cruser_id int(11) DEFAULT '0' NOT NULL,
@@ -306,6 +307,7 @@ CREATE TABLE sys_file (
 	title tinytext,
 	sha1 tinytext,
 	size int(11) DEFAULT '0' NOT NULL,
+	# creation/modification date of the file (not the record!)
 	creation_date int(11) DEFAULT '0' NOT NULL,
 	modification_date int(11) DEFAULT '0' NOT NULL,
 	width int(11) DEFAULT '0' NOT NULL,
@@ -325,29 +327,22 @@ CREATE TABLE sys_file (
 #
 CREATE TABLE sys_file_processedfile (
 	uid int(11) NOT NULL auto_increment,
-	pid int(11) DEFAULT '0' NOT NULL,
 	tstamp int(11) DEFAULT '0' NOT NULL,
 	crdate int(11) DEFAULT '0' NOT NULL,
-	cruser_id int(11) DEFAULT '0' NOT NULL,
-	deleted tinyint(4) DEFAULT '0' NOT NULL,
 
 	storage int(11) DEFAULT '0' NOT NULL,
 	original int(11) DEFAULT '0' NOT NULL,
 	identifier varchar(200) DEFAULT '' NOT NULL,
 	name tinytext,
 	configuration text,
-	context varchar(200) DEFAULT '' NOT NULL,
+	originalfilesha1 VARCHAR(40) DEFAULT '' NOT NULL,
+	task varchar(200) DEFAULT '' NOT NULL,
 	checksum varchar(255) DEFAULT '' NOT NULL,
-	is_processed varchar(200) DEFAULT '' NOT NULL,
-	extension varchar(255) DEFAULT '' NOT NULL,
-	mime_type varchar(255) DEFAULT '' NOT NULL,
-	sha1 tinytext,
-	size int(11) DEFAULT '0' NOT NULL,
-	width int(11) DEFAULT '0' NOT NULL,
-	height int(11) DEFAULT '0' NOT NULL,
 
 	PRIMARY KEY (uid),
-	KEY parent (pid)
+	KEY configuration (configuration),
+	KEY context (context)
+	KEY original (original)
 );
 
 #
