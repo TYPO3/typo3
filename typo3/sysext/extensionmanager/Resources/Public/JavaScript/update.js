525098ded696fdcdf385dcd854bd850a21d0322e
Hans Christian Reinl <info@drublic.de>
2012-10-23 21:48:22 +0200

    [TASK] EM: Improve appearance of "get extensions"
    * Change order of table-heads to reflect scribble better
    * Include link to all version of extensions as arrow
    * Move details of author into description and hide for now
    * Move loading mask on extension table
    
    Resolves: #42327
    Releases: 6.0
    
    Change-Id: I9e042f972a77e09482a5eeab429214bb13108f71

--- typo3/sysext/extensionmanager/Resources/Public/JavaScript/update.js
+++ typo3/sysext/extensionmanager/Resources/Public/JavaScript/update.js
@@ -2,7 +2,7 @@
 (function ($) {
 
 	$(document).ready(function() {
-		$('.updateFromTer a').each(function() {
+		$('.splash-receivedata a').each(function() {
 			$(this).data('href', $(this).attr('href'));
 			$(this).attr('href', '#');
 			$(this).click(function() {
@@ -17,20 +17,35 @@
 		if (forceUpdate == 1) {
 			url = url + '&tx_extensionmanager_tools_extensionmanagerextensionmanager%5BforceUpdateCheck%5D=1';
 		}
-		$('.updateFromTer .spinner').show();
-		$('#terTableWrapper').mask();
+		$('.splash-receivedata').addClass('is-shown');
+		$('.typo3-extensionmanager-headerRowRight .splash-receivedata').addClass('is-hidden');
+
+		$('#terTable_wrapper').addClass('is-loading');
+
 		$.ajax({
 			url: url,
 			dataType: 'json',
 			success: function(data) {
-				$('.updateFromTer .spinner').hide();
 
+				// Hide loader
+				$('.splash-receivedata').removeClass('is-shown');
+
+				// Something went wrong, show message
 				if (data.errorMessage.length) {
 					TYPO3.Flashmessage.display(TYPO3.Severity.warning, TYPO3.l10n.localize('extensionList.updateFromTerFlashMessage.title'), data.errorMessage, 10);
 				}
-				$('.updateFromTer .text').html(
+
+				// Message with latest updates
+				$('.typo3-extensionmanager-headerRowRight .splash-receivedata .text').html(
 					data.message
 				);
+
+				// Show content
+				$('#terTable_wrapper').removeClass('is-loading');
+
+				// Header: Show message
+				$('.typo3-extensionmanager-headerRowRight .splash-receivedata').removeClass('is-hidden');
+
 				if (data.updated) {
 					$.ajax({
 						url: window.location.href + '&tx_extensionmanager_tools_extensionmanagerextensionmanager%5Bformat%5D=json',
@@ -43,7 +58,6 @@
 						}
 					});
 				}
-				$('#terTableWrapper').unmask();
 			}
 		});
 	}
