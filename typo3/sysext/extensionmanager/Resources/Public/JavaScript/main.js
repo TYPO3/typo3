f308f2996363ada05dd7a6e61ad1cfe1c1808b24
Hans Christian Reinl <info@drublic.de>
2012-10-23 20:40:18 +0200

    [TASK] Refactor EM JavaScript
    * Use IIFE for faster access to jQuery and save $ use
    * Make JS lint-free
    
    Resolves: #42325
    Releases: 6.0
    
    Change-Id: Ia014de44570306ce9c5cd02c97a228205a7a2849
    Reviewed-on: http://review.typo3.org/15915
    Reviewed-by: Helmut Hummel
    Tested-by: Helmut Hummel

--- typo3/sysext/extensionmanager/Resources/Public/JavaScript/main.js
+++ typo3/sysext/extensionmanager/Resources/Public/JavaScript/main.js
@@ -1,157 +1,161 @@
-jQuery(document).ready(function() {
-	manageExtensionListing();
-	jQuery('th[title]').tooltip({offset: [-10, -30], position: 'bottom right', tipClass: 'headerTooltip'})
-	jQuery('td[title]').tooltip({offset: [-10, -60], position: 'bottom right'});
-	jQuery("#typo3-extension-configuration-forms ul").tabs("div.category");
+// IIFE for faster access to $ and save $ use
+(function ($) {
 
-	jQuery('#resetSearch').live('click', function (e) {
-		datatable.fnFilter('');
+	$(document).ready(function() {
+		manageExtensionListing();
+		$('th[title]').tooltip({offset: [-10, -30], position: 'bottom right', tipClass: 'headerTooltip'});
+		$('td[title]').tooltip({offset: [-10, -60], position: 'bottom right'});
+		$("#typo3-extension-configuration-forms ul").tabs("div.category");
+
+		$('#resetSearch').live('click', function (e) {
+			datatable.fnFilter('');
+		});
+
+		resetSearchField();
 	});
 
-	resetSearchField();
-});
-
-function getUrlVars() {
-	var vars = [], hash;
-	var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-	for(var i = 0; i < hashes.length; i++) {
-		hash = hashes[i].split('=');
-		vars.push(hash[0]);
-		vars[hash[0]] = hash[1];
+	function getUrlVars() {
+		var vars = [], hash;
+		var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+		for(var i = 0; i < hashes.length; i++) {
+			hash = hashes[i].split('=');
+			vars.push(hash[0]);
+			vars[hash[0]] = hash[1];
+		}
+		return vars;
 	}
-	return vars;
-}
 
-function manageExtensionListing() {
-	datatable = jQuery('#typo3-extension-list').dataTable({
-		"sPaginationType":"full_numbers",
-		"bJQueryUI":true,
-		"bLengthChange":false,
-		'iDisplayLength':15,
-		"bStateSave":true,
-		"fnDrawCallback": bindActions
-	});
+	function manageExtensionListing() {
+		datatable = $('#typo3-extension-list').dataTable({
+			"sPaginationType":"full_numbers",
+			"bJQueryUI":true,
+			"bLengthChange":false,
+			'iDisplayLength':15,
+			"bStateSave":true,
+			"fnDrawCallback": bindActions
+		});
 
-	var getVars = getUrlVars();
+		var getVars = getUrlVars();
 
-	// restore filter
-	if(datatable.length && getVars['search']) {
-		datatable.fnFilter(getVars['search']);
+		// restore filter
+		if(datatable.length && getVars['search']) {
+			datatable.fnFilter(getVars['search']);
+		}
 	}
-}
 
-function bindActions() {
-	jQuery('td[title], tr[title]').tooltip({offset: [-10, -60], position: 'bottom right'});
-	jQuery('.removeExtension').not('.transformed').each(function() {
-		jQuery(this).data('href', jQuery(this).attr('href'));
-		jQuery(this).attr('href', 'javascript:void(0);');
-		jQuery(this).addClass('transformed');
-		jQuery(this).click(function() {
-			if (jQuery(this).hasClass('isLoadedWarning')) {
-				TYPO3.Dialog.QuestionDialog({
-					title: TYPO3.l10n.localize('extensionList.removalConfirmation.title'),
-					msg: TYPO3.l10n.localize('extensionList.removalConfirmation.message'),
-					url: jQuery(this).data('href'),
-					fn: function(button, dummy, dialog) {
-						if (button == 'yes') {
-							confirmDeletionAndDelete(dialog.url)
+	function bindActions() {
+		$('td[title], tr[title]').tooltip({offset: [-10, -60], position: 'bottom right'});
+		$('.removeExtension').not('.transformed').each(function() {
+			$(this).data('href', $(this).attr('href'));
+			$(this).attr('href', '#');
+			$(this).addClass('transformed');
+			$(this).click(function() {
+				if ($(this).hasClass('isLoadedWarning')) {
+					TYPO3.Dialog.QuestionDialog({
+						title: TYPO3.l10n.localize('extensionList.removalConfirmation.title'),
+						msg: TYPO3.l10n.localize('extensionList.removalConfirmation.message'),
+						url: $(this).data('href'),
+						fn: function(button, dummy, dialog) {
+							if (button == 'yes') {
+								confirmDeletionAndDelete(dialog.url);
+							}
 						}
-					}
-				});
-			} else {
-				confirmDeletionAndDelete(jQuery(this).data('href'));
-			}
-		})
-	});
-
-	jQuery('.t3-icon-system-extension-update').parent().each(function() {
-		jQuery(this).data('href', jQuery(this).attr('href'));
-		jQuery(this).attr('href', 'javascript:void(0);');
-		jQuery(this).addClass('transformed');
-		jQuery(this).click(function() {
-			jQuery('#typo3-extension-manager').mask();
-			jQuery.ajax({
-				url: jQuery(this).data('href'),
-				dataType: 'json',
-				success: updateExtension
+					});
+				} else {
+					confirmDeletionAndDelete($(this).data('href'));
+				}
 			});
 		});
-	});
 
-}
-
-function updateExtension(data) {
-	var message = '<h1>' + TYPO3.l10n.localize('extensionList.updateConfirmation.title') + '</h1>';
-	message += '<h2>' + TYPO3.l10n.localize('extensionList.updateConfirmation.message') + '</h2>';
-	jQuery.each(data.updateComments, function(version, comment) {
-		message += '<h3>' + version + '</h3>';
-		message += '<div>' + comment + '</div>';
-	});
-
-	TYPO3.Dialog.QuestionDialog({
-		title: TYPO3.l10n.localize('extensionList.updateConfirmation.questionVersionComments'),
-		msg: message,
-		width: 600,
-		url: data.url,
-		fn: function(button, dummy, dialog) {
-			if (button == 'yes') {
-				jQuery.ajax({
-					url: dialog.url,
+		$('.t3-icon-system-extension-update').parent().each(function() {
+			$(this).data('href', $(this).attr('href'));
+			$(this).attr('href', '#');
+			$(this).addClass('transformed');
+			$(this).click(function() {
+				$('#typo3-extension-manager').mask();
+				$.ajax({
+					url: $(this).data('href'),
 					dataType: 'json',
-					success: function(data) {
-						jQuery('#typo3-extension-manager').unmask();
-						TYPO3.Flashmessage.display(TYPO3.Severity.information, TYPO3.l10n.localize('extensionList.updateFlashMessage.title'),
-								TYPO3.l10n.localize('extensionList.updateFlashMessage.message').replace(/\{0\}/g, data.extension), 15);
-					}
+					success: updateExtension
 				});
-			} else {
-				jQuery('#typo3-extension-manager').unmask();
-			}
-		}
-	});
-}
+			});
+		});
 
-
-function confirmDeletionAndDelete(url) {
-	TYPO3.Dialog.QuestionDialog({
-		title: TYPO3.l10n.localize('extensionList.removalConfirmation.title'),
-		msg: TYPO3.l10n.localize('extensionList.removalConfirmation.question'),
-		url: url,
-		fn: function(button, dummy, dialog) {
-			if (button == 'yes') {
-				jQuery('#typo3-extension-manager').mask();
-				jQuery.ajax({
-					url: dialog.url,
-					dataType: 'json',
-					success: removeExtension
-				});
-			}
-		}
-	});
-}
-
-function removeExtension(data) {
-	jQuery('#typo3-extension-manager').unmask();
-	if (data.success) {
-		datatable.fnDeleteRow(datatable.fnGetPosition(document.getElementById(data.extension)));
-	} else {
-		TYPO3.Flashmessage.display(TYPO3.Severity.error, TYPO3.l10n.localize('extensionList.removalConfirmation.title'), data.message, 15);
 	}
-}
 
-function resetSearchField() {
-	var dataTablesFilter = find(".dataTables_filter");
-	jQuery('.dataTables_wrapper').find('.dataTables_filter').append(jQuery('<span />', {
-		'class':'t3-icon t3-icon-actions t3-icon-actions-input t3-icon-input-clear t3-tceforms-input-clearer',
-		'id':'resetSearch',
-		'style':'display:none'
-	}));
-	jQuery('#typo3-extension-list_filter').mouseout(function() {
-		jQuery(this).find('#resetSearch').css('display', 'none');
-	});
-	jQuery('#typo3-extension-list_filter').mouseover(function() {
-		if (jQuery(this).find('input').val()) {
-			jQuery(this).find('#resetSearch').css('display', 'inline-block');
+	function updateExtension(data) {
+		var message = '<h1>' + TYPO3.l10n.localize('extensionList.updateConfirmation.title') + '</h1>';
+		message += '<h2>' + TYPO3.l10n.localize('extensionList.updateConfirmation.message') + '</h2>';
+		$.each(data.updateComments, function(version, comment) {
+			message += '<h3>' + version + '</h3>';
+			message += '<div>' + comment + '</div>';
+		});
+
+		TYPO3.Dialog.QuestionDialog({
+			title: TYPO3.l10n.localize('extensionList.updateConfirmation.questionVersionComments'),
+			msg: message,
+			width: 600,
+			url: data.url,
+			fn: function(button, dummy, dialog) {
+				if (button == 'yes') {
+					$.ajax({
+						url: dialog.url,
+						dataType: 'json',
+						success: function(data) {
+							$('#typo3-extension-manager').unmask();
+							TYPO3.Flashmessage.display(TYPO3.Severity.information, TYPO3.l10n.localize('extensionList.updateFlashMessage.title'),
+									TYPO3.l10n.localize('extensionList.updateFlashMessage.message').replace(/\{0\}/g, data.extension), 15);
+						}
+					});
+				} else {
+					$('#typo3-extension-manager').unmask();
+				}
+			}
+		});
+	}
+
+
+	function confirmDeletionAndDelete(url) {
+		TYPO3.Dialog.QuestionDialog({
+			title: TYPO3.l10n.localize('extensionList.removalConfirmation.title'),
+			msg: TYPO3.l10n.localize('extensionList.removalConfirmation.question'),
+			url: url,
+			fn: function(button, dummy, dialog) {
+				if (button == 'yes') {
+					$('#typo3-extension-manager').mask();
+					$.ajax({
+						url: dialog.url,
+						dataType: 'json',
+						success: removeExtension
+					});
+				}
+			}
+		});
+	}
+
+	function removeExtension(data) {
+		$('#typo3-extension-manager').unmask();
+		if (data.success) {
+			datatable.fnDeleteRow(datatable.fnGetPosition(document.getElementById(data.extension)));
+		} else {
+			TYPO3.Flashmessage.display(TYPO3.Severity.error, TYPO3.l10n.localize('extensionList.removalConfirmation.title'), data.message, 15);
 		}
-	})
-}
+	}
+
+	function resetSearchField() {
+		var dataTablesFilter = find(".dataTables_filter");
+		$('.dataTables_wrapper').find('.dataTables_filter').append($('<span />', {
+			'class':'t3-icon t3-icon-actions t3-icon-actions-input t3-icon-input-clear t3-tceforms-input-clearer',
+			'id':'resetSearch',
+			'style':'display:none'
+		}));
+		$('#typo3-extension-list_filter').mouseout(function() {
+			$(this).find('#resetSearch').css('display', 'none');
+		});
+		$('#typo3-extension-list_filter').mouseover(function() {
+			if ($(this).find('input').val()) {
+				$(this).find('#resetSearch').css('display', 'inline-block');
+			}
+		});
+	}
+}(jQuery));
--- typo3/sysext/extensionmanager/Resources/Public/JavaScript/update.js
+++ typo3/sysext/extensionmanager/Resources/Public/JavaScript/update.js
@@ -1,68 +1,72 @@
-jQuery(document).ready(function() {
-	jQuery('.updateFromTer a').each(function() {
-		jQuery(this).data('href', jQuery(this).attr('href'));
-		jQuery(this).attr('href', 'javascript:void(0);');
-		jQuery(this).click(function() {
-				// force update on click
-			updateFromTer(jQuery(this).data('href'), 1);
+// IIFE for faster access to jQuery and save $ use
+(function ($) {
+
+	$(document).ready(function() {
+		$('.updateFromTer a').each(function() {
+			$(this).data('href', $(this).attr('href'));
+			$(this).attr('href', '#');
+			$(this).click(function() {
+					// force update on click
+				updateFromTer($(this).data('href'), 1);
+			});
+			updateFromTer($(this).data('href'), 0);
 		});
-		updateFromTer(jQuery(this).data('href'), 0);
 	});
-});
 
-function updateFromTer(url, forceUpdate) {
-	var url = url;
-	if (forceUpdate == 1) {
-		url = url + '&tx_extensionmanager_tools_extensionmanagerextensionmanager%5BforceUpdateCheck%5D=1'
-	}
-	jQuery('.updateFromTer .spinner').show();
-	jQuery('#terTableWrapper').mask();
-	jQuery.ajax({
-		url: url,
-		dataType: 'json',
-		success: function(data) {
-			jQuery('.updateFromTer .spinner').hide();
+	function updateFromTer(url, forceUpdate) {
+		if (forceUpdate == 1) {
+			url = url + '&tx_extensionmanager_tools_extensionmanagerextensionmanager%5BforceUpdateCheck%5D=1';
+		}
+		$('.updateFromTer .spinner').show();
+		$('#terTableWrapper').mask();
+		$.ajax({
+			url: url,
+			dataType: 'json',
+			success: function(data) {
+				$('.updateFromTer .spinner').hide();
 
-			if (data.errorMessage.length) {
-				TYPO3.Flashmessage.display(TYPO3.Severity.warning, TYPO3.l10n.localize('extensionList.updateFromTerFlashMessage.title'), data.errorMessage, 10);
+				if (data.errorMessage.length) {
+					TYPO3.Flashmessage.display(TYPO3.Severity.warning, TYPO3.l10n.localize('extensionList.updateFromTerFlashMessage.title'), data.errorMessage, 10);
+				}
+				$('.updateFromTer .text').html(
+					data.message
+				);
+				if (data.updated) {
+					$.ajax({
+						url: window.location.href + '&tx_extensionmanager_tools_extensionmanagerextensionmanager%5Bformat%5D=json',
+						dataType: 'json',
+						success: function(data) {
+							$('#terTableWrapper').html(
+								data
+							);
+							transformPaginatorToAjax();
+						}
+					});
+				}
+				$('#terTableWrapper').unmask();
 			}
-			jQuery('.updateFromTer .text').html(
-				data.message
-			);
-			if (data.updated) {
-				jQuery.ajax({
-					url: window.location.href + '&tx_extensionmanager_tools_extensionmanagerextensionmanager%5Bformat%5D=json',
+		});
+	}
+
+	function transformPaginatorToAjax() {
+		$('.f3-widget-paginator a').each(function() {
+			$(this).data('href', $(this).attr('href'));
+			$(this).attr('href', '#');
+			$(this).click(function() {
+				$('#terTableWrapper').mask();
+				$.ajax({
+					url: $(this).data('href'),
 					dataType: 'json',
 					success: function(data) {
-						jQuery('#terTableWrapper').html(
+						$('#terTableWrapper').html(
 							data
 						);
+						$('#terTableWrapper').unmask();
 						transformPaginatorToAjax();
 					}
 				});
-			}
-			jQuery('#terTableWrapper').unmask();
-		}
-	});
-}
-
-function transformPaginatorToAjax() {
-	jQuery('.f3-widget-paginator a').each(function() {
-		jQuery(this).data('href', jQuery(this).attr('href'));
-		jQuery(this).attr('href', 'javascript:void(0);');
-		jQuery(this).click(function() {
-			jQuery('#terTableWrapper').mask();
-			jQuery.ajax({
-				url: jQuery(this).data('href'),
-				dataType: 'json',
-				success: function(data) {
-					jQuery('#terTableWrapper').html(
-						data
-					);
-					jQuery('#terTableWrapper').unmask();
-					transformPaginatorToAjax();
-				}
 			});
 		});
-	});
-}
+	}
+
+}(jQuery));
