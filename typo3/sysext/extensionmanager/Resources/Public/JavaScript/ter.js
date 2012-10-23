f5495073f8bb70a321eff616492fc8547503fadc
Hans Christian Reinl <info@drublic.de>
2012-10-23 20:40:18 +0200

    [TASK] Refactor EM JavaScript
    * Use IIFE for faster access to jQuery and save $ use
    * Make JS lint-free
    
    Resolves: #42325
    Releases: 6.0
    
    Change-Id: Ia014de44570306ce9c5cd02c97a228205a7a2849

--- typo3/sysext/extensionmanager/Resources/Public/JavaScript/ter.js
+++ typo3/sysext/extensionmanager/Resources/Public/JavaScript/ter.js
@@ -1,104 +1,108 @@
-jQuery(document).ready(function() {
-	jQuery('#terTable').dataTable({
-		"bJQueryUI":true,
-		"bLengthChange": false,
-		'iDisplayLength': 15,
-		"bStateSave": false,
-		"bInfo": false,
-		"bPaginate": false,
-		"bFilter": false,
-		"fnDrawCallback": bindDownload
-	});
+// IIFE for faster access to $ and save $ use
+(function ($) {
 
-	jQuery('#terVersionTable').dataTable({
-		"bJQueryUI":true,
-		"bLengthChange":false,
-		'iDisplayLength':15,
-		"bStateSave":false,
-		"bInfo":false,
-		"bPaginate":false,
-		"bFilter":false,
-		"aaSorting":[[0, 'desc']],
-		"fnDrawCallback":bindDownload
-	});
-
-	jQuery('#terSearchTable').dataTable({
-		"sPaginationType":"full_numbers",
-		"bJQueryUI":true,
-		"bLengthChange": false,
-		'iDisplayLength': 15,
-		"bStateSave": false,
-		"oLanguage": {
-			"sSearch": "Filter results:"
-		},
-		"aaSorting": [],
-		"fnDrawCallback": bindDownload
-	});
-	bindDownload();
-});
-
-function bindDownload() {
-	var installButtons = jQuery('.downloadFromTer form.download input[type=submit]');
-	installButtons.off('click');
-	installButtons.on('click', function(event) {
-		event.preventDefault();
-		var url = jQuery(event.currentTarget.form).attr('href');
-		downloadPath = jQuery(event.currentTarget.form).find('input.downloadPath:checked').val();
-		jQuery.ajax({
-			url: url,
-			dataType: 'json',
-			success: getDependencies
+	$(document).ready(function() {
+		$('#terTable').dataTable({
+			"bJQueryUI":true,
+			"bLengthChange": false,
+			'iDisplayLength': 15,
+			"bStateSave": false,
+			"bInfo": false,
+			"bPaginate": false,
+			"bFilter": false,
+			"fnDrawCallback": bindDownload
 		});
-	});
-}
 
-function getDependencies(data) {
-	if (data.hasDependencies) {
-		TYPO3.Dialog.QuestionDialog({
-			title: data.title,
-			msg: data.message,
-			url: data.url + '&tx_extensionmanager_tools_extensionmanagerextensionmanager[downloadPath]=' + downloadPath,
-			fn: getResolveDependenciesAndInstallResult
+		$('#terVersionTable').dataTable({
+			"bJQueryUI":true,
+			"bLengthChange":false,
+			'iDisplayLength':15,
+			"bStateSave":false,
+			"bInfo":false,
+			"bPaginate":false,
+			"bFilter":false,
+			"aaSorting":[[0, 'desc']],
+			"fnDrawCallback":bindDownload
 		});
-	} else {
-		if(data.hasErrors) {
-			TYPO3.Flashmessage.display(TYPO3.Severity.error, data.title, data.message, 10);
+
+		$('#terSearchTable').dataTable({
+			"sPaginationType":"full_numbers",
+			"bJQueryUI":true,
+			"bLengthChange": false,
+			'iDisplayLength': 15,
+			"bStateSave": false,
+			"oLanguage": {
+				"sSearch": "Filter results:"
+			},
+			"aaSorting": [],
+			"fnDrawCallback": bindDownload
+		});
+		bindDownload();
+	});
+
+	function bindDownload() {
+		var installButtons = $('.downloadFromTer form.download input[type=submit]');
+		installButtons.off('click');
+		installButtons.on('click', function(event) {
+			event.preventDefault();
+			var url = $(event.currentTarget.form).attr('href');
+			downloadPath = $(event.currentTarget.form).find('input.downloadPath:checked').val();
+			$.ajax({
+				url: url,
+				dataType: 'json',
+				success: getDependencies
+			});
+		});
+	}
+
+	function getDependencies(data) {
+		if (data.hasDependencies) {
+			TYPO3.Dialog.QuestionDialog({
+				title: data.title,
+				msg: data.message,
+				url: data.url + '&tx_extensionmanager_tools_extensionmanagerextensionmanager[downloadPath]=' + downloadPath,
+				fn: getResolveDependenciesAndInstallResult
+			});
 		} else {
-			var button = 'yes';
-			var dialog = [];
-			var dummy = '';
-			dialog['url'] = data.url + '&tx_extensionmanager_tools_extensionmanagerextensionmanager[downloadPath]=' + downloadPath;
-			getResolveDependenciesAndInstallResult(button, dummy, dialog)
+			if(data.hasErrors) {
+				TYPO3.Flashmessage.display(TYPO3.Severity.error, data.title, data.message, 10);
+			} else {
+				var button = 'yes';
+				var dialog = [];
+				var dummy = '';
+				dialog['url'] = data.url + '&tx_extensionmanager_tools_extensionmanagerextensionmanager[downloadPath]=' + downloadPath;
+				getResolveDependenciesAndInstallResult(button, dummy, dialog);
+			}
+		}
+		return false;
+	}
+
+	function getResolveDependenciesAndInstallResult(button, dummy, dialog) {
+		if (button == 'yes') {
+			var newUrl = dialog.url;
+			$.ajax({
+				url: newUrl,
+				dataType: 'json',
+				success: function (data) {
+					$('#typo3-extension-manager').unmask();
+					if (data.errorMessage.length) {
+						TYPO3.Flashmessage.display(TYPO3.Severity.error, TYPO3.l10n.localize('extensionList.dependenciesResolveDownloadError.title'), data.errorMessage, 5);
+					} else {
+						var successMessage = TYPO3.l10n.localize('extensionList.dependenciesResolveDownloadSuccess.message').replace(/\{0\}/g, data.extension) + ' <br />';
+						successMessage += '<br /><h3>' + TYPO3.l10n.localize('extensionList.dependenciesResolveDownloadSuccess.header') + ':</h3>';
+						$.each(data.result, function(index, value) {
+							successMessage += TYPO3.l10n.localize('extensionList.dependenciesResolveDownloadSuccess.item') + ' ' + index + ':<br /><ul>';
+							$.each(value, function(extkey, extdata) {
+								successMessage += '<li>' + extkey + '</li>';
+							});
+							successMessage += '</ul>';
+						});
+						TYPO3.Flashmessage.display(TYPO3.Severity.information, TYPO3.l10n.localize('extensionList.dependenciesResolveFlashMessage.title').replace(/\{0\}/g, data.extension), successMessage, 15);
+					}
+				}
+			});
+		} else {
+			$('#typo3-extension-manager').unmask();
 		}
 	}
-	return false;
-}
-
-function getResolveDependenciesAndInstallResult(button, dummy, dialog) {
-	if (button == 'yes') {
-		var newUrl = dialog.url;
-		jQuery.ajax({
-			url: newUrl,
-			dataType: 'json',
-			success: function (data) {
-				jQuery('#typo3-extension-manager').unmask();
-				if (data.errorMessage.length) {
-					TYPO3.Flashmessage.display(TYPO3.Severity.error, TYPO3.l10n.localize('extensionList.dependenciesResolveDownloadError.title'), data.errorMessage, 5);
-				} else {
-					var successMessage = TYPO3.l10n.localize('extensionList.dependenciesResolveDownloadSuccess.message').replace(/\{0\}/g, data.extension) + ' <br />';
-					successMessage += '<br /><h3>' + TYPO3.l10n.localize('extensionList.dependenciesResolveDownloadSuccess.header') + ':</h3>';
-					jQuery.each(data.result, function(index, value) {
-						successMessage += TYPO3.l10n.localize('extensionList.dependenciesResolveDownloadSuccess.item') + ' ' + index + ':<br /><ul>';
-						jQuery.each(value, function(extkey, extdata) {
-							successMessage += '<li>' + extkey + '</li>';
-						});
-						successMessage += '</ul>';
-					});
-					TYPO3.Flashmessage.display(TYPO3.Severity.information, TYPO3.l10n.localize('extensionList.dependenciesResolveFlashMessage.title').replace(/\{0\}/g, data.extension), successMessage, 15);
-				}
-			}
-		});
-	} else {
-		jQuery('#typo3-extension-manager').unmask();
-	}
-}
+}(jQuery));
--- typo3/sysext/extensionmanager/Resources/Public/JavaScript/upload.js
+++ typo3/sysext/extensionmanager/Resources/Public/JavaScript/upload.js
@@ -1,30 +1,34 @@
+// IIFE for faster access to $ and save $ use
+(function ($) {
 
-jQuery(document).ready(function() {
-	jQuery('.t3-icon-edit-upload').parent().not('.transformed').each(function () {
-		jQuery(this).data('href', jQuery(this).attr('href'));
-		jQuery(this).attr('href', 'javascript:void(0);');
-		jQuery(this).addClass('transformed');
-		jQuery(this).click(function () {
-			jQuery('.uploadForm').show();
-			jQuery.ajax({
-				url:jQuery(this).data('href'),
-				dataType:'html',
-				success:function (data) {
-					jQuery('.uploadForm').html(data);
-					handleUploadForm();
-				}
+	$(document).ready(function() {
+		$('.t3-icon-edit-upload').parent().not('.transformed').each(function () {
+			$(this).data('href', $(this).attr('href'));
+			$(this).attr('href', '#');
+			$(this).addClass('transformed');
+			$(this).click(function () {
+				$('.uploadForm').show();
+				$.ajax({
+					url:$(this).data('href'),
+					dataType:'html',
+					success:function (data) {
+						$('.uploadForm').html(data);
+						handleUploadForm();
+					}
+				});
 			});
 		});
 	});
-});
 
-function handleUploadForm() {
-	jQuery('#typo3-extensionmanager-upload-target').on('load', function() {
-		var ret = frames['typo3-extensionmanager-upload-target'].document.getElementsByTagName("body")[0].innerHTML;
-		var data = eval("("+ret+")");
-		if (data.success) {
-			TYPO3.Flashmessage.display(TYPO3.Severity.information, TYPO3.l10n.localize('extensionList.uploadFlashMessage.title'), TYPO3.l10n.localize('extensionList.uploadFlashMessage.message').replace(/\{0\}/g, data.extension), 15);
-			location.reload();
-		}
-	})
-}
+	function handleUploadForm() {
+		$('#typo3-extensionmanager-upload-target').on('load', function() {
+			var ret = frames['typo3-extensionmanager-upload-target'].document.getElementsByTagName("body")[0].innerHTML;
+			var data = eval("("+ret+")");
+			if (data.success) {
+				TYPO3.Flashmessage.display(TYPO3.Severity.information, TYPO3.l10n.localize('extensionList.uploadFlashMessage.title'), TYPO3.l10n.localize('extensionList.uploadFlashMessage.message').replace(/\{0\}/g, data.extension), 15);
+				location.reload();
+			}
+		});
+	}
+
+}(jQuery));
