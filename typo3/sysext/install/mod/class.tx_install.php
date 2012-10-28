19a760dee9a232008432829df9c8419309e074cd
Wouter Wolters <typo3@wouterwolters.nl>
2012-10-28 12:36:39 +0100

    [BUGFIX] Illegal string offsets in t3lib_stdgraphic
    Change-Id: I8be45d59b780595e25d6d7f5371300b6506baa2f
    Fixes: #38024
    Releases: 6.0, 4.7, 4.6, 4.5

--- typo3/sysext/install/mod/class.tx_install.php
+++ typo3/sysext/install/mod/class.tx_install.php
@@ -4572,7 +4572,7 @@ REMOTE_ADDR was '".t3lib_div::getIndpEnv('REMOTE_ADDR')."' (".t3lib_div::getIndp
 							if (!@is_file($theFile))	die('Error: '.$theFile.' was not a file');
 
 							$imageProc->imageMagickConvert_forceFileNameBody='read_'.$ext;
-							$fileInfo = $imageProc->imageMagickConvert($theFile,'jpg',"",'',"",'',"",1);
+							$fileInfo = $imageProc->imageMagickConvert($theFile, 'jpg', '', '', '', '', array(), TRUE);
 							$result = $this->displayTwinImage($fileInfo[3],$imageProc->IM_commands);
 							$this->message($headCode,"Read ".strtoupper($ext),$result[0],$result[1]);
 						}
@@ -4584,7 +4584,7 @@ REMOTE_ADDR was '".t3lib_div::getIndpEnv('REMOTE_ADDR')."' (".t3lib_div::getIndp
 						if (!@is_file($theFile))	die('Error: '.$theFile.' was not a file');
 
 						$imageProc->imageMagickConvert_forceFileNameBody='read_pdf';
-						$fileInfo = $imageProc->imageMagickConvert($theFile,'jpg',"170",'',"",'',"",1);
+						$fileInfo = $imageProc->imageMagickConvert($theFile, 'jpg', '170', '', '', '', array(), TRUE);
 						$result = $this->displayTwinImage($fileInfo[3],$imageProc->IM_commands);
 						$this->message($headCode,'Read PDF',$result[0],$result[1]);
 					}
@@ -4594,7 +4594,7 @@ REMOTE_ADDR was '".t3lib_div::getIndpEnv('REMOTE_ADDR')."' (".t3lib_div::getIndp
 						if (!@is_file($theFile))	die('Error: '.$theFile.' was not a file');
 
 						$imageProc->imageMagickConvert_forceFileNameBody='read_ai';
-						$fileInfo = $imageProc->imageMagickConvert($theFile,'jpg',"170",'',"",'',"",1);
+						$fileInfo = $imageProc->imageMagickConvert($theFile, 'jpg', '170', '', '', '', array(), TRUE);
 						$result = $this->displayTwinImage($fileInfo[3],$imageProc->IM_commands);
 						$this->message($headCode,'Read AI',$result[0],$result[1]);
 					}
@@ -4632,7 +4632,7 @@ REMOTE_ADDR was '".t3lib_div::getIndpEnv('REMOTE_ADDR')."' (".t3lib_div::getIndp
 					if (!@is_file($theFile))	die('Error: '.$theFile.' was not a file');
 
 					$imageProc->imageMagickConvert_forceFileNameBody='write_gif';
-					$fileInfo = $imageProc->imageMagickConvert($theFile,'gif',"",'',"",'',"",1);
+					$fileInfo = $imageProc->imageMagickConvert($theFile, 'gif', '', '', '', '', array(), TRUE);
 					if ($GLOBALS['TYPO3_CONF_VARS']['GFX']['gif_compress']) {
 						clearstatcache();
 						$prevSize=t3lib_div::formatSize(@filesize($fileInfo[3]));
@@ -4650,7 +4650,7 @@ REMOTE_ADDR was '".t3lib_div::getIndpEnv('REMOTE_ADDR')."' (".t3lib_div::getIndp
 					$theFile = t3lib_extMgm::extPath('install').'imgs/jesus.gif';
 
 					$imageProc->imageMagickConvert_forceFileNameBody='write_png';
-					$fileInfo = $imageProc->imageMagickConvert($theFile,'png',"",'',"",'',"",1);
+					$fileInfo = $imageProc->imageMagickConvert($theFile, 'png', '', '', '', '', array(), TRUE);
 					$result = $this->displayTwinImage($fileInfo[3],$imageProc->IM_commands);
 					$this->message($headCode,'Write PNG',$result[0],$result[1]);
 				} else {
@@ -4684,7 +4684,7 @@ REMOTE_ADDR was '".t3lib_div::getIndpEnv('REMOTE_ADDR')."' (".t3lib_div::getIndp
 					if (!@is_file($theFile))	die('Error: '.$theFile.' was not a file');
 
 					$imageProc->imageMagickConvert_forceFileNameBody='scale_gif';
-					$fileInfo = $imageProc->imageMagickConvert($theFile,'gif',"150",'',"",'',"",1);
+					$fileInfo = $imageProc->imageMagickConvert($theFile, 'gif', '150', '', '', '', array(), TRUE);
 					if ($GLOBALS['TYPO3_CONF_VARS']['GFX']['gif_compress']) {
 						clearstatcache();
 						$prevSize=t3lib_div::formatSize(@filesize($fileInfo[3]));
@@ -4701,7 +4701,7 @@ REMOTE_ADDR was '".t3lib_div::getIndpEnv('REMOTE_ADDR')."' (".t3lib_div::getIndp
 					if (!@is_file($theFile))	die('Error: '.$theFile.' was not a file');
 
 					$imageProc->imageMagickConvert_forceFileNameBody='scale_png';
-					$fileInfo = $imageProc->imageMagickConvert($theFile,'png',"150",'',"",'',"",1);
+					$fileInfo = $imageProc->imageMagickConvert($theFile, 'png', '150', '', '', '', array(), TRUE);
 					$result = $this->displayTwinImage($fileInfo[3],$imageProc->IM_commands);
 					$this->message($headCode,'PNG to PNG, 150 pixels wide',$result[0],$result[1]);
 
@@ -4709,7 +4709,7 @@ REMOTE_ADDR was '".t3lib_div::getIndpEnv('REMOTE_ADDR')."' (".t3lib_div::getIndp
 					$theFile = t3lib_extMgm::extPath('install').'imgs/jesus2_transp.gif';
 					if (!@is_file($theFile))	die('Error: '.$theFile.' was not a file');
 					$imageProc->imageMagickConvert_forceFileNameBody='scale_jpg';
-					$fileInfo = $imageProc->imageMagickConvert($theFile,'jpg',"150",'',"",'',"",1);
+					$fileInfo = $imageProc->imageMagickConvert($theFile, 'jpg', '150', '', '', '', array(), TRUE);
 					$result = $this->displayTwinImage($fileInfo[3],$imageProc->IM_commands);
 					$this->message($headCode,'GIF to JPG, 150 pixels wide',$result[0],$result[1]);
 				} else {
