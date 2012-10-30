ae62ead5bba4d15fd98846f72ec42a653be95c08
Georg Ringer <georg.ringer@gmail.com>
2012-10-30 12:55:30 +0100

    [TASK] Cleanup indexed_search
    The ext can be cleaned up a bit:
    * Remove not needed/old files
    * Remove namespace compatibility for example files
    
    Change-Id: I50ae60217c186f069908963aa2670d23c1fbe41e
    Resolves: #42513
    Releases: 6.0
    Reviewed-on: http://review.typo3.org/16038
    Reviewed-by: Wouter Wolters
    Reviewed-by: Mattias Nilsson
    Reviewed-by: Philipp Gampe
    Reviewed-by: Felix Kopp
    Reviewed-by: Helmut Hummel
    Tested-by: Helmut Hummel

--- typo3/sysext/indexed_search/example/class.crawlerhook.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*
- * @deprecated since 6.0, the classname tx_indexedsearch_crawlerhook and this file is obsolete
- * and will be removed by 7.0. The class was renamed and is now located at:
- * typo3/sysext/indexed_search/Classes/Example/CrawlerHook.php
- */
-require_once \TYPO3\CMS\Core\Extension\ExtensionManager::extPath('indexed_search') . 'Classes/Example/CrawlerHook.php';
-?>
