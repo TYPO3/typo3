04ad1c656a18c0f36d09774c42bd350cf1838f24
Alexander Schnitzler <alex.schnitzler@typovision.de>
2012-10-31 11:36:16 +0100

    [TASK] Move ObjectStorage from Generic to Persistence
    Actually move the file without adjusting
    the namespace.
    
    Fixes: #42545
    Releases: 6.0
    
    Change-Id: I445e2442231bfed3fbf6132735ad92084f60dca5
    Reviewed-on: http://review.typo3.org/16070
    Reviewed-by: Alexander Schnitzler
    Tested-by: Alexander Schnitzler

---
+++ typo3/sysext/extbase/ext_autoload.php
@@ -0,0 +1,6 @@
+<?php
+
+$extensionClassesPath = t3lib_extMgm::extPath('extbase') . 'Classes/';
+return array(
+	'typo3\cms\extbase\persistence\generic\objectstorage' => $extensionClassesPath . 'Persistence/ObjectStorage.php',
+);
