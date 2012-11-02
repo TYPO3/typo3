a3ed12ec020ba791876df9b1e6b51ae48964b525
Nicole Cordes <n.cordes@biz-design.biz>
2012-11-02 22:23:20 +0100

    [BUGFIX] Missing pid field in ext_tables_static+adt.sql
    In the ext_tables.sql there is a pid field which is left in the
    ext_tables_static+adt.sql file. This causes unnecessary ALTER queries in
    database comparison.
    
    Change-Id: Ia380f731c985a1739a88caf7c7f47fa3ec5a95cf
    Fixes: #42655
    Releases: 6.0

--- typo3/sysext/extensionmanager/ext_tables_static+adt.sql
+++ typo3/sysext/extensionmanager/ext_tables_static+adt.sql
@@ -4,6 +4,7 @@
 DROP TABLE IF EXISTS tx_extensionmanager_domain_model_repository;
 CREATE TABLE tx_extensionmanager_domain_model_repository (
   uid int(11) unsigned NOT NULL auto_increment,
+  pid int(11) unsigned DEFAULT '0' NOT NULL,
   title varchar(150) NOT NULL default '',
   description mediumtext,
   wsdl_url varchar(100) NOT NULL default '',
@@ -14,5 +15,5 @@ CREATE TABLE tx_extensionmanager_domain_model_repository (
 );
 
 
-INSERT INTO tx_extensionmanager_domain_model_repository VALUES ('1', 'TYPO3.org Main Repository', 'Main repository on typo3.org. This repository has some mirrors configured which are available with the mirror url.', 'http://typo3.org/wsdl/tx_ter_wsdl.php', 'http://repositories.typo3.org/mirrors.xml.gz', '1346191200', '0');
+INSERT INTO tx_extensionmanager_domain_model_repository VALUES ('1', '0', 'TYPO3.org Main Repository', 'Main repository on typo3.org. This repository has some mirrors configured which are available with the mirror url.', 'http://typo3.org/wsdl/tx_ter_wsdl.php', 'http://repositories.typo3.org/mirrors.xml.gz', '1346191200', '0');
 
