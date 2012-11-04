71515a7eb0c07f518d1507eb52f0e002f848c8d7
Helmut Hummel <helmut.hummel@typo3.org>
2012-11-04 19:40:03 +0100

    [BUGFIX] Include accidentally removed autoload entries
    Some of the removed entries have no pendant in the
    class alias map, which causes a fatal error e.g. in
    RTE image selector.
    
    Add them again and fix the class definition of
    ImageFolderTree.
    
    Fixes: #42677
    Releases: 6.0
    
    Change-Id: Ib6d6f2045012bac08feb612489a6334d3cb95649
    Reviewed-on: http://review.typo3.org/16193
    Reviewed-by: Anja Leichsenring
    Tested-by: Anja Leichsenring
    Reviewed-by: Wouter Wolters
    Tested-by: Wouter Wolters
    Reviewed-by: Helmut Hummel
    Tested-by: Helmut Hummel

--- typo3/sysext/core/ext_autoload.php
+++ typo3/sysext/core/ext_autoload.php
@@ -7,12 +7,18 @@ $t3libClasses = array(
 	't3lib_install' => PATH_t3lib . 'class.t3lib_install.php',
 	't3lib_readmail' => PATH_t3lib . 'class.t3lib_readmail.php',
 	't3lib_syntaxhl' => PATH_t3lib . 'class.t3lib_syntaxhl.php',
+	't3lib_userauthgroup' => PATH_t3lib . 'class.t3lib_userauthgroup.php',
 	't3lib_xml' => PATH_t3lib . 'class.t3lib_xml.php'
 );
 $typo3Classes = array(
 	'ext_posmap_pages' => PATH_typo3 . 'move_el.php',
 	'ext_posmap_tt_content' => PATH_typo3 . 'move_el.php',
 	'ext_tsparser' => PATH_typo3 . 'wizard_tsconfig.php',
+	'localfoldertree' => PATH_typo3 . 'class.browse_links.php',
+	'rtefoldertree' => PATH_typo3 . 'class.browse_links.php',
+	'rtepagetree' => PATH_typo3 . 'class.browse_links.php',
+	'tbe_foldertree' => PATH_typo3 . 'class.browse_links.php',
+	'tbe_pagetree' => PATH_typo3 . 'class.browse_links.php',
 	'transferdata' => PATH_typo3 . 'show_item.php',
 );
 return array_merge($t3libClasses, $typo3Classes);
