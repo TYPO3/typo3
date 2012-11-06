3c64fff1ba3df51e94478281c439182ff9dae0f3
Michael Klapper <michael.klapper@aoemedia.de>
2012-11-06 16:18:44 +0100

    [BUGFIX] Fix include path in file "t3lib/class.t3lib_cli.php"
    Resolves: #42731
    Releases: 6.0
    
    Change-Id: Icd08c7e9d1bb9f014ce77aae697e804aa4e17625

--- t3lib/class.t3lib_cli.php
+++ t3lib/class.t3lib_cli.php
@@ -4,5 +4,5 @@
  * and will be removed by 7.0. The class was renamed and is now located at:
  * typo3/sysext/cli/Classes/Controller/CommandLineController.php
  */
-require_once \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('core') . 'Classes/Core/Controller/CommandLineController.php';
+require_once \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('core') . 'Classes/Controller/CommandLineController.php';
 ?>
