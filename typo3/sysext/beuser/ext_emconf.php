00f7f89f20ef8c85a9a006f1edf69e7659112148
Steffen Ritter <info@rs-websystems.de>
2012-11-12 22:00:25 +0100

    [TASK] Raise version numbers of system extensions
    Change-Id: I600c93d9e5f5bb0dccd18caab40a5bf03df6c726
    Reviewed-on: http://review.typo3.org/16422
    Reviewed-by: Steffen Ritter
    Tested-by: Steffen Ritter

--- typo3/sysext/beuser/ext_emconf.php
+++ typo3/sysext/beuser/ext_emconf.php
@@ -1,14 +1,14 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "beuser".
-#
-# Auto generated 16-10-2012 14:17
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
+/***************************************************************
+ * Extension Manager/Repository config file for ext "beuser".
+ *
+ * Auto generated 12-11-2012 21:55
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Tools>User Admin',
@@ -34,7 +34,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
 	'version' => '1.1.0',
-	'_md5_values_when_last_written' => 'a:12:{s:19:"class.tx_beuser.php";s:4:"fac2";s:34:"class.tx_beuser_switchbackuser.php";s:4:"f010";s:12:"ext_icon.gif";s:4:"8f11";s:17:"ext_localconf.php";s:4:"c778";s:14:"ext_tables.php";s:4:"21c0";s:12:"doc/TODO.txt";s:4:"02ed";s:14:"mod/beuser.gif";s:4:"87e3";s:13:"mod/clear.gif";s:4:"cc11";s:12:"mod/conf.php";s:4:"fbc9";s:13:"mod/index.php";s:4:"6901";s:17:"mod/locallang.xlf";s:4:"921b";s:21:"mod/locallang_mod.xlf";s:4:"d713";}',
+	'_md5_values_when_last_written' => 'a:12:{s:19:"class.tx_beuser.php";s:4:"fac2";s:34:"class.tx_beuser_switchbackuser.php";s:4:"8052";s:12:"ext_icon.gif";s:4:"8f11";s:17:"ext_localconf.php";s:4:"c778";s:14:"ext_tables.php";s:4:"21c0";s:12:"doc/TODO.txt";s:4:"02ed";s:14:"mod/beuser.gif";s:4:"87e3";s:13:"mod/clear.gif";s:4:"cc11";s:12:"mod/conf.php";s:4:"fbc9";s:13:"mod/index.php";s:4:"6901";s:17:"mod/locallang.xlf";s:4:"921b";s:21:"mod/locallang_mod.xlf";s:4:"d713";}',
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.3.0-0.0.0',
