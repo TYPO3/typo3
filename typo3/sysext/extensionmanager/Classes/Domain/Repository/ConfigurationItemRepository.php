056196064f6962529aa1351a6dd8be8c888cd31d
Nicole Cordes <nicole@cordes.co>
2012-11-17 00:43:41 +0100

    [BUGFIX] Extension Manager doesn't care about configuration sorting
    When showing the configuration of an extension actually the Extension
    Manager doesn't care about the given sorting. This is because there is
    only a merging of configuration arrays.
    
    After merging we have to take care of the value in 'subcat' key and sort
    each subcategorie by those values.
    
    Change-Id: Idb27554be40603c4af7e0506f039d0c0416a0d9a
    Fixes: #43041
    Releases: 6.0

--- typo3/sysext/extensionmanager/Classes/Domain/Repository/ConfigurationItemRepository.php
+++ typo3/sysext/extensionmanager/Classes/Domain/Repository/ConfigurationItemRepository.php
@@ -90,6 +90,21 @@ class ConfigurationItemRepository {
 		foreach ($configuration as $configurationOption) {
 			$hierarchicConfiguration = \TYPO3\CMS\Core\Utility\GeneralUtility::array_merge_recursive_overrule($this->buildConfigurationArray($configurationOption, $extension), $hierarchicConfiguration);
 		}
+
+			// Flip category array as it was merged the other way around
+		$hierarchicConfiguration = array_reverse($hierarchicConfiguration);
+
+			// Sort configurations of each subcategory
+		foreach ($hierarchicConfiguration as &$catConfigurationArray) {
+			foreach ($catConfigurationArray as &$subcatConfigurationArray) {
+				uasort($subcatConfigurationArray, function ($a, $b) {
+					return strnatcmp($a['subcat'], $b['subcat']);
+				});
+			}
+			unset($subcatConfigurationArray);
+		}
+		unset($tempConfiguration);
+
 		$configurationObjectStorage = $this->convertHierarchicArrayToObject(\TYPO3\CMS\Core\Utility\GeneralUtility::array_merge_recursive_overrule($hierarchicConfiguration, $metaInformation));
 		return $configurationObjectStorage;
 	}
