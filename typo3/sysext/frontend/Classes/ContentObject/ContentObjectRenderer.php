89c9bcf69b3e02b684523f5b52cf8de6fc13f6d3
Nicole Cordes <nicole@cordes.co>
2012-11-19 21:45:30 +0100

    [BUGFIX] File relations are broken for import. in IMAGE cObject
    The file.import function can't handle file relations at the moment. This
    is needed when you want to import an image/file from e.g. pages/media. We
    have to change the handling when an integer is returned after resolving
    import. function.
    
    Change-Id: I4699d9e38b9a809fd194d33344678a64168d4300
    Fixes: #43101
    Releases: 6.0

--- typo3/sysext/frontend/Classes/ContentObject/ContentObjectRenderer.php
+++ typo3/sysext/frontend/Classes/ContentObject/ContentObjectRenderer.php
@@ -5009,8 +5009,10 @@ class ContentObjectRenderer {
 	 * @todo Define visibility
 	 */
 	public function getImgResource($file, $fileArray) {
-		if (is_array($fileArray)) {
-			switch ($file) {
+		if (!is_array($fileArray)) {
+			$fileArray = (array) $fileArray;
+		}
+		switch ($file) {
 			case 'GIFBUILDER':
 				$gifCreator = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Frontend\\Imaging\\GifBuilder');
 				$gifCreator->init();
@@ -5036,9 +5038,13 @@ class ContentObjectRenderer {
 								}
 							}
 						}
-						// clean ../ sections of the path and resolve to proper string. This is necessary for the Tx_File_BackwardsCompatibility_TslibContentAdapter to work.
-						$file = \TYPO3\CMS\Core\Utility\GeneralUtility::resolveBackPath($file);
-						$fileObject = \TYPO3\CMS\Core\Resource\ResourceFactory::getInstance()->retrieveFileOrFolderObject($file);
+						if (\TYPO3\CMS\Core\Utility\MathUtility::canBeInterpretedAsInteger($file)) {
+							$fileObject = \TYPO3\CMS\Core\Resource\ResourceFactory::getInstance()->getFileReferenceObject($file)->getOriginalFile();
+						} else {
+							// clean ../ sections of the path and resolve to proper string. This is necessary for the Tx_File_BackwardsCompatibility_TslibContentAdapter to work.
+							$file = \TYPO3\CMS\Core\Utility\GeneralUtility::resolveBackPath($file);
+							$fileObject = \TYPO3\CMS\Core\Resource\ResourceFactory::getInstance()->retrieveFileOrFolderObject($file);
+						}
 					}
 				} catch(\TYPO3\CMS\Core\Resource\Exception $exception) {
 					/** @var \TYPO3\CMS\Core\Log\Logger $logger */
@@ -5095,7 +5101,6 @@ class ContentObjectRenderer {
 					}
 				}
 				break;
-			}
 		}
 		$theImage = $GLOBALS['TSFE']->tmpl->getFileName($file);
 		// If image was processed by GIFBUILDER:
