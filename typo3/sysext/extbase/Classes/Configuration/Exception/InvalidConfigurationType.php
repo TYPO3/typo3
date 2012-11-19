6133535e6542257748b9d3cd3e1ec0605769da5d
Christian Kuhn <lolli@schwarzbu.ch>
2012-11-19 22:05:37 +0100

    [TASK] Comment legacy files to be removed with 6.2
    The 'old' class files before namespace switch are commented
    to be removed with TYPO3 7.0, but they should be scheduled
    for removal with 6.2.
    
    Change-Id: I01bad5874d1bf57e4272f7918557a8f53213cd0e
    Resolves: #43120
    Releases: 6.0
    Reviewed-on: http://review.typo3.org/16586
    Reviewed-by: Christian Kuhn
    Tested-by: Christian Kuhn

--- typo3/sysext/extbase/Classes/Configuration/Exception/InvalidConfigurationType.php
+++ typo3/sysext/extbase/Classes/Configuration/Exception/InvalidConfigurationType.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * @deprecated since 6.0, the classname Tx_Extbase_Configuration_Exception_InvalidConfigurationType and this file is obsolete
- * and will be removed by 7.0. The class was renamed and is now located at:
+ * and will be removed with 6.2. The class was renamed and is now located at:
  * typo3/sysext/extbase/Classes/Configuration/Exception/InvalidConfigurationTypeException.php
  */
 require_once \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('extbase') . 'Classes/Configuration/Exception/InvalidConfigurationTypeException.php';
--- typo3/sysext/extbase/Classes/Property/Exception/InvalidSource.php
+++ typo3/sysext/extbase/Classes/Property/Exception/InvalidSource.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * @deprecated since 6.0, the classname Tx_Extbase_Property_Exception_InvalidTarget and this file is obsolete
- * and will be removed by 7.0. The class was renamed and is now located at:
+ * and will be removed with 6.2. The class was renamed and is now located at:
  * typo3/sysext/extbase/Classes/Property/Exception/InvalidSourceException.php
  */
 require_once \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('extbase') . 'Classes/Property/Exception/InvalidSourceException.php';
