65ccf4aeeda08c4fb1e14fd70a3c2bf762b56e06
Steffen Ritter <info@rs-websystems.de>
2012-11-23 15:11:44 +0100

    [TASK] Allow Folder and Storage to recursively retrieve Files from Driver
    The drivers in FAL are capable of retrieving files recursively.
    This might be sinful in some cases and custom usages. Anyhow,
    the parameter $recursive is not passed up until Storage and
    Folder objects. With that users are forced to work with the
    driver directly.
    
    As it is highly discouraged to work directly on the driver,
    just pass up the parameter within the abstraction layers.
    
    Change-Id: Ibe8f9aa9e906617a42a9a7d5edba43f24f613587
    Releases: 6.0
    Fixes: #43249
    Reviewed-on: http://review.typo3.org/16697
    Reviewed-by: Helmut Hummel
    Tested-by: Helmut Hummel

--- typo3/sysext/core/Tests/Unit/Resource/ResourceStorageTest.php
+++ typo3/sysext/core/Tests/Unit/Resource/ResourceStorageTest.php
@@ -615,6 +615,33 @@ class ResourceStorageTest extends \TYPO3\CMS\Core\Tests\Unit\Resource\BaseTestCa
 		$this->fixture->replaceFile($mockedFile, PATH_site . uniqid());
 	}
 
+	/**
+	 * @test
+	 */
+	public function getFileListHandsOverRecursiveFALSEifNotExplicitlySet() {
+		$this->prepareFixture(array());
+		$driver = $this->createDriverMock(array('basePath' => $this->getMountRootUrl()), $this->fixture, array('getFileList'));
+		$driver->expects($this->once())
+			->method('getFileList')
+			->with($this->anything(), $this->anything(), $this->anything(), $this->anything(), $this->anything(), FALSE)
+			->will($this->returnValue(array()));
+		$this->fixture->getFileList('/');
+	}
+
+	/**
+	 * @test
+	 */
+	public function getFileListHandsOverRecursiveTRUEifSet() {
+
+		$this->prepareFixture(array());
+		$driver = $this->createDriverMock(array('basePath' => $this->getMountRootUrl()), $this->fixture, array('getFileList'));
+		$driver->expects($this->once())
+			->method('getFileList')
+			->with($this->anything(), $this->anything(), $this->anything(), $this->anything(), $this->anything(), TRUE)
+			->will($this->returnValue(array()));
+		$this->fixture->getFileList('/', 0, 0, TRUE, TRUE, TRUE);
+	}
+
 }
 
 ?>
