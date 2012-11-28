739d67a155bd5b53d20b8b84d6333c04349322eb
Alexander Schnitzler <alex.schnitzler@typovision.de>
2012-11-28 12:42:53 +0100

    [TASK] Update version to 6.1.0-dev
    Releases: 6.1
    Resolves: #43388
    Change-Id: I15df3b3084094b10abd3fc6c251529829e393e7e
    Reviewed-on: http://review.typo3.org/16835
    Reviewed-by: Wouter Wolters
    Reviewed-by: Mattias Nilsson
    Reviewed-by: Anja Leichsenring
    Tested-by: Anja Leichsenring

--- typo3/sysext/fluid/ext_emconf.php
+++ typo3/sysext/fluid/ext_emconf.php
@@ -1,13 +1,13 @@
 <?php
-########################################################################
-# Extension Manager/Repository config file for ext "fluid".
-#
-# Auto generated 11-10-2011 11:47
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
+/***********************************************************************
+ * Extension Manager/Repository config file for ext "fluid".
+ *
+ * Auto generated 11-10-2011 11:47
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ **********************************************************************/
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Fluid Templating Engine',
 	'description' => 'Fluid is a next-generation templating engine which makes the life of extension authors a lot easier!',
@@ -27,7 +27,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '6.0.0',
+	'version' => '6.1.0-dev',
 	'constraints' => array(
 		'depends' => array(
 			'extbase' => '6.0.0'
