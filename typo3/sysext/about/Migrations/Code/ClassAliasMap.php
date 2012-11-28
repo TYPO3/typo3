4cfdd06e0b9f0e80acd81b4267f762c7ae801d70
Wouter Wolters <typo3@wouterwolters.nl>
2012-11-28 22:06:00 +0100

    [TASK] Remove deprecated code from "about" and "aboutmodules"
    Remove deprecated code from "about" and "aboutmodules"
    
    Change-Id: I2296d4039ce8ffd16a39c3643365886b1f696a69
    Resolves: #44761
    Releases: 6.1

--- typo3/sysext/about/Migrations/Code/ClassAliasMap.php
+++ typo3/sysext/about/Migrations/Code/ClassAliasMap.php
@@ -1,7 +1,6 @@
 <?php
 return array(
 	'Tx_About_Controller_AboutController' => 'TYPO3\\CMS\\About\\Controller\\AboutController',
-	'tx_about_customSections' => 'TYPO3\\CMS\\About\\CustomSectionsInterface',
 	'Tx_About_Domain_Model_Extension' => 'TYPO3\\CMS\\About\\Domain\\Model\\Extension',
 	'Tx_About_Domain_Repository_ExtensionRepository' => 'TYPO3\\CMS\\About\\Domain\\Repository\\ExtensionRepository',
 	'Tx_About_ViewHelpers_SkinImageViewHelper' => 'TYPO3\\CMS\\About\\ViewHelpers\\SkinImageViewHelper',
--- typo3/sysext/aboutmodules/ext_autoload.php
+++
@@ -1,6 +0,0 @@
-<?php
-$extensionPath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('aboutmodules');
-return array(
-	'tx_aboutmodules_functions' => $extensionPath . 'class.tx_aboutmodules_functions.php'
-);
-?>
