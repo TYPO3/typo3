06ef4b8647cadcf598f764fb1b44aa9cf0df82f9
Andreas Lappe <nd@off-pist.de>
2012-11-30 00:00:02 +0100

    [TASK] ext:form Unit test all filters
    Add several tests for each filter of the form extension.
    
    Resolves: #43473
    Releases: 6.0
    
    Change-Id: I1c5ebf074cb68cbab12b3ea25cd895853284f686

---
+++ typo3/sysext/form/Tests/Unit/Filter/DigitFilterTest.php
@@ -0,0 +1,69 @@
+<?php
+namespace TYPO3\CMS\Form\Tests\Unit\Filter;
+
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2012 Andreas Lappe <nd@kaeufli.ch>, kaeufli.ch
+*
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+/**
+ * Test case
+ *
+ * @author Andreas Lappe <nd@kaeufli.ch>
+ */
+class DigitFilterTest extends \TYPO3\CMS\Core\Tests\UnitTestCase {
+
+	/**
+	 * @var \TYPO3\CMS\Form\Filter\DigitFilter
+	 */
+	protected $fixture;
+
+	public function setUp() {
+		$this->fixture = new \TYPO3\CMS\Form\Filter\DigitFilter();
+	}
+
+	public function tearDown() {
+		unset($this->fixture);
+	}
+
+	public function validDataProvider() {
+		return array(
+			'1,00 -> 100' => array('1,00', '100'),
+			'1E+49 -> 149' => array('1E+49', '149'),
+			'100 -> 100' => array('100', '100'),
+			'00000 -> 00000' => array('00000', '00000'),
+			'ABCD -> ""' => array('ABCD', ''),
+		);
+	}
+
+	/**
+	 * @test
+	 * @dataProvider validDataProvider
+	 */
+	public function filterForStringsReturnsStringsFilteredToOnlyContainDigits($input, $expected) {
+		$this->assertSame(
+			$expected,
+			$this->fixture->filter($input)
+		);
+	}
+}
+?>
---
+++ typo3/sysext/form/Tests/Unit/Filter/RegExpFilterTest.php
@@ -0,0 +1,80 @@
+<?php
+namespace TYPO3\CMS\Form\Tests\Unit\Filter;
+
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2012 Andreas Lappe <nd@kaeufli.ch>, kaeufli.ch
+*
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+/**
+ * Test case
+ *
+ * @author Andreas Lappe <nd@kaeufli.ch>
+ */
+class RegExpFilterTest extends \TYPO3\CMS\Core\Tests\UnitTestCase {
+
+	/**
+	 * @var \TYPO3\CMS\Form\Filter\RegExpFilter
+	 */
+	protected $fixture;
+
+	public function setUp() {
+		$this->fixture = new \TYPO3\CMS\Form\Filter\RegExpFilter();
+	}
+
+	public function tearDown() {
+		unset($this->fixture);
+	}
+
+	public function dataProvider() {
+		return array(
+			'a-a -> aa for /-/' => array(
+				'a-a',
+				'/-/',
+				'aa'
+			),
+			'aaa -> "" for /.+/' => array(
+				'aaa',
+				'/.+/',
+				''
+			),
+			'aAa -> aa for /[^a]+/' => array(
+				'aAa',
+				'/[^a]+/',
+				'aa'
+			),
+		);
+	}
+
+	/**
+	 * @test
+	 * @dataProvider dataProvider
+	 */
+	public function filterForStringReturnsInputWithoutCharactersMatchedByRegularExpression($input, $regularExpression, $expected) {
+		$this->fixture->setRegularExpression($regularExpression);
+		$this->assertSame(
+			$expected,
+			$this->fixture->filter($input)
+		);
+	}
+}
+?>
