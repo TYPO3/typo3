5868e1f59f782ae770a4e445982f2dabccbc3940
Christian Kuhn <lolli@schwarzbu.ch>
2012-11-29 19:08:34 +0100

    [BUGFIX] ContentObjectRenderer instantiates old class names
    The content object renderer still instantiates content objects with
    tslib_content_ prefix. The patch changes this to the namespaced
    class. Additionally, all content objects are now tested for successful
    instantiation with additional unit tests. While creating those tests
    it became obvious that the ShockwaveFlashObject content object was
    named incorrectly, so this is adapted along the way.
    
    Change-Id: Ie12474df270b29e8e2d8d8894fa94d24747d94d8
    Fixes: #43456
    Releases: 6.1, 6.0
    Reviewed-on: http://review.typo3.org/16863
    Reviewed-by: Christian Kuhn
    Tested-by: Christian Kuhn

--- typo3/sysext/cms/tslib/content/class.tslib_content_shockwaveflashobject.php
+++ typo3/sysext/cms/tslib/content/class.tslib_content_shockwaveflashobject.php
@@ -2,7 +2,7 @@
 /*
  * @deprecated since 6.0, the classname tslib_content_ShockwaveFlashObject and this file is obsolete
  * and will be removed with 6.2. The class was renamed and is now located at:
- * typo3/sysext/frontend/Classes/ContentObject/ShockwaveFlashContentObject.php
+ * typo3/sysext/frontend/Classes/ContentObject/ShockwaveFlashObjectContentObject.php
  */
-require_once \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('frontend') . 'Classes/ContentObject/ShockwaveFlashContentObject.php';
+require_once \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('frontend') . 'Classes/ContentObject/ShockwaveFlashObjectContentObject.php';
 ?>
