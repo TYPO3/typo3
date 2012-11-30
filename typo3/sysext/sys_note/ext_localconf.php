0a87f6549f17235312d5146af9a3c49ed7a2a6d9
Kai Vogel <kai.vogel@speedprogs.de>
2012-11-30 23:35:12 +0100

    [FEATURE] Rebuild sys_note to match extbase scheme
    This patch extends the sys_note extension and adds a controller and
    model. Additionally it rewrites the repository to use the extbase
    persistance layer. And it provides the possibility to modify the view
    configuration via TypoScript.
    
    Resolves: #43495
    Releases: 6.1
    
    Change-Id: I372e9fe0761875cd8967e920417d89cd5ad379b9

--- typo3/sysext/sys_note/ext_localconf.php
+++ typo3/sysext/sys_note/ext_localconf.php
@@ -2,6 +2,14 @@
 if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
+
+// Register "switchableControllerActions" manually because it exists no plugin or module for sys_note
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['extbase']['extensions']['SysNote']['modules']['Note']['controllers'] = array(
+	'Note' => array(
+		'actions' => array('list')
+	)
+);
+
 // Hook into the list module
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['recordlist/mod1/index.php']['drawFooterHook'][$_EXTKEY] = 'EXT:' . $_EXTKEY . '/Classes/Hook/RecordListHook.php:TYPO3\\CMS\\SysNote\\Hook\\RecordListHook->render';
 // Hook into the page module
