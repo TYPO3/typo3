08c5b674c32b491954d517d278ace8519151a059
Dmitry Dulepov <dmitry@typo3.org>
2012-12-03 13:40:25 +0400

    [BUGFIX] TS is fetched from cache incorrectly sometimes
    If $TYPO3_CONF_VARS['FE']['defaultTypoScript_constants'] or
    $TYPO3_CONF_VARS['FE']['defaultTypoScript_setup'] are set in the Install
    tool, cached TS will not be found in cache. This would result in TS
    parsing every time when there are USER_INT objects on the page.
    Generally it will slow down the page with USER_INT objects by at least
    300% comparing to the cached version.
    
    Change-Id: Ic46e32e7012ee9b038c11c1e6ea8e66512aed62b
    Resolves: #43540
    Releases: 4.5, 4.6, 4.7, 6.0, 6.1

--- t3lib/class.t3lib_tstemplate.php
+++ t3lib/class.t3lib_tstemplate.php
@@ -182,6 +182,7 @@ class t3lib_TStemplate {
 	var $frames = array(); // Keys are frame names and values are type-values, which must be used to refer correctly to the content of the frames.
 	var $MPmap = ''; // Contains mapping of Page id numbers to MP variables.
 
+	protected $defaultTSAdded = FALSE;	// TRUE after the default TS is added
 
 	/**
 	 * Initialize
@@ -357,7 +358,14 @@ class t3lib_TStemplate {
 			if ($setupData && !$this->forceTemplateParsing) {
 					// If TypoScript setup structure was cached we unserialize it here:
 				$this->setup = unserialize($setupData);
+				if ($this->tt_track) {
+					$GLOBALS['TT']->setTSLogMessage('Using cached TS template data');
+				}
 			} else {
+				if ($this->tt_track) {
+					$GLOBALS['TT']->setTSLogMessage('Not using any cached TS data');
+				}
+
 					// Make configuration
 				$this->generateConfig();
 
@@ -447,6 +455,7 @@ class t3lib_TStemplate {
 		$this->rowSum = array();
 		$this->hierarchyInfoToRoot = array();
 		$this->absoluteRootLine = $theRootLine; // Is the TOTAL rootline
+		$this->defaultTSAdded = FALSE;
 
 		reset($this->absoluteRootLine);
 		$c = count($this->absoluteRootLine);
@@ -479,6 +488,14 @@ class t3lib_TStemplate {
 			$GLOBALS['TYPO3_DB']->sql_free_result($res);
 			$this->rootLine[] = $this->absoluteRootLine[$a];
 		}
+
+		if (!$this->defaultTSAdded) {
+			array_unshift($this->constants, '' . $GLOBALS['TYPO3_CONF_VARS']['FE']['defaultTypoScript_constants']); // Adding default TS/constants
+			array_unshift($this->config, '' . $GLOBALS['TYPO3_CONF_VARS']['FE']['defaultTypoScript_setup']); // Adding default TS/setup
+			array_unshift($this->editorcfg, '' . $GLOBALS['TYPO3_CONF_VARS']['FE']['defaultTypoScript_editorcfg']); // Adding default TS/editorcfg
+			$this->defaultTSAdded = TRUE;
+		}
+
 		$this->processIncludes();
 	}
 
@@ -783,9 +800,12 @@ class t3lib_TStemplate {
 	 */
 	function generateConfig() {
 			// Add default TS for all three code types:
-		array_unshift($this->constants, '' . $GLOBALS['TYPO3_CONF_VARS']['FE']['defaultTypoScript_constants']); // Adding default TS/constants
-		array_unshift($this->config, '' . $GLOBALS['TYPO3_CONF_VARS']['FE']['defaultTypoScript_setup']); // Adding default TS/setup
-		array_unshift($this->editorcfg, '' . $GLOBALS['TYPO3_CONF_VARS']['FE']['defaultTypoScript_editorcfg']); // Adding default TS/editorcfg
+		if (!$this->defaultTSAdded) {
+			array_unshift($this->constants, '' . $GLOBALS['TYPO3_CONF_VARS']['FE']['defaultTypoScript_constants']); // Adding default TS/constants
+			array_unshift($this->config, '' . $GLOBALS['TYPO3_CONF_VARS']['FE']['defaultTypoScript_setup']); // Adding default TS/setup
+			array_unshift($this->editorcfg, '' . $GLOBALS['TYPO3_CONF_VARS']['FE']['defaultTypoScript_editorcfg']); // Adding default TS/editorcfg
+			$this->defaultTSAdded = TRUE;
+		}
 
 			// Parse the TypoScript code text for include-instructions!
 		$this->processIncludes();
