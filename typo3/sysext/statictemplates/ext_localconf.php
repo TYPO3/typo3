8a2152829c7a9557a0a9ca159e32da2d152a7113
Stefan Neufeind <typo3.neufeind@speedpartner.de>
2012-12-07 01:09:07 +0100

    [BUGFIX] Adjust hooks after moving/namespacing
    Change-Id: I4e8de22ee8b04bcee8dabbc3bce1203f68146f51
    Fixes: #43676
    Releases: 6.0, 6.1

--- typo3/sysext/statictemplates/ext_localconf.php
+++ typo3/sysext/statictemplates/ext_localconf.php
@@ -2,5 +2,5 @@
 if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tstemplate.php']['includeStaticTypoScriptSources'][] = 'EXT:statictemplates/class.tx_statictemplates.php:tx_statictemplates->includeStaticTypoScriptSources';
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tstemplate.php']['includeStaticTypoScriptSources'][] = 'EXT:statictemplates/Classes/StaticTemplatesHook.php:TYPO3\\CMS\\Statictemplates\\StaticTemplatesHook->includeStaticTypoScriptSources';
 ?>
