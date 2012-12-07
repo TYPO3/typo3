719f9fec9b95bbf06870b33a49fd171e3b141bb6
Stefan Neufeind <typo3.neufeind@speedpartner.de>
2012-12-07 01:09:07 +0100

    [BUGFIX] Adjust hooks after moving/namespacing
    Change-Id: I4e8de22ee8b04bcee8dabbc3bce1203f68146f51
    Fixes: #43676
    Releases: 6.0, 6.1
    Reviewed-on: https://review.typo3.org/17481
    Reviewed-by: Helmut Hummel
    Tested-by: Helmut Hummel

--- typo3/sysext/feedit/ext_localconf.php
+++ typo3/sysext/feedit/ext_localconf.php
@@ -3,5 +3,5 @@ if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
 	// Register the edit panel view.
-$TYPO3_CONF_VARS['SC_OPTIONS']['typo3/classes/class.frontendedit.php']['edit'] = 'EXT:feedit/Classes/FrontendEditPanel.php:FrontendEditPanel';
+$TYPO3_CONF_VARS['SC_OPTIONS']['typo3/classes/class.frontendedit.php']['edit'] = 'EXT:feedit/Classes/FrontendEditPanel.php:TYPO3\\CMS\\Feedit\\FrontendEditPanel';
 ?>
