e7630c78d53ce3530e039e5cd32e71500b4f0be4
Philipp Gampe <philipp.gampe@typo3.org>
2012-12-09 16:19:23 +0100

    [BUGFIX] Missing Task.php
    During Ibef1d71f1d3a6aa3683331464b33118b88b1ec01 the file
    Classes/Task.php has been renamed to Classes/Task/AbstractTask.php, but
    the change has not been reflected in the old v4 filename.
    
    This changes the filename in the legacy file too.
    
    Fixes: #43715
    Related: #42803
    Releases: 6.0, 6.1
    
    Change-Id: Iefcf0d03b07aa30990015cacec39d50fa987df50

--- typo3/sysext/scheduler/class.tx_scheduler_task.php
+++ typo3/sysext/scheduler/class.tx_scheduler_task.php
@@ -4,5 +4,5 @@
  * and will be removed with 6.2. The class was renamed and is now located at:
  * typo3/sysext/scheduler/Classes/Task.php
  */
-require_once \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('scheduler') . 'Classes/Task.php';
+require_once \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('scheduler') . 'Classes/Task/AbstractTask.php';
 ?>
