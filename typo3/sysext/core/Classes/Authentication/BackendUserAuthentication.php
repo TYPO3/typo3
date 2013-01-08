b30b4af743f0e6a3145208468b3efbef6f553c16
Georg Ringer <georg.ringer@gmail.com>
2013-01-08 11:15:09 +0100

    [BUGFIX] LocalDriver needs to respect "is_writeable"
    The LocalDriver needs also to respect the flag "is_writeable".
    
    This means: if the flag is not set, no folder/file can be edited/moved....
    
    Change-Id: I6fc1edd452555381388bd5bfaa46dd8241dc853b
    Fixes: #44370
    Releases: 6.0,6.1

--- typo3/sysext/core/Classes/Authentication/BackendUserAuthentication.php
+++ typo3/sysext/core/Classes/Authentication/BackendUserAuthentication.php
@@ -1714,8 +1714,18 @@ class BackendUserAuthentication extends \TYPO3\CMS\Core\Authentication\AbstractU
 		$defaultFilePermissions = $this->getFilePermissions();
 		$storagePermissionsArray = $this->getTSConfig('permissions.file.storage.' . $storageObject->getUid());
 		$storageFilePermissions = $storagePermissionsArray['properties'];
+
+		$driver = $storageObject->getStorageRecord();
+		if ($driver['is_writable'] == 0) {
+			foreach($defaultFilePermissions as $permission => $value) {
+				if ($permission !== 'readFile' && $permission !== 'browseFolder') {
+					$defaultFilePermissions[$permission] = 0;
+				}
+			}
+		}
+
 		if (is_array($storageFilePermissions) && count($storageFilePermissions)) {
-			return array_merge($storageFilePermissions, $defaultFilePermissions);
+			return array_merge($defaultFilePermissions, $storageFilePermissions);
 		} else {
 			return $defaultFilePermissions;
 		}
