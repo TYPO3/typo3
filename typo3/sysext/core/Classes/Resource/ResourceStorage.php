40db1fe592ec73abc26c4d5541f3615d5ae57ffc
Georg Ringer <georg.ringer@gmail.com>
2013-01-10 10:37:21 +0100

    [BUGFIX] LocalDriver needs to respect "is_writeable"
    The LocalDriver needs also to respect the flag "is_writeable".
    This means: if the flag is not set, no folder/file can be edited/moved...
    
    Change-Id: I6fc1edd452555381388bd5bfaa46dd8241dc853b
    Fixes: #44370
    Releases: 6.0,6.1

--- typo3/sysext/core/Classes/Resource/ResourceStorage.php
+++ typo3/sysext/core/Classes/Resource/ResourceStorage.php
@@ -579,7 +579,7 @@ class ResourceStorage {
 			$isReadCheck = TRUE;
 		}
 		$isWriteCheck = FALSE;
-		if (in_array($action, array('write', 'delete'))) {
+		if (in_array($action, array('add', 'edit', 'write', 'upload', 'move', 'rename', 'unzip', 'remove'))) {
 			$isWriteCheck = TRUE;
 		}
 		// Check 3: Check the capabilities of the storage (and the driver)
@@ -625,7 +625,7 @@ class ResourceStorage {
 			$isReadCheck = TRUE;
 		}
 		$isWriteCheck = FALSE;
-		if (in_array($action, array('write', 'delete', 'deleteRecursive'))) {
+		if (in_array($action, array('add', 'move', 'write', 'remove', 'rename'))) {
 			$isWriteCheck = TRUE;
 		}
 		// Check 3: Check the capabilities of the storage (and the driver)
