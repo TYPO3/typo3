99b65274acf99794bad89ed03b713c01578d66fb
Helmut Hummel <helmut.hummel@typo3.org>
2013-01-13 18:24:51 +0100

    [BUGFIX] Fix incomplete mocking in ResourceStorage test
    Due to a not mocked FileRepository object, a warning
    is issued which leads to a failed test.
    
    Add a FileRepository mock and clean up all usage
    of the FileRepository in ResourceStorage.
    
    Fixes: #44439
    Releases: 6.0, 6.1
    
    Change-Id: I214fce4635d452764bd9edc8e11f863bd76961a6
    Reviewed-on: https://review.typo3.org/17508
    Reviewed-by: Helmut Hummel
    Tested-by: Helmut Hummel

--- typo3/sysext/core/Tests/Unit/Resource/ResourceStorageTest.php
+++ typo3/sysext/core/Tests/Unit/Resource/ResourceStorageTest.php
@@ -47,6 +47,11 @@ class ResourceStorageTest extends \TYPO3\CMS\Core\Tests\Unit\Resource\BaseTestCa
 		parent::setUp();
 		$this->singletonInstances = \TYPO3\CMS\Core\Utility\GeneralUtility::getSingletonInstances();
 		\TYPO3\CMS\Core\Utility\GeneralUtility::purgeInstances();
+		\TYPO3\CMS\Core\Utility\GeneralUtility::setSingletonInstance(
+			'TYPO3\\CMS\\Core\\Resource\\FileRepository',
+			$this->getMock('TYPO3\\CMS\\Core\\Resource\\FileRepository')
+		);
+
 	}
 
 	public function tearDown() {
