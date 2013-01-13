81185370de3f721a9fb89e5b2dc269deef143e32
Stefan Neufeind <typo3.neufeind@speedpartner.de>
2013-01-13 14:57:44 +0100

    [BUGFIX] Sending fails on multiple email-addresses
    Specifying multiple receipients (To or CC) for emails
    leads to an error upon form-submit without further explanation.
    This patch implements validation for multiple email-addresses of
    which only the valid ones are then returned.
    The sending-API can already take multiple recipients.
    
    Change-Id: I59b65adc4e09d298167738778c17d310b8ecce47
    Fixes: #44485
    Releases: 4.6, 4.7, 6.0, 6.1

--- typo3/sysext/form/Classes/PostProcess/MailPostProcessor.php
+++ typo3/sysext/form/Classes/PostProcess/MailPostProcessor.php
@@ -145,6 +145,28 @@ class MailPostProcessor implements \TYPO3\CMS\Form\PostProcess\PostProcessorInte
 	}
 
 	/**
+	 * Filter input-string for valid email-addresses
+	 *
+	 * @param string $emails If this is a string, it will be checked for one or more valid email addresses.
+	 * @return array List of valid email-addresses
+	 */
+	protected function filterValidEmails($emails) {
+		if (!is_string($emails)) {
+			// no valid addresses - empty list
+			return array();
+		}
+
+		$validEmails = array();
+		$emailsList = \TYPO3\CMS\Core\Utility\GeneralUtility::trimExplode(',', $emails);
+		foreach ($emailsList as $checkEmail) {
+			if (\TYPO3\CMS\Core\Utility\GeneralUtility::validEmail($checkEmail)) {
+				$validEmails[] = $checkEmail;
+			}
+		}
+		return $validEmails;
+	}
+
+	/**
 	 * Adds the receiver of the mail message when configured
 	 *
 	 * Checks the address if it is a valid email address
@@ -152,8 +174,9 @@ class MailPostProcessor implements \TYPO3\CMS\Form\PostProcess\PostProcessorInte
 	 * @return void
 	 */
 	protected function setTo() {
-		if ($this->typoScript['recipientEmail'] && \TYPO3\CMS\Core\Utility\GeneralUtility::validEmail($this->typoScript['recipientEmail'])) {
-			$this->mailMessage->setTo($this->typoScript['recipientEmail']);
+		$validEmails = $this->filterValidEmails($this->typoScript['recipientEmail']);
+		if(count($validEmails) > 0) {
+			$this->mailMessage->setTo($validEmails);
 		}
 	}
 
@@ -165,8 +188,9 @@ class MailPostProcessor implements \TYPO3\CMS\Form\PostProcess\PostProcessorInte
 	 * @return void
 	 */
 	protected function setCc() {
-		if ($this->typoScript['ccEmail'] && \TYPO3\CMS\Core\Utility\GeneralUtility::validEmail($this->typoScript['ccEmail'])) {
-			$this->mailMessage->AddCc(trim($this->typoScript['ccEmail']));
+		$validEmails = $this->filterValidEmails($this->typoScript['ccEmail']);
+		if(count($validEmails) > 0) {
+			$this->mailMessage->setCc($validEmails);
 		}
 	}
 
