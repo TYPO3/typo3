43bcc9c52756766bf63a0af9e143a6c687d732f8
Francois Suter <francois@typo3.org>
2013-01-22 23:16:28 +0100

    [BUGFIX] Missing subcategories labels in EM
    The (new) Extension Manager correctly uses subcategories for sorting
    and grouping extension configuration options (in the configuration
    form), but does not display the related labels. This makes the form
    rather weird and unhelpful. This information must be restored.
    
    Resolves: #44701
    Releases: 6.0, 6.1
    Change-Id: I0dde1d294edc403404ad39985f91e3772ef0b710

--- typo3/sysext/extensionmanager/Classes/Domain/Model/ConfigurationSubcategory.php
+++ typo3/sysext/extensionmanager/Classes/Domain/Model/ConfigurationSubcategory.php
@@ -26,8 +26,13 @@ namespace TYPO3\CMS\Extensionmanager\Domain\Model;
  *
  *  This copyright notice MUST APPEAR in all copies of the script!
  ***************************************************************/
+
 /**
- * Model for configuration subcategories
+ * Model for configuration sub categories
+ *
+ * Configuration options can be structured with categories and sub categories.
+ * Categories are usually displayed as tabs and sub categories are used to
+ * group configuration items in one tab.
  *
  * @author Susanne Moog <typo3@susannemoog.de>
  */
@@ -37,6 +42,11 @@ class ConfigurationSubcategory extends \TYPO3\CMS\Extbase\DomainObject\AbstractE
 	 * @var string
 	 */
 	protected $name = '';
+
+	/**
+	 * @var string The sub category label
+	 */
+	protected $label = '';
 
 	/**
 	 * @var \TYPO3\CMS\Extbase\Persistence\ObjectStorage<\TYPO3\CMS\Extensionmanager\Domain\Model\ConfigurationItem>
@@ -88,6 +98,25 @@ class ConfigurationSubcategory extends \TYPO3\CMS\Extbase\DomainObject\AbstractE
 	 */
 	public function getName() {
 		return $this->name;
+	}
+
+	/**
+	 * Set sub category label
+	 *
+	 * @param string $label
+	 * @return void
+	 */
+	public function setLabel($label) {
+		$this->label = $label;
+	}
+
+	/**
+	 * Get sub category label
+	 *
+	 * @return string
+	 */
+	public function getLabel() {
+		return $this->label;
 	}
 
 }
