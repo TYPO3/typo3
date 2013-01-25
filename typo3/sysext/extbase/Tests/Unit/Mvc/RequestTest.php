41e918f4ff25454c6b3ee397c8d25fc7ce661027
Alexander Schnitzler <alex.schnitzler@typovision.de>
2013-01-25 13:48:02 +0100

    [TASK] Improve code coverage of Mvc/Request
    Currently two important methods "getControllerObjectName"
    and "setControllerObjectName" aren't tested at all. This
    patch provides tests for both.
    
    Releases: 6.1
    Resolves: #44805
    Change-Id: I3bcbfc72c5b77da3d876489dd4423c79823171c1
    Reviewed-on: https://review.typo3.org/17702
    Reviewed-by: Daniel Hürtgen
    Tested-by: Daniel Hürtgen
    Reviewed-by: Christian Kuhn
    Tested-by: Christian Kuhn

--- typo3/sysext/extbase/Tests/Unit/Mvc/RequestTest.php
+++ typo3/sysext/extbase/Tests/Unit/Mvc/RequestTest.php
@@ -1,25 +1,32 @@
 <?php
 namespace TYPO3\CMS\Extbase\Tests\Unit\Mvc;
 
-/*                                                                        *
- * This script belongs to the Extbase framework.                            *
- *                                                                        *
- * It is free software; you can redistribute it and/or modify it under    *
- * the terms of the GNU Lesser General Public License as published by the *
- * Free Software Foundation, either version 3 of the License, or (at your *
- * option) any later version.                                             *
- *                                                                        *
- * This script is distributed in the hope that it will be useful, but     *
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHAN-    *
- * TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser       *
- * General Public License for more details.                               *
- *                                                                        *
- * You should have received a copy of the GNU Lesser General Public       *
- * License along with the script.                                         *
- * If not, see http://www.gnu.org/licenses/lgpl.html                      *
- *                                                                        *
- * The TYPO3 project - inspiring people to share!                         *
- *                                                                        */
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2010-2012 Extbase Team (http://forge.typo3.org/projects/typo3v4-mvc)
+ *  Extbase is a backport of TYPO3 Flow. All credits go to the TYPO3 Flow team.
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *  A copy is found in the textfile GPL.txt and important notices to the license
+ *  from the author is found in LICENSE.txt distributed with these scripts.
+ *
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
 /**
  * Testcase for the MVC Generic Request
  *
@@ -145,6 +152,16 @@ class RequestTest extends \TYPO3\CMS\Extbase\Tests\Unit\BaseTestCase {
 	/**
 	 * @test
 	 */
+	public function setArgumentShouldSetVendorIfVendorIsGiven() {
+		$request = $this->getMock('TYPO3\\CMS\\Extbase\\Mvc\\Request', array('setFormat'));
+		$request->expects($this->any())->method('setVendor')->with('VENDOR');
+		$request->setArgument('@vendor', 'VENDOR');
+		$this->assertFalse($request->hasArgument('@vendor'));
+	}
+
+	/**
+	 * @test
+	 */
 	public function internalArgumentsShouldNotBeReturnedAsNormalArgument() {
 		$request = new \TYPO3\CMS\Extbase\Mvc\Request();
 		$request->setArgument('__referrer', 'foo');
@@ -245,6 +262,110 @@ class RequestTest extends \TYPO3\CMS\Extbase\Tests\Unit\BaseTestCase {
 		$this->assertFalse($request->isDispatched());
 		$request->setDispatched(TRUE);
 		$this->assertTrue($request->isDispatched());
+	}
+
+	/**
+	 * DataProvider for explodeObjectControllerName
+	 *
+	 * @return array
+	 */
+	public function controllerArgumentsAndExpectedObjectName() {
+		return array(
+			'Vendor TYPO3\CMS, extension, controller given' => array(
+				array(
+					'vendorName' => 'TYPO3\\CMS',
+					'extensionName' => 'Ext',
+					'subpackageKey' => '',
+					'controllerName' => 'Foo',
+				),
+				'TYPO3\\CMS\\Ext\\Controller\\FooController',
+			),
+			'Vendor TYPO3\CMS, extension, subpackage, controlle given' => array(
+				array(
+					'vendorName' => 'TYPO3\\CMS',
+					'extensionName' => 'Fluid',
+					'subpackageKey' => 'ViewHelpers\\Widget',
+					'controllerName' => 'Paginate',
+				),
+				'TYPO3\\CMS\\Fluid\\ViewHelpers\\Widget\\Controller\\PaginateController',
+			),
+			'Vendor VENDOR, extension, controller given' => array(
+				array(
+					'vendorName' => 'VENDOR',
+					'extensionName' => 'Ext',
+					'subpackageKey' => '',
+					'controllerName' => 'Foo',
+				),
+				'VENDOR\\Ext\\Controller\\FooController',
+			),
+			'Vendor VENDOR, extension subpackage, controller given' => array(
+				array(
+					'vendorName' => 'VENDOR',
+					'extensionName' => 'Ext',
+					'subpackageKey' => 'ViewHelpers\\Widget',
+					'controllerName' => 'Foo',
+				),
+				'VENDOR\\Ext\\ViewHelpers\\Widget\\Controller\\FooController',
+			),
+			'No vendor, extension, controller given' => array(
+				array(
+					'vendorName' => NULL,
+					'extensionName' => 'Ext',
+					'subpackageKey' => '',
+					'controllerName' => 'Foo',
+				),
+				'Tx_Ext_Controller_FooController',
+			),
+			'No vendor, extension, subpackage, controller given' => array(
+				array(
+					'vendorName' => NULL,
+					'extensionName' => 'Fluid',
+					'subpackageKey' => 'ViewHelpers_Widget',
+					'controllerName' => 'Paginate',
+				),
+				'Tx_Fluid_ViewHelpers_Widget_Controller_PaginateController',
+			),
+		);
+	}
+
+	/**
+	 * @dataProvider controllerArgumentsAndExpectedObjectName
+	 *
+	 * @param array $controllerArguments
+	 * @param string $controllerObjectName
+	 * @test
+	 */
+	public function getControllerObjectNameResolvesControllerObjectNameCorrectly($controllerArguments, $controllerObjectName) {
+		/** @var $request \TYPO3\CMS\Extbase\Mvc\Request */
+		$request = $this->getAccessibleMock('TYPO3\\CMS\\Extbase\\Mvc\\Request', array('dummy'));
+		$request->_set('controllerVendorName', $controllerArguments['vendorName']);
+		$request->_set('controllerExtensionName', $controllerArguments['extensionName']);
+		$request->_set('controllerSubpackageKey', $controllerArguments['subpackageKey']);
+		$request->_set('controllerName', $controllerArguments['controllerName']);
+
+		$this->assertEquals($controllerObjectName, $request->getControllerObjectName());
+	}
+
+	/**
+	 * @dataProvider controllerArgumentsAndExpectedObjectName
+	 *
+	 * @param array $controllerArguments
+	 * @param string $controllerObjectName
+	 * @test
+	 */
+	public function setControllerObjectNameResolvesControllerObjectNameArgumentsCorrectly($controllerArguments, $controllerObjectName) {
+		/** @var $request \TYPO3\CMS\Extbase\Mvc\Request */
+		$request = $this->getAccessibleMock('TYPO3\\CMS\\Extbase\\Mvc\\Request', array('dummy'));
+		$request->setControllerObjectName($controllerObjectName);
+
+		$actualControllerArguments = array(
+			'vendorName' => $request->_get('controllerVendorName'),
+			'extensionName' => $request->_get('controllerExtensionName'),
+			'subpackageKey' => $request->_get('controllerSubpackageKey'),
+			'controllerName' => $request->_get('controllerName'),
+		);
+
+		$this->assertSame($controllerArguments, $actualControllerArguments);
 	}
 }
 
