39984ff399b94dc7b98b5448b5179472c596716e
Christian Kuhn <lolli@schwarzbu.ch>
2013-01-26 21:32:46 +0100

    [BUGFIX] Incomplete locale backup in unit test
    A test from issue #43445 sets some locale categories
    but does not properly reset to settings before. This
    can kill unit tests running afterwards.
    
    Change-Id: I068f0b85a5a677c5c3ae9a79b845f45d1ee9b590
    Fixes: #44835
    Related: #43445
    Releases: 6.1, 6.0, 4.7, 4.6, 4.5
    Reviewed-on: https://review.typo3.org/17739
    Reviewed-by: Anja Leichsenring
    Tested-by: Anja Leichsenring

--- typo3/sysext/fluid/Tests/Unit/ViewHelpers/Form/SelectViewHelperTest.php
+++ typo3/sysext/fluid/Tests/Unit/ViewHelpers/Form/SelectViewHelperTest.php
@@ -25,15 +25,29 @@ class Tx_Fluid_Tests_Unit_ViewHelpers_Form_SelectViewHelperTest extends Tx_Fluid
 	 */
 	protected $viewHelper;
 
+	/**
+	 * @var array Backup of current locale, it is manipulated in tests
+	 */
+	protected $backupLocales = array();
+
 	public function setUp() {
 		parent::setUp();
+		// Store all locale categories manipulated in tests for reconstruction in tearDown
+		$this->backupLocales = array(
+			'LC_COLLATE' => setlocale(LC_COLLATE, 0),
+			'LC_CTYPE' => setlocale(LC_CTYPE, 0),
+			'LC_MONETARY' => setlocale(LC_MONETARY, 0),
+			'LC_TIME' => setlocale(LC_MONETARY, 0),
+		);
 		$this->arguments['name'] = '';
 		$this->arguments['sortByOptionLabel'] = FALSE;
 		$this->viewHelper = $this->getAccessibleMock('Tx_Fluid_ViewHelpers_Form_SelectViewHelper', array('setErrorClassAttribute', 'registerFieldNameForFormTokenGeneration'));
 	}
 
 	public function tearDown() {
-		setlocale(LC_ALL, '');
+		foreach ($this->backupLocales as $category => $locale) {
+			setlocale($category, $locale);
+		}
 	}
 
 	/**
