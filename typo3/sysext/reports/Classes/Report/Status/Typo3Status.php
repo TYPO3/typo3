7b62faf703e6203683b3ef8c7d69d3b7b18648d5
Christian Kuhn <lolli@schwarzbu.ch>
2013-01-28 23:19:47 +0100

    [TASK] EXT:reports Improve xclass reporting
    Since TYPO3 CMS 6.0, the XCLASS registration changed and the old
    registration style does not work anymore. Additionally, 6.0 comes
    with a basic check for old XCLASS registration to warn users about
    that.
    
    The patch adds a new report for newly registered XCLASS'es and
    adapts the messages to better notify that neither old nor new
    registrations are a real problem, but that those classes should
    be kept an eye on during upgrading.
    
    Change-Id: I8c0052b9c7fb0e88aff62a71c9592bf51bcec7ad
    Resolves: #44895
    Releases: 6.1, 6.0

--- typo3/sysext/reports/Classes/Report/Status/Typo3Status.php
+++ typo3/sysext/reports/Classes/Report/Status/Typo3Status.php
@@ -39,7 +39,8 @@ class Typo3Status implements \TYPO3\CMS\Reports\StatusProviderInterface {
 	public function getStatus() {
 		$statuses = array(
 			'Typo3Version' => $this->getTypo3VersionStatus(),
-			'oldXclassStatus' => $this->getOldXclassUsageStatus()
+			'oldXclassStatus' => $this->getOldXclassUsageStatus(),
+			'registeredXclass' => $this->getRegisteredXclassStatus(),
 		);
 		return $statuses;
 	}
@@ -62,14 +63,70 @@ class Typo3Status implements \TYPO3\CMS\Reports\StatusProviderInterface {
 		$message = '';
 		$value = $GLOBALS['LANG']->getLL('status_none');
 		$severity = \TYPO3\CMS\Reports\Status::OK;
-		$xclasses = array_merge((array) $GLOBALS['TYPO3_CONF_VARS']['BE']['XCLASS'], (array) $GLOBALS['TYPO3_CONF_VARS']['FE']['XCLASS']);
+
+		$xclasses = array_merge(
+			(array) $GLOBALS['TYPO3_CONF_VARS']['BE']['XCLASS'],
+			(array) $GLOBALS['TYPO3_CONF_VARS']['FE']['XCLASS']
+		);
+
 		$numberOfXclasses = count($xclasses);
 		if ($numberOfXclasses > 0) {
 			$value = sprintf($GLOBALS['LANG']->getLL('status_oldXclassUsageFound'), $numberOfXclasses);
-			$message = '<ol><li>' . implode('</li><li>', $xclasses) . '</li></ol>';
-			$severity = \TYPO3\CMS\Reports\Status::WARNING;
+			$message = $GLOBALS['LANG']->getLL('status_oldXclassUsageFound_message') . '<br />';
+			$message .= '<ol><li>' . implode('</li><li>', $xclasses) . '</li></ol>';
+			$severity = \TYPO3\CMS\Reports\Status::NOTICE;
 		}
-		return \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Reports\\Status', $GLOBALS['LANG']->getLL('status_oldXclassUsage'), $value, $message, $severity);
+
+		return \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
+			'TYPO3\\CMS\\Reports\\Status',
+			$GLOBALS['LANG']->getLL('status_oldXclassUsage'),
+			$value,
+			$message,
+			$severity
+		);
+	}
+
+	/**
+	 * List any Xclasses registered in the stystem
+	 *
+	 * @return \TYPO3\CMS\Reports\Status
+	 */
+	protected function getRegisteredXclassStatus() {
+		$message = '';
+		$value = $GLOBALS['LANG']->getLL('status_none');
+		$severity = \TYPO3\CMS\Reports\Status::OK;
+
+		$xclassFoundArray = array();
+		if (array_key_exists('Objects', $GLOBALS['TYPO3_CONF_VARS']['SYS'])) {
+			foreach ($GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'] as $originalClass => $override) {
+				if (array_key_exists('className', $override)) {
+					$xclassFoundArray[$originalClass] = $override['className'];
+				}
+			}
+		}
+		if (count($xclassFoundArray) > 0) {
+			$value = $GLOBALS['LANG']->getLL('status_xclassUsageFound');
+			$message = $GLOBALS['LANG']->getLL('status_xclassUsageFound_message') . '<br />';
+			$message .= '<ol>';
+			foreach ($xclassFoundArray as $originalClass => $xClassName) {
+				$messageDetail = sprintf(
+					$GLOBALS['LANG']->getLL('status_xclassUsageFound_message_detail'),
+					$originalClass,
+					$xClassName
+				);
+				$message .= '<li>' . $messageDetail . '</li>';
+			}
+			$message .= '</ol>';
+			$severity = \TYPO3\CMS\Reports\Status::NOTICE;
+		}
+
+		return \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
+			'TYPO3\\CMS\\Reports\\Status',
+			$GLOBALS['LANG']->getLL('status_xclassUsage'),
+			$value,
+			$message,
+			$severity
+		);
 	}
 
 }
