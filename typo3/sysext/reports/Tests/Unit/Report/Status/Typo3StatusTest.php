772a8346b0bcc806f8727bc7e97725baf23c7996
Christian Kuhn <lolli@schwarzbu.ch>
2013-01-28 23:19:47 +0100

    [TASK] EXT:reports Improve xclass reporting
    Since TYPO3 CMS 6.0, the XCLASS registration changed and the old
    registration style does not work anymore. Additionally, 6.0 comes
    with a basic check for old XCLASS registration to warn users about
    that.
    
    The patch adds a new report for newly registered XCLASS'es and
    adapts the messages to better notify that neither old nor new
    registrations are a real problem, but that those classes should
    be kept an eye on during upgrading.
    
    Change-Id: I8c0052b9c7fb0e88aff62a71c9592bf51bcec7ad
    Resolves: #44895
    Releases: 6.1, 6.0

---
+++ typo3/sysext/reports/Tests/Unit/Report/Status/Typo3StatusTest.php
@@ -0,0 +1,81 @@
+<?php
+namespace TYPO3\CMS\Reports\Tests\Unit\Report\Status;
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2013 Christian Kuhn
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * Test case
+ */
+class Typo3StatusTest extends \TYPO3\CMS\Core\Tests\UnitTestCase {
+
+	/**
+	 * @test
+	 */
+	public function getStatusReturnsOldXclassStatusObjectWithSeverityOkIfNoOldXclassExists() {
+		$GLOBALS['TYPO3_CONF_VARS']['BE']['XCLASS'] = array();
+		$GLOBALS['TYPO3_CONF_VARS']['FE']['XCLASS'] = array();
+		$fixture = new \TYPO3\CMS\Reports\Report\Status\Typo3Status;
+		$result = $fixture->getStatus();
+		$statusObject = $result['oldXclassStatus'];
+		$this->assertSame(\TYPO3\CMS\Reports\Status::OK, $statusObject->getSeverity());
+	}
+
+	/**
+	 * @test
+	 */
+	public function getStatusReturnsOldXclassStatusObjectWithSeverityNoticeIfOldXclassExists() {
+		$GLOBALS['TYPO3_CONF_VARS']['BE']['XCLASS'] = array('foo' => 'bar');
+		$fixture = new \TYPO3\CMS\Reports\Report\Status\Typo3Status;
+		$result = $fixture->getStatus();
+		$statusObject = $result['oldXclassStatus'];
+		$this->assertSame(\TYPO3\CMS\Reports\Status::NOTICE, $statusObject->getSeverity());
+	}
+
+	/**
+	 * @test
+	 */
+	public function getStatusReturnsXclassStatusObjectWithSeverityOkIfNoXclassExists() {
+		$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'] = array();
+		$fixture = new \TYPO3\CMS\Reports\Report\Status\Typo3Status;
+		$result = $fixture->getStatus();
+		$statusObject = $result['registeredXclass'];
+		$this->assertSame(\TYPO3\CMS\Reports\Status::OK, $statusObject->getSeverity());
+	}
+
+	/**
+	 * @test
+	 */
+	public function getStatusReturnsXclassStatusObjectWithSeverityNoticeIfXclassExists() {
+		$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'] = array(
+			'foo' => array(
+				'className' => 'bar',
+			)
+		);
+		$fixture = new \TYPO3\CMS\Reports\Report\Status\Typo3Status;
+		$result = $fixture->getStatus();
+		$statusObject = $result['registeredXclass'];
+		$this->assertSame(\TYPO3\CMS\Reports\Status::NOTICE, $statusObject->getSeverity());
+	}
+}
+?>
