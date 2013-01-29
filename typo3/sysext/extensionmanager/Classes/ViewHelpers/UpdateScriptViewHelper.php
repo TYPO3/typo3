e00f74794ad357bbc20777049b576fa23357607c
Francois Suter <francois@typo3.org>
2013-01-29 20:31:52 +0100

    [BUGFIX] Restore support for Update script
    The new EM is missing support for the class.ext_update.php script.
    This patch restores it, giving it its own icon in the extension
    list view.
    
    Change-Id: Id5ea243bb689882959aa85cdbe700e784d83b2c0
    Resolves: #43244
    Releases: 6.1, 6.0

---
+++ typo3/sysext/extensionmanager/Classes/ViewHelpers/UpdateScriptViewHelper.php
@@ -0,0 +1,79 @@
+<?php
+namespace TYPO3\CMS\Extensionmanager\ViewHelpers;
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2013 Francois Suter, <francois.suter@typo3.org>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *  A copy is found in the textfile GPL.txt and important notices to the license
+ *  from the author is found in LICENSE.txt distributed with these scripts.
+ *
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * View helper for update script link
+ *
+ * @author Francois Suter <francois.suter@typo3.org>
+ */
+class UpdateScriptViewHelper extends \TYPO3\CMS\Fluid\ViewHelpers\Link\ActionViewHelper {
+
+	/**
+	 * @var \TYPO3\CMS\Extbase\Object\ObjectManagerInterface
+	 * @inject
+	 */
+	protected $objectManager;
+
+	/**
+	 * @var string
+	 */
+	protected $tagName = 'a';
+
+	/**
+	 * Renders a link to the update script screen if the extension has one
+	 *
+	 * @param string $extensionKey Extension key
+	 * @return string The rendered a tag
+	 */
+	public function render($extensionKey) {
+		$tag = '';
+
+		// If the "class.ext_update.php" file exists, build link to the update script screen
+		/** @var $updateScriptUtility \TYPO3\CMS\Extensionmanager\Utility\UpdateScriptUtility */
+		$updateScriptUtility = $this->objectManager->create('TYPO3\\CMS\\Extensionmanager\\Utility\\UpdateScriptUtility');
+		if ($updateScriptUtility->checkUpdateScriptExists($extensionKey)) {
+			$uriBuilder = $this->controllerContext->getUriBuilder();
+			$action = 'show';
+			$uri = $uriBuilder->reset()->uriFor(
+				$action,
+				array('extensionKey' => $extensionKey),
+				'UpdateScript'
+			);
+			$this->tag->addAttribute('href', $uri);
+			$this->tag->addAttribute('title', \TYPO3\CMS\Extbase\Utility\LocalizationUtility::translate('extensionList.update.script', 'extensionmanager'));
+			$this->tag->setContent(\TYPO3\CMS\Backend\Utility\IconUtility::getSpriteIcon('extensions-extensionmanager-update-script'));
+			$tag = $this->tag->render();
+		}
+		return $tag;
+	}
+
+}
+
+
+?>
