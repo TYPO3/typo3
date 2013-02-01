65b100750449a9dcfce887a6e37edbd582a4874a
Christian Kuhn <lolli@schwarzbu.ch>
2013-02-01 16:21:54 +0100

    [BUGFIX] Misspelled class name FrontendUserAuthtenication
    Fix class name, rename file, fix usage
    FrontendUserAuth*ten*ication
    FrontendUserAuth*ent*ication
    
    Change-Id: Id851db64a50a5938496dd4411caaec0bf7ba85af
    Fixes: 43464
    Releases: 6.1, 6.0

--- typo3/sysext/cms/tslib/class.tslib_feuserauth.php
+++ typo3/sysext/cms/tslib/class.tslib_feuserauth.php
@@ -2,7 +2,7 @@
 /*
  * @deprecated since 6.0, the classname tslib_feUserAuth and this file is obsolete
  * and will be removed with 6.2. The class was renamed and is now located at:
- * typo3/sysext/frontend/Classes/Authentication/FrontendUserAuthtenication.php
+ * typo3/sysext/frontend/Classes/Authentication/FrontendUserAuthentication.php
  */
-require_once \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('frontend') . 'Classes/Authentication/FrontendUserAuthtenication.php';
+require_once \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('frontend') . 'Classes/Authentication/FrontendUserAuthentication.php';
 ?>
