f2e56bab98ed878b2c138dc6c80d3ff216c89274
Wouter Wolters <typo3@wouterwolters.nl>
2013-02-02 16:24:02 +0100

    [TASK] Cleanup extra_page_cm_options
    Cleanup the code in extra_page_cm_options
    
    Change-Id: I324115dae3514cc7b6f4ea350d244d13a3f6d818
    Resolves: #45096
    Releases: 6.1
    Reviewed-on: https://review.typo3.org/17952
    Reviewed-by: Oliver Klee
    Reviewed-by: Christian Kuhn
    Tested-by: Christian Kuhn

--- typo3/sysext/extra_page_cm_options/ext_tables.php
+++ typo3/sysext/extra_page_cm_options/ext_tables.php
@@ -2,10 +2,10 @@
 if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
-if (TYPO3_MODE == 'BE') {
+if (TYPO3_MODE === 'BE') {
 	$GLOBALS['TBE_MODULES_EXT']['xMOD_alt_clickmenu']['extendCMclasses'][] = array(
 		'name' => 'TYPO3\\CMS\\ExtraPageCmOptions\\ExtraPageContextMenuOptions',
-		'path' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'class.tx_extrapagecmoptions.php'
+		'path' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Classes/ExtraPageContextMenuOptions.php'
 	);
 }
 ?>
