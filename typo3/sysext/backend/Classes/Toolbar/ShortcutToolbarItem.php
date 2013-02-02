7a3dcbc81b7c643a4eb7790fbad9759d64a0135b
Bindea Silviu <silviu.bindea@arxia.com>
2013-02-02 15:55:25 +0200

    [BUGFIX] Labels for grouping bookmarks are missing
    Using function sL to load locallang with labels for bookmarks
    
    Fixes: #44691
    Releases: 6.1, 6.0
    Change-Id: I509aa06e379e1397fea50c6a2c137db3994eff2a

--- typo3/sysext/backend/Classes/Toolbar/ShortcutToolbarItem.php
+++ typo3/sysext/backend/Classes/Toolbar/ShortcutToolbarItem.php
@@ -316,7 +316,7 @@ class ShortcutToolbarItem implements \TYPO3\CMS\Backend\Toolbar\ToolbarItemHookI
 		foreach ($this->shortcutGroups as $groupId => $groupLabel) {
 			$label = $groupLabel;
 			if ($groupLabel == '1') {
-				$label = $GLOBALS['LANG']->getLL('bookmark_group_' . abs($groupId), 1);
+				$label = $GLOBALS['LANG']->sL('LLL:EXT:lang/locallang_misc.xlf:bookmark_group_' . abs($groupId), 1);
 				if (empty($label)) {
 					// Fallback label
 					$label = $GLOBALS['LANG']->getLL('bookmark_group', 1) . ' ' . abs($groupId);
@@ -324,7 +324,7 @@ class ShortcutToolbarItem implements \TYPO3\CMS\Backend\Toolbar\ToolbarItemHookI
 			}
 			if ($groupId < 0) {
 				// Global group
-				$label = $GLOBALS['LANG']->getLL('bookmark_global', 1) . ': ' . (!empty($label) ? $label : abs($groupId));
+				$label = $GLOBALS['LANG']->sL('LLL:EXT:lang/locallang_misc.xlf:bookmark_global', 1) . ': ' . (!empty($label) ? $label : abs($groupId));
 				if ($groupId == -100) {
 					$label = $GLOBALS['LANG']->getLL('bookmark_global', 1) . ': ' . $GLOBALS['LANG']->getLL('bookmark_all', 1);
 				}
