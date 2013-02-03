f97aba075fc72b0d9369ed76207e167ec299a2ec
Wouter Wolters <typo3@wouterwolters.nl>
2013-02-03 23:51:45 +0100

    [TASK] Cleanup felogin
    Change-Id: I9ceeb1e7c142a3daaf20886f94c8a1d3295df99a
    Resolves: #45123
    Releases: 6.1

--- typo3/sysext/felogin/ext_tables.php
+++ typo3/sysext/felogin/ext_tables.php
@@ -4,32 +4,29 @@ if (!defined('TYPO3_MODE')) {
 }
 $_EXTCONF = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['felogin']);
 \TYPO3\CMS\Core\Utility\GeneralUtility::loadTCA('tt_content');
-if (\TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger(TYPO3_version) >= 4002000) {
-	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('*', 'FILE:EXT:' . $_EXTKEY . '/flexform.xml', 'login');
-} else {
-	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('default', 'FILE:EXT:' . $_EXTKEY . '/flexform.xml');
-}
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('*', 'FILE:EXT:' . $_EXTKEY . '/flexform.xml', 'login');
+
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTcaSelectItem('tt_content', 'CType', array(
-	'LLL:EXT:cms/locallang_ttc.xml:CType.I.10',
+	'LLL:EXT:cms/locallang_ttc.xlf:CType.I.10',
 	'login',
 	'i/tt_content_login.gif'
 ), 'mailform', 'after');
-$TCA['tt_content']['types']['login']['showitem'] = '--palette--;LLL:EXT:cms/locallang_ttc.xml:palette.general;general,
-													--palette--;LLL:EXT:cms/locallang_ttc.xml:palette.header;header,
-													--div--;LLL:EXT:cms/locallang_ttc.xml:tabs.plugin,
+$GLOBALS['TCA']['tt_content']['types']['login']['showitem'] = '--palette--;LLL:EXT:cms/locallang_ttc.xlf:palette.general;general,
+													--palette--;LLL:EXT:cms/locallang_ttc.xlf:palette.header;header,
+													--div--;LLL:EXT:cms/locallang_ttc.xlf:tabs.plugin,
 													pi_flexform;;;;1-1-1,
-													--div--;LLL:EXT:cms/locallang_ttc.xml:tabs.access,
-													--palette--;LLL:EXT:cms/locallang_ttc.xml:palette.visibility;visibility,
-													--palette--;LLL:EXT:cms/locallang_ttc.xml:palette.access;access,
-													--div--;LLL:EXT:cms/locallang_ttc.xml:tabs.appearance,
-													--palette--;LLL:EXT:cms/locallang_ttc.xml:palette.frames;frames,
-													--div--;LLL:EXT:cms/locallang_ttc.xml:tabs.behaviour,
-													--div--;LLL:EXT:cms/locallang_ttc.xml:tabs.extended';
+													--div--;LLL:EXT:cms/locallang_ttc.xlf:tabs.access,
+													--palette--;LLL:EXT:cms/locallang_ttc.xlf:palette.visibility;visibility,
+													--palette--;LLL:EXT:cms/locallang_ttc.xlf:palette.access;access,
+													--div--;LLL:EXT:cms/locallang_ttc.xlf:tabs.appearance,
+													--palette--;LLL:EXT:cms/locallang_ttc.xlf:palette.frames;frames,
+													--div--;LLL:EXT:cms/locallang_ttc.xlf:tabs.behaviour,
+													--div--;LLL:EXT:cms/locallang_ttc.xlf:tabs.extended';
 // Adds the redirect field to the fe_groups table
 $tempColumns = array(
 	'felogin_redirectPid' => array(
 		'exclude' => 1,
-		'label' => 'LLL:EXT:felogin/locallang_db.xml:felogin_redirectPid',
+		'label' => 'LLL:EXT:felogin/locallang_db.xlf:felogin_redirectPid',
 		'config' => array(
 			'type' => 'group',
 			'internal_type' => 'db',
@@ -52,7 +49,7 @@ $tempColumns = array(
 $tempColumns = array(
 	'felogin_redirectPid' => array(
 		'exclude' => 1,
-		'label' => 'LLL:EXT:felogin/locallang_db.xml:felogin_redirectPid',
+		'label' => 'LLL:EXT:felogin/locallang_db.xlf:felogin_redirectPid',
 		'config' => array(
 			'type' => 'group',
 			'internal_type' => 'db',
@@ -69,7 +66,7 @@ $tempColumns = array(
 	),
 	'felogin_forgotHash' => array(
 		'exclude' => 1,
-		'label' => 'LLL:EXT:felogin/locallang_db.xml:felogin_forgotHash',
+		'label' => 'LLL:EXT:felogin/locallang_db.xlf:felogin_forgotHash',
 		'config' => array(
 			'type' => 'passthrough'
 		)
