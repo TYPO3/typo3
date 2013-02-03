604ac6fb30fa24d2aff6ca6bc4845a1e5ba10a4b
Wouter Wolters <typo3@wouterwolters.nl>
2013-02-03 19:56:15 +0100

    [TASK] Remove option 'path' for extendCMclasses
    Change-Id: Iab264bd15d31e76b5a7d8972879f73d34ee8da0a
    Resolves: #45117
    Related: #45098
    Releases: 6.1
    Reviewed-on: https://review.typo3.org/17986
    Reviewed-by: Christian Kuhn
    Tested-by: Christian Kuhn

--- typo3/sysext/version/ext_tables.php
+++ typo3/sysext/version/ext_tables.php
@@ -6,7 +6,6 @@ if (TYPO3_MODE == 'BE') {
 	if (!\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('workspaces')) {
 		$GLOBALS['TBE_MODULES_EXT']['xMOD_alt_clickmenu']['extendCMclasses'][] = array(
 			'name' => 'TYPO3\\CMS\\Version\\ClickMenu\\VersionClickMenu',
-			'path' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'class.tx_version_cm1.php'
 		);
 	}
 }
