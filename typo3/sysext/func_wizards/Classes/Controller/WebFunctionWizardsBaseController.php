018167153957ac8dd722dfe3a4d85bbc8d62dc6d
Wouter Wolters <typo3@wouterwolters.nl>
2013-02-05 19:34:05 +0100

    [TASK] Cleanup func and func_wizards
    Change-Id: Ia23ed69335b6728c198b84b830104f11994e4663
    Resolves: #45180
    Releases: 6.1
    Reviewed-on: https://review.typo3.org/18025
    Reviewed-by: Christian Kuhn
    Tested-by: Christian Kuhn

--- typo3/sysext/func_wizards/Classes/Controller/WebFunctionWizardsBaseController.php
+++ typo3/sysext/func_wizards/Classes/Controller/WebFunctionWizardsBaseController.php
@@ -26,14 +26,7 @@ namespace TYPO3\CMS\FuncWizards\Controller;
  *
  *  This copyright notice MUST APPEAR in all copies of the script!
  ***************************************************************/
-/**
- * Contains a class for the Wizard function in the Web>Info module
- *
- * Revised for TYPO3 3.6 July/2003 by Kasper Skårhøj
- * XHTML compliant
- *
- * @author Kasper Skårhøj <kasperYYYY@typo3.com>
- */
+
 /**
  * The Wizard function in the Web>Info module
  * Creates a framework for adding wizard sub-sub-modules under the Wizard function in Web>Info
@@ -51,16 +44,18 @@ class WebFunctionWizardsBaseController extends \TYPO3\CMS\Backend\Module\Abstrac
 	 * Initialize.
 	 * Calls parent init function and then the handleExternalFunctionValue() function from the parent class
 	 *
-	 * @param object $pObj A reference to the parent (calling) object (which is probably an instance of an extension class to t3lib_SCbase)
+	 * @param object $pObj A reference to the parent (calling) object (which is probably an instance of an extension class to \TYPO3\CMS\Backend\Module\BaseScriptClass)
 	 * @param array $conf The configuration set for this module - from global array TBE_MODULES_EXT
 	 * @return void
-	 * @see t3lib_extobjbase::handleExternalFunctionValue(), t3lib_extobjbase::init()
+	 * @see \TYPO3\CMS\Backend\Module\AbstractFunctionModule::handleExternalFunctionValue()
+	 * @see \TYPO3\CMS\Backend\Module\AbstractFunctionModule::init()
 	 * @todo Define visibility
 	 */
 	public function init(&$pObj, $conf) {
 		// OK, handles ordinary init. This includes setting up the menu array with ->modMenu
 		parent::init($pObj, $conf);
-		// Making sure that any further external classes are added to the include_once array. Notice that inclusion happens twice in the main script because of this!!!
+		// Making sure that any further external classes are added to the include_once array.
+		// Notice that inclusion happens twice in the main script because of this!!!
 		$this->handleExternalFunctionValue();
 	}
 
@@ -68,16 +63,20 @@ class WebFunctionWizardsBaseController extends \TYPO3\CMS\Backend\Module\Abstrac
 	 * Modifies parent objects internal MOD_MENU array, adding items this module needs.
 	 *
 	 * @return array Items merged with the parent objects.
-	 * @see t3lib_extobjbase::init()
+	 * @see \TYPO3\CMS\Backend\Module\AbstractFunctionModule::init()
 	 * @todo Define visibility
 	 */
 	public function modMenu() {
-		global $LANG;
+		$GLOBALS['LANG']->includeLLFile('EXT:func_wizards/locallang.xlf');
 		$modMenuAdd = array(
 			$this->function_key => array()
 		);
 		$modMenuAdd[$this->function_key] = $this->pObj->mergeExternalItems($this->pObj->MCONF['name'], $this->function_key, $modMenuAdd[$this->function_key]);
-		$modMenuAdd[$this->function_key] = \TYPO3\CMS\Backend\Utility\BackendUtility::unsetMenuItems($this->pObj->modTSconfig['properties'], $modMenuAdd[$this->function_key], 'menu.' . $this->function_key);
+		$modMenuAdd[$this->function_key] = \TYPO3\CMS\Backend\Utility\BackendUtility::unsetMenuItems(
+			$this->pObj->modTSconfig['properties'],
+			$modMenuAdd[$this->function_key],
+			'menu.' . $this->function_key
+		);
 		return $modMenuAdd;
 	}
 
@@ -85,12 +84,17 @@ class WebFunctionWizardsBaseController extends \TYPO3\CMS\Backend\Module\Abstrac
 	 * Creation of the main content. Calling extObjContent() to trigger content generation from the sub-sub modules
 	 *
 	 * @return string The content
-	 * @see t3lib_extobjbase::extObjContent()
+	 * @see \TYPO3\CMS\Backend\Module\AbstractFunctionModule::extObjContent()
 	 * @todo Define visibility
 	 */
 	public function main() {
 		global $SOBE, $LANG;
-		$menu = $LANG->getLL('wiz_lWizards', 1) . ': ' . \TYPO3\CMS\Backend\Utility\BackendUtility::getFuncMenu($this->pObj->id, 'SET[wiz]', $this->pObj->MOD_SETTINGS['wiz'], $this->pObj->MOD_MENU['wiz']);
+		$menu = $LANG->getLL('wiz_lWizards', 1) . ': ' . \TYPO3\CMS\Backend\Utility\BackendUtility::getFuncMenu(
+			$this->pObj->id,
+			'SET[wiz]',
+			$this->pObj->MOD_SETTINGS['wiz'],
+			$this->pObj->MOD_MENU['wiz']
+		);
 		$theOutput .= $this->pObj->doc->section('', '<span class="nobr">' . $menu . '</span>');
 		$content = '';
 		$content .= $theOutput;
@@ -100,6 +104,5 @@ class WebFunctionWizardsBaseController extends \TYPO3\CMS\Backend\Module\Abstrac
 	}
 
 }
-
 
 ?>
