bb0c72d738018fadfcf715eaf4904da95f168495
Anja Leichsenring <aleichsenring@ab-softlab.de>
2013-02-06 21:03:01 +0100

    [BUGFIX] Fix typo and improve backup of system settings
    LC_MONETARY was saved to LC_TIME, this needs to be fixed.
    
    Instead of using the constants as is, use the API function to save
    and restore system settings.
    
    Fixes: #45218
    Releases: 4.5, 4.6, 4.7, 6.0, 6.1
    Change-Id: I3c3c9d552d89d69c3689e60dc00e392067b98464
    Reviewed-on: https://review.typo3.org/18075
    Reviewed-by: Anja Leichsenring
    Tested-by: Anja Leichsenring

--- typo3/sysext/fluid/Tests/Unit/ViewHelpers/Form/SelectViewHelperTest.php
+++ typo3/sysext/fluid/Tests/Unit/ViewHelpers/Form/SelectViewHelperTest.php
@@ -34,10 +34,10 @@ class Tx_Fluid_Tests_Unit_ViewHelpers_Form_SelectViewHelperTest extends Tx_Fluid
 		parent::setUp();
 		// Store all locale categories manipulated in tests for reconstruction in tearDown
 		$this->backupLocales = array(
-			LC_COLLATE => setlocale(LC_COLLATE, 0),
-			LC_CTYPE => setlocale(LC_CTYPE, 0),
-			LC_MONETARY => setlocale(LC_MONETARY, 0),
-			LC_TIME => setlocale(LC_MONETARY, 0),
+			'LC_COLLATE' => setlocale(LC_COLLATE, 0),
+			'LC_CTYPE' => setlocale(LC_CTYPE, 0),
+			'LC_MONETARY' => setlocale(LC_MONETARY, 0),
+			'LC_TIME' => setlocale(LC_TIME, 0),
 		);
 		$this->arguments['name'] = '';
 		$this->arguments['sortByOptionLabel'] = FALSE;
@@ -46,7 +46,7 @@ class Tx_Fluid_Tests_Unit_ViewHelpers_Form_SelectViewHelperTest extends Tx_Fluid
 
 	public function tearDown() {
 		foreach ($this->backupLocales as $category => $locale) {
-			setlocale($category, $locale);
+			setlocale(constant($category), $locale);
 		}
 	}
 
