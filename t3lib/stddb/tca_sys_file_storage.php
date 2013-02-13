6e5c761665aa2a0fb4f3cb86eae511cb9d4b776f
Christian Kuhn <lolli@schwarzbu.ch>
2013-02-13 17:13:58 +0100

    Revert "[TASK] Move t3lib/stddb files to ext:core"
    This reverts commit ef90adcee123ebc2d47c94380cc75e26c176668a
    
    Change-Id: I96ed7987e90addfeb44afe9577ee58ff5dab3344

--- t3lib/stddb/tca_sys_file_storage.php
+++ t3lib/stddb/tca_sys_file_storage.php
@@ -1,25 +1,16 @@
 <?php
-return array(
-	'ctrl' => array(
-		'title' => 'LLL:EXT:lang/locallang_tca.xlf:sys_file_storage',
-		'label' => 'name',
-		'tstamp' => 'tstamp',
-		'crdate' => 'crdate',
-		'cruser_id' => 'cruser_id',
-		'default_sortby' => 'ORDER BY name',
-		'delete' => 'deleted',
-		'rootLevel' => TRUE,
-		'versioningWS_alwaysAllowLiveEdit' => TRUE, // Only have LIVE records of file storages
-		'enablecolumns' => array(
-			'disabled' => 'hidden'
-		),
-		'dividers2tabs' => TRUE,
-		'requestUpdate' => 'driver',
-		'iconfile' => '_icon_ftp.gif',
-	),
+if (!defined('TYPO3_MODE')) {
+	die('Access denied.');
+}
+/**
+ * File storages
+ */
+$TCA['sys_file_storage'] = array(
+	'ctrl' => $TCA['sys_file_storage']['ctrl'],
 	'interface' => array(
 		'showRecordFieldList' => 'hidden,name,description,driver,processingfolder,configuration'
 	),
+	'feInterface' => $TCA['sys_file_storage']['feInterface'],
 	'columns' => array(
 		'hidden' => array(
 			'exclude' => 1,
@@ -115,4 +106,9 @@ return array(
 		'capabilities' => array('showitem' => 'is_browsable, is_public, is_writable', 'canNotCollapse' => TRUE)
 	)
 );
+/**
+ * @var \TYPO3\CMS\Core\Resource\Driver\DriverRegistry $registry
+ */
+$registry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Core\\Resource\\Driver\\DriverRegistry');
+$registry->addDriversToTCA();
 ?>
