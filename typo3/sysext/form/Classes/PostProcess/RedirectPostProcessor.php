0f9c83e5b48dda11ac493aef5e753d2d80fbdd42
Stefan Neufeind <typo3.neufeind@speedpartner.de>
2013-02-15 22:36:01 +0100

    [FEATURE] form: Postprocessor to do redirect
    * Implements a basic postprocessor to perform a redirect.
    * Adds it to the form-wizard.
    * Enables prepared ExtJS-code for removing a postprocessor.
    
    Resolves: #44482
    Resolves: #38094
    Releases: 6.1
    Change-Id: I7e09bc9aa9821ac20e890343eb95c34c010dfba3

---
+++ typo3/sysext/form/Classes/PostProcess/RedirectPostProcessor.php
@@ -0,0 +1,102 @@
+<?php
+namespace TYPO3\CMS\Form\PostProcess;
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2013 Stefan Neufeind <info@speedpartner.de>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+/**
+ * The redirect post processor
+ */
+class RedirectPostProcessor implements \TYPO3\CMS\Form\PostProcess\PostProcessorInterface {
+
+	/**
+	 * @var \TYPO3\CMS\Form\Domain\Model\Form
+	 */
+	protected $form;
+
+	/**
+	 * @var array
+	 */
+	protected $typoScript;
+
+	/**
+	 * @var \TYPO3\CMS\Form\Request
+	 */
+	protected $requestHandler;
+
+	/**
+	 * @var string
+	 */
+	protected $destination;
+
+	/**
+	 * @var array
+	 */
+	protected $dirtyHeaders = array();
+
+	/**
+	 * Constructor
+	 *
+	 * @param \TYPO3\CMS\Form\Domain\Model\Form $form Form domain model
+	 * @param array $typoScript Post processor TypoScript settings
+	 */
+	public function __construct(\TYPO3\CMS\Form\Domain\Model\Form $form, array $typoScript) {
+		$this->form = $form;
+		$this->typoScript = $typoScript;
+		$this->requestHandler = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Form\\Request');
+	}
+
+	/**
+	 * The main method called by the post processor
+	 *
+	 * @return string HTML message from this processor
+	 */
+	public function process() {
+		$this->setDestination();
+		return $this->render();
+	}
+
+	/**
+	 * Sets the redirect destination
+	 *
+	 * @return void
+	 */
+	protected function setDestination() {
+		$this->destination = '';
+		if ($this->typoScript['destination']) {
+			$urlConf = array('parameter' => $this->typoScript['destination']);
+			$this->destination = $GLOBALS['TSFE']->cObj->typoLink_URL($urlConf);
+		}
+	}
+
+	/**
+	 * Render the message after trying to send the mail
+	 *
+	 * @return string HTML message from the mail view
+	 */
+	protected function render() {
+		\TYPO3\CMS\Core\Utility\HttpUtility::redirect($this->destination);
+		return '';
+	}
+}
+
+?>
