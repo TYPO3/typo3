4e7656faf21b1ad205feac49e2e265a3e74fc393
Fabien Udriot <fabien.udriot@ecodev.ch>
2013-02-15 11:59:12 +0100

    [TASK] Update file type according to Iana standard
    This change relates to the File types in FAL.
    The Iana standard http://www.iana.org/assignments/media-types
    defines a list of Media types which are followed in TYPO3 CMS.
    It looks the "Software" type should be deprecated and replaced by
    "Application". This matches the mime-type e.g application/pdf and
    sounds more comprehensive.
    
    Change-Id: Ic2ec7173d10b6139e6e2a19fcead3086665a3aae
    Fixes: #45503
    Releases: 6.1
    Reviewed-on: https://review.typo3.org/18297
    Reviewed-by: Benjamin Mack
    Tested-by: Benjamin Mack

--- typo3/sysext/cms/Configuration/TCA/tt_content.php
+++ typo3/sysext/cms/Configuration/TCA/tt_content.php
@@ -734,7 +734,7 @@ return array(
 							--palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
 							--palette--;;filePalette'
 					),
-					\TYPO3\CMS\Core\Resource\File::FILETYPE_SOFTWARE => array(
+					\TYPO3\CMS\Core\Resource\File::FILETYPE_APPLICATION => array(
 						'showitem' => '
 							--palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
 							--palette--;;filePalette'
