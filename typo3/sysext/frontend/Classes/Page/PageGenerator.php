92129fdd954548d4520fdd79d52fedde51976ed9
Georg Ringer <georg.ringer@gmail.com>
2013-02-16 13:05:09 +0100

    [TASK] Remove old JS in page header
    Change-Id: I509ffe2cc3dc37cc08de3633c79127c1f50145a7
    Resolves: #45528
    Releases: 6.1

--- typo3/sysext/frontend/Classes/Page/PageGenerator.php
+++ typo3/sysext/frontend/Classes/Page/PageGenerator.php
@@ -718,18 +718,8 @@ class PageGenerator {
 			$GLOBALS['TSFE']->INTincScript_loadJSCode();
 		}
 		$JSef = self::JSeventFunctions();
-		// Adding default Java Script:
-		$scriptJsCode = '
-		var browserName = navigator.appName;
-		var browserVer = parseInt(navigator.appVersion);
-		var version = "";
-		var msie4 = (browserName == "Microsoft Internet Explorer" && browserVer >= 4);
-		if ((browserName == "Netscape" && browserVer >= 3) || msie4 || browserName=="Konqueror" || browserName=="Opera") {version = "n3";} else {version = "n2";}
-			// Blurring links:
-		function blurLink(theObject) {	//
-			if (msie4) {theObject.blur();}
-		}
-		' . $JSef[0];
+		$scriptJsCode = $JSef[0];
+
 		if ($GLOBALS['TSFE']->spamProtectEmailAddresses && $GLOBALS['TSFE']->spamProtectEmailAddresses !== 'ascii') {
 			$scriptJsCode .= '
 			// decrypt helper function
