8ead27e1cee191c929090b096b57335eff15f0a2
Wouter Wolters <typo3@wouterwolters.nl>
2013-02-18 21:47:09 +0100

    [TASK] Cleanup sysext "wizard_crpages" and "wizard_sortpages"
    This is a non-functional change that reformats the code, removes
    unnecessary empty lines and fixes reference to old classnames.
    
    Furthermore change "0" to FALSE where a boolean is needed.
    
    Change-Id: Ida0c80ce0c54c6054e248b29c716b740f5e65194
    Resolves: #46143
    Releases: 6.1

--- typo3/sysext/wizard_sortpages/ext_tables.php
+++ typo3/sysext/wizard_sortpages/ext_tables.php
@@ -2,8 +2,17 @@
 if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
-if (TYPO3_MODE == 'BE') {
-	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::insertModuleFunction('web_func', 'TYPO3\\CMS\\WizardSortPages\\View\\SortPagesWizardModuleFunction', \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'class.tx_wizardsortpages_webfunc_2.php', 'LLL:EXT:wizard_sortpages/locallang.php:wiz_sort', 'wiz');
-	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('_MOD_web_func', 'EXT:wizard_sortpages/locallang_csh.xml');
+if (TYPO3_MODE === 'BE') {
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::insertModuleFunction(
+		'web_func',
+		'TYPO3\\CMS\\WizardSortPages\\View\\SortPagesWizardModuleFunction',
+		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Classes/View/SortPagesWizardModuleFunction.php',
+		'LLL:EXT:wizard_sortpages/locallang.php:wiz_sort',
+		'wiz'
+	);
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
+		'_MOD_web_func',
+		'EXT:wizard_sortpages/locallang_csh.xlf'
+	);
 }
 ?>
