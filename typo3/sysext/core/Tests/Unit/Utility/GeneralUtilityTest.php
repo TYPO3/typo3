44399fb340d90175f710a3f56169ce7709a92b08
Dmitry Dulepov <dmitry@typo3.org>
2013-02-22 13:16:34 +0400

    [BUGFIX] GeneralUtility::getUrl() fails to process redirects sometimes
    If PHP's open_basedir is active, CURLOPT_FOLLOWLOCATION will be disabled
    by PHP. In such case any redirects will not work. This causes problems
    with extensions that fetch pages of the site and hit redirects. For
    example, if the site uses http and parts of it use https, extensions
    like "pagepath" or "solr" will not be able to work with "secondary"
    protocol.
    
    Change-Id: I2d9e50d2d00e4c8b0c930a929be7884fd0215c21
    Resolves: #45741
    Releases: 6.1, 6.0

--- typo3/sysext/core/Tests/Unit/Utility/GeneralUtilityTest.php
+++ typo3/sysext/core/Tests/Unit/Utility/GeneralUtilityTest.php
@@ -4082,6 +4082,60 @@ text with a ' . $urlMatch . '$|s'),
 		$this->assertTrue(preg_match($expectedPreg, Utility\GeneralUtility::substUrlsInPlainText($input, 1, 'http://example.com/index.php')) == 1);
 	}
 
+	/**
+	 * @test
+	 */
+	public function testGetUrlRedirects() {
+		$url = Utility\GeneralUtility::locationHeaderUrl('/typo3/sysext/core/Tests/Unit/Utility/Fixtures/redirect.php');
+
+		Utility\GeneralUtility::setGetUrlTestMode(TRUE);
+		$content = Utility\GeneralUtility::getUrl($url);
+		Utility\GeneralUtility::setGetUrlTestMode(FALSE);
+
+		self::assertEquals('Success.', $content);
+	}
+
+	/**
+	 * @test
+	 */
+	public function testGetUrlRedirectsWithoutUrl() {
+		$url = Utility\GeneralUtility::locationHeaderUrl('/typo3/sysext/core/Tests/Unit/Utility/Fixtures/redirect.php?redirectNoLocation=1');
+
+		Utility\GeneralUtility::setGetUrlTestMode(TRUE);
+		$content = Utility\GeneralUtility::getUrl($url);
+		Utility\GeneralUtility::setGetUrlTestMode(FALSE);
+
+		self::assertFalse($content);
+	}
+
+	/**
+	 * @test
+	 */
+	public function testGetUrlRedirectsWithoutUrlAndReport() {
+		$url = Utility\GeneralUtility::locationHeaderUrl('/typo3/sysext/core/Tests/Unit/Utility/Fixtures/redirect.php?redirectNoLocation=1');
+
+		$report = array();
+
+		Utility\GeneralUtility::setGetUrlTestMode(TRUE);
+		$content = Utility\GeneralUtility::getUrl($url, 1, FALSE, $report);
+		Utility\GeneralUtility::setGetUrlTestMode(FALSE);
+
+		self::assertFalse($content);
+		self::assertEquals(302, $report['http_code']);
+		self::assertEquals(52, $report['error']);
+	}
+
+	/**
+	 * @test
+	 */
+	public function testGetUrlRedirectsWithFollowActive() {
+		$url = Utility\GeneralUtility::locationHeaderUrl('/typo3/sysext/core/Tests/Unit/Utility/Fixtures/redirect.php');
+
+		$content = Utility\GeneralUtility::getUrl($url);
+
+		self::assertEquals('Success.', $content);
+	}
+
 }
 
 ?>
