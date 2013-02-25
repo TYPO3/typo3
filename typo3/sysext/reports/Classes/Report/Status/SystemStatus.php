ee411846516d5aba9a8b005f709911e2faf223ee
Christian Kuhn <lolli@schwarzbu.ch>
2013-02-25 21:50:02 +0100

    [FEATURE] Central system environment check
    This patch adds a new structure to the install tool to centralize
    all system and especially PHP related system checks cluttered in
    various modules and parts of the core at the moment.
    
    The idea is to have a basic, standalone script to test for all
    requirements TYPO3 CMS has in order to run smoothly. The script does
    not use any TYPO3 core code and has no further dependencies. With
    a later patch, the check will be integrated as the very first
    step during install, to give a good indicator if the final
    installed system will work well or if any system constraints are
    not met.
    
    Currently, the script is included as a new section in the install
    tool, called "System environment", where all checks are performed
    and each gives a status back. This will already help to support
    questions on mailing lists and should be the first step every user
    is guided to: "Do you have warnings or errors in the system
    environment?". The install tool implementation should also help
    already if moving an instance to a different server.
    
    Furthermore, there is a simple integration in the reports module
    that points to the install tool if issues are found.
    
    The class structure is as simple as possible, it is very easy to
    adapt single checks later on, the API is a single getStatus() method
    that returns all check object of the specific tests. Due to the
    nature of the script and the later usage in the installation
    process, things like localization and so on are not done and
    should also not be implemented later on.
    
    Most current check code is fetched from existing install tool,
    reports module and bootstrap code. This also make the main
    Installer.php class of the install tool already a bit better
    maintainable since 600 lines of code are removed from it.
    
    Change-Id: I64387ca9aaa4b429ee4a909cdab4386180af0d45
    Resolves: #46219
    Releases: 6.1

--- typo3/sysext/reports/Classes/Report/Status/SystemStatus.php
+++ typo3/sysext/reports/Classes/Report/Status/SystemStatus.php
@@ -31,28 +31,6 @@ namespace TYPO3\CMS\Reports\Report\Status;
 class SystemStatus implements \TYPO3\CMS\Reports\StatusProviderInterface {
 
 	/**
-	 * PHP modules which are required. Can be changed by hook in getMissingPhpModules()
-	 *
-	 * @var array
-	 */
-	protected $requiredPhpModules = array(
-		'fileinfo',
-		'filter',
-		'gd',
-		'json',
-		'mysql',
-		'pcre',
-		'session',
-		'SPL',
-		'standard',
-		'openssl',
-		'xml',
-		'zlib',
-		'soap',
-		'zip'
-	);
-
-	/**
 	 * Determines the Install Tool's status, mainly concerning its protection.
 	 *
 	 * @return array List of statuses
@@ -61,58 +39,10 @@ class SystemStatus implements \TYPO3\CMS\Reports\StatusProviderInterface {
 	public function getStatus() {
 		$this->executeAdminCommand();
 		$statuses = array(
-			'Php' => $this->getPhpStatus(),
-			'PhpMemoryLimit' => $this->getPhpMemoryLimitStatus(),
 			'PhpPeakMemory' => $this->getPhpPeakMemoryStatus(),
-			'Webserver' => $this->getWebserverStatus(),
-			'PhpModules' => $this->getMissingPhpModules()
+			'PhpModules' => $this->getMissingPhpModulesOfExtensions()
 		);
 		return $statuses;
-	}
-
-	/**
-	 * Checks the current PHP version against a minimum required version.
-	 *
-	 * @return \TYPO3\CMS\Reports\Status A status of whether a minimum PHP version requirement is met
-	 */
-	protected function getPhpStatus() {
-		$message = '';
-		$severity = \TYPO3\CMS\Reports\Status::OK;
-		if (version_compare(phpversion(), TYPO3_REQUIREMENTS_MINIMUM_PHP) < 0) {
-			$message = $GLOBALS['LANG']->getLL('status_phpTooOld');
-			$severity = \TYPO3\CMS\Reports\Status::ERROR;
-		}
-		return \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Reports\\Status', $GLOBALS['LANG']->getLL('status_phpVersion'), phpversion(), $message, $severity);
-	}
-
-	/**
-	 * Checks the current memory limit against a minimum required version.
-	 *
-	 * @return \TYPO3\CMS\Reports\Status A status of whether a minimum memory limit requirement is met
-	 */
-	protected function getPhpMemoryLimitStatus() {
-		$memoryLimit = ini_get('memory_limit');
-		$memoryLimitBytes = \TYPO3\CMS\Core\Utility\GeneralUtility::getBytesFromSizeMeasurement($memoryLimit);
-		$message = '';
-		$severity = \TYPO3\CMS\Reports\Status::OK;
-		if ($memoryLimitBytes > 0) {
-			if ($memoryLimitBytes < \TYPO3\CMS\Core\Utility\GeneralUtility::getBytesFromSizeMeasurement(TYPO3_REQUIREMENTS_RECOMMENDED_PHP_MEMORY_LIMIT)) {
-				$message = sprintf($GLOBALS['LANG']->getLL('status_phpMemoryRecommendation'), $memoryLimit, TYPO3_REQUIREMENTS_RECOMMENDED_PHP_MEMORY_LIMIT);
-				$severity = \TYPO3\CMS\Reports\Status::WARNING;
-			}
-			if ($memoryLimitBytes < \TYPO3\CMS\Core\Utility\GeneralUtility::getBytesFromSizeMeasurement(TYPO3_REQUIREMENTS_MINIMUM_PHP_MEMORY_LIMIT)) {
-				$message = sprintf($GLOBALS['LANG']->getLL('status_phpMemoryRequirement'), $memoryLimit, TYPO3_REQUIREMENTS_MINIMUM_PHP_MEMORY_LIMIT);
-				$severity = \TYPO3\CMS\Reports\Status::ERROR;
-			}
-			if ($severity > \TYPO3\CMS\Reports\Status::OK) {
-				if ($php_ini_path = get_cfg_var('cfg_file_path')) {
-					$message .= ' ' . sprintf($GLOBALS['LANG']->getLL('status_phpMemoryEditLimit'), $php_ini_path);
-				} else {
-					$message .= ' ' . $GLOBALS['LANG']->getLL('status_phpMemoryContactAdmin');
-				}
-			}
-		}
-		return \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Reports\\Status', $GLOBALS['LANG']->getLL('status_phpMemory'), $memoryLimitBytes > 0 ? $memoryLimit : $GLOBALS['LANG']->getLL('status_phpMemoryUnlimited'), $message, $severity);
 	}
 
 	/**
@@ -134,7 +64,7 @@ class SystemStatus implements \TYPO3\CMS\Reports\StatusProviderInterface {
 	/**
 	 * Checks if there was a request in the past which approached the memory limit
 	 *
-	 * @return tx_reports_reports_status_Status	A status of whether the memory limit was approached by one of the requests
+	 * @return \TYPO3\CMS\Reports\Status A status of whether the memory limit was approached by one of the requests
 	 */
 	protected function getPhpPeakMemoryStatus() {
 		/** @var $registry \TYPO3\CMS\Core\Registry */
@@ -159,30 +89,12 @@ class SystemStatus implements \TYPO3\CMS\Reports\StatusProviderInterface {
 	}
 
 	/**
-	 * Reports the webserver TYPO3 is running on.
-	 *
-	 * @return \TYPO3\CMS\Reports\Status The server software as a status
-	 */
-	protected function getWebserverStatus() {
-		$value = $_SERVER['SERVER_SOFTWARE'];
-		$message = '';
-		// The additional information are only important on a Windows system with Apache running.
-		// Even with lowest Apache ServerTokens (Prod[uctOnly]) the name is returned.
-		if (TYPO3_OS === 'WIN' && substr($value, 0, 6) === 'Apache') {
-			$message .= '<p>' . $GLOBALS['LANG']->getLL('status_webServer_infoText') . '</p>';
-			$message .= '<div class="typo3-message message-warning">' . $GLOBALS['LANG']->getLL('status_webServer_threadStackSize') . '</div>';
-		}
-		return \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Reports\\Status', $GLOBALS['LANG']->getLL('status_webServer'), $value, $message);
-	}
-
-	/**
-	 * Reports whether any of the required PHP modules are missing
+	 * Reports whether extensions need additional PHP modules different from standard core requirements
 	 *
 	 * @return \TYPO3\CMS\Reports\Status A status of missing PHP modules
 	 */
-	protected function getMissingPhpModules() {
-		// Hook to adjust the required PHP modules
-		$modules = $this->requiredPhpModules;
+	protected function getMissingPhpModulesOfExtensions() {
+		$modules = array();
 		if (is_array(${$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install/mod/class.tx_install.php']['requiredPhpModules']})) {
 			foreach (${$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install/mod/class.tx_install.php']['requiredPhpModules']} as $classData) {
 				$hookObject = \TYPO3\CMS\Core\Utility\GeneralUtility::getUserObj($classData);
