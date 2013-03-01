a92b60716f1eb3ae9c5ba552591d9af4f0aa5388
Alexander Opitz <alexander.opitz@netresearch.de>
2013-03-01 10:26:50 +0100

    [FEATURE] Only use one select to calculate page cache timeout
    To get the first time value we do 2 SQL select statements at the
    moment but this can be combined in one SQL select statement. This
    will save as time and reduces bandwidth to/from the SQL server.
    
    The one SQL select statement from 4.6 had a bug and was changed
    to use 2 SQL select statements instead of fixing the statement itself.
    So this is a revert to the old source with the fixed statement.
    
    Resolves: #38838
    Releases: 6.1
    
    Change-Id: Ia3b106cf0232ac80d287a8c50f7c8befaf0cc29b

--- typo3/sysext/frontend/Tests/Unit/Controller/TypoScriptFrontendControllerTest.php
+++ typo3/sysext/frontend/Tests/Unit/Controller/TypoScriptFrontendControllerTest.php
@@ -35,14 +35,22 @@ class TypoScriptFrontendControllerTest extends \TYPO3\CMS\Core\Tests\UnitTestCas
 	 */
 	private $fixture;
 
+	/**
+	 * @var string Name of the testing table for start-/endtime
+	 */
+	private $testingStartEndTable;
+
 	public function setUp() {
 		$this->fixture = $this->getAccessibleMock('\\TYPO3\\CMS\\Frontend\\Controller\\TypoScriptFrontendController', array('dummy'), array(), '', FALSE);
 		$this->fixture->TYPO3_CONF_VARS = $GLOBALS['TYPO3_CONF_VARS'];
 		$this->fixture->TYPO3_CONF_VARS['SYS']['encryptionKey'] = '170928423746123078941623042360abceb12341234231';
+
+		$this->testingStartEndTable = 'Testing_start_end';
 	}
 
 	public function tearDown() {
 		unset($this->fixture);
+		$GLOBALS['TYPO3_DB']->sql_query('DROP TABLE IF EXISTS ' . $this->testingStartEndTable . ';');
 	}
 
 	////////////////////////////////
@@ -205,6 +213,165 @@ class TypoScriptFrontendControllerTest extends \TYPO3\CMS\Core\Tests\UnitTestCas
 		$this->assertSame(array_reverse($rootLine), $this->fixture->tmpl->rootLine);
 	}
 
+	/**
+	 * @test
+	 * @expectedException \InvalidArgumentException
+	 */
+	public function getFirstTimeValueForRecordException() {
+		$this->setUpStartStopTableDB();
+
+		$pageContextMock = $this->getMock('TYPO3\\CMS\\Frontend\\Page\\PageRepository');
+		$pageContextMock
+			->expects($this->any())
+			->method('getRootline')
+			->will($this->returnValue($rootLine));
+		$this->fixture->_set('sys_page', $pageContextMock);
+
+		$this->getFirstTimeValueForRecordCall('', 1);
+	}
+
+	/**
+	 * @test
+	 */
+	public function getFirstTimeValueForRecordNotSpecified() {
+		$this->setUpStartStopTableDB();
+
+		$pageContextMock = $this->getMock('TYPO3\\CMS\\Frontend\\Page\\PageRepository');
+		$pageContextMock
+			->expects($this->any())
+			->method('getRootline')
+			->will($this->returnValue($rootLine));
+		$this->fixture->_set('sys_page', $pageContextMock);
+
+		$this->assertSame(
+			$this->getFirstTimeValueForRecordCall(123, 1),
+			PHP_INT_MAX,
+			'Should be PHP_INT_MAX as table has no start/endtime in TCA'
+		);
+
+		$this->setUpStartStopTableTCA();
+		$this->assertSame(
+			$this->getFirstTimeValueForRecordCall(123, 1),
+			PHP_INT_MAX,
+			'Should be PHP_INT_MAX as table has not this PID'
+		);
+	}
+
+	/**
+	 * @test
+	 */
+	public function getFirstTimeValueForRecordReturnCorrectData() {
+		$this->setUpStartStopTableDB();
+		$this->setUpStartStopTableTCA();
+		$this->setUpStartStopTableData();
+
+		$pageContextMock = $this->getMock('TYPO3\\CMS\\Frontend\\Page\\PageRepository');
+		$pageContextMock
+			->expects($this->any())
+			->method('getRootline')
+			->will($this->returnValue($rootLine));
+		$this->fixture->_set('sys_page', $pageContextMock);
+
+		$this->assertSame(
+			$this->getFirstTimeValueForRecordCall(234, 1),
+			2,
+			'The next start/endtime should be 2'
+		);
+
+		$this->assertSame(
+			$this->getFirstTimeValueForRecordCall(234, 2),
+			3,
+			'The next starttime should be 3'
+		);
+
+		$this->assertSame(
+			$this->getFirstTimeValueForRecordCall(234, 4),
+			5,
+			'The next endtime should be 5'
+		);
+
+		$this->assertSame(
+			$this->getFirstTimeValueForRecordCall(234, 5),
+			PHP_INT_MAX,
+			'The next start/endtime should be PHP_INT_MAX as there are no more'
+		);
+	}
+
+
+	/**
+	 * Call to fixture function
+	 *
+	 * @param integer $pid PageID to test
+	 * @param integer $now Time to test
+	 */
+	public function getFirstTimeValueForRecordCall($pid, $now) {
+		return $this->fixture->_call(
+			'getFirstTimeValueForRecord',
+			$this->testingStartEndTable . ':' . $pid,
+			$now
+		);
+	}
+
+	/**
+	 * Sets up the DB used for testing Start/Stop Timing
+	 *
+	 * @return void
+	 */
+	protected function setUpStartStopTableDB() {
+		$GLOBALS['TYPO3_DB']->sql_query('CREATE TABLE ' . $this->testingStartEndTable . ' (
+			`pid` int(11) NOT NULL,
+			`starttime` int(11) NOT NULL,
+			`endtime` int(11) NOT NULL
+		) ENGINE=InnoDB;
+		');
+		if ($error = $GLOBALS['TYPO3_DB']->sql_error()) {
+			throw new Exception('SQL Error: ' . $error);
+		}
+	}
+
+	/**
+	 * Sets up the TCA used for testing Start/Stop Timing
+	 *
+	 * @return void
+	 */
+	protected function setUpStartStopTableTCA() {
+		$GLOBALS['TCA'][$this->testingStartEndTable]['ctrl']['enablecolumns']
+			= array(
+				'starttime' => 'starttime',
+				'endtime' => 'endtime',
+			);
+	}
+
+	/**
+	 * Sets up the data used for testing Start/Stop Timing
+	 *
+	 * @return void
+	 */
+	protected function setUpStartStopTableData() {
+		$GLOBALS['TYPO3_DB']->sql_query(
+			'INSERT INTO ' . $this->testingStartEndTable
+			. ' (`pid`, `starttime`, `endtime`) VALUES'
+			. '(234, 0, 1),'
+			. '(234, 0, 2),'
+			. '(234, 0, 4),'
+			. '(234, 1, 1),'
+			. '(234, 1, 2),'
+			. '(234, 1, 4),'
+			. '(234, 2, 1),'
+			. '(234, 2, 2),'
+			. '(234, 2, 4),'
+			. '(234, 3, 1),'
+			. '(234, 3, 2),'
+			. '(234, 3, 4),'
+			. '(234, 4, 1),'
+			. '(234, 4, 2),'
+			. '(234, 4, 4),'
+			. '(234, 4, 5)'
+		);
+		if ($error = $GLOBALS['TYPO3_DB']->sql_error()) {
+			throw new Exception('SQL Error: ' . $error);
+		}
+	}
 }
 
 ?>
