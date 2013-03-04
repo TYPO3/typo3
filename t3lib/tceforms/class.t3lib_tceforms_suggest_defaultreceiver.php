9c998cd2df54c557b6464d8245e88e749a712e4d
Xavier Perseguers <xavier@typo3.org>
2013-03-04 08:38:56 +0100

    [BUGFIX] Invalid replacement hints in t3lib/
    The comment and require_once call in deprecated classes are wrong.
    
    Change-Id: I1d3e49bc977eef1c69551809596a5917a7ccf1f0
    Fixes: #45998
    Releases: 6.1, 6.0
    Reviewed-on: https://review.typo3.org/18582
    Reviewed-by: Wouter Wolters
    Reviewed-by: Mattias Nilsson
    Reviewed-by: Xavier Perseguers
    Tested-by: Xavier Perseguers

--- t3lib/tceforms/class.t3lib_tceforms_suggest_defaultreceiver.php
+++ t3lib/tceforms/class.t3lib_tceforms_suggest_defaultreceiver.php
@@ -2,7 +2,7 @@
 /*
  * @deprecated since 6.0, the classname t3lib_TCEforms_Suggest_DefaultReceiver and this file is obsolete
  * and will be removed with 6.2. The class was renamed and is now located at:
- * typo3/sysext/backend/Classes/Forms/Element/SuggestDefaultReceiver.php
+ * typo3/sysext/backend/Classes/Form/Element/SuggestDefaultReceiver.php
  */
-require_once \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('backend') . 'Classes/Forms/Element/SuggestDefaultReceiver.php';
+require_once \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('backend') . 'Classes/Form/Element/SuggestDefaultReceiver.php';
 ?>
