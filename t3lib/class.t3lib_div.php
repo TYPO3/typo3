00de6a5345e9c0498cd90c2aabf1582ce53e98bb
Franz G. Jahn <franzjahn@cron-it.de>
2013-03-06 11:48:59 +0100

    [SECURITY] Open redirection with jumpurl
    jumpurl allows redirect to any given URL. A hash on the url
    is now required to know if the jumpurl has been created
    by the system or by the outside.
    
    The hook "jumpurlRedirectHandler" can be used to allow
    redirects without hash or to custom redirects.
    
    Fixes: #28587
    Releases: 6.1, 6.0, 4.7, 4.6, 4.5
    Change-Id: I63da18b1963ec50cd95dd49d1669c9873b7bab54
    Security-Commit: a368fb9aa9811fd85d8e0a635c0bf80b68e4b9ed
    Security-Bulletin: TYPO3-CORE-SA-2013-001

--- t3lib/class.t3lib_div.php
+++ t3lib/class.t3lib_div.php
@@ -933,26 +933,27 @@ final class t3lib_div {
 	 * Returns a proper HMAC on a given input string and secret TYPO3 encryption key.
 	 *
 	 * @param string $input Input string to create HMAC from
+	 * @param string $additionalSecret additionalSecret to prevent hmac beeing used in a different context
 	 * @return string resulting (hexadecimal) HMAC currently with a length of 40 (HMAC-SHA-1)
 	 */
-	public static function hmac($input) {
+	public static function hmac($input, $additionalSecret = '') {
 		$hashAlgorithm = 'sha1';
 		$hashBlocksize = 64;
 		$hmac = '';
-
+		$secret = $GLOBALS['TYPO3_CONF_VARS']['SYS']['encryptionKey'] . $additionalSecret;
 		if (extension_loaded('hash') && function_exists('hash_hmac') && function_exists('hash_algos') && in_array($hashAlgorithm, hash_algos())) {
-			$hmac = hash_hmac($hashAlgorithm, $input, $GLOBALS['TYPO3_CONF_VARS']['SYS']['encryptionKey']);
+			$hmac = hash_hmac($hashAlgorithm, $input, $secret);
 		} else {
 				// outer padding
 			$opad = str_repeat(chr(0x5C), $hashBlocksize);
 				// inner padding
 			$ipad = str_repeat(chr(0x36), $hashBlocksize);
-			if (strlen($GLOBALS['TYPO3_CONF_VARS']['SYS']['encryptionKey']) > $hashBlocksize) {
+			if (strlen($secret) > $hashBlocksize) {
 					// keys longer than block size are shorten
-				$key = str_pad(pack('H*', call_user_func($hashAlgorithm, $GLOBALS['TYPO3_CONF_VARS']['SYS']['encryptionKey'])), $hashBlocksize, chr(0x00));
+				$key = str_pad(pack('H*', call_user_func($hashAlgorithm, $secret)), $hashBlocksize, chr(0));
 			} else {
 					// keys shorter than block size are zero-padded
-				$key = str_pad($GLOBALS['TYPO3_CONF_VARS']['SYS']['encryptionKey'], $hashBlocksize, chr(0x00));
+				$key = str_pad($secret, $hashBlocksize, chr(0));
 			}
 			$hmac = call_user_func($hashAlgorithm, ($key ^ $opad) . pack('H*', call_user_func($hashAlgorithm, ($key ^ $ipad) . $input)));
 		}
