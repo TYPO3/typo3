b78570832e4dc49ce0058d730ff55208080c82b8
TYPO3 Release Team <typo3v4@typo3.org>
2013-03-06 12:35:42 +0100

    [TASK] Set TYPO3 version to 6.0.4-dev
    Change-Id: Ied84dbe8f0b087b325705329414b7db0dc73b773

--- typo3/sysext/core/Classes/Core/SystemEnvironmentBuilder.php
+++ typo3/sysext/core/Classes/Core/SystemEnvironmentBuilder.php
@@ -114,7 +114,7 @@ class SystemEnvironmentBuilder {
 	 */
 	static protected function defineBaseConstants() {
 		// This version, branch and copyright
-		define('TYPO3_version', '6.0.3');
+		define('TYPO3_version', '6.0.4-dev');
 		define('TYPO3_branch', '6.0');
 		define('TYPO3_copyright_year', '1998-2012');
 		// TYPO3 external links
