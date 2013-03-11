7a308a8ad7511f7a95cc56dc983aa3d7f7d63afe
Stefan Froemken <froemken@gmail.com>
2013-03-11 18:36:49 +0100

    [BUGFIX] IdentityProperties were not set
    After submitting an invalid form twice, extbase tries to
    create the related submodel instead of edit.
    This is because of the missing __identity part
    for the related submodel
    
    Resolves: #46185
    Releases: 6.0, 6.1, 6.2
    Change-Id: If3ec15b9eff0fc8d9a7dc682518cbfd72bb4665b
    Reviewed-on: https://review.typo3.org/21101
    Reviewed-by: Stefan Neufeind
    Reviewed-by: Stefan Froemken
    Tested-by: Stefan Froemken
    Reviewed-by: Anja Leichsenring
    Tested-by: Anja Leichsenring

--- typo3/sysext/fluid/Classes/ViewHelpers/Form/AbstractFormFieldViewHelper.php
+++ typo3/sysext/fluid/Classes/ViewHelpers/Form/AbstractFormFieldViewHelper.php
@@ -104,6 +104,7 @@ abstract class AbstractFormFieldViewHelper extends \TYPO3\CMS\Fluid\ViewHelpers\
 		if ($this->hasArgument('value')) {
 			$value = $this->arguments['value'];
 		} elseif ($this->configurationManager->isFeatureEnabled('rewrittenPropertyMapper') && $this->hasMappingErrorOccurred()) {
+			$this->addAdditionalIdentityPropertiesIfNeeded();
 			$value = $this->getLastSubmittedFormData();
 		} elseif ($this->isObjectAccessorMode() && $this->viewHelperVariableContainer->exists('TYPO3\\CMS\\Fluid\\ViewHelpers\\FormViewHelper', 'formObject')) {
 			$this->addAdditionalIdentityPropertiesIfNeeded();
