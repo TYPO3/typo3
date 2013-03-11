4e65a1296cd7ef39d262212e560815cba5458d8c
Wouter Wolters <typo3@wouterwolters.nl>
2013-03-11 22:12:20 +0100

    [BUGFIX] Old files of filelist extension are wrong
    During the namespace change the old filelist extenion files
    are not correctly set. Instead of "filelist" this was replaced
    with "file_list".
    
    Fixes: #46155
    Related: #45143
    Releases: 6.0
    
    Change-Id: I33ebb12163dd30d35981f45dcd465f7340bf1f5a
    Reviewed-on: https://review.typo3.org/18845
    Reviewed-by: Christian Kuhn
    Tested-by: Christian Kuhn

--- typo3/class.filelistfoldertree.php
+++ typo3/class.filelistfoldertree.php
@@ -4,5 +4,5 @@
  * and will be removed with 6.2. The class was renamed and is now located at:
  * typo3/sysext/file_list/Classes/FileListFolderTree.php
  */
-require_once \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('file_list') . 'Classes/FileListFolderTree.php';
+require_once \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('filelist') . 'Classes/FileListFolderTree.php';
 ?>
