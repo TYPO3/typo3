bf641d0547d3ad0da6a9f73607fd75a10b81fba5
Christian Kuhn <lolli@schwarzbu.ch>
2013-03-16 11:41:01 +0100

    [TASK] Separate ext:statictemplates code from core
    The three menu types GMENU_LAYERS GMENU_FOLDOUT and TMENU_LAYERS use
    javascript files of statictemplates and belong to the extensions.
    Using the new factory of issue #46292, those three menu objects are
    now transferred to statictemplates extension.
    
    For upgrade to TYPO3 CMS 4.5 an ugrade wizard was created to install
    statictemplates system extension if the database tables was in use.
    Everything before 4.5 is out of support for a long time, so we can
    expect the extension to be already loaded if it is used. The old
    upgrade wizard is removed now.
    
    Some statictemplates specific code in ext:tstemplate is substituted
    with hooks, used by statictemplates.
    
    Change-Id: Id14dca12f09051c3003fedaeb971fef457f7a17e
    Resolves: #46358
    Related: #46292
    Releases: 6.1

--- typo3/sysext/statictemplates/ext_localconf.php
+++ typo3/sysext/statictemplates/ext_localconf.php
@@ -2,5 +2,29 @@
 if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tstemplate.php']['includeStaticTypoScriptSources'][] = 'EXT:statictemplates/Classes/StaticTemplatesHook.php:TYPO3\\CMS\\Statictemplates\\StaticTemplatesHook->includeStaticTypoScriptSources';
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tstemplate.php']['includeStaticTypoScriptSources'][] =
+	'TYPO3\\CMS\\Statictemplates\\StaticTemplatesHook->includeStaticTypoScriptSources';
+
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['TYPO3\\CMS\\Tstemplate\\Controller\\TypoScriptTemplateModuleController']['newStandardTemplateView'] =
+	'TYPO3\\CMS\\Statictemplates\\TypoScriptTemplateModuleControllerHook->render';
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['TYPO3\\CMS\\Tstemplate\\Controller\\TypoScriptTemplateModuleController']['newStandardTemplateHandler'] =
+	'TYPO3\\CMS\\Statictemplates\\TypoScriptTemplateModuleControllerHook->handle';
+
+// Register GMENU_LAYERS, GMENU_FOLDOUT and TMENU_LAYERS menu objects
+/** @var $menuContentObjectFactory \TYPO3\CMS\Frontend\ContentObject\Menu\MenuContentObjectFactory */
+$menuContentObjectFactory = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
+	'TYPO3\\CMS\\Frontend\\ContentObject\\Menu\\MenuContentObjectFactory'
+);
+$menuContentObjectFactory->registerMenuType(
+	'GMENU_LAYERS',
+	'TYPO3\\CMS\\Statictemplates\\ContentObject\\Menu\\GraphicalMenuLayersContentObject'
+);
+$menuContentObjectFactory->registerMenuType(
+	'GMENU_FOLDOUT',
+	'TYPO3\\CMS\\Statictemplates\\ContentObject\\Menu\\GraphicalMenuFoldoutContentObject'
+);
+$menuContentObjectFactory->registerMenuType(
+	'TMENU_LAYERS',
+	'TYPO3\\CMS\\Statictemplates\\ContentObject\\Menu\\TextMenuLayersContentObject'
+);
 ?>
