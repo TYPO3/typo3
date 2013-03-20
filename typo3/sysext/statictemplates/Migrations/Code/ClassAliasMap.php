6b11059995f8a6ee0479553a44e9f74858244d2c
Christian Kuhn <lolli@schwarzbu.ch>
2013-03-20 19:47:21 +0100

    [!!!][-FEATURE] Remove ext:statictemplates
    The patch removes the hopelessly outdated extension statictemplates.
    
    Change-Id: Ic19d8b8d6cc99eead98a7a27d91e5a9d42676254
    Resolves: #46469
    Related: #46470
    Related: #46358
    Releases: 6.1
    Reviewed-on: https://review.typo3.org/19113
    Reviewed-by: Christian Kuhn
    Tested-by: Christian Kuhn

--- typo3/sysext/statictemplates/Migrations/Code/ClassAliasMap.php
+++
@@ -1,5 +0,0 @@
-<?php
-return array(
-	'tx_statictemplates' => 'TYPO3\\CMS\\Statictemplates\\StaticTemplatesHook',
-);
-?>
--- typo3/sysext/statictemplates/class.tx_statictemplates.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*
- * @deprecated since 6.0, the classname tx_statictemplates and this file is obsolete
- * and will be removed with 6.2. The class was renamed and is now located at:
- * typo3/sysext/statictemplates/Classes/StaticTemplatesHook.php
- */
-require_once \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('statictemplates') . 'Classes/StaticTemplatesHook.php';
-?>
