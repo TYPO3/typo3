6fdc197d27d6c4786e72a8f9e441a0570d4830e7
Andreas Wolf <andreas.wolf@typo3.org>
2013-03-22 22:54:24 +0100

    [BUGFIX] Automatic creation of processed files folder fails
    The ResourceStorage tries to automatically create the processed files
    folder if it does not exist. The identifier of the folder is taken from
    the storage's configuration.
    
    The automatic creation however fails, because when splitting the
    identifier (e.g. /someFolder/_processed_/) by '/', the resulting array
    always contains empty elements. Creating a folder with an empty name
    fails however; therefore, empty elements should be skipped.
    
    Change-Id: Ia9d0fe5314f67cb01da20682dfb684b711bc3cf7
    Resolves: #46586
    Releases: 6.1, 6.0

--- typo3/sysext/core/Classes/Resource/ResourceStorage.php
+++ typo3/sysext/core/Classes/Resource/ResourceStorage.php
@@ -2006,6 +2006,9 @@ class ResourceStorage {
 				$processingFolderParts = explode('/', $processingFolder);
 				$parentFolder = $this->driver->getRootLevelFolder();
 				foreach ($processingFolderParts as $folderPart) {
+					if ($folderPart === '') {
+						continue;
+					}
 					if (!$this->driver->folderExistsInFolder($folderPart, $parentFolder)) {
 						$parentFolder = $this->driver->createFolder($folderPart, $parentFolder);
 					} else {
