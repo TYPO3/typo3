80640ad2607188ded7010be42359709458af9098
Steffen Ritter <info@rs-websystems.de>
2013-03-23 15:19:01 +0100

    [BUGFIX] Upload in backend does not index file
    If a file is uploaded within the backend (f.e. file-module)
    the file is not automatically indexed. If done within the
    file-module the indexing happens when the folder is shown.
    If the file is uploaded somehow else using the
    ExtendedFileUtility it's not indexed.
    
    Add the indexing to the upload function.
    
    Releases: 6.0, master
    Fixes: #46595
    Change-Id: I9ea30797390f960a8e382a1f2c456259d3daa60c

--- typo3/sysext/core/Classes/Utility/File/ExtendedFileUtility.php
+++ typo3/sysext/core/Classes/Utility/File/ExtendedFileUtility.php
@@ -1,5 +1,7 @@
 <?php
 namespace TYPO3\CMS\Core\Utility\File;
+use TYPO3\CMS\Core\Resource\File;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 /***************************************************************
  *  Copyright notice
@@ -26,6 +28,7 @@ namespace TYPO3\CMS\Core\Utility\File;
  *
  *  This copyright notice MUST APPEAR in all copies of the script!
  ***************************************************************/
+
 /**
  * Extending class to class t3lib_basicFileFunctions
  *
@@ -141,6 +144,12 @@ class ExtendedFileUtility extends \TYPO3\CMS\Core\Utility\File\BasicFileUtility 
 	 */
 	protected $fileFactory;
 
+
+	/**
+	 * @var \TYPO3\CMS\Core\Resource\FileRepository
+	 */
+	protected $fileRepository;
+
 	/**
 	 * Initialization of the class
 	 *
@@ -157,6 +166,8 @@ class ExtendedFileUtility extends \TYPO3\CMS\Core\Utility\File\BasicFileUtility 
 		$this->unzipPath = $unzipPath;
 		// Initialize Object Factory
 		$this->fileFactory = \TYPO3\CMS\Core\Resource\ResourceFactory::getInstance();
+		// Initialize Object Factory
+		$this->fileRepository = GeneralUtility::makeInstance('TYPO3\\CMS\\Core\\Resource\\FileRepository');
 		// Initializing file processing commands:
 		$this->fileCmdMap = $fileCmds;
 	}
@@ -853,7 +864,10 @@ class ExtendedFileUtility extends \TYPO3\CMS\Core\Utility\File\BasicFileUtility 
 				} else {
 					$conflictMode = 'cancel';
 				}
-				$resultObjects[] = $targetFolderObject->addUploadedFile($fileInfo, $conflictMode);
+				/** @var $fileObject File */
+				$fileObject = $targetFolderObject->addUploadedFile($fileInfo, $conflictMode);
+				$this->fileRepository->addToIndex($fileObject);
+				$resultObjects[] = $fileObject;
 				$this->writelog(1, 0, 1, 'Uploading file "%s" to "%s"', array($fileInfo['name'], $targetFolderObject->getIdentifier()));
 			} catch (\TYPO3\CMS\Core\Resource\Exception\UploadException $e) {
 				$this->writelog(1, 2, 106, 'The upload has failed, no uploaded file found!', '');
@@ -871,6 +885,7 @@ class ExtendedFileUtility extends \TYPO3\CMS\Core\Utility\File\BasicFileUtility 
 				$this->writelog(1, 1, 100, 'Uploaded file could not be moved! Write-permission problem in "%s"?', array($targetFolderObject->getIdentifier()));
 			}
 		}
+
 		return $resultObjects;
 	}
 
