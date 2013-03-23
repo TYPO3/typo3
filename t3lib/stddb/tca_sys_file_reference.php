285178dd77170bb0271c7efcb3916443ebc670f8
Benjamin Mack <benni@typo3.org>
2013-03-23 11:44:38 +0100

    [BUG] sys_file_reference: missing exclude fields for Non-Admins
    Currently, only link, but not description and alternative
    is a exclude field. So the others are ALWAYS visible.
    They need to be adjusted as well.
    
    Releases: 6.1, 6.0
    Resolves: #43939
    Change-Id: I47ed483c81dc354ad3d8f13ad1029c77c920a0e4

--- t3lib/stddb/tca_sys_file_reference.php
+++ t3lib/stddb/tca_sys_file_reference.php
@@ -160,7 +160,7 @@ $TCA['sys_file_reference'] = array(
 		'description' => array(
 			// This is used for captions in the frontend
 			'l10n_mode' => 'mergeIfNotBlank',
-			'exclude' => 0,
+			'exclude' => 1,
 			'label' => 'LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.description',
 			'config' => array(
 				'type' => 'text',
@@ -172,7 +172,7 @@ $TCA['sys_file_reference'] = array(
 		),
 		'alternative' => array(
 			'l10n_mode' => 'mergeIfNotBlank',
-			'exclude' => 0,
+			'exclude' => 1,
 			'label' => 'LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.alternative',
 			'config' => array(
 				'type' => 'input',
