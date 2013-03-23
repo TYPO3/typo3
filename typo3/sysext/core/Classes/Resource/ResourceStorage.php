c78da7672369d8b8e6f724798fa52c667b95ae63
Andreas Wolf <andreas.wolf@typo3.org>
2013-03-23 11:12:38 +0100

    [BUGFIX] Segfaults on object comparisons
    In the ResourceStorage class, the identity of source and target storage
    has to be verified for several operations (copy, move). When the
    comparison is done with a simple equality operator (==), the properties
    of both objects are compared when the objects are not identical. This
    comparison led to a segfault in several environments, presumably because
    of a meanwhile fixed PHP bug (https://bugs.php.net/bug.php?id=61557).
    
    To prevent these segfaults on affected PHP versions, the comparison is
    extended to check for object identity (===). This will not affect the
    behaviour of the code, as there should always be only one object per
    storage.
    
    Change-Id: I378c447f75c00859447cf1443f64b81e54161c54
    Resolves: #46584
    Releases: 6.1, 6.0

--- typo3/sysext/core/Classes/Resource/ResourceStorage.php
+++ typo3/sysext/core/Classes/Resource/ResourceStorage.php
@@ -1026,7 +1026,7 @@ class ResourceStorage {
 		// Call driver method to create a new file from an existing file object,
 		// and return the new file object
 		try {
-			if ($sourceStorage == $this) {
+			if ($sourceStorage === $this) {
 				$newFileObject = $this->driver->copyFileWithinStorage($file, $targetFolder, $targetFileName);
 			} else {
 				$tempPath = $file->getForLocalProcessing();
@@ -1149,7 +1149,7 @@ class ResourceStorage {
 		// Call driver method to move the file that also updates the file
 		// object properties
 		try {
-			if ($sourceStorage == $this) {
+			if ($sourceStorage === $this) {
 				$newIdentifier = $this->driver->moveFileWithinStorage($file, $targetFolder, $targetFileName);
 				$this->updateFile($file, $newIdentifier);
 			} else {
@@ -1365,7 +1365,7 @@ class ResourceStorage {
 		// Get all file objects now so we are able to update them after moving the folder
 		$fileObjects = $this->getAllFileObjectsInFolder($folderToMove);
 		try {
-			if ($sourceStorage == $this) {
+			if ($sourceStorage === $this) {
 				$fileMappings = $this->driver->moveFolderWithinStorage($folderToMove, $targetParentFolder, $newFolderName);
 			} else {
 				$fileMappings = $this->moveFolderBetweenStorages($folderToMove, $targetParentFolder, $newFolderName);
@@ -1420,7 +1420,7 @@ class ResourceStorage {
 		// call driver method to move the file
 		// that also updates the file object properties
 		try {
-			if ($sourceStorage == $this) {
+			if ($sourceStorage === $this) {
 				$this->driver->copyFolderWithinStorage($folderToCopy, $targetParentFolder, $newFolderName);
 				$returnObject = $this->getFolder($targetParentFolder->getSubfolder($newFolderName)->getIdentifier());
 			} else {
