51abd60ad2c3eda989e4fb0a753bf4605f5c63ac
Benjamin Mack <benni@typo3.org>
2013-03-23 15:04:41 +0100

    [BUGFIX] FAL Upgrade Wizards do not set pid
    The sys_file_reference records are originally set to
    be placed on rootLevel, the upgrade wizard does
    exactly that.
    
    However, the concept in this case is flawed
    - When creating new sys_file_reference records via IRRE,
       the sys_file_reference is stored on the page
    - When copying moving e.g. a tt_content record, all its
       child elements are placed on that new page as
       well (so this works)
    - rootLevel restriction is not needed in this case
    
    The patch does the following:
    - Modify the existing upgrade wizard to place the
       sys_file_references on the pages of the foreign
       tables (tt_content pid)
    - the rootLevel flag for sys_file_reference is removed
    - Add a new upgrade wizard is added to ensure the
    integrity of the sys-file-reference pids
    
    Releases: 6.1, 6.0
    Resolves: #46497
    Change-Id: Ic592024669f68ed1606ed36eb050d1dfa5ad66bb

--- t3lib/stddb/tables.php
+++ t3lib/stddb/tables.php
@@ -438,7 +438,6 @@ $TCA['sys_file_reference'] = array(
 		'cruser_id' => 'cruser_id',
 		'type' => 'uid_local:type',
 		'hideTable' => TRUE,
-		'rootLevel' => TRUE,
 		'sortby' => 'sorting',
 		'delete' => 'deleted',
 		'versioningWS' => TRUE,
