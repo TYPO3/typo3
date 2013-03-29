25ef78597e3d7fab67f76de8aa7cafb55e8e88bb
Christian Kuhn <lolli@schwarzbu.ch>
2013-03-29 15:42:31 +0100

    [BUGFIX][Cache] Wincache backend class constructor
    The class constructor of wincache cache backend misses the first
    parameter $context, so $options is treated as $context. Given
    backend options are not used this way, for example defaultLifetime
    has no effect.
    The patch adds the missing constructor parameter to be in line
    with the other cache backends.
    
    Change-Id: I488b9996d5ab65420f79b2cab4c75805ae89a77a
    Resolves: #46768
    Releases: 6.1, 6.0

--- typo3/sysext/core/Classes/Cache/Backend/WincacheBackend.php
+++ typo3/sysext/core/Classes/Cache/Backend/WincacheBackend.php
@@ -57,14 +57,15 @@ class WincacheBackend extends \TYPO3\CMS\Core\Cache\Backend\AbstractBackend impl
 	/**
 	 * Constructs this backend
 	 *
-	 * @param mixed $options Configuration options - unused here
+	 * @param string $context FLOW3's application context
+	 * @param array $options Configuration options
 	 * @throws \TYPO3\CMS\Core\Cache\Exception If wincache PHP extension is not loaded
 	 */
-	public function __construct($options = array()) {
+	public function __construct($context, array $options = array()) {
 		if (!extension_loaded('wincache')) {
 			throw new \TYPO3\CMS\Core\Cache\Exception('The PHP extension "wincache" must be installed and loaded in order to use the wincache backend.', 1343331520);
 		}
-		parent::__construct($options);
+		parent::__construct($context, $options);
 	}
 
 	/**
