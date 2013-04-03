09829bf1905a107117c48a43b6e7888624292081
Wouter Wolters <typo3@wouterwolters.nl>
2013-04-03 20:44:22 +0200

    [TASK] Introduce jQuery in the Install Tool
    Introduce jQuery in the Install Tool and remove all
    prototype JavaScript.
    
    Change-Id: I35f9ed136959c1bb523b6005fa8d8541181d09f9
    Resolves: #46897
    Releases: 6.2
    Reviewed-on: https://review.typo3.org/19548
    Reviewed-by: Christian Kuhn
    Tested-by: Christian Kuhn
    Reviewed-by: Anja Leichsenring
    Tested-by: Anja Leichsenring

--- typo3/sysext/install/Resources/Public/Javascript/install.js
+++ typo3/sysext/install/Resources/Public/Javascript/install.js
@@ -1,66 +1,44 @@
 /***************************************************************
-*
-*  Various JavaScript functions for the Install Tool
-*
-*  Copyright notice
-*
-*  (c) 2009-2010 Marcus Krause, Helmut Hummel, Lars Houmark
-*  All rights reserved
-*
-*  This script is part of the TYPO3 backend provided by
-*  Kasper Skaarhoj <kasper@typo3.com> together with TYPO3
-*
-*  Released under GNU/GPL (see license file in /typo3/)
-*
-*  This script is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-*
-*  This copyright notice MUST APPEAR in all copies of this script
-*
-***************************************************************/
-
+ *
+ *  Various JavaScript functions for the Install Tool
+ *
+ *  Copyright notice
+ *
+ *  (c) 2009-2010 Marcus Krause, Helmut Hummel, Lars Houmark
+ *  (c) 2013 Wouter Wolters <typo3@wouterwolters.nl>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 backend provided by
+ *  Kasper Skaarhoj <kasper@typo3.com> together with TYPO3
+ *
+ *  Released under GNU/GPL (see license file in /typo3/)
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ *  This copyright notice MUST APPEAR in all copies of this script
+ *
+ ***************************************************************/
 
 /**
- * Javascript class to provide AJAX calls for Install Tool
+ * Method to detect if Install Tool is loaded as standalone
+ * If this is the case then an extra class will be added to the body tag
  *
- * @author Marcus Krause, Helmut Hummel <security@typo3.org>
+ * @author Wouter Wolters <typo3@wouterwolters.nl>
  */
-var EncryptionKey = {
-	thisScript: '../../index.php',
-	eID: 'tx_install_ajax',
-	cmd: 'encryptionKey',
-
-		// loads the ecryption key by an AJAX call
-	load: function(obj) {
-			// fallback if AJAX is not possible (e.g. IE < 6)
-		if (typeof Ajax.getTransport() != 'object') {
-			window.location.href = this.thisScript + '?eID=' + this.eID + '&cmd=' + this.cmd;
-			return;
-		}
-
-		new Ajax.Request(this.thisScript, {
-			method: 'get',
-			parameters: '?eID=' + this.eID + '&cmd=' + this.cmd,
-			onComplete: function(xhr) {
-				document.getElementsByName('TYPO3_INSTALL[LocalConfiguration][encryptionKey]').item(0).value=xhr.responseText;
-			}.bind(this)
-		});
-	}
-};
-
-/**
- * Prototype method to detect if the Install Tool is loaded
- * in the backend or as a standalone.
- *
- * If it standalone, a class is added to the body tag in order
- * to different the CSS style for that version.
- *
- * @author Lars Houmark <lars@houmark.com>
- */
-document.observe("dom:loaded", function() {
+$(document).ready(function() {
 	if (top.location === document.location) {
-			// standalone-mode, add class to the body tag
-			$(document.body).addClassName('standalone');
+		// standalone-mode, add class to the body tag
+		$('body').addClass('standalone');
 	}
+
+	$('#encryptionKey').click(function() {
+		$.ajax({
+			url: '../../index.php?eID=tx_install_ajax&cmd=encryptionKey'
+		}).done(function(data) {
+			$('#t3-install-form-encryptionkey').val(data);
+		});
+	});
+
 });
