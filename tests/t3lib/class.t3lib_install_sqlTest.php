96f0592fa2156898aae2eaf99eac3c6298107ba9
Mario Rimann <mario.rimann@typo3.org>
2013-04-05 23:10:29 +0200

    [BUGFIX] False-Positives in SQL comparison
    When one has e.g. a field "foo INT(11) DEFAULT '0' NOT NULL" in
    it's ext_tables.sql definition, the comparison will always complain
    about that field, since the underlying DB lists this field as "int(11)"
    which is lowercased.
    
    This integrates a regex that lowercases the field types before
    comparing the field from the definition against the existing field.
    
    Change-Id: If76abbbca56d0ef0ab796a7f4e6bee1197ac39e6
    Resolves: #41344
    Releases: 6.2, 6.1, 6.0, 4.7, 4.5

---
+++ tests/t3lib/class.t3lib_install_sqlTest.php
@@ -0,0 +1,163 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2013 Mario Rimann <mario.rimann@typo3.org>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *  A copy is found in the textfile GPL.txt and important notices to the license
+ *  from the author is found in LICENSE.txt distributed with these scripts.
+ *
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * Testcase for class "t3lib_install_sql"
+ *
+ * @author Mario Rimann <mario.rimann@typo3.org>
+ */
+class t3lib_install_sqlTest extends tx_phpunit_testcase {
+
+	/**
+	 * @var t3lib_install_sql
+	 */
+	protected $fixture;
+
+	/**
+	 * Sets up the fixture for testing
+	 */
+	public function setUp() {
+		$this->fixture = new t3lib_install_sql();
+	}
+
+	/**
+	 * Tears down the fixture
+	 */
+	public function tearDown() {
+		unset($this->fixture);
+	}
+
+	/**
+	 * @test
+	 */
+	public function getDatabaseExtraFindsChangedFields() {
+		$differenceArray = $this->fixture->getDatabaseExtra(
+			array(
+				'tx_foo' => array(
+					'fields' => array(
+						'foo' => 'varchar(999) DEFAULT \'0\' NOT NULL'
+					)
+				)
+			),
+			array(
+				'tx_foo' => array(
+					'fields' => array(
+						'foo' => 'varchar(255) DEFAULT \'0\' NOT NULL'
+					)
+				)
+			)
+		);
+
+
+
+		$this->assertEquals(
+			$differenceArray,
+			array(
+				'extra' => array(),
+				'diff' => array(
+					'tx_foo' => array(
+						'fields' => array(
+							'foo' => 'varchar(999) DEFAULT \'0\''
+						)
+					)
+				),
+				diff_currentValues => array(
+					'tx_foo' => array(
+						'fields' => array(
+							'foo' => 'varchar(255) DEFAULT \'0\''
+						)
+					)
+				)
+			)
+		);
+	}
+
+	/**
+	 * @test
+	 */
+	public function getDatabaseExtraIgnoresCaseDifference() {
+		$differenceArray = $this->fixture->getDatabaseExtra(
+			array(
+				'tx_foo' => array(
+					'fields' => array(
+						'foo' => 'INT(11) DEFAULT \'0\' NOT NULL'
+					)
+				)
+			),
+			array(
+				'tx_foo' => array(
+					'fields' => array(
+						'foo' => 'int(11) DEFAULT \'0\' NOT NULL'
+					)
+				)
+			)
+		);
+
+
+		$this->assertEquals(
+			$differenceArray,
+			array(
+				'extra' => array(),
+				'diff' => array(),
+				diff_currentValues => NULL
+			)
+		);
+	}
+
+	/**
+	 * @test
+	 */
+	public function getDatabaseExtraDoesNotLowercaseReservedWordsForTheComparison() {
+		$differenceArray = $this->fixture->getDatabaseExtra(
+			array(
+				'tx_foo' => array(
+					'fields' => array(
+						'PRIMARY KEY (md5hash)'
+					)
+				)
+			),
+			array(
+				'tx_foo' => array(
+					'fields' => array(
+						'PRIMARY KEY (md5hash)')
+				)
+			)
+		);
+
+
+		$this->assertEquals(
+			$differenceArray,
+			array(
+				'extra' => array(),
+				'diff' => array(),
+				diff_currentValues => NULL
+			)
+		);
+	}
+}
+
+?>
