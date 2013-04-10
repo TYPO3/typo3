1ad7632c815e6f2e1a5ffef3cba641c8f9db85d9
Philipp Gampe <philipp.gampe@typo3.org>
2013-04-10 17:41:53 +0200

    [BUGFIX] Illegal string offset 'translations' in DatabaseRecordList
    The return value of TranslationConfigurationProvider->translationInfo
    normally is an array, but it is a string in case of an error.
    Move the line below the is_array() check, because it is only needed
    inside.
    
    Fixes: #47103
    Releases: 6.2, 6.1, 6.0, 4.7, 4.5
    Change-Id: Ia781eae987d9295bc2b299f83c087669c96dfd00
    Reviewed-on: https://review.typo3.org/19779
    Reviewed-by: Dmitry Dulepov
    Reviewed-by: Alexander Opitz
    Reviewed-by: Heiko Kromm
    Tested-by: Heiko Kromm
    Reviewed-by: Markus Klein
    Tested-by: Markus Klein

--- typo3/sysext/backend/Classes/RecordList/AbstractRecordList.php
+++ typo3/sysext/backend/Classes/RecordList/AbstractRecordList.php
@@ -154,8 +154,8 @@ abstract class AbstractRecordList {
 	 */
 	public $languageIconTitles = array();
 
-	// TranslateTools object
 	/**
+	 * @var \TYPO3\CMS\Backend\Configuration\TranslationConfigurationProvider
 	 * @todo Define visibility
 	 */
 	public $translateTools;
