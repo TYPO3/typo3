b8f38556e7853f5c1fcaf3898996d528f667a148
Christian Kuhn <lolli@schwarzbu.ch>
2013-04-12 15:35:24 +0200

    [BUGFIX] Referenced file names in ext_localconf
    Hook registrations can live without file names using
    the class loader.
    
    Change-Id: I3fec5b2fc73f7cb4e041d33febe278313b4fb15b
    Fixes: #47164
    Releases: 6.1
    Reviewed-on: https://review.typo3.org/19845
    Reviewed-by: Christian Kuhn
    Tested-by: Christian Kuhn

--- typo3/sysext/dbal/ext_localconf.php
+++ typo3/sysext/dbal/ext_localconf.php
@@ -8,11 +8,13 @@ $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\\CMS\\Core\\Database\\SqlPa
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\\CMS\\Recordlist\\RecordList\\DatabaseRecordList'] = array('className' => 'TYPO3\\CMS\\Dbal\\RecordList\\DatabaseRecordList');
 
 // Register a hook for the installer
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install/mod/class.tx_install.php']['stepOutput'][] = 'EXT:dbal/Classes/Hooks/InstallToolHooks.php:TYPO3\\CMS\\Dbal\\Hooks\\InstallToolHooks';
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install/mod/class.tx_install.php']['writeLocalconf'][] = 'EXT:dbal/Classes/Hooks/InstallToolHooks.php:TYPO3\\CMS\\Dbal\\Hooks\\InstallToolHooks';
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install/mod/class.tx_install.php']['requiredPhpModules'][] = 'EXT:dbal/Classes/Hooks/InstallToolHooks.php:TYPO3\\CMS\\Dbal\\Hooks\\InstallToolHooks';
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install/mod/class.tx_install.php']['stepOutput'][] = 'TYPO3\\CMS\\Dbal\\Hooks\\InstallToolHooks';
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install/mod/class.tx_install.php']['writeLocalconf'][] = 'TYPO3\\CMS\\Dbal\\Hooks\\InstallToolHooks';
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install/mod/class.tx_install.php']['requiredPhpModules'][] = 'TYPO3\\CMS\\Dbal\\Hooks\\InstallToolHooks';
+
 // Register a hook for the extension manager
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['typo3/mod/tools/em/index.php']['checkDBupdates'][] = 'EXT:dbal/Classes/Hooks/ExtensionManagerHooks.php:TYPO3\\CMS\\Dbal\\Hooks\\ExtensionManagerHooks';
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['typo3/mod/tools/em/index.php']['checkDBupdates'][] = 'TYPO3\\CMS\\Dbal\\Hooks\\ExtensionManagerHooks';
+
 // Register caches if not already done in localconf.php or a previously loaded extension.
 if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['dbal'])) {
 	$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['dbal'] = array(
