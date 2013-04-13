5facaea304da8bf3c69a00ded5c80819a39e3123
Christian Kuhn <lolli@schwarzbu.ch>
2013-04-13 14:13:56 +0200

    [BUGFIX] Inefficient cache handling in LocalizationFactory
    Calling has() in front of get() is inefficient and can be optimized.
    
    Change-Id: Id8048023cc2305534a494664b402d47911087db6
    Fixes: #47184
    Releases: 6.1, 6.0
    Reviewed-on: https://review.typo3.org/19921
    Reviewed-by: Christian Kuhn
    Tested-by: Christian Kuhn

--- typo3/sysext/core/Classes/Localization/LocalizationFactory.php
+++ typo3/sysext/core/Classes/Localization/LocalizationFactory.php
@@ -96,12 +96,14 @@ class LocalizationFactory implements \TYPO3\CMS\Core\SingletonInterface {
 			if ($this->store->hasData($fileReference, $languageKey)) {
 				return $this->store->getData($fileReference);
 			}
+
 			// If the content is in cache (system cache), use it
-			if ($this->cacheInstance->has($hash)) {
-				// Load data from the caching framework
-				$this->store->setData($fileReference, $languageKey, $this->cacheInstance->get($hash));
+			$data = $this->cacheInstance->get($hash);
+			if ($data !== FALSE) {
+				$this->store->setData($fileReference, $languageKey, $data);
 				return $this->store->getData($fileReference, $languageKey);
 			}
+
 			$this->store->setConfiguration($fileReference, $languageKey, $charset);
 			/** @var $parser \TYPO3\CMS\Core\Localization\Parser\LocalizationParserInterface */
 			$parser = $this->store->getParserInstance($fileReference);
