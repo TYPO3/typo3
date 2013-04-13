08275265b876724b388493a22f55858c11d9ea55
Stefan Neufeind <typo3.neufeind@speedpartner.de>
2013-04-14 00:20:36 +0200

    [TASK] Add namespaing to ConfigurationManagerInterface
    Resolves: #47201
    Releases: 6.0, 6.1
    Change-Id: I5098a63e9cad8848e2c14da9d9e99de6885d45e9
    Reviewed-on: https://review.typo3.org/19958
    Reviewed-by: Wouter Wolters
    Tested-by: Wouter Wolters

--- typo3/sysext/fluid/Classes/ViewHelpers/Be/TableListViewHelper.php
+++ typo3/sysext/fluid/Classes/ViewHelpers/Be/TableListViewHelper.php
@@ -92,7 +92,7 @@ class TableListViewHelper extends \TYPO3\CMS\Fluid\ViewHelpers\Be\AbstractBacken
 		$dblist->alternateBgColors = $alternateBackgroundColors;
 		$dblist->clickMenuEnabled = $enableClickMenu;
 		if ($storagePid === NULL) {
-			$frameworkConfiguration = $this->configurationManager->getConfiguration(\Tx_Extbase_Configuration_ConfigurationManagerInterface::CONFIGURATION_TYPE_FRAMEWORK);
+			$frameworkConfiguration = $this->configurationManager->getConfiguration(\TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface::CONFIGURATION_TYPE_FRAMEWORK);
 			$storagePid = $frameworkConfiguration['persistence']['storagePid'];
 		}
 		$dblist->start($storagePid, $tableName, (integer) \t3lib_div::_GP('pointer'), $filter, $levels, $recordsPerPage);
