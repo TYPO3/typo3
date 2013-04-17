edea68f07935f7e17f1d75d29f9884ce37af9d11
Dmitry Dulepov <dmitry.dulepov@gmail.com>
2013-04-17 11:26:01 +0400

    [FEATURE] Improve userFunc parameter parsing in conditions
    userFunc TypoScript conditions can be improved to use CSV-like
    syntax for parameters.
    
    Releases: 6.2
    Resolves: #47301
    Depends on: #47159
    
    Change-Id: Ia2a170d0b8eb160607fbd39bd9afbe6ffab679a5

---
+++ typo3/sysext/core/Tests/Unit/Configuration/ConditionMatcherTest.php
@@ -0,0 +1,159 @@
+<?php
+namespace TYPO3\CMS\Core\Tests\Unit\Configuration;
+
+use TYPO3\CMS\Backend\Configuration\TypoScript\ConditionMatching\ConditionMatcher;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2013 Dmitry Dulepov <dmitry.dulepov@gmail.com>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+require_once(ExtensionManagementUtility::extPath('core', 'Tests//Unit//Configuration//ConditionMatcherUserFuncs.php'));
+
+/**
+ * Test class for ConditionMatcher functions
+ *
+ * @author Dmitry Dulepov <dmitry.dulepov@gmail.com>
+ */
+class ConditionMatcherTest extends \TYPO3\CMS\Core\Tests\UnitTestCase {
+
+	/** @var ConditionMatcher */
+	protected $conditionMatcher;
+
+	public function setUp() {
+		$this->conditionMatcher = GeneralUtility::makeInstance('TYPO3\\CMS\\Backend\\Configuration\\TypoScript\\ConditionMatching\\ConditionMatcher');
+	}
+
+	/**
+	 * Tests that the condition matches calls user function with multiple
+	 * arguments.
+	 *
+	 * @test
+	 * @return void
+	 */
+	public function testUserFuncIsCalled() {
+		$this->assertTrue($this->conditionMatcher->match('[userFunc = user_testFunction]'));
+	}
+
+	/**
+	 * Tests that the condition matches calls user function with multiple
+	 * arguments.
+	 *
+	 * @test
+	 * @return void
+	 */
+	public function testUserFuncWithSingleArgument() {
+		$this->assertTrue($this->conditionMatcher->match('[userFunc = user_testFunctionWithSingleArgument(x)]'));
+	}
+
+	/**
+	 * Tests that the condition matches calls user function with multiple
+	 * arguments.
+	 *
+	 * @test
+	 * @return void
+	 */
+	public function testUserFuncWithMultipleArguments() {
+		$this->assertTrue($this->conditionMatcher->match('[userFunc = user_testFunctionWithThreeArguments(1,2,3)]'));
+	}
+
+	/**
+	 * Tests that the condition matches calls user function with multiple
+	 * arguments.
+	 *
+	 * @test
+	 * @return void
+	 */
+	public function testUserFuncReturnsFalse() {
+		$this->assertFalse($this->conditionMatcher->match('[userFunc = user_testFunctionFalse]'));
+	}
+
+	/**
+	 * Tests that the condition matches calls user function with multiple
+	 * arguments.
+	 *
+	 * @test
+	 * @return void
+	 */
+	public function testUserFuncWithMultipleArgumentsAndQuotes() {
+		$this->assertTrue($this->conditionMatcher->match('[userFunc = user_testFunctionWithThreeArguments(1,2,"3,4,5,6")]'));
+	}
+
+	/**
+	 * Tests that the condition matches calls user function with multiple
+	 * arguments.
+	 *
+	 * @test
+	 * @return void
+	 */
+	public function testUserFuncWithMultipleArgumentsAndQuotesAndSpaces() {
+		$this->assertTrue($this->conditionMatcher->match('[userFunc = user_testFunctionWithThreeArguments ( 1 , 2, "3, 4, 5, 6" ) ]'));
+	}
+
+	/**
+	 * Tests that the condition matches calls user function with multiple
+	 * arguments.
+	 *
+	 * @test
+	 * @return void
+	 */
+	public function testUserFuncWithMultipleArgumentsAndQuotesAndSpacesStripped() {
+		$this->assertTrue($this->conditionMatcher->match('[userFunc = user_testFunctionWithThreeArgumentsSpaces ( 1 , 2, "3, 4, 5, 6" ) ]'));
+	}
+
+	/**
+	 * Tests that the condition matches calls user function with multiple
+	 * arguments.
+	 *
+	 * @test
+	 * @return void
+	 */
+	public function testUserFuncWithSpacesInQuotes() {
+		$this->assertTrue($this->conditionMatcher->match('[userFunc = user_testFunctionWithSpaces(" 3, 4, 5, 6 ")]'));
+	}
+
+	/**
+	 * Tests that the condition matches calls user function with multiple
+	 * arguments.
+	 *
+	 * @test
+	 * @return void
+	 */
+	public function testUserFuncWithMultipleArgumentsAndQuotesAndSpacesStrippedAndEscapes() {
+		$this->assertTrue($this->conditionMatcher->match('[userFunc = user_testFunctionWithThreeArgumentsSpaces ( 1 , 2, "3, \"4, 5\", 6" ) ]'));
+	}
+
+	/**
+	 * Tests that the condition matches calls user function with multiple
+	 * arguments.
+	 *
+	 * @test
+	 * @return void
+	 */
+	public function testUserFuncWithQuoteMissing() {
+		$this->assertTrue($this->conditionMatcher->match('[userFunc = user_testFunctionWithQuoteMissing ("value \") ]'));
+	}
+
+}
+
+?>
