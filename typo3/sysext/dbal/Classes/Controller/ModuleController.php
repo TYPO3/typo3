0ba17b794a250a7ec030257fa12556c74f5c0840
Wouter Wolters <typo3@wouterwolters.nl>
2013-04-22 21:34:48 +0200

    [TASK] Remove deprecated code
    Remove the deprecated StandardDocumentTemplate
    
    Change-Id: I2f2d08da9134b65b27058d0b072d642c6ac34a34
    Resolves: #46954
    Releases: 6.1
    Reviewed-on: https://review.typo3.org/20087
    Reviewed-by: Markus Klein
    Reviewed-by: Stefan Neufeind
    Reviewed-by: Benjamin Mack
    Tested-by: Benjamin Mack

--- typo3/sysext/dbal/Classes/Controller/ModuleController.php
+++ typo3/sysext/dbal/Classes/Controller/ModuleController.php
@@ -64,7 +64,7 @@ class ModuleController extends \TYPO3\CMS\Backend\Module\BaseScriptClass {
 		// Clean up settings:
 		$this->MOD_SETTINGS = \TYPO3\CMS\Backend\Utility\BackendUtility::getModuleData($this->MOD_MENU, \TYPO3\CMS\Core\Utility\GeneralUtility::_GP('SET'), $this->MCONF['name']);
 		// Draw the header
-		$this->doc = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Backend\\Template\\StandardDocumentTemplate');
+		$this->doc = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Backend\\Template\\DocumentTemplate');
 		$this->doc->backPath = $GLOBALS['BACK_PATH'];
 		$this->doc->form = '<form action="" method="post">';
 		// JavaScript
