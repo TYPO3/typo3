68fc7f6748488f261a69255199ad36e98a98f045
Jigal van Hemert <jigal@xs4all.nl>
2013-04-27 16:45:16 +0200

    [TASK] Raise sysext versions to 6.2.0
    System extensions are shipped with the same version number as the TYPO3
    release they belong with. Also the dependency should be sett accordingly
    to make clear they are not to be used with other releases.
    
    Change-Id: I9774d0a4420a8c4e4f2504c9922ee74f00c3bff5
    Fixes: #47645
    Releases: 6.2
    Reviewed-on: https://review.typo3.org/20235
    Reviewed-by: Christian Kuhn
    Tested-by: Christian Kuhn

--- typo3/sysext/dbal/ext_emconf.php
+++ typo3/sysext/dbal/ext_emconf.php
@@ -30,12 +30,12 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_company' => '',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
-	'version' => '6.0.0',
+	'version' => '6.2.0',
 	'_md5_values_when_last_written' => '',
 	'constraints' => array(
 		'depends' => array(
-			'adodb' => '6.0.0-0.0.0',
-			'typo3' => '6.0.0-0.0.0'
+			'adodb' => '6.2.0-6.2.99',
+			'typo3' => '6.2.0-6.2.99',
 		),
 		'conflicts' => array(),
 		'suggests' => array()
