a0f2d70f421030a4487baef6356eb02365adb5ab
Jigal van Hemert <jigal@xs4all.nl>
2013-04-27 16:39:44 +0200

    [TASK] Raise sysext versions to 6.2.0
    System extensions are shipped with the same version number as the TYPO3 release they belong with. Also the dependency should be sett accordingly to make clear they are not to be used with other releases.
    
    Change-Id: I575e19bb2f2c1d0a6ee27753ec2e05dee1b2e42e
    Fixes: #47644
    Releases: 6.2

--- typo3/sysext/wizard_sortpages/ext_emconf.php
+++ typo3/sysext/wizard_sortpages/ext_emconf.php
@@ -27,12 +27,12 @@ $EM_CONF[$_EXTKEY] = array(
 	'author' => 'Kasper Skaarhoj',
 	'author_email' => 'kasperYYYY@typo3.com',
 	'author_company' => 'Curby Soft Multimedia',
-	'version' => '6.0.0',
+	'version' => '6.2.0',
 	'_md5_values_when_last_written' => '',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.0.0-0.0.0',
-			'func_wizards' => '6.0.0-0.0.0'
+			'typo3' => '6.2.0-6.2.99',
+			'func_wizards' => '6.2.0-6.2.99',
 		),
 		'conflicts' => array(),
 		'suggests' => array()
