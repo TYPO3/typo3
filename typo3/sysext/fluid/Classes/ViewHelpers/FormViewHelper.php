371cc22dc494d75e672e1c3783909cff7f5f4df0
Marc Bastian Heinrichs <typo3@mbh-software.de>
2013-04-28 14:24:03 +0200

    [TASK] Reschedule the removal for the old property mapper
    The old property mapper will be removed in two versions after 6.1
    
    Resolves: #47665
    Releases: 6.2, 6.1
    Change-Id: I35291e0a95aa521bf5d6cb21c712a05a29420afa
    Reviewed-on: https://review.typo3.org/20260
    Reviewed-by: Wouter Wolters
    Tested-by: Wouter Wolters

--- typo3/sysext/fluid/Classes/ViewHelpers/FormViewHelper.php
+++ typo3/sysext/fluid/Classes/ViewHelpers/FormViewHelper.php
@@ -260,7 +260,7 @@ class FormViewHelper extends \TYPO3\CMS\Fluid\ViewHelpers\Form\AbstractFormViewH
 			$result .= '<input type="hidden" name="' . $this->prefixFieldName('__referrer[@action]') . '" value="' . $actionName . '" />' . chr(10);
 			$result .= '<input type="hidden" name="' . $this->prefixFieldName('__referrer[arguments]') . '" value="' . htmlspecialchars($this->hashService->appendHmac(base64_encode(serialize($request->getArguments())))) . '" />' . chr(10);
 		} else {
-			// @deprecated since Fluid 1.4.0, will be removed with Fluid 6.1.
+			// @deprecated since Fluid 1.4.0, will be removed two versions after Fluid 6.1.
 			$result .= '<input type="hidden" name="' . $this->prefixFieldName('__referrer[extensionName]') . '" value="' . $extensionName . '" />' . chr(10);
 			$result .= '<input type="hidden" name="' . $this->prefixFieldName('__referrer[controllerName]') . '" value="' . $controllerName . '" />' . chr(10);
 			$result .= '<input type="hidden" name="' . $this->prefixFieldName('__referrer[actionName]') . '" value="' . $actionName . '" />' . chr(10);
