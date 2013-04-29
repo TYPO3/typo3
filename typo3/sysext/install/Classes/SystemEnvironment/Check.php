52181c16b9d7eaaa14f644a77138a3db5d7ac91f
Johannes Feustel <s@feustel.eu>
2013-04-29 11:12:00 +0200

    [BUGFIX] Resolves typo in system environment check
    Changes 'freettype' to 'freetype'
    
    Change-Id: I257cbe5fb084aa490fc73075911facd494e68346
    Resolves: #47687
    Releases: 6.1

--- typo3/sysext/install/Classes/SystemEnvironment/Check.php
+++ typo3/sysext/install/Classes/SystemEnvironment/Check.php
@@ -1039,7 +1039,7 @@ class Check {
 	protected function checkGdLibFreeTypeSupport() {
 		if (function_exists('imagettftext')) {
 			$status = new OkStatus();
-			$status->setTitle('PHP GD library has freettype font support');
+			$status->setTitle('PHP GD library has freetype font support');
 			$status->setMessage(
 				'There is a difference between the font size setting the GD' .
 				' library should be feeded with. If installation is completed' .
