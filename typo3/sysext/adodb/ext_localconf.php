797a8e5127e2132906a7790f9e948e42c886a7fe
Christian Kuhn <lolli@schwarzbu.ch>
2013-04-30 22:02:43 +0200

    [TASK] Remove dummy require files from ext:adodb
    Change-Id: Ia1d98cfbd904934c651946ad30ab6dcba367dc3f
    Resolves: #47763
    Related: #47756
    Releases: 6.2
    Reviewed-on: https://review.typo3.org/20367
    Reviewed-by: Anja Leichsenring
    Tested-by: Anja Leichsenring
    Reviewed-by: Philipp Gampe
    Tested-by: Philipp Gampe
    Reviewed-by: Christian Kuhn
    Tested-by: Christian Kuhn

--- typo3/sysext/adodb/ext_localconf.php
+++ typo3/sysext/adodb/ext_localconf.php
@@ -2,7 +2,7 @@
 if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
-require_once \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('adodb') . 'class.tx_adodb_tceforms.php';
+
 // Register as a data source application if the extension datasources is loaded:
 if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('datasources')) {
 	require_once \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('datasources') . 'class.tx_datasources_main.php';
