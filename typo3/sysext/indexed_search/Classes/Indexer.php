23d324006942fe7500d76a12c727beca35fe99e7
Ralf Hettinger <ng@ralfhettinger.de>
2013-04-30 16:15:54 +0200

    [BUGFIX] indexed_search/Indexer: correct frequency mapping for 1
    There was a slight glitch in indexed_search's indexer, which resulted in
    a wrong frequency mapping if the ratio between number of current word /
    all words is exactly 1.
    The "frequency" for this word is now mapped to the highest possible
    value.
    
    Resolves: #47745
    Releases: master, 6.2
    Change-Id: I1c9945b9550b80ad969f907f0bf1f14c3ae872f9
    Reviewed-on: http://review.typo3.org/32993
    Reviewed-by: Markus Klein <klein.t3@reelworx.at>
    Tested-by: Markus Klein <klein.t3@reelworx.at>

--- typo3/sysext/indexed_search/Classes/Indexer.php
+++ typo3/sysext/indexed_search/Classes/Indexer.php
@@ -2018,7 +2018,7 @@ class Indexer {
 	 */
 	public function freqMap($freq) {
 		$mapFactor = $this->freqMax * 100 * $this->freqRange;
-		if ($freq < 1) {
+		if ($freq <= 1) {
 			$newFreq = $freq * $mapFactor;
 			$newFreq = $newFreq > $this->freqRange ? $this->freqRange : $newFreq;
 		} else {
