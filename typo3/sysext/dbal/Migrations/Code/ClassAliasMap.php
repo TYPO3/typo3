fe938a8a3c8c8377cb52b4ce4c4c844493fe224d
Christian Kuhn <lolli@schwarzbu.ch>
2013-04-30 21:01:56 +0200

    [TASK] Remove unused hook ExtensionManagerHooks
    With the rewrite of the extension manager in 6.0, a hook of dbal in
    the old extension manager is unused and rotting as dead code in dbal.
    
    The interface that is implemented does not exist anymore.
    
    The hook is removed for now, there is currently no active
    contribution to handle this specific piece of code. If it is
    introduced later again, it needs to be adapted to the new extension
    manager, so the old hook could only be used as basic idea, but not
    as production code at the moment.
    
    Change-Id: Ie15962ba865b1be676c93be39228a7e9c25bd683
    Resolves: #47762
    Releases: 6.2
    Reviewed-on: https://review.typo3.org/20365
    Reviewed-by: Thomas Maroschik
    Tested-by: Thomas Maroschik
    Reviewed-by: Anja Leichsenring
    Tested-by: Anja Leichsenring
    Reviewed-by: Christian Kuhn
    Tested-by: Christian Kuhn

--- typo3/sysext/dbal/Migrations/Code/ClassAliasMap.php
+++ typo3/sysext/dbal/Migrations/Code/ClassAliasMap.php
@@ -3,7 +3,6 @@ return array(
 	'tx_dbal_autoloader' => 'TYPO3\\CMS\\Dbal\\Autoloader',
 	'tx_dbal_module1' => 'TYPO3\\CMS\\Dbal\\Controller\\ModuleController',
 	'tx_dbal_tsparserext' => 'TYPO3\\CMS\\Dbal\\ExtensionManager\\MessageDisplay',
-	'tx_dbal_em' => 'TYPO3\\CMS\\Dbal\\Hooks\\ExtensionManagerHooks',
 	'tx_dbal_installtool' => 'TYPO3\\CMS\\Dbal\\Hooks\\InstallToolHooks',
 	'tx_dbal_querycache' => 'TYPO3\\CMS\\Dbal\\QueryCache',
 	'ux_t3lib_DB' => 'TYPO3\\CMS\\Dbal\\Database\\DatabaseConnection',
--- typo3/sysext/dbal/ext_localconf.php
+++ typo3/sysext/dbal/ext_localconf.php
@@ -12,9 +12,6 @@ $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install/mod/class.tx_install.php'
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install/mod/class.tx_install.php']['writeLocalconf'][] = 'TYPO3\\CMS\\Dbal\\Hooks\\InstallToolHooks';
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install/mod/class.tx_install.php']['requiredPhpModules'][] = 'TYPO3\\CMS\\Dbal\\Hooks\\InstallToolHooks';
 
-// Register a hook for the extension manager
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['typo3/mod/tools/em/index.php']['checkDBupdates'][] = 'TYPO3\\CMS\\Dbal\\Hooks\\ExtensionManagerHooks';
-
 // Register caches if not already done in localconf.php or a previously loaded extension.
 if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['dbal'])) {
 	$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['dbal'] = array(
