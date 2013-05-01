eb23ccc8baa52d0a8f32bdc4bd1d1cf8a232fbb2
Wouter Wolters <typo3@wouterwolters.nl>
2013-05-01 23:11:57 +0200

    [TASK] Remove dummy files in EXT:form
    Change-Id: I80169971a48e43f1aa82d01dfb7a328807a74f30
    Resolves: #47802
    Related: #47756
    Releases: 6.2

--- typo3/sysext/form/Classes/View/Form/Element/Option.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*
- * @deprecated since 6.0, the classname tx_form_View_Form_Element_Option and this file is obsolete
- * and will be removed with 6.2. The class was renamed and is now located at:
- * typo3/sysext/form/Classes/View/Form/Element/OptionElementView.php
- */
-require_once \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('form') . 'Classes/View/Form/Element/OptionElementView.php';
-?>
