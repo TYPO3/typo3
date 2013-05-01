35307fedbd3be115abe7be4dcdd2c32ec8f8bc68
Christian Kuhn <lolli@schwarzbu.ch>
2013-05-01 21:37:45 +0200

    [TASK] Remove dummy files from ext:rtehtmlarea
    Change-Id: Idb2fe1df112c3794e6852f270f742cfe51b2c480
    Resolves: #47799
    Related: #47756
    Releases: 6.2

--- typo3/sysext/rtehtmlarea/ext_autoload.php
+++ typo3/sysext/rtehtmlarea/ext_autoload.php
@@ -4,17 +4,9 @@
  */
 $rtehtmlareaExtensionPath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('rtehtmlarea');
 return array(
-	'tx_rtehtmlarea_api' => $rtehtmlareaExtensionPath . 'class.tx_rtehtmlareaapi.php',
-	'tx_rtehtmlarea_base' => $rtehtmlareaExtensionPath . 'class.tx_rtehtmlarea_base.php',
-	'tx_rtehtmlarea_softrefproc' => $rtehtmlareaExtensionPath . 'hooks/softref/class.tx_rtehtmlarea_softrefproc.php',
-	'tx_rtehtmlarea_statusreport_conflictscheck' => $rtehtmlareaExtensionPath . 'hooks/statusreport/class.tx_rtehtmlarea_statusreport_conflictscheck.php',
 	'tx_rtehtmlarea_pi1' => $rtehtmlareaExtensionPath . 'pi1/class.tx_rtehtmlarea_pi1.php',
 	'tx_rtehtmlarea_pi2' => $rtehtmlareaExtensionPath . 'pi2/class.tx_rtehtmlarea_pi2.php',
 	'tx_rtehtmlarea_pi3' => $rtehtmlareaExtensionPath . 'pi3/class.tx_rtehtmlarea_pi3.php',
 	'AccessibilityLinkController' => $rtehtmlareaExtensionPath . 'Classes/Controller/AccessibilityLinkController.php',
-	'tx_rtehtmlarea_browse_links' => $rtehtmlareaExtensionPath . 'mod3/class.tx_rtehtmlarea_browse_links.php',
-	'tx_rtehtmlarea_select_image' => $rtehtmlareaExtensionPath . 'mod4/class.tx_rtehtmlarea_select_image.php',
-	'tx_rtehtmlarea_user' => $rtehtmlareaExtensionPath . 'mod5/class.tx_rtehtmlarea_user.php',
-	'tx_rtehtmlarea_parse_html' => $rtehtmlareaExtensionPath . 'mod6/class.tx_rtehtmlarea_parse_html.php'
 );
 ?>
--- typo3/sysext/rtehtmlarea/hooks/statusreport/class.tx_rtehtmlarea_statusreport_conflictscheck.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*
- * @deprecated since 6.0, the classname tx_rtehtmlarea_statusReport_conflictsCheck and this file is obsolete
- * and will be removed with 6.2. The class was renamed and is now located at:
- * typo3/sysext/rtehtmlarea/Classes/Hook/StatusReportConflictsCheckHook.php
- */
-require_once \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('rtehtmlarea') . 'Classes/Hook/StatusReportConflictsCheckHook.php';
-?>
