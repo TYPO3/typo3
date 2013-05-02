c781265e8cdc26b9efefbf29a6d6ba27bc9b7ac5
Christian Kuhn <lolli@schwarzbu.ch>
2013-05-02 20:55:12 +0200

    [TASK] Remove dummy files from ext:install
    Change-Id: I45ed37a19039418b1edc440b735809182833ce2d
    Resolves: #47848
    Related: #47756
    Releeases: 6.2

--- typo3/install/index.php
+++ typo3/install/index.php
@@ -55,7 +55,7 @@ require '../sysext/install/Classes/InstallBootstrap.php';
 if (!\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('install')) {
 	die('Install Tool is not loaded as an extension.<br />You must add the key "install" to the list of installed extensions in typo3conf/LocalConfiguration.php, $TYPO3_CONF_VARS[\'EXT\'][\'extListArray\'].');
 }
-require_once \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('install') . 'mod/class.tx_install.php';
+
 $install_check = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Install\\Installer', TRUE);
 $install_check->init();
 ?>
