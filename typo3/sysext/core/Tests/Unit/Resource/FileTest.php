49a0b4647f59450e28030edc5d186e0de5b3d9bf
Fabien Udriot <fabien.udriot@ecodev.ch>
2013-05-02 19:50:28 +0200

    [BUGFIX] ProcessedFile is persisted in sys_file as well
    The problem appears more generally as soon as @getProperties@ is
    called from a file object. FAL will try, by all means, indexing the file
    (storing the file into sys_file, in other word)
    
    The change set adds a method telling a File not be indexed by the API.
    This is particularly useful if you want to create objects but don't want
    them to be persisted.
    
    Change-Id: I8cf0c0f6389dc13e8c3855452f1d2519544b69b9
    Fixes: #47211
    Fixes: #47782
    Releases: 6.0, 6.1, 6.2

--- typo3/sysext/core/Tests/Unit/Resource/FileTest.php
+++ typo3/sysext/core/Tests/Unit/Resource/FileTest.php
@@ -392,7 +392,39 @@ class FileTest extends \TYPO3\CMS\Core\Tests\UnitTestCase {
 		$this->assertSame($expectedExtension, $fixture->getExtension());
 	}
 
+	/**
+	 * @test
+	 */
+	public function indexablePropertyIsByDefaultTrue() {
+		$fixture = new \TYPO3\CMS\Core\Resource\File(array());
+		$this->assertAttributeEquals(TRUE, 'indexable', $fixture);
+	}
 
+	/**
+	 * @test
+	 */
+	public function indexablePropertyCanBeSetAndGet() {
+		$fixture = new \TYPO3\CMS\Core\Resource\File(array());
+		foreach (array(FALSE, TRUE) as $value) {
+			$fixture->setIndexable($value);
+			$this->assertSame($value, $fixture->isIndexable());
+		}
+	}
+
+	/**
+	 * @test
+	 */
+	public function callMethodLoadIndexRecordWithPropertyIndexableSetToFalseAndCheckWhetherIsIndexedReturnsNull() {
+		$method = new \ReflectionMethod(
+			'TYPO3\CMS\Core\Resource\File', 'loadIndexRecord'
+		);
+		$method->setAccessible(TRUE);
+
+		$fixture = new \TYPO3\CMS\Core\Resource\File(array());
+		$fixture->setIndexable(FALSE);
+		$method->invoke($fixture);
+		$this->assertNull($fixture->isIndexed());
+	}
 }
 
 ?>
