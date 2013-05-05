6d80c93b56300d01b1f64074742788dbd5e64710
Jigal van Hemert <jigal.van.hemert@typo3.org>
2013-05-05 15:26:51 +0200

    [BUGFIX] Incorrect grouping of exclude fields in translated backend
    If you use the backend in a language other than English, then the
    grouping of the exclude fields in the be_group record is messed up when
    flexform fields are included.
    
    E.g. in a German backend, all tt_content fields are grouped below pages.
    
    Sort the table names beforehand and afterwards all fields inside each
    table.
    Besides the patch converts some variables names into meaningful ones.
    
    Fixes: #47839
    Related: #34098
    Releases: 6.2, 6.1, 6.0, 4.7, 4.5
    Change-Id: I28ebc69d5cfc0316bdf44558276d3e939d011c2d
    Reviewed-on: https://review.typo3.org/20523
    Reviewed-by: Jigal van Hemert
    Tested-by: Jigal van Hemert

--- tests/t3lib/class.t3lib_befuncTest.php
+++ tests/t3lib/class.t3lib_befuncTest.php
@@ -184,5 +184,221 @@ class t3lib_befuncTest extends tx_phpunit_testcase {
 
 		$this->assertEquals($selectFields, $expectedFields);
 	}
+
+	/**
+	 * Tests concerning getExcludeFields
+	 */
+
+	/**
+	 * @return array
+	 */
+	public function getExcludeFieldsDataProvider() {
+		return array(
+			'getExcludeFields does not return fields not configured as exclude field' => array(
+				array(
+					'tx_foo' => array(
+						'ctrl' => array(
+							'title' => 'foo',
+						),
+						'columns' => array(
+							'bar' => array(
+								'label' => 'bar',
+								'exclude' => 1
+							),
+							'baz' => array(
+								'label' => 'bar',
+							),
+						)
+					)
+				),
+				array(
+					array(
+						'foo: bar',
+						'tx_foo:bar',
+					),
+				)
+			),
+			'getExcludeFields returns fields from root level tables if root level restriction should be ignored' => array(
+				array(
+					'tx_foo' => array(
+						'ctrl' => array(
+							'title' => 'foo',
+							'rootLevel' => TRUE,
+							'security' => array(
+								'ignoreRootLevelRestriction' => TRUE,
+							),
+						),
+						'columns' => array(
+							'bar' => array(
+								'label' => 'bar',
+								'exclude' => 1
+							),
+						)
+					)
+				),
+				array(
+					array(
+						'foo: bar',
+						'tx_foo:bar',
+					),
+				)
+			),
+			'getExcludeFields does not return fields from root level tables' => array(
+				array(
+					'tx_foo' => array(
+						'ctrl' => array(
+							'title' => 'foo',
+							'rootLevel' => TRUE,
+						),
+						'columns' => array(
+							'bar' => array(
+								'label' => 'bar',
+								'exclude' => 1
+							),
+						)
+					)
+				),
+				array()
+			),
+			'getExcludeFields does not return fields from admin only level tables' => array(
+				array(
+					'tx_foo' => array(
+						'ctrl' => array(
+							'title' => 'foo',
+							'adminOnly' => TRUE,
+						),
+						'columns' => array(
+							'bar' => array(
+								'label' => 'bar',
+								'exclude' => 1
+							),
+						)
+					)
+				),
+				array()
+			),
+			'getExcludeFields sorts tables and properties with flexform fields properly' => array(
+				array(
+					'tx_foo' => array(
+						'ctrl' => array(
+							'title' => 'foo'
+						),
+						'columns' => array(
+							'foo' => array(
+								'label' => 'foo',
+								'exclude' => 1
+							),
+							'bar' => array(
+								'label' => 'bar',
+								'exclude' => 1
+							),
+							'abarfoo' => array(
+								'label' => 'abarfoo',
+								'config' => array(
+									'type' => 'flex',
+									'ds' => array(
+										'*,dummy' => '<?xml version="1.0" encoding="utf-8"?>
+<T3DataStructure>
+	<sheets>
+		<sGeneral>
+			<ROOT>
+				<type>array</type>
+				<el>
+					<xmlTitle>
+						<TCEforms>
+							<exclude>1</exclude>
+							<label>The Title:</label>
+							<config>
+								<type>input</type>
+								<size>48</size>
+							</config>
+						</TCEforms>
+					</xmlTitle>
+				</el>
+			</ROOT>
+		</sGeneral>
+	</sheets>
+</T3DataStructure>'
+									)
+								)
+							)
+						)
+					),
+					'tx_foobar' => array(
+						'ctrl' => array(
+							'title' => 'foobar'
+						),
+						'columns' => array(
+							'foo' => array(
+								'label' => 'foo',
+								'exclude' => 1
+							),
+							'bar' => array(
+								'label' => 'bar',
+								'exclude' => 1
+							)
+						)
+					),
+					'tx_bar' => array(
+						'ctrl' => array(
+							'title' => 'bar'
+						),
+						'columns' => array(
+							'foo' => array(
+								'label' => 'foo',
+								'exclude' => 1
+							),
+							'bar' => array(
+								'label' => 'bar',
+								'exclude' => 1
+							)
+						)
+					)
+				),
+				array(
+					array(
+						'bar: bar',
+						'tx_bar:bar'
+					),
+					array(
+						'bar: foo',
+						'tx_bar:foo'
+					),
+					array(
+						'abarfoo dummy: The Title:',
+						'tx_foo:abarfoo;dummy;sGeneral;xmlTitle'
+					),
+					array(
+						'foo: bar',
+						'tx_foo:bar'
+					),
+					array(
+						'foo: foo',
+						'tx_foo:foo'
+					),
+					array(
+						'foobar: bar',
+						'tx_foobar:bar'
+					),
+					array(
+						'foobar: foo',
+						'tx_foobar:foo'
+					),
+				)
+			)
+		);
+	}
+
+	/**
+	 * @param $tca
+	 * @param $expected
+	 *
+	 * @test
+	 * @dataProvider getExcludeFieldsDataProvider
+	 */
+	public function getExcludeFieldsReturnsCorrectFieldList($tca, $expected) {
+		$GLOBALS['TCA'] = $tca;
+		$this->assertSame($expected, t3lib_BEfunc::getExcludeFields());
+	}
 }
 ?>
