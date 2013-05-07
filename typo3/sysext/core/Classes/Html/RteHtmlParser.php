256ecdb48446f2125f3dbbb17989cafbe04fff64
Alexander Opitz <opitz.alexander@googlemail.com>
2013-05-07 10:25:06 +0200

    [BUGFIX] Catch FolderNotFound Exception on links/images
    If you have RTE content which have false links/images to files, we
    get a FolderNotFoundException (FileNotFoundException in 6.0). This
    should be handled.
    
    Reproduce
    * Create content element text
    * In RTE change to text mode
    * Add <img src="nofile" /> for image
    or
    * Add <a href="nowhere">link</a> for link
    * save
    
    Resolves: #46785
    Releases: 6.2, 6.1, 6.0
    Change-Id: I7b959825e8e36dc5b1941c9fa411afa321a594fb

--- typo3/sysext/core/Classes/Html/RteHtmlParser.php
+++ typo3/sysext/core/Classes/Html/RteHtmlParser.php
@@ -573,12 +573,16 @@ class RteHtmlParser extends \TYPO3\CMS\Core\Html\HtmlParser {
 						$attribArray['src'] = preg_replace('#^' . preg_quote($sitePath, '#') . '#', '', $attribArray['src']);
 						// If the image is not magic and does not have a file uid, try to add the uid
 						if (!$attribArray['data-htmlarea-file-uid'] && !$isMagicImage) {
-							$fileOrFolderObject = $fileFactory->retrieveFileOrFolderObject($attribArray['src']);
-							if ($fileOrFolderObject instanceof \TYPO3\CMS\Core\Resource\FileInterface) {
-								$fileIdentifier = $fileOrFolderObject->getIdentifier();
-								$fileObject = $fileOrFolderObject->getStorage()->getFile($fileIdentifier);
-								$attribArray['data-htmlarea-file-uid'] = $fileObject->getUid();
-								$attribArray['data-htmlarea-file-table'] = 'sys_file';
+							try {
+								$fileOrFolderObject = $fileFactory->retrieveFileOrFolderObject($attribArray['src']);
+								if ($fileOrFolderObject instanceof \TYPO3\CMS\Core\Resource\FileInterface) {
+									$fileIdentifier = $fileOrFolderObject->getIdentifier();
+									$fileObject = $fileOrFolderObject->getStorage()->getFile($fileIdentifier);
+									$attribArray['data-htmlarea-file-uid'] = $fileObject->getUid();
+									$attribArray['data-htmlarea-file-table'] = 'sys_file';
+								}
+							} catch (\TYPO3\CMS\Core\Resource\Exception\FolderDoesNotExistException $notFoundException) {
+								// Nothing to be done if file/folder not found
 							}
 						}
 						$attribArray['src'] = $siteUrl . $attribArray['src'];
@@ -774,7 +778,11 @@ class RteHtmlParser extends \TYPO3\CMS\Core\Html\HtmlParser {
 						} elseif ($fileChar) {
 							// It is an internal file or folder
 							// Try to transform the href into a FAL reference
-							$fileOrFolderObject = \TYPO3\CMS\Core\Resource\ResourceFactory::getInstance()->retrieveFileOrFolderObject($link_param);
+							try {
+								$fileOrFolderObject = \TYPO3\CMS\Core\Resource\ResourceFactory::getInstance()->retrieveFileOrFolderObject($link_param);
+							} catch (\TYPO3\CMS\Core\Resource\Exception\FolderDoesNotExistException $notFoundException) {
+								// Nothing to be done if file/folder not found
+							}
 							if ($fileOrFolderObject instanceof \TYPO3\CMS\Core\Resource\Folder) {
 								// It's a folder
 								$folderIdentifier = $fileOrFolderObject->getIdentifier();
@@ -787,6 +795,7 @@ class RteHtmlParser extends \TYPO3\CMS\Core\Html\HtmlParser {
 							} else {
 								$href = $siteUrl . $link_param;
 							}
+							$href = $siteUrl . $link_param;
 						} else {
 							// integer or alias (alias is without slashes or periods or commas, that is 'nospace,alphanum_x,lower,unique' according to tables.php!!)
 							// Splitting the parameter by ',' and if the array counts more than 1 element it's a id/type/parameters triplet
