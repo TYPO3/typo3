0155d0eda2574df477b5e51a9b3f9e19c9cad69f
Daniel Minder <daniel.minder@uni-due.de>
2013-05-07 19:52:42 +0200

    [TASK] Update PEAR packages
    Update PEAR package HTTP_Request2 to version 2.1.1 and Net_URL2 to 2.0.0
    so that TYPO3 contains the bug fixes contained in these releases.
    
    Resolves: #48027
    Resolves: #32387
    Related: #37085 #41295
    Releases: 4.7, 6.0, 6.1, 6.2
    Change-Id: I5f5204bc589fe5aab2b8afd932acf674566181f3

--- typo3/contrib/pear/HTTP/Request2/Adapter/Mock.php
+++ typo3/contrib/pear/HTTP/Request2/Adapter/Mock.php
@@ -6,7 +6,7 @@
  *
  * LICENSE:
  *
- * Copyright (c) 2008-2011, Alexey Borzov <avb@php.net>
+ * Copyright (c) 2008-2012, Alexey Borzov <avb@php.net>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -33,12 +33,12 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * @category   HTTP
- * @package    HTTP_Request2
- * @author     Alexey Borzov <avb@php.net>
- * @license    http://opensource.org/licenses/bsd-license.php New BSD License
- * @version    SVN: $Id: Mock.php 308322 2011-02-14 13:58:03Z avb $
- * @link       http://pear.php.net/package/HTTP_Request2
+ * @category HTTP
+ * @package  HTTP_Request2
+ * @author   Alexey Borzov <avb@php.net>
+ * @license  http://opensource.org/licenses/bsd-license.php New BSD License
+ * @version  SVN: $Id: Mock.php 324937 2012-04-07 10:05:57Z avb $
+ * @link     http://pear.php.net/package/HTTP_Request2
  */
 
 /**
@@ -63,55 +63,71 @@ require_once 'HTTP/Request2/Adapter.php';
  * $response = $req->send();
  * </code>
  *
- * @category   HTTP
- * @package    HTTP_Request2
- * @author     Alexey Borzov <avb@php.net>
- * @version    Release: 2.0.0RC1
+ * @category HTTP
+ * @package  HTTP_Request2
+ * @author   Alexey Borzov <avb@php.net>
+ * @license  http://opensource.org/licenses/bsd-license.php New BSD License
+ * @version  Release: 2.1.1
+ * @link     http://pear.php.net/package/HTTP_Request2
  */
 class HTTP_Request2_Adapter_Mock extends HTTP_Request2_Adapter
 {
-   /**
-    * A queue of responses to be returned by sendRequest()
-    * @var  array
-    */
+    /**
+     * A queue of responses to be returned by sendRequest()
+     * @var  array
+     */
     protected $responses = array();
 
-   /**
-    * Returns the next response from the queue built by addResponse()
-    *
-    * If the queue is empty it will return default empty response with status 400,
-    * if an Exception object was added to the queue it will be thrown.
-    *
-    * @param    HTTP_Request2
-    * @return   HTTP_Request2_Response
-    * @throws   Exception
-    */
+    /**
+     * Returns the next response from the queue built by addResponse()
+     *
+     * Only responses without explicit URLs or with URLs equal to request URL
+     * will be considered. If matching response is not found or the queue is
+     * empty then default empty response with status 400 will be returned,
+     * if an Exception object was added to the queue it will be thrown.
+     *
+     * @param HTTP_Request2 $request HTTP request message
+     *
+     * @return   HTTP_Request2_Response
+     * @throws   Exception
+     */
     public function sendRequest(HTTP_Request2 $request)
     {
-        if (count($this->responses) > 0) {
-            $response = array_shift($this->responses);
-            if ($response instanceof HTTP_Request2_Response) {
-                return $response;
-            } else {
-                // rethrow the exception
-                $class   = get_class($response);
-                $message = $response->getMessage();
-                $code    = $response->getCode();
-                throw new $class($message, $code);
+        $requestUrl = (string)$request->getUrl();
+        $response   = null;
+        foreach ($this->responses as $k => $v) {
+            if (!$v[1] || $requestUrl == $v[1]) {
+                $response = $v[0];
+                array_splice($this->responses, $k, 1);
+                break;
             }
-        } else {
+        }
+        if (!$response) {
             return self::createResponseFromString("HTTP/1.1 400 Bad Request\r\n\r\n");
+
+        } elseif ($response instanceof HTTP_Request2_Response) {
+            return $response;
+
+        } else {
+            // rethrow the exception
+            $class   = get_class($response);
+            $message = $response->getMessage();
+            $code    = $response->getCode();
+            throw new $class($message, $code);
         }
     }
 
-   /**
-    * Adds response to the queue
-    *
-    * @param    mixed   either a string, a pointer to an open file,
-    *                   an instance of HTTP_Request2_Response or Exception
-    * @throws   HTTP_Request2_Exception
-    */
-    public function addResponse($response)
+    /**
+     * Adds response to the queue
+     *
+     * @param mixed  $response either a string, a pointer to an open file,
+     *                         an instance of HTTP_Request2_Response or Exception
+     * @param string $url      A request URL this response should be valid for
+     *                         (see {@link http://pear.php.net/bugs/bug.php?id=19276})
+     *
+     * @throws   HTTP_Request2_Exception
+     */
+    public function addResponse($response, $url = null)
     {
         if (is_string($response)) {
             $response = self::createResponseFromString($response);
@@ -122,16 +138,17 @@ class HTTP_Request2_Adapter_Mock extends HTTP_Request2_Adapter
         ) {
             throw new HTTP_Request2_Exception('Parameter is not a valid response');
         }
-        $this->responses[] = $response;
+        $this->responses[] = array($response, $url);
     }
 
-   /**
-    * Creates a new HTTP_Request2_Response object from a string
-    *
-    * @param    string
-    * @return   HTTP_Request2_Response
-    * @throws   HTTP_Request2_Exception
-    */
+    /**
+     * Creates a new HTTP_Request2_Response object from a string
+     *
+     * @param string $str string containing HTTP response message
+     *
+     * @return   HTTP_Request2_Response
+     * @throws   HTTP_Request2_Exception
+     */
     public static function createResponseFromString($str)
     {
         $parts       = preg_split('!(\r?\n){2}!m', $str, 2);
@@ -147,13 +164,14 @@ class HTTP_Request2_Adapter_Mock extends HTTP_Request2_Adapter
         return $response;
     }
 
-   /**
-    * Creates a new HTTP_Request2_Response object from a file
-    *
-    * @param    resource    file pointer returned by fopen()
-    * @return   HTTP_Request2_Response
-    * @throws   HTTP_Request2_Exception
-    */
+    /**
+     * Creates a new HTTP_Request2_Response object from a file
+     *
+     * @param resource $fp file pointer returned by fopen()
+     *
+     * @return   HTTP_Request2_Response
+     * @throws   HTTP_Request2_Exception
+     */
     public static function createResponseFromFile($fp)
     {
         $response = new HTTP_Request2_Response(fgets($fp));
