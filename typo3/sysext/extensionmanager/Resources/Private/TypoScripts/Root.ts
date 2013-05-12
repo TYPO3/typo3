aabb0470d8e7335813e6566a480da5073c83625d
Felix Kopp <felix-source@phorax.com>
2013-05-12 18:17:51 +0200

    [TASK] Move TS to Resources/Private/TypoScripts/
    TYPO3 Neos defines a path for TypoScript2 resources:
    Resources/Private/TypoScripts/Root.ts2
    
    Following this definition typoscript files in core are moved
    accordingly for shipped core extensions.
    
    Resolves: #48118
    Releases: 6.2
    Change-Id: Ib0a5c92c4a5f4cfc9556a4fbf66995c9879e866a

---
+++ typo3/sysext/extensionmanager/Resources/Private/TypoScripts/Root.ts
@@ -0,0 +1,9 @@
+module.tx_extensionmanager {
+	settings {
+		repositoryUid = 1
+	}
+	features {
+		 # if enabled, default controller and/or action is skipped when creating URIs through the URI Builder (see http://forge.typo3.org/projects/typo3v4-mvc/wiki/Skip_default_arguments_in_URIs)
+		skipDefaultArguments = 0
+	}
+}
