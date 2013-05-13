a6c2099bfa22226e2eefe8ff8e040784b5c984f9
Jonas Renggli <jonas.renggli@swisscom.com>
2013-05-13 11:41:38 +0200

    [BUGFIX] Use init.php for initializing constants in thumbs.php
    Running typo3/thumbs.php having an empty cache causes entries with
    invalid paths in autoloader cache. This bugfix only applies the changes
    on typo3/thumbs.php from bf119ecd4e408cabace9791b3216e08001664f5d
    without changing the hole bootstrap mechanism.
    
    Resolves: #44017
    Related: #36889
    Releases: 4.7
    Change-Id: Iecbb09eeb179d3d949849c7fbba845bb8e15aa37

--- typo3/thumbs.php
+++ typo3/thumbs.php
@@ -35,14 +35,7 @@
 // *******************************
 error_reporting(E_ALL & ~(E_STRICT | E_NOTICE | E_DEPRECATED));
 
-define('PATH_thisScript', str_replace('//', '/', str_replace('\\', '/',
-	(PHP_SAPI == 'fpm-fcgi' || PHP_SAPI == 'cgi' || PHP_SAPI == 'isapi' || PHP_SAPI == 'cgi-fcgi') &&
-	($_SERVER['ORIG_PATH_TRANSLATED'] ? $_SERVER['ORIG_PATH_TRANSLATED'] : $_SERVER['PATH_TRANSLATED']) ?
-	($_SERVER['ORIG_PATH_TRANSLATED'] ? $_SERVER['ORIG_PATH_TRANSLATED'] : $_SERVER['PATH_TRANSLATED']) :
-	($_SERVER['ORIG_SCRIPT_FILENAME'] ? $_SERVER['ORIG_SCRIPT_FILENAME'] : $_SERVER['SCRIPT_FILENAME']))));
-
-define('PATH_site', substr(dirname(PATH_thisScript).'/',0,-strlen('typo3/')));	// Abs. path to directory with the frontend (one above the admin-dir)
-define('PATH_t3lib', PATH_site.'t3lib/');
+require('init.php');
 
 
 // ******************
