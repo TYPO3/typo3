0ea0ca75ebd06e029b26c1325f52aa1eaf84bb9a
Wouter Wolters <typo3@wouterwolters.nl>
2013-05-13 21:40:59 +0200

    [TASK] Remove dummy files from ext:wizard_crpages and wizard_sortpages
    * Remove dummy files
    * Rename old class names to the new ones
    
    Change-Id: I77ea8d6a48d10bc56031231a00999655cbecb487
    Resolves: #48155
    Related: #47756
    Releases: 6.2

--- typo3/sysext/wizard_crpages/class.tx_wizardcrpages_webfunc_2.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*
- * @deprecated since 6.0, the classname tx_wizardcrpages_webfunc_2 and this file is obsolete
- * and will be removed with 6.2. The class was renamed and is now located at:
- * typo3/sysext/wizard_create_pages/Classes/Controller/CreatePagesWizardModuleFunctionController.php
- */
-require_once \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('wizard_crpages') . 'Classes/Controller/CreatePagesWizardModuleFunctionController.php';
-?>
