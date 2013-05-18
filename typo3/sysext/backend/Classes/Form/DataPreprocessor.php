87aaeb926fd8687a6501ee18d99951fb14ca4fdf
Wouter Wolters <typo3@wouterwolters.nl>
2013-05-18 17:45:17 +0200

    [BUGFIX] Some TCAdefaults work only with "Page (inside)"
    Some TCAdefaults work only with "Page (inside)" and not in
    the wizard.
    
    Affected defaults:
    * TCAdefaults.pages.hidden = 1
    * TCAdefaults.pages.doktype = 2
    
    Change-Id: I71eb301909c31eb8cd160dbe6beadc4ba5102b66
    Resolves: #14955
    Releases: 6.2, 6.1, 6.0, 4.7, 4.5

--- typo3/sysext/backend/Classes/Form/DataPreprocessor.php
+++ typo3/sysext/backend/Classes/Form/DataPreprocessor.php
@@ -166,7 +166,7 @@ class DataPreprocessor {
 								// Gets the list of fields to copy from the previous record.
 								$fArr = \TYPO3\CMS\Core\Utility\GeneralUtility::trimExplode(',', $GLOBALS['TCA'][$table]['ctrl']['useColumnsForDefaultValues'], 1);
 								foreach ($fArr as $theF) {
-									if (isset($GLOBALS['TCA'][$table]['columns'][$theF])) {
+									if (isset($GLOBALS['TCA'][$table]['columns'][$theF]) && !isset($newRow[$theF])) {
 										$newRow[$theF] = $row[$theF];
 									}
 								}
