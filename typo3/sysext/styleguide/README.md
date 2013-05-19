09ef39fb90ae0100af75a7ead301545ca4ec0f08
Felix Kopp <felix-source@phorax.com>
2013-05-19 02:01:08 +0200

    [FEATURE] styleguide: Initial commit

--- typo3/sysext/styleguide/README.md
+++ typo3/sysext/styleguide/README.md
@@ -1,4 +1,10 @@
 TYPO3.CMS.Styleguide
 ====================
 
-TYPO3.CMS.Styleguide
+Extension for TYPO3 CMS to star standard content elements.
+
+Icons
+Icon by Yusuke Kamiyamane - http://p.yusukekamiyamane.com/
+
+Html
+Examples by Zurb Foundation - http://foundation.zurb.com/docs/
---
+++ typo3/sysext/styleguide/ext_tables.php
@@ -0,0 +1,26 @@
+<?php
+if (!defined('TYPO3_MODE')) {
+	die ('Access denied.');
+}
+
+if (TYPO3_MODE === 'BE') {
+	/**
+	 * Registers "Styleguide" backend module
+	 */
+	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+		'TYPO3.CMS.' . $_EXTKEY,
+		'tools',
+		'styleguide',
+		'',
+		array(
+			'Styleguide' => 'index'
+		),
+		array(
+			'access' => 'user,group',
+			'icon'   => 'EXT:' . $_EXTKEY . '/Resources/Public/Icons/module.png',
+			'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_styleguide.xlf',
+		)
+	);
+}
+
+?>
