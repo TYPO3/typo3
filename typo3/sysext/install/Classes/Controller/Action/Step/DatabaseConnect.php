c1519b884ba1d9e2f7c5b32a15001eabb7d24128
Philipp Gampe <philipp.gampe@typo3.org>
2013-05-20 16:58:20 +0200

    [BUGFIX] Mysql Socket connection fails
    Some people use a socket to connect to a (local) TYPO3 database.
    This is not possible any more with TYPO3 6.1, because it is not possible
    any more to specify a socket.
    Formerly one could append the socket to the hostname similar to the port
    setting.
    
    Introduce a new config option for the socket.
    $GLOBALS['TYPO3_CONF_VARS']['DB']['socket']
    
    The default socket is NULL, thus the behavior is unchanged if no socket
    is defined.
    
    @see http://php.net/manual/en/mysqli.real-connect.php
    
    Resolves: #48117
    Releases: 6.2, 6.1
    Change-Id: I968a72dbaf90b3f319c03e697137630534d325c1

--- typo3/sysext/install/Classes/Controller/Action/Step/DatabaseConnect.php
+++ typo3/sysext/install/Classes/Controller/Action/Step/DatabaseConnect.php
@@ -147,6 +147,18 @@ class DatabaseConnect extends Action\AbstractAction implements StepInterface {
 				}
 			}
 
+			if (isset($postValues['socket'])) {
+				if (file_exists($postValues['socket'])) {
+					$localConfigurationPathValuePairs['DB/socket'] = $postValues['socket'];
+				} else {
+					/** @var $errorStatus \TYPO3\CMS\Install\Status\ErrorStatus */
+					$errorStatus = $this->objectManager->get('TYPO3\\CMS\\Install\\Status\\ErrorStatus');
+					$errorStatus->setTitle('File does not exist');
+					$errorStatus->setMessage('Given file does not exist on server');
+					$result[] = $errorStatus;
+				}
+			}
+
 			if (isset($postValues['database'])) {
 				$value = $postValues['database'];
 				if (strlen($value) <= 50) {
@@ -218,7 +230,8 @@ class DatabaseConnect extends Action\AbstractAction implements StepInterface {
 			->assign('password', $GLOBALS['TYPO3_CONF_VARS']['DB']['password'] ?: '')
 			->assign('host', $this->getConfiguredHost() ?: '127.0.0.1')
 			->assign('port', $this->getConfiguredOrDefaultPort())
-			->assign('database', $GLOBALS['TYPO3_CONF_VARS']['DB']['database'] ?: '');
+			->assign('database', $GLOBALS['TYPO3_CONF_VARS']['DB']['database'] ?: '')
+			->assign('socket', $GLOBALS['TYPO3_CONF_VARS']['DB']['socket'] ?: '');
 
 		if ($isDbalEnabled) {
 			$this->view->assign('selectedDbalDriver', $this->getSelectedDbalDriver());
@@ -229,7 +242,8 @@ class DatabaseConnect extends Action\AbstractAction implements StepInterface {
 				->assign('renderConnectDetailsUsername', TRUE)
 				->assign('renderConnectDetailsPassword', TRUE)
 				->assign('renderConnectDetailsHost', TRUE)
-				->assign('renderConnectDetailsPort', TRUE);
+				->assign('renderConnectDetailsPort', TRUE)
+				->assign('renderConnectDetailsSocket', TRUE);
 		}
 
 		return $this->view->render();
@@ -329,6 +343,7 @@ class DatabaseConnect extends Action\AbstractAction implements StepInterface {
 		$databaseConnection->setDatabasePassword($password);
 		$databaseConnection->setDatabaseHost($this->getConfiguredHost());
 		$databaseConnection->setDatabasePort($this->getConfiguredPort());
+		$databaseConnection->setDatabaseSocket($this->getConfiguredSocket());
 
 		$result = FALSE;
 		if (@$databaseConnection->sql_pconnect()) {
@@ -486,6 +501,16 @@ class DatabaseConnect extends Action\AbstractAction implements StepInterface {
 			$port = $hostPortArray[1];
 		}
 		return (int)$port;
+	}
+
+	/**
+	 * Returns configured socket, if set
+	 *
+	 * @return string|NULL
+	 */
+	protected function getConfiguredSocket() {
+		$socket = isset($GLOBALS['TYPO3_CONF_VARS']['DB']['socket']) ? $GLOBALS['TYPO3_CONF_VARS']['DB']['socket'] : NULL;
+		return $socket;
 	}
 }
 ?>
