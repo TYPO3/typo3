de32905b7bdce4c4b7f2a3125c03ebe61dd5abcc
Helmut Hummel <helmut.hummel@typo3.org>
2013-05-24 23:42:49 +0200

    [TASK] Cleanup openid extension
    Cleanup remaining CGL issues after namespace change.
    Implement some best practices like strict string comparison
    or dependency injection pattern where appropriate.
    
    Last but not least make use of use statements for frequently
    used class names.
    
    Resolves: #48522
    Releases: 6.2
    Change-Id: I1f03d2fdf53ea7be42455663b36af56bc479fa21
    Reviewed-on: https://review.typo3.org/21035
    Reviewed-by: Wouter Wolters
    Reviewed-by: Susanne Moog
    Tested-by: Susanne Moog
    Reviewed-by: Christian Kuhn
    Tested-by: Christian Kuhn

--- typo3/sysext/openid/class.tx_openid_eid.php
+++ typo3/sysext/openid/class.tx_openid_eid.php
@@ -6,6 +6,6 @@
  */
 require_once \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('openid') . 'Classes/OpenidEid.php';
 $module = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Openid\\OpenidEid');
-/* @var tx_openid_eID $module */
+/* @var \TYPO3\CMS\Openid\OpenidEid $module */
 $module->main();
 ?>
