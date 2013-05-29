4cb50cd0759d4a1c1e826520c807c71dcdcd32b9
Wouter Wolters <typo3@wouterwolters.nl>
2013-05-29 19:06:33 +0200

    [FEATURE] Switch View Helper
    Simple view helper that allows you to render content depending
    on a given value or expression.
    It bahaves similar to a basic switch statement in PHP.
    
    Usage:
    
    <f:switch expression="{person.gender}">
      <f:case case="male">Mr.</f:case>
      <f:case case="female">Mrs.</f:case>
    </f:switch>
    
    Change-Id: I44a7066dfa86785f795069e0f06391cb45fa373c
    Resolves: #48653
    Releases: 6.2

---
+++ typo3/sysext/fluid/Classes/ViewHelpers/CaseViewHelper.php
@@ -0,0 +1,43 @@
+<?php
+namespace TYPO3\CMS\Fluid\ViewHelpers;
+
+/*                                                                        *
+ * This script is backported from the TYPO3 Flow package "TYPO3.Fluid".   *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU Lesser General Public License, either version 3   *
+ *  of the License, or (at your option) any later version.                *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+/**
+ * Case view helper that is only usable within the SwitchViewHelper.
+ * @see \TYPO3\CMS\Fluid\ViewHelpers\SwitchViewHelper
+ *
+ * @api
+ */
+class CaseViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper {
+
+	/**
+	 * @param mixed $case
+	 * @return string the contents of this view helper if $case equals the expression of the surrounding switch view helper, otherwise an empty string
+	 * @throws \TYPO3\CMS\Fluid\Core\ViewHelper\Exception
+	 * @api
+	 */
+	public function render($case) {
+		$viewHelperVariableContainer = $this->renderingContext->getViewHelperVariableContainer();
+		if (!$viewHelperVariableContainer->exists('TYPO3\CMS\Fluid\ViewHelpers\SwitchViewHelper', 'switchExpression')) {
+			throw new \TYPO3\CMS\Fluid\Core\ViewHelper\Exception('The case View helper can only be used within a switch View helper', 1368112037);
+		}
+		$switchExpression = $viewHelperVariableContainer->get('TYPO3\CMS\Fluid\ViewHelpers\SwitchViewHelper', 'switchExpression');
+
+		// non-type-safe comparison by intention
+		if ($switchExpression == $case) {
+			$viewHelperVariableContainer->addOrUpdate('TYPO3\CMS\Fluid\ViewHelpers\SwitchViewHelper', 'break', TRUE);
+			return $this->renderChildren();
+		}
+		return '';
+	}
+}
+?>
