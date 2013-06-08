897a6d09e2eb2f97f509d0f83d327b0469855f38
Wouter Wolters <typo3@wouterwolters.nl>
2013-06-08 17:51:41 +0200

    [TASK] Cleanup wizard initializing
    The patch cleans up the initialization of the various
    wizards we have in TYPO3 CMS. Things that are improved:
    
    * Add constructor to all controllers
    * Move language inclusion to constructor
    * Move init() and sometimes printContent() to the constructor
    * Make init() protected
    * Add controller to $GLOBALS['SOBE']
    
    Change-Id: I9783e347a3bf983fae8b6bbbbc972ecf56f31f83
    Resolves: #48963
    Releases: 6.2
    Reviewed-on: https://review.typo3.org/21293
    Reviewed-by: Christian Kuhn
    Tested-by: Christian Kuhn
    Reviewed-by: Anja Leichsenring
    Tested-by: Anja Leichsenring

--- typo3/sysext/backend/Classes/Controller/Wizard/EditController.php
+++ typo3/sysext/backend/Classes/Controller/Wizard/EditController.php
@@ -51,12 +51,21 @@ class EditController {
 	public $doClose;
 
 	/**
+	 * Constructor
+	 */
+	public function __construct() {
+		$GLOBALS['LANG']->includeLLFile('EXT:lang/locallang_wizards.xlf');
+		$GLOBALS['SOBE'] = $this;
+
+		$this->init();
+	}
+
+	/**
 	 * Initialization of the script
 	 *
 	 * @return void
-	 * @todo Define visibility
 	 */
-	public function init() {
+	protected function init() {
 		$this->P = GeneralUtility::_GP('P');
 		// Used for the return URL to alt_doc.php so that we can close the window.
 		$this->doClose = GeneralUtility::_GP('doClose');
@@ -67,7 +76,6 @@ class EditController {
 	 * Makes a header-location redirect to an edit form IF POSSIBLE from the passed data - otherwise the window will just close.
 	 *
 	 * @return void
-	 * @todo Define visibility
 	 */
 	public function main() {
 		if ($this->doClose) {
