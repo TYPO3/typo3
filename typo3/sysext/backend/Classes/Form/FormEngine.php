643319ab92f13c0fa2586c2ef295013edac5376a
Stefan Galinski <stefan.galinski@gmail.com>
2013-06-10 19:58:42 +0200

    [BUGFIX] Exception on thumbnail generation in TCA
    In order to test this you are needing a non-FAL
    TCA file reference field. Just reference something
    here, remove the reference in the filesystem again
    and reload the TCA of the record. An exception will
    be thrown, because the file isn't available anymore.
    
    Fixes: #49009
    Releases: 6.2, 6.1, 6.0
    Change-Id: I6eccb3ef77de703551a64f52ab3904cead517c8a

--- typo3/sysext/backend/Classes/Form/FormEngine.php
+++ typo3/sysext/backend/Classes/Form/FormEngine.php
@@ -2360,8 +2360,32 @@ function ' . $evalData . '(value) {
 					} else {
 						$rowCopy = array();
 						$rowCopy[$field] = $imgPath;
-						$thumbnailCode = \TYPO3\CMS\Backend\Utility\BackendUtility::thumbCode($rowCopy, $table, $field, $this->backPath, 'thumbs.php', $config['uploadfolder'], 0, ' align="middle"');
-						$imgs[] = '<span class="nobr">' . $thumbnailCode . $imgPath . '</span>';
+						$thumbnailCode = '';
+						try {
+							$thumbnailCode = \TYPO3\CMS\Backend\Utility\BackendUtility::thumbCode(
+								$rowCopy, $table, $field, $this->backPath, 'thumbs.php',
+								$config['uploadfolder'], 0, ' align="middle"'
+							);
+							$thumbnailCode = '<span class="nobr">' . $thumbnailCode . $imgPath . '</span>';
+
+						} catch (\Exception $exception) {
+							/** @var $flashMessage FlashMessage */
+							$message = $exception->getMessage();
+							$flashMessage = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
+								'TYPO3\\CMS\\Core\\Messaging\\FlashMessage',
+								htmlspecialchars($message), '', \TYPO3\CMS\Core\Messaging\FlashMessage::ERROR, TRUE
+							);
+							$class = 'TYPO3\\CMS\\Core\\Messaging\\FlashMessageService';
+							/** @var $flashMessageService \TYPO3\CMS\Core\Messaging\FlashMessageService */
+							$flashMessageService = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance($class);
+							$defaultFlashMessageQueue = $flashMessageService->getMessageQueueByIdentifier();
+							$defaultFlashMessageQueue->enqueue($flashMessage);
+
+							$logMessage = $message . ' (' . $table . ':' . $row['uid'] . ')';
+							\TYPO3\CMS\Core\Utility\GeneralUtility::sysLog($logMessage, 'core', \TYPO3\CMS\Core\Utility\GeneralUtility::SYSLOG_SEVERITY_WARNING);
+						}
+
+						$imgs[] = $thumbnailCode;
 					}
 				}
 				$thumbsnail = implode('<br />', $imgs);
