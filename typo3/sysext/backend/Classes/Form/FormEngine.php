f29d5fb0d68623e76e37c781acd3a5f040a9bb77
Cedric Ziel <cedric@cedric-ziel.com>
2013-06-17 12:17:24 +0200

    [BUGFIX] Not possible to configure RTE for flexform fields
    This fixes TYPO3 not properly processing TCA definitions where
    the defaultExtras of a richText Area in custom Models. It allows
    the integrator to use a pattern like
    'defaultExtras' =>
    'richtext[bold|italic]:rte_transform[flag=rte_enabled|mode=ts]',
    to enable the corresponding features only.
    
    If no configuration in richtext[] is given, it falls back to standard
    configuration.
    
    Neccessary changes:
    * Checking for parameters in destination conf if not, gives empty array
    * Cleaning up the css of the DefaultInline Plugin for RTEHtmlArea. It
      Overwrote the default css with non-spritey background positions
      improperly although not providing _additional_ Icons
    
    Resolves: #24075
    Releases: 6.1
    Change-Id: Ibbcaf06c1457f49791be32a35b595ff98daf559c

--- typo3/sysext/backend/Classes/Form/FormEngine.php
+++ typo3/sysext/backend/Classes/Form/FormEngine.php
@@ -1488,9 +1488,13 @@ function ' . $evalData . '(value) {
 				list($tscPID, $thePidValue) = $this->getTSCpid($table, $row['uid'], $row['pid']);
 				// If the pid-value is not negative (that is, a pid could NOT be fetched)
 				if ($thePidValue >= 0) {
-					$RTEsetup = $GLOBALS['BE_USER']->getTSConfig('RTE', BackendUtility::getPagesTSconfig($tscPID));
-					$RTEtypeVal = BackendUtility::getTCAtypeValue($table, $row);
-					$thisConfig = BackendUtility::RTEsetup($RTEsetup['properties'], $table, $field, $RTEtypeVal);
+					$RTEtypeVal = \TYPO3\CMS\Backend\Utility\BackendUtility::getTCAtypeValue($table, $row);
+					if(!$specConf['richtext']['parameters']) {
+						$RTEsetup = $GLOBALS['BE_USER']->getTSConfig('RTE', \TYPO3\CMS\Backend\Utility\BackendUtility::getPagesTSconfig($tscPID));
+						$thisConfig = \TYPO3\CMS\Backend\Utility\BackendUtility::RTEsetup($RTEsetup['properties'], $table, $field, $RTEtypeVal);
+					} else {
+						$thisConfig = array();
+					}
 					if (!$thisConfig['disabled']) {
 						if (!$this->disableRTE) {
 							$this->RTEcounter++;
