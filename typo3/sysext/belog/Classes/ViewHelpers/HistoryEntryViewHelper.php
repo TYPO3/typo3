8a1d33aa711a0b64a005b51d0dbab130e83908ed
Georg Ringer <georg.ringer@gmail.com>
2013-06-17 14:57:47 +0200

    [BUGFIX] Use correct path in belog modules
    Belog module links to history view but path is wrong in
    the info module.
    
    Change-Id: I3e45218b2bd8b7d69d75459f41c37512de3dd23f
    Releases: 6.2, 6.1, 6.0
    Fixes: #49091

--- typo3/sysext/belog/Classes/ViewHelpers/HistoryEntryViewHelper.php
+++ typo3/sysext/belog/Classes/ViewHelpers/HistoryEntryViewHelper.php
@@ -25,6 +25,7 @@ namespace TYPO3\CMS\Belog\ViewHelpers;
  *
  *  This copyright notice MUST APPEAR in all copies of the script!
  ***************************************************************/
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 /**
  * Get history entry from for log entry
@@ -59,7 +60,7 @@ class HistoryEntryViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractVi
 		$historyIcon = \TYPO3\CMS\Backend\Utility\IconUtility::getSpriteIcon('actions-document-history-open', array(
 			'title' => \TYPO3\CMS\Extbase\Utility\LocalizationUtility::translate('showHistory', $this->controllerContext->getRequest()->getControllerExtensionName())
 		));
-		$historyHref = 'show_rechis.php?sh_uid=' . $historyEntry->getUid() . '&returnUrl=' . rawurlencode(\TYPO3\CMS\Core\Utility\GeneralUtility::getIndpEnv('REQUEST_URI'));
+		$historyHref = GeneralUtility::getIndpEnv('TYPO3_SITE_URL') . 'typo3/show_rechis.php?sh_uid=' . $historyEntry->getUid() . '&returnUrl=' . rawurlencode(\TYPO3\CMS\Core\Utility\GeneralUtility::getIndpEnv('REQUEST_URI'));
 		$historyLink = '<a href="' . htmlspecialchars($historyHref) . '">' . $historyIcon . '</a>';
 		return $historyLabel . '&nbsp;' . $historyLink;
 	}
