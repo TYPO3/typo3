f60f01d5bc91a533e90d9d7a84f6bff47238dd06
Oliver Hader <oliver@typo3.org>
2013-06-19 12:48:56 +0200

    [BUGFIX] Workspace selector in top toolbar cannot scroll
    If there are too many workspaces defined, some of them cannot
    be accessed using the workspace selector in in the top toolbar
    due to a missing scrolling behaviour of that component.
    
    Fixes: #11014
    Releases: 6.0, 6.1, 6.2
    Change-Id: I2247cb606f25268239336e5df5eec8dcbbcaac9d

--- typo3/sysext/workspaces/Resources/Public/JavaScript/workspacemenu.js
+++ typo3/sysext/workspaces/Resources/Public/JavaScript/workspacemenu.js
@@ -39,10 +39,7 @@ var WorkspaceMenu = Class.create({
 	initialize: function() {
 
 		Ext.onReady(function() {
-			Event.observe(
-				window, 'resize',
-				function() { TYPO3BackendToolbarManager.positionMenu('workspace-selector-menu'); }
-			);
+			Event.observe(window, 'resize', TYPO3BackendWorkspaceMenu.updateDimension);
 			if (top.TYPO3.configuration.inWorkspace == 1) {
 				Ext.getBody().addClass('typo3-in-workspace');
 				this.updateTopBar(top.TYPO3.Workspaces.workspaceTitle);
@@ -73,6 +70,7 @@ var WorkspaceMenu = Class.create({
 
 		if (!toolbarItem.hasClassName('toolbar-item-active')) {
 			toolbarItem.addClassName('toolbar-item-active');
+			TYPO3BackendWorkspaceMenu.updateDimension();
 			Effect.Appear(menu, {duration: 0.2});
 			TYPO3BackendToolbarManager.hideOthers(toolbarItem);
 		} else {
@@ -164,6 +162,18 @@ var WorkspaceMenu = Class.create({
 			}
 			userItem.insertHtml('beforeEnd', '<span id="typo3-topbar-workspaces-title">@' + Ext.util.Format.htmlEncode(workspaceTitle) + '</span>')
 		}
+	},
+
+	updateDimension: function() {
+		var toolbarItem = Ext.get('workspace-selector-menu');
+		var menu = toolbarItem.select('ul').first();
+
+		TYPO3BackendToolbarManager.positionMenu('workspace-selector-menu');
+
+		menu.setStyle(
+			'max-height',
+			(top.TYPO3.Backend.getHeight() - toolbarItem.getY() - toolbarItem.getHeight() - 10) + 'px'
+		);
 	}
 
 });
