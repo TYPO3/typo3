2df77291ca3e7c45ddb5cecfdd1b54d395f9d261
Wouter Wolters <typo3@wouterwolters.nl>
2013-06-20 22:19:32 +0200

    Revert "[BUGFIX] Nested FLUIDTEMPLATE doesn't work anymore"
    This reverts commit 1ba19b02542087ee2f26407be28d938d663c17a4
    
    The Unit-test breaks because it is based on a FEATURE that has been merged in 6.1, commit 17ba001ccaec73a90b499de19d1aeefc74242b44
    
    Change-Id: If1fec2c489bc1258716171236165f9ce388559e4

--- typo3/sysext/frontend/Classes/ContentObject/FluidTemplateContentObject.php
+++ typo3/sysext/frontend/Classes/ContentObject/FluidTemplateContentObject.php
@@ -74,7 +74,6 @@ class FluidTemplateContentObject extends \TYPO3\CMS\Frontend\ContentObject\Abstr
 	 * @return string The HTML output
 	 */
 	public function render($conf = array()) {
-		$parentView = $this->view;
 		$this->initializeStandaloneViewInstance();
 
 		if (!is_array($conf)) {
@@ -90,10 +89,8 @@ class FluidTemplateContentObject extends \TYPO3\CMS\Frontend\ContentObject\Abstr
 		$this->assignContentObjectDataAndCurrent($conf);
 
 		$content = $this->renderFluidView();
-		$content = $this->applyStandardWrapToRenderedContent($content, $conf);
 
-		$this->view = $parentView;
-		return $content;
+		return $this->applyStandardWrapToRenderedContent($content, $conf);
 	}
 
 	/**
