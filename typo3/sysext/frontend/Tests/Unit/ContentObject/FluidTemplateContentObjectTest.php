298f50f26084976b7235cd2285f9d10b69b6361c
Wouter Wolters <typo3@wouterwolters.nl>
2013-06-20 22:19:32 +0200

    Revert "[BUGFIX] Nested FLUIDTEMPLATE doesn't work anymore"
    This reverts commit 1ba19b02542087ee2f26407be28d938d663c17a4
    
    The Unit-test breaks because it is based on a FEATURE that
    has been merged in 6.1, 
    commit 17ba001ccaec73a90b499de19d1aeefc74242b44
    
    Change-Id: If1fec2c489bc1258716171236165f9ce388559e4
    Reviewed-on: https://review.typo3.org/21552
    Reviewed-by: Wouter Wolters
    Tested-by: Wouter Wolters

--- typo3/sysext/frontend/Tests/Unit/ContentObject/FluidTemplateContentObjectTest.php
+++ typo3/sysext/frontend/Tests/Unit/ContentObject/FluidTemplateContentObjectTest.php
@@ -500,40 +500,5 @@ class FluidTemplateContentObjectTest extends \TYPO3\CMS\Core\Tests\UnitTestCase 
 			->with('baz', array('foo' => 'bar'));
 		$this->fixture->render($configuration);
 	}
-
-	/**
-	 * @test
-	 */
-	public function renderWorksWithNestedFluidtemplate() {
-		$this->addMockViewToFixture();
-		$configuration = array(
-			'10' => 'FLUIDTEMPLATE',
-			'10.' => array(
-				'template' => 'TEXT',
-				'template.' => array(
-					'value' => 'A{anotherFluidTemplate}C'
-				),
-				'variables.' => array(
-					'anotherFluidTemplate' => 'FLUIDTEMPLATE',
-					'anotherFluidTemplate.' => array(
-						'template' => 'TEXT',
-						'template.' => array(
-							'value' => 'B',
-						),
-					),
-				),
-			),
-		);
-		$expectedResult = 'ABC';
-
-		// not using mocks - actual rendering
-		$contentObjectRenderer = new \TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
-		$fluidTemplateContentObject = new \TYPO3\CMS\Frontend\ContentObject\ContentObjectArrayContentObject(
-			$contentObjectRenderer
-		);
-		$result = $fluidTemplateContentObject->render($configuration);
-
-		$this->assertEquals($expectedResult, $result);
-	}
 }
 ?>
