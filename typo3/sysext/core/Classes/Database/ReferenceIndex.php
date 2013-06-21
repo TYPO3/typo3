f6a06b0c55b95e51f6a8da3d2611fbea19d37056
Claus Due <claus@wildside.dk>
2013-06-21 20:47:45 +0200

    [BUGFIX] Flexform field is available but not passed to sub-function calls
    This change fixes three occurrences of calls to FlexForm DS
    processing methods which accept a $fieldName argument but in
    the current state of the code is given an empty string instead
    of the $field variable which exists in all three contexts and
    contains the proper value.
    
    Passing the field name along to these sub functions allows hook
    subscribers to use the field name - among other things. In any
    case this fixes an unexpected behaviour turning it into an
    expected one.
    
    Releases: 6.2, 6.1, 6.0, 4.7, 4.5
    Change-Id: I078b0734f3bfa74a9acd61773c5c380f091286f8

--- typo3/sysext/core/Classes/Database/ReferenceIndex.php
+++ typo3/sysext/core/Classes/Database/ReferenceIndex.php
@@ -367,7 +367,7 @@ class ReferenceIndex {
 				if ($conf['type'] == 'flex') {
 					// Get current value array:
 					// NOTICE: failure to resolve Data Structures can lead to integrity problems with the reference index. Please look up the note in the JavaDoc documentation for the function \TYPO3\CMS\Backend\Utility\BackendUtility::getFlexFormDS()
-					$dataStructArray = \TYPO3\CMS\Backend\Utility\BackendUtility::getFlexFormDS($conf, $row, $table, '', $this->WSOL);
+					$dataStructArray = \TYPO3\CMS\Backend\Utility\BackendUtility::getFlexFormDS($conf, $row, $table, $field, $this->WSOL);
 					$currentValueArray = \TYPO3\CMS\Core\Utility\GeneralUtility::xml2array($value);
 					// Traversing the XML structure, processing files:
 					if (is_array($currentValueArray)) {
