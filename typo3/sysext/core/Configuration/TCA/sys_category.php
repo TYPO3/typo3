e862e9a4d949059832bf3b1a41b894a07ae1b7cd
Markus Klein <klein.t3@mfc-linz.at>
2013-06-24 13:22:15 +0200

    [BUGFIX] Don't show translated categories in parent tree
    The parent category tree should only show categories in default
    language.
    
    Resolves: #44915
    Releases: 6.2, 6.1, 6.0
    Change-Id: I7920cf7ae208ba2db3bd24f7da5439824064c167

--- typo3/sysext/core/Configuration/TCA/sys_category.php
+++ typo3/sysext/core/Configuration/TCA/sys_category.php
@@ -144,7 +144,7 @@ return array(
 				'type' => 'select',
 				'renderMode' => 'tree',
 				'foreign_table' => 'sys_category',
-				'foreign_table_where' => ' ORDER BY sys_category.title ASC',
+				'foreign_table_where' => ' AND sys_category.sys_language_uid IN (-1,0) ORDER BY sys_category.title ASC',
 				'treeConfig' => array(
 					'parentField' => 'parent'
 				)
