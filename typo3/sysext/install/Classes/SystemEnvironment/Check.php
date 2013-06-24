dec0bfb93b6ed878f640db5c0505e665cf43e1b8
Wouter Wolters <typo3@wouterwolters.nl>
2013-06-24 23:56:49 +0200

    [BUGFIX] Fix spelling issue in Install Tool
    compatibilyt -> compatibility
    
    Change-Id: I3bed3fa4b0477870f393498a44c5cb766d1b7c33
    Resolves: #49393
    Releases: 6.2, 6.1

--- typo3/sysext/install/Classes/SystemEnvironment/Check.php
+++ typo3/sysext/install/Classes/SystemEnvironment/Check.php
@@ -703,7 +703,7 @@ class Check {
 				$status->setMessage(
 					'suhosin.executor.include.whitelist= ' . implode(' ', $currentWhiteListArray) . '. vfs' .
 					' is currently not a hard requirement of TYPO3 CMS but tons of unit tests rely on it.' .
-					' Furthermore, vfs is likely a base for an additional compatibilyt layer in the future.' .
+					' Furthermore, vfs is likely a base for an additional compatibility layer in the future.' .
 					' A useful setting is "suhosin.executor.include.whitelist = phar vfs".'
 				);
 			} else {
