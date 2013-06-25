fffc1817bd2031824f19c0fff7f139638a0ad312
Reinhard Führicht <rf@typoheads.at>
2013-06-25 15:41:13 +0200

    [BUGFIX] Sorting ignored in "Template Overview"
    The overview of all TS templates shows a tree like view listing
    of all pages having a ts template on them.
    But this view doesn't sort the pages by "sorting" like
    in the pagetree.
    
    Resolves: #22774
    Releases: master, 6.2
    Change-Id: If7279f14697aab02735da66de5cd3cb2185f2b3a
    Reviewed-on: http://review.typo3.org/21366
    Reviewed-by: Benjamin Mack <benni@typo3.org>
    Tested-by: Benjamin Mack <benni@typo3.org>
    Reviewed-by: Wouter Wolters <typo3@wouterwolters.nl>
    Tested-by: Wouter Wolters <typo3@wouterwolters.nl>

--- typo3/sysext/tstemplate/Classes/Controller/TypoScriptTemplateModuleController.php
+++ typo3/sysext/tstemplate/Classes/Controller/TypoScriptTemplateModuleController.php
@@ -204,13 +204,21 @@ class TypoScriptTemplateModuleController extends BaseScriptClass {
 			$markers['CONTENT'] = $this->content;
 		} else {
 			// Template pages:
-			$db = $this->getDatabaseConnection();
-			$res = $db->exec_SELECTquery('pages.uid, count(*) AS count, max(sys_template.root) AS root_max_val, min(sys_template.root) AS root_min_val', 'pages,sys_template', 'pages.uid=sys_template.pid' . BackendUtility::deleteClause('pages') . BackendUtility::versioningPlaceholderClause('pages') . BackendUtility::deleteClause('sys_template') . BackendUtility::versioningPlaceholderClause('sys_template'), 'pages.uid');
+			$records = $this->getDatabaseConnection()->exec_SELECTgetRows(
+				'pages.uid, count(*) AS count, max(sys_template.root) AS root_max_val, min(sys_template.root) AS root_min_val',
+				'pages,sys_template',
+				'pages.uid=sys_template.pid'
+					. BackendUtility::deleteClause('pages')
+					. BackendUtility::versioningPlaceholderClause('pages')
+					. BackendUtility::deleteClause('sys_template')
+					. BackendUtility::versioningPlaceholderClause('sys_template'),
+				'pages.uid',
+				'pages.pid, pages.sorting'
+			);
 			$pArray = array();
-			while ($row = $db->sql_fetch_assoc($res)) {
-				$this->setInPageArray($pArray, BackendUtility::BEgetRootLine($row['uid'], 'AND 1=1'), $row);
+			foreach ($records as $record) {
+				$this->setInPageArray($pArray, BackendUtility::BEgetRootLine($record['uid'], 'AND 1=1'), $record);
 			}
-			$db->sql_free_result($res);
 
 			$table = '<div class="table-fit"><table class="table table-striped table-hover" id="ts-overview">' .
 					'<thead>' .
