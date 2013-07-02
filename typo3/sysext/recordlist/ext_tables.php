a5651424f23560e8f525a14ff7cf9ba6f0e37ebd
Wouter Wolters <typo3@wouterwolters.nl>
2013-07-02 12:03:28 +0200

    [TASK] Cleanup ext:recordlist
    * Remove ext_autoload.php
    * Refactor ext_tables.php
    * Move code from mod1/index.php to Classes/RecordList.php
    
    Change-Id: I251ff7631042487aa8d85c1b2483be8db1fb9992
    Resolves: #49578
    Releases: 6.2

--- typo3/sysext/recordlist/ext_tables.php
+++ typo3/sysext/recordlist/ext_tables.php
@@ -3,7 +3,15 @@ if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
 if (TYPO3_MODE === 'BE') {
-	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModulePath('web_list', \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'mod1/');
-	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModule('web', 'list', '', \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'mod1/');
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModulePath(
+		'web_list',
+		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'mod1/'
+	);
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModule(
+		'web',
+		'list',
+		'',
+		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'mod1/'
+	);
 }
 ?>
