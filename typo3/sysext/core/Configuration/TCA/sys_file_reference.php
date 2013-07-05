fc4a39523dfff9c21c4f90559e2f5549342db494
Andreas Wolf <andreas.wolf@typo3.org>
2013-07-05 15:15:56 +0200

    [TASK] sys_file/sys_file_reference override strategy
    Until now, the sys_file_reference records use the regular "null value"
    checkbox to switch between the value from sys_file and a custom value
    the user can supply. This is however confusing for users, as there is no
    explanation what the checkbox really does and also the placeholder text
    is not removed when checking the checkbox (but an empty string is stored
    in the database and thus used for output afterwards).
    
    This commit changes the appearance to have a checkbox on top and toggle
    between a classical read-only field and a real input field.
    
    Change-Id: I2370b17c2cdf475aa6742cbca7cd12b6c8558130
    Resolves: #49685

--- typo3/sysext/core/Configuration/TCA/sys_file_reference.php
+++ typo3/sysext/core/Configuration/TCA/sys_file_reference.php
@@ -152,9 +152,10 @@ return array(
 			'label' => 'LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.title',
 			'config' => array(
 				'type' => 'input',
-				'eval' => 'null',
 				'size' => '20',
+				'eval' => 'null',
 				'placeholder' => '__row|uid_local|title',
+				'mode' => 'useOrOverridePlaceholder',
 			)
 		),
 		'link' => array(
@@ -183,10 +184,11 @@ return array(
 			'label' => 'LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.description',
 			'config' => array(
 				'type' => 'text',
-				'eval' => 'null',
 				'cols' => '20',
 				'rows' => '5',
+				'eval' => 'null',
 				'placeholder' => '__row|uid_local|description',
+				'mode' => 'useOrOverridePlaceholder',
 			)
 		),
 		'alternative' => array(
@@ -195,9 +197,10 @@ return array(
 			'label' => 'LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.alternative',
 			'config' => array(
 				'type' => 'input',
-				'eval' => 'null',
 				'size' => '20',
+				'eval' => 'null',
 				'placeholder' => '__row|uid_local|alternative',
+				'mode' => 'useOrOverridePlaceholder',
 			),
 		),
 	),
