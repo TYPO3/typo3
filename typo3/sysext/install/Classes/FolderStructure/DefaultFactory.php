f610981c92dfcf7a697d9ea74f0284db6d1c79e4
Christian Kuhn <lolli@schwarzbu.ch>
2013-07-05 21:11:49 +0200

    [BUGFIX] Folder structure honors file and folderCreateMask
    The folder structure class construct takes care of permissions
    of files and folders. The patch adds support for config values
    fileCreateMask and folderCreateMask.
    
    Resolves: #49715
    Related: #49162
    Releases: 6.2
    
    Change-Id: I9f41ba7f038992ef77cf212229fca524e346431f
    Reviewed-on: https://review.typo3.org/21971
    Reviewed-by: Steffen Ritter
    Tested-by: Steffen Ritter

--- typo3/sysext/install/Classes/FolderStructure/DefaultFactory.php
+++ typo3/sysext/install/Classes/FolderStructure/DefaultFactory.php
@@ -30,232 +30,14 @@ namespace TYPO3\CMS\Install\FolderStructure;
 class DefaultFactory {
 
 	/**
-	 * @var array Expected folder structure
-	 */
-	protected $expectedDefaultStructure = array();
-
-	/**
-	 * Constructor
-	 */
-	public function __construct() {
-		$this->expectedDefaultStructure = array(
-			// Cut off trailing forward / from PATH_site, so root node has no trailing slash like all others
-			'name' => substr(PATH_site, 0, -1),
-			'targetPermission' => '2770',
-			'children' => array(
-				array(
-					'name' => 'typo3temp',
-					'type' => 'TYPO3\\CMS\\install\\FolderStructure\\DirectoryNode',
-					'targetPermission' => '2770',
-					'children' => array(
-						array(
-							'name' => 'index.html',
-							'type' => 'TYPO3\\CMS\\install\\FolderStructure\\FileNode',
-							'targetPermission' => '0660',
-							'targetContent' => '',
-						),
-						array(
-							'name' => 'compressor',
-							'type' => 'TYPO3\\CMS\\install\\FolderStructure\\DirectoryNode',
-							'targetPermission' => '2770',
-						),
-						array(
-							'name' => 'cs',
-							'type' => 'TYPO3\\CMS\\install\\FolderStructure\\DirectoryNode',
-							'targetPermission' => '2770',
-						),
-						array(
-							'name' => 'Cache',
-							'type' => 'TYPO3\\CMS\\install\\FolderStructure\\DirectoryNode',
-							'targetPermission' => '2770',
-						),
-						array(
-							'name' => 'GB',
-							'type' => 'TYPO3\\CMS\\install\\FolderStructure\\DirectoryNode',
-							'targetPermission' => '2770',
-						),
-						array(
-							'name' => 'llxml',
-							'type' => 'TYPO3\\CMS\\install\\FolderStructure\\DirectoryNode',
-							'targetPermission' => '2770',
-						),
-						array(
-							'name' => 'locks',
-							'type' => 'TYPO3\\CMS\\install\\FolderStructure\\DirectoryNode',
-							'targetPermission' => '2770',
-						),
-						array(
-							'name' => 'pics',
-							'type' => 'TYPO3\\CMS\\install\\FolderStructure\\DirectoryNode',
-							'targetPermission' => '2770',
-						),
-						array(
-							'name' => 'sprites',
-							'type' => 'TYPO3\\CMS\\install\\FolderStructure\\DirectoryNode',
-							'targetPermission' => '2770',
-						),
-						array(
-							'name' => 'temp',
-							'type' => 'TYPO3\\CMS\\install\\FolderStructure\\DirectoryNode',
-							'targetPermission' => '2770',
-						),
-					),
-				),
-				array(
-					'name' => 'typo3conf',
-					'type' => 'TYPO3\\CMS\\install\\FolderStructure\\DirectoryNode',
-					'targetPermission' => '2770',
-					'children' => array(
-						array(
-							'name' => 'ext',
-							'type' => 'TYPO3\\CMS\\install\\FolderStructure\\DirectoryNode',
-							'targetPermission' => '2770',
-						),
-						array(
-							'name' => 'l10n',
-							'type' => 'TYPO3\\CMS\\install\\FolderStructure\\DirectoryNode',
-							'targetPermission' => '2770',
-						),
-					),
-				),
-				array(
-					'name' => 'uploads',
-					'type' => 'TYPO3\\CMS\\install\\FolderStructure\\DirectoryNode',
-					'targetPermission' => '2770',
-					'children' => array(
-						array(
-							'name' => 'index.html',
-							'type' => 'TYPO3\\CMS\\install\\FolderStructure\\FileNode',
-							'targetPermission' => '0660',
-							'targetContent' =>
-								'<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">' . LF .
-								'<HTML>' . LF .
-								'<HEAD>' . LF .
-								TAB . '<TITLE></TITLE>' . LF .
-								'<META http-equiv=Refresh Content="0; Url=../">' . LF .
-								'</HEAD>' . LF .
-								'</HTML>',
-						),
-						array(
-							'name' => 'media',
-							'type' => 'TYPO3\\CMS\\install\\FolderStructure\\DirectoryNode',
-							'targetPermission' => '2770',
-							'children' => array(
-								array(
-									'name' => 'index.html',
-									'type' => 'TYPO3\\CMS\\install\\FolderStructure\\FileNode',
-									'targetPermission' => '0660',
-									'targetContent' => '',
-								),
-							),
-						),
-						array(
-							'name' => 'pics',
-							'type' => 'TYPO3\\CMS\\install\\FolderStructure\\DirectoryNode',
-							'targetPermission' => '2770',
-							'children' => array(
-								array(
-									'name' => 'index.html',
-									'type' => 'TYPO3\\CMS\\install\\FolderStructure\\FileNode',
-									'targetPermission' => '0660',
-									'targetContent' => '',
-								),
-							),
-						),
-						array(
-							'name' => 'tf',
-							'type' => 'TYPO3\\CMS\\install\\FolderStructure\\DirectoryNode',
-							'targetPermission' => '2770',
-							'children' => array(
-								array(
-									'name' => 'index.html',
-									'type' => 'TYPO3\\CMS\\install\\FolderStructure\\FileNode',
-									'targetPermission' => '0660',
-									'targetContent' => '',
-								),
-							),
-						),
-					),
-				),
-				array(
-					'name' => 'fileadmin',
-					'type' => 'TYPO3\\CMS\\install\\FolderStructure\\DirectoryNode',
-					'targetPermission' => '2770',
-					'children' => array(
-						array(
-							'name' => '_temp_',
-							'type' => 'TYPO3\\CMS\\install\\FolderStructure\\DirectoryNode',
-							'targetPermission' => '2770',
-							'children' => array(
-								array(
-									'name' => '.htaccess',
-									'type' => 'TYPO3\\CMS\\install\\FolderStructure\\FileNode',
-									'targetPermission' => '0660',
-									'targetContent' =>
-										'# This file restricts access to the fileadmin/_temp_ directory. It is' . LF .
-										'# meant to protect temporary files which could contain sensible' . LF .
-										'# information. Please do not touch.' . LF .
-										LF .
-										'Order deny,allow' . LF .
-										'Deny from all' . LF,
-								),
-								array(
-									'name' => 'index.html',
-									'type' => 'TYPO3\\CMS\\install\\FolderStructure\\FileNode',
-									'targetPermission' => '0660',
-									'targetContent' =>
-										'<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">' . LF .
-										'<HTML>' . LF .
-										'<HEAD>' . LF .
-										TAB . '<TITLE></TITLE>' . LF .
-										'<META http-equiv=Refresh Content="0; Url=/">' . LF .
-										'</HEAD>' . LF .
-										'</HTML>',
-								),
-							),
-						),
-						array(
-							'name' => 'user_upload',
-							'type' => 'TYPO3\\CMS\\install\\FolderStructure\\DirectoryNode',
-							'targetPermission' => '2770',
-							'children' => array(
-								array(
-									'name' => '_temp_',
-									'type' => 'TYPO3\\CMS\\install\\FolderStructure\\DirectoryNode',
-									'targetPermission' => '2770',
-									'children' => array(
-										array(
-											'name' => 'index.html',
-											'type' => 'TYPO3\\CMS\\install\\FolderStructure\\FileNode',
-											'targetPermission' => '0660',
-											'targetContent' => '',
-										),
-									),
-								),
-								array(
-									'name' => 'index.html',
-									'type' => 'TYPO3\\CMS\\install\\FolderStructure\\FileNode',
-									'targetPermission' => '0660',
-									'targetContent' => '',
-								),
-							),
-						),
-					),
-				),
-			),
-		);
-	}
-
-	/**
 	 * Get default structure object hierarchy
 	 *
 	 * @throws Exception
 	 * @return RootNode
 	 * @TODO: Use objectManager instead of new (will be injected)
-	 * @TODO: Handle targetPermission for files / directory if set in TYPO3_CONF_VARS
 	 */
 	public function getStructure() {
-		$rootNode = new RootNode($this->expectedDefaultStructure, NULL);
+		$rootNode = new RootNode($this->getDefaultStructureDefinition(), NULL);
 		if (!($rootNode instanceof RootNodeInterface)) {
 			throw new Exception(
 				'Root node must implement RootNodeInterface',
@@ -271,5 +53,224 @@ class DefaultFactory {
 		}
 		return $structureFacade;
 	}
+
+	/**
+	 * Default definition of folder and file structure with dynamic
+	 * permission settings
+	 *
+	 * @return array
+	 */
+	protected function getDefaultStructureDefinition() {
+		$filePermission = $GLOBALS['TYPO3_CONF_VARS']['BE']['fileCreateMask'];
+		$directoryPermission = $GLOBALS['TYPO3_CONF_VARS']['BE']['folderCreateMask'];
+
+		return array(
+			// Cut off trailing forward / from PATH_site, so root node has no trailing slash like all others
+			'name' => substr(PATH_site, 0, -1),
+			'targetPermission' => $directoryPermission,
+			'children' => array(
+				array(
+					'name' => 'typo3temp',
+					'type' => 'TYPO3\\CMS\\install\\FolderStructure\\DirectoryNode',
+					'targetPermission' => $directoryPermission,
+					'children' => array(
+						array(
+							'name' => 'index.html',
+							'type' => 'TYPO3\\CMS\\install\\FolderStructure\\FileNode',
+							'targetPermission' => $filePermission,
+							'targetContent' => '',
+						),
+						array(
+							'name' => 'compressor',
+							'type' => 'TYPO3\\CMS\\install\\FolderStructure\\DirectoryNode',
+							'targetPermission' => $directoryPermission,
+						),
+						array(
+							'name' => 'cs',
+							'type' => 'TYPO3\\CMS\\install\\FolderStructure\\DirectoryNode',
+							'targetPermission' => $directoryPermission,
+						),
+						array(
+							'name' => 'Cache',
+							'type' => 'TYPO3\\CMS\\install\\FolderStructure\\DirectoryNode',
+							'targetPermission' => $directoryPermission,
+						),
+						array(
+							'name' => 'GB',
+							'type' => 'TYPO3\\CMS\\install\\FolderStructure\\DirectoryNode',
+							'targetPermission' => $directoryPermission,
+						),
+						array(
+							'name' => 'llxml',
+							'type' => 'TYPO3\\CMS\\install\\FolderStructure\\DirectoryNode',
+							'targetPermission' => $directoryPermission,
+						),
+						array(
+							'name' => 'locks',
+							'type' => 'TYPO3\\CMS\\install\\FolderStructure\\DirectoryNode',
+							'targetPermission' => $directoryPermission,
+						),
+						array(
+							'name' => 'pics',
+							'type' => 'TYPO3\\CMS\\install\\FolderStructure\\DirectoryNode',
+							'targetPermission' => $directoryPermission,
+						),
+						array(
+							'name' => 'sprites',
+							'type' => 'TYPO3\\CMS\\install\\FolderStructure\\DirectoryNode',
+							'targetPermission' => $directoryPermission,
+						),
+						array(
+							'name' => 'temp',
+							'type' => 'TYPO3\\CMS\\install\\FolderStructure\\DirectoryNode',
+							'targetPermission' => $directoryPermission,
+						),
+					),
+				),
+				array(
+					'name' => 'typo3conf',
+					'type' => 'TYPO3\\CMS\\install\\FolderStructure\\DirectoryNode',
+					'targetPermission' => $directoryPermission,
+					'children' => array(
+						array(
+							'name' => 'ext',
+							'type' => 'TYPO3\\CMS\\install\\FolderStructure\\DirectoryNode',
+							'targetPermission' => $directoryPermission,
+						),
+						array(
+							'name' => 'l10n',
+							'type' => 'TYPO3\\CMS\\install\\FolderStructure\\DirectoryNode',
+							'targetPermission' => $directoryPermission,
+						),
+					),
+				),
+				array(
+					'name' => 'uploads',
+					'type' => 'TYPO3\\CMS\\install\\FolderStructure\\DirectoryNode',
+					'targetPermission' => $directoryPermission,
+					'children' => array(
+						array(
+							'name' => 'index.html',
+							'type' => 'TYPO3\\CMS\\install\\FolderStructure\\FileNode',
+							'targetPermission' => $filePermission,
+							'targetContent' =>
+								'<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">' . LF .
+								'<HTML>' . LF .
+								'<HEAD>' . LF .
+								TAB . '<TITLE></TITLE>' . LF .
+								'<META http-equiv=Refresh Content="0; Url=../">' . LF .
+								'</HEAD>' . LF .
+								'</HTML>',
+						),
+						array(
+							'name' => 'media',
+							'type' => 'TYPO3\\CMS\\install\\FolderStructure\\DirectoryNode',
+							'targetPermission' => $directoryPermission,
+							'children' => array(
+								array(
+									'name' => 'index.html',
+									'type' => 'TYPO3\\CMS\\install\\FolderStructure\\FileNode',
+									'targetPermission' => $filePermission,
+									'targetContent' => '',
+								),
+							),
+						),
+						array(
+							'name' => 'pics',
+							'type' => 'TYPO3\\CMS\\install\\FolderStructure\\DirectoryNode',
+							'targetPermission' => $directoryPermission,
+							'children' => array(
+								array(
+									'name' => 'index.html',
+									'type' => 'TYPO3\\CMS\\install\\FolderStructure\\FileNode',
+									'targetPermission' => $filePermission,
+									'targetContent' => '',
+								),
+							),
+						),
+						array(
+							'name' => 'tf',
+							'type' => 'TYPO3\\CMS\\install\\FolderStructure\\DirectoryNode',
+							'targetPermission' => $directoryPermission,
+							'children' => array(
+								array(
+									'name' => 'index.html',
+									'type' => 'TYPO3\\CMS\\install\\FolderStructure\\FileNode',
+									'targetPermission' => $filePermission,
+									'targetContent' => '',
+								),
+							),
+						),
+					),
+				),
+				array(
+					'name' => 'fileadmin',
+					'type' => 'TYPO3\\CMS\\install\\FolderStructure\\DirectoryNode',
+					'targetPermission' => $directoryPermission,
+					'children' => array(
+						array(
+							'name' => '_temp_',
+							'type' => 'TYPO3\\CMS\\install\\FolderStructure\\DirectoryNode',
+							'targetPermission' => $directoryPermission,
+							'children' => array(
+								array(
+									'name' => '.htaccess',
+									'type' => 'TYPO3\\CMS\\install\\FolderStructure\\FileNode',
+									'targetPermission' => $filePermission,
+									'targetContent' =>
+										'# This file restricts access to the fileadmin/_temp_ directory. It is' . LF .
+										'# meant to protect temporary files which could contain sensible' . LF .
+										'# information. Please do not touch.' . LF .
+										LF .
+										'Order deny,allow' . LF .
+										'Deny from all' . LF,
+								),
+								array(
+									'name' => 'index.html',
+									'type' => 'TYPO3\\CMS\\install\\FolderStructure\\FileNode',
+									'targetPermission' => $filePermission,
+									'targetContent' =>
+										'<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">' . LF .
+										'<HTML>' . LF .
+										'<HEAD>' . LF .
+										TAB . '<TITLE></TITLE>' . LF .
+										'<META http-equiv=Refresh Content="0; Url=/">' . LF .
+										'</HEAD>' . LF .
+										'</HTML>',
+								),
+							),
+						),
+						array(
+							'name' => 'user_upload',
+							'type' => 'TYPO3\\CMS\\install\\FolderStructure\\DirectoryNode',
+							'targetPermission' => $directoryPermission,
+							'children' => array(
+								array(
+									'name' => '_temp_',
+									'type' => 'TYPO3\\CMS\\install\\FolderStructure\\DirectoryNode',
+									'targetPermission' => $directoryPermission,
+									'children' => array(
+										array(
+											'name' => 'index.html',
+											'type' => 'TYPO3\\CMS\\install\\FolderStructure\\FileNode',
+											'targetPermission' => $filePermission,
+											'targetContent' => '',
+										),
+									),
+								),
+								array(
+									'name' => 'index.html',
+									'type' => 'TYPO3\\CMS\\install\\FolderStructure\\FileNode',
+									'targetPermission' => $filePermission,
+									'targetContent' => '',
+								),
+							),
+						),
+					),
+				),
+			),
+		);
+	}
+
 }
 ?>
