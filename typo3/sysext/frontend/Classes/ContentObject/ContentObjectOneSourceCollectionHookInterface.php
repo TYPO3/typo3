1316fe39bff5138cbc83106571391b2d97b448eb
Martin Ficzel <ficzel@work.de>
2013-07-06 19:06:03 +0200

    [FEATURE] Add responsive Image Rendering
    The Image cObject can render a sourceCollection to support
    different display-resolutions and screen sizes.
    
    Resolves: #49723
    Releases: 6.2
    Change-Id: Ief02532f8f0a4e8e4b78ba15e7a9fd47c6cbd463
    Reviewed-on: https://review.typo3.org/22052
    Reviewed-by: Christian Kuhn
    Tested-by: Christian Kuhn
    Reviewed-by: Sebastian Fischer
    Tested-by: Sebastian Fischer
    Reviewed-by: Anja Leichsenring
    Tested-by: Anja Leichsenring

---
+++ typo3/sysext/frontend/Classes/ContentObject/ContentObjectOneSourceCollectionHookInterface.php
@@ -0,0 +1,46 @@
+<?php
+namespace TYPO3\CMS\Frontend\ContentObject;
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2013 Ingo Schmitt <is@marketing-factory.de>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *  A copy is found in the textfile GPL.txt and important notices to the license
+ *  from the author is found in LICENSE.txt distributed with these scripts.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * Interface for classes which hook into getSourceCollection for additional processing
+ */
+interface ContentObjectOneSourceCollectionHookInterface {
+
+	/**
+	 * Renders One Source Collection
+	 *
+	 * @param array $sourceRenderConfiguration Array with TypoScript Properties for the imgResource
+	 * @param array $sourceConfiguration
+	 * @param string $oneSourceCollection already prerendered SourceCollection
+	 * @param \TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer $parentObject Parent content object
+	 * @internal param array $configuration Array with the Source Configuration
+	 * @return string HTML Content for oneSourceCollection
+	 */
+	public function getOneSourceCollection(array $sourceRenderConfiguration, array $sourceConfiguration, $oneSourceCollection, \TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer &$parentObject);
+
+}
