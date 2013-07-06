7f5f628ccc2524fe80b605068c0f014078af15d3
Alexander Schnitzler <alex.schnitzler@typovision.de>
2013-07-06 17:39:05 +0200

    [BUGFIX] Undefined switchable controller actions must be removed
    Currently there is no check if switchable controller actions in
    pi_flexform are still present in the configuration flexform. So,
    when removing the flexforms in the configuration file, the
    switchable controller actions section is not deleted in pi_flexform.
    
    This change removes all occurences in the present pi_flexform, so
    if no switchable controller actions are defined any longer, they
    will be removed, else they are overwritten by the current flexform
    data.
    
    Releases: 6.2
    Resolves: #31198
    Change-Id: I6046b29dbf436d8c970593dbc6fd2c216f222c52

---
+++ typo3/sysext/extbase/Classes/Hook/DataHandler/CheckFlexFormValue.php
@@ -0,0 +1,65 @@
+<?php
+namespace TYPO3\CMS\Extbase\Hook\DataHandler;
+
+/*                                                                        *
+ * This script belongs to the Extbase framework                           *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU Lesser General Public License as published by the *
+ * Free Software Foundation, either version 3 of the License, or (at your *
+ * option) any later version.                                             *
+ *                                                                        *
+ * This script is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHAN-    *
+ * TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser       *
+ * General Public License for more details.                               *
+ *                                                                        *
+ * You should have received a copy of the GNU Lesser General Public       *
+ * License along with the script.                                         *
+ * If not, see http://www.gnu.org/licenses/lgpl.html                      *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+use \TYPO3\CMS\Core\DataHandling\DataHandler;
+
+/**
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License, version 3 or later
+ * @api
+ */
+class CheckFlexFormValue {
+
+	/**
+	 * Check flexform value before merge
+	 *
+	 * @param DataHandler $dataHander
+	 * @param array &$currentValue
+	 * @param array &$newValue
+	 * @return void
+	 */
+	public function checkFlexFormValue_beforeMerge(DataHandler $dataHander, array &$currentValue, array &$newValue) {
+		$currentValue = $this->removeSwitchableControllerActionsRecursive($currentValue);
+	}
+
+	/**
+	 * Remove switchable controller actions recursively
+	 *
+	 * @param array $a
+	 * @return array
+	 */
+	protected function removeSwitchableControllerActionsRecursive(array $a) {
+		$r = array();
+
+		foreach ($a as $k => $v) {
+			if ($k === 'switchableControllerActions') {
+				continue;
+			}
+
+			$r[$k] = is_array($v) ? $this->removeSwitchableControllerActionsRecursive($v) : $v;
+		}
+
+		return $r;
+	}
+}
+
+?>
