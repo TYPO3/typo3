e6e0b4f1488671a2cf69940d1287bd5ddd363947
Tim Spiekerkoetter <tim.spiekerkoetter@hdnet.de>
2013-07-06 12:35:35 +0200

    [FEATURE] Added autofocus attribute to textfield and textarea
    The ViewHelpers f:form.textfield and f:form.textarea were lacking the
    HTML5 "autofocus" attribute. It is available for button, input and
    textarea. Now Fluid reflects this correctly by adding this attribute to
    the initializeArguments methods for TextareaViewHelper and
    TextfieldViewHelper.
    
    Resolves: #48689
    Releases: 6.2
    Change-Id: I00480dc4e76b2854dc0be36d150d32c7b432d93d
    Reviewed-on: https://review.typo3.org/21994
    Reviewed-by: Wouter Wolters
    Tested-by: Wouter Wolters
    Reviewed-by: Christian Kuhn
    Tested-by: Christian Kuhn

--- typo3/sysext/fluid/Classes/ViewHelpers/Form/TextfieldViewHelper.php
+++ typo3/sysext/fluid/Classes/ViewHelpers/Form/TextfieldViewHelper.php
@@ -40,6 +40,7 @@ class TextfieldViewHelper extends \TYPO3\CMS\Fluid\ViewHelpers\Form\AbstractForm
 	 */
 	public function initializeArguments() {
 		parent::initializeArguments();
+		$this->registerTagAttribute('autofocus', 'string', 'Specifies that a input should automatically get focus when the page loads');
 		$this->registerTagAttribute('disabled', 'string', 'Specifies that the input element should be disabled when the page loads');
 		$this->registerTagAttribute('maxlength', 'int', 'The maxlength attribute of the input field (will not be validated)');
 		$this->registerTagAttribute('readonly', 'string', 'The readonly attribute of the input field');
