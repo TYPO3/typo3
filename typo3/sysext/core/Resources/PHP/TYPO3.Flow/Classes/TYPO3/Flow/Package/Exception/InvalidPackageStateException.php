6938e8f7abb0d1cb119bc65ea8a8fdd0e5f48b6d
Thomas Maroschik <tmaroschik@dfau.de>
2013-07-08 21:22:31 +0200

    [WIP][FEATURE] Integrate preliminary PackageManager API
    Resolves: #47018
    Releases: 6.2
    Change-Id: I52abedaf470cebddb3553e37581021bf368380e6

---
+++ typo3/sysext/core/Resources/PHP/TYPO3.Flow/Classes/TYPO3/Flow/Package/Exception/InvalidPackageStateException.php
@@ -0,0 +1,23 @@
+<?php
+namespace TYPO3\Flow\Package\Exception;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow framework.                       *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU Lesser General Public License, either version 3   *
+ * of the License, or (at your option) any later version.                 *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+/**
+ * An "Invalid Package State" exception
+ *
+ * @api
+ */
+class InvalidPackageStateException extends \TYPO3\Flow\Package\Exception {
+
+}
+
+?>
