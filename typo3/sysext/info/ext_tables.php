e0822ef3303d2d0c14114124612087bb93e8c5cd
Wouter Wolters <typo3@wouterwolters.nl>
2013-07-12 21:46:59 +0200

    [TASK] Cleanup ext:info and ext:info_pagetsconfig
    - Remove require_once calls
    - Replace global $LANG and $BACK_PATH
    - Make some function calls use real boolean
    
    Change-Id: I6e913e183a8357076b55d84c961e1104d643e81e
    Resolves: #49992
    Releases: 6.2

--- typo3/sysext/info/ext_tables.php
+++ typo3/sysext/info/ext_tables.php
@@ -3,6 +3,11 @@ if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
 if (TYPO3_MODE === 'BE') {
-	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModule('web', 'info', '', \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'mod1/');
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModule(
+		'web',
+		'info',
+		'',
+		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'mod1/'
+	);
 }
 ?>
