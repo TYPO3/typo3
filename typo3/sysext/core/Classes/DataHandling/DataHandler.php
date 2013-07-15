e00f0ecb28badf274868e9728ccd0d99f153bc5f
Bernhard Kraft <kraft@webconsulting.at>
2013-07-15 14:20:40 +0200

    [BUGFIX] Sorting of tt_content localized elements after copy
    When making a copy of a content element on a page with two or more
    languages, the pasted element will appear in the correct position on the
    target page whereas the localized partner elements will appear on the
    correct position (colPos) only if the destination record has a
    translation.
    
    Change-Id: I5f4da5378d3c1b6ca0c9eb13ff7e89e7030d9a04
    Resolves: #50027
    Releases: 6.2, 6.1, 6.0

--- typo3/sysext/core/Classes/DataHandling/DataHandler.php
+++ typo3/sysext/core/Classes/DataHandling/DataHandler.php
@@ -3379,7 +3379,18 @@ class DataHandler {
 				if ($localizedDestPid < 0) {
 					$this->copyRecord($table, $record['uid'], $localizedDestPid, $first, $overrideValues, $excludeFields, $record[$GLOBALS['TCA'][$table]['ctrl']['languageField']]);
 				} else {
-					$this->copyRecord($table, $record['uid'], $destPid < 0 ? $tscPID : $destPid, $first, $overrideValues, $excludeFields, $record[$GLOBALS['TCA'][$table]['ctrl']['languageField']]);
+					$newId = $this->copyRecord($table, $record['uid'], $destPid < 0 ? $tscPID : $destPid, $first, $overrideValues, $excludeFields, $record[$GLOBALS['TCA'][$table]['ctrl']['languageField']]);
+					// If the original record got copied after another one (destination record) but there is no
+					// translated version of the destination record then at least apply the "copyAfterDuplFields"
+					// but take care not to reset "sys_language_uid"
+					if ($newId && $destPid < 0) {
+
+						$newData = $this->fixCopyAfterDuplFields($table, $newId, $overrideValues[$GLOBALS['TCA'][$table]['ctrl']['transOrigPointerField']], FALSE);
+						unset($newData[$GLOBALS['TCA'][$table]['ctrl']['languageField']]);
+						if (count($newData)) {
+							$this->updateDB($table, $newId, $newData);
+						}
+					}
 				}
 			}
 		}
@@ -3516,6 +3527,11 @@ class DataHandler {
 				$this->moveRecord_procFields($table, $uid, $destPid);
 				// Create query for update:
 				$GLOBALS['TYPO3_DB']->exec_UPDATEquery($table, 'uid=' . intval($uid), $updateFields);
+				// fixCopyAfterDuplFields.
+				// This has to be done before "moveL10nOverlayRecords" so that "colPos" of the moved record is set properly then
+				if ($origDestPid < 0) {
+					$this->fixCopyAfterDuplFields($table, $uid, abs($origDestPid), 1);
+				}
 				// Check for the localizations of that element
 				$this->moveL10nOverlayRecords($table, $uid, $destPid, $destPid);
 				// Call post processing hooks:
@@ -3540,10 +3556,6 @@ class DataHandler {
 				// Clear cache after moving
 				$this->clear_cache($table, $uid);
 				$this->fixUniqueInPid($table, $uid);
-				// fixCopyAfterDuplFields
-				if ($origDestPid < 0) {
-					$this->fixCopyAfterDuplFields($table, $uid, abs($origDestPid), 1);
-				}
 			} else {
 				$destPropArr = $this->getRecordProperties('pages', $destPid);
 				$this->log($table, $uid, 4, 0, 1, 'Attempt to move page \'%s\' (%s) to inside of its own rootline (at page \'%s\' (%s))', 10, array($propArr['header'], $uid, $destPropArr['header'], $destPid), $propArr['pid']);
@@ -3569,6 +3581,10 @@ class DataHandler {
 						$this->moveRecord_procFields($table, $uid, $destPid);
 						// Create query for update:
 						$GLOBALS['TYPO3_DB']->exec_UPDATEquery($table, 'uid=' . intval($uid), $updateFields);
+						// fixCopyAfterDuplFields
+						if ($origDestPid < 0) {
+							$this->fixCopyAfterDuplFields($table, $uid, abs($origDestPid), 1);
+						}
 						// Check for the localizations of that element
 						$this->moveL10nOverlayRecords($table, $uid, $destPid, $originalRecordDestinationPid);
 						// Call post processing hooks:
@@ -3594,10 +3610,6 @@ class DataHandler {
 						$this->clear_cache($table, $uid);
 						// fixUniqueInPid
 						$this->fixUniqueInPid($table, $uid);
-						// fixCopyAfterDuplFields
-						if ($origDestPid < 0) {
-							$this->fixCopyAfterDuplFields($table, $uid, abs($origDestPid), 1);
-						}
 					} else {
 						$destPropArr = $this->getRecordProperties('pages', $destPid);
 						$this->log($table, $uid, 4, 0, 1, 'Attempt to move page \'%s\' (%s) to inside of its own rootline (at page \'%s\' (%s))', 10, array($propArr['header'], $uid, $destPropArr['header'], $destPid), $propArr['pid']);
@@ -3711,6 +3723,16 @@ class DataHandler {
 					$this->moveRecord($table, $record['uid'], $localizedDestPid);
 				} else {
 					$this->moveRecord($table, $record['uid'], $destPid);
+					// If the original record gets moved after another one (destination record) but there is no
+					// translated version of the destination record then at least apply the "copyAfterDuplFields"
+					// but take care not to reset "sys_language_uid"
+					if ($originalRecordDestinationPid < 0) {
+						$newData = $this->fixCopyAfterDuplFields($table, $record['uid'], $uid, FALSE);
+						unset($newData[$GLOBALS['TCA'][$table]['ctrl']['languageField']]);
+						if (count($newData)) {
+							$this->updateDB($table, $record['uid'], $newData);
+						}
+					}
 				}
 			}
 		}
