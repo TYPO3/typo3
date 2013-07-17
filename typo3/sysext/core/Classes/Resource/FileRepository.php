7ad81cb1e663798d6119f0b501606807b833586c
Marc Bastian Heinrichs <typo3@mbh-software.de>
2013-07-17 15:46:38 +0200

    [FEATURE] Support offset and max in cObj FILES references
    Currently it is only possible to render all files by references
    with the content object FILES. But a common use case is
    that e.g. only the first image should be rendered.
    This patch adds support for rendering files by reference
    with a offset and maximum.
    
    Documentation: #50119
    Resolves: #44113
    Releases: 6.2
    Change-Id: Ia2741f2c87db6ac66e2e5d4e9b128c6a125d3a25

--- typo3/sysext/core/Classes/Resource/FileRepository.php
+++ typo3/sysext/core/Classes/Resource/FileRepository.php
@@ -197,10 +197,18 @@ class FileRepository extends AbstractRepository {
 	 * @throws \InvalidArgumentException
 	 * @api
 	 */
-	public function findByRelation($tableName, $fieldName, $uid) {
+	public function findByRelation($tableName, $fieldName, $uid, $offset = NULL, $max = NULL) {
+		$limit = '';
 		$itemList = array();
 		if (!\TYPO3\CMS\Core\Utility\MathUtility::canBeInterpretedAsInteger($uid)) {
 			throw new \InvalidArgumentException('Uid of related record has to be an integer.', 1316789798);
+		}
+		if ($offset !== NULL && $max !== NULL) {
+			$limit .= (string)$offset . ',' . (string)$max;
+		} elseif ($max !== NULL) {
+			$limit .= (string)$max;
+		} elseif($offset !== NULL) {
+			$limit .= '0,99999999999';
 		}
 		$references = $GLOBALS['TYPO3_DB']->exec_SELECTgetRows(
 			'*',
@@ -211,7 +219,8 @@ class FileRepository extends AbstractRepository {
 				' AND uid_foreign=' . intval($uid) .
 				' AND fieldname=' . $GLOBALS['TYPO3_DB']->fullQuoteStr($fieldName, 'sys_file_reference'),
 			'',
-			'sorting_foreign'
+			'sorting_foreign',
+			$limit
 		);
 		foreach ($references as $referenceRecord) {
 			$itemList[] = $this->createFileReferenceObject($referenceRecord);
