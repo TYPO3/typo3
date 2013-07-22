1c7620409e934477cdebc1212297497966e4bfaa
Markus Klein <klein.t3@mfc-linz.at>
2013-07-22 19:23:51 +0200

    [BUGFIX] ThumbnailView uses wrong md5 function
    ThumbnailView::init() uses GeneralUtility::shortMD5() to check a hash,
    but the hash was made with md5(), hence the thumb is never displayed.
    
    Resolves: #50301
    Releases: 6.2, 6.1, 6.0
    Change-Id: I8750ec4e4189c4ea24d502b4185b015a2fbefea2

--- typo3/sysext/backend/Classes/Utility/BackendUtility.php
+++ typo3/sysext/backend/Classes/Utility/BackendUtility.php
@@ -1632,7 +1632,7 @@ class BackendUtility {
 	 * @param string $thumbScript Must point to "thumbs.php" relative to the script position
 	 * @param string $theFile Must be the proper reference to the file that thumbs.php should show
 	 * @param string $tparams The additional attributes for the image tag
-	 * @param integer $size The size of the thumbnail send along to "thumbs.php
+	 * @param integer $size The size of the thumbnail sent along to "thumbs.php"
 	 * @return string Image tag
 	 */
 	static public function getThumbNail($thumbScript, $theFile, $tparams = '', $size = '') {
@@ -1641,7 +1641,8 @@ class BackendUtility {
 		$params .= trim($size) ? '&size=' . trim($size) : '';
 		$params .= '&md5sum=' . md5($check);
 		$url = $thumbScript . '?' . $params;
-		$th = '<img src="' . htmlspecialchars($url) . '" title="' . trim(basename($theFile)) . '"' . ($tparams ? ' ' . $tparams : '') . ' alt="" />';
+		$title = $alt = trim(basename($theFile));
+		$th = '<img src="' . htmlspecialchars($url) . '" title="' . $title . '"' . ($tparams ? ' ' . $tparams : '') . ' alt="' . $alt . '" />';
 		return $th;
 	}
 
