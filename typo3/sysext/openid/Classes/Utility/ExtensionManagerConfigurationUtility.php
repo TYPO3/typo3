44a514c16b27cba826042e6b84d721b7e8802c33
Christian Weiske <cweiske@cweiske.de>
2013-07-25 23:27:20 +0200

    [FEATURE] Show OpenID provider buttons on login form
    To make it easier for users to select their OpenID provider,
    the login screen now shows three large and several small OpenID provider
    logos.
    
    The user can click on them and gets directly redirected to the provider.
    Some providers have user-specific OpenID URLs - those get filled into
    the username field, with username pre-selected. Now the user can directly
    begin to type his username and press return.
    
    In the extension configuration, administrators can setup which OpenID
    provider buttons to show in large and in small.
    
    Resolves: #49399
    Releases: 6.2
    Depends: Id31238760fe4e58e2a823beddaeb454ba28d59be
    Change-Id: Ib51a1c30fe9e6946e2b6bbeb5f0d3dac88f0f9f7

---
+++ typo3/sysext/openid/Classes/Utility/ExtensionManagerConfigurationUtility.php
@@ -0,0 +1,56 @@
+<?php
+namespace TYPO3\CMS\Openid\Utility;
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) Christian Weiske <cweiske@cweiske.de>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *  A copy is found in the textfile GPL.txt and important notices to the license
+ *  from the author is found in LICENSE.txt distributed with these scripts.
+ *
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * class providing extensino configuration helpers for openid.
+ *
+ * @author Christian Weiske <cweiske@cweiske.de>
+ */
+class ExtensionManagerConfigurationUtility
+{
+	/**
+	 * Render a textare for custom OpenId provider configuration.
+	 *
+	 * @param array $params Field information to be rendered
+	 *                      - fieldName
+	 *                      - fieldValue
+	 *                      - propertyName
+	 * @param \TYPO3\CMS\Core\TypoScript\ConfigurationForm $pObj The calling parent object.
+	 *
+	 * @return string Messages as HTML if something needs to be reported
+	 */
+	public function providerInput(array $params, $pObj)
+	{
+		return '<textarea rows="5" cols="80" name="' . $params['fieldName'] . '">'
+			. htmlspecialchars($params['fieldValue'])
+			. '</textarea>';
+	}
+
+}
+?>
