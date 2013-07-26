f58dc856776ba6abc7869f34d27c2903aa4bad8b
Wouter Wolters <typo3@wouterwolters.nl>
2013-07-26 17:35:17 +0200

    [BUGFIX] Indexing of external files does not work in indexed_search
    After the namespace change a wrong replace was done in
    indexed_search. Replace it with the correct class.
    
    Change-Id: I35295f4fecde241ec3004a8ee6b7dd71d5e34e93
    Resolves: #47106
    Releases: 6.2, 6.1, 6.0

--- typo3/sysext/indexed_search/Classes/Indexer.php
+++ typo3/sysext/indexed_search/Classes/Indexer.php
@@ -797,6 +797,7 @@ class Indexer {
 							);
 							unset($params['conf']['content']);
 							$crawler->addQueueEntry_callBack(0, $params, 'EXT:indexed_search/class.crawler.php:&TYPO3\\CMS\\IndexedSearch\\Controller\\SearchFormController_files', $this->conf['id']);
+							$crawler->addQueueEntry_callBack(0, $params, '&TYPO3\\CMS\\IndexedSearch\\Hook\\CrawlerFilesHook', $this->conf['id']);
 							$this->log_setTSlogMessage('media "' . $params['document'] . '" added to "crawler" queue.', 1);
 						} else {
 							$this->indexRegularDocument($linkInfo['href'], FALSE, $linkSource, $ext);
@@ -808,7 +809,7 @@ class Indexer {
 								'conf' => $this->conf
 							);
 							unset($params['conf']['content']);
-							$crawler->addQueueEntry_callBack(0, $params, 'EXT:indexed_search/class.crawler.php:&TYPO3\\CMS\\IndexedSearch\\Controller\\SearchFormController_files', $this->conf['id']);
+							$crawler->addQueueEntry_callBack(0, $params, '&TYPO3\\CMS\\IndexedSearch\\Hook\\CrawlerFilesHook', $this->conf['id']);
 							$this->log_setTSlogMessage('media "' . $params['document'] . '" added to "crawler" queue.', 1);
 						} else {
 							$this->indexRegularDocument($linkSource);
