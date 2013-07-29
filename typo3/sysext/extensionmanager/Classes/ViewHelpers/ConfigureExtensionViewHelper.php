0a306e207d325fcae29df7691ee73255233cf4e6
Nicole Cordes <typo3@cordes.co>
2013-07-29 18:13:53 +0200

    [TASK] Make the extension titles link to the configuration
    In the extension manager make the extension titles link to the
    configuration again. This will provide an additional clickable area and
    it allows the integrators to continue using the known behavior.
    
    Resolves: #50234
    Releases: 6.2
    Change-Id: I120e8fb47628fe118291cbdd57951a0238ebcd71

--- typo3/sysext/extensionmanager/Classes/ViewHelpers/ConfigureExtensionViewHelper.php
+++ typo3/sysext/extensionmanager/Classes/ViewHelpers/ConfigureExtensionViewHelper.php
@@ -42,10 +42,13 @@ class ConfigureExtensionViewHelper extends \TYPO3\CMS\Fluid\ViewHelpers\Link\Act
 	 * Renders a configure extension link if the extension has configuration options
 	 *
 	 * @param array $extension
+	 * @param string $content
+	 *
 	 * @return string the rendered a tag
 	 */
-	public function render($extension) {
+	public function render($extension, $content = '') {
 		$tag = '';
+		$content = htmlspecialchars($content);
 		if ($extension['installed'] && file_exists(PATH_site . $extension['siteRelPath'] . '/ext_conf_template.txt')) {
 			$uriBuilder = $this->controllerContext->getUriBuilder();
 			$action = 'showConfigurationForm';
@@ -56,8 +59,13 @@ class ConfigureExtensionViewHelper extends \TYPO3\CMS\Fluid\ViewHelpers\Link\Act
 			), 'Configuration');
 			$this->tag->addAttribute('href', $uri);
 			$this->tag->addAttribute('title', \TYPO3\CMS\Extbase\Utility\LocalizationUtility::translate('extensionList.configure', 'extensionmanager'));
-			$this->tag->setContent(\TYPO3\CMS\Backend\Utility\IconUtility::getSpriteIcon('actions-system-extension-configure'));
+			if ($content === '') {
+				$content = \TYPO3\CMS\Backend\Utility\IconUtility::getSpriteIcon('actions-system-extension-configure');
+			}
+			$this->tag->setContent($content);
 			$tag = $this->tag->render();
+		} elseif ($content !== NULL) {
+			$tag = $content;
 		}
 		return $tag;
 	}
