7536d6c308f2e9099dc028a4908f79d9f75c4a5d
Georg Ringer <georg.ringer@gmail.com>
2013-08-06 14:17:09 +0200

    [BUGFIX] Use proper implementation of a wordwrap
    The page module uses a custom implementation of a wordwrap
    as this was not supported by the browser in those earlier days.
    
    The big drawback is that there is no UTF8 support for this which
    leads to empty preview boxes when e.g. using Japanese.
    
    The 6.1 version of this patch changes the wordWrapper() usage only
    and does not deprecate the method.
    
    Change-Id: I7caab8e14a9a0b7ccbd38c692b0cf98635967567
    Resolves: #15797
    Releases: 6.2, 6.1

--- typo3/sysext/backend/Classes/View/PageLayoutView.php
+++ typo3/sysext/backend/Classes/View/PageLayoutView.php
@@ -1632,7 +1632,7 @@ class PageLayoutView extends \TYPO3\CMS\Recordlist\RecordList\AbstractDatabaseRe
 	public function renderText($input) {
 		$input = strip_tags($input);
 		$input = \TYPO3\CMS\Core\Utility\GeneralUtility::fixed_lgd_cs($input, 1500);
-		return nl2br(htmlspecialchars(trim($this->wordWrapper($input)), ENT_QUOTES, 'UTF-8', FALSE));
+		return nl2br(htmlspecialchars(trim($input), ENT_QUOTES, 'UTF-8', FALSE));
 	}
 
 	/**
