69b8684c50c40c434db69097ca3175b673837a47
Georg Ringer <georg.ringer@gmail.com>
2013-08-06 14:17:09 +0200

    [BUGFIX] Use proper implementation of a wordwrap
    The page module uses a custom implementation of a wordwrap
    as this was not supported by the browser in those earlier days.
    
    The big drawback is that there is no UTF8 support for this which
    leads to empty preview boxes when e.g. using Japanese.
    
    Change-Id: I7caab8e14a9a0b7ccbd38c692b0cf98635967567
    Resolves: #15797
    Releases: 6.2, 6.1

--- typo3/sysext/backend/Classes/View/PageLayoutView.php
+++ typo3/sysext/backend/Classes/View/PageLayoutView.php
@@ -1626,7 +1626,7 @@ class PageLayoutView extends \TYPO3\CMS\Recordlist\RecordList\AbstractDatabaseRe
 	public function renderText($input) {
 		$input = strip_tags($input);
 		$input = GeneralUtility::fixed_lgd_cs($input, 1500);
-		return nl2br(htmlspecialchars(trim($this->wordWrapper($input)), ENT_QUOTES, 'UTF-8', FALSE));
+		return nl2br(htmlspecialchars(trim($input), ENT_QUOTES, 'UTF-8', FALSE));
 	}
 
 	/**
@@ -1693,15 +1693,11 @@ class PageLayoutView extends \TYPO3\CMS\Recordlist\RecordList\AbstractDatabaseRe
 	 * @param integer $max Max number of chars in a word before it will be wrapped.
 	 * @param string $char Character to insert when wrapping.
 	 * @return string Processed output.
+	 * @deprecated since 6.2, CSS is used (word-break: break-all;)
 	 * @todo Define visibility
 	 */
-	public function wordWrapper($content, $max = 50, $char = ' -') {
-		$array = preg_split('/[ ' . LF . ']/', $content);
-		foreach ($array as $val) {
-			if (strlen($val) > $max) {
-				$content = str_replace($val, substr(chunk_split($val, $max, $char), 0, -1), $content);
-			}
-		}
+	public function wordWrapper($content, $max = 50, $char = ' XXX') {
+		GeneralUtility::logDeprecatedFunction();
 		return $content;
 	}
 
