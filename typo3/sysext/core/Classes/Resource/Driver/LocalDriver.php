9c075e941368f6c5ec306f8007c9352314b50e41
Nicole Cordes <typo3@cordes.co>
2013-08-06 22:22:06 +0200

    [BUGFIX] Failing tests in Resource\Driver\LocalDriverTest on Windows
    The test createdFilesAreEmpty depends on another test which returns a
    mocked object. In the mock two returnValues for getIdentifier() and
    getName() were set. But due to PHPUnit architecture mocked functions
    belong to the test there were defined and cannot be shared. This is why
    the mock has to be initialized within the test itself. The test only
    works because on unix system you can open a folder which returns an
    empty string.
    
    Furthermore if you use any php function to get a folder path we have to
    fix the containing backslashes.
    
    Finally due to some errors we have to rewind any iterator of class
    RecursiveIteratorIterator as otherwise a wrong index is returned.
    
    Resolves: #50844
    Releases: 6.2, 6.1, 6.0
    Change-Id: I13f9efbd1618056fcd66220e3de5e12af79e4dc6

--- typo3/sysext/core/Classes/Resource/Driver/LocalDriver.php
+++ typo3/sysext/core/Classes/Resource/Driver/LocalDriver.php
@@ -319,6 +319,9 @@ class LocalDriver extends AbstractHierarchicalFilesystemDriver {
 			if ($this->applyFilterMethodsToDirectoryItem($filterMethods, $iteratorItem, $identifier, $path) === FALSE) {
 				continue;
 			}
+
+			// dirname returns "/" when called with "/" as the argument, so strip trailing slashes here to be sure
+			$path = rtrim(GeneralUtility::fixWindowsFilePath(dirname($identifier)), '/') . '/';
 			if (isset($itemRows[$identifier])) {
 				list($key, $item) = $this->{$itemHandlerMethod}($iteratorItem, $path, $itemRows[$identifier]);
 			} else {
@@ -837,11 +840,13 @@ class LocalDriver extends AbstractHierarchicalFilesystemDriver {
 		$sourceFolderPath = $this->getAbsolutePath($folderToCopy);
 		/** @var $iterator \RecursiveDirectoryIterator */
 		$iterator = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($sourceFolderPath), \RecursiveIteratorIterator::SELF_FIRST);
+		// Rewind the iterator as this is important for some systems e.g. Windows
+		$iterator->rewind();
 		while ($iterator->valid()) {
 			/** @var $current \RecursiveDirectoryIterator */
 			$current = $iterator->current();
 			$fileName = $current->getFilename();
-			$itemSubPath = $iterator->getSubPathname();
+			$itemSubPath = GeneralUtility::fixWindowsFilePath($iterator->getSubPathname());
 			if ($current->isDir() && !($fileName === '..' || $fileName === '.')) {
 				mkdir($targetFolderPath . $itemSubPath);
 			} elseif ($current->isFile()) {
