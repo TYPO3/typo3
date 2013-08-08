24f1b96baa27ae281c85b644999ff06879e9f41f
Susanne Moog <typo3@susannemoog.de>
2013-08-08 20:29:22 +0200

    [FEATURE] Uninstall broken ext in install tool
    Adds buttons to uninstall incompatible extensions
    to the install tool extension compatibility tester.
    You may install extensions one-by-one or all at
    once.
    
    Change-Id: I717e127128f0f26e5b6711e0c1db04cf8d8cbfce
    Resolves: #50940
    Releases: 6.2
    Reviewed-on: https://review.typo3.org/22972
    Reviewed-by: Wouter Wolters
    Tested-by: Wouter Wolters
    Reviewed-by: Christian Kuhn
    Tested-by: Christian Kuhn

---
+++ typo3/sysext/install/Classes/Controller/Action/Ajax/ClearCache.php
@@ -0,0 +1,61 @@
+<?php
+namespace TYPO3\CMS\Install\Controller\Action\Ajax;
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2013 Susanne Moog <typo3@susanne-moog.de>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+use TYPO3\CMS\Install\Controller\Action;
+use TYPO3\CMS\Core\Utility;
+
+/**
+ * Clear Cache
+ *
+ * This is an ajax wrapper for clearing the cache. Used for example
+ * after uninstalling an extension via ajax.
+ *
+ * @see \TYPO3\CMS\Install\Service\ClearCacheService
+ */
+class ClearCache extends Action\AbstractAction implements Action\ActionInterface {
+
+	/**
+	 * Handle this action
+	 *
+	 * @return string content
+	 */
+	public function handle() {
+		$this->initialize();
+		return $this->clearCache();
+	}
+
+	/**
+	 * Wraps the cache service
+	 *
+	 * @return string
+	 */
+	protected function clearCache() {
+		/** @var \TYPO3\CMS\Install\Service\ClearCacheService $clearCacheService */
+		$clearCacheService = $this->objectManager->get('TYPO3\\CMS\\Install\\Service\\ClearCacheService');
+		$clearCacheService->clearAll();
+		return 'OK';
+	}
+}
