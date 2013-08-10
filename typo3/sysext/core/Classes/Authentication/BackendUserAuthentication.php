7ce53db572ffa5720ce34f25c6ba563286a06fb7
Helmut Hummel <helmut.hummel@typo3.org>
2013-08-10 19:40:47 +0200

    [BUGFIX] Take into account all file and folder permissions
    When handling the old file permission settings from
    user or user group records, all current FAL user
    permissions should be set accordingly, to not allow
    file actions with FAL which were not allowed before.
    
    In particular no write / change actions should be
    allowed if no permissions are granted in the records.
    
    Releases: 6.0, 6.1, 6.2
    Resolves: #51005
    Related: #51007
    
    Change-Id: I873f23ed02428842621c8256df5792e54abbfe2c
    Reviewed-on: https://review.typo3.org/23035
    Reviewed-by: Steffen Ritter
    Tested-by: Steffen Ritter
    Reviewed-by: Helmut Hummel
    Tested-by: Helmut Hummel

--- typo3/sysext/core/Classes/Authentication/BackendUserAuthentication.php
+++ typo3/sysext/core/Classes/Authentication/BackendUserAuthentication.php
@@ -1619,6 +1619,8 @@ class BackendUserAuthentication extends \TYPO3\CMS\Core\Authentication\AbstractU
 						$defaultOptions['moveFile'] = FALSE;
 						$defaultOptions['renameFile'] = FALSE;
 						$defaultOptions['removeFile'] = FALSE;
+						$defaultOptions['editFile'] = FALSE;
+						$defaultOptions['writeFile'] = FALSE;
 					}
 					if ($oldFileOperationPermissions ^ 2) {
 						$defaultOptions['unzipFile'] = FALSE;
@@ -1628,6 +1630,7 @@ class BackendUserAuthentication extends \TYPO3\CMS\Core\Authentication\AbstractU
 						$defaultOptions['moveFolder'] = FALSE;
 						$defaultOptions['renameFolder'] = FALSE;
 						$defaultOptions['removeFolder'] = FALSE;
+						$defaultOptions['writeFolder'] = FALSE;
 					}
 					if ($oldFileOperationPermissions ^ 8) {
 						$defaultOptions['copyFolder'] = FALSE;
