7952d184fcac54a1446801f8876c2d90b2db08c1
Roland Schenke <roland.schenke@typo3.org>
2013-08-30 10:30:16 +0200

    [BUGFIX] Backend Layout Grid Wizard not fully visible in Mac Firefox 22
    Under Firefox the outer table's height and width of the backend layout
    wizard is set to 100%. Since there is content before the table, this
    leads to the table being cut of, as there are no scrollbars. According
    to the suggestion of Philipp Gampe the table's height and width are
    changed to 90%.
    
    Resolves: #50424
    Releases: 6.2, 6.1, 6.0, 4.7, 4.5
    Change-Id: I589fdfd8de0a8d6ebb4b3ee4fd2a173341116016

--- typo3/sysext/backend/Classes/Controller/BackendLayoutWizardController.php
+++ typo3/sysext/backend/Classes/Controller/BackendLayoutWizardController.php
@@ -176,7 +176,7 @@ class BackendLayoutWizardController {
 		$content .= '<a href="#" title="' . $GLOBALS['LANG']->sL('LLL:EXT:lang/locallang_core.php:rm.closeDoc', TRUE) . '" onclick="window.close();return true;">' . \TYPO3\CMS\Backend\Utility\IconUtility::getSpriteIcon('actions-document-close') . '</a>';
 		$content .= $this->doc->spacer(10);
 		$content .= '
-		<table border="0" width="100%" height="100%" id="outer_container">
+		<table border="0" width="90%" height="90%" id="outer_container">
 			<tr>
 				<td class="editor_cell">
 					<div id="editor">
