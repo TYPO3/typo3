14067de391f452bda1bb3bab5bfe80e21f6bf169
Wouter Wolters <typo3@wouterwolters.nl>
2013-08-30 20:54:35 +0200

    [TASK] Remove old class files in ext:extensionmanager
    Remove old class files in ext:extensionmanager
    
    Change-Id: I2c5589b0feb37b8608eae6241e0f6c35d61e9a38
    Resolves: #51599
    Releases: 6.2
    Reviewed-on: https://review.typo3.org/23503
    Reviewed-by: Oliver Klee
    Reviewed-by: Kai Ole Hartwig
    Tested-by: Kai Ole Hartwig
    Reviewed-by: Christian Kuhn
    Tested-by: Christian Kuhn

--- typo3/sysext/extensionmanager/Classes/Service/Management.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*
- * @deprecated since 6.0, the classname Tx_Extensionmanager_Service_Management and this file is obsolete
- * and will be removed with 6.2. The class was renamed and is now located at:
- * typo3/sysext/extensionmanager/Classes/Service/ExtensionManagementService.php
- */
-require_once \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('extensionmanager') . 'Classes/Service/ExtensionManagementService.php';
-?>
