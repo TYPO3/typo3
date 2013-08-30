e78f34c475fe895ef6e27104f91bbf12ae8ace38
Wouter Wolters <typo3@wouterwolters.nl>
2013-08-30 19:34:20 +0200

    [TASK] Remove old class files in ext:sys_action
    Remove old class files in ext:sys_action
    
    Change-Id: I89941f8ace57e1b5470cc3038ffcc583a84eb1b4
    Resolves: #51595
    Releases: 6.2

--- typo3/sysext/sys_action/ext_autoload.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Register necessary class names with autoloader
-$extensionPath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('sys_action');
-return array(
-	'tx_sysaction_list' => $extensionPath . 'task/class.tx_sysaction_list.php',
-	'tx_sysaction_task' => $extensionPath . 'task/class.tx_sysaction_task.php'
-);
-?>
