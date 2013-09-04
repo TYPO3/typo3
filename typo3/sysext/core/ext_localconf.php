d4464b7dba4a76db14123fddddc843d351b7350f
Helmut Hummel <helmut.hummel@typo3.org>
2013-09-04 19:55:56 +0200

    [BUGFIX] Do not register storage factory slot during install
    In install tool context TYPO3_MODE is set to BE
    but there is no backend user initialized.
    Because of that the storage permission aspect
    fails as it requires a valid backend user object.
    
    Do not register the slot when in install tool context.
    
    Releases: 6.0, 6.1, 6.2
    Resolves: #51733
    Change-Id: I2da95ccd357b730b97bcf99bef21b18c04469274

--- typo3/sysext/core/ext_localconf.php
+++ typo3/sysext/core/ext_localconf.php
@@ -3,7 +3,7 @@ if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
 
-if (TYPO3_MODE === 'BE') {
+if (TYPO3_MODE === 'BE' && !(defined('TYPO3_enterInstallScript') && TYPO3_enterInstallScript)) {
 	\TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\SignalSlot\\Dispatcher')->connect(
 		'TYPO3\\CMS\\Core\\Resource\\ResourceFactory',
 		\TYPO3\CMS\Core\Resource\ResourceFactory::SIGNAL_PostProcessStorage,
