aec4ce344adf03b0bac56d079b368d9b97fd2139
Francois Suter <francois@typo3.org>
2013-09-05 15:06:12 +0200

    [FEATURE] Categories-based tt_content menu
    Provide a new menu/sitemap type for displaying list
    of categorized content elements. The default rendering
    simply provides links to the parent page with
    anchor pointing to the specific content element.
    
    Resolves: #52137
    Releases: 6.2
    
    Change-Id: I59c9eeb63148eaf12d8810fd7e64628e3f2d61aa

--- typo3/sysext/frontend/Classes/ContentObject/RecordsContentObject.php
+++ typo3/sysext/frontend/Classes/ContentObject/RecordsContentObject.php
@@ -27,13 +27,30 @@ namespace TYPO3\CMS\Frontend\ContentObject;
  *
  *  This copyright notice MUST APPEAR in all copies of the script!
  ***************************************************************/
+
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
 /**
  * Contains RECORDS class object.
  *
  * @author Xavier Perseguers <typo3@perseguers.ch>
  * @author Steffen Kamper <steffen@typo3.org>
  */
-class RecordsContentObject extends \TYPO3\CMS\Frontend\ContentObject\AbstractContentObject {
+class RecordsContentObject extends AbstractContentObject {
+
+	/**
+	 * List of all items with table and uid information
+	 *
+	 * @var array
+	 */
+	protected $itemArray = array();
+
+	/**
+	 * List of all selected records with full data, arranged per table
+	 *
+	 * @var array
+	 */
+	protected $data = array();
 
 	/**
 	 * Rendering the cObject, RECORDS
@@ -51,60 +68,66 @@ class RecordsContentObject extends \TYPO3\CMS\Frontend\ContentObject\AbstractCon
 		}
 		$tables = isset($conf['tables.']) ? $this->cObj->stdWrap($conf['tables'], $conf['tables.']) : $conf['tables'];
 		$source = isset($conf['source.']) ? $this->cObj->stdWrap($conf['source'], $conf['source.']) : $conf['source'];
-		if ($tables && $source) {
-			$allowedTables = $tables;
+		$categories = isset($conf['categories.']) ? $this->cObj->stdWrap($conf['categories'], $conf['categories.']) : $conf['categories'];
+
+		$tablesArray = array_unique(GeneralUtility::trimExplode(',', $tables, TRUE));
+		if ($tables) {
+			// Add tables which have a configuration (note that this may create duplicate entries)
 			if (is_array($conf['conf.'])) {
-				foreach ($conf['conf.'] as $k => $v) {
-					if (substr($k, -1) != '.') {
-						$allowedTables .= ',' . $k;
+				foreach ($conf['conf.'] as $key => $value) {
+					if (substr($key, -1) != '.' && !in_array($key, $tablesArray)) {
+						$tablesArray[] = $key;
 					}
 				}
 			}
-			/** @var \TYPO3\CMS\Core\Database\RelationHandler $loadDB*/
-			$loadDB = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Core\\Database\\RelationHandler');
-			$loadDB->setFetchAllFields(TRUE);
-			$loadDB->start($source, $allowedTables);
-			foreach ($loadDB->tableArray as $table => $v) {
-				if (is_array($GLOBALS['TCA'][$table])) {
-					$loadDB->additionalWhere[$table] = $this->cObj->enableFields($table);
-				}
+
+			// Get the data, depending on collection method.
+			// Property "source" is considered more precise and thus takes precedence over "categories"
+			if ($source) {
+				$this->collectRecordsFromSource($source, $tablesArray);
+			} elseif ($categories) {
+				$this->collectRecordsFromCategories($categories, $tablesArray);
 			}
-			$loadDB->getFromDB();
-			reset($loadDB->itemArray);
-			$data = $loadDB->results;
-			$cObj = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Frontend\\ContentObject\\ContentObjectRenderer');
-			$cObj->setParent($this->cObj->data, $this->cObj->currentRecord);
-			$this->cObj->currentRecordNumber = 0;
-			$this->cObj->currentRecordTotal = count($loadDB->itemArray);
-			foreach ($loadDB->itemArray as $val) {
-				$row = $data[$val['table']][$val['id']];
-				// Versioning preview:
-				$GLOBALS['TSFE']->sys_page->versionOL($val['table'], $row);
-				// Language overlay:
-				if (is_array($row) && $GLOBALS['TSFE']->sys_language_contentOL) {
-					if ($val['table'] === 'pages') {
-						$row = $GLOBALS['TSFE']->sys_page->getPageOverlay($row);
-					} else {
-						$row = $GLOBALS['TSFE']->sys_page->getRecordOverlay($val['table'], $row, $GLOBALS['TSFE']->sys_language_content, $GLOBALS['TSFE']->sys_language_contentOL);
+
+			if (count($this->itemArray) > 0) {
+				/** @var \TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer $cObj */
+				$cObj = GeneralUtility::makeInstance('TYPO3\\CMS\\Frontend\\ContentObject\\ContentObjectRenderer');
+				$cObj->setParent($this->cObj->data, $this->cObj->currentRecord);
+				$this->cObj->currentRecordNumber = 0;
+				$this->cObj->currentRecordTotal = count($this->itemArray);
+				foreach ($this->itemArray as $val) {
+					$row = $this->data[$val['table']][$val['id']];
+					// Perform overlays if necessary (records coming from category collections are already overlaid)
+					if ($source) {
+						// Versioning preview
+						$GLOBALS['TSFE']->sys_page->versionOL($val['table'], $row);
+						// Language overlay
+						if (is_array($row) && $GLOBALS['TSFE']->sys_language_contentOL) {
+							if ($val['table'] === 'pages') {
+								$row = $GLOBALS['TSFE']->sys_page->getPageOverlay($row);
+							} else {
+								$row = $GLOBALS['TSFE']->sys_page->getRecordOverlay($val['table'], $row, $GLOBALS['TSFE']->sys_language_content, $GLOBALS['TSFE']->sys_language_contentOL);
+							}
+						}
 					}
-				}
-				// Might be unset in the content overlay things...
-				if (is_array($row)) {
-					$dontCheckPid = isset($conf['dontCheckPid.']) ? $this->cObj->stdWrap($conf['dontCheckPid'], $conf['dontCheckPid.']) : $conf['dontCheckPid'];
-					if (!$dontCheckPid) {
-						$row = $this->cObj->checkPid($row['pid']) ? $row : '';
-					}
-					if ($row && !$GLOBALS['TSFE']->recordRegister[($val['table'] . ':' . $val['id'])]) {
-						$renderObjName = $conf['conf.'][$val['table']] ? $conf['conf.'][$val['table']] : '<' . $val['table'];
-						$renderObjKey = $conf['conf.'][$val['table']] ? 'conf.' . $val['table'] : '';
-						$renderObjConf = $conf['conf.'][$val['table'] . '.'];
-						$this->cObj->currentRecordNumber++;
-						$cObj->parentRecordNumber = $this->cObj->currentRecordNumber;
-						$GLOBALS['TSFE']->currentRecord = $val['table'] . ':' . $val['id'];
-						$this->cObj->lastChanged($row['tstamp']);
-						$cObj->start($row, $val['table']);
-						$tmpValue = $cObj->cObjGetSingle($renderObjName, $renderObjConf, $renderObjKey);
-						$theValue .= $tmpValue;
+					// Might be unset during the overlay process
+					if (is_array($row)) {
+						$dontCheckPid = isset($conf['dontCheckPid.']) ? $this->cObj->stdWrap($conf['dontCheckPid'], $conf['dontCheckPid.']) : $conf['dontCheckPid'];
+						if (!$dontCheckPid) {
+							$row = $this->cObj->checkPid($row['pid']) ? $row : '';
+						}
+						if ($row && !$GLOBALS['TSFE']->recordRegister[($val['table'] . ':' . $val['id'])]) {
+							$renderObjName = $conf['conf.'][$val['table']] ? $conf['conf.'][$val['table']] : '<' . $val['table'];
+							$renderObjKey = $conf['conf.'][$val['table']] ? 'conf.' . $val['table'] : '';
+							$renderObjConf = $conf['conf.'][$val['table'] . '.'];
+							$this->cObj->currentRecordNumber++;
+							$cObj->parentRecordNumber = $this->cObj->currentRecordNumber;
+							$GLOBALS['TSFE']->currentRecord = $val['table'] . ':' . $val['id'];
+							$this->cObj->lastChanged($row['tstamp']);
+							$cObj->start($row, $val['table']);
+							$tmpValue = $cObj->cObjGetSingle($renderObjName, $renderObjConf, $renderObjKey);
+							$theValue .= $tmpValue;
+						}
 					}
 				}
 			}
@@ -121,7 +144,99 @@ class RecordsContentObject extends \TYPO3\CMS\Frontend\ContentObject\AbstractCon
 		return $theValue;
 	}
 
-}
+	/**
+	 * Collects records according to the configured source
+	 *
+	 * @param string $source Source of records
+	 * @param array $tables List of tables
+	 * @return void
+	 */
+	protected function collectRecordsFromSource($source, $tables) {
+		/** @var \TYPO3\CMS\Core\Database\RelationHandler $loadDB*/
+		$loadDB = GeneralUtility::makeInstance('TYPO3\\CMS\\Core\\Database\\RelationHandler');
+		$loadDB->setFetchAllFields(TRUE);
+		$loadDB->start($source, implode(',', $tables));
+		foreach ($loadDB->tableArray as $table => $v) {
+			if (is_array($GLOBALS['TCA'][$table])) {
+				$loadDB->additionalWhere[$table] = $this->cObj->enableFields($table);
+			}
+		}
+		$loadDB->getFromDB();
+		reset($loadDB->itemArray);
+		$this->itemArray = $loadDB->itemArray;
+		$this->data = $loadDB->results;
+	}
 
+	/**
+	 * Collects records for all selected tables and categories.
+	 *
+	 * @param string $selectedCategories Comma-separated list of categories
+	 * @param array $tables List of tables
+	 * @return void
+	 */
+	protected function collectRecordsFromCategories($selectedCategories, $tables) {
+		$selectedCategories = array_unique(GeneralUtility::intExplode(',', $selectedCategories, TRUE));
+
+		// Loop on all selected tables
+		foreach ($tables as $table) {
+
+			// Get the records for each selected category
+			$tableRecords = array();
+			$categoriesPerRecord = array();
+			foreach ($selectedCategories as $aCategory) {
+				try {
+					$collection = \TYPO3\CMS\Frontend\Category\Collection\CategoryCollection::load(
+						$aCategory,
+						TRUE,
+						$table
+					);
+					if ($collection->count() > 0) {
+						// Add items to the collection of records for the current table
+						foreach ($collection as $item) {
+							$tableRecords[$item['uid']] = $item;
+							// Keep track of all categories a given item belongs to
+							if (!isset($categoriesPerRecord[$item['uid']])) {
+								$categoriesPerRecord[$item['uid']] = array();
+							}
+							$categoriesPerRecord[$item['uid']][] = $aCategory;
+						}
+					}
+				}
+				catch (\Exception $e) {
+					$message = sprintf(
+						'Could not get records for category id %d. Error: %s (%d)',
+						$aCategory,
+						$e->getMessage(),
+						$e->getCode()
+					);
+					$this->getTimeTracker()->setTSlogMessage($message, 2);
+				}
+			}
+			// Store the resulting records into the itemArray and data results array
+			if (count($tableRecords) > 0) {
+				$this->data[$table] = array();
+				foreach ($tableRecords as $record) {
+					$this->itemArray[] = array(
+						'id' => $record['uid'],
+						'table' => $table
+					);
+					// Add to the record the categories it belongs to
+					$record['_categories'] = implode(',', $categoriesPerRecord[$record['uid']]);
+					$this->data[$table][$record['uid']] = $record;
+				}
+			}
+
+		}
+	}
+
+	/**
+	 * Wrapper around the $GLOBALS['TT'] variable
+	 *
+	 * @return \TYPO3\CMS\Core\TimeTracker\TimeTracker
+	 */
+	protected function getTimeTracker() {
+		return $GLOBALS['TT'];
+	}
+}
 
 ?>
