51a482bf8ae29121e1252453b517f791e3b81b54
arjenhoekema <arjenhoekema@gmail.com>
2013-09-05 09:49:11 +0200

    [BUGFIX] Extbase scheduler task serialization
    When using Command Controllers as scheduled tasks the Extbase
    Scheduled Task is serialized. Some singleton properties like
    'objectManager', 'commandManager' and 'taskExecutor' are serialized
    leading to duplicates of this objects.
    
    Added magic "__sleep" and "__wakeup" methods destructing and
    re-constructing the above objects.
    
    Resolves: #50723
    Releases: 6.2, 6.1, 6.0
    Change-Id: I0a84487a1d8885f75dd4b4eaf004bb0dc93645c2

--- typo3/sysext/extbase/Classes/Scheduler/Task.php
+++ typo3/sysext/extbase/Classes/Scheduler/Task.php
@@ -59,6 +59,40 @@ class Task extends \TYPO3\CMS\Scheduler\Task\AbstractTask {
 	protected $taskExecutor;
 
 	/**
+	 * Intanciates the Object Manager
+	 */
+	public function __construct() {
+		parent::__construct();
+		$this->objectManager = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\Object\\ObjectManager');
+		$this->commandManager = $this->objectManager->get('TYPO3\\CMS\\Extbase\\Mvc\\Cli\\CommandManager');
+		$this->taskExecutor = $this->objectManager->get('TYPO3\\CMS\\Extbase\\Scheduler\\TaskExecutor');
+	}
+
+	/**
+	 * Sleep
+	 *
+	 * @return array Properties to serialize
+	 */
+	public function __sleep() {
+		$properties = get_object_vars($this);
+		unset($properties['commandManager']);
+		unset($properties['objectManager']);
+		unset($properties['taskExecutor']);
+		return array_keys($properties);
+	}
+
+	/**
+	 * Wakeup
+	 *
+	 * @return void
+	 */
+	public function __wakeup() {
+		$this->objectManager = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\Object\\ObjectManager');
+		$this->commandManager = $this->objectManager->get('TYPO3\\CMS\\Extbase\\Mvc\\Cli\\CommandManager');
+		$this->taskExecutor = $this->objectManager->get('TYPO3\\CMS\\Extbase\\Scheduler\\TaskExecutor');
+	}
+
+	/**
 	 * Function execute from the Scheduler
 	 *
 	 * @return boolean TRUE on successful execution, FALSE on error
