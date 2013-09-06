40cb0a435e31dd0378151ce6613ad54c8e7d075b
Georg Ringer <georg.ringer@gmail.com>
2013-09-06 10:00:04 +0200

    [TASK] Use a 401 header if login is not successful
    If login is not correct, a 401 should be used instead of a 200.
    
    Change-Id: Ia2fa139e89fe19df77bb0530b4fbce502506f524
    Resolves: #51803
    Releases: 6.2,6.1,6.0,4.5
    Reviewed-on: https://review.typo3.org/23801
    Reviewed-by: Markus Klein
    Tested-by: Markus Klein

--- typo3/sysext/core/Classes/Authentication/AbstractUserAuthentication.php
+++ typo3/sysext/core/Classes/Authentication/AbstractUserAuthentication.php
@@ -1,6 +1,34 @@
 <?php
 namespace TYPO3\CMS\Core\Authentication;
 
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 1999-2013 Kasper Skårhøj (kasperYYYY@typo3.com)
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *  A copy is found in the textfile GPL.txt and important notices to the license
+ *  from the author is found in LICENSE.txt distributed with these scripts.
+ *
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+use TYPO3\CMS\Core\Utility\HttpUtility;
+
 /**
  * Authentication of users in TYPO3
  *
@@ -775,10 +803,11 @@ abstract class AbstractUserAuthentication {
 						// strip port from server
 						$server = str_replace($sslPortSuffix, '', $server);
 					}
-					\TYPO3\CMS\Core\Utility\HttpUtility::redirect('http://' . $server . '/' . $address . TYPO3_mainDir . $backendScript);
+					HttpUtility::redirect('http://' . $server . '/' . $address . TYPO3_mainDir . $backendScript);
 				}
 			}
 		} elseif ($activeLogin || count($tempuserArr)) {
+			HttpUtility::setResponseCode(HttpUtility::HTTP_STATUS_401);
 			$this->loginFailure = TRUE;
 			if ($this->writeDevLog && !count($tempuserArr) && $activeLogin) {
 				\TYPO3\CMS\Core\Utility\GeneralUtility::devLog('Login failed: ' . \TYPO3\CMS\Core\Utility\GeneralUtility::arrayToLogString($loginData), 'TYPO3\\CMS\\Core\\Authentication\\AbstractUserAuthentication', 2);
