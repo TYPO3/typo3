91d723d8286ed4fe1f9f80c2d7dc8cded6eebc24
Wouter Wolters <typo3@wouterwolters.nl>
2013-09-09 22:16:44 +0200

    [BUGFIX] Fix missing translation in ext:reports
    Fix missing translation for ext:sv in ext:reports.
    This was not trasfered to the new location for
    the locallang.xlf file.
    
    Change-Id: I594d9c89afbc42c5b6447fc2306a88d3c027b40e
    Resolves: #51873
    Releases: 6.2
    Reviewed-on: https://review.typo3.org/23711
    Reviewed-by: Stefan Neufeind
    Tested-by: Stefan Neufeind

--- typo3/sysext/sv/ext_tables.php
+++ typo3/sysext/sv/ext_tables.php
@@ -4,8 +4,8 @@ if (!defined('TYPO3_MODE')) {
 }
 if (TYPO3_MODE === 'BE') {
 	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['reports']['sv']['services'] = array(
-		'title' => 'LLL:EXT:sv/reports/locallang.xlf:report_title',
-		'description' => 'LLL:EXT:sv/reports/locallang.xlf:report_description',
+		'title' => 'LLL:EXT:sv/Resources/Private/Language/locallang.xlf:report_title',
+		'description' => 'LLL:EXT:sv/Resources/Private/Language/locallang.xlf:report_description',
 		'icon' => 'EXT:sv/Resources/Public/Images/tx_sv_report.png',
 		'report' => 'TYPO3\\CMS\\Sv\\Report\\ServicesListReport'
 	);
