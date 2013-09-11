9668df7e61af99038296f34e716ba6fbc2451612
Timo Webler <timo.webler@dkd.de>
2013-09-11 15:43:25 +0200

    [BUGFIX] Consider placeholders in fixVersioningPid
    Method fixVersioningPid does not analyze the new location for an element.
    Check for a move placeholder to get the new location.
    Same behavior as in frontend.
    See \TYPO3\CMS\Frontend\Page\PageRepository::fixVersioningPid
    
    Resolves: #51913
    Releases: master, 6.2
    Change-Id: I08a8eab714ec1530f3c655f0440c98342120e328
    Reviewed-on: http://review.typo3.org/23747
    Reviewed-by: Tomas Norre Mikkelsen <tomasnorre@gmail.com>
    Reviewed-by: Benni Mack <benni@typo3.org>
    Reviewed-by: Daniel Goerz <ervaude@gmail.com>
    Tested-by: Daniel Goerz <ervaude@gmail.com>
    Reviewed-by: Stefan Neufeind <typo3.neufeind@speedpartner.de>
    Tested-by: Stefan Neufeind <typo3.neufeind@speedpartner.de>

--- typo3/sysext/backend/Classes/Utility/BackendUtility.php
+++ typo3/sysext/backend/Classes/Utility/BackendUtility.php
@@ -4056,6 +4056,17 @@ class BackendUtility {
 					$rr['_ORIG_pid'] = $rr['pid'];
 					$rr['pid'] = $oidRec['pid'];
 				}
+				// Use target PID in case of move pointer
+				if (
+					!isset($rr['t3ver_state'])
+					|| VersionState::cast($rr['t3ver_state'])->equals(VersionState::MOVE_POINTER)
+				) {
+					$movePlaceholder = self::getMovePlaceholder($table, $oid, 'pid');
+					if ($movePlaceholder) {
+						$rr['_ORIG_pid'] = $rr['pid'];
+						$rr['pid'] = $movePlaceholder['pid'];
+					}
+				}
 			}
 		}
 	}
