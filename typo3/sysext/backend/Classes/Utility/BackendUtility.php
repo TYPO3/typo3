20da11a0c9a9082f7882edb03bcb4a6e794bda7d
Tim Lochmueller <tim@fruit-lab.de>
2013-09-11 14:54:29 +0200

    [BUGFIX] No RTE Transformation for subtype_value_field-fields of Plugins
    The RTE transformation depends on the result of BackendUtility
    getTCAtypes. If the RTE field is in the subtypes_addlist of a plugin
    the transformation is not performed. The patch add the related
    subtype fields to the return value.
    
    Resolves: #51901
    Releases: 6.2, 6.1, 6.0
    Change-Id: I8bba3d03b5187b06fec5a9b5a0e151466d814d60

--- typo3/sysext/backend/Classes/Utility/BackendUtility.php
+++ typo3/sysext/backend/Classes/Utility/BackendUtility.php
@@ -717,6 +717,17 @@ class BackendUtility {
 			$typesConf = $GLOBALS['TCA'][$table]['types'][$fieldValue];
 			// Get fields list and traverse it
 			$fieldList = explode(',', $typesConf['showitem']);
+
+			// Add subtype fields for a valid RTE transformation
+			// The RTE run the DB -> RTE transformation only, if the RTE field ist part of the getTCAtypes array
+			if (isset($typesConf['subtype_value_field'])) {
+				$subType = $rec[$typesConf['subtype_value_field']];
+				if (isset($typesConf['subtypes_addlist'][$subType])) {
+					$subFields = \TYPO3\CMS\Core\Utility\GeneralUtility::trimExplode(',', $typesConf['subtypes_addlist'][$subType], TRUE);
+					$fieldList = array_merge($fieldList, $subFields);
+				}
+			}
+
 			$altFieldList = array();
 			// Traverse fields in types config and parse the configuration into a nice array:
 			foreach ($fieldList as $k => $v) {
