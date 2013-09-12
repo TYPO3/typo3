5e1f2fc3b644f88cbfd8326bce7631b8cb27837c
TYPO3 Release Team <typo3v4@typo3.org>
2013-09-12 11:11:31 +0200

    [RELEASE] Release of TYPO3 6.0.10
    Change-Id: I0f9c4e958d535b7273f327b183666b8ed7b28694
    Reviewed-on: https://review.typo3.org/23775
    Reviewed-by: TYPO3 Release Team
    Tested-by: TYPO3 Release Team

--- typo3/sysext/core/Classes/Core/SystemEnvironmentBuilder.php
+++ typo3/sysext/core/Classes/Core/SystemEnvironmentBuilder.php
@@ -114,7 +114,7 @@ class SystemEnvironmentBuilder {
 	 */
 	static protected function defineBaseConstants() {
 		// This version, branch and copyright
-		define('TYPO3_version', '6.0.10-dev');
+		define('TYPO3_version', '6.0.10');
 		define('TYPO3_branch', '6.0');
 		define('TYPO3_copyright_year', '1998-2013');
 
