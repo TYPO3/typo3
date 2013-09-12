e786c9c3887c2c9bb1ed5941cf093a9fefe5a818
TYPO3 Release Team <typo3v4@typo3.org>
2013-09-12 11:18:55 +0200

    [TASK] Set TYPO3 version to 4.7.16-dev
    Change-Id: I9339c2f3f95de90f91240f0bff03296c7d3da2f4
    Reviewed-on: https://review.typo3.org/23778
    Reviewed-by: TYPO3 Release Team
    Tested-by: TYPO3 Release Team

--- t3lib/config_default.php
+++ t3lib/config_default.php
@@ -727,7 +727,7 @@ if (TYPO3_MODE === 'BE') {
 $T3_VAR = array();	// Initialize.
 
 	// TYPO3 version
-$TYPO_VERSION = '4.7.15';	// @deprecated: Will be removed in TYPO3 6.0. Use the constants defined below
+$TYPO_VERSION = '4.7.16-dev';	// @deprecated: Will be removed in TYPO3 6.0. Use the constants defined below
 define('TYPO3_version', $TYPO_VERSION);
 define('TYPO3_branch', '4.7');
 define('TYPO3_copyright_year', '1998-2013');
