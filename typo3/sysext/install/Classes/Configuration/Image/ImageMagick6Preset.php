d05fcff51d5cf3950c0430515648f250c7dbfc2b
Christian Kuhn <lolli@schwarzbu.ch>
2013-09-15 15:50:08 +0200

    [BUGFIX] Install tool: Use sRGB colorspace in presets
    Image Magick versions 6.7 and above need sRGB as colorspace,
    otherwise images are too dark. This also works for older versions
    like 6.6. Use sRGB as default is image configuration presets.
    
    Change-Id: I7c50ee932b1335892eb228b10fc00136cfc54527
    Resolves: #52017
    Releases: 6.2
    Reviewed-on: https://review.typo3.org/23840
    Reviewed-by: Steffen Müller
    Tested-by: Steffen Müller
    Reviewed-by: Wouter Wolters
    Tested-by: Wouter Wolters

--- typo3/sysext/install/Classes/Configuration/Image/ImageMagick6Preset.php
+++ typo3/sysext/install/Classes/Configuration/Image/ImageMagick6Preset.php
@@ -57,7 +57,7 @@ class ImageMagick6Preset extends AbstractImagePreset implements Configuration\Pr
 		'GFX/im_v5effects' => 1,
 		'GFX/im_mask_temp_ext_gif' => 1,
 		'GFX/im_combine_filename' => 'composite',
-		'GFX/colorspace' => 'RGB',
+		'GFX/colorspace' => 'sRGB',
 	);
 
 	/**
