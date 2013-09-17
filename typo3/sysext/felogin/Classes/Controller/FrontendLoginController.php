1294fe753c48cc4258d0cf3c3a321cf7e847812c
Wouter Wolters <typo3@wouterwolters.nl>
2013-09-17 22:47:24 +0200

    [BUGFIX] felogin: Unknown modifier in regular expression
    A regular expression in FrontendLoginController
    contains an unknown modifier. Fix it by replacing the
    / to # at the beginning and the end of the regular
    expression.
    
    Resolves: #52059
    Releases: 6.2, 6.1, 6.0
    Change-Id: Id4d3439c1cdbec691d977570bf76ba0c7bad493c
    Reviewed-on: https://review.typo3.org/26474
    Reviewed-by: Markus Klein
    Tested-by: Markus Klein

--- typo3/sysext/felogin/Classes/Controller/FrontendLoginController.php
+++ typo3/sysext/felogin/Classes/Controller/FrontendLoginController.php
@@ -590,7 +590,7 @@ class FrontendLoginController extends \TYPO3\CMS\Frontend\Plugin\AbstractPlugin 
 							$url = $this->referer;
 							// Is referring url allowed to redirect?
 							$match = array();
-							if (preg_match('/^http://([[:alnum:]._-]+)//', $url, $match)) {
+							if (preg_match('#^http://([[:alnum:]._-]+)/#', $url, $match)) {
 								$redirect_domain = $match[1];
 								$found = FALSE;
 								foreach (\TYPO3\CMS\Core\Utility\GeneralUtility::trimExplode(',', $this->conf['domains'], TRUE) as $d) {
@@ -924,6 +924,5 @@ class FrontendLoginController extends \TYPO3\CMS\Frontend\Plugin\AbstractPlugin 
 	}
 
 }
-
 
 ?>
