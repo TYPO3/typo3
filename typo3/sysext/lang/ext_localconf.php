90a072e4ecd9f27a9f3590c782de6d28d71541d2
Christian Kuhn <lolli@schwarzbu.ch>
2013-09-26 15:00:08 +0200

    [TASK] Use SimpleFileBackend for t3lib_l10n cache
    The language cache by default uses the FileBackend to store its data.
    Language cache entries need to be deleted only if new extensions are
    loaded and if new language overlays are fetched. They do not need
    tagging and can have an unlimited lifetime.
    Switching to SimpleFileBackend removes the tagging and sets unlimited
    lifetime by to reduce read and write load on this cache.
    
    Change-Id: I5c4778f4c38ae369b6873574e961fa65208d77a1
    Resolves: #52295
    Related: #52125
    Releases: 6.2, 6.1, 6.0

--- typo3/sysext/lang/ext_localconf.php
+++ typo3/sysext/lang/ext_localconf.php
@@ -2,13 +2,7 @@
 if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
-// Register cache t3lib_l10n
-if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['t3lib_l10n'])) {
-	$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['t3lib_l10n'] = array();
-}
-if (!isset($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['t3lib_l10n']['backend'])) {
-	$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['t3lib_l10n']['backend'] = 'TYPO3\CMS\Core\Cache\Backend\FileBackend';
-}
+
 if (isset($GLOBALS['TYPO3_CONF_VARS']['SYS']['lang']['cache']['clear_menu']) && $GLOBALS['TYPO3_CONF_VARS']['SYS']['lang']['cache']['clear_menu']) {
 	// Register Clear Cache Menu hook
 	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['additionalBackendItems']['cacheActions']['clearLangCache'] = 'TYPO3\CMS\Lang\ClearLanguageCacheMenuItem';
