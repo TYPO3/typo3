1ef880a5c69ae71f50c4e2222000aa15e9db680e
Christian Kuhn <lolli@schwarzbu.ch>
2013-09-26 18:46:29 +0200

    [TASK] Remove "Clear language cache" menu item
    * To show the item, SYS/lang/cache/clear_menu has to be manually
    enabled, and a (not documented) userTSconfig option must be set.
    * The item *never* worked since the underlying LanguageCacheClearer
    class expects an array with cacheCmd set to either "all" or
    "temp_cached", that is not set in the according AJAX call of the
    menu action.
    * Class LanguageCacheClearer hooks into DataHandler and is only
    triggered on clear "all" cache.
    * Clearing "all" cache flushes all caches anyway, so the hook
    currently triggers flushing the language cache a second time.
    * SYS/lang/cache/lifetime is unused and can be removed.
    
    Since the functionality is broken, not documented, obsolete and
    "internal" code of ext:lang, the whole thing is removed without
    further deprecation.
    
    Change-Id: Ic0d19bf31109832c9ed88d81d883516c109cf47e
    Resolves: #52312
    Releases: 6.2

--- typo3/sysext/lang/ext_localconf.php
+++ typo3/sysext/lang/ext_localconf.php
@@ -3,16 +3,6 @@ if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
 
-if (isset($GLOBALS['TYPO3_CONF_VARS']['SYS']['lang']['cache']['clear_menu']) && $GLOBALS['TYPO3_CONF_VARS']['SYS']['lang']['cache']['clear_menu']) {
-	// Register Clear Cache Menu hook
-	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['additionalBackendItems']['cacheActions']['clearLangCache'] = 'TYPO3\CMS\Lang\ClearLanguageCacheMenuItem';
-} else {
-	// Clear l10n cache when the user clears all caches
-	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearCachePostProc']['clearLangCache'] = 'TYPO3\CMS\Lang\LanguageCacheClearer->clearCache';
-}
-// Register Ajax call
-$GLOBALS['TYPO3_CONF_VARS']['BE']['AJAX']['lang::clearCache'] = 'TYPO3\CMS\Lang\LanguageCacheClearer->clearCache';
-
 // Register language update command controller
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = 'TYPO3\\CMS\\Lang\\Command\\UpdateCommandController';
 ?>
