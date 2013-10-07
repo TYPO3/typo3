31e44bdbc70d13d07f7d9aecf0e1338e861b18ce
Marc Bastian Heinrichs <typo3@mbh-software.de>
2013-10-07 13:00:05 +0200

    [BUGFIX] Fix namespace in FileMountRepositoryTest
    Currently a "Tests\Unit\" is missing in between.
    
    Resolves: #46845
    Releases: 6.1, 6.0
    Change-Id: I2cb4f7fedf826ff35b66ecd4ea600bc60b41123b
    Reviewed-on: https://review.typo3.org/24450
    Reviewed-by: Christian Kuhn
    Tested-by: Christian Kuhn

--- typo3/sysext/extbase/Tests/Unit/Domain/Repository/FileMountRepositoryTest.php
+++ typo3/sysext/extbase/Tests/Unit/Domain/Repository/FileMountRepositoryTest.php
@@ -1,5 +1,5 @@
 <?php
-namespace TYPO3\CMS\Extbase\Domain\Repository;
+namespace TYPO3\CMS\Extbase\Tests\Unit\Domain\Repository;
 
 /***************************************************************
  * Copyright notice
