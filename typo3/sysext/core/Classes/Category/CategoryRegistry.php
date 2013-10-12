4eb44cd81c0c436666e53c56bf50f431e5fb8dfe
Frans Saris <franssaris@gmail.com>
2013-10-12 23:19:17 +0200

    [TASK] Remove add/edit wizards for defaultCategorizedTables
    Removed the add/edit wizards in default TCA column
    configuration for the SYS.defaultCategorizedTables.
    
    Resolves: #52699
    Releases: 6.2
    Change-Id: I7b08115c0ab0b2e2d58968a398d03e018bda3d78
    Reviewed-on: https://review.typo3.org/24663
    Reviewed-by: Francois Suter
    Tested-by: Francois Suter
    Reviewed-by: Wouter Wolters
    Tested-by: Wouter Wolters

--- typo3/sysext/core/Classes/Category/CategoryRegistry.php
+++ typo3/sysext/core/Classes/Category/CategoryRegistry.php
@@ -315,29 +315,6 @@ class CategoryRegistry implements \TYPO3\CMS\Core\SingletonInterface {
 						'showHeader' => TRUE,
 					),
 				),
-				'wizards' => array(
-					'_PADDING' => 1,
-					'_VERTICAL' => 1,
-					'edit' => array(
-						'type' => 'popup',
-						'title' => 'Edit',
-						'script' => 'wizard_edit.php',
-						'icon' => 'edit2.gif',
-						'popup_onlyOpenIfSelected' => 1,
-						'JSopenParams' => 'height=350,width=580,status=0,menubar=0,scrollbars=1',
-					),
-					'add' => Array(
-						'type' => 'script',
-						'title' => 'Create new',
-						'icon' => 'add.gif',
-						'params' => array(
-							'table' => 'sys_category',
-							'pid' => '###CURRENT_PID###',
-							'setValue' => 'prepend'
-						),
-						'script' => 'wizard_add.php',
-					),
-				),
 			);
 
 			if (!empty($options['fieldConfiguration'])) {
