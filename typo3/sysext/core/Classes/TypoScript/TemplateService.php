27a224bb22c96f59cf3fc72d1d13e4584729dd11
Tomita Militaru <militarutomita@gmail.com>
2013-11-11 22:25:29 +0200

    [BUGFIX] IMAGE content object accepts directories
    Adds checks for file existence to avoid returning directory
    paths instead of path to file.
    
    Resolves: #51781
    Releases: master, 6.2
    Change-Id: I8f879f38b95e6d9562a8883d35664550d8fa8774
    Reviewed-on: http://review.typo3.org/25286
    Reviewed-by: Wouter Wolters <typo3@wouterwolters.nl>
    Tested-by: Wouter Wolters <typo3@wouterwolters.nl>
    Reviewed-by: Stefan Neufeind <typo3.neufeind@speedpartner.de>
    Reviewed-by: Markus Klein <klein.t3@reelworx.at>
    Tested-by: Markus Klein <klein.t3@reelworx.at>

--- typo3/sysext/core/Classes/TypoScript/TemplateService.php
+++ typo3/sysext/core/Classes/TypoScript/TemplateService.php
@@ -1127,7 +1127,7 @@ class TemplateService {
 
 		// if this is an URL, it can be returned directly
 		$urlScheme = parse_url($file, PHP_URL_SCHEME);
-		if ($urlScheme === 'https' || $urlScheme === 'http') {
+		if ($urlScheme === 'https' || $urlScheme === 'http' || is_file(PATH_site . $file)) {
 			return $file;
 		}
 
