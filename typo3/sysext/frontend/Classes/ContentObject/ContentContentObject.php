d2881f5f31bacdd33480de8791b087b8722feead
Markus Klein <klein.t3@mfc-linz.at>
2013-11-19 18:36:53 +0100

    [BUGFIX] Allow to render the same TS object twice
    CONTENT/RECORDS elements prevent to render the same object twice.
    
    Resolves: #53768
    Resolves: #28745
    Releases: 6.2, 6.1, 6.0
    Change-Id: I30750f2dc848521999c3734129439d7f6f90aae1
    (cherry picked from commit 1b9d3a59f82b290ae16073e1df5f83fa48363db8)
    Reviewed-on: https://review.typo3.org/27516
    Reviewed-by: Anja Leichsenring
    Tested-by: Anja Leichsenring

--- typo3/sysext/frontend/Classes/ContentObject/ContentContentObject.php
+++ typo3/sysext/frontend/Classes/ContentObject/ContentContentObject.php
@@ -47,7 +47,7 @@ class ContentContentObject extends \TYPO3\CMS\Frontend\ContentObject\AbstractCon
 		// If the currentRecord is set, we register, that this record has invoked this function.
 		// It's should not be allowed to do this again then!!
 		if ($originalRec) {
-			$GLOBALS['TSFE']->recordRegister[$originalRec]++;
+			++$GLOBALS['TSFE']->recordRegister[$originalRec];
 		}
 		$conf['table'] = isset($conf['table.']) ? trim($this->cObj->stdWrap($conf['table'], $conf['table.'])) : trim($conf['table']);
 		$tablePrefix = \TYPO3\CMS\Core\Utility\GeneralUtility::trimExplode('_', $conf['table'], TRUE);
@@ -150,6 +150,9 @@ class ContentContentObject extends \TYPO3\CMS\Frontend\ContentObject\AbstractCon
 		}
 		// Restore
 		$GLOBALS['TSFE']->currentRecord = $originalRec;
+		if ($originalRec) {
+			--$GLOBALS['TSFE']->recordRegister[$originalRec];
+		}
 		return $theValue;
 	}
 
