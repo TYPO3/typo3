d9e65462ae81c18889eb9e8438fab2f89df7a2f4
Ernesto Baschny <ernst@cron-it.de>
2014-01-28 12:15:10 +0100

    [TASK] Updates prototype and scriptaculous, fixing IE9+ issues
    Upgrades prototype from 1.6.0.3 to 1.7.1 and scriptaculous
    from 1.8.2 to 1.9.0.
    
    Solves the problem with sorting IRRE elements in IE9+, for example.
    
    Resolves: #51768
    Releases: 6.2, 6.1, 6.0, 4.7, 4.5
    Change-Id: I5ea11b2e926ae0f23d1c6d85a0ff5ba24995eebb
    Reviewed-on: https://review.typo3.org/29472
    Reviewed-by: Markus Klein
    Tested-by: Markus Klein
    Reviewed-by: Stefan Neufeind
    Tested-by: Stefan Neufeind

--- typo3/contrib/scriptaculous/sound.js
+++ typo3/contrib/scriptaculous/sound.js
@@ -1,6 +1,6 @@
-// script.aculo.us sound.js v1.8.2, Tue Nov 18 18:30:58 +0100 2008
+// script.aculo.us sound.js v1.9.0, Thu Dec 23 16:54:48 -0500 2010
 
-// Copyright (c) 2005-2008 Thomas Fuchs (http://script.aculo.us, http://mir.aculo.us)
+// Copyright (c) 2005-2010 Thomas Fuchs (http://script.aculo.us, http://mir.aculo.us)
 //
 // Based on code created by Jules Gravinese (http://www.webveteran.com/)
 //
@@ -50,6 +50,10 @@ Sound = {
 if(Prototype.Browser.Gecko && navigator.userAgent.indexOf("Win") > 0){
   if(navigator.plugins && $A(navigator.plugins).detect(function(p){ return p.name.indexOf('QuickTime') != -1 }))
     Sound.template = new Template('<object id="sound_#{track}_#{id}" width="0" height="0" type="audio/mpeg" data="#{url}"/>');
+  else if(navigator.plugins && $A(navigator.plugins).detect(function(p){ return p.name.indexOf('Windows Media') != -1 }))
+    Sound.template = new Template('<object id="sound_#{track}_#{id}" type="application/x-mplayer2" data="#{url}"></object>');
+  else if(navigator.plugins && $A(navigator.plugins).detect(function(p){ return p.name.indexOf('RealPlayer') != -1 }))
+    Sound.template = new Template('<embed type="audio/x-pn-realaudio-plugin" style="height:0" id="sound_#{track}_#{id}" src="#{url}" loop="false" autostart="true" hidden="true"/>');
   else
     Sound.play = function(){};
-}
+} 
