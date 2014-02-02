93fa77f6122f17ecb136f9c686a8931d86f7cb21
Felix Kopp <felix-source@phorax.com>
2014-02-02 11:37:18 +0100

    [FEATURE] styleguide: Add preview: Buttons with icon

---
+++ typo3/sysext/styleguide/Classes/ViewHelpers/SpriteManagerIconViewHelper.php
@@ -0,0 +1,47 @@
+<?php
+namespace TYPO3\CMS\Styleguide\ViewHelpers;
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2012-2013 Felix Kopp <felix-source@phorax.com>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *  A copy is found in the text file GPL.txt and important notices to the license
+ *  from the author is found in LICENSE.txt distributed with these scripts.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * Displays sprite icon identified by iconName key
+ *
+ * @author Felix Kopp <felix-source@phorax.com>
+ */
+class SpriteManagerIconViewHelper extends \TYPO3\CMS\Fluid\ViewHelpers\Be\AbstractBackendViewHelper {
+
+	/**
+	 * Prints sprite icon html for $iconName key
+	 *
+	 * @param string $iconName
+	 * @param array $options
+	 * @return string
+	 */
+	public function render($iconName, $options = array()) {
+		return \TYPO3\CMS\Backend\Utility\IconUtility::getSpriteIcon($iconName, $options);
+	}
+
+}
