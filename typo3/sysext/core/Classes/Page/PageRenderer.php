bb56fba04bb6a77dbfafd7629a07fb49e037f407
Stanislas Rolland <typo3@sjbr.ca>
2014-02-13 16:20:49 -0500

    [BUGFIX] RTE on first new IRRE record keeps loading in IE
    Problem: IE raises a syntax error when it encounters html comments in
    the JavaScript code. The html comments are added by the page renderer.
    Solution: There is no need for the page renderer to wrap inline
    javascript as html comments.
    
    Resolves: #55457
    Releases: 6.2, 6.1
    Change-Id: Iae180a73778ca3bb1c9934c887315b969888b10d
    Reviewed-on: https://review.typo3.org/27624
    Reviewed-by: Oliver Klee
    Tested-by: Leon de Rijke
    Tested-by: Simon Ouellet
    Reviewed-by: Wouter Wolters
    Tested-by: Wouter Wolters

--- typo3/sysext/core/Classes/Page/PageRenderer.php
+++ typo3/sysext/core/Classes/Page/PageRenderer.php
@@ -501,8 +501,8 @@ class PageRenderer implements \TYPO3\CMS\Core\SingletonInterface {
 		}
 		$this->backPath = isset($backPath) ? $backPath : $GLOBALS['BACK_PATH'];
 		$this->inlineJavascriptWrap = array(
-			'<script type="text/javascript">' . LF . '/*<![CDATA[*/' . LF . '<!-- ' . LF,
-			'// -->' . LF . '/*]]>*/' . LF . '</script>' . LF
+			'<script type="text/javascript">' . LF . '/*<![CDATA[*/' . LF,
+			'/*]]>*/' . LF . '</script>' . LF
 		);
 		$this->inlineCssWrap = array(
 			'<style type="text/css">' . LF . '/*<![CDATA[*/' . LF . '<!-- ' . LF,
