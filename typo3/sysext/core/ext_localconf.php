49252eff5a0757b59f995f8311109ee9419e9c38
Helmut Hummel <helmut.hummel@typo3.org>
2014-02-13 17:39:50 +0100

    [BUGFIX] Upgrade Wizard fails with exception
    The package manager throws an exception "core" package
    already being registered. This is caused by the signal
    which instantiates the PackageManager while in
    install tool the actual class is FailsafePackageManager
    
    Solution is to not register the slot in install tool
    as the failsafe package manager already takes care
    of scanning for available packages.
    
    Releases: 6.2
    Resolves: #55969
    Change-Id: I34b33e4ed947b532c24265e16dcc52a7aac4487e
    Reviewed-on: https://review.typo3.org/27616
    Reviewed-by: Wouter Wolters
    Reviewed-by: Markus Klein
    Tested-by: Markus Klein
    Reviewed-by: Helmut Hummel
    Tested-by: Helmut Hummel

--- typo3/sysext/core/ext_localconf.php
+++ typo3/sysext/core/ext_localconf.php
@@ -3,21 +3,19 @@ if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
 
-if (TYPO3_MODE === 'BE') {
+if (TYPO3_MODE === 'BE' && !(TYPO3_REQUESTTYPE & TYPO3_REQUESTTYPE_INSTALL)) {
+	\TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\SignalSlot\\Dispatcher')->connect(
+		'TYPO3\\CMS\\Core\\Resource\\ResourceFactory',
+		\TYPO3\CMS\Core\Resource\ResourceFactory::SIGNAL_PostProcessStorage,
+		'TYPO3\\CMS\\Core\\Resource\\Security\\StoragePermissionsAspect',
+		'addUserPermissionsToStorage'
+	);
 	\TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\SignalSlot\\Dispatcher')->connect(
 		'PackageManagement',
 		'packagesMayHaveChanged',
 		'TYPO3\\CMS\\Core\\Package\\PackageManager',
 		'scanAvailablePackages'
 	);
-	if (!(TYPO3_REQUESTTYPE & TYPO3_REQUESTTYPE_INSTALL)) {
-		\TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\SignalSlot\\Dispatcher')->connect(
-			'TYPO3\\CMS\\Core\\Resource\\ResourceFactory',
-			\TYPO3\CMS\Core\Resource\ResourceFactory::SIGNAL_PostProcessStorage,
-			'TYPO3\\CMS\\Core\\Resource\\Security\\StoragePermissionsAspect',
-			'addUserPermissionsToStorage'
-		);
-	}
 }
 
 \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\SignalSlot\\Dispatcher')->connect(
