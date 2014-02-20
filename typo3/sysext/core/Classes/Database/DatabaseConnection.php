b8eeb554a9b8dbb5b03850250fd8b50739e6a745
Markus Klein <klein.t3@mfc-linz.at>
2014-02-20 10:36:45 +0100

    [BUGFIX] DatabaseConnection::listQuery wrong usage of strpos()
    In DatabaseConnection::listQuery strpos() is used with wrong
    parameter order.
    
    Resolves: #56135
    Releases: 6.2, 6.1, 6.0
    Change-Id: Iaa18d46442a2aac21a836216cb61ae376bbb2090
    Reviewed-on: https://review.typo3.org/27750
    Reviewed-by: Stefan Neufeind
    Tested-by: Stefan Neufeind

--- typo3/sysext/core/Classes/Database/DatabaseConnection.php
+++ typo3/sysext/core/Classes/Database/DatabaseConnection.php
@@ -667,12 +667,12 @@ class DatabaseConnection {
 	 * @param string $field Field name
 	 * @param string $value Value to find in list
 	 * @param string $table Table in which we are searching (for DBAL detection of quoteStr() method)
-	 * @throws \InvalidArgumentException
 	 * @return string WHERE clause for a query
+	 * @throws \InvalidArgumentException
 	 */
 	public function listQuery($field, $value, $table) {
-		$value = (string) $value;
-		if (strpos(',', $value) !== FALSE) {
+		$value = (string)$value;
+		if (strpos($value, ',') !== FALSE) {
 			throw new \InvalidArgumentException('$value must not contain a comma (,) in $this->listQuery() !', 1294585862);
 		}
 		$pattern = $this->quoteStr($value, $table);
