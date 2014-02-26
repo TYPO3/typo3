71a7f96c3e6337cbe1647c784cfed4964d47de31
Markus Klein <klein.t3@mfc-linz.at>
2014-02-26 01:13:37 +0100

    [BUGFIX] Fix reference to FileListLocalisation.js
    FileListLocalisation.js was moved from backend to filelist
    with #55810, but it was forgotten to adapt the reference
    in InlineElement.
    
    Resolves: #55979
    Releases: 6.2
    Change-Id: I102ffe25c255f8ac39a49d4022ee3ab73ff1914c
    Reviewed-on: https://review.typo3.org/27861
    Reviewed-by: Anja Leichsenring
    Tested-by: Anja Leichsenring
    Reviewed-by: Frans Saris
    Reviewed-by: Helmut Hummel
    Tested-by: Helmut Hummel

--- typo3/sysext/backend/Classes/Form/Element/InlineElement.php
+++ typo3/sysext/backend/Classes/Form/Element/InlineElement.php
@@ -936,7 +936,7 @@ class InlineElement {
 
 		/** @var $pageRenderer \TYPO3\CMS\Core\Page\PageRenderer */
 		$pageRenderer = $GLOBALS['SOBE']->doc->getPageRenderer();
-		$pageRenderer->loadRequireJsModule('TYPO3/CMS/Backend/FileListLocalisation');
+		$pageRenderer->loadRequireJsModule('TYPO3/CMS/Filelist/FileListLocalisation');
 		$pageRenderer->loadRequireJsModule('TYPO3/CMS/Backend/DragUploader');
 		$pageRenderer->addInlineLanguagelabelFile(
 			\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('lang') . 'locallang_core.xlf',
