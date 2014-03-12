44100fa675c7735b43703cb59f637700a9ad1b46
Frans Saris <franssaris@gmail.com>
2014-03-12 14:05:43 +0100

    [BUGFIX] Add missing icons for main backend modules
    Resolves: #56809
    Releases: 6.2
    Change-Id: I5ea532065ef0be0fd279fe53d9498ae0df849af8
    Reviewed-on: https://review.typo3.org/28293
    Reviewed-by: Georg Ringer
    Tested-by: Georg Ringer

--- typo3/mod/tools/conf.php
+++ typo3/mod/tools/conf.php
@@ -1,4 +1,5 @@
 <?php
+$MLANG['default']['tabs_images']['tab'] = 'tool.gif';
 $MLANG['default']['ll_ref'] = 'LLL:EXT:lang/locallang_mod_admintools.xlf';
 $MCONF['name'] = 'tools';
 $MCONF['access'] = 'admin';
--- typo3/mod/web/conf.php
+++ typo3/mod/web/conf.php
@@ -1,4 +1,5 @@
 <?php
+$MLANG['default']['tabs_images']['tab'] = 'website.gif';
 $MLANG['default']['ll_ref'] = 'LLL:EXT:lang/locallang_mod_web.xlf';
 $MCONF['defaultMod'] = 'list';
 $MCONF['navFrameScript'] = '../../alt_db_navframe.php';
