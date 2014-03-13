8ea8d4f0a7f2453e1e1017bd84f6225750df0a5b
Markus Klein <klein.t3@mfc-linz.at>
2014-03-13 12:28:55 +0100

    [BUGFIX] Show thumbnails in list module
    Add missing checks if type column exists in a row.
    
    Resolves: #56830
    Releases: 6.2, 6.1
    Change-Id: Ifafdb0c51266c76620696c6d94471b3efddba112
    Reviewed-on: https://review.typo3.org/28342
    Reviewed-by: Andreas Kiessling
    Tested-by: Andreas Kiessling
    Reviewed-by: Alexander Opitz
    Tested-by: Alexander Opitz
    Reviewed-by: Markus Klein
    Tested-by: Markus Klein

--- typo3/sysext/recordlist/Classes/RecordList/DatabaseRecordList.php
+++ typo3/sysext/recordlist/Classes/RecordList/DatabaseRecordList.php
@@ -644,8 +644,15 @@ class DatabaseRecordList extends \TYPO3\CMS\Recordlist\RecordList\AbstractDataba
 					// - a thumbnail column exists
 					// - there is content in it
 					// - the thumbnail column is visible for the current type
-					$typeColumn = $GLOBALS['TCA'][$table]['ctrl']['type'];
-					$type = $row[$typeColumn];
+					$type = 0;
+					if (isset($GLOBALS['TCA'][$table]['ctrl']['type'])) {
+						$typeColumn = $GLOBALS['TCA'][$table]['ctrl']['type'];
+						$type = $row[$typeColumn];
+					}
+					// If current type doesn't exist, set it to 0 (or to 1 for historical reasons, if 0 doesn't exist)
+					if (!isset($GLOBALS['TCA'][$table]['types'][$type])) {
+						$type = isset($GLOBALS['TCA'][$table]['types'][0]) ? 0 : 1;
+					}
 					$visibleColumns = $GLOBALS['TCA'][$table]['types'][$type]['showitem'];
 
 					if ($this->thumbs &&
