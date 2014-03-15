e41d68d74ae779781367b988e18369ecb27d2c77
Ernesto Baschny <ernst@cron-it.de>
2014-03-15 20:36:47 +0100

    Revert "[BUGFIX] Implement locking in ClassLoader"
    This reverts commit 76284d26065af1a0a5b141f5f16305a5821609fa
    
    The implemented locking is fine, but currently breaks a fresh new
    installation (when there is no typo3temp directory yet). We remove
    this feature for the release of beta7 and merge it afterwards again
    to be able to improve upon it during the next week before the release
    candidate.
    
    Change-Id: Ie69352e5c7e3a224b0a0c71457efe3dbb8d90aac
    Reviewed-on: https://review.typo3.org/28426
    Reviewed-by: Ernesto Baschny
    Tested-by: Ernesto Baschny

--- typo3/sysext/core/Classes/Core/ClassLoader.php
+++ typo3/sysext/core/Classes/Core/ClassLoader.php
@@ -27,7 +27,6 @@ namespace TYPO3\CMS\Core\Core;
  *  This copyright notice MUST APPEAR in all copies of the script!
  ***************************************************************/
 
-use TYPO3\CMS\Core\Locking\Locker;
 use TYPO3\CMS\Core\Package\PackageInterface;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Core\Cache;
@@ -89,16 +88,6 @@ class ClassLoader {
 	protected $packageClassesPaths = array();
 
 	/**
-	 * @var bool Is TRUE while loading the Locker class to prevent a deadlock in the implicit call to loadClass
-	 */
-	protected $isLoadingLocker = FALSE;
-
-	/**
-	 * @var \TYPO3\CMS\Core\Locking\Locker
-	 */
-	protected $lockObject = NULL;
-
-	/**
 	 * Constructor
 	 *
 	 * @param ApplicationContext $context
@@ -149,8 +138,6 @@ class ClassLoader {
 	/**
 	 * Loads php files containing classes or interfaces found in the classes directory of
 	 * a package and specifically registered classes.
-	 *
-	 * Caution: This function may be called "recursively" by the spl_autoloader if a class depends on another classes.
 	 *
 	 * @param string $className Name of the class/interface to load
 	 * @return bool
@@ -214,22 +201,13 @@ class ClassLoader {
 	/**
 	 * Builds the class loading information and writes it to the cache. It handles Locking for this cache.
 	 *
-	 * Caution: The function loadClass can be called "recursively" by spl_autoloader. This needs to be observed when
-	 * locking for cache access. Only the first call to loadClass may acquire and release the lock!
-	 *
 	 * @param string $cacheEntryIdentifier Cache identifier for this class
 	 * @param string $className Name of class this information is for
 	 *
 	 * @return array|null The class information or NULL if class was not found
 	 */
 	protected function buildCachedClassLoadingInformation($cacheEntryIdentifier, $className) {
-		// We do not need locking if we are in earlyCache mode
-		$didLock = FALSE;
-		if (!$this->isEarlyCache) {
-			$didLock = $this->acquireLock();
-		}
-
-		// Look again into the cache after we got the lock, data might have been generated meanwhile
+		// Look again into cache after we got the look
 		$classLoadingInformation = $this->getClassLoadingInformationFromCache($cacheEntryIdentifier);
 		if ($classLoadingInformation === NULL) {
 			$classLoadingInformation = $this->buildClassLoadingInformation($className);
@@ -244,18 +222,12 @@ class ClassLoader {
 				$this->classesCache->set($cacheEntryIdentifier, '');
 			}
 		}
-
-		$this->releaseLock($didLock);
-
 		return $classLoadingInformation;
 	}
 
 	/**
-	 * Builds the class loading information
-	 *
-	 * @param string $className Name of class this information is for
-	 *
-	 * @return array|null The class information or NULL if class was not found
+	 * @param string $className
+	 * @return array|null
 	 */
 	public function buildClassLoadingInformation($className) {
 		$classLoadingInformation = $this->buildClassLoadingInformationForClassFromCorePackage($className);
@@ -471,24 +443,17 @@ class ClassLoader {
 	 * @return void
 	 */
 	protected function buildPackageNamespacesAndClassesPaths() {
-		$didLock = $this->acquireLock();
-
-		// Take a look again, after lock is acquired
-		if (!$this->loadPackageNamespacesFromCache()) {
-			foreach ($this->packages as $package) {
-				$this->buildPackageNamespaceAndClassesPath($package);
-			}
-			$this->sortPackageNamespaces();
-			$this->savePackageNamespacesAndClassesPathsToCache();
-			// The class alias map has to be rebuilt first, because ext_autoload files can contain
-			// old class names that need established class aliases.
-			$classNameToAliasMapping = $this->classAliasMap->setPackages($this->packages)->buildMappingAndInitializeEarlyInstanceMapping();
-			$this->loadClassFilesFromAutoloadRegistryIntoRuntimeClassInformationCache($this->packages);
-			$this->classAliasMap->buildMappingFiles($classNameToAliasMapping);
-			$this->transferRuntimeClassInformationCacheEntriesToClassesCache();
+		foreach ($this->packages as $package) {
+			$this->buildPackageNamespaceAndClassesPath($package);
 		}
-
-		$this->releaseLock($didLock);
+		$this->sortPackageNamespaces();
+		$this->savePackageNamespacesAndClassesPathsToCache();
+		// The class alias map has to be rebuilt first, because ext_autoload files can contain
+		// old class names that need established class aliases.
+		$classNameToAliasMapping = $this->classAliasMap->setPackages($this->packages)->buildMappingAndInitializeEarlyInstanceMapping();
+		$this->loadClassFilesFromAutoloadRegistryIntoRuntimeClassInformationCache($this->packages);
+		$this->classAliasMap->buildMappingFiles($classNameToAliasMapping);
+		$this->transferRuntimeClassInformationCacheEntriesToClassesCache();
 	}
 
 	/**
@@ -532,7 +497,6 @@ class ClassLoader {
 	 * Extracts the namespace from a package
 	 *
 	 * @param \TYPO3\Flow\Package\PackageInterface $package
-	 * @return void
 	 */
 	protected function buildPackageNamespace(\TYPO3\Flow\Package\PackageInterface $package) {
 		$packageNamespace = $package->getNamespace();
@@ -686,54 +650,4 @@ class ClassLoader {
 		return static::$staticAliasMap->getAliasesForClassName($className);
 	}
 
-	/**
-	 * Acquires a lock for the cache if we didn't already lock before.
-	 *
-	 * @return bool TRUE if the cache was acquired by this call and needs to be released
-	 * @throws \RuntimeException
-	 */
-	protected function acquireLock() {
-		if (!$this->isLoadingLocker) {
-			$lockObject = $this->getLocker();
-
-			// We didn't lock yet so do it
-			if (!$lockObject->getLockStatus()) {
-				if (!$lockObject->acquireExclusiveLock()) {
-					throw new \RuntimeException('Could not acquire lock for ClassLoader cache creation.', 1394480725);
-				}
-				return TRUE;
-			}
-		}
-		return FALSE;
-	}
-
-	/**
-	 * Releases a lock
-	 *
-	 * @param bool $needRelease The result of the call to acquireLock()
-	 *
-	 * @return void
-	 */
-	protected function releaseLock($needRelease) {
-		if ($needRelease) {
-			$lockObject = $this->getLocker();
-			$lockObject->release();
-		}
-	}
-
-	/**
-	 * Gets the TYPO3 Locker object or creates an instance of it.
-	 *
-	 * @return \TYPO3\CMS\Core\Locking\Locker
-	 */
-	protected function getLocker() {
-		if (NULL === $this->lockObject) {
-			$this->isLoadingLocker = TRUE;
-			$this->lockObject = new Locker('ClassLoader-cache-classes', Locker::LOCKING_METHOD_SIMPLE);
-			$this->lockObject->setEnableLogging(FALSE);
-			$this->isLoadingLocker = FALSE;
-		}
-
-		return $this->lockObject;
-	}
 }
