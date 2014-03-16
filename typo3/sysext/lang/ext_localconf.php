3b0e223d552f6aceab96fade3a404ec30492c03d
Ernesto Baschny <ernst@cron-it.de>
2014-03-16 01:31:32 +0100

    [TASK] Remove renamed language command controller
    Follow-up to 0d69c2d9e7b89ea63522d0b5e54aadf20ec1245d.
    
    The renamed language command controller (language:update) was introduced
    in 6.2, so we can simply remove the original name instead of deprecating
    it.
    
    This will only affect installations which are already using this task
    since 6.2 alpha2.
    
    Resolves: #54128
    Releases: 6.2
    Change-Id: Id6bb709bd30c9044c949c431aa2a595c1035c601
    Reviewed-on: https://review.typo3.org/28433
    Reviewed-by: Christian Kuhn
    Tested-by: Christian Kuhn
    Reviewed-by: Georg Ringer
    Tested-by: Georg Ringer

--- typo3/sysext/lang/ext_localconf.php
+++ typo3/sysext/lang/ext_localconf.php
@@ -4,8 +4,4 @@ if (!defined('TYPO3_MODE')) {
 }
 
 // Register language update command controller
-// @deprecated since TYPO3 CMS 6.2 will be removed in 2 versions
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = 'TYPO3\\CMS\\Lang\\Command\\UpdateCommandController';
-
-// Register language update command controller
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = 'TYPO3\\CMS\\Lang\\Command\\LanguageCommandController';
