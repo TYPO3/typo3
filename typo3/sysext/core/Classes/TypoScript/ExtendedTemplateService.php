df9399a1352cbafeaaa5fe39cc0084f8537804ea
Xavier Perseguers <xavier@typo3.org>
2014-03-27 11:40:51 +0100

    [BUGFIX] TS/EM constant categories cannot be localized
    Constant categories may now be localized using a construct similar
    to defining custom subcategories:
    
    Fixes: #57369
    Documentation: #57371
    Releases: 6.2
    Change-Id: I6bea094d98897756772a6f616c8a2f3ab5fe7844
    Reviewed-on: https://review.typo3.org/28903
    Reviewed-by: Wouter Wolters
    Reviewed-by: Fabien Udriot
    Tested-by: Fabien Udriot
    Tested-by: Wouter Wolters
    Reviewed-by: Xavier Perseguers
    Tested-by: Xavier Perseguers

--- typo3/sysext/core/Classes/TypoScript/ExtendedTemplateService.php
+++ typo3/sysext/core/Classes/TypoScript/ExtendedTemplateService.php
@@ -4,7 +4,7 @@ namespace TYPO3\CMS\Core\TypoScript;
 /***************************************************************
  *  Copyright notice
  *
- *  (c) 1999-2013 Kasper Skårhøj (kasperYYYY@typo3.com)
+ *  (c) 1999-2014 Kasper Skårhøj (kasperYYYY@typo3.com)
  *  All rights reserved
  *
  *  This script is part of the TYPO3 project. The TYPO3 project is
@@ -67,6 +67,13 @@ class ExtendedTemplateService extends \TYPO3\CMS\Core\TypoScript\TemplateService
 		// Advanced functions, which are used very seldomly.
 		'all' => array()
 	);
+
+	/**
+	 * Translated categories
+	 *
+	 * @var array
+	 */
+	protected $categoryLabels = array();
 
 	// This will be filled with the available categories of the current template.
 	/**
@@ -910,7 +917,11 @@ class ExtendedTemplateService extends \TYPO3\CMS\Core\TypoScript\TemplateService
 									case 'cat':
 										// List of categories.
 										$catSplit = explode('/', strtolower($keyValPair[1]));
-										$editableComments[$const]['cat'] = trim($catSplit[0]);
+										$catSplit[0] = trim($catSplit[0]);
+										if (isset($this->categoryLabels[$catSplit[0]])) {
+											$catSplit[0] = $this->categoryLabels[$catSplit[0]];
+										}
+										$editableComments[$const]['cat'] = $catSplit[0];
 										// This is the subcategory. Must be a key in $this->subCategories[]. catSplit[2] represents the search-order within the subcat.
 										$catSplit[1] = trim($catSplit[1]);
 										if ($catSplit[1] && isset($this->subCategories[$catSplit[1]])) {
@@ -923,6 +934,14 @@ class ExtendedTemplateService extends \TYPO3\CMS\Core\TypoScript\TemplateService
 									case 'label':
 										// Label
 										$editableComments[$const]['label'] = trim($keyValPair[1]);
+										break;
+									case 'customcategory':
+										// Custom category label
+										$customCategory = explode('=', $keyValPair[1], 2);
+										if (trim($customCategory[0])) {
+											$categoryKey = strtolower($customCategory[0]);
+											$this->categoryLabels[$categoryKey] = $GLOBALS['LANG']->sL($customCategory[1]);
+										}
 										break;
 									case 'customsubcategory':
 										// Custom subCategory label
