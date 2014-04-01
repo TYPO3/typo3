feddcb7043762f17dcbc14e006d95f2e1e02169a
Philipp Gampe <philipp.gampe@typo3.org>
2014-04-01 03:56:18 +0200

    [FEATURE] Introduce a stream wrapper to overlay file paths
    Implement a stream wrapper for the file:// protocol that can intercept
    any call to the filesystem.
    Transparently rewrite registered paths such that they can be replaced
    by vfs:// stream wrappers.
    
    Resolves: #57477
    Releases: master
    Change-Id: I3bd2e12f58d618883aa962b1d090b9c172c89be1
    Reviewed-on: http://review.typo3.org/29011
    Reviewed-by: Mathias Schreiber <mathias.schreiber@wmdb.de>
    Tested-by: Mathias Schreiber <mathias.schreiber@wmdb.de>
    Reviewed-by: Stefan Neufeind <typo3.neufeind@speedpartner.de>
    Tested-by: Stefan Neufeind <typo3.neufeind@speedpartner.de>
    Reviewed-by: Anja Leichsenring <aleichsenring@ab-softlab.de>
    Tested-by: Anja Leichsenring <aleichsenring@ab-softlab.de>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>

---
+++ typo3/sysext/core/Tests/Unit/FileStreamWrapperTest.php
@@ -0,0 +1,97 @@
+<?php
+namespace TYPO3\CMS\Core\Tests\Unit;
+
+/**
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use org\bovigo\vfs\vfsStream;
+use org\bovigo\vfs\visitor\vfsStreamStructureVisitor;
+use TYPO3\CMS\Core\Tests\FileStreamWrapper;
+
+/**
+ * Test case for \TYPO3\CMS\Core\Tests\Unit\FileStreamWrapper
+ */
+class FileStreamWrapperTest extends \TYPO3\CMS\Core\Tests\UnitTestCase {
+
+	/**
+	 * @test
+	 */
+	public function pathsAreOverlaidAndFinalDirectoryStructureCanBeQueried() {
+		$root = vfsStream::setup('root');
+		$subfolder = vfsStream::newDirectory('fileadmin');
+		$root->addChild($subfolder);
+		// Load fixture files and folders from disk
+		vfsStream::copyFromFileSystem(__DIR__ . '/TypoScript/Fixtures', $subfolder, 1024*1024);
+		FileStreamWrapper::init(PATH_site);
+		FileStreamWrapper::registerOverlayPath('fileadmin', 'vfs://root/fileadmin', FALSE);
+
+		// Use file functions as normal
+		mkdir(PATH_site . 'fileadmin/test/');
+		$file = PATH_site . 'fileadmin/test/Foo.bar';
+		file_put_contents($file, 'Baz');
+		$content = file_get_contents($file);
+		$this->assertSame('Baz', $content);
+
+		$expectedFileSystem = array(
+			'root' => array(
+				'fileadmin' => array(
+					'ext_typoscript_setup.txt' => 'test.Core.TypoScript = 1',
+					'test' => array('Foo.bar' => 'Baz'),
+				),
+			),
+		);
+		$this->assertEquals($expectedFileSystem, vfsStream::inspect(new vfsStreamStructureVisitor())->getStructure());
+
+		FileStreamWrapper::destroy();
+	}
+
+	/**
+	 * @test
+	 */
+	public function windowsPathsCanBeProcessed() {
+		$cRoot = 'C:\\Windows\\Root\\Path\\';
+		$root = vfsStream::setup('root');
+		FileStreamWrapper::init($cRoot);
+		FileStreamWrapper::registerOverlayPath('fileadmin', 'vfs://root/fileadmin');
+
+		touch($cRoot . 'fileadmin\\someFile.txt');
+		$expectedFileStructure = array(
+			'root' => array(
+				'fileadmin' => array('someFile.txt' => NULL),
+			),
+		);
+
+		$this->assertEquals($expectedFileStructure, vfsStream::inspect(new vfsStreamStructureVisitor())->getStructure());
+		FileStreamWrapper::destroy();
+	}
+
+	/**
+	 * @test
+	 */
+	public function symlinksCanBeCreated() {
+		$this->markTestSkipped('symlink() is not routed through the stream wrapper as of PHP 5.5, therefore we cannot test it');
+		/*
+		 * symlink() is not routed through the stream wrapper as of PHP 5.5,
+		 *  therefore we cannot test it.
+		 */
+		vfsStream::setup('root');
+		FileStreamWrapper::init(PATH_site);
+		FileStreamWrapper::registerOverlayPath('fileadmin', 'vfs://root/fileadmin');
+
+		$path = PATH_site . 'fileadmin/';
+		touch($path . 'file1.txt');
+		symlink($path . 'file1.txt', $path . 'file2.txt');
+
+		$this->assertTrue(is_link($path . 'file2.txt'));
+	}
+}
