c100e5e4f7f426201fc43b3cf650025c57b31502
Marc Bastian Heinrichs <typo3@mbh-software.de>
2014-04-03 11:02:34 +0200

    [BUGFIX] Make Extbase Cli\RequestBuilder a Singleton
    Like the Web\RequestBuilder the Cli\RequestBuilder is a service
    object for the RequestHandler. It gets injected and has no
    internal state, thus it should be a Singleton.
    
    Resolves: #57591
    Releases: 6.2
    Change-Id: I959fcfdbc4182cd86cd38d438a0ff7f0de1845f1
    Reviewed-on: https://review.typo3.org/29112
    Reviewed-by: Wouter Wolters
    Reviewed-by: Stefan Neufeind
    Tested-by: Stefan Neufeind

--- typo3/sysext/extbase/Classes/Mvc/Cli/RequestBuilder.php
+++ typo3/sysext/extbase/Classes/Mvc/Cli/RequestBuilder.php
@@ -29,7 +29,7 @@ namespace TYPO3\CMS\Extbase\Mvc\Cli;
  *
  * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License, version 3 or later
  */
-class RequestBuilder {
+class RequestBuilder implements \TYPO3\CMS\Core\SingletonInterface {
 
 	/**
 	 * @var \TYPO3\CMS\Extbase\Object\ObjectManagerInterface
