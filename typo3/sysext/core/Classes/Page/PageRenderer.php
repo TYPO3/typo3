ee766f68950a732339f9f1d1911ffd06c0bdedf5
Markus Klein <klein.t3@mfc-linz.at>
2014-04-15 01:49:50 +0200

    [BUGFIX] Do not unset() PageRenderer class members
    Instead reset them to a default value.
    
    Resolves: #57920
    Releases: 6.2
    Change-Id: I900cbd46b5573a84133efe1cf9fbf29d483e8096
    Reviewed-on: https://review.typo3.org/29469
    Reviewed-by: Stefan Neufeind
    Tested-by: Stefan Neufeind

--- typo3/sysext/core/Classes/Page/PageRenderer.php
+++ typo3/sysext/core/Classes/Page/PageRenderer.php
@@ -2238,7 +2238,7 @@ class PageRenderer implements \TYPO3\CMS\Core\SingletonInterface {
 				$this->includeLanguageFileForInline($languageLabelFile['fileRef'], $languageLabelFile['selectionPrefix'], $languageLabelFile['stripFromSelectionName'], $languageLabelFile['$errorMode']);
 			}
 		}
-		unset($this->inlineLanguageLabelFiles);
+		$this->inlineLanguageLabelFiles = array();
 		// Convert labels/settings back to UTF-8 since json_encode() only works with UTF-8:
 		if ($this->getCharSet() !== 'utf-8') {
 			if ($this->inlineLanguageLabels) {
@@ -2270,7 +2270,7 @@ class PageRenderer implements \TYPO3\CMS\Core\SingletonInterface {
 					. $code
 				. ' });'
 				. $this->inlineJavascriptWrap[1];
-			unset($this->extOnReadyCode);
+			$this->extOnReadyCode = array();
 			// Include TYPO3.l10n object
 			if (TYPO3_MODE === 'BE') {
 				$out .= '<script src="' . $this->processJsFile(($this->backPath . 'sysext/lang/Resources/Public/JavaScript/Typo3Lang.js')) . '" type="text/javascript" charset="utf-8"></script>' . LF;
