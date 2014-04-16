68763faf3bbcaafb5753ae952fe3614ad9aeb10b
Jigal van Hemert <jigal.van.hemert@typo3.org>
2014-04-16 10:58:57 +0200

    [BUGFIX] DBAL sql_fetch_* must return boolean or array
    If the end of a result set is reached some drivers produce NULL.
    This must be changed into FALSE to be compatible with the
    non-DBAL version of these functions.
    
    Resolves: #57957
    Releases: 6.2, 6.1
    Change-Id: I664dd10735f88754c74e20ecd5c07fad5ef2b78d
    Reviewed-on: https://review.typo3.org/29513
    Reviewed-by: Jigal van Hemert
    Tested-by: Jigal van Hemert

--- typo3/sysext/dbal/Classes/Database/DatabaseConnection.php
+++ typo3/sysext/dbal/Classes/Database/DatabaseConnection.php
@@ -2179,6 +2179,10 @@ class DatabaseConnection extends \TYPO3\CMS\Core\Database\DatabaseConnection {
 				$output = $this->map_assocArray($output, $tables, 1);
 			}
 		}
+		if ($output === NULL) {
+			// Needed for compatibility
+			$output = FALSE;
+		}
 		// Return result:
 		return $output;
 	}
@@ -2226,6 +2230,10 @@ class DatabaseConnection extends \TYPO3\CMS\Core\Database\DatabaseConnection {
 		case 'userdefined':
 			$output = $res->sql_fetch_row();
 			break;
+		}
+		if ($output === NULL) {
+			// Needed for compatibility
+			$output = FALSE;
 		}
 		return $output;
 	}
