b3753e930f750dc81fd80505dabf116f52697141
TYPO3 Release Team <typo3v4@typo3.org>
2014-04-16 23:45:27 +0200

    [TASK] Set TYPO3 version to 6.2.2-dev
    Change-Id: I6e25181228d9c65bc4eee0b2309aed54ce24d181
    Reviewed-on: https://review.typo3.org/29524
    Reviewed-by: TYPO3 Release Team
    Tested-by: TYPO3 Release Team

--- typo3/sysext/core/Classes/Core/SystemEnvironmentBuilder.php
+++ typo3/sysext/core/Classes/Core/SystemEnvironmentBuilder.php
@@ -90,7 +90,7 @@ class SystemEnvironmentBuilder {
 	 */
 	static protected function defineBaseConstants() {
 		// This version, branch and copyright
-		define('TYPO3_version', '6.2.1');
+		define('TYPO3_version', '6.2.2-dev');
 		define('TYPO3_branch', '6.2');
 		define('TYPO3_copyright_year', '1998-2014');
 
