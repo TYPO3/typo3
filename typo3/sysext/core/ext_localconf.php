d3c9706c827074adaefd8a79ecf5024fa4b9c756
Marc Bastian Heinrichs <typo3@mbh-software.de>
2014-04-23 17:28:46 +0200

    [SECURITY] Prevent edit of file metadata of files with no access
    By forging edit URLs it was possible to edit
    meta data records of files which were not
    within a user mount.
    
    Implement several hooks to check access to the file
    and only grant access to a meta data record if the
    user has access to the file.
    
    Resolves: #56644
    Releases: master, 6.2
    Security-Bulletin: TYPO3-CORE-SA-2015-002
    Change-Id: I0f0704af2e7f01d16b9420f9ba4ac1a7846b5270
    Reviewed-on: http://review.typo3.org/40804
    Reviewed-by: Benjamin Mack <benni@typo3.org>
    Tested-by: Benjamin Mack <benni@typo3.org>
    Reviewed-by: Helmut Hummel <helmut.hummel@typo3.org>
    Tested-by: Helmut Hummel <helmut.hummel@typo3.org>

--- typo3/sysext/core/ext_localconf.php
+++ typo3/sysext/core/ext_localconf.php
@@ -5,12 +5,19 @@ defined('TYPO3_MODE') or die();
 $signalSlotDispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\SignalSlot\Dispatcher::class);
 
 if (TYPO3_MODE === 'BE' && !(TYPO3_REQUESTTYPE & TYPO3_REQUESTTYPE_INSTALL)) {
+	// FAL SECURITY CHECKS
 	$signalSlotDispatcher->connect(
 		\TYPO3\CMS\Core\Resource\ResourceFactory::class,
 		\TYPO3\CMS\Core\Resource\ResourceFactoryInterface::SIGNAL_PostProcessStorage,
 		\TYPO3\CMS\Core\Resource\Security\StoragePermissionsAspect::class,
 		'addUserPermissionsToStorage'
 	);
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = 'TYPO3\\CMS\\Core\\Resource\\Security\\FileMetadataPermissionsAspect';
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['typo3/alt_doc.php']['makeEditForm_accessCheck'][] = 'TYPO3\\CMS\\Core\\Resource\\Security\\FileMetadataPermissionsAspect->isAllowedToShowEditForm';
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tceforms_inline.php']['checkAccess'][] = 'TYPO3\\CMS\\Core\\Resource\\Security\\FileMetadataPermissionsAspect->isAllowedToShowEditForm';
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['checkModifyAccessList'][] = 'TYPO3\\CMS\\Core\\Resource\\Security\\FileMetadataPermissionsAspect';
+
+	// PACKAGE MANAGEMENT
 	$signalSlotDispatcher->connect(
 		'PackageManagement',
 		'packagesMayHaveChanged',
