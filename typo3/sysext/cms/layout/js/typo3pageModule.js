aa54cf90270c2b29a033a6dff9b813a2faea1da6
Benjamin Mack <benni@typo3.org>
2014-04-30 15:07:03 +0200

    [CLEANUP] Page Module: Do hovering via CSS, not JS
    The ExtJS functionality for adding active classes can
    be done via CSS hover pseudo classes.
    
    This is faster (native browser functionality) and
    also removes some ExtJS code. With this change
    however, the hovering is not disabled
    anymore when dragging.
    
    Releases: 6.2
    Resolves: #58359
    Change-Id: Ifcc0307b436cf68dfcfe561444e779410978d576
    Reviewed-on: https://review.typo3.org/29744
    Tested-by: Stefan Neufeind
    Reviewed-by: Nadir S.
    Tested-by: Nadir S.
    Reviewed-by: Nico Schober
    Tested-by: Nico Schober
    Reviewed-by: Christian Kuhn
    Tested-by: Christian Kuhn
    Reviewed-by: Nicole Cordes
    Tested-by: Nicole Cordes

--- typo3/sysext/cms/layout/js/typo3pageModule.js
+++ typo3/sysext/cms/layout/js/typo3pageModule.js
@@ -31,89 +31,7 @@ TYPO3.Components.PageModule = {
 	 * Initialization
 	 */
 	init: function() {
-		this.enableHighlighting();
 		this.enableDragDrop();
-	},
-
-	/**
-	 * This method is used to bind the highlighting function "setElementActive"
-	 * to the mouseover event and the "setElementInactive" to the mouseout event.
-	 */
-	enableHighlighting: function() {
-		Ext.select('div.t3-page-ce')
-			.on('mouseover',this.setElementActive, this)
-			.on('mouseout',this.setElementInactive, this);
-		Ext.select('td.t3-page-column')
-			.on('mouseover',this.setColumnActive, this)
-			.on('mouseout',this.setColumnInactive, this);
-		Ext.select('#typo3-dblist-sysnotes div.single-note')
-			.on('mouseover',this.setSysnoteActive, this)
-			.on('mouseout',this.setSysnoteInactive, this);
-	},
-
-	/**
-	 * This method is used to unbind the highlighting function "setElementActive"
-	 * from the mouseover event and the "setElementInactive" from the mouseout event.
-	 */
-	disableHighlighting: function() {
-		Ext.select('div.t3-page-ce')
-			.un('mouseover', this.setElementActive, this)
-			.un('mouseout', this.setElementInactive, this);
-		Ext.select('td.t3-page-column')
-			.un('mouseover',this.setColumnActive, this)
-			.un('mouseout',this.setColumnInactive, this);
-	},
-
-	/**
-	 * This method is used as an event handler when the
-	 * user hovers the a content element.
-	 */
-	setElementActive: function(event, target) {
-		Ext.get(target).findParent('div.t3-page-ce', null, true).addClass('active');
-	},
-
-	/**
-	 * This method is used as event handler to unset active state of
-	 * a content element when the mouse of the user leaves the
-	 * content element.
-	 */
-	setElementInactive: function(event, target) {
-		Ext.get(target).findParent('div.t3-page-ce', null, true).removeClass('active');
-
-	},
-
-	/**
-	 * This method is used as an event handler when the
-	 * user hovers the a content element.
-	 */
-	setColumnActive: function(event, target) {
-		Ext.get(target).findParent('td.t3-page-column', null, true).addClass('active');
-	},
-
-	/**
-	 * This method is used as event handler to unset active state of
-	 * a content element when the mouse of the user leaves the
-	 * content element.
-	 */
-	setColumnInactive: function(event, target) {
-		Ext.get(target).findParent('td.t3-page-column', null, true).removeClass('active');
-	},
-
-	/**
-	 * This method is used as an event handler when the
-	 * user hovers the a sysnote.
-	 */
-	setSysnoteActive: function(event, target) {
-		Ext.get(target).findParent('div.single-note', null, true).addClass('active');
-	},
-
-	/**
-	 * This method is used as event handler to unset active state of
-	 * a sysnote when the mouse of the user leaves the sysnote.
-	 */
-	setSysnoteInactive: function(event, target) {
-		Ext.get(target).findParent('div.single-note', null, true).removeClass('active');
-
 	},
 
 	/**
@@ -138,7 +56,6 @@ TYPO3.Components.PageModule = {
 
 				// Hide create new element button
 				this.el.findParent('td.t3-page-column', null, true).removeClass('active');
-				TYPO3.Components.PageModule.disableHighlighting();
 
 				var dropZones = Ext.select('.t3-page-ce-dropzone');
 				var self = this;
@@ -188,7 +105,6 @@ TYPO3.Components.PageModule = {
 				this.el.child('.t3-page-ce-dragitem').removeClass('dragitem-shadow');
 				// Show create new element button again
 				this.el.child('.t3-icon-document-new').removeClass('drag-start');
-				TYPO3.Components.PageModule.enableHighlighting();
 
 				// Remove dragitem-shadow after dragging
 				this.el.child('.t3-page-ce-dragitem').removeClass('dragitem-shadow');
