28fa581179f12fb7ec162cdaecb78f6227c86db4
Marc Bastian Heinrichs <typo3@mbh-software.de>
2014-05-14 16:34:42 +0200

    [BUGFIX] Fix saving non public export presets
    Fix sql error "Column 'public' cannot be null" on saving
    with unchecked box public.
    
    Resolves: #57405
    Releases: 6.2
    Change-Id: I1582728cc56781fdc12409956008fc82a6cd0d03
    Reviewed-on: https://review.typo3.org/30112
    Reviewed-by: Fabien Udriot
    Tested-by: Fabien Udriot
    Reviewed-by: Markus Klein
    Tested-by: Markus Klein

--- typo3/sysext/impexp/Classes/Controller/ImportExportController.php
+++ typo3/sysext/impexp/Classes/Controller/ImportExportController.php
@@ -1134,6 +1134,8 @@ class ImportExportController extends \TYPO3\CMS\Backend\Module\BaseScriptClass {
 		$msg = '';
 		// Save preset
 		$beUser = $this->getBackendUser();
+		// cast public checkbox to int, since this is a int field and NULL is not allowed
+		$inData['preset']['public'] = (int)$inData['preset']['public'];
 		if (isset($presetData['save'])) {
 			$preset = $this->getPreset($presetData['select']);
 			// Update existing
