3ee99e99c29861cbde65b785e8dd6b94979bfec9
Markus Klein <klein.t3@mfc-linz.at>
2014-05-15 19:08:06 +0200

    [BUGFIX] Wrong system requirements link
    Resolves: #58842
    Releases: 6.2, 6.1
    Change-Id: Ibaf87d32778349d5a87009bcd2b365447e6488fa
    Reviewed-on: https://review.typo3.org/30151
    Reviewed-by: Markus Klein
    Tested-by: Markus Klein

--- typo3/sysext/core/Classes/Core/SystemEnvironmentBuilder.php
+++ typo3/sysext/core/Classes/Core/SystemEnvironmentBuilder.php
@@ -93,7 +93,7 @@ class SystemEnvironmentBuilder {
 		define('TYPO3_URL_CONTRIBUTE', 'http://typo3.org/contribute/');
 		define('TYPO3_URL_SECURITY', 'http://typo3.org/teams/security/');
 		define('TYPO3_URL_DOWNLOAD', 'http://typo3.org/download/');
-		define('TYPO3_URL_SYSTEMREQUIREMENTS', 'http://typo3.org/about/typo3-the-cms/system-requirements/');
+		define('TYPO3_URL_SYSTEMREQUIREMENTS', 'http://typo3.org/typo3-cms/overview/requirements/');
 		define('TYPO3_URL_DONATE', 'http://typo3.org/donate/online-donation/');
 
 		// A tabulator, a linefeed, a carriage return, a CR-LF combination
