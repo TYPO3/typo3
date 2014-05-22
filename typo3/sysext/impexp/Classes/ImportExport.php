844dc392d5ea8359d41f6624385ef7e610a993b8
Marc Bastian Heinrichs <typo3@mbh-software.de>
2014-05-22 10:32:03 +0200

    [BUGFIX] Catch all errors on adding files of sys_files to export
    Adding files from sys_file records is not robust enough. For example
    sys_file records could exist for already deleted files. In this case a
    RuntimeException is thrown, which is not caught currently.
    
    Solution: catch all Exceptions thrown from Resources in
    export_addSysFile and report them to the user.
    
    Resolves: #59015
    Releases: master, 6.2
    Change-Id: If7ce51a2ae756b43dc30d5545b076310972342f4
    Reviewed-on: http://review.typo3.org/37566
    Reviewed-by: Anja Leichsenring <aleichsenring@ab-softlab.de>
    Tested-by: Anja Leichsenring <aleichsenring@ab-softlab.de>

--- typo3/sysext/impexp/Classes/ImportExport.php
+++ typo3/sysext/impexp/Classes/ImportExport.php
@@ -985,11 +985,8 @@ class ImportExport {
 		}
 		try {
 			$fileContent = $file->getContents();
-		} catch (\TYPO3\CMS\Core\Resource\Exception\InsufficientFileAccessPermissionsException $e) {
-			$this->error('File ' . $file->getPublicUrl() . ': ' . $e->getMessage());
-			return;
-		} catch (\TYPO3\CMS\Core\Resource\Exception\IllegalFileExtensionException $e) {
-			$this->error('File ' . $file->getPublicUrl() . ': ' . $e->getMessage());
+		} catch (\Exception $e) {
+			$this->error('Error when trying to add file ' . $file->getCombinedIdentifier() . ': ' . $e->getMessage());
 			return;
 		}
 		$fileUid = $file->getUid();
