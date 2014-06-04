36b6df8403b685f0f543135620c773dad67a862b
Marc Bastian Heinrichs <typo3@mbh-software.de>
2014-06-04 16:40:53 +0200

    [BUGFIX] Fix empty globalString LIT condition comparison
    An empty "LIT:" condition results in NULL, so the strict compare
    doesn't match with the empty string.
    
    Solution is to cast the haystack to string before, like it was
    done anyway later in the preg_match.
    
    Releases: 6.3, 6.2, 6.1
    Resolves: #59344
    Change-Id: I1e4b00e454d921eedd0c03a1767d4ec57c321bd7
    Reviewed-on: https://review.typo3.org/30559
    Reviewed-by: Markus Klein
    Tested-by: Markus Klein
    Reviewed-by: Marc Bastian Heinrichs
    Reviewed-by: Wouter Wolters
    Tested-by: Wouter Wolters

--- typo3/sysext/core/Classes/Configuration/TypoScript/ConditionMatching/AbstractConditionMatcher.php
+++ typo3/sysext/core/Classes/Configuration/TypoScript/ConditionMatching/AbstractConditionMatcher.php
@@ -287,8 +287,8 @@ abstract class AbstractConditionMatcher {
 				break;
 			case 'useragent':
 				$test = trim($value);
-				if (strlen($test)) {
-					return $this->searchStringWildcard($browserInfo['useragent'], $test);
+				if ($test !== '') {
+					return $this->searchStringWildcard((string)$browserInfo['useragent'], $test);
 				}
 				break;
 			case 'language':
@@ -412,7 +412,7 @@ abstract class AbstractConditionMatcher {
 				foreach ($values as $test) {
 					$point = strcspn($test, '=');
 					$theVarName = substr($test, 0, $point);
-					$nv = $this->getVariable(trim($theVarName));
+					$nv = (string)$this->getVariable(trim($theVarName));
 					$testValue = substr($test, $point + 1);
 					if ($this->searchStringWildcard($nv, trim($testValue))) {
 						return TRUE;
@@ -515,7 +515,7 @@ abstract class AbstractConditionMatcher {
 	 * Evaluates a $leftValue based on an operator: "<", ">", "<=", ">=", "!=" or "="
 	 *
 	 * @param string $test The value to compare with on the form [operator][number]. Eg. "< 123
-	 * @param integer $leftValue The value on the left side
+	 * @param float $leftValue The value on the left side
 	 * @return boolean If $value is "50" and $test is "< 123" then it will return TRUE.
 	 */
 	protected function compareNumber($test, $leftValue) {
@@ -587,7 +587,7 @@ abstract class AbstractConditionMatcher {
 				// Replace the marker with .* to match anything (wildcard)
 				$regex = str_replace(array('###MANY###', '###ONE###'), array('.*', '.'), $regex);
 			}
-			$result = (bool) preg_match($regex, ((string) $haystack));
+			$result = (bool)preg_match($regex, $haystack);
 		}
 		return $result;
 	}
