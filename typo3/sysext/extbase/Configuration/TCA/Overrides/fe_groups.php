382457d241514ea200eaadc4c258ed75cdc57727
Anja Leichsenring <aleichsenring@ab-softlab.de>
2014-06-07 08:58:30 +0200

    [BUGFIX] Move TCA for fe_users and fe_groups into TCA/Overrides
    TCA manipulation code must no longer reside in ext_tables.php, but
    moved to Configuration/TCA/Overrides/<tablename>.php. Leftovers
    in ext_tables might disturb loading order of TCA changes.
    
    Change-Id: Ie919a856fbd8242d19110c8db8d01ff7865c913c
    Resolves: #59397
    Releases: 6.2
    Reviewed-on: https://review.typo3.org/30620
    Reviewed-by: Krzysztof Adamczyk
    Tested-by: Krzysztof Adamczyk
    Reviewed-by: Steffen Ritter
    Tested-by: Steffen Ritter

---
+++ typo3/sysext/extbase/Configuration/TCA/Overrides/fe_groups.php
@@ -0,0 +1,31 @@
+<?php
+if (!isset($GLOBALS['TCA']['fe_groups']['ctrl']['type'])) {
+	$tca = array(
+		'ctrl' => array(
+			'type' => 'tx_extbase_type',
+		),
+		'columns' => array(
+			'tx_extbase_type' => array(
+				'exclude' => 1,
+				'label' => 'LLL:EXT:extbase/Resources/Private/Language/locallang_db.xlf:fe_groups.tx_extbase_type',
+				'config' => array(
+					'type' => 'select',
+					'items' => array(
+						array('LLL:EXT:extbase/Resources/Private/Language/locallang_db.xlf:fe_groups.tx_extbase_type.0', '0'),
+						array('LLL:EXT:extbase/Resources/Private/Language/locallang_db.xlf:fe_groups.tx_extbase_type.Tx_Extbase_Domain_Model_FrontendUserGroup', 'Tx_Extbase_Domain_Model_FrontendUserGroup')
+					),
+					'size' => 1,
+					'maxitems' => 1,
+					'default' => '0'
+				)
+			)
+		),
+		'types' => array(
+			'Tx_Extbase_Domain_Model_FrontendUserGroup' => $GLOBALS['TCA']['fe_groups']['types']['0'],
+		),
+	);
+	$GLOBALS['TCA']['fe_groups'] = array_replace_recursive($GLOBALS['TCA']['fe_groups'], $tca);
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('fe_groups', 'tx_extbase_type');
+} else {
+	$GLOBALS['TCA']['fe_groups']['types']['Tx_Extbase_Domain_Model_FrontendUserGroup'] = $GLOBALS['TCA']['fe_groups']['types']['0'];
+}
---
+++ typo3/sysext/extbase/Configuration/TCA/Overrides/fe_users.php
@@ -0,0 +1,31 @@
+<?php
+if (!isset($GLOBALS['TCA']['fe_users']['ctrl']['type'])) {
+	$tca = array(
+		'ctrl' => array(
+			'type' => 'tx_extbase_type',
+		),
+		'columns' => array(
+			'tx_extbase_type' => array(
+				'exclude' => 1,
+				'label' => 'LLL:EXT:extbase/Resources/Private/Language/locallang_db.xlf:fe_users.tx_extbase_type',
+				'config' => array(
+					'type' => 'select',
+					'items' => array(
+						array('LLL:EXT:extbase/Resources/Private/Language/locallang_db.xlf:fe_users.tx_extbase_type.0', '0'),
+						array('LLL:EXT:extbase/Resources/Private/Language/locallang_db.xlf:fe_users.tx_extbase_type.Tx_Extbase_Domain_Model_FrontendUser', 'Tx_Extbase_Domain_Model_FrontendUser')
+					),
+					'size' => 1,
+					'maxitems' => 1,
+					'default' => '0'
+				)
+			)
+		),
+		'types' => array(
+			'Tx_Extbase_Domain_Model_FrontendUser' => $GLOBALS['TCA']['fe_users']['types']['0'],
+		),
+	);
+	$GLOBALS['TCA']['fe_users'] = array_replace_recursive($GLOBALS['TCA']['fe_users'], $tca);
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('fe_users', 'tx_extbase_type');
+} else {
+	$GLOBALS['TCA']['fe_users']['types']['Tx_Extbase_Domain_Model_FrontendUser'] = $GLOBALS['TCA']['fe_users']['types']['0'];
+}
