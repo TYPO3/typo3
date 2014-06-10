7a11858c6a9aaf178a9b907a15398f434bc97760
Anja Leichsenring <aleichsenring@ab-softlab.de>
2014-06-10 21:56:42 +0200

    [BUGFIX] Move TCA modification of EXT:csc to TCA/Overrides
    TCA manipulation code must no longer reside in ext_tables.php, but
    moved to Configuration/TCA/Overrides/<tablename>.php. Leftovers
    in ext_tables might disturb loading order of TCA changes and spoil
    proper caching.
    
    Change-Id: I3a357ddff0fe5d6f16caa67b4085372c27594234
    Resolves: #59465
    Related: #59414
    Releases: 6.3, 6.2
    Reviewed-on: https://review.typo3.org/30650
    Reviewed-by: Wouter Wolters
    Tested-by: Wouter Wolters
    Reviewed-by: Marcin Sągol
    Reviewed-by: Christian Kuhn
    Tested-by: Christian Kuhn

---
+++ typo3/sysext/css_styled_content/Configuration/TCA/Overrides/tt_content.php
@@ -0,0 +1,26 @@
+<?php
+if (!defined('TYPO3_MODE')) {
+	die('Access denied.');
+}
+// Add flexform
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('*', 'FILE:EXT:css_styled_content/flexform_ds.xml', 'table');
+
+$GLOBALS['TCA']['tt_content']['types']['table']['showitem'] = 'CType;;4;;1-1-1, hidden, header;;3;;2-2-2, linkToTop;;;;4-4-4,
+			--div--;LLL:EXT:cms/locallang_ttc.xlf:CType.I.5, layout;;10;;3-3-3, cols, bodytext;;9;nowrap:wizards[table], pi_flexform,
+			--div--;LLL:EXT:cms/locallang_tca.xlf:pages.tabs.access, starttime, endtime, fe_group';
+
+$GLOBALS['TCA']['tt_content']['columns']['section_frame']['config']['items'][0] = array(
+	'LLL:EXT:css_styled_content/locallang_db.xlf:tt_content.tx_cssstyledcontent_section_frame.I.0', '0'
+);
+
+$GLOBALS['TCA']['tt_content']['columns']['section_frame']['config']['items'][9] = array(
+	'LLL:EXT:css_styled_content/locallang_db.xlf:tt_content.tx_cssstyledcontent_section_frame.I.9', '66'
+);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('css_styled_content', 'static/', 'CSS Styled Content');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('css_styled_content', 'static/v4.5/', 'CSS Styled Content TYPO3 v4.5');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('css_styled_content', 'static/v4.6/', 'CSS Styled Content TYPO3 v4.6');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('css_styled_content', 'static/v4.7/', 'CSS Styled Content TYPO3 v4.7');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('css_styled_content', 'static/v6.0/', 'CSS Styled Content TYPO3 v6.0');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('css_styled_content', 'static/v6.1/', 'CSS Styled Content TYPO3 v6.1');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('css_styled_content', 'static/v6.2/', 'CSS Styled Content TYPO3 v6.2');
