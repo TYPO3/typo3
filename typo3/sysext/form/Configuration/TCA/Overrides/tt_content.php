6edae9c2aa8c0b9cf58707938160b96383085c10
Anja Leichsenring <aleichsenring@ab-softlab.de>
2014-06-10 22:10:03 +0200

    [BUGFIX] Move TCA modification of EXT:form to TCA/Overrides.
    TCA manipulation code must no longer reside in ext_tables.php, but
    moved to Configuration/TCA/Overrides/<tablename>.php. Leftovers
    in ext_tables might disturb loading order of TCA changes and spoil
    proper caching.
    
    Change-Id: I789ea89318d76d0c709229139b0f5296042cded7
    Resolves: #59466
    Related: #59414
    Releases: 6.3, 6.2
    Reviewed-on: https://review.typo3.org/31341
    Reviewed-by: Anja Leichsenring
    Tested-by: Anja Leichsenring

---
+++ typo3/sysext/form/Configuration/TCA/Overrides/tt_content.php
@@ -0,0 +1,32 @@
+<?php
+if (!defined('TYPO3_MODE')) {
+	die('Access denied.');
+}
+$GLOBALS['TCA']['tt_content']['columns']['bodytext']['config']['wizards']['forms'] = array(
+	'notNewRecords' => 1,
+	'enableByTypeConfig' => 1,
+	'type' => 'script',
+	'title' => 'Form wizard',
+	'icon' => 'wizard_forms.gif',
+	'module' => array(
+		'name' => 'wizard_form'
+	),
+	'params' => array(
+		'xmlOutput' => 0
+	)
+);
+$GLOBALS['TCA']['tt_content']['types']['mailform']['showitem'] = '
+	CType;;4;;1-1-1,
+	hidden,
+	header;;3;;2-2-2,
+	linkToTop;;;;3-3-3,
+	--div--;LLL:EXT:cms/locallang_ttc.xlf:CType.I.8,
+	bodytext;LLL:EXT:cms/locallang_ttc.xlf:bodytext.ALT.mailform;;nowrap:wizards[forms];3-3-3,
+	--div--;LLL:EXT:cms/locallang_tca.xlf:pages.tabs.access,
+	starttime,
+	endtime,
+	fe_group
+';
+
+// Add Default TS to Include static (from extensions)
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('form', 'Configuration/TypoScript/', 'Default TS');
--- typo3/sysext/form/ext_tables.php
+++ typo3/sysext/form/ext_tables.php
@@ -3,9 +3,6 @@ if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
 
-// Add Default TS to Include static (from extensions)
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/', 'Default TS');
-
 if (TYPO3_MODE === 'BE') {
 	// Register wizard
 	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModulePath(
@@ -13,29 +10,3 @@ if (TYPO3_MODE === 'BE') {
 		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Modules/Wizards/FormWizard/'
 	);
 }
-
-$GLOBALS['TCA']['tt_content']['columns']['bodytext']['config']['wizards']['forms'] = array(
-	'notNewRecords' => 1,
-	'enableByTypeConfig' => 1,
-	'type' => 'script',
-	'title' => 'Form wizard',
-	'icon' => 'wizard_forms.gif',
-	'module' => array(
-		'name' => 'wizard_form'
-	),
-	'params' => array(
-		'xmlOutput' => 0
-	)
-);
-$GLOBALS['TCA']['tt_content']['types']['mailform']['showitem'] = '
-	CType;;4;;1-1-1,
-	hidden,
-	header;;3;;2-2-2,
-	linkToTop;;;;3-3-3,
-	--div--;LLL:EXT:cms/locallang_ttc.xlf:CType.I.8,
-	bodytext;LLL:EXT:cms/locallang_ttc.xlf:bodytext.ALT.mailform;;nowrap:wizards[forms];3-3-3,
-	--div--;LLL:EXT:cms/locallang_tca.xlf:pages.tabs.access,
-	starttime,
-	endtime,
-	fe_group
-';
