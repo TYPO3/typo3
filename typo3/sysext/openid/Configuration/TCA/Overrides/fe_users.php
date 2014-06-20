920f93cc4a150c74e91a65d0309291327e600c39
Anja Leichsenring <aleichsenring@ab-softlab.de>
2014-06-20 11:46:16 +0200

    [BUGFIX] Move TCA modification of EXT:openid to TCA/Overrides
    TCA manipulation code must no longer reside in ext_tables.php, but
    moved to Configuration/TCA/Overrides/<tablename>.php. Leftovers
    in ext_tables might disturb loading order of TCA changes and spoil
    proper caching.
    
    Change-Id: I7b5240eccde3d49531bb734adf26ee623829dc59
    Resolves: #59698
    Related: #59414
    Releases: 6.3, 6.2
    Reviewed-on: https://review.typo3.org/30910
    Reviewed-by: Olivier Dobberkau
    Reviewed-by: Sebastian Fischer
    Reviewed-by: Christian Kuhn
    Tested-by: Christian Kuhn

---
+++ typo3/sysext/openid/Configuration/TCA/Overrides/fe_users.php
@@ -0,0 +1,32 @@
+<?php
+
+// Prepare new columns for fe_users table
+$tempColumns = array(
+	'tx_openid_openid' => array(
+		'exclude' => 0,
+		'label' => 'LLL:EXT:openid/locallang_db.xlf:be_users.tx_openid_openid',
+		'config' => array(
+			'type' => 'input',
+			'size' => '30',
+			// Requirement: unique (BE users are unique in the whole system)
+			'eval' => 'trim,nospace,uniqueInPid',
+			'wizards' => array(
+				'_PADDING' => 2,
+				'0' => array(
+					'type' => 'popup',
+					'title' => 'Add OpenID',
+					'module' => array(
+						'name' => 'wizard_openid'
+					),
+					'icon' => 'EXT:openid/ext_icon.gif',
+					'JSopenParams' => ',width=600,height=400,status=0,menubar=0,scrollbars=0',
+				)
+			),
+		)
+	)
+);
+// Add new columns to fe_users table
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('fe_users', $tempColumns);
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addFieldsToAllPalettesOfField('fe_users', 'username', 'tx_openid_openid');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('fe_users', 'EXT:openid' . '/locallang_csh.xlf');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('_MOD_user_setup', 'EXT:openid/locallang_csh_mod.xlf');
