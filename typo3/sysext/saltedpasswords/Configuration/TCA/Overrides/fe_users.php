aef174f5fa8d9feaa81c9fae2553d2217f68c2dc
Anja Leichsenring <aleichsenring@ab-softlab.de>
2014-06-20 16:57:30 +0200

    [BUGFIX] Move TCA modification of saltedpasswords to TCA/Overrides
    TCA manipulation code must no longer reside in ext_tables.php, but
    moved to Configuration/TCA/Overrides/<tablename>.php. Leftovers
    in ext_tables might disturb loading order of TCA changes and spoil
    proper caching.
    
    Change-Id: I328cbcb718ee33beec19379ea6047a18cdc370b0
    Resolves: #59704
    Related: #59414
    Releases: 6.3, 6.2
    Reviewed-on: https://review.typo3.org/31351
    Reviewed-by: Wouter Wolters
    Tested-by: Wouter Wolters

---
+++ typo3/sysext/saltedpasswords/Configuration/TCA/Overrides/fe_users.php
@@ -0,0 +1,15 @@
+<?php
+defined('TYPO3_MODE') or die();
+
+$GLOBALS['TCA']['fe_users']['columns']['password']['config']['max'] = 100;
+if (\TYPO3\CMS\Saltedpasswords\Utility\SaltedPasswordsUtility::isUsageEnabled('FE')) {
+	// Get eval field operations methods as array keys
+	$operations = array_flip(\TYPO3\CMS\Core\Utility\GeneralUtility::trimExplode(',', $GLOBALS['TCA']['fe_users']['columns']['password']['config']['eval'], TRUE));
+	// Remove md5 and temporary password from the list of evaluated methods
+	unset($operations['md5'], $operations['password']);
+	// Append new methods to have "password" as last operation.
+	$operations['tx_saltedpasswords_eval_fe'] = 1;
+	$operations['password'] = 1;
+	$GLOBALS['TCA']['fe_users']['columns']['password']['config']['eval'] = implode(',', array_keys($operations));
+	unset($operations);
+}
