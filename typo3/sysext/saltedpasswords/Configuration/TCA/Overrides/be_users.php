ead385dd214d87808989fb5a6f1f8b2f94fa71a3
Anja Leichsenring <aleichsenring@ab-softlab.de>
2014-06-20 16:57:30 +0200

    [BUGFIX] Move TCA modification of saltedpasswords to TCA/Overrides
    TCA manipulation code must no longer reside in ext_tables.php, but
    moved to Configuration/TCA/Overrides/<tablename>.php. Leftovers
    in ext_tables might disturb loading order of TCA changes and spoil
    proper caching.
    
    Change-Id: I328cbcb718ee33beec19379ea6047a18cdc370b0
    Resolves: #59704
    Related: #59414
    Releases: 6.3, 6.2
    Reviewed-on: https://review.typo3.org/30943
    Reviewed-by: Christian Kuhn
    Tested-by: Christian Kuhn
    Reviewed-by: Markus Klein
    Reviewed-by: Marcin Sągol
    Reviewed-by: Wouter Wolters
    Tested-by: Wouter Wolters

---
+++ typo3/sysext/saltedpasswords/Configuration/TCA/Overrides/be_users.php
@@ -0,0 +1,15 @@
+<?php
+defined('TYPO3_MODE') or die();
+
+$GLOBALS['TCA']['be_users']['columns']['password']['config']['max'] = 100;
+
+// Backend configuration for saltedpasswords
+// Get eval field operations methods as array keys
+$operations = array_flip(\TYPO3\CMS\Core\Utility\GeneralUtility::trimExplode(',', $GLOBALS['TCA']['be_users']['columns']['password']['config']['eval'], TRUE));
+// Remove md5 and temporary password from the list of evaluated methods
+unset($operations['md5'], $operations['password']);
+// Append new methods to have "password" as last operation.
+$operations['tx_saltedpasswords_eval_be'] = 1;
+$operations['password'] = 1;
+$GLOBALS['TCA']['be_users']['columns']['password']['config']['eval'] = implode(',', array_keys($operations));
+unset($operations);
--- typo3/sysext/saltedpasswords/ext_tables.php
+++ typo3/sysext/saltedpasswords/ext_tables.php
@@ -2,29 +2,6 @@
 if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
-$GLOBALS['TCA']['fe_users']['columns']['password']['config']['max'] = 100;
-if (\TYPO3\CMS\Saltedpasswords\Utility\SaltedPasswordsUtility::isUsageEnabled('FE')) {
-	// Get eval field operations methods as array keys
-	$operations = array_flip(\TYPO3\CMS\Core\Utility\GeneralUtility::trimExplode(',', $GLOBALS['TCA']['fe_users']['columns']['password']['config']['eval'], TRUE));
-	// Remove md5 and temporary password from the list of evaluated methods
-	unset($operations['md5'], $operations['password']);
-	// Append new methods to have "password" as last operation.
-	$operations['tx_saltedpasswords_eval_fe'] = 1;
-	$operations['password'] = 1;
-	$GLOBALS['TCA']['fe_users']['columns']['password']['config']['eval'] = implode(',', array_keys($operations));
-	unset($operations);
-}
-$GLOBALS['TCA']['be_users']['columns']['password']['config']['max'] = 100;
 
-// Backend configuration for saltedpasswords
-// Get eval field operations methods as array keys
-$operations = array_flip(\TYPO3\CMS\Core\Utility\GeneralUtility::trimExplode(',', $GLOBALS['TCA']['be_users']['columns']['password']['config']['eval'], TRUE));
-// Remove md5 and temporary password from the list of evaluated methods
-unset($operations['md5'], $operations['password']);
-// Append new methods to have "password" as last operation.
-$operations['tx_saltedpasswords_eval_be'] = 1;
-$operations['password'] = 1;
-$GLOBALS['TCA']['be_users']['columns']['password']['config']['eval'] = implode(',', array_keys($operations));
-unset($operations);
 // Add context sensitive help (csh) for scheduler task
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('_txsaltedpasswords', 'EXT:' . $_EXTKEY . '/locallang_csh_saltedpasswords.xlf');
