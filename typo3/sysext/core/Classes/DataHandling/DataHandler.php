2d24474257e7603983275f8f5ceb3d919bff4818
Marc Bastian Heinrichs <typo3@mbh-software.de>
2014-06-21 15:49:34 +0200

    [TASK] Use resource API to deleted files from uploads in DataHandler
    The DataHandler deletes files from the uploads folder. Since also a
    sys_file record could exist for such file, the resource API should be
    used to delete it.
    
    Resolves: #58967
    Releases: master, 6.2
    Change-Id: I6c0b3683657e1c4da5f9ab0a2016e33b1e472f62
    Reviewed-on: http://review.typo3.org/31001
    Reviewed-by: Michael Oehlhof <typo3@oehlhof.de>
    Reviewed-by: Philipp Gampe <philipp.gampe@typo3.org>
    Reviewed-by: Frans Saris <franssaris@gmail.com>
    Tested-by: Frans Saris <franssaris@gmail.com>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>

--- typo3/sysext/core/Classes/DataHandling/DataHandler.php
+++ typo3/sysext/core/Classes/DataHandling/DataHandler.php
@@ -4415,7 +4415,8 @@ class DataHandler {
 		if (is_array($files) && $dsArr['TCEforms']['config']['internal_type'] === 'file') {
 			foreach ($files as $dat) {
 				if (@is_file($dat['ID_absFile'])) {
-					unlink($dat['ID_absFile']);
+					$file = $this->getResourceFactory()->retrieveFileOrFolderObject($dat['ID_absFile']);
+					$file->delete();
 				} else {
 					$this->log('', 0, 3, 0, 100, 'Delete: Referenced file \'' . $dat['ID_absFile'] . '\' that was supposed to be deleted together with its record which didn\'t exist');
 				}
@@ -6562,7 +6563,10 @@ class DataHandler {
 	 */
 	public function removeRegisteredFiles() {
 		foreach ($this->removeFilesStore as $file) {
-			unlink($file);
+			if (@is_file($file)) {
+				$file = $this->getResourceFactory()->retrieveFileOrFolderObject($file);
+				$file->delete();
+			}
 		}
 	}
 
@@ -6811,8 +6815,10 @@ class DataHandler {
 				if ($theFile) {
 					switch ($func) {
 						case 'deleteAll':
-							if (@is_file(($uploadPath . '/' . $theFile))) {
-								unlink($uploadPath . '/' . $theFile);
+							$theFileFullPath = $uploadPath . '/' . $theFile;
+							if (@is_file($theFileFullPath)) {
+								$file = $this->getResourceFactory()->retrieveFileOrFolderObject($theFileFullPath);
+								$file->delete();
 							} else {
 								$this->log($table, 0, 3, 0, 100, 'Delete: Referenced file that was supposed to be deleted together with it\'s record didn\'t exist');
 							}
@@ -7474,4 +7480,11 @@ class DataHandler {
 		return GeneralUtility::makeInstance(CacheManager::class);
 	}
 
+	/**
+	 * Gets the resourceFactory
+	 * @return ResourceFactory
+	 */
+	protected function getResourceFactory() {
+		return \TYPO3\CMS\Core\Resource\ResourceFactory::getInstance();
+	}
 }
