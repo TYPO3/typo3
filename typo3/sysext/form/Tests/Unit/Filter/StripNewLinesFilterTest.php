5f608be7b283750b760f7674b4e9101c3a40fd33
Michael Schams <typo3@2014.trash.schams.net>
2014-06-22 15:21:16 +1000

    [TASK] Re-work/simplify copyright header in PHP files - Part 5
    This patch replaces the copyright/license header in PHP files with a
    new, simplified one. The new header does not show the year figure, nor
    an author name, and refers to the LICENSE.txt file for the full
    copyright information. License is: GPL2 or any later version.
    
    This is a multi-part commit due to the huge number of changed files.
    See issue #59781 for further details.
    
    Resolves: #59781
    Releases: 6.3, 6.2
    Change-Id: I7429b8e9c507a4833215750268d1975402679828
    Reviewed-on: https://review.typo3.org/31026
    Reviewed-by: Krzysztof Adamczyk
    Tested-by: Krzysztof Adamczyk
    Reviewed-by: Markus Klein
    Tested-by: Markus Klein

--- typo3/sysext/form/Tests/Unit/Filter/StripNewLinesFilterTest.php
+++ typo3/sysext/form/Tests/Unit/Filter/StripNewLinesFilterTest.php
@@ -1,29 +1,18 @@
 <?php
 namespace TYPO3\CMS\Form\Tests\Unit\Filter;
 
-/***************************************************************
- *  Copyright notice
+/**
+ * This file is part of the TYPO3 CMS project.
  *
- *  (c) 2012-2013 Andreas Lappe <nd@kaeufli.ch>, kaeufli.ch
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
  *
- *  All rights reserved
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
  *
- *  This script is part of the TYPO3 project. The TYPO3 project is
- *  free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  The GNU General Public License can be found at
- *  http://www.gnu.org/copyleft/gpl.html.
- *
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  This copyright notice MUST APPEAR in all copies of the script!
- ***************************************************************/
+ * The TYPO3 project - inspiring people to share!
+ */
 
 /**
  * Test case
