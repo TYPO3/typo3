62f120b9c0929607d9b6f9e0089ef1fe9bf868ba
Michael Schams <typo3@2014.trash.schams.net>
2014-06-22 15:14:02 +1000

    [TASK] Re-work/simplify copyright header in PHP files - Part 3
    This patch replaces the copyright/license header in PHP files with a
    new, simplified one. The new header does not show the year figure, nor
    an author name, and refers to the LICENSE.txt file for the full
    copyright information. License is: GPL2 or any later version.
    
    This is a multi-part commit due to the huge number of changed files.
    See issue #59779 for further details.
    
    Resolves: #59779
    Releases: 6.3, 6.2
    Change-Id: I30ad23ccfccd86ba9e98a3b46b11836c1628f499
    Reviewed-on: https://review.typo3.org/31050
    Reviewed-by: Markus Klein
    Tested-by: Markus Klein

--- typo3/sysext/core/Classes/Package/Exception/PackageManagerCacheUnavailableException.php
+++ typo3/sysext/core/Classes/Package/Exception/PackageManagerCacheUnavailableException.php
@@ -1,8 +1,18 @@
 <?php
 namespace TYPO3\CMS\Core\Package\Exception;
 
-// Copyright header is left out here by intention and should be restored once
-// this file is committed.
+/**
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
 
 /**
  * A package cache unavailable exception
