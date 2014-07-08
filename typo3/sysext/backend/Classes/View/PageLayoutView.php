b1c86f2aaf5aae73bcf194973dcf6c4eab61a9b8
Nicole Cordes <typo3@cordes.co>
2014-07-08 09:53:33 +0200

    [BUGFIX] Movements pollute colPos value of content elements
    Due to commit I148ca1b023226f2f99417b3baf238b72346e721f the information
    concerning previous and next content elements in one row is messed up.
    This patch helps to build information which depends on colPos again and
    prevents records being moved to another column.
    
    Resolves: #48939
    Resolves: #49055
    Releases: 6.2, 6.1, 6.0, 4.7, 4.5
    Change-Id: I3a15321ee11a1f7d96b58b8b7a5ab14098664b22
    Reviewed-on: https://review.typo3.org/31493
    Reviewed-by: Oliver Hader
    Tested-by: Oliver Hader

--- typo3/sysext/backend/Classes/View/PageLayoutView.php
+++ typo3/sysext/backend/Classes/View/PageLayoutView.php
@@ -443,6 +443,7 @@ class PageLayoutView extends \TYPO3\CMS\Recordlist\RecordList\AbstractDatabaseRe
 					';
 					$editUidList = '';
 					$rowArr = $contentRecordsPerColumn[$key];
+					$this->generateTtContentDataArray($rowArr);
 					foreach ((array) $rowArr as $rKey => $row) {
 						if ($this->tt_contentConfig['languageMode']) {
 							$languageColumn[$key][$lP] = $head[$key] . $content[$key];
@@ -690,6 +691,7 @@ class PageLayoutView extends \TYPO3\CMS\Recordlist\RecordList\AbstractDatabaseRe
 				foreach ($cList as $counter => $key) {
 					$c = 0;
 					$rowArr = $contentRecordsPerColumn[$key];
+					$this->generateTtContentDataArray($rowArr);
 					$numberOfContentElementsInColumn = count($rowArr);
 					$rowOut = '';
 					// If it turns out that there are not content elements in the column, then display a big button which links directly to the wizard script:
@@ -1575,11 +1577,6 @@ class PageLayoutView extends \TYPO3\CMS\Recordlist\RecordList\AbstractDatabaseRe
 	 * @todo Define visibility
 	 */
 	public function getResult($result, $table = 'tt_content') {
-		// Initialize:
-		$editUidList = '';
-		$recs = array();
-		$nextTree = $this->nextThree;
-		$c = 0;
 		$output = array();
 		// Traverse the result:
 		while ($row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($result)) {
@@ -1587,26 +1584,9 @@ class PageLayoutView extends \TYPO3\CMS\Recordlist\RecordList\AbstractDatabaseRe
 			if ($row) {
 				// Add the row to the array:
 				$output[] = $row;
-				// Set an internal register:
-				$recs[$c] = $row['uid'];
-				// Create the list of the next three ids (for editing links...)
-				for ($a = 0; $a < $nextTree; $a++) {
-					if (isset($recs[$c - $a]) && !\TYPO3\CMS\Core\Utility\GeneralUtility::inList($this->tt_contentData['nextThree'][$recs[$c - $a]], $row['uid'])) {
-						$this->tt_contentData['nextThree'][$recs[$c - $a]] .= $row['uid'] . ',';
-					}
-				}
-				// Set next/previous ids:
-				if (isset($recs[$c - 1])) {
-					if (isset($recs[$c - 2])) {
-						$this->tt_contentData['prev'][$row['uid']] = -$recs[($c - 2)];
-					} else {
-						$this->tt_contentData['prev'][$row['uid']] = $row['pid'];
-					}
-					$this->tt_contentData['next'][$recs[$c - 1]] = -$row['uid'];
-				}
-				$c++;
 			}
 		}
+		$this->generateTtContentDataArray($output);
 		// Return selected records
 		return $output;
 	}
@@ -1616,6 +1596,40 @@ class PageLayoutView extends \TYPO3\CMS\Recordlist\RecordList\AbstractDatabaseRe
 	 * Various helper functions
 	 *
 	 ********************************/
+	/**
+	 * Generates the data for previous and next elements which is needed for movements.
+	 *
+	 * @param array $rowArray
+	 * @return void
+	 */
+	protected function generateTtContentDataArray(array $rowArray) {
+		$this->tt_contentData = array(
+			'nextThree' => array(),
+			'next' => array(),
+			'prev' => array(),
+		);
+		foreach ($rowArray as $key => $value) {
+			// Create the list of the next three ids (for editing links...)
+			for ($i = 0; $i < $this->nextThree; $i++) {
+				if (isset($rowArray[$key - $i])
+					&& !\TYPO3\CMS\Core\Utility\GeneralUtility::inList($this->tt_contentData['nextThree'][$rowArray[$key - $i]['uid']], $value['uid'])
+				) {
+					$this->tt_contentData['nextThree'][$rowArray[$key - $i]['uid']] .= $value['uid'] . ',';
+				}
+			}
+
+			// Create information for next and previous content elements
+			if (isset($rowArray[$key - 1])) {
+				if (isset($rowArray[$key - 2])) {
+					$this->tt_contentData['prev'][$value['uid']] = -$rowArray[$key - 2]['uid'];
+				} else {
+					$this->tt_contentData['prev'][$value['uid']] = $value['pid'];
+				}
+				$this->tt_contentData['next'][$rowArray[$key - 1]['uid']] = -$value['uid'];
+			}
+		}
+	}
+
 	/**
 	 * Counts and returns the number of records on the page with $pid
 	 *
