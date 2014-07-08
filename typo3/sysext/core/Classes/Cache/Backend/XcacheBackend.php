3cbad6cdec96475eaaa5d9b1eeb24517833a735c
Markus Klein <klein.t3@mfc-linz.at>
2014-07-08 23:00:10 +0200

    [BUGFIX] Followup to CacheBackend patch
    Re-add a missing use statement that vanished during rebase.
    
    Resolves: #59587
    Releases: 6.3, 6.2, 6.1
    Change-Id: I16ac1b9d5a00c4300202eaef493de43fc988a81d
    Reviewed-on: https://review.typo3.org/31520
    Reviewed-by: Wouter Wolters
    Tested-by: Wouter Wolters

--- typo3/sysext/core/Classes/Cache/Backend/XcacheBackend.php
+++ typo3/sysext/core/Classes/Cache/Backend/XcacheBackend.php
@@ -14,6 +14,8 @@ namespace TYPO3\CMS\Core\Cache\Backend;
  * The TYPO3 project - inspiring people to share!
  */
 
+use TYPO3\CMS\Core\Cache\Exception;
+
 /**
  * A caching backend which stores cache entries by using xcache opcode cache.
  *
