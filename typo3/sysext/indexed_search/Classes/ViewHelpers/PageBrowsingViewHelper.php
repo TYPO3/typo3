905404fd432e1fd18507087b883ee7feba98e592
Markus Klein <klein.t3@reelworx.at>
2014-07-15 19:30:24 +0200

    [BUGFIX] Fix indexed_search page browsing calculation
    Fix the calculation of pages shown by the indexed_search page browsers
    to avoid negative page numbers or numbers exceeding the maximum available
    page number.
    
    Resolves: #60346
    Releases: master, 6.2
    Change-Id: I97de1c1afe42e29d450733f6741bca049f9803f6
    Reviewed-on: http://review.typo3.org/32992
    Reviewed-by: Markus Klein <klein.t3@reelworx.at>
    Tested-by: Markus Klein <klein.t3@reelworx.at>

--- typo3/sysext/indexed_search/Classes/ViewHelpers/PageBrowsingViewHelper.php
+++ typo3/sysext/indexed_search/Classes/ViewHelpers/PageBrowsingViewHelper.php
@@ -31,50 +31,59 @@ class PageBrowsingViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractVi
 	/**
 	 * Main render function
 	 *
-	 * @param integer $maximumNumberOfResultPages
-	 * @param integer $numberOfResults
-	 * @param integer $resultsPerPage
-	 * @param integer $currentPage
-	 * @param string|NULL $freeIndexUid
+	 * @param integer $maximumNumberOfResultPages The number of page links shown
+	 * @param integer $numberOfResults Total number of results
+	 * @param integer $resultsPerPage Number of results per page
+	 * @param integer $currentPage The current page starting with 0
+	 * @param string|NULL $freeIndexUid List of integers pointing to free indexing configurations to search. -1 represents no filtering, 0 represents TYPO3 pages only, any number above zero is a uid of an indexing configuration!
 	 * @return string The content
 	 */
-	public function render($maximumNumberOfResultPages, $numberOfResults, $resultsPerPage, $currentPage = 1, $freeIndexUid = NULL) {
+	public function render($maximumNumberOfResultPages, $numberOfResults, $resultsPerPage, $currentPage = 0, $freeIndexUid = NULL) {
 		$pageCount = ceil($numberOfResults / $resultsPerPage);
-		$content = '';
 		// only show the result browser if more than one page is needed
-		if ($pageCount > 1) {
-			$currentPage = (int)$currentPage;
-			// prev page
-			// show on all pages after the 1st one
-			if ($currentPage > 0) {
-				$label = \TYPO3\CMS\Extbase\Utility\LocalizationUtility::translate('displayResults.previous', 'IndexedSearch');
-				$content .= '<li>' . $this->makecurrentPageSelector_link($label, $currentPage - 1, $freeIndexUid) . '</li>';
-			}
-			$maximumNumberOfResultPages = \TYPO3\CMS\Core\Utility\MathUtility::forceIntegerInRange($maximumNumberOfResultPages, 1, 200000000, 10);
-			$min = max(0, $currentPage + 1 - ceil($maximumNumberOfResultPages / 2));
-			$max = $min + $maximumNumberOfResultPages;
-			if ($max > $pageCount) {
-				$min -= $max - $pageCount;
-			}
-			for ($a = $min; $a < $pageCount && $a < $max; $a++) {
-				$label = \TYPO3\CMS\Extbase\Utility\LocalizationUtility::translate('displayResults.page', 'IndexedSearch');
-				$label = trim($label . ' ' . ($a + 1));
-				$label = $this->makecurrentPageSelector_link($label, $a, $freeIndexUid);
-				if ($a === $currentPage) {
-					$content .= '<li class="tx-indexedsearch-browselist-currentPage"><strong>' . $label . '</strong></li>';
-				} else {
-					$content .= '<li>' . $label . '</li>';
-				}
-			}
-			// next link
-			if ($currentPage + 1 < $pageCount) {
-				$label = \TYPO3\CMS\Extbase\Utility\LocalizationUtility::translate('displayResults.next', 'IndexedSearch');
-				$content .= '<li>' . $this->makecurrentPageSelector_link($label, ($currentPage + 1), $freeIndexUid) . '</li>';
-			}
-			$content = '<ul class="tx-indexedsearch-browsebox">' . $content . '</ul>';
+		if ($pageCount === 1) {
+			return '';
 		}
 
-		return $content;
+		// Check if $currentPage is in range
+		$currentPage = \TYPO3\CMS\Core\Utility\MathUtility::forceIntegerInRange($currentPage, 0, $pageCount - 1);
+
+		$content = '';
+		// prev page
+		// show on all pages after the 1st one
+		if ($currentPage > 0) {
+			$label = \TYPO3\CMS\Extbase\Utility\LocalizationUtility::translate('displayResults.previous', 'IndexedSearch');
+			$content .= '<li>' . $this->makecurrentPageSelector_link($label, $currentPage - 1, $freeIndexUid) . '</li>';
+		}
+		// Check if $maximumNumberOfResultPages is in range
+		$maximumNumberOfResultPages = \TYPO3\CMS\Core\Utility\MathUtility::forceIntegerInRange($maximumNumberOfResultPages, 1, $pageCount, 10);
+		// Assume $currentPage is in the middle and calculate the index limits of the result page listing
+		$minPage = $currentPage - floor($maximumNumberOfResultPages / 2);
+		$maxPage = $minPage + $maximumNumberOfResultPages - 1;
+		// Check if the indexes are within the page limits
+		if ($minPage < 0) {
+			$maxPage -= $minPage;
+			$minPage = 0;
+		} elseif ($maxPage >= $pageCount) {
+			$minPage -= $maxPage - $pageCount + 1;
+			$maxPage = $pageCount - 1;
+		}
+		$pageLabel = \TYPO3\CMS\Extbase\Utility\LocalizationUtility::translate('displayResults.page', 'IndexedSearch');
+		for ($a = $minPage; $a <= $maxPage; $a++) {
+			$label = trim($pageLabel . ' ' . ($a + 1));
+			$label = $this->makecurrentPageSelector_link($label, $a, $freeIndexUid);
+			if ($a === $currentPage) {
+				$content .= '<li class="tx-indexedsearch-browselist-currentPage"><strong>' . $label . '</strong></li>';
+			} else {
+				$content .= '<li>' . $label . '</li>';
+			}
+		}
+		// next link
+		if ($currentPage < $pageCount - 1) {
+			$label = \TYPO3\CMS\Extbase\Utility\LocalizationUtility::translate('displayResults.next', 'IndexedSearch');
+			$content .= '<li>' . $this->makecurrentPageSelector_link($label, ($currentPage + 1), $freeIndexUid) . '</li>';
+		}
+		return '<ul class="tx-indexedsearch-browsebox">' . $content . '</ul>';
 	}
 
 	/**
