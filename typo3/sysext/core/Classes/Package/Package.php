e6f3ab7e826d34ffd1e17a3f461de0d4e4c4a6fd
Peter Niederlag <peter.niederlag@typo3.org>
2014-07-24 12:50:24 +0200

    [BUGFIX] Make call to isPackageKeyValid non-static
    Function isPackagKeyValid on PackageManeger is not a static function
    and must be called on an object instance.
    
    Resolves: #59619
    Releases: 6.3, 6.2
    Change-Id: I03b068db00ccdf76b6e8658721e630b7de6ebfd8
    Reviewed-on: http://review.typo3.org/31778
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>

--- typo3/sysext/core/Classes/Package/Package.php
+++ typo3/sysext/core/Classes/Package/Package.php
@@ -70,7 +70,7 @@ class Package extends \TYPO3\Flow\Package\Package implements PackageInterface {
 	 * @throws \TYPO3\Flow\Package\Exception\InvalidPackageManifestException if no composer manifest file could be found
 	 */
 	public function __construct(\TYPO3\Flow\Package\PackageManager $packageManager, $packageKey, $packagePath, $classesPath = NULL, $manifestPath = '') {
-		if (!\TYPO3\CMS\Core\Package\PackageManager::isPackageKeyValid($packageKey)) {
+		if (!$packageManager->isPackageKeyValid($packageKey)) {
 			throw new \TYPO3\Flow\Package\Exception\InvalidPackageKeyException('"' . $packageKey . '" is not a valid package key.', 1217959511);
 		}
 		if (!(@is_dir($packagePath) || (\TYPO3\Flow\Utility\Files::is_link($packagePath) && is_dir(\TYPO3\Flow\Utility\Files::getNormalizedPath($packagePath))))) {
