82c886a33a167d92cc8d6c0bc623d45369bf2ccd
Helmut Hummel <helmut.hummel@typo3.org>
2014-07-27 10:55:35 +0200

    [BUGFIX] Fix Extbase language fallback in query parser
    Currently when TYPO3 is configured to do language fallback
    and a record is translated in language A, but not
    in language B and language B is requested, then this
    record is excluded.
    
    This is the case because of a wrong subselect condition.
    
    Extbase selects records in the requested translation
    or if no translation is available in the default language.
    However the check if translation is available looks
    for *any* translation not only for a translation in the
    requested language. Thus the record from the default
    language is not selected at all if there are translations
    available in any other language.
    
    Solution is to change the subselect condition to check
    for the currently requested language.
    
    Releases: 6.1, 6.2, master
    Resolves: #60613
    Change-Id: I8ebd68e1f5741d3557910ae2f8c2d19474548d01
    Reviewed-on: http://review.typo3.org/31859
    Reviewed-by: Susanne Moog <typo3@susannemoog.de>
    Tested-by: Susanne Moog <typo3@susannemoog.de>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>

--- typo3/sysext/extbase/Tests/Unit/Persistence/Generic/Storage/Typo3DbQueryParserTest.php
+++ typo3/sysext/extbase/Tests/Unit/Persistence/Generic/Storage/Typo3DbQueryParserTest.php
@@ -115,7 +115,7 @@ class Typo3DbQueryParserTest extends \TYPO3\CMS\Core\Tests\UnitTestCase {
 		$querySettings->setLanguageUid(2);
 		$mockTypo3DbQueryParser = $this->getAccessibleMock('TYPO3\\CMS\\Extbase\\Persistence\\Generic\\Storage\\Typo3DbQueryParser', array('dummy'), array(), '', FALSE);
 		$mockTypo3DbQueryParser->_callRef('addSysLanguageStatement', $table, $sql, $querySettings);
-		$expectedSql = array('additionalWhereClause' => array('(' . $table . '.sys_language_uid IN (2,-1) OR (' . $table . '.sys_language_uid=0 AND ' . $table . '.uid NOT IN (SELECT ' . $table . '.l10n_parent FROM ' . $table . ' WHERE ' . $table . '.l10n_parent>0 AND ' . $table . '.sys_language_uid>0)))'));
+		$expectedSql = array('additionalWhereClause' => array('(' . $table . '.sys_language_uid IN (2,-1) OR (' . $table . '.sys_language_uid=0 AND ' . $table . '.uid NOT IN (SELECT ' . $table . '.l10n_parent FROM ' . $table . ' WHERE ' . $table . '.l10n_parent>0 AND ' . $table . '.sys_language_uid=2)))'));
 		$this->assertSame($expectedSql, $sql);
 	}
 
@@ -139,7 +139,7 @@ class Typo3DbQueryParserTest extends \TYPO3\CMS\Core\Tests\UnitTestCase {
 				' OR (' . $table . '.sys_language_uid=0 AND ' . $table . '.uid NOT IN (' .
 				'SELECT ' . $table . '.l10n_parent FROM ' . $table .
 				' WHERE ' . $table . '.l10n_parent>0 AND ' .
-				$table . '.sys_language_uid>0 AND ' .
+				$table . '.sys_language_uid=2 AND ' .
 				$table . '.deleted=0)))')
 		);
 		$this->assertSame($expectedSql, $sql);
@@ -166,7 +166,7 @@ class Typo3DbQueryParserTest extends \TYPO3\CMS\Core\Tests\UnitTestCase {
 				' OR (' . $table . '.sys_language_uid=0 AND ' . $table . '.uid NOT IN (' .
 				'SELECT ' . $table . '.l10n_parent FROM ' . $table .
 				' WHERE ' . $table . '.l10n_parent>0 AND ' .
-				$table . '.sys_language_uid>0 AND ' .
+				$table . '.sys_language_uid=2 AND ' .
 				$table . '.deleted=0)))')
 		);
 		$this->assertSame($expectedSql, $sql);
