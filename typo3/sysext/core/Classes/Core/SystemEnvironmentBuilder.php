38921060793e3fedcb9746c2befd9e08a963788f
Alexander Bohndorf <bohndorf@web.de>
2014-09-22 09:46:35 +0200

    [BUGFIX] Allow lower case for drive letters on Windows in CLI scripts
    Resolves: #61749
    Releases: master, 6.2, 6.1
    Change-Id: I88a3489311e8f94669876ddbe59589f68cd5708e
    Reviewed-on: http://review.typo3.org/33320
    Reviewed-by: Markus Klein <klein.t3@reelworx.at>
    Tested-by: Markus Klein <klein.t3@reelworx.at>

--- typo3/sysext/core/Classes/Core/SystemEnvironmentBuilder.php
+++ typo3/sysext/core/Classes/Core/SystemEnvironmentBuilder.php
@@ -381,7 +381,7 @@ class SystemEnvironmentBuilder {
 		// Find out if path is relative or not
 		$isRelativePath = FALSE;
 		if (TYPO3_OS === 'WIN') {
-			if (!preg_match('/^([A-Z]:)?\\\\/', $scriptPath)) {
+			if (!preg_match('/^([a-zA-Z]:)?\\\\/', $scriptPath)) {
 				$isRelativePath = TRUE;
 			}
 		} else {
