be671f76c8362be1ffd2b7356b66c073a4ac46e4
Felix Kopp <felix-source@phorax.com>
2014-09-26 00:15:31 +0200

    [TASK] Sys_note: Bring headline & delete action icon
    To finalze the rebrush of sys notes a section headline is added:
    The delete action can be accessed right from the note.
    
    Resolves: #26796
    Releases: master
    Change-Id: I80923ee523d21880d877a0e1cc9cc2f355f59b3a
    Reviewed-on: http://review.typo3.org/32980
    Reviewed-by: Felix Kopp <felix-source@phorax.com>
    Tested-by: Felix Kopp <felix-source@phorax.com>
    Reviewed-by: Markus Klein <klein.t3@reelworx.at>
    Tested-by: Markus Klein <klein.t3@reelworx.at>

---
+++ typo3/sysext/sys_note/Classes/ViewHelpers/DeleteLinkViewHelper.php
@@ -0,0 +1,35 @@
+<?php
+namespace TYPO3\CMS\SysNote\ViewHelpers;
+
+/**
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+/**
+ * ViewHelper to create a link to delete a note
+ */
+class DeleteLinkViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper {
+
+	/**
+	 * Create link to delete a note
+	 *
+	 * @param integer $id uid of the note
+	 * @return string link
+	 */
+	public function render($id) {
+		$redirectUrl = \TYPO3\CMS\Core\Utility\GeneralUtility::getIndpEnv('REQUEST_URI');
+		$parameters = 'cmd[sys_note][' . $id . '][delete]=1';
+		$url = $GLOBALS['BACK_PATH'] . 'tce_db.php?&' . $parameters . '&redirect=' . ($redirectUrl == '' ? '\' + T3_THIS_LOCATION + \'' : rawurlencode($redirectUrl)) . \TYPO3\CMS\Backend\Utility\BackendUtility::getUrlToken('tceAction');
+		return $url;
+	}
+
+}
