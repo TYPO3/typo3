5a7d181030950084218be1ef92189ba5e9022533
Christian Kuhn <lolli@schwarzbu.ch>
2014-10-03 01:55:39 +0200

    [TASK] styleguide: Set extension compatible with core version 7

--- typo3/sysext/styleguide/ext_emconf.php
+++ typo3/sysext/styleguide/ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '0.8.0',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2.0-6.2.99',
+			'typo3' => '6.2.0-7.99.999',
 		),
 		'conflicts' => array(
 		),
