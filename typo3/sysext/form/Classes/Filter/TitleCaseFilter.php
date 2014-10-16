a56a0b649550f7a77ca59d2b313a67dd0f126e06
Graeme Worthy <graeme@workben.ch>
2014-10-16 12:50:44 +0200

    [BUGFIX] Handle utf8 strings in TitleCaseFilter
    Use proper charset aware string conversion API to handle
    the value given to the TitleCaseFilter.
    
    Resolves: #62279
    Releases: master, 6.2
    Change-Id: I500af9f1fb6769ea35197e19b2a0a7876d7eb736
    Reviewed-on: http://review.typo3.org/33335
    Reviewed-by: Markus Klein <klein.t3@reelworx.at>
    Tested-by: Markus Klein <klein.t3@reelworx.at>
    Reviewed-by: Philipp Gampe <philipp.gampe@typo3.org>
    Tested-by: Philipp Gampe <philipp.gampe@typo3.org>

--- typo3/sysext/form/Classes/Filter/TitleCaseFilter.php
+++ typo3/sysext/form/Classes/Filter/TitleCaseFilter.php
@@ -13,13 +13,14 @@ namespace TYPO3\CMS\Form\Filter;
  *
  * The TYPO3 project - inspiring people to share!
  */
+use TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController;
 
 /**
  * Title filter
  *
  * @author Patrick Broens <patrick@patrickbroens.nl>
  */
-class TitleCaseFilter implements \TYPO3\CMS\Form\Filter\FilterInterface {
+class TitleCaseFilter implements FilterInterface {
 
 	/**
 	 * Convert alphabetic characters to title case
@@ -28,7 +29,15 @@ class TitleCaseFilter implements \TYPO3\CMS\Form\Filter\FilterInterface {
 	 * @return string
 	 */
 	public function filter($value) {
-		return ucwords(strtolower((string) $value));
+		$tsfe = $this->getTypoScriptFrontendController();
+		$lower = $tsfe->csConvObj->conv_case($tsfe->renderCharset, $value, 'toLower');
+		return ucwords($lower);
 	}
 
+	/**
+	 * @return TypoScriptFrontendController
+	 */
+	protected function getTypoScriptFrontendController() {
+		return $GLOBALS['TSFE'];
+	}
 }
