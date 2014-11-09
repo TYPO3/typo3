6fbbf736eda63b7ccd4e7209057b5f8cb6acd304
Georg Ringer <georg.ringer@gmail.com>
2014-11-09 18:48:20 +0100

    [TASK] Remove StyleGenerationHook in t3skin
    This hook is called inside t3skin and not used anymore because
    styling is now done in a better way.
    Also classes like bgColor5 are not used anymore.
    
    Resolves: #62797
    Releases: master
    Change-Id: Idc6e18030c8d96daf1577f51640252fe0ce14d4e
    Reviewed-on: http://review.typo3.org/33924
    Reviewed-by: Wouter Wolters <typo3@wouterwolters.nl>
    Tested-by: Wouter Wolters <typo3@wouterwolters.nl>
    Reviewed-by: Frank Nägler <typo3@naegler.net>
    Tested-by: Frank Nägler <typo3@naegler.net>

--- typo3/sysext/t3skin/ext_localconf.php
+++ typo3/sysext/t3skin/ext_localconf.php
@@ -5,7 +5,6 @@ defined('TYPO3_MODE') or die();
 	RTE.default.skin = EXT:' . $_EXTKEY . '/rtehtmlarea/htmlarea.css
 	RTE.default.FE.skin = EXT:' . $_EXTKEY . '/rtehtmlarea/htmlarea.css
 ');
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['typo3/template.php']['preStartPageHook']['TYPO3\\CMS\\T3skin\\Hook\\StyleGenerationHook'] = 'TYPO3\\CMS\\T3skin\\Hook\\StyleGenerationHook->preStartPageHook';
 
 \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\SignalSlot\\Dispatcher')->connect(
 	'TYPO3\\CMS\\Backend\\Utility\\IconUtility',
