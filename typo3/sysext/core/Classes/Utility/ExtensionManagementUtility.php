0551d35ceaca0088e25250a596dede73dc78a299
Xavier Perseguers <xavier@typo3.org>
2014-11-10 19:07:04 +0100

    [BUGFIX] Wrong hint in deprecation log for usage of feInterface
    Any extension may set parameter $addTofeInterface to 1 in the
    corresponding ext_tables.php file.
    
    Assumption that only EXT:statictemplates was setting this parameter
    makes it hard to fix faulty third-party extensions.
    
    Releases: master, 6.2
    Resolves: #62835
    Change-Id: Ib2e75a618eacfb24df8f532f30aa2b55c29597fb
    Reviewed-on: http://review.typo3.org/33960
    Reviewed-by: Xavier Perseguers <xavier@typo3.org>
    Tested-by: Xavier Perseguers <xavier@typo3.org>
    Reviewed-on: http://review.typo3.org/33963

--- typo3/sysext/core/Classes/Utility/ExtensionManagementUtility.php
+++ typo3/sysext/core/Classes/Utility/ExtensionManagementUtility.php
@@ -251,7 +251,7 @@ class ExtensionManagementUtility {
 			$GLOBALS['TCA'][$table]['columns'] = array_merge($GLOBALS['TCA'][$table]['columns'], $columnArray);
 			if ($addTofeInterface) {
 				GeneralUtility::deprecationLog(
-					'Usage of feInterface is no longer part of the TYPO3 CMS Core. Please check EXT:statictemplates.'
+					'Usage of feInterface is no longer part of the TYPO3 CMS Core. Please check EXT:' . $GLOBALS['_EXTKEY'] . '.'
 				);
 			}
 		}
