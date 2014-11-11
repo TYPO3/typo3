c29a725415ce58429ffaf858b8bef240b4847816
Frederic Gaus <gaus@flagbit.de>
2014-11-11 19:35:36 +0100

    [BUGFIX] Wrong generation of metadata in FAL
    This patch fixes the generation of metadata if the metadata
    of a file is missing in sys_file_metadata. Without this patch
    the metadata is generated without taking care of images and their
    width and height. As a result, images are generated with
    width=0 height=0. See bugreport for details.
    
    The change also introduces the basic structure to fetch
    general info of files via the SplFileInfo objects API.
    
    Resolves: #62829
    Releases: master, 6.2
    Change-Id: Ie99fc74f3439cef5dbd3702e57596b37390da679
    Reviewed-on: http://review.typo3.org/36454
    Reviewed-by: Anja Leichsenring <aleichsenring@ab-softlab.de>
    Tested-by: Anja Leichsenring <aleichsenring@ab-softlab.de>

---
+++ typo3/sysext/core/Tests/Unit/Type/File/ImageInfoTest.php
@@ -0,0 +1,33 @@
+<?php
+namespace TYPO3\CMS\Core\Tests\Unit\Type\File;
+
+/**
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use TYPO3\CMS\Core\Tests\UnitTestCase;
+
+/**
+ * Test case
+ */
+class ImageInfoTest extends UnitTestCase {
+
+	/**
+	 * @test
+	 */
+	public function classImageInfoCanBeInstantiated() {
+		$className = 'TYPO3\CMS\Core\Type\File\ImageInfo';
+		$classInstance = new \TYPO3\CMS\Core\Type\File\ImageInfo('FooFileName');
+		$this->assertInstanceOf($className, $classInstance);
+	}
+
+}
