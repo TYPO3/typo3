ea83a61a2111f92cd74cdca32d1cc2fd02d45965
Benjamin Mack <benni@typo3.org>
2014-11-13 10:46:10 +0100

    Revert "[TASK] Replace DynTabMenu with jQuery/RequireJS plugin"
    This reverts commit c362ba52cf04d4bf93a5043be7fc39c27671cbcf
    because it breaks the RTE
    
    Change-Id: Icf9d3ce1f21b494732bebc45791dd19c42a466a3
    Reviewed-on: http://review.typo3.org/34045
    Reviewed-by: Stefan Froemken <froemken@gmail.com>
    Tested-by: Stefan Froemken <froemken@gmail.com>

--- typo3/sysext/backend/Resources/Public/JavaScript/tabmenu.js
+++ typo3/sysext/backend/Resources/Public/JavaScript/tabmenu.js
@@ -13,7 +13,6 @@
 
 /**
  * javascript functions regarding the "dyntabmenu" used throughout the TYPO3 backend
- * @deprecated with TYPO3 CMS 7, will be removed with CMS 8
  */
 
 var DTM_array = DTM_array || [];
