c526154b16ebf397f7c4af6dfe0e8521928342fb
Frederic Gaus <gaus@flagbit.de>
2014-11-13 13:42:54 +0100

    [BUGFIX] Fix bug in HMENU special prev
    This patch fixes a regression of patch #61721.
    The array before this patch had numeric indexes and thus the first
    one was not recognized in the prev menu.
    
    Releases: master, 6.2
    Resolves: #62482
    Related: #61721
    Change-Id: I2b05ffa3973fe7ca31b6e4ccd193d44718d1ec1a
    Reviewed-on: http://review.typo3.org/34063
    Reviewed-by: Markus Klein <klein.t3@reelworx.at>
    Tested-by: Markus Klein <klein.t3@reelworx.at>
    Reviewed-by: Mathias Schreiber <mathias.schreiber@wmdb.de>
    Reviewed-by: Nicole Cordes <typo3@cordes.co>
    Reviewed-by: Wouter Wolters <typo3@wouterwolters.nl>

--- typo3/sysext/frontend/Classes/ContentObject/Menu/AbstractMenuContentObject.php
+++ typo3/sysext/frontend/Classes/ContentObject/Menu/AbstractMenuContentObject.php
@@ -925,7 +925,7 @@ class AbstractMenuContentObject {
 		$filteredPages = array();
 		foreach ($pages as $aPage) {
 			if ($this->filterMenuPages($aPage, $banned, $aPage['doktype'] === PageRepository::DOKTYPE_SPACER)) {
-				$filteredPages[] = $aPage;
+				$filteredPages[$aPage['uid']] = $aPage;
 			}
 		}
 		return $filteredPages;
