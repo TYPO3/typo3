ee967b66ad7ef570e724b2c1f0329a385f094fa9
Benjamin Kott <benjamin.kott@wfp2.com>
2014-11-15 18:54:09 +0100

    [FEATURE] Module menu restyling
    Restyle the module menu and prepare appicon support.
    
    Resolves: #62995
    Releases: master
    Change-Id: Ic6a55c434b3cf13320c6b0417050d2cd273b531b
    Reviewed-on: http://review.typo3.org/34216
    Tested-by: Georg Ringer <georg.ringer@gmail.com>
    Reviewed-by: Frank Nägler <typo3@naegler.net>
    Tested-by: Frank Nägler <typo3@naegler.net>
    Reviewed-by: Felix Kopp <felix-source@phorax.com>
    Tested-by: Felix Kopp <felix-source@phorax.com>

--- typo3/sysext/backend/Resources/Public/JavaScript/modulemenu.js
+++ typo3/sysext/backend/Resources/Public/JavaScript/modulemenu.js
@@ -36,36 +36,39 @@ TYPO3.ModuleMenu.App = {
 			me.showModule(top.startInModule[0]);
 		} else {
 			// fetch first module
-			me.showModule(jQuery('.t3-menuitem-submodule:first').attr('id'));
+			me.showModule(jQuery('.typo3-module-menu-item:first').attr('id'));
 		}
 
 		// check if there are collapsed items in the local storage
 		var collapsedMainMenuItems = this.getCollapsedMainMenuItems();
 		jQuery.each(collapsedMainMenuItems, function(key, itm) {
-			var $headerElement = jQuery('#' + key).find('.modgroup:first');
-			if ($headerElement.length > 0) {
-				$headerElement.addClass('collapsed').removeClass('expanded').next('.t3-menuitem-submodules').slideUp('fast');
+			var $group = jQuery('#' + key);
+			if ($group.length > 0) {
+				var $groupContainer = $group.find('.typo3-module-menu-group-container');
+				$group.addClass('collapsed').removeClass('expanded');
+				$groupContainer.slideUp('fast');
 			}
 		});
-
 		me.initializeEvents();
 	},
 
 	initializeEvents: function() {
 		var me = this;
-		jQuery(document).on('click', '.t3-menuitem-main .modgroup', function() {
-			var $headerElement = jQuery(this);
-			if ($headerElement.hasClass('expanded')) {
-				me.addCollapsedMainMenuItem($headerElement.parent().attr('id'));
-				$headerElement.addClass('collapsed').removeClass('expanded').next('.t3-menuitem-submodules').slideUp();
+		jQuery(document).on('click', '.typo3-module-menu-group .typo3-module-menu-group-header', function() {
+			var $group = jQuery(this).parent('.typo3-module-menu-group');
+			var $groupContainer = $group.find('.typo3-module-menu-group-container');
+			if ($group.hasClass('expanded')) {
+				me.addCollapsedMainMenuItem($group.attr('id'));
+				$group.addClass('collapsed').removeClass('expanded');
+				$groupContainer.slideUp();
 			} else {
-				me.removeCollapseMainMenuItem($headerElement.parent().attr('id'));
-				$headerElement.addClass('expanded').removeClass('collapsed').next('.t3-menuitem-submodules').slideDown();
+				me.removeCollapseMainMenuItem($group.attr('id'));
+				$group.addClass('expanded').removeClass('collapsed');
+				$groupContainer.slideDown();
 			}
 		});
-
 		// register clicking on sub modules
-		jQuery(document).on('click', '.t3-menuitem-submodule', function(evt) {
+		jQuery(document).on('click', '.typo3-module-menu-item,.t3-menuitem-submodule', function(evt) {
 			evt.preventDefault();
 			me.showModule(jQuery(this).attr('id'));
 		});
@@ -183,7 +186,7 @@ TYPO3.ModuleMenu.App = {
 	},
 
 	highlightModuleMenuItem: function(module, mainModule) {
-		jQuery('.t3-menuitem-submodule.active').removeClass('active');
+		jQuery('.typo3-module-menu-item.active').removeClass('active');
 		jQuery('#' + module).addClass('active');
 	},
 
