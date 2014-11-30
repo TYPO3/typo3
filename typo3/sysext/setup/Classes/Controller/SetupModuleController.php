d47cc2864330e3e768e070c5fca4fe9b0ab45994
Jigal van Hemert <jigal.van.hemert@typo3.org>
2014-11-30 12:36:58 +0100

    [BUGFIX] Correct message when language is missing
    Message in User settings has to refer to the Language module
    instead of the Extension Manager.
    
    Resolves: #63449
    Releases: master, 6.2
    Change-Id: I6865330281b77336595af6e056975974ec700c15
    Reviewed-on: http://review.typo3.org/34811
    Reviewed-by: Torben Hansen <derhansen@gmail.com>
    Tested-by: Torben Hansen <derhansen@gmail.com>
    Reviewed-by: Wouter Wolters <typo3@wouterwolters.nl>
    Tested-by: Wouter Wolters <typo3@wouterwolters.nl>

--- typo3/sysext/setup/Classes/Controller/SetupModuleController.php
+++ typo3/sysext/setup/Classes/Controller/SetupModuleController.php
@@ -625,7 +625,7 @@ class SetupModuleController {
 				<select id="field_lang" name="data[lang]" class="form-control">' . implode('', $languageOptions) . '
 				</select>';
 		if ($GLOBALS['BE_USER']->uc['lang'] && !@is_dir((PATH_typo3conf . 'l10n/' . $GLOBALS['BE_USER']->uc['lang']))) {
-			$languageUnavailableWarning = 'The selected language "' . $GLOBALS['LANG']->getLL(('lang_' . $GLOBALS['BE_USER']->uc['lang']), TRUE) . '" is not available before the language pack is installed.<br />' . ($GLOBALS['BE_USER']->isAdmin() ? 'You can use the Extension Manager to easily download and install new language packs.' : 'Please ask your system administrator to do this.');
+			$languageUnavailableWarning = 'The selected language "' . $GLOBALS['LANG']->getLL(('lang_' . $GLOBALS['BE_USER']->uc['lang']), TRUE) . '" is not available before the language files are installed.<br />' . ($GLOBALS['BE_USER']->isAdmin() ? 'You can use the Language module to easily download new language files.' : 'Please ask your system administrator to do this.');
 			$languageUnavailableMessage = GeneralUtility::makeInstance(\TYPO3\CMS\Core\Messaging\FlashMessage::class, $languageUnavailableWarning, '', \TYPO3\CMS\Core\Messaging\FlashMessage::WARNING);
 			$languageCode = $languageUnavailableMessage->render() . $languageCode;
 		}
