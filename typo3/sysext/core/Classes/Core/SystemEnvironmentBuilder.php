a52fdf64f9604d46a7f935e7344719adb8c25000
Oliver Hader <oliver@typo3.org>
2014-12-02 20:34:59 +0100

    Revert "[TASK] Set TYPO3 version to 7.0.1-dev"
    This reverts commit 485830b907b1ca5c66d193fabca6ee7ae3190564.
    
    Change-Id: I74e71ad4a51a9b0036db6f23436b66872749ab28
    Reviewed-on: http://review.typo3.org/34951
    Reviewed-by: Oliver Hader <oliver.hader@typo3.org>
    Tested-by: Oliver Hader <oliver.hader@typo3.org>

--- typo3/sysext/core/Classes/Core/SystemEnvironmentBuilder.php
+++ typo3/sysext/core/Classes/Core/SystemEnvironmentBuilder.php
@@ -77,7 +77,7 @@ class SystemEnvironmentBuilder {
 	 */
 	static protected function defineBaseConstants() {
 		// This version, branch and copyright
-		define('TYPO3_version', '7.0.1-dev');
+		define('TYPO3_version', '7.0.0');
 		define('TYPO3_branch', '7.0');
 		define('TYPO3_copyright_year', '1998-2014');
 
