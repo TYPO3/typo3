26511d633fccf8fc00aa0c98eaab565759ba5f10
Stefan Neufeind <typo3.neufeind@speedpartner.de>
2014-12-14 00:52:43 +0100

    [TASK] Extbase-Repository: Use ClassNamingUtility from core
    Use core-functionality for conversion of repository-name
    to its model-name.
    
    Change-Id: I2a3247dd05ea24f12d1c235d8bf39bebdfbdb9ff
    Resolves: #63809
    Releases: master
    Reviewed-on: http://review.typo3.org/35345
    Reviewed-by: Susanne Moog <typo3@susannemoog.de>
    Tested-by: Susanne Moog <typo3@susannemoog.de>
    Reviewed-by: Markus Klein <klein.t3@reelworx.at>
    Tested-by: Markus Klein <klein.t3@reelworx.at>

--- typo3/sysext/extbase/Classes/Persistence/Repository.php
+++ typo3/sysext/extbase/Classes/Persistence/Repository.php
@@ -14,6 +14,8 @@ namespace TYPO3\CMS\Extbase\Persistence;
  * The TYPO3 project - inspiring people to share!
  */
 
+use TYPO3\CMS\Core\Utility\ClassNamingUtility;
+
 /**
  * The base repository - will usually be extended by a more concrete repository.
  *
@@ -68,9 +70,7 @@ class Repository implements RepositoryInterface, \TYPO3\CMS\Core\SingletonInterf
 	 */
 	public function __construct(\TYPO3\CMS\Extbase\Object\ObjectManagerInterface $objectManager) {
 		$this->objectManager = $objectManager;
-
-		$nsSeparator = strpos($this->getRepositoryClassName(), '\\') !== FALSE ? '\\\\' : '_';
-		$this->objectType = preg_replace(array('/' . $nsSeparator . 'Repository' . $nsSeparator . '(?!.*' . $nsSeparator . 'Repository' . $nsSeparator . ')/', '/Repository$/'), array($nsSeparator . 'Model' . $nsSeparator, ''), $this->getRepositoryClassName());
+		$this->objectType = ClassNamingUtility::translateRepositoryNameToModelName($this->getRepositoryClassName());
 	}
 
 	/**
