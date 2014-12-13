b7ae6764041a6a00517cf9e23bf177bd92b7f1e4
Anja Leichsenring <aleichsenring@ab-softlab.de>
2014-12-13 19:07:40 +0100

    [BUGFIX] Fix failing unit test
    A unit test causes a fatal error due to newly introduced dependency
    to SignalSlotDispatcher. The fix extends the mocking to prevent
    the FATAL error.
    
    Change-Id: I02c6cf2cda07bd965a3cbbd1512d7e80fac8f908
    Resolves: #63882
    Relates: #62960
    Releases: master
    Reviewed-on: http://review.typo3.org/35395
    Reviewed-by: Georg Ringer <georg.ringer@gmail.com>
    Tested-by: Georg Ringer <georg.ringer@gmail.com>

--- typo3/sysext/core/Tests/Unit/Mail/MailerTest.php
+++ typo3/sysext/core/Tests/Unit/Mail/MailerTest.php
@@ -27,7 +27,7 @@ class MailerTest extends \TYPO3\CMS\Core\Tests\UnitTestCase {
 	protected $fixture;
 
 	public function setUp() {
-		$this->fixture = $this->getMock(\TYPO3\CMS\Core\Mail\Mailer::class, array('noMethodMocked'), array(), '', FALSE);
+		$this->fixture = $this->getMock(\TYPO3\CMS\Core\Mail\Mailer::class, array('emitPostInitializeMailerSignal'), array(), '', FALSE);
 	}
 
 	//////////////////////////
