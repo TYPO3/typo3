a02c882d555a16b8f6ef705e8eb9c505ffbcfc8b
Benjamin Mack <benni@typo3.org>
2015-01-02 11:19:21 +0100

    [BUGFIX] Change fe_users DB field size to 255 chars
    When having email addresses as usernames the field
    fe_users.username gets too small. Resizing to 255
    characters solves a lot of problems. Same goes
    for the email field.
    
    Releases: master, 6.2
    Resolves: #55484
    Change-Id: I03e4fe1228a6b22d24ad222a9fab578059917847
    Reviewed-on: http://review.typo3.org/35718
    Reviewed-by: Mathias Schreiber <mathias.schreiber@wmdb.de>
    Tested-by: Mathias Schreiber <mathias.schreiber@wmdb.de>
    Reviewed-by: Wouter Wolters <typo3@wouterwolters.nl>
    Tested-by: Wouter Wolters <typo3@wouterwolters.nl>
    Reviewed-by: Benjamin Mack <benni@typo3.org>
    Tested-by: Benjamin Mack <benni@typo3.org>
    (cherry picked from commit 5966dd4b1bdec1c356a96265246a928cc25b72cf)
    Reviewed-on: http://review.typo3.org/35721

--- typo3/sysext/frontend/Configuration/TCA/fe_users.php
+++ typo3/sysext/frontend/Configuration/TCA/fe_users.php
@@ -30,7 +30,7 @@ return array(
 			'config' => array(
 				'type' => 'input',
 				'size' => '20',
-				'max' => '50',
+				'max' => '255',
 				'eval' => 'nospace,lower,uniqueInPid,required'
 			)
 		),
@@ -141,7 +141,7 @@ return array(
 				'type' => 'input',
 				'size' => '20',
 				'eval' => 'trim',
-				'max' => '80'
+				'max' => '255'
 			)
 		),
 		'title' => array(
