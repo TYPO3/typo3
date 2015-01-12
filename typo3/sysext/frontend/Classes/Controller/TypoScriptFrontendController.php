4ca17f67dbf2e3a9faecb396d63ebb2a1b1a7afc
Helmut Hummel <helmut.hummel@typo3.org>
2015-01-12 18:23:49 +0100

    [BUGFIX] Fix regression in prefixLocalAchors feature
    The security fix for #62723 missed an edge case were the
    TypoScriptFrontendController main cObj property is not yet initialized.
    
    Check for this case and create it.
    
    Resolves: #63896
    Releases: 4.5, 6.2, master
    Change-Id: Ief9338453517e095d120007d8d13ba6405d6fbb8
    Reviewed-on: http://review.typo3.org/36029
    Reviewed-by: Helmut Hummel <helmut.hummel@typo3.org>
    Tested-by: Helmut Hummel <helmut.hummel@typo3.org>

--- typo3/sysext/frontend/Classes/Controller/TypoScriptFrontendController.php
+++ typo3/sysext/frontend/Classes/Controller/TypoScriptFrontendController.php
@@ -4361,6 +4361,9 @@ if (version == "n3") {
 	 */
 	public function prefixLocalAnchorsWithScript() {
 		if (!$this->beUserLogin) {
+			if (!is_object($this->cObj)) {
+				$this->newCObj();
+			}
 			$scriptPath = $this->cObj->getUrlToCurrentLocation();
 		} else {
 			// To break less existing sites, we allow the REQUEST_URI to be used for the prefix
