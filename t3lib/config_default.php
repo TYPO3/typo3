6cf78f61e87ea9536dc836062b9152e6b51520a2
Benjamin Mack <benni@typo3.org>
2015-01-29 16:41:23 +0100

    [TASK] Update TYPO3 copyright in all branches
    The copyright notice (e.g. shown in the footer
    of Backend Login) should be updated to 2015.
    
    Resolves: #64597
    Releases: master, 6.2, 4.5
    Change-Id: I84bfe7eb53acb8896c45fc0788f53da4edadbdf8
    Reviewed-on: http://review.typo3.org/36368
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>

--- t3lib/config_default.php
+++ t3lib/config_default.php
@@ -677,7 +677,7 @@ $T3_VAR = array();	// Initialize.
 $TYPO_VERSION = '4.5.40-dev';	// deprecated: use the constants defined below
 define('TYPO3_version', $TYPO_VERSION);
 define('TYPO3_branch', '4.5');
-define('TYPO3_copyright_year', '1998-2013');
+define('TYPO3_copyright_year', '1998-2015');
 define('TYPO3_user_agent', 'User-Agent: TYPO3/'.TYPO3_version);
 
 // Database-variables are cleared!
