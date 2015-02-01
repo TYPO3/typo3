59a444969f2c05e0c56950571e417451e0512574
Sebastian Fischer <typo3@evoweb.de>
2015-02-01 10:33:02 +0100

    [BUGFIX] Create typo3temp/_processed_/ by default
    By this the needed folder for files from default storage
    is created.
    
    Resolves: #55833
    Releases: master, 6.2
    Change-Id: I5f31203a58d2a093362305aebf1f264f175980c3
    Reviewed-on: http://review.typo3.org/36560
    Reviewed-by: Frans Saris <franssaris@gmail.com>
    Tested-by: Frans Saris <franssaris@gmail.com>
    Reviewed-by: Ingo Schmitt <is@marketing-factory.de>
    Tested-by: Ingo Schmitt <is@marketing-factory.de>
    Reviewed-by: Wouter Wolters <typo3@wouterwolters.nl>
    Tested-by: Wouter Wolters <typo3@wouterwolters.nl>

--- typo3/sysext/install/Classes/FolderStructure/DefaultFactory.php
+++ typo3/sysext/install/Classes/FolderStructure/DefaultFactory.php
@@ -129,6 +129,11 @@ class DefaultFactory {
 							'type' => 'TYPO3\\CMS\\install\\FolderStructure\\DirectoryNode',
 							'targetPermission' => $directoryPermission,
 						),
+						array(
+							'name' => '_processed_',
+							'type' => 'TYPO3\\CMS\\install\\FolderStructure\\DirectoryNode',
+							'targetPermission' => $directoryPermission,
+						),
 					),
 				),
 				array(
