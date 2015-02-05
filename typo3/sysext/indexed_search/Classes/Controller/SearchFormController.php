TYPO3/typo3

commit bf63a1c2c2fa10eb821ef4cae8f91e7bb00dff63

[!!!][TASK] Rewrite backend modules of indexed_search
 * Move info modules and admin module to a central place
 * Use extbase & fluid
 * Add translations

The old modules are removed with a different commit to make it possible
to compare the output of old and new!

Releases: master
Resolves: #61511
Change-Id: Id750b0ad78e8ab115b917d4772281d3aceca6eee
Reviewed-on: http://review.typo3.org/32708
Reviewed-by: Markus Klein <klein.t3@reelworx.at>
Tested-by: Susanne Moog <typo3@susannemoog.de>
Reviewed-by: Tymoteusz Motylewski <t.motylewski@gmail.com>
Tested-by: Tymoteusz Motylewski <t.motylewski@gmail.com>

typo3/sysext/indexed_search/Classes/Controller/SearchFormController.php
@@ -29,7 +29,7 @@ class SearchFormController extends \TYPO3\CMS\Frontend\Plugin\AbstractPlugin {
 	public $prefixId = 'tx_indexedsearch';
 
 	// Same as class name
-	public $scriptRelPath = 'pi/class.tx_indexedsearch.php';
+	public $scriptRelPath = 'Classes/Controller/SearchFormController.php';
 
 	// Path to this script relative to the extension dir.
 	public $extKey = 'indexed_search';

typo3/sysext/indexed_search/Classes/Controller/AdministrationController.php
@@ -1,0 +1,369 @@
+<?php
+namespace TYPO3\CMS\IndexedSearch\Controller;
+
+/**
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use TYPO3\CMS\Core\Authentication\BackendUserAuthentication;
+use TYPO3\CMS\Core\Database\DatabaseConnection;
+use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
+use TYPO3\CMS\Extbase\Mvc\Web\Request as WebRequest;
+use TYPO3\CMS\IndexedSearch\Domain\Repository\AdministrationRepository;
+use TYPO3\CMS\Backend\Utility\BackendUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\IndexedSearch\Indexer;
+use TYPO3\CMS\Lang\LanguageService;
+
+/**
+ * Administration controller
+ */
+class AdministrationController extends ActionController {
+
+	/**
+	 * @var AdministrationRepository
+	 */
+	protected $administrationRepository;
+
+	/**
+	 * @var int Current page id
+	 */
+	protected $pageUid = 0;
+
+	/**
+	 * @var int Max lists per page
+	 */
+	protected $maxListPerPage = 50;
+
+	/**
+	 * @var array External parsers
+	 */
+	protected $external_parsers = array();
+
+	/**
+	 * @var array Configuration defined in the Extension Manager
+	 */
+	protected $indexerConfig = array();
+
+	/**
+	 * @var bool is metaphone enabled
+	 */
+	protected $enableMetaphoneSearch = FALSE;
+
+	/**
+	 * Indexer object
+	 *
+	 * @var \TYPO3\CMS\IndexedSearch\Indexer
+	 */
+	protected $indexer;
+
+	/**
+	 * Function will be called before every other action
+	 *
+	 * @return void
+	 */
+	public function initializeAction() {
+		$this->pageUid = (int)GeneralUtility::_GET('id');
+		$this->maxListPerPage = GeneralUtility::_GP('listALL') ? 100000 : 100;
+		$this->indexerConfig = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['indexed_search']);
+		$this->enableMetaphoneSearch = (bool)$this->indexerConfig['enableMetaphoneSearch'];
+		$this->indexer = GeneralUtility::makeInstance(Indexer::class);
+
+		parent::initializeAction();
+	}
+
+	/**
+	 * Override the action name if found in the uc of the user
+	 *
+	 * @param \TYPO3\CMS\Extbase\Mvc\RequestInterface $request
+	 * @param \TYPO3\CMS\Extbase\Mvc\ResponseInterface $response
+	 * @throws \TYPO3\CMS\Extbase\Mvc\Exception\UnsupportedRequestTypeException
+	 */
+	public function processRequest(\TYPO3\CMS\Extbase\Mvc\RequestInterface $request, \TYPO3\CMS\Extbase\Mvc\ResponseInterface $response) {
+		$vars = GeneralUtility::_GET('tx_indexedsearch_web_indexedsearchisearch');
+
+		$beUser = $this->getBackendUserAuthentication();
+		if (is_array($vars) && isset($vars['action']) && method_exists($this, $vars['action'] . 'Action')) {
+			$action = $vars['action'];
+
+			switch($action) {
+				case 'saveStopwordsKeywords':
+					$action = 'statisticDetails';
+					break;
+				case 'deleteIndexedItem':
+					$action = 'statistic';
+					break;
+			}
+
+			$beUser->uc['indexed_search']['action'] = $action;
+			$beUser->uc['indexed_search']['arguments'] = $request->getArguments();
+			$beUser->writeUC();
+		} elseif (isset($beUser->uc['indexed_search']['action'])) {
+			if ($request instanceof WebRequest) {
+				$request->setControllerActionName($beUser->uc['indexed_search']['action']);
+			}
+			if (isset($beUser->uc['indexed_search']['arguments'])) {
+				$request->setArguments($beUser->uc['indexed_search']['arguments']);
+			}
+		}
+
+		parent::processRequest($request, $response);
+	}
+
+	/**
+	 * @param \TYPO3\CMS\IndexedSearch\Domain\Repository\AdministrationRepository $administrationRepository
+	 * @return void
+	 */
+	public function injectAdministrationRepository(AdministrationRepository $administrationRepository) {
+		$this->administrationRepository = $administrationRepository;
+	}
+
+	/**
+	 * Index action contains the most important statistics
+	 *
+	 * @return void
+	 */
+	public function indexAction() {
+		$this->view->assignMultiple(array(
+			'records' => $this->administrationRepository->getRecordsNumbers(),
+			'phash' => $this->administrationRepository->getPageHashTypes()
+		));
+
+		if ($this->pageUid) {
+			$last24hours = ' AND tstamp > ' . ($GLOBALS['EXEC_TIME'] - 24 * 60 * 60);
+			$last30days = ' AND tstamp > ' . ($GLOBALS['EXEC_TIME'] - 30 * 24 * 60 * 60);
+
+			$this->view->assignMultiple(array(
+				'pageUid' => $this->pageUid,
+				'all' => $this->administrationRepository->getGeneralSearchStatistic('', $this->pageUid),
+				'last24hours' => $this->administrationRepository->getGeneralSearchStatistic($last24hours, $this->pageUid),
+				'last30days' => $this->administrationRepository->getGeneralSearchStatistic($last30days, $this->pageUid),
+			));
+		}
+	}
+
+	/**
+	 * Statistics for pages
+	 *
+	 * @return void
+	 */
+	public function pagesAction() {
+		$this->view->assign('records', $this->administrationRepository->getPageStatistic());
+	}
+
+	/**
+	 * Statistics for external documents
+	 *
+	 * @return void
+	 */
+	public function externalDocumentsAction() {
+		$this->view->assign('records', $this->administrationRepository->getExternalDocumentsStatistic());
+	}
+
+	/**
+	 * Statistics for a given page hash
+	 *
+	 * @param int $pageHash
+	 * @return void
+	 */
+	public function statisticDetailsAction($pageHash = 0) {
+		$pageHash = (int)$pageHash;
+		$db = $this->getDatabaseConnection();
+		$pageHashRow = $db->exec_SELECTgetSingleRow('*', 'index_phash', 'phash = ' . (int)$pageHash);
+
+		if (!is_array($pageHashRow)) {
+			$this->redirect('statistic');
+		}
+
+		$debugRow = $db->exec_SELECTgetRows('*', 'index_debug', 'phash = ' . (int)$pageHash);
+		$debugInfo = array();
+		$lexer = '';
+		if (is_array($debugRow)) {
+			$debugInfo = unserialize($debugRow[0]['debuginfo']);
+			$lexer = $debugInfo['lexer'];
+			unset($debugInfo['lexer']);
+		}
+		$pageRecord = BackendUtility::getRecord('pages', $pageHashRow['data_page_id']);
+		$keywords = is_array($pageRecord) ? array_flip(GeneralUtility::trimExplode(',', $pageRecord['keywords'], TRUE)) : array();
+		$wordRecords = $db->exec_SELECTgetRows(
+			'index_words.*, index_rel.*',
+			'index_rel, index_words',
+			'index_rel.phash = ' . (int)$pageHash . ' AND index_words.wid = index_rel.wid',
+			'',
+			'index_words.baseword'
+		);
+		foreach($wordRecords as $id => $row) {
+			if (isset($keywords[$row['baseword']])) {
+				$wordRecords[$id]['is_keyword'] = TRUE;
+			}
+		}
+		$metaphoneRows = $metaphone = array();
+		if ($this->enableMetaphoneSearch && is_array($wordRecords)) {
+			// Group metaphone hash
+			foreach ($wordRecords as $row) {
+				$metaphoneRows[$row['metaphone']][] = $row['baseword'];
+			}
+
+			foreach ($metaphoneRows as $hash => $words) {
+				if (count($words) > 1) {
+					$metaphone[] = array(
+						'metaphone' => $this->indexer->metaphone($words[0], 1), $hash,
+						'words' => $words,
+						'hash' => $hash
+					);
+				}
+			}
+		}
+		$this->view->assignMultiple(array(
+			'phash' => $pageHash,
+			'phashRow' => $pageHashRow,
+			'words' => $wordRecords,
+			'sections' => $db->exec_SELECTgetRows(
+				'*',
+				'index_section',
+				'index_section.phash = ' . (int)$pageHash
+			),
+			'topCount' => $db->exec_SELECTgetRows(
+				'index_words.baseword, index_words.metaphone, index_rel.*',
+				'index_rel, index_words',
+				'index_rel.phash = ' . (int)$pageHash . ' AND index_words.wid = index_rel.wid
+					 AND index_words.is_stopword=0',
+			 	'',
+			 	'index_rel.count DESC',
+				 '20'
+			 ),
+			'topFrequency' => $db->exec_SELECTgetRows(
+				'index_words.baseword, index_words.metaphone, index_rel.*',
+				'index_rel, index_words',
+				'index_rel.phash = ' . (int)$pageHash . ' AND index_words.wid = index_rel.wid
+					 AND index_words.is_stopword=0',
+			 	'',
+				'index_rel.freq DESC',
+				'20'
+			),
+			'debug' => $debugInfo,
+			'lexer' => $lexer,
+			'metaphone' => $metaphone,
+			'page' => $pageRecord,
+			'keywords' => $keywords
+	));
+	}
+
+	/**
+	 * Save stop words and keywords
+     *
+	 * @param string $pageHash
+	 * @param int $pageId
+	 * @param array $stopwords
+	 * @param array $keywords
+	 * @return void
+	 */
+	public function saveStopwordsKeywordsAction($pageHash, $pageId, $stopwords = array(), $keywords = array()) {
+		if ($this->getBackendUserAuthentication()->isAdmin()) {
+			if (is_array($stopwords) && !empty($stopwords)) {
+				$this->administrationRepository->saveStopWords($stopwords);
+			}
+			if (is_array($keywords) && !empty($keywords)) {
+				$this->administrationRepository->saveKeywords($keywords, $pageId);
+			}
+		}
+
+		$this->redirect('statisticDetails', NULL, NULL, array('pageHash' => $pageHash));
+	}
+
+	/**
+	 * Statistics for a given word id
+	 *
+	 * @param int $id
+	 * @param int $pageHash
+	 * @return void
+	 */
+	public function wordDetailAction($id = 0, $pageHash = 0) {
+		$rows = $this->getDatabaseConnection()->exec_SELECTgetRows(
+			'index_phash.*, index_section.*, index_rel.*',
+			'index_rel, index_section, index_phash',
+			'index_rel.wid = ' . (int)$id . ' AND index_rel.phash = index_section.phash' . ' AND index_section.phash = index_phash.phash',
+			'',
+			'index_rel.freq DESC'
+		);
+
+		$this->view->assignMultiple(array(
+			'rows' => $rows,
+			'phash' => $pageHash
+		));
+	}
+
+	/**
+	 * General statistics
+	 *
+	 * @param int $depth
+	 * @param string $mode
+	 * @return void
+	 */
+	public function statisticAction($depth = 1, $mode = 'overview') {
+		if (is_array($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['indexed_search']['external_parsers'])) {
+			foreach ($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['indexed_search']['external_parsers'] as $extension => $_objRef) {
+				/** @var \TYPO3\CMS\IndexedSearch\FileContentParser $fileContentParser */
+				$fileContentParser = GeneralUtility::getUserObj($_objRef);
+				if ($fileContentParser->softInit($extension)) {
+					$this->external_parsers[$extension] = $fileContentParser;
+				}
+			}
+		}
+		$this->administrationRepository->external_parsers = $this->external_parsers;
+
+		$allLines = $this->administrationRepository->getTree($this->pageUid, $depth, $mode);
+
+		$this->view->assignMultiple(array(
+			'levelTranslations' => explode('|', $this->getLanguageService()->sL('LLL:EXT:lang/locallang_core.xlf:labels.enterSearchLevels')),
+			'tree' => $allLines,
+			'pageUid' => $this->pageUid,
+			'mode' => $mode,
+			'depth' => $depth
+		));
+	}
+
+
+	/**
+	 * Remove item from index
+	 *
+	 * @param string $id
+	 * @param int $depth
+	 * @param string $mode
+	 * @return void
+	 */
+	public function deleteIndexedItemAction($id, $depth = 1, $mode = 'overview') {
+		$this->administrationRepository->removeIndexedPhashRow($id, $this->pageUid, $depth);
+		$this->redirect('statistic', NULL, NULL, array('depth' => $depth, 'mode' => $mode));
+	}
+
+	/**
+	 * @return DatabaseConnection
+	 */
+	protected function getDatabaseConnection() {
+		return $GLOBALS['TYPO3_DB'];
+	}
+
+	/**
+	 * @return BackendUserAuthentication
+	 */
+	protected function getBackendUserAuthentication() {
+		return $GLOBALS['BE_USER'];
+	}
+
+	/**
+	 * @return LanguageService
+	 */
+	protected function getLanguageService() {
+		return $GLOBALS['LANG'];
+	}
+}

commit 204800dc4f6d6b04ceb8b1b4d02cde994a2ab10c

[BUGFIX] Indexed Search pibased plugin can load again translations
This patch fixes a regression introduced in bf63a1c,
which changed the variable $scriptRelPath with a wrong
file location. The old file name is reverted so that
AbstractPlugin will look for the locallang.xlf file at
the correct location.

The patch also reformats the properties comments of the class.

Reference: https://review.typo3.org/#/c/32708/

Resolves: #64879
Releases: master
Change-Id: I58a07f11e382bde2ed84835ca0820ff5e00e1998
Reviewed-on: http://review.typo3.org/36677
Reviewed-by: Nicole Cordes <typo3@cordes.co>
Tested-by: Nicole Cordes <typo3@cordes.co>
Reviewed-by: Daniel Maier <dani-maier@gmx.de>
Tested-by: Daniel Maier <dani-maier@gmx.de>
Reviewed-by: Göran Bodenschatz <coding@46halbe.de>
Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
Tested-by: Christian Kuhn <lolli@schwarzbu.ch>

typo3/sysext/indexed_search/Classes/Controller/SearchFormController.php
@@ -26,68 +26,144 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
  */
 class SearchFormController extends \TYPO3\CMS\Frontend\Plugin\AbstractPlugin {
 
+	/**
+	 * Used as fieldname prefix
+	 *
+	 * @var string
+	 */
 	public $prefixId = 'tx_indexedsearch';
 
-	// Same as class name
-	public $scriptRelPath = 'Classes/Controller/SearchFormController.php';
+	/**
+	 * Path to this script relative to the extension dir.
+	 *
+	 * @var string
+	 * @TODO This is still set to the "old" class location since the locallang.xlf file in the same dir is loaded by pi_loadLL
+	 */
+	public $scriptRelPath = 'pi/class.tx_indexedsearch.php';
 
-	// Path to this script relative to the extension dir.
+	/**
+	 * Extension key.
+	 *
+	 * @var string
+	 */
 	public $extKey = 'indexed_search';
 
-	// The extension key.
+	/**
+	 * See document for info about this flag...
+	 *
+	 * @var int
+	 */
 	public $join_pages = 0;
 
-	// See document for info about this flag...
+
 	public $defaultResultNumber = 10;
 
+	/**
+	 * Internal variable
+	 *
+	 * @var array
+	 */
 	public $operator_translate_table = array(array('+', 'AND'), array('|', 'OR'), array('-', 'AND NOT'));
 
-	// Internal variable
+	/**
+	 * Root-page ids to search in (rl0 field where clause, see initialize() function)
+	 *
+	 * @var int|string id or comma separated list of ids
+	 */
 	public $wholeSiteIdList = 0;
 
-	// Root-page PIDs to search in (rl0 field where clause, see initialize() function)
-	// Internals:
+	/**
+	 * Search Words and operators
+	 *
+	 * @var array
+	 */
 	public $sWArr = array();
 
-	// Search Words and operators
+	/**
+	 * Selector box values for search configuration form
+	 *
+	 * @var array
+	 */
 	public $optValues = array();
 
-	// Selector box values for search configuration form
+	/**
+	 * Will hold the first row in result - used to calculate relative hit-ratings.
+	 *
+	 * @var array
+	 */
 	public $firstRow = array();
 
-	// Will hold the first row in result - used to calculate relative hit-ratings.
+	/**
+	 * Caching of page path
+	 *
+	 * @var array
+	 */
 	public $cache_path = array();
 
-	// Caching of page path
+	/**
+	 * Caching of root line data
+	 *
+	 * @var array
+	 */
 	public $cache_rl = array();
 
-	// Caching of root line data
+	/**
+	 * Required fe_groups memberships for display of a result.
+	 *
+	 * @var array
+	 */
 	public $fe_groups_required = array();
 
-	// Required fe_groups memberships for display of a result.
+	/**
+	 * sys_domain records
+	 *
+	 * @var array
+	 */
 	public $domain_records = array();
 
-	// Select clauses for individual words
+	/**
+	 * Select clauses for individual words
+	 *
+	 * @var array
+	 */
 	public $wSelClauses = array();
 
-	// Domain records (?)
+	/**
+	 * Page tree sections for search result.
+	 *
+	 * @var array
+	 */
 	public $resultSections = array();
 
-	// Page tree sections for search result.
+	/**
+	 * External parser objects
+	 * @var array
+	 */
 	public $external_parsers = array();
 
-	// External parser objects
+	/**
+	 * Storage of icons....
+	 *
+	 * @var array
+	 */
 	public $iconFileNameCache = array();
 
-	// Storage of icons....
-	public $templateCode;
+	/**
+	 * Will hold the content of $conf['templateFile']
+	 *
+	 * @var string
+	 */
+	public $templateCode = '';
 
-	// Will hold the content of $conf['templateFile']
 	public $hiddenFieldList = 'ext, type, defOp, media, order, group, lang, desc, results';
 
+	/**
+	 * Indexer configuration, coming from $GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['indexed_search']
+	 *
+	 * @var array
+	 */
 	public $indexerConfig = array();
 
-	// Indexer configuration, coming from $GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['indexed_search']
 	public $enableMetaphoneSearch = FALSE;
 
 	public $storeMetaphoneInfoAsWords;
@@ -281,7 +357,7 @@ class SearchFormController extends \TYPO3\CMS\Frontend\Plugin\AbstractPlugin {
 			}
 			$this->optValues['sections']['rl1_' . implode(',', array_keys($firstLevelMenu))] = $this->pi_getLL('opt_RL1ALL');
 		}
-		// Setting the list of root PIDs for the search. Notice, these page IDs MUST have a TypoScript template with root flag on them! Basically this list is used to select on the "rl0" field and page ids are registered as "rl0" only if a TypoScript template record with root flag is there.
+		// Setting the list of root IDs for the search. Notice, these page IDs MUST have a TypoScript template with root flag on them! Basically this list is used to select on the "rl0" field and page ids are registered as "rl0" only if a TypoScript template record with root flag is there.
 		// This happens AFTER the use of $this->wholeSiteIdList above because the above will then fetch the menu for the CURRENT site - regardless of this kind of searching here. Thus a general search will lookup in the WHOLE database while a specific section search will take the current sections...
 		if ($this->conf['search.']['rootPidList']) {
 			$this->wholeSiteIdList = implode(',', GeneralUtility::intExplode(',', $this->conf['search.']['rootPidList']));