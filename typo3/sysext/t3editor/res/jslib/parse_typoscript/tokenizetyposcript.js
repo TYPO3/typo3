149a94180add7c2d490fad6d245238f3e3e4bb14
Frederic Gaus <gaus@flagbit.de>
2015-02-10 17:02:07 +0100

    [BUGFIX] Code completion in t3editor behind operator
    Fix code completion in t3editor when substituing a value with
    completed code behind a typoscript operator.
    
    Change-Id: Id1fbb99aba850ee0787dba4b8562d0a22c119979
    Resolves: #25594
    Releases: master, 6.2
    Reviewed-on: http://review.typo3.org/36804
    Reviewed-by: Wouter Wolters <typo3@wouterwolters.nl>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Reviewed-by: Benjamin Mack <benni@typo3.org>
    Tested-by: Benjamin Mack <benni@typo3.org>

--- typo3/sysext/t3editor/res/jslib/parse_typoscript/tokenizetyposcript.js
+++ typo3/sysext/t3editor/res/jslib/parse_typoscript/tokenizetyposcript.js
@@ -2580,13 +2580,12 @@ var tokenizeTypoScript = function() {
 				token = nextUntilUnescaped(ch) || result("string", "string");
 
 			} else if (
-			   ( ch == "<" ||
-				   ch == ">" ||
-  				 ( ch == "="
-  				   && source.peek() != "<"
-  				 )
-			   )
-			   && source.peek() != "\n" ) { // there must be some value behind the operator!
+				(ch == "<" ||
+					ch == ">" ||
+					ch == "." ||
+					(ch == "=" && source.peek() != "<")
+				)
+				&& source.peek() != "\n" ) { // there must be some value behind the operator!
 				this.inValue = true;
 				token = result(ch, "ts-operator");
 
