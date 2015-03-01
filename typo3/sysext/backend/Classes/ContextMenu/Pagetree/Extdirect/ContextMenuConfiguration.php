512228f22f2a93395826e1724a0b08df5328a7bc
Michael Oehlhof <typo3@oehlhof.de>
2015-03-02 00:25:53 +0100

    [TASK] Code cleanup ContextMenuConfiguration.php
    backend/Classes/ContextMenu/Pagetree/Extdirect/ContextMenuConfiguration.php
    To get it "green" in PhpStorm several changes were made:
    
    - set correct parameter type in PhpDoc comment
    - add missing return value
    
    Resolves: #65444
    Releases: master
    Change-Id: Iebb9c4c789612f229f4d4562cfd1445d98703594
    Reviewed-on: http://review.typo3.org/37443
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>

--- typo3/sysext/backend/Classes/ContextMenu/Pagetree/Extdirect/ContextMenuConfiguration.php
+++ typo3/sysext/backend/Classes/ContextMenu/Pagetree/Extdirect/ContextMenuConfiguration.php
@@ -35,7 +35,7 @@ class ContextMenuConfiguration extends \TYPO3\CMS\Backend\ContextMenu\Extdirect\
 	/**
 	 * Returns the actions for the given node information's
 	 *
-	 * @param stdClass $nodeData
+	 * @param \stdClass $nodeData
 	 * @return array
 	 */
 	public function getActionsForNodeArray($nodeData) {
@@ -45,6 +45,7 @@ class ContextMenuConfiguration extends \TYPO3\CMS\Backend\ContextMenu\Extdirect\
 		$this->initDataProvider();
 		$this->dataProvider->setContextMenuType('table.' . $node->getType());
 		$actionCollection = $this->dataProvider->getActionsForNode($node);
+		$actions = array();
 		if ($actionCollection instanceof \TYPO3\CMS\Backend\ContextMenu\ContextMenuActionCollection) {
 			$actions = $actionCollection->toArray();
 		}
