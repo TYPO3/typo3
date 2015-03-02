d9a5dc9b654579098b692dce849bfc9108b90cca
Benjamin Mack <benni@typo3.org>
2015-03-02 11:30:56 +0100

    [TASK] Move leftover code from cms to frontend
    Leftover code that registers modules
    that are located within EXT:frontend should
    be moved from EXT:cms to EXT:frontend
    
    Resolves: #65449
    Releases: master
    Change-Id: Icde91ae85eee74c5fc5492a7263afcd0bc85c82f
    Reviewed-on: http://review.typo3.org/37447
    Reviewed-by: Wouter Wolters <typo3@wouterwolters.nl>
    Tested-by: Wouter Wolters <typo3@wouterwolters.nl>
    Reviewed-by: Anja Leichsenring <aleichsenring@ab-softlab.de>
    Tested-by: Anja Leichsenring <aleichsenring@ab-softlab.de>

--- typo3/sysext/frontend/ext_tables.php
+++ typo3/sysext/frontend/ext_tables.php
@@ -3,3 +3,20 @@ defined('TYPO3_MODE') or die();
 
 // Add allowed records to pages
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('pages_language_overlay,tt_content,sys_template,sys_domain,backend_layout');
+
+if (TYPO3_MODE === 'BE') {
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('_MOD_web_layout', 'EXT:cms/locallang_csh_weblayout.xlf');
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('_MOD_web_info', 'EXT:cms/locallang_csh_webinfo.xlf');
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::insertModuleFunction(
+		'web_info',
+		\TYPO3\CMS\Frontend\Controller\PageInformationController::class,
+		NULL,
+		'LLL:EXT:cms/locallang_tca.xlf:mod_tx_cms_webinfo_page'
+	);
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::insertModuleFunction(
+		'web_info',
+		\TYPO3\CMS\Frontend\Controller\TranslationStatusController::class,
+		NULL,
+		'LLL:EXT:cms/locallang_tca.xlf:mod_tx_cms_webinfo_lang'
+	);
+}
