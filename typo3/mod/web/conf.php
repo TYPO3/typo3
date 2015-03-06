d93b2065e71c7433444a2a40e89c6399383a0275
Wouter Wolters <typo3@wouterwolters.nl>
2015-03-06 18:48:42 +0100

    [TASK] Remove configuration for alt_db_navframe.php
    Remove configuration for alt_db_navframe.php in typo3/mod/web/conf.php
    
    Resolves: #65579
    Releases: master
    Change-Id: I6c4400abf3e8d9cbbc7934456794ee2bc3d3d31d
    Reviewed-on: http://review.typo3.org/37604
    Reviewed-by: Benjamin Mack <benni@typo3.org>
    Tested-by: Benjamin Mack <benni@typo3.org>
    Reviewed-by: Frank Nägler <typo3@naegler.net>
    Tested-by: Frank Nägler <typo3@naegler.net>

--- typo3/mod/web/conf.php
+++ typo3/mod/web/conf.php
@@ -1,6 +1,5 @@
 <?php
 $MLANG['default']['tabs_images']['tab'] = 'website.gif';
 $MLANG['default']['ll_ref'] = 'LLL:EXT:lang/locallang_mod_web.xlf';
-$MCONF['navFrameScript'] = '../../alt_db_navframe.php';
 $MCONF['name'] = 'web';
 $MCONF['access'] = 'user,group';
