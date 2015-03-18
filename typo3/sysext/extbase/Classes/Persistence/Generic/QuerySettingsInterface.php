0a6dfcd782c370722c463a894007c6dd74f3e6fd
Christian Kuhn <lolli@schwarzbu.ch>
2015-03-18 14:06:57 +0100

    [TASK] Improve deprecation message of raw query result
    Hint "use argument on ->execute() instead" in the interface as well
    to quickly find this when navigating through code with IDE.
    
    Change-Id: I5cc2f594486bd7ec30abd3ec42eb404f85e92529
    Resolves: #65806
    Releases: 6.2
    Reviewed-on: http://review.typo3.org/37924
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>

--- typo3/sysext/extbase/Classes/Persistence/Generic/QuerySettingsInterface.php
+++ typo3/sysext/extbase/Classes/Persistence/Generic/QuerySettingsInterface.php
@@ -167,7 +167,7 @@ interface QuerySettingsInterface {
 	 *
 	 * @param boolean $returnRawQueryResult TRUE, if the QueryResult should be returned unmapped; otherwise FALSE.
 	 * @return void
-	 * @deprecated since Extbase 6.2, will be removed two versions later
+	 * @deprecated since Extbase 6.2, will be removed two versions later. Please use argument in query->execute() instead.
 	 */
 	public function setReturnRawQueryResult($returnRawQueryResult);
 
@@ -175,7 +175,7 @@ interface QuerySettingsInterface {
 	 * Returns the state, if the QueryResult should be returned unmapped.
 	 *
 	 * @return boolean TRUE, if the QueryResult should be returned unmapped; otherwise FALSE.
-	 * @deprecated since Extbase 6.2, will be removed two versions later
+	 * @deprecated since Extbase 6.2, will be removed two versions later. Please use argument in query->execute() instead.
 	 */
 	public function getReturnRawQueryResult();
 
