9aaba260ce306944c36a62e20fa8d2444a21eb3f
Wouter Wolters <typo3@wouterwolters.nl>
2015-03-19 10:19:24 +0100

    [BUGFIX] Fix order of generator meta tag
    Change TYPO3 6.2 CMS to TYPO3 CMS 6.2
    
    Releases: master,6.2
    Resolves: #65836
    Change-Id: I5486d6fef6a09923ee1c211442675bd2f3b6def0
    Reviewed-on: http://review.typo3.org/38282
    Reviewed-by: Markus Klein <klein.t3@reelworx.at>
    Tested-by: Markus Klein <klein.t3@reelworx.at>

--- typo3/sysext/frontend/Classes/Page/PageGenerator.php
+++ typo3/sysext/frontend/Classes/Page/PageGenerator.php
@@ -1117,7 +1117,7 @@ class PageGenerator {
 		$endingSlash = $xhtml ? ' /' : '';
 
 		$metaTags = array(
-			'<meta name="generator" content="TYPO3 ' . TYPO3_branch . ' CMS"' . $endingSlash . '>'
+			'<meta name="generator" content="TYPO3 CMS ' . TYPO3_branch . '"' . $endingSlash . '>'
 		);
 
 		/** @var TypoScriptService $typoScriptService */
