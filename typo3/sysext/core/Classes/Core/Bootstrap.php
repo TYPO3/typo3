501edf0ab38d8201cc797e7e062f05e8f8424f2d
Benjamin Mack <benni@typo3.org>
2015-03-22 13:43:54 +0100

    [TASK] Remove unneeded function call in TYPO3 Bootstrap
    The method "loadAdditionalConfigurationFromExtensions()"
    inside the TYPO3 Bootstrap is just used as a wrapper
    now for calling the extLocalconf method itself,
    and only used within the Bootstrap class within the boostrap->loadTypo3LoadedExtAndExtLocalconf() method.
    
    Resolves: #65892
    Releases: master
    Change-Id: I993221e2152ee9b0128dd61379828789fe033a2e
    Reviewed-on: http://review.typo3.org/37995
    Reviewed-by: Jigal van Hemert <jigal.van.hemert@typo3.org>
    Tested-by: Jigal van Hemert <jigal.van.hemert@typo3.org>
    Reviewed-by: Jan Helke <typo3@helke.de>
    Tested-by: Jan Helke <typo3@helke.de>
    Reviewed-by: Wouter Wolters <typo3@wouterwolters.nl>
    Tested-by: Wouter Wolters <typo3@wouterwolters.nl>

--- typo3/sysext/core/Classes/Core/Bootstrap.php
+++ typo3/sysext/core/Classes/Core/Bootstrap.php
@@ -496,7 +496,7 @@ class Bootstrap {
 	 * @internal This is not a public API method, do not use in own extensions
 	 */
 	public function loadTypo3LoadedExtAndExtLocalconf($allowCaching = TRUE) {
-		$this->loadAdditionalConfigurationFromExtensions($allowCaching);
+		Utility\ExtensionManagementUtility::loadExtLocalconf($allowCaching);
 		return $this;
 	}
 
@@ -782,20 +782,6 @@ class Bootstrap {
 		define('TYPO3_REQUESTTYPE_AJAX', 8);
 		define('TYPO3_REQUESTTYPE_INSTALL', 16);
 		define('TYPO3_REQUESTTYPE', (TYPO3_MODE == 'FE' ? TYPO3_REQUESTTYPE_FE : 0) | (TYPO3_MODE == 'BE' ? TYPO3_REQUESTTYPE_BE : 0) | (defined('TYPO3_cliMode') && TYPO3_cliMode ? TYPO3_REQUESTTYPE_CLI : 0) | (defined('TYPO3_enterInstallScript') && TYPO3_enterInstallScript ? TYPO3_REQUESTTYPE_INSTALL : 0) | ($GLOBALS['TYPO3_AJAX'] ? TYPO3_REQUESTTYPE_AJAX : 0));
-		return $this;
-	}
-
-	/**
-	 * Load extension configuration files (ext_localconf.php)
-	 *
-	 * The ext_localconf.php files in extensions are meant to make changes
-	 * to the global $TYPO3_CONF_VARS configuration array.
-	 *
-	 * @param bool $allowCaching
-	 * @return Bootstrap
-	 */
-	protected function loadAdditionalConfigurationFromExtensions($allowCaching = TRUE) {
-		Utility\ExtensionManagementUtility::loadExtLocalconf($allowCaching);
 		return $this;
 	}
 
