04785def87f8920f182cc292a8f6e3fcc96c5229
Markus Klein <klein.t3@reelworx.at>
2015-04-22 21:15:02 +0200

    [BUGFIX] Use API to register AJAX handler for recycler
    Releases: master
    Resolves: #66546
    Change-Id: Ic64205454a511efbd2b32a1eb7763e4ff0a653fd
    Reviewed-on: http://review.typo3.org/38862
    Reviewed-by: Wouter Wolters <typo3@wouterwolters.nl>
    Tested-by: Wouter Wolters <typo3@wouterwolters.nl>
    Reviewed-by: Markus Klein <klein.t3@reelworx.at>
    Tested-by: Markus Klein <klein.t3@reelworx.at>

--- typo3/sysext/recycler/ext_localconf.php
+++ typo3/sysext/recycler/ext_localconf.php
@@ -4,7 +4,10 @@ defined('TYPO3_MODE') or die();
 if (TYPO3_MODE === 'BE') {
 	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerAjaxHandler('RecyclerAjaxController::dispatch', \TYPO3\CMS\Recycler\Controller\RecyclerAjaxController::class . '->dispatch');
 }
-$GLOBALS['TYPO3_CONF_VARS']['BE']['AJAX']['RecyclerAjaxController::init'] = \TYPO3\CMS\Recycler\Task\CleanerTask::class . '->init';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerAjaxHandler(
+	'RecyclerAjaxController::init',
+	\TYPO3\CMS\Recycler\Task\CleanerTask::class . '->init'
+);
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][\TYPO3\CMS\Recycler\Task\CleanerTask::class] = array(
 	'extension' => 'recycler',
