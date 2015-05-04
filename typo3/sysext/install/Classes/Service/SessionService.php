edf1c78e86fea9f27bf2f683845d5838bcbd830e
Andreas Fernandez <a.fernandez@scripting-base.de>
2015-05-04 11:16:12 +0200

    [TASK] Improve .htaccess files
    Improve the example .htaccess file by adding rules for caching,
    MIME types and CORS. Also, the rewrite rules are extended to block
    access to certain files and folders.
    
    Additionally all rules are made compatible for Apache 2.4 as well.
    
    Resolves: #23078
    Resolves: #66235
    Releases: master, 6.2
    Change-Id: I629f524b5a209769601f04a74bb7434736058ab8
    Reviewed-on: http://review.typo3.org/40792
    Reviewed-by: Markus Klein <markus.klein@typo3.org>
    Tested-by: Markus Klein <markus.klein@typo3.org>

--- typo3/sysext/install/Classes/Service/SessionService.php
+++ typo3/sysext/install/Classes/Service/SessionService.php
@@ -130,7 +130,20 @@ class SessionService implements \TYPO3\CMS\Core\SingletonInterface {
 					1294587484
 				);
 			}
-			GeneralUtility::writeFile($sessionSavePath . '/.htaccess', 'Order deny, allow' . LF . 'Deny from all');
+			$htaccessContent = '
+# Apache < 2.3
+<IfModule !mod_authz_core.c>
+	Order allow,deny
+	Deny from all
+	Satisfy All
+</IfModule>
+
+# Apache ≥ 2.3
+<IfModule mod_authz_core.c>
+	Require all denied
+</IfModule>
+			';
+			GeneralUtility::writeFile($sessionSavePath . '/.htaccess', $htaccessContent);
 			$indexContent = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">';
 			$indexContent .= '<HTML><HEAD<TITLE></TITLE><META http-equiv=Refresh Content="0; Url=../../">';
 			$indexContent .= '</HEAD></HTML>';
