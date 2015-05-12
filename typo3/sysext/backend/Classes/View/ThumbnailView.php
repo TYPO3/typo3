8f20d1e4dd92f722c0b3dcdf82f3cc78a8f38673
Stephan Großberndt <stephan@grossberndt.de>
2015-05-12 13:07:32 +0200

    [BUGFIX] Valid Content-Type header for jpg thumbnails
    Use "Content-Type: image/jpeg" instead of invalid "image/jpg" derived
    from the file extension. The invalid Content-Type in combination with
    "X-Content-Type-Options: nosniff" leads to thumbnails not showing up in
    Internet Explorer.
    
    While at it also correct the case of "Etag" to "ETag".
    
    Resolves: #66913
    Releases: master, 6.2
    Change-Id: I9a31817e32e4e8b2b99b3008f8381608c5ac06e9
    Reviewed-on: http://review.typo3.org/39482
    Reviewed-by: Frederic Gaus <frederic.gaus@flagbit.de>
    Reviewed-by: Xavier Perseguers <xavier@typo3.org>
    Reviewed-by: Morton Jonuschat <m.jonuschat@mojocode.de>
    Tested-by: Morton Jonuschat <m.jonuschat@mojocode.de>
    Reviewed-by: Andreas Fernandez <typo3@scripting-base.de>
    Tested-by: Andreas Fernandez <typo3@scripting-base.de>

--- typo3/sysext/backend/Classes/View/ThumbnailView.php
+++ typo3/sysext/backend/Classes/View/ThumbnailView.php
@@ -225,9 +225,9 @@ class ThumbnailView {
 				// The thumbnail is read and output to the browser
 				if ($fd = @fopen($this->output, 'rb')) {
 					$fileModificationTime = filemtime($this->output);
-					header('Content-type: image/' . $outext);
+					header('Content-Type: image/' . ($outext === 'jpg' ? 'jpeg' : $outext));
 					header('Last-Modified: ' . date('r', $fileModificationTime));
-					header('Etag: ' . md5($this->output) . '-' . $fileModificationTime);
+					header('ETag: ' . md5($this->output) . '-' . $fileModificationTime);
 					// Expiration time is chosen arbitrary to 1 month
 					header('Expires: ' . date('r', ($fileModificationTime + 30 * 24 * 60 * 60)));
 					fpassthru($fd);
