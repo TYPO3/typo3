e3fbdaddf1504912beacf29d1204e2531d0059d7
Morton Jonuschat <m.jonuschat@mojocode.de>
2015-05-20 21:06:03 +0200

    [BUGFIX] Initialize database connection in ClearCacheService
    Without calling initialize() on the DatabaseConnection object
    the DBAL the resulting connection configuration will default
    to the "native" driver type (MySQL) which results in an error
    as there is no MySQL server to contact with the given connection
    parameters.
    
    Resolves: #67066
    Releases: master, 6.2
    Change-Id: Id5d70a122dcca806ee1122cecacad0c8aef27547
    Reviewed-on: http://review.typo3.org/39636
    Reviewed-by: Markus Klein <markus.klein@typo3.org>
    Tested-by: Markus Klein <markus.klein@typo3.org>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Reviewed-by: Wouter Wolters <typo3@wouterwolters.nl>
    Reviewed-by: Andreas Fernandez <typo3@scripting-base.de>
    Tested-by: Andreas Fernandez <typo3@scripting-base.de>

--- typo3/sysext/install/Classes/Service/ClearCacheService.php
+++ typo3/sysext/install/Classes/Service/ClearCacheService.php
@@ -110,6 +110,7 @@ class ClearCacheService {
 			$database->setDatabasePort($GLOBALS['TYPO3_CONF_VARS']['DB']['port']);
 			$database->setDatabaseSocket($GLOBALS['TYPO3_CONF_VARS']['DB']['socket']);
 			$database->setDatabaseName($GLOBALS['TYPO3_CONF_VARS']['DB']['database']);
+			$database->initialize();
 			$database->connectDB();
 		}
 		return $database;
