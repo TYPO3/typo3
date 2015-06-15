8f585a635f8aec7e1eba4b9ff3e149b6f21b9826
Benjamin Mack <benni@typo3.org>
2015-06-15 20:27:36 +0200

    [TASK] Update TYPO3 version to TYPO3 7.4-dev
    Releases: master
    Change-Id: I7b4847cccc33dbbebe8d09af2a32001f5de238dd
    Reviewed-on: http://review.typo3.org/40268
    Reviewed-by: Benjamin Mack <benni@typo3.org>
    Tested-by: Benjamin Mack <benni@typo3.org>

--- typo3/sysext/core/Classes/Core/SystemEnvironmentBuilder.php
+++ typo3/sysext/core/Classes/Core/SystemEnvironmentBuilder.php
@@ -84,8 +84,8 @@ class SystemEnvironmentBuilder {
 	 */
 	static protected function defineBaseConstants() {
 		// This version, branch and copyright
-		define('TYPO3_version', '7.3.1-dev');
-		define('TYPO3_branch', '7.3');
+		define('TYPO3_version', '7.4-dev');
+		define('TYPO3_branch', '7.4');
 		define('TYPO3_copyright_year', '1998-2015');
 
 		// TYPO3 external links
