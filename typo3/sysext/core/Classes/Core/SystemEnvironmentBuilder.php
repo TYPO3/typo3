dd60eaba7c6e44cafaa732603fb39e2dba694bf9
TYPO3 Release Team <typo3cms@typo3.org>
2015-06-15 20:25:21 +0200

    [RELEASE] Release of TYPO3 7.3.0
    Change-Id: Ia515c614e2f43a2c3708cbd06343f5e5dd6e9292
    Reviewed-on: http://review.typo3.org/40266
    Reviewed-by: TYPO3 Release Team <typo3cms@typo3.org>
    Tested-by: TYPO3 Release Team <typo3cms@typo3.org>

--- typo3/sysext/core/Classes/Core/SystemEnvironmentBuilder.php
+++ typo3/sysext/core/Classes/Core/SystemEnvironmentBuilder.php
@@ -84,7 +84,7 @@ class SystemEnvironmentBuilder {
 	 */
 	static protected function defineBaseConstants() {
 		// This version, branch and copyright
-		define('TYPO3_version', '7.3-dev');
+		define('TYPO3_version', '7.3.0');
 		define('TYPO3_branch', '7.3');
 		define('TYPO3_copyright_year', '1998-2015');
 
