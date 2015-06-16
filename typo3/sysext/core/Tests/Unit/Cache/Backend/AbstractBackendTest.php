d7f8a3782d83512e06a22531402623c8234627c3
Alexander Krist <mail@alex-krist.de>
2015-06-16 12:57:34 +0200

    [TASK] Removes eval() in abstract backend unit tests
    Adds a new AccessibleProxy for AbstractBackend and removes the ugly
    usage of eval() from AbstractBackendTest in unit tests.
    
    Change-Id: I3c471219fe37ecf8a51cb02b60c75130fbb4ad25
    Resolves: #67489
    Related: #67468
    Releases: master
    Reviewed-on: http://review.typo3.org/40288
    Reviewed-by: Jan Helke <typo3@helke.de>
    Tested-by: Jan Helke <typo3@helke.de>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>

--- typo3/sysext/core/Tests/Unit/Cache/Backend/AbstractBackendTest.php
+++ typo3/sysext/core/Tests/Unit/Cache/Backend/AbstractBackendTest.php
@@ -14,52 +14,22 @@ namespace TYPO3\CMS\Core\Tests\Unit\Cache\Backend;
  * The TYPO3 project - inspiring people to share!
  */
 
+use TYPO3\CMS\Core\Tests\Unit\Cache\Backend\Fixtures\ConcreteBackendFixture;
+use TYPO3\CMS\Core\Tests\UnitTestCase;
+
 /**
- * Testcase for the abstract cache backend
- *
- * This file is a backport from FLOW3
+ * Test case
  *
  * @author Ingo Renner <ingo@typo3.org>
  */
-class AbstractBackendTest extends \TYPO3\CMS\Core\Tests\UnitTestCase {
-
-	/**
-	 * @var \TYPO3\CMS\Core\Cache\Backend\AbstractBackend
-	 */
-	protected $backend;
-
-	/**
-	 * @return void
-	 */
-	protected function setUp() {
-		$className = $this->getUniqueId('ConcreteBackend_');
-		eval('
-			class ' . $className . ' extends TYPO3\\CMS\\Core\\Cache\\Backend\\AbstractBackend {
-				public function set($entryIdentifier, $data, array $tags = array(), $lifetime = NULL) {}
-				public function get($entryIdentifier) {}
-				public function has($entryIdentifier) {}
-				public function remove($entryIdentifier) {}
-				public function flush() {}
-				public function flushByTag($tag) {}
-				public function findIdentifiersByTag($tag) {}
-				public function collectGarbage() {}
-				public function setSomeOption($value) {
-					$this->someOption = $value;
-				}
-				public function getSomeOption() {
-					return $this->someOption;
-				}
-			}
-		');
-		$this->backend = new $className('Testing');
-	}
+class AbstractBackendTest extends UnitTestCase {
 
 	/**
 	 * @test
 	 */
 	public function theConstructorCallsSetterMethodsForAllSpecifiedOptions() {
-		$className = get_class($this->backend);
-		$backend = new $className('Testing', array('someOption' => 'someValue'));
+		// The fixture class implements methods setSomeOption() and getSomeOption()
+		$backend = new ConcreteBackendFixture('Testing', array('someOption' => 'someValue'));
 		$this->assertSame('someValue', $backend->getSomeOption());
 	}
 
---
+++ typo3/sysext/core/Tests/Unit/Cache/Backend/Fixtures/ConcreteBackendFixture.php
@@ -0,0 +1,56 @@
+<?php
+namespace TYPO3\CMS\Core\Tests\Unit\Cache\Backend\Fixtures;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use TYPO3\CMS\Core\Cache\Backend\AbstractBackend;
+
+/**
+ * Fixture implementing one set option method.
+ */
+class ConcreteBackendFixture extends AbstractBackend {
+	protected $someOption;
+
+	public function set($entryIdentifier, $data, array $tags = array(), $lifetime = NULL) {
+	}
+
+	public function get($entryIdentifier) {
+	}
+
+	public function has($entryIdentifier) {
+	}
+
+	public function remove($entryIdentifier) {
+	}
+
+	public function flush() {
+	}
+
+	public function flushByTag($tag) {
+	}
+
+	public function findIdentifiersByTag($tag) {
+	}
+
+	public function collectGarbage() {
+	}
+
+	public function setSomeOption($value) {
+		$this->someOption = $value;
+	}
+
+	public function getSomeOption() {
+		return $this->someOption;
+	}
+}
