4467b6d6d274ad9304c2afa1fe1d28bea2881b65
Frank Nägler <typo3@naegler.net>
2015-06-20 12:03:05 +0200

    [BUGFIX] TypeError: this.I18N was undefined in backend
    Fixed JS Error in backend. 
    Can be verified in EXT:styleguide mega record.
    
    Resolves: #67644
    Releases: master
    Change-Id: Ib549594c46c8e81d7e048d617353abe20e29d08d
    Reviewed-on: http://review.typo3.org/40480
    Reviewed-by: Michael Oehlhof <typo3@oehlhof.de>
    Tested-by: Michael Oehlhof <typo3@oehlhof.de>
    Reviewed-by: Wouter Wolters <typo3@wouterwolters.nl>
    Tested-by: Wouter Wolters <typo3@wouterwolters.nl>

--- typo3/sysext/rtehtmlarea/Resources/Public/JavaScript/HTMLArea/Plugin/Plugin.js
+++ typo3/sysext/rtehtmlarea/Resources/Public/JavaScript/HTMLArea/Plugin/Plugin.js
@@ -37,10 +37,9 @@ define('TYPO3/CMS/Rtehtmlarea/HTMLArea/Plugin/Plugin',
 		this.editorId = editor.editorId;
 		this.editorConfiguration = editor.config;
 		this.name = pluginName;
-		try {
+		this.I18N = {};
+		if (typeof HTMLArea.I18N !== 'undefined' && typeof HTMLArea.I18N[this.name] !== 'undefined') {
 			this.I18N = HTMLArea.I18N[this.name];
-		} catch(e) {
-			this.I18N = new Object();
 		}
 		this.configurePlugin(editor);
 	};
