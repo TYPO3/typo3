603edc3f3316ba8dc87a96b6d8f23f0b039b4564
Mathias Brodala <mbrodala@pagemachine.de>
2015-06-30 10:17:23 +0200

    [BUGFIX] 1st level cache for ``QueryResult::count()``
    Counting the results of the ``QueryResultInterface`` proxy returned by
    Repository finder methods always executed a ``COUNT query`` even if
    the results had been fetched or counted already.
    
    With this patch the number of results is cached as soon as the result
    is initialized and/or counted once.
    
    Resolves: #67837
    Releases: master, 6.2
    Change-Id: I98f680372b845f992ad3d436647b0cf5e460b606
    Reviewed-on: http://review.typo3.org/40778
    Reviewed-by: Markus Klein <markus.klein@typo3.org>
    Tested-by: Markus Klein <markus.klein@typo3.org>

--- typo3/sysext/extbase/Classes/Persistence/Generic/QueryResult.php
+++ typo3/sysext/extbase/Classes/Persistence/Generic/QueryResult.php
@@ -35,6 +35,11 @@ class QueryResult implements QueryResultInterface {
 	protected $persistenceManager;
 
 	/**
+	 * @var int|NULL
+	 */
+	protected $numberOfResults;
+
+	/**
 	 * @var \TYPO3\CMS\Extbase\Persistence\QueryInterface
 	 */
 	protected $query;
@@ -104,11 +109,14 @@ class QueryResult implements QueryResultInterface {
 	 * @api
 	 */
 	public function count() {
-		if (is_array($this->queryResult)) {
-			return count($this->queryResult);
-		} else {
-			return $this->persistenceManager->getObjectCountByQuery($this->query);
+		if ($this->numberOfResults === NULL) {
+			if (is_array($this->queryResult)) {
+				$this->numberOfResults = count($this->queryResult);
+			} else {
+				$this->numberOfResults = $this->persistenceManager->getObjectCountByQuery($this->query);
+			}
 		}
+		return $this->numberOfResults;
 	}
 
 	/**
