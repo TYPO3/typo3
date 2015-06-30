902f12af17f0962ab4df617d5670a474a777b0b7
Benjamin Mack <benni@typo3.org>
2015-06-30 10:29:23 +0200

    [TASK] Change misleading texts to "lowlevel" modules
    Resolves: #18486
    Releases: master
    Change-Id: Iba2514001570428e50eb55a75cee793ff7a409fb
    Reviewed-on: http://review.typo3.org/40751
    Reviewed-by: Wouter Wolters <typo3@wouterwolters.nl>
    Tested-by: Wouter Wolters <typo3@wouterwolters.nl>
    Reviewed-by: Xavier Perseguers <xavier@typo3.org>
    Tested-by: Xavier Perseguers <xavier@typo3.org>

--- typo3/sysext/lowlevel/ext_emconf.php
+++ typo3/sysext/lowlevel/ext_emconf.php
@@ -1,7 +1,7 @@
 <?php
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'Tools>Config+DBint',
-	'description' => 'Enables the \'Config\' and \'DBint\' modules for technical analysis of the system. This includes raw database search, checking relations, counting pages and records etc.',
+	'title' => 'System > Configuration + DB Check',
+	'description' => 'Enables the \'Config\' and \'DB Check\' modules for technical analysis of the system. This includes raw database search, checking relations, counting pages and records etc.',
 	'category' => 'module',
 	'state' => 'stable',
 	'uploadfolder' => 0,
