1e54574246a386e7f46e7740dbe8fc512e8b1608
Nicole Cordes <typo3@cordes.co>
2015-07-13 11:22:51 +0200

    [BUGFIX] Notify about wrong backend user group setting
    An editor can't edit a plugin as long as the page content type "Insert
    Plugin" isn't allowed for the backend user group even if the access to
    a plugin is defined. This patch adds a notification about missing
    configuration to improve usability for integrators.
    
    Releases: master
    Resolves: #61559
    Change-Id: Id7c56c2514ec4525f731c101e8e6e7dbade274e1
    Reviewed-on: http://review.typo3.org/41116
    Reviewed-by: Susanne Moog <typo3@susannemoog.de>
    Tested-by: Susanne Moog <typo3@susannemoog.de>
    Reviewed-by: Wouter Wolters <typo3@wouterwolters.nl>
    Tested-by: Wouter Wolters <typo3@wouterwolters.nl>

---
+++ typo3/sysext/core/Classes/Hooks/BackendUserGroupIntegrityCheck.php
@@ -0,0 +1,75 @@
+<?php
+namespace TYPO3\CMS\Core\Hooks;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use TYPO3\CMS\Backend\Utility\BackendUtility;
+use TYPO3\CMS\Core\DataHandling\DataHandler;
+use TYPO3\CMS\Core\Messaging\FlashMessage;
+use TYPO3\CMS\Core\Messaging\FlashMessageQueue;
+use TYPO3\CMS\Core\Messaging\FlashMessageService;
+use TYPO3\CMS\Core\SingletonInterface;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Utility\StringUtility;
+use TYPO3\CMS\Lang\LanguageService;
+
+/**
+ * DataHandler hook class to check the integrity of submitted be_groups data
+ */
+class BackendUserGroupIntegrityCheck {
+
+	/**
+	 * @param string $status
+	 * @param string $table
+	 * @param int $id
+	 * @param array $fieldArray
+	 * @param DataHandler $parentObject
+	 */
+	public function processDatamap_afterDatabaseOperations($status, $table, $id, $fieldArray, $parentObject) {
+		if ($table !== 'be_groups' || $GLOBALS['TYPO3_CONF_VARS']['BE']['explicitADmode'] !== 'explicitAllow') {
+			return;
+		}
+
+		$backendUserGroup = BackendUtility::getRecord($table, $id, 'explicit_allowdeny');
+		$explicitAllowDenyFields = GeneralUtility::trimExplode(',', $backendUserGroup['explicit_allowdeny']);
+		foreach ($explicitAllowDenyFields as $value) {
+			if (StringUtility::beginsWith($value, 'tt_content:list_type:')) {
+				if (!in_array('tt_content:CType:list:ALLOW', $explicitAllowDenyFields, TRUE)) {
+					/** @var $flashMessage FlashMessage */
+					$flashMessage = GeneralUtility::makeInstance(
+						FlashMessage::class,
+						$this->getLanguageService()->sl('LLL:EXT:lang/locallang_core.xlf:error.backendUserGroupListTypeError.message'),
+						$this->getLanguageService()->sl('LLL:EXT:lang/locallang_core.xlf:error.backendUserGroupListTypeError.header'),
+						FlashMessage::WARNING,
+						TRUE
+					);
+					/** @var $flashMessageService FlashMessageService */
+					$flashMessageService = GeneralUtility::makeInstance(FlashMessageService::class);
+					/** @var $defaultFlashMessageQueue FlashMessageQueue */
+					$defaultFlashMessageQueue = $flashMessageService->getMessageQueueByIdentifier();
+					$defaultFlashMessageQueue->enqueue($flashMessage);
+				}
+				return;
+			}
+		}
+	}
+
+	/**
+	 * @return LanguageService
+	 */
+	protected function getLanguageService() {
+		return $GLOBALS['LANG'];
+	}
+
+}
