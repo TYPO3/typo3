634bb23da4a98b88286f98d9e3531e6dce00257a
Michael Oehlhof <typo3@oehlhof.de>
2015-07-18 19:27:51 +0200

    [TASK] EXT:scheduler Uniform extension directory structure
    Moved the code from the mod1 directory to the Modules/Scheduler directory.
    
    Resolves: #68351
    Releases: master
    Change-Id: Icf4e737c905873e96d54334f83324bdb364cbc6b
    Reviewed-on: http://review.typo3.org/41686
    Reviewed-by: Susanne Moog <typo3@susannemoog.de>
    Tested-by: Susanne Moog <typo3@susannemoog.de>
    Reviewed-by: Frederic Gaus <frederic.gaus@flagbit.de>
    Tested-by: Frederic Gaus <frederic.gaus@flagbit.de>
    Reviewed-by: Wouter Wolters <typo3@wouterwolters.nl>
    Tested-by: Wouter Wolters <typo3@wouterwolters.nl>

--- typo3/sysext/scheduler/ext_tables.php
+++ typo3/sysext/scheduler/ext_tables.php
@@ -7,14 +7,14 @@ if (TYPO3_MODE === 'BE') {
 		'system',
 		'txschedulerM1',
 		'',
-		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'mod1/',
+		'EXT:scheduler/Modules/Scheduler/',
 		array(
 			'script' => '_DISPATCH',
 			'access' => 'admin',
 			'name' => 'system_txschedulerM1',
 			'labels' => array(
 				'tabs_images' => array(
-					'tab' => '../Resources/Public/Icons/module-scheduler.svg',
+					'tab' => 'EXT:scheduler/Resources/Public/Icons/module-scheduler.svg',
 				),
 				'll_ref' => 'LLL:EXT:scheduler/Resources/Private/Language/locallang_mod.xlf',
 			),
