e19beef7f7f4baede864149d9e1b9998774c56e2
Michael Oehlhof <typo3@oehlhof.de>
2015-07-18 11:44:25 +0200

    [TASK] EXT:func Uniform extension directory structure
    Moved the code from the mod1 directory to the Modules/AdvancedFunctions
    directory.
    
    Resolves: #68334
    Releases: master
    Change-Id: Idd3da42aab317d26b7af73d6820fb1f75d4250ee
    Reviewed-on: http://review.typo3.org/41660
    Reviewed-by: Wouter Wolters <typo3@wouterwolters.nl>
    Tested-by: Wouter Wolters <typo3@wouterwolters.nl>
    Reviewed-by: Jigal van Hemert <jigal.van.hemert@typo3.org>
    Tested-by: Jigal van Hemert <jigal.van.hemert@typo3.org>

--- typo3/sysext/func/ext_tables.php
+++ typo3/sysext/func/ext_tables.php
@@ -6,14 +6,14 @@ if (TYPO3_MODE === 'BE') {
 		'web',
 		'func',
 		'',
-		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'mod1/',
+		'EXT:func/Modules/AdvancedFunctions/',
 		array(
 			'script' => '_DISPATCH',
 			'access' => 'user,group',
 			'name' => 'web_func',
 			'labels' => array(
 				'tabs_images' => array(
-					'tab' => '../Resources/Public/Icons/module-func.svg',
+					'tab' => 'EXT:func/Resources/Public/Icons/module-func.svg',
 				),
 				'll_ref' => 'LLL:EXT:lang/locallang_mod_web_func.xlf',
 			),
