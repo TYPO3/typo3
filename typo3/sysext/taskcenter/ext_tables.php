a62f76909d69c605426438568bf07df9a8b98e35
Wouter Wolters <typo3@wouterwolters.nl>
2015-07-21 17:12:07 +0200

    [TASK] EXT:taskcenter Uniform extension directory structure
    Resolves: #68432
    Releases: master
    Change-Id: I677375f761a170e6c88fae91584f5bf83c502cbf
    Reviewed-on: http://review.typo3.org/41782
    Reviewed-by: Susanne Moog <typo3@susannemoog.de>
    Tested-by: Susanne Moog <typo3@susannemoog.de>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>

--- typo3/sysext/taskcenter/ext_tables.php
+++ typo3/sysext/taskcenter/ext_tables.php
@@ -4,20 +4,20 @@ defined('TYPO3_MODE') or die();
 if (TYPO3_MODE === 'BE') {
 	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModulePath(
 		'tools_txtaskcenterM1',
-		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'task/'
+		'EXT:taskcenter/Modules/Taskcenter/'
 	);
 	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModule(
 		'user',
 		'task',
 		'top',
-		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'task/',
+		'EXT:taskcenter/Modules/Taskcenter/',
 		array(
 			'script' => '_DISPATCH',
 			'access' => 'group,user',
 			'name' => 'user_task',
 			'labels' => array(
 				'tabs_images' => array(
-					'tab' => '../Resources/Public/Icons/module-taskcenter.svg',
+					'tab' => 'EXT:taskcenter/Resources/Public/Icons/module-taskcenter.svg',
 				),
 				'll_ref' => 'LLL:EXT:taskcenter/Resources/Private/Language/locallang_mod.xlf',
 			),
