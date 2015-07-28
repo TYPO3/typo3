a7bf599c731f0f712d8051eebcc33ecda2acbf86
Wouter Wolters <typo3@wouterwolters.nl>
2015-07-28 18:56:24 +0200

    [BUGFIX] Prefix font awesome icon path for admin_panel.css
    Font Awesome icons are prefixed with #TSFE_ADMIN_PANEL_FORM to
    prevent issues with CSS from the website that uses Font Awesome too.
    
    Resolves: #68591
    Releases: master
    Change-Id: I7e5b7f80c9bcf1508f938fb38d74f643dabd2b9a
    Reviewed-on: http://review.typo3.org/42058
    Reviewed-by: Markus Klein <markus.klein@typo3.org>
    Reviewed-by: Stefan Neufeind <typo3.neufeind@speedpartner.de>
    Tested-by: Stefan Neufeind <typo3.neufeind@speedpartner.de>

--- typo3/sysext/frontend/Classes/Http/RequestHandler.php
+++ typo3/sysext/frontend/Classes/Http/RequestHandler.php
@@ -267,6 +267,7 @@ class RequestHandler implements RequestHandlerInterface {
 		// Admin panel
 		if ($this->controller->isBackendUserLoggedIn() && $GLOBALS['BE_USER'] instanceof FrontendBackendUserAuthentication) {
 			if ($GLOBALS['BE_USER']->extAdmEnabled) {
+				// Style sheet is also used for frontend editing.
 				$this->controller->content = str_ireplace('</head>', $GLOBALS['BE_USER']->adminPanel->getAdminPanelHeaderData() . '</head>', $this->controller->content);
 			}
 			if ($GLOBALS['BE_USER']->isAdminPanelVisible()) {
