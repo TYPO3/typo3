db84ebca64f394267aa35c5eb567ad8fe2f78a3d
Helmut Hummel <helmut.hummel@typo3.org>
2015-07-28 17:24:53 +0200

    [FEATURE] Add command to dump class loading information
    Releases: master
    Resolves: #68589
    Change-Id: I57debc378de1f831e2d06d16ac61b6ed6df45e2c
    Reviewed-on: http://review.typo3.org/42046
    Reviewed-by: Sascha Egerer <sascha@sascha-egerer.de>
    Reviewed-by: Markus Klein <markus.klein@typo3.org>
    Reviewed-by: Wouter Wolters <typo3@wouterwolters.nl>
    Tested-by: Wouter Wolters <typo3@wouterwolters.nl>
    Reviewed-by: Stefan Neufeind <typo3.neufeind@speedpartner.de>
    Tested-by: Stefan Neufeind <typo3.neufeind@speedpartner.de>
    Reviewed-by: Helmut Hummel <helmut.hummel@typo3.org>
    Tested-by: Helmut Hummel <helmut.hummel@typo3.org>

--- typo3/sysext/extensionmanager/Classes/Command/ExtensionCommandController.php
+++ typo3/sysext/extensionmanager/Classes/Command/ExtensionCommandController.php
@@ -14,6 +14,8 @@ namespace TYPO3\CMS\Extensionmanager\Command;
  * The TYPO3 project - inspiring people to share!
  */
 
+use TYPO3\CMS\Core\Core\Bootstrap;
+use TYPO3\CMS\Core\Core\ClassLoadingInformation;
 use TYPO3\CMS\Extbase\Mvc\Controller\CommandController;
 
 /**
@@ -62,6 +64,24 @@ class ExtensionCommandController extends CommandController {
 		/** @var $service \TYPO3\CMS\Extensionmanager\Utility\InstallUtility */
 		$service = $this->objectManager->get(\TYPO3\CMS\Extensionmanager\Utility\InstallUtility::class);
 		$service->uninstall($extensionKey);
+	}
+
+	/**
+	 * Updates class loading information.
+	 *
+	 * This command is only needed during development. The extension manager takes care
+	 * creating or updating this info properly during extension (de-)activation.
+	 *
+	 * @return void
+	 */
+	public function updateClassLoadingInformationCommand() {
+		if (Bootstrap::usesComposerClassLoading()) {
+			$this->output->outputLine('<error>Class loading information is managed by composer. Use "composer dump-autoload" command to update the information.</error>');
+			$this->quit(1);
+		} else {
+			ClassLoadingInformation::writeClassLoadingInformation();
+			$this->output->outputLine('Class Loading information has been updated.');
+		}
 	}
 
 	/**
