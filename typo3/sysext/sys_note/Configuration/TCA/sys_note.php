a1b8a91345f2707033fb8355f1387bfc2520a64f
Georg Ringer <georg.ringer@gmail.com>
2015-08-11 08:47:13 +0200

    [FOLLOWUP][TASK] Move sys_note icons from EXT:t3skin to EXT:sys_note
    Change the path in the TCA of the sys_note record itself aswell
    
    Change-Id: I1f529b30228685042d5264c2d5009f8c246f5154
    Resolves: #68883
    Releases: master
    Reviewed-on: http://review.typo3.org/42498
    Reviewed-by: Benni Mack <benni@typo3.org>
    Tested-by: Benni Mack <benni@typo3.org>
    Reviewed-by: Andreas Fernandez <typo3@scripting-base.de>
    Tested-by: Andreas Fernandez <typo3@scripting-base.de>

--- typo3/sysext/sys_note/Configuration/TCA/sys_note.php
+++ typo3/sysext/sys_note/Configuration/TCA/sys_note.php
@@ -22,10 +22,10 @@ return array(
 				'type' => 'select',
 				'items' => array(
 					array('', '0'),
-					array('LLL:EXT:sys_note/Resources/Private/Language/locallang_tca.xlf:sys_note.category.I.1', '1', 'sysext/t3skin/icons/ext/sys_note/icon-instruction.png'),
-					array('LLL:EXT:sys_note/Resources/Private/Language/locallang_tca.xlf:sys_note.category.I.3', '3', 'sysext/t3skin/icons/ext/sys_note/icon-note.png'),
-					array('LLL:EXT:sys_note/Resources/Private/Language/locallang_tca.xlf:sys_note.category.I.4', '4', 'sysext/t3skin/icons/ext/sys_note/icon-todo.png'),
-					array('LLL:EXT:sys_note/Resources/Private/Language/locallang_tca.xlf:sys_note.category.I.2', '2', 'sysext/t3skin/icons/ext/sys_note/icon-template.png')
+					array('LLL:EXT:sys_note/Resources/Private/Language/locallang_tca.xlf:sys_note.category.I.1', '1', 'sysext/sys_note/Resources/Public/Icons/instruction.png'),
+					array('LLL:EXT:sys_note/Resources/Private/Language/locallang_tca.xlf:sys_note.category.I.3', '3', 'sysext/sys_note/Resources/Public/Icons/note.png'),
+					array('LLL:EXT:sys_note/Resources/Private/Language/locallang_tca.xlf:sys_note.category.I.4', '4', 'sysext/sys_note/Resources/Public/Icons/todo.png'),
+					array('LLL:EXT:sys_note/Resources/Private/Language/locallang_tca.xlf:sys_note.category.I.2', '2', 'sysext/sys_note/Resources/Public/Icons/template.png')
 				),
 				'default' => '0'
 			)
