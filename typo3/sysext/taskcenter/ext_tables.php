2f77dbd474da2777732651d560b2f69b950bf1e6
Wouter Wolters <typo3@wouterwolters.nl>
2015-08-18 23:18:25 +0200

    [TASK] Remove addModulePath call for ext:taskcenter
    Wrong module name is used.
    Is not in tools but in user.
    
    Resolves: #69174
    Releases: master
    Change-Id: Ic2d4a287529b0898c529fb1e04ee7f0b37e4586e
    Reviewed-on: http://review.typo3.org/42734
    Reviewed-by: Daniel Goerz <ervaude@gmail.com>
    Tested-by: Daniel Goerz <ervaude@gmail.com>
    Reviewed-by: Markus Klein <markus.klein@typo3.org>
    Tested-by: Markus Klein <markus.klein@typo3.org>

--- typo3/sysext/taskcenter/ext_tables.php
+++ typo3/sysext/taskcenter/ext_tables.php
@@ -2,10 +2,6 @@
 defined('TYPO3_MODE') or die();
 
 if (TYPO3_MODE === 'BE') {
-	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModulePath(
-		'tools_txtaskcenterM1',
-		'EXT:taskcenter/Modules/Taskcenter/'
-	);
 	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModule(
 		'user',
 		'task',
