acbcaa6809ac0ebda96f328354a33d3a921c5a77
Wouter Wolters <typo3@wouterwolters.nl>
2015-08-21 00:34:46 +0200

    [TASK] Add missing TYPO3_MODE checks in ext_localconf.php
    Resolves: #69222
    Releases: master
    Change-Id: I4e0d737a8035df1c178a584eed203dd48176babe
    Reviewed-on: http://review.typo3.org/42796
    Reviewed-by: Georg Ringer <georg.ringer@gmail.com>
    Tested-by: Georg Ringer <georg.ringer@gmail.com>

--- typo3/sysext/version/ext_tables.php
+++ typo3/sysext/version/ext_tables.php
@@ -1,7 +1,7 @@
 <?php
 defined('TYPO3_MODE') or die();
 
-if (TYPO3_MODE == 'BE' && !\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('workspaces')) {
+if (TYPO3_MODE === 'BE' && !\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('workspaces')) {
 	$GLOBALS['TBE_MODULES_EXT']['xMOD_alt_clickmenu']['extendCMclasses'][] = array(
 		'name' => \TYPO3\CMS\Version\ClickMenu\VersionClickMenu::class,
 	);
