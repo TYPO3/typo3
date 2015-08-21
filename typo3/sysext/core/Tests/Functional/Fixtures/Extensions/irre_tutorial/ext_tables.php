bda2e43ed86cef11759f6954e2dca8ae528e1512
Georg Ringer <georg.ringer@gmail.com>
2015-08-21 10:12:54 +0200

    [FOLLOWUP][TASK] Combine several calls to allowTableOnStandardPages
    The function accepts a comma separated string to add multiple tables.
    
    Change-Id: I51fb442969bb569cf6936c3272551daa861fdbd8
    Resolves: #69218
    Releases: master
    Reviewed-on: http://review.typo3.org/42802
    Reviewed-by: Wouter Wolters <typo3@wouterwolters.nl>
    Tested-by: Wouter Wolters <typo3@wouterwolters.nl>
    Reviewed-by: Jan Helke <typo3@helke.de>
    Reviewed-by: Georg Ringer <georg.ringer@gmail.com>
    Tested-by: Georg Ringer <georg.ringer@gmail.com>

--- typo3/sysext/core/Tests/Functional/Fixtures/Extensions/irre_tutorial/ext_tables.php
+++ typo3/sysext/core/Tests/Functional/Fixtures/Extensions/irre_tutorial/ext_tables.php
@@ -5,7 +5,7 @@ defined('TYPO3_MODE') or die();
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('irre_tutorial', 'Configuration/TypoScript', 'IRRE Tutorial');
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages(
-	'tx_irretutorial_1nff_hotel,tx_irretutorial_1nff_offer,tx_irretutorial_1nff_price,tx_irretutorial_mnasym_hotel,tx_irretutorial_1ncsv_hotel,tx_irretutorial_1ncsv_offer'
+	'tx_irretutorial_1nff_hotel,tx_irretutorial_1nff_offer,tx_irretutorial_1nff_price,tx_irretutorial_mnasym_hotel,tx_irretutorial_mnasym_hotel_offer_rel,tx_irretutorial_1ncsv_hotel,tx_irretutorial_1ncsv_offer'
 );
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages(
 	'tx_irretutorial_mnasym_offer,tx_irretutorial_mnasym_price,tx_irretutorial_mnmmasym_hotel,tx_irretutorial_mnmmasym_offer,tx_irretutorial_mnattr_offer,tx_irretutorial_1ncsv_price'
