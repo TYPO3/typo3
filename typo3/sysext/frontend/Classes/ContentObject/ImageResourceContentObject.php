119d9607090bf390a1cb299e692b8f2e90fbe399
Heiko Kromm <h.kromm@paints.de>
2015-09-03 21:27:13 +0200

    [BUGFIX] IMG_RESOURCE does not prepend absRefPrefix
    Rendering an imageResource with TypoScript IMG_RESOURCE
    does not prepend the absRefPrefix.
    This will cause problems loading images when not using config.baseUrl.
    If it is not an absolute url we prepend absRefPrefix.
    
    Resolves: #69516
    Releases: master, 6.2
    Change-Id: Icfca8bfe50cb5e1816cf73783a09d065d299521a
    Reviewed-on: http://review.typo3.org/43157
    Reviewed-by: Helmut Hummel <helmut.hummel@typo3.org>
    Tested-by: Helmut Hummel <helmut.hummel@typo3.org>

--- typo3/sysext/frontend/Classes/ContentObject/ImageResourceContentObject.php
+++ typo3/sysext/frontend/Classes/ContentObject/ImageResourceContentObject.php
@@ -30,7 +30,11 @@ class ImageResourceContentObject extends \TYPO3\CMS\Frontend\ContentObject\Abstr
 	public function render($conf = array()) {
 		$GLOBALS['TSFE']->lastImgResourceInfo = $this->cObj->getImgResource($conf['file'], $conf['file.']);
 		if ($GLOBALS['TSFE']->lastImgResourceInfo) {
-			$imageResource = $GLOBALS['TSFE']->lastImgResourceInfo[3];
+			$urlPrefix = '';
+			if (parse_url($GLOBALS['TSFE']->lastImgResourceInfo[3], PHP_URL_HOST) === NULL) {
+				$urlPrefix = $GLOBALS['TSFE']->absRefPrefix;
+			}
+			$imageResource = $urlPrefix . $GLOBALS['TSFE']->lastImgResourceInfo[3];
 			$theValue = isset($conf['stdWrap.']) ? $this->cObj->stdWrap($imageResource, $conf['stdWrap.']) : $imageResource;
 		} else {
 			$theValue = '';
