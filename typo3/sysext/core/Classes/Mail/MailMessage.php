1cd0c99f66a4cd076808f4f019ea52b9d55cede2
Morton Jonuschat <m.jonuschat@mojocode.de>
2015-09-14 13:40:41 +0200

    [TASK] IDNA encode domains in email addresses for MailMessage
    SwiftMailer does not support email addresses with UTF8 characters in the
    domain name. UTF8 characters in the domain name are valid as long as
    they are IDNA/punycode encoded.
    
    To work around the SwiftMailer issue MailMessage has been extended with
    custom setters that encode the email addresses before passing them to
    SwiftMailer.
    
    Resolves: #69208
    Releases: master
    Change-Id: I9835340c8d216f37dc94dfb65dc70822c2a47702
    Reviewed-on: http://review.typo3.org/43294
    Reviewed-by: Wouter Wolters <typo3@wouterwolters.nl>
    Reviewed-by: Nicole Cordes <typo3@cordes.co>
    Tested-by: Nicole Cordes <typo3@cordes.co>
    Reviewed-by: Loek Hilgersom <loek@netcoop.nl>
    Tested-by: Loek Hilgersom <loek@netcoop.nl>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>

--- typo3/sysext/core/Classes/Mail/MailMessage.php
+++ typo3/sysext/core/Classes/Mail/MailMessage.php
@@ -80,4 +80,171 @@ class MailMessage extends \Swift_Message {
 		return $this->failedRecipients;
 	}
 
+	/**
+	 * Set the return-path (the bounce address) of this message.
+	 *
+	 * @param string $address
+	 * @return \TYPO3\CMS\Core\Mail\MailMessage
+	 */
+	public function setReturnPath($address) {
+		$address = $this->idnaEncodeAddresses($address);
+		return parent::setReturnPath($address);
+	}
+
+	/**
+	 * Set the sender of this message.
+	 *
+	 * This does not override the From field, but it has a higher significance.
+	 *
+	 * @param string $address
+	 * @param string $name optional
+	 * @return \TYPO3\CMS\Core\Mail\MailMessage
+	 */
+	public function setSender($address, $name = NULL) {
+		$address = $this->idnaEncodeAddresses($address);
+		return parent::setSender($address, $name);
+	}
+
+	/**
+	 * Set the from address of this message.
+	 *
+	 * You may pass an array of addresses if this message is from multiple people.
+	 *
+	 * If $name is passed and the first parameter is a string, this name will be
+	 * associated with the address.
+	 *
+	 * @param string|array $addresses
+	 * @param string $name optional
+	 * @return \TYPO3\CMS\Core\Mail\MailMessage
+	 */
+	public function setFrom($addresses, $name = NULL) {
+		$addresses = $this->idnaEncodeAddresses($addresses);
+		return parent::setFrom($addresses, $name);
+	}
+
+	/**
+	 * Set the reply-to address of this message.
+	 *
+	 * You may pass an array of addresses if replies will go to multiple people.
+	 *
+	 * If $name is passed and the first parameter is a string, this name will be
+	 * associated with the address.
+	 *
+	 * @param string|array $addresses
+	 * @param string $name optional
+	 * @return \TYPO3\CMS\Core\Mail\MailMessage
+	 */
+	public function setReplyTo($addresses, $name = NULL) {
+		$addresses = $this->idnaEncodeAddresses($addresses);
+		return parent::setReplyTo($addresses, $name);
+	}
+
+	/**
+	 * Set the to addresses of this message.
+	 *
+	 * If multiple recipients will receive the message an array should be used.
+	 * Example: array('receiver@domain.org', 'other@domain.org' => 'A name')
+	 *
+	 * If $name is passed and the first parameter is a string, this name will be
+	 * associated with the address.
+	 *
+	 * @param string|array $addresses
+	 * @param string $name optional
+	 * @return \TYPO3\CMS\Core\Mail\MailMessage
+	 */
+	public function setTo($addresses, $name = NULL) {
+		$addresses = $this->idnaEncodeAddresses($addresses);
+		return parent::setTo($addresses, $name);
+	}
+
+	/**
+	 * Set the Cc addresses of this message.
+	 *
+	 * If $name is passed and the first parameter is a string, this name will be
+	 * associated with the address.
+	 *
+	 * @param string|array $addresses
+	 * @param string $name optional
+	 * @return \TYPO3\CMS\Core\Mail\MailMessage
+	 */
+	public function setCc($addresses, $name = NULL) {
+		$addresses = $this->idnaEncodeAddresses($addresses);
+		return parent::setCc($addresses, $name);
+	}
+
+	/**
+	 * Set the Bcc addresses of this message.
+	 *
+	 * If $name is passed and the first parameter is a string, this name will be
+	 * associated with the address.
+	 *
+	 * @param string|array $addresses
+	 * @param string $name optional
+	 * @return \TYPO3\CMS\Core\Mail\MailMessage
+	 */
+	public function setBcc($addresses, $name = NULL) {
+		$addresses = $this->idnaEncodeAddresses($addresses);
+		return parent::setBcc($addresses, $name);
+	}
+
+	/**
+	 * Ask for a delivery receipt from the recipient to be sent to $addresses.
+	 *
+	 * @param array $addresses
+	 * @return \TYPO3\CMS\Core\Mail\MailMessage
+	 */
+	public function setReadReceiptTo($addresses) {
+		$addresses = $this->idnaEncodeAddresses($addresses);
+		return parent::setReadReceiptTo($addresses);
+	}
+
+	/**
+	 * IDNA encode email addresses. Accepts addresses in all formats that SwiftMailer supports
+	 *
+	 * @param string|array $addresses
+	 * @return string|array
+	 */
+	protected function idnaEncodeAddresses($addresses) {
+		if (!is_array($addresses)) {
+			return $this->idnaEncodeAddress($addresses);
+		}
+		$newAddresses = [];
+		foreach ($addresses as $email => $name) {
+			if (ctype_digit($email)) {
+				$newAddresses[] = $this->idnaEncodeAddress($name);
+			} else {
+				$newAddresses[$this->idnaEncodeAddress($email)] = $name;
+			}
+		}
+
+		return $newAddresses;
+	}
+
+	/**
+	 * IDNA encode the domain part of an email address if it contains non ASCII characters
+	 *
+	 * @param mixed $email
+	 * @return mixed
+	 * @see \TYPO3\CMS\Core\Utility\GeneralUtility::validEmail
+	 */
+	protected function idnaEncodeAddress($email) {
+		// Early return in case input is not a string
+		if (!is_string($email)) {
+			return $email;
+		}
+		// Split on the last "@" since adresses like "foo@bar"@example.org are valid
+		$atPosition = strrpos($email, '@');
+		if (!$atPosition || $atPosition + 1 === strlen($email)) {
+			// Return if no @ found or it is placed at the very beginning or end of the email
+			return $email;
+		}
+		$domain = substr($email, $atPosition + 1);
+		$local = substr($email, 0, $atPosition);
+		if (!mb_check_encoding($domain, 'ASCII')) {
+			$domain = \TYPO3\CMS\Core\Utility\GeneralUtility::idnaEncode($domain);
+		}
+
+		return $local . '@' . $domain;
+	}
+
 }
