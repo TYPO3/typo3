2ef9e011a7f7dbef380e2d5429deb06f597ce1dd
Benjamin Mack <benni@typo3.org>
2015-09-17 15:32:24 +0200

    [TASK] Remove CSS Styled Content option of image position items
    The extension configuration option that disables the disabling of
    the superfluous options on CType=image (like "Left of Text") is
    removed in favor of always disabling these options, which is the default
    anyway.
    
    Integrators can still override this change in their own PageTSconfig.
    
    Resolves: #69920
    Releases: master
    Change-Id: Ibb9662be3cee5eed6e03dff3d4f7e758449c2591
    Reviewed-on: http://review.typo3.org/43398
    Reviewed-by: Andreas Fernandez <typo3@scripting-base.de>
    Reviewed-by: Morton Jonuschat <m.jonuschat@mojocode.de>
    Tested-by: Morton Jonuschat <m.jonuschat@mojocode.de>
    Reviewed-by: Wouter Wolters <typo3@wouterwolters.nl>
    Tested-by: Wouter Wolters <typo3@wouterwolters.nl>

--- typo3/sysext/css_styled_content/ext_localconf.php
+++ typo3/sysext/css_styled_content/ext_localconf.php
@@ -1,13 +1,10 @@
 <?php
 defined('TYPO3_MODE') or die();
 
-// unserializing the configuration so we can use it here:
-$_EXTCONF = unserialize($_EXTCONF);
-if (!$_EXTCONF || $_EXTCONF['removePositionTypes']) {
-	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('
-		TCEFORM.tt_content.imageorient.types.image.removeItems = 8,9,10,17,18,25,26
-	');
-}
+// Disable image positions that make no sense on CType=image (it leaves just "above left", "center" and "right")
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('
+	TCEFORM.tt_content.imageorient.types.image.removeItems = 8,9,10,17,18,25,26
+');
 
 // Mark the delivered TypoScript templates as "content rendering template" (providing the hooks of "static template 43" = content (default))
 $GLOBALS['TYPO3_CONF_VARS']['FE']['contentRenderingTemplates'][] = 'cssstyledcontent/static/';
