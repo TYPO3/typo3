16595994936f04e89e72df617cb4f9591283ad06
Mathias Schreiber <mathias.schreiber@wmdb.de>
2015-10-04 22:44:19 +0200

    [FOLLOWUP][BUGFIX] Revert BackendActionController in favor of BackendView
    Fixed a typo and added the correct license text in ViewHelper header
    
    Releases: master
    Resolves: #70358
    Change-Id: I074be930bd4d0b6ed18e63ea1980a025740e45db
    Reviewed-on: http://review.typo3.org/43799
    Reviewed-by: Wouter Wolters <typo3@wouterwolters.nl>
    Tested-by: Wouter Wolters <typo3@wouterwolters.nl>
    Reviewed-by: Michael Oehlhof <typo3@oehlhof.de>
    Tested-by: Michael Oehlhof <typo3@oehlhof.de>
    Reviewed-by: Benjamin Kott <info@bk2k.info>
    Tested-by: Benjamin Kott <info@bk2k.info>

--- typo3/sysext/backend/Classes/View/BackendTemplateView.php
+++ typo3/sysext/backend/Classes/View/BackendTemplateView.php
@@ -20,7 +20,8 @@ use TYPO3\CMS\Extbase\Mvc\Controller\ControllerContext;
 use TYPO3\CMS\Fluid\View\TemplateView;
 
 /**
- * Decorates the main template view. Should be used as view if you want Fluid Templating in a backend moduel
+ * Decorates the main template view. Should be used as view if you want to use
+ * Fluid templates in a backend module in order to have a consistent backend.
  *
  * @api
  */
