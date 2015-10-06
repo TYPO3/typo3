ebd27245a016c722a8d4ffeeae69b4215c6d97aa
Ralf Zimmermann <ralf.zimmermann@tritum.de>
2015-10-06 12:49:19 +0200

    [BUGFIX] EXT:form - Set version information
    Resolves: #70431
    Releases: master
    Change-Id: I7552a7c936f8d4260f1415a31d89d371274d74e2
    Reviewed-on: http://review.typo3.org/43853
    Reviewed-by: Wouter Wolters <typo3@wouterwolters.nl>
    Tested-by: Wouter Wolters <typo3@wouterwolters.nl>
    Reviewed-by: Andreas Fernandez <typo3@scripting-base.de>
    Tested-by: Andreas Fernandez <typo3@scripting-base.de>

--- typo3/sysext/form/ext_emconf.php
+++ typo3/sysext/form/ext_emconf.php
@@ -10,6 +10,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'author' => 'Patrick Broens, Ralf Zimmermann',
 	'author_email' => 'patrick@patrickbroens.nl, ralf.zimmermann@tritum.de',
 	'author_company' => '',
+	'version' => '7.6.0',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '7.6.0-7.6.99',
