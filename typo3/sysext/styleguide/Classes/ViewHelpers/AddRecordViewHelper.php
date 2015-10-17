be997fb1e3b728474c58b87d6f2bca4168c852ce
Wouter Wolters <typo3@wouterwolters.nl>
2015-10-17 15:03:50 +0200

    [BUGFIX] styleguide: Remove hardcoded alt_doc.php reference

---
+++ typo3/sysext/styleguide/Classes/ViewHelpers/AddRecordViewHelper.php
@@ -0,0 +1,62 @@
+<?php
+namespace TYPO3\CMS\Styleguide\ViewHelpers;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use Closure;
+use TYPO3\CMS\Backend\Utility\BackendUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Fluid\Core\Rendering\RenderingContextInterface;
+use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
+use TYPO3\CMS\Fluid\Core\ViewHelper\Facets\CompilableInterface;
+
+/**
+ * Class AddRecordViewHelper
+ */
+class AddRecordViewHelper extends AbstractViewHelper implements CompilableInterface
+{
+    /**
+     * Renders a link to add new record
+     *
+     * @return string
+     */
+    public function render()
+    {
+        return static::renderStatic(
+            [],
+            $this->buildRenderChildrenClosure(),
+            $this->renderingContext
+        );
+    }
+
+    /**
+     * Renders a link to add new tx_styleguide_forms
+     *
+     * @param array $arguments
+     * @param Closure $renderChildrenClosure
+     * @param RenderingContextInterface $renderingContext
+     *
+     * @return string
+     */
+    public static function renderStatic(array $arguments, Closure $renderChildrenClosure, RenderingContextInterface $renderingContext)
+    {
+        $params = [
+            'edit' => ['tx_styleguide_forms' => [0 => 'new']],
+            'returnUrl' => GeneralUtility::getIndpEnv('REQUEST_URI')
+        ];
+
+        return BackendUtility::getModuleUrl('record_edit', $params);
+    }
+}
+
