5f8eb9cf53dd424dbb2f1eb37a004cbbdb6f2aa0
Xavier Perseguers <xavier@typo3.org>
2015-10-22 11:37:13 +0200

    [BUGFIX] Ranking of a file may be 0
    No ranking is not the same as 1 star value.
    
    Change-Id: Ia53d99c7999d7a1fe59425bd390615872fa897c4
    Resolves: #70900
    Releases: master
    Reviewed-on: https://review.typo3.org/44216
    Reviewed-by: Markus Klein <markus.klein@typo3.org>
    Tested-by: Markus Klein <markus.klein@typo3.org>
    Reviewed-by: Andreas Fernandez <typo3@scripting-base.de>
    Tested-by: Andreas Fernandez <typo3@scripting-base.de>

--- typo3/sysext/filemetadata/Configuration/TCA/Overrides/sys_file_metadata.php
+++ typo3/sysext/filemetadata/Configuration/TCA/Overrides/sys_file_metadata.php
@@ -283,6 +283,7 @@ $tca = array(
                 'minitems' => 1,
                 'maxitems' => 1,
                 'items' => array(
+                    array(0, 0),
                     array(1, 1),
                     array(2, 2),
                     array(3, 3),
