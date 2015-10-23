96fabbf06587cd85634ca8c5eb6bea30d1f00714
Mathias Schreiber <mathias.schreiber@wmdb.de>
2015-10-23 23:01:59 +0200

    [BUGFIX] Adjust renderForeignRecordHeader to array
    The FormDataProvider refactoring changed DB values
    from strings to arrays when used with select fields.
    The code usage in InlineRecordContainer is adopted to
    use the new data structure.
    
    Resolves: #70951
    Releases: master
    Change-Id: Id992232c36521622a06fbd8892e66398dcede35c
    Reviewed-on: https://review.typo3.org/44254
    Reviewed-by: Sebastian Fischer <typo3@evoweb.de>
    Tested-by: Sebastian Fischer <typo3@evoweb.de>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>

--- typo3/sysext/backend/Classes/Form/Container/InlineRecordContainer.php
+++ typo3/sysext/backend/Classes/Form/Container/InlineRecordContainer.php
@@ -345,7 +345,7 @@ class InlineRecordContainer extends AbstractContainer
                 $recTemp = BackendUtility::getRecordWSOL($itemParts[0], $itemParts[1]);
                 $recTitle = BackendUtility::getRecordTitle($itemParts[0], $recTemp, false);
             } else {
-                $recTitle = BackendUtility::getProcessedValueExtra($foreign_table, $titleCol, $rec[$titleCol], 0, 0, false);
+                $recTitle = BackendUtility::getProcessedValueExtra($foreign_table, $titleCol, $rec[$titleCol][0], 0, 0, false);
             }
             $recTitle = BackendUtility::getRecordTitlePrep($recTitle);
             if (trim($recTitle) === '') {
