d44fddfe0d72b20984dbef148bf3e1dc9eb102d6
Andreas Fernandez <a.fernandez@scripting-base.de>
2015-10-24 22:20:57 +0200

    [TASK] Update autocomplete to 1.2.24
    This patch updates jQuery.autocomplete to version 1.2.24.
    This patch also adds a modification made by the Core team. The
    corresponding Pull Request against the library repository was not
    merged yet.
    
    Resolves: #70988
    Releases: master
    Change-Id: Ie9fde5d24d67d2ece882887b02011f0e65ecc672
    Reviewed-on: https://review.typo3.org/44255
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Reviewed-by: Stefan Neufeind <typo3.neufeind@speedpartner.de>
    Tested-by: Stefan Neufeind <typo3.neufeind@speedpartner.de>

--- typo3/sysext/core/Resources/Public/JavaScript/Contrib/jquery.autocomplete.js
+++ typo3/sysext/core/Resources/Public/JavaScript/Contrib/jquery.autocomplete.js
@@ -127,15 +127,15 @@
     $.Autocomplete = Autocomplete;
 
     Autocomplete.formatResult = function (suggestion, currentValue) {
-        var htmlSafeString = suggestion.value
+        var pattern = '(' + utils.escapeRegExChars(currentValue) + ')';
+
+        return suggestion.value
+            .replace(new RegExp(pattern, 'gi'), '<strong>$1<\/strong>')
             .replace(/&/g, '&amp;')
             .replace(/</g, '&lt;')
             .replace(/>/g, '&gt;')
-            .replace(/"/g, '&quot;');
-
-        var pattern = '(' + utils.escapeRegExChars(currentValue) + ')';
-
-        return htmlSafeString.replace(new RegExp(pattern, 'gi'), '<strong>$1<\/strong>');
+            .replace(/"/g, '&quot;')
+            .replace(/&lt;(\/?strong)&gt;/g, '<$1>');
     };
 
     Autocomplete.prototype = {
@@ -217,7 +217,7 @@
         onBlur: function () {
             this.enableKillerFn();
         },
-        
+
         abortAjax: function () {
             var that = this;
             if (that.currentRequest) {
@@ -346,7 +346,11 @@
             var that = this;
             that.stopKillSuggestions();
             that.intervalId = window.setInterval(function () {
-                that.hide();
+                if (that.visible) {
+                    that.el.val(that.currentValue);
+                    that.hide();
+                }
+
                 that.stopKillSuggestions();
             }, 50);
         },
