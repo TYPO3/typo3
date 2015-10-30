df0b9a53b3a82aa1351484164081d768b65fd844
Markus Klein <markus.klein@typo3.org>
2015-10-30 14:53:33 +0100

    [TASK] FSC: Do not show pageTS template if it's included by default
    If the extension fluid_styled_content is configured to include the
    pageTS by default, there is no need to show the template in the
    page resources.
    
    Resolves: #71170
    Releases: master
    Change-Id: I61d44fa0c5b347d2622f8117afb8735c7a597b80
    Reviewed-on: https://review.typo3.org/44400
    Reviewed-by: Daniel Goerz <ervaude@gmail.com>
    Tested-by: Daniel Goerz <ervaude@gmail.com>
    Reviewed-by: Jan Helke <typo3@helke.de>
    Tested-by: Jan Helke <typo3@helke.de>

--- typo3/sysext/fluid_styled_content/ext_localconf.php
+++ typo3/sysext/fluid_styled_content/ext_localconf.php
@@ -1,16 +1,23 @@
 <?php
 defined('TYPO3_MODE') or die();
 
-// Get the extension configuration
-$extConf = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY]);
-
 // Define TypoScript as content rendering template
 $GLOBALS['TYPO3_CONF_VARS']['FE']['contentRenderingTemplates'][] = 'fluidstyledcontent/Configuration/TypoScript/Static/';
 
 // Register for hook to show preview of tt_content element of CType="textmedia" in page module
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawItem']['textmedia'] = \TYPO3\CMS\FluidStyledContent\Hooks\TextmediaPreviewRenderer::class;
 
-if (!isset($extConf['loadContentElementWizardTsConfig']) || (int)$extConf['loadContentElementWizardTsConfig'] === 1) {
-    // Include new content elements to modWizards
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:fluid_styled_content/Configuration/PageTSconfig/NewContentElementWizard.ts">');
+if (TYPO3_MODE === 'BE') {
+    call_user_func(
+        function ($extKey) {
+            // Get the extension configuration
+            $extConf = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$extKey]);
+
+            if (!isset($extConf['loadContentElementWizardTsConfig']) || (int)$extConf['loadContentElementWizardTsConfig'] === 1) {
+                // Include new content elements to modWizards
+                \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:fluid_styled_content/Configuration/PageTSconfig/NewContentElementWizard.ts">');
+            }
+        },
+        $_EXTKEY
+    );
 }
