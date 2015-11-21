a313fedbe6548cefe14002ab31d0299ccc471696
Felix Kopp <felix-source@phorax.com>
2015-11-21 12:26:57 +0100

    [FEATURE] styleguide: Kauderwelsch cli command

---
+++ typo3/sysext/styleguide/Classes/Command/StyleguideCommandController.php
@@ -0,0 +1,44 @@
+<?php
+namespace TYPO3\CMS\Styleguide\Command;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use TYPO3\CMS\Core\Core\Bootstrap;
+use TYPO3\CMS\Core\Core\ClassLoadingInformation;
+use TYPO3\CMS\Extbase\Mvc\Controller\CommandController;
+
+/**
+ * Styleguide commands
+ */
+class StyleguideCommandController extends CommandController
+{
+    /**
+     * @var bool
+     */
+    protected $requestAdminPermissions = true;
+
+    /**
+     * Random Kauderwelsch quote
+     *
+     * @return string
+     * @cli
+     */
+    public function KauderwelschCommand()
+    {
+        /** @var $service \TYPO3\CMS\Styleguide\Service\KauderwelschService */
+        $service = $this->objectManager->get(\TYPO3\CMS\Styleguide\Service\KauderwelschService::class);
+        return $service->getLoremIpsum();
+    }
+
+}
--- typo3/sysext/styleguide/ext_localconf.php
+++ typo3/sysext/styleguide/ext_localconf.php
@@ -5,3 +5,6 @@ $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tce']['formevals']['TYPO3\\CMS\\Style
 
 // Register evaluateFieldValue() and deevaluateFieldValue() for input_21 field
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tce']['formevals']['TYPO3\\CMS\\Styleguide\\UserFunctions\\FormEngine\\TypeText9Eval'] = '';
+
+// Register command controller for console command
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = \TYPO3\CMS\Styleguide\Command\StyleguideCommandController::class;
