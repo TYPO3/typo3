2dbcfaad45983c46516e39ca36956088eb66e4f0
Georg Ringer <georg.ringer@gmail.com>
2015-12-15 11:34:33 +0100

    [SECURITY] XSS in Link Validator
    Properly escape user input when showing broken links
    in EXT:linkvalidator
    
    Resolves: #68735
    Releases: master,6.2
    Security-Commit: 8c9a2eaa768534d8781073889c07e075e1c3c34e
    Security-Bulletins: TYPO3-CORE-SA-2015-010, 011, 012, 013, 014, 015
    Change-Id: Ic2cf727c4b3c213503254ee79301553eb6739a1a
    Reviewed-on: https://review.typo3.org/45268
    Reviewed-by: Oliver Hader <oliver.hader@typo3.org>
    Tested-by: Oliver Hader <oliver.hader@typo3.org>

--- typo3/sysext/linkvalidator/Classes/Report/LinkValidatorReport.php
+++ typo3/sysext/linkvalidator/Classes/Report/LinkValidatorReport.php
@@ -513,13 +513,10 @@ class LinkValidatorReport extends \TYPO3\CMS\Backend\Module\AbstractFunctionModu
 			$GLOBALS['BACK_PATH'],
 			$requestUri
 		));
-		$actionLink .= '" title="' . $GLOBALS['LANG']->getLL('list.edit') . '">';
+		$actionLink .= '" title="' . htmlspecialchars($GLOBALS['LANG']->getLL('list.edit')) . '">';
 		$actionLink .= \TYPO3\CMS\Backend\Utility\IconUtility::getSpriteIcon('actions-document-open');
 		$actionLink .= '</a>';
 		$elementHeadline = $row['headline'];
-		if (empty($elementHeadline)) {
-			$elementHeadline = '<i>' . $GLOBALS['LANG']->getLL('list.no.headline') . '</i>';
-		}
 		// Get the language label for the field from TCA
 		if ($GLOBALS['TCA'][$table]['columns'][$row['field']]['label']) {
 			$fieldName = $GLOBALS['TCA'][$table]['columns'][$row['field']]['label'];
@@ -537,16 +534,20 @@ class LinkValidatorReport extends \TYPO3\CMS\Backend\Module\AbstractFunctionModu
 			$row,
 			array('title' => $table . ':' . $row['record_uid'])
 		);
-		$element .= $elementHeadline;
-		$element .= ' ' . sprintf($GLOBALS['LANG']->getLL('list.field'), $fieldName);
+		if (empty($elementHeadline)) {
+			$element .= '<i>' . htmlspecialchars($GLOBALS['LANG']->getLL('list.no.headline')) . '</i>';
+		} else {
+			$element .= htmlspecialchars($elementHeadline);
+		}
+		$element .= ' ' . htmlspecialchars(sprintf($GLOBALS['LANG']->getLL('list.field'), $fieldName));
 		$markerArray['actionlink'] = $actionLink;
 		$markerArray['path'] = BackendUtility::getRecordPath($row['record_pid'], '', 0, 0);
 		$markerArray['element'] = $element;
-		$markerArray['headlink'] = $row['link_title'];
-		$markerArray['linktarget'] = $brokenUrl;
+		$markerArray['headlink'] = htmlspecialchars($row['link_title']);
+		$markerArray['linktarget'] = htmlspecialchars($brokenUrl);
 		$response = unserialize($row['url_response']);
 		if ($response['valid']) {
-			$linkMessage = '<span style="color: green;">' . $GLOBALS['LANG']->getLL('list.msg.ok') . '</span>';
+			$linkMessage = '<span style="color: green;">' . htmlspecialchars($GLOBALS['LANG']->getLL('list.msg.ok')) . '</span>';
 		} else {
 			$linkMessage = '<span style="color: red;">' . $hookObj->getErrorMessage($response['errorParams']) . '</span>';
 		}
@@ -554,7 +555,7 @@ class LinkValidatorReport extends \TYPO3\CMS\Backend\Module\AbstractFunctionModu
 		$lastRunDate = date($GLOBALS['TYPO3_CONF_VARS']['SYS']['ddmmyy'], $row['last_check']);
 		$lastRunTime = date($GLOBALS['TYPO3_CONF_VARS']['SYS']['hhmm'], $row['last_check']);
 		$message = sprintf($GLOBALS['LANG']->getLL('list.msg.lastRun'), $lastRunDate, $lastRunTime);
-		$markerArray['lastcheck'] = $message;
+		$markerArray['lastcheck'] = htmlspecialchars($message);
 		// Return the table html code as string
 		return \TYPO3\CMS\Core\Html\HtmlParser::substituteMarkerArray($brokenLinksItemTemplate, $markerArray, '###|###', TRUE, TRUE);
 	}
