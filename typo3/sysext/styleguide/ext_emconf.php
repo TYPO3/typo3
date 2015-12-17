e1f3deccf3f9fa3fed91013f14b66ab6f848ec67
Jens Marquardt <github@jensmarquardt.com>
2015-12-17 19:23:05 +0100

    [TASK] styleguide: Set minimum TYPO3 version to 7.6.1 due to the use of the TCA type group with internal_type folder

--- typo3/sysext/styleguide/ext_emconf.php
+++ typo3/sysext/styleguide/ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '7.6.0',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '7.6.0-7.99.99',
+            'typo3' => '7.6.1-7.99.99',
         ),
         'conflicts' => array(
         ),
