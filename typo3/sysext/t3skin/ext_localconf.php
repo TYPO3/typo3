70b39357d3538ee57b0c7638a830c316a85c3574
Benni Mack <benni@typo3.org>
2016-01-12 15:54:02 +0100

    [TASK] Remove RTE t3skin css overrides
    Resolves: #72655
    Releases: master
    Change-Id: I49bad0f2fcb0a5fdd8e7916bc6fc87c3caab4d6c
    Reviewed-on: https://review.typo3.org/45837
    Reviewed-by: Georg Ringer <georg.ringer@gmail.com>
    Tested-by: Georg Ringer <georg.ringer@gmail.com>
    Reviewed-by: Morton Jonuschat <m.jonuschat@mojocode.de>
    Tested-by: Morton Jonuschat <m.jonuschat@mojocode.de>

--- typo3/sysext/t3skin/ext_localconf.php
+++
@@ -1,7 +0,0 @@
-<?php
-defined('TYPO3_MODE') or die();
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('
-	RTE.default.skin = EXT:t3skin/rtehtmlarea/htmlarea.css
-	RTE.default.FE.skin = EXT:t3skin/rtehtmlarea/htmlarea.css
-');
