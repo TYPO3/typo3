9d28a8419883b6641391038228e04077c7e58eeb
Benni Mack <benni@typo3.org>
2016-01-13 09:20:10 +0100

    [TASK] Remove unused userTSconfig default options
    These options are not used at all anymore.
    
    Resolves: #72672
    Releases: master
    Change-Id: I6f108f6335bd74fd2ac75c3da9b4dc63d5da0f13
    Reviewed-on: https://review.typo3.org/45862
    Reviewed-by: Wouter Wolters <typo3@wouterwolters.nl>
    Tested-by: Wouter Wolters <typo3@wouterwolters.nl>
    Reviewed-by: Andreas Fernandez <typo3@scripting-base.de>
    Tested-by: Andreas Fernandez <typo3@scripting-base.de>

--- typo3/sysext/core/Classes/Authentication/BackendUserAuthentication.php
+++ typo3/sysext/core/Classes/Authentication/BackendUserAuthentication.php
@@ -281,12 +281,9 @@ class BackendUserAuthentication extends \TYPO3\CMS\Core\Authentication\AbstractU
         'thumbnailsByDefault' => 1,
         'emailMeAtLogin' => 0,
         'startModule' => 'help_AboutmodulesAboutmodules',
-        'hideSubmoduleIcons' => 0,
         'titleLen' => 50,
         'edit_RTE' => '1',
         'edit_docModuleUpload' => '1',
-        // Default is 245 pixels
-        'navFrameResizable' => 0,
         'resizeTextareas' => 1,
         'resizeTextareas_MaxHeight' => 500,
         'resizeTextareas_Flexible' => 0
