0c1d85b425bd895bad96321fc0d2a8a69d6498c2
Morton Jonuschat <m.jonuschat@mojocode.de>
2016-01-16 13:49:50 +0100

    [FOLLOWUP][BUGFIX] Backend users: Exception in element browser with PHP 7
    Resolves: #72767
    Releases: master, 7.6
    Change-Id: I0c585c0c50674aabf40aeec0af176bb34ad0edfd
    Reviewed-on: https://review.typo3.org/46006
    Reviewed-by: Anja Leichsenring <aleichsenring@ab-softlab.de>
    Tested-by: Anja Leichsenring <aleichsenring@ab-softlab.de>
    Reviewed-by: Georg Ringer <georg.ringer@gmail.com>
    Tested-by: Georg Ringer <georg.ringer@gmail.com>

--- typo3/sysext/recordlist/Classes/Tree/View/ElementBrowserPageTreeView.php
+++ typo3/sysext/recordlist/Classes/Tree/View/ElementBrowserPageTreeView.php
@@ -48,13 +48,13 @@ class ElementBrowserPageTreeView extends \TYPO3\CMS\Backend\Tree\View\ElementBro
         if ($ext_pArrPages && $v['uid']) {
             $iconFactory = GeneralUtility::makeInstance(IconFactory::class);
             $ficon = $iconFactory->getIconForRecord('pages', $v, Icon::SIZE_SMALL)->render();
-            $out = '<span data-uid="' . htmlspecialchars($row['uid']) . '" data-table="pages" data-title="' . htmlspecialchars($row['title']) . '" data-icon="' . htmlspecialchars($ficon) . '">';
+            $out = '<span data-uid="' . htmlspecialchars($v['uid']) . '" data-table="pages" data-title="' . htmlspecialchars($v['title']) . '" data-icon="' . htmlspecialchars($ficon) . '">';
             $out .= '<a href="#" data-close="1">' . $title . '</a>';
             $out .= '</span>';
             return $out;
         }
 
-        $parameters = GeneralUtility::implodeArrayForUrl('', $this->linkParameterProvider->getUrlParameters(['pid' => $row['uid']]));
+        $parameters = GeneralUtility::implodeArrayForUrl('', $this->linkParameterProvider->getUrlParameters(['pid' => $v['uid']]));
         return '<a href="#" onclick="return jumpToUrl(' . htmlspecialchars(GeneralUtility::quoteJSvalue($this->getThisScript() . ltrim($parameters, '&'))) . ');">' . $title . '</a>';
     }
 }
