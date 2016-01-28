d87cb74d535f8e85aad3d092570500bb88ecfc4a
Benni Mack <benni@typo3.org>
2016-01-28 19:27:06 +0100

    [BUGFIX] Remove obsolete CSH labels
    The new labels that were replaced for 4.5 can now be migrated
    to the location of the "old" CSH language files.
    
    The hooks can be removed therefore.
    
    Resolves: #72997
    Releases: master, 7.6
    Change-Id: Ief763b1e362ff616f8e4a87f2643882d9bd9b96a
    Reviewed-on: https://review.typo3.org/46294
    Reviewed-by: Georg Ringer <georg.ringer@gmail.com>
    Tested-by: Georg Ringer <georg.ringer@gmail.com>
    Reviewed-by: Xavier Perseguers <xavier@typo3.org>
    Tested-by: Xavier Perseguers <xavier@typo3.org>

--- typo3/sysext/context_help/ext_tables.php
+++ typo3/sysext/context_help/ext_tables.php
@@ -11,6 +11,3 @@ defined('TYPO3_MODE') or die();
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('sys_template', 'EXT:context_help/Resources/Private/Language/locallang_csh_systmpl.xlf');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tt_content', 'EXT:context_help/Resources/Private/Language/locallang_csh_ttcontent.xlf');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('sys_file_reference', 'EXT:context_help/Resources/Private/Language/locallang_csh_sysfilereference.xlf');
-// Labels for TYPO3 4.5 and greater.  These labels override the ones set above, while still falling back to the original labels if no translation is available.
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['EXT:context_help/Resources/Private/Language/locallang_csh_pages.xlf'][] = 'EXT:context_help/Resources/Private/Language/4.5/locallang_csh_pages.xlf';
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['EXT:context_help/Resources/Private/Language/locallang_csh_ttcontent.xlf'][] = 'EXT:context_help/Resources/Private/Language/4.5/locallang_csh_ttcontent.xlf';
