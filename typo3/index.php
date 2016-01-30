90e01ad4787ea9cf8c0aee8f1fb6ae244e9f31f1
Mathias Brodala <mbrodala@pagemachine.de>
2016-01-30 13:20:07 +0100

    [TASK] Move typo3/index.php to backend extension
    This moves the "typo3/index.php" to the backend extension
    as "backend.php". The "typo3/index.php" becomes a stub which
    includes the "backend.php".
    
    This setup works in the following cases:
    
    1) Package "typo3/cms" is the root package
    2) The "typo3/" directory is symlinked into the web-dir
    3) The "typo3/" directory is copied into the web-dir
    
    Resolves: #73026
    Releases: master
    Change-Id: I0e171d03cbca9e3cd91d9dc94b01f16b6109ac5d
    Reviewed-on: https://review.typo3.org/46392
    Reviewed-by: Joerg Boesche <typo3@joergboesche.de>
    Tested-by: Joerg Boesche <typo3@joergboesche.de>
    Reviewed-by: Susanne Moog <typo3@susannemoog.de>
    Tested-by: Susanne Moog <typo3@susannemoog.de>
    Reviewed-by: Gordon Brüggemann <gordon.brueggemann@gmx.de>
    Tested-by: Gordon Brüggemann <gordon.brueggemann@gmx.de>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>

--- typo3/index.php
+++ typo3/index.php
@@ -1,26 +1,3 @@
 <?php
-/*
- * This file is part of the TYPO3 CMS project.
- *
- * It is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License, either version 2
- * of the License, or any later version.
- *
- * For the full copyright and license information, please read the
- * LICENSE.txt file that was distributed with this source code.
- *
- * The TYPO3 project - inspiring people to share!
- */
 
-// Exit early if php requirement is not satisfied.
-if (version_compare(PHP_VERSION, '7.0.0', '<')) {
-    die('This version of TYPO3 CMS requires PHP 7.0 or above');
-}
-
-/**
- * Main entry point for all backend-related scripts.
- */
-call_user_func(function () {
-    $classLoader = require __DIR__ . '/../vendor/autoload.php';
-    (new \TYPO3\CMS\Backend\Http\Application($classLoader))->run();
-});
+require __DIR__ . '/sysext/backend/Resources/Private/Php/backend.php';
---
+++ typo3/sysext/backend/Resources/Private/Php/backend.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+// Exit early if php requirement is not satisfied.
+if (version_compare(PHP_VERSION, '7.0.0', '<')) {
+    die('This version of TYPO3 CMS requires PHP 7.0 or above');
+}
+
+// Set up the application for the backend
+call_user_func(function () {
+    $classLoader = require __DIR__ . '/../../../../../../vendor/autoload.php';
+
+    (new \TYPO3\CMS\Backend\Http\Application($classLoader))->run();
+});
