57fc7e35cb812aafcae40da53752b0ae466651f6
Mathias Brodala <mbrodala@pagemachine.de>
2016-01-30 13:33:44 +0100

    [TASK] Move typo3/cli_dispatch.phpsh to backend extension
    This moves the "typo3/cli_dispatch.phpsh" to the backend
    extension as "cli.php". The "typo3/cli_dispatch.phpsh"
    becomes a stub which includes the "cli.php".
    
    Resolves: #73029
    Releases: master
    Change-Id: If5daea584291a9a89c4b805c98799945c424f65f
    Reviewed-on: https://review.typo3.org/46394
    Reviewed-by: Oliver Klee <typo3-coding@oliverklee.de>
    Reviewed-by: Xavier Perseguers <xavier@typo3.org>
    Reviewed-by: Oliver Hader <oliver.hader@typo3.org>
    Tested-by: Oliver Hader <oliver.hader@typo3.org>
    Reviewed-by: Frank Naegler <frank.naegler@typo3.org>
    Tested-by: Frank Naegler <frank.naegler@typo3.org>

---
+++ typo3/sysext/backend/Resources/Private/Php/cli.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+// Exit early if php requirement is not satisfied.
+if (version_compare(PHP_VERSION, '7.0.0', '<')) {
+    die('This version of TYPO3 CMS requires PHP 7.0 or above');
+}
+
+/**
+ * Command Line Interface module dispatcher
+ *
+ * This script takes a "cliKey" as first argument and uses that to dispatch
+ * the call to a registered script with that key.
+ * Valid cliKeys must be registered in
+ * $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['GLOBAL']['cliKeys'].
+ */
+call_user_func(function() {
+    $classLoader = require __DIR__ . '/../../../../../../vendor/autoload.php';
+
+    (new \TYPO3\CMS\Backend\Console\Application($classLoader))->run();
+});
