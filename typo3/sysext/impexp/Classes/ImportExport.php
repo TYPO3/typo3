df80dee7b34f8a5bdef4dd55897ef9b6d14f0648
Benni Mack <benni@typo3.org>
2016-02-05 22:30:55 +0100

    [BUGFIX] ImpExp: Correct casing for FAL function call
    The function call on the folder object must use correct casing of
    the function name for PHP 7 compatibility.
    
    Resolves: #73158
    Releases: master, 7.6, 6.2
    Change-Id: I09e45663707b462914f361de560eba1b3e3bcbf0
    Reviewed-on: https://review.typo3.org/46513
    Reviewed-by: Benni Mack <benni@typo3.org>
    Tested-by: Benni Mack <benni@typo3.org>

--- typo3/sysext/impexp/Classes/ImportExport.php
+++ typo3/sysext/impexp/Classes/ImportExport.php
@@ -1700,7 +1700,7 @@ class ImportExport {
 				$this->error('Error: the import folder in the default upload folder could not be created! No files will be imported!');
 			}
 		} else {
-			$this->legacyImportFolder = $folder->getSubFolder($this->legacyImportTargetPath);
+			$this->legacyImportFolder = $folder->getSubfolder($this->legacyImportTargetPath);
 		}
 
 	}
