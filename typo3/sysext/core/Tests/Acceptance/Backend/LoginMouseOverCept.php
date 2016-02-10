7c5bb2a80b08a6015bbe1a25534c01b95adfbf94
Roland Golla <rolandgolla@gmail.com>
2016-02-10 10:40:01 +0100

    [TASK] Enable codeception and add simple mouse test
    Resolves: #73216
    Releases: master
    Change-Id: I0390afebbf296748452dd85a0fd5f34fb53adff4
    Reviewed-on: https://review.typo3.org/46566
    Reviewed-by: Oliver Hader <oliver.hader@typo3.org>
    Reviewed-by: Frank Naegler <frank.naegler@typo3.org>
    Tested-by: Frank Naegler <frank.naegler@typo3.org>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>

---
+++ typo3/sysext/core/Tests/Acceptance/Backend/LoginMouseOverCept.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This tests login mouse over button.
+ */
+
+$I = new AcceptanceTester($scenario);
+$I->wantTo('check login functions');
+$I->amOnPage('/typo3');
+$I->waitForElement('#t3-username');
+$I->wantTo('mouse over css change login button');
+
+$bs = $I->executeInSelenium(function(\Facebook\WebDriver\Remote\RemoteWebDriver $webdriver) {
+    return $webdriver->findElement(WebDriverBy::cssSelector('#t3-login-submit'))->getCSSValue('box-shadow');
+});
+
+$I->moveMouseOver('#t3-login-submit');
+$I->wait(1);
+$bsmo = $I->executeInSelenium(function(\Facebook\WebDriver\Remote\RemoteWebDriver $webdriver) {
+    return $webdriver->findElement(WebDriverBy::cssSelector('#t3-login-submit'))->getCSSValue('box-shadow');
+});
+$this->assertFalse($bs == $bsmo);
