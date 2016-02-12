d5f456e32a3be21c0111bf0e1ef012bab90eea67
Benni Mack <benni@typo3.org>
2016-02-12 08:20:39 +0100

    [BUGFIX] Remove invalid AJAX endpoint for EXT:recycler
    A leftover old-school AJAX handler
    with an invalid endpoint (pointing to an actual
    Cleaner Scheduler Task) was removed.
    
    Resolves: #73351
    Releases: master, 7.6
    Change-Id: I953a50e7d381e894cc473e6bab1fbb854551bf5c
    Reviewed-on: https://review.typo3.org/46605
    Reviewed-by: Morton Jonuschat <m.jonuschat@mojocode.de>
    Tested-by: Morton Jonuschat <m.jonuschat@mojocode.de>
    Reviewed-by: Wouter Wolters <typo3@wouterwolters.nl>
    Tested-by: Wouter Wolters <typo3@wouterwolters.nl>

--- typo3/sysext/recycler/ext_localconf.php
+++ typo3/sysext/recycler/ext_localconf.php
@@ -1,11 +1,6 @@
 <?php
 defined('TYPO3_MODE') or die();
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerAjaxHandler(
-    'RecyclerAjaxController::init',
-    \TYPO3\CMS\Recycler\Task\CleanerTask::class . '->init'
-);
-
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][\TYPO3\CMS\Recycler\Task\CleanerTask::class] = array(
     'extension' => 'recycler',
     'title' => 'LLL:EXT:recycler/Resources/Private/Language/locallang_tasks.xlf:cleanerTaskTitle',
