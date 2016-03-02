ed32c255e5f87dd507333c2c413544b95fb28e4a
Christian Kuhn <lolli@schwarzbu.ch>
2016-03-02 18:48:00 +0100

    [TASK] Cleanup TestBase
    We usually do not add inline @string annotations. Remove them.
    
    Change-Id: Idf83dbfdc7fc092e5d70d4004e8c5093eb26f491
    Resolves: #73878
    Releases: master
    Reviewed-on: https://review.typo3.org/47001
    Reviewed-by: Andreas Fernandez <typo3@scripting-base.de>
    Tested-by: Andreas Fernandez <typo3@scripting-base.de>
    Reviewed-by: Wouter Wolters <typo3@wouterwolters.nl>
    Tested-by: Wouter Wolters <typo3@wouterwolters.nl>

--- typo3/sysext/core/Tests/Testbase.php
+++ typo3/sysext/core/Tests/Testbase.php
@@ -80,9 +80,7 @@ class Testbase {
      */
     public function defineSitePath()
     {
-        /** @var string */
         define('PATH_site', $this->getWebRoot());
-        /** @var string */
         define('PATH_thisScript', PATH_site . 'typo3/cli_dispatch.phpsh');
         $_SERVER['SCRIPT_NAME'] = PATH_thisScript;
 
@@ -101,7 +99,6 @@ class Testbase {
     public function defineOriginalRootPath()
     {
         if (!defined('ORIGINAL_ROOT')) {
-            /** @var string */
             define('ORIGINAL_ROOT', $this->getWebRoot());
         }
 
@@ -117,7 +114,6 @@ class Testbase {
      */
     public function defineTypo3ModeBe()
     {
-        /** @var string */
         define('TYPO3_MODE', 'BE');
     }
 
@@ -128,7 +124,6 @@ class Testbase {
      */
     public function setTypo3TestingContext()
     {
-        /** @var string */
         putenv('TYPO3_CONTEXT=Testing');
     }
 
