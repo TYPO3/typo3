8e187593cfd382fbbcc309c523cd2bd4132ce23f
Christian Kuhn <lolli@schwarzbu.ch>
2016-03-09 11:55:22 +0100

    [BUGFIX] styleguide: Properly register tca record icon
    Fixes the missing icon in "TCA / Records" action.

--- typo3/sysext/styleguide/ext_localconf.php
+++ typo3/sysext/styleguide/ext_localconf.php
@@ -8,3 +8,11 @@ $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tce']['formevals']['TYPO3\\CMS\\Style
 
 // Register command controller for console command
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = \TYPO3\CMS\Styleguide\Command\StyleguideCommandController::class;
+
+/** @var \TYPO3\CMS\Core\Imaging\IconRegistry $iconRegistry */
+$iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+$iconRegistry->registerIcon(
+    'tcarecords-tx_styleguide_forms-default',
+    TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+    [ 'source' => 'EXT:styleguide/Resources/Public/Icons/tx_styleguide.svg' ]
+);
