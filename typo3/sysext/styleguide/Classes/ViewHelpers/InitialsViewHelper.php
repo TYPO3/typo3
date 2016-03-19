260b989a2fcfc0b6dce6b9a1902918e51f4382dd
Felix Kopp <felix-source@phorax.com>
2016-03-19 12:55:21 +0100

    [FEATURE] styleguide: Index/home layout fence
    Signed-off-by: Felix Kopp <felix-source@phorax.com>

---
+++ typo3/sysext/styleguide/Classes/ViewHelpers/InitialsViewHelper.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+namespace TYPO3\CMS\Styleguide\ViewHelpers;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
+
+/**
+ * Class CodeViewHelper
+ */
+class InitialsViewHelper extends AbstractViewHelper
+{
+
+    /**
+     * Initializes the arguments
+     */
+    public function initializeArguments()
+    {
+        $this->registerArgument('text', 'string', 'the text to trim into initials', true);
+    }
+
+    /**
+     * @return string
+     */
+    public function render()
+    {
+        return substr($this->arguments['text'], 0, 1) . strtolower(substr($this->arguments['text'], 1, 1));
+    }
+}
