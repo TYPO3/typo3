f6e95e507d5da63af984fee6209319b79f3af350
Felix Kopp <felix-source@phorax.com>
2016-03-19 12:18:35 +0100

    [BUGFIX] styleguide: Watch less sub-directories
    Signed-off-by: Felix Kopp <felix-source@phorax.com>

--- typo3/sysext/styleguide/Gruntfile.js
+++ typo3/sysext/styleguide/Gruntfile.js
@@ -46,7 +46,7 @@ module.exports = function(grunt) {
 		},
 		watch: {
 			less: {
-				files: '<%= paths.private %>**/*.less',
+				files: '<%= paths.private %>/**/*.less',
 				tasks: 'less'
 			}
 		}
