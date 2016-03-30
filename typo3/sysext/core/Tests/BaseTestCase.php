794b9d76ec529ebbb5bc8f80ddfad394b498a494
Oliver Klee <typo3-coding@oliverklee.de>
2016-03-30 15:52:55 +0200

    [BUGFIX] Allow passing null for $methods in getAccessibleMock
    The method getMock() in the parent class also allows null for
    $methods, meaning "mock no methods".
    
    Releases: master, 7.6
    Resolves: #75316
    Change-Id: I7e14d4bc4082b41ed52ab967df87876a3755a105
    Reviewed-on: https://review.typo3.org/47426
    Reviewed-by: Wouter Wolters <typo3@wouterwolters.nl>
    Reviewed-by: Susanne Moog <typo3@susannemoog.de>
    Tested-by: Susanne Moog <typo3@susannemoog.de>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>

--- typo3/sysext/core/Tests/BaseTestCase.php
+++ typo3/sysext/core/Tests/BaseTestCase.php
@@ -43,7 +43,7 @@ abstract class BaseTestCase extends \PHPUnit_Framework_TestCase
      * Creates a mock object which allows for calling protected methods and access of protected properties.
      *
      * @param string $originalClassName name of class to create the mock object of, must not be empty
-     * @param array<string> $methods name of the methods to mock
+     * @param string[]|null $methods name of the methods to mock, null for "mock no methods"
      * @param array $arguments arguments to pass to constructor
      * @param string $mockClassName the class name to use for the mock class
      * @param bool $callOriginalConstructor whether to call the constructor
@@ -56,7 +56,7 @@ abstract class BaseTestCase extends \PHPUnit_Framework_TestCase
      * @throws \InvalidArgumentException
      */
     protected function getAccessibleMock(
-        $originalClassName, array $methods = array(), array $arguments = array(), $mockClassName = '',
+        $originalClassName, $methods = array(), array $arguments = array(), $mockClassName = '',
         $callOriginalConstructor = true, $callOriginalClone = true, $callAutoload = true
     ) {
         if ($originalClassName === '') {
