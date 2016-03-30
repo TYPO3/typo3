c7c202fef7c0cdfed24095b2e812504c5327240d
Andreas Fernandez <a.fernandez@scripting-base.de>
2016-03-30 11:15:44 +0200

    [BUGFIX] Treat numbers for comparison in FormEngineValidation as integer
    This patch fixes the issue that the number-based rules are treated
    as strings. The numbers are converted to integers now.
    
    Also, the ``range`` is changed: Instead of defining any fallback
    values if either ``lower`` or ``upper`` are not set, the value is now
    only compared against those limits when they exist.
    
    Resolves: #75307
    Releases: master, 7.6
    Change-Id: Iab321a7637e2ccc8c662355ccef1e25bc0b78be7
    Reviewed-on: https://review.typo3.org/47418
    Reviewed-by: Jigal van Hemert <jigal.van.hemert@typo3.org>
    Tested-by: Jigal van Hemert <jigal.van.hemert@typo3.org>
    Reviewed-by: Susanne Moog <typo3@susannemoog.de>
    Tested-by: Susanne Moog <typo3@susannemoog.de>
    Reviewed-by: Andreas Fernandez <typo3@scripting-base.de>
    Tested-by: Andreas Fernandez <typo3@scripting-base.de>

--- typo3/sysext/backend/Resources/Public/JavaScript/FormEngineValidation.js
+++ typo3/sysext/backend/Resources/Public/JavaScript/FormEngineValidation.js
@@ -247,6 +247,8 @@ define(['jquery', 'TYPO3/CMS/Backend/FormEngine'], function ($, FormEngine) {
 		var selected = 0;
 		var returnValue = value;
 		var $relatedField;
+		var minItems;
+		var maxItems;
 		$.each(rules, function(k, rule) {
 			switch (rule.type) {
 				case 'required':
@@ -261,20 +263,31 @@ define(['jquery', 'TYPO3/CMS/Backend/FormEngine'], function ($, FormEngine) {
 							$relatedField = $(document).find('[name="' + $field.data('relatedfieldname') + '"]');
 							if ($relatedField.length) {
 								selected = FormEngineValidation.trimExplode(',', $relatedField.val()).length;
-								if (selected < rule.minItems || selected > rule.maxItems) {
-									markParent = true;
-								}
 							} else {
 								selected = $field.val();
-								if (selected < rule.minItems || selected > rule.maxItems) {
+							}
+							if (typeof rule.minItems !== 'undefined') {
+								minItems = rule.minItems * 1;
+								if (!isNaN(minItems) && selected < minItems) {
+									markParent = true;
+								}
+							}
+							if (typeof rule.maxItems !== 'undefined') {
+								maxItems = rule.maxItems * 1;
+								if (!isNaN(maxItems) && selected > maxItems) {
 									markParent = true;
 								}
 							}
 						}
-						if (rule.config.lower || rule.config.upper) {
-							var minValue = rule.config.lower || 0;
-							var maxValue = rule.config.upper || Number.MAX_VALUE;
-							if (value < minValue || value > maxValue) {
+						if (typeof rule.config.lower !== 'undefined') {
+							var minValue = rule.config.lower * 1;
+							if (!isNaN(minValue) && value < minValue) {
+								markParent = true;
+							}
+						}
+						if (typeof rule.config.upper !== 'undefined') {
+							var maxValue = rule.config.upper * 1;
+							if (!isNaN(maxValue) && value < minValue) {
 								markParent = true;
 							}
 						}
@@ -285,12 +298,18 @@ define(['jquery', 'TYPO3/CMS/Backend/FormEngine'], function ($, FormEngine) {
 						$relatedField = $(document).find('[name="' + $field.data('relatedfieldname') + '"]');
 						if ($relatedField.length) {
 							selected = FormEngineValidation.trimExplode(',', $relatedField.val()).length;
-							if (selected < rule.minItems || selected > rule.maxItems) {
-								markParent = true;
-							}
 						} else {
 							selected = $field.find('option:selected').length;
-							if (selected < rule.minItems || selected > rule.maxItems) {
+						}
+						if (typeof rule.minItems !== 'undefined') {
+							minItems = rule.minItems * 1;
+							if (!isNaN(minItems) && selected < minItems) {
+								markParent = true;
+							}
+						}
+						if (typeof rule.maxItems !== 'undefined') {
+							maxItems = rule.maxItems * 1;
+							if (!isNaN(maxItems) && selected > maxItems) {
 								markParent = true;
 							}
 						}
@@ -299,16 +318,34 @@ define(['jquery', 'TYPO3/CMS/Backend/FormEngine'], function ($, FormEngine) {
 				case 'group':
 					if (rule.minItems || rule.maxItems) {
 						selected = $field.find('option').length;
-						if (selected < rule.minItems || selected > rule.maxItems) {
-							markParent = true;
+						if (typeof rule.minItems !== 'undefined') {
+							minItems = rule.minItems * 1;
+							if (!isNaN(minItems) && selected < minItems) {
+								markParent = true;
+							}
+						}
+						if (typeof rule.maxItems !== 'undefined') {
+							maxItems = rule.maxItems * 1;
+							if (!isNaN(maxItems) && selected > maxItems) {
+								markParent = true;
+							}
 						}
 					}
 					break;
 				case 'inline':
 					if (rule.minItems || rule.maxItems) {
 						selected = FormEngineValidation.trimExplode(',', $field.val()).length;
-						if (selected < rule.minItems || selected > rule.maxItems) {
-							markParent = true;
+						if (typeof rule.minItems !== 'undefined') {
+							minItems = rule.minItems * 1;
+							if (!isNaN(minItems) && selected < minItems) {
+								markParent = true;
+							}
+						}
+						if (typeof rule.maxItems !== 'undefined') {
+							maxItems = rule.maxItems * 1;
+							if (!isNaN(maxItems) && selected > maxItems) {
+								markParent = true;
+							}
 						}
 					}
 					break;
