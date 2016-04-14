f42c4fea675d493f5659f3d7ad18911479694f4a
Wouter Wolters <typo3@wouterwolters.nl>
2016-04-14 11:18:09 +0200

    [FOLLOWUP][TASK] Correct body-Tag id in Backend
    Fix acceptance test
    
    Resolves: #75525
    Releases: master
    Change-Id: Icfcc8aa7c04f11005c6ce774fcce064fa8b480e5
    Reviewed-on: https://review.typo3.org/47656
    Reviewed-by: Sascha Egerer <sascha@sascha-egerer.de>
    Tested-by: Sascha Egerer <sascha@sascha-egerer.de>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>

--- typo3/sysext/core/Tests/Acceptance/Backend/Page/AddPageInPageModuleCest.php
+++ typo3/sysext/core/Tests/Acceptance/Backend/Page/AddPageInPageModuleCest.php
@@ -96,7 +96,7 @@ class AddPageInPageModuleCest
         $I->moveMouseOver($pageActions);
         $I->waitForElement('#typo3-pagetree-contextmenu-sub1');
         $I->click($delete);
-        $yesButtonPopup = '#-main > div.x-window.x-window-plain.x-window-dlg > div.x-window-bwrap > div.x-window-bl > div > div > div > div.x-panel-fbar.x-small-editor.x-toolbar-layout-ct > table > tbody > tr > td.x-toolbar-left > table > tbody > tr > td:nth-child(2) > table > tbody > tr:nth-child(2) > td.x-btn-mc > em > button';
+        $yesButtonPopup = '#main > div.x-window.x-window-plain.x-window-dlg > div.x-window-bwrap > div.x-window-bl > div > div > div > div.x-panel-fbar.x-small-editor.x-toolbar-layout-ct > table > tbody > tr > td.x-toolbar-left > table > tbody > tr > td:nth-child(2) > table > tbody > tr:nth-child(2) > td.x-btn-mc > em > button';
         $I->waitForElement($yesButtonPopup);
         $I->click($yesButtonPopup);
         $I->wait(2);
