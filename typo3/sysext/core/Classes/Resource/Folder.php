576677d6175868920733bd8aa207c2d42247c027
Frans Saris <franssaris@gmail.com>
2016-04-19 18:04:20 +0200

    [BUGFIX] Check if folder is within the filemount
    Before using the filemount base folder as root for the
    folder->getReadablePath() check if the folder is within the filemount.
    
    Change-Id: I8a4436fb2a0e512c7d2d1fc50b9d08febae8b256
    Resolves: #73735
    Releases: master, 7.6, 6.2
    Reviewed-on: https://review.typo3.org/47817
    Reviewed-by: Nicole Cordes <typo3@cordes.co>
    Tested-by: Nicole Cordes <typo3@cordes.co>

--- typo3/sysext/core/Classes/Resource/Folder.php
+++ typo3/sysext/core/Classes/Resource/Folder.php
@@ -105,7 +105,16 @@ class Folder implements FolderInterface {
 	 */
 	public function getReadablePath($rootId = NULL) {
 		if ($rootId === NULL) {
-			$rootId = $this->storage->getRootLevelFolder()->getIdentifier();
+			// Find first matching filemount and use that as root
+			foreach ($this->storage->getFileMounts() as $fileMount) {
+				if ($this->storage->isWithinFolder($fileMount['folder'], $this)) {
+					$rootId = $fileMount['folder']->getIdentifier();
+					break;
+				}
+			}
+			if ($rootId === NULL) {
+				$rootId = $this->storage->getRootLevelFolder()->getIdentifier();
+			}
 		}
 		$readablePath = '/';
 		if ($this->identifier !== $rootId) {
