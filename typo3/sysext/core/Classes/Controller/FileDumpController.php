7ad9178c61f90d6ae5e39f7ce4d11a66157c7f85
Wouter Wolters <typo3@wouterwolters.nl>
2016-05-11 16:14:53 +0200

    [BUGFIX] Catch exceptions while dumping a file
    While dumping a file in the frontend no exceptions are
    caught. When a exception happens a 500 HTTP status is thrown.
    
    Catch all exceptions to return the correct 404 HTTP status.
    
    Releases: master,7.6,6.2
    Resolves: #76153
    Change-Id: Ice7097b6a41432ec580f99185d352307c5f55394
    Reviewed-on: https://review.typo3.org/50665
    Tested-by: TYPO3com <no-reply@typo3.com>
    Reviewed-by: Markus Klein <markus.klein@typo3.org>
    Tested-by: Markus Klein <markus.klein@typo3.org>

--- typo3/sysext/core/Classes/Controller/FileDumpController.php
+++ typo3/sysext/core/Classes/Controller/FileDumpController.php
@@ -57,13 +57,17 @@ class FileDumpController
 
         if (GeneralUtility::hmac(implode('|', $parameters), 'resourceStorageDumpFile') === $this->getGetOrPost($request, 'token')) {
             if (isset($parameters['f'])) {
-                $file = ResourceFactory::getInstance()->getFileObject($parameters['f']);
-                if ($file->isDeleted() || $file->isMissing()) {
+                try {
+                    $file = ResourceFactory::getInstance()->getFileObject($parameters['f']);
+                    if ($file->isDeleted() || $file->isMissing()) {
+                        $file = null;
+                    }
+                } catch (\Exception $e) {
                     $file = null;
                 }
             } else {
                 $file = GeneralUtility::makeInstance(ProcessedFileRepository::class)->findByUid($parameters['p']);
-                if ($file->isDeleted()) {
+                if (!$file || $file->isDeleted()) {
                     $file = null;
                 }
             }
