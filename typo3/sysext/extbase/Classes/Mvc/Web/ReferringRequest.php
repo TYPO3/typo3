21ed4054212babb7ec75d80a24f95c6ba25bd2fb
Helmut Hummel <info@helhum.io>
2016-05-24 09:44:08 +0200

    [SECURITY] Validate complete referring request
    Instead of only checking for valid request arguments by using a hmac,
    we now check the complete request including action, controller and vendor
    to avoid spoofing these arguments and bypassing other security checks
    during forwarding to the referring action.
    
    Additionally, ReferringRequest is now separate from regular Request.
    The meaning of properties starting with "@" is only valid for
    processing a referring request. To avoid mixed concerns in using
    the same Request implementation for regular requests and referring
    requests, they are separated now.
    
    Resolves: #76231
    Resolves: #76256
    Releases: master, 7.6, 6.2
    Security-Commit: e4eb0e63ace525a68f172aa9be1af23d69ea2ab2
    Security-Bulletin: TYPO3-CORE-SA-2016-013
    Change-Id: I334b2aa9ea3de0778adb38f007b1bd5e5a6a1be5
    Reviewed-on: https://review.typo3.org/48256
    Reviewed-by: Oliver Hader <oliver.hader@typo3.org>
    Tested-by: Oliver Hader <oliver.hader@typo3.org>

---
+++ typo3/sysext/extbase/Classes/Mvc/Web/ReferringRequest.php
@@ -0,0 +1,56 @@
+<?php
+namespace TYPO3\CMS\Extbase\Mvc\Web;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+/**
+ * Represents a referring web request.
+ *
+ * @api
+ */
+class ReferringRequest extends Request
+{
+    /**
+     * Sets the value of the specified argument
+     *
+     * @param string $argumentName Name of the argument to set
+     * @param mixed $value The new value
+     */
+    public function setArgument($argumentName, $value)
+    {
+        parent::setArgument($argumentName, $value);
+
+        switch ($argumentName) {
+            case '@extension':
+                $this->setControllerExtensionName($value);
+                break;
+            case '@subpackage':
+                $this->setControllerSubpackageKey($value);
+                break;
+            case '@controller':
+                $this->setControllerName($value);
+                break;
+            case '@action':
+                $this->setControllerActionName($value);
+                break;
+            case '@format':
+                $this->setFormat($value);
+                break;
+            case '@vendor':
+                $this->setControllerVendorName($value);
+                break;
+        }
+    }
+
+}
