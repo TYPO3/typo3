2b4c58865de3ad98f91f77ad17dfb34e485babcd
Wouter Wolters <typo3@wouterwolters.nl>
2016-06-24 17:01:37 +0200

    [TASK] Update mso/idna-convert to 1.1.0
    Resolves: #76790
    Releases: master,7.6
    Change-Id: Ib477526a6e5df8275b4fff55a3742811fc20237e
    Reviewed-on: https://review.typo3.org/48816
    Reviewed-by: Wouter Wolters <typo3@wouterwolters.nl>
    Tested-by: Wouter Wolters <typo3@wouterwolters.nl>
    Reviewed-by: Morton Jonuschat <m.jonuschat@mojocode.de>
    Tested-by: Morton Jonuschat <m.jonuschat@mojocode.de>

--- typo3/sysext/core/Classes/Utility/GeneralUtility.php
+++ typo3/sysext/core/Classes/Utility/GeneralUtility.php
@@ -92,7 +92,7 @@ class GeneralUtility
     /**
      * IDNA converter
      *
-     * @var \idna_convert
+     * @var \Mso\IdnaConvert\IdnaConvert
      */
     protected static $idnaConverter = null;
 
@@ -1311,7 +1311,7 @@ class GeneralUtility
             return self::$idnaStringCache[$value];
         } else {
             if (!self::$idnaConverter) {
-                self::$idnaConverter = new \idna_convert(array('idn_version' => 2008));
+                self::$idnaConverter = new \Mso\IdnaConvert\IdnaConvert(array('idn_version' => 2008));
             }
             self::$idnaStringCache[$value] = self::$idnaConverter->encode($value);
             return self::$idnaStringCache[$value];
