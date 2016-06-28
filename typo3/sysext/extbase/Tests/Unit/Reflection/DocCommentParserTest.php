30328a5d6de233f6945bc6be862abb15a4a2a3eb
Helmut Hummel <info@helhum.io>
2016-06-28 09:38:32 +0200

    [BUGFIX] Strip trailing slash from parsed doc comment
    If methods do not end with tag values (arguments or return statement)
    the doc comment parser returned a slash as last line,
    which is now stripped off.
    
    Resolves: #76815
    Releases: master, 7.6
    Change-Id: I15d52104c190fbbf1e4ee5146595810f961ca3c7
    Reviewed-on: https://review.typo3.org/48758
    Reviewed-by: Jigal van Hemert <jigal.van.hemert@typo3.org>
    Tested-by: Jigal van Hemert <jigal.van.hemert@typo3.org>

---
+++ typo3/sysext/extbase/Tests/Unit/Reflection/DocCommentParserTest.php
@@ -0,0 +1,40 @@
+<?php
+namespace TYPO3\CMS\Extbase\Tests\Unit\Reflection;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use TYPO3\CMS\Extbase\Reflection\DocCommentParser;
+
+/**
+ * Test case
+ */
+class DocCommentParserTest extends \TYPO3\CMS\Core\Tests\UnitTestCase
+{
+    /**
+     * @test
+     */
+    public function stripsSlashFromMethodComment()
+    {
+        $commentParser = new DocCommentParser();
+        $comment = '/**
+ * Here is some text, but neither an argument nor a return type.
+ */
+';
+        $commentParser->parseDocComment($comment);
+        $this->assertSame(
+            'Here is some text, but neither an argument nor a return type.',
+            $commentParser->getDescription()
+        );
+    }
+}
