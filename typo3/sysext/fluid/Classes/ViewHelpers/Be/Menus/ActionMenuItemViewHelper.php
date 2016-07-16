bea6460d2ac5fd4811a31fad0ba4a64810e0556e
Roberto Torresani <erreti@gmail.com>
2016-07-16 22:49:24 +0200

    [TASK] Move arguments to initializeArguments() in ActionMenuItemVH in ext:fluid
    Move the argument registrations away from the render() method
    to initializeArguments(), to prevent any errors with PHP7 and
    subclassed ViewHelpers if/when render() method signatures change.
    
    Resolves: #77133
    Releases: master
    Change-Id: Ifb5b2522306da3afc29423212ee0042f7dd9db0d
    Reviewed-on: https://review.typo3.org/49010
    Tested-by: Bamboo TYPO3com <info@typo3.com>
    Reviewed-by: Andreas Fernandez <typo3@scripting-base.de>
    Reviewed-by: Joerg Boesche <typo3@joergboesche.de>
    Reviewed-by: Susanne Moog <typo3@susannemoog.de>
    Tested-by: Susanne Moog <typo3@susannemoog.de>

--- typo3/sysext/fluid/Classes/ViewHelpers/Be/Menus/ActionMenuItemViewHelper.php
+++ typo3/sysext/fluid/Classes/ViewHelpers/Be/Menus/ActionMenuItemViewHelper.php
@@ -50,17 +50,32 @@ class ActionMenuItemViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\Abstract
     protected $tagName = 'option';
 
     /**
+     * Initialize arguments.
+     *
+     * @api
+     * @throws \TYPO3Fluid\Fluid\Core\ViewHelper\Exception
+     */
+    public function initializeArguments()
+    {
+        $this->registerArgument('label', 'string', 'label of the option tag', true);
+        $this->registerArgument('controller', 'string', 'controller to be associated with this ActionMenuItem', true);
+        $this->registerArgument('action', 'string', 'the action to be associated with this ActionMenuItem', true);
+        $this->registerArgument('arguments', 'array', 'additional controller arguments to be passed to the action when this ActionMenuItem is selected', false, array());
+    }
+
+    /**
      * Renders an ActionMenu option tag
      *
-     * @param string $label label of the option tag
-     * @param string $controller controller to be associated with this ActionMenuItem
-     * @param string $action the action to be associated with this ActionMenuItem
-     * @param array $arguments additional controller arguments to be passed to the action when this ActionMenuItem is selected
      * @return string the rendered option tag
      * @see \TYPO3\CMS\Fluid\ViewHelpers\Be\Menus\ActionMenuViewHelper
      */
-    public function render($label, $controller, $action, array $arguments = array())
+    public function render()
     {
+        $label = $this->arguments['label'];
+        $controller = $this->arguments['controller'];
+        $action = $this->arguments['action'];
+        $arguments = $this->arguments['arguments'];
+
         $uriBuilder = $this->controllerContext->getUriBuilder();
         $uri = $uriBuilder->reset()->uriFor($action, $arguments, $controller);
         $this->tag->addAttribute('value', $uri);
