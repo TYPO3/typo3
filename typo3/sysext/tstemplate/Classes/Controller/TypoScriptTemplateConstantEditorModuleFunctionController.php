fcc832a92115f5bec702be01cfc2558d34cff087
Florian Steller <florian-steller@outlook.de>
2016-07-22 13:41:05 +0200

    [BUGFIX] Changed Constant Editor and Info/Modify subline to h3
    Resolves: #77212
    Releases: master
    Change-Id: Ia6414b34fe3910fcdaa085e98295826abb239b39
    Reviewed-on: https://review.typo3.org/49166
    Tested-by: Bamboo TYPO3com <info@typo3.com>
    Reviewed-by: Mathias Schreiber <mathias.schreiber@wmdb.de>
    Tested-by: Mathias Schreiber <mathias.schreiber@wmdb.de>
    Reviewed-by: Frederic Gaus <frederic.gaus@flagbit.de>
    Tested-by: Frederic Gaus <frederic.gaus@flagbit.de>
    Reviewed-by: Andreas Fernandez <typo3@scripting-base.de>
    Tested-by: Andreas Fernandez <typo3@scripting-base.de>

--- typo3/sysext/tstemplate/Classes/Controller/TypoScriptTemplateConstantEditorModuleFunctionController.php
+++ typo3/sysext/tstemplate/Classes/Controller/TypoScriptTemplateConstantEditorModuleFunctionController.php
@@ -145,7 +145,7 @@ class TypoScriptTemplateConstantEditorModuleFunctionController extends AbstractF
             // Resetting the menu (stop)
             $iconFactory = GeneralUtility::makeInstance(IconFactory::class);
             $content = $iconFactory->getIconForRecord('sys_template', $tplRow, Icon::SIZE_SMALL)->render() . '<strong>&nbsp;' . $this->pObj->linkWrapTemplateTitle($tplRow['title'], 'constants') . '</strong>' . (trim($tplRow['sitetitle']) ? htmlspecialchars(' (' . $tplRow['sitetitle'] . ')') : '');
-            $theOutput .= '<h2>' . htmlspecialchars($lang->getLL('editConstants')) . '</h2><div>' . $content . '</div>';
+            $theOutput .= '<h3>' . htmlspecialchars($lang->getLL('editConstants')) . '</h3><div>' . $content . '</div>';
             if ($manyTemplatesMenu) {
                 $theOutput .= '<div>' . $manyTemplatesMenu . '</div>';
             }
--- typo3/sysext/tstemplate/Classes/Controller/TypoScriptTemplateInformationModuleFunctionController.php
+++ typo3/sysext/tstemplate/Classes/Controller/TypoScriptTemplateInformationModuleFunctionController.php
@@ -254,7 +254,7 @@ class TypoScriptTemplateInformationModuleFunctionController extends AbstractFunc
                 }
             }
             $content = '<a href="#" class="t3-js-clickmenutrigger" data-table="sys_template" data-uid="' . $tplRow['uid'] . '" data-listframe="1">' . $this->iconFactory->getIconForRecord('sys_template', $tplRow, Icon::SIZE_SMALL)->render() . '</a><strong>&nbsp;' . htmlspecialchars($tplRow['title']) . '</strong>' . (trim($tplRow['sitetitle']) ? htmlspecialchars(' (' . $tplRow['sitetitle'] . ')') : '');
-            $theOutput .= '<h2>' . htmlspecialchars($lang->getLL('templateInformation')) . '</h2><div>' . $content . '</div>';
+            $theOutput .= '<h3>' . htmlspecialchars($lang->getLL('templateInformation')) . '</h3><div>' . $content . '</div>';
             if ($manyTemplatesMenu) {
                 $theOutput .= '<div>' . $manyTemplatesMenu . '</div>';
             }
