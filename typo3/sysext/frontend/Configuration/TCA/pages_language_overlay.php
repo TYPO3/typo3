ca05d3ff63b2332a29a07e87804268c01de4e541
Georg Ringer <georg.ringer@gmail.com>
2016-08-22 07:31:50 +0200

    [TASK] Improve media of pages_language_overlay
    FAL relations in translated pages must have the same
    properties as in regular pages which includes link fields and the
    cropping editor.
    
    Resolves: #77541
    Releases: master, 7.6
    Change-Id: Ice77cd6c752e46ee048d07ca60c9b76cfc5509e8
    Tested-by: Morton Jonuschat <m.jonuschat@mojocode.de>
    (cherry picked from commit bd4ad2448d5ec1d3afc98636c1cba70e3a482778)
    Reviewed-on: https://review.typo3.org/49535
    Reviewed-by: Morton Jonuschat <m.jonuschat@mojocode.de>

--- typo3/sysext/frontend/Configuration/TCA/pages_language_overlay.php
+++ typo3/sysext/frontend/Configuration/TCA/pages_language_overlay.php
@@ -151,7 +151,42 @@ return array(
         'media' => array(
             'exclude' => 1,
             'label' => $GLOBALS['TCA']['pages']['columns']['media']['label'],
-            'config' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::getFileFieldTCAConfig('media')
+            'config' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::getFileFieldTCAConfig('media', array(
+                    // Use the imageoverlayPalette instead of the basicoverlayPalette
+                    'foreign_types' => array(
+                        '0' => array(
+                            'showitem' => '
+								--palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
+								--palette--;;filePalette'
+                        ),
+                        \TYPO3\CMS\Core\Resource\File::FILETYPE_TEXT => array(
+                            'showitem' => '
+								--palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
+								--palette--;;filePalette'
+                        ),
+                        \TYPO3\CMS\Core\Resource\File::FILETYPE_IMAGE => array(
+                            'showitem' => '
+								--palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
+								--palette--;;filePalette'
+                        ),
+                        \TYPO3\CMS\Core\Resource\File::FILETYPE_AUDIO => array(
+                            'showitem' => '
+								--palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.audioOverlayPalette;audioOverlayPalette,
+								--palette--;;filePalette'
+                        ),
+                        \TYPO3\CMS\Core\Resource\File::FILETYPE_VIDEO => array(
+                            'showitem' => '
+								--palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.videoOverlayPalette;videoOverlayPalette,
+								--palette--;;filePalette'
+                        ),
+                        \TYPO3\CMS\Core\Resource\File::FILETYPE_APPLICATION => array(
+                            'showitem' => '
+								--palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
+								--palette--;;filePalette'
+                        )
+                    )
+                )
+            )
         ),
         'url' => array(
             'exclude' => 1,
