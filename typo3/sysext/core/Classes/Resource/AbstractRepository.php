c658186bc7261127965144b43e646ee71d07f8e7
Philipp Gampe <philipp.gampe@typo3.org>
2016-09-01 21:52:29 +0200

    [FOLLOWUP][TASK] Migrate FAL AbstractRepository
    Fix broken image preview in backend on non-public
    storages.
    
    Change-Id: I5aa1e286a69485dbdd641cc5803066e7cdd18fed
    Related: #77407
    Resolves: #77764
    Releases: master
    Reviewed-on: https://review.typo3.org/49733
    Tested-by: Bamboo TYPO3com <info@typo3.com>
    Reviewed-by: Wouter Wolters <typo3@wouterwolters.nl>
    Tested-by: Wouter Wolters <typo3@wouterwolters.nl>
    Reviewed-by: Morton Jonuschat <m.jonuschat@mojocode.de>
    Tested-by: Morton Jonuschat <m.jonuschat@mojocode.de>

--- typo3/sysext/core/Classes/Resource/AbstractRepository.php
+++ typo3/sysext/core/Classes/Resource/AbstractRepository.php
@@ -201,7 +201,7 @@ abstract class AbstractRepository implements RepositoryInterface, SingletonInter
             throw new \InvalidArgumentException('The UID has to be an integer. UID given: "' . $uid . '"', 1316779798);
         }
         $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable($this->table);
-        if ($this->getEnvironmentMode() === 'FE') {
+        if ($this->getEnvironmentMode() === 'FE' && !empty($GLOBALS['TSFE']->sys_page)) {
             $queryBuilder->setRestrictions(GeneralUtility::makeInstance(FrontendRestrictionContainer::class));
         }
         $row = $queryBuilder
