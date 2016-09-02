9b2ad8cc87c736e49e7d796a568dec23c16f9eed
Frank Naegler <frank.naegler@typo3.org>
2016-09-02 14:30:37 +0200

    [FEATURE] styleguide: Add Filter for icon types

--- typo3/sysext/styleguide/Resources/Public/JavaScript/FindIcons.js
+++ typo3/sysext/styleguide/Resources/Public/JavaScript/FindIcons.js
@@ -15,13 +15,36 @@
  * Javascript functions regarding the FindIcons module
  */
 define('TYPO3/CMS/Styleguide/FindIcons', ['jquery'], function($) {
+	$('.t3js-filter-buttons button').click(function(e) {
+		e.preventDefault();
+		$('#search-field').val($(this).data('filter')).trigger('keyup');
+	});
 	$('#search-field').keyup(function() {
-		var $typedQuery = TYPO3.jQuery(this).val();
-		if ($typedQuery === '') {
+		var typedQuery = TYPO3.jQuery(this).val();
+		if (typedQuery === '') {
 			TYPO3.jQuery('#t3js-filter-container [data-icon-identifier]').show();
 		} else {
-			TYPO3.jQuery('#t3js-filter-container [data-icon-identifier]').hide();
-			TYPO3.jQuery('#t3js-filter-container [data-icon-identifier*="' + $typedQuery + '"]').show();
+			if (typedQuery.indexOf('type:') !== -1) {
+				var parts = typedQuery.split(':');
+				var type = parts[1];
+				switch (type.toLowerCase()) {
+					case 'bitmap':
+						TYPO3.jQuery('#t3js-filter-container [data-icon-identifier]').hide();
+						TYPO3.jQuery('#t3js-filter-container img:not([src$=".svg"])').closest('[data-icon-identifier]').show();
+						break;
+					case 'font':
+						TYPO3.jQuery('#t3js-filter-container [data-icon-identifier]').hide();
+						TYPO3.jQuery('#t3js-filter-container i.fa').closest('[data-icon-identifier]').show();
+						break;
+					case 'vector':
+						TYPO3.jQuery('#t3js-filter-container [data-icon-identifier]').hide();
+						TYPO3.jQuery('#t3js-filter-container img[src$=".svg"]').closest('[data-icon-identifier]').show();
+						break;
+				}
+			} else {
+				TYPO3.jQuery('#t3js-filter-container [data-icon-identifier]').hide();
+				TYPO3.jQuery('#t3js-filter-container [data-icon-identifier*="' + typedQuery + '"]').show();
+			}
 		}
 	});
 });
