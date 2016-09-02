290015b4ea2a37f76a9296505c4394712ada168d
Tomita Militaru <militarutomita@gmail.com>
2016-09-02 18:46:48 +0200

    [BUGFIX] Check for null in debug_check_recordset
    Check for null in function debug_check_record set
    and return false if value is null.
    
    Resolves: #77755
    Releases: master,7.6,6.2
    Change-Id: I757d3ee3c4db9d52f1c4f7a4f07262fe187b2496
    Reviewed-on: https://review.typo3.org/49801
    Reviewed-by: Morton Jonuschat <m.jonuschat@mojocode.de>
    Tested-by: Morton Jonuschat <m.jonuschat@mojocode.de>

--- typo3/sysext/core/Classes/Database/DatabaseConnection.php
+++ typo3/sysext/core/Classes/Database/DatabaseConnection.php
@@ -1856,7 +1856,7 @@ class DatabaseConnection
      */
     public function debug_check_recordset($res)
     {
-        if ($res !== false) {
+        if ($res !== false && $res !== null) {
             return true;
         }
         $trace = debug_backtrace(0);
