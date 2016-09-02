51177a8840ebf95589c031534ad8bf8ef3f559c0
Stefan Froemken <froemken@gmail.com>
2016-09-02 18:33:43 +0200

    [BUGFIX] Test for curl
    Currently we check for curl by the deprecated
    setting curlUse.
    With this patch we will check for curl
    via function_exists
    
    Resolves: #77794
    Releases: master
    Change-Id: I4502d0044f749be3f0934f2f76693f155f8339d7
    Reviewed-on: https://review.typo3.org/49772
    Reviewed-by: Benni Mack <benni@typo3.org>
    Tested-by: Benni Mack <benni@typo3.org>
    Reviewed-by: Philipp Gampe <philipp.gampe@typo3.org>
    Tested-by: Philipp Gampe <philipp.gampe@typo3.org>

--- typo3/sysext/install/Classes/SystemEnvironment/SetupCheck.php
+++ typo3/sysext/install/Classes/SystemEnvironment/SetupCheck.php
@@ -83,7 +83,7 @@ class SetupCheck
     protected function checkDownloadsPossible()
     {
         $allowUrlFopen = (bool)ini_get('allow_url_fopen');
-        $curlEnabled = !empty($GLOBALS['TYPO3_CONF_VARS']['SYS']['curlUse']);
+        $curlEnabled = function_exists('curl_version');
         if ($allowUrlFopen || $curlEnabled) {
             $status = new Status\OkStatus();
             $status->setTitle('Fetching external URLs is allowed');
@@ -91,7 +91,7 @@ class SetupCheck
             $status = new Status\WarningStatus();
             $status->setTitle('Fetching external URLs is not allowed');
             $status->setMessage(
-                'Either enable PHP runtime setting "allow_url_fopen"' . LF . 'or enable curl by setting [SYS][curlUse] accordingly.'
+                'Either enable PHP runtime setting "allow_url_fopen"' . LF . 'or compile curl into your PHP with --with-curl.'
             );
         }
 
