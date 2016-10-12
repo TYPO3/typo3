7efc092447487c9a8dab3dbd6970c4f5677fabc4
Nicole Cordes <typo3@cordes.co>
2016-10-12 17:55:27 +0200

    [BUGFIX] Use reflection information in getGettablePropertyNames
    In \TYPO3\CMS\Extbase\Reflection\ObjectAccess there is a method to get
    all available property names of an object. Currently all get/is/has
    methods are joined as they can be fetch from Extbase as well. But for
    those methods it is necessary to respect their arguments as Extbase
    calls those functions without any argument.
    This can trigger PHP warnings.
    
    The patch uses a class reflection to get public properties and inspect
    the method arguments. Only those functions without arguments or only
    optional arguments are considered as valid property name.
    
    Resolves: #78270
    Releases: master, 7.6
    Change-Id: Ie286dca2a249b73d3dc58f7388dda593a678db3d
    Reviewed-on: https://review.typo3.org/51361
    Tested-by: TYPO3com <no-reply@typo3.com>
    Reviewed-by: Philipp Gampe <philipp.gampe@typo3.org>
    Tested-by: Philipp Gampe <philipp.gampe@typo3.org>

--- typo3/sysext/extbase/Tests/Unit/Reflection/ObjectAccessTest.php
+++ typo3/sysext/extbase/Tests/Unit/Reflection/ObjectAccessTest.php
@@ -282,8 +282,19 @@ class ObjectAccessTest extends \TYPO3\CMS\Core\Tests\UnitTestCase
     public function getGettablePropertyNamesReturnsAllPropertiesWhichAreAvailable()
     {
         $gettablePropertyNames = \TYPO3\CMS\Extbase\Reflection\ObjectAccess::getGettablePropertyNames($this->dummyObject);
-        $expectedPropertyNames = ['anotherBooleanProperty', 'anotherProperty', 'booleanProperty', 'property', 'property2', 'publicProperty', 'publicProperty2'];
+        $expectedPropertyNames = ['anotherBooleanProperty', 'anotherProperty', 'booleanProperty', 'property', 'property2', 'publicProperty', 'publicProperty2', 'someValue'];
         $this->assertEquals($gettablePropertyNames, $expectedPropertyNames, 'getGettablePropertyNames returns not all gettable properties.');
+    }
+
+    /**
+     * @test
+     */
+    public function getGettablePropertyNamesRespectsMethodArguments()
+    {
+        $dateTimeZone = new \DateTimeZone('+2');
+        $gettablePropertyNames = \TYPO3\CMS\Extbase\Reflection\ObjectAccess::getGettablePropertyNames($dateTimeZone);
+        $expectedPropertyNames = ['location', 'name'];
+        $this->assertEquals($gettablePropertyNames, $expectedPropertyNames, 'getGettablePropertyNames does not respect method arguments.');
     }
 
     /**
@@ -322,7 +333,8 @@ class ObjectAccessTest extends \TYPO3\CMS\Core\Tests\UnitTestCase
             'property' => 'string1',
             'property2' => null,
             'publicProperty' => null,
-            'publicProperty2' => 42
+            'publicProperty2' => 42,
+            'someValue' => true,
         ];
         $this->assertEquals($allProperties, $expectedProperties, 'expectedProperties did not return the right values for the properties.');
     }
