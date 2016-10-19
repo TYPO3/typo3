cdd7efefd94f6073b486eeab16a7f2b47a90551b
Mathias Brodala <mbrodala@pagemachine.de>
2016-10-19 12:02:56 +0200

    [CLEANUP] Simplify checkFilePermissions.sh ignore list
    Initialize the array directly.
    
    Resolves: #78341
    Releases: master
    Change-Id: I3ae5688d0cee452979c18eb334e575144a222380
    Reviewed-on: https://review.typo3.org/50272
    Reviewed-by: Oliver Klee <typo3-coding@oliverklee.de>
    Tested-by: TYPO3com <no-reply@typo3.com>
    Reviewed-by: Stefan Neufeind <typo3.neufeind@speedpartner.de>
    Tested-by: Stefan Neufeind <typo3.neufeind@speedpartner.de>
    Reviewed-by: Markus Klein <markus.klein@typo3.org>
    Tested-by: Markus Klein <markus.klein@typo3.org>

--- typo3/sysext/core/Build/Scripts/checkFilePermissions.sh
+++ typo3/sysext/core/Build/Scripts/checkFilePermissions.sh
@@ -10,15 +10,16 @@
 ##########################
 
 # Array of files to ignore the file permission check
-IGNORE=()
-IGNORE[0]="typo3/cli_dispatch.phpsh"
-IGNORE[1]="typo3/sysext/core/Build/Scripts/cglFixMyCommit.sh"
-IGNORE[2]="typo3/sysext/core/Build/Scripts/checkFilePermissions.sh"
-IGNORE[3]="typo3/sysext/core/Build/Scripts/duplicateExceptionCodeCheck.sh"
-IGNORE[4]="typo3/sysext/core/Build/Scripts/splitFunctionalTests.sh"
-IGNORE[5]="typo3/sysext/core/Build/Scripts/xlfcheck.sh"
-IGNORE[6]="typo3/sysext/core/Build/Scripts/validateRstFiles.sh"
-IGNORE[7]="typo3/sysext/core/bin/typo3"
+IGNORE=(
+  "typo3/cli_dispatch.phpsh"
+  "typo3/sysext/core/Build/Scripts/cglFixMyCommit.sh"
+  "typo3/sysext/core/Build/Scripts/checkFilePermissions.sh"
+  "typo3/sysext/core/Build/Scripts/duplicateExceptionCodeCheck.sh"
+  "typo3/sysext/core/Build/Scripts/splitFunctionalTests.sh"
+  "typo3/sysext/core/Build/Scripts/xlfcheck.sh"
+  "typo3/sysext/core/Build/Scripts/validateRstFiles.sh"
+  "typo3/sysext/core/bin/typo3"
+)
 
 COUNTER=0
 
