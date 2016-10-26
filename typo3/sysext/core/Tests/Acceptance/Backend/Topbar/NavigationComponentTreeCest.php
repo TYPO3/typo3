31044f0c45f3e13a4ee2706713867b3549a1f41b
Gordon Brüggemann <gordon.brueggemann@di-unternehmer.com>
2016-10-26 17:46:21 +0200

    [TASK] Acceptance test for NavigationComponent Tree
    Resolves: #78452
    Releases: master
    Change-Id: I097a9572662e7a4f36f1e0899b92fd2c41b89afe
    Reviewed-on: https://review.typo3.org/50390
    Tested-by: TYPO3com <no-reply@typo3.com>
    Reviewed-by: Anja Leichsenring <aleichsenring@ab-softlab.de>
    Tested-by: Anja Leichsenring <aleichsenring@ab-softlab.de>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>

---
+++ typo3/sysext/core/Tests/Acceptance/Backend/Topbar/NavigationComponentTreeCest.php
@@ -0,0 +1,74 @@
+<?php
+namespace TYPO3\CMS\Core\Tests\Acceptance\Backend\Topbar;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use TYPO3\CMS\Core\Tests\Acceptance\Step\Backend\Admin;
+
+/**
+ * Acceptance test for the Navigation Component Tree
+ */
+class NavigationComponentTreeCest
+{
+    /**
+     * @param Admin $I
+     */
+    public function _before(Admin $I)
+    {
+        $I->useExistingSession();
+    }
+
+    /**
+     * @param Admin $I
+     */
+    public function checkTreefExpandsAndCollapseByPageModule(Admin $I)
+    {
+        $treeArea = '.scaffold-content-navigation-expanded';
+        $I->wantTo('check Page Module for Expands And Collapse');
+        $I->click('Page');
+        $I->waitForElement($treeArea);
+        $I->see('New TYPO3 site', $treeArea);
+        $I->wantTo('check Page Module for Collapse');
+        $I->click('button.t3js-topbar-button-navigationcomponent');
+        $I->waitForElementNotVisible($treeArea);
+        $I->cantSee('New TYPO3 site', $treeArea);
+        $I->wantTo('check Page Module for Expands');
+        $I->click('button.t3js-topbar-button-navigationcomponent');
+        $I->waitForElement($treeArea);
+
+        $I->see('New TYPO3 site', $treeArea);
+    }
+
+    /**
+     * @param Admin $I
+     */
+    public function checkTreefExpandsAndCollapseByFileModule(Admin $I)
+    {
+        $I->wantTo('check File Module for Expands And Collapse');
+        $I->click('Filelist');
+        $I->switchToIFrame('nav_frame');
+        $I->see('fileadmin', '.t3js-module-body');
+        $I->switchToIFrame();
+        $I->wantTo('check File Module for Collapse');
+        $I->click('button.t3js-topbar-button-navigationcomponent');
+        $I->switchToIFrame('nav_frame');
+        $I->cantSee('fileadmin', '.t3js-module-body');
+        $I->switchToIFrame();
+        $I->wantTo('check File Module for Expands');
+        $I->click('button.t3js-topbar-button-navigationcomponent');
+        $I->switchToIFrame('nav_frame');
+        $I->see('fileadmin', '.t3js-module-body');
+        $I->switchToIFrame();
+    }
+}
