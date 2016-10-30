84fd0139e25ad538ea544c119b9dc4e29feb924a
Christian Kuhn <lolli@schwarzbu.ch>
2016-10-30 11:30:43 +0100

    [TASK] styleguide: Add a "customPermOptions" example
    Related: https://github.com/TYPO3-CMS/styleguide/issues/77075

--- typo3/sysext/styleguide/ext_tables.php
+++ typo3/sysext/styleguide/ext_tables.php
@@ -55,6 +55,23 @@ if (TYPO3_MODE === 'BE') {
         ]
     );
 
+    // Register some custom permission options shown in BE group access lists
+    $GLOBALS['TYPO3_CONF_VARS']['BE']['customPermOptions'] = [
+        'tx_styleguide_custom' => [
+            'header' => 'Custom styleguide permissions',
+                'items' => [
+                    'key1' => [
+                        'Option 1',
+                        // Icon has been registered above
+                        'tcarecords-tx_styleguide_forms-default',
+                        'Description 1',
+                    ],
+                'key2' => [
+                    'Option 2'
+                ],
+            ]
+        ]
+    ];
 
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_styleguide_elements_basic');
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_styleguide_elements_forms');
