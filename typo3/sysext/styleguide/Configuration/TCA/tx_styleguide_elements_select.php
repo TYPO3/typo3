90fc0b6f281d2c26727153cc5d89a52b979b973a
Christian Kuhn <lolli@schwarzbu.ch>
2016-12-01 16:33:03 +0100

    [TASK] styleguide: TCA: select tree add sorting and two static items

--- typo3/sysext/styleguide/Configuration/TCA/tx_styleguide_elements_select.php
+++ typo3/sysext/styleguide/Configuration/TCA/tx_styleguide_elements_select.php
@@ -473,13 +473,18 @@ return [
 
         'select_tree_1' => [
             'exclude' => 1,
-            'label' => 'select_tree_1 pages, showHeader=true, expandAll=true, size=20, maxitems=4',
+            'label' => 'select_tree_1 pages, showHeader=true, expandAll=true, size=20, maxitems=4, order by sorting, static items',
             'config' => [
                 'type' => 'select',
                 'renderType' => 'selectTree',
                 'foreign_table' => 'pages',
+                'foreign_table_where' => 'ORDER BY pages.sorting',
                 'size' => 20,
                 'maxitems' => 4,
+                'items' => [
+                    [ 'static from tca 4711', 4711 ],
+                    [ 'static from tca 4712', 4712 ],
+                ],
                 'treeConfig' => [
                     'parentField' => 'pid',
                     'appearance' => [
