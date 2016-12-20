b236079f17ef9449a6a124a8fd67247114684d8f
Georg Ringer <georg.ringer@gmail.com>
2016-12-20 08:06:57 +0100

    [BUGFIX] Remove range from sorting field
    The range configuration for a sorting field doesn't make any sense
    and can lead to unexpected bugs as sorting can be rewritten
    
    Resolves: #78986
    Releases: master, 7.6, 6.2
    Change-Id: I738877e6f9587d2570506f695fb7426a7536a964
    Reviewed-on: https://review.typo3.org/51284
    Tested-by: TYPO3com <no-reply@typo3.com>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>

--- typo3/sysext/core/Configuration/TCA/sys_file_reference.php
+++ typo3/sysext/core/Configuration/TCA/sys_file_reference.php
@@ -136,10 +136,6 @@ return array(
 				'max' => '4',
 				'eval' => 'int',
 				'checkbox' => '0',
-				'range' => array(
-					'upper' => '1000',
-					'lower' => '10'
-				),
 				'default' => 0
 			)
 		),
