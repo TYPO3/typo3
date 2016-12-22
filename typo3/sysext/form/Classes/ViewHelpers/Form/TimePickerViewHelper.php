3e93c060711f09938565fd92f9e4b894c85a3db5
Ralf Zimmermann <ralf.zimmermann@tritum.de>
2016-12-22 16:26:46 +0100

    [TASK] EXT:form - license headers
    Add a reference to the original authors since most of the
    Frontend functionality is heavily inspired and copied from
    the Neos/Flow Form package.
    
    Resolves: #79074
    Releases: master
    Change-Id: I178a7314a0da70a7406df3bcc58f3dd28bdff463
    Reviewed-on: https://review.typo3.org/51025
    Tested-by: TYPO3com <no-reply@typo3.com>
    Reviewed-by: Anja Leichsenring <aleichsenring@ab-softlab.de>
    Tested-by: Anja Leichsenring <aleichsenring@ab-softlab.de>
    Reviewed-by: Georg Ringer <georg.ringer@gmail.com>
    Tested-by: Georg Ringer <georg.ringer@gmail.com>

--- typo3/sysext/form/Classes/ViewHelpers/Form/TimePickerViewHelper.php
+++ typo3/sysext/form/Classes/ViewHelpers/Form/TimePickerViewHelper.php
@@ -5,6 +5,8 @@ namespace TYPO3\CMS\Form\ViewHelpers\Form;
 /*
  * This file is part of the TYPO3 CMS project.
  *
+ * It originated from the Neos.Form package (www.neos.io)
+ *
  * It is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License, either version 2
  * of the License, or any later version.
