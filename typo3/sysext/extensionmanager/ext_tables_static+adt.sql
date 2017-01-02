b51fdede06b7b131f656a75e5142068d65152ac9
Johannes Goslar <jogo@kronberger-spiele.de>
2017-01-02 17:11:46 +0100

    [BUGFIX][DBAL] Fix ExtensionManager repository insertion
    Resolves: #79143
    Releases: master
    Change-Id: I3cc6f0cff6980439f8f967f8ea627e074444f28c
    Reviewed-on: https://review.typo3.org/51108
    Tested-by: TYPO3com <no-reply@typo3.com>
    Reviewed-by: Morton Jonuschat <m.jonuschat@mojocode.de>
    Tested-by: Morton Jonuschat <m.jonuschat@mojocode.de>
    Reviewed-by: Georg Ringer <georg.ringer@gmail.com>
    Tested-by: Georg Ringer <georg.ringer@gmail.com>

--- typo3/sysext/extensionmanager/ext_tables_static+adt.sql
+++ typo3/sysext/extensionmanager/ext_tables_static+adt.sql
@@ -14,6 +14,4 @@ CREATE TABLE tx_extensionmanager_domain_model_repository (
   PRIMARY KEY (uid)
 );
 
-
-INSERT INTO tx_extensionmanager_domain_model_repository VALUES ('1', '0', 'TYPO3.org Main Repository', 'Main repository on typo3.org. This repository has some mirrors configured which are available with the mirror url.', 'https://typo3.org/wsdl/tx_ter_wsdl.php', 'https://repositories.typo3.org/mirrors.xml.gz', '1346191200', '0');
-
+INSERT INTO tx_extensionmanager_domain_model_repository (title, description, wsdl_url, mirror_list_url, last_update) VALUES ('TYPO3.org Main Repository', 'Main repository on typo3.org. This repository has some mirrors configured which are available with the mirror url.', 'https://typo3.org/wsdl/tx_ter_wsdl.php', 'https://repositories.typo3.org/mirrors.xml.gz', '1346191200');
