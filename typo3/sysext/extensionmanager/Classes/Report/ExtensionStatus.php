e538ed3f154f447ddbaf78785a31c9ab2ee72a17
Mathias Brodala <mbrodala@pagemachine.de>
2017-01-11 15:41:36 +0100

    [BUGFIX] Skip main extension repository check in Composer mode
    Extensions are not installed and updated from the main extension
    repository in Composer mode, thus skip this check completely.
    
    Resolves: #79277
    Releases: master, 7.6
    Change-Id: I6cb209b5ebae04b7e29b55a52ac61ce0f4b261c2
    Reviewed-on: https://review.typo3.org/51267
    Tested-by: TYPO3com <no-reply@typo3.com>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Reviewed-by: Wouter Wolters <typo3@wouterwolters.nl>
    Reviewed-by: Georg Ringer <georg.ringer@gmail.com>
    Tested-by: Georg Ringer <georg.ringer@gmail.com>

--- typo3/sysext/extensionmanager/Classes/Report/ExtensionStatus.php
+++ typo3/sysext/extensionmanager/Classes/Report/ExtensionStatus.php
@@ -14,6 +14,8 @@ namespace TYPO3\CMS\Extensionmanager\Report;
  * The TYPO3 project - inspiring people to share!
  */
 
+use TYPO3\CMS\Core\Core\Bootstrap;
+
 /**
  * Extension status reports
  */
@@ -74,7 +76,10 @@ class ExtensionStatus implements \TYPO3\CMS\Reports\StatusProviderInterface
     public function getStatus()
     {
         $status = [];
-        $status['mainRepositoryStatus'] = $this->getMainRepositoryStatus();
+
+        if (!Bootstrap::usesComposerClassLoading()) {
+            $status['mainRepositoryStatus'] = $this->getMainRepositoryStatus();
+        }
 
         $extensionStatus = $this->getSecurityStatusOfExtensions();
         $status['extensionsSecurityStatusInstalled'] = $extensionStatus->loaded;
