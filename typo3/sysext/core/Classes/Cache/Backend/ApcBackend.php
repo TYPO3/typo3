e0b8226394d9eaa9cdbecf04550f52936f3c1a21
tbringewatt <t.bringewatt@mittwald.de>
2017-02-03 15:45:02 +0100

    [TASK] Do not throw exception on unavailable APC(u) cache backend
    Log an error (keeping the website available) instead of throwing an
    exception (and thus completely disabling it) if TYPO3 is configured to
    use the APC / APCu cache backend but saving data to the cache failed.
    
    Resolves: #79636
    Releases: master, 8.7
    
    Change-Id: I3adf829825017a3061e921d5d3ec74f20e18311e
    Reviewed-on: https://review.typo3.org/52785
    Tested-by: TYPO3com <no-reply@typo3.com>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>

--- typo3/sysext/core/Classes/Cache/Backend/ApcBackend.php
+++ typo3/sysext/core/Classes/Cache/Backend/ApcBackend.php
@@ -14,6 +14,8 @@ namespace TYPO3\CMS\Core\Cache\Backend;
  * The TYPO3 project - inspiring people to share!
  */
 
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
 /**
  * A caching backend which stores cache entries by using APC.
  *
@@ -144,7 +146,8 @@ class ApcBackend extends \TYPO3\CMS\Core\Cache\Backend\AbstractBackend implement
             $this->removeIdentifierFromAllTags($entryIdentifier);
             $this->addIdentifierToTags($entryIdentifier, $tags);
         } else {
-            throw new \TYPO3\CMS\Core\Cache\Exception('Could not set value.', 1232986877);
+            $errorMessage = 'Error using APCu: Could not save data in the cache.';
+            GeneralUtility::sysLog($errorMessage, 'core', GeneralUtility::SYSLOG_SEVERITY_ERROR);
         }
     }
 
