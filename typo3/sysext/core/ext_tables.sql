bf399d1ec087f5511918a6f2e7916a5ee865e555
Christian Kuhn <lolli@schwarzbu.ch>
2017-02-04 13:44:51 +0100

    [BUGFIX] Limit primary key length on be_session and fe_session table
    Patch for issue #79566 increased the field length of ses_name in
    fe_session and be_session table from 32 to 255.
    This field is used together with ses_id as primary key.
    With utf8, this now exceeds maximum key length of 757 bytes.
    To fix the issue, ses_name is cut down to 100 for the time being,
    the patch should be fine for master and 7.6 this way.
    
    Change-Id: I06abecca0f8df483590cf0a3c193134083e8f5f1
    Resolves: #79628
    Related: #79566
    Releases: master, 7.6
    Reviewed-on: https://review.typo3.org/51538
    Tested-by: TYPO3com <no-reply@typo3.com>
    Reviewed-by: Benjamin Kott <benjamin.kott@outlook.com>
    Tested-by: Benjamin Kott <benjamin.kott@outlook.com>
    Reviewed-by: Mathias Schreiber <mathias.schreiber@typo3.com>
    Tested-by: Mathias Schreiber <mathias.schreiber@typo3.com>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>

--- typo3/sysext/core/ext_tables.sql
+++ typo3/sysext/core/ext_tables.sql
@@ -37,7 +37,7 @@ CREATE TABLE be_groups (
 #
 CREATE TABLE be_sessions (
 	ses_id varchar(32) DEFAULT '' NOT NULL,
-	ses_name varchar(255) DEFAULT '' NOT NULL,
+	ses_name varchar(100) DEFAULT '' NOT NULL,
 	ses_iplock varchar(39) DEFAULT '' NOT NULL,
 	ses_userid int(11) unsigned DEFAULT '0' NOT NULL,
 	ses_tstamp int(11) unsigned DEFAULT '0' NOT NULL,
--- typo3/sysext/frontend/ext_tables.sql
+++ typo3/sysext/frontend/ext_tables.sql
@@ -61,7 +61,7 @@ CREATE TABLE fe_session_data (
 #
 CREATE TABLE fe_sessions (
 	ses_id varchar(32) DEFAULT '' NOT NULL,
-	ses_name varchar(255) DEFAULT '' NOT NULL,
+	ses_name varchar(100) DEFAULT '' NOT NULL,
 	ses_iplock varchar(39) DEFAULT '' NOT NULL,
 	ses_userid int(11) unsigned DEFAULT '0' NOT NULL,
 	ses_tstamp int(11) unsigned DEFAULT '0' NOT NULL,
