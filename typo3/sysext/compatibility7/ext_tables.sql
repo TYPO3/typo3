739ecb10686d5f931617cc1f79ecdffe789d1d9f
Georg Ringer <georg.ringer@gmail.com>
2017-02-12 20:40:40 +0100

    [TASK] Streamline ext_tables.sql of compatibility ext
    Move the 2 fields of tt_content into one table definition.
    
    Resolves: #79767
    Releases: master
    Change-Id: Ie376d095f1ebc4611635b0c487d43796ffa6fdc9
    Reviewed-on: https://review.typo3.org/51645
    Reviewed-by: Thomas Hohn <thomas@hohn.dk>
    Tested-by: Thomas Hohn <thomas@hohn.dk>
    Reviewed-by: Andreas Fernandez <typo3@scripting-base.de>
    Tested-by: Andreas Fernandez <typo3@scripting-base.de>

--- typo3/sysext/compatibility7/ext_tables.sql
+++ typo3/sysext/compatibility7/ext_tables.sql
@@ -2,7 +2,8 @@
 # Table structure for table 'tt_content'
 #
 CREATE TABLE tt_content (
-	menu_type varchar(30) DEFAULT '0' NOT NULL
+	menu_type varchar(30) DEFAULT '0' NOT NULL,
+	select_key varchar(80) DEFAULT '' NOT NULL
 );
 
 #
@@ -10,11 +11,4 @@ CREATE TABLE tt_content (
 #
 CREATE TABLE pages (
 	url_scheme tinyint(3) unsigned DEFAULT '0' NOT NULL
-);
-
-#
-# Table structure for table 'tt_content'
-#
-CREATE TABLE tt_content (
-	select_key varchar(80) DEFAULT '' NOT NULL
 );
