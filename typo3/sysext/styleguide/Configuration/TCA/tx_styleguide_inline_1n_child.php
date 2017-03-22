a52e28548eadef122d832901cfcc06a1e68c3aac
Christian Kuhn <lolli@schwarzbu.ch>
2017-03-22 21:09:12 +0100

    [TASK] styleguide: TCA: Prepare inline_1n for localization

--- typo3/sysext/styleguide/Configuration/TCA/tx_styleguide_inline_1n_child.php
+++ typo3/sysext/styleguide/Configuration/TCA/tx_styleguide_inline_1n_child.php
@@ -3,47 +3,25 @@ return [
     'ctrl' => [
         'title' => 'Form engine - inline 1:n foreign field child',
         'label' => 'input_1',
-        'iconfile' => 'EXT:styleguide/Resources/Public/Icons/tx_styleguide.svg',
         'tstamp' => 'tstamp',
         'crdate' => 'crdate',
         'cruser_id' => 'cruser_id',
         'delete' => 'deleted',
+        'sortby' => 'sorting',
+        'iconfile' => 'EXT:styleguide/Resources/Public/Icons/tx_styleguide.svg',
+        'languageField' => 'sys_language_uid',
+        'transOrigPointerField' => 'l10n_parent',
+        'transOrigDiffSourceField' => 'l10n_diffsource',
+        'translationSource' => 'l10n_source',
         'enablecolumns' => [
             'disabled' => 'disable',
         ],
-        'sortby' => 'sorting',
     ],
 
 
     'columns' => [
 
 
-        'sys_language_uid' => [
-            'label'  => 'LLL:EXT:lang/Resources/Private/Language/locallang_general.xlf:LGL.language',
-            'config' => [
-                'type' => 'select',
-                'renderType' => 'selectSingle',
-                'foreign_table' => 'sys_language',
-                'foreign_table_where' => 'ORDER BY sys_language.title',
-                'items' => [
-                    ['LLL:EXT:lang/Resources/Private/Language/locallang_general.xlf:LGL.allLanguages', -1],
-                    ['LLL:EXT:lang/Resources/Private/Language/locallang_general.xlf:LGL.default_value', 0]
-                ]
-            ]
-        ],
-        'l10n_parent' => [
-            'displayCond' => 'FIELD:sys_language_uid:>:0',
-            'label' => 'Translation parent',
-            'config' => [
-                'type' => 'select',
-                'renderType' => 'selectSingle',
-                'items' => [
-                    ['', 0],
-                ],
-                'foreign_table' => 'tx_styleguide_inline_1n_child',
-                'foreign_table_where' => 'AND tx_styleguide_inline_1n_child.pid=###CURRENT_PID### AND tx_styleguide_inline_1n_child.sys_language_uid IN (-1,0)',
-            ]
-        ],
         'l10n_diffsource' => [
             'config' => [
                 'type' => 'passthrough'
@@ -53,6 +31,59 @@ return [
             'label' => 'LLL:EXT:lang/Resources/Private/Language/locallang_general.xlf:LGL.disable',
             'config' => [
                 'type' => 'check'
+            ]
+        ],
+        'sys_language_uid' => [
+            'exclude' => true,
+            'label' => 'LLL:EXT:lang/Resources/Private/Language/locallang_general.xlf:LGL.language',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectSingle',
+                'special' => 'languages',
+                'items' => [
+                    [
+                        'LLL:EXT:lang/Resources/Private/Language/locallang_general.xlf:LGL.allLanguages',
+                        -1,
+                        'flags-multiple'
+                    ],
+                ],
+                'default' => 0,
+            ]
+        ],
+        'l10n_parent' => [
+            'exclude' => true,
+            'displayCond' => 'FIELD:sys_language_uid:>:0',
+            'label' => 'Translation parent',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectSingle',
+                'items' => [
+                    [
+                        '',
+                        0
+                    ]
+                ],
+                'foreign_table' => 'tx_styleguide_inline_1n_child',
+                'foreign_table_where' => 'AND tx_styleguide_inline_1n_child.pid=###CURRENT_PID### AND tx_styleguide_inline_1n_child.sys_language_uid IN (-1,0)',
+                'default' => 0
+            ]
+        ],
+        'l10n_source' => [
+            'exclude' => true,
+            'displayCond' => 'FIELD:sys_language_uid:>:0',
+            'label' => 'Translation source',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectSingle',
+                'items' => [
+                    [
+                        '',
+                        0
+                    ]
+                ],
+                'foreign_table' => 'tx_styleguide_inline_1n_child',
+                'foreign_table_where' => 'AND tx_styleguide_inline_1n_child.pid=###CURRENT_PID### AND tx_styleguide_inline_1n_child.uid!=###THIS_UID###',
+                'default' => 0
             ]
         ],
 
@@ -103,7 +134,7 @@ return [
         '0' => [
             'showitem' => '
                 --div--;General, input_1, group_db_1, select_tree_1,
-                --div--;Visibility, sys_language_uid, l18n_parent, l18n_diffsource, disable, parentid, parenttable
+                --div--;meta, disable, sys_language_uid, l10n_parent, l10n_source,
             ',
         ],
     ],
