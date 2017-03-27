888a1c46714cd14fd78810a3f7c7d7afede8b68f
Benjamin Kott <benjamin.kott@wfp2.com>
2017-03-27 21:53:54 +0200

    [TASK] Replace clearable icon with SVG from TYPO3 icon set
    Resolves: #80498
    Releases: master
    Change-Id: I139922f0bb86e1ff1dc4bd9258491bd5eedaedc2
    Reviewed-on: https://review.typo3.org/52195
    Reviewed-by: Wouter Wolters <typo3@wouterwolters.nl>
    Tested-by: Wouter Wolters <typo3@wouterwolters.nl>
    Reviewed-by: Jigal van Hemert <jigal.van.hemert@typo3.org>
    Tested-by: Jigal van Hemert <jigal.van.hemert@typo3.org>
    Reviewed-by: Markus Klein <markus.klein@typo3.org>
    Tested-by: Markus Klein <markus.klein@typo3.org>

--- typo3/sysext/backend/Resources/Public/JavaScript/jquery.clearable.js
+++ typo3/sysext/backend/Resources/Public/JavaScript/jquery.clearable.js
@@ -35,6 +35,23 @@
 			'onClear': function() {}
 		};
 
+		// The inlined markup represent the current generated markup from the
+		// icon api for the icon actions-close that can be found in the official
+		// icon repository and is registered in the backend icon api.
+		//
+		// It´s not possible to use/open the backend icon api without opening
+		// new possible vectors for attackers to sniff system informations.
+		//
+		// When the icon definition of actions-close changes also the inlined
+		// icon should be updated.
+		//
+		// https://github.com/typo3/typo3.icons
+		var closeIcon = '<span class="t3js-icon icon icon-size-small icon-state-default icon-actions-close" data-identifier="actions-close">' +
+			'<span class="icon-markup">' +
+			'<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="M11.9 5.5L9.4 8l2.5 2.5c.2.2.2.5 0 .7l-.7.7c-.2.2-.5.2-.7 0L8 9.4l-2.5 2.5c-.2.2-.5.2-.7 0l-.7-.7c-.2-.2-.2-.5 0-.7L6.6 8 4.1 5.5c-.2-.2-.2-.5 0-.7l.7-.7c.2-.2.5-.2.7 0L8 6.6l2.5-2.5c.2-.2.5-.2.7 0l.7.7c.2.2.2.5 0 .7z" class="icon-color"/></svg>' +
+			'</span>' +
+			'</span>';
+
 		// Merge defaults and given options. Given options have higher priority
 		// because they are the last argument.
 		var settings = $.extend({}, defaults, options);
@@ -55,7 +72,7 @@
 				// Wrap it with a div and add a span that is the trigger for
 				// clearing.
 				$input.wrap('<div class="form-control-clearable" />');
-				$input.after('<button type="button" class="close' + hiddenClass + '" tabindex="-1" aria-hidden="true"><span class="fa fa-times" /></button>');
+				$input.after('<button type="button" class="close' + hiddenClass + '" tabindex="-1" aria-hidden="true">' + closeIcon + '</button>');
 				$input.addClass('t3js-clearable');
 
 				var $clearer = $input.next();
