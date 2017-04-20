0a5ad14cd26697339b00586b800326eec8588175
Wouter Wolters <typo3@wouterwolters.nl>
2017-04-20 20:23:59 +0200

    [BUGFIX] Use correct TypoScriptService in ClickEnlargeViewHelper
    The Extbase TypoScriptService is used but should use the one from
    the core.
    
    Resolves: #80918
    Releases: master,8.7
    Change-Id: Ice000c0b6807482d442705362ba5fa444baf8eba
    Reviewed-on: https://review.typo3.org/52530
    Tested-by: TYPO3com <no-reply@typo3.com>
    Reviewed-by: Andreas Fernandez <typo3@scripting-base.de>
    Reviewed-by: Benni Mack <benni@typo3.org>
    Tested-by: Benni Mack <benni@typo3.org>
    Reviewed-by: Georg Ringer <georg.ringer@gmail.com>
    Tested-by: Georg Ringer <georg.ringer@gmail.com>

--- typo3/sysext/fluid_styled_content/Classes/ViewHelpers/Link/ClickEnlargeViewHelper.php
+++ typo3/sysext/fluid_styled_content/Classes/ViewHelpers/Link/ClickEnlargeViewHelper.php
@@ -16,8 +16,8 @@ namespace TYPO3\CMS\FluidStyledContent\ViewHelpers\Link;
 
 use TYPO3\CMS\Core\Resource\FileInterface;
 use TYPO3\CMS\Core\Resource\FileReference;
+use TYPO3\CMS\Core\TypoScript\TypoScriptService;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Extbase\Service\TypoScriptService;
 use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
 use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
 use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
