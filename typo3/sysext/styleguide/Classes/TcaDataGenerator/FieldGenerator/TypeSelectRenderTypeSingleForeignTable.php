901cef8c16d89607947c85ffbe1e36e0620fe716
Henrik Elsner <helsner@dfau.de>
2017-04-25 17:32:08 +0200

    [TASK] styleguide: Implement record creation
    This commit enables the creation of localized records,
    when the styleguide data creation is triggered.
    At the moment this only works for the fitting tables with the
    needed table columns (l10n_*).

--- typo3/sysext/styleguide/Classes/TcaDataGenerator/FieldGenerator/TypeSelectRenderTypeSingleForeignTable.php
+++ typo3/sysext/styleguide/Classes/TcaDataGenerator/FieldGenerator/TypeSelectRenderTypeSingleForeignTable.php
@@ -67,6 +67,7 @@ class TypeSelectRenderTypeSingleForeignTable extends AbstractFieldGenerator impl
             $sourceFile = GeneralUtility::getFileAbsFileName('EXT:styleguide/Resources/Public/Images/Pictures/' . $fileName);
             $targetFile = $basicFileUtility->getUniqueName($sourceFile, PATH_site . 'uploads/tx_styleguide');
             GeneralUtility::upload_copy_move($sourceFile, $targetFile);
+            // in case of exception at this point (basename requires parameter, null given) => empty uploads/tx_styleguide
             $finalFileName = basename($targetFile);
 
             // Insert an empty row again to have the uid already. This is useful for
--- typo3/sysext/styleguide/Classes/TcaDataGenerator/TableHandler/General.php
+++ typo3/sysext/styleguide/Classes/TcaDataGenerator/TableHandler/General.php
@@ -20,6 +20,7 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Styleguide\TcaDataGenerator\RecordData;
 use TYPO3\CMS\Styleguide\TcaDataGenerator\RecordFinder;
 use TYPO3\CMS\Styleguide\TcaDataGenerator\TableHandlerInterface;
+use TYPO3\CMS\Styleguide\Service\TranslateCopyService;
 
 /**
  * General table handler
@@ -64,5 +65,32 @@ class General extends AbstractTableHandler implements TableHandlerInterface
             $fieldValues,
             [ 'uid' => $fieldValues['uid'] ]
         );
+
+        /** @var TranslateCopyService $translateCopyService */
+        $translateCopyService = GeneralUtility::makeInstance(TranslateCopyService::class);
+        $demoLanguages = $recordFinder->findUidsOfDemoLanguages();
+
+        $translatedRecord = -42;
+        foreach ($demoLanguages as $demoLanguageIndex => $demoLanguageUid) {
+            switch ($demoLanguageIndex) {
+                case 0:
+                    $translateCopyService->copyRecordToLanguage($tableName, $fieldValues['uid'], $demoLanguageUid);
+                    break;
+                case 1:
+                    $result = $translateCopyService->localizeRecord($tableName, $fieldValues['uid'], $demoLanguageUid);
+                    $translatedRecord = $result[$tableName][$fieldValues['uid']];
+                    break;
+                case 2:
+                    $result = $translateCopyService->localizeRecord($tableName, $translatedRecord, $demoLanguageUid);
+                    $translatedRecord = $result[$tableName][$translatedRecord];
+                    break;
+                case 3:
+                    $translateCopyService->copyRecordToLanguage($tableName, $translatedRecord, $demoLanguageUid);
+                    break;
+                default:
+                    $translateCopyService->localizeRecord($tableName, $fieldValues['uid'], $demoLanguageUid);
+                    break;
+            }
+        }
     }
 }
