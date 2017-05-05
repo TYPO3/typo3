e283ca7817bcf869b448e332b8de5c063459d2cb
Christian Futterlieb <christian@futterlieb.ch>
2017-05-05 16:12:22 +0200

    [BUGFIX] Correct typos in AbstractPlugin::pi_list_browseresults()
    * Add a forgotten '$'
    * Encapsulate ternary condition before concatenating strings
    
    Resolves: #81153
    Releases: master, 8.7
    Change-Id: I0e180d2115435f0f41fb7b89862ff5d868c4dbd5
    Reviewed-on: https://review.typo3.org/52726
    Reviewed-by: Jan Helke <typo3@helke.de>
    Tested-by: Jan Helke <typo3@helke.de>
    Reviewed-by: Oliver Klee <typo3-coding@oliverklee.de>
    Tested-by: TYPO3com <no-reply@typo3.com>

--- typo3/sysext/frontend/Classes/Plugin/AbstractPlugin.php
+++ typo3/sysext/frontend/Classes/Plugin/AbstractPlugin.php
@@ -613,7 +613,7 @@ class AbstractPlugin
                     $links[] = $this->cObj->wrap($this->pi_linkTP_keepPIvars($hscText ? htmlspecialchars($label) : $label, [$pointerName => null], $pi_isOnlyFields), $wrapper['inactiveLinkWrap']);
                 } else {
                     $label = $this->pi_getLL('pi_list_browseresults_first', '<< First');
-                    $links[] = $this->cObj->wrap(hscText ? htmlspecialchars($label) : $label, $wrapper['disabledLinkWrap']);
+                    $links[] = $this->cObj->wrap($hscText ? htmlspecialchars($label) : $label, $wrapper['disabledLinkWrap']);
                 }
             }
             // Link to previous page
@@ -632,7 +632,7 @@ class AbstractPlugin
                     $pageText = ($a * $results_at_a_time + 1) . '-' . min($count, ($a + 1) * $results_at_a_time);
                 } else {
                     $label = $this->pi_getLL('pi_list_browseresults_page', 'Page');
-                    $pageText = trim($hscText ? htmlspecialchars($label) : $label . ' ' . ($a + 1));
+                    $pageText = trim(($hscText ? htmlspecialchars($label) : $label) . ' ' . ($a + 1));
                 }
                 // Current page
                 if ($pointer == $a) {
