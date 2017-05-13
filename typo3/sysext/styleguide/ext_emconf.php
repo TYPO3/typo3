56eb9233e6ec2a36990ca13065cd4578f4a1008d
Josef Glatz <josefglatz@gmail.com>
2017-05-13 15:47:47 +0200

    [TASK] styleguide: Update TYPO3 version constraint
    to support TYPO3 9.x-dev

--- typo3/sysext/styleguide/ext_emconf.php
+++ typo3/sysext/styleguide/ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '8.0.7',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '8.0.0-8.99.99',
+            'typo3' => '8.0.0-9.99.99',
         ),
         'conflicts' => array(
         ),
