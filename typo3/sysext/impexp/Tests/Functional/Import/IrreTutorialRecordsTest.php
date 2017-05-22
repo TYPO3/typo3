2d99d6cec44df0b7f9d390db2b7f19cd84696b14
Christian Kuhn <lolli@schwarzbu.ch>
2017-05-22 15:39:09 +0200

    [BUGFIX] mssql: ext:impexp test fixes
    The patch adds fixture comparison files
    for mssql platform on ext:impexp functional
    tests.
    
    Change-Id: I2ff9048b9f6395c555f72a68b16684e9bf22de84
    Resolves: #81501
    Releases: master, 8.7
    Reviewed-on: https://review.typo3.org/53155
    Reviewed-by: Frank Naegler <frank.naegler@typo3.org>
    Tested-by: Frank Naegler <frank.naegler@typo3.org>
    Tested-by: TYPO3com <no-reply@typo3.com>

--- typo3/sysext/impexp/Tests/Functional/Import/IrreTutorialRecordsTest.php
+++ typo3/sysext/impexp/Tests/Functional/Import/IrreTutorialRecordsTest.php
@@ -1,5 +1,5 @@
 <?php
-namespace TYPO3\CMS\Impexp\Tests\Functional\Import\IrreTutorialRecords;
+namespace TYPO3\CMS\Impexp\Tests\Functional\Import;
 
 /*
  * This file is part of the TYPO3 CMS project.
--- typo3/sysext/impexp/Tests/Functional/Import/PagesAndTtContentWithImagesInEmptyDatabaseTest.php
+++ typo3/sysext/impexp/Tests/Functional/Import/PagesAndTtContentWithImagesInEmptyDatabaseTest.php
@@ -1,5 +1,5 @@
 <?php
-namespace TYPO3\CMS\Impexp\Tests\Functional\Import\PagesAndTtContentWithImages;
+namespace TYPO3\CMS\Impexp\Tests\Functional\Import;
 
 /*
  * This file is part of the TYPO3 CMS project.
