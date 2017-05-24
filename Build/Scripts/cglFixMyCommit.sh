447099666154a97d7df01413a46fc8a8e87d28a2
Elmar Hinz <t3elmar@gmail.com>
2017-05-24 18:10:23 +0200

    [FOLLOWUP][TASK] Squeeze every bit of performance from CGL fixer
    Support case sensitve file systems again.
    
    Releases: master, 8.7
    Resolves: #80814
    Change-Id: I82274b328e9cfa75fb35360145d7a668d1270dc3
    Reviewed-on: https://review.typo3.org/52939
    Tested-by: TYPO3com <no-reply@typo3.com>
    Reviewed-by: Oliver Klee <typo3-coding@oliverklee.de>
    Reviewed-by: Claus Due <claus@phpmind.net>
    Tested-by: Claus Due <claus@phpmind.net>
    Reviewed-by: Tymoteusz Motylewski <t.motylewski@gmail.com>
    Tested-by: Tymoteusz Motylewski <t.motylewski@gmail.com>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>

--- Build/Scripts/cglFixMyCommit.sh
+++ Build/Scripts/cglFixMyCommit.sh
@@ -13,7 +13,7 @@
 
 php_no_xdebug () {
     temporaryPath="$(mktemp -t php.XXXX).ini"
-    php -i | grep "\.ini" | grep -o -e '\(/[a-z0-9._-]\+\)\+\.ini' | grep -v xdebug | xargs awk 'FNR==1{print ""}1' > "${temporaryPath}"
+    php -i | grep "\.ini" | grep -o -e '\(/[A-Za-z0-9._-]\+\)\+\.ini' | grep -v xdebug | xargs awk 'FNR==1{print ""}1' > "${temporaryPath}"
     php -n -c "${temporaryPath}" "$@"
     RETURN=$?
     rm -f "${temporaryPath}"
