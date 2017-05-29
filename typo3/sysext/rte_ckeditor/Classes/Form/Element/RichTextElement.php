5b0e5eb35cac7b00ff4cd6327e4be72e3fe39abf
Ralf Hettinger <ralf.hettinger@brain-appeal.com>
2017-05-29 22:38:43 +0200

    [BUGFIX] rte_ckeditor triggers has-change on change
    CKEditor fields now triggers FormEngine.Validation.markFieldAsChanged
    therefore a warning of unsaved changes on closing the form is raised
    based on changes within these CKEditor fields.
    
    Resolves: #68955
    Releases: master, 8.7
    Change-Id: I0da98c2204b20b0f6cd27a53b66f2df3d369a145
    Reviewed-on: https://review.typo3.org/53247
    Tested-by: TYPO3com <no-reply@typo3.com>
    Reviewed-by: Benni Mack <benni@typo3.org>
    Tested-by: Benni Mack <benni@typo3.org>

--- typo3/sysext/rte_ckeditor/Classes/Form/Element/RichTextElement.php
+++ typo3/sysext/rte_ckeditor/Classes/Form/Element/RichTextElement.php
@@ -184,10 +184,11 @@ class RichTextElement extends AbstractFormElement
         return 'function(CKEDITOR) {
                 ' . $externalPlugins . '
                 CKEDITOR.replace("' . $fieldId . '", ' . json_encode($configuration) . ');
-                require([\'TYPO3/CMS/Backend/FormEngine\'], function(FormEngine) {
+                require([\'jquery\', \'TYPO3/CMS/Backend/FormEngine\'], function($, FormEngine) {
                     CKEDITOR.instances.' . $fieldId . '.on(\'change\', function() {
                         CKEDITOR.instances.' . $fieldId . '.updateElement();
                         FormEngine.Validation.validate();
+                        FormEngine.Validation.markFieldAsChanged($(\'#' . $fieldId . '\'));
                     });
                 });
         }';
