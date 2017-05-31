afadfd24fde877acf3d3252bb544bcd8d084a835
Christian Kuhn <lolli@schwarzbu.ch>
2017-05-31 17:18:15 +0200

    [TASK] Refactor ext:impexp functional tests
    The ext:impexp functional tests are rather hard to read and
    follow. The patch untangles them to make them better maintainable:
    
    * Combine the two different import / export abstract classes into 1 class
    * Don't inherit from AbstractDataHandlerActionTestCase anymore
    * Give single test cases better file names and do not hide them in sub dirs
    * Move all fixtures to Fixtures/ dir
    * Use less helper methods to import and test things
    * File paths are more straight and with less helper magic
    
    Change-Id: Iae36771b94045f99444f394129cbcf183168b584
    Resolves: #81393
    Releases: master, 8.7
    Reviewed-on: https://review.typo3.org/52981
    Tested-by: TYPO3com <no-reply@typo3.com>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>

---
+++ typo3/sysext/impexp/Tests/Functional/Import/IrreTutorialRecordsTest.php
@@ -0,0 +1,49 @@
+<?php
+namespace TYPO3\CMS\Impexp\Tests\Functional\Import\IrreTutorialRecords;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Impexp\Import;
+use TYPO3\CMS\Impexp\Tests\Functional\AbstractImportExportTestCase;
+
+/**
+ * Test case
+ */
+class IrreTutorialRecordsTest extends AbstractImportExportTestCase
+{
+    /**
+     * @var array
+     */
+    protected $testExtensionsToLoad = [
+        'typo3/sysext/core/Tests/Functional/Fixtures/Extensions/irre_tutorial',
+    ];
+
+    /**
+     * @test
+     */
+    public function importIrreRecords()
+    {
+        $subject = GeneralUtility::makeInstance(Import::class);
+        $subject->init();
+
+        $subject->loadFile(
+            __DIR__ . '/../Fixtures/XmlImports/irre-records.xml',
+            1
+        );
+        $subject->importData(0);
+
+        $this->assertCSVDataSet('EXT:impexp/Tests/Functional/Fixtures/DatabaseAssertions/importIrreRecords.csv');
+    }
+}
---
+++ typo3/sysext/impexp/Tests/Functional/Import/PagesAndTtContentWithImagesInEmptyDatabaseTest.php
@@ -0,0 +1,202 @@
+<?php
+namespace TYPO3\CMS\Impexp\Tests\Functional\Import\PagesAndTtContentWithImages;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Impexp\Import;
+use TYPO3\CMS\Impexp\Tests\Functional\AbstractImportExportTestCase;
+
+/**
+ * Test case
+ */
+class PagesAndTtContentWithImagesInEmptyDatabaseTest extends AbstractImportExportTestCase
+{
+    /**
+     * @test
+     */
+    public function importPagesAndRelatedTtContentWithImagesOnCaseSensitiveFilesystems()
+    {
+        $subject = GeneralUtility::makeInstance(Import::class);
+        $subject->init();
+
+        if (!$this->isCaseSensitiveFilesystem()) {
+            $this->markTestSkipped('Test not available on case insensitive filesystems.');
+        }
+
+        $subject->loadFile(
+            __DIR__ . '/../Fixtures/XmlImports/pages-and-ttcontent-with-image.xml',
+            1
+        );
+        $subject->importData(0);
+
+        $this->testFilesToDelete[] = PATH_site . 'fileadmin/user_upload/typo3_image2.jpg';
+
+        $this->assertCSVDataSet('EXT:impexp/Tests/Functional/Fixtures/DatabaseAssertions/importPagesAndRelatedTtContentWithImagesOnCaseSensitiveFilesystems.csv');
+
+        $this->assertFileEquals(__DIR__ . '/../Fixtures/Folders/fileadmin/user_upload/typo3_image2.jpg', PATH_site . 'fileadmin/user_upload/typo3_image2.jpg');
+    }
+
+    /**
+     * @test
+     */
+    public function importPagesAndRelatedTtContentWithImagesOnCaseInsensitiveFilesystems()
+    {
+        $subject = GeneralUtility::makeInstance(Import::class);
+        $subject->init();
+
+        if ($this->isCaseSensitiveFilesystem()) {
+            $this->markTestSkipped('Test not available on case sensitive filesystems.');
+        }
+
+        $subject->loadFile(
+            __DIR__ . '/../Fixtures/XmlImports/pages-and-ttcontent-with-image.xml',
+            1
+        );
+        $subject->importData(0);
+
+        $this->testFilesToDelete[] = PATH_site . 'fileadmin/user_upload/typo3_image2.jpg';
+
+        $this->assertCSVDataSet('EXT:impexp/Tests/Functional/Fixtures/DatabaseAssertions/importPagesAndRelatedTtContentWithImagesOnCaseInsensitiveFilesystems.csv');
+
+        $this->assertFileEquals(__DIR__ . '/../Fixtures/Folders/fileadmin/user_upload/typo3_image2.jpg', PATH_site . 'fileadmin/user_upload/typo3_image2.jpg');
+    }
+
+    /**
+     * @test
+     */
+    public function importPagesAndRelatedTtContentWithImagesButWithoutStorageOnCaseSensitiveFilesystems()
+    {
+        $subject = GeneralUtility::makeInstance(Import::class);
+        $subject->init();
+
+        if (!$this->isCaseSensitiveFilesystem()) {
+            $this->markTestSkipped('Test not available on case insensitive filesystems.');
+        }
+
+        $subject->loadFile(
+            __DIR__ . '/../Fixtures/XmlImports/pages-and-ttcontent-with-image-without-storage.xml',
+            1
+        );
+        $subject->importData(0);
+
+        $this->testFilesToDelete[] = PATH_site . 'fileadmin/user_upload/typo3_image2.jpg';
+
+        $this->assertCSVDataSet('EXT:impexp/Tests/Functional/Fixtures/DatabaseAssertions/importPagesAndRelatedTtContentWithImagesButWithoutStorageOnCaseSensitiveFilesystems.csv');
+
+        $this->assertFileEquals(__DIR__ . '/../Fixtures/Folders/fileadmin/user_upload/typo3_image2.jpg', PATH_site . 'fileadmin/user_upload/typo3_image2.jpg');
+    }
+
+    /**
+     * @test
+     */
+    public function importPagesAndRelatedTtContentWithImagesButWithoutStorageOnCaseInsensitiveFilesystems()
+    {
+        $subject = GeneralUtility::makeInstance(Import::class);
+        $subject->init();
+
+        if ($this->isCaseSensitiveFilesystem()) {
+            $this->markTestSkipped('Test not available on case sensitive filesystems.');
+        }
+
+        $subject->loadFile(
+            __DIR__ . '/../Fixtures/XmlImports/pages-and-ttcontent-with-image-without-storage.xml',
+            1
+        );
+        $subject->importData(0);
+
+        $this->testFilesToDelete[] = PATH_site . 'fileadmin/user_upload/typo3_image2.jpg';
+
+        $this->assertCSVDataSet('EXT:impexp/Tests/Functional/Fixtures/DatabaseAssertions/importPagesAndRelatedTtContentWithImagesButWithoutStorageOnCaseInsensitiveFilesystems.csv');
+
+        $this->assertFileEquals(__DIR__ . '/../Fixtures/Folders/fileadmin/user_upload/typo3_image2.jpg', PATH_site . 'fileadmin/user_upload/typo3_image2.jpg');
+    }
+
+    /**
+     * @test
+     */
+    public function importPagesAndRelatedTtContentWithImagesWithSpacesInPath()
+    {
+        $subject = GeneralUtility::makeInstance(Import::class);
+        $subject->init();
+
+        $subject->loadFile(
+            __DIR__ . '/../Fixtures/XmlImports/pages-and-ttcontent-with-image-with-spaces-in-path.xml',
+            1
+        );
+        $subject->importData(0);
+
+        $this->testFilesToDelete[] = PATH_site . 'fileadmin/user_upload/folder_with_spaces/typo3_image2.jpg';
+        $this->testFilesToDelete[] = PATH_site . 'fileadmin/user_upload/folder_with_spaces/typo3_image3.jpg';
+
+        $this->assertCSVDataSet('EXT:impexp/Tests/Functional/Fixtures/DatabaseAssertions/importPagesAndRelatedTtContentWithImagesWithSpacesInPath.csv');
+
+        $this->assertFileEquals(__DIR__ . '/../Fixtures/Folders/fileadmin/user_upload/typo3_image2.jpg', PATH_site . 'fileadmin/user_upload/folder_with_spaces/typo3_image2.jpg');
+        $this->assertFileEquals(__DIR__ . '/../Fixtures/Folders/fileadmin/user_upload/typo3_image3.jpg', PATH_site . 'fileadmin/user_upload/folder_with_spaces/typo3_image3.jpg');
+    }
+
+    /**
+     * @test
+     */
+    public function importPagesAndRelatedTtContentWithImagesButNotIncluded()
+    {
+        $subject = GeneralUtility::makeInstance(Import::class);
+        $subject->init();
+
+        $subject->loadFile(
+            // Files are parallel to the fixture .xml file in a folder - impexp tests for /../ not allowed in path, so we set an absolute path here
+            PATH_site . 'typo3/sysext/impexp/Tests/Functional/Fixtures/XmlImports/pages-and-ttcontent-with-image-but-not-included.xml',
+            1
+        );
+        $subject->importData(0);
+
+        $this->testFilesToDelete[] = PATH_site . 'fileadmin/user_upload/typo3_image2.jpg';
+
+        $this->assertCSVDataSet('EXT:impexp/Tests/Functional/Fixtures/DatabaseAssertions/importPagesAndRelatedTtContentWithImagesButNotIncluded.csv');
+
+        $this->assertFileEquals(__DIR__ . '/../Fixtures/Folders/fileadmin/user_upload/typo3_image2.jpg', PATH_site . 'fileadmin/user_upload/typo3_image2.jpg');
+    }
+
+    /**
+     * @test
+     * @group mysql
+     */
+    public function importPagesAndRelatedTtContentWithImageWithForcedUids()
+    {
+        // @todo: Fix impexp / test with postgres: Probably, force uid's is not possible with postgres or needs
+        // @todo: to be adapted in somehow.
+
+        $subject = GeneralUtility::makeInstance(Import::class);
+        $subject->init();
+
+        $subject->loadFile(
+            __DIR__ . '/../Fixtures/XmlImports/pages-and-ttcontent-with-image-with-forced-uids.xml',
+            1
+        );
+        $subject->force_all_UIDS = true;
+        $subject->importData(0);
+
+        $this->testFilesToDelete[] = PATH_site . 'fileadmin/user_upload/typo3_image2.jpg';
+
+        $this->assertCSVDataSet('EXT:impexp/Tests/Functional/Fixtures/DatabaseAssertions/importPagesAndRelatedTtContentWithImageWithForcedUids.csv');
+
+        $this->assertFileEquals(__DIR__ . '/../Fixtures/Folders/fileadmin/user_upload/typo3_image2.jpg', PATH_site . 'fileadmin/user_upload/typo3_image2.jpg');
+
+        $expectedErrors = [
+                'Forcing uids of sys_file records is not supported! They will be imported as new records!'
+        ];
+        $errors = $subject->errorLog;
+        $this->assertSame($expectedErrors, $errors);
+    }
+}
