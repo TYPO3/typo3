7b4099f4f0c5c9daf6a890c3639c1537368a927e
Jigal van Hemert <jigal.van.hemert@typo3.org>
2017-06-03 17:30:39 +0200

    [BUGFIX] Hide empty descriptions in textmedia in page module
    In the Page module with textmedia content elements the descriptions
    are shown below the thumbnails of linked media. If there is no
    description an empty line is now suppressed.
    
    Resolves: #81408
    Releases: master, 8.7, 7.6
    Change-Id: I9f342908f179550bcf2156328117c464bb8c672f
    Reviewed-on: https://review.typo3.org/53093
    Reviewed-by: Jo Hasenau <info@cybercraft.de>
    Tested-by: Jo Hasenau <info@cybercraft.de>
    Tested-by: TYPO3com <no-reply@typo3.com>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>

--- typo3/sysext/frontend/Classes/Hooks/PageLayoutView/TextmediaPreviewRenderer.php
+++ typo3/sysext/frontend/Classes/Hooks/PageLayoutView/TextmediaPreviewRenderer.php
@@ -53,7 +53,10 @@ class TextmediaPreviewRenderer implements PageLayoutViewDrawItemHookInterface
                     $linkedContent = '';
 
                     foreach ($fileReferences as $fileReference) {
-                        $linkedContent .= htmlspecialchars($fileReference->getDescription()) . '<br />';
+                        $description = $fileReference->getDescription();
+                        if ($description !== null && $description !== '') {
+                            $linkedContent .= htmlspecialchars($description) . '<br />';
+                        }
                     }
 
                     $itemContent .= $parentObject->linkEditContent($linkedContent, $row);
