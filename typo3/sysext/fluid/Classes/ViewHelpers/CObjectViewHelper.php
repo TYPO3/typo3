5fa6c73cbadb05dbe7af89b73a87dd10d30d0191
Markus Klein <markus.klein@typo3.org>
2017-06-12 11:26:15 +0200

    [BUGFIX] Better documentation of cObject viewhelper
    Add more information about the table argument and when it should
    be set.
    
    Resolves: #81545
    Releases: master, 8.7
    Change-Id: Ic95794869b384e0932d59747df545f15afe2825f
    Reviewed-on: https://review.typo3.org/53188
    Tested-by: TYPO3com <no-reply@typo3.com>
    Reviewed-by: Robert Wildling <robertwildling@gmail.com>
    Tested-by: Robert Wildling <robertwildling@gmail.com>
    Reviewed-by: Wouter Wolters <typo3@wouterwolters.nl>
    Tested-by: Wouter Wolters <typo3@wouterwolters.nl>

--- typo3/sysext/fluid/Classes/ViewHelpers/CObjectViewHelper.php
+++ typo3/sysext/fluid/Classes/ViewHelpers/CObjectViewHelper.php
@@ -82,7 +82,7 @@ class CObjectViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHel
         $this->registerArgument('data', 'mixed', 'the data to be used for rendering the cObject. Can be an object, array or string. If this argument is not set, child nodes will be used');
         $this->registerArgument('typoscriptObjectPath', 'string', 'the TypoScript setup path of the TypoScript object to render', true);
         $this->registerArgument('currentValueKey', 'string', 'currentValueKey');
-        $this->registerArgument('table', 'string', 'table', false, '');
+        $this->registerArgument('table', 'string', 'the table name associated with "data" argument. Typically tt_content or one of your custom tables. This argument should be set if rendering a FILES cObject where file references are used, or if the data argument is a database record.', false, '');
     }
 
     /**
