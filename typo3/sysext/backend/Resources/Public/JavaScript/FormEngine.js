8f9729c66316887966256d7b6c15c20c12f492e5
Wolfgang Klinger <wk@plan2.net>
2017-07-14 23:47:49 +0200

    [BUGFIX] Escape value in FormEngine.removeOption selector
    Resolves: #81806
    Releases: master, 8.7, 7.6
    Change-Id: I1efc630a5579be8c8a1569667253f2572b9261ed
    Reviewed-on: https://review.typo3.org/53499
    Tested-by: TYPO3com <no-reply@typo3.com>
    Reviewed-by: Wouter Wolters <typo3@wouterwolters.nl>
    Reviewed-by: Benjamin Kluge <b.kluge@neusta.de>
    Tested-by: Benjamin Kluge <b.kluge@neusta.de>
    Reviewed-by: Joerg Boesche <typo3@joergboesche.de>
    Reviewed-by: Henning Liebe <h.liebe@neusta.de>
    Tested-by: Henning Liebe <h.liebe@neusta.de>
    Reviewed-by: Susanne Moog <susanne.moog@typo3.org>
    Tested-by: Susanne Moog <susanne.moog@typo3.org>

--- typo3/sysext/backend/Resources/Public/JavaScript/FormEngine.js
+++ typo3/sysext/backend/Resources/Public/JavaScript/FormEngine.js
@@ -578,7 +578,7 @@ define(['jquery',
 
 		$selected.each(function() {
 			$availableFieldEl
-				.find('option[value="' + $(this).attr('value') + '"]')
+				.find('option[value="' + $.escapeSelector($(this).attr('value')) + '"]')
 				.removeClass('hidden')
 				.prop('disabled', false);
 		});
