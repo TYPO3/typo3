50878f71f70c17ebd7f6a106b3eda7ba1c12e61c
Tymoteusz Motylewski <t.motylewski@gmail.com>
2017-08-19 19:16:49 +0200

    [TASK] Functional tests for translated tt_content rendering in Extbase
    Introduce and Extbase version of the LocalizedContentRenderingTest.
    It tests how language related TS settings influences content rendering.
    
    Also changes exception code in ContentController, to make bamboo happy.
    
    This test suite is documenting current behaviour, which is in fact
    inconsistent with TypoScript tt_content rendering.
    
    The next step is to provide a fix for extbase with a feature switch,
    so this test suite will become a version with feature swich being disabled.
    
    Resolves: #81823
    Releases: master, 8.7, 7.6
    Change-Id: Ia616e5089b0209cfb895216726f7192f39c88a45
    Reviewed-on: https://review.typo3.org/53426
    Tested-by: TYPO3com <no-reply@typo3.com>
    Reviewed-by: Benni Mack <benni@typo3.org>
    Tested-by: Benni Mack <benni@typo3.org>
    Reviewed-by: Wouter Wolters <typo3@wouterwolters.nl>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>

---
+++ typo3/sysext/extbase/Tests/Functional/Fixtures/Extensions/blog_example/Classes/Controller/ContentController.php
@@ -0,0 +1,116 @@
+<?php
+declare(strict_types=1);
+
+namespace ExtbaseTeam\BlogExample\Controller;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+/**
+ * ContentController
+ */
+class ContentController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
+{
+    /**
+     * @inject
+     * @var \ExtbaseTeam\BlogExample\Domain\Repository\TtContentRepository
+     */
+    protected $contentRepository;
+
+    /**
+     * @var string
+     */
+    protected $defaultViewObjectName = \TYPO3\CMS\Extbase\Mvc\View\JsonView::class;
+
+    /**
+     * @inject
+     * @var \TYPO3\CMS\Extbase\Persistence\Generic\Mapper\DataMapFactory
+     */
+    protected $dataMapFactory;
+
+    /**
+     * @return array
+     */
+    public function listAction()
+    {
+        $content = $this->contentRepository->findAll();
+        $value[$this->getRuntimeIdentifier()] = $this->getStructure($content);
+
+        $this->view->assign('value', $value);
+    }
+
+    /**
+     * @param \TYPO3\CMS\Extbase\Mvc\RequestInterface $request
+     * @param \TYPO3\CMS\Extbase\Mvc\ResponseInterface $response
+     * @throws \RuntimeException
+     */
+    public function processRequest(\TYPO3\CMS\Extbase\Mvc\RequestInterface $request, \TYPO3\CMS\Extbase\Mvc\ResponseInterface $response)
+    {
+        try {
+            parent::processRequest($request, $response);
+        } catch (\TYPO3\CMS\Extbase\Property\Exception $exception) {
+            throw new \RuntimeException(
+                $this->getRuntimeIdentifier() . ': ' . $exception->getMessage() . ' (' . $exception->getCode() . ')',
+                1476122223
+            );
+        }
+    }
+
+    /**
+     * @param \Iterator|\TYPO3\CMS\Extbase\DomainObject\AbstractEntity[] $iterator
+     * @return array
+     */
+    protected function getStructure($iterator)
+    {
+        $structure = [];
+
+        if (!$iterator instanceof \Iterator) {
+            $iterator = [$iterator];
+        }
+
+        foreach ($iterator as $entity) {
+            $dataMap = $this->dataMapFactory->buildDataMap(get_class($entity));
+            $tableName = $dataMap->getTableName();
+            $identifier = $tableName . ':' . $entity->getUid();
+            $properties = \TYPO3\CMS\Extbase\Reflection\ObjectAccess::getGettableProperties($entity);
+
+            $structureItem = [];
+            foreach ($properties as $propertyName => $propertyValue) {
+                $columnMap = $dataMap->getColumnMap($propertyName);
+                if ($columnMap !== null) {
+                    $propertyName = $columnMap->getColumnName();
+                }
+                if ($propertyValue instanceof \Iterator) {
+                    $structureItem[$propertyName] = $this->getStructure($propertyValue);
+                } else {
+                    $structureItem[$propertyName] = $propertyValue;
+                }
+            }
+            $structure[$identifier] = $structureItem;
+        }
+
+        return $structure;
+    }
+
+    /**
+     * @return string
+     */
+    protected function getRuntimeIdentifier()
+    {
+        $arguments = [];
+        foreach ($this->request->getArguments() as $argumentName => $argumentValue) {
+            $arguments[] = $argumentName . '=' . $argumentValue;
+        }
+        return $this->request->getControllerActionName() . '(' . implode(', ', $arguments) . ')';
+    }
+}
