1b0da009cbd233d79d8f68a7d3cfb29ad66faf8c
Tymoteusz Motylewski <t.motylewski@gmail.com>
2017-08-21 15:57:09 +0200

    Provide inline 1:1 example with relation field on the parent side

---
+++ typo3/sysext/styleguide/Configuration/TCA/tx_styleguide_inline_11_child.php
@@ -0,0 +1,146 @@
+<?php
+return [
+    'ctrl' => [
+        'title' => 'Form engine - inline 1:1 child',
+        'label' => 'input_1',
+        'tstamp' => 'tstamp',
+        'crdate' => 'crdate',
+        'cruser_id' => 'cruser_id',
+        'delete' => 'deleted',
+        'sortby' => 'sorting',
+        'iconfile' => 'EXT:styleguide/Resources/Public/Icons/tx_styleguide.svg',
+        'versioningWS' => true,
+        'origUid' => 't3_origuid',
+        'languageField' => 'sys_language_uid',
+        'transOrigPointerField' => 'l10n_parent',
+        'transOrigDiffSourceField' => 'l10n_diffsource',
+        'translationSource' => 'l10n_source',
+        'enablecolumns' => [
+            'disabled' => 'hidden',
+            'starttime' => 'starttime',
+            'endtime' => 'endtime',
+        ],
+    ],
+
+
+    'columns' => [
+        'hidden' => [
+            'exclude' => 1,
+            'config' => [
+                'type' => 'check',
+                'items' => [
+                    '1' => [
+                        '0' => 'Disable',
+                    ],
+                ],
+            ],
+        ],
+        'starttime' => [
+            'exclude' => 1,
+            'label' => 'Publish Date',
+            'config' => [
+                'type' => 'input',
+                'renderType' => 'inputDateTime',
+                'eval' => 'datetime',
+                'default' => '0'
+            ],
+            'l10n_mode' => 'exclude',
+            'l10n_display' => 'defaultAsReadonly'
+        ],
+        'endtime' => [
+            'exclude' => 1,
+            'label' => 'Expiration Date',
+            'config' => [
+                'type' => 'input',
+                'renderType' => 'inputDateTime',
+                'eval' => 'datetime',
+                'default' => '0',
+                'range' => [
+                    'upper' => mktime(0, 0, 0, 12, 31, 2020)
+                ]
+            ],
+            'l10n_mode' => 'exclude',
+            'l10n_display' => 'defaultAsReadonly'
+        ],
+        'sys_language_uid' => [
+            'exclude' => true,
+            'label' => 'LLL:EXT:lang/Resources/Private/Language/locallang_general.xlf:LGL.language',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectSingle',
+                'special' => 'languages',
+                'items' => [
+                    [
+                        'LLL:EXT:lang/Resources/Private/Language/locallang_general.xlf:LGL.allLanguages',
+                        -1,
+                        'flags-multiple'
+                    ],
+                ],
+                'default' => 0,
+            ]
+        ],
+        'l10n_parent' => [
+            'exclude' => true,
+            'displayCond' => 'FIELD:sys_language_uid:>:0',
+            'label' => 'Translation parent',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectSingle',
+                'items' => [
+                    [
+                        '',
+                        0
+                    ]
+                ],
+                'foreign_table' => 'tx_styleguide_inline_11_child',
+                'foreign_table_where' => 'AND tx_styleguide_inline_11_child.pid=###CURRENT_PID### AND tx_styleguide_inline_11_child.sys_language_uid IN (-1,0)',
+                'default' => 0
+            ]
+        ],
+        'l10n_source' => [
+            'exclude' => true,
+            'displayCond' => 'FIELD:sys_language_uid:>:0',
+            'label' => 'Translation source',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectSingle',
+                'items' => [
+                    [
+                        '',
+                        0
+                    ]
+                ],
+                'foreign_table' => 'tx_styleguide_inline_11_child',
+                'foreign_table_where' => 'AND tx_styleguide_inline_11_child.pid=###CURRENT_PID### AND tx_styleguide_inline_11_child.uid!=###THIS_UID###',
+                'default' => 0
+            ]
+        ],
+        'l10n_diffsource' => [
+            'config' => [
+                'type' => 'passthrough',
+                'default' => ''
+            ]
+        ],
+        'input_1' => [
+            'l10n_mode' => 'prefixLangTitle',
+            'label' => 'input_1',
+            'config' => [
+                'type' => 'input',
+                'size' => '30',
+            ],
+        ],
+    ],
+
+
+    'types' => [
+        '0' => [
+            'showitem' => '
+                input_1,
+                --div--;meta,
+                    disable, starttime, endtime, sys_language_uid, l10n_parent, l10n_source,
+            ',
+        ],
+    ],
+
+
+];
