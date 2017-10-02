6d091df97886f6cd0ef9b6237bf31376890cda17
Benni Mack <benni@typo3.org>
2017-10-03 01:34:02 +0200

    [BUGFIX] Set content-length header to the end of output
    Resolves: #76100
    Resolves: #65719
    Releases: master, 8.7
    Change-Id: I2e389dd6ecb497a164a457230f106a64373c1907
    Reviewed-on: https://review.typo3.org/54269
    Tested-by: TYPO3com <no-reply@typo3.com>
    Reviewed-by: Markus Klein <markus.klein@typo3.org>
    Tested-by: Markus Klein <markus.klein@typo3.org>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>

--- typo3/sysext/frontend/Classes/Http/RequestHandler.php
+++ typo3/sysext/frontend/Classes/Http/RequestHandler.php
@@ -266,6 +266,17 @@ class RequestHandler implements RequestHandlerInterface
         if ($sendTSFEContent) {
             /** @var \TYPO3\CMS\Core\Http\Response $response */
             $response = GeneralUtility::makeInstance(\TYPO3\CMS\Core\Http\Response::class);
+            // Send content-length header.
+            // Notice that all HTML content outside the length of the content-length header will be cut off!
+            // Therefore content of unknown length from included PHP-scripts and if admin users are logged
+            // in (admin panel might show...) or if debug mode is turned on, we disable it!
+            if (
+                (!isset($this->controller->config['config']['enableContentLengthHeader']) || $this->controller->config['config']['enableContentLengthHeader'])
+                && !$this->controller->beUserLogin && !$GLOBALS['TYPO3_CONF_VARS']['FE']['debug']
+                && !$this->controller->config['config']['debug'] && !$this->controller->doWorkspacePreview()
+            ) {
+                header('Content-Length: ' . strlen($this->controller->content));
+            }
             $response->getBody()->write($this->controller->content);
         }
         // Debugging Output
