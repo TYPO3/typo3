7872ee830e05c12c42ccc96bcdcf63f4b1700168
Markus Klein <markus.klein@typo3.org>
2017-10-03 18:18:26 +0200

    [BUGFIX] Never load disabled domains in TSFE
    This fixes an inappropriate transformation of old enableFields
    condition to doctrine.
    
    This is a regression fix to #77400.
    
    Resolves: #82661
    Related: #77400
    Releases: master, 8.7
    Change-Id: Ic18c9e5f59b48f30651a988c5ec9d243c8b186e3
    Reviewed-on: https://review.typo3.org/54296
    Tested-by: TYPO3com <no-reply@typo3.com>
    Reviewed-by: Benni Mack <benni@typo3.org>
    Tested-by: Benni Mack <benni@typo3.org>

--- typo3/sysext/frontend/Classes/Controller/TypoScriptFrontendController.php
+++ typo3/sysext/frontend/Classes/Controller/TypoScriptFrontendController.php
@@ -22,9 +22,9 @@ use TYPO3\CMS\Core\Charset\CharsetConverter;
 use TYPO3\CMS\Core\Controller\ErrorPageController;
 use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Database\Query\QueryHelper;
+use TYPO3\CMS\Core\Database\Query\Restriction\DefaultRestrictionContainer;
 use TYPO3\CMS\Core\Database\Query\Restriction\DeletedRestriction;
 use TYPO3\CMS\Core\Database\Query\Restriction\EndTimeRestriction;
-use TYPO3\CMS\Core\Database\Query\Restriction\FrontendRestrictionContainer;
 use TYPO3\CMS\Core\Database\Query\Restriction\StartTimeRestriction;
 use TYPO3\CMS\Core\Error\Http\PageNotFoundException;
 use TYPO3\CMS\Core\Error\Http\ServiceUnavailableException;
@@ -4659,7 +4659,7 @@ class TypoScriptFrontendController
             $sysDomainData = $runtimeCache->get($entryIdentifier);
         } else {
             $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('sys_domain');
-            $queryBuilder->setRestrictions(GeneralUtility::makeInstance(FrontendRestrictionContainer::class));
+            $queryBuilder->setRestrictions(GeneralUtility::makeInstance(DefaultRestrictionContainer::class));
             $result = $queryBuilder
                 ->select('uid', 'pid', 'domainName', 'forced')
                 ->from('sys_domain')
