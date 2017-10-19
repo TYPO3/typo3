24b5c5826676fdf14876758e1ada5009e2529216
Susanne Moog <susanne.moog@typo3.com>
2017-10-19 14:49:50 +0200

    [TASK] Extend WorkspaceServiceTests for fetchPagesWithVersionsInTable
    Add new tests for fetchPagesWithVersionsInTable and correct comments.
    
    Change-Id: I2fe08656ecddf3f7391e6012bb969dd85c2cfb24
    Resolves: #82811
    Releases: master, 8.7
    Reviewed-on: https://review.typo3.org/54445
    Tested-by: TYPO3com <no-reply@typo3.com>
    Reviewed-by: Oliver Hader <oliver.hader@typo3.org>
    Tested-by: Oliver Hader <oliver.hader@typo3.org>

--- typo3/sysext/workspaces/Classes/Service/WorkspaceService.php
+++ typo3/sysext/workspaces/Classes/Service/WorkspaceService.php
@@ -960,20 +960,22 @@ class WorkspaceService implements SingletonInterface
      *
      * Result:
      * [
+     *   'sys_template' => [],
      *   'tt_content' => [
-     *     1 => 1,
-     *     11 => 11,
-     *     13 => 13,
-     *     15 => 15
+     *     1 => true,
+     *     11 => true,
+     *     13 => true,
+     *     15 => true
      *   ],
      *   'tx_something => [
-     *     15 => 15,
-     *     11 => 11,
-     *     21 => 21
+     *     15 => true,
+     *     11 => true,
+     *     21 => true
      *   ],
      * ]
      *
      * @param int $workspaceId
+     *
      * @return array
      */
     public function getPagesWithVersionsInTable($workspaceId)
@@ -994,10 +996,10 @@ class WorkspaceService implements SingletonInterface
      *
      * Result:
      * [
-     *   1 => 1,
-     *   11 => 11,
-     *   13 => 13,
-     *   15 => 15
+     *   1 => true,
+     *   11 => true,
+     *   13 => true,
+     *   15 => true
      * ],
      *
      * @param int $workspaceId
