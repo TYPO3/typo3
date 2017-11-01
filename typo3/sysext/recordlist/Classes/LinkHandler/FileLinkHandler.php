ce60414e9558f8971be8af664783bac5ca4eff2f
Christian Kuhn <lolli@schwarzbu.ch>
2017-11-01 14:05:33 +0100

    [BUGFIX] Element file browser respects allowedExtensions
    The type=input, renderType=inputLink field control has an option
    "allowedExtensions" to restrict the file element browser to specific
    file endings. This option is now correctly considered in file browser.
    
    Change-Id: Ic42bd0de06002f308d7db4098c704ca2574e4018
    Resolves: #82861
    Releases: master, 8.7
    Reviewed-on: https://review.typo3.org/54573
    Tested-by: TYPO3com <no-reply@typo3.com>
    Reviewed-by: Markus Klein <markus.klein@typo3.org>
    Tested-by: Markus Klein <markus.klein@typo3.org>

--- typo3/sysext/recordlist/Classes/LinkHandler/FileLinkHandler.php
+++ typo3/sysext/recordlist/Classes/LinkHandler/FileLinkHandler.php
@@ -132,8 +132,8 @@ class FileLinkHandler extends AbstractLinkHandler implements LinkHandlerInterfac
             // Render the file or folderlist
             if ($selectedFolder->checkActionPermission('read')) {
                 $this->view->assign('selectedFolder', $selectedFolder);
-                $parameters = $this->linkBrowser->getUrlParameters();
-                $allowedExtensions = isset($parameters['allowedExtensions']) ? $parameters['allowedExtensions'] : '';
+                $parameters = $this->linkBrowser->getParameters();
+                $allowedExtensions = $parameters['params']['allowedExtensions'] ?? '';
                 $this->expandFolder($selectedFolder, $allowedExtensions);
             }
         }
