cadbc4d29cb9d6b8c22509e26bec1d6306b22439
Mathias Brodala <mbrodala@pagemachine.de>
2017-12-07 12:05:07 +0100

    [TASK] Add various Composer scripts
    This vastly simplifies running common commands for TYPO3 development.
    
    E.g. executing PHPUnit can be done like this:
    
        composer test:php:unit
    
    Since Composer uses Symfony Console this can even be shortened:
    
        composer t:p:u
    
    Also arbitrary arguments can be passed to the executed command:
    
        composer t:p:u -- --filter unitTestMethodName
    
    Change-Id: I3ef62417227b3c4c1c3d8f8f3662052ca36ed885
    Resolves: #83249
    Related: #84836
    Releases: master, 8.7
    Reviewed-on: https://review.typo3.org/54959
    Tested-by: TYPO3com <no-reply@typo3.com>
    Reviewed-by: Sybille Peters <sypets@gmx.de>
    Tested-by: Sybille Peters <sypets@gmx.de>
    Reviewed-by: Jigal van Hemert <jigal.van.hemert@typo3.org>
    Tested-by: Jigal van Hemert <jigal.van.hemert@typo3.org>
    Reviewed-by: Patricia Tiedemann <pixelaeffchen@googlemail.com>
    Tested-by: Patricia Tiedemann <pixelaeffchen@googlemail.com>
    Reviewed-by: Mathias Brodala <mbrodala@pagemachine.de>
    Tested-by: Mathias Brodala <mbrodala@pagemachine.de>

---
+++ Build/Scripts/composer/InstallerScripts.php
@@ -0,0 +1,67 @@
+<?php
+declare(strict_types = 1);
+namespace TYPO3\CMS\Composer\Scripts;
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2018 Helmut Hummel <info@helhum.io>
+ *  All rights reserved
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *  A copy is found in the text file GPL.txt and important notices to the license
+ *  from the author is found in LICENSE.txt distributed with these scripts.
+ *
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+use Composer\Script\Event;
+use Symfony\Component\Filesystem\Filesystem as SymfonyFilesystem;
+use TYPO3\CMS\Composer\Plugin\Util\Filesystem;
+
+class InstallerScripts
+{
+    public static function enableCommitMessageHook(Event $event)
+    {
+        $symfonyFilesystem = new SymfonyFilesystem();
+        $filesystem = new Filesystem();
+        try {
+            $filesystem->copy('Build/git-hooks/commit-msg', '.git/hooks/commit-msg');
+            if (!is_executable('.git/hooks/commit-msg')) {
+                $symfonyFilesystem->chmod('.git/hooks/commit-msg', 0755);
+            }
+        } catch (\Symfony\Component\Filesystem\Exception\IOException $e) {
+            $event->getIO()->writeError('<warning>Exception:enableCommitMessageHook:' . $e->getMessage() . '</warning>');
+        }
+    }
+
+    public static function enablePreCommitHook(Event $event)
+    {
+        if (DIRECTORY_SEPARATOR === '\\') {
+            return;
+        }
+        $symfonyFilesystem = new SymfonyFilesystem();
+        $filesystem = new Filesystem();
+        try {
+            $filesystem->copy('Build/git-hooks/unix+mac/pre-commit', '.git/hooks/pre-commit');
+            if (!is_executable('.git/hooks/pre-commit')) {
+                $symfonyFilesystem->chmod('.git/hooks/pre-commit', 0755);
+            }
+        } catch (\Symfony\Component\Filesystem\Exception\IOException $e) {
+            $event->getIO()->writeError('<warning>Exception:enablePreCommitHook:' . $e->getMessage() . '</warning>');
+        }
+    }
+
+    public static function disablePreCommitHook(Event $event)
+    {
+        $filesystem = new Filesystem();
+        $filesystem->remove('.git/hooks/pre-commit');
+    }
+}
