509037f62c53a4f0b9c9957bff523f93cf7d6ab9
Georg Ringer <georg.ringer@gmail.com>
2017-12-09 19:23:05 +0100

    [FOLLOWUP][BUGFIX] Use Fluid standalone abstract classes
    Resolves: #82414
    Releases: master
    Change-Id: I5152b9929d8755adfb8d2d77c03178178c8a8742
    Reviewed-on: https://review.typo3.org/55002
    Reviewed-by: Benni Mack <benni@typo3.org>
    Tested-by: TYPO3com <no-reply@typo3.com>
    Tested-by: Benni Mack <benni@typo3.org>
    Reviewed-by: Susanne Moog <susanne.moog@typo3.org>
    Tested-by: Susanne Moog <susanne.moog@typo3.org>

--- typo3/sysext/fluid/Classes/ViewHelpers/Form/AbstractFormViewHelper.php
+++ typo3/sysext/fluid/Classes/ViewHelpers/Form/AbstractFormViewHelper.php
@@ -14,13 +14,15 @@ namespace TYPO3\CMS\Fluid\ViewHelpers\Form;
  * The TYPO3 project - inspiring people to share!
  */
 
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractTagBasedViewHelper;
+
 /**
  * Abstract Form View Helper. Bundles functionality related to direct property access of objects in other Form ViewHelpers.
  *
  * If you set the "property" attribute to the name of the property to resolve from the object, this class will
  * automatically set the name and value of a form element.
  */
-abstract class AbstractFormViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractTagBasedViewHelper
+abstract class AbstractFormViewHelper extends AbstractTagBasedViewHelper
 {
     /**
      * @var \TYPO3\CMS\Extbase\Persistence\PersistenceManagerInterface
