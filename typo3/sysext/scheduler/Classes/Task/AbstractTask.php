8e6177874b1e3735e0dba56d0d32424f4a34b833
Georg Ringer <georg.ringer@gmail.com>
2018-01-28 12:42:17 +0100

    [BUGFIX] Properly check `serialized_executions` to avoid warnings with PHP 7.2
    To avoid warnings with PHP 7.2, `serialized_executions` must be checked
    for being not an empty value.
    
    Resolves: #83691
    Releases: master, 8.7, 7.6
    Change-Id: I5855a03d6669ec0e82931cfc0110c2b7ca134ad4
    Reviewed-on: https://review.typo3.org/55474
    Tested-by: TYPO3com <no-reply@typo3.com>
    Reviewed-by: Stefan Neufeind <typo3.neufeind@speedpartner.de>
    Reviewed-by: Oliver Klee <typo3-coding@oliverklee.de>
    Reviewed-by: Wouter Wolters <typo3@wouterwolters.nl>
    Tested-by: Stefan Neufeind <typo3.neufeind@speedpartner.de>
    Reviewed-by: Oliver Hader <oliver.hader@typo3.org>
    Tested-by: Oliver Hader <oliver.hader@typo3.org>

--- typo3/sysext/scheduler/Classes/Task/AbstractTask.php
+++ typo3/sysext/scheduler/Classes/Task/AbstractTask.php
@@ -430,7 +430,7 @@ abstract class AbstractTask implements LoggerAwareInterface
             ->fetch();
 
         $runningExecutions = [];
-        if ($row && $row['serialized_executions'] !== '') {
+        if ($row && !empty($row['serialized_executions'])) {
             $runningExecutions = unserialize($row['serialized_executions']);
         }
         // Count the number of existing executions and use that number as a key
