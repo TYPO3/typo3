665c0201c95ad46ce411b0a528d8be52119962cb
Christian Kuhn <lolli@schwarzbu.ch>
2018-02-04 14:45:48 +0100

    [TASK] Increase cache db backend blob
    Sets the typo3 database cache backend "content" field
    from mediumblob to longblob.
    Mediumblob on mysql equals 16MB max storage, which can
    be too small for cache entries. Switching to longblob
    increases single cache row size to 4GB and costs only
    one additional byte per row.
    
    Change-Id: I1ab5b84b6845280e77e339cbe4a59e0043f84cc2
    Resolves: #83772
    Releases: master
    Reviewed-on: https://review.typo3.org/55549
    Tested-by: TYPO3com <no-reply@typo3.com>
    Reviewed-by: Mathias Schreiber <mathias.schreiber@typo3.com>
    Tested-by: Mathias Schreiber <mathias.schreiber@typo3.com>
    Reviewed-by: Andreas Fernandez <a.fernandez@scripting-base.de>
    Tested-by: Andreas Fernandez <a.fernandez@scripting-base.de>

--- typo3/sysext/core/Resources/Private/Sql/Cache/Backend/Typo3DatabaseBackendCache.sql
+++ typo3/sysext/core/Resources/Private/Sql/Cache/Backend/Typo3DatabaseBackendCache.sql
@@ -2,7 +2,7 @@ CREATE TABLE ###CACHE_TABLE### (
 	id int(11) unsigned NOT NULL auto_increment,
 	identifier varchar(250) DEFAULT '' NOT NULL,
 	expires int(11) unsigned DEFAULT '0' NOT NULL,
-	content mediumblob,
+	content longblob,
 	PRIMARY KEY (id),
 	KEY cache_id (identifier,expires)
 ) ENGINE=InnoDB;
