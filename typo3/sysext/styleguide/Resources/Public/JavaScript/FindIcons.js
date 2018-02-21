501acfd8d55c6c0cd165a379ed908017e739ad2e
Christian Kuhn <lolli@schwarzbu.ch>
2018-02-21 11:29:40 +0100

    [BUGFIX] styleguide: Icon search javascript use $ instead of TYPO3.jquery

--- typo3/sysext/styleguide/Resources/Public/JavaScript/FindIcons.js
+++ typo3/sysext/styleguide/Resources/Public/JavaScript/FindIcons.js
@@ -20,30 +20,30 @@ define('TYPO3/CMS/Styleguide/FindIcons', ['jquery'], function($) {
 		$('#search-field').val($(this).data('filter')).trigger('keyup');
 	});
 	$('#search-field').keyup(function() {
-		var typedQuery = TYPO3.jQuery(this).val();
+		var typedQuery = $(this).val();
 		if (typedQuery === '') {
-			TYPO3.jQuery('#t3js-filter-container [data-icon-identifier]').show();
+			$('#t3js-filter-container [data-icon-identifier]').show();
 		} else {
 			if (typedQuery.indexOf('type:') !== -1) {
 				var parts = typedQuery.split(':');
 				var type = parts[1];
 				switch (type.toLowerCase()) {
 					case 'bitmap':
-						TYPO3.jQuery('#t3js-filter-container [data-icon-identifier]').hide();
-						TYPO3.jQuery('#t3js-filter-container img:not([src$=".svg"])').closest('[data-icon-identifier]').show();
+						$('#t3js-filter-container [data-icon-identifier]').hide();
+						$('#t3js-filter-container img:not([src$=".svg"])').closest('[data-icon-identifier]').show();
 						break;
 					case 'font':
-						TYPO3.jQuery('#t3js-filter-container [data-icon-identifier]').hide();
-						TYPO3.jQuery('#t3js-filter-container i.fa').closest('[data-icon-identifier]').show();
+						$('#t3js-filter-container [data-icon-identifier]').hide();
+						$('#t3js-filter-container i.fa').closest('[data-icon-identifier]').show();
 						break;
 					case 'vector':
-						TYPO3.jQuery('#t3js-filter-container [data-icon-identifier]').hide();
-						TYPO3.jQuery('#t3js-filter-container img[src$=".svg"]').closest('[data-icon-identifier]').show();
+						$('#t3js-filter-container [data-icon-identifier]').hide();
+						$('#t3js-filter-container img[src$=".svg"]').closest('[data-icon-identifier]').show();
 						break;
 				}
 			} else {
-				TYPO3.jQuery('#t3js-filter-container [data-icon-identifier]').hide();
-				TYPO3.jQuery('#t3js-filter-container [data-icon-identifier*="' + typedQuery + '"]').show();
+				$('#t3js-filter-container [data-icon-identifier]').hide();
+				$('#t3js-filter-container [data-icon-identifier*="' + typedQuery + '"]').show();
 			}
 		}
 	});
