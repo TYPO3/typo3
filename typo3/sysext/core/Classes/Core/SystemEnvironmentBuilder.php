cb0837e77794c2af1c60d442966bd74bc9893d78
Guido Schmechel <littlegee@web.de>
2018-02-24 09:35:34 +0100

    [TASK] Update readme copyright date to 2018
    Releases: master, 8.7, 7.6
    Resolves: #84028
    
    Change-Id: Ia6094c57c976ecf6e89b1d99aca0a1b12684e0b2
    Reviewed-on: https://review.typo3.org/55881
    Tested-by: TYPO3com <no-reply@typo3.com>
    Reviewed-by: Benni Mack <benni@typo3.org>
    Tested-by: Benni Mack <benni@typo3.org>
    Reviewed-by: Mathias Schreiber <mathias.schreiber@typo3.com>
    Tested-by: Mathias Schreiber <mathias.schreiber@typo3.com>
    Reviewed-by: Wouter Wolters <typo3@wouterwolters.nl>
    Tested-by: Wouter Wolters <typo3@wouterwolters.nl>

--- typo3/sysext/core/Classes/Core/SystemEnvironmentBuilder.php
+++ typo3/sysext/core/Classes/Core/SystemEnvironmentBuilder.php
@@ -85,7 +85,7 @@ class SystemEnvironmentBuilder
         // This version, branch and copyright
         define('TYPO3_version', '9.2.0-dev');
         define('TYPO3_branch', '9.2');
-        define('TYPO3_copyright_year', '1998-2017');
+        define('TYPO3_copyright_year', '1998-2018');
 
         // TYPO3 external links
         define('TYPO3_URL_GENERAL', 'https://typo3.org/');
