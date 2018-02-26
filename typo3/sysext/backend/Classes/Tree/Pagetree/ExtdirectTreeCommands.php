3c4c6090a8ebd827915b9641dbc2cfbb3ad1fac6
Tymoteusz Motylewski <t.motylewski@gmail.com>
2018-02-26 13:51:58 +0100

    [BUGFIX] Show error message when hiding page from context menu
    Renamed key in the response to match what is expected
    in actions.js evaluateResponse method.
    
    Releases: 8.7, 7.6
    Resolves: #82282
    Change-Id: I4528963aded3a8203ab144c8353fc0509aa8a1e7
    Reviewed-on: https://review.typo3.org/55905
    Tested-by: TYPO3com <no-reply@typo3.com>
    Reviewed-by: Frank Naegler <frank.naegler@typo3.org>
    Tested-by: Frank Naegler <frank.naegler@typo3.org>

--- typo3/sysext/backend/Classes/Tree/Pagetree/ExtdirectTreeCommands.php
+++ typo3/sysext/backend/Classes/Tree/Pagetree/ExtdirectTreeCommands.php
@@ -42,7 +42,7 @@ class ExtdirectTreeCommands
         } catch (\Exception $exception) {
             $returnValue = [
                 'success' => false,
-                'error' => $exception->getMessage()
+                'message' => $exception->getMessage()
             ];
         }
         return $returnValue;
