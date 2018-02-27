cc1b65891d2861f55ac2252a7d35a4af39dcfba5
Georg Ringer <georg.ringer@gmail.com>
2018-02-27 05:28:57 +0100

    [FEATURE] Add API to anonymize IP addresses
    Add an API to anonymize IP addresses.
    The core uses this API to anonymize all IP addresses before
    storing them. E.g. when logging.
    
    This a necessary tool in order to comply with data and privacy
    protections laws.
    
    Resolves: #84053
    Releases: master, 8.7
    Change-Id: Id45ee94696dee4fa2293e1226f2076883f6b9ade
    Reviewed-on: https://review.typo3.org/55912
    Tested-by: TYPO3com <no-reply@typo3.com>
    Reviewed-by: Mathias Schreiber <mathias.schreiber@typo3.com>
    Tested-by: Mathias Schreiber <mathias.schreiber@typo3.com>
    Reviewed-by: Frank Naegler <frank.naegler@typo3.org>
    Tested-by: Frank Naegler <frank.naegler@typo3.org>

---
+++ typo3/sysext/core/Classes/Utility/IpAnonymizationUtility.php
@@ -0,0 +1,87 @@
+<?php
+declare(strict_types = 1);
+namespace TYPO3\CMS\Core\Utility;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+/**
+ * Anonymize a given IP
+ *
+ * Inspired by https://github.com/geertw/php-ip-anonymizer
+ */
+class IpAnonymizationUtility
+{
+
+    /**
+     * IPv4 netmask used to anonymize IPv4 address.
+     *
+     * 1) Mask host
+     * 2) Mask host and subnet
+     *
+     * @var array
+     */
+    const MASKV4 = [
+        1 => '255.255.255.0',
+        2 => '255.255.0.0'
+    ];
+
+    /**
+     * IPv6 netmask used to anonymize IPv6 address.
+     *
+     * 1) Mask Interface ID
+     * 2) Mask Interface ID and SLA ID
+     *
+     * @var array
+     */
+    const MASKV6 = [
+        1 => 'ffff:ffff:ffff:ffff:0000:0000:0000:0000',
+        2 => 'ffff:ffff:ffff:0000:0000:0000:0000:0000',
+    ];
+
+    /**
+     * Anonymize given IP
+     *
+     * @param string $address IP address
+     * @param int $mask Allowed values are 0 (masking disabled), 1 (mask host), 2 (mask host and subnet)
+     * @return string
+     * @throws \UnexpectedValueException
+     */
+    public static function anonymizeIp(string $address, int $mask = null): string
+    {
+        if ($mask === null) {
+            $mask = (int)$GLOBALS['TYPO3_CONF_VARS']['SYS']['ipAnonymization'];
+        }
+        if ($mask < 0 || $mask > 2) {
+            throw new \UnexpectedValueException(sprintf('The provided value "%d" is not an allowed value for the IP mask.', $mask), 1519739203);
+        }
+        if ($mask === 0) {
+            return $address;
+        }
+        if (empty($address)) {
+            return '';
+        }
+
+        $packedAddress = inet_pton($address);
+        $length = strlen($packedAddress);
+
+        if ($length === 4) {
+            $bitMask = self::MASKV4[$mask];
+        } elseif ($length === 16) {
+            $bitMask = self::MASKV6[$mask];
+        } else {
+            return '';
+        }
+        return inet_ntop($packedAddress & inet_pton($bitMask));
+    }
+}
