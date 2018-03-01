80f602fdb10b9cb57d04aeffafd1dccd4a775058
Benjamin Franzke <bfr@qbus.de>
2018-03-01 11:45:24 +0100

    [TASK] Set COMPOSER_ROOT_VERSION to 9.2.0@dev for bamboo
    If typo3/testing-framework needs to be synced with changes
    in core it needs to require the dev branch of the upcoming
    release as the compatibility to previous releases needs to
    be dropped.
    Therefore it has to define a dependency in composer.json like:
      "typo3/cms-core": "^9.2@dev"
    
    If composer install is executed with COMPOSER_ROOT_VERSION
    set to 9.1.0 that produces a conflict, therefore we raise
    the version to 9.2.0@dev. That actually aligns the setting
    with the branch alias (9.2.x-dev) set in composer.json.
    
    Releases: master
    Resolves: #84102
    Change-Id: Ib9a6f1027cd738bcc1ecf00ef4e059ad179fc7c0
    Reviewed-on: https://review.typo3.org/55965
    Tested-by: TYPO3com <no-reply@typo3.com>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>

--- Build/bamboo/src/main/java/core/AbstractCoreSpec.java
+++ Build/bamboo/src/main/java/core/AbstractCoreSpec.java
@@ -45,7 +45,7 @@ abstract public class AbstractCoreSpec {
     protected static String projectName = "TYPO3 Core";
     protected static String projectKey = "CORE";
 
-    protected String composerRootVersionEnvironment = "COMPOSER_ROOT_VERSION=9.1.0";
+    protected String composerRootVersionEnvironment = "COMPOSER_ROOT_VERSION=9.2.0@dev";
 
     protected String testingFrameworkBuildPath = "vendor/typo3/testing-framework/Resources/Core/Build/";
 
