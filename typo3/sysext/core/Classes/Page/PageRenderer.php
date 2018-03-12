0162ff19abffb3f12797ff100d6425b84ea68529
Oliver Hader <oliver@typo3.org>
2018-03-12 13:10:42 +0100

    [BUGFIX] Handle config.absRefPrefix on JS/CSS files correctly again
    Issue #77589 introduced automatic resolving of relative paths during
    v8.4.0 development in the PageRenderer component. Doing this in some
    backend context is fine - however if applied for frontend rendering,
    config.absRefPrefix was not applied anymore to these files.
    
    Reason for this behavior is applying PathUtility::getAbsoluteWebPath
    which prefixes files with /<TYPO3_SITE_PATH>/ - however absRefPrefix
    only works on relative URIs. Extending the absRefPrefix patterns in
    TypoScriptFrontendController::setAbsRefPrefix() by mentioned prefix
    would adjust explicitly defined absolute paths as well.
    
    To solve this bug, the prefix /<TYPO3_SITE_PATH>/ is
    not applied in frontend rendering contexts anymore.
    
    Resolves: #83630
    Releases: master, 8.7
    Change-Id: Ib01f698c14fb646173ec24e26027c7d657700f87
    Reviewed-on: https://review.typo3.org/56108
    Tested-by: TYPO3com <no-reply@typo3.com>
    Reviewed-by: Susanne Moog <susanne.moog@typo3.org>
    Tested-by: Susanne Moog <susanne.moog@typo3.org>
    Reviewed-by: Markus Klein <markus.klein@typo3.org>
    Tested-by: Markus Klein <markus.klein@typo3.org>
    Reviewed-by: Stefan Neufeind <typo3.neufeind@speedpartner.de>
    Tested-by: Stefan Neufeind <typo3.neufeind@speedpartner.de>

--- typo3/sysext/core/Classes/Page/PageRenderer.php
+++ typo3/sysext/core/Classes/Page/PageRenderer.php
@@ -2482,7 +2482,7 @@ class PageRenderer implements \TYPO3\CMS\Core\SingletonInterface
         } elseif (TYPO3_MODE === 'FE') {
             $filename = GeneralUtility::createVersionNumberedFilename($filename);
         }
-        return PathUtility::getAbsoluteWebPath($filename);
+        return $this->getAbsoluteWebPath($filename);
     }
 
     /**
@@ -2509,9 +2509,26 @@ class PageRenderer implements \TYPO3\CMS\Core\SingletonInterface
         }
         if ($prepareForOutput) {
             $file = GeneralUtility::createVersionNumberedFilename($file);
-            $file = PathUtility::getAbsoluteWebPath($file);
+            $file = $this->getAbsoluteWebPath($file);
         }
         return $file;
+    }
+
+    /**
+     * Gets absolute web path of filename for backend disposal.
+     * Resolving the absolute path in the frontend with conflict with
+     * applying config.absRefPrefix in frontend rendering process.
+     *
+     * @param string $file
+     * @return string
+     * @see TypoScriptFrontendController::setAbsRefPrefix()
+     */
+    protected function getAbsoluteWebPath(string $file): string
+    {
+        if (TYPO3_MODE === 'FE') {
+            return $file;
+        }
+        return PathUtility::getAbsoluteWebPath($file);
     }
 
     /**
