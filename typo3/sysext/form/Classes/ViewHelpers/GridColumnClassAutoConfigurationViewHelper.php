6d443448ccb2f1673b83c246fb99a7aab3737e5c
Benjamin Kott <benjamin.kott@wfp2.com>
2018-03-12 10:23:15 +0100

    [BUGFIX] Correct automatic grid column calculation for forms
    The calculation now only includes direct children of the
    row elements to produce a meaningful division
    of the available space. If the available space is already
    consumed by elements the next auto calculated
    elements will now consume the space of a full row.
    
    Resolves: #84207
    Releases: master, 8.7
    Change-Id: I5dabddbcd8c7b84236fdbbef3925d27197e99a39
    Reviewed-on: https://review.typo3.org/56117
    Tested-by: TYPO3com <no-reply@typo3.com>
    Reviewed-by: Susanne Moog <susanne.moog@typo3.org>
    Tested-by: Susanne Moog <susanne.moog@typo3.org>

--- typo3/sysext/form/Classes/ViewHelpers/GridColumnClassAutoConfigurationViewHelper.php
+++ typo3/sysext/form/Classes/ViewHelpers/GridColumnClassAutoConfigurationViewHelper.php
@@ -61,7 +61,7 @@ class GridColumnClassAutoConfigurationViewHelper extends AbstractViewHelper
 
         $gridRowElement = $formElement->getParentRenderable();
         $gridContainerElement = $gridRowElement->getParentRenderable();
-        $gridRowEChildElements = $gridRowElement->getElementsRecursively();
+        $gridRowEChildElements = $gridRowElement->getElements();
 
         if ($gridContainerElement instanceof GridContainerInterface) {
             $gridViewPortConfiguration = $gridContainerElement->getProperties()['gridColumnClassAutoConfiguration'];
@@ -112,7 +112,7 @@ class GridColumnClassAutoConfigurationViewHelper extends AbstractViewHelper
                 $restElements = (int)$columnsToCalculate[$viewPortName]['elements'];
 
                 if ($restColumnsToDivide < 1) {
-                    $restColumnsToDivide = 1;
+                    $restColumnsToDivide = $gridSize;
                 }
                 if ($restElements < 1) {
                     $restElements = 1;
