ff2bb98e63d47f8af62534efcf671139d0b01bb2
Guido Schmechel <littlegee@web.de>
2018-03-13 21:22:03 +0100

    [BUGFIX] getTreeList must return comma-separated list
    While retrieving the list with begin as argument for a
    recursive call, getTreeList builds the comma-separated list
    correctly.
    
    Resolves: #83328
    Releases: master, 8.7, 7.6
    Change-Id: I643477dd5c0cbfb67532dd96eb0b479e0cb532b8
    Reviewed-on: https://review.typo3.org/57026
    Tested-by: TYPO3com <no-reply@typo3.com>
    Reviewed-by: Markus Klein <markus.klein@typo3.org>
    Tested-by: Markus Klein <markus.klein@typo3.org>

--- typo3/sysext/core/Classes/Database/QueryGenerator.php
+++ typo3/sysext/core/Classes/Database/QueryGenerator.php
@@ -1560,7 +1560,11 @@ class QueryGenerator
                     $theList .= ',' . $row['uid'];
                 }
                 if ($depth > 1) {
-                    $theList .= $this->getTreeList($row['uid'], $depth - 1, $begin - 1, $perms_clause);
+                    $theSubList = $this->getTreeList($row['uid'], $depth - 1, $begin - 1, $perms_clause);
+                    if (!empty($theList) && !empty($theSubList) && ($theSubList[0] !== ',')) {
+                        $theList .= ',';
+                    }
+                    $theList .= $theSubList;
                 }
             }
             $databaseConnection->sql_free_result($res);
