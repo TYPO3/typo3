c908cc58b94e2403b33fd88ece8f882b574b6fb6
Frank Naegler <frank.naegler@typo3.org>
2018-04-05 14:57:23 +0200

    [TASK] Streamline HTTP Response codes
    This patch streamlines all lists of HTTP response codes.
    
    Resolves: #84626
    Related: #84490
    Related: #84481
    Releases: master, 8.7
    Change-Id: Id7d3675e5f0f07a9c8920c40f70ddd194db994d0
    Reviewed-on: https://review.typo3.org/56562
    Reviewed-by: Stefan Neufeind <typo3.neufeind@speedpartner.de>
    Tested-by: Stefan Neufeind <typo3.neufeind@speedpartner.de>
    Reviewed-by: Mathias Brodala <mbrodala@pagemachine.de>
    Tested-by: Mathias Brodala <mbrodala@pagemachine.de>
    Reviewed-by: Andreas Fernandez <a.fernandez@scripting-base.de>
    Tested-by: Andreas Fernandez <a.fernandez@scripting-base.de>

--- typo3/sysext/extbase/Classes/Mvc/Web/Response.php
+++ typo3/sysext/extbase/Classes/Mvc/Web/Response.php
@@ -66,10 +66,12 @@ class Response extends \TYPO3\CMS\Extbase\Mvc\Response
      * @var array
      */
     protected $statusMessages = [
+        // INFORMATIONAL CODES
         100 => 'Continue',
         101 => 'Switching Protocols',
         102 => 'Processing',
-        // RFC 2518
+        103 => 'Early Hints',
+        // SUCCESS CODES
         200 => 'OK',
         201 => 'Created',
         202 => 'Accepted',
@@ -77,14 +79,20 @@ class Response extends \TYPO3\CMS\Extbase\Mvc\Response
         204 => 'No Content',
         205 => 'Reset Content',
         206 => 'Partial Content',
-        207 => 'Multi-Status',
+        207 => 'Multi-status',
+        208 => 'Already Reported',
+        226 => 'IM Used',
+        // REDIRECTION CODES
         300 => 'Multiple Choices',
         301 => 'Moved Permanently',
         302 => 'Found',
         303 => 'See Other',
         304 => 'Not Modified',
         305 => 'Use Proxy',
+        306 => 'Switch Proxy', // Deprecated
         307 => 'Temporary Redirect',
+        308 => 'Permanent Redirect',
+        // CLIENT ERROR
         400 => 'Bad Request',
         401 => 'Unauthorized',
         402 => 'Payment Required',
@@ -99,18 +107,32 @@ class Response extends \TYPO3\CMS\Extbase\Mvc\Response
         411 => 'Length Required',
         412 => 'Precondition Failed',
         413 => 'Request Entity Too Large',
-        414 => 'Request-URI Too Long',
+        414 => 'URI Too Long',
         415 => 'Unsupported Media Type',
-        416 => 'Requested Range Not Satisfiable',
+        416 => 'Requested range not satisfiable',
         417 => 'Expectation Failed',
+        418 => 'I\'m a teapot',
+        422 => 'Unprocessable Entity',
+        423 => 'Locked',
+        424 => 'Failed Dependency',
+        425 => 'Unordered Collection',
+        426 => 'Upgrade Required',
+        428 => 'Precondition Required',
+        429 => 'Too Many Requests',
+        431 => 'Request Header Fields Too Large',
+        451 => 'Unavailable For Legal Reasons',
+        // SERVER ERROR
         500 => 'Internal Server Error',
         501 => 'Not Implemented',
         502 => 'Bad Gateway',
         503 => 'Service Unavailable',
-        504 => 'Gateway Timeout',
-        505 => 'HTTP Version Not Supported',
+        504 => 'Gateway Time-out',
+        505 => 'HTTP Version not supported',
+        506 => 'Variant Also Negotiates',
         507 => 'Insufficient Storage',
-        509 => 'Bandwidth Limit Exceeded'
+        508 => 'Loop Detected',
+        509 => 'Bandwidth Limit Exceeded',
+        511 => 'Network Authentication Required',
     ];
 
     /**
