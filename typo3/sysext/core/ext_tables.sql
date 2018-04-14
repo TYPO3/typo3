54b081edb7ddfc32415efa0135e30d8f478e7954
Guido Schmechel <guido.schmechel@brandung.de>
2018-04-14 19:59:20 +0200

    [TASK] Increase title length of filemount
    Increase length of sys_filemount.title from 30 to 255 characters.
    
    Resolves: #50979
    Releases: master, 8.7
    Change-Id: I37fc5a7877073bdfa485adb3d25dc8f2bd9b76ff
    Reviewed-on: https://review.typo3.org/56661
    Tested-by: TYPO3com <no-reply@typo3.com>
    Tested-by: Riccardo De Contardi <erredeco@gmail.com>
    Reviewed-by: Frans Saris <franssaris@gmail.com>
    Tested-by: Frans Saris <franssaris@gmail.com>
    Reviewed-by: Wouter Wolters <typo3@wouterwolters.nl>
    Tested-by: Wouter Wolters <typo3@wouterwolters.nl>

--- typo3/sysext/core/ext_tables.sql
+++ typo3/sysext/core/ext_tables.sql
@@ -224,7 +224,7 @@ CREATE TABLE sys_filemounts (
 	uid int(11) unsigned NOT NULL auto_increment,
 	pid int(11) unsigned DEFAULT '0' NOT NULL,
 	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
-	title varchar(30) DEFAULT '' NOT NULL,
+	title varchar(255) DEFAULT '' NOT NULL,
 	description varchar(2000) DEFAULT '' NOT NULL,
 	path varchar(255) DEFAULT '' NOT NULL,
 	base int(11) unsigned DEFAULT '0' NOT NULL,
