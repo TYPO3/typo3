c41128b0516b33f42272d9be834f88bcaf9c7a1c
Oliver Hader <oliver@typo3.org>
2018-05-22 14:20:16 +0200

    [TASK] Set TYPO3 version to 9.2.1-dev
    Change-Id: I3a5b194210e25a056909c79f5048d2e03c97f5c2
    Reviewed-on: https://review.typo3.org/57031
    Reviewed-by: Oliver Hader <oliver.hader@typo3.org>
    Tested-by: Oliver Hader <oliver.hader@typo3.org>

--- typo3/sysext/rsaauth/ext_emconf.php
+++ typo3/sysext/rsaauth/ext_emconf.php
@@ -10,10 +10,10 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => 0,
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '9.2.0',
+    'version' => '9.2.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.2.0',
+            'typo3' => '9.2.1',
         ],
         'conflicts' => [],
         'suggests' => [],
