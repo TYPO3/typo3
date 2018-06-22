09b94a99621056dac233c41d2bc437efb296badf
Andreas Wolf <dev@a-w.io>
2018-06-23 00:13:48 +0200

    [TASK] Add test for (wrong) unique eval handling in pages:alias
    Change-Id: Ie47dbc9153d8a4dcbde15a7eb61c207bbcc34480
    Resolves: #86780
    Related: #84267
    Related: #83572
    Releases: master, 8.7
    Reviewed-on: https://review.typo3.org/57322
    Tested-by: TYPO3com <no-reply@typo3.com>
    Reviewed-by: Susanne Moog <susanne.moog@typo3.org>
    Tested-by: Susanne Moog <susanne.moog@typo3.org>
    Reviewed-by: Benni Mack <benni@typo3.org>
    Tested-by: Benni Mack <benni@typo3.org>

---
+++ typo3/sysext/core/Tests/Functional/DataHandling/DataHandler/GetUniqueTranslationTest.php
@@ -0,0 +1,42 @@
+<?php
+declare(strict_types = 1);
+
+namespace TYPO3\CMS\Core\Tests\Functional\DataHandling\DataHandler;
+
+use TYPO3\CMS\Backend\Utility\BackendUtility;
+use TYPO3\CMS\Core\Tests\Functional\DataHandling\AbstractDataHandlerActionTestCase;
+
+class GetUniqueTranslationTest extends AbstractDataHandlerActionTestCase
+{
+    /**
+     * @var int
+     */
+    const PAGE_DATAHANDLER = 88;
+
+    /**
+     * @var string
+     */
+    protected $scenarioDataSetDirectory = 'typo3/sysext/core/Tests/Functional/DataHandling/DataHandler/DataSet/';
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->importScenarioDataSet('LiveDefaultPages');
+        $this->importScenarioDataSet('LiveDefaultElements');
+        $this->backendUser->workspace = 0;
+    }
+
+    /**
+     * @test
+     */
+    public function valueOfUniqueFieldExcludedInTranslationIsUntouchedInTranslation(): void
+    {
+        $map = $this->actionService->localizeRecord('pages', self::PAGE_DATAHANDLER, 1);
+        $newPageId = $map['pages'][self::PAGE_DATAHANDLER];
+        $originalLanguageRecord = BackendUtility::getRecord('pages', self::PAGE_DATAHANDLER);
+        $translatedRecord = BackendUtility::getRecord('pages', $newPageId);
+
+        $this->assertEquals('datahandler', $originalLanguageRecord['alias']);
+        $this->assertEquals('datahandler0', $translatedRecord['alias']);
+    }
+}
