df66054f0d44877bf36d33beeae5287e34866bde
Christian Weiske <weiske@mogic.com>
2018-06-26 21:46:25 +0200

    [BUGFIX] Use live record uid for preview in workspace
    When editing a record in a workspace in the Backend,
    the "preview" button needs to use the live record uid
    and not the uid of the workspace version.
    
    Change-Id: I690c11e50b6275290175397cc9f28e1532b1d61d
    Releases: master, 9.5
    Resolves: #81271
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/60333
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Oliver Hader <oliver.hader@typo3.org>
    Reviewed-by: Oliver Hader <oliver.hader@typo3.org>

--- typo3/sysext/backend/Classes/Controller/EditDocumentController.php
+++ typo3/sysext/backend/Classes/Controller/EditDocumentController.php
@@ -1028,6 +1028,11 @@ class EditDocumentController
             }
         }
 
+        // Always use live workspace record uid for the preview
+        if ($GLOBALS['TCA'][$table]['ctrl']['versioningWS'] ?? false && $recordArray['t3ver_oid'] > 0) {
+            $recordId = $recordArray['t3ver_oid'];
+        }
+
         // map record data to GET parameters
         if (isset($previewConfiguration['fieldToParameterMap.'])) {
             foreach ($previewConfiguration['fieldToParameterMap.'] as $field => $parameterName) {
