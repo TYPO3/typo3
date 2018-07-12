530ae9649905e0369dd0599d62ede6c73266279b
Michael Giek <michael.giek@b13.de>
2018-07-12 12:14:18 +0200

    [BUGFIX] Set the redirect hook before the page-resolver
    The source page redirects now also with get-parameters to the
    target page.
    
    Resolves: #85538
    Related: #85488
    Releases: master
    Change-Id: Ibc1ae6afdbe11065156201e8118838203e427cf3
    Reviewed-on: https://review.typo3.org/57569
    Tested-by: TYPO3com <no-reply@typo3.com>
    Reviewed-by: Benni Mack <benni@typo3.org>
    Tested-by: Benni Mack <benni@typo3.org>
    Reviewed-by: Frank Naegler <frank.naegler@typo3.org>
    Tested-by: Frank Naegler <frank.naegler@typo3.org>

--- typo3/sysext/redirects/Configuration/RequestMiddlewares.php
+++ typo3/sysext/redirects/Configuration/RequestMiddlewares.php
@@ -8,7 +8,7 @@ return [
         'typo3/cms-redirects/redirecthandler' => [
             'target' => \TYPO3\CMS\Redirects\Http\Middleware\RedirectHandler::class,
             'before' => [
-                'typo3/cms-frontend/prepare-tsfe-rendering',
+                'typo3/cms-frontend/page-resolver',
             ]
         ],
     ],
