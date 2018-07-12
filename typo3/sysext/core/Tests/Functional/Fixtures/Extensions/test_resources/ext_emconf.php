06256af9f2b1ef49fa454c70412ce79de327be97
Oliver Hader <oliver@typo3.org>
2018-07-12 13:29:13 +0200

    [RELEASE] Release of TYPO3 8.7.17
    Change-Id: I21f8e42feb8a9c5e1b72b36c906c25411b30472e
    Reviewed-on: https://review.typo3.org/57575
    Reviewed-by: Oliver Hader <oliver.hader@typo3.org>
    Tested-by: Oliver Hader <oliver.hader@typo3.org>

--- typo3/sysext/core/Tests/Functional/Fixtures/Extensions/test_resources/ext_emconf.php
+++ typo3/sysext/core/Tests/Functional/Fixtures/Extensions/test_resources/ext_emconf.php
@@ -3,7 +3,7 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'Test Resources',
     'description' => 'Test Resources',
     'category' => 'example',
-    'version' => '9.4.0',
+    'version' => '8.7.17',
     'state' => 'beta',
     'uploadfolder' => 0,
     'createDirs' => '',
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => '',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.4.0'
+            'typo3' => '8.7.17'
         ],
         'conflicts' => [],
         'suggests' => [],
