c590a4de2d5680930821a8270164c5d3823291e5
Susanne Moog <susanne.moog@typo3.org>
2018-08-01 21:14:57 +0200

    [BUGFIX] Fix fluid error in admin panel
    Resolves: #85702
    Releases: master
    Change-Id: I6dc4dabdf565b14fd4271c476881fbd8d06d9a77
    Reviewed-on: https://review.typo3.org/57762
    Tested-by: TYPO3com <no-reply@typo3.com>
    Reviewed-by: Susanne Moog <susanne.moog@typo3.org>
    Reviewed-by: Benni Mack <benni@typo3.org>
    Tested-by: Benni Mack <benni@typo3.org>
    Reviewed-by: Markus Klein <markus.klein@typo3.org>
    Tested-by: Markus Klein <markus.klein@typo3.org>

---
+++ typo3/sysext/adminpanel/Classes/ViewHelpers/IsArrayViewHelper.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types = 1);
+
+namespace TYPO3\CMS\Adminpanel\ViewHelpers;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+
+/**
+ * Checks whether the given value is an array
+ */
+class IsArrayViewHelper extends AbstractViewHelper
+{
+    public function initializeArguments()
+    {
+        $this->registerArgument('value', 'mixed', 'The variable being checked', true);
+    }
+
+    /**
+     * @param array $arguments
+     * @param \Closure $renderChildrenClosure
+     * @param RenderingContextInterface $renderingContext
+     * @return bool
+     */
+    public static function renderStatic(
+        array $arguments,
+        \Closure $renderChildrenClosure,
+        RenderingContextInterface $renderingContext
+    ): bool {
+        return is_array($arguments['value']);
+    }
+}
