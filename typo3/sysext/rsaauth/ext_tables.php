e1476c5ee2135ea093928d8c21be2c4ce75ea47f
Benni Mack <benni@typo3.org>
2018-08-20 08:46:04 +0200

    [BUGFIX] Remove check for TYPO3_MODE in ext_tables
    With TYPO3 v9.0, all simple TYPO3_MODE checks of all system extensions
    were removed, however EXT:rsaauth was somehow forgotten.
    
    Resolves: #85904
    Releases: master
    Change-Id: If76b25813d323e0604a0a48596f2cd3d113f833a
    Reviewed-on: https://review.typo3.org/57963
    Tested-by: TYPO3com <no-reply@typo3.com>
    Reviewed-by: Björn Jacob <bjoern.jacob@tritum.de>
    Tested-by: Björn Jacob <bjoern.jacob@tritum.de>
    Reviewed-by: Wouter Wolters <typo3@wouterwolters.nl>
    Tested-by: Wouter Wolters <typo3@wouterwolters.nl>

--- typo3/sysext/rsaauth/ext_tables.php
+++ typo3/sysext/rsaauth/ext_tables.php
@@ -1,6 +1,4 @@
 <?php
 defined('TYPO3_MODE') or die();
 
-if (TYPO3_MODE === 'BE') {
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['reports']['tx_reports']['status']['providers']['security'][] = \TYPO3\CMS\Rsaauth\Report\RsauthStatus::class;
-}
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['reports']['tx_reports']['status']['providers']['security'][] = \TYPO3\CMS\Rsaauth\Report\RsauthStatus::class;
