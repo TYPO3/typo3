8fd7b9c963511a65f24c72c14b7a286f5168a07d
Gerald Aistleitner <g.aistleitner@siwa.at>
2018-08-23 08:28:58 +0200

    [BUGFIX] UnableToLinkException::getLinkText() must be of the type string, null returned
    Changed the default value of linkText to '' instead of null to prevent strict_type throwing an error.
    
    Resolves: #85933
    Releases: master, 8.7
    Change-Id: I0a7567cd784360654649c11e686784ad3867858d
    Reviewed-on: https://review.typo3.org/57995
    Tested-by: TYPO3com <no-reply@typo3.com>
    Reviewed-by: Joerg Kummer <typo3@enobe.de>
    Tested-by: Joerg Kummer <typo3@enobe.de>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>

--- typo3/sysext/frontend/Classes/Typolink/UnableToLinkException.php
+++ typo3/sysext/frontend/Classes/Typolink/UnableToLinkException.php
@@ -33,9 +33,9 @@ class UnableToLinkException extends Exception
      * @param string $message [optional] The Exception message to throw.
      * @param int $code [optional] The Exception code.
      * @param \Throwable $previous [optional] The previous throwable used for the exception chaining.
-     * @param string $linkText
+     * @param string $linkText [optional]
      */
-    public function __construct($message = '', $code = 0, \Throwable $previous = null, $linkText = null)
+    public function __construct($message = '', $code = 0, \Throwable $previous = null, $linkText = '')
     {
         parent::__construct($message, $code, $previous);
         $this->linkText = $linkText;
