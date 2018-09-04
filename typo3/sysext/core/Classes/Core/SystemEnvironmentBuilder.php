8eb9df8c69d44b1842485b7de53fe5ea62081955
Oliver Hader <oliver@typo3.org>
2018-09-04 14:07:49 +0200

    [RELEASE] Release of TYPO3 9.4.0
    Change-Id: I10cf279d74af2cfc2964704ba31c6ce2b7be3493
    Reviewed-on: https://review.typo3.org/58180
    Reviewed-by: Oliver Hader <oliver.hader@typo3.org>
    Tested-by: Oliver Hader <oliver.hader@typo3.org>

--- typo3/sysext/core/Classes/Core/SystemEnvironmentBuilder.php
+++ typo3/sysext/core/Classes/Core/SystemEnvironmentBuilder.php
@@ -115,7 +115,7 @@ class SystemEnvironmentBuilder
         }
 
         // This version, branch and copyright
-        define('TYPO3_version', '9.4.0-dev');
+        define('TYPO3_version', '9.4.0');
         define('TYPO3_branch', '9.4');
         define('TYPO3_copyright_year', '1998-2018');
 
