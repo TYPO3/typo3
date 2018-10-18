0e92db5c695c2a028873825011dd8f150789d618
Andreas Fernandez <a.fernandez@scripting-base.de>
2018-10-18 15:27:42 +0200

    [TASK] Move FieldControl/ListModule handling to module
    The FieldControl `ListModule` now uses a dedicated module for its
    handling and no longer uses inline `onclick` handlers nor inline
    FormEngine calls.
    
    Resolves: #86647
    Releases: master
    Change-Id: Ibf47ee8ebaea584f88ccb9615681bd579e4cd305
    Reviewed-on: https://review.typo3.org/58649
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Reviewed-by: Anja Leichsenring <aleichsenring@ab-softlab.de>
    Tested-by: Anja Leichsenring <aleichsenring@ab-softlab.de>
    Reviewed-by: Wouter Wolters <typo3@wouterwolters.nl>
    Tested-by: Wouter Wolters <typo3@wouterwolters.nl>

--- typo3/sysext/backend/Classes/Form/FieldControl/ListModule.php
+++ typo3/sysext/backend/Classes/Form/FieldControl/ListModule.php
@@ -17,6 +17,7 @@ namespace TYPO3\CMS\Backend\Form\FieldControl;
 
 use TYPO3\CMS\Backend\Form\AbstractNode;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Utility\StringUtility;
 
 /**
  * Renders the icon with link parameters to jump to the list module
@@ -82,9 +83,8 @@ class ListModule extends AbstractNode
             ],
         ];
 
-        $onClick = [];
-        $onClick[] = 'this.blur();';
-        $onClick[] = 'return TYPO3.FormEngine.preventFollowLinkIfNotSaved(this.getAttribute(\'href\'));';
+        $id = StringUtility::getUniqueId('t3js-formengine-fieldcontrol-');
+
         /** @var \TYPO3\CMS\Backend\Routing\UriBuilder $uriBuilder */
         $uriBuilder = GeneralUtility::makeInstance(\TYPO3\CMS\Backend\Routing\UriBuilder::class);
 
@@ -92,8 +92,11 @@ class ListModule extends AbstractNode
             'iconIdentifier' => 'actions-system-list-open',
             'title' => $title,
             'linkAttributes' => [
-                'onClick' => implode('', $onClick),
+                'id' => htmlspecialchars($id),
                 'href' => (string)$uriBuilder->buildUriFromRoute('wizard_list', $urlParameters),
+            ],
+            'requireJsModules' => [
+                ['TYPO3/CMS/Backend/FormEngine/FieldControl/ListModule' => 'function(FieldControl) {new FieldControl(' . GeneralUtility::quoteJSvalue('#' . $id) . ');}'],
             ],
         ];
     }
