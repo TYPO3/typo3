bf961f42e745c10243083f2a045a98271d0de8c9
Georg Ringer <georg.ringer@gmail.com>
2018-10-24 09:46:43 +0200

    [TASK] Allow rendering of description for InputSlugElement
    The description must be available for the InputSlugElement as well to give
    editors an additional guidance.
    
    Resolves: #86720
    Releases: master
    Change-Id: Iaa70a6e62209eb72cbe51be91727c8bed775fac6
    Reviewed-on: https://review.typo3.org/58681
    Reviewed-by: Riny van Tiggelen <info@online-gamer.nl>
    Reviewed-by: Mathias Brodala <mbrodala@pagemachine.de>
    Tested-by: TYPO3com <no-reply@typo3.com>
    Reviewed-by: Daniel Goerz <daniel.goerz@posteo.de>
    Reviewed-by: Andreas Fernandez <a.fernandez@scripting-base.de>
    Tested-by: Andreas Fernandez <a.fernandez@scripting-base.de>
    Tested-by: Joerg Kummer <typo3@enobe.de>
    Reviewed-by: Jürgen Heym <juergen.heym@hof-university.de>
    Tested-by: Jürgen Heym <juergen.heym@hof-university.de>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Reviewed-by: Benni Mack <benni@typo3.org>
    Tested-by: Benni Mack <benni@typo3.org>

--- typo3/sysext/backend/Classes/Form/Element/InputSlugElement.php
+++ typo3/sysext/backend/Classes/Form/Element/InputSlugElement.php
@@ -28,6 +28,18 @@ use TYPO3\CMS\Core\Utility\StringUtility;
  */
 class InputSlugElement extends AbstractFormElement
 {
+
+    /**
+     * Default field information enabled for this element.
+     *
+     * @var array
+     */
+    protected $defaultFieldInformation = [
+        'tcaDescription' => [
+            'renderType' => 'tcaDescription',
+        ],
+    ];
+
     /**
      * Default field wizards enabled for this element.
      *
