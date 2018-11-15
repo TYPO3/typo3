d7dc71685b33a31a95f94f0bd9024066619c15ce
Benni Mack <benni@typo3.org>
2018-11-15 11:28:11 +0100

    [BUGFIX] Select proper records in DBAL workspace restrictions
    A new WorkspaceRestriction is added to solve all issues
    once and for all.
    
    For now, this restriction is only added in v8.7 but not in use yet.
    
    Resolves: #84985
    Releases: master, 8.7
    Change-Id: I22d5f276460107802bef3d390e6781434f1c28d3
    Reviewed-on: https://review.typo3.org/58902
    Tested-by: TYPO3com <no-reply@typo3.com>
    Reviewed-by: Benni Mack <benni@typo3.org>
    Tested-by: Benni Mack <benni@typo3.org>

---
+++ typo3/sysext/core/Tests/Unit/Database/Query/Restriction/WorkspaceRestrictionTest.php
@@ -0,0 +1,73 @@
+<?php
+declare(strict_types = 1);
+namespace TYPO3\CMS\Core\Tests\Unit\Database\Query\Restriction;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use TYPO3\CMS\Core\Database\Query\Restriction\WorkspaceRestriction;
+
+class WorkspaceRestrictionTest extends AbstractRestrictionTestCase
+{
+    /**
+     * @test
+     */
+    public function buildExpressionAddsLiveWorkspaceWhereClause()
+    {
+        $GLOBALS['TCA']['aTable']['ctrl'] = [
+            'versioningWS' => true,
+        ];
+        $subject = new WorkspaceRestriction(0);
+        $expression = $subject->buildExpression(['aTable' => 'aTable'], $this->expressionBuilder);
+        $this->assertSame('("aTable"."t3ver_wsid" = 0) AND ("aTable"."pid" <> -1)', (string)$expression);
+    }
+
+    /**
+     * @test
+     */
+    public function buildExpressionAddsNonLiveWorkspaceWhereClause()
+    {
+        $GLOBALS['TCA']['aTable']['ctrl'] = [
+            'versioningWS' => true,
+        ];
+        $subject = new WorkspaceRestriction(42);
+        $expression = $subject->buildExpression(['aTable' => 'aTable'], $this->expressionBuilder);
+        $this->assertSame('("aTable"."t3ver_wsid" IN (0, 42)) AND ("aTable"."pid" <> -1)', (string)$expression);
+    }
+
+    /**
+     * @test
+     */
+    public function buildExpressionAddsLiveWorkspaceLimitedWhereClause()
+    {
+        $GLOBALS['TCA']['aTable']['ctrl'] = [
+            'versioningWS' => false,
+        ];
+        $subject = new WorkspaceRestriction(0);
+        $expression = $subject->buildExpression(['aTable' => 'aTable'], $this->expressionBuilder);
+        $this->assertSame('', (string)$expression);
+    }
+
+    /**
+     * @test
+     */
+    public function buildExpressionAddsNonLiveWorkspaceLimitedWhereClause()
+    {
+        $GLOBALS['TCA']['aTable']['ctrl'] = [
+            'versioningWS' => false,
+        ];
+        $subject = new WorkspaceRestriction(42);
+        $expression = $subject->buildExpression(['aTable' => 'aTable'], $this->expressionBuilder);
+        $this->assertSame('', (string)$expression);
+    }
+}
