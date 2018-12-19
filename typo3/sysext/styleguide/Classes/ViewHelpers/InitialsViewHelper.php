881c9527f0a982f97577ece26f27ac007c8afa01
Christian Kuhn <lolli@schwarzbu.ch>
2018-12-19 21:06:53 +0100

    [BUGFIX] styleguide: Use of old fluid abstract view helper class

--- typo3/sysext/styleguide/Classes/ViewHelpers/InitialsViewHelper.php
+++ typo3/sysext/styleguide/Classes/ViewHelpers/InitialsViewHelper.php
@@ -14,7 +14,7 @@ namespace TYPO3\CMS\Styleguide\ViewHelpers;
  *
  * The TYPO3 project - inspiring people to share!
  */
-use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
 
 /**
  * Class CodeViewHelper
