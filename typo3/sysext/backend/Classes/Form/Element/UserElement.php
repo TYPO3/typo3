f564137c36834f59aae183c23716239e2d771978
Christian Kuhn <lolli@schwarzbu.ch>
2019-01-16 20:26:40 +0100

    [!!!][TASK] Remove deprecated code from UserElement
    Resolves: #87464
    Releases: master
    Change-Id: I20fac10c82f22c3e2dec0ed02e5f2a769866c966
    Reviewed-on: https://review.typo3.org/59461
    Tested-by: TYPO3com <noreply@typo3.com>
    Reviewed-by: Georg Ringer <georg.ringer@gmail.com>
    Tested-by: Georg Ringer <georg.ringer@gmail.com>
    Reviewed-by: Benni Mack <benni@typo3.org>
    Tested-by: Benni Mack <benni@typo3.org>

--- typo3/sysext/backend/Classes/Form/Element/UserElement.php
+++ typo3/sysext/backend/Classes/Form/Element/UserElement.php
@@ -15,8 +15,6 @@ namespace TYPO3\CMS\Backend\Form\Element;
  * The TYPO3 project - inspiring people to share!
  */
 
-use TYPO3\CMS\Core\Utility\GeneralUtility;
-
 /**
  * Generation of elements of the type "user". This is a dummy implementation.
  *
@@ -33,42 +31,14 @@ class UserElement extends AbstractFormElement
      */
     public function render()
     {
-        $parameterArray = $this->data['parameterArray'];
+        // Render some dummy output to explain this element should usually not be called at all.
         $resultArray = $this->initializeResultArray();
-
-        if (empty($parameterArray['fieldConf']['config']['userFunc'])) {
-            // If there is no (deprecated) userFunc, render some dummy output to explain this element
-            // should usually not be called at all.
-            // @deprecated The if can be removed in TYPO3 v10.0, keeping the body only.
-            $resultArray['html'] = '<div class="alert alert-warning">';
-            $resultArray['html'] .= 'This is dummy output: Field <code>' . htmlspecialchars($this->data['fieldName']) . '</code>';
-            $resultArray['html'] .= 'of table <code>' . htmlspecialchars($this->data['tableName']) . '</code>';
-            $resultArray['html'] .= ' is registered as type="user" element without a specific renderType.';
-            $resultArray['html'] .= ' Please look up details in TCA reference documentation for type="user".';
-            $resultArray['html'] .= '</div>';
-            return $resultArray;
-        }
-
-        // @deprecated since TYPO3 v9, everything below will be removed in TYPO3 v10.0.
-        trigger_error(
-            'Properties "userFunc", "noTableWrapping" and "parameters" will be removed in TYPO3 v10.0. Use a renderType instead.',
-            E_USER_DEPRECATED
-        );
-        $parameterArray['table'] = $this->data['tableName'];
-        $parameterArray['field'] = $this->data['fieldName'];
-        $parameterArray['row'] = $this->data['databaseRow'];
-        $parameterArray['parameters'] = $parameterArray['fieldConf']['config']['parameters'] ?? [];
-        $html = GeneralUtility::callUserFunction(
-            $parameterArray['fieldConf']['config']['userFunc'],
-            $parameterArray,
-            $this
-        );
-        if (!isset($parameterArray['fieldConf']['config']['noTableWrapping'])
-            || (bool)$parameterArray['fieldConf']['config']['noTableWrapping'] === false
-        ) {
-            $html = '<div class="formengine-field-item t3js-formengine-field-item">' . $html . '</div>';
-        }
-        $resultArray['html'] = $html;
+        $resultArray['html'] = '<div class="alert alert-warning">';
+        $resultArray['html'] .= 'This is dummy output: Field <code>' . htmlspecialchars($this->data['fieldName']) . '</code>';
+        $resultArray['html'] .= 'of table <code>' . htmlspecialchars($this->data['tableName']) . '</code>';
+        $resultArray['html'] .= ' is registered as type="user" element without a specific renderType.';
+        $resultArray['html'] .= ' Please look up details in TCA reference documentation for type="user".';
+        $resultArray['html'] .= '</div>';
         return $resultArray;
     }
 }
