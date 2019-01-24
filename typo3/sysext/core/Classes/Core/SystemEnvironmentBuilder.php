511dbfec39cdf940330a9fd62673b773d8c1c521
Josef Glatz <josefglatz@gmail.com>
2019-01-24 14:15:17 +0100

    [TASK] Reduce information disclosure of the used TYPO3 version
    Reduce information disclosure of the used TYPO3 version by adding
    a dynamic 4-digit year in the TYPO3_copyright_year constant.
    
    That makes it more difficult to find out the used TYPO3 version
    just by looking at the frontend source code of a website.
    
    Releases: master, 9.5
    Resolves: #87543
    Change-Id: Ic169548c7111715eecedfce4ced5c8a3564d102a
    Reviewed-on: https://review.typo3.org/59565
    Tested-by: TYPO3com <noreply@typo3.com>
    Reviewed-by: Georg Ringer <georg.ringer@gmail.com>
    Tested-by: Georg Ringer <georg.ringer@gmail.com>
    Reviewed-by: Mathias Brodala <mbrodala@pagemachine.de>
    Reviewed-by: Richard Haeser <richard@maxserv.com>
    Tested-by: Richard Haeser <richard@maxserv.com>
    Reviewed-by: Jigal van Hemert <jigal.van.hemert@typo3.org>
    Tested-by: Jigal van Hemert <jigal.van.hemert@typo3.org>
    Reviewed-by: Oliver Klee <typo3-coding@oliverklee.de>
    Reviewed-by: Stephan Großberndt <stephan.grossberndt@typo3.org>
    Reviewed-by: Fabien Udriot <fabien.udriot@ecodev.ch>
    Tested-by: Fabien Udriot <fabien.udriot@ecodev.ch>
    Reviewed-by: Mona Muzaffar <mona.muzaffar@gmx.de>
    Tested-by: Mona Muzaffar <mona.muzaffar@gmx.de>
    Reviewed-by: André Schließer <andy.schliesser@gmail.com>
    Tested-by: André Schließer <andy.schliesser@gmail.com>
    Reviewed-by: Andreas Wolf <andreas.wolf@typo3.org>
    Tested-by: Andreas Wolf <andreas.wolf@typo3.org>

--- typo3/sysext/core/Classes/Core/SystemEnvironmentBuilder.php
+++ typo3/sysext/core/Classes/Core/SystemEnvironmentBuilder.php
@@ -118,7 +118,7 @@ class SystemEnvironmentBuilder
         // This version, branch and copyright
         define('TYPO3_version', '10.0.0-dev');
         define('TYPO3_branch', '10.0');
-        define('TYPO3_copyright_year', '1998-2019');
+        define('TYPO3_copyright_year', '1998-' . date('Y'));
 
         // TYPO3 external links
         define('TYPO3_URL_GENERAL', 'https://typo3.org/');
