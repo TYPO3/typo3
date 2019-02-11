1ee70ea993ac52e2ce854400ee86d956b50f2958
Richard Haeser <richard@maxserv.com>
2019-02-11 20:24:39 +0100

    [BUGFIX] Cast lastMod property to int
    To prevent type mismatches when using pdo_mysql as the database driver,
    the data for the lastMod field is cast to an integer.
    
    Resolves: #87689
    Releases: master, 9.5
    Change-Id: I696f62a3b08d464708c9e1ab9edb4c733835f475
    Reviewed-on: https://review.typo3.org/59687
    Tested-by: TYPO3com <noreply@typo3.com>
    Reviewed-by: Georg Ringer <georg.ringer@gmail.com>
    Tested-by: Georg Ringer <georg.ringer@gmail.com>

--- typo3/sysext/seo/Classes/XmlSitemap/AbstractXmlSitemapDataProvider.php
+++ typo3/sysext/seo/Classes/XmlSitemap/AbstractXmlSitemapDataProvider.php
@@ -104,7 +104,7 @@ abstract class AbstractXmlSitemapDataProvider implements XmlSitemapDataProviderI
         $lastMod = 0;
         foreach ($this->items as $item) {
             if ((int)$item['lastMod'] > $lastMod) {
-                $lastMod = $item['lastMod'];
+                $lastMod = (int)$item['lastMod'];
             }
         }
 
