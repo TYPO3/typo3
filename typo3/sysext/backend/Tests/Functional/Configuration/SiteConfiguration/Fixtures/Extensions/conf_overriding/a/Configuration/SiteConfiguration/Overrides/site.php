d4c698c60c8ec3d1cfac56a5c79b364904c5e2d9
Jan Delius <me@jandelius.eu>
2019-03-04 13:52:41 +0100

    [BUGFIX] Use finder instance correctly
    Within SiteTcaConfiguration, the $finder is now reused as much as
    possible. A new clean instance is used for the individual actions.
    
    Resolves: #87832
    Releases: master, 9.5
    Change-Id: I34f2d08c51042f2cfe6b4a09ac6152e0b552ab14
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/59839
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Rico Drexler <rico.drexler@gmx.de>
    Tested-by: Susanne Moog <look@susi.dev>
    Reviewed-by: Jörg Bösche <typo3@joergboesche.de>
    Reviewed-by: Susanne Moog <look@susi.dev>

---
+++ typo3/sysext/backend/Tests/Functional/Configuration/SiteConfiguration/Fixtures/Extensions/conf_overriding/a/Configuration/SiteConfiguration/Overrides/site.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types = 1);
+defined('TYPO3_MODE') or die();
+
+$GLOBALS['SiteConfiguration']['site']['columns']['tx_a_a'] = [
+    'label' => 'a',
+    'description' => '',
+    'config' => [
+        'type' => 'input',
+        'size' => 25,
+        'max' => 255,
+    ]
+];
+
+$GLOBALS['SiteConfiguration']['site']['types']['0']['showitem'] .= ',tx_a_a';
