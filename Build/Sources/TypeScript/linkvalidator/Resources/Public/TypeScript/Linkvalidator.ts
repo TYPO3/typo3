baf328e498a6ce4ed0a636740e49ab7bb6fa6370
Frank Naegler <frank.naegler@typo3.org>
2019-03-15 10:47:54 +0100

    [TASK] Migrate TYPO3/CMS/Linkvalidator/* to TypeScript
    Resolves: #87920
    Releases: master
    Change-Id: Ibb3d28e85b6beddfd4bacca8303cca51c46ac6b3
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/60255
    Tested-by: Andreas Fernandez <a.fernandez@scripting-base.de>
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Daniel Goerz <daniel.goerz@posteo.de>
    Reviewed-by: Andreas Fernandez <a.fernandez@scripting-base.de>
    Reviewed-by: Daniel Goerz <daniel.goerz@posteo.de>

---
+++ Build/Sources/TypeScript/linkvalidator/Resources/Public/TypeScript/Linkvalidator.ts
@@ -0,0 +1,64 @@
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+import * as $ from 'jquery';
+import Notification = require('TYPO3/CMS/Backend/Notification');
+
+/**
+ * Module: TYPO3/CMS/Linkvalidator/Linkvalidator
+ */
+class Linkvalidator {
+  constructor() {
+    this.initializeEvents();
+  }
+
+  public toggleActionButton(prefix: string): void {
+    let buttonDisable = true;
+    $('.' + prefix).each((index: number, element: HTMLInputElement): void => {
+      if ($(element).prop('checked')) {
+        buttonDisable = false;
+      }
+    });
+
+    if (prefix === 'check') {
+      $('#updateLinkList').prop('disabled', buttonDisable);
+    } else {
+      $('#refreshLinkList').prop('disabled', buttonDisable);
+    }
+  }
+
+  /**
+   * Registers listeners
+   */
+  protected initializeEvents(): void {
+    $('.refresh').on('click', (): void => {
+      this.toggleActionButton('refresh');
+    });
+
+    $('.check').on('click', (): void => {
+      this.toggleActionButton('check');
+    });
+
+    $('.t3js-update-button').on('click', (e: JQueryEventObject): void => {
+      const $element = $(e.currentTarget);
+      const name = $element.attr('name');
+      let message = 'Event triggered';
+      if (name === 'refreshLinkList' || name === 'updateLinkList') {
+        message = $element.data('notification-message');
+      }
+      Notification.success(message);
+    });
+  }
+}
+
+export = new Linkvalidator();
