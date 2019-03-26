d0daf82c5ab62f4949334722ac9b7eec07a11001
Henrik Elsner <helsner@dfau.de>
2019-03-26 16:04:14 +0100

    [BUGFIX] DB check - search query, array handling on operator change
    Exception is thrown when changing the operator from an array handling one
    to a string handling. ("is in list" to "equals")
    Also the selection is not set properly after changing.
    
    Resolves: #88010
    Releases: master, 9.5
    
    Change-Id: Ibf7f607a51ac92d2c45dbfe16384f71c959e6c1f
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/60342
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Mona Muzaffar <mona.muzaffar@gmx.de>
    Tested-by: Daniel Windloff
    Tested-by: Anja Leichsenring <aleichsenring@ab-softlab.de>
    Reviewed-by: Mona Muzaffar <mona.muzaffar@gmx.de>
    Reviewed-by: Anja Leichsenring <aleichsenring@ab-softlab.de>

--- typo3/sysext/core/Classes/Database/QueryGenerator.php
+++ typo3/sysext/core/Classes/Database/QueryGenerator.php
@@ -676,6 +676,11 @@ class QueryGenerator
                             $conf['inputValue'] = implode(',', $conf['inputValue']);
                         }
                         $lineHTML[] = '<input class="form-control t3js-clearable" type="text" value="' . htmlspecialchars($conf['inputValue']) . '" name="' . $fieldPrefix . '[inputValue]">';
+                    } elseif ($conf['comparison'] === 64) {
+                        if (is_array($conf['inputValue'])) {
+                            $conf['inputValue'] = $conf['inputValue'][0];
+                        }
+                        $lineHTML[] = '<select class="form-control t3js-submit-change" name="' . $fieldPrefix . '[inputValue]">';
                     } else {
                         $lineHTML[] = '<select class="form-control t3js-submit-change" name="' . $fieldPrefix . '[inputValue]">';
                     }
@@ -1240,7 +1245,7 @@ class QueryGenerator
         $first = 1;
         foreach ($queryConfig as $key => $conf) {
             // Convert ISO-8601 timestamp (string) into unix timestamp (int)
-            if (strtotime($conf['inputValue'])) {
+            if (!is_array($conf['inputValue']) && strtotime($conf['inputValue'])) {
                 $conf['inputValue'] = strtotime($conf['inputValue']);
                 if ($conf['inputValue1'] && strtotime($conf['inputValue1'])) {
                     $conf['inputValue1'] = strtotime($conf['inputValue1']);
@@ -1334,7 +1339,7 @@ class QueryGenerator
             } else {
                 $inputVal = 0;
             }
-        } elseif (strtotime($conf['inputValue' . $suffix])) {
+        } elseif (!is_array($conf['inputValue' . $suffix]) && strtotime($conf['inputValue' . $suffix])) {
             $inputVal = $conf['inputValue' . $suffix];
         } else {
             $inputVal = (float)$conf['inputValue' . $suffix];
