7fabc1dc03a6378b9c061bb85e6967c6c248eda2
Jonas Schwabe <jonas.schwabe@eyeworkers.de>
2019-03-29 17:52:11 +0100

    [BUGFIX] Use PageTitle API for indexed_search
    The value of indexedDocTitle was not updated when a title had been
    generated by the PageTitle API. This simply writes the generated
    page title to indexedDocTitle so that it can be used in the
    indexed hook.
    
    Resolves: #88041
    Releases: master, 9.5
    Change-Id: I8ac1fd8295447aafa7e175fb09fa11f565442019
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/60440
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Jörg Bösche <typo3@joergboesche.de>
    Tested-by: Oliver Klee <typo3-coding@oliverklee.de>
    Tested-by: Richard Haeser <richard@maxserv.com>
    Reviewed-by: Jörg Bösche <typo3@joergboesche.de>
    Reviewed-by: Oliver Klee <typo3-coding@oliverklee.de>
    Reviewed-by: Richard Haeser <richard@maxserv.com>

--- typo3/sysext/frontend/Classes/Controller/TypoScriptFrontendController.php
+++ typo3/sysext/frontend/Classes/Controller/TypoScriptFrontendController.php
@@ -3725,6 +3725,10 @@ class TypoScriptFrontendController implements LoggerAwareInterface
         $titleProvider = GeneralUtility::makeInstance(PageTitleProviderManager::class);
         $pageTitle = $titleProvider->getTitle();
 
+        if ($pageTitle !== '') {
+            $this->indexedDocTitle = $pageTitle;
+        }
+
         $titleTagContent = $this->printTitle(
             $pageTitle,
             (bool)($this->config['config']['noPageTitle'] ?? false),
