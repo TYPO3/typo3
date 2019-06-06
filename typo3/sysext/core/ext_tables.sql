2bdaf3ae8e6d3e0a743f272fa7e21a541d7474c7
Benni Mack <benni@typo3.org>
2019-06-06 07:10:37 +0200

    [!!!][TASK] Drop DB field sys_language.static_lang_isocode
    The database field "static_lang_isocode" in the DB table
    "sys_language" is configured via static_info_tables, however
    due to legacy reasons, this was added in TYPO3 Core,
    as some references were put there. It is now possible
    to remove any references to this field, and have the third-party
    extension deal with it.
    
    Resolves: #88583
    Releases: master
    Change-Id: I5c366c84d21e0d291ec32d44c0b2c2e1064f669d
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/60887
    Tested-by: Daniel Goerz <daniel.goerz@posteo.de>
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Andreas Fernandez <a.fernandez@scripting-base.de>
    Reviewed-by: Daniel Goerz <daniel.goerz@posteo.de>
    Reviewed-by: Oliver Klee <typo3-coding@oliverklee.de>
    Reviewed-by: Björn Jacob <bjoern.jacob@tritum.de>
    Reviewed-by: Andreas Fernandez <a.fernandez@scripting-base.de>

--- typo3/sysext/core/ext_tables.sql
+++ typo3/sysext/core/ext_tables.sql
@@ -428,8 +428,7 @@ CREATE TABLE sys_log (
 CREATE TABLE sys_language (
 	title varchar(80) DEFAULT '' NOT NULL,
 	flag varchar(20) DEFAULT '' NOT NULL,
-	language_isocode varchar(2) DEFAULT '' NOT NULL,
-	static_lang_isocode int(11) unsigned DEFAULT '0' NOT NULL
+	language_isocode varchar(2) DEFAULT '' NOT NULL
 );
 
 #
