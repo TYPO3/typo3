0567ec3b1e9f3b32dfa0f96f9b5103f2bed529cb
Benni Mack <benni@typo3.org>
2019-06-07 09:29:11 +0200

    [BUGFIX] Ignore cHash when given but not needed
    If a cHash GET parameter is given, but there
    are no GET parameters that are relevant, a
    hash_calc() call against an empty string is done.
    
    However, the change now allows an invalid
    cHash if no check is necessary. This could
    happen when upgrading from older instances
    where a cHash is not needed anymore.
    
    Bots would not then fill up the error log
    but get the new page (with a valid 200 result)
    
    Resolves: #41033
    Releases: master, 9.5
    Change-Id: Id02701fcbece371a6b9ce0f92fe0be55dd972325
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/60952
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Benni Mack <benni@typo3.org>
    Reviewed-by: Benni Mack <benni@typo3.org>

---
+++ typo3/sysext/frontend/Tests/Unit/Middleware/PageArgumentValidatorTest.php
@@ -0,0 +1,111 @@
+<?php
+declare(strict_types = 1);
+
+namespace TYPO3\CMS\Frontend\Tests\Unit\Middleware;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use TYPO3\CMS\Core\Error\Http\PageNotFoundException;
+use TYPO3\CMS\Core\Http\Response;
+use TYPO3\CMS\Core\Http\ServerRequest;
+use TYPO3\CMS\Core\Routing\PageArguments;
+use TYPO3\CMS\Core\TimeTracker\TimeTracker;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController;
+use TYPO3\CMS\Frontend\Middleware\PageArgumentValidator;
+use TYPO3\CMS\Frontend\Middleware\PageResolver;
+use TYPO3\TestingFramework\Core\AccessibleObjectInterface;
+use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
+
+class PageArgumentValidatorTest extends UnitTestCase
+{
+    /**
+     * @var bool Reset singletons created by subject
+     */
+    protected $resetSingletonInstances = true;
+
+    /**
+     * @var TypoScriptFrontendController|AccessibleObjectInterface
+     */
+    protected $controller;
+
+    /**
+     * @var RequestHandlerInterface
+     */
+    protected $responseOutputHandler;
+
+    /**
+     * @var PageResolver|AccessibleObjectInterface
+     */
+    protected $subject;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $timeTrackerStub = new TimeTracker(false);
+        GeneralUtility::setSingletonInstance(TimeTracker::class, $timeTrackerStub);
+        $this->controller = $this->getAccessibleMock(TypoScriptFrontendController::class, ['dummy'], [], '', false);
+
+        // A request handler which only runs through
+        $this->responseOutputHandler = new class implements RequestHandlerInterface {
+            public function handle(ServerRequestInterface $request): ResponseInterface
+            {
+                return new Response();
+            }
+        };
+    }
+
+    /**
+     * @test
+     */
+    public function givenCacheHashWithoutRequiredParametersIgnoresCacheHashAndUnsetsCacheHash(): void
+    {
+        $incomingUrl = 'https://example.com/lotus-flower/en/mr-magpie/bloom/';
+        $this->controller->id = 13;
+        $this->controller->cHash = 'XYZ';
+
+        $pageArguments = new PageArguments(13, '1', ['cHash' => 'XYZ'], ['parameter-from' => 'path']);
+
+        $request = new ServerRequest($incomingUrl, 'GET');
+        $request = $request->withAttribute('routing', $pageArguments);
+
+        $subject = new PageArgumentValidator($this->controller);
+        $response = $subject->process($request, $this->responseOutputHandler);
+        static::assertEquals(200, $response->getStatusCode());
+        static::assertEquals('', $this->controller->cHash);
+    }
+
+    /**
+     * @test
+     */
+    public function givenCacheHashNotMatchingCalculatedCacheHashTriggers404(): void
+    {
+        $incomingUrl = 'https://example.com/lotus-flower/en/mr-magpie/bloom/';
+        $this->controller->id = 13;
+        $this->controller->cHash = 'XYZ';
+
+        $pageArguments = new PageArguments(13, '1', ['cHash' => 'XYZ', 'dynamic' => 'argument'], ['parameter-from' => 'path']);
+
+        $request = new ServerRequest($incomingUrl, 'GET');
+        $request = $request->withAttribute('routing', $pageArguments);
+
+        static::expectException(PageNotFoundException::class);
+        static::expectExceptionMessage('Request parameters could not be validated (&cHash comparison failed)');
+        $subject = new PageArgumentValidator($this->controller);
+        $subject->process($request, $this->responseOutputHandler);
+    }
+}
