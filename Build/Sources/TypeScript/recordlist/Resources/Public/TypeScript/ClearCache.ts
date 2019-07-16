81844b402d52722f1f2371d3f412ff139cae7c32
Daniel Windloff <daniel.jc.windloff@googlemail.com>
2019-07-16 16:24:02 +0200

    [BUGFIX] Clear page cache: show status, prevent multiple clicks
    Replace the clear cache button with a spinner during the AJAX request.
    While the request is executed, the button is no clickable due to
    addiional properties and classes being set.
    
    Resolves: #88781
    Releases: master
    Change-Id: I06d53010d8bd5435ac6fc91ea17ecc6378043f14
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/61307
    Tested-by: Andreas Fernandez <a.fernandez@scripting-base.de>
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Frank Naegler <frank.naegler@typo3.org>
    Reviewed-by: Andreas Fernandez <a.fernandez@scripting-base.de>
    Reviewed-by: Frank Naegler <frank.naegler@typo3.org>

--- Build/Sources/TypeScript/recordlist/Resources/Public/TypeScript/ClearCache.ts
+++ Build/Sources/TypeScript/recordlist/Resources/Public/TypeScript/ClearCache.ts
@@ -13,37 +13,69 @@
 
 import * as $ from 'jquery';
 import Notification = require('TYPO3/CMS/Backend/Notification');
+import Icons = require('TYPO3/CMS/Backend/Icons');
+
+enum Identifiers {
+  clearCache = '.t3js-clear-page-cache',
+  icon = '.t3js-icon',
+}
 
 /**
  * Module: TYPO3/CMS/Recordlist/ClearCache
- * Folder selection
- * @exports TYPO3/CMS/Recordlist/ClearCache
  */
 class ClearCache {
   constructor() {
     $(() => {
-      $('.t3js-clear-page-cache').on('click', (event: JQueryEventObject): void => {
-        event.preventDefault();
-        const $me = $(event.currentTarget);
-        const id = $me.data('id');
-        $.ajax({
-          url: TYPO3.settings.ajaxUrls.web_list_clearpagecache + '&id=' + id,
-          cache: false,
-          dataType: 'json',
-          success: (data: any): void => {
-            if (data.success === true) {
-              Notification.success(data.title, data.message, 1);
-            } else {
-              Notification.error(data.title, data.message, 1);
-            }
-          },
-          error: (): void => {
-            Notification.error(
-                'Clearing page caches went wrong on the server side.',
-            );
-          },
-        });
+      this.registerClickHandler();
+    });
+  }
+
+  private registerClickHandler(): void {
+    $(Identifiers.clearCache + ':not([disabled])').on('click', (event: JQueryEventObject): void => {
+      event.preventDefault();
+      const $me = $(event.currentTarget);
+      const id = parseInt($me.data('id'), 10);
+      const $iconElement = $me.find(Identifiers.icon);
+
+      $me.prop('disabled', true).addClass('disabled');
+      Icons.getIcon('spinner-circle-dark', Icons.sizes.small, null, 'disabled').done((icon: string): void => {
+        $iconElement.replaceWith(icon);
       });
+
+      this.sendClearCacheRequest(id).always((): void => {
+        Icons.getIcon('actions-system-cache-clear', Icons.sizes.small).done((icon: string): void => {
+          $me.find(Identifiers.icon).replaceWith(icon);
+        });
+        $me.prop('disabled', false).removeClass('disabled');
+      });
+    });
+  }
+
+  /**
+   * Send an AJAX request to clear a page's cache
+   *
+   * @param {number} pageId
+   */
+  private sendClearCacheRequest(pageId: number): JQueryXHR {
+    return $.ajax({
+      url: TYPO3.settings.ajaxUrls.web_list_clearpagecache,
+      data: {
+        id: pageId,
+      },
+      cache: false,
+      dataType: 'json',
+      success: (data: any): void => {
+        if (data.success === true) {
+          Notification.success(data.title, data.message, 1);
+        } else {
+          Notification.error(data.title, data.message, 1);
+        }
+      },
+      error: (): void => {
+        Notification.error(
+          'Clearing page caches went wrong on the server side.',
+        );
+      },
     });
   }
 }
