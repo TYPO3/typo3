bbd7188fb9a60f50279b5d89cb9ae1b75615c571
Guido Schmechel <guido.schmechel@brandung.de>
2019-07-26 18:18:47 +0200

    [TASK] Allow longer URL in redirects
    Extend field length of source_path and target
    
    Resolves: #88336
    Releases: master, 9.5
    Change-Id: I46c241b5782b0a0b8b2947cf0b063340785aa4fe
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/61353
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Josef Glatz <josefglatz@gmail.com>
    Tested-by: Georg Ringer <georg.ringer@gmail.com>
    Reviewed-by: Oliver Klee <typo3-coding@oliverklee.de>
    Reviewed-by: Jörg Bösche <typo3@joergboesche.de>
    Reviewed-by: Josef Glatz <josefglatz@gmail.com>
    Reviewed-by: Georg Ringer <georg.ringer@gmail.com>

--- typo3/sysext/redirects/ext_tables.sql
+++ typo3/sysext/redirects/ext_tables.sql
@@ -3,13 +3,13 @@
 #
 CREATE TABLE sys_redirect (
 	source_host varchar(255) DEFAULT '' NOT NULL,
-	source_path varchar(255) DEFAULT '' NOT NULL,
+	source_path varchar(2048) DEFAULT '' NOT NULL,
 	is_regexp tinyint(1) unsigned DEFAULT '0' NOT NULL,
 
 	force_https tinyint(1) unsigned DEFAULT '0' NOT NULL,
 	respect_query_parameters tinyint(1) unsigned DEFAULT '0' NOT NULL,
 	keep_query_parameters tinyint(1) unsigned DEFAULT '0' NOT NULL,
-	target varchar(255) DEFAULT '' NOT NULL,
+	target varchar(2048) DEFAULT '' NOT NULL,
 	target_statuscode int(11) DEFAULT '307' NOT NULL,
 
 	hitcount int(11) DEFAULT '0' NOT NULL,
