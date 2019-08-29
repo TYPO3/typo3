eaf822e0ceafd7341a9d8c64c3574c91352e4bbb
Susanne Moog <look@susi.dev>
2019-08-29 15:29:34 +0200

    [BUGFIX] Support multiple env vars per line in Yaml
    Releases: master, 9.5
    Resolves: #89059
    Change-Id: Iafbe5cb8ade38f29c13fd8ded68e42d552a28891
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/61576
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Benjamin Franzke <bfr@qbus.de>
    Tested-by: Susanne Moog <look@susi.dev>
    Reviewed-by: Benjamin Franzke <bfr@qbus.de>
    Reviewed-by: Susanne Moog <look@susi.dev>

--- typo3/sysext/core/Tests/Unit/Configuration/Loader/YamlFileLoaderTest.php
+++ typo3/sysext/core/Tests/Unit/Configuration/Loader/YamlFileLoaderTest.php
@@ -154,34 +154,54 @@ betterthanbefore: \'%firstset.myinitialversion%\'
     {
         return [
             'plain' => [
-                'foo=heinz',
+                ['foo=heinz'],
                 'carl: \'%env(foo)%\'',
                 ['carl' => 'heinz']
             ],
             'quoted var' => [
-                'foo=heinz',
+                ['foo=heinz'],
                 "carl: '%env(''foo'')%'",
                 ['carl' => 'heinz']
             ],
             'double quoted var' => [
-                'foo=heinz',
+                ['foo=heinz'],
                 "carl: '%env(\"foo\")%'",
                 ['carl' => 'heinz']
             ],
             'var in the middle' => [
-                'foo=heinz',
+                ['foo=heinz'],
                 "carl: 'https://%env(foo)%/foo'",
                 ['carl' => 'https://heinz/foo']
             ],
             'quoted var in the middle' => [
-                'foo=heinz',
+                ['foo=heinz'],
                 "carl: 'https://%env(''foo'')%/foo'",
                 ['carl' => 'https://heinz/foo']
             ],
             'double quoted var in the middle' => [
-                'foo=heinz',
+                ['foo=heinz'],
                 "carl: 'https://%env(\"foo\")%/foo'",
                 ['carl' => 'https://heinz/foo']
+            ],
+            'two env vars' => [
+                ['foo=karl', 'bar=heinz'],
+                'carl: \'%env(foo)%::%env(bar)%\'',
+                ['carl' => 'karl::heinz']
+            ],
+            'three env vars' => [
+                ['foo=karl', 'bar=heinz', 'baz=bencer'],
+                'carl: \'%env(foo)%::%env(bar)%::%env(baz)%\'',
+                ['carl' => 'karl::heinz::bencer']
+            ],
+            'three env vars with baz being undefined' => [
+                ['foo=karl', 'bar=heinz'],
+                'carl: \'%env(foo)%::%env(bar)%::%env(baz)%\'',
+                ['carl' => 'karl::heinz::%env(baz)%']
+            ],
+            'three undefined env vars' => [
+                [],
+                'carl: \'%env(foo)%::%env(bar)%::%env(baz)%\'',
+                ['carl' => '%env(foo)%::%env(bar)%::%env(baz)%']
             ],
         ];
     }
@@ -191,13 +211,15 @@ betterthanbefore: \'%firstset.myinitialversion%\'
      *
      * @dataProvider loadWithEnvVarDataProvider
      * @test
-     * @param string $env
+     * @param array $envs
      * @param string $yamlContent
      * @param array $expected
      */
-    public function loadWithEnvVarPlaceholders(string $env, string $yamlContent, array $expected): void
+    public function loadWithEnvVarPlaceholders(array $envs, string $yamlContent, array $expected): void
     {
-        putenv($env);
+        foreach ($envs as $env) {
+            putenv($env);
+        }
         $fileName = 'Berta.yml';
         $fileContents = $yamlContent;
 
@@ -208,6 +230,8 @@ betterthanbefore: \'%firstset.myinitialversion%\'
         $output = $subject->load($fileName);
         $this->assertSame($expected, $output);
         putenv('foo=');
+        putenv('bar=');
+        putenv('baz=');
     }
 
     /**
