90ae60e1d9f29c8bb3ac7460345bd959ebfe3762
Anja Leichsenring <aleichsenring@ab-softlab.de>
2019-09-05 13:01:41 +0200

    [TASK] Add UnitTests for SiteConfiguration
    Resolves: #89089
    Releases: master, 9.5
    Change-Id: I7971e27e01bd26f1ea7fe03a008447c8475cac9e
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/61601
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Richard Haeser <richard@maxserv.com>
    Reviewed-by: Richard Haeser <richard@maxserv.com>

---
+++ typo3/sysext/core/Tests/Unit/Configuration/SiteConfigurationTest.php
@@ -0,0 +1,84 @@
+<?php
+declare(strict_types = 1);
+namespace TYPO3\CMS\Core\Tests\Unit\Configuration;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use Symfony\Component\Yaml\Yaml;
+use TYPO3\CMS\Core\Cache\CacheManager;
+use TYPO3\CMS\Core\Cache\Frontend\FrontendInterface as CacheFrontendInterface;
+use TYPO3\CMS\Core\Configuration\SiteConfiguration;
+use TYPO3\CMS\Core\Core\Environment;
+use TYPO3\CMS\Core\Http\Uri;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
+
+class SiteConfigurationTest extends UnitTestCase
+{
+    protected $resetSingletonInstances = true;
+
+    /**
+     * @var string
+     * store temporarily used files here
+     * will be removed after each test
+     */
+    protected $fixturePath;
+
+    /**
+     * @var SiteConfiguration
+     */
+    protected $subject;
+
+    protected function setup(): void
+    {
+        $basePath = Environment::getVarPath() . '/tests/unit';
+        $this->fixturePath = $basePath . '/fixture/config/sites';
+        if (!file_exists($this->fixturePath)) {
+            GeneralUtility::mkdir_deep($this->fixturePath);
+        }
+        $this->testFilesToDelete[] = $basePath;
+        $cacheManager = $this->prophesize(CacheManager::class);
+        GeneralUtility::setSingletonInstance(CacheManager::class, $cacheManager->reveal());
+        $cacheProphecy = $this->prophesize(CacheFrontendInterface::class);
+        $cacheManager->getCache('cache_core')->willReturn($cacheProphecy->reveal());
+        $this->subject = new SiteConfiguration($this->fixturePath);
+    }
+
+    /**
+     * @test
+     */
+    public function resolveAllExistingSitesReturnsEmptyArrayForNoSiteConfigsFound(): void
+    {
+        $this->assertEmpty($this->subject->resolveAllExistingSites());
+    }
+
+    /**
+     * @test
+     */
+    public function resolveAllExistingSitesReadsConfiguration(): void
+    {
+        $configuration = [
+            'rootPageId' => 42,
+            'base' => 'https://example.com'
+        ];
+        $yamlFileContents = Yaml::dump($configuration, 99, 2);
+        GeneralUtility::mkdir($this->fixturePath . '/home');
+        GeneralUtility::writeFile($this->fixturePath . '/home/config.yaml', $yamlFileContents);
+        $sites = $this->subject->resolveAllExistingSites();
+        $this->assertCount(1, $sites);
+        $currentSite = current($sites);
+        $this->assertSame(42, $currentSite->getRootPageId());
+        $this->assertEquals(new Uri('https://example.com'), $currentSite->getBase());
+    }
+}
