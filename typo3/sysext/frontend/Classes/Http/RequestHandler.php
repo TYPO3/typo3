32cf1ca08865bfe2b84c7d094da23e44cf05b30b
Georg Ringer <georg.ringer@gmail.com>
2019-09-09 14:20:22 +0200

    [TASK] Change commenting style of spam protecting code
    To avoid possible conflicts with JS minification, the commenting style
    is changed.
    
    Resolves: #89093
    Releases: master, 9.5
    Change-Id: I1afce194b89eeb64988f48c37b6ba1107d21f61b
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/61647
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Björn Jacob <bjoern.jacob@tritum.de>
    Tested-by: Andreas Fernandez <a.fernandez@scripting-base.de>
    Reviewed-by: Björn Jacob <bjoern.jacob@tritum.de>
    Reviewed-by: Andreas Fernandez <a.fernandez@scripting-base.de>

--- typo3/sysext/frontend/Classes/Http/RequestHandler.php
+++ typo3/sysext/frontend/Classes/Http/RequestHandler.php
@@ -693,7 +693,7 @@ class RequestHandler implements RequestHandlerInterface
 
         if ($controller->spamProtectEmailAddresses && $controller->spamProtectEmailAddresses !== 'ascii') {
             $scriptJsCode = '
-			// decrypt helper function
+			/* decrypt helper function */
 		function decryptCharcode(n,start,end,offset) {
 			n = n + offset;
 			if (offset > 0 && n > end) {
@@ -703,25 +703,25 @@ class RequestHandler implements RequestHandlerInterface
 			}
 			return String.fromCharCode(n);
 		}
-			// decrypt string
+			/* decrypt string */
 		function decryptString(enc,offset) {
 			var dec = "";
 			var len = enc.length;
 			for(var i=0; i < len; i++) {
 				var n = enc.charCodeAt(i);
 				if (n >= 0x2B && n <= 0x3A) {
-					dec += decryptCharcode(n,0x2B,0x3A,offset);	// 0-9 . , - + / :
+					dec += decryptCharcode(n,0x2B,0x3A,offset);	/* 0-9 . , - + / : */
 				} else if (n >= 0x40 && n <= 0x5A) {
-					dec += decryptCharcode(n,0x40,0x5A,offset);	// A-Z @
+					dec += decryptCharcode(n,0x40,0x5A,offset);	/* A-Z @ */
 				} else if (n >= 0x61 && n <= 0x7A) {
-					dec += decryptCharcode(n,0x61,0x7A,offset);	// a-z
+					dec += decryptCharcode(n,0x61,0x7A,offset);	/* a-z */
 				} else {
 					dec += enc.charAt(i);
 				}
 			}
 			return dec;
 		}
-			// decrypt spam-protected emails
+			/* decrypt spam-protected emails */
 		function linkTo_UnCryptMailto(s) {
 			location.href = decryptString(s,' . $controller->spamProtectEmailAddresses * -1 . ');
 		}
