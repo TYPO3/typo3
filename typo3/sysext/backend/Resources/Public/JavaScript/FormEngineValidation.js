c529deacba10349c588014696c0ac59334e9ac68
Harald Atteneder <harald@urbantrout.io>
2019-09-21 21:09:14 +0200

    [TASK] Keep old value on confirmation modal close
    When changing the doktype a confirmation modal appears. This patch
    reverts the value of the field to the original value after closing the
    modal without clicking *OK*.
    
    This works for all selects with 'onChange' => 'reload' and
    'renderType' => 'selectSingle'
    
    Resolves: #89220
    Releases: master,9.5
    Change-Id: I6d479f9180d64e8e4c25e96f52e60dca36863fd9
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/61809
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Sascha Rademacher <sascha.rademacher+typo3@gmail.com>
    Tested-by: Tobi Kretschmann <tobi@tobishome.de>
    Reviewed-by: Sascha Rademacher <sascha.rademacher+typo3@gmail.com>
    Reviewed-by: Tobi Kretschmann <tobi@tobishome.de>

--- typo3/sysext/backend/Resources/Public/JavaScript/FormEngineValidation.js
+++ typo3/sysext/backend/Resources/Public/JavaScript/FormEngineValidation.js
@@ -536,7 +536,12 @@ define(['jquery', 'moment'], function($, moment) {
    */
   FormEngineValidation.markFieldAsChanged = function($field) {
     var $paletteField = $field.closest('.t3js-formengine-palette-field');
-    $paletteField.addClass('has-change');
+
+    if ($field.data('original-value') && $field.data('original-value') == $field.val()) {
+      $paletteField.removeClass('has-change');
+    } else {
+      $paletteField.addClass('has-change');
+    }
   };
 
   /**
