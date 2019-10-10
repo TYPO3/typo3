24ea052411f5b752ed75dce66f4e5e6b99a4d9a7
Anja Leichsenring <aleichsenring@ab-softlab.de>
2019-10-10 16:59:29 +0200

    [TASK] Provide AcceptanceTest for stand alone install tool
    A simple test asserting the login and logout mechanics for
    the stand alone install tool are functional.
    Cards in the maintenance page received basic checks
    ensuring no Javascript errors occur.
    
    Additionally, typo3/testing-framework will be raised, so the
    new tests gets integrated into the automatic bamboo run.
    Used update command:
    	composer update typo3/testing-framework
    
    Resolves: #89393
    Resolves: #89397
    Releases: master
    Change-Id: I1eb916405dec682fa57ee07794d892b1fc6255ab
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/61945
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Andreas Fernandez <a.fernandez@scripting-base.de>
    Tested-by: Anja Leichsenring <aleichsenring@ab-softlab.de>
    Reviewed-by: Andreas Fernandez <a.fernandez@scripting-base.de>
    Reviewed-by: Anja Leichsenring <aleichsenring@ab-softlab.de>

---
+++ typo3/sysext/core/Tests/Acceptance/InstallTool/AbstractCest.php
@@ -0,0 +1,70 @@
+<?php
+declare(strict_types = 1);
+namespace TYPO3\CMS\Core\Tests\Acceptance\InstallTool;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+use TYPO3\CMS\Core\Core\Environment;
+use TYPO3\CMS\Core\Crypto\PasswordHashing\Argon2iPasswordHash;
+use TYPO3\CMS\Core\Tests\Acceptance\Support\BackendTester;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+class AbstractCest
+{
+    public const ENABLE_INSTALL_TOOL_FILEPATH = 'typo3conf/ENABLE_INSTALL_TOOL';
+    public const ADDITIONAL_CONFIGURATION_FILEPATH = 'typo3conf/AdditionalConfiguration.php';
+
+    public function _before(BackendTester $I)
+    {
+        $I->amOnPage('typo3/install.php');
+    }
+
+    public function _after(BackendTester $I)
+    {
+        $I->click('Logout');
+
+        $I->amGoingTo('clean up created files');
+        unlink(Environment::getProjectPath() . '/' . self::ADDITIONAL_CONFIGURATION_FILEPATH);
+
+        $I->dontSeeFileFound(self::ENABLE_INSTALL_TOOL_FILEPATH);
+        $I->dontSeeFileFound(self::ADDITIONAL_CONFIGURATION_FILEPATH);
+    }
+
+    protected function logIntoInstallTool(BackendTester $I)
+    {
+        $password = $this->setInstallToolPassword($I);
+
+        $I->writeToFile(AbstractCest::ENABLE_INSTALL_TOOL_FILEPATH, '');
+        $I->reloadPage();
+        $I->fillField('#t3-install-form-password', $password);
+        $I->click('Login');
+        $I->waitForText('Maintenance');
+    }
+
+    /**
+     * @param BackendTester $I
+     * @return string
+     * @throws \TYPO3\CMS\Core\Crypto\PasswordHashing\InvalidPasswordHashException
+     */
+    private function setInstallToolPassword(BackendTester $I): string
+    {
+        $hashMethod = GeneralUtility::makeInstance(Argon2iPasswordHash::class);
+        $password = 'temporary password';
+        $hashedPassword = $hashMethod->getHashedPassword($password);
+        $I->writeToFile(
+            self::ADDITIONAL_CONFIGURATION_FILEPATH,
+            '<?php' . PHP_EOL . '$GLOBALS[\'TYPO3_CONF_VARS\'][\'BE\'][\'installToolPassword\'] = \'' . $hashedPassword . '\';'
+        );
+        return $password;
+    }
+}
