9477f2e3c44a4600d58c75b22803892ebaffc26b
Daniel Siepmann <daniel.siepmann@typo3.org>
2019-10-15 13:22:54 +0200

    [BUGFIX] Make EXT:backend independent of EXT:install
    EXT:backend generates a URL to EXT:install, but has no dependency. This
    can lead to composer setups with EXT:backend but without EXT:install,
    leading to an exception while generating route.
    
    Therefore a suggest is added as a hint to install also EXT:install.
    Existing code is wrapped with an try / catch in order to work under both
    circumstances.
    
    Resolves: #89419
    Releases: master, 9.5
    Change-Id: Idedd299f218ae8f7ba9938800d2ac4a0c4e5614b
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/61999
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Andreas Fernandez <a.fernandez@scripting-base.de>
    Tested-by: Georg Ringer <georg.ringer@gmail.com>
    Tested-by: Susanne Moog <look@susi.dev>
    Reviewed-by: Karavas Aristeidis <aristeidis.karavas@gmail.com>
    Reviewed-by: Andreas Fernandez <a.fernandez@scripting-base.de>
    Reviewed-by: Georg Ringer <georg.ringer@gmail.com>
    Reviewed-by: Susanne Moog <look@susi.dev>

--- typo3/sysext/backend/Classes/Backend/ToolbarItems/SystemInformationToolbarItem.php
+++ typo3/sysext/backend/Classes/Backend/ToolbarItems/SystemInformationToolbarItem.php
@@ -22,6 +22,7 @@ use TYPO3\CMS\Core\Core\SystemEnvironmentBuilder;
 use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Localization\LanguageService;
 use TYPO3\CMS\Core\Page\PageRenderer;
+use TYPO3\CMS\Core\Routing\RouteNotFoundException;
 use TYPO3\CMS\Core\Utility\CommandUtility;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Core\Utility\StringUtility;
@@ -169,8 +170,15 @@ class SystemInformationToolbarItem implements ToolbarItemInterface
 
         $uriBuilder = GeneralUtility::makeInstance(UriBuilder::class);
         $view = $this->getFluidTemplateObject('SystemInformationDropDown.html');
+
+        try {
+            $environmentToolUrl = (string)$uriBuilder->buildUriFromRoute('tools_toolsenvironment');
+        } catch (RouteNotFoundException $e) {
+            $environmentToolUrl = '';
+        }
+
         $view->assignMultiple([
-            'environmentToolUrl' => (string)$uriBuilder->buildUriFromRoute('tools_toolsenvironment'),
+            'environmentToolUrl' => $environmentToolUrl,
             'messages' => $this->systemMessages,
             'count' => $this->totalCount > $this->maximumCountInBadge ? $this->maximumCountInBadge . '+' : $this->totalCount,
             'severityBadgeClass' => $this->severityBadgeClass,
