782b95036249d3941b8c4935e5b1b52b5b43b680
Andreas Fernandez <a.fernandez@scripting-base.de>
2019-10-16 15:02:49 +0200

    [BUGFIX] Clipboard: Check whether FAL object is an existing file
    The clipboard now checks whether a FAL object is a valid and existing
    file before creating an export link.
    
    Resolves: #89430
    Related: #88969
    Releases: master, 9.5, 8.7
    Change-Id: I12f221962df59fa032a5b06e59d11c16dd37351c
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/61981
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Daniel Goerz <daniel.goerz@posteo.de>
    Reviewed-by: Daniel Goerz <daniel.goerz@posteo.de>

--- typo3/sysext/backend/Classes/Clipboard/Clipboard.php
+++ typo3/sysext/backend/Classes/Clipboard/Clipboard.php
@@ -20,6 +20,7 @@ use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Database\Query\Restriction\DeletedRestriction;
 use TYPO3\CMS\Core\Imaging\Icon;
 use TYPO3\CMS\Core\Imaging\IconFactory;
+use TYPO3\CMS\Core\Resource\AbstractFile;
 use TYPO3\CMS\Core\Resource\ResourceFactory;
 use TYPO3\CMS\Core\Type\Bitmask\JsConfirmation;
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
@@ -764,8 +765,8 @@ class Clipboard
                     list($table, $uid) = explode('|', $k);
                     // Rendering files/directories on the clipboard
                     if ($table === '_FILE') {
-                        $file = ResourceFactory::getInstance()->getFileObjectFromCombinedIdentifier($v);
-                        if ($file !== null) {
+                        $file = ResourceFactory::getInstance()->getObjectFromCombinedIdentifier($v);
+                        if ($file instanceof AbstractFile) {
                             $params['tx_impexp']['record'][] = 'sys_file:' . $file->getUid();
                         }
                     } else {
