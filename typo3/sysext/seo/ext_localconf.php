9d7c1a3aeb0c4ea035f223dbbc0fc25d3e8fe6fe
Richard Haeser <richard@maxserv.com>
2019-10-22 12:52:37 +0200

    [TASK] Change to named keys in EXT:seo when using generateMetaTags hook
    For EXT:seo we now use named keys for the hooks we register for
    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['TYPO3\CMS\Frontend\Page\PageGenerator']['generateMetaTags']. This will make it easier to disable specific
    generators.
    
    Resolves: #89477
    Releases: master, 9.5
    Change-Id: I17e2ca78b484010010b775485e554133b11aaf4c
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/62042
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Chris Müller <typo3@krue.ml>
    Tested-by: Andreas Fernandez <a.fernandez@scripting-base.de>
    Reviewed-by: Chris Müller <typo3@krue.ml>
    Reviewed-by: Andreas Fernandez <a.fernandez@scripting-base.de>

--- typo3/sysext/seo/ext_localconf.php
+++ typo3/sysext/seo/ext_localconf.php
@@ -1,11 +1,11 @@
 <?php
 defined('TYPO3_MODE') or die();
 
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['TYPO3\CMS\Frontend\Page\PageGenerator']['generateMetaTags'][] =
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['TYPO3\CMS\Frontend\Page\PageGenerator']['generateMetaTags']['metatag'] =
     \TYPO3\CMS\Seo\MetaTag\MetaTagGenerator::class . '->generate';
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['TYPO3\CMS\Frontend\Page\PageGenerator']['generateMetaTags'][] =
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['TYPO3\CMS\Frontend\Page\PageGenerator']['generateMetaTags']['hreflang'] =
     \TYPO3\CMS\Seo\HrefLang\HrefLangGenerator::class . '->generate';
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['TYPO3\CMS\Frontend\Page\PageGenerator']['generateMetaTags'][] =
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['TYPO3\CMS\Frontend\Page\PageGenerator']['generateMetaTags']['canonical'] =
     \TYPO3\CMS\Seo\Canonical\CanonicalGenerator::class . '->generate';
 
 $metaTagManagerRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\MetaTag\MetaTagManagerRegistry::class);
