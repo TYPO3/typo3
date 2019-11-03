a6af4fd53bf728752a259726afa03a0b0be7d30e
Oliver Bartsch <bo@cedev.de>
2019-11-03 12:13:22 +0100

    [BUGFIX] Check parsed url for query part in BE Shortcuts
    This change adds null coalescing operators at multiple places
    in `ShortcutRepository` to avoid type errors if the shortcut url
    doesn't contain a query part (e.g. coming from legacy URLs
    when a TYPO3 v7 installation was migrated).
    
    Resolves: #89533
    Releases: master, 9.5
    Change-Id: I9bd1bdf0cf2f8d8954c4bf9eada6c720f2103af1
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/62213
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Daniel Goerz <daniel.goerz@posteo.de>
    Reviewed-by: Daniel Goerz <daniel.goerz@posteo.de>

--- typo3/sysext/backend/Classes/Backend/Shortcut/ShortcutRepository.php
+++ typo3/sysext/backend/Classes/Backend/Shortcut/ShortcutRepository.php
@@ -197,7 +197,7 @@ class ShortcutRepository
 
         $queryParts = parse_url($url);
         $queryParameters = [];
-        parse_str($queryParts['query'], $queryParameters);
+        parse_str($queryParts['query'] ?? '', $queryParameters);
 
         if (!empty($queryParameters['scheme'])) {
             throw new \RuntimeException('Shortcut URLs must be relative', 1518785877);
@@ -489,7 +489,7 @@ class ShortcutRepository
             $queryParts = parse_url($row['url']);
             // Explode GET vars recursively
             $queryParameters = [];
-            parse_str($queryParts['query'], $queryParameters);
+            parse_str($queryParts['query'] ?? '', $queryParameters);
 
             if ($row['module_name'] === 'xMOD_alt_doc.php' && is_array($queryParameters['edit'])) {
                 $shortcut['table'] = key($queryParameters['edit']);
@@ -731,16 +731,16 @@ class ShortcutRepository
     {
         $parsedUrl = parse_url($url);
         $parameters = [];
-        parse_str($parsedUrl['query'], $parameters);
+        parse_str($parsedUrl['query'] ?? '', $parameters);
 
         $uriBuilder = GeneralUtility::makeInstance(UriBuilder::class);
         // parse the returnUrl and replace the module token of it
         if (!empty($parameters['returnUrl'])) {
             $parsedReturnUrl = parse_url($parameters['returnUrl']);
             $returnUrlParameters = [];
-            parse_str($parsedReturnUrl['query'], $returnUrlParameters);
+            parse_str($parsedReturnUrl['query'] ?? '', $returnUrlParameters);
 
-            if (strpos($parsedReturnUrl['path'], 'index.php') !== false && !empty($returnUrlParameters['route'])) {
+            if (strpos($parsedReturnUrl['path'] ?? '', 'index.php') !== false && !empty($returnUrlParameters['route'])) {
                 $module = $returnUrlParameters['route'];
                 $parameters['returnUrl'] = (string)$uriBuilder->buildUriFromRoutePath($module, $returnUrlParameters);
                 $url = $parsedUrl['path'] . '?' . http_build_query($parameters, '', '&', PHP_QUERY_RFC3986);
