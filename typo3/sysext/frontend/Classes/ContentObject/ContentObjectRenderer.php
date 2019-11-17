cf2bbe5b4c9493bbff53988740039a0e07453aa5
Susanne Moog <look@susi.dev>
2019-11-17 17:33:45 +0100

    [BUGFIX] Ensures configuration for resolveAnchorLink is an array
    Strict typing requires $conf to be an array as parameter for
    resolveAnchorLink, however it may also be null. The fix ensures that
    on calling resolveAnchorLink a null coalescing operator ensures type
    array.
    
    Resolves: #89698
    Related: #87992
    Releases: master, 9.5
    Change-Id: Ie459a312845d45bc15a16ff02089b1acf67eb00a
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/62282
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Georg Ringer <georg.ringer@gmail.com>
    Reviewed-by: Georg Ringer <georg.ringer@gmail.com>

--- typo3/sysext/frontend/Classes/ContentObject/ContentObjectRenderer.php
+++ typo3/sysext/frontend/Classes/ContentObject/ContentObjectRenderer.php
@@ -5407,7 +5407,7 @@ class ContentObjectRenderer implements LoggerAwareInterface
         $title = $resolvedLinkParameters['title'];
 
         if (!$linkParameter) {
-            return $this->resolveAnchorLink($linkText, $conf);
+            return $this->resolveAnchorLink($linkText, $conf ?? []);
         }
 
         // Detecting kind of link and resolve all necessary parameters
