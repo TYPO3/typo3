ce6fdfad95bbeaf501d4dfb6607c2e1066496f0c
Alexander Schnitzler <git@alexanderschnitzler.de>
2019-11-18 14:34:17 +0100

    [TASK] Streamline autoloading of test classes in extbase
    - Adjust namespaces of classes according to PSR-4
    - Add a classmap for test fixtures
    
    Releases: master
    Resolves: #89706
    Change-Id: Iefd73f8039eb3db4de419ecaab13a18f6abb23e2
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/62341
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Jörg Bösche <typo3@joergboesche.de>
    Tested-by: Georg Ringer <georg.ringer@gmail.com>
    Reviewed-by: Mathias Brodala <mbrodala@pagemachine.de>
    Reviewed-by: Jörg Bösche <typo3@joergboesche.de>
    Reviewed-by: Oliver Klee <typo3-coding@oliverklee.de>
    Reviewed-by: Georg Ringer <georg.ringer@gmail.com>

--- typo3/sysext/extbase/Tests/Functional/Persistence/ImplementationClassNameTest.php
+++ typo3/sysext/extbase/Tests/Functional/Persistence/ImplementationClassNameTest.php
@@ -1,6 +1,6 @@
 <?php
 declare(strict_types = 1);
-namespace TYPO3\CMS\Extbase\Tests\Functional;
+namespace TYPO3\CMS\Extbase\Tests\Functional\Persistence;
 
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Object\ObjectManager;
