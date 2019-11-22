8bb73e3fece60d80523c43eedc751b7616f5e443
Benni Mack <benni@typo3.org>
2019-11-22 17:28:39 +0100

    [BUGFIX] Allow resolving unmapped type GET parameters with PageTypeDecorator
    When the PageTypeDecorator is used for routing, currently ALL available
    typeNums must be registered, otherwise links to pages with a typeNum
    just get their type removed (fallback to default).
    
    A new check is added when creating links (resolving these URLs with
    a GET type= parameter works just fine) for skipping unmapped arguments
    when decorating.
    
    Resolves: #87817
    Resolves: #88836
    Releases: master, 9.5
    Change-Id: I252cf80de2befed8396cc93b1bc737d77aa18b31
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/62383
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Henning Liebe <h.liebe@neusta.de>
    Tested-by: Susanne Moog <look@susi.dev>
    Tested-by: Daniel Goerz <daniel.goerz@posteo.de>
    Reviewed-by: Henning Liebe <h.liebe@neusta.de>
    Reviewed-by: Susanne Moog <look@susi.dev>
    Reviewed-by: Daniel Goerz <daniel.goerz@posteo.de>

--- typo3/sysext/core/Classes/Routing/Enhancer/PageTypeDecorator.php
+++ typo3/sysext/core/Classes/Routing/Enhancer/PageTypeDecorator.php
@@ -144,9 +144,14 @@ class PageTypeDecorator extends AbstractEnhancer implements DecoratingEnhancerIn
     {
         $type = isset($parameters['type']) ? (string)$parameters['type'] : null;
         $value = $this->resolveValue($type);
+        // If the type is > 0 but the value could not be resolved,
+        // the type is appended as GET argument, which can be resolved already anyway.
+        // This happens when the PageTypeDecorator is used, but hasn't been configured for all available types.
+        if ($value === '' && !empty($type)) {
+            return;
+        }
 
-        $considerIndex = $value !== ''
-            && in_array($value[0], static::ROUTE_PATH_DELIMITERS);
+        $considerIndex = $value !== '' && in_array($value[0], static::ROUTE_PATH_DELIMITERS);
         if ($value !== '' && !in_array($value[0], static::ROUTE_PATH_DELIMITERS)) {
             $value = '/' . $value;
         }
