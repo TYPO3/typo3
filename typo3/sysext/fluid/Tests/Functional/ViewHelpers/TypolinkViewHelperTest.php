ffc21c46578d7ebc413cf1676f184863bed4f6a9
Frank Naegler <frank.naegler@typo3.org>
2019-12-03 09:19:30 +0100

    [TASK] Add more tests for TypoLink handling in fluid
    Resolves: #89834
    Releases: master, 9.5
    Change-Id: I914c6e8a0739c4380be4fa1c1d165f3eb4f1aecd
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/62520
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Susanne Moog <look@susi.dev>
    Tested-by: Andreas Fernandez <a.fernandez@scripting-base.de>
    Reviewed-by: Susanne Moog <look@susi.dev>
    Reviewed-by: Andreas Fernandez <a.fernandez@scripting-base.de>

--- typo3/sysext/fluid/Tests/Functional/ViewHelpers/TypolinkViewHelperTest.php
+++ typo3/sysext/fluid/Tests/Functional/ViewHelpers/TypolinkViewHelperTest.php
@@ -72,6 +72,7 @@ class TypolinkViewHelperTest extends FunctionalTestCase
     }
 
     /**
+     * @param mixed $parameter
      * @param bool $addQueryString
      * @param string $addQueryStringMethod
      * @param string $addQueryStringExclude
@@ -82,6 +83,7 @@ class TypolinkViewHelperTest extends FunctionalTestCase
      * @dataProvider renderCreatesCorrectLinkProvider
      */
     public function renderCreatesCorrectLink(
+        $parameter,
         bool $addQueryString,
         string $addQueryStringMethod,
         string $addQueryStringExclude,
@@ -91,6 +93,7 @@ class TypolinkViewHelperTest extends FunctionalTestCase
         $view = new StandaloneView();
         $view->setTemplatePathAndFilename('typo3/sysext/fluid/Tests/Functional/ViewHelpers/Fixtures/' . $template . '.html');
         $view->assignMultiple([
+            'parameter' => $parameter,
             'uid' => 1,
             'addQueryString' => $addQueryString,
             'addQueryStringMethod' => $addQueryStringMethod,
@@ -106,6 +109,7 @@ class TypolinkViewHelperTest extends FunctionalTestCase
     {
         return [
             'link: default' => [
+                'parameter' => 1,
                 'addQueryString' => false,
                 'addQueryStringMethod' => 'GET',
                 'addQueryStringExclude' => '',
@@ -113,6 +117,7 @@ class TypolinkViewHelperTest extends FunctionalTestCase
                 'template' => 'link_typolink_viewhelper',
             ],
             'link: with add query string' => [
+                'parameter' => 1,
                 'addQueryString' => true,
                 'addQueryStringMethod' => 'GET',
                 'addQueryStringExclude' => '',
@@ -120,6 +125,7 @@ class TypolinkViewHelperTest extends FunctionalTestCase
                 'template' => 'link_typolink_viewhelper',
             ],
             'link: with add query string and exclude' => [
+                'parameter' => 1,
                 'addQueryString' => true,
                 'addQueryStringMethod' => 'GET',
                 'addQueryStringExclude' => 'temp',
@@ -127,6 +133,7 @@ class TypolinkViewHelperTest extends FunctionalTestCase
                 'template' => 'link_typolink_viewhelper',
             ],
             'uri: default' => [
+                'parameter' => 1,
                 'addQueryString' => false,
                 'addQueryStringMethod' => 'GET',
                 'addQueryStringExclude' => '',
@@ -134,6 +141,7 @@ class TypolinkViewHelperTest extends FunctionalTestCase
                 'template' => 'uri_typolink_viewhelper',
             ],
             'uri: with add query string' => [
+                'parameter' => 1,
                 'addQueryString' => true,
                 'addQueryStringMethod' => 'GET',
                 'addQueryStringExclude' => '',
@@ -141,10 +149,155 @@ class TypolinkViewHelperTest extends FunctionalTestCase
                 'template' => 'uri_typolink_viewhelper',
             ],
             'uri: with add query string and exclude' => [
+                'parameter' => 1,
                 'addQueryString' => true,
                 'addQueryStringMethod' => 'GET',
                 'addQueryStringExclude' => 'temp',
                 'expected' => '/en/?foo=bar&amp;cHash=afa4b37588ab917af3cfe2cd4464029d /en/',
+                'template' => 'uri_typolink_viewhelper',
+            ],
+            't3://url link: default' => [
+                'parameter' => 't3://url?url=https://example.org?param=1&other=dude',
+                'addQueryString' => false,
+                'addQueryStringMethod' => 'GET',
+                'addQueryStringExclude' => '',
+                'expected' => '<a href="https://example.org?param=1">This is a testlink</a> <a href="https://example.org?param=1">This is a testlink</a>',
+                'template' => 'link_typolink_viewhelper',
+            ],
+            't3://url link: with add query string' => [
+                'parameter' => 't3://url?url=https://example.org?param=1&other=dude',
+                'addQueryString' => true,
+                'addQueryStringMethod' => 'GET',
+                'addQueryStringExclude' => '',
+                'expected' => '<a href="https://example.org?param=1">This is a testlink</a> <a href="https://example.org?param=1">This is a testlink</a>',
+                'template' => 'link_typolink_viewhelper',
+            ],
+            't3://url link: with add query string and exclude' => [
+                'parameter' => 't3://url?url=https://example.org?param=1&other=dude',
+                'addQueryString' => true,
+                'addQueryStringMethod' => 'GET',
+                'addQueryStringExclude' => 'temp',
+                'expected' => '<a href="https://example.org?param=1">This is a testlink</a> <a href="https://example.org?param=1">This is a testlink</a>',
+                'template' => 'link_typolink_viewhelper',
+            ],
+            't3://url uri: default' => [
+                'parameter' => 't3://url?url=https://example.org?param=1&other=dude',
+                'addQueryString' => false,
+                'addQueryStringMethod' => 'GET',
+                'addQueryStringExclude' => '',
+                'expected' => 'https://example.org?param=1 https://example.org?param=1',
+                'template' => 'uri_typolink_viewhelper',
+            ],
+            't3://url uri: with add query string' => [
+                'parameter' => 't3://url?url=https://example.org?param=1&other=dude',
+                'addQueryString' => true,
+                'addQueryStringMethod' => 'GET',
+                'addQueryStringExclude' => '',
+                'expected' => 'https://example.org?param=1 https://example.org?param=1',
+                'template' => 'uri_typolink_viewhelper',
+            ],
+            't3://url uri: with add query string and exclude' => [
+                'parameter' => 't3://url?url=https://example.org?param=1&other=dude',
+                'addQueryString' => true,
+                'addQueryStringMethod' => 'GET',
+                'addQueryStringExclude' => 'temp',
+                'expected' => 'https://example.org?param=1 https://example.org?param=1',
+                'template' => 'uri_typolink_viewhelper',
+            ],
+            'mailto: link: default' => [
+                'parameter' => 'mailto:foo@typo3.org',
+                'addQueryString' => false,
+                'addQueryStringMethod' => 'GET',
+                'addQueryStringExclude' => '',
+                'expected' => '<a href="mailto:foo@typo3.org">This is a testlink</a> <a href="mailto:foo@typo3.org">This is a testlink</a>',
+                'template' => 'link_typolink_viewhelper',
+            ],
+            'mailto: link: with add query string' => [
+                'parameter' => 'mailto:foo@typo3.org',
+                'addQueryString' => true,
+                'addQueryStringMethod' => 'GET',
+                'addQueryStringExclude' => '',
+                'expected' => '<a href="mailto:foo@typo3.org">This is a testlink</a> <a href="mailto:foo@typo3.org">This is a testlink</a>',
+                'template' => 'link_typolink_viewhelper',
+            ],
+            'mailto: link: with add query string and exclude' => [
+                'parameter' => 'mailto:foo@typo3.org',
+                'addQueryString' => true,
+                'addQueryStringMethod' => 'GET',
+                'addQueryStringExclude' => 'temp',
+                'expected' => '<a href="mailto:foo@typo3.org">This is a testlink</a> <a href="mailto:foo@typo3.org">This is a testlink</a>',
+                'template' => 'link_typolink_viewhelper',
+            ],
+            'mailto: uri: default' => [
+                'parameter' => 'mailto:foo@typo3.org',
+                'addQueryString' => false,
+                'addQueryStringMethod' => 'GET',
+                'addQueryStringExclude' => '',
+                'expected' => 'mailto:foo@typo3.org mailto:foo@typo3.org',
+                'template' => 'uri_typolink_viewhelper',
+            ],
+            'mailto: uri: with add query string' => [
+                'parameter' => 'mailto:foo@typo3.org',
+                'addQueryString' => true,
+                'addQueryStringMethod' => 'GET',
+                'addQueryStringExclude' => '',
+                'expected' => 'mailto:foo@typo3.org mailto:foo@typo3.org',
+                'template' => 'uri_typolink_viewhelper',
+            ],
+            'mailto: uri: with add query string and exclude' => [
+                'parameter' => 'mailto:foo@typo3.org',
+                'addQueryString' => true,
+                'addQueryStringMethod' => 'GET',
+                'addQueryStringExclude' => 'temp',
+                'expected' => 'mailto:foo@typo3.org mailto:foo@typo3.org',
+                'template' => 'uri_typolink_viewhelper',
+            ],
+            'http://: link: default' => [
+                'parameter' => 'http://typo3.org/foo/?foo=bar',
+                'addQueryString' => false,
+                'addQueryStringMethod' => 'GET',
+                'addQueryStringExclude' => '',
+                'expected' => '<a href="http://typo3.org/foo/?foo=bar">This is a testlink</a> <a href="http://typo3.org/foo/?foo=bar">This is a testlink</a>',
+                'template' => 'link_typolink_viewhelper',
+            ],
+            'http://: link: with add query string' => [
+                'parameter' => 'http://typo3.org/foo/?foo=bar',
+                'addQueryString' => true,
+                'addQueryStringMethod' => 'GET',
+                'addQueryStringExclude' => '',
+                'expected' => '<a href="http://typo3.org/foo/?foo=bar">This is a testlink</a> <a href="http://typo3.org/foo/?foo=bar">This is a testlink</a>',
+                'template' => 'link_typolink_viewhelper',
+            ],
+            'http://: link: with add query string and exclude' => [
+                'parameter' => 'http://typo3.org/foo/?foo=bar',
+                'addQueryString' => true,
+                'addQueryStringMethod' => 'GET',
+                'addQueryStringExclude' => 'temp',
+                'expected' => '<a href="http://typo3.org/foo/?foo=bar">This is a testlink</a> <a href="http://typo3.org/foo/?foo=bar">This is a testlink</a>',
+                'template' => 'link_typolink_viewhelper',
+            ],
+            'http://: uri: default' => [
+                'parameter' => 'http://typo3.org/foo/?foo=bar',
+                'addQueryString' => false,
+                'addQueryStringMethod' => 'GET',
+                'addQueryStringExclude' => '',
+                'expected' => 'http://typo3.org/foo/?foo=bar http://typo3.org/foo/?foo=bar',
+                'template' => 'uri_typolink_viewhelper',
+            ],
+            'http://: uri: with add query string' => [
+                'parameter' => 'http://typo3.org/foo/?foo=bar',
+                'addQueryString' => true,
+                'addQueryStringMethod' => 'GET',
+                'addQueryStringExclude' => '',
+                'expected' => 'http://typo3.org/foo/?foo=bar http://typo3.org/foo/?foo=bar',
+                'template' => 'uri_typolink_viewhelper',
+            ],
+            'http://: uri: with add query string and exclude' => [
+                'parameter' => 'http://typo3.org/foo/?foo=bar',
+                'addQueryString' => true,
+                'addQueryStringMethod' => 'GET',
+                'addQueryStringExclude' => 'temp',
+                'expected' => 'http://typo3.org/foo/?foo=bar http://typo3.org/foo/?foo=bar',
                 'template' => 'uri_typolink_viewhelper',
             ],
         ];
