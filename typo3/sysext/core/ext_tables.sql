e40561d7ba6765806f2f1031d8375a6a2b64938f
Oliver Bartsch <bo@cedev.de>
2019-12-04 11:08:04 +0100

    [TASK] Add index to slug field
    `TYPO3\CMS\Core\Routing\PageRouter::getPagesFromDatabaseForCandidates`
    looks-up candidates for the page using the `slug` field (and also sorts by
    this value). This has some serious impact on the website performance on
    systems with lots of pages. In order to optimize this, an index should be
    set.
    
    Resolves: #88896
    Releases: 9.5
    Change-Id: Iae8653ebb24d8c1d5d97cfe7f42948087e2e3ef8
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/62532
    Tested-by: Oliver Hader <oliver.hader@typo3.org>
    Tested-by: Jonas Eberle <flightvision@googlemail.com>
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Benni Mack <benni@typo3.org>
    Reviewed-by: Oliver Hader <oliver.hader@typo3.org>
    Reviewed-by: Jonas Eberle <flightvision@googlemail.com>
    Reviewed-by: Benni Mack <benni@typo3.org>

--- typo3/sysext/core/ext_tables.sql
+++ typo3/sysext/core/ext_tables.sql
@@ -116,7 +116,8 @@ CREATE TABLE pages (
 
 	KEY alias (alias),
 	KEY determineSiteRoot (is_siteroot),
-	KEY language_identifier (l10n_parent,sys_language_uid)
+	KEY language_identifier (l10n_parent,sys_language_uid),
+	KEY slug (slug(127))
 );
 
 #
