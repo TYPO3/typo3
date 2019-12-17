0d975edaab3d8a21bad53024a0b8f32fdb5ad133
Frank Naegler <frank.naegler@typo3.org>
2019-12-17 10:51:49 +0100

    [SECURITY] Prevent SQLi in ext:lowlevel QueryGenerator
    Resolves: #89452
    Releases: master, 9.5, 8.7
    Security-Commit: befc2d3d188753b528e721ffaac46ac060fb7b82
    Security-Bulletin: TYPO3-CORE-SA-2019-025
    Change-Id: Ic8441f4eb992c663ed6f17600d427f81290f73e3
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/62707
    Tested-by: Oliver Hader <oliver.hader@typo3.org>
    Reviewed-by: Oliver Hader <oliver.hader@typo3.org>

--- typo3/sysext/core/Tests/Functional/Database/QueryGeneratorTest.php
+++ typo3/sysext/core/Tests/Functional/Database/QueryGeneratorTest.php
@@ -16,7 +16,10 @@ namespace TYPO3\CMS\Core\Tests\Functional\Database;
  * The TYPO3 project - inspiring people to share!
  */
 
+use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Database\QueryGenerator;
+use TYPO3\CMS\Core\Localization\LanguageService;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
 
 /**
@@ -24,6 +27,13 @@ use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
  */
 class QueryGeneratorTest extends FunctionalTestCase
 {
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->setUpBackendUserFromFixture(1);
+        $GLOBALS['LANG'] = new LanguageService();
+    }
+
     /**
      * @test
      */
@@ -325,5 +335,93 @@ class QueryGeneratorTest extends FunctionalTestCase
 
         $queryGenerator->init('queryConfig', 'aTable');
         $this->assertSame($expected, trim($queryGenerator->getQuery($inputConf), "\n\r"));
+    }
+
+    public function arbitraryDataIsEscapedDataProvider(): array
+    {
+        $dataSet = [];
+        $injectors = [
+            // INJ'ECT
+            'INJ%quoteCharacter%ECT',
+            // INJ '--
+            // ' ECT
+            'INJ %quoteCharacter%%commentStart% %commentEnd%%quoteCharacter% ECT'
+        ];
+        $comparisons = array_keys((new QueryGenerator())->compSQL);
+        foreach ($injectors as $injector) {
+            foreach ($comparisons as $comparison) {
+                $dataSet[] = [
+                    $injector,
+                    [
+                        'queryTable' => 'tt_content',
+                        'queryFields' => 'uid,' . $injector,
+                        'queryGroup' => $injector,
+                        'queryOrder' => $injector,
+                        'queryLimit' => $injector,
+                        'queryConfig' => serialize([
+                            [
+                                'operator' => $injector,
+                                'type' => 'FIELD_category_field', // falls back to CType (first field)
+                                'comparison' => $comparison,
+                                'inputValue' => $injector,
+
+                            ],
+                            [
+                                'operator' => $injector,
+                                'type' => 'FIELD_category_field',
+                                'comparison' => $comparison,
+                                'inputValue' => $injector,
+
+                            ],
+                        ]),
+                    ],
+                ];
+            }
+        }
+        return $dataSet;
+    }
+
+    /**
+     * @param string $injector
+     * @param array $settings
+     *
+     * @test
+     * @dataProvider arbitraryDataIsEscapedDataProvider
+     * @throws \Doctrine\DBAL\DBALException
+     */
+    public function arbitraryDataIsEscaped(string $injector, array $settings)
+    {
+        $databasePlatform = GeneralUtility::makeInstance(ConnectionPool::class)
+            ->getConnectionForTable('tt_content')->getDatabasePlatform();
+        $replacements = [
+            '%quoteCharacter%' => $databasePlatform->getStringLiteralQuoteCharacter(),
+            '%commentStart%' => $databasePlatform->getSqlCommentStartString(),
+            '%commentEnd%' => $databasePlatform->getSqlCommentEndString()
+        ];
+        $injector = str_replace(array_keys($replacements), $replacements, $injector);
+        $settings = $this->prepareSettings($settings, $replacements);
+
+        $queryGenerator = new QueryGenerator();
+        $queryGenerator->init('queryConfig', $settings['queryTable']);
+        $queryGenerator->makeSelectorTable($settings);
+        $queryGenerator->enablePrefix = true;
+
+        $queryString = $queryGenerator->getQuery($queryGenerator->queryConfig);
+        $query = $queryGenerator->getSelectQuery($queryString);
+
+        self::assertStringNotContainsString($injector, $query);
+    }
+
+    protected function prepareSettings(array $settings, array $replacements): array
+    {
+        foreach ($settings as $settingKey => &$settingValue) {
+            if (is_string($settingValue)) {
+                $settingValue = str_replace(array_keys($replacements), $replacements, $settingValue);
+            }
+            if (is_array($settingValue)) {
+                $settingValue = $this->prepareSettings($settingValue, $replacements);
+            }
+        }
+        return $settings;
     }
 }
