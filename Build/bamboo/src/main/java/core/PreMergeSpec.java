1d381c2834162025e1ce6bbc63e8c86efd3d191c
Anja Leichsenring <anja.leichsenring@typo3.com>
2019-12-27 09:52:33 +0100

    [TASK] Remove repository polling trigger from pre-merge test plans
    The trigger will cause the plan to be executed after merge, which
    is not needed because nightly plans take care of it.
    
    Resolves: #89972
    Releases: master, 9.5, 8.7
    Change-Id: Iafc32174d2234439915c2e741020c4626391f98a
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/62767
    Tested-by: Anja Leichsenring <aleichsenring@ab-softlab.de>
    Reviewed-by: Anja Leichsenring <aleichsenring@ab-softlab.de>

--- Build/bamboo/src/main/java/core/PreMergeSpec.java
+++ Build/bamboo/src/main/java/core/PreMergeSpec.java
@@ -66,10 +66,7 @@ public class PreMergeSpec extends AbstractPreMergeSpec {
                 stageMainStage
             )
             .linkedRepositories("github TYPO3 TYPO3.CMS 9.5")
-            .triggers(
-                new RepositoryPollingTrigger()
-                    .name("Repository polling for post-merge builds"),
-                getGerritTrigger())
+            .triggers(getGerritTrigger())
             .variables(
                 new Variable("changeUrl", ""),
                 new Variable("patchset", "")
