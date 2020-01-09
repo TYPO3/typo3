5e7759d3e8b91267ee7350ea8e9114b20a9f4de5
Benni Mack <benni@typo3.org>
2020-01-09 15:44:45 +0100

    [BUGFIX] disableNoCacheParameter option allows TypoScript cache option
    With this change applied, a TYPO3 installation having the
    global "disableNoCacheParameter" option activated will
    still disable the cache if set via TypoScript `config.no_cache = 1`.
    
    However, the GET parameter is still not evaluated when
    having the option activated.
    
    Resolves: #87227
    Releases: master, 9.5
    Change-Id: Id33bd3aa9c8d042deffadeb19d8d64d52fad3371
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/62905
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Tobi Kretschmann <tobi@tobishome.de>
    Reviewed-by: Tobi Kretschmann <tobi@tobishome.de>

--- typo3/sysext/frontend/Classes/Controller/TypoScriptFrontendController.php
+++ typo3/sysext/frontend/Classes/Controller/TypoScriptFrontendController.php
@@ -2692,7 +2692,7 @@ class TypoScriptFrontendController implements LoggerAwareInterface
         // No cache
         // Set $this->no_cache TRUE if the config.no_cache value is set!
         if ($this->config['config']['no_cache']) {
-            $this->set_no_cache('config.no_cache is set');
+            $this->set_no_cache('config.no_cache is set', true);
         }
         // Merge GET with defaultGetVars
         // Please note that this code will get removed in TYPO3 v10.0 as it is done in the PSR-15 middleware.
@@ -4504,7 +4504,7 @@ class TypoScriptFrontendController implements LoggerAwareInterface
             $trigger = $file . ' on line ' . $line;
             $warning = '$GLOBALS[\'TSFE\']->set_no_cache() was triggered by ' . $trigger . '.';
         }
-        if ($GLOBALS['TYPO3_CONF_VARS']['FE']['disableNoCacheParameter']) {
+        if (!$internal && $GLOBALS['TYPO3_CONF_VARS']['FE']['disableNoCacheParameter']) {
             $warning .= ' However, $TYPO3_CONF_VARS[\'FE\'][\'disableNoCacheParameter\'] is set, so it will be ignored!';
             $this->getTimeTracker()->setTSlogMessage($warning, 2);
         } else {
