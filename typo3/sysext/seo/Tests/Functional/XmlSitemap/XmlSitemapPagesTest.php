41666ee7cb72c03d5450fb69830be10df20ff087
Daniel Goerz <daniel.goerz@posteo.de>
2020-01-13 18:16:39 +0100

    [BUGFIX] Only include translated pages in sitemap
    When generating the list of pages for a sitemap in
    an alternative language only pages are considered that
    are actually translated or covered by the fallback
    strategy.
    
    Additionally the method generateItems() in the internal
    class PagesXmlSitemapDataProvider is now protected.
    
    Resolves: #90046
    Releases: master, 9.5
    Change-Id: I54557e0485ae7791ac51fa975991d6d9c9211b61
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/62865
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Susanne Moog <look@susi.dev>
    Tested-by: Richard Haeser <richard@maxserv.com>
    Reviewed-by: Susanne Moog <look@susi.dev>
    Reviewed-by: Richard Haeser <richard@maxserv.com>

--- typo3/sysext/seo/Tests/Functional/XmlSitemap/XmlSitemapPagesTest.php
+++ typo3/sysext/seo/Tests/Functional/XmlSitemap/XmlSitemapPagesTest.php
@@ -25,6 +25,16 @@ use TYPO3\TestingFramework\Core\Functional\Framework\Frontend\InternalResponse;
  */
 class XmlSitemapPagesTest extends AbstractTestCase
 {
+
+    /**
+     * @var array
+     */
+    protected const LANGUAGE_PRESETS = [
+        'EN' => ['id' => 0, 'title' => 'English', 'locale' => 'en_US.UTF8', 'iso' => 'en', 'hrefLang' => 'en-US', 'direction' => ''],
+        'FR' => ['id' => 1, 'title' => 'French', 'locale' => 'fr_FR.UTF8', 'iso' => 'fr', 'hrefLang' => 'fr-FR', 'direction' => ''],
+        'DE' => ['id' => 2, 'title' => 'German', 'locale' => 'de_DE.UTF8', 'iso' => 'de', 'hrefLang' => 'de-DE', 'direction' => ''],
+    ];
+
     /**
      * @var string[]
      */
@@ -36,11 +46,6 @@ class XmlSitemapPagesTest extends AbstractTestCase
      * @var string
      */
     protected $body;
-
-    /**
-     * @var InternalResponse
-     */
-    protected $response;
 
     protected function setUp(): void
     {
@@ -58,16 +63,10 @@ class XmlSitemapPagesTest extends AbstractTestCase
             'website-local',
             $this->buildSiteConfiguration(1, 'http://localhost/'),
             [
-                $this->buildDefaultLanguageConfiguration('EN', '/')
+                $this->buildDefaultLanguageConfiguration('EN', '/'),
+                $this->buildLanguageConfiguration('FR', '/fr/'),
+                $this->buildLanguageConfiguration('DE', '/de/', ['FR'])
             ]
-        );
-
-        $this->response = $this->executeFrontendRequest(
-            (new InternalRequest('http://localhost/'))->withQueryParameters([
-                'id' => 1,
-                'type' => 1533906435,
-                'sitemap' => 'pages'
-            ])
         );
     }
 
@@ -78,15 +77,16 @@ class XmlSitemapPagesTest extends AbstractTestCase
      */
     public function checkIfPagesSiteMapContainsExpectedEntries($urlPattern): void
     {
-        self::assertEquals(200, $this->response->getStatusCode());
-        self::assertArrayHasKey('Content-Length', $this->response->getHeaders());
-        self::assertGreaterThan(0, $this->response->getHeader('Content-Length')[0]);
-        self::assertArrayHasKey('Content-Type', $this->response->getHeaders());
-        self::assertEquals('application/xml;charset=utf-8', $this->response->getHeader('Content-Type')[0]);
-        self::assertArrayHasKey('X-Robots-Tag', $this->response->getHeaders());
-        self::assertEquals('noindex', $this->response->getHeader('X-Robots-Tag')[0]);
+        $response = $this->getResponse();
+        self::assertEquals(200, $response->getStatusCode());
+        self::assertArrayHasKey('Content-Length', $response->getHeaders());
+        self::assertGreaterThan(0, $response->getHeader('Content-Length')[0]);
+        self::assertArrayHasKey('Content-Type', $response->getHeaders());
+        self::assertEquals('application/xml;charset=utf-8', $response->getHeader('Content-Type')[0]);
+        self::assertArrayHasKey('X-Robots-Tag', $response->getHeaders());
+        self::assertEquals('noindex', $response->getHeader('X-Robots-Tag')[0]);
 
-        self::assertRegExp($urlPattern, (string)$this->response->getBody());
+        self::assertRegExp($urlPattern, (string)$response->getBody());
     }
 
     /**
@@ -96,7 +96,7 @@ class XmlSitemapPagesTest extends AbstractTestCase
     {
         self::assertStringNotContainsString(
             '<loc>http://localhost/canonicalized-page</loc>',
-            (string)$this->response->getBody()
+            (string)$this->getResponse()->getBody()
         );
     }
 
@@ -106,9 +106,49 @@ class XmlSitemapPagesTest extends AbstractTestCase
     public function pagesToCheckDataProvider(): array //18-03-2019 21:24:07
     {
         return [
-            'complete-entry' => ['/<url>\s+<loc>http:\/\/localhost\/complete\-entry<\/loc>\s+<lastmod>2017-04-10T08:00:00\+00:00<\/lastmod>\s+<changefreq>daily<\/changefreq>\s+<priority>0\.7<\/priority>\s+<\/url>/'],
-            'only-changefreq' => ['/<url>\s+<loc>http:\/\/localhost\/only\-changefreq<\/loc>\s+<lastmod>2017-04-10T08:00:00\+00:00<\/lastmod>\s+<changefreq>weekly<\/changefreq>\s+<priority>0\.5<\/priority>\s+<\/url>/'],
-            'clean' => ['/<url>\s+<loc>http:\/\/localhost\/clean<\/loc>\s+<lastmod>2017-04-10T08:00:00\+00:00<\/lastmod>\s+<priority>0\.5<\/priority>\s+<\/url>/'],
+            'complete-entry' => ['#<url>\s+<loc>http://localhost/complete\-entry</loc>\s+<lastmod>\d+-\d+-\d+T\d+:\d+:\d+\+\d+:\d+</lastmod>\s+<changefreq>daily</changefreq>\s+<priority>0\.7</priority>\s+</url>#'],
+            'only-changefreq' => ['#<url>\s+<loc>http://localhost/only\-changefreq</loc>\s+<lastmod>\d+-\d+-\d+T\d+:\d+:\d+\+\d+:\d+</lastmod>\s+<changefreq>weekly</changefreq>\s+<priority>0\.5</priority>\s+</url>#'],
+            'clean' => ['#<url>\s+<loc>http://localhost/clean</loc>\s+<lastmod>\d+-\d+-\d+T\d+:\d+:\d+\+\d+:\d+</lastmod>\s+<priority>0\.5</priority>\s+</url>#'],
         ];
+    }
+
+    /**
+     * @test
+     */
+    public function pagesSitemapContainsTranslatedPages(): void
+    {
+        $xml = new \SimpleXMLElement((string)$this->getResponse('http://localhost/fr/')->getBody());
+        self::assertEquals(3, $xml->count());
+    }
+
+    /**
+     * @test
+     */
+    public function pagesSitemapDoesNotContainsUntranslatedPages(): void
+    {
+        self::assertStringNotContainsString(
+            '<loc>http://localhost/dummy-1-4</loc>',
+            (string)$this->getResponse('http://localhost/fr/')->getBody()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function pagesSitemapRespectFallbackStrategy(): void
+    {
+        $xml = new \SimpleXMLElement((string)$this->getResponse('http://localhost/de/')->getBody());
+        self::assertEquals(4, $xml->count());
+    }
+
+    protected function getResponse(string $uri = 'http://localhost/'): InternalResponse
+    {
+        return $this->executeFrontendRequest(
+            (new InternalRequest($uri))->withQueryParameters([
+                'id' => 1,
+                'type' => 1533906435,
+                'sitemap' => 'pages'
+            ])
+        );
     }
 }
