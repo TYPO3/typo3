7820d832b024b05a24a2adfd7ae3da0a5871b832
Joerg Boesche <typo3@joergboesche.de>
2020-01-13 11:02:08 +0100

    [BUGFIX] Add language flags to sys category TCA config
    The sys_category TCA configuration behavior is now the same as in tt_content.
    Flags and labels for the languages in the select single dropdown.
    
    Resolves: #89607
    Releases: master, 9.5
    Change-Id: I5a4c2df7294b699136b62cbf4f68d2ceeae43b8e
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/62861
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Daniel Goerz <daniel.goerz@posteo.de>
    Reviewed-by: Daniel Goerz <daniel.goerz@posteo.de>

--- typo3/sysext/core/Configuration/TCA/sys_category.php
+++ typo3/sysext/core/Configuration/TCA/sys_category.php
@@ -67,18 +67,15 @@ return [
             'config' => [
                 'type' => 'select',
                 'renderType' => 'selectSingle',
-                'foreign_table' => 'sys_language',
-                'foreign_table_where' => 'ORDER BY sys_language.title',
+                'special' => 'languages',
                 'items' => [
-                    ['LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.allLanguages', -1],
-                    ['LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.default_value', 0]
-                ],
-                'default' => 0,
-                'fieldWizard' => [
-                    'selectIcons' => [
-                        'disabled' => false,
+                    [
+                        'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.allLanguages',
+                        -1,
+                        'flags-multiple'
                     ],
                 ],
+                'default' => 0,
             ]
         ],
         'l10n_parent' => [
