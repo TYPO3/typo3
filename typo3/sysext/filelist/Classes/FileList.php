d78b1553ff5ee76a3d827f6fc94c08bf359b9934
NGUYEN Duc Quan <quan@apart.lu>
2020-01-15 11:30:50 +0100

    [FEATURE] Make translation of filelist optional
    Adding the possibility to enable/disable filelist localization
    
    Resolves: #90114
    Releases: master
    Change-Id: I42529e035a1325ff41cf7584db73b268ddc58ac1
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/62922
    Tested-by: Felix P. <f.pachowsky@neusta.de>
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Daniel Goerz <daniel.goerz@posteo.de>
    Reviewed-by: Felix P. <f.pachowsky@neusta.de>
    Reviewed-by: Daniel Goerz <daniel.goerz@posteo.de>

--- typo3/sysext/filelist/Classes/FileList.php
+++ typo3/sysext/filelist/Classes/FileList.php
@@ -1015,7 +1015,7 @@ class FileList
                         $theData[$field] = $this->makeClip($fileObject);
                         break;
                     case '_LOCALIZATION_':
-                        if (!empty($systemLanguages) && $fileObject->isIndexed() && $fileObject->checkActionPermission('editMeta') && $this->getBackendUser()->check('tables_modify', 'sys_file_metadata')) {
+                        if (!empty($systemLanguages) && $fileObject->isIndexed() && $fileObject->checkActionPermission('editMeta') && $this->getBackendUser()->check('tables_modify', 'sys_file_metadata') && !is_null($GLOBALS['TCA']['sys_file_metadata']['ctrl']['languageField'])) {
                             $metaDataRecord = $fileObject->getMetaData()->get();
                             $translations = $this->getTranslationsForMetaData($metaDataRecord);
                             $languageCode = '';
