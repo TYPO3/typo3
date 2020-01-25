9a02c9c5aa28ab11e5fb26b750e81b7da67cab73
Elias Häußler <elias@haeussler.dev>
2020-01-25 15:14:03 +0100

    [TASK] Remove obsolete expression in ConditionMatcher
    With patch #88962 an obsolete expression has been introduced within the
    ConditionMatcher class which is now removed.
    
    Resolves: #90201
    Related: #88962
    Releases: master
    Change-Id: I8911ae8fad5e725d70d968e4250ccfcda87246c4
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/63033
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Daniel Goerz <daniel.goerz@posteo.de>
    Tested-by: Susanne Moog <look@susi.dev>
    Reviewed-by: Daniel Goerz <daniel.goerz@posteo.de>
    Reviewed-by: Susanne Moog <look@susi.dev>

--- typo3/sysext/backend/Classes/Configuration/TypoScript/ConditionMatching/ConditionMatcher.php
+++ typo3/sysext/backend/Classes/Configuration/TypoScript/ConditionMatching/ConditionMatcher.php
@@ -51,7 +51,7 @@ class ConditionMatcher extends AbstractConditionMatcher
         $tree->level = $treeLevel;
         $tree->rootLine = $this->rootline;
         $tree->rootLineIds = array_column($this->rootline, 'uid');
-        $tree->rootLineParentIds = $tree->rootLineParentIds = array_slice(array_column($this->rootline, 'pid'), 2);
+        $tree->rootLineParentIds = array_slice(array_column($this->rootline, 'pid'), 2);
 
         $backendUserAspect = $this->context->getAspect('backend.user');
         $backend = new \stdClass();
