5257a1ccdb67379e1d00e7ac90e2818887eac31a
Susanne Moog <look@susi.dev>
2020-02-28 18:06:21 +0100

    [BUGFIX] Fix Dashboard RSS widgets cache life time handling
    The SimpleFileBackend does not support cache life times, so RSS
    feeds are currently cached "forever". Using the FileBackend instead
    works as intended.
    
    Resolves: #90562
    Releases: master
    Change-Id: I7481210ce58f597bdc644a1587da71091d172a4d
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/63485
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Georg Ringer <georg.ringer@gmail.com>
    Tested-by: Oliver Bartsch <bo@cedev.de>
    Tested-by: Alexander Schnitzler <git@alexanderschnitzler.de>
    Tested-by: Daniel Goerz <daniel.goerz@posteo.de>
    Reviewed-by: Georg Ringer <georg.ringer@gmail.com>
    Reviewed-by: Oliver Bartsch <bo@cedev.de>
    Reviewed-by: Alexander Schnitzler <git@alexanderschnitzler.de>
    Reviewed-by: Daniel Siepmann <coding@daniel-siepmann.de>
    Reviewed-by: Daniel Goerz <daniel.goerz@posteo.de>

--- typo3/sysext/dashboard/ext_localconf.php
+++ typo3/sysext/dashboard/ext_localconf.php
@@ -9,7 +9,7 @@ call_user_func(static function () {
         if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['dashboard_rss'])) {
             $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['dashboard_rss'] = [
                 'frontend' => \TYPO3\CMS\Core\Cache\Frontend\VariableFrontend::class,
-                'backend' => \TYPO3\CMS\Core\Cache\Backend\SimpleFileBackend::class,
+                'backend' => \TYPO3\CMS\Core\Cache\Backend\FileBackend::class,
                 'options' => [
                     'defaultLifetime' => 900,
                 ],
