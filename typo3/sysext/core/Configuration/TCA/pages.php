4d9691b65b58de936fd2e942816eddf08ca7c9d2
Susanne Moog <look@susi.dev>
2020-02-28 16:01:40 +0100

    [BUGFIX] Add type definition for backend user section
    Since columnsOverrides it is possible to overwrite TCA definitions
    for specific columns and type combinations. However, as there is no
    specific type definition for backend user sections, the default type
    (and its overrides) were used. Backend user section was the only
    undefined type. The type definition has now been added.
    
    Resolves: #90577
    Releases: master, 9.5
    Change-Id: Ifded3bd9642afd7fffbb8918c9181ef1e3c7b6ea
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/63458
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Daniel Goerz <daniel.goerz@posteo.de>
    Reviewed-by: Daniel Goerz <daniel.goerz@posteo.de>

--- typo3/sysext/core/Configuration/TCA/pages.php
+++ typo3/sysext/core/Configuration/TCA/pages.php
@@ -960,6 +960,38 @@ return [
                 --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended,
             '
         ],
+        (string)\TYPO3\CMS\Frontend\Page\PageRepository::DOKTYPE_BE_USER_SECTION => [
+            'showitem' => '
+                --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
+                    --palette--;;standard,
+                    --palette--;;title,
+                --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_tca.xlf:pages.tabs.metadata,
+                    --palette--;;abstract,
+                    --palette--;;metatags,
+                    --palette--;;editorial,
+                --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_tca.xlf:pages.tabs.appearance,
+                    --palette--;;layout,
+                    --palette--;;replace,
+                --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_tca.xlf:pages.tabs.behaviour,
+                    --palette--;;links,
+                    --palette--;;caching,
+                    --palette--;;miscellaneous,
+                    --palette--;;module,
+                --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_tca.xlf:pages.tabs.resources,
+                    --palette--;;media,
+                    --palette--;;config,
+                --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,
+                    --palette--;;language,
+                --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_tca.xlf:pages.tabs.access,
+                    --palette--;;visibility,
+                    --palette--;;access,
+                --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:categories,
+                    categories,
+                --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:notes,
+                    rowDescription,
+                --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended,
+            '
+        ],
         // external URL
         (string)\TYPO3\CMS\Frontend\Page\PageRepository::DOKTYPE_LINK => [
             'showitem' => '
