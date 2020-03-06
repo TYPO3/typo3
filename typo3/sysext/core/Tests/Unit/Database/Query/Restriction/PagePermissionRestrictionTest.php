685e9a23214b66128d115c0baec92df7ae88e0dd
Benni Mack <benni@typo3.org>
2020-03-06 21:03:15 +0100

    [BUGFIX] Include Page Read Permissions to PageTreeController
    In order to reduce the amount of data fetched, the PageTree loaded
    for editors will only fetch pages that the editor actually sees, reducing
    the actual amount of pages to fetch.
    
    If a pagetree has 80.000 pages, but the editor can only see 50, the
    tree only loads these 50 pages.
    
    Resolves: #90880
    Releases: master, 9.5
    Change-Id: I57985484ace07fbfb919573351c210b291697ae3
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/63983
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Susanne Moog <look@susi.dev>
    Reviewed-by: Susanne Moog <look@susi.dev>

---
+++ typo3/sysext/core/Tests/Unit/Database/Query/Restriction/PagePermissionRestrictionTest.php
@@ -0,0 +1,126 @@
+<?php
+declare(strict_types = 1);
+namespace TYPO3\CMS\Core\Tests\Unit\Database\Query\Restriction;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use TYPO3\CMS\Core\Context\UserAspect;
+use TYPO3\CMS\Core\Database\Query\Restriction\PagePermissionRestriction;
+use TYPO3\CMS\Core\Type\Bitmask\Permission;
+
+class PagePermissionRestrictionTest extends AbstractRestrictionTestCase
+{
+
+    /**
+     * Builds a shell for the user aspect object which returns the checked values in the Restriction.
+     *
+     * @param bool $isLoggedIn
+     * @param bool $isAdmin
+     * @param int $userId
+     * @param array $groupIds
+     * @return UserAspect
+     */
+    protected function getPreparedUserAspect(bool $isLoggedIn, bool $isAdmin, int $userId, array $groupIds): UserAspect
+    {
+        return new class($isLoggedIn, $isAdmin, $userId, $groupIds) extends UserAspect {
+            private $isAdmin;
+            private $isLoggedIn;
+            private $userId;
+            private $groupIds;
+            public function __construct(bool $isLoggedIn, bool $isAdmin, int $userId, array $groupIds)
+            {
+                $this->isLoggedIn = $isLoggedIn;
+                $this->isAdmin = $isAdmin;
+                $this->userId = $userId;
+                $this->groupIds = $groupIds;
+            }
+            public function isAdmin(): bool
+            {
+                return $this->isAdmin;
+            }
+            public function isLoggedIn(): bool
+            {
+                return $this->isLoggedIn;
+            }
+            public function get($name)
+            {
+                if ($name === 'id') {
+                    return $this->userId;
+                }
+                return parent::get($name);
+            }
+            public function getGroupIds(): array
+            {
+                return $this->groupIds;
+            }
+        };
+    }
+
+    /**
+     * @test
+     */
+    public function buildRestrictionsOnlyWorksOnPagesTable()
+    {
+        $aspect = $this->getPreparedUserAspect(true, false, 2, [13]);
+        $subject = new PagePermissionRestriction($aspect, Permission::PAGE_SHOW);
+        $expression = $subject->buildExpression(['pages' => 'pages'], $this->expressionBuilder);
+        self::assertNotEmpty((string)$expression);
+        $expression = $subject->buildExpression(['anotherTable' => 'anotherTable'], $this->expressionBuilder);
+        self::assertEmpty((string)$expression);
+    }
+
+    /**
+     * @test
+     */
+    public function buildRestrictionsReturnsAZeroReturnSetWhenNotLoggedIn()
+    {
+        $aspect = $this->getPreparedUserAspect(false, false, 2, [13]);
+        $subject = new PagePermissionRestriction($aspect, Permission::PAGE_SHOW);
+        $expression = $subject->buildExpression(['pages' => 'pages'], $this->expressionBuilder);
+        self::assertSame('1 = 0', (string)$expression);
+    }
+
+    /**
+     * @test
+     */
+    public function buildRestrictionsIsSkippedForAdmins()
+    {
+        $aspect = $this->getPreparedUserAspect(true, true, 2, [13]);
+        $subject = new PagePermissionRestriction($aspect, Permission::PAGE_SHOW);
+        $expression = $subject->buildExpression(['pages' => 'pages'], $this->expressionBuilder);
+        self::assertEmpty((string)$expression);
+    }
+
+    /**
+     * @test
+     */
+    public function buildRestrictionsContainsNonAdminUserIdAsOwnerAndGroupIdsAsOwnerGroup()
+    {
+        $aspect = $this->getPreparedUserAspect(true, false, 2, [13, 14, 15, 16]);
+        $subject = new PagePermissionRestriction($aspect, Permission::PAGE_SHOW);
+        $expression = $subject->buildExpression(['pages' => 'pages'], $this->expressionBuilder);
+        self::assertEquals('("pages"."perms_everybody" & 1 = 1) OR (("pages"."perms_userid" = 2) AND ("pages"."perms_user" & 1 = 1)) OR (("pages"."perms_groupid" IN (13, 14, 15, 16)) AND ("pages"."perms_group" & 1 = 1))', (string)$expression);
+    }
+
+    /**
+     * @test
+     */
+    public function buildRestrictionsChecksForDeletionPermission()
+    {
+        $aspect = $this->getPreparedUserAspect(true, false, 42, [13, 14, 15, 16]);
+        $subject = new PagePermissionRestriction($aspect, Permission::PAGE_DELETE);
+        $expression = $subject->buildExpression(['pages' => 'pages'], $this->expressionBuilder);
+        self::assertEquals('("pages"."perms_everybody" & 4 = 4) OR (("pages"."perms_userid" = 42) AND ("pages"."perms_user" & 4 = 4)) OR (("pages"."perms_groupid" IN (13, 14, 15, 16)) AND ("pages"."perms_group" & 4 = 4))', (string)$expression);
+    }
+}
