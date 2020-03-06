586174ddbac15507cd2c115794cd53fa6a2d8d31
Andreas Fernandez <a.fernandez@scripting-base.de>
2020-03-06 13:15:12 +0100

    [TASK] Use Core API in ClearCache module
    This patch replaces jQuery in the ClearCache module used in RecordList
    with Core APIs in regards of AJAX and event handling.
    
    Resolves: #90663
    Releases: master
    Change-Id: I526a2136462c3cd494a8d3191b87f64257114b1a
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/63562
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Roland Golla <rolandgolla@gmail.com>
    Tested-by: Frank Nägler <frank.naegler@typo3.org>
    Tested-by: Daniel Goerz <daniel.goerz@posteo.de>
    Reviewed-by: Frank Nägler <frank.naegler@typo3.org>
    Reviewed-by: Daniel Goerz <daniel.goerz@posteo.de>

--- Build/Sources/TypeScript/recordlist/Resources/Public/TypeScript/ClearCache.ts
+++ Build/Sources/TypeScript/recordlist/Resources/Public/TypeScript/ClearCache.ts
@@ -11,9 +11,11 @@
  * The TYPO3 project - inspiring people to share!
  */
 
-import * as $ from 'jquery';
+import {AjaxResponse} from 'TYPO3/CMS/Core/Ajax/AjaxResponse';
 import Notification = require('TYPO3/CMS/Backend/Notification');
 import Icons = require('TYPO3/CMS/Backend/Icons');
+import RegularEvent = require('TYPO3/CMS/Core/Event/RegularEvent');
+import AjaxRequest = require('TYPO3/CMS/Core/Ajax/AjaxRequest');
 
 enum Identifiers {
   clearCache = '.t3js-clear-page-cache',
@@ -24,59 +26,62 @@ enum Identifiers {
  * Module: TYPO3/CMS/Recordlist/ClearCache
  */
 class ClearCache {
-  constructor() {
-    $(() => {
-      this.registerClickHandler();
-    });
-  }
-
-  private registerClickHandler(): void {
-    $(Identifiers.clearCache + ':not([disabled])').on('click', (event: JQueryEventObject): void => {
-      event.preventDefault();
-      const $me = $(event.currentTarget);
-      const id = parseInt($me.data('id'), 10);
-      const $iconElement = $me.find(Identifiers.icon);
-
-      $me.prop('disabled', true).addClass('disabled');
-      Icons.getIcon('spinner-circle-dark', Icons.sizes.small, null, 'disabled').done((icon: string): void => {
-        $iconElement.replaceWith(icon);
-      });
-
-      this.sendClearCacheRequest(id).always((): void => {
-        Icons.getIcon('actions-system-cache-clear', Icons.sizes.small).done((icon: string): void => {
-          $me.find(Identifiers.icon).replaceWith(icon);
-        });
-        $me.prop('disabled', false).removeClass('disabled');
-      });
-    });
+  private static setDisabled(element: HTMLButtonElement, isDisabled: boolean): void {
+    element.disabled = isDisabled;
+    element.classList.toggle('disabled', isDisabled);
   }
 
   /**
    * Send an AJAX request to clear a page's cache
    *
    * @param {number} pageId
+   * @return Promise<AjaxResponse>
    */
-  private sendClearCacheRequest(pageId: number): JQueryXHR {
-    return $.ajax({
-      url: TYPO3.settings.ajaxUrls.web_list_clearpagecache,
-      data: {
-        id: pageId,
-      },
-      cache: false,
-      dataType: 'json',
-      success: (data: any): void => {
-        if (data.success === true) {
-          Notification.success(data.title, data.message, 1);
-        } else {
-          Notification.error(data.title, data.message, 1);
-        }
-      },
-      error: (): void => {
-        Notification.error(
-          'Clearing page caches went wrong on the server side.',
-        );
-      },
+  private static sendClearCacheRequest(pageId: number): Promise<AjaxResponse> {
+    const request = new AjaxRequest(TYPO3.settings.ajaxUrls.web_list_clearpagecache).withQueryArguments({id: pageId}).get({cache: 'no-cache'});
+    request.then(async (response: AjaxResponse): Promise<void> => {
+      const data = await response.resolve();
+      if (data.success === true) {
+        Notification.success(data.title, data.message, 1);
+      } else {
+        Notification.error(data.title, data.message, 1);
+      }
+    }, (): void => {
+      Notification.error(
+        'Clearing page caches went wrong on the server side.',
+      );
     });
+
+    return request;
+  }
+
+  constructor() {
+    this.registerClickHandler();
+  }
+
+  private registerClickHandler(): void {
+    const trigger = document.querySelector(`${Identifiers.clearCache}:not([disabled])`);
+    if (trigger !== null) {
+      new RegularEvent('click', (e: Event): void => {
+        e.preventDefault();
+
+        // The action trigger behaves like a button
+        const me = e.currentTarget as HTMLButtonElement;
+        const id = parseInt(me.dataset.id, 10);
+        ClearCache.setDisabled(me, true);
+
+        Icons.getIcon('spinner-circle-dark', Icons.sizes.small, null, 'disabled').then((icon: string): void => {
+          me.querySelector(Identifiers.icon).outerHTML = icon;
+        });
+
+        ClearCache.sendClearCacheRequest(id).finally((): void => {
+          Icons.getIcon('actions-system-cache-clear', Icons.sizes.small).then((icon: string): void => {
+            me.querySelector(Identifiers.icon).outerHTML = icon;
+          });
+          ClearCache.setDisabled(me, false);
+        })
+      }).bindTo(trigger);
+    }
   }
 }
 
