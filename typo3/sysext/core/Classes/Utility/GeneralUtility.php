dafc3c6ee50a970c8098ab8dc88f35d4715288fa
Benni Mack <benni@typo3.org>
2020-03-08 11:45:40 +0100

    [BUGFIX] setSingletonInstance should respect XCLASS registrations
    GeneralUtility::makeInstance() stores singleton instances in their
    overridden class names, however, when using setSingletonInstance
    (which is used in TSFE) this is not connected, making a setSingletonInstance
    break with another makeInstance call afterwards - returning a new,
    non XCLASSED object.
    
    Resolves: #73671
    Resolves: #80399
    Releases: master, 9.5
    Change-Id: Ibb0005575db3fec55149a0624929c941c1491ed8
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/63618
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Georg Ringer <georg.ringer@gmail.com>
    Tested-by: Andreas Fernandez <a.fernandez@scripting-base.de>
    Reviewed-by: Georg Ringer <georg.ringer@gmail.com>
    Reviewed-by: Oliver Klee <typo3-coding@oliverklee.de>
    Reviewed-by: Oliver Bartsch <bo@cedev.de>
    Reviewed-by: Andreas Fernandez <a.fernandez@scripting-base.de>

--- typo3/sysext/core/Classes/Utility/GeneralUtility.php
+++ typo3/sysext/core/Classes/Utility/GeneralUtility.php
@@ -3549,7 +3549,9 @@ class GeneralUtility
     public static function setSingletonInstance($className, SingletonInterface $instance)
     {
         self::checkInstanceClassName($className, $instance);
-        self::$singletonInstances[$className] = $instance;
+        // Check for XCLASS registration (same is done in makeInstance() in order to store the singleton of the final class name)
+        $finalClassName = self::getClassName($className);
+        self::$singletonInstances[$finalClassName] = $instance;
     }
 
     /**
