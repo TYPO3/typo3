7cc2a367ccd3e65c7dc870b783f52de0e75033d9
Marco Huber <mail@marco-huber.de>
2020-03-14 15:00:19 +0100

    [BUGFIX] Avoid validation failure on multi select fields without option
    Multi Select fields without any option given will be added properly to
    the forms __trustedProperties list and therefor no longer cause
    validation errors.
    
    Resolves: #90761
    Releases: master, 9.5
    Change-Id: I3c73ffbbd38a9a2ae24ed4b271efc1c4ce248d53
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/63791
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Anja Leichsenring <aleichsenring@ab-softlab.de>
    Reviewed-by: Anja Leichsenring <aleichsenring@ab-softlab.de>

--- typo3/sysext/fluid/Tests/Unit/ViewHelpers/Form/SelectViewHelperTest.php
+++ typo3/sysext/fluid/Tests/Unit/ViewHelpers/Form/SelectViewHelperTest.php
@@ -304,6 +304,7 @@ class SelectViewHelperTest extends ViewHelperBaseTestcase
     public function multipleSelectCreatesExpectedOptions()
     {
         $this->tagBuilder = new TagBuilder();
+        $this->viewHelper->expects(self::exactly(3))->method('registerFieldNameForFormTokenGeneration')->with('myName[]');
 
         $this->arguments['options'] = [
             'value1' => 'label1',
@@ -320,6 +321,26 @@ class SelectViewHelperTest extends ViewHelperBaseTestcase
         $result = $this->viewHelper->initializeArgumentsAndRender();
         $expected = '<input type="hidden" name="myName" value="" /><select multiple="multiple" name="myName[]"><option value="value1" selected="selected">label1</option>' . \chr(10) . '<option value="value2">label2</option>' . \chr(10) . '<option value="value3" selected="selected">label3</option>' . \chr(10) . '</select>';
         $this->assertSame($expected, $result);
+    }
+
+    /**
+     * @test
+     */
+    public function multipleSelectWithoutOptionsCreatesExpectedOptions()
+    {
+        $this->tagBuilder = new TagBuilder();
+        $this->viewHelper->expects(self::once())->method('registerFieldNameForFormTokenGeneration')->with('myName[]');
+
+        $this->arguments['options'] = [];
+        $this->arguments['value'] = ['value3', 'value1'];
+        $this->arguments['name'] = 'myName';
+        $this->arguments['multiple'] = true;
+
+        $this->injectDependenciesIntoViewHelper($this->viewHelper);
+        $this->viewHelper->setTagBuilder($this->tagBuilder);
+        $result = $this->viewHelper->initializeArgumentsAndRender();
+        $expected = '<input type="hidden" name="myName" value="" /><select multiple="multiple" name="myName[]"></select>';
+        self::assertSame($expected, $result);
     }
 
     /**
