5abf6c38408adc6b735a3707515b2573c2cbd6da
Georg Ringer <georg.ringer@gmail.com>
2020-03-18 13:52:08 +0100

    [BUGFIX] Allow setting categories containing spaces
    Partly revert #89956 to reallow category titles in extension settings
    which contain a space.
    
    Resolves: #90776
    Releases: master
    Change-Id: I93358e1c10e502ba9c48402a78927e34c240215a
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/63764
    Tested-by: Andreas Fernandez <a.fernandez@scripting-base.de>
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Susanne Moog <look@susi.dev>
    Reviewed-by: Andreas Fernandez <a.fernandez@scripting-base.de>
    Reviewed-by: Oliver Bartsch <bo@cedev.de>
    Reviewed-by: Susanne Moog <look@susi.dev>

---
+++ typo3/sysext/install/Classes/ViewHelpers/Format/NoSpaceViewHelper.php
@@ -0,0 +1,53 @@
+<?php
+declare(strict_types = 1);
+namespace TYPO3\CMS\Install\ViewHelpers\Format;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+use TYPO3Fluid\Fluid\Core\ViewHelper\Traits\CompileWithRenderStatic;
+
+/**
+ * Remove all spaces in a string
+ *
+ * @internal
+ */
+class NoSpaceViewHelper extends AbstractViewHelper
+{
+    use CompileWithRenderStatic;
+
+    /**
+     * Initialize arguments
+     */
+    public function initializeArguments(): void
+    {
+        parent::initializeArguments();
+        $this->registerArgument('value', 'string', '', true);
+    }
+
+    /**
+     * Render a string with whitespaces stripped
+     *
+     * @param array $arguments
+     * @param \Closure $renderChildrenClosure
+     * @param RenderingContextInterface $renderingContext
+     *
+     * @return string
+     */
+    public static function renderStatic(array $arguments, \Closure $renderChildrenClosure, RenderingContextInterface $renderingContext): string
+    {
+        return preg_replace('/\s+/', '', $arguments['value']);
+    }
+}
