3e04c803f74cd7877e2e5533d56af287600743cd
Susanne Moog <look@susi.dev>
2020-03-19 10:18:17 +0100

    [BUGFIX] Ensure cache tags are available on cached pages
    When fetching a page from cache, the information which cache
    tags were used are not available anymore for any hooks in extensions
    (e.g. static_filecache).
    
    Resolves: #90775
    Releases: master, 9.5
    Change-Id: Ib1ca0f256411a2d979113ddbb0ef128bc79a0197
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/63751
    Tested-by: Tobias Gaertner <tobias.gaertner@benaja-websolutions.com>
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Benni Mack <benni@typo3.org>
    Reviewed-by: Tobias Gaertner <tobias.gaertner@benaja-websolutions.com>
    Reviewed-by: Benni Mack <benni@typo3.org>

--- typo3/sysext/frontend/Classes/Controller/TypoScriptFrontendController.php
+++ typo3/sysext/frontend/Classes/Controller/TypoScriptFrontendController.php
@@ -2454,6 +2454,8 @@ class TypoScriptFrontendController implements LoggerAwareInterface
                     // Setting flag, so we know, that some cached content has been loaded
                     $this->cacheContentFlag = true;
                     $this->cacheExpires = $row['expires'];
+                    // Restore the current tags as they can be retrieved by getPageCacheTags()
+                    $this->pageCacheTags = $row['cacheTags'] ?? [];
 
                     // Restore page title information, this is needed to generate the page title for
                     // partially cached pages.
@@ -3386,6 +3388,8 @@ class TypoScriptFrontendController implements LoggerAwareInterface
             $tags = GeneralUtility::trimExplode(',', $this->page['cache_tags'], true);
             $this->pageCacheTags = array_merge($this->pageCacheTags, $tags);
         }
+        // Add the cache themselves as well, because they are fetched by getPageCacheTags()
+        $cacheData['cacheTags'] = $this->pageCacheTags;
         $this->pageCache->set($this->newHash, $cacheData, $this->pageCacheTags, $expirationTstamp - $GLOBALS['EXEC_TIME']);
     }
 
