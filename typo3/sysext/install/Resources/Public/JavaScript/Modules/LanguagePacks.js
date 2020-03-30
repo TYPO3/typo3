49bca73dd3cabe3bcf292964e4ce94aa97bb9a1a
Georg Ringer <georg.ringer@gmail.com>
2020-03-30 11:08:18 +0200

    [BUGFIX] Always show download icons in language pack module
    The Install Tool module "Manage Language Packs" is improved by the
    following changes:
    
    * If all language packs of an extension are available, the download icon
    is still shown to make downloads of a single language pack per extension
    possible.
    * The icon itself changed from refresh to download as there is no
    refresh possible. There is no lock on a specific date or version to
    reload language packs. Every download can contain new or changed
    translations.
    
    Resolves: #86535
    Releases: master, 9.5
    Change-Id: I130c169b933b6e4b13c533928a46984bcb93979e
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/63982
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Benni Mack <benni@typo3.org>
    Reviewed-by: Benni Mack <benni@typo3.org>

--- typo3/sysext/install/Resources/Public/JavaScript/Modules/LanguagePacks.js
+++ typo3/sysext/install/Resources/Public/JavaScript/Modules/LanguagePacks.js
@@ -39,7 +39,6 @@ define([
     selectorDeactivateLanguageIcon: '#t3js-languagePacks-deactivate-icon',
     selectorUpdate: '.t3js-languagePacks-update',
     selectorLanguageUpdateIcon: '#t3js-languagePacks-languageUpdate-icon',
-    selectorExtensionPackMissesIcon: '#t3js-languagePacks-extensionPack-misses-icon',
     selectorNotifications: '.t3js-languagePacks-notifications',
 
     currentModal: {},
@@ -424,11 +423,9 @@ define([
 
     extensionMatrixHtml: function(data) {
       var securityUtility = new SecurityUtility();
-      var packMissesIcon = this.currentModal.find(this.selectorExtensionPackMissesIcon).html();
       var updateIcon = this.currentModal.find(this.selectorLanguageUpdateIcon).html();
       var tooltip = '';
       var extensionTitle = '';
-      var allPackagesExist = true;
       var rowCount = 0;
       var $markupContainer = $('<div>');
 
@@ -455,15 +452,6 @@ define([
 
       var $tbody = $('<tbody>');
       data.extensions.forEach(function(extension) {
-        allPackagesExist = true;
-        extension.packs.forEach(function(pack) {
-          if (pack.exists === false) {
-            allPackagesExist = false;
-          }
-        });
-        if (allPackagesExist === true) {
-          return;
-        }
         rowCount++;
         if (extension.icon !== '') {
           extensionTitle = $('<span>').append(
@@ -491,16 +479,22 @@ define([
             } else {
               tooltip = 'Language pack not downloaded. Click to download';
             }
-            $column.append(
-              $('<a>', {
-                'class': 'btn btn-default t3js-languagePacks-update',
-                'data-extension': extension.key,
-                'data-iso': pack.iso,
-                'data-toggle': 'tooltip',
-                'title': securityUtility.encodeHtml(tooltip),
-              }).append(packMissesIcon)
-            );
+          } else {
+            if (pack.lastUpdate === null) {
+              tooltip = 'Downloaded. Click to renew';
+            } else {
+              tooltip = 'Language pack downloaded at ' + pack.lastUpdate + '. Click to renew';
+            }
           }
+          $column.append(
+            $('<a>', {
+              'class': 'btn btn-default t3js-languagePacks-update',
+              'data-extension': extension.key,
+              'data-iso': pack.iso,
+              'data-toggle': 'tooltip',
+              'title': securityUtility.encodeHtml(tooltip),
+            }).append(updateIcon)
+          );
         });
         $tbody.append($tr);
       });
