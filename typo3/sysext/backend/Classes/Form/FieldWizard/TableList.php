70fd2f944ff190814f422ae2b2b7eb385143d5f6
Oliver Bartsch <bo@cedev.de>
2020-04-04 19:20:10 +0200

    [TASK] Respect disabled ElementBrowser also in TableList
    The ElementBrowser which is typically used for type=group,
    can be disabled via the `fieldControl`.
    
    This configuration is now also respected by the TableList
    buttons, which are also links for the ElementBrowser.
    
    Resolves: #88979
    Releases: master, 9.5
    Change-Id: Ib48586207480e4e61403ad6b0b694b51ee6e38c2
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/64076
    Tested-by: Josef Glatz <josefglatz@gmail.com>
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Oliver Bartsch <bo@cedev.de>
    Tested-by: Benni Mack <benni@typo3.org>
    Reviewed-by: Josef Glatz <josefglatz@gmail.com>
    Reviewed-by: Oliver Bartsch <bo@cedev.de>
    Reviewed-by: Benni Mack <benni@typo3.org>

--- typo3/sysext/backend/Classes/Form/FieldWizard/TableList.php
+++ typo3/sysext/backend/Classes/Form/FieldWizard/TableList.php
@@ -44,7 +44,7 @@ class TableList extends AbstractNode
         $itemName = $parameterArray['itemFormElName'];
 
         if (empty($config['allowed']) || !is_string($config['allowed']) || !isset($config['internal_type']) || $config['internal_type'] !== 'db') {
-            // No handling if the field has no, or funny "allowed" setting, and if internal_type is not "db"
+            // No handling if the field has no, or funny "allowed" setting and if internal_type is not "db"
             return $result;
         }
 
@@ -59,10 +59,18 @@ class TableList extends AbstractNode
                 $allowedTablesHtml[] = '</span>';
             } else {
                 $label = $languageService->sL($GLOBALS['TCA'][$tableName]['ctrl']['title']);
-                $allowedTablesHtml[] = '<a href="#" class="btn btn-default t3js-element-browser" data-mode="db" data-params="' . htmlspecialchars($itemName . '|||' . $tableName) . '">';
-                $allowedTablesHtml[] =  $iconFactory->getIconForRecord($tableName, [], Icon::SIZE_SMALL)->render();
-                $allowedTablesHtml[] =  htmlspecialchars($label);
-                $allowedTablesHtml[] = '</a>';
+                $icon = $iconFactory->getIconForRecord($tableName, [], Icon::SIZE_SMALL)->render();
+                if ((bool)($config['fieldControl']['elementBrowser']['disabled'] ?? false)) {
+                    $allowedTablesHtml[] = '<span class="tablelist-item-nolink">';
+                    $allowedTablesHtml[] =  $icon;
+                    $allowedTablesHtml[] =  htmlspecialchars($label);
+                    $allowedTablesHtml[] = '</span>';
+                } else {
+                    $allowedTablesHtml[] = '<a href="#" class="btn btn-default t3js-element-browser" data-mode="db" data-params="' . htmlspecialchars($itemName . '|||' . $tableName) . '">';
+                    $allowedTablesHtml[] =  $icon;
+                    $allowedTablesHtml[] =  htmlspecialchars($label);
+                    $allowedTablesHtml[] = '</a>';
+                }
             }
         }
 
