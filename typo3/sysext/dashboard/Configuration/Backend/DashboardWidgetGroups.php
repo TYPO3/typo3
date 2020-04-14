0b65470d8e9a271e58e942ad294276483b7e7b4e
Richard Haeser <richard@maxserv.com>
2020-04-14 21:29:46 +0200

    [TASK] Renaming of TYPO3 widget and TYPO3 widget group
    As the title TYPO3 for a widget as well as a widget group is not
    giving a lot of information about the purpose of the widget and
    the group, both the widget and the widget group are renamed.
    
    The widget is called About TYPO3 now. The group is called TYPO3
    community now. Additionally a group called News is added and
    both the TYPO3 News and TYPO3 security advisories are moved to
    that group.
    
    The TYPO3 Community group will remain to make sure no installations
    will break by removing a widget group. The group won't show up
    when no widgets are in this group though.
    
    Resolves: #90865
    Releases: master
    Change-Id: I468692edfafc74597d3f21b1ecb4d90cf26ad41a
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/64171
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Georg Ringer <georg.ringer@gmail.com>
    Tested-by: Richard Haeser <richard@maxserv.com>
    Reviewed-by: Georg Ringer <georg.ringer@gmail.com>
    Reviewed-by: Björn Jacob <bjoern.jacob@tritum.de>
    Reviewed-by: Richard Haeser <richard@maxserv.com>

--- typo3/sysext/dashboard/Configuration/Backend/DashboardWidgetGroups.php
+++ typo3/sysext/dashboard/Configuration/Backend/DashboardWidgetGroups.php
@@ -10,6 +10,9 @@ return [
     'typo3' => [
         'title' => 'LLL:EXT:dashboard/Resources/Private/Language/locallang.xlf:widget_group.typo3',
     ],
+    'news' => [
+        'title' => 'LLL:EXT:dashboard/Resources/Private/Language/locallang.xlf:widget_group.news',
+    ],
     'documentation' => [
         'title' => 'LLL:EXT:dashboard/Resources/Private/Language/locallang.xlf:widget_group.documentation',
     ],
