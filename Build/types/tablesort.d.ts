7519d7bf01dfe5a6f8203844b9aa2c8df59358a9
Andreas Fernandez <a.fernandez@scripting-base.de>
2020-04-23 17:57:20 +0200

    [BUGFIX] Re-introduce sorting for updateable and (un)installed extensions
    While migrating DataTables to tablesort, the sorting for updateable or
    (un)installed extensions went lost. This patch adds the ability to sort
    these columns again.
    
    Resolves: #91182
    Related: #86343
    Releases: master
    Change-Id: Ia33c3dcc789967a984090ccf26150f6d319e85e9
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/64310
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Josef Glatz <josefglatz@gmail.com>
    Tested-by: Benni Mack <benni@typo3.org>
    Reviewed-by: Josef Glatz <josefglatz@gmail.com>
    Reviewed-by: Benni Mack <benni@typo3.org>

--- Build/types/tablesort.d.ts
+++ Build/types/tablesort.d.ts
@@ -3,4 +3,5 @@ interface Tablesort {
 
 declare const Tablesort: {
   new(table: Element, options?: {[key: string]: any}): Tablesort;
+  extend(name: string, pattern: Function, sort: Function): void;
 }
