1fc9f156fa3783e1f3327afa2ec772b8253c379f
Benni Mack <benni@typo3.org>
2020-04-28 11:22:07 +0200

    [RELEASE] Release of TYPO3 9.5.16
    Change-Id: Id3abd17d74ad904873cf1a43514fb91b02566f4a
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/64337
    Tested-by: Benni Mack <benni@typo3.org>
    Reviewed-by: Benni Mack <benni@typo3.org>

--- typo3/sysext/core/Classes/Information/Typo3Version.php
+++ typo3/sysext/core/Classes/Information/Typo3Version.php
@@ -20,7 +20,7 @@ namespace TYPO3\CMS\Core\Information;
  */
 class Typo3Version
 {
-    protected const VERSION = '9.5.16-dev';
+    protected const VERSION = '9.5.16';
     protected const BRANCH = '9.5';
 
     /**
