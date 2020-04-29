32d9f05c3dbc26260f551aa17362b7df0a6983c1
Tymoteusz Motylewski <t.motylewski@gmail.com>
2020-04-29 16:47:06 +0200

    [BUGFIX] Reintroduce filtering out non accessible pages on query time
    Filter out pages user has no access to on query time in page tree.
    This patch reintroduce a change which was reverted with
    https://review.typo3.org/c/Packages/TYPO3.CMS/+/64369
    
    Resolves: #91221
    Related: #90880
    Related: #91348
    Releases: master, 10.4, 9.5
    Change-Id: Id90752c331bc6fc12b0d3a7d047adacf08cb7804
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/64346
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Uwe Trotzek <trotzek@citeq.de>
    Tested-by: Richard Haeser <richard@maxserv.com>
    Reviewed-by: Benni Mack <benni@typo3.org>
    Reviewed-by: Richard Haeser <richard@maxserv.com>

--- typo3/sysext/backend/Classes/Tree/Repository/PageTreeRepository.php
+++ typo3/sysext/backend/Classes/Tree/Repository/PageTreeRepository.php
@@ -17,6 +17,7 @@ declare(strict_types=1);
 
 namespace TYPO3\CMS\Backend\Tree\Repository;
 
+use TYPO3\CMS\Backend\Utility\BackendUtility;
 use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Database\Query\Restriction\DeletedRestriction;
 use TYPO3\CMS\Core\Database\Query\Restriction\WorkspaceRestriction;
@@ -120,9 +121,9 @@ class PageTreeRepository
      * @param callable $callback a callback to be used to check for permissions and filter out pages not to be included.
      * @return array
      */
-    public function getTree(int $entryPoint, callable $callback = null): array
+    public function getTree(int $entryPoint, callable $callback = null, array $dbMounts = []): array
     {
-        $this->fetchAllPages();
+        $this->fetchAllPages($dbMounts);
         if ($entryPoint === 0) {
             $tree = $this->fullPageTree;
         } else {
@@ -159,7 +160,7 @@ class PageTreeRepository
      *
      * @return array the full page tree of the whole installation
      */
-    protected function fetchAllPages(): array
+    protected function fetchAllPages($dbMounts): array
     {
         if (!empty($this->fullPageTree)) {
             return $this->fullPageTree;
@@ -186,6 +187,19 @@ class PageTreeRepository
             )
             ->execute()
             ->fetchAll();
+
+        $ids = array_column($pageRecords, 'uid');
+        foreach ($dbMounts as $mount) {
+            $entryPointRootLine = BackendUtility::BEgetRootLine($mount, '', false, $this->fields);
+            foreach ($entryPointRootLine as $page) {
+                $pageId = (int)$page['uid'];
+                if (in_array($pageId, $ids) || $pageId === 0) {
+                    continue;
+                }
+                $pageRecords[] = $page;
+                $ids[] = $pageId;
+            }
+        }
 
         $livePagePids = [];
         $movePlaceholderData = [];
