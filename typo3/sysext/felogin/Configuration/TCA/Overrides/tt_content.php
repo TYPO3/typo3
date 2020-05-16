a3f569172864722c05ed223d2f612f985b00f21c
Oliver Bartsch <bo@cedev.de>
2020-05-16 11:38:11 +0200

    [TASK] Move felogin pi_flexform into plugin tab
    The plugin configuration of felogin is moved to the
    common used "Plugin" tab, to unify the position
    through the core.
    
    Resolves: #91416
    Releases: master, 10.4
    Change-Id: Ibbf26be6787795021e7ba6a0d3138bbf18121087
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/64500
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Benni Mack <benni@typo3.org>
    Reviewed-by: Benni Mack <benni@typo3.org>

--- typo3/sysext/felogin/Configuration/TCA/Overrides/tt_content.php
+++ typo3/sysext/felogin/Configuration/TCA/Overrides/tt_content.php
@@ -24,6 +24,7 @@ call_user_func(static function () {
         --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
             --palette--;;general,
             --palette--;;headers,
+        --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.plugin,
             pi_flexform,
         --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
             --palette--;;frames,
