079cc536973332a4784c0f199bd8936311ab4aa5
Oliver Hader <oliver@typo3.org>
2020-06-15 18:04:45 +0200

    [BUGFIX] Adjust processing order of routes during URL generation
    Prior to this patch routes were processed in reverse definition order.
    Routes defined last came first. Depending on the existence of variable
    defaults this behavior produced a couple of unexpected results.
    
      first:
        routePath: '/route/{a}/{b}'
      second:
        routePath: '/route/{c}'
        defaults:
          c: '0'
    
    The example above, processed in reverse order and having parameters
    `a` and `b` given, still resulted in `second` route being used since
    the missing parameter `c` was defined by corresponding variable default.
    
    This change adjusts the order of routes depending on given parameters,
    completeness of a route (when all parameters are given, even defaults).
    Sorting is adjusted based on the following criteria:
    
    * default routes (e.g. `/my-page`) - processed later
    * static routes (e.g. `/my-page/list`) - processed later
    * all variables are given (complete) - processed earlier
      (e.g. parameters `a` and `b` are given for route `/route/{a}/{b}`)
    * all mandatory variables are given (complete) - processed earlier
    * less missing variable defaults - processed earlier
    * less variable defaults amount - processed earlier
    
    Tests in class `RouteSorterTest` provide more examples & details.
    
    Resolves: #90924
    Releases: master, 10.4, 9.5
    Change-Id: I26f56e6905472a501ff487295da23b3bc3b5c77e
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/64877
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Benni Mack <benni@typo3.org>
    Tested-by: Aimeos <aimeos@aimeos.org>
    Tested-by: Oliver Hader <oliver.hader@typo3.org>
    Reviewed-by: Benni Mack <benni@typo3.org>
    Reviewed-by: Aimeos <aimeos@aimeos.org>
    Reviewed-by: Oliver Hader <oliver.hader@typo3.org>

---
+++ typo3/sysext/core/Tests/Unit/Routing/RouteSorterTest.php
@@ -0,0 +1,273 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3\CMS\Core\Tests\Unit\Routing;
+
+use TYPO3\CMS\Core\Routing\Route;
+use TYPO3\CMS\Core\Routing\RouteSorter;
+use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
+
+class RouteSorterTest extends UnitTestCase
+{
+    public function routesAreSortedForGenerationDataProvider(): array
+    {
+        return [
+            'default route only' => [
+                // routes
+                [
+                    $this->createDefaultRoute('/default'),
+                ],
+                // given parameters
+                [],
+                // expected route paths order
+                [
+                    '/default',
+                ]
+            ],
+            'static, default route' => [
+                [
+                    $this->createDefaultRoute('/default-1'),
+                    $this->createRoute('/list'),
+                    $this->createDefaultRoute('/default-2'),
+                ],
+                [],
+                [
+                    '/list',
+                    '/default-1',
+                    '/default-2',
+                ]
+            ],
+            'mandatory, static, default route' => [
+                [
+                    $this->createDefaultRoute('/default'),
+                    $this->createRoute('/list'),
+                    $this->createRoute('/list/{page}', ['page' => 0]),
+                ],
+                [],
+                [
+                    '/list/{page}',
+                    '/list',
+                    '/default',
+                ]
+            ],
+            // not really important, since missing mandatory
+            // variables would have been skipped during generation
+            'ambiguous routes, no parameters, most probable' => [
+                [
+                    $this->createRoute('/list'),
+                    $this->createRoute('/list/{uid}'),
+                    $this->createRoute('/list/{uid}/{category}', ['category' => 0]),
+                    $this->createRoute('/list/{page}', ['page' => 0]),
+                    $this->createRoute('/list/{category}', ['category' => 0]),
+                ],
+                [],
+                [
+                    '/list/{page}', // no parameters given -> defaults take precedence
+                    '/list/{category}', // no parameters given -> defaults take precedence
+                    '/list/{uid}',
+                    '/list/{uid}/{category}',
+                    '/list',
+                ]
+            ],
+            'mandatory first, ambiguous parameters' => [
+                [
+                    $this->createRoute('/list'),
+                    $this->createRoute('/list/{uid}'),
+                    $this->createRoute('/list/{uid}/{category}', ['category' => 0]),
+                    $this->createRoute('/list/{page}', ['page' => 0]),
+                    $this->createRoute('/list/{category}', ['category' => 0]),
+                ],
+                [
+                    'uid' => 123,
+                    'page' => 1,
+                ],
+                [
+                    '/list/{uid}', // value for {uid} given, complete mandatory first -> takes precedence
+                    '/list/{page}', // value for {page} given, complete first -> takes precedence
+                    '/list/{uid}/{category}',
+                    '/list/{category}',
+                    '/list',
+                ]
+            ],
+            'complete first, ambiguous parameters #1' => [
+                [
+                    $this->createRoute('/list'),
+                    $this->createRoute('/list/{uid}'),
+                    $this->createRoute('/list/{uid}/{category}', ['category' => 0]),
+                    $this->createRoute('/list/{page}', ['page' => 0]),
+                    $this->createRoute('/list/{category}', ['category' => 0]),
+                ],
+                [
+                    'category' => 1,
+                    'page' => 1,
+                ],
+                [
+                    '/list/{page}', // value for default {page} given, complete first -> takes precedence
+                    '/list/{category}',
+                    '/list/{uid}',
+                    '/list/{uid}/{category}',
+                    '/list',
+                ]
+            ],
+            'complete first, ambiguous parameters #2' => [
+                [
+                    $this->createRoute('/list'),
+                    $this->createRoute('/list/{uid}'),
+                    $this->createRoute('/list/{uid}/{category}', ['category' => 0]),
+                    $this->createRoute('/list/{page}', ['page' => 0]),
+                    $this->createRoute('/list/{category}', ['category' => 0]),
+                ],
+                [
+                    'uid' => 123,
+                    'page' => 1,
+                    'category' => 2,
+                ],
+                [
+                    '/list/{uid}/{category}', // values for {uid} and {category} given, complete first -> takes precedence
+                    '/list/{uid}',
+                    '/list/{page}',
+                    '/list/{category}',
+                    '/list',
+                ]
+            ],
+            // not really important, just to show order is kept
+            'defaults only, no parameters given #1' => [
+                [
+                    $this->createRoute('/list/{defA}/{defB}/{defC}', ['defA' => 0, 'defB' => 0, 'defC' => 0]),
+                    $this->createRoute('/list/{defD}/{defE}/{defF}', ['defD' => 0, 'defE' => 0, 'defF' => 0]),
+                ],
+                [
+                ],
+                [
+                    '/list/{defA}/{defB}/{defC}',
+                    '/list/{defD}/{defE}/{defF}',
+                ]
+            ],
+            // not really important, just to show order is kept
+            'defaults only, no parameters given #2' => [
+                [
+                    $this->createRoute('/list/{defD}/{defE}/{defF}', ['defD' => 0, 'defE' => 0, 'defF' => 0]),
+                    $this->createRoute('/list/{defA}/{defB}/{defC}', ['defA' => 0, 'defB' => 0, 'defC' => 0]),
+                ],
+                [
+                ],
+                [
+                    '/list/{defD}/{defE}/{defF}',
+                    '/list/{defA}/{defB}/{defC}',
+                ]
+            ],
+            'defaults only, {defF} given, best match' => [
+                [
+                    $this->createRoute('/list/{defA}/{defB}/{defC}', ['defA' => 0, 'defB' => 0, 'defC' => 0]),
+                    $this->createRoute('/list/{defD}/{defE}/{defF}', ['defD' => 0, 'defE' => 0, 'defF' => 0]),
+                ],
+                [
+                    'defF' => 1,
+                ],
+                [
+                    '/list/{defD}/{defE}/{defF}', // {defF} given, best match -> takes precedence
+                    '/list/{defA}/{defB}/{defC}',
+                ]
+            ],
+            'mixed variables, ambiguous parameters, complete mandatory first #1' => [
+                [
+                    $this->createRoute('/list/{d}/{e}/{defF}', ['defF' => 0]),
+                    $this->createRoute('/list/{a}/{defB}/{defC}', ['defB' => 0, 'defC' => 0]),
+                ],
+                [
+                    'a' => 1,
+                    'd' => 1,
+                    'defF' => 1,
+                ],
+                [
+                    '/list/{a}/{defB}/{defC}', // mandatory {a} given, complete mandatory first -> takes precedence
+                    '/list/{d}/{e}/{defF}',
+                ]
+            ],
+            'mixed variables, ambiguous parameters, complete mandatory first #2' => [
+                [
+                    $this->createRoute('/list/{a}/{defB}/{defC}', ['defB' => 0, 'defC' => 0]),
+                    $this->createRoute('/list/{d}/{e}/{defF}', ['defF' => 0]),
+                ],
+                [
+                    'd' => 1,
+                    'e' => 1,
+                    'defB' => 1,
+                    'defC' => 1,
+                ],
+                [
+                    '/list/{d}/{e}/{defF}', // mandatory {d} and {e} given, complete mandatory first -> takes precedence
+                    '/list/{a}/{defB}/{defC}',
+                ]
+            ],
+            'mixed variables, ambiguous parameters, complete first' => [
+                [
+                    $this->createRoute('/list/{d}/{e}/{defF}', ['defF' => 0]),
+                    $this->createRoute('/list/{a}/{defB}/{defC}', ['defB' => 0, 'defC' => 0]),
+                ],
+                [
+                    'd' => 1,
+                    'e' => 1,
+                    'a' => 1,
+                    'defB' => 1,
+                    'defC' => 1,
+                ],
+                [
+                    '/list/{a}/{defB}/{defC}', // all parameters given, complete first -> takes precedence
+                    '/list/{d}/{e}/{defF}',
+                ]
+            ],
+        ];
+    }
+
+    /**
+     * @param Route[] $givenRoutes
+     * @param array<string, string> $givenParameters
+     * @param string[] $expectation
+     *
+     * @test
+     * @dataProvider routesAreSortedForGenerationDataProvider
+     */
+    public function routesAreSortedForGeneration(array $givenRoutes, array $givenParameters, array $expectation): void
+    {
+        $sorter = (new RouteSorter())
+            ->withRoutes($givenRoutes)
+            ->withOriginalParameters($givenParameters);
+        $routes = $sorter->sortRoutesForGeneration()->getRoutes();
+        $routePaths = array_map([$this, 'getRoutePath'], array_values($routes));
+        self::assertSame($expectation, $routePaths);
+    }
+
+    private function getRoutePath(Route $route): string
+    {
+        return $route->getPath();
+    }
+
+    private function createRoute(string $path, array $defaults = []): Route
+    {
+        $route = new Route($path);
+        $route->setDefaults($defaults);
+        return $route;
+    }
+
+    private function createDefaultRoute(string $path): Route
+    {
+        $route = new Route($path);
+        $route->setOption('_isDefault', true);
+        return $route;
+    }
+}
