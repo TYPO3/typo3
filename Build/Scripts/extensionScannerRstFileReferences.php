2cd7b2647a42b6d6356624ce1337daea8054a8dd
Anja Leichsenring <aleichsenring@ab-softlab.de>
2020-07-25 10:02:40 +0200

    [BUGFIX] Respect different matcher config of ConstructorArgumentMatcher
    The extensionScannerRstFileReferences check expects rstFiles listed
    in first level of the configuration array. ConstructorArgumentMatcher
    puts them in the second level. Thus, no files are found during the
    check, indicated by PHP warnings.
    
    The rstFiles given to ConstructorArgumentMatcher have never been
    checked due to this incompatibility.
    
    Resolves: #91866
    Relates: #90722
    Releases: master, 10.4, 9.5
    Change-Id: I36abbef7e1eb5af372a8655d4a3d593f298fa404
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/65119
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Oliver Hader <oliver.hader@typo3.org>
    Reviewed-by: Oliver Hader <oliver.hader@typo3.org>

--- Build/Scripts/extensionScannerRstFileReferences.php
+++ Build/Scripts/extensionScannerRstFileReferences.php
@@ -1,5 +1,6 @@
 #!/usr/bin/env php
 <?php
+declare(strict_types = 1);
 /*
  * This file is part of the TYPO3 CMS project.
  *
@@ -13,57 +14,116 @@
  * The TYPO3 project - inspiring people to share!
  */
 
+require __DIR__ . '/../../vendor/autoload.php';
+
+if (PHP_SAPI !== 'cli') {
+    die('Script must be called from command line.' . chr(10));
+}
+
 /**
  * Core integrity test script:
  *
  * Find all ReST files configured in EXT:install/Configuration/ExtensionScanner/Php
  * and verify they exist in EXT:core/Documentation/Changelog
  */
-call_user_func(function () {
-    if (php_sapi_name() !== 'cli') {
-        die('Script must be called from command line.' . chr(10));
-    }
-    require __DIR__ . '/../../vendor/autoload.php';
+class ExtensionScannerRstFileReferencesChecker
+{
+    /**
+     * @var array<string, string>
+     */
+    private $invalidRestFiles = [];
 
-    $finder = new Symfony\Component\Finder\Finder();
-    $matcherConfigurationFiles = $finder->files()
-        ->in(__DIR__ . '/../../typo3/sysext/install/Configuration/ExtensionScanner/Php');
+    /**
+     * @var array<string, string>
+     */
+    private $validRestFiles = [];
 
-    $invalidRestFiles = [];
-    $validRestFiles = [];
-    foreach ($matcherConfigurationFiles as $matcherConfigurationFile) {
-        /** @var SplFileInfo $matcherConfigurationFile */
-        $matcherConfigurations = require $matcherConfigurationFile->getPathName();
-        foreach ($matcherConfigurations as $matcherConfiguration) {
-            foreach ($matcherConfiguration['restFiles'] as $restFile) {
-                if (in_array($restFile, $validRestFiles, true)) {
-                    // Local cache as guard to not check same file over and over again
-                    continue;
-                }
-                $restFinder = new \Symfony\Component\Finder\Finder();
-                $restFileLocation = $restFinder->files()
-                    ->in(__DIR__ . '/../../typo3/sysext/core/Documentation/Changelog')
-                    ->name($restFile);
-                if ($restFileLocation->count() !== 1) {
-                    $invalidRestFiles[] = [
-                        'configurationFile' => $matcherConfigurationFile->getPathName(),
-                        'invalidFile' => $restFile,
-                    ];
+    /**
+     * @var array<string, int>
+     */
+    private $existingRestFiles = [];
+
+    public function check(): int
+    {
+        $this->existingRestFiles = $this->fetchExistingRstFiles();
+
+        $finder = new Symfony\Component\Finder\Finder();
+        $matcherConfigurationFiles = $finder->files()
+            ->in(__DIR__ . '/../../typo3/sysext/install/Configuration/ExtensionScanner/Php');
+
+        foreach ($matcherConfigurationFiles as $matcherConfigurationFile) {
+            /** @var SplFileInfo $matcherConfigurationFile */
+            $matcherConfigurations = require $matcherConfigurationFile->getPathName();
+            foreach ($matcherConfigurations as $matcherConfiguration) {
+                if (!isset($matcherConfiguration['restFiles'])) {
+                    // `ConstructorArgumentsMatcher` is using an additional level which has to be checked
+                    foreach ($matcherConfiguration as $nestedMatcherConfiguration) {
+                        $this->checkRstFiles($nestedMatcherConfiguration, $matcherConfigurationFile);
+                    }
                 } else {
-                    $validRestFiles[] = $restFile;
+                    $this->checkRstFiles($matcherConfiguration, $matcherConfigurationFile);
                 }
             }
         }
+
+        if (empty($this->invalidRestFiles)) {
+            return 0; // we are done, nothing found. Script will exit with 0
+        }
+        echo "ReST files references in extension scanner configuration that could not be found:\n";
+        foreach ($this->invalidRestFiles as $invalid) {
+            printf(
+                " - '%s' in file '%s'\n",
+                $invalid['invalidFile'],
+                $invalid['configurationFile']
+            );
+        }
+        echo "\n";
+        return 1; // we got findings, script will exit with 1
     }
 
-    if (empty($invalidRestFiles)) {
-        exit(0);
+    private function fetchExistingRstFiles(): array
+    {
+        $finder = new Symfony\Component\Finder\Finder();
+        $finder->files()
+            ->name('*.rst')
+            ->notName(['Changelog-*.rst', 'Index.rst', 'Master.rst'])
+            ->in(__DIR__ . '/../../typo3/sysext/core/Documentation/Changelog');
+        $fileNames = array_map(
+            function (SplFileInfo $file) {
+                return $file->getFilename();
+            },
+            iterator_to_array($finder)
+        );
+        // remove array keys containing the full file path again
+        $fileNames = array_values($fileNames);
+        // e.g. `['SomeFileA.rst' => 1, 'SomeOtherFile.rst' => 4]` counting occurrences
+        return array_count_values($fileNames);
     }
 
-    foreach ($invalidRestFiles as $invalid) {
-        echo 'Referenced ReST file ' . $invalid['invalidFile']
-            . ' in extension scanner configuration file ' . $invalid['configurationFile']
-            . ' not found' . chr(10) . chr(10);
+    private function checkRstFiles($matcherConfiguration, $matcherConfigurationFile): void
+    {
+        if (!is_array($matcherConfiguration['restFiles'] ?? null)) {
+            throw new \InvalidArgumentException(sprintf(
+                'Configuration has no `restFiles` section defined in %s',
+                $matcherConfigurationFile
+            ));
+        }
+        foreach ($matcherConfiguration['restFiles'] as $restFile) {
+            if (in_array($restFile, $this->validRestFiles, true)) {
+                // Local cache as guard to not check same file over and over again
+                continue;
+            }
+            if (($this->existingRestFiles[$restFile] ?? 0) === 1) {
+                $this->validRestFiles[] = $restFile;
+            } else {
+                $this->invalidRestFiles[] = [
+                    'configurationFile' => $matcherConfigurationFile->getPathName(),
+                    'invalidFile' => $restFile,
+                ];
+            }
+        }
     }
-    exit(1);
-});
+}
+
+$checker = new ExtensionScannerRstFileReferencesChecker();
+exit($checker->check());
