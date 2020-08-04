e44e10f04b497cecdf89d456d8dd4c8e8276c4c1
Benni Mack <benni@typo3.org>
2020-08-04 09:43:41 +0200

    [BUGFIX] Improve error message when deleting a page with disallowed tables
    When deleting a page with records an editor does not have access to,
    the error message "Attempt to delete records from disallowed tables"
    is shown which does not give a hint to the editor what tables are an issue.
    
    This change improves the error message to show the tables
    that are prohibited.
    
    Resolves: #55307
    Releases: master, 10.4
    Change-Id: Ibe451e7042b9832815e2d4a26104e06adc94656d
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/65165
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Anja Leichsenring <aleichsenring@ab-softlab.de>
    Reviewed-by: Anja Leichsenring <aleichsenring@ab-softlab.de>

--- typo3/sysext/core/Classes/DataHandling/DataHandler.php
+++ typo3/sysext/core/Classes/DataHandling/DataHandler.php
@@ -5135,8 +5135,8 @@ class DataHandler implements LoggerAwareInterface
             $pagesInBranch = [$uid];
         }
 
-        if (!$this->checkForRecordsFromDisallowedTables($pagesInBranch)) {
-            return 'Attempt to delete records from disallowed tables';
+        if ($disallowedTables = $this->checkForRecordsFromDisallowedTables($pagesInBranch)) {
+            return 'Attempt to delete records from disallowed tables (' . implode(', ', $disallowedTables) . ')';
         }
 
         foreach ($pagesInBranch as $pageInBranch) {
@@ -8013,15 +8013,16 @@ class DataHandler implements LoggerAwareInterface
      * Check if there are records from tables on the pages to be deleted which the current user is not allowed to
      *
      * @param int[] $pageIds IDs of pages which should be checked
-     * @return bool Return TRUE, if permission granted
+     * @return string[]|null Return null, if permission granted, otherwise an array with the tables that are not allowed to be deleted
      * @see canDeletePage()
      */
-    protected function checkForRecordsFromDisallowedTables(array $pageIds)
+    protected function checkForRecordsFromDisallowedTables(array $pageIds): ?array
     {
         if ($this->admin) {
-            return true;
+            return null;
         }
 
+        $disallowedTables = [];
         if (!empty($pageIds)) {
             $tableNames = $this->compileAdminTables();
             foreach ($tableNames as $table) {
@@ -8038,11 +8039,11 @@ class DataHandler implements LoggerAwareInterface
                     ->execute()
                     ->fetchColumn(0);
                 if ($count && ($this->tableReadOnly($table) || !$this->checkModifyAccessList($table))) {
-                    return false;
+                    $disallowedTables[] = $table;
                 }
             }
         }
-        return true;
+        return !empty($disallowedTables) ? $disallowedTables : null;
     }
 
     /**
