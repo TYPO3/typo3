941bb150d72e3980a67a8709aa22254548028421
Christian Kuhn <lolli@schwarzbu.ch>
2020-08-06 14:44:16 +0200

    [BUGFIX] Workspace preview shows correct pages in sub menus
    A missing restriction in PageRenderer->getSubpagesForPages()
    leads to the workspace preview of a page changed in a
    workspace to render both live and workspace versions as menu
    items in multi level menus. Adapt the query to fetch the
    correct combination of live and overlay records.
    
    Resolves: #91943
    Related: #89555
    Releases: master, 10.4
    Change-Id: I924803fc2997a9a5c990bc4cf8a628113aae0b22
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/65209
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Benni Mack <benni@typo3.org>
    Reviewed-by: Benni Mack <benni@typo3.org>

--- typo3/sysext/core/Classes/Domain/Repository/PageRepository.php
+++ typo3/sysext/core/Classes/Domain/Repository/PageRepository.php
@@ -32,6 +32,7 @@ use TYPO3\CMS\Core\Database\Query\Restriction\FrontendRestrictionContainer;
 use TYPO3\CMS\Core\Database\Query\Restriction\FrontendWorkspaceRestriction;
 use TYPO3\CMS\Core\Database\Query\Restriction\HiddenRestriction;
 use TYPO3\CMS\Core\Database\Query\Restriction\QueryRestrictionContainerInterface;
+use TYPO3\CMS\Core\Database\Query\Restriction\WorkspaceRestriction;
 use TYPO3\CMS\Core\Error\Http\ShortcutTargetPageNotFoundException;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Core\Utility\MathUtility;
@@ -765,7 +766,9 @@ class PageRepository implements LoggerAwareInterface
     ): array {
         $relationField = $parentPages ? 'pid' : 'uid';
         $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('pages');
-        $queryBuilder->getRestrictions()->removeAll();
+        $queryBuilder->getRestrictions()
+            ->removeAll()
+            ->add(GeneralUtility::makeInstance(WorkspaceRestriction::class, $this->versioningWorkspaceId));
 
         $res = $queryBuilder->select(...GeneralUtility::trimExplode(',', $fields, true))
             ->from('pages')
