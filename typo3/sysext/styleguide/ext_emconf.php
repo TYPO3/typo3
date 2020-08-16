797741ab0c672227cb9700b2766321e9ab86ae42
Christian Kuhn <lolli@schwarzbu.ch>
2020-08-16 18:23:16 +0200

    [TASK] styleguide: Update ext_emconf.php

--- typo3/sysext/styleguide/ext_emconf.php
+++ typo3/sysext/styleguide/ext_emconf.php
@@ -2,11 +2,10 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'TYPO3 CMS Backend Styleguide and Testing use cases',
-    'description' => 'Presents most supported styles for TYPO3 backend modules. Mocks typography, tables, forms, buttons, flash messages and helpers. More at https://github.com/7elix/TYPO3.CMS.Styleguide',
+    'description' => 'Presents most supported styles for TYPO3 backend modules. Mocks typography, tables, forms, buttons, flash messages and helpers. More at https://github.com/TYPO3/styleguide',
     'category' => 'plugin',
-    'author' => 'Felix Kopp',
-    'author_email' => 'felix-source@phorax.com',
-    'author_company' => 'PHORAX',
+    'author' => 'TYPO3 Core Team',
+    'author_email' => 'typo3cms@typo3.org',
     'state' => 'stable',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
