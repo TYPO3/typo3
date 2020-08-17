7d0961e2d8d580c710446435eaebc1169123b203
Kevin Ditscheid <kevin@the-coding-owl.de>
2020-08-17 14:26:34 +0200

    [BUGFIX] Handle missing files in ImageService
    This commit hardens the extbase ImageService to not
    hard crash if $processedImage->getPublicUrl() returns
    a NULL value.
    
    Resolves: #92032
    Releases: master, 10.4
    Change-Id: I09ed122362b6202ee971a4e886490334a25f0502
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/65353
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Markus Klein <markus.klein@typo3.org>
    Tested-by: Daniel Goerz <daniel.goerz@posteo.de>
    Reviewed-by: Markus Klein <markus.klein@typo3.org>
    Reviewed-by: Daniel Goerz <daniel.goerz@posteo.de>

--- typo3/sysext/extbase/Classes/Service/ImageService.php
+++ typo3/sysext/extbase/Classes/Service/ImageService.php
@@ -90,6 +90,11 @@ class ImageService implements SingletonInterface
     public function getImageUri(FileInterface $image, bool $absolute = false): string
     {
         $imageUrl = $image->getPublicUrl();
+        if ($imageUrl === null) {
+            // Image is missing probably, return an empty string instead of parsing
+            return '';
+        }
+
         $parsedUrl = parse_url($imageUrl);
         // no prefix in case of an already fully qualified URL
         if (isset($parsedUrl['host'])) {
@@ -179,11 +184,15 @@ class ImageService implements SingletonInterface
      */
     protected function setCompatibilityValues(ProcessedFile $processedImage): void
     {
-        $imageInfoValues = $this->getCompatibilityImageResourceValues($processedImage);
-        GeneralUtility::makeInstance(AssetCollector::class)->addMedia(
-            $processedImage->getPublicUrl(),
-            $imageInfoValues
-        );
+        $publicUrl = $processedImage->getPublicUrl();
+        if ($publicUrl !== null) {
+            // only add the processed image to AssetCollector if the public url is not NULL
+            $imageInfoValues = $this->getCompatibilityImageResourceValues($processedImage);
+            GeneralUtility::makeInstance(AssetCollector::class)->addMedia(
+                $publicUrl,
+                $imageInfoValues
+            );
+        }
     }
 
     /**
