c9971e5df504e1ef8abed5d1af5d164ac6d2b861
Daniel Windloff <daniel.jc.windloff@googlemail.com>
2020-09-08 07:35:34 +0200

    [TASK] DatabaseRecordList Drop `getVisibleColumns()`
    After the removal of TCA [ctrl][thumbnails] in #92118, the method
    'getVisibleColumns()' isn't used any more. Therefore, it has been
    removed.
    
    Releases: master
    Resolves: #92226
    Change-Id: Iff0ad528e96a0f89fc1f59d9728d9a96374f793f
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/65629
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Andreas Fernandez <a.fernandez@scripting-base.de>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Reviewed-by: Oliver Klee <typo3-coding@oliverklee.de>
    Reviewed-by: Andreas Fernandez <a.fernandez@scripting-base.de>

--- typo3/sysext/recordlist/Classes/RecordList/DatabaseRecordList.php
+++ typo3/sysext/recordlist/Classes/RecordList/DatabaseRecordList.php
@@ -3689,35 +3689,6 @@ class DatabaseRecordList
     }
 
     /**
-     * Flatten palettes into types showitem
-     *
-     * By replacing the palettes in showitem of a TCA type with each palette content, the fields within a palette
-     * can be considered as visible database columns for a backend form.
-     *
-     * @param array $tableTCA
-     * @param string $type
-     * @return string
-     */
-    protected function getVisibleColumns(array $tableTCA, string $type)
-    {
-        $visibleColumns = $tableTCA['types'][$type]['showitem'] ?? '';
-
-        if (strpos($visibleColumns, '--palette--') !== false) {
-            $matches = [];
-            preg_match_all('/--palette--\s*;[^;]*;\s*(\w+)/', $visibleColumns, $matches, PREG_SET_ORDER);
-            if (!empty($matches)) {
-                foreach ($matches as $palette) {
-                    $paletteColumns = $tableTCA['palettes'][$palette[1]]['showitem'] ?? '';
-                    $paletteColumns = rtrim($paletteColumns, ", \t\r\n");
-                    $visibleColumns = str_replace($palette[0], $paletteColumns, $visibleColumns);
-                }
-            }
-        }
-
-        return $visibleColumns;
-    }
-
-    /**
      * Returns the language service
      * @return LanguageService
      */
