4ac31bba52796f788178b76b398d1651fadea393
Georg Ringer <georg.ringer@gmail.com>
2020-09-14 23:09:41 +0200

    [BUGFIX] Increase size of DB field filelink_sorting
    The field ´filelink_sorting` of the DB table tt_content contains
    the sorting field. Its size should be set to 64 characters to be able
    to use any column name for sorting.
    
    Resolves: #92294
    Releases: master, 10.4
    Change-Id: Iea9f7fcc42902222f6ea2683483bb4bddf5d25e6
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/65737
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Benni Mack <benni@typo3.org>
    Reviewed-by: Benni Mack <benni@typo3.org>

--- typo3/sysext/frontend/ext_tables.sql
+++ typo3/sysext/frontend/ext_tables.sql
@@ -122,7 +122,7 @@ CREATE TABLE tt_content (
 	linkToTop tinyint(3) unsigned DEFAULT '0' NOT NULL,
 	file_collections text,
 	filelink_size tinyint(3) unsigned DEFAULT '0' NOT NULL,
-	filelink_sorting varchar(17) DEFAULT '' NOT NULL,
+	filelink_sorting varchar(64) DEFAULT '' NOT NULL,
 	filelink_sorting_direction varchar(4) DEFAULT '' NOT NULL,
 	target varchar(30) DEFAULT '' NOT NULL,
 	date int(10) unsigned DEFAULT '0' NOT NULL,
