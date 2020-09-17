8da21da372460c0ba66facb1f8f69623bd307dd2
Alexander Nitsche <typo3@alexandernitsche.com>
2020-09-17 18:37:31 +0200

    [TASK] Enable EXT:impexp to include presets in export
    Only tables with TCA configurations are available for the export of a
    TYPO3 instance. The stored export configurations - so called presets -
    were missing the TCA configuration and thus had to be exported and
    imported separately in a database client.
    
    This patch adds a TCA configuration for the export configurations and
    thus makes them exportable and importable along with the main dump.
    The presets are saved on PID=0 and hidden from record lists to
    force the user to continue managing them with the export module.
    
    Resolves: #92346
    Releases: master, 10.4
    Change-Id: Ic5a9babc91a93f8bf1561b697c4fca0ad548f734
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/66582
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Anja Leichsenring <aleichsenring@ab-softlab.de>
    Reviewed-by: Anja Leichsenring <aleichsenring@ab-softlab.de>

--- typo3/sysext/impexp/ext_tables.sql
+++ typo3/sysext/impexp/ext_tables.sql
@@ -2,13 +2,10 @@
 # Table structure for table 'tx_impexp_presets'
 #
 CREATE TABLE tx_impexp_presets (
-  uid int(11) NOT NULL auto_increment,
-  user_uid int(11) DEFAULT '0' NOT NULL,
   title varchar(255) DEFAULT '' NOT NULL,
   public tinyint(3) DEFAULT '0' NOT NULL,
   item_uid int(11) DEFAULT '0' NOT NULL,
   preset_data blob,
-  PRIMARY KEY (uid),
   KEY lookup (item_uid)
 );
 
