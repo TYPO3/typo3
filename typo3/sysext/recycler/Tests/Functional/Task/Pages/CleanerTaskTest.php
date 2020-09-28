6312ab5d03eeb42c5a9c3c0e9fafe9245ee2d9a1
Anja Leichsenring <aleichsenring@ab-softlab.de>
2020-09-28 23:01:52 +0200

    [TASK] Move CleanerTask tests to functional tests
    Unit tests requiring database connection and mocking half the world are
    better of to be reimplemented as functional tests.
    This has been done to two of CleanerTask tests, which are easier to read
    and understand now.
    
    Resolves: #92437
    Releases: master, 10.4
    Change-Id: I3c36f2c37050cef3cb1aca96874cb889456552d8
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/65952
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Oliver Bartsch <bo@cedev.de>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Reviewed-by: Oliver Bartsch <bo@cedev.de>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>

---
+++ typo3/sysext/recycler/Tests/Functional/Task/Pages/CleanerTaskTest.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3\CMS\Recycler\Tests\Functional\Task\Pages;
+
+use TYPO3\CMS\Recycler\Task\CleanerTask;
+use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
+
+/**
+ * Test Case
+ */
+class CleanerTaskTest extends FunctionalTestCase
+{
+    protected $coreExtensionsToLoad = ['recycler', 'scheduler'];
+
+    /**
+     * @test
+     */
+    public function taskRemovesDeletedPages()
+    {
+        $this->importCSVDataSet(__DIR__ . '/DataSet/Fixtures/pages.csv');
+        $subject = new CleanerTask();
+        $subject->setTcaTables(['pages']);
+        $result = $subject->execute();
+        $this->assertCSVDataSet('typo3/sysext/recycler/Tests/Functional/Task/Pages/DataSet/Assertion/pages_deleted.csv');
+        self::assertTrue($result);
+    }
+
+    /**
+     * @test
+     */
+    public function taskRemovesOnlyPagesLongerDeletedThanPeriod()
+    {
+        $this->importCSVDataSet(__DIR__ . '/DataSet/Fixtures/pages.csv');
+        $subject = new CleanerTask();
+        $subject->setTcaTables(['pages']);
+
+        // this is when the test was created. One of the fixtures (uid 4) has this date
+        $creationDate = date_create_immutable_from_format('d.m.Y', '28.09.2020')->setTime(0, 0, 0);
+        // we want to set the period in a way that older records get deleted, but not the one created today
+        $difference = $creationDate->diff(new \DateTime(), true);
+        // let's set the amount of days one higher than the reference date
+        $period = (int)$difference->format('%d') + 1;
+        $subject->setPeriod($period);
+        $result = $subject->execute();
+        $this->assertCSVDataSet('typo3/sysext/recycler/Tests/Functional/Task/Pages/DataSet/Assertion/pages_deleted_with_period.csv');
+        self::assertTrue($result);
+    }
+
+    /**
+     * @test
+     */
+    public function taskFailsOnError()
+    {
+        $subject = new CleanerTask();
+        $GLOBALS['TCA']['not_existing_table']['ctrl']['delete'] = 'deleted';
+        $subject->setTcaTables(['not_existing_table']);
+        $result = $subject->execute();
+        self::assertFalse($result);
+    }
+}
