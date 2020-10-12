e6dd1adde51218dc38a5985d8d8629e15b38fb35
Oliver Bartsch <bo@cedev.de>
2020-10-12 10:59:30 +0200

    [FEATURE] Show extension constraints in extension manager
    The "all versions" view of the extension manager, displaying details
    on any given extension, is extended to
    also show the extension constraints, similar to the display in TER.
    
    Furthermore a warning is shown for TYPO3 and PHP dependencies
    if they do not match the current version.
    
    Resolves: #92538
    Releases: master
    Change-Id: I918dfd1d80eb4fffcc633278f742429dfdfdea47
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/66102
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Anja Leichsenring <aleichsenring@ab-softlab.de>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Reviewed-by: Anja Leichsenring <aleichsenring@ab-softlab.de>
    Reviewed-by: Wouter Wolters <typo3@wouterwolters.nl>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>

---
+++ typo3/sysext/extensionmanager/Classes/ViewHelpers/ConstraintsViewHelper.php
@@ -0,0 +1,87 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3\CMS\Extensionmanager\ViewHelpers;
+
+use TYPO3\CMS\Core\Utility\VersionNumberUtility;
+use TYPO3\CMS\Extensionmanager\Domain\Model\Dependency;
+use TYPO3\CMS\Extensionmanager\Domain\Model\Extension;
+use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+
+/**
+ * Returns the grouped constraints of an extension
+ *
+ * @internal
+ */
+class ConstraintsViewHelper extends AbstractViewHelper
+{
+    public function initializeArguments(): void
+    {
+        $this->registerArgument('extension', Extension::class, 'extension to process', true);
+    }
+
+    public static function renderStatic(
+        array $arguments,
+        \Closure $renderChildrenClosure,
+        RenderingContextInterface $renderingContext
+    ): array {
+        $groupedConstraints = [];
+
+        foreach ($arguments['extension']->getDependencies() as $dependency) {
+            $groupedConstraints[$dependency->getType()][self::getTransformedIdentifier($dependency->getIdentifier())] = [
+                'version' => self::getVersionString($dependency->getLowestVersion(), $dependency->getHighestVersion()),
+                'versionCompatible' => self::isVersionCompatible($dependency)
+            ];
+        }
+
+        return $groupedConstraints;
+    }
+
+    protected static function getTransformedIdentifier(string $identifier): string
+    {
+        return in_array($identifier, Dependency::$specialDependencies, true)
+            ? strtoupper($identifier)
+            : strtolower($identifier);
+    }
+
+    protected static function getVersionString(string $lowestVersion, string $highestVersion): string
+    {
+        $version = '';
+
+        if ($lowestVersion !== '') {
+            $version .= '(' . $lowestVersion;
+            if ($highestVersion !== '') {
+                $version .= ' - ' . $highestVersion;
+            }
+            $version .= ')';
+        }
+
+        return $version;
+    }
+
+    protected static function isVersionCompatible(Dependency $dependency): bool
+    {
+        if ($dependency->getIdentifier() === 'typo3') {
+            return $dependency->isVersionCompatible(VersionNumberUtility::getNumericTypo3Version());
+        }
+        if ($dependency->getIdentifier() === 'php') {
+            return $dependency->isVersionCompatible(PHP_VERSION);
+        }
+        return true;
+    }
+}
