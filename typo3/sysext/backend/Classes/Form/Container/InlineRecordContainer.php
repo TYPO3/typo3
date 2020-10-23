c7ff1304b586f8c7edfd7e9d9f77d2ecb14ac7ec
Olaf Schmidt-Wischhöfer <olaf.schmidt-wischhoefer@mehrwert.de>
2020-10-23 12:31:43 +0200

    [BUGFIX] Use a HTML button for the collapsable IRRE header
    Also add aria-expanded and aria-controls arguments for screen readers
    
    Resolves: #92682
    Releases: master, 10.4
    Change-Id: I48d0edcbea4d185d11216048d7847ad3574d704d
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/66271
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Michael Telgkamp <michael.telgkamp@mindscreen.de>
    Tested-by: Benni Mack <benni@typo3.org>
    Reviewed-by: Michael Telgkamp <michael.telgkamp@mindscreen.de>
    Reviewed-by: Benni Mack <benni@typo3.org>

--- typo3/sysext/backend/Classes/Form/Container/InlineRecordContainer.php
+++ typo3/sysext/backend/Classes/Form/Container/InlineRecordContainer.php
@@ -215,17 +215,20 @@ class InlineRecordContainer extends AbstractContainer
                 'data-placeholder-record' => $data['isInlineDefaultLanguageRecordInLocalizedParentContext'] ? '1' : '0'
             ];
 
+            $ariaExpanded = $data['isInlineChildExpanded'] ? 'true' : 'false';
+            $ariaControls = htmlspecialchars($objectId . '_fields', ENT_QUOTES | ENT_HTML5);
+            $ariaAttributesString = 'aria-expanded="' . $ariaExpanded . '" aria-controls="' . $ariaControls . '"';
             $html = '
 				<div ' . GeneralUtility::implodeAttributes($containerAttributes, true) . '>
-					<div class="panel-heading" data-toggle="formengine-inline" id="' . htmlspecialchars($hashedObjectId) . '_header" data-expandSingle="' . ($inlineConfig['appearance']['expandSingle'] ? 1 : 0) . '">
+					<div class="panel-heading" data-toggle="formengine-inline" id="' . htmlspecialchars($hashedObjectId, ENT_QUOTES | ENT_HTML5) . '_header" data-expandSingle="' . ($inlineConfig['appearance']['expandSingle'] ? 1 : 0) . '">
 						<div class="form-irre-header">
 							<div class="form-irre-header-cell form-irre-header-icon">
 								<span class="caret"></span>
 							</div>
-							' . $this->renderForeignRecordHeader($data) . '
+							' . $this->renderForeignRecordHeader($data, $ariaAttributesString) . '
 						</div>
 					</div>
-					<div class="panel-collapse" id="' . htmlspecialchars($objectId) . '_fields">' . $html . $hiddenFieldHtml . $combinationHtml . '</div>
+					<div class="panel-collapse" id="' . $ariaControls . '">' . $html . $hiddenFieldHtml . $combinationHtml . '</div>
 				</div>';
         }
 
@@ -325,9 +328,10 @@ class InlineRecordContainer extends AbstractContainer
      * Later on the command-icons are inserted here.
      *
      * @param array $data Current data
+     * @param string $ariaAttributesString HTML aria attributes for the collapse button
      * @return string The HTML code of the header
      */
-    protected function renderForeignRecordHeader(array $data)
+    protected function renderForeignRecordHeader(array $data, string $ariaAttributesString)
     {
         $languageService = $this->getLanguageService();
         $inlineConfig = $data['inlineParentConfig'];
@@ -397,13 +401,15 @@ class InlineRecordContainer extends AbstractContainer
         }
 
         if (!empty($inlineConfig['appearance']['headerThumbnail']['field']) && $thumbnail) {
-            $mediaContainer = '<div class="form-irre-header-cell form-irre-header-thumbnail" id="' . $objectId . '_thumbnailcontainer">' . $thumbnail . '</div>';
+            $mediaContainer = '<div class="form-irre-header-thumbnail" id="' . $objectId . '_thumbnailcontainer">' . $thumbnail . '</div>';
         } else {
-            $mediaContainer = '<div class="form-irre-header-cell form-irre-header-icon" id="' . $objectId . '_iconcontainer">' . $iconImg . '</div>';
+            $mediaContainer = '<div class="form-irre-header-icon" id="' . $objectId . '_iconcontainer">' . $iconImg . '</div>';
         }
-        $header = $mediaContainer . '
-				<div class="form-irre-header-cell form-irre-header-body">' . $label . '</div>
-				<div class="form-irre-header-cell form-irre-header-control t3js-formengine-irre-control">' . $ctrl . '</div>';
+        $header = '<button class="form-irre-header-cell form-irre-header-button" ' . $ariaAttributesString . '>' .
+            $mediaContainer .
+            '<div class="form-irre-header-body">' . $label . '</div>' .
+            '</button>' .
+            '<div class="form-irre-header-cell form-irre-header-control t3js-formengine-irre-control">' . $ctrl . '</div>';
 
         return $header;
     }
