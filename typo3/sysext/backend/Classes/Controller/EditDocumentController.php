7877f53abf7b5814ec4667cb977cb6c943812e64
Xavier Perseguers <xavier@typo3.org>
2020-11-07 10:07:10 +0100

    [BUGFIX] Reintroduce the check on options.saveDocNew
    Due to a refactoring and "unrolling" of buttons in TYPO3 v9, the TSconfig
    options.saveDocNew has been forgotten when adding a button to create a
    new record right while editing another one.
    
    Even if the new button nowadays is physically not a "Save and create new"
    action in the label, the behaviour is strictly the same as any unchanged
    edit will trigger a modal asking whether the changes should be persisted.
    
    Releases: master, 10.4, 9.5
    Resolves: #87321
    Resolves: #92788
    Change-Id: Ic79f7ff06afef0cf9423780eef2d5324e5613664
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/66559
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Eric Chavaillaz <eric@hemmer.ch>
    Tested-by: Markus Klein <markus.klein@typo3.org>
    Tested-by: Anja Leichsenring <aleichsenring@ab-softlab.de>
    Reviewed-by: Eric Chavaillaz <eric@hemmer.ch>
    Reviewed-by: Markus Klein <markus.klein@typo3.org>
    Reviewed-by: Josef Glatz <josefglatz@gmail.com>
    Reviewed-by: Anja Leichsenring <aleichsenring@ab-softlab.de>

--- typo3/sysext/backend/Classes/Controller/EditDocumentController.php
+++ typo3/sysext/backend/Classes/Controller/EditDocumentController.php
@@ -1539,6 +1539,7 @@ class EditDocumentController
                     )
                 )
             )
+            && $this->getTsConfigOption($this->firstEl['table'], 'saveDocNew')
         ) {
             $classNames = 't3js-editform-new';
 
