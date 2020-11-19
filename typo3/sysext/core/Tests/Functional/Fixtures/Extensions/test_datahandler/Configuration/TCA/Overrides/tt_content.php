2648ba09328d5f7577641927c84bafc3de6cfd5f
Christian Kuhn <lolli@schwarzbu.ch>
2020-11-19 15:09:03 +0100

    [BUGFIX] Do not change TCA in ext_tables in test_datahandler
    Test related fixture extension 'test_datahandler' still has calls
    in ext_tables.php that changes $GLOBALS['TCA']. This is forbidden
    for a while. Move those calls to Configuration/TCA/Overrides.
    
    Resolves: #92879
    Releases: master, 10.4
    Change-Id: I4cc66ca41caf52d872b0ff1a06a90d180a739ddb
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/66706
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Benni Mack <benni@typo3.org>
    Reviewed-by: Benni Mack <benni@typo3.org>

---
+++ typo3/sysext/core/Tests/Functional/Fixtures/Extensions/test_datahandler/Configuration/TCA/Overrides/tt_content.php
@@ -0,0 +1,80 @@
+<?php
+
+defined('TYPO3_MODE') or die();
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns(
+    'tt_content',
+    [
+        'tx_testdatahandler_select' => [
+            'exclude' => true,
+            'label' => 'DataHandler Test Select',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectMultipleSideBySide',
+                'foreign_table' => 'tx_testdatahandler_element',
+                'minitems' => 1,
+                'maxitems' => 10,
+                'autoSizeMax' => 10,
+                'default' => '',
+            ],
+        ],
+        'tx_testdatahandler_select_dynamic' => [
+            'exclude' => true,
+            'label' => 'DataHandler Test Select',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectMultipleSideBySide',
+                'items' => [
+                    ['predefined label', 'predefined value']
+                ],
+                'itemsProcFunc' => 'TYPO3\TestDatahandler\Classes\Tca\SelectElementItems->getItems',
+                'minitems' => 1,
+                'maxitems' => 10,
+                'autoSizeMax' => 10,
+                'default' => '',
+            ],
+        ],
+        'tx_testdatahandler_group' => [
+            'exclude' => true,
+            'label' => 'DataHandler Test Group',
+            'config' => [
+                'type' => 'group',
+                'internal_type' => 'db',
+                'allowed' => 'tx_testdatahandler_element',
+                'minitems' => 1,
+                'maxitems' => 10,
+                'autoSizeMax' => 10,
+                'default' => '',
+            ],
+        ],
+        'tx_testdatahandler_radio' => [
+            'exclude' => true,
+            'label' => 'DataHandler Test Radio',
+            'config' => [
+                'type' => 'radio',
+                'items' => [
+                    ['predefined label', 'predefined value']
+                ],
+                'itemsProcFunc' => 'TYPO3\TestDatahandler\Classes\Tca\RadioElementItems->getItems',
+                'default' => '',
+            ],
+        ],
+        'tx_testdatahandler_checkbox' => [
+            'exclude' => true,
+            'label' => 'DataHandler Test Checkbox',
+            'config' => [
+                'type' => 'check',
+                'items' => [
+                    ['predefined label', 'predefined value']
+                ],
+                'itemsProcFunc' => 'TYPO3\TestDatahandler\Classes\Tca\CheckboxElementItems->getItems',
+                'default' => '',
+            ],
+        ],
+    ]
+);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
+    'tt_content',
+    '--div--;DataHandler Test, tx_testdatahandler_select, tx_testdatahandler_group'
+);
