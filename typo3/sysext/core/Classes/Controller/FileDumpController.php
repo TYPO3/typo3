932ff134418df0ddce498a6e6b6e344d1fa18484
Stephan Großberndt <stephan.grossberndt@typo3.org>
2020-11-30 14:41:19 +0100

    [BUGFIX] Avoid exception when trying to download a non-existing file
    This patch fixes a regression introduced by change
    https://review.typo3.org/c/Packages/TYPO3.CMS/+/65126 which lead to
    an HTTP 500 exception instead of returning HTTP 404 by making sure
    that FileDumpController->isFileValid() is not called with NULL as
    parameter.
    
    Resolves: #92957
    Related: #91754
    Releases: master, 10.4, 9.5
    Change-Id: I824606867063f421f1a6fdfde4788846ddcc41a7
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/66947
    Tested-by: Daniel Goerz <daniel.goerz@posteo.de>
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Oliver Hader <oliver.hader@typo3.org>
    Reviewed-by: Daniel Goerz <daniel.goerz@posteo.de>
    Reviewed-by: Oliver Hader <oliver.hader@typo3.org>

--- typo3/sysext/core/Classes/Controller/FileDumpController.php
+++ typo3/sysext/core/Classes/Controller/FileDumpController.php
@@ -180,9 +180,7 @@ class FileDumpController
         if (isset($parameters['f'])) {
             try {
                 $file = $this->resourceFactory->getFileObject($parameters['f']);
-                if ($file->isDeleted() || $file->isMissing()) {
-                    $file = null;
-                } elseif (!$this->isFileValid($file)) {
+                if ($file->isDeleted() || $file->isMissing() || !$this->isFileValid($file)) {
                     $file = null;
                 }
             } catch (\Exception $e) {
@@ -191,10 +189,7 @@ class FileDumpController
         } elseif (isset($parameters['r'])) {
             try {
                 $file = $this->resourceFactory->getFileReferenceObject($parameters['r']);
-                if ($file->isMissing()) {
-                    $file = null;
-                }
-                if (!$this->isFileValid($file->getOriginalFile())) {
+                if ($file->isMissing() || !$this->isFileValid($file->getOriginalFile())) {
                     $file = null;
                 }
             } catch (\Exception $e) {
@@ -205,10 +200,7 @@ class FileDumpController
                 $processedFileRepository = GeneralUtility::makeInstance(ProcessedFileRepository::class);
                 /** @var ProcessedFile|null $file */
                 $file = $processedFileRepository->findByUid($parameters['p']);
-                if (!$file || $file->isDeleted()) {
-                    $file = null;
-                }
-                if (!$this->isFileValid($file->getOriginalFile())) {
+                if (!$file || $file->isDeleted() || !$this->isFileValid($file->getOriginalFile())) {
                     $file = null;
                 }
             } catch (\Exception $e) {
