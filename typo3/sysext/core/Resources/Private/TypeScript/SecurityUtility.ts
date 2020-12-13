a911cf810990f40492642f3c5169395948585156
Oliver Hader <oliver@typo3.org>
2020-12-13 10:43:03 +0100

    [TASK] Harden client-side SecurityUtility.encodeHtml
    Ensures client-side function `SecurityUtility.encodeHtml` behaves like
    `htmlspecialchars(..., ENT_QUOTES)`. The function is used for complete
    nodes only, but now could be used for parts as well.
    
    Resolves: #93068
    Releases: master, 10.4, 9.5
    Change-Id: I74b09676d0fdb8ddf09e7fc639480742fe645e9b
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/67107
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Benni Mack <benni@typo3.org>
    Reviewed-by: Benni Mack <benni@typo3.org>

--- typo3/sysext/core/Resources/Private/TypeScript/SecurityUtility.ts
+++ typo3/sysext/core/Resources/Private/TypeScript/SecurityUtility.ts
@@ -66,7 +66,9 @@ class SecurityUtility {
     // apply arbitrary data a text node
     // thus browser is capable of properly encoding
     anvil.innerText = value;
-    return anvil.innerHTML;
+    return anvil.innerHTML
+      .replace(/"/g, '&quot;')
+      .replace(/'/g, '&apos;');
   }
 
   /**
