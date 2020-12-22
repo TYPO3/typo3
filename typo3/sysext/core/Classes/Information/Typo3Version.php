6a5e2d4097ef0a0e3ea955af93cf83810d6fa234
Benni Mack <benni@typo3.org>
2020-12-22 09:07:34 +0100

    [RELEASE] Release of TYPO3 11.0.0
    Change-Id: Ibcf3165f1628bec14ee50aaa601606ae37977f0f
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/67238
    Tested-by: Benni Mack <benni@typo3.org>
    Reviewed-by: Benni Mack <benni@typo3.org>

--- typo3/sysext/core/Classes/Information/Typo3Version.php
+++ typo3/sysext/core/Classes/Information/Typo3Version.php
@@ -19,7 +19,7 @@ namespace TYPO3\CMS\Core\Information;
 
 class Typo3Version
 {
-    protected const VERSION = '11.0.0-dev';
+    protected const VERSION = '11.0.0';
     protected const BRANCH = '11.0';
 
     /**
