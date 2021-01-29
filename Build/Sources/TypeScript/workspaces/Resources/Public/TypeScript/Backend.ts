109c781a4c6c8110d1bd89228218913402b799cb
Benni Mack <benni@typo3.org>
2021-01-29 16:07:46 +0100

    [BUGFIX] Only show workspace next/prev stage buttons with access
    The buttons in the info dialog of a record in the workspaces
    module shows the buttons "Publish to live" (or send to next
    stage) now only shows the buttons if the editor has access to it,
    same with sending an element back to the previous stage.
    
    Resolves: #93384
    Releases: master, 10.4
    Change-Id: I47b394307379f3456cc8ea47b75715937e6a93a2
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/67552
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Oliver Bartsch <bo@cedev.de>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Reviewed-by: Oliver Bartsch <bo@cedev.de>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>

--- Build/Sources/TypeScript/workspaces/Resources/Public/TypeScript/Backend.ts
+++ Build/Sources/TypeScript/workspaces/Resources/Public/TypeScript/Backend.ts
@@ -854,7 +854,7 @@ class Backend extends Workspaces {
         ),
       );
 
-      if ($tr.data('stage') !== $tr.data('prevStage')) {
+      if (item.label_PrevStage !== false && $tr.data('stage') !== $tr.data('prevStage')) {
         modalButtons.push({
           text: item.label_PrevStage.title,
           active: true,
@@ -867,16 +867,18 @@ class Backend extends Workspaces {
         });
       }
 
-      modalButtons.push({
-        text: item.label_NextStage.title,
-        active: true,
-        btnClass: 'btn-default',
-        name: 'nextstage',
-        trigger: (): void => {
-          Modal.currentModal.trigger('modal-dismiss');
-          this.sendToStage($tr, 'next');
-        },
-      });
+      if (item.label_NextStage !== false) {
+        modalButtons.push({
+          text: item.label_NextStage.title,
+          active: true,
+          btnClass: 'btn-default',
+          name: 'nextstage',
+          trigger: (): void => {
+            Modal.currentModal.trigger('modal-dismiss');
+            this.sendToStage($tr, 'next');
+          },
+        });
+      }
       modalButtons.push({
         text: TYPO3.lang.close,
         active: true,
