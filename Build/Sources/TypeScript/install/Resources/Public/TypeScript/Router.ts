769810cdf3be3dab78f2d07a155224a47c393c90
Nikita Hovratov <nikita.h@live.de>
2021-02-27 14:36:16 +0100

    [TASK] Remove systemLogLevel example in Router.ts
    This example configuration was accidentally added by a later commit
    after it was already removed.
    
    Resolves: #93600
    Related: #89645, #89740
    Releases: master, 10.4
    Change-Id: I79bbbf3210e67e0f5425563ba58765115db3cd12
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/68139
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Anja Leichsenring <aleichsenring@ab-softlab.de>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Reviewed-by: Anja Leichsenring <aleichsenring@ab-softlab.de>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>

--- Build/Sources/TypeScript/install/Resources/Public/TypeScript/Router.ts
+++ Build/Sources/TypeScript/install/Resources/Public/TypeScript/Router.ts
@@ -234,7 +234,7 @@ class Router {
         + ' to manually increase debugging output in <code>typo3conf/LocalConfiguration.php</code>:'
         + '<code>[\'BE\'][\'debug\'] => true</code>, <code>[\'SYS\'][\'devIPmask\'] => \'*\'</code>, '
         + '<code>[\'SYS\'][\'displayErrors\'] => 1</code>,'
-        + '<code>[\'SYS\'][\'systemLogLevel\'] => 0</code>, <code>[\'SYS\'][\'exceptionalErrors\'] => 12290</code></p>'
+        + '<code>[\'SYS\'][\'exceptionalErrors\'] => 12290</code></p>'
         + '</div>'
         + '</div>'
         + '<div class="panel-group" role="tablist" aria-multiselectable="true">'
