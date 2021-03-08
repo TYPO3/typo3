ce413e7021d20ad1587e05950cd8dcb7d3b6cbf2
Georg Ringer <georg.ringer@gmail.com>
2021-03-08 20:53:35 +0100

    [BUGFIX] Trim locallang label references in LanguageService
    LanguageService::sL(), also known as the split label function,
    is widely used through Core and third-party extensions. It
    checks if the given input is a label reference and otherwise
    just returns the input.
    
    Since this method is often used with dynamic values, coming
    from e.g. XML files, there might be whitespace in the input
    string. In such case the label reference can not be resolved,
    even if the key is valid, and the input gets returned unchanged.
    
    To prevent this behaviour, LanguageService::sL() now applies
    trim() to the input, whenever a specifc strpos() check takes place.
    
    Resolves: #93676
    Releases: master, 10.4
    Change-Id: If195b5fed75f79912edd6478ad3a47a27157155d
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/68314
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Richard Haeser <richard@richardhaeser.com>
    Reviewed-by: Richard Haeser <richard@richardhaeser.com>

---
+++ typo3/sysext/core/Tests/Functional/Localization/LanguageServiceTest.php
@@ -0,0 +1,104 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3\CMS\Core\Tests\Functional\Localization;
+
+use TYPO3\CMS\Core\Localization\LanguageService;
+use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
+
+class LanguageServiceTest extends FunctionalTestCase
+{
+    /**
+     * @var LanguageService
+     */
+    protected $subject;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->subject = LanguageService::create('default');
+    }
+
+    /**
+     * @test
+     * @dataProvider splitLabelTestDataProvider
+     */
+    public function splitLabelTest(string $input, string $expected): void
+    {
+        self::assertEquals($expected, $this->subject->sL($input));
+    }
+
+    public function splitLabelTestDataProvider(): \Generator
+    {
+        yield 'String without whitespace' => [
+            'Edit content',
+            'Edit content'
+        ];
+        yield 'String with leading whitespace' => [
+            '  Edit content',
+            '  Edit content'
+        ];
+        yield 'String with trailing whitespace' => [
+            'Edit content   ',
+            'Edit content   '
+        ];
+        yield 'String with outer whitespace' => [
+            '    Edit content   ',
+            '    Edit content   '
+        ];
+        yield 'String with inner whitespace' => [
+            'Edit    content',
+            'Edit    content'
+        ];
+        yield 'String with inner and outer whitespace' => [
+            '    Edit    content    ',
+            '    Edit    content    '
+        ];
+        yield 'String containing the LLL: key' => [
+            'You can use LLL: to ...',
+            'You can use LLL: to ...'
+        ];
+        yield 'String starting with the LLL: key' => [
+            'LLL: can be used to ...',
+            '' // @todo Should this special case be handled to return the input string?
+        ];
+        yield 'Locallang label without whitespace' => [
+            'LLL:EXT:core/Resources/Private/Language/locallang_core.xlf:cm.editcontent',
+            'Edit content'
+        ];
+        yield 'Locallang label with leading whitespace' => [
+            '    LLL:EXT:core/Resources/Private/Language/locallang_core.xlf:cm.editcontent',
+            'Edit content'
+        ];
+        yield 'Locallang label with trailing whitespace' => [
+            'LLL:EXT:core/Resources/Private/Language/locallang_core.xlf:cm.editcontent    ',
+            'Edit content'
+        ];
+        yield 'Locallang label with outer whitespace' => [
+            '    LLL:EXT:core/Resources/Private/Language/locallang_core.xlf:cm.editcontent    ',
+            'Edit content'
+        ];
+        yield 'Locallang label with inner whitespace' => [
+            'LLL:    EXT:    core/Resources/Private/Language/locallang_core.xlf:cm.editcontent',
+            'Edit content'
+        ];
+        yield 'Locallang label with inner and outer whitespace' => [
+            '    LLL:    EXT:    core/Resources/Private/Language/locallang_core.xlf:cm.editcontent    ',
+            'Edit content'
+        ];
+    }
+}
