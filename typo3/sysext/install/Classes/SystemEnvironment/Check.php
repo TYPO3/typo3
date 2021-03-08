d10aedc4dabed3f5a51608cca079ff231b18f3fd
Alexander Nitsche <typo3@alexandernitsche.com>
2021-03-08 14:31:31 +0100

    [TASK] Replace wiki links by documentation links
    The wiki.typo3.org is marked as deprecated and gets
    replaced soon by docs.typo3.org permanently. Replace
    all wiki links by actual documentation links.
    
    Resolves: #93677
    Releases: master, 10.4, 9.5
    Change-Id: Ib1bc42a6f2192580581499a9a0c1deb8d21ae2e0
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/68252
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Oliver Bartsch <bo@cedev.de>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Reviewed-by: Oliver Bartsch <bo@cedev.de>
    Reviewed-by: Lina Wolf <112@linawolf.de>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>

--- typo3/sysext/install/Classes/SystemEnvironment/Check.php
+++ typo3/sysext/install/Classes/SystemEnvironment/Check.php
@@ -571,7 +571,7 @@ class Check implements CheckInterface
                     . '* The PHP extension eaccelerator is known to break this if'
                     . ' it is compiled without --with-eaccelerator-doc-comment-inclusion flag.'
                     . ' This compile flag must be specified, otherwise TYPO3 CMS will not work.' . LF
-                    . 'For more information take a look in our wiki ' . Typo3Information::URL_OPCACHE . '.',
+                    . 'For more information take a look in our documentation ' . Typo3Information::URL_OPCACHE . '.',
                 'PHP Doc comment reflection broken',
                 FlashMessage::ERROR
             ));
