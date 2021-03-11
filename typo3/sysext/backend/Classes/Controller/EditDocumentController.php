5a5bbe5351aebb68f70e1243a17cd82de5980426
Benoit Chenu <benoit.chenu@gmail.com>
2021-03-11 17:57:33 +0100

    [BUGFIX] Unknown column 't3ver_oid' in 'field list'
    EditDocumentController adds t3ver_oid field in SQL query for tables which don't use workspaces
    
    Resolves: #93708
    Releases: master, 10.4
    Change-Id: Ic711ce86a6f5c4f4a23044c02c67418dd85e2dae
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/68322
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>

--- typo3/sysext/backend/Classes/Controller/EditDocumentController.php
+++ typo3/sysext/backend/Classes/Controller/EditDocumentController.php
@@ -642,7 +642,7 @@ class EditDocumentController
             // Setting a blank editconf array for a new record:
             $this->editconf = [];
             // Determine related page ID for regular live context
-            if ((int)$nRec['t3ver_oid'] === 0) {
+            if ((int)($nRec['t3ver_oid'] ?? 0) === 0) {
                 if ($insertRecordOnTop) {
                     $relatedPageId = $nRec['pid'];
                 } else {
@@ -679,7 +679,7 @@ class EditDocumentController
             }
 
             $recordFields = 'pid,uid';
-            if (!BackendUtility::isTableWorkspaceEnabled($nTable)) {
+            if (BackendUtility::isTableWorkspaceEnabled($nTable)) {
                 $recordFields .= ',t3ver_oid';
             }
             $nRec = BackendUtility::getRecord($nTable, $nUid, $recordFields);
@@ -687,7 +687,7 @@ class EditDocumentController
             // Setting a blank editconf array for a new record:
             $this->editconf = [];
 
-            if ((int)$nRec['t3ver_oid'] === 0) {
+            if ((int)($nRec['t3ver_oid'] ?? 0) === 0) {
                 $relatedPageId = -$nRec['uid'];
             } else {
                 $relatedPageId = -$nRec['t3ver_oid'];
@@ -859,7 +859,7 @@ class EditDocumentController
         }
 
         // Always use live workspace record uid for the preview
-        if (BackendUtility::isTableWorkspaceEnabled($table) && $recordArray['t3ver_oid'] > 0) {
+        if (BackendUtility::isTableWorkspaceEnabled($table) && ($recordArray['t3ver_oid'] ?? 0) > 0) {
             $recordId = $recordArray['t3ver_oid'];
         }
 
