5354abdf6841f09023819e356443077584f56353
Oliver Hader <oliver@typo3.org>
2021-03-18 08:29:35 +0100

    [RELEASE] Release of TYPO3 9.5.26
    Change-Id: Ic96a203889dffd2800f2b5fd39767a824350c8fe
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/68497
    Tested-by: Oliver Hader <oliver.hader@typo3.org>
    Reviewed-by: Oliver Hader <oliver.hader@typo3.org>

--- typo3/sysext/core/Classes/Information/Typo3Version.php
+++ typo3/sysext/core/Classes/Information/Typo3Version.php
@@ -20,7 +20,7 @@ namespace TYPO3\CMS\Core\Information;
  */
 class Typo3Version
 {
-    protected const VERSION = '9.5.26-dev';
+    protected const VERSION = '9.5.26';
     protected const BRANCH = '9.5';
 
     /**
