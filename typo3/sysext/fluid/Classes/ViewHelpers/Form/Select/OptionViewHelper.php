d67a7053c41b24dc0d44bfe4eebcb02473b20dfc
Oliver Bartsch <bo@cedev.de>
2021-03-22 22:01:12 +0100

    [BUGFIX] Ensure compared values are of same type
    When strict type comparison was introduced to
    the OptionViewHelper in #90540, it was not properly
    ensured that the values to compare are of same type.
    
    The issue is fixed by always typecasting the selected
    value to string, before comparing it to the value in
    question, which will always be a string, according to
    its type hint.
    
    Resolves: #93801
    Releases: master, 10.4
    Change-Id: I3ea1d885a35e3223919facfe6d1825332914a7ed
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/68560
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Georg Ringer <georg.ringer@gmail.com>
    Tested-by: Benni Mack <benni@typo3.org>
    Reviewed-by: Oliver Klee <typo3-coding@oliverklee.de>
    Reviewed-by: Jonas Eberle <flightvision@googlemail.com>
    Reviewed-by: Georg Ringer <georg.ringer@gmail.com>
    Reviewed-by: Benni Mack <benni@typo3.org>

--- typo3/sysext/fluid/Classes/ViewHelpers/Form/Select/OptionViewHelper.php
+++ typo3/sysext/fluid/Classes/ViewHelpers/Form/Select/OptionViewHelper.php
@@ -73,11 +73,11 @@ class OptionViewHelper extends AbstractFormFieldViewHelper
     {
         $selectedValue = $this->renderingContext->getViewHelperVariableContainer()->get(SelectViewHelper::class, 'selectedValue');
         if (is_array($selectedValue)) {
-            return in_array($value, $selectedValue, true);
+            return in_array($value, array_map('strval', $selectedValue), true);
         }
         if ($selectedValue instanceof \Iterator) {
-            return in_array($value, iterator_to_array($selectedValue), true);
+            return in_array($value, array_map('strval', iterator_to_array($selectedValue)), true);
         }
-        return $value === $selectedValue;
+        return $value === (string)$selectedValue;
     }
 }
