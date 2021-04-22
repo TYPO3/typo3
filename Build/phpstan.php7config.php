5c7cc33e0df791722e325a2e6460672c2ec07ccb
Alexander Schnitzler <git@alexanderschnitzler.de>
2021-04-22 12:47:38 +0200

    [TASK] Use %currentWorkingDirectory% in phpstan config files
    To avoid issues due to relative paths in config files, all
    paths are now prepended with the %currentWorkingDirectory%
    variable of phpstan. That way, the exact environment of
    config files is irrelevant.
    
    Releases: master
    Resolves: #93966
    Change-Id: I3115b7ecca171ee2be30ac0172f772a7eec9e533
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/68842
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Oliver Bartsch <bo@cedev.de>
    Tested-by: Daniel Goerz <daniel.goerz@posteo.de>
    Reviewed-by: Oliver Bartsch <bo@cedev.de>
    Reviewed-by: Daniel Goerz <daniel.goerz@posteo.de>

--- Build/phpstan.php7config.php
+++ Build/phpstan.php7config.php
@@ -9,67 +9,67 @@ if (PHP_MAJOR_VERSION === 7) {
         '#Class GdImage not found.#',
         [
             'message' => '#^Parameter \\#[1-4]{1} \\$[a-z]* of function [a-z_]* expects resource, resource\\|XmlParser given\\.$#',
-            'path' => '../typo3/sysext/extensionmanager/Classes/Utility/Parser/ExtensionXmlPushParser.php',
+            'path' => '%currentWorkingDirectory%/typo3/sysext/extensionmanager/Classes/Utility/Parser/ExtensionXmlPushParser.php',
             'count' => 9,
         ],
         [
             'message' => '#^Parameter \\#1 \\$sem_identifier of function sem_release expects resource, resource\\|SysvSemaphore given\\.$#',
-            'path' => '../typo3/sysext/core/Classes/Locking/SemaphoreLockStrategy.php',
+            'path' => '%currentWorkingDirectory%/typo3/sysext/core/Classes/Locking/SemaphoreLockStrategy.php',
             'count' => 1,
         ],
         [
             'message' => '#^Parameter \\#1 \\$sem_identifier of function sem_remove expects resource, resource\\|SysvSemaphore given\\.$#',
-            'path' => '../typo3/sysext/core/Classes/Locking/SemaphoreLockStrategy.php',
+            'path' => '%currentWorkingDirectory%/typo3/sysext/core/Classes/Locking/SemaphoreLockStrategy.php',
             'count' => 1,
         ],
         [
             'message' => '#^Parameter \\#2 \\$algo of function password_hash expects string\\|null, int\\|string\\|null given\\.$#',
-            'path' => '../typo3/sysext/core/Classes/Crypto/PasswordHashing/AbstractArgon2PasswordHash.php',
+            'path' => '%currentWorkingDirectory%/typo3/sysext/core/Classes/Crypto/PasswordHashing/AbstractArgon2PasswordHash.php',
             'count' => 1
         ],
         [
             'message' => '#^Parameter \\#2 \\$algo of function password_needs_rehash expects string\\|null, int\\|string\\|null given\\.$#',
-            'path' => '../typo3/sysext/core/Classes/Crypto/PasswordHashing/AbstractArgon2PasswordHash.php',
+            'path' => '%currentWorkingDirectory%/typo3/sysext/core/Classes/Crypto/PasswordHashing/AbstractArgon2PasswordHash.php',
             'count' => 1
         ],
         [
             'message' => '#^Parameter \\#1 \\$im of function imagecolorallocate expects resource, resource\\|false given\\.$#',
-            'path' => '../typo3/sysext/install/Classes/Controller/EnvironmentController.php',
+            'path' => '%currentWorkingDirectory%/typo3/sysext/install/Classes/Controller/EnvironmentController.php',
             'count' => 6
         ],
         [
             'message' => '#^Parameter \\#1 \\$im of function imagefilledrectangle expects resource, resource\\|false given\\.$#',
-            'path' => '../typo3/sysext/install/Classes/Controller/EnvironmentController.php',
+            'path' => '%currentWorkingDirectory%/typo3/sysext/install/Classes/Controller/EnvironmentController.php',
             'count' => 4
         ],
         [
             'message' => '#^Parameter \\#1 \\$im of function imagegif expects resource, resource\\|false given\\.$#',
-            'path' => '../typo3/sysext/install/Classes/Controller/EnvironmentController.php',
+            'path' => '%currentWorkingDirectory%/typo3/sysext/install/Classes/Controller/EnvironmentController.php',
             'count' => 1
         ],
         [
             'message' => '#^Parameter \\#1 \\$im of function imagettftext expects resource, resource\\|false given\\.$#',
-            'path' => '../typo3/sysext/install/Classes/Controller/EnvironmentController.php',
+            'path' => '%currentWorkingDirectory%/typo3/sysext/install/Classes/Controller/EnvironmentController.php',
             'count' => 1
         ],
         [
             'message' => '#^Parameter \\#6 \\$col of function imagefilledrectangle expects int, int\\|false given\\.$#',
-            'path' => '../typo3/sysext/install/Classes/Controller/EnvironmentController.php',
+            'path' => '%currentWorkingDirectory%/typo3/sysext/install/Classes/Controller/EnvironmentController.php',
             'count' => 4
         ],
         [
             'message' => '#^Parameter \\#6 \\$col of function imagettftext expects int, int\\|false given\\.$#',
-            'path' => '../typo3/sysext/install/Classes/Controller/EnvironmentController.php',
+            'path' => '%currentWorkingDirectory%/typo3/sysext/install/Classes/Controller/EnvironmentController.php',
             'count' => 1
         ],
         [
             'message' => '#^Parameter \\#1 \\$im of function imagedestroy expects resource, resource\\|false given\\.$#',
-            'path' => '../typo3/sysext/install/Classes/SystemEnvironment/Check.php',
+            'path' => '%currentWorkingDirectory%/typo3/sysext/install/Classes/SystemEnvironment/Check.php',
             'count' => 3
         ],
         [
             'message' => '#^Parameter \\#2 \\$col of function imagecolortransparent expects int, int\\|false given\\.$#',
-            'path' => '../typo3/sysext/frontend/Classes/Imaging/GifBuilder.php',
+            'path' => '%currentWorkingDirectory%/typo3/sysext/frontend/Classes/Imaging/GifBuilder.php',
             'count' => 1
         ]
     ];
--- Build/phpstan.php8config.php
+++ Build/phpstan.php8config.php
@@ -8,57 +8,57 @@ if (PHP_MAJOR_VERSION === 8) {
     $config['parameters']['ignoreErrors'] = [
         [
             'message' => '#^Parameter \\#1 \\$image of function imagedestroy expects GdImage, GdImage\\|false given\\.$#',
-            'path' => '../typo3/sysext/install/Classes/SystemEnvironment/Check.php',
+            'path' => '%currentWorkingDirectory%/typo3/sysext/install/Classes/SystemEnvironment/Check.php',
             'count' => 3,
         ],
         [
             'message' => '#^Parameter \\#6 \\$color of function imagefilledrectangle expects int, int\\|false given\\.$#',
-            'path' => '../typo3/sysext/install/Classes/Controller/EnvironmentController.php',
+            'path' => '%currentWorkingDirectory%/typo3/sysext/install/Classes/Controller/EnvironmentController.php',
             'count' => 4,
         ],
         [
             'message' => '#^Parameter \\#1 \\$image of function imagefilledrectangle expects GdImage, GdImage\\|false given\\.$#',
-            'path' => '../',
+            'path' => '%currentWorkingDirectory%/',
             'count' => 4,
         ],
         [
             'message' => '#^Parameter \\#1 \\$image of function imagegif expects GdImage, GdImage\\|false given\\.$#',
-            'path' => '../',
+            'path' => '%currentWorkingDirectory%/',
             'count' => 1,
         ],
         [
             'message' => '#^Parameter \\#6 \\$color of function imagettftext expects int, int\\|false given\\.$#',
-            'path' => '../',
+            'path' => '%currentWorkingDirectory%/',
             'count' => 1,
         ],
         [
             'message' => '#^Parameter \\#1 \\$image of function imagettftext expects GdImage, GdImage\\|false given\\.$#',
-            'path' => '../',
+            'path' => '%currentWorkingDirectory%/',
             'count' => 1,
         ],
         [
             'message' => '#^Parameter \\#1 \\$image of function imagecolorallocate expects GdImage, GdImage\\|false given\\.$#',
-            'path' => '../',
+            'path' => '%currentWorkingDirectory%/',
             'count' => 6,
         ],
         [
             'message' => '#^Parameter \\#[1-4]{1} \\$[a-z]* of function [a-z_]* expects XmlParser, resource\\|XmlParser given\\.$#',
-            'path' => '../typo3/sysext/extensionmanager/Classes/Utility/Parser/ExtensionXmlPushParser.php',
+            'path' => '%currentWorkingDirectory%/typo3/sysext/extensionmanager/Classes/Utility/Parser/ExtensionXmlPushParser.php',
             'count' => 9,
         ],
         [
             'message' => '#^Parameter \\#1 \\$semaphore of function sem_release expects SysvSemaphore, resource\\|SysvSemaphore given\\.$#',
-            'path' => '../typo3/sysext/core/Classes/Locking/SemaphoreLockStrategy.php',
+            'path' => '%currentWorkingDirectory%/typo3/sysext/core/Classes/Locking/SemaphoreLockStrategy.php',
             'count' => 1,
         ],
         [
             'message' => '#^Parameter \\#1 \\$semaphore of function sem_remove expects SysvSemaphore, resource\\|SysvSemaphore given\\.$#',
-            'path' => '../typo3/sysext/core/Classes/Locking/SemaphoreLockStrategy.php',
+            'path' => '%currentWorkingDirectory%/typo3/sysext/core/Classes/Locking/SemaphoreLockStrategy.php',
             'count' => 1,
         ],
         [
             'message' => '#^Parameter \\#2 \\$color of function imagecolortransparent expects int\\|null, int\\|false given\\.$#',
-            'path' => '../typo3/sysext/frontend/Classes/Imaging/GifBuilder.php',
+            'path' => '%currentWorkingDirectory%/typo3/sysext/frontend/Classes/Imaging/GifBuilder.php',
             'count' => 1
         ]
     ];
