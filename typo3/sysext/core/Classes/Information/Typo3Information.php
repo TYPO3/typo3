c012b2055043d321df2bfb91381b377e196b1217
Georg Ringer <georg.ringer@gmail.com>
2021-05-24 18:41:05 +0200

    [TASK] Update url to license information
    As the page about licensing changed, the url should be
    updated to avoid unneeded redirects.
    
    Resolves: #94188
    Releases: master, 10.4, 9.5
    Change-Id: I1e4103df925972df63cbe8ece4a780e5af713ac0
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/69253
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Richard Haeser <richard@richardhaeser.com>
    Tested-by: Oliver Bartsch <bo@cedev.de>
    Reviewed-by: Richard Haeser <richard@richardhaeser.com>
    Reviewed-by: Oliver Bartsch <bo@cedev.de>

--- typo3/sysext/core/Classes/Information/Typo3Information.php
+++ typo3/sysext/core/Classes/Information/Typo3Information.php
@@ -25,7 +25,7 @@ use TYPO3\CMS\Core\Localization\LanguageService;
 class Typo3Information
 {
     public const URL_COMMUNITY = 'https://typo3.org/';
-    public const URL_LICENSE = 'https://typo3.org/typo3-cms/overview/licenses/';
+    public const URL_LICENSE = 'https://typo3.org/project/licenses/';
     public const URL_EXCEPTION = 'https://typo3.org/go/exception/CMS/';
     public const URL_DONATE = 'https://typo3.org/community/contribute/donate/';
     public const URL_OPCACHE = 'https://docs.typo3.org/m/typo3/guide-installation/master/en-us/Troubleshooting/Index.html#opcode-cache-messages';
