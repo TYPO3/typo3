13e76498fd9fe8206a3e2389f3e99134d9c8c5c7
Christian Kuhn <lolli@schwarzbu.ch>
2021-05-30 17:53:42 +0200

    [TASK] Remove unused ext:beuser context menu entry code
    Did you know there's supposed to be a context menu
    entry on pages in 'more' section that jumps to the edit
    access view of the permission module? No? Well, it's
    broken since the v8 context menu rewrite and never
    rendered due to insufficient initialization.
    
    forge doesn't have a single bug report about this, it
    seems nobody ever missed that entry. This is probably
    due to its limited use - the access module is used
    by admins rather seldom, as soon as the basic access
    rights system has been done for an instance and some
    pageTS settings take care of setting proper access for
    new pages, there is little need to fiddle with it
    often. And if it's used, it's accessed via the main
    module.
    
    Let's drop the implementation of the context menu
    entry instead of fixing it, to not bloat the 'more'
    section with an entry nobody missed.
    
    Resolves: #94237
    Related: #78192
    Releases: master
    Change-Id: I6c87ad8b9f4f2945b29b540e800811ab607bdadd
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/69329
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Simon Gilli <typo3@gilbertsoft.org>
    Tested-by: Anja Leichsenring <aleichsenring@ab-softlab.de>
    Reviewed-by: Simon Gilli <typo3@gilbertsoft.org>
    Reviewed-by: Anja Leichsenring <aleichsenring@ab-softlab.de>

--- typo3/sysext/beuser/ext_localconf.php
+++ typo3/sysext/beuser/ext_localconf.php
@@ -3,5 +3,3 @@
 defined('TYPO3') or die();
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_userauth.php']['logoff_pre_processing'][] = \TYPO3\CMS\Beuser\Hook\SwitchBackUserHook::class . '->switchBack';
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['typo3/backend.php']['constructPostProcess'][] = \TYPO3\CMS\Beuser\Hook\BackendControllerHook::class . '->addJavaScript';
-$GLOBALS['TYPO3_CONF_VARS']['BE']['ContextMenu']['ItemProviders'][1486418730] = \TYPO3\CMS\Beuser\ContextMenu\ItemProvider::class;
