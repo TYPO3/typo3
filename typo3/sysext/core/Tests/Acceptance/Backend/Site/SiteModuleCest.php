96798f4ff28365b64c28306c3da2962f16c67628
Christian Kuhn <lolli@schwarzbu.ch>
2021-06-13 00:39:28 +0200

    [BUGFIX] Correct ac test file namespace
    It's TYPO3\CMS\Core\Tests\Acceptance\Backend\Site\SiteModuleCest
    not TYPO3\CMS\Core\Tests\Acceptance\Backend\Redirect\SiteModuleCest
    
    Resolves: #94330
    Releases: master, 10.4
    Change-Id: I0661feec976aeeddce179d9177e36e729fc0b72d
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/69477
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>

--- typo3/sysext/core/Tests/Acceptance/Backend/Site/SiteModuleCest.php
+++ typo3/sysext/core/Tests/Acceptance/Backend/Site/SiteModuleCest.php
@@ -15,7 +15,7 @@ declare(strict_types=1);
  * The TYPO3 project - inspiring people to share!
  */
 
-namespace TYPO3\CMS\Core\Tests\Acceptance\Backend\Redirect;
+namespace TYPO3\CMS\Core\Tests\Acceptance\Backend\Site;
 
 use TYPO3\CMS\Core\Tests\Acceptance\Support\BackendTester;
 use TYPO3\CMS\Core\Tests\Acceptance\Support\Helper\ModalDialog;
