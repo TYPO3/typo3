5a70786953f4cc951c94b92857e15a6f0e4f5b89
Rico Sonntag <rico.sonntag@netresearch.de>
2021-06-18 14:32:34 +0200

    Fixed missing is_array check in setValueByPath
    PHP Warning: array_key_exists() expects parameter 2 to be array, null given in /var/www/public/typo3/sysext/core/Classes/Utility/ArrayUtility.php line 297

--- typo3/sysext/core/Classes/Utility/ArrayUtility.php
+++ typo3/sysext/core/Classes/Utility/ArrayUtility.php
@@ -289,7 +289,7 @@ class ArrayUtility
                 throw new \RuntimeException('Invalid path segment specified', 1341406846);
             }
             // Create cell if it doesn't exist
-            if (!array_key_exists($segment, $pointer)) {
+            if (is_array($pointer) && !array_key_exists($segment, $pointer)) {
                 $pointer[$segment] = [];
             }
             // Set pointer to new cell
