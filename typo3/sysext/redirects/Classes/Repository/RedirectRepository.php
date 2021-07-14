49b5a49a38771d14c462370d0b9a0965e22b9fbe
Daniel Goerz <daniel.goerz@posteo.de>
2021-07-14 17:46:19 +0200

    [BUGFIX] Make "Never hit" redirect filter work in combination with other filters
    Also this patch assures the filter auto submitting
    is working in case no redirects were found.
    
    Resolves: #94560
    Releases: master
    Change-Id: I26157a13857976d9a4a3a31627790156f5b6cad2
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/69852
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Jochen <rothjochen@gmail.com>
    Tested-by: Oliver Bartsch <bo@cedev.de>
    Reviewed-by: Jochen <rothjochen@gmail.com>
    Reviewed-by: Oliver Bartsch <bo@cedev.de>

--- typo3/sysext/redirects/Classes/Repository/RedirectRepository.php
+++ typo3/sysext/redirects/Classes/Repository/RedirectRepository.php
@@ -97,12 +97,14 @@ class RedirectRepository
         }
 
         if ($demand->hasMaxHits()) {
-            $queryBuilder->andWhere(
-                $queryBuilder->expr()->lt('hitcount', $queryBuilder->createNamedParameter($demand->getMaxHits(), \PDO::PARAM_INT))
+            $constraints[] = $queryBuilder->expr()->lt(
+                'hitcount',
+                $queryBuilder->createNamedParameter($demand->getMaxHits(), \PDO::PARAM_INT)
             );
             // When max hits is set, exclude records which explicitly disabled the hitcount feature
-            $queryBuilder->andWhere(
-                $queryBuilder->expr()->eq('disable_hitcount', $queryBuilder->createNamedParameter(0, \PDO::PARAM_INT))
+            $constraints[] = $queryBuilder->expr()->eq(
+                'disable_hitcount',
+                $queryBuilder->createNamedParameter(0, \PDO::PARAM_INT)
             );
         }
 
