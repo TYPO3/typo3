e566f832137941565d2bd777f9f92c95b29eba5c
Oliver Hader <oliver@typo3.org>
2021-07-19 16:01:59 +0200

    [BUGFIX] Upgrade packages chart.js, codemirror, ckeditor4
    Upgrade JavaScript packages chart.js, codemirror and ckeditor4
    addressing known and disclosed vulnerabilities.
    
    * chart.js: Prototype Pollution
      https://app.snyk.io/vuln/SNYK-JS-CHARTJS-1018716
    * codemirror: Regular Expression DoS (ReDoS)
      https://app.snyk.io/vuln/SNYK-JS-CODEMIRROR-1016937
    * ckeditor4: Cross-Site Scripting
      https://app.snyk.io/vuln/SNYK-JS-CKEDITOR4-1303090
    
    Executed command:
    ```
    cd Build; nvm use; yarn upgrade chart.js codemirror ckeditor4
    ```
    
    Resolves: #94583
    Releases: master, 11.3, 10.4, 9.5
    Change-Id: I56c1948f5785f4ecf9f51998f006825a952280bd
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/69963
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Oliver Hader <oliver.hader@typo3.org>
    Reviewed-by: Oliver Hader <oliver.hader@typo3.org>

--- typo3/sysext/rte_ckeditor/Resources/Public/JavaScript/Contrib/plugins/font/lang/lv.js
+++ typo3/sysext/rte_ckeditor/Resources/Public/JavaScript/Contrib/plugins/font/lang/lv.js
@@ -1,1 +1,1 @@
-﻿CKEDITOR.plugins.setLang("font","lv",{fontSize:{label:"Izmērs",voiceLabel:"Fonta izmeŗs",panelTitle:"Izmērs"},label:"Šrifts",panelTitle:"Šrifts",voiceLabel:"Fonts"});
+﻿CKEDITOR.plugins.setLang("font","lv",{fontSize:{label:"Lielums",voiceLabel:"Fonta lielums",panelTitle:"Fonta lielums"},label:"Fonts",panelTitle:"Fonta nosaukums",voiceLabel:"Fonts"});
