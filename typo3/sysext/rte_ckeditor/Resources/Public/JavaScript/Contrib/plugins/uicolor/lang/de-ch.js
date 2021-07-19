048d29576fdf5cda9c06fd492c510fa1aa215153
Oliver Hader <oliver@typo3.org>
2021-07-19 16:01:59 +0200

    [BUGFIX] Upgrade packages chart.js, codemirror, ckeditor4
    Upgrade JavaScript packages chart.js, codemirror and ckeditor4
    addressing known and disclosed vulnerabilities.
    
    * chart.js: Prototype Pollution
      https://app.snyk.io/vuln/SNYK-JS-CHARTJS-1018716
    * codemirror: Regular Expression DoS (ReDoS)
      https://app.snyk.io/vuln/SNYK-JS-CODEMIRROR-1016937
    * ckeditor4: Cross-Site Scripting
      https://app.snyk.io/vuln/SNYK-JS-CKEDITOR4-1303090
    
    Executed command:
    ```
    cd Build; nvm use; yarn upgrade chart.js codemirror ckeditor4
    ```
    
    Resolves: #94583
    Releases: master, 10.4, 9.5
    Change-Id: I56c1948f5785f4ecf9f51998f006825a952280bd
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/69956
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Oliver Bartsch <bo@cedev.de>
    Tested-by: Oliver Hader <oliver.hader@typo3.org>
    Reviewed-by: Oliver Bartsch <bo@cedev.de>
    Reviewed-by: Oliver Hader <oliver.hader@typo3.org>

--- typo3/sysext/rte_ckeditor/Resources/Public/JavaScript/Contrib/plugins/uicolor/lang/de-ch.js
+++ typo3/sysext/rte_ckeditor/Resources/Public/JavaScript/Contrib/plugins/uicolor/lang/de-ch.js
@@ -2,4 +2,4 @@
  Copyright (c) 2003-2021, CKSource - Frederico Knabben. All rights reserved.
  For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
 */
-CKEDITOR.plugins.setLang("uicolor","de-ch",{title:"UI-Farbpipette",options:"Color Options",highlight:"Highlight",selected:"Selected Color",predefined:"Vordefinierte Farbsätze",config:"Fügen Sie diese Zeichenfolge in die Datei config.js ein."});
+CKEDITOR.plugins.setLang("uicolor","de-ch",{title:"UI-Farbpipette",options:"Farboptionen",highlight:"Hervorheben",selected:"Ausgewählte Farbe",predefined:"Vordefinierte Farbsätze",config:"Fügen Sie diese Zeichenfolge in die Datei config.js ein."});
