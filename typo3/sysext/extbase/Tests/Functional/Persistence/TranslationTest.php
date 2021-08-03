98a059e8b1e0f909f7bdad60b015e6e0af1ca20d
Andreas Fernandez <a.fernandez@scripting-base.de>
2021-08-03 09:05:04 +0200

    [TASK] Add tests for wrong translation behavior of Extbase
    Scenario:
    
    * a site with another language being configured as "strict"
    * an Extbase plugin consuming a list of records identified by their
      respective uid
    * plugin and records are localized to the site's language
    
    Expectation:
    
    The plugin receives the UIDs of the original record UIDs having
    sys_language_uid = 0. The plugin uses a custom Extbase Repository and
    passes the UIDs as array to a ->in() clause:
    
    ```
    $query = $this->createQuery();
    $query->matching($query->in('uid', [1, 2, 3]));
    ```
    
    According to the site configuration, Extbase should be able to find these
    records in the default language and either overlay if translated, or
    discard them if no translations are available.
    
    Actual result:
    
    Extbase is not able to find any record and thus returns an empty result
    set. The identified cause is that Extbase searches the database for the
    given record UIDs in conjunction with the current language id,
    returning an empty set by definition.
    
    To prove the wrong behavior, tests are added that show the broken
    behavior, the actual fix will come with another patch in #88137.
    
    Resolves: #94694
    Related: #88137
    Releases: master, 10.4
    Change-Id: I9a7a6ec5d1b5cfb82e1fc05365fde6e45df12dfc
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/70192
    Tested-by: Anja Leichsenring <aleichsenring@ab-softlab.de>
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Benni Mack <benni@typo3.org>
    Reviewed-by: Anja Leichsenring <aleichsenring@ab-softlab.de>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Reviewed-by: Benni Mack <benni@typo3.org>

--- typo3/sysext/extbase/Tests/Functional/Persistence/TranslationTest.php
+++ typo3/sysext/extbase/Tests/Functional/Persistence/TranslationTest.php
@@ -106,7 +106,7 @@ class TranslationTest extends FunctionalTestCase
         self::assertFalse($querySettings->getLanguageOverlayMode());
 
         $postCount = $query->execute()->count();
-        self::assertSame(3, $postCount);
+        self::assertSame(4, $postCount);
     }
 
     /**
@@ -165,7 +165,7 @@ class TranslationTest extends FunctionalTestCase
 
         $postCount = $query->execute()->count();
 
-        self::assertSame(1, $postCount);
+        self::assertSame(2, $postCount);
     }
 
     /**
@@ -192,6 +192,26 @@ class TranslationTest extends FunctionalTestCase
     }
 
     /**
+     * @test
+     */
+    public function fetchingPostsByInClauseReturnsDefaultPostsWithFallback(): void
+    {
+        $query = $this->postRepository->createQuery();
+        $querySettings = $query->getQuerySettings();
+        $querySettings->setStoragePageIds([1]);
+        $querySettings->setRespectSysLanguage(false);
+        $querySettings->setLanguageOverlayMode(true);
+        $querySettings->setLanguageUid(2);
+
+        $query->matching($query->in('uid', [4]));
+        /** @var Post[]|array $posts */
+        $posts = $query->execute()->toArray();
+
+        self::assertCount(1, $posts);
+        self::assertSame('Post2', $posts[0]->getTitle());
+    }
+
+    /**
      * This tests shows overlays in action
      *
      * @test
@@ -211,8 +231,9 @@ class TranslationTest extends FunctionalTestCase
         /** @var Post[]|array $posts */
         $posts = $query->execute()->toArray();
 
-        self::assertCount(1, $posts);
+        self::assertCount(2, $posts);
         self::assertSame('GR:Post1', $posts[0]->getTitle());
+        self::assertSame('GR:Post11', $posts[1]->getTitle());
     }
 
     /**
@@ -235,8 +256,79 @@ class TranslationTest extends FunctionalTestCase
         /** @var Post[]|array $posts */
         $posts = $query->execute()->toArray();
 
-        self::assertCount(1, $posts);
+        self::assertCount(2, $posts);
         self::assertSame('GR:Post1', $posts[0]->getTitle());
+        self::assertSame('GR:Post11', $posts[1]->getTitle());
+    }
+
+    public function fetchingTranslatedPostByUidDataProvider(): array
+    {
+        return [
+            'with one id' => [
+                'input' => [12],
+                'expectedTitles' => ['GR:Post11'],
+            ],
+            'with two ids' => [
+                'input' => [12, 1],
+                'expectedTitles' => ['GR:Post11', 'GR:Post1'],
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider fetchingTranslatedPostByUidDataProvider
+     * @test
+     */
+    public function fetchingTranslatedPostByInClauseWithStrictLanguageSettings(array $input, array $expectedTitles): void
+    {
+        $query = $this->postRepository->createQuery();
+        $querySettings = $query->getQuerySettings();
+        $querySettings->setStoragePageIds([1]);
+        $querySettings->setRespectSysLanguage(true);
+        $querySettings->setLanguageUid(2);
+        $querySettings->setLanguageOverlayMode('hideNonTranslated');
+
+        $query->matching($query->in('uid', $input));
+        /** @var Post[]|array $posts */
+        $posts = $query->execute()->toArray();
+
+        // @todo: wrong assertion
+        // We're simulating a strict language configuration where a blog post (uid=12 or uid=1) has been translated to another
+        // language. However, Extbase is not able to find the translated record via ->in() and therefore returns an
+        // empty result set. This will be fixed with https://review.typo3.org/c/Packages/TYPO3.CMS/+/67893
+        self::assertCount(0, $posts);
+        // self::assertCount(count($expectedTitles), $posts);
+        // self::assertEqualsCanonicalizing($expectedTitles, array_map(static function(Post $post) { return $post->getTitle(); }, $posts));
+    }
+
+    /**
+     * @dataProvider fetchingTranslatedPostByUidDataProvider
+     * @test
+     */
+    public function fetchingTranslatedPostByEqualsUidClauseWithStrictLanguageSettings(array $input, array $expectedTitles): void
+    {
+        $query = $this->postRepository->createQuery();
+        $querySettings = $query->getQuerySettings();
+        $querySettings->setStoragePageIds([1]);
+        $querySettings->setRespectSysLanguage(true);
+        $querySettings->setLanguageUid(2);
+        $querySettings->setLanguageOverlayMode('hideNonTranslated');
+
+        $constraints = [];
+        foreach ($input as $uid) {
+            $constraints[] = $query->equals('uid', $uid);
+        }
+        $query->matching($query->logicalOr(...$constraints));
+        /** @var Post[]|array $posts */
+        $posts = $query->execute()->toArray();
+
+        // @todo: wrong assertion
+        // We're simulating a strict language configuration where a blog post (uid=12 or uid=1) has been translated to another
+        // language. However, Extbase is not able to find the translated record via ->equals(uid=12 OR uid=1 OR ...) and therefore returns an
+        // empty result set. This will be fixed with https://review.typo3.org/c/Packages/TYPO3.CMS/+/67893
+        self::assertCount(0, $posts);
+        // self::assertCount(count($expectedTitles), $posts);
+        // self::assertEqualsCanonicalizing($expectedTitles, array_map(static function(Post $post) { return $post->getTitle(); }, $posts));
     }
 
     /**
@@ -313,7 +405,7 @@ class TranslationTest extends FunctionalTestCase
         /** @var Post[] $posts */
         $posts = $query->execute()->toArray();
 
-        self::assertCount(4, $posts);
+        self::assertCount(5, $posts);
         self::assertSame('Post10', $posts[3]->getTitle());
     }
 
@@ -339,10 +431,11 @@ class TranslationTest extends FunctionalTestCase
         /** @var Post[] $posts */
         $posts = $query->execute()->toArray();
 
-        self::assertCount(2, $posts);
+        self::assertCount(3, $posts);
 
         self::assertSame('GR:Post1', $posts[0]->getTitle());
         self::assertSame('GR:Post10', $posts[1]->getTitle());
+        self::assertSame('GR:Post11', $posts[2]->getTitle());
     }
 
     /**
@@ -370,13 +463,14 @@ class TranslationTest extends FunctionalTestCase
         /** @var Post[] $posts */
         $posts = $query->execute()->toArray();
 
-        self::assertCount(4, $posts);
+        self::assertCount(5, $posts);
 
         self::assertSame('GR:Post1', $posts[0]->getTitle());
         self::assertSame('Post2', $posts[1]->getTitle());
         self::assertSame('Post3', $posts[2]->getTitle());
-        // once the issue is fixed this assertion should be GR:Post10
+        // once the issue is fixed this assertions should be GR:Post10
         self::assertSame('Post10', $posts[3]->getTitle());
+        self::assertSame('GR:Post11', $posts[4]->getTitle());
     }
 
     /**
@@ -420,8 +514,9 @@ class TranslationTest extends FunctionalTestCase
         $query->matching($query->equals('blog.title', 'Blog1'));
         /** @var Post[]|array $posts */
         $posts = $query->execute()->toArray();
-        self::assertCount(1, $posts);
+        self::assertCount(2, $posts);
         self::assertSame('GR:Post1', $posts[0]->getTitle());
+        self::assertSame('GR:Post11', $posts[1]->getTitle());
     }
 
     /**
