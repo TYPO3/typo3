4d80fb73e0c6c291a43a4c360952f49b99675fa2
Oliver Hader <oliver@typo3.org>
2021-08-12 13:56:26 +0200

    [TASK] Forward initiator to typo3/html-sanitizer
    A new `SanitizerInitiator` is added and forwarded to
    `typo3/html-sanitizer`. This allows getting a full stack-trace
    when HTML nodes have been sanitized/modified and to debug the
    actual cause (initiator) much better.
    
    To receive corresponding initiator stack-traces
    
    * logging for TYPO3.HtmlSanitizer namespace needs to be enabled
    * TypoScript `config.debug = 1` must be set, or as a fall-back
      `$GLOBALS['TYPO3_CONF_VARS']['FE']['debug'] = true;` must be set
    * HTML sanitizer must have found and modified invalid tags/attributes
    
    Resolves: #94837
    Releases: master, 11.3, 10.4, 9.5
    Change-Id: I0239785d347d2c4ad6153ccb26130556399949d8
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/70490
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Oliver Hader <oliver.hader@typo3.org>
    Reviewed-by: Oliver Hader <oliver.hader@typo3.org>

---
+++ typo3/sysext/core/Classes/Html/SanitizerInitiator.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 project.
+ *
+ * It is free software; you can redistribute it and/or modify it under the terms
+ * of the MIT License (MIT). For the full copyright and license information,
+ * please read the LICENSE file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3\CMS\Core\Html;
+
+use TYPO3\HtmlSanitizer\InitiatorInterface;
+
+/**
+ * Initiator for HTML sanitization process, forwarded to sanitizer and used during logging.
+ *
+ * @internal
+ */
+class SanitizerInitiator implements InitiatorInterface
+{
+    /**
+     * @var string
+     */
+    protected $trace;
+
+    public function __construct(string $trace)
+    {
+        $this->trace = $trace;
+    }
+
+    public function __toString(): string
+    {
+        return $this->trace;
+    }
+}
