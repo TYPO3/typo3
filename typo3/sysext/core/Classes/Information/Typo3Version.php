d8d0dffef3244bc25cce491a44debd083b10ed8f
Oliver Hader <oliver@typo3.org>
2021-09-21 10:52:20 +0200

    [RELEASE] Release of TYPO3 10.4.21
    Change-Id: Ib333a4f0005102c6930db34015bdbcf457a7307b
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/71158
    Tested-by: Oliver Hader <oliver.hader@typo3.org>
    Reviewed-by: Oliver Hader <oliver.hader@typo3.org>

--- typo3/sysext/core/Classes/Information/Typo3Version.php
+++ typo3/sysext/core/Classes/Information/Typo3Version.php
@@ -19,7 +19,7 @@ namespace TYPO3\CMS\Core\Information;
 
 class Typo3Version
 {
-    protected const VERSION = '10.4.21-dev';
+    protected const VERSION = '10.4.21';
     protected const BRANCH = '10.4';
 
     /**
