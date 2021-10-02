a64d78c5df3832d0e7eb2307061da776bb66e19a
Sybille Peters <sypets@gmx.de>
2021-10-02 18:53:58 +0200

    [BUGFIX] Make Linkvalidator event listener more robust
    A page link may not contain the query parameter uid.
    Make sure that the event listener is robust and handles
    unfilled array elements.
    
    Resolves: #95166
    Releases: master, 10.4
    Change-Id: Ia82d09704055e2ae5519ec1d1d8189ccc51bc1bc
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/71418
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Oliver Bartsch <bo@cedev.de>
    Tested-by: Stefan Bürk <stefan@buerk.tech>
    Tested-by: Benni Mack <benni@typo3.org>
    Reviewed-by: Oliver Bartsch <bo@cedev.de>
    Reviewed-by: Stefan Bürk <stefan@buerk.tech>
    Reviewed-by: Benni Mack <benni@typo3.org>

---
+++ typo3/sysext/linkvalidator/Tests/Functional/EventListener/CheckBrokenRteLinkEventListenerTest.php
@@ -0,0 +1,83 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3\CMS\Linkvalidator\Tests\Functional\EventListener;
+
+use TYPO3\CMS\Core\Html\Event\BrokenLinkAnalysisEvent;
+use TYPO3\CMS\Linkvalidator\EventListener\CheckBrokenRteLinkEventListener;
+use TYPO3\CMS\Linkvalidator\Repository\BrokenLinkRepository;
+use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
+
+class CheckBrokenRteLinkEventListenerTest extends FunctionalTestCase
+{
+    protected CheckBrokenRteLinkEventListener $subject;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->subject = new CheckBrokenRteLinkEventListener(
+            new BrokenLinkRepository()
+        );
+    }
+
+    /**
+     * @test
+     * @dataProvider checkPageLinkTestDataProvider
+     */
+    public function checkPageLinkTest(string $linkType, array $linkData, bool $isMarkedAsBroken): void
+    {
+        $this->importDataSet('PACKAGE:typo3/testing-framework/Resources/Core/Functional/Fixtures/pages.xml');
+
+        $event = new BrokenLinkAnalysisEvent(
+            $linkType,
+            $linkData
+        );
+
+        $this->subject->checkPageLink($event);
+        self::assertEquals($isMarkedAsBroken, $event->isBrokenLink());
+    }
+
+    public function checkPageLinkTestDataProvider(): \Generator
+    {
+        yield 'No uid parameter given' => [
+            'page',
+            [
+                'parameters' => 'alias=-',
+                'type' => 'page',
+            ],
+            false,
+        ];
+        yield 'Page exist' => [
+            'page',
+            [
+                'parameters' => 'alias=foo',
+                'pageuid' => '2',
+                'type' => 'page',
+            ],
+            false,
+        ];
+        yield 'Page not found' => [
+            'page',
+            [
+                'parameters' => 'alias=foo',
+                'pageuid' => '12345',
+                'type' => 'page',
+            ],
+            true,
+        ];
+    }
+}
