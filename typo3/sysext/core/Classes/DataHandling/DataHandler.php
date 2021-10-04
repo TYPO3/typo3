168a50dd5d8cad2b3362625a55b876cb89bec891
Andreas Fernandez <a.fernandez@scripting-base.de>
2021-10-04 21:28:25 +0200

    Revert "[TASK] Fix undefined array key error in DataHandler logs"
    This reverts commit 84a5e8679384e276200c04f52b6ea84d7762aa67.
    
    Reason for revert:
    
    The proposed and merged solution of #95066 has some undetected flaws:
    
    * array unpacking fails if the array is associative (happens with SU
      mode, see https://github.com/TYPO3/typo3/blob/master/typo3/sysext/core/Classes/Error/ErrorHandler.php#L254)
    * assumes all required arguments for sprintf() are available
    
    Since both cases cannot be solved in a clean way it's better to revert the original patch and fix only the original issue.
    
    Change-Id: Ib7811f6174d517e4b7c24f53b9e0d93c8914d5a1
    Reverts: #95066
    Resolves: #95469
    Releases: master
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/71357
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Oliver Bartsch <bo@cedev.de>
    Tested-by: Andreas Fernandez <a.fernandez@scripting-base.de>
    Reviewed-by: Oliver Bartsch <bo@cedev.de>
    Reviewed-by: Oliver Klee <typo3-coding@oliverklee.de>
    Reviewed-by: Andreas Fernandez <a.fernandez@scripting-base.de>

--- typo3/sysext/core/Classes/DataHandling/DataHandler.php
+++ typo3/sysext/core/Classes/DataHandling/DataHandler.php
@@ -8855,8 +8855,10 @@ class DataHandler implements LoggerAwareInterface
 
         while ($row = $result->fetchAssociative()) {
             $log_data = unserialize($row['log_data']) ?: [];
-            $msg = $row['error'] . ': ' . sprintf($row['details'], ...$log_data);
+            $msg = $row['error'] . ': ' . sprintf($row['details'], $log_data[0] ?? '', $log_data[1] ?? '', $log_data[2] ?? '', $log_data[3] ?? '', $log_data[4] ?? '');
+            /** @var FlashMessage $flashMessage */
             $flashMessage = GeneralUtility::makeInstance(FlashMessage::class, $msg, '', $row['error'] === SystemLogErrorClassification::WARNING ? FlashMessage::WARNING : FlashMessage::ERROR, true);
+            /** @var FlashMessageService $flashMessageService */
             $flashMessageService = GeneralUtility::makeInstance(FlashMessageService::class);
             $defaultFlashMessageQueue = $flashMessageService->getMessageQueueByIdentifier();
             $defaultFlashMessageQueue->enqueue($flashMessage);
