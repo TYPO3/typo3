17890022031fcd79a66bfa4c845dfe93dad55c10
Christian Kuhn <lolli@schwarzbu.ch>
2021-10-11 14:17:42 +0200

    [BUGFIX] No exclude for transOrigPointerField
    One of the functional test related extensions still sets
    'exclude' TCA property for a transOrigPointerField, which
    is deprecated and migrated away since fixing #89550.
    
    Resolves: #95561
    Related: #89550
    Releases: master
    Change-Id: I1f5bce2ef3ef6daee0ce57fb0e34bb4a48022fdb
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/71530
    Tested-by: Oliver Bartsch <bo@cedev.de>
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Jochen <rothjochen@gmail.com>
    Tested-by: Nikita Hovratov <nikita.h@live.de>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Reviewed-by: Oliver Bartsch <bo@cedev.de>
    Reviewed-by: Nikita Hovratov <nikita.h@live.de>
    Reviewed-by: Jochen <rothjochen@gmail.com>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>

--- typo3/sysext/core/Tests/Functional/Fixtures/Extensions/test_datahandler/Configuration/TCA/tx_testdatahandler_element.php
+++ typo3/sysext/core/Tests/Functional/Fixtures/Extensions/test_datahandler/Configuration/TCA/tx_testdatahandler_element.php
@@ -31,7 +31,6 @@ return [
         ],
         'l10n_parent' => [
             'displayCond' => 'FIELD:sys_language_uid:>:0',
-            'exclude'     => 1,
             'label'       => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.l18n_parent',
             'config'      => [
                 'type'  => 'select',
