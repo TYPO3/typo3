dcba2a7ce93eaef0ad025dc21fdeb85636d7b4f4
Benni Mack <benni@typo3.org>
2021-10-12 14:17:52 +0200

    [RELEASE] Release of TYPO3 11.5.1
    Change-Id: I9a38139470a45db0f2f0062165b69180a1b58bc1
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/71561
    Tested-by: Benni Mack <benni@typo3.org>
    Reviewed-by: Benni Mack <benni@typo3.org>

--- typo3/sysext/core/Classes/Information/Typo3Version.php
+++ typo3/sysext/core/Classes/Information/Typo3Version.php
@@ -19,7 +19,7 @@ namespace TYPO3\CMS\Core\Information;
 
 class Typo3Version
 {
-    protected const VERSION = '11.5.1-dev';
+    protected const VERSION = '11.5.1';
     protected const BRANCH = '11.5';
 
     /**
