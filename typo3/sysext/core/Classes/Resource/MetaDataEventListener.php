685b64b31d4edad5d8ddeee62f945e9c9368bde8
André Buchmann <andy.schliesser@gmail.com>
2021-11-04 08:21:52 +0100

    [BUGFIX] Translate image dimensions on file replace
    Width and height of an image is stored in translatable metadata
    records. These properties are extracted by FAL API when a new image is
    uploaded. The TYPO3\CMS\Core\Resource\Index\Indexer::updateIndexEntry
    method uses the MetaDataAspect to save the new file dimensions.
    
    As the MetaDataAspect has no support for multi-language, it only updates
    the default language. A new PSR-14 event listener is added on
    AfterFileMetaDataUpdatedEvent to overcome the lack of translation
    support. It updates the width and height of all corresponding
    sys_file_metadata translations.
    
    Wrong dimensions leads to rendering issues in the frontend when the
    image is cropped as the crop area can't be calculated properly.
    
    Resolves: #87162
    Releases: master, 10.4
    Change-Id: Ic1d66a24f23f4312325b3f74e5959b2d2b3c1cf6
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/72044
    Tested-by: André Buchmann <andy.schliesser@gmail.com>
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Jochen <rothjochen@gmail.com>
    Tested-by: Benni Mack <benni@typo3.org>
    Reviewed-by: André Buchmann <andy.schliesser@gmail.com>
    Reviewed-by: Jochen <rothjochen@gmail.com>
    Reviewed-by: Benni Mack <benni@typo3.org>

---
+++ typo3/sysext/core/Classes/Resource/MetaDataEventListener.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3\CMS\Core\Resource;
+
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Resource\Event\AfterFileMetaDataUpdatedEvent;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+/**
+ * @internal Marked as internal for now, methods in this class may change any time.
+ */
+final class MetaDataEventListener
+{
+    private string $tableName = 'sys_file_metadata';
+
+    /**
+     * @throws \Doctrine\DBAL\Driver\Exception
+     */
+    public function afterFileMetaDataUpdated(AfterFileMetaDataUpdatedEvent $event): void
+    {
+        $record = $event->getRecord();
+
+        if ((int)$record['width'] <= 0 || (int)$record['height'] <= 0) {
+            return;
+        }
+
+        $metaData = [
+            'width' => (int)$record['width'],
+            'height' => (int)$record['height'],
+        ];
+
+        // Fetch translated meta data records
+        $connection = GeneralUtility::makeInstance(ConnectionPool::class)->getConnectionForTable($this->tableName);
+        $translations = $connection->select(
+            ['uid'],
+            $this->tableName,
+            ['file' => $event->getFileUid(), 'l10n_parent' => $event->getMetaDataUid()]
+        )->fetchFirstColumn();
+
+        if (empty($translations)) {
+            return;
+        }
+
+        // Update width and height of all translations
+        foreach ($translations as $uid) {
+            if ((int)$uid > 0) {
+                $connection->update(
+                    $this->tableName,
+                    $metaData,
+                    [
+                        'uid' => (int)$uid,
+                    ],
+                );
+            }
+        }
+    }
+}
