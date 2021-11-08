9f23ccc8b090bfe6403b3527304b23662a5e2274
Sybille Peters <sypets@gmx.de>
2021-11-08 09:19:44 +0100

    [TASK] Fix return type declaration for slug provider
    PHPDoc block for method PageSlugCandidateProvider::getCandidatesForPath()
    had incorrect return type of string[].
    
    This is changed to array<int,array<string,mixed>>.
    
    Resolves: #95901
    Releases: master, 10.4
    Change-Id: I9a7cd326acf3dd88bfb2833b50b135a9d84164eb
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/72076
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Oliver Bartsch <bo@cedev.de>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Reviewed-by: Oliver Klee <typo3-coding@oliverklee.de>
    Reviewed-by: Oliver Bartsch <bo@cedev.de>

--- typo3/sysext/core/Classes/Routing/PageSlugCandidateProvider.php
+++ typo3/sysext/core/Classes/Routing/PageSlugCandidateProvider.php
@@ -68,7 +68,7 @@ class PageSlugCandidateProvider
      *
      * @param string $urlPath
      * @param SiteLanguage $language
-     * @return string[]
+     * @return array<int,array<string,mixed>>
      */
     public function getCandidatesForPath(string $urlPath, SiteLanguage $language): array
     {
