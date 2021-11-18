bec5aa24466818ee323292b88e20e135ecb27cef
Jochen Roth <jochen.roth@b13.com>
2021-11-18 23:22:33 +0100

    [BUGFIX] Properly check permissions for system notes
    The NoteController, used to display system notes in a
    couple of modules, e.g. page layout, does now properly
    check the corresponding user permissions (show/edit/delete).
    
    Resolves: #96037
    Releases: master, 11.5
    Change-Id: Ic0e6670f7befa23802382c0818454c4e7c34a253
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/72353
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>

--- typo3/sysext/sys_note/Classes/Controller/NoteController.php
+++ typo3/sysext/sys_note/Classes/Controller/NoteController.php
@@ -17,6 +17,9 @@ declare(strict_types=1);
 
 namespace TYPO3\CMS\SysNote\Controller;
 
+use TYPO3\CMS\Backend\Utility\BackendUtility;
+use TYPO3\CMS\Core\Authentication\BackendUserAuthentication;
+use TYPO3\CMS\Core\Type\Bitmask\Permission;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Fluid\View\StandaloneView;
 use TYPO3\CMS\SysNote\Domain\Repository\SysNoteRepository;
@@ -33,6 +36,8 @@ class NoteController
      */
     protected $notesRepository;
 
+    protected array $pagePermissionCache = [];
+
     public function __construct()
     {
         $this->notesRepository = GeneralUtility::makeInstance(SysNoteRepository::class);
@@ -47,22 +52,59 @@ class NoteController
      */
     public function listAction($pids, int $position = null): string
     {
-        if (empty($pids) || empty($GLOBALS['BE_USER']->user['uid'])) {
+        $backendUser = $this->getBackendUser();
+        if (empty($pids)
+            || empty($backendUser->user[$backendUser->userid_column])
+            || !$backendUser->check('tables_select', 'sys_note')
+        ) {
             return '';
         }
 
-        $notes = $this->notesRepository->findByPidsAndAuthorId($pids, (int)$GLOBALS['BE_USER']->user['uid'], $position);
-        if ($notes) {
-            $view = GeneralUtility::makeInstance(StandaloneView::class);
-            $view->setTemplatePathAndFilename(GeneralUtility::getFileAbsFileName(
-                'EXT:sys_note/Resources/Private/Templates/Note/List.html'
-            ));
-            $view->setLayoutRootPaths(['EXT:sys_note/Resources/Private/Layouts']);
-            $view->getRequest()->setControllerExtensionName('SysNote');
-            $view->assign('notes', $notes);
-            return $view->render();
+        $notes = $this->notesRepository->findByPidsAndAuthorId($pids, (int)$backendUser->user[$backendUser->userid_column], $position);
+        if (!$notes) {
+            return '';
+        }
+        $view = GeneralUtility::makeInstance(StandaloneView::class);
+        $view->setTemplatePathAndFilename(GeneralUtility::getFileAbsFileName(
+            'EXT:sys_note/Resources/Private/Templates/Note/List.html'
+        ));
+        $view->setLayoutRootPaths(['EXT:sys_note/Resources/Private/Layouts']);
+        $view->getRequest()->setControllerExtensionName('SysNote');
+        $view->assign('notes', $this->enrichWithEditPermissions($notes));
+        return $view->render();
+    }
+
+    protected function enrichWithEditPermissions(array $notes): array
+    {
+        $backendUser = $this->getBackendUser();
+        $hasEditAccess = $backendUser->isAdmin() || $backendUser->check('tables_modify', 'sys_note');
+
+        foreach ($notes as &$note) {
+            if (!$hasEditAccess) {
+                // If no edit access, disable edit and delete options for all notes
+                $note['canBeEdited'] = false;
+                $note['canBeDeleted'] = false;
+                continue;
+            }
+            // Check content edit permissions for the note
+            $pid = (int)($note['pid'] ?? 0);
+            if (!isset($this->pagePermissionCache[$pid])) {
+                // Calculate and cache the content edit permissions for this $pid
+                $permissionClause = $backendUser->getPagePermsClause(Permission::PAGE_SHOW);
+                $pageRow = BackendUtility::readPageAccess($pid, $permissionClause) ?: [];
+                $this->pagePermissionCache[$pid] = $backendUser->doesUserHaveAccess($pageRow, Permission::CONTENT_EDIT);
+            }
+            $note['canBeEdited'] = $this->pagePermissionCache[$pid];
+            // For delete, also take user TSconfig into account
+            $note['canBeDeleted'] = $this->pagePermissionCache[$pid]
+                && !(bool)trim($backendUser->getTSConfig()['options.']['disableDelete.']['sys_note'] ?? $backendUser->getTSConfig()['options.']['disableDelete'] ?? '');
         }
 
-        return '';
+        return $notes;
+    }
+
+    protected function getBackendUser(): BackendUserAuthentication
+    {
+        return $GLOBALS['BE_USER'];
     }
 }
