d533bcd51f18dd9dd6b986dbce22163945715ed8
Anja Leichsenring <aleichsenring@ab-softlab.de>
2021-11-18 13:20:02 +0100

    [BUGFIX] Use correct l10n_parent relation in ext:test_datahandler
    There was a copy&paste leftover, leading to translated records
    being related to the wrong table.
    
    By fixing the wrong TCA configuration, sys_refindex entries in
    several test fixtures had to be adjusted.
    
    Resolves: #96025
    Releases: master, 11.5
    Change-Id: I197688e80e98a45a595e5304a49249ecfdcbfe91
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/72230
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Stefan Bürk <stefan@buerk.tech>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Reviewed-by: Stefan Bürk <stefan@buerk.tech>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>

--- typo3/sysext/core/Tests/Functional/Fixtures/Extensions/test_datahandler/Configuration/TCA/tx_testdatahandler_element.php
+++ typo3/sysext/core/Tests/Functional/Fixtures/Extensions/test_datahandler/Configuration/TCA/tx_testdatahandler_element.php
@@ -38,8 +38,8 @@ return [
                 'items' => [
                     ['', 0],
                 ],
-                'foreign_table'       => 'tx_irretutorial_1nff_hotel',
-                'foreign_table_where' => 'AND {#tx_irretutorial_1nff_hotel}.{#pid}=###CURRENT_PID### AND {#tx_irretutorial_1nff_hotel}.{#sys_language_uid} IN (-1,0)',
+                'foreign_table'       => 'tx_testdatahandler_element',
+                'foreign_table_where' => 'AND {#tx_testdatahandler_element}.{#pid}=###CURRENT_PID### AND {#tx_testdatahandler_element}.{#sys_language_uid} IN (-1,0)',
                 'default' => 0,
             ],
         ],
@@ -71,8 +71,8 @@ return [
     'types' => [
         '0' => [
             'showitem' =>
-                '--div--;LLL:EXT:irre_tutorial/Resources/Private/Language/locallang_db.xlf:tabs.general, title,' .
-                '--div--;LLL:EXT:irre_tutorial/Resources/Private/Language/locallang_db.xlf:tabs.visibility, sys_language_uid, l10n_parent, l10n_diffsource, hidden',
+                '--div--;LLL:EXT:test_datahandler/Resources/Private/Language/locallang_db.xlf:tabs.general, title,' .
+                '--div--;LLL:EXT:test_datahandler/Resources/Private/Language/locallang_db.xlf:tabs.visibility, sys_language_uid, l10n_parent, l10n_diffsource, hidden',
         ],
     ],
     'palettes' => [
