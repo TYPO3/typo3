2a67725ef9bece8927cb625e47b88b4a92cbcda4
Christian Kuhn <lolli@schwarzbu.ch>
2021-11-30 19:13:04 +0100

    [TASK] styleguide: Use be_sessions.xml from TF again
    typo3/testing-framework has a proper v12 compatible
    version of be_sessions.xml again. We thus don't need
    the cms-core source anymore.

--- typo3/sysext/styleguide/Tests/Acceptance/Support/Extension/BackendStyleguideEnvironment.php
+++ typo3/sysext/styleguide/Tests/Acceptance/Support/Extension/BackendStyleguideEnvironment.php
@@ -43,7 +43,7 @@ class BackendStyleguideEnvironment extends BackendEnvironment
         ],
         'xmlDatabaseFixtures' => [
             'PACKAGE:typo3/testing-framework/Resources/Core/Acceptance/Fixtures/be_users.xml',
-            'typo3/sysext/core/Tests/Acceptance/Fixtures/be_sessions.xml',
+            'PACKAGE:typo3/testing-framework/Resources/Core/Acceptance/Fixtures/be_sessions.xml',
             'PACKAGE:typo3/testing-framework/Resources/Core/Acceptance/Fixtures/be_groups.xml',
         ],
     ];
