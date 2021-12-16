a8b9eefb2d27e6a20d405a2930c8de4e77458a0b
Christian Kuhn <lolli@schwarzbu.ch>
2021-12-16 17:55:44 +0100

    [TASK] Allow EXT: syntax in f:uri.resource VH
    f:uri.resource is frequently used to load CSS and
    JavaScript resources. It is the only ViewHelper that
    does not understand the general "EXT:" syntax that
    is usually available when extension resource files
    are referenced.
    
    This is now added to improve consistency and
    documented as best practice in VH documentation.
    
    Change-Id: I504c79eef349b1ea6cc70cc0d7753319e1f356d9
    Resolves: #96414
    Releases: main, 11.5
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/72760
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Stefan Bürk <stefan@buerk.tech>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Reviewed-by: Stefan Bürk <stefan@buerk.tech>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>

--- typo3/sysext/fluid/Classes/ViewHelpers/Uri/ResourceViewHelper.php
+++ typo3/sysext/fluid/Classes/ViewHelpers/Uri/ResourceViewHelper.php
@@ -15,8 +15,11 @@
 
 namespace TYPO3\CMS\Fluid\ViewHelpers\Uri;
 
+use TYPO3\CMS\Core\Resource\Exception\InvalidFileException;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Core\Utility\PathUtility;
+use TYPO3\CMS\Extbase\Mvc\RequestInterface;
+use TYPO3\CMS\Fluid\Core\Rendering\RenderingContext;
 use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
 use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
 use TYPO3Fluid\Fluid\Core\ViewHelper\Traits\CompileWithRenderStatic;
@@ -27,37 +30,49 @@ use TYPO3Fluid\Fluid\Core\ViewHelper\Traits\CompileWithRenderStatic;
  * Examples
  * ========
  *
+ * Best practice with EXT: syntax
+ * ------------------------------
+ *
+ * ::
+ *
+ *    <link href="{f:uri.resource(path:'EXT:indexed_search/Resources/Public/Css/Stylesheet.css')}" rel="stylesheet" />
+ *
+ * Output::
+ *
+ *    <link href="typo3/sysext/indexed_search/Resources/Public/Css/Stylesheet.css" rel="stylesheet" />
+ *
+ * Preferred syntax that works in both extbase and non-extbase context.
+ *
  * Defaults
  * --------
  *
  * ::
  *
- *    <link href="{f:uri.resource(path:'css/stylesheet.css')}" rel="stylesheet" />
+ *    <link href="{f:uri.resource(path:'Css/Stylesheet.css')}" rel="stylesheet" />
  *
  * Output::
  *
- *    <link href="typo3conf/ext/example_extension/Resources/Public/css/stylesheet.css" rel="stylesheet" />
+ *    <link href="typo3conf/ext/example_extension/Resources/Public/Css/Stylesheet.css" rel="stylesheet" />
  *
- * Depending on current extension.
+ * Works only in extbase context since it uses the extbase request to find current extension, magically adds 'Resources/Public' to path.
  *
  * With extension name
  * -------------------
  *
  * ::
  *
- *    <link href="{f:uri.resource(path:'css/stylesheet.css', extensionName: 'AnotherExtension')}" rel="stylesheet" />
+ *    <link href="{f:uri.resource(path:'Css/Stylesheet.css', extensionName: 'AnotherExtension')}" rel="stylesheet" />
  *
  * Output::
  *
- *    <link href="typo3conf/ext/another_extension/Resources/Public/css/stylesheet.css" rel="stylesheet" />
+ *    <link href="typo3conf/ext/another_extension/Resources/Public/Css/Stylesheet.css" rel="stylesheet" />
+ *
+ * Magically adds 'Resources/Public' to path.
  */
 class ResourceViewHelper extends AbstractViewHelper
 {
     use CompileWithRenderStatic;
 
-    /**
-     * Initialize arguments
-     */
     public function initializeArguments()
     {
         $this->registerArgument('path', 'string', 'The path and filename of the resource (relative to Public resource directory of the extension).', true);
@@ -68,24 +83,101 @@ class ResourceViewHelper extends AbstractViewHelper
     /**
      * Render the URI to the resource. The filename is used from child content.
      *
-     * @param array $arguments
-     * @param \Closure $renderChildrenClosure
-     * @param RenderingContextInterface $renderingContext
      * @return string The URI to the resource
+     * @throws InvalidFileException
+     * @throws \RuntimeException
      */
     public static function renderStatic(array $arguments, \Closure $renderChildrenClosure, RenderingContextInterface $renderingContext)
     {
-        $path = $arguments['path'];
-        $extensionName = $arguments['extensionName'];
-        $absolute = $arguments['absolute'];
-
-        if ($extensionName === null) {
-            $extensionName = $renderingContext->getRequest()->getControllerExtensionName();
-        }
-        $uri = PathUtility::getPublicResourceWebPath('EXT:' . GeneralUtility::camelCaseToLowerCaseUnderscored($extensionName) . '/Resources/Public/' . $path);
-        if ($absolute === true) {
+        $uri = PathUtility::getPublicResourceWebPath(self::resolveExtensionPath($arguments, $renderingContext));
+        if ($arguments['absolute']) {
             $uri = GeneralUtility::locationHeaderUrl($uri);
         }
+
         return $uri;
+    }
+
+    /**
+     * Resolves the extension path, either directly when possible, or from extension name and request
+     *
+     * @param array $arguments
+     * @param RenderingContextInterface $renderingContext
+     * @return string
+     */
+    private static function resolveExtensionPath(array $arguments, RenderingContextInterface $renderingContext): string
+    {
+        $path = $arguments['path'];
+        if (PathUtility::isExtensionPath($path)) {
+            return $path;
+        }
+
+        return sprintf(
+            'EXT:%s/Resources/Public/%s',
+            self::resolveExtensionKey($arguments, $renderingContext),
+            ltrim($path, '/')
+        );
+    }
+
+    /**
+     * Resolves extension key either from given extension name argument or from request
+     *
+     * @param array $arguments
+     * @param RenderingContextInterface $renderingContext
+     * @return string
+     */
+    private static function resolveExtensionKey(array $arguments, RenderingContextInterface $renderingContext): string
+    {
+        $extensionName = $arguments['extensionName'];
+        if ($extensionName === null) {
+            return self::resolveValidatedRequest($arguments, $renderingContext)->getControllerExtensionKey();
+        }
+
+        return GeneralUtility::camelCaseToLowerCaseUnderscored($extensionName);
+    }
+
+    /**
+     * Resolves and validates the request from rendering context
+     *
+     * @param array $arguments
+     * @param RenderingContextInterface $renderingContext
+     * @return RequestInterface
+     */
+    private static function resolveValidatedRequest(array $arguments, RenderingContextInterface $renderingContext): RequestInterface
+    {
+        if (!$renderingContext instanceof RenderingContext) {
+            throw new \RuntimeException(
+                sprintf(
+                    'RenderingContext must be instance of "%s", but is instance of "%s"',
+                    RenderingContext::class,
+                    get_class($renderingContext)
+                ),
+                1640095993
+            );
+        }
+        $request = $renderingContext->getRequest();
+        if (!$request instanceof RequestInterface) {
+            throw new \RuntimeException(
+                sprintf(
+                    'ViewHelper f:uri.resource needs an Extbase Request object to resolve extension name for given path "%s".'
+                    . ' If not in Extbase context, either set argument "extensionName",'
+                    . ' or (better) use the standard EXT: syntax for path attribute like \'path="EXT:indexed_search/Resources/Public/Icons/Extension.svg"\'.',
+                    $arguments['path']
+                ),
+                1639672666
+            );
+        }
+        if ($request->getControllerExtensionKey() === '') {
+            throw new \RuntimeException(
+                sprintf(
+                    'Can not resolve extension key for given path "%s".'
+                    . ' If not in Extbase context, either set argument "extensionName",'
+                    . ' or (better) use the standard EXT: syntax for path attribute like \'path="EXT:indexed_search/Resources/Public/Icons/Extension.svg"\'.',
+                    $arguments['path']
+                ),
+                1640097205
+            );
+        }
+
+        return $request;
     }
 }
