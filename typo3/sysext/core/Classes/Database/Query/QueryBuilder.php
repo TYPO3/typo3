81f5d39bbece514ab9a29afaae8d02bd9de6b1ab
Ingo Fabbri <inf@mmcagentur.at>
2022-01-04 17:34:45 +0100

    [BUGFIX] Add CompositeExpression argument docblock type-hint
    This patch adds missing 'CompositeExpression' as union
    type for QueryBuilder methods 'where()', 'andWhere()'
    and 'orWhere()' to match the ability of the underlaying
    doctrine/dbal QueryBuilder methods.
    
    Resolves: #96457
    Releases: main, 11.5, 10.4
    Change-Id: Ib9330ffabcdc7680bfd5bb8a991cec60889c9773
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/72858
    Tested-by: Stefan Bürk <stefan@buerk.tech>
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Benni Mack <benni@typo3.org>
    Reviewed-by: Stefan Bürk <stefan@buerk.tech>
    Reviewed-by: Benni Mack <benni@typo3.org>

--- typo3/sysext/core/Classes/Database/Query/QueryBuilder.php
+++ typo3/sysext/core/Classes/Database/Query/QueryBuilder.php
@@ -784,7 +784,7 @@ class QueryBuilder
      * Specifies one or more restrictions to the query result.
      * Replaces any previously specified restrictions, if any.
      *
-     * @param array<int,mixed> $predicates
+     * @param array<int,mixed>|CompositeExpression $predicates
      * @return QueryBuilder This QueryBuilder instance.
      */
     public function where(...$predicates): QueryBuilder
@@ -804,7 +804,7 @@ class QueryBuilder
      * Adds one or more restrictions to the query results, forming a logical
      * conjunction with any previously specified restrictions.
      *
-     * @param array<int,string> $where The query restrictions.
+     * @param array<int,string>|CompositeExpression $where The query restrictions.
      *
      * @return QueryBuilder This QueryBuilder instance.
      *
@@ -827,7 +827,7 @@ class QueryBuilder
      * Adds one or more restrictions to the query results, forming a logical
      * disjunction with any previously specified restrictions.
      *
-     * @param array<int,string> $where The WHERE statement.
+     * @param array<int,string>|CompositeExpression $where The WHERE statement.
      *
      * @return QueryBuilder This QueryBuilder instance.
      *
@@ -920,7 +920,7 @@ class QueryBuilder
      * Specifies a restriction over the groups of the query.
      * Replaces any previous having restrictions, if any.
      *
-     * @param array<int,string> $having The restriction over the groups.
+     * @param array<int,string>|CompositeExpression $having The restriction over the groups.
      *
      * @return QueryBuilder This QueryBuilder instance.
      */
@@ -934,7 +934,7 @@ class QueryBuilder
      * Adds a restriction over the groups of the query, forming a logical
      * conjunction with any existing having restrictions.
      *
-     * @param array<int,string> $having The restriction to append.
+     * @param array<int,string>|CompositeExpression $having The restriction to append.
      *
      * @return QueryBuilder This QueryBuilder instance.
      */
@@ -949,7 +949,7 @@ class QueryBuilder
      * Adds a restriction over the groups of the query, forming a logical
      * disjunction with any existing having restrictions.
      *
-     * @param array<int,string> $having The restriction to add.
+     * @param array<int,string>|CompositeExpression $having The restriction to add.
      *
      * @return QueryBuilder This QueryBuilder instance.
      */
