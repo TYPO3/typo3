253e7dc15d7bad76772c1271d3592b8e1084a651
Elias Häußler <elias@haeussler.dev>
2022-01-11 12:50:44 +0100

    [BUGFIX] Streamline typehints of variadic arguments in QueryBuilder
    Doctrine uses mixed type for most of their method arguments in
    QueryBuilder. Since TYPO3 core uses variadic arguments for some
    methods in the wrapping QueryBuilder, argument types have been
    streamlined to match the ones in the concrete QueryBuilder
    instance from Doctrine.
    
    Resolves: #96507
    Related: #96457
    Releases: main, 11.5, 10.4
    Change-Id: I834240f764feac7af9fba7db0d45aaf4927abb2e
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/73009
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>

--- typo3/sysext/core/Classes/Database/Query/QueryBuilder.php
+++ typo3/sysext/core/Classes/Database/Query/QueryBuilder.php
@@ -791,7 +791,7 @@ class QueryBuilder
      * Specifies one or more restrictions to the query result.
      * Replaces any previously specified restrictions, if any.
      *
-     * @param array<int,mixed>|CompositeExpression $predicates
+     * @param string|CompositeExpression ...$predicates
      * @return QueryBuilder This QueryBuilder instance.
      */
     public function where(...$predicates): QueryBuilder
@@ -811,7 +811,7 @@ class QueryBuilder
      * Adds one or more restrictions to the query results, forming a logical
      * conjunction with any previously specified restrictions.
      *
-     * @param array<int,string>|CompositeExpression $where The query restrictions.
+     * @param string|CompositeExpression ...$where The query restrictions.
      *
      * @return QueryBuilder This QueryBuilder instance.
      *
@@ -834,7 +834,7 @@ class QueryBuilder
      * Adds one or more restrictions to the query results, forming a logical
      * disjunction with any previously specified restrictions.
      *
-     * @param array<int,string>|CompositeExpression $where The WHERE statement.
+     * @param string|CompositeExpression ...$where The WHERE statement.
      *
      * @return QueryBuilder This QueryBuilder instance.
      *
@@ -857,7 +857,7 @@ class QueryBuilder
      * Specifies a grouping over the results of the query.
      * Replaces any previously specified groupings, if any.
      *
-     * @param array<int,string> $groupBy The grouping expression.
+     * @param string ...$groupBy The grouping expression.
      *
      * @return QueryBuilder This QueryBuilder instance.
      */
@@ -871,7 +871,7 @@ class QueryBuilder
     /**
      * Adds a grouping expression to the query.
      *
-     * @param array<int,string> $groupBy The grouping expression.
+     * @param string ...$groupBy The grouping expression.
      *
      * @return QueryBuilder This QueryBuilder instance.
      */
@@ -927,7 +927,7 @@ class QueryBuilder
      * Specifies a restriction over the groups of the query.
      * Replaces any previous having restrictions, if any.
      *
-     * @param array<int,string>|CompositeExpression $having The restriction over the groups.
+     * @param mixed ...$having The restriction over the groups.
      *
      * @return QueryBuilder This QueryBuilder instance.
      */
@@ -941,7 +941,7 @@ class QueryBuilder
      * Adds a restriction over the groups of the query, forming a logical
      * conjunction with any existing having restrictions.
      *
-     * @param array<int,string>|CompositeExpression $having The restriction to append.
+     * @param mixed ...$having The restriction to append.
      *
      * @return QueryBuilder This QueryBuilder instance.
      */
@@ -956,7 +956,7 @@ class QueryBuilder
      * Adds a restriction over the groups of the query, forming a logical
      * disjunction with any existing having restrictions.
      *
-     * @param array<int,string>|CompositeExpression $having The restriction to add.
+     * @param mixed ...$having The restriction to add.
      *
      * @return QueryBuilder This QueryBuilder instance.
      */
