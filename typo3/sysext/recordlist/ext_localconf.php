f57c8cd58f96b8fe5dc39e57bd94b7399fcbdbec
Christian Kuhn <lolli@schwarzbu.ch>
2022-01-25 10:15:09 +0100

    [BUGFIX] Remove element browser registration for 'file_reference'
    The element browser 'file_reference' can't be reached
    anymore since the removal of type="group"
    internal_type="file_reference" in v10.
    
    The registration is dropped.
    
    For the other three types:
    'db' is the type="group" internal_type="db" modal
    'folder' is the type="group" internal_type="folder" modal
    'file' is the FAL "Add image" modal
    
    Resolves: #96640
    Related: #87428
    Releases: main
    Change-Id: Ic649567f7f0fccf4631360a6b4edd00a6e58e022
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/73147
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Benni Mack <benni@typo3.org>
    Tested-by: Stefan Bürk <stefan@buerk.tech>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Reviewed-by: Benni Mack <benni@typo3.org>
    Reviewed-by: Stefan Bürk <stefan@buerk.tech>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>

--- typo3/sysext/recordlist/ext_localconf.php
+++ typo3/sysext/recordlist/ext_localconf.php
@@ -11,5 +11,4 @@ defined('TYPO3') or die();
 // Register element browsers
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ElementBrowsers']['db'] = DatabaseBrowser::class;
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ElementBrowsers']['file'] = FileBrowser::class;
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ElementBrowsers']['file_reference'] = FileBrowser::class;
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ElementBrowsers']['folder'] = FolderBrowser::class;
