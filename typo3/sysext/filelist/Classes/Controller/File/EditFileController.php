a032dc46458500f06781225801e52408c5fb40fa
Christian Kuhn <lolli@schwarzbu.ch>
2022-02-05 22:40:37 +0100

    [TASK] Streamline EditFileController
    General refactoring of this controller with various
    simplifications like less state and transition to
    new backend templating strategy.
    
    Resolves: #96753
    Related: #96730
    Releases: main
    Change-Id: Ic396124349e81de46ace792420426128488bb493
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/73310
    Tested-by: Nikita Hovratov <nikita.h@live.de>
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Stefan Bürk <stefan@buerk.tech>
    Tested-by: Benni Mack <benni@typo3.org>
    Reviewed-by: Nikita Hovratov <nikita.h@live.de>
    Reviewed-by: Stefan Bürk <stefan@buerk.tech>
    Reviewed-by: Benni Mack <benni@typo3.org>

--- typo3/sysext/filelist/Classes/Controller/File/EditFileController.php
+++ typo3/sysext/filelist/Classes/Controller/File/EditFileController.php
@@ -19,6 +19,7 @@ namespace TYPO3\CMS\Filelist\Controller\File;
 
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\StreamFactoryInterface;
 use TYPO3\CMS\Backend\Form\FormResultCompiler;
 use TYPO3\CMS\Backend\Form\NodeFactory;
 use TYPO3\CMS\Backend\Routing\UriBuilder;
@@ -26,259 +27,165 @@ use TYPO3\CMS\Backend\Template\Components\ButtonBar;
 use TYPO3\CMS\Backend\Template\ModuleTemplate;
 use TYPO3\CMS\Backend\Template\ModuleTemplateFactory;
 use TYPO3\CMS\Core\Authentication\BackendUserAuthentication;
-use TYPO3\CMS\Core\Http\HtmlResponse;
-use TYPO3\CMS\Core\Http\RedirectResponse;
+use TYPO3\CMS\Core\Http\ResponseFactory;
 use TYPO3\CMS\Core\Imaging\Icon;
 use TYPO3\CMS\Core\Imaging\IconFactory;
 use TYPO3\CMS\Core\Localization\LanguageService;
-use TYPO3\CMS\Core\Messaging\FlashMessage;
-use TYPO3\CMS\Core\Messaging\FlashMessageService;
+use TYPO3\CMS\Core\Messaging\AbstractMessage;
 use TYPO3\CMS\Core\Resource\Exception\InsufficientFileAccessPermissionsException;
-use TYPO3\CMS\Core\Resource\File;
-use TYPO3\CMS\Core\Resource\Folder;
+use TYPO3\CMS\Core\Resource\Exception\InvalidFileException;
+use TYPO3\CMS\Core\Resource\FileInterface;
 use TYPO3\CMS\Core\Resource\ResourceFactory;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Fluid\View\BackendTemplateView;
 
 /**
- * Script Class for rendering the file editing screen
+ * Edit text files via FormEngine. Reachable via FileList module "Edit content".
+ *
  * @internal This class is a specific Backend controller implementation and is not considered part of the Public TYPO3 API.
  */
 class EditFileController
 {
-    /**
-     * Module content accumulated.
-     *
-     * @var string
-     */
-    protected $content;
+    protected array $dataColumnTca = [
+        'label' => '',
+        'config' => [
+            'type' => 'text',
+            'cols' => 48,
+            'wrap' => 'off',
+            'enableTabulator' => true,
+            'fixedFont' => true,
+        ],
+    ];
 
-    /**
-     * Original input target
-     *
-     * @var string
-     */
-    protected $origTarget;
-
-    /**
-     * The original target, but validated.
-     *
-     * @var string
-     */
-    protected $target;
-
-    /**
-     * Return URL of list module.
-     *
-     * @var string
-     */
-    protected $returnUrl;
-
-    /**
-     * the file that is being edited on
-     *
-     * @var File|Folder|null
-     */
-    protected $fileObject;
-
-    /**
-     * ModuleTemplate object
-     *
-     * @var ModuleTemplate
-     */
-    protected $moduleTemplate;
+    protected array $formEngineData = [
+        'databaseRow' => [
+            'uid' => 0,
+            'data' => '',
+            'target' => 0,
+            'redirect' => '',
+        ],
+        'tableName' => 'editfile',
+        'processedTca' => [
+            'columns' => [
+                'data' => [],
+                'target' => [
+                    'config' => [
+                        'type' => 'input',
+                        'renderType' => 'hidden',
+                    ],
+                ],
+                'redirect' => [
+                    'config' => [
+                        'type' => 'input',
+                        'renderType' => 'hidden',
+                    ],
+                ],
+            ],
+            'types' => [
+                1 => [
+                    'showitem' => 'data,target,redirect',
+                ],
+            ],
+        ],
+        'recordTypeValue' => 1,
+        'inlineStructure' => [],
+        'renderType' => 'fullRecordContainer',
+    ];
 
     public function __construct(
-        protected IconFactory $iconFactory,
-        protected UriBuilder $uriBuilder,
-        protected ResourceFactory $resourceFactory,
-        protected ModuleTemplateFactory $moduleTemplateFactory,
+        protected readonly IconFactory $iconFactory,
+        protected readonly UriBuilder $uriBuilder,
+        protected readonly ResourceFactory $resourceFactory,
+        protected readonly ModuleTemplateFactory $moduleTemplateFactory,
+        protected readonly ResponseFactory $responseFactory,
+        protected readonly StreamFactoryInterface $streamFactory,
     ) {
     }
 
     /**
-     * Processes the request, currently everything is handled and put together via "process()"
-     *
-     * @param ServerRequestInterface $request the current request
-     * @return ResponseInterface the response with the content
+     * Render the edit file content form using FormEngine.
      */
     public function mainAction(ServerRequestInterface $request): ResponseInterface
     {
-        $this->init($request);
-        if ($response = $this->process()) {
-            return $response;
-        }
-
-        return new HtmlResponse($this->moduleTemplate->renderContent());
-    }
-
-    /**
-     * Initialize script class
-     *
-     * @param ServerRequestInterface $request
-     *
-     * @throws InsufficientFileAccessPermissionsException
-     */
-    protected function init(ServerRequestInterface $request): void
-    {
-        $this->moduleTemplate = $this->moduleTemplateFactory->create($request);
+        $languageService = $this->getLanguageService();
+        $view = $this->moduleTemplateFactory->create($request, 'typo3/cms-filelist');
         $parsedBody = $request->getParsedBody();
         $queryParams = $request->getQueryParams();
 
-        // Setting target, which must be a file reference to a file within the mounts.
-        $this->target = $this->origTarget = $parsedBody['target'] ?? $queryParams['target'] ?? '';
-        // create the file object
-        if ($this->target) {
-            $this->fileObject = $this->resourceFactory->retrieveFileOrFolderObject($this->target);
+        $combinedIdentifier = $parsedBody['target'] ?? $queryParams['target'] ?? '';
+        $file = $this->resourceFactory->retrieveFileOrFolderObject($combinedIdentifier);
+
+        if (!$file instanceof FileInterface) {
+            throw new InvalidFileException('Referenced target "' . $combinedIdentifier . '" could not be resolved to a valid file', 1294586841);
         }
-        // Cleaning and checking target directory
-        if (!$this->fileObject) {
-            $title = $this->getLanguageService()->sL('LLL:EXT:filelist/Resources/Private/Language/locallang_mod_file_list.xlf:paramError');
-            $message = $this->getLanguageService()->sL('LLL:EXT:filelist/Resources/Private/Language/locallang_mod_file_list.xlf:targetNoDir');
-            throw new \RuntimeException($title . ': ' . $message, 1294586841);
+        if ($file->getStorage()->getUid() === 0) {
+            throw new InsufficientFileAccessPermissionsException('You are not allowed to access files outside your storages', 1375889832);
         }
-        if ($this->fileObject->getStorage()->getUid() === 0) {
-            throw new InsufficientFileAccessPermissionsException(
-                'You are not allowed to access files outside your storages',
-                1375889832
-            );
-        }
-        $this->returnUrl = GeneralUtility::sanitizeLocalUrl(
+
+        $returnUrl = GeneralUtility::sanitizeLocalUrl(
             $parsedBody['returnUrl']
-                ?? $queryParams['returnUrl']
-                ?? (string)$this->uriBuilder->buildUriFromRoute('file_FilelistList', [
-                    'id' => $this->fileObject->getParentFolder()->getCombinedIdentifier(),
-                ])
+            ?? $queryParams['returnUrl']
+            ?? (string)$this->uriBuilder->buildUriFromRoute('file_FilelistList', [
+                'id' => $file->getParentFolder()->getCombinedIdentifier(),
+            ])
         );
-    }
 
-    /**
-     * Main function, rendering the actual content of the editing page
-     *
-     * @return ResponseInterface|null Possible redirect response
-     */
-    protected function process(): ?ResponseInterface
-    {
-        $dataColumnDefinition = [
-            'label' => htmlspecialchars($this->getLanguageService()->sL('LLL:EXT:core/Resources/Private/Language/locallang_common.xlf:file'))
-                . ' ' . htmlspecialchars($this->target),
-            'config' => [
-                'type' => 'text',
-                'cols' => 48,
-                'wrap' => 'OFF',
-            ],
-            'defaultExtras' => 'fixed-font: enable-tab',
-        ];
+        if (!$file->isTextFile()) {
+            $extList = $GLOBALS['TYPO3_CONF_VARS']['SYS']['textfile_ext'];
+            $view->addFlashMessage('Files with that extension are not editable. Allowed extensions are: ' . $extList, '', AbstractMessage::ERROR, true);
+            return $this->responseFactory->createResponse(400)->withHeader('location', $returnUrl);
+        }
 
-        $this->getButtonsInternal();
-        // Hook: before compiling the output
+        $this->addDocHeaderButtons($view, $returnUrl);
+
+        $dataColumnDefinition = $this->dataColumnTca;
+        $dataColumnDefinition['label'] = htmlspecialchars($languageService->sL('LLL:EXT:core/Resources/Private/Language/locallang_common.xlf:file')) . ' ' . htmlspecialchars($combinedIdentifier);
+
+        // @todo: Consider dropping this hook. Working on $content is useless here and
+        //        overriding $dataColumnDefinition could be done via FormEngine.
+        $hookContent = '';
         foreach ($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['typo3/file_edit.php']['preOutputProcessingHook'] ?? [] as $hookFunction) {
-            $hookParameters = [
-                'content' => &$this->content,
-                'target' => &$this->target,
-                'dataColumnDefinition' => &$dataColumnDefinition,
-            ];
+            $hookParameters = ['content' => &$hookContent, 'target' => &$combinedIdentifier, 'dataColumnDefinition' => &$dataColumnDefinition];
             GeneralUtility::callUserFunction($hookFunction, $hookParameters, $this);
         }
 
-        $assigns = [];
-        $assigns['moduleUrlTceFile'] = (string)$this->uriBuilder->buildUriFromRoute('tce_file');
-        $assigns['fileName'] = $this->fileObject->getName();
+        $formData = $this->formEngineData;
+        $formData['databaseRow']['data'] = $file->getContents();
+        $formData['databaseRow']['target'] = $file->getUid();
+        $formData['databaseRow']['redirect'] = (string)$this->uriBuilder->buildUriFromRoute('file_edit', ['target' => $combinedIdentifier]);
+        $formData['processedTca']['columns']['data'] = $dataColumnDefinition;
 
-        try {
-            $extList = $GLOBALS['TYPO3_CONF_VARS']['SYS']['textfile_ext'];
-            if (!$this->fileObject->isTextFile()) {
-                // @todo throw a minor exception here, not the global one
-                throw new \Exception('Files with that extension are not editable. Allowed extensions are: ' . $extList, 1476050135);
-            }
-
-            $fullIdentifier= $this->fileObject->getCombinedIdentifier();
-            $returnUrl = (string)$this->uriBuilder->buildUriFromRoute('file_edit', ['target' => $fullIdentifier]);
-
-            // Making the formfields
-            $formData = [
-                'databaseRow' => [
-                    'uid' => 0,
-                    'data' => $this->fileObject->getContents(),
-                    'target' => $this->fileObject->getUid(),
-                    'redirect' => $returnUrl,
-                ],
-                'tableName' => 'editfile',
-                'processedTca' => [
-                    'columns' => [
-                        'data' => $dataColumnDefinition,
-                        'target' => [
-                            'config' => [
-                                'type' => 'input',
-                                'renderType' => 'hidden',
-                            ],
-                        ],
-                        'redirect' => [
-                            'config' => [
-                                'type' => 'input',
-                                'renderType' => 'hidden',
-                            ],
-                        ],
-                    ],
-                    'types' => [
-                        1 => [
-                            'showitem' => 'data,target,redirect',
-                        ],
-                    ],
-                ],
-                'recordTypeValue' => 1,
-                'inlineStructure' => [],
-                'renderType' => 'fullRecordContainer',
-            ];
-
-            $resultArray = GeneralUtility::makeInstance(NodeFactory::class)->create($formData)->render();
-            $formResultCompiler = GeneralUtility::makeInstance(FormResultCompiler::class);
-            $formResultCompiler->mergeResult($resultArray);
-
-            $form = $formResultCompiler->addCssFiles()
-                . $resultArray['html']
-                . $formResultCompiler->printNeededJSFunctions();
-
-            $assigns['form'] = $form;
-        } catch (\Exception $e) {
-            // @todo catch dedicated exceptions, not the global one, if possible
-            $flashMessage = GeneralUtility::makeInstance(FlashMessage::class, $e->getMessage(), '', FlashMessage::ERROR, true);
-
-            $flashMessageService = GeneralUtility::makeInstance(FlashMessageService::class);
-            $defaultFlashMessageQueue = $flashMessageService->getMessageQueueByIdentifier();
-            $defaultFlashMessageQueue->enqueue($flashMessage);
-
-            return new RedirectResponse($this->returnUrl, 500);
-        }
+        $resultArray = GeneralUtility::makeInstance(NodeFactory::class)->create($formData)->render();
+        $formResultCompiler = GeneralUtility::makeInstance(FormResultCompiler::class);
+        $formResultCompiler->mergeResult($resultArray);
 
         // Rendering of the output via fluid
-        $view = GeneralUtility::makeInstance(BackendTemplateView::class);
-        $view->setTemplateRootPaths(['EXT:filelist/Resources/Private/Templates']);
-        $view->assignMultiple($assigns);
-        $pageContent = $view->render('File/EditFile');
+        $view->assignMultiple([
+            'moduleUrlTceFile' => (string)$this->uriBuilder->buildUriFromRoute('tce_file'),
+            'fileName' => $file->getName(),
+            'hookContent' => $hookContent,
+            'form' => $formResultCompiler->addCssFiles() . $resultArray['html'] . $formResultCompiler->printNeededJSFunctions(),
+        ]);
+        $content = $view->render('File/EditFile');
 
-        // Hook: after compiling the output
+        // @todo: Consider dropping this hook: Working late on content could be done via template override or FormEngine
+        //        and overriding $combinedIdentifier is useless here.
         foreach ($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['typo3/file_edit.php']['postOutputProcessingHook'] ?? [] as $hookFunction) {
             $hookParameters = [
-                'pageContent' => &$pageContent,
-                'target' => &$this->target,
+                'pageContent' => &$content,
+                'target' => &$combinedIdentifier,
             ];
             GeneralUtility::callUserFunction($hookFunction, $hookParameters, $this);
         }
 
-        $this->content .= $pageContent;
-        $this->moduleTemplate->setContent($this->content);
-        return null;
+        return $this->responseFactory->createResponse()
+            ->withHeader('Content-Type', 'text/html; charset=utf-8')
+            ->withBody($this->streamFactory->createStream($content));
     }
 
-    /**
-     * Builds the buttons for the docheader
-     */
-    protected function getButtonsInternal(): void
+    protected function addDocHeaderButtons(ModuleTemplate $view, string $returnUrl): void
     {
-        $buttonBar = $this->moduleTemplate->getDocHeaderComponent()->getButtonBar();
-
-        $lang = $this->getLanguageService();
+        $languageService = $this->getLanguageService();
+        $buttonBar = $view->getDocHeaderComponent()->getButtonBar();
         // CSH button
         $helpButton = $buttonBar->makeHelpButton()
             ->setFieldName('file_edit')
@@ -291,15 +198,15 @@ class EditFileController
             ->setValue('1')
             ->setForm('EditFileController')
             ->setShowLabelText(true)
-            ->setTitle($lang->sL('LLL:EXT:filelist/Resources/Private/Language/locallang.xlf:file_edit.php.submit'))
+            ->setTitle($languageService->sL('LLL:EXT:filelist/Resources/Private/Language/locallang.xlf:file_edit.php.submit'))
             ->setIcon($this->iconFactory->getIcon('actions-document-save', Icon::SIZE_SMALL));
         $buttonBar->addButton($saveButton, ButtonBar::BUTTON_POSITION_LEFT, 20);
 
         // Cancel button
         $closeButton = $buttonBar->makeLinkButton()
             ->setShowLabelText(true)
-            ->setHref($this->returnUrl)
-            ->setTitle($lang->sL('LLL:EXT:core/Resources/Private/Language/locallang_core.xlf:labels.cancel'))
+            ->setHref($returnUrl)
+            ->setTitle($languageService->sL('LLL:EXT:core/Resources/Private/Language/locallang_core.xlf:labels.cancel'))
             ->setIcon($this->iconFactory->getIcon('actions-close', Icon::SIZE_SMALL));
         $buttonBar->addButton($closeButton, ButtonBar::BUTTON_POSITION_LEFT, 10);
     }
