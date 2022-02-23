1ca23edf637be5d988adf91e78dbcfdaed803903
Stefan Bürk <stefan@buerk.tech>
2022-02-23 14:43:50 +0100

    [BUGFIX] Use additional packageName for LinkBrowser view
    Since the LinkBrowser uses custom templates, while usually
    initialized outside of ext:recordlist, the packageName has
    to be passed to the factory to announce the template paths.
    
    Resolves: #97007
    Related: #96962
    Releases: main
    Change-Id: I9c24c85dbfcc0dd991259ed1b4c2d0c5e0954b83
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/73676
    Tested-by: Oliver Bartsch <bo@cedev.de>
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Benjamin Franzke <bfr@qbus.de>
    Reviewed-by: Oliver Bartsch <bo@cedev.de>
    Reviewed-by: Benjamin Franzke <bfr@qbus.de>

--- typo3/sysext/recordlist/Classes/Controller/AbstractLinkBrowserController.php
+++ typo3/sysext/recordlist/Classes/Controller/AbstractLinkBrowserController.php
@@ -158,7 +158,7 @@ abstract class AbstractLinkBrowserController
         $this->loadLinkHandlers();
         $this->initCurrentUrl();
 
-        $view = $this->backendViewFactory->create($request);
+        $view = $this->backendViewFactory->create($request, ['typo3/cms-recordlist']);
         $menuData = $this->buildMenuArray();
         $renderLinkAttributeFields = $this->renderLinkAttributeFields($view);
         if (method_exists($this->displayedLinkHandler, 'setView')) {
