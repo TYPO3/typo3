a405766b9afde25080aa230310875165201402d6
Helmut Hummel <typo3@helhum.io>
2022-03-02 10:10:53 +0100

    [TASK] Move TS condition check to TS condition matcher
    The ExpressionLanguageResolver should be generic and not
    implement some TypoScript related logic.
    
    Therefore the check for "ELSE" expression is moved to
    AbstractConditionMatcher
    
    Dead code is removed on the go.
    
    Resolves: #97077
    Releases: 10.4, 11.5, main
    Change-Id: Ic5037b46c65ffb80f2f5b242e671e2454922171c
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/73762
    Reviewed-by: Torben Hansen <derhansen@gmail.com>
    Reviewed-by: Benni Mack <benni@typo3.org>
    Reviewed-by: Helmut Hummel <typo3@helhum.io>
    Tested-by: Benni Mack <benni@typo3.org>
    Tested-by: Torben Hansen <derhansen@gmail.com>
    Tested-by: Helmut Hummel <typo3@helhum.io>

--- typo3/sysext/core/Tests/Unit/Configuration/TypoScript/ConditionMatching/AbstractConditionMatcherTest.php
+++ typo3/sysext/core/Tests/Unit/Configuration/TypoScript/ConditionMatching/AbstractConditionMatcherTest.php
@@ -31,6 +31,7 @@ use TYPO3\CMS\Core\Core\ApplicationContext;
 use TYPO3\CMS\Core\Core\Environment;
 use TYPO3\CMS\Core\Core\SystemEnvironmentBuilder;
 use TYPO3\CMS\Core\ExpressionLanguage\ProviderConfigurationLoader;
+use TYPO3\CMS\Core\ExpressionLanguage\Resolver;
 use TYPO3\CMS\Core\Http\ServerRequest;
 use TYPO3\CMS\Core\Package\PackageInterface;
 use TYPO3\CMS\Core\Package\PackageManager;
@@ -380,5 +381,18 @@ class AbstractConditionMatcherTest extends UnitTestCase
         $this->initConditionMatcher();
         $result = $this->evaluateExpressionMethod->invokeArgs($this->conditionMatcher, ['ip("devIP")']);
         self::assertSame($expectedResult, $result);
+    }
+
+    /**
+     * @test
+     */
+    public function typoScriptElseConditionIsNotEvaluatedAndAlwaysReturnsFalse(): void
+    {
+        $this->initConditionMatcher();
+        $expressionProperty = new \ReflectionProperty(AbstractConditionMatcher::class, 'expressionLanguageResolver');
+        $resolverProphecy = $this->prophesize(Resolver::class);
+        $resolverProphecy->evaluate(Argument::cetera())->shouldNotBeCalled();
+        $expressionProperty->setValue($this->conditionMatcher, $resolverProphecy->reveal());
+        self::assertFalse($this->evaluateExpressionMethod->invokeArgs($this->conditionMatcher, ['ELSE']));
     }
 }
