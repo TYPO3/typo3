e983a7b57556297f87ad9026b57598dff5e5e163
Georg Ringer <georg.ringer@gmail.com>
2022-03-04 09:31:16 +0100

    [BUGFIX] Fix type error in TcaItemsProcessorFunctions
    Cast the variable to string before handing it to preg_replace to avoid
    a type error.
    
    Resolves: #97088
    Releases: master, 11.5
    Change-Id: I60c3eb850049aec3cba7f3bade3a99849683152e
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/73806
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Benni Mack <benni@typo3.org>
    Tested-by: Oliver Bartsch <bo@cedev.de>
    Reviewed-by: André Buchmann <andy.schliesser@gmail.com>
    Reviewed-by: Benni Mack <benni@typo3.org>
    Reviewed-by: Oliver Klee <typo3-coding@oliverklee.de>
    Reviewed-by: Oliver Bartsch <bo@cedev.de>

--- typo3/sysext/core/Classes/Hooks/TcaItemsProcessorFunctions.php
+++ typo3/sysext/core/Classes/Hooks/TcaItemsProcessorFunctions.php
@@ -157,7 +157,7 @@ class TcaItemsProcessorFunctions
                 // Add item to be selected:
                 $fieldDefinition['items'][] = [
                     '[' . $allowDenyModeLabel . '] ' . $itemLabel,
-                    $groupKey . ':' . preg_replace('/[:|,]/', '', $itemValue) . ':' . $allowDenyMode,
+                    $groupKey . ':' . preg_replace('/[:|,]/', '', (string)$itemValue) . ':' . $allowDenyMode,
                     $icons[$allowDenyMode],
                 ];
             }
