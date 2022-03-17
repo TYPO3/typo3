565a7208fe5ff034f79bff42356eef6ad65aae87
Lars Michalowski <l.michalowski@fishfarm.de>
2022-03-17 09:00:12 +0100

    [BUGFIX] Ensure imageLinkWrap works with UTF-8 filenames
    The unset($info['originalFile']) introduced in #90565
    broke an access to that array key below in the method.
    This leads to the situation that files with UTF-8
    characters are not found anymore since the imageLinkWrap()
    method is fed with an encoded version of the filename
    and not the original file object anymore.
    The patch parks $info['originalFile'] in a variable
    to access it for imageLinkWrap() again.
    
    Releases: main, 11.5
    Resolves: #93716
    Resolves: #90757
    Related: #90565
    Change-Id: I2471283bd6825c093745b2331a12737de00c105f
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/73983
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Stefan Bürk <stefan@buerk.tech>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Reviewed-by: Stefan Bürk <stefan@buerk.tech>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>

--- typo3/sysext/frontend/Classes/ContentObject/ImageContentObject.php
+++ typo3/sysext/frontend/Classes/ContentObject/ImageContentObject.php
@@ -74,6 +74,7 @@ class ImageContentObject extends AbstractContentObject
             $source = $info[3];
         }
         // Remove file objects for AssetCollector, as it only allows to store scalar values
+        $infoOriginalFile = $info['originalFile'];
         unset($info['originalFile'], $info['processedFile']);
         GeneralUtility::makeInstance(AssetCollector::class)->addMedia(
             $source,
@@ -107,7 +108,7 @@ class ImageContentObject extends AbstractContentObject
         if ($linkWrap !== '') {
             $theValue = $this->linkWrap((string)$theValue, $linkWrap);
         } elseif ($conf['imageLinkWrap'] ?? false) {
-            $originalFile = !empty($info['originalFile']) ? $info['originalFile'] : $info['origFile'];
+            $originalFile = !empty($infoOriginalFile) ? $infoOriginalFile : urldecode($info['origFile']);
             $theValue = $this->cObj->imageLinkWrap($theValue, $originalFile, $conf['imageLinkWrap.']);
         }
         $wrap = $this->cObj->stdWrapValue('wrap', $conf ?? []);
