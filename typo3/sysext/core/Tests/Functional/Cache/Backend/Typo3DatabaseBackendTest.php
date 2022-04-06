d95eca33d2ea811bd568dfda4614aef844ee27a8
Linus Stubert <stubert@rz.uni-mannheim.de>
2022-04-06 09:57:30 +0200

    [BUGFIX] Fix caching performance issues for MySQL on large sites
    Removed special treatment of MySQL connections which caused extremely
    inefficient join deletes and Backend crashes on large sites with more
    than about 10.000 pages.
    
    Additionally, some mockings and dbms exclusions are removed in the
    functional test. Because we do not have a dbms agnostic code switch
    anymore, `*Mysql()` and `*nonMysql()` test variants are combined to
    avoid superflous running tests twice.
    
    Resolves: #90518
    Releases: main, 12.4
    Change-Id: Icad6f7d766d0a13f93a1c76a15bc12ab5ab27f79
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/78916
    Reviewed-by: Stefan Bürk <stefan@buerk.tech>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Stefan Bürk <stefan@buerk.tech>
    Tested-by: core-ci <typo3@b13.com>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>

--- typo3/sysext/core/Tests/Functional/Cache/Backend/Typo3DatabaseBackendTest.php
+++ typo3/sysext/core/Tests/Functional/Cache/Backend/Typo3DatabaseBackendTest.php
@@ -413,94 +413,28 @@ final class Typo3DatabaseBackendTest extends FunctionalTestCase
 
     /**
      * @test
-     *
-     * @group not-postgres
-     * @group not-sqlite
      */
-    public function flushByTagWorksWithEmptyCacheTablesWithMysql(): void
+    public function flushByTagWorksWithEmptyCacheTables(): void
     {
-        $subject = $this->getSubjectObject(true);
+        $subject = $this->getSubjectObject();
         $subject->flushByTag('tagB');
     }
 
     /**
      * @test
-     *
-     * @group not-postgres
-     * @group not-sqlite
      */
-    public function flushByTagsWorksWithEmptyCacheTablesWithMysql(): void
+    public function flushByTagsWorksWithEmptyCacheTables(): void
     {
-        $subject = $this->getSubjectObject(true);
+        $subject = $this->getSubjectObject();
         $subject->flushByTags(['tagB']);
     }
 
     /**
      * @test
-     *
-     * @group not-postgres
-     * @group not-sqlite
      */
-    public function flushByTagRemovesCorrectRowsFromDatabaseWithMysql(): void
+    public function flushByTagRemovesCorrectRowsFromDatabase(): void
     {
-        $subject = $this->getSubjectObject(true);
-        $subject->flushByTag('tagB');
-
-        $cacheTableConnection = (new ConnectionPool())->getConnectionForTable('cache_pages');
-        self::assertSame(0, $cacheTableConnection->count('*', 'cache_pages', ['identifier' => 'idA']));
-        self::assertSame(0, $cacheTableConnection->count('*', 'cache_pages', ['identifier' => 'idB']));
-        self::assertSame(1, $cacheTableConnection->count('*', 'cache_pages', ['identifier' => 'idC']));
-        $tagsTableConnection = (new ConnectionPool())->getConnectionForTable('cache_pages_tags');
-        self::assertSame(0, $tagsTableConnection->count('*', 'cache_pages_tags', ['identifier' => 'idA']));
-        self::assertSame(0, $tagsTableConnection->count('*', 'cache_pages_tags', ['identifier' => 'idB']));
-        self::assertSame(2, $tagsTableConnection->count('*', 'cache_pages_tags', ['identifier' => 'idC']));
-    }
-
-    /**
-     * @test
-     *
-     * @group not-postgres
-     * @group not-sqlite
-     */
-    public function flushByTagsRemovesCorrectRowsFromDatabaseWithMysql(): void
-    {
-        $subject = $this->getSubjectObject(true);
-        $subject->flushByTags(['tagC', 'tagD']);
-
-        $cacheTableConnection = (new ConnectionPool())->getConnectionForTable('cache_pages');
-        self::assertSame(1, $cacheTableConnection->count('*', 'cache_pages', ['identifier' => 'idA']));
-        self::assertSame(0, $cacheTableConnection->count('*', 'cache_pages', ['identifier' => 'idB']));
-        self::assertSame(0, $cacheTableConnection->count('*', 'cache_pages', ['identifier' => 'idC']));
-        $tagsTableConnection = (new ConnectionPool())->getConnectionForTable('cache_pages_tags');
-        self::assertSame(2, $tagsTableConnection->count('*', 'cache_pages_tags', ['identifier' => 'idA']));
-        self::assertSame(0, $tagsTableConnection->count('*', 'cache_pages_tags', ['identifier' => 'idB']));
-        self::assertSame(0, $tagsTableConnection->count('*', 'cache_pages_tags', ['identifier' => 'idC']));
-    }
-
-    /**
-     * @test
-     */
-    public function flushByTagWorksWithEmptyCacheTablesWithNonMysql(): void
-    {
-        $subject = $this->getSubjectObject(true, false);
-        $subject->flushByTag('tagB');
-    }
-
-    /**
-     * @test
-     */
-    public function flushByTagsWorksWithEmptyCacheTablesWithNonMysql(): void
-    {
-        $subject = $this->getSubjectObject(true, false);
-        $subject->flushByTags(['tagB', 'tagC']);
-    }
-
-    /**
-     * @test
-     */
-    public function flushByTagRemovesCorrectRowsFromDatabaseWithNonMysql(): void
-    {
-        $subject = $this->getSubjectObject(true, false);
+        $subject = $this->getSubjectObject();
         $subject->flushByTag('tagB');
 
         $cacheTableConnection = (new ConnectionPool())->getConnectionForTable('cache_pages');
@@ -516,9 +450,9 @@ final class Typo3DatabaseBackendTest extends FunctionalTestCase
     /**
      * @test
      */
-    public function flushByTagsRemovesCorrectRowsFromDatabaseWithNonMysql(): void
+    public function flushByTagsRemovesCorrectRowsFromDatabase(): void
     {
-        $subject = $this->getSubjectObject(true, false);
+        $subject = $this->getSubjectObject();
         $subject->flushByTags(['tagC', 'tagD']);
 
         $cacheTableConnection = (new ConnectionPool())->getConnectionForTable('cache_pages');
@@ -533,163 +467,22 @@ final class Typo3DatabaseBackendTest extends FunctionalTestCase
 
     /**
      * @test
-     *
-     * @group not-postgres
-     * @group not-sqlite
      */
-    public function collectGarbageWorksWithEmptyTableWithMysql(): void
+    public function collectGarbageWorksWithEmptyTable(): void
     {
-        $subject = $this->getSubjectObject(true);
-        $subject->collectGarbage();
-    }
-
-    /**
-     * @test
-     *
-     * @group not-postgres
-     * @group not-sqlite
-     */
-    public function collectGarbageRemovesCacheEntryWithExpiredLifetimeWithMysql(): void
-    {
-        $frontendMock = $this->createMock(FrontendInterface::class);
-        $frontendMock->method('getIdentifier')->willReturn('pages');
-
-        // Must be mocked here to test for "mysql" version implementation
-        $subject = $this->getMockBuilder(Typo3DatabaseBackend::class)
-            ->onlyMethods(['isConnectionMysql'])
-            ->setConstructorArgs(['Testing'])
-            ->getMock();
-        $subject->expects(self::once())->method('isConnectionMysql')->willReturn(true);
-        $subject->setCache($frontendMock);
-
-        // idA should be expired after EXEC_TIME manipulation, idB should stay
-        $subject->set('idA', 'dataA', [], 60);
-        $subject->set('idB', 'dataB', [], 240);
-
-        $GLOBALS['EXEC_TIME'] = $GLOBALS['EXEC_TIME'] + 120;
-
-        $subject->collectGarbage();
-
-        $cacheTableConnection = (new ConnectionPool())->getConnectionForTable('cache_pages');
-        self::assertSame(0, $cacheTableConnection->count('*', 'cache_pages', ['identifier' => 'idA']));
-        self::assertSame(1, $cacheTableConnection->count('*', 'cache_pages', ['identifier' => 'idB']));
-    }
-
-    /**
-     * @test
-     *
-     * @group not-postgres
-     * @group not-sqlite
-     */
-    public function collectGarbageRemovesTagEntriesForCacheEntriesWithExpiredLifetimeWithMysql(): void
-    {
-        $frontendMock = $this->createMock(FrontendInterface::class);
-        $frontendMock->method('getIdentifier')->willReturn('pages');
-
-        // Must be mocked here to test for "mysql" version implementation
-        $subject = $this->getMockBuilder(Typo3DatabaseBackend::class)
-            ->onlyMethods(['isConnectionMysql'])
-            ->setConstructorArgs(['Testing'])
-            ->getMock();
-        $subject->expects(self::once())->method('isConnectionMysql')->willReturn(true);
-        $subject->setCache($frontendMock);
-
-        // tag rows tagA and tagB should be removed by garbage collector after EXEC_TIME manipulation
-        $subject->set('idA', 'dataA', ['tagA', 'tagB'], 60);
-        $subject->set('idB', 'dataB', ['tagB', 'tagC'], 240);
-
-        $GLOBALS['EXEC_TIME'] = $GLOBALS['EXEC_TIME'] + 120;
-
-        $subject->collectGarbage();
-
-        $tagsTableConnection = (new ConnectionPool())->getConnectionForTable('cache_pages_tags');
-        self::assertSame(0, $tagsTableConnection->count('*', 'cache_pages_tags', ['identifier' => 'idA']));
-        self::assertSame(2, $tagsTableConnection->count('*', 'cache_pages_tags', ['identifier' => 'idB']));
-    }
-
-    /**
-     * @test
-     *
-     * @group not-postgres
-     * @group not-sqlite
-     */
-    public function collectGarbageRemovesOrphanedTagEntriesFromTagsTableWithMysql(): void
-    {
-        $frontendMock = $this->createMock(FrontendInterface::class);
-        $frontendMock->method('getIdentifier')->willReturn('pages');
-
-        // Must be mocked here to test for "mysql" version implementation
-        $subject = $this->getMockBuilder(Typo3DatabaseBackend::class)
-            ->onlyMethods(['isConnectionMysql'])
-            ->setConstructorArgs(['Testing'])
-            ->getMock();
-        $subject->expects(self::once())->method('isConnectionMysql')->willReturn(true);
-        $subject->setCache($frontendMock);
-
-        // tag rows tagA and tagB should be removed by garbage collector after EXEC_TIME manipulation
-        $subject->set('idA', 'dataA', ['tagA', 'tagB'], 60);
-        $subject->set('idB', 'dataB', ['tagB', 'tagC'], 240);
-
-        $tagsTableConnection = (new ConnectionPool())->getConnectionForTable('cache_pages_tags');
-
-        // Push two orphaned tag row into db - tags that have no related cache record anymore for whatever reason
-        $tagsTableConnection->insert(
-            'cache_pages_tags',
-            [
-                'identifier' => 'idC',
-                'tag' => 'tagC',
-            ]
-        );
-        $tagsTableConnection->insert(
-            'cache_pages_tags',
-            [
-                'identifier' => 'idC',
-                'tag' => 'tagD',
-            ]
-        );
-
-        $GLOBALS['EXEC_TIME'] = $GLOBALS['EXEC_TIME'] + 120;
-
-        $subject->collectGarbage();
-
-        self::assertSame(0, $tagsTableConnection->count('*', 'cache_pages_tags', ['identifier' => 'idA']));
-        self::assertSame(2, $tagsTableConnection->count('*', 'cache_pages_tags', ['identifier' => 'idB']));
-        self::assertSame(0, $tagsTableConnection->count('*', 'cache_pages_tags', ['identifier' => 'idC']));
-    }
-
-    /**
-     * @test
-     */
-    public function collectGarbageWorksWithEmptyTableWithNonMysql(): void
-    {
-        $frontendMock = $this->createMock(FrontendInterface::class);
-        $frontendMock->method('getIdentifier')->willReturn('pages');
-
-        // Must be mocked here to test for "mysql" version implementation
-        $subject = $this->getMockBuilder(Typo3DatabaseBackend::class)
-            ->onlyMethods(['isConnectionMysql'])
-            ->setConstructorArgs(['Testing'])
-            ->getMock();
-        $subject->expects(self::once())->method('isConnectionMysql')->willReturn(false);
-        $subject->setCache($frontendMock);
-
+        $subject = $this->getSubjectObject();
         $subject->collectGarbage();
     }
 
     /**
      * @test
      */
-    public function collectGarbageRemovesCacheEntryWithExpiredLifetimeWithNonMysql(): void
+    public function collectGarbageRemovesCacheEntryWithExpiredLifetime(): void
     {
         $frontendMock = $this->createMock(FrontendInterface::class);
         $frontendMock->method('getIdentifier')->willReturn('pages');
 
-        // Must be mocked here to test for "mysql" version implementation
-        $subject = $this->getMockBuilder(Typo3DatabaseBackend::class)
-            ->onlyMethods(['isConnectionMysql'])
-            ->setConstructorArgs(['Testing'])
-            ->getMock();
-        $subject->expects(self::once())->method('isConnectionMysql')->willReturn(false);
+        $subject = new Typo3DatabaseBackend('Testing');
         $subject->setCache($frontendMock);
 
         // idA should be expired after EXEC_TIME manipulation, idB should stay
@@ -708,17 +501,12 @@ final class Typo3DatabaseBackendTest extends FunctionalTestCase
     /**
      * @test
      */
-    public function collectGarbageRemovesTagEntriesForCacheEntriesWithExpiredLifetimeWithNonMysql(): void
+    public function collectGarbageRemovesTagEntriesForCacheEntriesWithExpiredLifetime(): void
     {
         $frontendMock = $this->createMock(FrontendInterface::class);
         $frontendMock->method('getIdentifier')->willReturn('pages');
 
-        // Must be mocked here to test for "mysql" version implementation
-        $subject = $this->getMockBuilder(Typo3DatabaseBackend::class)
-            ->onlyMethods(['isConnectionMysql'])
-            ->setConstructorArgs(['Testing'])
-            ->getMock();
-        $subject->expects(self::once())->method('isConnectionMysql')->willReturn(false);
+        $subject = new Typo3DatabaseBackend('Testing');
         $subject->setCache($frontendMock);
 
         // tag rows tagA and tagB should be removed by garbage collector after EXEC_TIME manipulation
@@ -737,17 +525,12 @@ final class Typo3DatabaseBackendTest extends FunctionalTestCase
     /**
      * @test
      */
-    public function collectGarbageRemovesOrphanedTagEntriesFromTagsTableWithNonMysql(): void
+    public function collectGarbageRemovesOrphanedTagEntriesFromTagsTable(): void
     {
         $frontendMock = $this->createMock(FrontendInterface::class);
         $frontendMock->method('getIdentifier')->willReturn('pages');
 
-        // Must be mocked here to test for "mysql" version implementation
-        $subject = $this->getMockBuilder(Typo3DatabaseBackend::class)
-            ->onlyMethods(['isConnectionMysql'])
-            ->setConstructorArgs(['Testing'])
-            ->getMock();
-        $subject->expects(self::once())->method('isConnectionMysql')->willReturn(false);
+        $subject = new Typo3DatabaseBackend('Testing');
         $subject->setCache($frontendMock);
 
         // tag rows tagA and tagB should be removed by garbage collector after EXEC_TIME manipulation
@@ -802,24 +585,12 @@ final class Typo3DatabaseBackendTest extends FunctionalTestCase
         self::assertSame(0, $tagsTableConnection->count('*', 'cache_pages_tags', []));
     }
 
-    /**
-     * @param bool $returnMockObject
-     * @param bool $isConnectionMysql
-     */
-    protected function getSubjectObject($returnMockObject = false, $isConnectionMysql = true): Typo3DatabaseBackend
+    protected function getSubjectObject(): Typo3DatabaseBackend
     {
         $frontendMock = $this->createMock(FrontendInterface::class);
         $frontendMock->method('getIdentifier')->willReturn('pages');
 
-        if (!$returnMockObject) {
-            $subject = new Typo3DatabaseBackend('Testing');
-        } else {
-            $subject = $this->getMockBuilder(Typo3DatabaseBackend::class)
-                ->onlyMethods(['isConnectionMysql'])
-                ->setConstructorArgs(['Testing'])
-                ->getMock();
-            $subject->expects(self::once())->method('isConnectionMysql')->willReturn($isConnectionMysql);
-        }
+        $subject = new Typo3DatabaseBackend('Testing');
         $subject->setCache($frontendMock);
 
         $subject->set('idA', 'dataA', ['tagA', 'tagB']);
