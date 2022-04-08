d5c8345b0d6d148a242e8385468cef1bf517e93d
Nikita Hovratov <nikita.h@live.de>
2022-04-08 17:47:20 +0200

    [BUGFIX] Prevent duplicate page tree items when entry points intersect
    In a scenario, where a backend user has two entry
    points, in which the second one intersects the first
    one (is a direct sub-page), pages were shown
    multiple times.
    
    The reason behind this was, that inside
    PageTreeRepository->groupAndSortPages() every page
    was added regardless if it was already added in a
    previous run. This is the case, when the same pages
    are found in different depth levels.
    
    E.g.
    
    1 - Home (DB mount 1)
      2 - Page 1
      2 - Page 2 (DB mount 2)
        3 - Subpage 1 // will be added for level 2 and 3
        3 - Subpage 2
    
    To prevent this, a guard clause was added, which
    checks whether the page was already added by
    comparing the uid value.
    
    Resolves: #96512
    Resolves: #97774
    Related: #96367
    Releases: main, 11.5, 10.4
    Change-Id: I65439011b894f05faef9cb507d905b3d7d3b709f
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/74258
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Silvia Bigler <silvia@dunkelweiss.li>
    Tested-by: Stefan Bürk <stefan@buerk.tech>
    Reviewed-by: Silvia Bigler <silvia@dunkelweiss.li>
    Reviewed-by: Stefan Bürk <stefan@buerk.tech>

---
+++ typo3/sysext/backend/Tests/Functional/Tree/Repository/PageTreeRepositoryTest.php
@@ -0,0 +1,240 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3\CMS\Backend\Tests\Functional\Tree\Repository;
+
+use TYPO3\CMS\Backend\Tests\Functional\Tree\Repository\Fixtures\Tree\NormalizeTreeTrait;
+use TYPO3\CMS\Backend\Tree\Repository\PageTreeRepository;
+use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
+
+class PageTreeRepositoryTest extends FunctionalTestCase
+{
+    use NormalizeTreeTrait;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->importCSVDataSet('typo3/sysext/backend/Tests/Functional/Tree/Repository/Fixtures/PageTree.csv');
+        $this->importCSVDataSet(__DIR__ . '/../../Fixtures/be_users.csv');
+        $this->setUpBackendUser(1);
+    }
+
+    public function getTreeLevelsReturnsGroupedAndSortedPageTreeArrayDataProvider(): iterable
+    {
+        yield 'Single entry point with depth 2' => [
+            'pageTree' => [
+                'uid' => 0,
+                'title' => 'Core',
+            ],
+            'depth' => 2,
+            'entryPointIds' => [
+                2,
+            ],
+            'expected' => [
+                'uid' => 0,
+                'title' => 'Core',
+                '_children' => [
+                    [
+                        'uid' => 2,
+                        'title' => 'Main Area',
+                        '_children' => [
+                            [
+                                'uid' => 20,
+                                'title' => 'Main Area Sub 1',
+                                '_children' => [],
+                            ],
+                            [
+                                'uid' => 21,
+                                'title' => 'Main Area Sub 2',
+                                '_children' => [
+                                    [
+                                        'uid' => 30,
+                                        'title' => 'Sub Area 1',
+                                        '_children' => [],
+                                    ],
+                                    [
+                                        'uid' => 31,
+                                        'title' => 'Sub Area 2',
+                                        '_children' => [],
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ];
+
+        yield 'Single entry point with depth 1' => [
+            'pageTree' => [
+                'uid' => 0,
+                'title' => 'Core',
+            ],
+            'depth' => 1,
+            'entryPointIds' => [
+                2,
+            ],
+            'expected' => [
+                'uid' => 0,
+                'title' => 'Core',
+                '_children' => [
+                    [
+                        'uid' => 2,
+                        'title' => 'Main Area',
+                        '_children' => [
+                            [
+                                'uid' => 20,
+                                'title' => 'Main Area Sub 1',
+                                '_children' => [],
+                            ],
+                            [
+                                'uid' => 21,
+                                'title' => 'Main Area Sub 2',
+                                '_children' => [],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ];
+
+        yield 'Two entry points parallel to each other' => [
+            'pageTree' => [
+                'uid' => 0,
+                'title' => 'Core',
+            ],
+            'depth' => 2,
+            'entryPointIds' => [
+                2,
+                3,
+            ],
+            'expected' => [
+                'uid' => 0,
+                'title' => 'Core',
+                '_children' => [
+                    [
+                        'uid' => 2,
+                        'title' => 'Main Area',
+                        '_children' => [
+                            [
+                                'uid' => 20,
+                                'title' => 'Main Area Sub 1',
+                                '_children' => [],
+                            ],
+                            [
+                                'uid' => 21,
+                                'title' => 'Main Area Sub 2',
+                                '_children' => [
+                                    [
+                                        'uid' => 30,
+                                        'title' => 'Sub Area 1',
+                                        '_children' => [],
+                                    ],
+                                    [
+                                        'uid' => 31,
+                                        'title' => 'Sub Area 2',
+                                        '_children' => [],
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                    [
+                        'uid' => 3,
+                        'title' => 'Home 2',
+                        '_children' => [],
+                    ],
+                ],
+            ],
+        ];
+
+        yield 'Two entry points intersecting each other' => [
+            'pageTree' => [
+                'uid' => 0,
+                'title' => 'Core',
+            ],
+            'depth' => 2,
+            'entryPointIds' => [
+                2,
+                21,
+            ],
+            'expected' => [
+                'uid' => 0,
+                'title' => 'Core',
+                '_children' => [
+                    [
+                        'uid' => 2,
+                        'title' => 'Main Area',
+                        '_children' => [
+                            [
+                                'uid' => 20,
+                                'title' => 'Main Area Sub 1',
+                                '_children' => [],
+                            ],
+                            [
+                                'uid' => 21,
+                                'title' => 'Main Area Sub 2',
+                                '_children' => [
+                                    [
+                                        'uid' => 30,
+                                        'title' => 'Sub Area 1',
+                                        '_children' => [],
+                                    ],
+                                    [
+                                        'uid' => 31,
+                                        'title' => 'Sub Area 2',
+                                        '_children' => [],
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                    [
+                        'uid' => 21,
+                        'title' => 'Main Area Sub 2',
+                        '_children' => [
+                            [
+                                'uid' => 30,
+                                'title' => 'Sub Area 1',
+                                '_children' => [],
+                            ],
+                            [
+                                'uid' => 31,
+                                'title' => 'Sub Area 2',
+                                '_children' => [],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider getTreeLevelsReturnsGroupedAndSortedPageTreeArrayDataProvider
+     * @test
+     */
+    public function getTreeLevelsReturnsGroupedAndSortedPageTreeArray(array $pageTree, int $depth, array $entryPointIds, array $expected): void
+    {
+        $pageTreeRepository = new PageTreeRepository();
+        $actual = $pageTreeRepository->getTreeLevels($pageTree, $depth, $entryPointIds);
+        $actual = $this->sortTreeArray([$actual]);
+        $keepProperties = array_flip(['uid', 'title', '_children']);
+        $actual = $this->normalizeTreeArray($actual, $keepProperties);
+        self::assertEquals($expected, $actual[0]);
+    }
+}
