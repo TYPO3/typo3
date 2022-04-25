fd6e5e038013709cd1121fc10378f11a3b0b6651
Oliver Bartsch <bo@cedev.de>
2022-04-25 14:00:50 +0200

    [BUGFIX] Check parsed option in EmailFinisher
    To prevent PHP warnings for passing invalid
    arguments to "foreach", the parsed option
    is now checked to be a non empty array.
    
    Resolves: #97463
    Releases: main, 11.5
    Change-Id: Id66a448886baf99ad0b2aaef1d3291ac0aae7dff
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/74387
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Stefan Bürk <stefan@buerk.tech>
    Tested-by: Nikita Hovratov <nikita.h@live.de>
    Tested-by: Oliver Bartsch <bo@cedev.de>
    Reviewed-by: Stefan Bürk <stefan@buerk.tech>
    Reviewed-by: Oliver Klee <typo3-coding@oliverklee.de>
    Reviewed-by: Nikita Hovratov <nikita.h@live.de>
    Reviewed-by: Oliver Bartsch <bo@cedev.de>

--- typo3/sysext/form/Classes/Domain/Finishers/EmailFinisher.php
+++ typo3/sysext/form/Classes/Domain/Finishers/EmailFinisher.php
@@ -234,6 +234,10 @@ class EmailFinisher extends AbstractFinisher
     protected function getRecipients(string $listOption): array
     {
         $recipients = $this->parseOption($listOption) ?? [];
+        if (!is_array($recipients) || $recipients === []) {
+            return [];
+        }
+
         $addresses = [];
         foreach ($recipients as $address => $name) {
             if (!GeneralUtility::validEmail($address)) {
