b9a0ff26daa4751c15a17d2520bd29a9bdf01d74
Larry Garfield <larry@garfieldtech.com>
2022-05-26 15:53:44 -0500

    [TASK] Add tests for log `#[Channel(...)]` attribute
    v11 introduced support for using a Channel attribute to define
    what log channel should be injected into a class, either via
    the constructor or the LoggingAwareInterface. However, the
    initial patch lacked any tests.
    
    This patch adds basic tests to verify that the attribute works,
    and indirectly document it.
    
    Resolves: #97695
    Related: #95079
    Related: #95044
    Releases: main, 11.5
    Change-Id: I7cc97fc839ccd4015ac20b1bce787d4cbbc40a26
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/74750
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Stefan Bürk <stefan@buerk.tech>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Reviewed-by: Stefan Bürk <stefan@buerk.tech>

---
+++ typo3/sysext/core/Tests/Functional/Fixtures/Extensions/test_logger/ext_emconf.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Logger Test',
+    'description' => 'Logger Test',
+    'category' => 'example',
+    'version' => '12.0.0',
+    'state' => 'beta',
+    'author' => 'Larry Garfield',
+    'author_email' => 'larry.garfield@typo3.org',
+    'author_company' => 'TYPO3 GmbH',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '12.0.0',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
