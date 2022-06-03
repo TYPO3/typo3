6957bb32ce42c9c051a1c5b982a108bd4a8cbd90
Andreas Fernandez <a.fernandez@scripting-base.de>
2022-06-03 11:54:42 +0200

    [BUGFIX] Late bind drag uploader
    If TCA with a file upload is used within IRRE (e.g. as done by
    EXT:news), creating a new record or opening an existing record now sets
    up the drag uploader.
    
    As the DragUploader module is loaded independently from the FormEngine
    main module, loading order may vary, rendering an event-based solution
    unstable. To solve this issue without any major refactorings, a
    MutationObserver is installed by the DragUploader module.
    
    Resolves: #97676
    Releases: main, 11.5
    Change-Id: I36a3922999842f4d0bddfc7e4b148ec39880d9df
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/74813
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Georg Ringer <georg.ringer@gmail.com>
    Reviewed-by: Georg Ringer <georg.ringer@gmail.com>

--- Build/Sources/TypeScript/backend/Resources/Public/TypeScript/DragUploader.ts
+++ Build/Sources/TypeScript/backend/Resources/Public/TypeScript/DragUploader.ts
@@ -835,6 +835,12 @@ class DragUploader {
     $(() => {
       $('.t3js-drag-uploader').dragUploader(opts);
     });
+
+    // @todo Refactor the FormEngine integration of the uploader to instance new uploaders via event handlers
+    const observer = new MutationObserver((): void => {
+      $('.t3js-drag-uploader').dragUploader(opts);
+    });
+    observer.observe(document, {childList: true, subtree: true});
   }
 }
 
