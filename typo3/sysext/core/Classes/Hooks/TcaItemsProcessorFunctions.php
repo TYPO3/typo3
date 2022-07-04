f1e915c73431ba1df9547bcd804da6c0db3904b6
Francois Suter <sutfra@gmail.com>
2022-07-04 16:39:26 +0200

    [BUGFIX] Enforce string type for custom permission key
    Make sure that item key for custom permission options are cast
    to string before being parsed for invalid characters.
    
    Resolves: #97852
    Releases: main, 11.5
    Change-Id: Id8ff1997b875171316784d00bdc7bf72702d7645
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/74949
    Reviewed-by: Oliver Klee <typo3-coding@oliverklee.de>
    Reviewed-by: Benni Mack <benni@typo3.org>
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Benni Mack <benni@typo3.org>

--- typo3/sysext/core/Classes/Hooks/TcaItemsProcessorFunctions.php
+++ typo3/sysext/core/Classes/Hooks/TcaItemsProcessorFunctions.php
@@ -182,7 +182,7 @@ class TcaItemsProcessorFunctions
                 }
                 $fieldDefinition['items'][] = [
                     $languageService->sL($itemConfig[0] ?? ''),
-                    $customOptionsKey . ':' . preg_replace('/[:|,]/', '', $itemKey),
+                    $customOptionsKey . ':' . preg_replace('/[:|,]/', '', (string)$itemKey),
                     $icon,
                     null,
                     $helpText,
