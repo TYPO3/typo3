7bef9961f6b23ce51e9a27164c10603e854f831d
Nikita Hovratov <nikita.h@live.de>
2022-07-08 09:17:50 +0200

    [BUGFIX] Use `$validator->setOptions()` in ValidatorResolverService
    With the refactoring in #96998 the validator options are now passed
    with the `setOptions` method instead of passing them as constructor
    argument.
    
    Resolves: #97871
    Related: #96998
    Releases: main
    Change-Id: I9a52560debc5227e0d3049f13d8b897f56baf2d5
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/75048
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Stefan Bürk <stefan@buerk.tech>
    Tested-by: Benni Mack <benni@typo3.org>
    Reviewed-by: Oliver Klee <typo3-coding@oliverklee.de>
    Reviewed-by: Stefan Bürk <stefan@buerk.tech>
    Reviewed-by: Benni Mack <benni@typo3.org>

--- typo3/sysext/felogin/Classes/Service/ValidatorResolverService.php
+++ typo3/sysext/felogin/Classes/Service/ValidatorResolverService.php
@@ -20,6 +20,7 @@ namespace TYPO3\CMS\FrontendLogin\Service;
 use Generator;
 use TYPO3\CMS\Core\SingletonInterface;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Validation\Validator\ValidatorInterface;
 
 /**
  * @internal this is a concrete TYPO3 implementation and solely used for EXT:felogin and not part of TYPO3's Core API.
@@ -30,9 +31,10 @@ class ValidatorResolverService implements SingletonInterface
      * Resolves Validator classes based on the validator config. This array can either
      * contain a FQCN or an array with keys "className"(string) and "options"(array).
      *
-     * @param array $validatorConfig
+     * @template T of ValidatorInterface
+     * @param array<class-string<T>|array{className: class-string<T>, options: array}> $validatorConfig
      *
-     * @return Generator|null
+     * @return Generator<T>|null
      */
     public function resolve(array $validatorConfig): ?Generator
     {
@@ -40,7 +42,9 @@ class ValidatorResolverService implements SingletonInterface
             if (is_string($validator)) {
                 yield GeneralUtility::makeInstance($validator);
             } elseif (is_array($validator)) {
-                yield GeneralUtility::makeInstance($validator['className'], $validator['options']);
+                $validatorInstance = GeneralUtility::makeInstance($validator['className']);
+                $validatorInstance->setOptions($validator['options']);
+                yield $validatorInstance;
             }
         }
     }
