4fde34c5b39315d72f88a9733f784c88bbf183c8
Larry Garfield <larry@garfieldtech.com>
2022-07-20 11:23:25 -0500

    [BUGFIX] Fix dead code path in LanguageService
    This check likely predates types being specified, which
    makes it unnecessary.
    
    Used command:
    
    > ./Build/Scripts/runTests.sh -s phpstanGenerateBaseline
    
    Resolves: #97979
    Releases: main, 11.5
    Change-Id: I78af547b9b3c1412c50cc36373973b160f3a82d4
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/75249
    Tested-by: Stefan Bürk <stefan@buerk.tech>
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Andreas Fernandez <a.fernandez@scripting-base.de>
    Reviewed-by: Stefan Bürk <stefan@buerk.tech>
    Reviewed-by: Andreas Fernandez <a.fernandez@scripting-base.de>

--- typo3/sysext/core/Classes/Localization/LanguageService.php
+++ typo3/sysext/core/Classes/Localization/LanguageService.php
@@ -227,7 +227,7 @@ class LanguageService
                 unset($labels[$this->lang]);
             }
         }
-        return is_array($labels) ? $labels : [];
+        return $labels;
     }
 
     /**
