09d43bb3d480cf4bde4a304aed1e6010c467687e
Larry Garfield <larry@garfieldtech.com>
2022-07-22 16:53:38 -0500

    [TASK] Remove superfluous cast in LiveSearch
    Used command:
    
    > ./Build/Scripts/runTests.sh -s phpstanGenerateBaseline
    
    Resolves: #98008
    Releases: main, 11.5
    Change-Id: Iecadad3cc1e5089f0ab7282f5b5302ead66ce6a7
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/75270
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Stefan Bürk <stefan@buerk.tech>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Reviewed-by: Stefan Bürk <stefan@buerk.tech>
    Reviewed-by: Oliver Klee <typo3-coding@oliverklee.de>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>

--- typo3/sysext/backend/Classes/Search/LiveSearch/LiveSearch.php
+++ typo3/sysext/backend/Classes/Search/LiveSearch/LiveSearch.php
@@ -325,7 +325,7 @@ class LiveSearch
                     $constraints[] = $queryBuilder->expr()->like(
                         $fieldName,
                         $queryBuilder->createNamedParameter(
-                            '%' . $queryBuilder->escapeLikeWildcards((int)$this->queryString) . '%',
+                            '%' . $queryBuilder->escapeLikeWildcards($this->queryString) . '%',
                             \PDO::PARAM_STR
                         )
                     );
