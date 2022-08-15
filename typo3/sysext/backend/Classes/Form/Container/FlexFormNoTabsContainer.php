685746aa180c7c1789b4917d82e3f6cead375957
Torben Hansen <derhansen@gmail.com>
2022-08-15 15:45:39 +0200

    [BUGFIX] Prevent warning when FlexForm is empty in PHP8
    When a FlexForm has an empty data structure, a warning is thrown
    in PHP8, because it is not checked in FlexFormNoTabsContainer if
    the resolved sheet name actually exists as key in the
    flexFormRowData array.
    
    This patch uses the null coalescing operator to fix the problem.
    
    Resolves: #98147
    Releases: main, 11.5
    Change-Id: Ie4bde3af79a9a473cc02aff9f1ec299fff1bc259
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/75448
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Benni Mack <benni@typo3.org>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Reviewed-by: Oliver Klee <typo3-coding@oliverklee.de>
    Reviewed-by: Benni Mack <benni@typo3.org>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>

--- typo3/sysext/backend/Classes/Form/Container/FlexFormNoTabsContainer.php
+++ typo3/sysext/backend/Classes/Form/Container/FlexFormNoTabsContainer.php
@@ -50,7 +50,7 @@ class FlexFormNoTabsContainer extends AbstractContainer
         // Determine this single sheet name, most often it ends up with sDEF, except if only one sheet was defined
         $flexFormSheetNames = array_keys($flexFormDataStructureArray['sheets']);
         $sheetName = array_pop($flexFormSheetNames);
-        $flexFormRowDataSubPart = $flexFormRowData['data'][$sheetName]['lDEF'] ?: [];
+        $flexFormRowDataSubPart = $flexFormRowData['data'][$sheetName]['lDEF'] ?? [];
 
         unset($flexFormDataStructureArray['meta']);
 
