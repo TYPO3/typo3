54ef8ceea343262012d800b0072b8b1a6aece1d5
Oliver Hader <oliver@typo3.org>
2022-08-25 09:04:27 +0200

    [RELEASE] Release of TYPO3 11.5.15
    Change-Id: I16a29c9ce3ae6d3d430b2d09b0cc952533e67cfe
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/75547
    Tested-by: Oliver Hader <oliver.hader@typo3.org>
    Reviewed-by: Oliver Hader <oliver.hader@typo3.org>

--- typo3/sysext/core/Classes/Information/Typo3Version.php
+++ typo3/sysext/core/Classes/Information/Typo3Version.php
@@ -19,7 +19,7 @@ namespace TYPO3\CMS\Core\Information;
 
 class Typo3Version
 {
-    protected const VERSION = '11.5.15-dev';
+    protected const VERSION = '11.5.15';
     protected const BRANCH = '11.5';
 
     /**
