aac4dbd89474e73ebcaf4e0c3eac6dcda5d5c4e7
Oliver Bartsch <bo@cedev.de>
2022-09-08 15:23:08 +0200

    [BUGFIX] Properly evaluate whether toolbar item provides a dropdown
    The ToolbarItemInterface requires the methods
    hasDropDown() as well as getDropDown(). Since
    #98214 the evaluation whether a toolbar item
    provides a dropdown is done in Fluid, using the
    corresponding magic "{toolbarItem.dropDown}".
    
    Fluid however first checks for a "Getter" and
    therefore uses the getDropDown() method instead
    of the hasDropDown() method to determine whether
    a toolbar item provides a dropdown. This led to
    display errors in case a toolbar item only provides
    a dropdown for some use cases as this was now
    always used.
    
    This is now fixed using a dedicated conditional
    ViewHelper.
    
    Resolves: #98299
    Related: #98214
    Releases: main
    Change-Id: I3aabfd0debe95ae35f2d9459c483d55e2260292b
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/75663
    Tested-by: Andreas Fernandez <a.fernandez@scripting-base.de>
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Benjamin Franzke <bfr@qbus.de>
    Reviewed-by: Andreas Fernandez <a.fernandez@scripting-base.de>
    Reviewed-by: Benjamin Franzke <bfr@qbus.de>

---
+++ typo3/sysext/backend/Classes/ViewHelpers/Toolbar/IfHasDropdownViewHelper.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3\CMS\Backend\ViewHelpers\Toolbar;
+
+use TYPO3\CMS\Backend\Toolbar\ToolbarItemInterface;
+use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractConditionViewHelper;
+
+/**
+ * Checks whether a toolbar item provides a dropdown menu
+ *
+ * @internal
+ */
+class IfHasDropdownViewHelper extends AbstractConditionViewHelper
+{
+    public function initializeArguments(): void
+    {
+        parent::initializeArguments();
+        $this->registerArgument('class', ToolbarItemInterface::class, 'The toolbar item class to be checked for providing a drop down', true);
+    }
+
+    public static function verdict(array $arguments, RenderingContextInterface $renderingContext): bool
+    {
+        return $arguments['class'] instanceof ToolbarItemInterface && $arguments['class']->hasDropDown();
+    }
+}
