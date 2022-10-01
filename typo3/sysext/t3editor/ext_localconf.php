a42faa3602d7c020e48fa983ce12030a1af9f6d7
Benni Mack <benni@typo3.org>
2022-10-01 16:01:36 +0200

    [TASK] Remove unused implementation from file_edit hooks
    A followup to #97452 where a hook was removed but TYPO3 Core still
    used it - the hook implementation is now removed.
    
    Resolves: #98494
    Related: #97452
    Releases: main
    Change-Id: I90265620d3488520ba37f9ecd5cf33be61e56102
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/75933
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Georg Ringer <georg.ringer@gmail.com>
    Tested-by: Kevin Appelt <kevin.appelt@icloud.com>
    Tested-by: Stefan Bürk <stefan@buerk.tech>
    Reviewed-by: Georg Ringer <georg.ringer@gmail.com>
    Reviewed-by: Kevin Appelt <kevin.appelt@icloud.com>
    Reviewed-by: Stefan Bürk <stefan@buerk.tech>

--- typo3/sysext/t3editor/ext_localconf.php
+++ typo3/sysext/t3editor/ext_localconf.php
@@ -3,12 +3,8 @@
 declare(strict_types=1);
 
 use TYPO3\CMS\T3editor\Form\Element\T3editorElement;
-use TYPO3\CMS\T3editor\Hook\FileEditHook;
 
 defined('TYPO3') or die();
-
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['typo3/file_edit.php']['preOutputProcessingHook'][] =
-    FileEditHook::class . '->preOutputProcessingHook';
 
 // Register backend FormEngine node
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1433089350] = [
