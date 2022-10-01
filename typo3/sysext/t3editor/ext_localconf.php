1a3526abfa7a04423b869dc7854766033b6c8d07
Benjamin Franzke <bfr@qbus.de>
2022-10-01 09:37:08 +0200

    [TASK] Remove outdated t3editor requirejs configuration
    With the upgrade to codemirror v6 in #96575 it was forgotten to
    remove the RequireJS configuration that is no longer needed, as
    codemirror v6 is loaded via ES6 modules and importmap configuration.
    
    Releases: main
    Resolves: #98491
    Related: #96575
    Change-Id: I6fc849ea1ad8bc4f493447f492140617ea6445af
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/75929
    Tested-by: Oliver Hader <oliver.hader@typo3.org>
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Benni Mack <benni@typo3.org>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Reviewed-by: Oliver Hader <oliver.hader@typo3.org>
    Reviewed-by: Benni Mack <benni@typo3.org>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>

--- typo3/sysext/t3editor/ext_localconf.php
+++ typo3/sysext/t3editor/ext_localconf.php
@@ -4,16 +4,11 @@ declare(strict_types=1);
 
 use TYPO3\CMS\T3editor\Form\Element\T3editorElement;
 use TYPO3\CMS\T3editor\Hook\FileEditHook;
-use TYPO3\CMS\T3editor\Hook\PageRendererRenderPreProcess;
 
 defined('TYPO3') or die();
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['typo3/file_edit.php']['preOutputProcessingHook'][] =
     FileEditHook::class . '->preOutputProcessingHook';
-
-// Hook to add t3editor requirejs config to PageRenderer in backend
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php']['render-preProcess'][] =
-    PageRendererRenderPreProcess::class . '->addRequireJsConfiguration';
 
 // Register backend FormEngine node
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1433089350] = [
