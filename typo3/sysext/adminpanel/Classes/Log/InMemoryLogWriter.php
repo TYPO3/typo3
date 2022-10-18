e3089207663bdb2cbc393679943822a8d58eac7e
Sascha Egerer <sascha@sascha-egerer.de>
2022-10-18 23:56:55 +0200

    [BUGFIX] Interpolate log messages in InMemoryLogWriter
    Log messages may have placeholders that must be interpolated
    based on the context provided to the message.
    
    Resolves: #98653
    Releases: main, 11.5
    Change-Id: I953bb41ba52599b7f65328027fe9e905ef5d35d0
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/76177
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>

--- typo3/sysext/adminpanel/Classes/Log/InMemoryLogWriter.php
+++ typo3/sysext/adminpanel/Classes/Log/InMemoryLogWriter.php
@@ -66,7 +66,7 @@ class InMemoryLogWriter extends AbstractWriter
             return $this;
         }
 
-        self::$log[] = $record;
+        self::$log[] = (clone $record)->setMessage($this->interpolate($record->getMessage(), $record->getData()));
 
         return $this;
     }
