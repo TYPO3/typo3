2f1991ca48b526ef9facdf24620b4e147802ffd4
Andreas Fernandez <a.fernandez@scripting-base.de>
2022-10-21 16:19:49 +0200

    [BUGFIX] Load modal CSS for EXT:dashboard only in BackendController
    EXT:dashboard has some custom CSS for its modal contents. Due to how
    modals are rendered, the CSS needs to be loaded in the global backend
    scaffold.
    
    Previously, the CSS was unnecessarily loaded in every backend module as
    the CSS was configured as a "skin", which comes from a time where the
    TYPO3 backend was fully skinnable. Using this approach is probably a
    workaround due to lacking API back then.
    
    Since TYPO3 v12, there is a `AfterBackendPageRenderEvent` event
    available in the outer backend controller which is now used to load the
    custom CSS.
    
    Resolves: #98697
    Releases: main
    Change-Id: Icf5799ae9fbd23eec6f525e478cd242598f27ea0
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/76220
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Anja Leichsenring <aleichsenring@ab-softlab.de>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Reviewed-by: Anja Leichsenring <aleichsenring@ab-softlab.de>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>

---
+++ typo3/sysext/dashboard/Classes/EventListener/AfterBackendPageRenderEventListener.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3\CMS\Dashboard\EventListener;
+
+use TYPO3\CMS\Core\Page\PageRenderer;
+
+/**
+ * Adds custom CSS needed for EXT:dashboard in the "outer" backend scaffold
+ */
+final class AfterBackendPageRenderEventListener
+{
+    public function __construct(private readonly PageRenderer $pageRenderer)
+    {
+    }
+
+    public function __invoke(): void
+    {
+        $this->pageRenderer->addCssFile('EXT:dashboard/Resources/Public/Css/Modal/style.css');
+    }
+}
--- typo3/sysext/dashboard/ext_tables.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-defined('TYPO3') or die();
-
-$GLOBALS['TBE_STYLES']['skins']['dashboard']['stylesheetDirectories']['modal'] = 'EXT:dashboard/Resources/Public/Css/Modal/';
