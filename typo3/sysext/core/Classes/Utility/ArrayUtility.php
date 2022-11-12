e11b2cce25993b874493258c8d4fd9480a458e62
Oliver Hader <oliver@typo3.org>
2022-11-12 17:01:56 +0100

    [TASK] Streamline TYPO3\CMS\Core\Utility\ArrayUtility::filterRecursive
    * actually use `array_filter` internally
    * allow using `array_filter` modes `ARRAY_FILTER_USE_KEY`
      and `ARRAY_FILTER_USE_BOTH`
    
    Resolves: #99071
    Releases: main, 11.5
    Change-Id: I1b95ae0a7551250125a9eeabbd0ee7e66e25a6d9
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/76635
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Stefan Bürk <stefan@buerk.tech>
    Reviewed-by: Stefan Bürk <stefan@buerk.tech>

--- typo3/sysext/core/Classes/Utility/ArrayUtility.php
+++ typo3/sysext/core/Classes/Utility/ArrayUtility.php
@@ -918,12 +918,17 @@ class ArrayUtility
     /**
      * Recursively filter an array
      *
-     * @param array $array
-     * @param callable|null $callback
-     * @return array the filtered array
-     * @see https://secure.php.net/manual/en/function.array-filter.php
+     * Example:
+     * filterRecursive(
+     *   ['a' => ['b' =>  null]],
+     *   static fn ($item) => $item !== null,
+     *   ARRAY_FILTER_USE_BOTH
+     * )
+     *
+     * @param 0|ARRAY_FILTER_USE_KEY|ARRAY_FILTER_USE_BOTH $mode
+     * @see https://www.php.net/manual/en/function.array-filter.php
      */
-    public static function filterRecursive(array $array, callable $callback = null): array
+    public static function filterRecursive(array $array, callable $callback = null, int $mode = 0): array
     {
         $callback = $callback ?: static function ($value) {
             return (bool)$value;
@@ -931,15 +936,10 @@ class ArrayUtility
 
         foreach ($array as $key => $value) {
             if (is_array($value)) {
-                $array[$key] = self::filterRecursive($value, $callback);
-            }
-
-            if (!$callback($value)) {
-                unset($array[$key]);
+                $array[$key] = self::filterRecursive($value, $callback, $mode);
             }
         }
-
-        return $array;
+        return array_filter($array, $callback, $mode);
     }
 
     /**
