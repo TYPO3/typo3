72187ddb1b97353019f40a37a44cb178d640e13d
Nikita Hovratov <nikita.h@live.de>
2022-11-20 22:48:08 +0100

    [TASK] Convert ExtendedFileUtilityTest to functional
    Resolves: #98767
    Releases: main
    Change-Id: I5b2b9082e5e2a8264ed63f11a37255e586ea963e
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/76752
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Reviewed-by: Stefan Bürk <stefan@buerk.tech>
    Reviewed-by: Benni Mack <benni@typo3.org>
    Tested-by: Stefan Bürk <stefan@buerk.tech>
    Tested-by: core-ci <typo3@b13.com>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>

---
+++ typo3/sysext/core/Tests/Functional/Utility/File/ExtendedFileUtilityTest.php
@@ -0,0 +1,87 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3\CMS\Core\Tests\Functional\Utility\File;
+
+use TYPO3\CMS\Core\Localization\LanguageServiceFactory;
+use TYPO3\CMS\Core\Resource\StorageRepository;
+use TYPO3\CMS\Core\Utility\File\ExtendedFileUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
+
+class ExtendedFileUtilityTest extends FunctionalTestCase
+{
+    protected array $pathsToProvideInTestInstance = [
+        'typo3/sysext/core/Tests/Functional/Utility/Fixtures/Folders/' => 'fileadmin/',
+    ];
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->importCSVDataSet('typo3/sysext/core/Tests/Functional/Utility/Fixtures/DataSet/sys_refindex.csv');
+        $this->importCSVDataSet('typo3/sysext/core/Tests/Functional/Utility/Fixtures/DataSet/sys_file.csv');
+        $GLOBALS['LANG'] = GeneralUtility::makeInstance(LanguageServiceFactory::class)->create('default');
+    }
+
+    /**
+     * @test
+     */
+    public function folderHasFilesInUseReturnsTrueIfItHasFilesInUse(): void
+    {
+        /** @var StorageRepository $storageRepository */
+        $storageRepository = $this->get(StorageRepository::class);
+        $resourceStorage = $storageRepository->getDefaultStorage();
+        $folder = $resourceStorage->getFolder('FolderWithUsedFile');
+
+        $extendedFileUtility = new ExtendedFileUtility();
+        $result = $extendedFileUtility->folderHasFilesInUse($folder);
+
+        self::assertTrue($result);
+    }
+
+    /**
+     * @test
+     */
+    public function folderHasFilesInUseReturnsFalseIfItHasNoFilesInUse(): void
+    {
+        /** @var StorageRepository $storageRepository */
+        $storageRepository = $this->get(StorageRepository::class);
+        $resourceStorage = $storageRepository->getDefaultStorage();
+        $folder = $resourceStorage->getFolder('FolderWithUnusedFile');
+
+        $extendedFileUtility = new ExtendedFileUtility();
+        $result = $extendedFileUtility->folderHasFilesInUse($folder);
+
+        self::assertFalse($result);
+    }
+
+    /**
+     * @test
+     */
+    public function folderHasFilesInUseReturnsFalseIfItHasNoFiles(): void
+    {
+        /** @var StorageRepository $storageRepository */
+        $storageRepository = $this->get(StorageRepository::class);
+        $resourceStorage = $storageRepository->getDefaultStorage();
+        $folder = $resourceStorage->createFolder('EmptyFolder');
+
+        $extendedFileUtility = new ExtendedFileUtility();
+        $result = $extendedFileUtility->folderHasFilesInUse($folder);
+
+        self::assertFalse($result);
+    }
+}
