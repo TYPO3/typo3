8b6f49266c5e52a6cf2853c4343ab9c36316ee52
Andreas Fernandez <a.fernandez@scripting-base.de>
2022-11-22 13:12:57 +0100

    [BUGFIX] Remove obsolete declarations from index.d.ts
    Resolves: #99161
    Releases: main
    Change-Id: I760bab1962bd61b5ea0e66d592e62470f3b2a376
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/76767
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Anja Leichsenring <aleichsenring@ab-softlab.de>
    Tested-by: Frank Nägler <frank.naegler@typo3.com>
    Reviewed-by: Georg Ringer <georg.ringer@gmail.com>
    Reviewed-by: Frank Nägler <frank.naegler@typo3.com>
    Reviewed-by: Anja Leichsenring <aleichsenring@ab-softlab.de>

--- Build/types/TYPO3/index.d.ts
+++ Build/types/TYPO3/index.d.ts
@@ -30,77 +30,10 @@ declare namespace TYPO3 {
   export const configuration: any;
   export namespace CMS {
     export namespace Backend {
-      export class FormEngineValidation {
-        public readonly errorClass: string;
-        public markFieldAsChanged(field: HTMLInputElement|HTMLTextAreaElement|HTMLSelectElement|JQuery): void;
-        public trimExplode(delimiter: string, string: string): Array<string>;
-        public parseInt(value: number|string|boolean): number;
-        public parseDouble(value: number|string|boolean): string;
-        public ltrim(value: string): string;
-        public btrim(value: string): string;
-        public getYear(timeObj: Date): number|null;
-        public getDate(timeObj: Date): number;
-        public splitStr(theStr1: string, delim: string, index: number): string;
-        public initializeInputFields(): JQuery;
-        public registerCustomEvaluation(name: string, handler: Function): void;
-        public formatValue(type: string, value: string|number, config: Object): string;
-        public updateInputField(fieldName: string): void;
-        public validate(section?: Element): void;
-        public validateField(field: HTMLInputElement|HTMLTextAreaElement|HTMLSelectElement|JQuery, value?: string): string;
-        public processValue(command: string, value: string, config: Object): string;
-        public initialize(): void;
-      }
-
-      export namespace FormEngine {
-        export namespace Element {
-          export class SelectTree {
-            public dispatch: {[key: string]: Function};
-            public constructor();
-            public initialize(selector: HTMLElement|JQuery, settings: {[key: string]: any}): boolean;
-          }
-        }
-      }
-
       // @todo transform to proper interface, once FormEngine.js is migrated to TypeScript
       export interface OnFieldChangeItem {
         name: string;
         data: {[key: string]: string|number|boolean|null}
-      }
-
-      export class FormEngine {
-        public readonly Validation: FormEngineValidation;
-        public legacyFieldChangedCb(): void;
-        public getFieldElement(fieldName: string, appendix?: string, noFallback?: boolean): JQuery;
-        public updateHiddenFieldValueFromSelect(selectFieldEl: HTMLSelectElement, originalFieldEl: HTMLSelectElement): void;
-        public preventFollowLinkIfNotSaved(href: string): boolean;
-        public setSelectOptionFromExternalSource(
-          fieldName: string,
-          value: string,
-          label: string,
-          title: string,
-          exclusiveValues?: string,
-          optionEl?: HTMLOptionElement,
-        ): void;
-        public reinitialize(): void;
-        public openPopupWindow(mode: string, params: string): JQuery;
-        public initializeNullNoPlaceholderCheckboxes(): void;
-        public initializeNullWithPlaceholderCheckboxes(): void;
-        public requestFormEngineUpdate(askForUpdate: boolean): void;
-        public processOnFieldChange(items: OnFieldChangeItem[]): void;
-        public enableOptGroup(option: HTMLOptionElement): void;
-      }
-
-      export class MultiStepWizard {
-        public addSlide(identifier: string, title: string, content: string, severity: number, callback?: Function): MultiStepWizard;
-        public lockNextStep(): JQuery;
-        public unlockNextStep(): JQuery;
-        public lockPrevStep(): JQuery;
-        public unlockPrevStep(): JQuery;
-        public blurCancelStep(): JQuery;
-        public getComponent(): JQuery;
-        public addFinalProcessingSlide(callback?: Function): Promise<void>;
-        public show(): MultiStepWizard;
-        public dismiss(): MultiStepWizard;
       }
     }
   }
@@ -108,20 +41,6 @@ declare namespace TYPO3 {
 
 declare namespace TBE_EDITOR {
   export const customEvalFunctions: { [key: string]: Function };
-}
-
-/**
- * Current AMD/RequireJS modules are returning *instances* of ad-hoc *classes*, make that known to TypeScript
- */
-
-declare module '@typo3/backend/form-engine-validation' {
-  const _exported: TYPO3.CMS.Backend.FormEngineValidation;
-  export default _exported;
-}
-
-declare module '@typo3/backend/form-engine' {
-  const _exported: TYPO3.CMS.Backend.FormEngine;
-  export default _exported;
 }
 
 declare module '@typo3/ckeditor5-bundle' {
@@ -196,12 +115,7 @@ interface JQueryTypedEvent<T extends Event> extends JQueryEventObject {
  * Required to make jQuery plugins "available" in TypeScript
  */
 interface JQuery {
-  datetimepicker(options?: any): JQuery;
-
   dragUploader(options?: any): JQuery;
-
-  // To be able to use devbridge-autocomplete we have to override the definition of jquerui
-  autocomplete(options?: { [key: string]: any }): any;
   disablePagingAction(): void;
 }
 
