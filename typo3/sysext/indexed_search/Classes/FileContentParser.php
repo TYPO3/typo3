5b0c51cd4bc8f9da89918ae2554088beb0a71a2e
Tomas Norre Mikkelsen <tomasnorre@gmail.com>
2022-11-30 12:37:31 +0100

    [BUGFIX] uft8 encode text to allow special chars in PDF metadata
    This encodes the PDF metadata to UTF-8 to ensure that special chars
    like æ ø å ü ö ä are allowed without cutting of the metadata text.
    
    Resolves: #80085
    Releases: main, 11.5, 10.4
    Change-Id: I02b0730dd659b54c0d8c7186a2089419bd56d2a2
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/76861
    Reviewed-by: Andreas Fernandez <a.fernandez@scripting-base.de>
    Tested-by: Benni Mack <benni@typo3.org>
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Andreas Fernandez <a.fernandez@scripting-base.de>
    Reviewed-by: Benni Mack <benni@typo3.org>

--- typo3/sysext/indexed_search/Classes/FileContentParser.php
+++ typo3/sysext/indexed_search/Classes/FileContentParser.php
@@ -796,7 +796,7 @@ class FileContentParser
             foreach ($pdfInfoArray as $line) {
                 $parts = explode(':', $line, 2);
                 if (count($parts) > 1 && trim($parts[0])) {
-                    $res[strtolower(trim($parts[0]))] = trim($parts[1]);
+                    $res[strtolower(trim($parts[0]))] = mb_convert_encoding(trim($parts[1]), 'UTF-8', 'ISO-8859-1');
                 }
             }
         }
