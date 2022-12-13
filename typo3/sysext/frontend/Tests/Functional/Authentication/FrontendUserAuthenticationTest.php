27c5ac8f52fb5031b19a421f31792d5732d4c5bd
Nikita Hovratov <nikita.h@live.de>
2022-12-13 23:42:30 +0100

    [TASK] Convert FrontendUserAuthenticationTest partially to functional
    For the rest all prophecy usages have been replaced by native phpunit
    mocks.
    
    Resolves: #98727
    Releases: main
    Change-Id: I1682cf2970a008b98213b9aaa0c377e223b2caae
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/77121
    Reviewed-by: Nikita Hovratov <nikita.h@live.de>
    Reviewed-by: Torben Hansen <derhansen@gmail.com>
    Tested-by: Torben Hansen <derhansen@gmail.com>
    Tested-by: Benni Mack <benni@typo3.org>
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Nikita Hovratov <nikita.h@live.de>
    Tested-by: Oliver Klee <typo3-coding@oliverklee.de>
    Reviewed-by: Benni Mack <benni@typo3.org>

---
+++ typo3/sysext/frontend/Tests/Functional/Authentication/FrontendUserAuthenticationTest.php
@@ -0,0 +1,98 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3\CMS\Frontend\Tests\Functional\Authentication;
+
+use GuzzleHttp\Cookie\SetCookie;
+use Psr\Log\NullLogger;
+use TYPO3\CMS\Core\Security\Nonce;
+use TYPO3\CMS\Core\Security\RequestToken;
+use TYPO3\CMS\Core\Tests\Functional\SiteHandling\SiteBasedTestTrait;
+use TYPO3\CMS\Frontend\Authentication\FrontendUserAuthentication;
+use TYPO3\TestingFramework\Core\Functional\Framework\Frontend\InternalRequest;
+use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
+
+class FrontendUserAuthenticationTest extends FunctionalTestCase
+{
+    use SiteBasedTestTrait;
+
+    private const ROOT_PAGE_ID = 1;
+    protected const LANGUAGE_PRESETS = [];
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->importCSVDataSet('typo3/sysext/frontend/Tests/Functional/Fixtures/pages.csv');
+        $this->writeSiteConfiguration(
+            'frontend_authentication',
+            $this->buildSiteConfiguration(self::ROOT_PAGE_ID, '/'),
+        );
+        $this->setUpFrontendRootPage(self::ROOT_PAGE_ID, ['typo3/sysext/frontend/Tests/Functional/Fixtures/TypoScript/page.typoscript']);
+    }
+
+    /**
+     * @test
+     */
+    public function feSessionsAreNotStoredForAnonymousSessions(): void
+    {
+        $response = $this->executeFrontendSubRequest((new InternalRequest())->withPageId(self::ROOT_PAGE_ID));
+
+        self::assertStringNotContainsString('fe_typo_user', $response->getHeaderLine('Set-Cookie'));
+        $this->assertCSVDataSet('typo3/sysext/frontend/Tests/Functional/Authentication/Fixtures/fe_sessions_empty.csv');
+    }
+
+    /**
+     * @test
+     */
+    public function canCreateNewAndExistingSessionWithValidRequestToken(): void
+    {
+        $this->importCSVDataSet('typo3/sysext/frontend/Tests/Functional/Authentication/Fixtures/fe_users.csv');
+
+        $nonce = Nonce::create();
+        $requestToken = RequestToken::create('core/user-auth/fe')->toHashSignedJwt($nonce);
+        $request = (new InternalRequest())
+            ->withPageId(self::ROOT_PAGE_ID)
+            ->withMethod('POST')
+            ->withParsedBody(
+                [
+                    'user' => 'testuser',
+                    'pass' => 'test',
+                    'logintype' => 'login',
+                    '__RequestToken' => $requestToken,
+                ]
+            )
+            ->withCookieParams(['typo3nonce_' . $nonce->getSigningIdentifier()->name => $nonce->toHashSignedJwt()]);
+
+        $response = $this->executeFrontendSubRequest($request);
+
+        self::assertStringContainsString('fe_typo_user', $response->getHeaderLine('Set-Cookie'));
+        $this->assertCSVDataSet('typo3/sysext/frontend/Tests/Functional/Authentication/Fixtures/fe_sessions_filled.csv');
+
+        // Now check whether the existing session is retrieved by providing the retrieved JWT token in the cookie params.
+        $cookie = SetCookie::fromString($response->getHeaderLine('Set-Cookie'));
+        $request = (new InternalRequest())
+            ->withPageId(self::ROOT_PAGE_ID)
+            ->withCookieParams([$cookie->getName() => $cookie->getValue()]);
+
+        $frontendUserAuthentication = new FrontendUserAuthentication();
+        $frontendUserAuthentication->setLogger(new NullLogger());
+        $frontendUserAuthentication->start($request);
+
+        self::assertNotNull($frontendUserAuthentication->user);
+        self::assertEquals('testuser', $frontendUserAuthentication->user['username']);
+    }
+}
--- typo3/sysext/frontend/Tests/Unit/Authentication/FrontendUserAuthenticationTest.php
+++ typo3/sysext/frontend/Tests/Unit/Authentication/FrontendUserAuthenticationTest.php
@@ -17,112 +17,17 @@ declare(strict_types=1);
 
 namespace TYPO3\CMS\Frontend\Tests\Unit\Authentication;
 
-use Doctrine\DBAL\Result;
-use Prophecy\Argument;
-use Prophecy\PhpUnit\ProphecyTrait;
-use Psr\EventDispatcher\EventDispatcherInterface;
-use Psr\Http\Message\ServerRequestInterface;
 use Psr\Log\NullLogger;
-use TYPO3\CMS\Core\Authentication\AuthenticationService;
-use TYPO3\CMS\Core\Authentication\IpLocker;
-use TYPO3\CMS\Core\Context\Context;
-use TYPO3\CMS\Core\Context\SecurityAspect;
-use TYPO3\CMS\Core\Database\ConnectionPool;
-use TYPO3\CMS\Core\Database\Query\Expression\ExpressionBuilder;
-use TYPO3\CMS\Core\Database\Query\QueryBuilder;
-use TYPO3\CMS\Core\EventDispatcher\EventDispatcher;
 use TYPO3\CMS\Core\Http\ServerRequest;
-use TYPO3\CMS\Core\Security\JwtTrait;
-use TYPO3\CMS\Core\Security\RequestToken;
-use TYPO3\CMS\Core\Session\Backend\SessionBackendInterface;
 use TYPO3\CMS\Core\Session\UserSession;
 use TYPO3\CMS\Core\Session\UserSessionManager;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Core\Utility\StringUtility;
 use TYPO3\CMS\Frontend\Authentication\FrontendUserAuthentication;
 use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
 
-/**
- * Test cases for FrontendUserAuthentication
- *
- * @todo: Some of these tests would be better suited as functional tests
- */
 class FrontendUserAuthenticationTest extends UnitTestCase
 {
-    use ProphecyTrait;
-    use JwtTrait;
-
-    private const NOT_CHECKED_INDICATOR = '--not-checked--';
-
     protected bool $resetSingletonInstances = true;
-
-    /**
-     * User properties should not be set for anonymous sessions
-     *
-     * @test
-     */
-    public function userFieldIsNotSetForAnonymousSessions(): void
-    {
-        $GLOBALS['TYPO3_CONF_VARS']['SYS']['encryptionKey'] = 'secret-encryption-key-test';
-        $uniqueSessionId = StringUtility::getUniqueId('test');
-        $uniqueSessionIdJwt = self::encodeHashSignedJwt(
-            [
-                'identifier' => $uniqueSessionId,
-                'time' => (new \DateTimeImmutable())->format(\DateTimeImmutable::RFC3339),
-            ],
-            self::createSigningKeyFromEncryptionKey(UserSession::class)
-        );
-
-        // Prepare a request with session id cookie
-        $request = new ServerRequest('http://example.com/', 'GET', null, [], []);
-        $request = $request->withCookieParams(['fe_typo_user' => $uniqueSessionIdJwt]);
-
-        // Main session backend setup
-        $sessionBackendProphecy = $this->prophesize(SessionBackendInterface::class);
-        $sessionRecord = [
-            'ses_id' => $uniqueSessionId . self::NOT_CHECKED_INDICATOR,
-            'ses_data' => serialize(['foo' => 'bar']),
-            'ses_tstamp' => time(),
-            'ses_userid' => 0,
-            'ses_iplock' => '[DISABLED]',
-        ];
-        $sessionBackendProphecy->get($uniqueSessionId)->shouldBeCalled()->willReturn($sessionRecord);
-
-        $userSessionManager = new UserSessionManager(
-            $sessionBackendProphecy->reveal(),
-            86400,
-            new IpLocker(0, 0)
-        );
-        $userSessionManager->setLogger(new NullLogger());
-        $subject = new FrontendUserAuthentication();
-        $subject->setLogger(new NullLogger());
-        $subject->initializeUserSessionManager($userSessionManager);
-        $subject->start($request);
-
-        self::assertIsNotArray($subject->user);
-        self::assertEquals('bar', $subject->getSessionData('foo'));
-        self::assertEquals($uniqueSessionId, $subject->getSession()->getIdentifier());
-    }
-
-    /**
-     * @test
-     */
-    public function storeSessionDataOnAnonymousUserWithNoData(): void
-    {
-        $userSessionManager = $this->prophesize(UserSessionManager::class);
-        $userSessionManager->createFromRequestOrAnonymous(Argument::cetera())->willReturn(UserSession::createNonFixated('newSessionId'));
-        // Verify new session id is generated
-        $userSessionManager->createAnonymousSession()->willReturn(UserSession::createNonFixated('newSessionId'));
-        // set() and update() shouldn't be called since no session cookie is set
-        $userSessionManager->elevateToFixatedUserSession(Argument::cetera())->shouldNotBeCalled();
-        $userSessionManager->updateSession(Argument::cetera())->shouldNotBeCalled();
-
-        $subject = new FrontendUserAuthentication();
-        $subject->setLogger(new NullLogger());
-        $subject->initializeUserSessionManager($userSessionManager->reveal());
-        $subject->start($this->prophesize(ServerRequestInterface::class)->reveal());
-        $subject->storeSessionData();
-    }
 
     /**
      * Setting and immediately removing session data should be handled correctly.
@@ -135,7 +40,7 @@ class FrontendUserAuthenticationTest extends UnitTestCase
         $uniqueSessionId = StringUtility::getUniqueId('test');
 
         $sessionRecord = [
-            'ses_id' => $uniqueSessionId . self::NOT_CHECKED_INDICATOR,
+            'ses_id' => $uniqueSessionId . '--not-checked--',
             'ses_data' => serialize(['foo' => 'bar']),
             'ses_userid' => 0,
             'ses_iplock' => '[DISABLED]',
@@ -143,23 +48,23 @@ class FrontendUserAuthenticationTest extends UnitTestCase
         $userSession = UserSession::createFromRecord($sessionRecord['ses_id'], $sessionRecord);
 
         // Main session backend setup
-        $userSessionManager = $this->prophesize(UserSessionManager::class);
-        $userSessionManager->createFromRequestOrAnonymous(Argument::cetera())->willReturn($userSession);
+        $userSessionManagerMock = $this->createMock(UserSessionManager::class);
+        $userSessionManagerMock->method('createFromRequestOrAnonymous')->with(self::anything())->willReturn($userSession);
         // Verify new session id is generated
-        $userSessionManager->createAnonymousSession()->willReturn(UserSession::createNonFixated('newSessionId'));
+        $userSessionManagerMock->method('createAnonymousSession')->willReturn(UserSession::createNonFixated('newSessionId'));
         // set() and update() shouldn't be called since no session cookie is set
         // remove() should be called with given session id
-        $userSessionManager->isSessionPersisted(Argument::cetera())->shouldBeCalled()->willReturn(true);
-        $userSessionManager->removeSession(Argument::cetera())->shouldBeCalled();
+        $userSessionManagerMock->expects(self::once())->method('isSessionPersisted')->with(self::anything())->willReturn(true);
+        $userSessionManagerMock->expects(self::once())->method('removeSession')->with(self::anything());
 
         // set() and update() shouldn't be called since no session cookie is set
-        $userSessionManager->elevateToFixatedUserSession(Argument::cetera())->shouldNotBeCalled();
-        $userSessionManager->updateSession(Argument::cetera())->shouldNotBeCalled();
+        $userSessionManagerMock->expects(self::never())->method('elevateToFixatedUserSession')->with(self::anything());
+        $userSessionManagerMock->expects(self::never())->method('updateSession')->with(self::anything());
 
         $subject = new FrontendUserAuthentication();
-        $subject->initializeUserSessionManager($userSessionManager->reveal());
+        $subject->initializeUserSessionManager($userSessionManagerMock);
         $subject->setLogger(new NullLogger());
-        $subject->start($this->prophesize(ServerRequestInterface::class)->reveal());
+        $subject->start(new ServerRequest());
         $subject->setSessionData('foo', 'bar');
         $subject->removeSessionData();
         self::assertNull($subject->getSessionData('someKey'));
@@ -177,18 +82,18 @@ class FrontendUserAuthenticationTest extends UnitTestCase
 
         // Main session backend setup
         $userSession = UserSession::createNonFixated($uniqueSessionId);
-        $userSessionManager = $this->prophesize(UserSessionManager::class);
-        $userSessionManager->createFromRequestOrAnonymous(Argument::cetera())->willReturn($userSession);
-        $userSessionManager->createAnonymousSession(Argument::cetera())->willReturn($userSession);
+        $userSessionManagerMock = $this->createMock(UserSessionManager::class);
+        $userSessionManagerMock->method('createFromRequestOrAnonymous')->withAnyParameters()->willReturn($userSession);
+        $userSessionManagerMock->method('createAnonymousSession')->withAnyParameters()->willReturn($userSession);
         // Verify new session id is generated
         // set() and update() shouldn't be called since no session cookie is set
         // remove() should be called with given session id
-        $userSessionManager->isSessionPersisted(Argument::cetera())->shouldBeCalled()->willReturn(true);
-        $userSessionManager->removeSession(Argument::cetera())->shouldNotBeCalled();
+        $userSessionManagerMock->expects(self::once())->method('isSessionPersisted')->with(self::anything())->willReturn(true);
+        $userSessionManagerMock->expects(self::never())->method('removeSession')->with(self::anything());
 
         // set() and update() shouldn't be called since no session cookie is set
-        $userSessionManager->elevateToFixatedUserSession(Argument::cetera())->shouldNotBeCalled();
-        $userSessionManager->updateSession(Argument::cetera())->shouldBeCalled();
+        $userSessionManagerMock->expects(self::never())->method('elevateToFixatedUserSession')->with(self::anything());
+        $userSessionManagerMock->expects(self::once())->method('updateSession')->with(self::anything());
 
         // new session should be written
         $sessionRecord = [
@@ -201,12 +106,12 @@ class FrontendUserAuthenticationTest extends UnitTestCase
         ];
         $userSessionToBePersisted = UserSession::createFromRecord($uniqueSessionId, $sessionRecord, true);
         $userSessionToBePersisted->set('foo', 'bar');
-        $userSessionManager->updateSession($userSessionToBePersisted)->shouldBeCalled();
+        $userSessionManagerMock->expects(self::once())->method('updateSession')->with($userSessionToBePersisted);
 
         $subject = new FrontendUserAuthentication();
-        $subject->initializeUserSessionManager($userSessionManager->reveal());
+        $subject->initializeUserSessionManager($userSessionManagerMock);
         $subject->setLogger(new NullLogger());
-        $subject->start($this->prophesize(ServerRequestInterface::class)->reveal());
+        $subject->start(new ServerRequest());
         self::assertEmpty($subject->getSessionData($uniqueSessionId));
         self::assertEmpty($subject->user);
         $subject->setSessionData('foo', 'bar');
@@ -216,126 +121,5 @@ class FrontendUserAuthenticationTest extends UnitTestCase
         $prev = error_reporting(0);
         $subject->storeSessionData();
         error_reporting($prev);
-    }
-
-    /**
-     * Session data should be loaded when a session cookie is available and a user is authenticated
-     *
-     * @test
-     */
-    public function canLoadExistingAuthenticatedSession(): void
-    {
-        $uniqueSessionId = StringUtility::getUniqueId('test');
-        $currentTime = $GLOBALS['EXEC_TIME'];
-
-        // Main session backend setup
-        $sessionRecord = [
-            'ses_id' => $uniqueSessionId . self::NOT_CHECKED_INDICATOR,
-            'ses_userid' => 1,
-            'ses_iplock' => '[DISABLED]',
-            'ses_tstamp' => $currentTime,
-            'ses_data' => serialize(['foo' => 'bar']),
-            'ses_permanent' => 0,
-        ];
-        $userSession = UserSession::createFromRecord($uniqueSessionId, $sessionRecord);
-        $userSessionManager = $this->prophesize(UserSessionManager::class);
-        $userSessionManager->createAnonymousSession()->willReturn(UserSession::createNonFixated('not-in-use'));
-        $userSessionManager->createFromRequestOrAnonymous(Argument::cetera())->willReturn($userSession);
-        $userSessionManager->hasExpired($userSession)->willReturn(false);
-        $userSessionManager->updateSessionTimestamp($userSession)->shouldBeCalled()->willReturn($userSession);
-
-        // Mock call to fe_users table and let it return a valid user row
-        $connectionPoolFeUserProphecy = $this->prophesize(ConnectionPool::class);
-        GeneralUtility::addInstance(ConnectionPool::class, $connectionPoolFeUserProphecy->reveal());
-        $queryBuilderFeUserProphecy = $this->prophesize(QueryBuilder::class);
-        $queryBuilderFeUserProphecyRevelation = $queryBuilderFeUserProphecy->reveal();
-        $connectionPoolFeUserProphecy->getQueryBuilderForTable('fe_users')->willReturn($queryBuilderFeUserProphecyRevelation);
-        $queryBuilderFeUserProphecy->select('*')->willReturn($queryBuilderFeUserProphecyRevelation);
-        $queryBuilderFeUserProphecy->setRestrictions(Argument::cetera())->shouldBeCalled();
-        $queryBuilderFeUserProphecy->from('fe_users')->shouldBeCalled()->willReturn($queryBuilderFeUserProphecyRevelation);
-        $expressionBuilderFeUserProphecy = $this->prophesize(ExpressionBuilder::class);
-        $queryBuilderFeUserProphecy->expr()->willReturn($expressionBuilderFeUserProphecy->reveal());
-        $queryBuilderFeUserProphecy->createNamedParameter(Argument::cetera())->willReturnArgument(0);
-        $expressionBuilderFeUserProphecy->eq(Argument::cetera())->willReturn('1=1');
-        $queryBuilderFeUserProphecy->where(Argument::cetera())->shouldBeCalled()->willReturn($queryBuilderFeUserProphecyRevelation);
-        $statementFeUserProphecy = $this->prophesize(Result::class);
-        $queryBuilderFeUserProphecy->executeQuery()->shouldBeCalled()->willReturn($statementFeUserProphecy->reveal());
-        $statementFeUserProphecy->fetchAssociative()->willReturn(
-            [
-                'uid' => 1,
-                'username' => 'existingUserName',
-                'password' => 'abc',
-                'deleted' => 0,
-                'disabled' => 0,
-            ]
-        );
-
-        $subject = new FrontendUserAuthentication();
-        $subject->initializeUserSessionManager($userSessionManager->reveal());
-        $subject->setLogger(new NullLogger());
-        $subject->start($this->prophesize(ServerRequestInterface::class)->reveal());
-
-        self::assertNotNull($subject->user);
-        self::assertEquals('existingUserName', $subject->user['username']);
-    }
-
-    /**
-     * @test
-     */
-    public function canLogUserInWithoutAnonymousSession(): void
-    {
-        $GLOBALS['BE_USER'] = [];
-
-        GeneralUtility::setSingletonInstance(
-            EventDispatcherInterface::class,
-            $this->getMockBuilder(EventDispatcher::class)->disableOriginalConstructor()->getMock()
-        );
-
-        // provide request-token
-        $context = GeneralUtility::makeInstance(Context::class);
-        $securityAspect = SecurityAspect::provideIn($context);
-        $requestToken = RequestToken::create('core/user-auth/fe');
-        $securityAspect->setReceivedRequestToken($requestToken);
-
-        // Main session backend setup
-        $userSession = UserSession::createNonFixated('newSessionId');
-        $elevatedUserSession = UserSession::createFromRecord('newSessionId', ['ses_userid' => 1], true);
-        $userSessionManager = $this->prophesize(UserSessionManager::class);
-        $userSessionManager->createAnonymousSession()->willReturn(UserSession::createNonFixated('not-in-use'));
-        $userSessionManager->createFromRequestOrAnonymous(Argument::cetera())->willReturn($userSession);
-        $userSessionManager->removeSession($userSession)->shouldBeCalled();
-        $userSessionManager->elevateToFixatedUserSession(Argument::cetera())->shouldBeCalled()->willReturn($elevatedUserSession);
-
-        // Mock the login data and auth services here since fully prophesize this is a lot of hassle
-        $subject = $this->getAccessibleMock(
-            FrontendUserAuthentication::class,
-            [
-                'getLoginFormData',
-                'getAuthServices',
-                'updateLoginTimestamp',
-                'setSessionCookie',
-            ]
-        );
-        $subject->setLogger(new NullLogger());
-        $subject->initializeUserSessionManager($userSessionManager->reveal());
-
-        // Mock a login attempt
-        $subject->method('getLoginFormData')->willReturn([
-            'status' => 'login',
-            'uname' => 'existingUserName',
-            'uident' => 'abc',
-        ]);
-
-        $authServiceMock = $this->getMockBuilder(AuthenticationService::class)->getMock();
-        $authServiceMock->method('getUser')->willReturn([
-            'uid' => 1,
-            'username' => 'existingUserName',
-        ]);
-        // Auth services can return status codes: 0 (failed/abort), 100 (not responsible, continue), 200 (ok)
-        $authServiceMock->method('authUser')->willReturn(200);
-        // We need to wrap the array to something thats is \Traversable, in PHP 7.1 we can use traversable pseudo type instead
-        $subject->method('getAuthServices')->willReturn(new \ArrayIterator([$authServiceMock]));
-        $subject->start($this->prophesize(ServerRequestInterface::class)->reveal());
-        self::assertEquals('existingUserName', $subject->user['username'] ?? null);
     }
 }
