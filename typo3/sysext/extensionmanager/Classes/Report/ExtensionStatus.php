9503bfa135eb2c0e0a67f82da9c8a5d0e139f187
Oliver Bartsch <bo@cedev.de>
2022-12-16 13:27:12 +0100

    [TASK] Disable extension repository status check in composer mode
    The extension repository status check in the
    reports module is now skipped in composer mode.
    
    Resolves: #99385
    Releases: main, 11.5
    Change-Id: Ia3c049e1df5000045249a5308601bf2f75bf6ec9
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/77178
    Reviewed-by: Oliver Hader <oliver.hader@typo3.org>
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Oliver Bartsch <bo@cedev.de>
    Reviewed-by: Oliver Bartsch <bo@cedev.de>
    Tested-by: Frank Nägler <frank.naegler@typo3.com>
    Reviewed-by: Frank Nägler <frank.naegler@typo3.com>
    Tested-by: Oliver Hader <oliver.hader@typo3.org>

--- typo3/sysext/extensionmanager/Classes/Report/ExtensionStatus.php
+++ typo3/sysext/extensionmanager/Classes/Report/ExtensionStatus.php
@@ -15,6 +15,7 @@
 
 namespace TYPO3\CMS\Extensionmanager\Report;
 
+use TYPO3\CMS\Core\Core\Environment;
 use TYPO3\CMS\Core\Localization\LanguageService;
 use TYPO3\CMS\Core\Localization\LanguageServiceFactory;
 use TYPO3\CMS\Core\Type\ContextualFeedbackSeverity;
@@ -53,7 +54,10 @@ class ExtensionStatus implements StatusProviderInterface
     public function getStatus(): array
     {
         $status = [];
-        $status['mainRepositoryStatus'] = $this->getMainRepositoryStatus();
+
+        if (!Environment::isComposerMode()) {
+            $status['mainRepositoryStatus'] = $this->getMainRepositoryStatus();
+        }
 
         $extensionStatus = $this->getSecurityStatusOfExtensions();
         $status['extensionsSecurityStatusInstalled'] = $extensionStatus->loaded ?? [];
