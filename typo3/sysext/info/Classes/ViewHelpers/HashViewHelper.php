d1c3f0fe74051b90e9a463d4d1183397db7f709c
Jasmina Ließmann <minapokhalo+typo3@gmail.com>
2022-12-28 10:43:01 +0100

    [TASK] Make list tree collapsible in 'TSconfig page' submodule
    To reduce the size of the list and improve clarity, the
    individual list entries can now be expanded and collapsed.
    
    Resolves: #99577
    Releases: main
    Change-Id: I0703955f6ec171b69b371c4b08aa76e2d2a12cc9
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/77453
    Tested-by: Anja Leichsenring <aleichsenring@ab-softlab.de>
    Tested-by: core-ci <typo3@b13.com>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Reviewed-by: Anja Leichsenring <aleichsenring@ab-softlab.de>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>

---
+++ typo3/sysext/info/Classes/ViewHelpers/HashViewHelper.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3\CMS\Info\ViewHelpers;
+
+use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+
+/**
+ * Creates a hash from the given string
+ *
+ * @internal
+ */
+final class HashViewHelper extends AbstractViewHelper
+{
+    public function initializeArguments(): void
+    {
+        $this->registerArgument('value', 'string', 'The string to be hashed', true);
+    }
+
+    /**
+     * @param array{value: string} $arguments
+     */
+    public static function renderStatic(array $arguments, \Closure $renderChildrenClosure, RenderingContextInterface $renderingContext): string
+    {
+        return hash('xxh3', $arguments['value']);
+    }
+}
