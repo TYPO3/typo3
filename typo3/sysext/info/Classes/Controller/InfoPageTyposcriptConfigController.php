4228166f705b66ed6a9b37d303d6934856e7ccb8
Anja Leichsenring <aleichsenring@ab-softlab.de>
2023-01-18 11:32:57 +0100

    [TASK] Resolve inheritance of InfoPageTsController
    The inheritance gets in the way of further plans to modernize the
    info module and therefor needs to go. No functional changes have
    been made to the class up to this point.
    
    Resolves: #99598
    Releases: main
    Change-Id: If84b8a2529f5934bcf3a2651a304b3655dfbd3e2
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/77465
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: core-ci <typo3@b13.com>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>

--- typo3/sysext/info/Classes/Controller/InfoPageTyposcriptConfigController.php
+++ typo3/sysext/info/Classes/Controller/InfoPageTyposcriptConfigController.php
@@ -17,14 +17,28 @@ declare(strict_types=1);
 
 namespace TYPO3\CMS\Info\Controller;
 
+use Psr\EventDispatcher\EventDispatcherInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use TYPO3\CMS\Backend\Module\ModuleInterface;
+use TYPO3\CMS\Backend\Module\ModuleProvider;
+use TYPO3\CMS\Backend\Routing\PreviewUriBuilder;
+use TYPO3\CMS\Backend\Routing\UriBuilder;
+use TYPO3\CMS\Backend\Template\Components\ButtonBar;
+use TYPO3\CMS\Backend\Template\ModuleTemplate;
+use TYPO3\CMS\Backend\Template\ModuleTemplateFactory;
 use TYPO3\CMS\Backend\Utility\BackendUtility;
+use TYPO3\CMS\Core\Authentication\BackendUserAuthentication;
 use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Database\Query\Restriction\DeletedRestriction;
 use TYPO3\CMS\Core\Database\Query\Restriction\WorkspaceRestriction;
+use TYPO3\CMS\Core\Domain\Repository\PageRepository;
 use TYPO3\CMS\Core\Imaging\Icon;
+use TYPO3\CMS\Core\Imaging\IconFactory;
+use TYPO3\CMS\Core\Localization\LanguageService;
 use TYPO3\CMS\Core\Page\JavaScriptModuleInstruction;
+use TYPO3\CMS\Core\Page\PageRenderer;
+use TYPO3\CMS\Core\Type\Bitmask\Permission;
 use TYPO3\CMS\Core\TypoScript\IncludeTree\TsConfigTreeBuilder;
 use TYPO3\CMS\Core\TypoScript\Tokenizer\Line\LineStream;
 use TYPO3\CMS\Core\TypoScript\Tokenizer\LosslessTokenizer;
@@ -32,17 +46,71 @@ use TYPO3\CMS\Core\Utility\ArrayUtility;
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Core\Utility\MathUtility;
+use TYPO3\CMS\Info\Controller\Event\ModifyInfoModuleContentEvent;
 
 /**
  * Page TSconfig viewer in Web -> Info
  * @internal This class is a specific Backend controller implementation and is not part of the TYPO3's Core API.
  */
-class InfoPageTyposcriptConfigController extends InfoModuleController
+class InfoPageTyposcriptConfigController
 {
+    protected ModuleInterface $currentModule;
+    protected ?ModuleTemplate $view;
+    public array $pageinfo = [];
+
+    /**
+     * Value of the GET/POST var 'id' = the current page ID
+     */
+    protected int $id;
+
+    public function __construct(
+        protected readonly IconFactory $iconFactory,
+        protected readonly UriBuilder $uriBuilder,
+        protected readonly ModuleProvider $moduleProvider,
+        protected readonly PageRenderer $pageRenderer,
+        protected readonly ModuleTemplateFactory $moduleTemplateFactory,
+        protected readonly EventDispatcherInterface $eventDispatcher,
+    ) {
+    }
+
     public function handleRequest(ServerRequestInterface $request): ResponseInterface
     {
         $this->getLanguageService()->includeLLFile('EXT:info/Resources/Private/Language/InfoPageTsConfig.xlf');
-        $this->init($request);
+        $this->id = (int)($request->getQueryParams()['id'] ?? $request->getParsedBody()['id'] ?? 0);
+        $this->view = $this->moduleTemplateFactory->create($request);
+        $this->getLanguageService()->includeLLFile('EXT:info/Resources/Private/Language/locallang_mod_web_info.xlf');
+        $this->currentModule = $request->getAttribute('module');
+        $this->pageinfo = BackendUtility::readPageAccess($this->id, $this->getBackendUser()->getPagePermsClause(Permission::PAGE_SHOW)) ?: [];
+        $this->view->setTitle(
+            $this->getLanguageService()->sL($this->currentModule->getTitle()),
+            $this->id !== 0 && isset($this->pageinfo['title']) ? $this->pageinfo['title'] : ''
+        );
+        // The page will show only if there is a valid page and if this page
+        // may be viewed by the user
+        if ($this->pageinfo !== []) {
+            $this->view->getDocHeaderComponent()->setMetaInformation($this->pageinfo);
+        }
+        $accessContent = false;
+        $backendUser = $this->getBackendUser();
+        if (($this->id && $this->pageinfo !== []) || ($backendUser->isAdmin() && !$this->id)) {
+            $accessContent = true;
+            if (!$this->id && $backendUser->isAdmin()) {
+                $this->pageinfo = ['title' => '[root-level]', 'uid' => 0, 'pid' => 0];
+            }
+            $this->view->assign('id', $this->id);
+            $this->view->assign('formAction', (string)$this->uriBuilder->buildUriFromRoute($this->currentModule->getIdentifier()));
+            // Setting up the buttons and the module menu for the doc header
+            $this->getButtons();
+            $this->view->makeDocHeaderModuleMenu(['id' => $this->id]);
+        }
+        $event = $this->eventDispatcher->dispatch(
+            new ModifyInfoModuleContentEvent($accessContent, $request, $this->currentModule, $this->view)
+        );
+        $this->view->assignMultiple([
+            'accessContent' => $accessContent,
+            'headerContent' => $event->getHeaderContent(),
+            'footerContent' => $event->getFooterContent(),
+        ]);
 
         $backendUser = $this->getBackendUser();
         $moduleData = $request->getAttribute('moduleData');
@@ -325,5 +393,63 @@ class InfoPageTyposcriptConfigController extends InfoModuleController
         return '<textarea class="form-control" rows="' . count(explode(LF, $content)) . '" disabled>'
             . htmlspecialchars($content)
             . '</textarea>';
+    }
+
+    /**
+     * Create the panel of buttons for submitting the form or otherwise perform operations.
+     */
+    protected function getButtons(): void
+    {
+        $languageService = $this->getLanguageService();
+        $buttonBar = $this->view->getDocHeaderComponent()->getButtonBar();
+
+        if ($this->id) {
+            // View
+            $pagesTSconfig = BackendUtility::getPagesTSconfig($this->pageinfo['uid']);
+            if (isset($pagesTSconfig['TCEMAIN.']['preview.']['disableButtonForDokType'])) {
+                $excludeDokTypes = GeneralUtility::intExplode(
+                    ',',
+                    (string)$pagesTSconfig['TCEMAIN.']['preview.']['disableButtonForDokType'],
+                    true
+                );
+            } else {
+                // exclude sysfolders and recycler by default
+                $excludeDokTypes = [
+                    PageRepository::DOKTYPE_RECYCLER,
+                    PageRepository::DOKTYPE_SYSFOLDER,
+                    PageRepository::DOKTYPE_SPACER,
+                ];
+            }
+            if (!in_array((int)$this->pageinfo['doktype'], $excludeDokTypes, true)) {
+                // View page
+                $previewDataAttributes = PreviewUriBuilder::create((int)$this->pageinfo['uid'])
+                    ->withRootLine(BackendUtility::BEgetRootLine($this->pageinfo['uid']))
+                    ->buildDispatcherDataAttributes();
+                $viewButton = $buttonBar->makeLinkButton()
+                    ->setHref('#')
+                    ->setDataAttributes($previewDataAttributes ?? [])
+                    ->setShowLabelText(true)
+                    ->setTitle($languageService->sL('LLL:EXT:core/Resources/Private/Language/locallang_core.xlf:labels.showPage'))
+                    ->setIcon($this->iconFactory->getIcon('actions-view-page', Icon::SIZE_SMALL));
+                $buttonBar->addButton($viewButton, ButtonBar::BUTTON_POSITION_LEFT);
+            }
+        }
+
+        // Shortcut
+        $shortcutButton = $buttonBar->makeShortcutButton()
+            ->setRouteIdentifier($this->currentModule->getIdentifier())
+            ->setDisplayName($this->currentModule->getTitle())
+            ->setArguments(['id' => $this->id]);
+        $buttonBar->addButton($shortcutButton, ButtonBar::BUTTON_POSITION_RIGHT);
+    }
+
+    protected function getLanguageService(): LanguageService
+    {
+        return $GLOBALS['LANG'];
+    }
+
+    protected function getBackendUser(): BackendUserAuthentication
+    {
+        return $GLOBALS['BE_USER'];
     }
 }
