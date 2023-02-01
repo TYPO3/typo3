ecd2cba72c9388793efee9f412b5e8c25ecbd2c0
Chris Müller <typo3@krue.ml>
2023-02-01 17:46:31 +0100

    [TASK] Enhance documentation of CountrySelectViewHelper
    Related: #99618
    Resolves: #99788
    Releases: main
    Change-Id: I826a40e306a77403dd06270eb83f5131920c219d
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/77661
    Tested-by: Stefan Bürk <stefan@buerk.tech>
    Tested-by: core-ci <typo3@b13.com>
    Reviewed-by: Stefan Bürk <stefan@buerk.tech>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>

--- typo3/sysext/fluid/Classes/ViewHelpers/Form/CountrySelectViewHelper.php
+++ typo3/sysext/fluid/Classes/ViewHelpers/Form/CountrySelectViewHelper.php
@@ -23,12 +23,15 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Utility\LocalizationUtility;
 
 /**
- * Renders an :html:`<select>` tag with all available countries as options.
+ * Renders a :html:`<select>` tag with all available countries as options.
  *
  * Examples
  * ========
  *
- * Example::
+ * Basic usage
+ * -----------
+ *
+ * ::
  *
  *    <f:form.countrySelect name="country" value="{defaultCountry}" />
  *
@@ -38,7 +41,39 @@ use TYPO3\CMS\Extbase\Utility\LocalizationUtility;
  *      <option value="BE">Belgium</option>
  *      <option value="FR">France</option>
  *      ....
- *     </select>
+ *    </select>
+ *
+ * Prioritize countries
+ * --------------------
+ *
+ * Define a list of countries which should be listed as first options in the
+ * form element::
+ *
+ *    <f:form.countrySelect
+ *      name="country"
+ *      value="AT"
+ *      prioritizedCountries="{0: 'DE', 1: 'AT', 2: 'CH'}"
+ *    />
+ *
+ *  Additionally, Austria is pre-selected.
+ *
+ * Display another language
+ * ------------------------
+ *
+ * A combination of optionLabelField and alternativeLanguage is possible. For
+ * instance, if you want to show the localized official names but not in your
+ * default language but in French. You can achieve this by using the following
+ * combination::
+ *
+ *    <f:form.countrySelect
+ *      name="country"
+ *      optionLabelField="localizedOfficialName"
+ *      alternativeLanguage="fr"
+ *      sortByOptionLabel="true"
+ *    />
+ *
+ * Bind an object
+ * --------------
  *
  * You can also use the "property" attribute if you have bound an object to the form.
  * See :ref:`<f:form> <typo3-fluid-form>` for more documentation.
