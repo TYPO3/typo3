084335fe0de96e413d94ef529bd385a163f2a3ea
Andreas Fernandez <a.fernandez@scripting-base.de>
2023-02-26 15:32:41 +0100

    [BUGFIX] Avoid useless database updates in DataHandler
    DataHandler has an internal check that compares the sent payload with
    the state of the affected database record and removes fields from the
    payload that didn't change.
    
    Everytime a database record is updated, DataHandler resets the internal
    stage to "0" to start a new editing workflow, if the affected database
    table supports workspaces. This handling is unfortunately executed
    *after* the payload comparison is done, leading to a payload that
    always instructs to update the field `t3ver_stage`, even if the change
    is not necessary, leading to superfluous database updates. This has a
    massive negative performance impact in simple shared hosting
    environments, especially with tt_content records that have many
    file references attached.
    
    The payload comparison is now executed after setting `t3ver_stage` to
    filter out superfluous updates.
    
    Resolves: #100035
    Releases: main, 11.5
    Change-Id: Ie0e29f7efe8d6289849bf117fbc852108fac1dd7
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/77948
    Reviewed-by: Georg Ringer <georg.ringer@gmail.com>
    Tested-by: Andreas Fernandez <a.fernandez@scripting-base.de>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Georg Ringer <georg.ringer@gmail.com>
    Reviewed-by: Benni Mack <benni@typo3.org>
    Tested-by: core-ci <typo3@b13.com>
    Reviewed-by: Markus Klein <markus.klein@typo3.org>
    Reviewed-by: Andreas Fernandez <a.fernandez@scripting-base.de>
    Tested-by: Markus Klein <markus.klein@typo3.org>

--- typo3/sysext/core/Classes/DataHandling/DataHandler.php
+++ typo3/sysext/core/Classes/DataHandling/DataHandler.php
@@ -1066,16 +1066,17 @@ class DataHandler implements LoggerAwareInterface
                     if ($GLOBALS['TCA'][$table]['ctrl']['crdate'] ?? false) {
                         $fieldArray[$GLOBALS['TCA'][$table]['ctrl']['crdate']] = $GLOBALS['EXEC_TIME'];
                     }
-                } elseif ($this->checkSimilar) {
+                }
+                // Set stage to "Editing" to make sure we restart the workflow
+                if (BackendUtility::isTableWorkspaceEnabled($table)) {
+                    $fieldArray['t3ver_stage'] = 0;
+                }
+                if ($status !== 'new' && $this->checkSimilar) {
                     // Removing fields which are equal to the current value:
                     $fieldArray = $this->compareFieldArrayWithCurrentAndUnset($table, $id, $fieldArray);
                 }
                 if (($GLOBALS['TCA'][$table]['ctrl']['tstamp'] ?? false) && !empty($fieldArray)) {
                     $fieldArray[$GLOBALS['TCA'][$table]['ctrl']['tstamp']] = $GLOBALS['EXEC_TIME'];
-                }
-                // Set stage to "Editing" to make sure we restart the workflow
-                if (BackendUtility::isTableWorkspaceEnabled($table)) {
-                    $fieldArray['t3ver_stage'] = 0;
                 }
                 // Hook: processDatamap_postProcessFieldArray
                 foreach ($hookObjectsArr as $hookObj) {
