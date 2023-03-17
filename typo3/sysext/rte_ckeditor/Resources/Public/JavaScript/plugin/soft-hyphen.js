78a5a4f2eb421680af750e8f7e13b9e10a7cba68
Andreas Fernandez <a.fernandez@scripting-base.de>
2023-03-17 17:03:31 +0100

    [TASK] Upgrade TypeScript to 5.0
    TypeScript 5.0 has been release a few days ago and brings promising
    features, see [1]. This patch therefore updates TypeScript to 5.0
    with the following commands executed:
    
        npm i --save-dev typescript@~5.0.0
        npm run build
    
    [1] https://devblogs.microsoft.com/typescript/announcing-typescript-5-0/
    
    Resolves: #100209
    Releases: main, 11.5
    Change-Id: I19df4f073f3cdeeed2023b57cad647c5a53638a0
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/78157
    Reviewed-by: Andreas Fernandez <a.fernandez@scripting-base.de>
    Tested-by: core-ci <typo3@b13.com>
    Reviewed-by: Benni Mack <benni@typo3.org>
    Tested-by: Benni Mack <benni@typo3.org>
    Reviewed-by: Markus Klein <markus.klein@typo3.org>
    Tested-by: Markus Klein <markus.klein@typo3.org>
    Tested-by: Andreas Fernandez <a.fernandez@scripting-base.de>

--- typo3/sysext/rte_ckeditor/Resources/Public/JavaScript/plugin/soft-hyphen.js
+++ typo3/sysext/rte_ckeditor/Resources/Public/JavaScript/plugin/soft-hyphen.js
@@ -10,4 +10,4 @@
  *
  * The TYPO3 project - inspiring people to share!
  */
-import{Core}from"@typo3/ckeditor5-bundle.js";import Whitespace from"@typo3/rte-ckeditor/plugin/whitespace.js";export default class SoftHyphen extends Core.Plugin{init(){console.warn("The TYPO3 CKEditor5 SoftHyphen plugin is deprecated and will be removed with v13. Please use the Whitespace plugin instead.")}}SoftHyphen.pluginName="SoftHyphen",SoftHyphen.requires=[Whitespace];
+import{Core}from"@typo3/ckeditor5-bundle.js";import Whitespace from"@typo3/rte-ckeditor/plugin/whitespace.js";class SoftHyphen extends Core.Plugin{init(){console.warn("The TYPO3 CKEditor5 SoftHyphen plugin is deprecated and will be removed with v13. Please use the Whitespace plugin instead.")}}SoftHyphen.pluginName="SoftHyphen",SoftHyphen.requires=[Whitespace];export default SoftHyphen;
