4e36a5003d9c97342989be3326507fc779a1aab7
Peter Kraume <peter.kraume@gmx.de>
2023-03-28 18:10:38 +0200

    [TASK] Change order of backend modules in Site Management
    The redirects backend module should have precedence before the
    Page TSConfig and the TypoScript module.
    
    Resolves: #100340
    Releases: main
    
    Change-Id: I7f0c56c59b9f7b6ac3d850087b6764a7ac1eb3be
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/78309
    Reviewed-by: Andreas Fernandez <a.fernandez@scripting-base.de>
    Tested-by: core-ci <typo3@b13.com>
    Reviewed-by: Benni Mack <benni@typo3.org>
    Tested-by: Benni Mack <benni@typo3.org>
    Tested-by: Andreas Fernandez <a.fernandez@scripting-base.de>

--- typo3/sysext/redirects/Configuration/Backend/Modules.php
+++ typo3/sysext/redirects/Configuration/Backend/Modules.php
@@ -8,6 +8,7 @@ use TYPO3\CMS\Redirects\Controller\ManagementController;
 return [
     'site_redirects' => [
         'parent' => 'site',
+        'position' => ['after' => 'site_configuration'],
         'access' => 'user',
         'path' => '/module/site/redirects',
         'iconIdentifier' => 'module-redirects',
