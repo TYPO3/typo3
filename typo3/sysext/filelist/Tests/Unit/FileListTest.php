802504d6d58a11336b7fda4a9e704ff33dd3c4af
Christian Kuhn <lolli@schwarzbu.ch>
2023-04-02 11:04:37 +0200

    [TASK] Split a non-static data provider
    Splitting the data provider that uses $this
    to create mocks into single tests is the
    easiest solution, even though it duplicates
    code a bit.
    
    Resolves: #100384
    Related: #100249
    Related: #100263
    Releases: main
    Change-Id: I0f3cbae44ca578ce0147f37bc155e75bb7638242
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/78359
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Oliver Klee <typo3-coding@oliverklee.de>
    Reviewed-by: Oliver Klee <typo3-coding@oliverklee.de>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Stefan Bürk <stefan@buerk.tech>
    Reviewed-by: Stefan Bürk <stefan@buerk.tech>

--- typo3/sysext/filelist/Tests/Unit/FileListTest.php
+++ typo3/sysext/filelist/Tests/Unit/FileListTest.php
@@ -17,7 +17,6 @@ declare(strict_types=1);
 
 namespace TYPO3\CMS\Filelist\Tests\Unit;
 
-use Generator;
 use TYPO3\CMS\Core\Localization\LanguageService;
 use TYPO3\CMS\Core\Localization\Locale;
 use TYPO3\CMS\Core\Resource\File;
@@ -28,23 +27,13 @@ use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
 final class FileListTest extends UnitTestCase
 {
     /**
-     * @dataProvider sortResourcesDataProvider
      * @test
      */
-    public function sortDescendingResourcesByGivenField(array $resources, string $sortField, array $expectedResources): void
+    public function sortResourcesByNameSortsCorrectly(): void
     {
         $languageServiceMock = $this->createMock(LanguageService::class);
         $languageServiceMock->method('getLocale')->willReturn(new Locale('pl'));
 
-        $fileList = $this->getAccessibleMock(FileList::class, ['getLanguageService'], [], '', false);
-        $fileList->method('getLanguageService')->willReturn($languageServiceMock);
-
-        $sortedResources = $fileList->_call('sortResources', $resources, $sortField);
-        self::assertSame($expectedResources, $sortedResources);
-    }
-
-    public function sortResourcesDataProvider(): Generator
-    {
         $file1 = $this->createMock(File::class);
         $file1->method('hasProperty')->with('name')->willReturn(true);
         $file1->method('getProperty')->with('name')->willReturn('file1.jpg');
@@ -72,7 +61,54 @@ final class FileListTest extends UnitTestCase
         $folder2 = $this->createMock(Folder::class);
         $folder2->method('getName')->willReturn('folder2');
 
-        yield 'sort by name' => [[$file2, $folder1, $folder2, $file1, $file3, $file4], 'name', [$file4, $file3, $file2, $file1, $folder2, $folder1]];
-        yield 'sort by extension' => [[$file2, $folder1, $folder2, $file1, $file3, $file4], 'fileext', [$file3, $file2, $file1, $file4, $folder2, $folder1]];
+        $resources = [$file2, $folder1, $folder2, $file1, $file3, $file4];
+        $expected = [$file4, $file3, $file2, $file1, $folder2, $folder1];
+
+        $fileList = $this->getAccessibleMock(FileList::class, ['getLanguageService'], [], '', false);
+        $fileList->method('getLanguageService')->willReturn($languageServiceMock);
+        self::assertSame($expected, $fileList->_call('sortResources', $resources, 'name'));
+    }
+
+    /**
+     * @test
+     */
+    public function sortResourcesByFileextNameSortsCorrectly(): void
+    {
+        $languageServiceMock = $this->createMock(LanguageService::class);
+        $languageServiceMock->method('getLocale')->willReturn(new Locale('pl'));
+
+        $file1 = $this->createMock(File::class);
+        $file1->method('hasProperty')->with('name')->willReturn(true);
+        $file1->method('getProperty')->with('name')->willReturn('file1.jpg');
+        $file1->method('getExtension')->willReturn('jpg');
+
+        $file2 = $this->createMock(File::class);
+        $file2->method('getProperty')->with('name')->willReturn('fileA.png');
+        $file2->method('hasProperty')->with('name')->willReturn(true);
+        $file2->method('getExtension')->willReturn('png');
+
+        //this file has an accented letter Ą, so it should be sorted between fileA and fileB
+        $file3 = $this->createMock(File::class);
+        $file3->method('getProperty')->with('name')->willReturn('fileĄ.png');
+        $file3->method('hasProperty')->with('name')->willReturn(true);
+        $file3->method('getExtension')->willReturn('png');
+
+        $file4 = $this->createMock(File::class);
+        $file4->method('getProperty')->with('name')->willReturn('fileB.aif');
+        $file4->method('hasProperty')->with('name')->willReturn(true);
+        $file4->method('getExtension')->willReturn('aif');
+
+        $folder1 = $this->createMock(Folder::class);
+        $folder1->method('getName')->willReturn('folder1');
+
+        $folder2 = $this->createMock(Folder::class);
+        $folder2->method('getName')->willReturn('folder2');
+
+        $resources = [$file2, $folder1, $folder2, $file1, $file3, $file4];
+        $expected = [$file3, $file2, $file1, $file4, $folder2, $folder1];
+
+        $fileList = $this->getAccessibleMock(FileList::class, ['getLanguageService'], [], '', false);
+        $fileList->method('getLanguageService')->willReturn($languageServiceMock);
+        self::assertSame($expected, $fileList->_call('sortResources', $resources, 'fileext'));
     }
 }
