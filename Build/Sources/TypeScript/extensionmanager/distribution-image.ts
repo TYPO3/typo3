1c34a1e25c847a2d5febfa0332d5d938e0c8f2d8
Andreas Fernandez <a.fernandez@scripting-base.de>
2023-04-06 12:18:05 +0200

    [TASK] Migrate `typo3-extensionmanager-distribution-image` to Lit
    To get rid of much boilerplate code, the custom component
    `typo3-extensionmanager-distribution-image` used to render distribution
    images in EXT:extensionmanager is migrated to Lit.
    
    Resolves: #100508
    Releases: main
    Change-Id: I304f666b9a1ff2598c8d7adb705a9f113fb7e2f0
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/78515
    Tested-by: core-ci <typo3@b13.com>
    Reviewed-by: Oliver Hader <oliver.hader@typo3.org>
    Tested-by: Oliver Hader <oliver.hader@typo3.org>
    Reviewed-by: Benni Mack <benni@typo3.org>
    Tested-by: Benni Mack <benni@typo3.org>

--- Build/Sources/TypeScript/extensionmanager/distribution-image.ts
+++ Build/Sources/TypeScript/extensionmanager/distribution-image.ts
@@ -11,78 +11,45 @@
  * The TYPO3 project - inspiring people to share!
  */
 
+import { css, html, LitElement, nothing, TemplateResult } from 'lit';
+import { customElement, property } from 'lit/decorators';
+
 /**
  * Module @typo3/extensionmanager/distribution-image
  *
  * @example
  * <typo3-extensionmanager-distribution-image image="some/image.jpg" fallback="/some/fallback/image.jpg"/>
- *
- * This is based on W3C custom elements ("web components") specification, see
- * https://developer.mozilla.org/en-US/docs/Web/Web_Components/Using_custom_elements
  */
-export class DistributionImage extends HTMLElement {
-  private image: string;
-  private welcomeImage: string;
-  private fallback: string;
-  private imageElement: HTMLImageElement;
+@customElement('typo3-extensionmanager-distribution-image')
+export class DistributionImage extends LitElement {
+  static styles = css`
+    img {
+      display: block;
+      width: 100%;
+      height: auto;
+    }
+  `;
 
-  public connectedCallback(): void {
-    this.image = this.getAttribute('image') || '' as string;
-    this.welcomeImage = this.getAttribute('welcomeImage') || '' as string;
-    this.fallback = this.getAttribute('fallback') || '' as string;
+  @property({ type: String }) alt: string;
+  @property({ type: String }) image: string;
+  @property({ type: String }) welcomeImage: string;
+  @property({ type: String }) fallback: string;
 
-    if (!this.image.length && !this.fallback.length) {
-      return;
+  protected render(): TemplateResult|symbol {
+    if (!this.image && !this.fallback) {
+      return nothing;
     }
 
-    this.attachShadow({ mode: 'open' });
-    this.imageElement = document.createElement('img');
-
-    const alt: string = this.getAttribute('alt') || '';
-    if (alt.length) {
-      this.imageElement.setAttribute('alt', alt);
-    }
-
-    const title: string = this.getAttribute('title') || '';
-    if (title.length) {
-      this.imageElement.setAttribute('title', title);
-    }
-
-    if (this.welcomeImage.length) {
-      this.imageElement.addEventListener('error', this.onError);
-      this.imageElement.setAttribute('src', this.welcomeImage);
-    } else if (this.image.length) {
-      this.imageElement.addEventListener('error', this.onError);
-      this.imageElement.setAttribute('src', this.image);
-    } else {
-      this.imageElement.setAttribute('src', this.fallback);
-    }
-
-    const style: HTMLStyleElement = document.createElement('style');
-    style.textContent = `
-      img {
-        display: block;
-        width: 300px;
-        height: auto;
-      }
-    `;
-
-    this.shadowRoot.append(this.imageElement, style);
+    const imageToUse = this.welcomeImage || this.image || this.fallback;
+    return html`<img alt="${this.alt}" src="${imageToUse}" @error="${imageToUse !== this.fallback ? this.onError : nothing}">`;
   }
 
-  public disconnectedCallback(): void {
-    if (this.image.length && this.imageElement !== null) {
-      this.imageElement.removeEventListener('error', this.onError);
-    }
-  }
-
-  private onError = () => {
-    if (this.image.length && this.imageElement.getAttribute('src') === this.welcomeImage) {
-      this.imageElement.setAttribute('src', this.image);
+  private onError(e: Event): void {
+    const imageElement = e.target as HTMLImageElement;
+    if (this.image.length && imageElement.getAttribute('src') === this.welcomeImage) {
+      imageElement.setAttribute('src', this.image);
     } else if (this.fallback.length) {
-      this.imageElement.setAttribute('src', this.fallback);
+      imageElement.setAttribute('src', this.fallback);
     }
-  };
+  }
 }
-
-window.customElements.define('typo3-extensionmanager-distribution-image', DistributionImage);
