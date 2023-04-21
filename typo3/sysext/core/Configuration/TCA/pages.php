0f49281a93407621cbe9ff60a746a7ced79af476
Christian Eßl <indy.essl@gmail.com>
2023-04-21 20:39:49 +0200

    [TASK] Remove PseudoSiteTcaDisplayCondition
    The tca display condition for checking pseudo sites no longer
    serves a purpose.
    
    Resolves: #100711
    Releases: main
    Change-Id: I08c0ff2e2eec04a19a42ae2ceee6387b65d82fb4
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/78791
    Tested-by: Oliver Klee <typo3-coding@oliverklee.de>
    Reviewed-by: Oliver Klee <typo3-coding@oliverklee.de>
    Reviewed-by: Benni Mack <benni@typo3.org>
    Tested-by: Benni Mack <benni@typo3.org>
    Tested-by: core-ci <typo3@b13.com>
    Reviewed-by: Georg Ringer <georg.ringer@gmail.com>
    Tested-by: Georg Ringer <georg.ringer@gmail.com>

--- typo3/sysext/core/Configuration/TCA/pages.php
+++ typo3/sysext/core/Configuration/TCA/pages.php
@@ -161,7 +161,6 @@ return [
         ],
         'slug' => [
             'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_tca.xlf:pages.slug',
-            'displayCond' => 'USER:' . \TYPO3\CMS\Core\Compatibility\PseudoSiteTcaDisplayCondition::class . '->isInPseudoSite:pages:false',
             'config' => [
                 'type' => 'slug',
                 'size' => 50,
