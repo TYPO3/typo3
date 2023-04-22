d13e6f863d9398d4889e31c8094048655f00ca21
Oliver Hader <oliver@typo3.org>
2023-04-22 07:42:12 +0200

    [TASK] Let CSP Policy tests be functional only
    The test subject got more and more dependencies to other
    concepts and cannot be tested as standalone unit anymore.
    
    Resolves: #100712
    Releases: main
    Change-Id: I9ac19981c436a73ea374dd1e082de16b956dc641
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/78792
    Reviewed-by: Georg Ringer <georg.ringer@gmail.com>
    Tested-by: Oliver Hader <oliver.hader@typo3.org>
    Tested-by: Georg Ringer <georg.ringer@gmail.com>
    Reviewed-by: Oliver Hader <oliver.hader@typo3.org>
    Tested-by: core-ci <typo3@b13.com>

--- typo3/sysext/core/Tests/Functional/Security/ContentSecurityPolicy/PolicyTest.php
+++ typo3/sysext/core/Tests/Functional/Security/ContentSecurityPolicy/PolicyTest.php
@@ -19,14 +19,14 @@ namespace TYPO3\CMS\Core\Tests\Functional\Security\ContentSecurityPolicy;
 
 use TYPO3\CMS\Core\Security\ContentSecurityPolicy\Directive;
 use TYPO3\CMS\Core\Security\ContentSecurityPolicy\HashProxy;
+use TYPO3\CMS\Core\Security\ContentSecurityPolicy\HashValue;
 use TYPO3\CMS\Core\Security\ContentSecurityPolicy\Policy;
+use TYPO3\CMS\Core\Security\ContentSecurityPolicy\SourceKeyword;
+use TYPO3\CMS\Core\Security\ContentSecurityPolicy\SourceScheme;
+use TYPO3\CMS\Core\Security\ContentSecurityPolicy\UriValue;
 use TYPO3\CMS\Core\Security\Nonce;
 use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
 
-/**
- * In contrast to the other `PolicyTest` (unit-test), these
- * tests rely on platform information like, package states.
- */
 final class PolicyTest extends FunctionalTestCase
 {
     private Nonce $nonce;
@@ -41,7 +41,7 @@ final class PolicyTest extends FunctionalTestCase
     /**
      * @test
      */
-    public function hashValueIsCompiled(): void
+    public function hashProxyIsCompiled(): void
     {
         // ```
         // cat typo3/sysext/core/Tests/Unit/Security/ContentSecurityPolicy/Fixtures/app-fixture.js \
@@ -53,5 +53,178 @@ final class PolicyTest extends FunctionalTestCase
         );
         $policy = (new Policy())->extend(Directive::ScriptSrc, $hashProxy);
         self::assertSame("script-src 'sha256-dawsv3oUbEz6NVoOxXFAu0k7W3I/PS6NucUIAmvoIng='", $policy->compile($this->nonce));
+    }
+
+    /**
+     * @test
+     */
+    public function hashValueIsCompiled(): void
+    {
+        $hash = hash('sha256', 'test', true);
+        $hashB64 = base64_encode($hash);
+        $policy = (new Policy())->extend(Directive::ScriptSrc, HashValue::create($hash));
+        self::assertSame("script-src 'sha256-$hashB64'", $policy->compile($this->nonce));
+    }
+
+    /**
+     * @test
+     */
+    public function constructorSetsdefaultDirective(): void
+    {
+        $policy = (new Policy(SourceKeyword::self));
+        self::assertSame("default-src 'self'", $policy->compile($this->nonce));
+    }
+
+    /**
+     * @test
+     */
+    public function defaultDirectiveIsModified(): void
+    {
+        $policy = (new Policy(SourceKeyword::self))
+            ->default(SourceKeyword::none);
+        self::assertSame("default-src 'none'", $policy->compile($this->nonce));
+    }
+
+    /**
+     * @test
+     */
+    public function defaultDirectiveConsidersVeto(): void
+    {
+        $policy = (new Policy(SourceKeyword::self))
+            ->default(SourceKeyword::unsafeEval, SourceKeyword::none);
+        self::assertSame("default-src 'none'", $policy->compile($this->nonce));
+    }
+
+    /**
+     * @test
+     */
+    public function newDirectiveExtendsDefault(): void
+    {
+        $policy = (new Policy(SourceKeyword::self))
+            ->extend(Directive::ScriptSrc, SourceKeyword::unsafeInline);
+        self::assertSame("default-src 'self'; script-src 'self' 'unsafe-inline'", $policy->compile($this->nonce));
+    }
+
+    /**
+     * @test
+     */
+    public function newDirectiveDoesNotExtendDefault(): void
+    {
+        $policy = (new Policy(SourceKeyword::self))
+            ->set(Directive::ScriptSrc, SourceKeyword::unsafeInline);
+        self::assertSame("default-src 'self'; script-src 'unsafe-inline'", $policy->compile($this->nonce));
+    }
+
+    /**
+     * @test
+     */
+    public function directiveIsReduced(): void
+    {
+        $policy = (new Policy())
+            ->set(Directive::ScriptSrc, SourceKeyword::self, SourceKeyword::unsafeInline)
+            ->reduce(Directive::ScriptSrc, SourceKeyword::self);
+        self::assertSame("script-src 'unsafe-inline'", $policy->compile($this->nonce));
+    }
+
+    /**
+     * @test
+     */
+    public function sourceSchemeIsCompiled(): void
+    {
+        $policy = (new Policy(SourceKeyword::self, SourceScheme::blob));
+        self::assertSame("default-src 'self' blob:", $policy->compile($this->nonce));
+    }
+
+    /**
+     * @test
+     */
+    public function nonceProxyIsCompiled(): void
+    {
+        $policy = (new Policy(SourceKeyword::self, SourceKeyword::nonceProxy));
+        self::assertSame("default-src 'self' 'nonce-{$this->nonce->b64}'", $policy->compile($this->nonce));
+    }
+
+    /**
+     * @test
+     */
+    public function directiveIsRemoved(): void
+    {
+        $policy = (new Policy(SourceKeyword::self))
+            ->remove(Directive::DefaultSrc);
+        self::assertSame('', $policy->compile($this->nonce));
+    }
+
+    /**
+     * @test
+     */
+    public function superfluousDirectivesArePurged(): void
+    {
+        $policy = (new Policy(SourceKeyword::self, SourceScheme::data))
+            ->set(Directive::ScriptSrc, SourceKeyword::self, SourceScheme::data);
+        self::assertSame("default-src 'self' data:", $policy->compile($this->nonce));
+    }
+
+    /**
+     * @test
+     */
+    public function backendPolicyIsCompiled(): void
+    {
+        $policy = (new Policy())
+            ->default(SourceKeyword::self)
+            ->extend(Directive::ScriptSrc, SourceKeyword::nonceProxy)
+            ->extend(Directive::StyleSrc, SourceKeyword::unsafeInline)
+            ->set(Directive::StyleSrcAttr, SourceKeyword::unsafeInline)
+            ->extend(Directive::ImgSrc, SourceScheme::data)
+            ->set(Directive::WorkerSrc, SourceKeyword::self, SourceScheme::blob)
+            ->extend(Directive::FrameSrc, SourceScheme::blob);
+        self::assertSame(
+            "default-src 'self'; script-src 'self' 'nonce-{$this->nonce->b64}'; "
+            . "style-src 'self' 'unsafe-inline'; style-src-attr 'unsafe-inline'; "
+            . "img-src 'self' data:; worker-src 'self' blob:; frame-src 'self' blob:",
+            $policy->compile($this->nonce)
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function containedDirectiveSourcesAreDetermined(): void
+    {
+        $policy = (new Policy())
+            ->extend(Directive::ScriptSrc, SourceKeyword::unsafeInline, SourceScheme::data, new UriValue('https://example.org'))
+            ->extend(Directive::StyleSrc, SourceKeyword::unsafeInline, SourceScheme::blob, new UriValue('https://example.com/path'));
+
+        self::assertTrue($policy->containsDirective(Directive::ScriptSrc, SourceScheme::data, new UriValue('https://example.org')));
+        self::assertTrue($policy->containsDirective(Directive::StyleSrc, SourceScheme::blob, new UriValue('https://example.com/path')));
+        self::assertFalse($policy->containsDirective(Directive::ConnectSrc, SourceScheme::https));
+    }
+
+    /**
+     * @test
+     */
+    public function coveredDirectiveSourcesAreDetermined(): void
+    {
+        $policy = (new Policy())
+            ->extend(Directive::ScriptSrc, SourceKeyword::unsafeInline, SourceScheme::data, new UriValue('*.example.org'))
+            ->extend(Directive::StyleSrc, SourceKeyword::unsafeInline, SourceScheme::blob, new UriValue('https://*.example.com'));
+
+        self::assertTrue($policy->coversDirective(Directive::ScriptSrc, SourceScheme::data, new UriValue('https://sub.example.org/path/file.js')));
+        self::assertTrue($policy->coversDirective(Directive::StyleSrc, SourceScheme::blob, new UriValue('https://sub.example.com/path/file.css')));
+        self::assertFalse($policy->coversDirective(Directive::ConnectSrc, SourceScheme::https));
+    }
+
+    /**
+     * @test
+     */
+    public function containedPolicyIsDetermined(): void
+    {
+        $policy = (new Policy())
+            ->extend(Directive::ScriptSrc, SourceKeyword::unsafeInline, SourceScheme::data, new UriValue('https://example.org'))
+            ->extend(Directive::StyleSrc, SourceKeyword::unsafeInline, SourceScheme::blob, new UriValue('https://example.com/path'));
+        $other = (new Policy())
+            ->extend(Directive::ScriptSrc, SourceScheme::data, new UriValue('https://example.org'))
+            ->extend(Directive::StyleSrc, SourceScheme::blob, new UriValue('https://example.com/path'));
+        self::assertTrue($policy->contains($other));
+        self::assertFalse($other->contains($policy));
     }
 }
