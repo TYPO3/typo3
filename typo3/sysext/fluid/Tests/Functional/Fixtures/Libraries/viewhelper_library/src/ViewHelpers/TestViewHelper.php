f6927ef20aa21dc0b9921429692c4510b32cf7c9
Simon Praetorius <simon@praetorius.me>
2023-05-02 11:15:06 +0200

    [BUGFIX] Allow ViewHelpers from libraries
    With TYPO3 11 it was possible to use ViewHelpers from composer libraries
    that aren't TYPO3 extensions and thus don't use Symfony dependency
    injection. With the removal of objectManager, this doesn't work anymore
    in TYPO3 12 because the dependency injection can only instantiate
    ViewHelper classes that are defined as public services explicitly.
    
    This change reintroduces that functionality by falling back to "new" if
    the class exists but isn't registered as a service. Fluid standalone
    uses the same method to instantiate a ViewHelper.
    
    Resolves: #100785
    Releases: main, 12.4
    Change-Id: I495e006fa15eb815fb3734b4c73361821869f4ad
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/78917
    Tested-by: Anja Leichsenring <aleichsenring@ab-softlab.de>
    Tested-by: Stefan Bürk <stefan@buerk.tech>
    Reviewed-by: Anja Leichsenring <aleichsenring@ab-softlab.de>
    Reviewed-by: Stefan Bürk <stefan@buerk.tech>
    Tested-by: core-ci <typo3@b13.com>

---
+++ typo3/sysext/fluid/Tests/Functional/Fixtures/Libraries/viewhelper_library/src/ViewHelpers/TestViewHelper.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3Tests\ViewhelperLibrary\ViewHelpers;
+
+use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+use TYPO3Fluid\Fluid\Core\ViewHelper\Traits\CompileWithRenderStatic;
+
+final class TestViewHelper extends AbstractViewHelper
+{
+    use CompileWithRenderStatic;
+
+    public static function renderStatic(
+        array $arguments,
+        \Closure $renderChildrenClosure,
+        RenderingContextInterface $renderingContext
+    ) {
+        return 'test viewhelper working';
+    }
+}
