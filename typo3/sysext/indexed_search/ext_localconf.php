cbb9360b122900e3315c155e222e73bda289dfaa
Georg Ringer <georg.ringer@gmail.com>
2023-05-17 16:01:52 +0200

    [BUGFIX] Remove correct indexed_search entries on cache clearing
    Just clear potential the page being edited from indexed_search's
    index. The previous implementation did also take potential child
    pages, into account, but not the page itself.
    
    Resolves: #100877
    Releases: main, 12.4, 11.5
    Change-Id: I4c8e0c70e68744f629cb1b1c9f62b5f3028502fd
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/80090
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Stefan B�rk <stefan@buerk.tech>
    Reviewed-by: Stefan B�rk <stefan@buerk.tech>

--- typo3/sysext/indexed_search/ext_localconf.php
+++ typo3/sysext/indexed_search/ext_localconf.php
@@ -23,7 +23,7 @@ ExtensionUtility::configurePlugin(
 );
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-cached']['indexed_search'] = TypoScriptFrontendHook::class . '->indexPageContent';
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearPageCacheEval']['indexed_search'] = DeleteIndexedData::class . '->delete';
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearCachePostProc']['indexed_search'] = DeleteIndexedData::class . '->delete';
 
 // Configure default document parsers:
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['indexed_search']['external_parsers'] = [
