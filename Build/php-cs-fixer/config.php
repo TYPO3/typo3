d0345124bb1ee48d43601c32c98e607e941dda26
Benni Mack <benni@typo3.org>
2023-05-28 00:40:13 +0200

    [TASK] Slim down php-cs-fixer configuration file
    TYPO3 uses PER standard definitions and some more additions
    when it comes to PHP Coding guidelines.
    
    However, the main config file of TYPO3 defines some configuration
    options which are already in PSR-12 RuleSet (which is the current PER definition)
    see https://github.com/PHP-CS-Fixer/PHP-CS-Fixer/blob/v3.17.0/src/RuleSet/Sets/PSR12Set.php
    which can be removed.
    
    Resolves: #100957
    Releases: main, 12.4
    Change-Id: I5cff15cf94e896ba81213bf43b705e2d88fd0b7a
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/79174
    Reviewed-by: Stefan Bürk <stefan@buerk.tech>
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Stefan Bürk <stefan@buerk.tech>

--- Build/php-cs-fixer/config.php
+++ Build/php-cs-fixer/config.php
@@ -16,7 +16,7 @@ declare(strict_types=1);
  */
 
 /**
- * This file represents the configuration for Code Sniffing PSR-2-related
+ * This file represents the configuration for Code Sniffing PER-related
  * automatic checks of coding guidelines.
  *
  * Run it using runTests.sh, see 'runTests.sh -h' for more options.
@@ -32,7 +32,7 @@ if (PHP_SAPI !== 'cli') {
 }
 
 // Return a Code Sniffing configuration using
-// all sniffers needed for PSR-2
+// all sniffers needed for PER
 // and additionally:
 //  - Remove leading slashes in use clauses.
 //  - PHP single-line arrays should not have trailing comma.
@@ -51,27 +51,21 @@ return (new \PhpCsFixer\Config())
         '@DoctrineAnnotation' => true,
         '@PER' => true,
         'array_syntax' => ['syntax' => 'short'],
-        'blank_line_after_opening_tag' => true,
         'cast_spaces' => ['space' => 'none'],
-        'compact_nullable_typehint' => true,
         'concat_space' => ['spacing' => 'one'],
         'declare_equal_normalize' => ['space' => 'none'],
         'declare_parentheses' => true,
         'dir_constant' => true,
         'function_to_constant' => ['functions' => ['get_called_class', 'get_class', 'get_class_this', 'php_sapi_name', 'phpversion', 'pi']],
         'function_typehint_space' => true,
-        'lowercase_cast' => true,
-        'method_argument_space' => ['on_multiline' => 'ensure_fully_multiline'],
         'modernize_strpos' => true,
         'modernize_types_casting' => true,
         'native_function_casing' => true,
-        'new_with_braces' => true,
         'no_alias_functions' => true,
         'no_blank_lines_after_phpdoc' => true,
         'no_empty_phpdoc' => true,
         'no_empty_statement' => true,
         'no_extra_blank_lines' => true,
-        'no_leading_import_slash' => true,
         'no_leading_namespace_whitespace' => true,
         'no_null_property_initialization' => true,
         'no_short_bool_cast' => true,
@@ -82,8 +76,7 @@ return (new \PhpCsFixer\Config())
         'no_unused_imports' => true,
         'no_useless_else' => true,
         'no_useless_nullsafe_operator' => true,
-        'no_whitespace_in_blank_line' => true,
-        'ordered_imports' => true,
+        'ordered_imports' => ['imports_order' => ['class', 'function', 'const'], 'sort_algorithm' => 'alpha'],
         'php_unit_construct' => ['assertions' => ['assertEquals', 'assertSame', 'assertNotEquals', 'assertNotSame']],
         'php_unit_mock_short_will_return' => true,
         'php_unit_test_case_static_method_calls' => ['call_type' => 'self'],
@@ -98,7 +91,6 @@ return (new \PhpCsFixer\Config())
         'single_quote' => true,
         'single_space_around_construct' => true,
         'single_line_comment_style' => ['comment_types' => ['hash']],
-        'single_trait_insert_per_statement' => true,
         'trailing_comma_in_multiline' => ['elements' => ['arrays']],
         'whitespace_after_comma_in_array' => ['ensure_single_space' => true],
         'yoda_style' => ['equal' => false, 'identical' => false, 'less_and_greater' => false],
