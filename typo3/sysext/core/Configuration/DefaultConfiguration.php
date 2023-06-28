fbed3b1ac6adefcd394382784817607e68c816d2
Tymoteusz Motylewski <t.motylewski@gmail.com>
2023-06-28 11:02:49 +0200

    [TASK] Add more excludedParameters from common systems
    Include Matomo, Google, Hubspot.
    
    Resolves: #101180
    Releases: main, 12.4, 11.5
    Change-Id: I15ba4d4ec84e5abfb22e5f83d02535e06e2de40f
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/79539
    Tested-by: core-ci <typo3@b13.com>
    Reviewed-by: Benni Mack <benni@typo3.org>
    Tested-by: Benni Mack <benni@typo3.org>

--- typo3/sysext/core/Configuration/DefaultConfiguration.php
+++ typo3/sysext/core/Configuration/DefaultConfiguration.php
@@ -1416,19 +1416,61 @@ return [
             'cachedParametersWhiteList' => [],
             'excludedParameters' => [
                 'L',
+                // Matomo
                 'mtm_campaign',
                 'mtm_keyword',
+                'mtm_kwd',
+                'mtm_source',
+                'mtm_medium',
+                'mtm_content',
+                'mtm_cid',
+                'mtm_group',
+                'mtm_placement',
+                // Piwik
                 'pk_campaign',
                 'pk_kwd',
+                // Google
                 'utm_source',
                 'utm_medium',
                 'utm_campaign',
                 'utm_term',
                 'utm_content',
+                'utm_id',
+                'utm_source_platform',
+                'utm_creative_format',
+                'utm_marketing_tactic',
+                'gtm_debug',
+                // Google ads
                 'gad',
                 'gclid',
+                'dclid',
+                // Facebook
                 'fbclid',
+                // Microsoft Bing Ads.
                 'msclkid',
+                // HubSpot Ad Tracking Parameters
+                'hsa_acc',
+                'hsa_ad',
+                'hsa_cam',
+                'hsa_grp',
+                'hsa_kw',
+                'hsa_mt',
+                'hsa_net',
+                'hsa_src',
+                'hsa_tgt',
+                'hsa_ver',
+                'hsa_ol',
+                'hsa_la',
+                // HubSpot Email Tracking Parameters
+                '_hsenc',
+                '_hsmi',
+                // HubSpot CTA Tracking Parameters
+                '__hssc',
+                '__hstc',
+                '__hsfp',
+                'hsCtaTracking',
+                // HubSpot Form Tracking Parameters
+                'submissionGuid',
             ],
             'requireCacheHashPresenceParameters' => [],
             'excludeAllEmptyParameters' => false,
