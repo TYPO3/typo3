874758503ff03937880be89640399f849dbc2e17
Stefan Bürk <stefan@buerk.tech>
2023-07-03 10:31:54 +0200

    [TASK] styleguide: Set correct core version in `ext_emconf.php`
    This change sets the version in the `ext_emconf.php`
    to the next version, matching the current TYPO3 core
    main branch version.

--- typo3/sysext/styleguide/ext_emconf.php
+++ typo3/sysext/styleguide/ext_emconf.php
@@ -7,10 +7,10 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'TYPO3 Core Team',
     'author_email' => 'typo3cms@typo3.org',
     'state' => 'stable',
-    'version' => '12.0.0',
+    'version' => '13.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.0.0-12.99.99',
+            'typo3' => '13.0.0-13.99.99',
         ],
         'conflicts' => [
         ],
