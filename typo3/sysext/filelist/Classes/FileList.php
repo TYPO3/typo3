7e0e64328bc69fec6b54d1e58da4bd8459f72b06
markup <info@markup-web.de>
2023-07-12 14:10:29 +0200

    [BUGFIX] Fix show/hide thumbnails and clipboard in filelist module
    Bring back functionality of the options to show or hide
    thumbnails and clipboard in filelist module.
    
    $params may contain integer values. If the value is not an
    array, cast it to string so that these values are kept.
    
    Resolves: #101337
    Related: #101306
    Releases: main, 12.4
    Change-Id: I7361a7c63657cb786e42577ca7553893733041ae
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/80011
    Reviewed-by: Stefan B�rk <stefan@buerk.tech>
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Nikita Hovratov <nikita.h@live.de>
    Reviewed-by: Nikita Hovratov <nikita.h@live.de>
    Tested-by: Stefan B�rk <stefan@buerk.tech>

--- typo3/sysext/filelist/Classes/FileList.php
+++ typo3/sysext/filelist/Classes/FileList.php
@@ -1471,7 +1471,7 @@ class FileList
         // We always map it to the id here.
         $params['expandFolder'] = $params['id'];
         $params = array_filter($params, static function ($value) {
-            return (is_string($value) && trim($value) !== '') || (is_array($value) && $value !== []);
+            return (is_array($value) && $value !== []) || (trim((string)$value) !== '');
         });
 
         return (string)$this->uriBuilder->buildUriFromRoute($route->getOption('_identifier'), $params);
