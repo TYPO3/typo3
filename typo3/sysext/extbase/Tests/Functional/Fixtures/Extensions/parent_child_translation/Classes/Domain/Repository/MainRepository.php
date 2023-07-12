0d1db41c78cbbe5e8e490f89da3df2413458fcdb
André Buchmann <andy.schliesser@gmail.com>
2023-07-12 15:29:31 +0200

    [BUGFIX] Localize children of all languages element
    When the parent record has been set to sys_language_uid=-1, but the
    child record has translations, it should be displayed in the correct
    language. Setting the languageUid for the querySettings to -1 results
    in display of the default language record of the child.
    
    Resolves: #92768
    Releases: main, 12.4, 11.5
    Change-Id: I25bcdbb182c1b35cbc08e5f91339453b0986cdcc
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/80012
    Reviewed-by: Benni Mack <benni@typo3.org>
    Tested-by: core-ci <typo3@b13.com>
    Reviewed-by: Stefan Bürk <stefan@buerk.tech>
    Tested-by: Benni Mack <benni@typo3.org>
    Tested-by: Stefan Bürk <stefan@buerk.tech>

---
+++ typo3/sysext/extbase/Tests/Functional/Fixtures/Extensions/parent_child_translation/Classes/Domain/Repository/MainRepository.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3Tests\ParentChildTranslation\Domain\Repository;
+
+use TYPO3\CMS\Extbase\Persistence\Generic\Typo3QuerySettings;
+use TYPO3\CMS\Extbase\Persistence\QueryInterface;
+use TYPO3\CMS\Extbase\Persistence\Repository;
+use TYPO3Tests\ParentChildTranslation\Domain\Model\Main;
+
+/**
+ * The repository for Mains
+ *
+ * @extends Repository<Main>
+ */
+class MainRepository extends Repository
+{
+    protected $defaultOrderings = [
+        'uid' => QueryInterface::ORDER_ASCENDING,
+    ];
+
+    public function initializeObject(): void
+    {
+        /** @var Typo3QuerySettings $defaultQuerySettings */
+        $defaultQuerySettings = $this->createQuery()->getQuerySettings();
+        $defaultQuerySettings->setRespectStoragePage(false);
+        $this->setDefaultQuerySettings($defaultQuerySettings);
+    }
+}
---
+++ typo3/sysext/extbase/Tests/Functional/Fixtures/Extensions/parent_child_translation/ext_tables.sql
@@ -0,0 +1,24 @@
+#
+# Table structure for table 'tx_parentchildtranslation_domain_model_main'
+#
+CREATE TABLE tx_parentchildtranslation_domain_model_main (
+	title varchar(255) NOT NULL DEFAULT '',
+	child int(11) unsigned DEFAULT '0' NOT NULL,
+	squeeze int(11) unsigned DEFAULT '0' NOT NULL,
+);
+
+#
+# Table structure for table 'tx_parentchildtranslation_domain_model_squeeze'
+#
+CREATE TABLE tx_parentchildtranslation_domain_model_squeeze (
+	title varchar(255) NOT NULL DEFAULT '',
+	parent int(11) unsigned DEFAULT '0' NOT NULL,
+	child int(11) unsigned DEFAULT '0' NOT NULL,
+);
+
+#
+# Table structure for table 'tx_parentchildtranslation_domain_model_child'
+#
+CREATE TABLE tx_parentchildtranslation_domain_model_child (
+	title varchar(255) NOT NULL DEFAULT ''
+);
