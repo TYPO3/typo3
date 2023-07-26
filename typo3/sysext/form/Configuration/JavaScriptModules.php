22788b0f505cc189ed2dea020dad6a7814f1b596
Benjamin Franzke <ben@bnf.dev>
2023-07-26 07:49:50 +0200

    [TASK] Remove backend.contextmenu tag from EXT:form importmap
    The importmap tag `backend.contextmenu` was added to EXT:form
    because it registers a context-menu item provider (for files).
    It turns out that the provider is based on the EXT:filelist
    provider and therefore doesn't require own javascript
    (`@typo3/filelist/context-menu-actions` is used).
    
    Resolves: #101445
    Releases: main, 12.4
    Change-Id: Ibdce39c971aae69889849b52847f62ce1298baf7
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/80176
    Tested-by: core-ci <typo3@b13.com>
    Reviewed-by: Stefan B�rk <stefan@buerk.tech>
    Tested-by: Stefan B�rk <stefan@buerk.tech>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>

--- typo3/sysext/form/Configuration/JavaScriptModules.php
+++ typo3/sysext/form/Configuration/JavaScriptModules.php
@@ -5,9 +5,6 @@ return [
         'backend',
         'core',
     ],
-    'tags' => [
-        'backend.contextmenu',
-    ],
     'imports' => [
         '@typo3/form/backend/' => 'EXT:form/Resources/Public/JavaScript/backend/',
     ],
