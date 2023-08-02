30b50fd89a6e83ce5027dde077749386f1ad6560
Andreas Fernandez <a.fernandez@scripting-base.de>
2023-08-02 20:20:20 +0200

    [BUGFIX] Fix paging in record list when pressing the Enter key
    Submitting the paging form in the record list's single table view was
    broken as the "View webpage" button wasn't explictly defined as a
    button, which in fact made it a submit button that opened the first
    page in the record list in a new tab.
    
    This patch does two things:
    
    * The button element is now explicitly declared being a "dumb" button
    * The JavaScript building the URL is simplified to use the existing URL
      in the form, which makes the data attribute that holds the "template"
      obsolete
    
    Resolves: #101540
    Related: #101276
    Releases: main, 12.4
    Change-Id: I9184b3871452aed0b130a32a1405434e0fb9a9db
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/80898
    Reviewed-by: Andreas Fernandez <a.fernandez@scripting-base.de>
    Tested-by: Andreas Fernandez <a.fernandez@scripting-base.de>
    Tested-by: core-ci <typo3@b13.com>

--- typo3/sysext/backend/Classes/RecordList/DatabaseRecordList.php
+++ typo3/sysext/backend/Classes/RecordList/DatabaseRecordList.php
@@ -1374,6 +1374,7 @@ class DatabaseRecordList
                 && ($attributes = $this->getPreviewUriBuilder($table, $row)->serializeDispatcherAttributes()) !== null
             ) {
                 $viewAction = '<button'
+                    . ' type="button"'
                     . ' class="btn btn-default" ' . $attributes
                     . ' title="' . htmlspecialchars($this->getLanguageService()->sL('LLL:EXT:core/Resources/Private/Language/locallang_core.xlf:labels.showPage')) . '">';
                 if ($table === 'pages') {
