fd59d01b48c0782aea924fe24f0e483177f58e26
Christian Kuhn <lolli@schwarzbu.ch>
2023-08-02 18:13:04 +0200

    [TASK] Have correct blog_example related_posts TCA
    Similar to the recently fixed other MM TCA
    setup of blog_example, the "related_posts"
    is adapted as well.
    
    This removes the last mm table definition from
    ext_tables.sql - the auto-creation kicks in as
    expected now.
    
    Resolves: #101537
    Related: #101528
    Releases: main, 12.4
    Change-Id: Ia92ce6bbc78a493e1a2028bd9ab7a31952181dd3
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/80299
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>

--- typo3/sysext/extbase/Tests/Functional/Fixtures/Extensions/blog_example/ext_tables.sql
+++ typo3/sysext/extbase/Tests/Functional/Fixtures/Extensions/blog_example/ext_tables.sql
@@ -62,21 +62,6 @@ CREATE TABLE tx_blogexample_domain_model_tag (
 );
 
 #
-#
-# Table structure for table 'tx_blogexample_post_post_mm'
-# Broken. See comment on TCA tx_blogexample_domain_model_post related_posts field
-#
-CREATE TABLE tx_blogexample_post_post_mm (
-	uid_local int(11) unsigned DEFAULT '0' NOT NULL,
-	uid_foreign int(11) unsigned DEFAULT '0' NOT NULL,
-	sorting int(11) unsigned DEFAULT '0' NOT NULL,
-	sorting_foreign int(11) unsigned DEFAULT '0' NOT NULL,
-
-	KEY uid_local (uid_local),
-	KEY uid_foreign (uid_foreign)
-);
-
-#
 # Table structure for table 'tx_blogexample_domain_model_dateexample'
 # @deprectaed Can be removed as soon as int / native type is enforced
 #
