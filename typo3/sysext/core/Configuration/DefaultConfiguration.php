d869c2c9189fb6507e5913e3431a71cbe645de3d
Daniel Hettler <daniel.koether@outlook.de>
2023-08-07 22:10:01 +0200

    [TASK] Allow Piwik PRO debug mode param '_stg_debug'
    The URL param '_stg_debug' is needed to call the debug mode on-site
    
    Resolves: #101613
    Releases: main, 11.5, 12.4
    Change-Id: Iff94ede5c3d388fe8b130e10e5b48658511a0bc3
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/80435
    Reviewed-by: Oliver Klee <typo3-coding@oliverklee.de>
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Andreas Fernandez <a.fernandez@scripting-base.de>
    Reviewed-by: Andreas Fernandez <a.fernandez@scripting-base.de>
    Reviewed-by: Stefan B�rk <stefan@buerk.tech>
    Tested-by: Stefan B�rk <stefan@buerk.tech>

--- typo3/sysext/core/Configuration/DefaultConfiguration.php
+++ typo3/sysext/core/Configuration/DefaultConfiguration.php
@@ -1427,6 +1427,7 @@ return [
                 // Piwik
                 'pk_campaign',
                 'pk_kwd',
+                '_stg_debug',
                 // Google
                 'utm_source',
                 'utm_medium',
