5ea53d6007ef7d26b74bff56dbb5f03d48b48fc8
Sybille Peters <sypets@gmx.de>
2023-08-13 18:39:00 +0200

    [TASK] Improve linktypes checkboxes UI for Linkvalidator
    Improve selection of Linkvalidator linkypes checkboxes form
    by adding a "Toggle all" checkbox as is already used in
    FormEngine `selectCheckBox` renderType.
    
    This improves the usability of the form and makes multiple
    clicks unnecessary.
    
    Resolves: #101674
    Releases: main
    Change-Id: I8e15cd9765590352804e4b2eacc7f879510cb681
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/80542
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Stefan Bürk <stefan@buerk.tech>
    Tested-by: core-ci <typo3@b13.com>
    Reviewed-by: Stefan Bürk <stefan@buerk.tech>

--- Build/Sources/TypeScript/linkvalidator/linkvalidator.ts
+++ Build/Sources/TypeScript/linkvalidator/linkvalidator.ts
@@ -16,7 +16,14 @@ import RegularEvent from '@typo3/core/event/regular-event';
 
 enum Selectors {
   settingsContainerSelector = '.t3js-linkvalidator-settings',
-  actionButtonSelector = '.t3js-linkvalidator-action-button'
+  actionButtonSelector = '.t3js-linkvalidator-action-button',
+  linktypesTableSelector = '#check-options-table',
+  toggleAllLinktypesSelector = '.t3js-linkvalidator-settings input[type="checkbox"].options-by-type-toggle-all',
+  linktypesSelector = '.t3js-linkvalidator-settings input[type="checkbox"].options-by-type'
+}
+
+enum Identifier {
+  toggleAllLinktypesId = 'options-by-type-toggle-all'
 }
 
 /**
@@ -24,6 +31,7 @@ enum Selectors {
  */
 class Linkvalidator {
   constructor() {
+    this.enableTriggerCheckBox();
     this.initializeEvents();
     document.querySelectorAll(Selectors.settingsContainerSelector).forEach((container: HTMLElement): void => {
       Linkvalidator.toggleActionButtons(container);
@@ -37,10 +45,35 @@ class Linkvalidator {
     );
   }
 
+  private static allCheckBoxesAreChecked(checkBoxes: NodeListOf<HTMLInputElement>): boolean {
+    const checkboxArray = Array.from(checkBoxes);
+    return checkBoxes.length === checkboxArray.filter((checkBox: HTMLInputElement) => checkBox.checked).length;
+  }
+
+  /**
+   * Enables the "Toggle all" checkbox on document load if all child checkboxes are checked
+   */
+  private enableTriggerCheckBox(): void {
+    const checkBoxes: NodeListOf<HTMLInputElement> = document.querySelectorAll(Selectors.linktypesSelector);
+    (document.getElementById(Identifier.toggleAllLinktypesId) as HTMLInputElement).checked = Linkvalidator.allCheckBoxesAreChecked(checkBoxes);
+  }
+
   private initializeEvents(): void {
-    new RegularEvent('change', (e: Event, checkbox: HTMLInputElement): void => {
-      Linkvalidator.toggleActionButtons(checkbox.closest(Selectors.settingsContainerSelector));
-    }).delegateTo(document, [Selectors.settingsContainerSelector, 'input[type="checkbox"]'].join(' '));
+    // toggleAll (checkboxes): on change
+    new RegularEvent('change', (e: Event, currentTarget: HTMLInputElement): void => {
+      const checkBoxes: NodeListOf<HTMLInputElement> = document.querySelectorAll(Selectors.linktypesSelector);
+      const checkIt = !Linkvalidator.allCheckBoxesAreChecked(checkBoxes);
+
+      checkBoxes.forEach((checkBox: HTMLInputElement): void => {
+        checkBox.checked = checkIt;
+      });
+      currentTarget.checked = checkIt;
+    }).delegateTo(document, Selectors.toggleAllLinktypesSelector);
+
+    // toggle (checkbox): on change
+    new RegularEvent('change', (): void => {
+      this.enableTriggerCheckBox();
+    }).delegateTo(document, Selectors.linktypesSelector);
 
     new RegularEvent('click', (e: PointerEvent, actionButton: HTMLInputElement): void => {
       Notification.success(actionButton.dataset.notificationMessage || 'Event triggered', '', 2);
