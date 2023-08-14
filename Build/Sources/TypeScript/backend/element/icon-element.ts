6304e00652ef4e737542bd423b78ab3883c831cc
Benjamin Franzke <ben@bnf.dev>
2023-08-14 10:37:00 +0200

    [BUGFIX] Make <typo3-backend-icon> CSS backwards compatible
     * Fix positioning in inline contents.
       Inline icon-alignment is done with the `vertical-align: -22%` hack.
       It was introduced for icons by #69005, reintroduced for the icon
       element in #97876 and was then removed again in #99879
       (possibly because of conflicts with block-usage).
    
     * Fix evaluation of icon-opacity-disabled, which was ignored because
       a semicolon was missing between the variable declaration and
       `display: inline-block`
    
     * Avoid unneeded icon-unify redefinition via variants. That's
       only used to hide an implicit condition to shadow size=default.
       But that case can rather be expressed using a fallback in the
       var() expression.
    
     * Ensure that global CSS properties are taken into account
       by avoiding a redefinition via :host() and using var()
       fallbacks instead.
    
    Resolves: #101684
    Resolves: #104895
    Related: #97876
    Related: #99879
    Releases: main, 12.4
    Change-Id: I37236f2ca7211c0555db7039014adf6e93d2ece2
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/75419
    Reviewed-by: Andreas Kienast <akienast@scripting-base.de>
    Tested-by: Andreas Kienast <akienast@scripting-base.de>
    Reviewed-by: Benjamin Franzke <ben@bnf.dev>
    Tested-by: Benjamin Franzke <ben@bnf.dev>
    Reviewed-by: Benjamin Kott <benjamin.kott@outlook.com>
    Tested-by: Benjamin Kott <benjamin.kott@outlook.com>
    Tested-by: core-ci <typo3@b13.com>

--- Build/Sources/TypeScript/backend/element/icon-element.ts
+++ Build/Sources/TypeScript/backend/element/icon-element.ts
@@ -65,15 +65,13 @@ export class IconElement extends LitElement {
 
     return this.iconTask.render({
       pending: () => html`<typo3-backend-spinner size=${this.size}></typo3-backend-size>`,
-      complete: (markup) => html`<div class="icon-wrapper">${unsafeHTML(markup)}</div>`,
+      complete: (markup) => html`${unsafeHTML(markup)}`,
       error: () => html`
-        <div class="icon-wrapper">
-          <span class="t3js-icon icon icon-size-${this.size} icon-state-${this.state} icon-default-not-found" data-identifier="default-not-found" aria-hidden="true">
-	          <span class="icon-markup">
-              <svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" viewBox="0 0 16 16"><g><path fill="#CD201F" d="m11 12 3-2v6H2v-6l3 2 3-2 3 2z"/><path fill="#212121" d="m8 10.3 2.86 1.91.14.09.14-.09 2.61-1.74v5.28H2.25v-5.28l2.61 1.74.14.09.14-.09L8 10.3m6-.3-3 2-3-2-3 2-3-2v6h12v-6z" opacity=".2"/><path fill="#CD201F" d="M14 4v4l-3 2-3-2-3 2-3-2V0h8l4 4z"/><path fill="#212121" d="M13.75 7.87 11 9.7 8.14 7.79 8 7.7l-.14.09L5 9.7 2.25 7.87V.25H10V0H2v8l3 2 3-2 3 2 3-2V4h-.25z" opacity=".2"/><path fill="#FFF" d="M14 4h-4V0l4 4z" opacity=".3"/><path fill="#212121" d="m14 8-4-4h4v4z" opacity=".3"/></g></svg>
-	          </span>
-          </span>
-        </div>
+        <span class="t3js-icon icon icon-size-${this.size} icon-state-${this.state} icon-default-not-found" data-identifier="default-not-found" aria-hidden="true">
+	        <span class="icon-markup">
+            <svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" viewBox="0 0 16 16"><g><path fill="#CD201F" d="m11 12 3-2v6H2v-6l3 2 3-2 3 2z"/><path fill="#212121" d="m8 10.3 2.86 1.91.14.09.14-.09 2.61-1.74v5.28H2.25v-5.28l2.61 1.74.14.09.14-.09L8 10.3m6-.3-3 2-3-2-3 2-3-2v6h12v-6z" opacity=".2"/><path fill="#CD201F" d="M14 4v4l-3 2-3-2-3 2-3-2V0h8l4 4z"/><path fill="#212121" d="M13.75 7.87 11 9.7 8.14 7.79 8 7.7l-.14.09L5 9.7 2.25 7.87V.25H10V0H2v8l3 2 3-2 3 2 3-2V4h-.25z" opacity=".2"/><path fill="#FFF" d="M14 4h-4V0l4 4z" opacity=".3"/><path fill="#212121" d="m14 8-4-4h4v4z" opacity=".3"/></g></svg>
+	        </span>
+        </span>
       `
     });
   }
--- typo3/sysext/backend/Resources/Public/JavaScript/element/spinner-element.js
+++ typo3/sysext/backend/Resources/Public/JavaScript/element/spinner-element.js
@@ -11,15 +11,13 @@
  * The TYPO3 project - inspiring people to share!
  */
 var __decorate=function(e,t,n,r){var o,s=arguments.length,i=s<3?t:null===r?r=Object.getOwnPropertyDescriptor(t,n):r;if("object"==typeof Reflect&&"function"==typeof Reflect.decorate)i=Reflect.decorate(e,t,n,r);else for(var c=e.length-1;c>=0;c--)(o=e[c])&&(i=(s<3?o(i):s>3?o(t,n,i):o(t,n))||i);return s>3&&i&&Object.defineProperty(t,n,i),i};import{html,LitElement}from"lit";import{customElement,property}from"lit/decorators.js";import{Sizes}from"@typo3/backend/enum/icon-types.js";import{IconStyles}from"@typo3/backend/icons.js";let SpinnerElement=class extends LitElement{constructor(){super(...arguments),this.size=Sizes.default}render(){return html`
-      <div class="icon-wrapper">
-        <span class="icon icon-size-${this.size} icon-state-default icon-spin">
-          <span class="icon-markup">
-            <svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" viewBox="0 0 16 16">
-              <g fill="currentColor">
-                <path d="M8 15c-3.86 0-7-3.141-7-7 0-3.86 3.14-7 7-7 3.859 0 7 3.14 7 7 0 3.859-3.141 7-7 7zM8 3C5.243 3 3 5.243 3 8s2.243 5 5 5 5-2.243 5-5-2.243-5-5-5z" opacity=".3"/><path d="M14 9a1 1 0 0 1-1-1c0-2.757-2.243-5-5-5a1 1 0 0 1 0-2c3.859 0 7 3.14 7 7a1 1 0 0 1-1 1z"/>
-              </g>
-            </svg>
-          </span>
+      <span class="icon icon-size-${this.size} icon-state-default icon-spin">
+        <span class="icon-markup">
+          <svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" viewBox="0 0 16 16">
+            <g fill="currentColor">
+              <path d="M8 15c-3.86 0-7-3.141-7-7 0-3.86 3.14-7 7-7 3.859 0 7 3.14 7 7 0 3.859-3.141 7-7 7zM8 3C5.243 3 3 5.243 3 8s2.243 5 5 5 5-2.243 5-5-2.243-5-5-5z" opacity=".3"/><path d="M14 9a1 1 0 0 1-1-1c0-2.757-2.243-5-5-5a1 1 0 0 1 0-2c3.859 0 7 3.14 7 7a1 1 0 0 1-1 1z"/>
+            </g>
+          </svg>
         </span>
-      </div>
+      </span>
     `}};SpinnerElement.styles=IconStyles.getStyles(),__decorate([property({type:String})],SpinnerElement.prototype,"size",void 0),SpinnerElement=__decorate([customElement("typo3-backend-spinner")],SpinnerElement);export{SpinnerElement};
