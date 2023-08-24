6b33d8f4dfbcd429a9a23036281dce1c483fbe4e
Oliver Hader <oliver@typo3.org>
2023-08-24 13:09:00 +0200

    [BUGFIX] Fix several undefined array key issues in DataHandler scope
    DataMapProcessor tries to access `$possibleChainedIds[0]` which
    might not be defined, since that array is the result of a previous
    `array_intersect` invokation, which keeps the array keys of the
    original input array.
    
    DataHandler tries to access `$this->copyMappingArray[$table][$uid]`,
    which might not be defined in case the previous copy command could
    not be executed (e.g. since the record was deleted already).
    
    Resolves: #101748
    Releases: main, 12.4, 11.5
    Change-Id: Ibcbc412c7df62ce17dfa2c0f6fc120ccebb953d8
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/80670
    Tested-by: core-ci <typo3@b13.com>
    Reviewed-by: Andreas Fernandez <a.fernandez@scripting-base.de>
    Reviewed-by: Oliver Klee <typo3-coding@oliverklee.de>
    Tested-by: Andreas Fernandez <a.fernandez@scripting-base.de>

--- typo3/sysext/core/Classes/DataHandling/DataHandler.php
+++ typo3/sysext/core/Classes/DataHandling/DataHandler.php
@@ -3552,7 +3552,7 @@ class DataHandler implements LoggerAwareInterface
                                 } else {
                                     $this->copyRecord($table, (int)$id, $target, true, [], '', 0, $ignoreLocalization);
                                 }
-                                $procId = $this->copyMappingArray[$table][$id];
+                                $procId = $this->copyMappingArray[$table][$id] ?? null;
                                 break;
                             case 'localize':
                                 $this->useTransOrigPointerField = true;
@@ -3573,7 +3573,7 @@ class DataHandler implements LoggerAwareInterface
                                 break;
                         }
                         $this->useTransOrigPointerField = $backupUseTransOrigPointerField;
-                        if (is_array($pasteUpdate)) {
+                        if (is_array($pasteUpdate) && $procId > 0) {
                             $pasteDatamap[$table][$procId] = $pasteUpdate;
                         }
                     }
@@ -3782,7 +3782,7 @@ class DataHandler implements LoggerAwareInterface
                 $CPtable = $this->int_pageTreeInfo([], $uid, (int)$this->copyTree, $theNewRootID);
                 // Now copying the subpages:
                 foreach ($CPtable as $thePageUid => $thePagePid) {
-                    $newPid = $this->copyMappingArray['pages'][$thePagePid];
+                    $newPid = $this->copyMappingArray['pages'][$thePagePid] ?? null;
                     if (isset($newPid)) {
                         $this->copySpecificPage($thePageUid, $newPid, $copyTablesAlongWithPage);
                     } else {
@@ -6744,7 +6744,7 @@ class DataHandler implements LoggerAwareInterface
                     $thePidToUpdate = $theUidToUpdate;
                 } elseif (isset($this->registerDBPids[$table][$uid])) {
                     $thePidToUpdate = $this->registerDBPids[$table][$uid];
-                    $thePidToUpdate = $this->copyMappingArray_merged['pages'][$thePidToUpdate];
+                    $thePidToUpdate = $this->copyMappingArray_merged['pages'][$thePidToUpdate] ?? null;
                 }
 
                 // Update child records if change to pid is required
@@ -6803,7 +6803,7 @@ class DataHandler implements LoggerAwareInterface
             $thePidToUpdate = $theUidToUpdate;
         } elseif (isset($this->registerDBPids[$table][$uid])) {
             $thePidToUpdate = $this->registerDBPids[$table][$uid];
-            $thePidToUpdate = $this->copyMappingArray_merged['pages'][$thePidToUpdate];
+            $thePidToUpdate = $this->copyMappingArray_merged['pages'][$thePidToUpdate] ?? null;
         }
 
         if ($thePidToUpdate && $updatePidForRecords !== []) {
