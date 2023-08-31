1ef77286e7bac52b5137cef50fa52459c862b481
Friedemann Altrock <hallo@faltrock.de>
2023-08-31 16:16:21 +0200

    [BUGFIX] Respect crop of FileReference objects in getImgResource
    ContentObjectRenderer::getImgResource is used for the content objects
    IMAGE, IMG_RESOURCE, as well as GIFBUILDER to process images.
    
    When it is passed a FileReference object, it should respect the crop
    settings of this reference, same as when it is passed a reference UID
    plus the treatIdAsReference config.
    
    Resolves: #101810
    Releases: main, 12.4, 11.5
    Change-Id: I399106399ec283561c00fa5cf61d6c7fe2871e49
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/80812
    Tested-by: Oliver Klee <typo3-coding@oliverklee.de>
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Stefan Bürk <stefan@buerk.tech>
    Reviewed-by: Stefan Bürk <stefan@buerk.tech>
    Tested-by: Nikita Hovratov <nikita.h@live.de>
    Reviewed-by: Nikita Hovratov <nikita.h@live.de>

--- typo3/sysext/frontend/Classes/ContentObject/ContentObjectRenderer.php
+++ typo3/sysext/frontend/Classes/ContentObject/ContentObjectRenderer.php
@@ -3643,6 +3643,7 @@ class ContentObjectRenderer implements LoggerAwareInterface
     public function getImgResource($file, $fileArray)
     {
         $importedFile = null;
+        $fileReference = null;
         if (empty($file) && empty($fileArray)) {
             return null;
         }
@@ -3664,6 +3665,7 @@ class ContentObjectRenderer implements LoggerAwareInterface
             if ($file instanceof File) {
                 $fileObject = $file;
             } elseif ($file instanceof FileReference) {
+                $fileReference = $file;
                 $fileObject = $file->getOriginalFile();
             } else {
                 try {
@@ -3710,7 +3712,7 @@ class ContentObjectRenderer implements LoggerAwareInterface
                 $processingConfiguration['noScale'] = $this->stdWrapValue('noScale', $fileArray ?? []);
                 $processingConfiguration['additionalParameters'] = $this->stdWrapValue('params', $fileArray ?? []);
                 $processingConfiguration['frame'] = (int)$this->stdWrapValue('frame', $fileArray ?? []);
-                if (isset($fileReference)) {
+                if ($fileReference instanceof FileReference) {
                     $processingConfiguration['crop'] = $this->getCropAreaFromFileReference($fileReference, $fileArray);
                 } else {
                     $processingConfiguration['crop'] = $this->getCropAreaFromFromTypoScriptSettings($fileObject, $fileArray);
