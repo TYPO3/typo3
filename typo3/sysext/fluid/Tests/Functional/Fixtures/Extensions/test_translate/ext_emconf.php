3b4e993a3dee06f142ef80463a3b11f68a18bd33
Benni Mack <benni@typo3.org>
2023-09-12 08:53:55 +0200

    [RELEASE] Release of TYPO3 12.4.6
    Change-Id: Iff5650d67aec9e2deebe013a0761481158b566e6
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/80977
    Tested-by: Benni Mack <benni@typo3.org>
    Reviewed-by: Benni Mack <benni@typo3.org>

--- typo3/sysext/fluid/Tests/Functional/Fixtures/Extensions/test_translate/ext_emconf.php
+++ typo3/sysext/fluid/Tests/Functional/Fixtures/Extensions/test_translate/ext_emconf.php
@@ -6,14 +6,14 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'Test translate view helper',
     'description' => 'Test translate view helper test_translate',
     'category' => 'example',
-    'version' => '13.0.0',
+    'version' => '12.4.6',
     'state' => 'beta',
     'author' => 'Christian Kuhn',
     'author_email' => 'lolli@schwarzbu.ch',
     'author_company' => '',
     'constraints' => [
         'depends' => [
-            'typo3' => '13.0.0',
+            'typo3' => '12.4.6',
         ],
         'conflicts' => [],
         'suggests' => [],
