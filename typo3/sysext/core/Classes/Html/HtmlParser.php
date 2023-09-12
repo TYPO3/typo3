634d4eddf9f573b2fe59bdd709e41b46363e46b9
Philipp Kitzberger <typo3@kitze.net>
2023-09-12 13:41:29 +0200

    [BUGFIX] Avoid undefined array key 'casesensitiveComp'
    Resolves: #96537
    Releases: main, 12.4, 11.5
    Change-Id: I0b2b4dbf47d58f21df8ffb3d91a889c22b092280
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/81048
    Tested-by: core-ci <typo3@b13.com>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>

--- typo3/sysext/core/Classes/Html/HtmlParser.php
+++ typo3/sysext/core/Classes/Html/HtmlParser.php
@@ -539,7 +539,7 @@ class HtmlParser
                                                         $tagAttrib[0][$attr] = $params['list'][0];
                                                     }
                                                 } else {
-                                                    if (!in_array($this->caseShift($tagAttrib[0][$attr] ?? '', $params['casesensitiveComp'] ?? false), (array)$this->caseShift($params['list'], $params['casesensitiveComp'], $tagName))) {
+                                                    if (!in_array($this->caseShift($tagAttrib[0][$attr] ?? '', $params['casesensitiveComp'] ?? false), (array)$this->caseShift($params['list'], $params['casesensitiveComp'] ?? false, $tagName))) {
                                                         $tagAttrib[0][$attr] = $params['list'][0];
                                                     }
                                                 }
