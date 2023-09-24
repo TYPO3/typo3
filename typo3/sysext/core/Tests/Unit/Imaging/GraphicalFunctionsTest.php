d336f1034931cacd69ab36d61b6109a99af4b9ce
Benni Mack <benni@typo3.org>
2023-09-24 16:27:41 +0200

    [TASK] Add tests to GraphicalFunctions getImageScale
    This change adds more examples to getImageScale
    to have a good test bed in place before we move
    code around for v13.
    
    Resolves: #102026
    Releases: main, 12.4
    Change-Id: I82bfa00fb8f8342270505071d161de2462b782ea
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/81189
    Reviewed-by: Benjamin Franzke <ben@bnf.dev>
    Tested-by: Benjamin Franzke <ben@bnf.dev>
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Oliver Klee <typo3-coding@oliverklee.de>

--- typo3/sysext/core/Tests/Unit/Imaging/GraphicalFunctionsTest.php
+++ typo3/sysext/core/Tests/Unit/Imaging/GraphicalFunctionsTest.php
@@ -27,77 +27,260 @@ final class GraphicalFunctionsTest extends UnitTestCase
     /**
      * Dataprovider for getScaleForImage
      */
-    public static function getScaleForImageDataProvider(): array
+    public static function getScaleForImageDataProvider(): \Generator
     {
-        return [
-            'Get image scale for a width of 150px' => [
-                [
-                    170,
-                    136,
-                ],
-                '150',
-                '',
-                [],
-                [
-                    'crs' => false,
-                    'origW' => 150,
-                    'origH' => 0,
-                    'max' => 0,
-                    0 => 150,
-                    1 => 120,
-                ],
+        yield 'Get image scale for a width of 150px' => [
+            [170, 136],
+            '150',
+            '',
+            [],
+            true,
+            [
+                'crs' => false,
+                'origW' => 150,
+                'origH' => 0,
+                'max' => 0,
+                0 => 150,
+                1 => 120,
             ],
-            'Get image scale with a maximum width of 100px' => [
-                [
-                    170,
-                    136,
-                ],
-                '',
-                '',
-                [
-                    'maxW' => 100,
-                ],
-                [
-                    'crs' => false,
-                    'origW' => 100,
-                    'origH' => 0,
-                    'max' => 1,
-                    0 => 100,
-                    1 => 80,
-                ],
+        ];
+        yield 'Get image scale with a maximum width of 100px' => [
+            [170, 136],
+            '',
+            '',
+            [
+                'maxW' => 100,
             ],
-            'Get image scale with a minimum width of 200px' => [
-                [
-                    170,
-                    136,
-                ],
-                '',
-                '',
-                [
-                    'minW' => 200,
-                ],
-                [
-                    'crs' => false,
-                    'origW' => 0,
-                    'origH' => 0,
-                    'max' => 0,
-                    0 => 200,
-                    1 => 136,
-                ],
+            true,
+            [
+                'crs' => false,
+                'origW' => 100,
+                'origH' => 0,
+                'max' => 1,
+                0 => 100,
+                1 => 80,
             ],
-            'No PHP warning for zero in input dimensions when scaling' => [
-                [0, 0],
-                '50',
-                '',
-                [],
-                [
-                    'crs' => false,
-                    'origW' => 50,
-                    'origH' => 0,
-                    'max' => 0,
-                    0 => 0,
-                    1 => 0,
-                ],
+        ];
+        yield 'Get image scale with a minimum width of 200px' => [
+            [170, 136],
+            '',
+            '',
+            [
+                'minW' => 200,
+            ],
+            true,
+            [
+                'crs' => false,
+                'origW' => 0,
+                'origH' => 0,
+                'max' => 0,
+                0 => 200,
+                1 => 136,
+            ],
+        ];
+        yield 'No PHP warning for zero in input dimensions when scaling' => [
+            [0, 0],
+            '50',
+            '',
+            [],
+            true,
+            [
+                'crs' => false,
+                'origW' => 50,
+                'origH' => 0,
+                'max' => 0,
+                0 => 0,
+                1 => 0,
+            ],
+        ];
+        yield 'width from original image and explicitly given scales an image down' => [
+            [100, 900],
+            '50',
+            '',
+            [],
+            true,
+            [
+                'crs' => false,
+                'origW' => 50,
+                'origH' => 0,
+                'max' => 0,
+                0 => 50,
+                1 => 450,
+            ],
+        ];
+        yield 'width from original image with maxH set, also fills "origH" value' => [
+            [100, 900],
+            '50',
+            '',
+            ['maxH' => 300],
+            true,
+            [
+                'crs' => false,
+                'origW' => 50,
+                'origH' => 300,
+                'max' => 1,
+                0 => 33,
+                1 => 300,
+            ],
+        ];
+        yield 'width from original image and explicitly given scales an image up' => [
+            [100, 900],
+            '150',
+            '',
+            [],
+            true,
+            [
+                'crs' => false,
+                'origW' => 150,
+                'origH' => 0,
+                'max' => 0,
+                0 => 150,
+                1 => 1350,
+            ],
+        ];
+        yield 'width from original image and explicitly given scales an image up but is disabled' => [
+            [100, 900],
+            '150',
+            '',
+            [],
+            false,
+            [
+                'crs' => false,
+                'origW' => 150,
+                'origH' => 0,
+                'max' => 0,
+                0 => 100,
+                1 => 900,
+            ],
+            false,
+        ];
+        yield 'min width explicitly given scales an image up but is disabled resulting in do not keep aspect ratio' => [
+            [100, 900],
+            '',
+            '',
+            ['minW' => 150],
+            false,
+            [
+                'crs' => false,
+                'origW' => 0,
+                'origH' => 0,
+                'max' => 0,
+                0 => 150,
+                1 => 900,
+            ],
+            false,
+        ];
+        yield 'no orig image given monitors "origW"' => [
+            [0, 0],
+            '50',
+            '100',
+            [],
+            true,
+            [
+                'crs' => false,
+                'origW' => 50,
+                'origH' => 100,
+                'max' => 0,
+                0 => 0,
+                1 => 0,
+            ],
+        ];
+        yield 'Incoming instructions use "m" in width with given height' => [
+            [100, 900],
+            '50m',
+            '800',
+            [],
+            true,
+            [
+                'crs' => false,
+                'origW' => 50,
+                'origH' => 800,
+                'max' => 1,
+                0 => 50,
+                1 => 450,
+            ],
+        ];
+        yield 'Incoming instructions use "m" in width without height' => [
+            [100, 900],
+            '50m',
+            '',
+            [],
+            true,
+            [
+                'crs' => false,
+                'origW' => 50,
+                'origH' => 0,
+                'max' => 1,
+                0 => 50,
+                1 => 450,
+            ],
+        ];
+        yield 'Incoming instructions use "c" in width with given height' => [
+            [100, 900],
+            '50c',
+            '800',
+            [],
+            true,
+            [
+                'cropH' => 0,
+                'cropV' => 0,
+                'crs' => true,
+                'origW' => 50,
+                'origH' => 800,
+                'max' => 0,
+                0 => 89,
+                1 => 800,
+            ],
+        ];
+        yield 'Incoming instructions use "c" in width but without height' => [
+            [100, 900],
+            '50c',
+            '',
+            [],
+            true,
+            [
+                'cropH' => 0,
+                'cropV' => 0,
+                'crs' => true,
+                'origW' => 50,
+                'origH' => 0,
+                'max' => 0,
+                0 => 50,
+                1 => 450,
+            ],
+        ];
+        yield 'Incoming instructions use "c" in width on both sides' => [
+            [100, 900],
+            '50c20',
+            '650c40',
+            [],
+            true,
+            [
+                'cropH' => 20,
+                'cropV' => 40,
+                'crs' => true,
+                'origW' => 50,
+                'origH' => 650,
+                'max' => 0,
+                0 => 72,
+                1 => 650,
+            ],
+        ];
+        yield 'Incoming instructions use "c" in width on both sides with given height' => [
+            [100, 900],
+            '50c20',
+            '800',
+            [],
+            true,
+            [
+                'cropH' => 20,
+                'cropV' => 0,
+                'crs' => true,
+                'origW' => 50,
+                'origH' => 800,
+                'max' => 0,
+                0 => 89,
+                1 => 800,
             ],
         ];
     }
@@ -106,9 +289,11 @@ final class GraphicalFunctionsTest extends UnitTestCase
      * @test
      * @dataProvider getScaleForImageDataProvider
      */
-    public function getScaleForImage($info, $width, $height, $options, $expected): void
+    public function getScaleForImage(array $info, string $width, string $height, array $options, bool $mayScaleUp, array $expected): void
     {
-        $result = (new GraphicalFunctions())->getImageScale($info, $width, $height, $options);
+        $GLOBALS['TYPO3_CONF_VARS']['GFX']['processor_allowUpscaling'] = $mayScaleUp;
+        $subject = new GraphicalFunctions();
+        $result = $subject->getImageScale($info, $width, $height, $options);
         self::assertSame($result, $expected);
     }
 
