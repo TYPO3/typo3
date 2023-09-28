b56bac9b4466f22cf1ba772a2c339ba2645b7d08
Benjamin Franzke <ben@bnf.dev>
2023-09-28 23:38:40 +0200

    [BUGFIX] Improve CKEditor5 whitespace plugin usability
     * Use insertText command as provided by @ckeditor/ckeditor5-typing
       to match the behaviour of the upstream specialcharacters plugin:
       * Place cursor after inserted text
       * Replace existing selection when applying the button/shortcut
       * Disable button in source-editing mode
         (also fix ckeditor5 integration to work in case source-editing
         mode is disabled, to be able to test/verify that case)
     * Add tooltip to the toolbar button
     * Add a shortcut hint to the toolbar button tooltip
     * Show consecutive softhyphens
     * Show selections on softhypens and non-breaking-spaces
    
    Resolves: #101728
    Releases: main, 12.4
    Change-Id: I8c38a04a3c24de353edef921cc193f3fad718b4f
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/81296
    Reviewed-by: Oliver Hader <oliver.hader@typo3.org>
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Stefan Bürk <stefan@buerk.tech>
    Tested-by: Benjamin Franzke <ben@bnf.dev>
    Reviewed-by: Stefan Bürk <stefan@buerk.tech>
    Tested-by: Oliver Hader <oliver.hader@typo3.org>
    Reviewed-by: Benjamin Franzke <ben@bnf.dev>

--- Build/Sources/TypeScript/rte_ckeditor/plugin/whitespace.ts
+++ Build/Sources/TypeScript/rte_ckeditor/plugin/whitespace.ts
@@ -1,4 +1,5 @@
-import { Core, UI, Utils } from '@typo3/ckeditor5-bundle';
+import { Core, UI, Utils, Typing } from '@typo3/ckeditor5-bundle';
+import type { InsertTextCommand } from '@ckeditor/ckeditor5-typing';
 
 /**
  * CKEditor5 Whitespace Plugin
@@ -18,17 +19,11 @@ import { Core, UI, Utils } from '@typo3/ckeditor5-bundle';
  */
 export default class Whitespace extends Core.Plugin {
   static readonly pluginName = 'Whitespace';
+  static readonly requires = [ Typing.Typing ] as const;
 
   public init(): void {
     const editor = this.editor;
-
-    editor.ui.componentFactory.add('softhyphen', locale => {
-      const button = new UI.ButtonView(locale);
-      button.label = 'Soft-Hyphen';
-      button.icon = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" xml:space="preserve"><path d="M4.25 3C3.082 4.683 2 6.917 2 10.026 2 13.083 3.114 15.282 4.25 17H3c-1.008-1.425-2-3.624-2-6.974.016-3.384.992-5.583 2-7.026h1.25zM17 3c1.008 1.443 1.984 3.642 2 7.026 0 3.35-.992 5.549-2 6.974h-1.25c1.136-1.718 2.25-3.917 2.25-6.974 0-3.11-1.082-5.343-2.25-7.026H17zM6 9h8v2H6z"/></svg>';
-      button.on('execute', () => this.insertSoftHyphen());
-      return button;
-    });
+    const inputCommand: InsertTextCommand = editor.commands.get('insertText');
 
     // CKEditor should map Ctrl to Cmd on MacOs, but for
     // some reason the shortcut is blocked and cannot be
@@ -37,6 +32,18 @@ export default class Whitespace extends Core.Plugin {
     // For this reason we are using "Alt" as controlKey on MacOS.
     // All other System use "Ctrl" as controlKey.
     const controlKey = Utils.env.isMac ? 'Alt' : 'Ctrl';
+
+    editor.ui.componentFactory.add('softhyphen', locale => {
+      const button = new UI.ButtonView(locale);
+      button.label = 'Soft-Hyphen';
+      button.icon = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" xml:space="preserve"><path d="M4.25 3C3.082 4.683 2 6.917 2 10.026 2 13.083 3.114 15.282 4.25 17H3c-1.008-1.425-2-3.624-2-6.974.016-3.384.992-5.583 2-7.026h1.25zM17 3c1.008 1.443 1.984 3.642 2 7.026 0 3.35-.992 5.549-2 6.974h-1.25c1.136-1.718 2.25-3.917 2.25-6.974 0-3.11-1.082-5.343-2.25-7.026H17zM6 9h8v2H6z"/></svg>';
+      button.keystroke = `${controlKey}+Shift+-`;
+      button.tooltip = true;
+      button.bind('isEnabled').to(inputCommand);
+      button.on('execute', () => this.insertSoftHyphen());
+      return button;
+    });
+
     editor.keystrokes.set([controlKey, 'Shift', 189], (data, cancel) => {
       this.insertSoftHyphen();
       cancel();
@@ -55,19 +62,23 @@ export default class Whitespace extends Core.Plugin {
 
         const viewWriter = conversionApi.writer;
         const chunks = data.item.data
-          .split(/([\u00AD,\u00A0])/)
+          // Using a regex caputure group in order for the
+          // split chars to be included in the `chunks` array
+          .split(/([\u00AD\u00A0])/)
           .filter((value: string) => value !== '');
         let currentPosition = data.range.start;
 
         chunks.forEach((chunk: string) => {
+          const text = chunk === '\u00AD' ? '-' : chunk;
           viewWriter.insert(
             conversionApi.mapper.toViewPosition(currentPosition),
-            viewWriter.createText(chunk)
+            viewWriter.createText(text)
           );
 
           if (chunk === '\u00AD' || chunk === '\u00A0') {
             const className = chunk === '\u00AD' ? 'softhyphen' : 'nbsp';
-            const wrapper = viewWriter.createAttributeElement('span', { class: 'ck ck-' + className });
+            const id = Math.random().toString(16).slice(2);
+            const wrapper = viewWriter.createAttributeElement('span', { class: `ck ck-${className}` }, { id });
             const wrapperRange = viewWriter.createRange(
               conversionApi.mapper.toViewPosition(currentPosition),
               conversionApi.mapper.toViewPosition(currentPosition.getShiftedBy(chunk.length))
@@ -84,18 +95,14 @@ export default class Whitespace extends Core.Plugin {
   private insertNonBreakingSpace(): void {
     const editor = this.editor;
 
-    editor.model.change(writer => {
-      const insertPosition = editor.model.document.selection.getFirstPosition();
-      writer.insertText('\u00A0', insertPosition);
-    });
+    editor.execute('insertText', { text: '\u00A0' });
+    editor.editing.view.focus();
   }
 
   private insertSoftHyphen(): void {
     const editor = this.editor;
 
-    editor.model.change(writer => {
-      const insertPosition = editor.model.document.selection.getFirstPosition();
-      writer.insertText('\u00AD', insertPosition);
-    });
+    editor.execute('insertText', { text: '\u00AD' });
+    editor.editing.view.focus();
   }
 }
