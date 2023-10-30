a4a05e93391d1de2148917edd2215825b7e19cf4
Benjamin Franzke <ben@bnf.dev>
2023-10-30 17:35:40 +0100

    [TASK] Respect the AsCommand `hidden` constructor argument
    Symfony's AsCommand attribute encodes `hidden = true` into
    a trailing `|` for the name attribute. Drop that for our
    command list and mark the command as hidden in the command
    registry.
    
    Resolves: #102292
    Related: #101567
    Releases: main, 12.4
    Change-Id: I6415e3f5525764aab336bab20432c032e0665632
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/81660
    Reviewed-by: Benjamin Franzke <ben@bnf.dev>
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Benjamin Franzke <ben@bnf.dev>

---
+++ typo3/sysext/core/Tests/Functional/Fixtures/Extensions/test_di/Classes/Command/HiddenTestCommand.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3Tests\TestDi\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(name: 'testdi:ascommand:hidden', description: 'This is a hidden command.', hidden: true)]
+class HiddenTestCommand extends Command
+{
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        return Command::SUCCESS;
+    }
+}
