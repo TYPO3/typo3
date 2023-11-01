70a2af4a2fc01cfdd52760baab082631024bbb86
Christian Kuhn <lolli@schwarzbu.ch>
2023-11-01 22:09:43 +0100

    [TASK] styleguide: Clean up CI related files
    https://github.com/TYPO3-CMS/styleguide/pull/475
    
    As a last patch in styleguide standalone repo
    before monorepo merge, we remove the CI
    infrastructure since it will be handled
    by core monorepo.

--- typo3/sysext/styleguide/ext_emconf.php
+++ typo3/sysext/styleguide/ext_emconf.php
@@ -10,11 +10,9 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '13.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '13.0.0-13.99.99',
+            'typo3' => '13.0.0',
         ],
-        'conflicts' => [
-        ],
-        'suggests' => [
-        ],
+        'conflicts' => [],
+        'suggests' => [],
     ],
 ];
