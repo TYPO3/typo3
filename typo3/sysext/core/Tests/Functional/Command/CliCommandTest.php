289df6571d75668541662714f176d0e8534be4d2
Jochen Roth <jochen.roth@b13.com>
2023-11-20 18:56:42 +0100

    [TASK] Move cli command tests from acceptance to functional tests
    Currently, cli commands are tested with codeception acceptance tests.
    This takes quite some time to test all CLI commands available.
    
    This has been change to run all CLI commands using functional testing
    speeds up this process.
    
    Resolves: #102417
    Releases: main, 12.4
    Change-Id: I473c6f50796fe243ef4cd5166d313b68c1fcdddc
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/81809
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Reviewed-by: Benjamin Franzke <ben@bnf.dev>
    Tested-by: Oliver Bartsch <bo@cedev.de>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Benjamin Franzke <ben@bnf.dev>
    Reviewed-by: Oliver Bartsch <bo@cedev.de>

---
+++ typo3/sysext/core/Tests/Functional/Command/CliCommandTest.php
@@ -0,0 +1,89 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3\CMS\Core\Tests\Functional\Command;
+
+use TYPO3\CMS\Core\Tests\Functional\SiteHandling\SiteBasedTestTrait;
+
+final class CliCommandTest extends AbstractCommandTestCase
+{
+    use SiteBasedTestTrait;
+
+    protected const LANGUAGE_PRESETS = [
+        'EN' => ['id' => 0, 'title' => 'English', 'locale' => 'en_US.UTF8'],
+    ];
+
+    protected array $coreExtensionsToLoad = [
+        'lowlevel',
+        'redirects',
+        'extensionmanager',
+        'scheduler',
+        'workspaces',
+        'styleguide',
+    ];
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->writeSiteConfiguration(
+            'show-me',
+            $this->buildSiteConfiguration(8000, 'https://show.me/')
+        );
+    }
+
+    public static function commandTestDataProvider(): array
+    {
+        return [
+            ['command' => 'styleguide:generate tca', 'args' => '', 'code' => 0],
+            ['command' => 'styleguide:generate frontend', 'args' => '', 'code' => 0],
+            ['command' => 'cleanup:localprocessedfiles', 'args' => '-v', 'code' => 0],
+            ['command' => 'cache:flush', 'args' => '', 'code' => 0],
+            ['command' => 'cache:warmup', 'args' => '', 'code' => 0],
+            ['command' => 'cleanup:flexforms', 'args' => '', 'code' => 0],
+            ['command' => 'cleanup:deletedrecords', 'args' => '', 'code' => 0],
+            ['command' => 'cleanup:orphanrecords', 'args' => '', 'code' => 0],
+            ['command' => 'cleanup:previewlinks', 'args' => '', 'code' => 0],
+            ['command' => 'cleanup:versions', 'args' => '', 'code' => 0],
+            ['command' => 'extension:list', 'args' => '', 'code' => 0],
+            ['command' => 'extension:setup', 'args' => '', 'code' => 0],
+            ['command' => 'extension:deactivate workspaces', 'args' => '', 'code' => 0],
+            ['command' => 'extension:activate workspaces', 'args' => '', 'code' => 0],
+            ['command' => 'language:update', 'args' => '', 'code' => 0],
+            ['command' => 'mailer:spool:send', 'args' => '', 'code' => 1],
+            ['command' => 'redirects:checkintegrity', 'args' => '', 'code' => 0],
+            ['command' => 'redirects:cleanup', 'args' => '', 'code' => 0],
+            ['command' => 'referenceindex:update', 'args' => '--check', 'code' => 0],
+            ['command' => 'scheduler:run', 'args' => '', 'code' => 0],
+            ['command' => 'site:list', 'args' => '', 'code' => 0],
+            ['command' => 'site:show show-me', 'args' => '', 'code' => 0],
+            ['command' => 'syslog:list', 'args' => '', 'code' => 0],
+            ['command' => 'upgrade:list', 'args' => '', 'code' => 0],
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider commandTestDataProvider
+     */
+    public function cliCommand(string $command, string $args, int $expectedExitCode): void
+    {
+        $result = $this->executeConsoleCommand($command, $args);
+
+        self::assertEquals($expectedExitCode, $result['status']);
+    }
+}
