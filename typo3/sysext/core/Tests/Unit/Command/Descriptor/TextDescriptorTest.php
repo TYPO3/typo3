85829d0062c5aadb82e4e433c76d03f9031863b7
Tim Weisenberger <tim.weisenberger@netlogix.de>
2023-11-21 20:35:55 +0100

    [BUGFIX] Set default value for command description
    The function `TextDescriptor::describeApplication` did throw an
    unhandled exception if the TYPO3 CMS installation had a registered
    CLI command without a description.
    
    This has now been fixed. In case a command has no description defined
    an empty string is used.
    
    Additionally, some tests were added.
    
    Resolves: #102360
    Releases: main, 12.4
    Change-Id: I01df74b743b99bfaeef08e2b4ffbac004c2937a1
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/81788
    Tested-by: Benjamin Franzke <ben@bnf.dev>
    Tested-by: core-ci <typo3@b13.com>
    Reviewed-by: Benjamin Franzke <ben@bnf.dev>

---
+++ typo3/sysext/core/Tests/Unit/Command/Descriptor/TextDescriptorTest.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3\CMS\Core\Tests\Unit\Command\Descriptor;
+
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Output\OutputInterface;
+use TYPO3\CMS\Core\Command\Descriptor\TextDescriptor;
+use TYPO3\CMS\Core\Console\CommandRegistry;
+use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
+
+final class TextDescriptorTest extends UnitTestCase
+{
+    /**
+     * @test
+     */
+    public function ensureEmptyStringIsSetForDescriptionIfNoDescriptionForCommandIsGiven(): void
+    {
+        $commandRegistry = $this->createMock(CommandRegistry::class);
+        $commandRegistry
+            ->method('filter')
+            ->willReturn([
+                'command1' => [
+                    'name' => 'command1',
+                    'description' => 'description1',
+                ],
+                'command2' => [
+                    'name' => 'command2',
+                ],
+            ]);
+
+        $output = $this->createMock(OutputInterface::class);
+
+        $matcher = self::exactly(2);
+        $output
+            ->expects($matcher)
+            ->method('write')
+            ->willReturnCallback(function (string $description) use ($matcher) {
+                match ($matcher->numberOfInvocations()) {
+                    1 => self::assertEquals("command1   description1\n", $description),
+                    2 => self::assertEquals("command2   \n", $description),
+                    default => self::fail('Unexpected number of invocations')
+                };
+            });
+
+        $subject = $this->getAccessibleMock(TextDescriptor::class, null, [$commandRegistry, false]);
+        $subject->_set('output', $output);
+        $subject->_call(
+            'describeApplication',
+            $this->createMock(Application::class),
+            ['raw_text' => true]
+        );
+    }
+}
