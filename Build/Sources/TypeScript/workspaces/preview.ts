1297613aa87404b742cae4134259ac591c1106ec
Andreas Nedbal <andy@pixelde.su>
2023-11-22 18:27:28 +0100

    [TASK] Remove jQuery from workspace preview
    Resolves: #102465
    Releases: main
    Change-Id: I0f20dd7b5a68d5dd857e558a5e0a8dbfe09352eb
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/81868
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Andreas Kienast <a.fernandez@scripting-base.de>
    Reviewed-by: Jochen Roth <rothjochen@gmail.com>
    Tested-by: Jochen Roth <rothjochen@gmail.com>
    Reviewed-by: Andreas Kienast <a.fernandez@scripting-base.de>

--- Build/Sources/TypeScript/workspaces/preview.ts
+++ Build/Sources/TypeScript/workspaces/preview.ts
@@ -14,12 +14,12 @@
 import { AjaxResponse } from '@typo3/core/ajax/ajax-response';
 import { SeverityEnum } from '@typo3/backend/enum/severity';
 import DocumentService from '@typo3/core/document-service';
-import $ from 'jquery';
 import Modal from '@typo3/backend/modal';
 import Utility from '@typo3/backend/utility';
 import Workspaces from './workspaces';
 import ThrottleEvent from '@typo3/core/event/throttle-event';
 import '@typo3/workspaces/renderable/send-to-stage-form';
+import RegularEvent from '@typo3/core/event/regular-event';
 
 enum Identifiers {
   topbar = '#typo3-topbar',
@@ -41,7 +41,7 @@ enum Identifiers {
  */
 class Preview extends Workspaces {
   private currentSlidePosition: number = 100;
-  private readonly elements: { [key: string]: JQuery } = {};
+  private readonly elements: { [key: string]: HTMLElement } = {};
 
   constructor() {
     super();
@@ -55,28 +55,26 @@ class Preview extends Workspaces {
 
   /**
    * Calculate the available space based on the viewport height
-   *
-   * @returns {Number}
    */
   private static getAvailableSpace(): number {
-    const $viewportHeight = $(window).height();
-    const $topbarHeight = $(Identifiers.topbar).outerHeight();
+    const viewportHeight = document.documentElement.clientHeight;
+    const topbarHeight = (document.querySelector(Identifiers.topbar) as HTMLElement).offsetHeight;
 
-    return $viewportHeight - $topbarHeight;
+    return viewportHeight - topbarHeight;
   }
 
   /**
    * Fetches and stores often required elements
    */
   private getElements(): void {
-    this.elements.$liveView = $(Identifiers.liveView);
-    this.elements.$workspacePanel = $(Identifiers.workspacePanel);
-    this.elements.$stageSlider = $(Identifiers.stageSlider);
-    this.elements.$workspaceView = $(Identifiers.workspaceView);
-    this.elements.$stageButtonsContainer = $(Identifiers.stageButtonsContainer);
-    this.elements.$previewModeContainer = $(Identifiers.previewModeContainer);
-    this.elements.$activePreviewMode = $(Identifiers.activePreviewMode);
-    this.elements.$workspacePreview = $(Identifiers.workspacePreview);
+    this.elements.liveView = document.querySelector(Identifiers.liveView) as HTMLElement;
+    this.elements.workspacePanel = document.querySelector(Identifiers.workspacePanel) as HTMLElement;
+    this.elements.stageSlider = document.querySelector(Identifiers.stageSlider) as HTMLElement;
+    this.elements.workspaceView = document.querySelector(Identifiers.workspaceView) as HTMLElement;
+    this.elements.stageButtonsContainer = document.querySelector(Identifiers.stageButtonsContainer) as HTMLElement;
+    this.elements.previewModeContainer = document.querySelector(Identifiers.previewModeContainer) as HTMLElement;
+    this.elements.activePreviewMode = document.querySelector(Identifiers.activePreviewMode) as HTMLElement;
+    this.elements.workspacePreview = document.querySelector(Identifiers.workspacePreview) as HTMLElement;
   }
 
   /**
@@ -86,39 +84,37 @@ class Preview extends Workspaces {
     new ThrottleEvent('resize', (): void => {
       this.resizeViews();
     }, 50).bindTo(window);
-    $(document)
-      .on('click', Identifiers.discardAction, this.renderDiscardWindow)
-      .on('click', Identifiers.sendToStageAction, this.renderSendPageToStageWindow)
-      .on('click', '.t3js-workspace-recipients-selectall', (): void => {
-        $('.t3js-workspace-recipient', window.top.document).not(':disabled').prop('checked', true);
-      })
-      .on('click', '.t3js-workspace-recipients-deselectall', (): void => {
-        $('.t3js-workspace-recipient', window.top.document).not(':disabled').prop('checked', false);
-      })
-    ;
+    new RegularEvent('click', this.renderDiscardWindow.bind(this)).delegateTo(document, Identifiers.discardAction);
+    new RegularEvent('click', this.renderSendPageToStageWindow.bind(this)).delegateTo(document, Identifiers.sendToStageAction);
+    new RegularEvent('click', () => {
+      window.top.document.querySelectorAll('.t3js-workspace-recipient:not(:disabled)').forEach((element: HTMLInputElement) => {
+        element.checked = true;
+      });
+    }).delegateTo(document, '.t3js-workspace-recipients-selectall');
+    new RegularEvent('click', () => {
+      window.top.document.querySelectorAll('.t3js-workspace-recipient:not(:disabled)').forEach((element: HTMLInputElement) => {
+        element.checked = false;
+      });
+    }).delegateTo(document, '.t3js-workspace-recipients-deselectall');
 
-    new ThrottleEvent('input', this.updateSlidePosition, 25).bindTo(document.querySelector(Identifiers.stageSlider));
-    this.elements.$previewModeContainer.find('[data-preview-mode]').on('click', this.changePreviewMode);
+    new ThrottleEvent('input', this.updateSlidePosition.bind(this), 10).bindTo(document.querySelector(Identifiers.stageSlider));
+    new RegularEvent('click', this.changePreviewMode.bind(this)).delegateTo(this.elements.previewModeContainer, '[data-preview-mode]');
   }
 
   /**
    * Renders the staging buttons
-   *
-   * @param {String} buttons
    */
   private renderStageButtons(buttons: string): void {
-    this.elements.$stageButtonsContainer.html(buttons);
+    this.elements.stageButtonsContainer.innerHTML = buttons;
   }
 
   /**
    * Updates the position of the comparison slider
-   *
-   * @param {Event} e
    */
-  private readonly updateSlidePosition = (e: Event): void => {
+  private updateSlidePosition(e: Event): void {
     this.currentSlidePosition = parseInt((e.target as HTMLInputElement).value, 10);
     this.resizeViews();
-  };
+  }
 
   /**
    * Resize the views based on the current viewport height and slider position
@@ -127,19 +123,19 @@ class Preview extends Workspaces {
     const availableSpace = Preview.getAvailableSpace();
     const relativeHeightOfLiveView = (this.currentSlidePosition - 100) * -1;
     const absoluteHeightOfLiveView = Math.round(Math.abs(availableSpace * relativeHeightOfLiveView / 100));
-    const outerHeightDifference = this.elements.$liveView.outerHeight() - this.elements.$liveView.height();
+    const outerHeightDifference = this.elements.liveView.offsetHeight - this.elements.liveView.clientHeight;
 
-    this.elements.$workspacePreview.height(availableSpace);
+    this.elements.workspacePreview.style.height = availableSpace + 'px';
 
-    if (this.elements.$activePreviewMode.data('activePreviewMode') === 'slider') {
-      this.elements.$liveView.height(absoluteHeightOfLiveView - outerHeightDifference);
+    if (this.elements.activePreviewMode.dataset.activePreviewMode === 'slider') {
+      this.elements.liveView.style.height = (absoluteHeightOfLiveView - outerHeightDifference) + 'px';
     }
   }
 
   /**
    * Renders the discard window
    */
-  private readonly renderDiscardWindow = (): void => {
+  private renderDiscardWindow(): void {
     const modal = Modal.confirm(
       TYPO3.lang['window.discardAll.title'],
       TYPO3.lang['window.discardAll.message'],
@@ -162,7 +158,7 @@ class Preview extends Workspaces {
       ],
     );
     modal.addEventListener('button.clicked', (e: Event): void => {
-      if ((<HTMLAnchorElement>e.target).name === 'ok') {
+      if ((e.target as HTMLButtonElement).name === 'ok') {
         this.sendRemoteRequest([
           this.generateRemoteActionsPayload('discardStagesFromPage', [TYPO3.settings.Workspaces.id]),
           this.generateRemoteActionsPayload('updateStageChangeButtons', [TYPO3.settings.Workspaces.id]),
@@ -170,18 +166,17 @@ class Preview extends Workspaces {
           modal.hideModal();
           this.renderStageButtons((await response.resolve())[1].result);
           // Reloading live view IFRAME
-          this.elements.$workspaceView.attr('src', this.elements.$workspaceView.attr('src'));
+          this.elements.workspaceView.setAttribute('src', this.elements.workspaceView.getAttribute('src'));
         });
       }
     });
-  };
+  }
 
   /**
    * Renders the "send page to stage" window
    */
-  private readonly renderSendPageToStageWindow = (e: JQueryEventObject): void => {
-    const me = (<HTMLElement>e.currentTarget);
-    const direction = me.dataset.direction;
+  private renderSendPageToStageWindow(e: Event, target: HTMLElement): void {
+    const direction = target.dataset.direction;
     let actionName;
 
     if (direction === 'prev') {
@@ -199,11 +194,11 @@ class Preview extends Workspaces {
       const resolvedResponse = await response.resolve();
       const modal = this.renderSendToStageWindow(resolvedResponse);
       modal.addEventListener('button.clicked', (modalEvent: Event): void => {
-        const target = modalEvent.target as HTMLButtonElement;
-        if (target.name === 'ok') {
+        const modalTarget = modalEvent.target as HTMLButtonElement;
+        if (modalTarget.name === 'ok') {
           const serializedForm = Utility.convertFormToObject(modal.querySelector('form'));
           serializedForm.affects = resolvedResponse[0].result.affects;
-          serializedForm.stageId = me.dataset.stageId;
+          serializedForm.stageId = parseInt(target.dataset.stageId, 10);
 
           this.sendRemoteRequest([
             this.generateRemoteActionsPayload('sentCollectionToStage', [serializedForm]),
@@ -216,38 +211,35 @@ class Preview extends Workspaces {
         }
       });
     });
-  };
+  }
 
   /**
    * Changes the preview mode
-   *
-   * @param {Event} e
    */
-  private readonly changePreviewMode = (e: JQueryEventObject): void => {
+  private changePreviewMode(e: Event, target: HTMLElement): void {
     e.preventDefault();
 
-    const $trigger = $(e.currentTarget);
-    const currentPreviewMode = this.elements.$activePreviewMode.data('activePreviewMode');
-    const newPreviewMode = $trigger.data('previewMode');
+    const currentPreviewMode = this.elements.activePreviewMode.dataset.activePreviewMode;
+    const newPreviewMode = target.dataset.previewMode;
 
-    this.elements.$activePreviewMode.text($trigger.text()).data('activePreviewMode', newPreviewMode);
-    this.elements.$workspacePreview.parent()
-      .removeClass('preview-mode-' + currentPreviewMode)
-      .addClass('preview-mode-' + newPreviewMode);
+    this.elements.activePreviewMode.textContent = target.textContent;
+    this.elements.activePreviewMode.dataset.activePreviewMode = newPreviewMode;
+    this.elements.workspacePreview.parentElement.classList.remove('preview-mode-' + currentPreviewMode)
+    this.elements.workspacePreview.parentElement.classList.add('preview-mode-' + newPreviewMode);
 
     if (newPreviewMode === 'slider') {
-      this.elements.$stageSlider.parent().toggle(true);
+      this.elements.stageSlider.parentElement.style.display = '';
       this.resizeViews();
     } else {
-      this.elements.$stageSlider.parent().toggle(false);
+      this.elements.stageSlider.parentElement.style.display = 'none';
 
       if (newPreviewMode === 'vbox') {
-        this.elements.$liveView.height('100%');
+        this.elements.liveView.style.height = '100%';
       } else {
-        this.elements.$liveView.height('50%');
+        this.elements.liveView.style.height = '50%';
       }
     }
-  };
+  }
 }
 
 export default new Preview();
