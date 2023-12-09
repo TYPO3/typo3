eda26b6b4006973c1abc7898961845093dc1b518
Nikita Hovratov <nikita.h@live.de>
2023-12-09 21:25:32 +0100

    [TASK] Remove layout from subtypes_excludelist in indexedsearch_pi2
    It is unclear why `layout` was excluded for the indexed search plugin.
    In contrary to `pages` and `recursive` this field has no functionality
    for extbase plugin.
    
    In preparation for eventual deprecation of plugins of type list, this
    exclusion is removed.
    
    Resolves: #102642
    Releases: main
    Change-Id: I714714bf9d99ea7c65c2461684ed873b126bd55d
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/82161
    Tested-by: Stefan Bürk <stefan@buerk.tech>
    Reviewed-by: Stefan Bürk <stefan@buerk.tech>
    Reviewed-by: Oliver Bartsch <bo@cedev.de>
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Oliver Bartsch <bo@cedev.de>

--- typo3/sysext/indexed_search/Configuration/TCA/Overrides/tt_content.php
+++ typo3/sysext/indexed_search/Configuration/TCA/Overrides/tt_content.php
@@ -3,12 +3,12 @@
 defined('TYPO3') or die();
 
 call_user_func(static function () {
-    // Registers FE plugin and hide layout, pages and recursive fields in BE
+    // Registers FE plugin and hides pages and recursive fields in BE
     $pluginSignature = \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
         'IndexedSearch',
         'Pi2',
         'LLL:EXT:indexed_search/Resources/Private/Language/locallang_db.xlf:plugin_title',
         'mimetypes-x-content-form-search'
     );
-    $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,pages,recursive';
+    $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'pages,recursive';
 });
