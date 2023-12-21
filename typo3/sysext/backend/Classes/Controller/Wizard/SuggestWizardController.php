8e54ff8ebb67d79548dd793d5c18fb406955fc05
Markus Klein <markus.klein@typo3.org>
2023-12-21 20:54:59 +0100

    [BUGFIX] Avoid PHP warning when checking TCA hideTable
    Resolves: #102713
    Releases: main, 12.4, 11.5
    Change-Id: I18c841d9085221f4b6ff826600965b159af7971b
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/82245
    Tested-by: core-ci <typo3@b13.com>
    Reviewed-by: Nikita Hovratov <nikita.h@live.de>
    Tested-by: Nikita Hovratov <nikita.h@live.de>

--- typo3/sysext/backend/Classes/Controller/Wizard/SuggestWizardController.php
+++ typo3/sysext/backend/Classes/Controller/Wizard/SuggestWizardController.php
@@ -190,7 +190,7 @@ class SuggestWizardController
      */
     protected function isTableHidden(array $tableConfig)
     {
-        return (bool)$tableConfig['ctrl']['hideTable'];
+        return (bool)($tableConfig['ctrl']['hideTable'] ?? false);
     }
 
     /**
