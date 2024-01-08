13a7acc01f367c0b3188804a2ad579b0c0e53f83
Torben Hansen <derhansen@gmail.com>
2024-01-08 19:23:50 +0100

    [TASK] Consider soft delete in file reference delete method
    With #102770 the `delete()` method for a FileReference object has
    been implemented. This was however performed using a hard delete
    of the file reference record in the database, which is not
    appropriate, since the delete functionality must take into account,
    if the table is configured for soft or hard deletes.
    
    This patch changes the implementation, so the file reference is
    either hard or soft deleted depending on the TCA configuration.
    If the delete operation was successful, the reference index
    for affected records is now updated. Additionally, functional
    tests are extended to ensure, that the file used in the file
    reference remains when the file reference is deleted and that
    the reference index is updated accordingly.
    
    Resolves: #102785
    Related: #102770
    Releases: main, 12.4
    Change-Id: I43beb066c2259441aa2ed7e3fba0e242b7d8fbbb
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/82404
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Reviewed-by: Stefan Bürk <stefan@buerk.tech>
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Stefan Bürk <stefan@buerk.tech>

--- typo3/sysext/core/Tests/Functional/Resource/FileReferenceTest.php
+++ typo3/sysext/core/Tests/Functional/Resource/FileReferenceTest.php
@@ -18,33 +18,49 @@ declare(strict_types=1);
 namespace TYPO3\CMS\Core\Tests\Functional\Resource;
 
 use PHPUnit\Framework\Attributes\Test;
-use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Core\Environment;
 use TYPO3\CMS\Core\Resource\FileReference;
 use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
 
 final class FileReferenceTest extends FunctionalTestCase
 {
+    /**
+     * @var array<string, non-empty-string>
+     */
+    protected array $pathsToProvideInTestInstance = [
+        'typo3/sysext/core/Tests/Functional/Resource/Fixtures/ProcessedFileTest.jpg' => 'fileadmin/ProcessedFileTest.jpg',
+    ];
+
     public function setUp(): void
     {
         parent::setUp();
-        $this->importCSVDataSet(__DIR__ . '/Fixtures/FileReference.csv');
+        $this->importCSVDataSet(__DIR__ . '/Fixtures/FileReference/FileReference.csv');
+    }
+
+    #[Test]
+    public function fileReferenceCanBeSoftDeleted(): void
+    {
+        $fileReference = new FileReference(['uid' => 1, 'uid_local' => 1, 'tablenames' => 'tt_content', 'uid_foreign' => 1]);
+        self::assertTrue($fileReference->delete());
+
+        // Ensure file reference is soft deleted and reference index is clear
+        $this->assertCSVDataSet(__DIR__ . '/Fixtures/FileReference/FileReferenceSoftDelete.csv');
+
+        // Ensure file of file relation is not deleted
+        self::assertFileExists(Environment::getPublicPath() . '/fileadmin/ProcessedFileTest.jpg');
     }
 
     #[Test]
     public function fileReferenceCanBeDeleted(): void
     {
-        $fileReference = new FileReference(['uid' => 1, 'uid_local' => 1]);
+        unset($GLOBALS['TCA']['sys_file_reference']['ctrl']['delete']);
+        $fileReference = new FileReference(['uid' => 1, 'uid_local' => 1, 'tablenames' => 'tt_content', 'uid_foreign' => 1]);
         self::assertTrue($fileReference->delete());
 
-        // Ensure file reference is really deleted in table
-        $row = (new ConnectionPool())
-            ->getConnectionForTable('sys_file_reference')
-            ->select(
-                ['uid'],
-                'sys_file_reference',
-                ['uid' => 1]
-            )
-            ->fetchAssociative();
-        self::assertFalse($row);
+        // Ensure file reference is soft deleted and reference index is clear
+        $this->assertCSVDataSet(__DIR__ . '/Fixtures/FileReference/FileReferenceHardDelete.csv');
+
+        // Ensure file of file relation is not deleted
+        self::assertFileExists(Environment::getPublicPath() . '/fileadmin/ProcessedFileTest.jpg');
     }
 }
