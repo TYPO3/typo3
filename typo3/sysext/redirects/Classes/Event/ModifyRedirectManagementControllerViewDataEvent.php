48f13c6de8d40f7dd3ea770748b8d086769a6829
Chris Müller <typo3@krue.ml>
2024-01-28 08:38:58 +0100

    [TASK] Explain methods in ModifyRedirectManagementControllerViewDataEvent
    Some methods contain information of possible use cases which may be
    valuable to the user. The docblocks are also rendered in the documentation.
    
    Resolves: #102963
    Related: #99802
    Related: #99485
    Releases: main
    Change-Id: Ib3b5fefbe7c4e09ca0794cd474727a4fa27207c1
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/82681
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Reviewed-by: Guido Schmechel <guido.schmechel@brandung.de>
    Tested-by: Oliver Klee <typo3-coding@oliverklee.de>
    Tested-by: core-ci <typo3@b13.com>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Reviewed-by: Oliver Klee <typo3-coding@oliverklee.de>
    Tested-by: Guido Schmechel <guido.schmechel@brandung.de>

--- typo3/sysext/redirects/Classes/Event/ModifyRedirectManagementControllerViewDataEvent.php
+++ typo3/sysext/redirects/Classes/Event/ModifyRedirectManagementControllerViewDataEvent.php
@@ -41,86 +41,137 @@ final class ModifyRedirectManagementControllerViewDataEvent
         private array $integrityStatusCodes,
     ) {}
 
+    /**
+     * Return the demand object used to retrieve the redirects.
+     */
     public function getDemand(): Demand
     {
         return $this->demand;
     }
 
+    /**
+     * Can be used to set the demand object.
+     */
     public function setDemand(Demand $demand): void
     {
         $this->demand = $demand;
     }
 
+    /**
+     * Return the retrieved redirects.
+     */
     public function getRedirects(): array
     {
         return $this->redirects;
     }
 
+    /**
+     * Can be used to set the redirects, for example, after enriching redirect fields.
+     */
     public function setRedirects(array $redirects): void
     {
         $this->redirects = $redirects;
     }
 
+    /**
+     * Return the current PSR-7 request.
+     */
     public function getRequest(): ServerRequestInterface
     {
         return $this->request;
     }
 
+    /**
+     * Returns the hosts to be used for the host filter select box.
+     */
     public function getHosts(): array
     {
         return $this->hosts;
     }
 
+    /**
+     * Can be used to update which hosts are available in the filter select box.
+     */
     public function setHosts(array $hosts): void
     {
         $this->hosts = $hosts;
     }
 
+    /**
+     * Returns the status codes for the filter select box.
+     */
     public function getStatusCodes(): array
     {
         return $this->statusCodes;
     }
 
+    /**
+     * Can be used to update which status codes are available in the filter select box.
+     */
     public function setStatusCodes(array $statusCodes): void
     {
         $this->statusCodes = $statusCodes;
     }
 
+    /**
+     * Returns creation types for the filter select box.
+     */
     public function getCreationTypes(): array
     {
         return $this->creationTypes;
     }
 
+    /**
+     * Can be used to update which creation types are available in the filter select box.
+     */
     public function setCreationTypes(array $creationTypes): void
     {
         $this->creationTypes = $creationTypes;
     }
 
+    /**
+     * Returns, if hit counter should be displayed.
+     */
     public function getShowHitCounter(): bool
     {
         return $this->showHitCounter;
     }
 
+    /**
+     * Can be used to manage, if the hit counter should be displayed.
+     */
     public function setShowHitCounter(bool $showHitCounter): void
     {
         $this->showHitCounter = $showHitCounter;
     }
 
+    /**
+     * Returns the current view object, without controller data assigned yet.
+     */
     public function getView(): ViewInterface
     {
         return $this->view;
     }
 
+    /**
+     * Can be used to assign additional data to the view.
+     */
     public function setView(ViewInterface $view): void
     {
         $this->view = $view;
     }
 
+    /**
+     * Returns all integrity status codes.
+     */
     public function getIntegrityStatusCodes(): array
     {
         return $this->integrityStatusCodes;
     }
 
+    /**
+     * Allows to set integrity status codes. It can be used to filter for integrity status codes.
+     */
     public function setIntegrityStatusCodes(array $integrityStatusCodes): void
     {
         $this->integrityStatusCodes = $integrityStatusCodes;
