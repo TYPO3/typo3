22db97b11dc3a4aee4d737d0cdf51208bc526ab2
Sascha Nowak <sascha.nowak@netlogix.de>
2024-01-29 19:23:21 +0100

    [BUGFIX] Prevent memory leak when fetching a lot of database records
    When fetching a lot of records from database the memory is nearly
    doubled in the fetchAll function. It seems php keeps references or
    creates an array copy that could not be garbage collected. Enforcing
    a manuell gc_collect_cycles does not help either.
    
    Resolves: #102986
    Releases: main, 12.4, 11.5
    Change-Id: I2da5f1e0126aa1dc867c8fb3074be4a11491f8f3
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/82725
    Tested-by: core-ci <typo3@b13.com>
    Reviewed-by: Benjamin Franzke <ben@bnf.dev>
    Tested-by: Benjamin Franzke <ben@bnf.dev>

--- typo3/sysext/core/Classes/Database/Driver/DriverResult.php
+++ typo3/sysext/core/Classes/Database/Driver/DriverResult.php
@@ -156,15 +156,11 @@ class DriverResult implements ResultInterface
     protected function mapResourceToString($record)
     {
         if (is_array($record)) {
-            return array_map(
-                static function ($value) {
-                    if (is_resource($value)) {
-                        $value = stream_get_contents($value);
-                    }
-                    return $value;
-                },
-                $record
-            );
+            foreach ($record as $k => $value) {
+                if (is_resource($value)) {
+                    $record[$k] = stream_get_contents($value);
+                }
+            }
         }
 
         return $record;
