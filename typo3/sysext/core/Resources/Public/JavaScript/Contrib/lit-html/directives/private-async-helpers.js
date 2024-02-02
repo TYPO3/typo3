bedbe0df6a5b9dac71a7ba73ebf66f5ba376d0d0
Andreas Kienast <a.fernandez@scripting-base.de>
2024-02-02 19:04:18 +0100

    [TASK] Update `lit` packages
    Executed commands:
    
        npm install --save \
            @lit/reactive-element@^2.0.4 \
            lit@^3.1.2 \
            lit-element@^4.0.4 \
            lit-html@^3.1.2
        npm run build
    
    Resolves: #103024
    Releases: main, 12.4
    Change-Id: Iec49465ce10b9da4cd2a05bcc13c09dd49508ef0
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/82770
    Reviewed-by: Benjamin Franzke <ben@bnf.dev>
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Benjamin Franzke <ben@bnf.dev>

--- typo3/sysext/core/Resources/Public/JavaScript/Contrib/lit-html/directives/private-async-helpers.js
+++ typo3/sysext/core/Resources/Public/JavaScript/Contrib/lit-html/directives/private-async-helpers.js
@@ -3,4 +3,4 @@
  * Copyright 2021 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
  */
-const t=async(t,s)=>{for await(const i of t)if(!1===await s(i))return};class s{constructor(t){this.G=t}disconnect(){this.G=void 0}reconnect(t){this.G=t}deref(){return this.G}}class i{constructor(){this.Y=void 0,this.Z=void 0}get(){return this.Y}pause(){this.Y??=new Promise((t=>this.Z=t))}resume(){this.Z?.(),this.Y=this.Z=void 0}}export{i as Pauser,s as PseudoWeakRef,t as forAwaitOf};
+const t=async(t,s)=>{for await(const i of t)if(!1===await s(i))return};class s{constructor(t){this.Y=t}disconnect(){this.Y=void 0}reconnect(t){this.Y=t}deref(){return this.Y}}class i{constructor(){this.Z=void 0,this.q=void 0}get(){return this.Z}pause(){this.Z??=new Promise((t=>this.q=t))}resume(){this.q?.(),this.Z=this.q=void 0}}export{i as Pauser,s as PseudoWeakRef,t as forAwaitOf};
