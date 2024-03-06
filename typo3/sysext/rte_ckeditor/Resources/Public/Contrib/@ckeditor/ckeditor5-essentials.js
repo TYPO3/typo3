7ede908d40bec64bc210ae590ada281477ddc471
Andreas Kienast <a.fernandez@scripting-base.de>
2024-03-06 14:18:05 +0100

    [TASK] Update ckeditor5 to v41.2
    This commit updates ckeditor5 to v41.2. See changelog at
    https://github.com/ckeditor/ckeditor5/releases/tag/v41.2.0.
    
    Executed commands:
    
        npm i --save \
            @ckeditor/ckeditor5-alignment@^41.2.0 \
            @ckeditor/ckeditor5-autoformat@^41.2.0 \
            @ckeditor/ckeditor5-basic-styles@^41.2.0 \
            @ckeditor/ckeditor5-block-quote@^41.2.0 \
            @ckeditor/ckeditor5-clipboard@^41.2.0 \
            @ckeditor/ckeditor5-code-block@^41.2.0 \
            @ckeditor/ckeditor5-core@^41.2.0 \
            @ckeditor/ckeditor5-editor-classic@^41.2.0 \
            @ckeditor/ckeditor5-engine@^41.2.0 \
            @ckeditor/ckeditor5-essentials@^41.2.0 \
            @ckeditor/ckeditor5-find-and-replace@^41.2.0 \
            @ckeditor/ckeditor5-font@^41.2.0 \
            @ckeditor/ckeditor5-heading@^41.2.0 \
            @ckeditor/ckeditor5-horizontal-line@^41.2.0 \
            @ckeditor/ckeditor5-html-support@^41.2.0 \
            @ckeditor/ckeditor5-indent@^41.2.0 \
            @ckeditor/ckeditor5-link@^41.2.0 \
            @ckeditor/ckeditor5-list@^41.2.0 \
            @ckeditor/ckeditor5-paragraph@^41.2.0 \
            @ckeditor/ckeditor5-paste-from-office@^41.2.0 \
            @ckeditor/ckeditor5-remove-format@^41.2.0 \
            @ckeditor/ckeditor5-show-blocks@^41.2.0 \
            @ckeditor/ckeditor5-source-editing@^41.2.0 \
            @ckeditor/ckeditor5-special-characters@^41.2.0 \
            @ckeditor/ckeditor5-style@^41.2.0 \
            @ckeditor/ckeditor5-table@^41.2.0 \
            @ckeditor/ckeditor5-theme-lark@^41.2.0 \
            @ckeditor/ckeditor5-ui@^41.2.0 \
            @ckeditor/ckeditor5-undo@^41.2.0 \
            @ckeditor/ckeditor5-utils@^41.2.0 \
            @ckeditor/ckeditor5-word-count@^41.2.0
        npm run build
    
    Resolves: #103295
    Releases: main, 12.4
    Change-Id: I1e0d676ac151644e09c8d7654d7a4f78b71ea661
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/83255
    Reviewed-by: Andreas Kienast <a.fernandez@scripting-base.de>
    Tested-by: Andreas Kienast <a.fernandez@scripting-base.de>
    Tested-by: core-ci <typo3@b13.com>

--- typo3/sysext/rte_ckeditor/Resources/Public/Contrib/@ckeditor/ckeditor5-essentials.js
+++ typo3/sysext/rte_ckeditor/Resources/Public/Contrib/@ckeditor/ckeditor5-essentials.js
@@ -1,5 +1,5 @@
-import{Plugin as r}from"@ckeditor/ckeditor5-core";import{Clipboard as t}from"@ckeditor/ckeditor5-clipboard";import{Enter as e,ShiftEnter as o}from"@ckeditor/ckeditor5-enter";import{SelectAll as i}from"@ckeditor/ckeditor5-select-all";import{Typing as c}from"@ckeditor/ckeditor5-typing";import{Undo as d}from"@ckeditor/ckeditor5-undo";
+import{Plugin as r}from"@ckeditor/ckeditor5-core";import{Clipboard as t}from"@ckeditor/ckeditor5-clipboard";import{Enter as o,ShiftEnter as e}from"@ckeditor/ckeditor5-enter";import{SelectAll as i}from"@ckeditor/ckeditor5-select-all";import{Typing as c}from"@ckeditor/ckeditor5-typing";import{Undo as d}from"@ckeditor/ckeditor5-undo";import{AccessibilityHelp as m}from"@ckeditor/ckeditor5-ui";
 /**
  * @license Copyright (c) 2003-2024, CKSource Holding sp. z o.o. All rights reserved.
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
- */class m extends r{static get requires(){return[t,e,i,o,c,d]}static get pluginName(){return"Essentials"}}export{m as Essentials};
+ */class k extends r{static get requires(){return[m,t,o,i,e,c,d]}static get pluginName(){return"Essentials"}}export{k as Essentials};
