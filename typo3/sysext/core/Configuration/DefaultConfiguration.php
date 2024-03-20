040be42faa3782c54dcd690fb5e7b56a90d6614f
Andreas Kienast <a.fernandez@scripting-base.de>
2024-03-20 12:51:06 +0100

    [BUGFIX] Add `_ga` to `excludedParameters`
    This commit adds `_ga` to the `excludedParameters` configuration used
    for the cHash calculation.
    
    Resolves: #103435
    Releases: main, 12.4, 11.5
    Change-Id: I5c79c9264d1d8f09f45db947d714d27b3da4fb00
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/83529
    Tested-by: JAKOTA Design Group GmbH <suchowski@jakota.de>
    Reviewed-by: Oliver Klee <typo3-coding@oliverklee.de>
    Tested-by: core-ci <typo3@b13.com>
    Reviewed-by: David Steeb <david.steeb@b13.com>
    Tested-by: Andreas Kienast <a.fernandez@scripting-base.de>
    Reviewed-by: Andreas Kienast <a.fernandez@scripting-base.de>
    Reviewed-by: Benni Mack <benni@typo3.org>
    Tested-by: David Steeb <david.steeb@b13.com>
    Reviewed-by: JAKOTA Design Group GmbH <suchowski@jakota.de>
    Tested-by: Benni Mack <benni@typo3.org>

--- typo3/sysext/core/Configuration/DefaultConfiguration.php
+++ typo3/sysext/core/Configuration/DefaultConfiguration.php
@@ -1410,6 +1410,7 @@ return [
                 'utm_creative_format',
                 'utm_marketing_tactic',
                 'gtm_debug',
+                '_ga',
                 '_gl',
                 // Google ads
                 'gad',
