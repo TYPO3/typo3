ceeec020ef4dfa9772b65727679eeaaebf03d218
Benni Mack <benni@typo3.org>
2024-04-09 15:09:11 +0200

    [RELEASE] Release of TYPO3 12.4.14
    Change-Id: Ibd990830d7ad9d43707be802c6a24a1388589d18
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/83716
    Tested-by: Benni Mack <benni@typo3.org>
    Reviewed-by: Benni Mack <benni@typo3.org>

--- typo3/sysext/redirects/Tests/Functional/Fixtures/Extensions/test_bolt/ext_emconf.php
+++ typo3/sysext/redirects/Tests/Functional/Fixtures/Extensions/test_bolt/ext_emconf.php
@@ -6,14 +6,14 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'This extension simulates b13/bolt.',
     'description' => 'This extension simulates b13/bolt.',
     'category' => 'example',
-    'version' => '13.1.0',
+    'version' => '12.4.14',
     'state' => 'beta',
     'author' => 'Stefan Bürk',
     'author_email' => 'stefan@buerk.tech',
     'author_company' => '',
     'constraints' => [
         'depends' => [
-            'typo3' => '13.1.0',
+            'typo3' => '12.4.14',
         ],
         'conflicts' => [],
         'suggests' => [],
