1e70ebf736935413b0531004839362b4fb0755a5
Benni Mack <benni@typo3.org>
2024-05-14 10:02:31 +0200

    [SECURITY] Protect frame GET parameter in tx_cms_showpic eID
    The "frame" parameter is no longer evaluated in the showpic eID as
    it allowed uncontrolled resource consumption. This parameter was
    actually never used by ContentObjectRenderer and existed since
    the initial commit and is therefore put behind a feature flag.
    
    Resolves: #103306
    Releases: main, 13.1, 12.4, 11.5
    Change-Id: I87019e58c078c8ccafc0b7ce42fe28b49dc068e4
    Security-Bulletin: TYPO3-CORE-SA-2024-010
    Security-References: CVE-2024-34358
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/84263
    Reviewed-by: Oliver Hader <oliver.hader@typo3.org>
    Tested-by: Oliver Hader <oliver.hader@typo3.org>

--- typo3/sysext/frontend/Classes/Controller/ShowImageController.php
+++ typo3/sysext/frontend/Classes/Controller/ShowImageController.php
@@ -19,6 +19,7 @@ namespace TYPO3\CMS\Frontend\Controller;
 
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use TYPO3\CMS\Core\Configuration\Features;
 use TYPO3\CMS\Core\Crypto\HashService;
 use TYPO3\CMS\Core\Exception;
 use TYPO3\CMS\Core\Http\Response;
@@ -76,7 +77,7 @@ class ShowImageController
     protected $crop;
 
     /**
-     * @var int
+     * @var int|null
      */
     protected $frame;
 
@@ -105,6 +106,10 @@ class ShowImageController
 </body>
 </html>
 EOF;
+
+    public function __construct(
+        protected readonly Features $features
+    ) {}
 
     /**
      * Init function, setting the input vars in the global space.
@@ -151,7 +156,12 @@ EOF;
             throw new Exception('File processing for local storage is denied', 1594043425);
         }
 
-        $this->frame = $this->request->getQueryParams()['frame'] ?? null;
+        if ($this->features->isFeatureEnabled('security.frontend.allowInsecureFrameOptionInShowImageController')) {
+            $frameValue = $this->request->getQueryParams()['frame'] ?? null;
+            if ($frameValue !== null && MathUtility::canBeInterpretedAsInteger($frameValue)) {
+                $this->frame = (int)$frameValue;
+            }
+        }
     }
 
     /**
