c3a5e0d7cb66a011e3db7cdb8bb014080fd359d5
Stefan Bürk <stefan@buerk.tech>
2024-05-15 17:53:09 +0200

    [TASK] Add some `ThumbnailViewHelper` functional tests
    This change adds some tests for the `EXT:backend`
    ThumbnailViewHelper, mainly to cover fix #102571.
    
    Resolves: #103846
    Related: #102571
    Releases: main, 12.4
    Change-Id: Id1acf6ddf6bc68c2dff0bfb7875859e07768e2af
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/84283
    Tested-by: Anja Leichsenring <aleichsenring@ab-softlab.de>
    Reviewed-by: Anja Leichsenring <aleichsenring@ab-softlab.de>
    Tested-by: core-ci <typo3@b13.com>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>

---
+++ typo3/sysext/backend/Tests/Functional/ViewHelpers/ThumbnailViewHelperTest.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3\CMS\Backend\Tests\Functional\ViewHelpers;
+
+use PHPUnit\Framework\Attributes\Test;
+use TYPO3\CMS\Core\Resource\ResourceFactory;
+use TYPO3\CMS\Fluid\Core\Rendering\RenderingContextFactory;
+use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
+use TYPO3Fluid\Fluid\View\TemplateView;
+
+final class ThumbnailViewHelperTest extends FunctionalTestCase
+{
+    protected array $pathsToProvideInTestInstance = [
+        'typo3/sysext/backend/Tests/Functional/ViewHelpers/Fixtures/ThumbnailViewHelper/Folders/fileadmin/' => 'fileadmin/',
+    ];
+
+    #[Test]
+    public function sysFileAsImageAttrbuteReturnsExpectedImageTag(): void
+    {
+        $this->importCSVDataSet(__DIR__ . '/Fixtures/ThumbnailViewHelper/fal_image.csv');
+        $resourceFactory = $this->get(ResourceFactory::class);
+        $file = $resourceFactory->getFileObject(1);
+        $context = $this->get(RenderingContextFactory::class)->create();
+        $context->getViewHelperResolver()->addNamespace('be', 'TYPO3\\CMS\\Backend\\ViewHelpers');
+        $context->getTemplatePaths()->setTemplateSource('<be:thumbnail image="{imageObject}" />');
+        $context->getVariableProvider()->add('imageObject', $file);
+        $expected = '<img src="fileadmin/_processed_/3/7/preview_ImageViewHelperFalTest_252565634e.jpg" width="64" height="64" alt="alt text from metadata" />';
+
+        self::assertEquals($expected, (new TemplateView($context))->render());
+    }
+
+    #[Test]
+    public function sysFileReferenceAsImageAttrbuteReturnsExpectedImageTag(): void
+    {
+        $this->importCSVDataSet(__DIR__ . '/Fixtures/ThumbnailViewHelper/fal_image.csv');
+        $resourceFactory = $this->get(ResourceFactory::class);
+        $file = $resourceFactory->getFileReferenceObject(1);
+        $context = $this->get(RenderingContextFactory::class)->create();
+        $context->getViewHelperResolver()->addNamespace('be', 'TYPO3\\CMS\\Backend\\ViewHelpers');
+        $context->getTemplatePaths()->setTemplateSource('<be:thumbnail image="{imageObject}" />');
+        $context->getVariableProvider()->add('imageObject', $file);
+        $expected = '<img src="fileadmin/_processed_/3/7/preview_ImageViewHelperFalTest_252565634e.jpg" width="64" height="64" alt="alt text from metadata" />';
+
+        self::assertEquals($expected, (new TemplateView($context))->render());
+    }
+}
