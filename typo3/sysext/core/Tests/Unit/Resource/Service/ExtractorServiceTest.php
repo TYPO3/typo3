e2aff12c4222ee5d7c85b70af007fa17afd6068b
Stefan Bürk <stefan@buerk.tech>
2024-06-08 17:17:50 +0200

    [TASK] Mitigate PHPUnit `NameAlreadyInUseException`
    PHPUnit changed the way how double creation is handled
    internally and now emits an exception if a manually defined
    `mock class name` should be registered. In some places
    within the unit tests, manual mock class names have been
    used, because the names are needed for cross referencing
    at a later point. For this, different approaches have
    been used, for example hardcoded MD5 hashes as strings
    or creating hashes of static values like `md5('1')`,
    and also reused hashes in different places or in
    tests using data providers. This now leads to an
    exception:
    
        PHPUnit\Framework\MockObject\Generator\NameAlreadyInUseException
    
        The name "b70551b2b2db62b6b15a9bbfcbd50614" is already in use
    
    This change mitigates the issue by using
    `StringUtility::getUniqueId('somePrefix')` as is already done
    in other places.
    
    As a sideeffect, four phpstan baseline entries can be removed.
    
    Used command(s):
    
      Build/Scripts/runTests.sh -s phpstanGenerateBaseline
    
    [1] https://github.com/sebastianbergmann/phpunit/pull/5804
    
    Resolves: #104005
    Releases: main, 12.4
    Change-Id: Icc558844275c9ae9f67a0e7c20daa318f5ad6b41
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/84530
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Andreas Wolf <andreas.wolf@typo3.org>
    Reviewed-by: Andreas Wolf <andreas.wolf@typo3.org>

--- typo3/sysext/core/Tests/Unit/Resource/Service/ExtractorServiceTest.php
+++ typo3/sysext/core/Tests/Unit/Resource/Service/ExtractorServiceTest.php
@@ -25,6 +25,7 @@ use TYPO3\CMS\Core\Resource\Index\ExtractorInterface;
 use TYPO3\CMS\Core\Resource\Index\ExtractorRegistry;
 use TYPO3\CMS\Core\Resource\ResourceStorage;
 use TYPO3\CMS\Core\Resource\Service\ExtractorService;
+use TYPO3\CMS\Core\Utility\StringUtility;
 use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
 
 final class ExtractorServiceTest extends UnitTestCase
@@ -105,7 +106,7 @@ final class ExtractorServiceTest extends UnitTestCase
         $fileMock->method('getType')->willReturn(FileType::IMAGE->value);
         $fileMock->method('getStorage')->willReturn($storageMock);
 
-        $extractorClass1 = md5('1');
+        $extractorClass1 = StringUtility::getUniqueId('extractor');
         $extractorObject1 = $this->getMockBuilder(ExtractorInterface::class)
             ->setMockClassName($extractorClass1)
             ->getMock();
@@ -120,7 +121,7 @@ final class ExtractorServiceTest extends UnitTestCase
             'height' => 600,
         ]);
 
-        $extractorClass2 = md5('2');
+        $extractorClass2 = StringUtility::getUniqueId('extractor');
         $extractorObject2 = $this->getMockBuilder(ExtractorInterface::class)
             ->setMockClassName($extractorClass2)
             ->getMock();
@@ -224,7 +225,7 @@ final class ExtractorServiceTest extends UnitTestCase
         $fileMock->expects(self::any())->method('getType')->willReturn(FileType::IMAGE->value);
         $fileMock->expects(self::any())->method('getStorage')->willReturn($storageMock);
 
-        $extractorClass1 = md5('1');
+        $extractorClass1 = StringUtility::getUniqueId('extractor');
         $extractorObject1 = $this->getMockBuilder(ExtractorInterface::class)
             ->setMockClassName($extractorClass1)
             ->getMock();
@@ -239,7 +240,7 @@ final class ExtractorServiceTest extends UnitTestCase
             'bar' => 'first',
         ]);
 
-        $extractorClass2 = md5('2');
+        $extractorClass2 = StringUtility::getUniqueId('extractor');
         $extractorObject2 = $this->getMockBuilder(ExtractorInterface::class)
             ->setMockClassName($extractorClass2)
             ->getMock();
