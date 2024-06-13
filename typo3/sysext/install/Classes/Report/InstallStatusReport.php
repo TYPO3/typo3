84bf86b45ea4decc50ed4771e5a17298e5ab452d
Garvin Hicking <gh@faktor-e.de>
2024-06-13 09:04:01 +0200

    [TASK] Prevent requiring "typo3conf" for Composer mode in Dir. Status
    Currently both the Install Tool and Environment > Directory Status
    sets the directory "typo3conf" as being mandatory and tries to
    create it. This is no longer needed in Composer mode.
    
    Resolves: #104074
    Releases: main, 12.4
    Change-Id: I880ec2b7ff0be642c2673bb7110ef95f3dc125d5
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/84672
    Tested-by: core-ci <typo3@b13.com>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Stefan Bürk <stefan@buerk.tech>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Reviewed-by: Mathias Brodala <mbrodala@pagemachine.de>
    Reviewed-by: Stefan Bürk <stefan@buerk.tech>

--- typo3/sysext/install/Classes/Report/InstallStatusReport.php
+++ typo3/sysext/install/Classes/Report/InstallStatusReport.php
@@ -91,7 +91,6 @@ final class InstallStatusReport implements StatusProviderInterface
             $varPath . '/transient/' => 2,
             $varPath . '/charset/' => 2,
             $varPath . '/lock/' => 2,
-            $sitePath . '/typo3conf/' => 2,
             Environment::getLabelsPath() => 0,
             $sitePath . '/' . $GLOBALS['TYPO3_CONF_VARS']['BE']['fileadminDir'] => -1,
             $sitePath . '/' . $GLOBALS['TYPO3_CONF_VARS']['BE']['fileadminDir'] . '_temp_/' => 0,
@@ -100,6 +99,8 @@ final class InstallStatusReport implements StatusProviderInterface
         // Check for writable extension folder files in non-composer mode only
         if (!Environment::isComposerMode()) {
             $checkWritable[Environment::getExtensionsPath()] = 0;
+            $checkWritable[$sitePath . '/typo3conf/'] = 2;
+
         }
 
         foreach ($checkWritable as $path => $requirementLevel) {
