f086b0dff39786f3f2825c7f3a7275f88726020a
Christian Kuhn <lolli@schwarzbu.ch>
2024-06-18 09:42:56 +0200

    [TASK] Avoid long varchar() in pages table
    Prefer TEXT over VARCHAR(2048) for pages:canonical_link.
    This gives more "room" for other bigger fields in this
    table. It is possible since #103578 allows to handle
    DEFAULT with TEXT.
    
    Resolves: #104138
    Related: #104063
    Related: #101553
    Related: #104068
    Related: #103578
    Releases: main
    Change-Id: I1bd82ce016565d83fe654331b83c930f0da49cfc
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/84748
    Reviewed-by: Anja Leichsenring <aleichsenring@ab-softlab.de>
    Reviewed-by: Georg Ringer <georg.ringer@gmail.com>
    Tested-by: Stefan Bürk <stefan@buerk.tech>
    Tested-by: core-ci <typo3@b13.com>
    Reviewed-by: Stefan Bürk <stefan@buerk.tech>
    Tested-by: Georg Ringer <georg.ringer@gmail.com>
    Tested-by: Anja Leichsenring <aleichsenring@ab-softlab.de>

--- typo3/sysext/seo/ext_tables.sql
+++ typo3/sysext/seo/ext_tables.sql
@@ -1,6 +1,4 @@
 CREATE TABLE pages (
-	# @todo: this should be text with default '' when implemented, see todo in DefaultTceSchema
-	canonical_link varchar(2048) DEFAULT '' NOT NULL,
 	# @todo: db analyzer makes this varchar which would be ok, but the default is lost. needs review
 	sitemap_priority decimal(2,1) DEFAULT '0.5' NOT NULL,
 );
