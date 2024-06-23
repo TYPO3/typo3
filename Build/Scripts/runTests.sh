33cf90cdb3dd6972618632b8a7aa95a674744959
Stefan Bürk <stefan@buerk.tech>
2024-06-23 18:15:09 +0200

    [TASK] Allow additional composer options for `-s composerInstall`
    This change modifies the `Build/Script/runTests.sh`
    command dispatcher to allow passing composer options
    to the `composer install` subcommand, for example
    
      Build/Scripts/runTests.sh -s composerInstall \
        -- --prefer-source
    
    Note that this is already possible using the generic
    `-s composer` dispatcher but would resolve some user
    confusion.
    
    This is useful to install dependencies as git
    checkouts, for example when working on packages
    like `typo3/testing-framework` changes. Albeit
    the need to remove the vendor folder first is
    needed in case of the example.
    
    Resolves: #104186
    Releases: main, 12.4, 11.5
    Change-Id: Id07c90f3b6902ad8866249563eceeb2dafbc919b
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/84870
    Tested-by: Anja Leichsenring <aleichsenring@ab-softlab.de>
    Reviewed-by: Stefan Bürk <stefan@buerk.tech>
    Tested-by: Benni Mack <benni@typo3.org>
    Reviewed-by: Benni Mack <benni@typo3.org>
    Reviewed-by: Anja Leichsenring <aleichsenring@ab-softlab.de>
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Stefan Bürk <stefan@buerk.tech>

--- Build/Scripts/runTests.sh
+++ Build/Scripts/runTests.sh
@@ -884,7 +884,8 @@ case ${TEST_SUITE} in
         SUITE_EXIT_CODE=$?
         ;;
     composerInstall)
-        ${CONTAINER_BIN} run ${CONTAINER_COMMON_PARAMS} --name composer-install-${SUFFIX} -e COMPOSER_CACHE_DIR=.cache/composer -e COMPOSER_ROOT_VERSION=${COMPOSER_ROOT_VERSION} ${IMAGE_PHP} composer install --no-progress --no-interaction
+        COMMAND=(composer install --no-progress --no-interaction "$@")
+        ${CONTAINER_BIN} run ${CONTAINER_COMMON_PARAMS} --name composer-install-${SUFFIX} -e COMPOSER_CACHE_DIR=.cache/composer -e COMPOSER_ROOT_VERSION=${COMPOSER_ROOT_VERSION} ${IMAGE_PHP} "${COMMAND[@]}"
         SUITE_EXIT_CODE=$?
         ;;
     composerInstallMax)
