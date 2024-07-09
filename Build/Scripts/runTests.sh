93545648b9024b23b96af807a5e44608449f4416
Stefan Bürk <stefan@buerk.tech>
2024-07-09 12:56:18 +0200

    [TASK] Allow PHP 8.4 in runTests.sh
    This change adds PHP8.4 as allowed version to the
    `Build/Scripts/runTests.sh` script as preparation
    to validate and check the Core and 3rd party libs
    with PHP8.4 locally.
    
    Note that unit tests already revealing deprecation
    and errors for 3rd party extension which can now
    be reported and for checked upstream updates.
    
    Resolves: #104337
    Releases: main, 12.4, 11.5
    Change-Id: I76d3e37b393c347173fcdbe874d6a0737fb76d33
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/85178
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Benni Mack <benni@typo3.org>
    Reviewed-by: Stefan Bürk <stefan@buerk.tech>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Reviewed-by: Benni Mack <benni@typo3.org>
    Tested-by: Stefan Bürk <stefan@buerk.tech>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>

--- Build/Scripts/runTests.sh
+++ Build/Scripts/runTests.sh
@@ -167,6 +167,9 @@ getPhpImageVersion() {
         8.3)
             echo -n "1.13"
             ;;
+        8.4)
+            echo -n "1.0"
+            ;;
     esac
 }
 
@@ -288,10 +291,11 @@ Options:
         Hack functional or acceptance tests into #numberOfChunks pieces and run tests of #chunk.
         Example -c 3/13
 
-    -p <8.2|8.3>
+    -p <8.2|8.3|8.4>
         Specifies the PHP minor version to be used
             - 8.2 (default): use PHP 8.2
             - 8.3: use PHP 8.3
+            - 8.4: use PHP 8.4
 
     -t sets|systemplate
         Only with -s acceptance|acceptanceComposer
@@ -442,7 +446,7 @@ while getopts ":a:b:s:c:d:i:t:p:xy:nhug" OPT; do
             ;;
         p)
             PHP_VERSION=${OPTARG}
-            if ! [[ ${PHP_VERSION} =~ ^(8.2|8.3)$ ]]; then
+            if ! [[ ${PHP_VERSION} =~ ^(8.2|8.3|8.4)$ ]]; then
                 INVALID_OPTIONS+=("${OPTARG}")
             fi
             ;;
