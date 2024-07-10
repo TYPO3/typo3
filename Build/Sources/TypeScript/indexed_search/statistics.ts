8ac29ae88a72d564ce917ce679d24e13ca63ca5c
Oliver Bartsch <bo@cedev.de>
2024-07-10 16:33:35 +0200

    [TASK] Use custom element for indexed search statistics
    This also removed a duplicated check for the element's type.
    
    Resolves: #104354
    Releases: main
    Change-Id: Ied1cb75ad556efee5a942cb89c6a65f9a8290c19
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/85218
    Reviewed-by: Jochen Roth <rothjochen@gmail.com>
    Tested-by: Oliver Bartsch <bo@cedev.de>
    Reviewed-by: Oliver Bartsch <bo@cedev.de>
    Tested-by: Jochen Roth <rothjochen@gmail.com>
    Reviewed-by: Andreas Kienast <a.fernandez@scripting-base.de>
    Tested-by: Andreas Kienast <a.fernandez@scripting-base.de>
    Tested-by: core-ci <typo3@b13.com>

---
+++ Build/Sources/TypeScript/indexed_search/statistics.ts
@@ -0,0 +1,50 @@
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+import SortableTable from '@typo3/backend/sortable-table';
+
+/**
+ * Module: @typo3/indexed-search/statistics
+ *
+ * Functionality for the indexed search statistics module
+ *
+ * @example
+ * <typo3-indexed-search-statistics>
+ *   ...
+ * </typo3-indexed-search-statistics>
+ *
+ * This is based on W3C custom elements ("web components") specification, see
+ * https://developer.mozilla.org/en-US/docs/Web/Web_Components/Using_custom_elements
+ */
+class IndexedSearchStatistics extends HTMLElement {
+  constructor() {
+    super();
+    const slot: HTMLSlotElement = document.createElement('slot');
+    slot.addEventListener('slotchange', () => this.initializeWordList(slot));
+    this.attachShadow({ mode: 'open' }).append(slot);
+  }
+
+  public connectedCallback(): void {
+    this.initializeWordList(this.shadowRoot.querySelector('slot'));
+  }
+
+  private initializeWordList(slot: HTMLSlotElement): void {
+    const wordList: HTMLTableElement = (slot.assignedElements()[0] ?? null) as HTMLTableElement|null;
+    if (wordList === null || wordList.tagName.toLowerCase() !== 'table') {
+      throw new Error(`Sortable table could not be initialized. Expected <table> child name, but found: ${wordList}`);
+    }
+    new SortableTable(wordList);
+  }
+}
+
+window.customElements.define('typo3-indexed-search-statistics', IndexedSearchStatistics);
---
+++ typo3/sysext/indexed_search/Resources/Public/JavaScript/statistics.js
@@ -0,0 +1,13 @@
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+import SortableTable from"@typo3/backend/sortable-table.js";class IndexedSearchStatistics extends HTMLElement{constructor(){super();const e=document.createElement("slot");e.addEventListener("slotchange",(()=>this.initializeWordList(e))),this.attachShadow({mode:"open"}).append(e)}connectedCallback(){this.initializeWordList(this.shadowRoot.querySelector("slot"))}initializeWordList(e){const t=e.assignedElements()[0]??null;if(null===t||"table"!==t.tagName.toLowerCase())throw new Error(`Sortable table could not be initialized. Expected <table> child name, but found: ${t}`);new SortableTable(t)}}window.customElements.define("typo3-indexed-search-statistics",IndexedSearchStatistics);
