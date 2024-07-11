2cc9e372b89ed0a9975e22aea31ff35181f822f4
Christian Kuhn <lolli@schwarzbu.ch>
2024-07-12 01:29:15 +0200

    [TASK] Properly set bcrypt cost
    BcryptPasswordHash has an option to set a cost
    value. The lower barrier used to be identical
    to PHP constant PASSWORD_BCRYPT_DEFAULT_COST.
    
    Tests use the lower barrier to speed up tests.
    With PHP <8.4, PASSWORD_BCRYPT_DEFAULT_COST is
    10, it has been raised to 12 with PHP 8.4.
    
    The patch frees our code from access to this
    constant and hard codes 10 as lower barrier
    check. This keeps the API as is, and this hash
    is by default set to 12 already in default
    options if none are provided.
    
    Resolves: #104373
    Releases: main, 12.4, 11.5
    Change-Id: I4c1e94fdc50aa3075570549ccdd3b81a2de2e98a
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/85235
    Reviewed-by: Stefan Bürk <stefan@buerk.tech>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Reviewed-by: Oliver Bartsch <bo@cedev.de>
    Tested-by: Stefan Bürk <stefan@buerk.tech>
    Tested-by: Oliver Bartsch <bo@cedev.de>
    Tested-by: core-ci <typo3@b13.com>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>

--- typo3/sysext/core/Classes/Crypto/PasswordHashing/BcryptPasswordHash.php
+++ typo3/sysext/core/Classes/Crypto/PasswordHashing/BcryptPasswordHash.php
@@ -36,19 +36,15 @@ class BcryptPasswordHash implements PasswordHashInterface
     protected const PREFIX = '$2y$';
 
     /**
-     * Raise default PHP cost (10). At the time of this writing, this leads to
-     * 150-200ms computing time on a casual I7 CPU.
-     *
-     * @var array
+     * Set default PHP cost: Default is 10 with PHP <8.4, 12 since PHP 8.4. At the time
+     * of this writing, this leads to 150-200ms computing time on a casual I7 CPU.
      */
-    protected $options = [
+    protected array $options = [
         'cost' => 12,
     ];
 
     /**
      * Constructor sets options if given
-     *
-     * @throws \InvalidArgumentException
      */
     public function __construct(array $options = [])
     {
@@ -57,7 +53,7 @@ class BcryptPasswordHash implements PasswordHashInterface
         if (isset($options['cost'])) {
             if (!$this->isValidBcryptCost((int)$options['cost'])) {
                 throw new \InvalidArgumentException(
-                    'cost must not be lower than ' . PASSWORD_BCRYPT_DEFAULT_COST . ' or higher than 31',
+                    'cost must not be lower than 10 or higher than 31',
                     1533902002
                 );
             }
@@ -152,6 +148,6 @@ class BcryptPasswordHash implements PasswordHashInterface
      */
     protected function isValidBcryptCost(int $cost): bool
     {
-        return $cost >= PASSWORD_BCRYPT_DEFAULT_COST && $cost <= 31;
+        return $cost >= 10 && $cost <= 31;
     }
 }
