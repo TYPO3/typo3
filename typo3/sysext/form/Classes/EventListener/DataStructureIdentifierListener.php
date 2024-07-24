295c7868e2916ade4190bdcdffa7809fe9aaa124
Garvin Hicking <gh@faktor-e.de>
2024-07-24 14:55:40 +0200

    [TASK] Display additional error info in DataStructureIdentifierListener
    In #102557 additional sprintf() placeholders were added, but
    were missing to be introduced to `DataStructureIdentifierListener`.
    
    This change propagates the needed identifier to the output, too.
    
    Resolves: #104433
    Related: #102557
    Releases: main, 12.4
    Change-Id: Idb4f3f37f8fb347d77f3ca0038962a7280291930
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/85380
    Tested-by: core-ci <typo3@b13.com>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>

--- typo3/sysext/form/Classes/EventListener/DataStructureIdentifierListener.php
+++ typo3/sysext/form/Classes/EventListener/DataStructureIdentifierListener.php
@@ -175,7 +175,7 @@ class DataStructureIdentifierListener
             }
         } catch (NoSuchFileException|ParseErrorException $e) {
             $dataStructure = $this->addSelectedPersistenceIdentifier($identifier['ext-form-persistenceIdentifier'], $dataStructure);
-            $this->addInvalidFrameworkConfigurationFlashMessage($e);
+            $this->addInvalidFrameworkConfigurationFlashMessage($e, $identifier['ext-form-persistenceIdentifier']);
         }
 
         $event->setDataStructure($dataStructure);
@@ -303,10 +303,11 @@ class DataStructureIdentifierListener
         return $dataStructure;
     }
 
-    protected function addInvalidFrameworkConfigurationFlashMessage(\Exception $e): void
+    protected function addInvalidFrameworkConfigurationFlashMessage(\Exception $e, string $identifier = ''): void
     {
         $messageText = sprintf(
             $this->getLanguageService()->sL('LLL:EXT:form/Resources/Private/Language/Database.xlf:tt_content.preview.invalidFrameworkConfiguration.text'),
+            $identifier,
             $e->getMessage()
         );
 
