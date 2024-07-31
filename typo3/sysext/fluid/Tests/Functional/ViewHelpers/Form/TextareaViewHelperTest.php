f63f95f835e6aa13d26d43cd3d83be81d6a0e3b9
Simon Praetorius <simon@praetorius.me>
2024-07-31 11:52:31 +0200

    [TASK] Improve test coverage for TagBasedViewHelper
    To prevent unintended changes in ViewHelper API in the future,
    more test cases are added to TextareaViewHelper as an example
    of a TagBasedViewHelper.
    
    Resolves: #104507
    Related: #104490
    Releases: main
    Change-Id: I9f56f86e2a177e3a5ec453d655d8e2cc09355b8c
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/85400
    Tested-by: Garvin Hicking <gh@faktor-e.de>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Reviewed-by: Garvin Hicking <gh@faktor-e.de>
    Tested-by: core-ci <typo3@b13.com>
    Reviewed-by: Simon Praetorius <simon@praetorius.me>
    Tested-by: Simon Praetorius <simon@praetorius.me>

--- typo3/sysext/fluid/Tests/Functional/ViewHelpers/Form/TextareaViewHelperTest.php
+++ typo3/sysext/fluid/Tests/Functional/ViewHelpers/Form/TextareaViewHelperTest.php
@@ -36,40 +36,126 @@ final class TextareaViewHelperTest extends FunctionalTestCase
         return [
             'renderCorrectlySetsTagName' => [
                 '<f:form.textarea />',
+                [],
                 '<textarea name=""></textarea>',
             ],
             'renderCorrectlySetsNameAttributeAndContent' => [
                 '<f:form.textarea name="NameOfTextarea" value="Current value" />',
+                [],
                 '<textarea name="NameOfTextarea">Current value</textarea>',
             ],
             'renderEscapesTextareaContent' => [
                 '<f:form.textarea name="NameOfTextarea" value="some <tag> & \"quotes\"" />',
+                [],
                 '<textarea name="NameOfTextarea">some &lt;tag&gt; &amp; &quot;quotes&quot;</textarea>',
             ],
             'renderAddsPlaceholder' => [
                 '<f:form.textarea name="NameOfTextarea" placeholder="SomePlaceholder" />',
+                [],
                 '<textarea placeholder="SomePlaceholder" name="NameOfTextarea"></textarea>',
             ],
+
+            // "readonly" is registered as string argument, which should lead to its
+            // value being passed directly to the tag as string.
             'renderAddsReadonly' => [
                 '<f:form.textarea name="NameOfTextarea" readonly="foo" />',
+                [],
                 '<textarea readonly="foo" name="NameOfTextarea"></textarea>',
             ],
+            'renderAddsReadonly0' => [
+                '<f:form.textarea name="NameOfTextarea" readonly="0" />',
+                [],
+                '<textarea readonly="0" name="NameOfTextarea"></textarea>',
+            ],
+            'renderAddsReadonly1' => [
+                '<f:form.textarea name="NameOfTextarea" readonly="1" />',
+                [],
+                '<textarea readonly="1" name="NameOfTextarea"></textarea>',
+            ],
+            'renderAddsReadonlyTrue' => [
+                '<f:form.textarea name="NameOfTextarea" readonly="{var}" />',
+                ['var' => true],
+                '<textarea readonly="1" name="NameOfTextarea"></textarea>',
+            ],
+            'renderAddsReadonlyFalse' => [
+                '<f:form.textarea name="NameOfTextarea" readonly="{var}" />',
+                ['var' => false],
+                '<textarea readonly="" name="NameOfTextarea"></textarea>',
+            ],
+            'renderAddsReadonlyNull' => [
+                '<f:form.textarea name="NameOfTextarea" readonly="{var}" />',
+                ['var' => null],
+                '<textarea name="NameOfTextarea"></textarea>',
+            ],
+            'renderAddsReadonlyUndefined' => [
+                '<f:form.textarea name="NameOfTextarea" readonly="{var}" />',
+                [],
+                '<textarea name="NameOfTextarea"></textarea>',
+            ],
+            // Fluid removes empty string arguments that are used as tag attributes,
+            // so the resulting tag has no "readonly" attribute
+            'renderAddsEmptyReadonly' => [
+                '<f:form.textarea name="NameOfTextarea" readonly="" />',
+                [],
+                '<textarea name="NameOfTextarea"></textarea>',
+            ],
+
+            // "required" is registered as bool argument, which should lead to
+            // the resulting attribute being present or not based on the passed value
             'renderAddsRequired' => [
                 '<f:form.textarea name="NameOfTextarea" required="true" />',
+                [],
                 '<textarea name="NameOfTextarea" required="required"></textarea>',
+            ],
+            'renderAddsRequired0' => [
+                '<f:form.textarea name="NameOfTextarea" required="0" />',
+                [],
+                '<textarea name="NameOfTextarea"></textarea>',
+            ],
+            'renderAddsRequired1' => [
+                '<f:form.textarea name="NameOfTextarea" required="1" />',
+                [],
+                '<textarea name="NameOfTextarea" required="required"></textarea>',
+            ],
+            'renderAddsRequiredTrue' => [
+                '<f:form.textarea name="NameOfTextarea" required="{var}" />',
+                ['var' => true],
+                '<textarea name="NameOfTextarea" required="required"></textarea>',
+            ],
+            'renderAddsRequiredFalse' => [
+                '<f:form.textarea name="NameOfTextarea" required="{var}" />',
+                ['var' => false],
+                '<textarea name="NameOfTextarea"></textarea>',
+            ],
+            'renderAddsRequiredNull' => [
+                '<f:form.textarea name="NameOfTextarea" required="{var}" />',
+                ['var' => null],
+                '<textarea name="NameOfTextarea"></textarea>',
+            ],
+            'renderAddsRequiredUndefined' => [
+                '<f:form.textarea name="NameOfTextarea" required="{var}" />',
+                [],
+                '<textarea name="NameOfTextarea"></textarea>',
+            ],
+            'renderAddsEmptyRequired' => [
+                '<f:form.textarea name="NameOfTextarea" required="" />',
+                [],
+                '<textarea name="NameOfTextarea"></textarea>',
             ],
         ];
     }
 
     #[DataProvider('renderDataProvider')]
     #[Test]
-    public function render(string $template, string $expected): void
+    public function render(string $template, array $variables, string $expected): void
     {
         $context = $this->get(RenderingContextFactory::class)->create();
         $context->getTemplatePaths()->setTemplateSource($template);
         $serverRequest = (new ServerRequest())->withAttribute('extbase', new ExtbaseRequestParameters());
         $context->setRequest(new Request($serverRequest));
-        self::assertSame($expected, (new TemplateView($context))->render());
+        $view = new TemplateView($context);
+        $view->assignMultiple($variables);
+        self::assertSame($expected, $view->render());
     }
 
     #[Test]
