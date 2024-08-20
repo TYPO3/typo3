f5e34cd11d753ff8b1d74f29aec52531d0ba0d7a
Oliver Bartsch <bo@cedev.de>
2024-08-20 16:17:21 +0200

    [BUGFIX] Fix undefined array key warnings in GifBuilder
    Resolves: #104639
    Releases: main, 12.4, 11.5
    Change-Id: I74a0cd9e856283d26a382582cc5d6b8e99887cd4
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/85699
    Tested-by: core-ci <typo3@b13.com>
    Reviewed-by: Andreas Kienast <a.fernandez@scripting-base.de>
    Tested-by: Andreas Kienast <a.fernandez@scripting-base.de>

--- typo3/sysext/frontend/Classes/Imaging/GifBuilder.php
+++ typo3/sysext/frontend/Classes/Imaging/GifBuilder.php
@@ -433,7 +433,7 @@ class GifBuilder extends GraphicalFunctions
                                     $isStdWrapped = [];
                                     foreach ($conf['shadow.'] as $key => $value) {
                                         $parameter = rtrim($key, '.');
-                                        if (!$isStdWrapped[$parameter] && isset($conf[$parameter . '.'])) {
+                                        if (!($isStdWrapped[$parameter] ?? false) && isset($conf[$parameter . '.'])) {
                                             $conf['shadow.'][$parameter] = $this->cObj->stdWrapValue($parameter, $conf);
                                             $isStdWrapped[$parameter] = 1;
                                         }
@@ -444,7 +444,7 @@ class GifBuilder extends GraphicalFunctions
                                     $isStdWrapped = [];
                                     foreach ($conf['emboss.'] as $key => $value) {
                                         $parameter = rtrim($key, '.');
-                                        if (!$isStdWrapped[$parameter] && isset($conf[$parameter . '.'])) {
+                                        if (!($isStdWrapped[$parameter] ?? false) && isset($conf[$parameter . '.'])) {
                                             $conf['emboss.'][$parameter] = $this->cObj->stdWrapValue($parameter, $conf);
                                             $isStdWrapped[$parameter] = 1;
                                         }
@@ -455,7 +455,7 @@ class GifBuilder extends GraphicalFunctions
                                     $isStdWrapped = [];
                                     foreach ($conf['outline.'] as $key => $value) {
                                         $parameter = rtrim($key, '.');
-                                        if (!$isStdWrapped[$parameter] && isset($conf[$parameter . '.'])) {
+                                        if (!($isStdWrapped[$parameter] ?? false) && isset($conf[$parameter . '.'])) {
                                             $conf['outline.'][$parameter] = $this->cObj->stdWrapValue($parameter, $conf);
                                             $isStdWrapped[$parameter] = 1;
                                         }
