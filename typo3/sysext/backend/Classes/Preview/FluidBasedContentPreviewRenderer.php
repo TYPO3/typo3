d2493f3a7a5912e01f4b10bd848d0579d80de711
Christian Kuhn <lolli@schwarzbu.ch>
2024-08-22 20:08:24 +0200

    [TASK] Extract a hardcoded fluid template source to file
    Hard coded fluid code within the framework (except
    in fluid view helper tests) is not a good thing:
    It makes substituting the view with something
    else impossible.
    The patch extracts a setTemplateSource() to a file.
    This is a pre-patch to a general view factory
    implementation.
    
    Resolves: #104769
    Releases: main
    Change-Id: Ifb95e274a276b15c882694735ccda4cdcb2130d9
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/85821
    Reviewed-by: Benni Mack <benni@typo3.org>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Stefan Bürk <stefan@buerk.tech>
    Tested-by: Benni Mack <benni@typo3.org>
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Reviewed-by: Stefan Bürk <stefan@buerk.tech>

--- typo3/sysext/backend/Classes/Preview/FluidBasedContentPreviewRenderer.php
+++ typo3/sysext/backend/Classes/Preview/FluidBasedContentPreviewRenderer.php
@@ -99,14 +99,13 @@ final class FluidBasedContentPreviewRenderer implements LoggerAwareInterface
                 'file' => $fluidTemplateFileAbsolutePath,
                 'exception' => $e,
             ]);
-
             if ($this->getBackendUser()->shallDisplayDebugInformation()) {
                 $view = GeneralUtility::makeInstance(StandaloneView::class);
+                $view->setTemplatePathAndFilename('EXT:backend/Resources/Private/Templates/PageLayout/FluidBasedContentPreviewRenderingException.html');
                 $view->assign('error', [
                     'message' => str_replace(Environment::getProjectPath(), '', $e->getMessage()),
                     'title' => 'Error while rendering FluidTemplate preview using ' . str_replace(Environment::getProjectPath(), '', $fluidTemplateFileAbsolutePath),
                 ]);
-                $view->setTemplateSource('<f:be.infobox title="{error.title}" state="{f:constant(name: \'TYPO3\CMS\Fluid\ViewHelpers\Be\InfoboxViewHelper::STATE_ERROR\')}">{error.message}</f:be.infobox>');
                 return $view->render();
             }
             return null;
