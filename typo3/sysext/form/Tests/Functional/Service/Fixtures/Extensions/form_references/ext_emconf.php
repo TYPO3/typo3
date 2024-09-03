9951e36813a423409059f5444f97c22a6996b5a4
Markus <noreply@example.com>
2024-09-03 14:40:09 +0000

    [BUGFIX] Resolve references of extension forms
    The EXT:form management GUI shows all forms and where
    they are used (referenced).
    
    Forms can be either stored in FAL-based storages
    (mainly used by editors) or be shipped in extensions
    (EXT:my_extension/Resources/Private/Forms/my.form.yaml).
    
    Clicking on the number of references opens a popup
    that reveals all references to a given form, but this
    could not deal with EXT:... notations and throwing
    an exception, instead of returning the sys_refindex
    records that actually contain the exact reference.
    
    This patch now resolves the sys_refindex differently,
    depending on whether these notations are used:
    
    * FAL-based storage with UID
    * FAL-based storage with string
    * public EXT:... notation
    * private EXT:... notation
    
    Tests are added to ensure this behavior.
    
    Inspecting references will no longer trigger an
    exception, but an empty result set to continue
    the sys_refindex inspection.
    
    Releases: main, 13.4, 12.4
    Resolves: #102160
    Change-Id: I7adbc12bfa4b7a10fa37f03a6527cacd1e255680
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/87034
    Tested-by: Garvin Hicking <gh@faktor-e.de>
    Tested-by: core-ci <typo3@b13.com>
    Reviewed-by: Garvin Hicking <gh@faktor-e.de>

---
+++ typo3/sysext/form/Tests/Functional/Service/Fixtures/Extensions/form_references/ext_emconf.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => '',
+    'description' => '',
+    'category' => 'example',
+    'author' => 'TYPO3 core team',
+    'author_company' => '',
+    'author_email' => '',
+    'state' => 'stable',
+    'version' => '12.4.24',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '12.4.24',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
