c21877d6ad35e2d372667a00e2bf3fbd02fcef87
Markus <noreply@example.com>
2024-09-03 14:40:09 +0000

    [BUGFIX] Resolve references of extension forms
    The EXT:form management GUI shows all forms and where
    they are used (referenced).
    
    Forms can be either stored in FAL-based storages
    (mainly used by editors) or be shipped in extensions
    (EXT:my_extension/Resources/Private/Forms/my.form.yaml).
    
    Clicking on the number of references opens a popup
    that reveals all references to a given form, but this
    could not deal with EXT:... notations and throwing
    an exception, instead of returning the sys_refindex
    records that actually contain the exact reference.
    
    This patch now resolves the sys_refindex differently,
    depending on whether these notations are used:
    
    * FAL-based storage with UID
    * FAL-based storage with string
    * public EXT:... notation
    * private EXT:... notation
    
    Tests are added to ensure this behavior.
    
    Inspecting references will no longer trigger an
    exception, but an empty result set to continue
    the sys_refindex inspection.
    
    Releases: main, 13.4, 12.4
    Resolves: #102160
    Change-Id: I7adbc12bfa4b7a10fa37f03a6527cacd1e255680
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/87033
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Garvin Hicking <gh@faktor-e.de>
    Reviewed-by: Garvin Hicking <gh@faktor-e.de>

--- typo3/sysext/form/Classes/Service/DatabaseService.php
+++ typo3/sysext/form/Classes/Service/DatabaseService.php
@@ -19,8 +19,12 @@ namespace TYPO3\CMS\Form\Service;
 
 use TYPO3\CMS\Core\Database\Connection;
 use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Resource\Exception\ResourceDoesNotExistException;
+use TYPO3\CMS\Core\Resource\File;
 use TYPO3\CMS\Core\Resource\ResourceFactory;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Utility\MathUtility;
+use TYPO3\CMS\Core\Utility\PathUtility;
 
 /**
  * This class is subjected to change.
@@ -35,6 +39,15 @@ class DatabaseService
      * Returns an array with all sys_refindex database rows which be
      * connected to a formDefinition identified by $persistenceIdentifier
      *
+     * $persistenceIdentifier string can contain:
+     * - number     -> interpreted as a sys_file reference UID to a FAL-stored YAML file (user-generated content)
+     * - EXT:...    -> interpreted as a NON-FAL extension-based file
+     * - any string -> interpreted as FAL-based filename
+     *
+     * Note that we explicitly do NOT check for file existence here,
+     * because we want to be able to reveal sys_refindex entries to files
+     * that have been deleted meanwhile!
+     *
      * @internal
      */
     public function getReferencesByPersistenceIdentifier(string $persistenceIdentifier): array
@@ -43,25 +56,69 @@ class DatabaseService
             throw new \InvalidArgumentException('$persistenceIdentifier must not be empty.', 1472238493);
         }
 
-        $resourceFactory = GeneralUtility::makeInstance(ResourceFactory::class);
-        $file = $resourceFactory->retrieveFileOrFolderObject($persistenceIdentifier);
+        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('sys_refindex');
+        $constraints = [$queryBuilder->expr()->eq('softref_key', $queryBuilder->createNamedParameter('formPersistenceIdentifier'))];
 
-        if ($file === null) {
-            return [];
+        // Indicator whether the string-based lookup in sys_refindex shall be performed (true; non FAL-based) or not (false; FAL-based)
+        $useStringReference = false;
+
+        // Check what $persistenceIdentifier contains.
+        if (PathUtility::isExtensionPath($persistenceIdentifier)) {
+            // Uses "EXT:" notation, so it cannot be a FAL identifier.
+            // We pass the whole "EXT:..." lookup through to the sys_refindex query
+            // due to its constraint on softref_key=formPersistenceIdentifier,
+            // we expect no false entries even with "weird" string notations. If sys_refindex
+            // has it, we yield it.
+            $useStringReference = true;
+        } else {
+            // Anything else would be either a notation like "/fileadmin/something.form.yaml"
+            // or a numeric identifier for a sys_file.
+            $resourceFactory = GeneralUtility::makeInstance(ResourceFactory::class);
+
+            try {
+                // We use this "bulk method" because this is the best-bet from resourceFactory
+                // to resolve both an integer-ish input value or a FAL value. There is no
+                // substitute for an "only get a file, not a directory" lookup.
+                $file = $resourceFactory->retrieveFileOrFolderObject($persistenceIdentifier);
+
+                if ($file === null) {
+                    // The associated identifier could (no longer) be retrieved via FAL.
+                    // However, we do want to see existing entries to such stale entries to
+                    // be able to reveal bad references, either by its ref_string or ref_uid
+
+                    if (MathUtility::canBeInterpretedAsInteger($persistenceIdentifier)) {
+                        $constraints[] = $queryBuilder->expr()->eq('ref_uid', $queryBuilder->createNamedParameter($persistenceIdentifier, Connection::PARAM_INT));
+                    } else {
+                        $useStringReference = true;
+                    }
+                } elseif ($file instanceof File) {
+                    // We succeeded in retrieving the FAL file object.
+                    $constraints[] = $queryBuilder->expr()->eq('ref_uid', $queryBuilder->createNamedParameter($file->getUid(), Connection::PARAM_INT));
+                } else {
+                    // We might have retrieved a "Folder" object. Fall back to passthrough
+                    // with the intent, to retrieve all possible sys_refindex entries.
+                    // If that fails, it's ok to return an empty array.
+                    $useStringReference = true;
+                }
+            } catch (ResourceDoesNotExistException) {
+                // This exception gets triggered when $persistenceIdentifier is not something
+                // that could be resolved by the bulk-method.
+                // As above, we want to retrieve all the possible sys_refindex entries,
+                // so we fall back again to "ref_string".
+                // This should happen when $persistenceIdentifier is set to a string like '/fileadmin/somefile.form.yaml',
+                // and a FAL storage could be retrieved, but not the actual file.
+                $useStringReference = true;
+            }
         }
 
-        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('sys_refindex');
+        if ($useStringReference) {
+            $constraints[] = $queryBuilder->expr()->eq('ref_string', $queryBuilder->createNamedParameter($persistenceIdentifier));
+        }
 
         return $queryBuilder
             ->select('*')
             ->from('sys_refindex')
-            ->where(
-                $queryBuilder->expr()->eq('softref_key', $queryBuilder->createNamedParameter('formPersistenceIdentifier')),
-                $queryBuilder->expr()->or(
-                    $queryBuilder->expr()->eq('ref_string', $queryBuilder->createNamedParameter($persistenceIdentifier)),
-                    $queryBuilder->expr()->eq('ref_uid', $queryBuilder->createNamedParameter($file->getUid(), Connection::PARAM_INT))
-                )
-            )
+            ->where(...$constraints)
             ->executeQuery()
             ->fetchAllAssociative();
     }
---
+++ typo3/sysext/form/Tests/Functional/Service/Fixtures/Extensions/form_references/ext_emconf.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => '',
+    'description' => '',
+    'category' => 'example',
+    'author' => 'TYPO3 core team',
+    'author_company' => '',
+    'author_email' => '',
+    'state' => 'stable',
+    'version' => '13.4.2',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '13.4.2',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
