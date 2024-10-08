f98c944f6e3111ffd699791dcfb0c2ace7f76195
Oliver Hader <oliver@typo3.org>
2024-10-08 11:07:07 +0200

    [RELEASE] Release of TYPO3 13.3.1
    Change-Id: I5f7fcbb38783b321ddb957a34b5c1adc8b8c41d0
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/86508
    Tested-by: Oliver Hader <oliver.hader@typo3.org>
    Reviewed-by: Oliver Hader <oliver.hader@typo3.org>

--- typo3/sysext/core/Classes/Information/Typo3Version.php
+++ typo3/sysext/core/Classes/Information/Typo3Version.php
@@ -19,7 +19,7 @@ namespace TYPO3\CMS\Core\Information;
 
 class Typo3Version
 {
-    protected const VERSION = '13.3.1-dev';
+    protected const VERSION = '13.3.1';
     protected const BRANCH = '13.3';
 
     public function getVersion(): string
