ac9da9c710178ef8973d6d361ea5be26d565e078
Oliver Bartsch <bo@cedev.de>
2024-10-14 21:48:59 +0200

    [TASK] Adjust DocumentationViewHelper to clear linking funcionality
    This adjusts the DocumentationViewHelper class
    comment to properly document which links will
    be generated. This is now also covered by
    corresponding tests.
    
    Resolves: #105308
    Related: #105275
    Releases: main, 12.4
    Change-Id: Ie0191659c8d2cebab7a071ef4a548a4fc087ed1f
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/86603
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Oliver Bartsch <bo@cedev.de>
    Reviewed-by: Oliver Bartsch <bo@cedev.de>

---
+++ typo3/sysext/backend/Tests/Functional/ViewHelpers/Link/DocumentationViewHelperTest.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3\CMS\Backend\Tests\Functional\ViewHelpers\Link;
+
+use PHPUnit\Framework\Attributes\Test;
+use TYPO3\CMS\Core\Core\SystemEnvironmentBuilder;
+use TYPO3\CMS\Core\Http\NormalizedParams;
+use TYPO3\CMS\Core\Http\ServerRequest;
+use TYPO3\CMS\Core\Information\Typo3Version;
+use TYPO3\CMS\Fluid\Core\Rendering\RenderingContextFactory;
+use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
+use TYPO3Fluid\Fluid\View\TemplateView;
+
+final class DocumentationViewHelperTest extends FunctionalTestCase
+{
+    protected bool $initializeDatabase = false;
+
+    protected ServerRequest $request;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->request = (new ServerRequest('https://www.example.com/'))
+            ->withAttribute('applicationType', SystemEnvironmentBuilder::REQUESTTYPE_BE)
+            ->withAttribute('normalizedParams', new NormalizedParams([], [], '', ''));
+    }
+
+    #[Test]
+    public function renderReturnsValidLink(): void
+    {
+        $context = $this->get(RenderingContextFactory::class)->create();
+        $context->setRequest($this->request);
+        $context->getViewHelperResolver()->addNamespace('be', 'TYPO3\\CMS\\Backend\\ViewHelpers');
+        $context->getTemplatePaths()->setTemplateSource('<be:link.documentation identifier="foobar" class="baz" additionalAttributes="{\'target\':\'_self\', \'rel\':\'nofollow\'}">see documentation</be:link.documentation>');
+        $result = urldecode((new TemplateView($context))->render());
+
+        $typo3Version = (new Typo3Version())->getBranch();
+        self::assertSame('<a target="_blank" rel="noreferrer" class="baz" href="https://docs.typo3.org/permalink/foobar@' . $typo3Version . '">see documentation</a>', $result);
+    }
+
+    #[Test]
+    public function renderThrowsExceptionForInvalidIdentifier(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionCode(1728643940);
+
+        $context = $this->get(RenderingContextFactory::class)->create();
+        $context->setRequest($this->request);
+        $context->getViewHelperResolver()->addNamespace('be', 'TYPO3\\CMS\\Backend\\ViewHelpers');
+        $context->getTemplatePaths()->setTemplateSource('<be:link.documentation identifier="foo@bar" class="baz">see documentation</be:link.documentation>');
+        (new TemplateView($context))->render();
+    }
+}
