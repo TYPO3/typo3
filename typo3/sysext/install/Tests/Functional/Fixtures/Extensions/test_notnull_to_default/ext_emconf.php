fda3390db10c241ea6bdf68313cab29b6d98d7df
Oliver Hader <oliver@typo3.org>
2024-11-12 11:38:46 +0100

    [RELEASE] Release of TYPO3 13.4.1
    Change-Id: Ib9b608fdebf60a8876c6d73352e0a84c3bf7d7b8
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/86970
    Tested-by: Oliver Hader <oliver.hader@typo3.org>
    Reviewed-by: Oliver Hader <oliver.hader@typo3.org>

--- typo3/sysext/install/Tests/Functional/Fixtures/Extensions/test_notnull_to_default/ext_emconf.php
+++ typo3/sysext/install/Tests/Functional/Fixtures/Extensions/test_notnull_to_default/ext_emconf.php
@@ -6,11 +6,11 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'Test Extension for NullToDefaultUpdateWizardTest',
     'description' => 'Test Extension for NullToDefaultUpdateWizardTest',
     'category' => 'example',
-    'version' => '14.0.0',
+    'version' => '13.4.1',
     'state' => 'stable',
     'constraints' => [
         'depends' => [
-            'typo3' => '14.0.0',
+            'typo3' => '13.4.1',
         ],
         'conflicts' => [],
         'suggests' => [],
