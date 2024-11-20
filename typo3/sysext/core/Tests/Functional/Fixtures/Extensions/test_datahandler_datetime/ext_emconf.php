bad8f38827b041574ffbdd9645e9bed686497c20
Benjamin Franzke <ben@bnf.dev>
2024-11-20 17:20:15 +0100

    [TASK] Add functional DataHandler tests for type=datetime
    Add functional DataHandler tests for type=datetime that assert correct
    persistence to database and test in different timezones.
    
    We document current findings and oddities which are to be streamlined
    by #105549 in TYPO3 v14.
    
    Releases: main, 13.4, 12.4
    Resolves: #105661
    Related: #105549
    Change-Id: I2c65027ea0ce7419cd9eff54b60683408a242e63
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/87102
    Tested-by: Andreas Kienast <akienast@scripting-base.de>
    Reviewed-by: Benjamin Franzke <ben@bnf.dev>
    Tested-by: core-ci <typo3@b13.com>
    Reviewed-by: Andreas Kienast <akienast@scripting-base.de>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Benjamin Franzke <ben@bnf.dev>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>

---
+++ typo3/sysext/core/Tests/Functional/Fixtures/Extensions/test_datahandler_datetime/ext_emconf.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+$EM_CONF[$_EXTKEY] = [
+    'title'            => 'Test DataHandler with datetime fields',
+    'description'      => 'TYPO3 extension to be used for functional tests in TYPO3 core',
+    'version' => '14.0.0',
+    'state'            => 'beta',
+    'author'           => '',
+    'author_email'     => '',
+    'constraints'      => [
+        'depends' => [
+            'typo3' => '14.0.0',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
