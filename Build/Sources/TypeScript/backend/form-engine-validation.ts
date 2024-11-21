c79dfdaa26e8efffb7f887ce8d23279420d50c74
Oliver Bartsch <bo@cedev.de>
2024-11-21 09:50:01 +0100

    [BUGFIX] Set FormEngine error class on field and its label
    Like done for the `has-change` class in #105096
    is the FormEngine `has-error` class now added
    to the corresponding field and its label instead
    of the container element. This prevents wrong
    inheritance of the styles, which occurred e.g.
    in inline elements.
    
    Resolves: #105596
    Related: #105096
    Releases: main, 13.4
    Change-Id: I301f38d801748622c2d86b03fb0c9c896cb26cf1
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/87136
    Tested-by: Benjamin Kott <benjamin.kott@outlook.com>
    Reviewed-by: Andreas Kienast <akienast@scripting-base.de>
    Reviewed-by: Benjamin Kott <benjamin.kott@outlook.com>
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Andreas Kienast <akienast@scripting-base.de>

--- Build/Sources/TypeScript/backend/form-engine-validation.ts
+++ Build/Sources/TypeScript/backend/form-engine-validation.ts
@@ -51,7 +51,9 @@ export default class FormEngineValidation {
   public static rulesSelector: string = '[data-formengine-validation-rules]';
   public static inputSelector: string = '[data-formengine-input-params]';
   public static markerSelector: string = '.t3js-formengine-validation-marker';
+  public static labelSelector: string = '.t3js-formengine-label';
   public static errorClass: string = 'has-error';
+  public static validationErrorClass: string = 'has-validation-error';
   public static passwordDummy: string = '********';
 
   /**
@@ -220,7 +222,8 @@ export default class FormEngineValidation {
         case 'required':
           if (value === '') {
             markParent = true;
-            field.closest(FormEngineValidation.markerSelector).classList.add(FormEngineValidation.errorClass);
+            field.classList.add(FormEngineValidation.errorClass);
+            field.closest(FormEngineValidation.markerSelector)?.querySelector(FormEngineValidation.labelSelector)?.classList.add(FormEngineValidation.errorClass);
           }
           break;
         case 'range':
@@ -356,11 +359,8 @@ export default class FormEngineValidation {
     }
 
     const isValid = !markParent;
-    const validationMarker = field.closest(FormEngineValidation.markerSelector);
-    if (validationMarker !== null) {
-      // Validation marker may be unavailable (e.g. due to maximized ckeditor)
-      validationMarker.classList.toggle(FormEngineValidation.errorClass, !isValid);
-    }
+    field.classList.toggle(FormEngineValidation.errorClass, !isValid);
+    field.closest(FormEngineValidation.markerSelector)?.querySelector(FormEngineValidation.labelSelector)?.classList.toggle(FormEngineValidation.errorClass, !isValid);
 
     FormEngineValidation.markParentTab(field, isValid);
     formEngineFormElement.dispatchEvent(new CustomEvent<PostValidationEvent>('t3-formengine-postfieldvalidation', { detail: { field: field, isValid: isValid }, cancelable: false, bubbles: true }));
@@ -501,7 +501,7 @@ export default class FormEngineValidation {
   public static validate(section?: Element): void {
     if (typeof section === 'undefined' || section instanceof Document) {
       formEngineFormElement.querySelectorAll(FormEngineValidation.markerSelector + ', .t3js-tabmenu-item').forEach((tabMenuItem: HTMLElement): void => {
-        tabMenuItem.classList.remove(FormEngineValidation.errorClass, 'has-validation-error')
+        tabMenuItem.classList.remove(FormEngineValidation.validationErrorClass)
       });
     }
 
@@ -608,7 +608,7 @@ export default class FormEngineValidation {
       formEngineFormElement
         .querySelector('[data-bs-target="#' + id + '"]')
         .closest('.t3js-tabmenu-item')
-        .classList.toggle('has-validation-error', !isValid);
+        .classList.toggle(FormEngineValidation.validationErrorClass, !isValid);
     });
   }
 
