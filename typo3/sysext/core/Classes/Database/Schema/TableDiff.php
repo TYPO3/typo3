68824b1c433741c83a9249115d954dd418150d41
Christian Kuhn <lolli@schwarzbu.ch>
2024-11-30 17:27:00 +0100

    [TASK] Remove outdated `@todo` in TableDiff
    This one is not valid anymore.
    
    Resolves: #105731
    Releases: main
    Change-Id: I84af8e12e6d14cbcef8530e8c415a25b498b8720
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/87295
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Stefan Bürk <stefan@buerk.tech>
    Reviewed-by: Stefan Bürk <stefan@buerk.tech>

--- typo3/sysext/core/Classes/Database/Schema/TableDiff.php
+++ typo3/sysext/core/Classes/Database/Schema/TableDiff.php
@@ -191,8 +191,6 @@ class TableDiff extends DoctrineTableDiff
             // really dropping tables instead. Therefore, we need to add here an empty check for the reintroduced
             // property.See for example: ConnectionMigrator->migrateUnprefixedRemovedTablesToRenames
             && $this->getNewName() !== null && $this->getNewName() !== ''
-            // @todo doctrine/dbal 3.5 deprecated schema events, thus a new way to provide table option has to
-            //       be found and implemented. Recheck this afterwards.
             && $this->getTableOptions() === [];
     }
 
