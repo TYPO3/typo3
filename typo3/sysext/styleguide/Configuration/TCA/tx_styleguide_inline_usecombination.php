55dc6278543a0994660048b0319a019e734d44ba
Andreas Kienast <akienast@scripting-base.de>
2024-12-11 13:43:58 +0100

    [TASK] Add example for `useCombination` with single select
    The styleguide example "inline usecombination" is extended by a new
    example that uses a combination field without `autoSizeMax` set.
    
    Resolves: #105807
    Releases: main, 13.4
    Change-Id: I92c17dad56cb80d36a4084c93aa8e31ec2950a37
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/87495
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Georg Ringer <georg.ringer@gmail.com>
    Reviewed-by: Georg Ringer <georg.ringer@gmail.com>

--- typo3/sysext/styleguide/Configuration/TCA/tx_styleguide_inline_usecombination.php
+++ typo3/sysext/styleguide/Configuration/TCA/tx_styleguide_inline_usecombination.php
@@ -32,6 +32,32 @@ return [
                 'foreign_selector' => 'select_child',
                 'foreign_unique' => 'select_child',
                 'maxitems' => 9999,
+                'appearance' => [
+                    'newRecordLinkAddTitle' => 1,
+                    'useCombination' => true,
+                    'collapseAll' => false,
+                    'levelLinksPosition' => 'top',
+                    'showSynchronizationLink' => 1,
+                    'showPossibleLocalizationRecords' => 1,
+                    'showAllLocalizationLink' => 1,
+                ],
+            ],
+        ],
+
+        'inline_2' => [
+            'label' => 'inline_2',
+            'config' => [
+                'type' => 'inline',
+                'foreign_table' => 'tx_styleguide_inline_usecombination_mm',
+                'foreign_field' => 'select_parent',
+                'foreign_selector' => 'select_child',
+                'foreign_unique' => 'select_child',
+                'maxitems' => 9999,
+                // @todo: The difference inline_2 to inline_1 is this 'autoSizeMax' setting which leads to
+                //        different UI rendering. The example re-uses the same "mm" table, which is not a
+                //        realistic nor fully supported scenario since it leads to ambiguity when looking
+                //        at the relation from the MM side "up" to the inline parent. This misuse is accepted
+                //        for this scenario for now.
                 'autoSizeMax' => 10,
                 'appearance' => [
                     'newRecordLinkAddTitle' => 1,
@@ -48,7 +74,7 @@ return [
 
     'types' => [
         '0' => [
-            'showitem' => 'inline_1',
+            'showitem' => 'inline_1, inline_2',
         ],
     ],
 
