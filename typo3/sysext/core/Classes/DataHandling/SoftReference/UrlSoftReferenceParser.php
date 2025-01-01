2a16f0268e1b115a99723a6c57e89d23f4a048c9
Sybille Peters <sypets@gmx.de>
2025-01-01 10:50:58 +0100

    [BUGFIX] Parse URLs with non-ASCII characters
    The UrlSoftReferenceParser did not handle URLs with non-ASCII
    characters in the domain, because it used a regex using a-z
    to match for letters. This will not work for unicode characters
    outside of the ASCII range (such as umlauts like ö).
    
    This change replaces the ASCII letter with the unicode language
    meta container `\p{L}` to extract unicode domains also from the
    provided content.
    
    Resolves: #105753
    Releases: main, 13.4, 12.4
    Change-Id: I7670848ea6bd669390115d9a8d77f790ed538889
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/87618
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Stefan Bürk <stefan@buerk.tech>
    Reviewed-by: Stefan Bürk <stefan@buerk.tech>

--- typo3/sysext/core/Classes/DataHandling/SoftReference/UrlSoftReferenceParser.php
+++ typo3/sysext/core/Classes/DataHandling/SoftReference/UrlSoftReferenceParser.php
@@ -22,7 +22,17 @@ namespace TYPO3\CMS\Core\DataHandling\SoftReference;
  */
 class UrlSoftReferenceParser extends AbstractSoftReferenceParser
 {
-    protected const REGEXP = '/([^[:alnum:]"\']+)((https?|ftp):\\/\\/(?:[!#$&-;=?-\[\]_a-z~]+|%[0-9a-fA-F]{2})+)([[:space:]])?/';
+    /**
+     * We do not use a-z for letters, so we can also allow unicode characters. For this purpose we use \p{L}, for example.
+     * And we must use the modifier "u" in the regex.
+     * Domains may contain umlauts (ä,ö,ü).
+     *
+     * PHP regex with unicode character properties:
+     * \p{L}: letter
+     * \p{Ll}: lower case letter
+     * @see https://www.php.net/manual/en/regexp.reference.unicode.php
+     */
+    protected const REGEXP = '/([^[:alnum:]"\']+)((https?|ftp):\\/\\/(?:[!#$&-;=?-\[\]_\\p{L}~]+|%[0-9\\p{L}]{2})+)([[:space:]])?/u';
 
     public function parse(string $table, string $field, int $uid, string $content, string $structurePath = ''): SoftReferenceParserResult
     {
