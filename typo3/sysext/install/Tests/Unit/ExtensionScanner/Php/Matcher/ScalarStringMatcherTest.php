12c84f87131855a72256b8bd14a34206228a9a89
Garvin Hicking <gh@faktor-e.de>
2025-01-07 22:38:47 +0100

    [TASK] Add ScalarStringMatcher Extension Scanner for TYPO3_MODE reports
    An upgrade to TYPO3 v12 can be painful when backend or frontend
    modules (or even the Database Analyzer) bails out with a white
    page. Usually this would lead to inspection of logfiles,
    checking if PHP's `error_reporting` or `display_errors` is
    not properly set, looking for PHP/Webserver coredumps,
    problems in HTTP headers, CSP errors and much more, just
    to find out that a
    
      `defined('TYPO3_MODE') or die()`
    
    was left in some PHP file of an extension, which needs
    to be replaced with `TYPO3` (see #92947 and #96107).
    
    The Extension Scanner only scanned for the full
    PHP constant usage before.
    
    With this patch, these "magic strings" can now also
    be scanned and reported, making debugging of a
    malfunctioning upgrade much easier for developers
    and especially integrators.
    
    Resolves: #105898
    Related: #92947
    Related: #96107
    Releases: main, 13.4, 12.4
    Change-Id: I1dc9cebee1773e9c40da69831640486b02615911
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/87663
    Reviewed-by: Stefan Bürk <stefan@buerk.tech>
    Tested-by: Stefan Bürk <stefan@buerk.tech>
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Garvin Hicking <gh@faktor-e.de>
    Reviewed-by: Garvin Hicking <gh@faktor-e.de>

---
+++ typo3/sysext/install/Tests/Unit/ExtensionScanner/Php/Matcher/ScalarStringMatcherTest.php
@@ -0,0 +1,117 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3\CMS\Install\Tests\Unit\ExtensionScanner\Php\Matcher;
+
+use PhpParser\NodeTraverser;
+use PhpParser\NodeVisitor\NameResolver;
+use PhpParser\ParserFactory;
+use PhpParser\PhpVersion;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Test;
+use TYPO3\CMS\Install\ExtensionScanner\Php\GeneratorClassesResolver;
+use TYPO3\CMS\Install\ExtensionScanner\Php\Matcher\ScalarStringMatcher;
+use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
+
+final class ScalarStringMatcherTest extends UnitTestCase
+{
+    #[Test]
+    public function hitsFromFixtureAreFound(): void
+    {
+        $parser = (new ParserFactory())->createForVersion(PhpVersion::fromComponents(8, 2));
+        $fixtureFile = __DIR__ . '/Fixtures/ScalarStringMatcherFixture.php';
+        $statements = $parser->parse(file_get_contents($fixtureFile));
+
+        $traverser = new NodeTraverser();
+        $traverser->addVisitor(new NameResolver());
+        $traverser->addVisitor(new GeneratorClassesResolver());
+
+        $configuration = [
+            'TYPO3_MODE' => [
+                'restFiles' => [
+                    'Deprecation-92947-DeprecateTYPO3_MODEAndTYPO3_REQUESTTYPEConstants.rst',
+                    'Breaking-96107-DeprecatedFunctionalityRemoved.rst',
+                ],
+            ],
+            'list_type' => [
+                'restFiles' => [
+                    'Breaking-105377-DeprecatedFunctionalityRemoved.rst',
+                ],
+            ],
+        ];
+        $subject = new ScalarStringMatcher($configuration);
+        $traverser->addVisitor($subject);
+        $traverser->traverse($statements);
+        $expectedHitLineNumbers = [
+            28,
+            29,
+            30,
+            31,
+            34,
+            49,
+        ];
+        $actualHitLineNumbers = [];
+        foreach ($subject->getMatches() as $hit) {
+            $actualHitLineNumbers[] = $hit['line'];
+        }
+        self::assertEquals($expectedHitLineNumbers, $actualHitLineNumbers);
+    }
+
+    public static function matchesReturnsExpectedRestFilesDataProvider(): array
+    {
+        return [
+            'a straight match, double quote' => [
+                [
+                    'TYPO3_MODE' => [
+                        'restFiles' => [
+                            'Foo-1.rst',
+                            'Foo-2.rst',
+                        ],
+                    ],
+                ],
+                '<?php
+                $foo = "TYPO3_MODE";',
+                [
+                    0 => [
+                        'restFiles' => [
+                            'Foo-1.rst',
+                            'Foo-2.rst',
+                        ],
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    #[DataProvider('matchesReturnsExpectedRestFilesDataProvider')]
+    #[Test]
+    public function matchesReturnsExpectedRestFiles(array $configuration, string $phpCode, array $expected): void
+    {
+        $parser = (new ParserFactory())->createForVersion(PhpVersion::fromComponents(8, 2));
+        $statements = $parser->parse($phpCode);
+
+        $subject = new ScalarStringMatcher($configuration);
+
+        $traverser = new NodeTraverser();
+        $traverser->addVisitor(new NameResolver());
+        $traverser->addVisitor($subject);
+        $traverser->traverse($statements);
+
+        $result = $subject->getMatches();
+        self::assertSame($expected[0]['restFiles'], $result[0]['restFiles']);
+    }
+}
