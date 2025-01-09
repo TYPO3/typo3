10d652f6a1f6415bd41423f7f635c78f32e71bd6
Andreas Kienast <akienast@scripting-base.de>
2025-01-09 15:34:03 +0100

    [TASK] Update ckeditor to v44.1
    This commit updates ckeditor to v44.1.
    
    Notable change: With v44, it became mandatory to provide a
    `licenseKey` [1] in any case.
    
    Executed commands:
    
        npm --prefix overrides/ckeditor5 version 44.1.0
        npm install --save ./overrides/ckeditor5 $(
            grep @ckeditor/ package.json | \
            grep -v @ckeditor/ckeditor5-dev-utils | \
            grep -v @ckeditor/ckeditor5-inspector | \
            sed 's/^[^"]*"\([^"]*\)".*/\1@44.1.0/' \
        )
    
    [1] https://github.com/ckeditor/ckeditor5/blob/master/docs/updating/update-to-44.md#required-license-key-configuration
    
    Resolves: #105896
    Releases: main, 13.4, 12.4
    Change-Id: I1699bb9c34150219f722ed8d72f22e8c782e8bfd
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/87677
    Tested-by: core-ci <typo3@b13.com>
    Reviewed-by: Andreas Kienast <akienast@scripting-base.de>
    Tested-by: Andreas Kienast <akienast@scripting-base.de>

--- typo3/sysext/rte_ckeditor/Resources/Public/Contrib/@ckeditor/ckeditor5-essentials.js
+++ typo3/sysext/rte_ckeditor/Resources/Public/Contrib/@ckeditor/ckeditor5-essentials.js
@@ -1,5 +1,5 @@
-import{Plugin as r}from"@ckeditor/ckeditor5-core";import{Clipboard as t}from"@ckeditor/ckeditor5-clipboard";import{Enter as o,ShiftEnter as e}from"@ckeditor/ckeditor5-enter";import{SelectAll as i}from"@ckeditor/ckeditor5-select-all";import{Typing as c}from"@ckeditor/ckeditor5-typing";import{Undo as d}from"@ckeditor/ckeditor5-undo";import{AccessibilityHelp as m}from"@ckeditor/ckeditor5-ui";
+import{Plugin as r}from"@ckeditor/ckeditor5-core";import{Clipboard as t}from"@ckeditor/ckeditor5-clipboard";import{Enter as i,ShiftEnter as e}from"@ckeditor/ckeditor5-enter";import{SelectAll as o}from"@ckeditor/ckeditor5-select-all";import{Typing as c}from"@ckeditor/ckeditor5-typing";import{Undo as d}from"@ckeditor/ckeditor5-undo";import{AccessibilityHelp as m}from"@ckeditor/ckeditor5-ui";
 /**
  * @license Copyright (c) 2003-2024, CKSource Holding sp. z o.o. All rights reserved.
- * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
- */class k extends r{static get requires(){return[m,t,o,i,e,c,d]}static get pluginName(){return"Essentials"}}export{k as Essentials};
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-licensing-options
+ */class k extends r{static get requires(){return[m,t,i,o,e,c,d]}static get pluginName(){return"Essentials"}static get isOfficialPlugin(){return!0}}export{k as Essentials};
