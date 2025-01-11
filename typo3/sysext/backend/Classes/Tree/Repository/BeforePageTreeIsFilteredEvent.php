d66434139cf620767e041b980ac17a369313ca91
Lina Wolf <noreply@example.com>
2025-01-11 10:53:02 +0000

    [DOCS] Fix FQN in change log entry for BeforePageTreeIsFilteredEvent
    Additionally, the property documentation is
    also added to the event class directly.
    
    Resolves: #105916
    Related: #105833
    Releases: main
    Change-Id: I5e18fd631473b8ff774cc4e26e1365225f85e7e8
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/87701
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Oliver Bartsch <bo@cedev.de>
    Reviewed-by: Benni Mack <benni@typo3.org>
    Tested-by: Benni Mack <benni@typo3.org>
    Reviewed-by: Oliver Bartsch <bo@cedev.de>

--- typo3/sysext/backend/Classes/Tree/Repository/BeforePageTreeIsFilteredEvent.php
+++ typo3/sysext/backend/Classes/Tree/Repository/BeforePageTreeIsFilteredEvent.php
@@ -26,9 +26,11 @@ use TYPO3\CMS\Core\Database\Query\QueryBuilder;
 final class BeforePageTreeIsFilteredEvent
 {
     public function __construct(
+        /** @param CompositeExpression $searchParts The search parts to be used for filtering */
         public CompositeExpression $searchParts,
-        /** @param int[] $searchUids */
+        /** @param int[] $searchUids The UIDs to be used for filtering by a special search part, which is added by Core always after listener evaluation */
         public array $searchUids,
+        /** @param string $searchPhrase The complete search phrase, as entered by the user */
         public readonly string $searchPhrase,
         /** @param QueryBuilder $queryBuilder This instance is provided for context and to simplify the creation of the search parts and must not be manipulated by listeners */
         public readonly QueryBuilder $queryBuilder,
