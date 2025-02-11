b1fddc388da5c8de77ec8ab7920225d7737b9318
Benni Mack <benni@typo3.org>
2025-02-11 09:51:58 +0100

    [RELEASE] Release of TYPO3 13.4.5
    Change-Id: I25f24a267e84e9d9fadcfbac62e050f7bf28f63f
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/88167
    Reviewed-by: Benni Mack <benni@typo3.org>
    Tested-by: Benni Mack <benni@typo3.org>

--- typo3/sysext/core/Tests/Functional/Fixtures/Extensions/test_mm/ext_emconf.php
+++ typo3/sysext/core/Tests/Functional/Fixtures/Extensions/test_mm/ext_emconf.php
@@ -6,14 +6,14 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'DataHandler Testing MM',
     'description' => 'DataHandler Testing MM',
     'category' => 'example',
-    'version' => '14.0.0',
+    'version' => '13.4.5',
     'state' => 'beta',
     'author' => 'Oliver Bartsch',
     'author_email' => 'bo@cedev.de',
     'author_company' => '',
     'constraints' => [
         'depends' => [
-            'typo3' => '14.0.0',
+            'typo3' => '13.4.5',
         ],
         'conflicts' => [],
         'suggests' => [],
