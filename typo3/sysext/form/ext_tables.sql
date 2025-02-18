f03be04f7292b969336d610cf040d5c605df69cd
Garvin Hicking <gh@faktor-e.de>
2025-02-18 13:58:39 +0100

    [TASK] Add index to sys_refindex for EXT:form softref_key lookup
    This patch adds another sys_refindex key for EXT:form specific
    database queries like:
    
    ```
    SELECT ref_uid
      FROM sys_refindex
     WHERE softref_key='formPersistenceIdentifier'
       AND ref_uid > 0;
    ```
    
    which would otherwise create a full-table scan and perform bad.
    
    Resolves: #106037
    Releases: main, 13.4
    Change-Id: I3bb775ac8f638e6f9a62221a6530b016f1f2a034
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/88284
    Tested-by: Garvin Hicking <gh@faktor-e.de>
    Tested-by: core-ci <typo3@b13.com>
    Reviewed-by: Garvin Hicking <gh@faktor-e.de>

--- typo3/sysext/form/ext_tables.sql
+++ typo3/sysext/form/ext_tables.sql
@@ -3,5 +3,7 @@ CREATE TABLE sys_refindex (
 	# @todo: Solve differently somehow. It is essentially needed because not all form.yaml
 	#        are FAL resources, but can be provided by extensions, too. See the registered
 	#        softref parser for more details, too.
-	KEY lookup_string (ref_string(191))
+	KEY lookup_string (ref_string(191)),
+	# Prevent full table scan for queries like "WHERE softref_key='formPersistenceIdentifier' and ref_uid > 0"
+	KEY idx_softref_key (softref_key,ref_uid)
 );
