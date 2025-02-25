111b6752aae6ee62e8ab3d781bdaadc73e2a0a63
Philipp Wrann <noreply@example.com>
2025-02-25 12:10:52 +0000

    [TASK] Avoid loading metadata for files when obtaining the "file" property
    Because FAL loads all files in one folder before applying a pagination
    strategy, custom drivers, that make use of heavy i/o tasks may have very
    poor performance, especially when dealing with many files in one folder.
    
    By avoiding access to the metadata of a file the response time can still be
    reduced substantially.
    
    The default file-list rendering will sort by "file" which is the sys_file
    uid but will be obtained via the metadata aspect. This is not necessary as
    the file object should be able to return the "name" without using the
    metadata aspect.
    
    Releases: main, 13.4, 12.4
    Resolves: #106248
    Change-Id: I286f39d57f9f9e5a192a3148ba9953e5abf8f26d
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/88745
    Reviewed-by: Benni Mack <benni@typo3.org>
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Benni Mack <benni@typo3.org>

--- typo3/sysext/filelist/Classes/FileList.php
+++ typo3/sysext/filelist/Classes/FileList.php
@@ -1651,6 +1651,8 @@ class FileList
                 return $resource->getModificationTime() . 't';
             case 'crdate':
                 return $resource->getCreationTime() . 'c';
+            case 'file':
+                return $resource->getName();
             default:
                 return $resource->hasProperty($sortField) ? (string)$resource->getProperty($sortField) : '';
         }
