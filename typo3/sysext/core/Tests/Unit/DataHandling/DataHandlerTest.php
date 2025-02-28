ac7dfa4ac576c410946828c8f9b8d8f8859c4359
Oliver Bartsch <bo@cedev.de>
2025-02-28 12:10:23 +0100

    [BUGFIX] Handle NULL as JSON value
    By default a TCA type "json" field,
    corresponding database column is
    generated as nullable with NULL as
    default value.
    
    When not setting a TCA default value,
    new records therefore might be created
    with NULL as value, which no longer
    fails with a TypeError as we now also
    handle NULL in DataHandler.
    
    Resolves: #106274
    Releases: main, 13.4, 12.4
    Change-Id: Iee4890379fd81a850f32603e461ade509f2c3ed6
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/88400
    Reviewed-by: Garvin Hicking <gh@faktor-e.de>
    Tested-by: Georg Ringer <georg.ringer@gmail.com>
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Garvin Hicking <gh@faktor-e.de>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Reviewed-by: Georg Ringer <georg.ringer@gmail.com>

--- typo3/sysext/core/Tests/Unit/DataHandling/DataHandlerTest.php
+++ typo3/sysext/core/Tests/Unit/DataHandling/DataHandlerTest.php
@@ -1138,6 +1138,10 @@ final class DataHandlerTest extends UnitTestCase
             '',
             ['value' => []],
         ];
+        yield 'Converts null to array' => [
+            null,
+            ['value' => []],
+        ];
         yield 'Handles invalid JSON' => [
             '_-invalid-_',
             [],
@@ -1154,7 +1158,7 @@ final class DataHandlerTest extends UnitTestCase
 
     #[DataProvider('checkValueForJsonDataProvider')]
     #[Test]
-    public function checkValueForJson(string|array $input, array $expected): void
+    public function checkValueForJson(string|array|null $input, array $expected): void
     {
         self::assertSame(
             $expected,
