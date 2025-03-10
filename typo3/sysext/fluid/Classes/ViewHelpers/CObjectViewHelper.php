ab92893d886755e21051e7bf368fa99fd3181302
Lina Wolf <112@linawolf.de>
2025-03-10 13:18:57 +0100

    [DOCS] Move extended Fluid cObject examples into Fluid Reference
    The example section was moved into the ViewHelper Reference and
    improved with https://github.com/TYPO3-Documentation/TYPO3CMS-Reference-ViewHelper/pull/92
    
    Resolves: #106348
    Releases: main, 13.4
    Change-Id: I309d38e20e741a42f4da009c4deed13abe7927a3
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/88555
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Simon Praetorius <simon@praetorius.me>
    Reviewed-by: Simon Praetorius <simon@praetorius.me>
    Tested-by: Georg Ringer <georg.ringer@gmail.com>
    Reviewed-by: Georg Ringer <georg.ringer@gmail.com>
    Tested-by: Garvin Hicking <gh@faktor-e.de>

--- typo3/sysext/fluid/Classes/ViewHelpers/CObjectViewHelper.php
+++ typo3/sysext/fluid/Classes/ViewHelpers/CObjectViewHelper.php
@@ -34,52 +34,7 @@ use TYPO3Fluid\Fluid\Core\ViewHelper\Exception;
  * .. note::
  *    You have to ensure proper escaping (htmlspecialchars/intval/etc.) on your own!
  *
- * Examples
- * ========
- *
- * Render lib object
- * -----------------
- *
- * ::
- *
- *    <f:cObject typoscriptObjectPath="lib.someLibObject" />
- *
- * Rendered :typoscript:`lib.someLibObject`.
- *
- * Specify cObject data & current value
- * ------------------------------------
- *
- * ::
- *
- *    <f:cObject typoscriptObjectPath="lib.customHeader" data="{article}" currentValueKey="title" />
- *
- * Rendered :typoscript:`lib.customHeader`. Data and current value will be available in TypoScript.
- *
- * Inline notation
- * ---------------
- *
- * ::
- *
- *    {article -> f:cObject(typoscriptObjectPath: 'lib.customHeader')}
- *
- * Rendered :typoscript:`lib.customHeader`. Data will be available in TypoScript.
- *
- * Accessing the data in TypoScript
- * --------------------------------
- *
- * .. code-block:: typoscript
- *
- *    lib.customHeader = COA
- *    lib.customHeader {
- *        10 = TEXT
- *        10.field = author
- *        20 = TEXT
- *        20.current = 1
- *    }
- *
- * When passing an object with ``{data}``, the properties of the object are accessible with :typoscript:`.field` in
- * TypoScript. If only a single value is passed or the ``currentValueKey`` is specified, :typoscript:`.current = 1`
- * can be used in the TypoScript.
+ * @see https://docs.typo3.org/permalink/t3viewhelper:typo3-fluid-cobject
  */
 final class CObjectViewHelper extends AbstractViewHelper
 {
