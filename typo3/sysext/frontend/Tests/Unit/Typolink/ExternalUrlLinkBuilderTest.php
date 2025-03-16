2c654808d639bacce090585415bf36c3497535a3
Georg Ringer <georg.ringer@gmail.com>
2025-03-16 09:01:01 +0100

    [BUGFIX] Handle relative links with / as internal links
    If a link is created not using the link API but using an external URL
    starting with "/", it should still use the internal target and not
    the default "_blank" value.
    
    Resolves: #105331
    Releases: main, 13.4, 12.4
    Change-Id: Ice7a96ae1d1c62392cf169808351132ed265deaf
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/88615
    Tested-by: core-ci <typo3@b13.com>
    Reviewed-by: Georg Ringer <georg.ringer@gmail.com>
    Tested-by: Georg Ringer <georg.ringer@gmail.com>

---
+++ typo3/sysext/frontend/Tests/Unit/Typolink/ExternalUrlLinkBuilderTest.php
@@ -0,0 +1,106 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3\CMS\Frontend\Tests\Unit\Typolink;
+
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\MockObject\MockObject;
+use TYPO3\CMS\Core\Http\ServerRequest;
+use TYPO3\CMS\Core\Routing\PageArguments;
+use TYPO3\CMS\Core\Site\Entity\Site;
+use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
+use TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController;
+use TYPO3\CMS\Frontend\Typolink\ExternalUrlLinkBuilder;
+use TYPO3\TestingFramework\Core\AccessibleObjectInterface;
+use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
+
+final class ExternalUrlLinkBuilderTest extends UnitTestCase
+{
+    #[Test]
+    public function targetIsAppliedProperly(): void
+    {
+        $linkDetails = [
+            'type' => 'url',
+            'url' => 'https://example.com',
+        ];
+        $subject = $this->prepareSubject();
+        $actualResult = $subject->build($linkDetails, '', 'custom-target', []);
+        self::assertSame('https://example.com', $actualResult->getLinkText());
+        self::assertSame('https://example.com', $actualResult->getUrl());
+        self::assertSame('custom-target', $actualResult->getTarget());
+    }
+
+    #[Test]
+    public function targetFallbackIsAppliedFromExtTargetWithSchemeAndDomain(): void
+    {
+        $linkDetails = [
+            'type' => 'url',
+            'url' => 'https://example.com',
+        ];
+        $subject = $this->prepareSubject();
+        $actualResult = $subject->build($linkDetails, '', '', []);
+        self::assertSame('externalFallback', $actualResult->getTarget());
+    }
+
+    #[Test]
+    public function targetFallbackIsAppliedFromExtTargetWithProtocolRelativeDomain(): void
+    {
+        $linkDetails = [
+            'type' => 'url',
+            // see https://en.wikipedia.org/wiki/Wikipedia:Protocol-relative_URL
+            'url' => '//example.com',
+        ];
+        $subject = $this->prepareSubject();
+        $actualResult = $subject->build($linkDetails, '', '', []);
+        self::assertSame('externalFallback', $actualResult->getTarget());
+    }
+    #[Test]
+    public function targetFallbackIsAppliedForAbsolutePath(): void
+    {
+        $linkDetails = [
+            'type' => 'url',
+            'url' => '/other-system-like-a-blog-on-the-same-domain',
+        ];
+        $subject = $this->prepareSubject();
+        $actualResult = $subject->build($linkDetails, '', '', []);
+        self::assertSame('internalFallback', $actualResult->getTarget());
+    }
+
+    private function prepareSubject(): ExternalUrlLinkBuilder&MockObject&AccessibleObjectInterface
+    {
+        $request = new ServerRequest('https://example.com');
+        $request = $request->withAttribute('routing', new PageArguments(1, '', [], [], []));
+        $request = $request->withAttribute('site', new Site('mySite', 1, []));
+
+        $tsfe = $this
+            ->getMockBuilder(TypoScriptFrontendController::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $tsfe->config = [
+            'config' => [
+                'extTarget' => 'externalFallback',
+                'intTarget' => 'internalFallback',
+            ],
+        ];
+        $cObj = new ContentObjectRenderer();
+        $cObj->setRequest($request);
+        $subject = $this->getAccessibleMock(ExternalUrlLinkBuilder::class, null, [], '', false);
+        $subject->_set('contentObjectRenderer', $cObj);
+        $subject->_set('typoScriptFrontendController', $tsfe);
+        return $subject;
+    }
+}
