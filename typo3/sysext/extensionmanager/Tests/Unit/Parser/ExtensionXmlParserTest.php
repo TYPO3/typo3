63ff3b5bef185e3f9d57c421d4ca6cf68c80c363
Garvin Hicking <gh@faktor-e.de>
2025-03-18 08:51:37 +0100

    [TASK] Remove deprecated call to `xml_set_object()`, add tests
    With PHP 8.4, the function `xml_set_object()` has been deprecated,
    see [1][2].
    
    This function allowed to set a reference to an object when using
    strings in `xml_set_...()` specifications for callables.
    
    TYPO3 already uses proper `[$this, 'method']` callable specifications,
    so using `xml_set_object()` did not have any effect here.
    Thus, it can not only be removed for PHP 8.4 compatibility, but also
    for at least PHP 8.0 (according to [2] already since PHP 5.3).
    
    Since any PHP version compatible to the scope of this patch's TYPO3
    versions can deal without this call, it can be safely
    removed.
    
    Since the `parseXml()` methods did not provide any test coverage,
    this is added with this patch, too.
    
    [1] https://www.php.net/manual/en/migration84.deprecated.php#migration84.deprecated.core.xml
    [2] https://externals.io/message/123816
    
    Resolves: #106376
    Releases: main, 13.4, 12.4
    Change-Id: I3f825afccde4939d26a315204de13826ec831aaa
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/88710
    Tested-by: core-ci <typo3@b13.com>
    Reviewed-by: Georg Ringer <georg.ringer@gmail.com>
    Tested-by: Georg Ringer <georg.ringer@gmail.com>

--- typo3/sysext/extensionmanager/Tests/Unit/Parser/ExtensionXmlParserTest.php
+++ typo3/sysext/extensionmanager/Tests/Unit/Parser/ExtensionXmlParserTest.php
@@ -19,7 +19,9 @@ namespace TYPO3\CMS\Extensionmanager\Tests\Unit\Parser;
 
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Test;
+use TYPO3\CMS\Extensionmanager\Exception\ExtensionManagerException;
 use TYPO3\CMS\Extensionmanager\Parser\ExtensionXmlParser;
+use TYPO3\CMS\Extensionmanager\Tests\Unit\Fixtures\ExtensionXmlParserObserverFixture;
 use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
 
 final class ExtensionXmlParserTest extends UnitTestCase
@@ -80,5 +82,109 @@ final class ExtensionXmlParserTest extends UnitTestCase
             '11.2.3',
             true,
         ];
+    }
+
+    #[Test]
+    public function parseXmlOnMissingFileThrowsException(): void
+    {
+        $subject = $this->getAccessibleMock(ExtensionXmlParser::class, null);
+
+        $this->expectException(ExtensionManagerException::class);
+        $this->expectExceptionCode(1342640689);
+        $subject->parseXml('/path/to/missing/file');
+    }
+
+    #[Test]
+    public function parseXmlWithValidXmlWorks(): void
+    {
+        $subject = $this->getAccessibleMock(ExtensionXmlParser::class, null);
+
+        $observer = new ExtensionXmlParserObserverFixture();
+        $subject->attach($observer);
+        $subject->parseXml(__DIR__ . '/../Fixtures/valid-extensions.xml');
+
+        $expectedStructureFromFixture = [
+            0 => [
+                'extkey' => 'news',
+                'version' => '1.0.0',
+                'alldownloadcounter' => 9001,
+                'downloadcounteR' => 9001,
+                'title' => 'News',
+                'ownerusername' => 'supergeorg',
+                'authorname' => 'Best Georg',
+                'authoremail' => 'support@example.com',
+                'authorcompany' => 'The Georg Company',
+                'lastuploaddate' => 1215424196,
+                't3xfilemd5' => '8b2bcd42d41f54564b150d3d1ed33030',
+                'state' => 'stable',
+                'reviewstate' => 0,
+                'category' => 'plugin',
+                'description' => 'It\'s news.',
+                'dependencies' => 'a:1:{s:7:"depends";a:3:{s:5:"typo3";s:9:"4.1-0.0.0";s:3:"php";s:9:"5.1-0.0.0";s:3:"cms";s:0:"";}}',
+                'uploadcomment' => 'New news, extra extra. Read all about it.',
+                'documentationlink' => 'https://docs.typo3.org/typo3cms/extensions/news',
+                'distributionimage' => '',
+                'distributionwelcomeimage' => '',
+            ],
+            1 => [
+                'extkey' => 'news',
+                'version' => '0.1.0',
+                'alldownloadcounter' => 9001,
+                'downloadcounteR' => 1,
+                'title' => 'News',
+                'ownerusername' => 'supergeorg',
+                'authorname' => 'Best Georg',
+                'authoremail' => 'support@example.com',
+                'authorcompany' => 'The Georg Company',
+                'lastuploaddate' => 1215424196,
+                't3xfilemd5' => '8b2bcd42d41f54564b150d3d1ed33030',
+                'state' => 'beta',
+                'reviewstate' => 0,
+                'category' => 'plugin',
+                'description' => 'It\'s news.',
+                'dependencies' => 'a:1:{s:7:"depends";a:3:{s:5:"typo3";s:9:"4.1-0.0.0";s:3:"php";s:9:"5.1-0.0.0";s:3:"cms";s:0:"";}}',
+                'uploadcomment' => 'New news, extra extra. Read all about it.',
+                'documentationlink' => 'https://docs.typo3.org/typo3cms/extensions/news',
+                'distributionimage' => '',
+                'distributionwelcomeimage' => '',
+            ],
+            2 => [
+                'extkey' => 'fake-news',
+                'version' => '1.0.0',
+                'alldownloadcounter' => 123123123,
+                'downloadcounteR' => 123123124,
+                'title' => 'Fake News',
+                'ownerusername' => 'anyone',
+                'authorname' => 'Mirror Universe Georg',
+                'authoremail' => 'support@example.com',
+                'authorcompany' => 'Starship Fakeprice',
+                'lastuploaddate' => 1215424196,
+                't3xfilemd5' => '8b2bcd42d41f54564b150d3d1ed33030',
+                'state' => 'stable',
+                'reviewstate' => 0,
+                'category' => 'plugin',
+                'description' => 'It\'s fake news.',
+                'dependencies' => 'a:1:{s:7:"depends";a:3:{s:5:"typo3";s:9:"4.1-0.0.0";s:3:"php";s:9:"5.1-0.0.0";s:3:"cms";s:0:"";}}',
+                'uploadcomment' => 'Be kind.',
+                'documentationlink' => 'https://docs.typo3.org/typo3cms/extensions/news',
+                'distributionimage' => '',
+                'distributionwelcomeimage' => '',
+            ],
+        ];
+
+        self::assertEquals($expectedStructureFromFixture, $observer->rows);
+    }
+
+    #[Test]
+    public function parseXmlWithInvalidXmlThrowsException(): void
+    {
+        $subject = $this->getAccessibleMock(ExtensionXmlParser::class, null);
+
+        $observer = new ExtensionXmlParserObserverFixture();
+        $subject->attach($observer);
+
+        $this->expectException(ExtensionManagerException::class);
+        $this->expectExceptionCode(1342640703);
+        $subject->parseXml(__DIR__ . '/../Fixtures/invalid-extensions.xml');
     }
 }
