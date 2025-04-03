1315a40f558a3e1b454dd9ba199dc8f5194788c5
Benjamin Franzke <ben@bnf.dev>
2025-04-03 10:52:15 +0200

    [BUGFIX] Improve accessibility in sudo-mode modal
    Improve accessibility for password managers by providing the
    username field for user password authentication and use our
    toplevel module API to cover the entire backend and re-use
    our existing modal implementation, including coverage of the
    entire backend instead of the module frame only.
    
    Releases: main, 13.4, 12.4
    Resolves: #106571
    Change-Id: I9ce9ca9181f11b9d717b7d56602a9e26cd4c518c
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/89123
    Reviewed-by: Andreas Kienast <akienast@scripting-base.de>
    Tested-by: Andreas Kienast <akienast@scripting-base.de>
    Tested-by: Benjamin Franzke <ben@bnf.dev>
    Reviewed-by: Oliver Hader <oliver.hader@typo3.org>
    Tested-by: core-ci <typo3@b13.com>
    Reviewed-by: Benjamin Franzke <ben@bnf.dev>
    Tested-by: Oliver Hader <oliver.hader@typo3.org>

--- Build/Sources/TypeScript/backend/security/element/sudo-mode.ts
+++ Build/Sources/TypeScript/backend/security/element/sudo-mode.ts
@@ -14,9 +14,12 @@
 import { customElement, property, query, state } from 'lit/decorators';
 import { html, LitElement, nothing, type PropertyValues, type TemplateResult } from 'lit';
 import AjaxRequest from '@typo3/core/ajax/ajax-request';
-import { lll } from '@typo3/core/lit-helper';
+import { AjaxResponse } from '@typo3/core/ajax/ajax-response';
 import Viewport from '@typo3/backend/viewport';
-import type { AjaxResponse } from '@typo3/core/ajax/ajax-response';
+import { topLevelModuleImport } from '@typo3/backend/utility/top-level-module-import';
+import Modal, { Sizes } from '@typo3/backend/modal';
+import { SeverityEnum } from '@typo3/backend/enum/severity';
+import type { ReactiveElement } from '@lit/reactive-element';
 
 interface SudoModeResponse {
   message: string;
@@ -25,6 +28,43 @@ interface SudoModeResponse {
   }
 }
 
+interface Labels {
+  cancel: string;
+  verify: string;
+  password: string;
+  verifyWithUserPassword: string;
+  verifyWithInstallToolPassword: string;
+  accessGranted: string;
+  invalidPassword: string;
+  userPasswordMode: string;
+  installToolPasswordMode: string;
+  verificationFailed: string;
+  verificationExpired: string;
+}
+
+abstract class SudoModeProperties extends LitElement {
+  @property({ type: String }) verifyActionUri: string;
+  @property({ type: String }) cancelUri: string;
+  @property({ type: Boolean, attribute: 'has-fatal-error' }) hasFatalError: boolean;
+  @property({ type: Boolean, attribute: 'allow-install-tool-password' }) allowInstallToolPassword: boolean;
+  @property({ type: Object }) labels: Labels;
+}
+
+export const initiateSudoModeModal = async (properties: Omit<SudoModeProperties, keyof LitElement>): Promise<void> => {
+  const isInIframe = window.location !== window.parent.location;
+  if (isInIframe) {
+    // Create a top-level instance
+    topLevelModuleImport('@typo3/backend/security/element/sudo-mode.js');
+  }
+  const el = top.document.createElement('typo3-backend-security-sudo-mode');
+  Object.assign(el, properties);
+  top.document.body.append(el);
+  return new Promise<void>((resolve, reject) => {
+    el.addEventListener('typo3:sudo-mode:verified', () => resolve());
+    el.addEventListener('typo3:sudo-mode:finished', () => reject());
+  });
+};
+
 /**
  * Web Component showing the sudo mode password dialogs. The password verification
  * happens via AJAX, the redirect to the actually requested resources is triggered
@@ -32,8 +72,74 @@ interface SudoModeResponse {
  * `top` frame directly (compared to using `target` in e.g. Fluid HTML).
  */
 @customElement('typo3-backend-security-sudo-mode')
-export class SudoMode extends LitElement {
-  @property({ type: String }) verifyActionUri: string;
+export class SudoMode extends SudoModeProperties {
+
+  protected override render() {
+    return nothing;
+  }
+
+  protected override async firstUpdated() {
+    const isInIframe = window.location !== window.parent.location;
+    // Launched from /sudo-mode/module route
+    if (isInIframe) {
+      try {
+        await initiateSudoModeModal(this.getPropertyValues());
+      } catch {
+        // Go back to previous route when the modal is closed without verification
+        history.go(-1);
+      }
+      return;
+    }
+
+    Modal.advanced({
+      title: this.hasFatalError ? this.labels.verificationFailed : this.labels.verifyWithUserPassword,
+      severity: this.hasFatalError ? SeverityEnum.error : SeverityEnum.notice,
+      size: Sizes.small,
+      additionalCssClasses: ['modal-sudo-mode-verification'],
+      buttons: [
+        this.hasFatalError ? {
+          text: this.labels.cancel,
+          active: true,
+          btnClass: 'btn-default',
+          trigger: () => {
+            top.location.href = this.cancelUri;
+          },
+        } : {
+          text: this.labels.verify,
+          active: true,
+          name: 'verify',
+          form: 'verify-sudo-mode',
+          btnClass: 'btn-primary',
+        }
+      ],
+      content: html`
+        <typo3-backend-security-sudo-mode-form
+          .labels=${this.labels}
+          .verifyActionUri=${this.verifyActionUri}
+          .cancelUri=${this.cancelUri}
+          .hasFatalError=${this.hasFatalError}
+          .allowInstallToolPassword=${this.allowInstallToolPassword}
+          @typo3:sudo-mode:verified=${() => this.dispatchEvent(new Event('typo3:sudo-mode:verified'))}
+        ></typo3-backend-security-sudo-mode-form>
+      `
+    }).addEventListener('typo3-modal-hidden', (): void => {
+      this.dispatchEvent(new Event('typo3:sudo-mode:finished'));
+      this.remove();
+    });
+  }
+
+  protected getPropertyValues(): Omit<this, keyof LitElement> {
+    const properties = {} as Omit<this, keyof LitElement>;
+    const ctor = this.constructor as typeof ReactiveElement;
+    for (const key of ctor.elementProperties.keys() as IterableIterator<keyof Omit<this, keyof LitElement>>) {
+      properties[key] = this[key];
+    }
+    return properties;
+  }
+}
+
+@customElement('typo3-backend-security-sudo-mode-form')
+export class SudoModeForm extends SudoModeProperties {
   @state() useInstallToolPassword = false;
   @state() errorMessage: string = null;
   @query('#password') passwordElement: HTMLInputElement;
@@ -43,65 +149,83 @@ export class SudoMode extends LitElement {
   }
 
   protected override render(): TemplateResult {
-    return html`
-      <div id="sudo-mode-verification" class="modal modal-severity-notice modal-size-small" tabindex="-1" role="dialog">
-        <div class="modal-dialog" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h4 class="modal-title">
-                ${lll(this.useInstallToolPassword ? 'verifyWithInstallToolPassword' : 'verifyWithUserPassword')}
-              </h4>
-            </div>
-            <div class="modal-body">
-              <div>
-                ${this.errorMessage ? html`
-                  <div class="alert alert-danger" id="invalid-password">${lll(this.errorMessage) || this.errorMessage}</div>
-                ` : nothing}
-                <form method="post" class="form" id="verify-sudo-mode" spellcheck="false" @submit=${(evt: SubmitEvent) => this.verifyPassword(evt)}>
-                  <div class="form-group">
-                    <label class="form-label" for="password">${lll('password')}</label>
-                    <input required="required" class="form-control" id="password" type="password" name="password"
-                            autocomplete=${this.useInstallToolPassword ? 'section-install current-password' : 'current-password'}>
-                  </div>
-                </form>
-                <div class="text-end">
-                  <a href="#" @click=${(evt: MouseEvent) => this.toggleUseInstallToolPassword(evt)}>
-                    ${lll(this.useInstallToolPassword ? 'userPasswordMode' : 'installToolPasswordMode')}
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div class="modal-footer">
-              <button type="submit" form="verify-sudo-mode" class="btn btn-primary" role="button">
-                ${lll('verify')}
-              </button>
-            </div>
-          </div>
+    if (this.hasFatalError) {
+      return html`
+        <div>
+          <div class="alert alert-danger">${this.labels.verificationExpired}</div>
         </div>
+      `;
+    }
+    return html`
+      <div>
+        ${this.errorMessage ? html`
+          <div class="alert alert-danger" id="invalid-password">${this.labels[this.errorMessage as keyof Labels] || this.errorMessage}</div>
+        ` : nothing}
+        <form method="post" class="form" id="verify-sudo-mode" spellcheck="false" @submit=${(evt: SubmitEvent) => this.verifyPassword(evt)}>
+          ${this.useInstallToolPassword ? nothing : html`
+            <input hidden aria-hidden="true" type="text" autocomplete="username" value=${TYPO3.configuration.username}>
+          `}
+          <div class="form-group">
+            <label class="form-label" for="password">${this.labels.password}</label>
+            <input required="required" class="form-control" id="password" type="password" name="password"
+                   autocomplete=${this.useInstallToolPassword ? 'section-install current-password' : 'current-password'}>
+          </div>
+        </form>
+        ${!this.allowInstallToolPassword ? nothing : html`
+          <div class="text-end">
+            <a href="#" @click=${(evt: MouseEvent) => this.toggleUseInstallToolPassword(evt)}>
+              ${this.useInstallToolPassword ? this.labels.userPasswordMode : this.labels.installToolPasswordMode}
+            </a>
+          </div>
+        `}
       </div>
     `;
   }
 
-  protected override firstUpdated(_changedProperties: PropertyValues): void {
-    super.firstUpdated(_changedProperties);
-    this.passwordElement.focus();
+  protected override updated(changedProperties: PropertyValues): void {
+    if (changedProperties.has('useInstallToolPassword')) {
+      this.closest('typo3-backend-modal').modalTitle = this.getModalTitle();
+    }
   }
 
-  private verifyPassword(evt: SubmitEvent): void {
+  protected override firstUpdated(_changedProperties: PropertyValues): void {
+    super.firstUpdated(_changedProperties);
+    this.passwordElement?.focus();
+  }
+
+  protected getModalTitle() {
+    if (this.hasFatalError) {
+      return this.labels.verificationFailed;
+    }
+    if (this.useInstallToolPassword) {
+      return this.labels.verifyWithInstallToolPassword;
+    }
+    return this.labels.verifyWithUserPassword;
+  }
+
+  private async verifyPassword(evt: SubmitEvent): Promise<void> {
     evt.preventDefault();
     this.errorMessage = null;
-    (new AjaxRequest(this.verifyActionUri))
-      .post({ password: this.passwordElement.value, useInstallToolPassword: this.useInstallToolPassword ? 1 : 0 })
-      .then(async (ajaxResponse: AjaxResponse) => {
-        const response: SudoModeResponse = await ajaxResponse.resolve('application/json');
-        if (response.redirect) {
-          Viewport.ContentContainer.setUrl(response.redirect.uri);
-        }
-      })
-      .catch(async (ajaxResponse: AjaxResponse) => {
-        const response = await ajaxResponse.resolve('application/json');
-        this.errorMessage = response.message;
+    try {
+      const response = await new AjaxRequest(this.verifyActionUri).post({
+        password: this.passwordElement.value,
+        useInstallToolPassword: this.useInstallToolPassword ? 1 : 0
       });
+
+      const responseData: SudoModeResponse = await response.resolve('application/json');
+      this.dispatchEvent(new Event('typo3:sudo-mode:verified'));
+      this.closest('typo3-backend-modal').hideModal();
+      if (responseData.redirect) {
+        Viewport.ContentContainer.setUrl(responseData.redirect.uri);
+      }
+    } catch (e: unknown) {
+      if (e instanceof AjaxResponse) {
+        const response = await e.resolve('application/json');
+        this.errorMessage = response.message;
+      } else {
+        throw e;
+      }
+    }
   }
 
   private toggleUseInstallToolPassword(evt: MouseEvent): void {
@@ -113,5 +237,6 @@ export class SudoMode extends LitElement {
 declare global {
   interface HTMLElementTagNameMap {
     'typo3-backend-security-sudo-mode': SudoMode;
+    'typo3-backend-security-sudo-mode-form': SudoModeForm;
   }
 }
