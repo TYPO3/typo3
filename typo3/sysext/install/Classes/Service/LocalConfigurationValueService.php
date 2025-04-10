7aa686d741efb9e46c4849aabb83c00f19574d59
Garvin Hicking <gh@faktor-e.de>
2025-04-10 17:57:58 +0200

    [TASK] Add GUI-managed map (keys+values) and list for Configuration
    This patch allows to manage PHP arrays with keys and values ("map")
    and array with numerical keys and values ("element-list") in the
    backend module "Admin Tools > Settings > Configure Installation-Wide
    Options".
    
    Currently, array values like GFX.processor_stripColorProfileParameters
    contain an input box where array values need to be entered
    by maintainers/integrators separated by a comma. This is prone to
    errors and visually not appealing.
    
    With this patch, key/value pairs (or just value lists) are shown inside
    a table, where each row can be removed or new rows
    added. This works both with string-indexed maps and also
    numerical indexes.
    
    For this, the type "map" has been created,
    and a similar GUI is available for "element-list",
    where only array values are maintained. Note that the
    type "list" was already utilized for a single input text
    with commaseparated values, so the new type needed a
    distinct naming.
    
    Some configuration options [1] have been converted from
    "array" to "element-list" to showcase this, and a
    demo option "GFX.imagefile_defaultProcessing" has been
    added (see #93981, see patchsets up to PS11).
    
    This allows for a resulting configuration like:
    
    ```
    'imagefile_defaultProcessing' = [
      'default' => 'png',
      'jpg'     => 'avif',
      'webp'    => 'avif',
    ]
    ```
    
    SCREENSHOTS:
    
    https://www.dropbox.com/scl/fi/t8jcmbmadlzkh48oew2xd/Screenshot-2025-04-11-at-10.32.36.png?rlkey=5asx0y95u0rxqk0dw5y0aejbz&dl=1
    https://www.dropbox.com/scl/fi/synvsizzrmnqzmq0iwyt6/Screenshot-2025-04-11-at-10.36.58.png?rlkey=24t7eunq9jtog70njl01vxb9r&dl=1
    
    [1] GFX.processor_stripColorProfileParameters
        SYS.allowedPhpDisableFunctions
        BE.lockRootPath
    
    Resolves: #106553
    Related: #93981
    Releases: main
    Change-Id: Id5fdba21283dd28d8ca344a7c22b9dd47ef75b0c
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/89332
    Reviewed-by: Benni Mack <benni@typo3.org>
    Tested-by: Benni Mack <benni@typo3.org>
    Tested-by: core-ci <typo3@b13.com>

--- typo3/sysext/install/Classes/Service/LocalConfigurationValueService.php
+++ typo3/sysext/install/Classes/Service/LocalConfigurationValueService.php
@@ -66,6 +66,21 @@ class LocalConfigurationValueService
     /**
      * Because configuration entries can be at any sub-array level, we need
      * to check entries recursively.
+     * Supported description types are:
+     *  - `bool`            boolean on/off toggles
+     *  - `dropdown`        dropdowns
+     *  - `text`            single-line text
+     *  - `int`             number inputs
+     *  - `list`            single-line text with comma-separated values
+     *  - `multiline`       multi-line text input
+     *  - `password`        password input
+     *  - `mixed`           mixed-types, which can behave either as "text" or an array (only via manually editing `settings.php`)
+     *  - `container`       a container for grouping multiple inputs
+     *  - `phpClass`        a string representing a PHP classname
+     *  - `errors`          a special dropdowns for PHP error mappings
+     *  - `array`           comma-separated values treated as a "list" (like an "array with numerical values")
+     *  - `map`             array keys+values (`$someArray['someKey' => 'someValue']`)
+     *  - `element-list`    numerical indexed array values (`$someArray[] = 'someValue'`)
      */
     protected function recursiveConfigurationFetching(array $sections, array $sectionsFromCurrentConfiguration, array $descriptions, array $path = []): array
     {
@@ -112,9 +127,28 @@ class LocalConfigurationValueService
                         $itemData['type'] = 'number';
                         $itemData['value'] = (int)$value;
                         break;
+                    case 'map':
+                        $itemData['type'] = 'map';
+                        // Compatibility
+                        $itemData['value'] = is_array($value) ? implode(',', $value) : (string)$value;
+                        $itemData['values'] = is_array($value) ? $value : null;
+                        $itemData['hideValue'] = true;
+                        $itemData['arrayKey'] = $descriptionInfo['arrayKey'] ?? 'Key';
+                        $itemData['arrayValue'] = $descriptionInfo['arrayValue'] ?? 'Value';
+                        break;
+                    case 'element-list':
+                        // Same as above, but without special array key (just numerical index).
+                        $itemData['type'] = 'element-list';
+                        // Compatibility
+                        $itemData['value'] = is_array($value) ? implode(',', $value) : (string)$value;
+                        $itemData['values'] = is_array($value) ? $value : null;
+                        $itemData['hideValue'] = true;
+                        $itemData['arrayValue'] = $descriptionInfo['arrayValue'] ?? 'Value';
+                        break;
                     case 'array':
                         $itemData['type'] = 'input';
                         // @todo The line below should be improved when the array handling is introduced in the global settings manager.
+                        // @todo Also the types 'map' and 'element-list' above should be revisited then.
                         $itemData['value'] = is_array($value)
                             ? implode(',', $value)
                             : (string)$value;
@@ -193,6 +227,43 @@ class LocalConfigurationValueService
                 // Cast integer values to integers (but only for values that can not contain a string as well)
                 $value = (int)$value;
                 $valueHasChanged = (int)$oldValue !== $value;
+            } elseif ($dataType === 'map') {
+                $oldValueAsJson = json_encode($oldValue);
+                $valueHasChanged = $oldValueAsJson !== json_encode($value);
+                // Validate array
+                if (!is_array($value)) {
+                    $value = [];
+                }
+                $cleanedArray = [];
+                foreach ($value as $arrayKey => $arrayValue) {
+                    if (!is_scalar($arrayValue)) {
+                        // Sub-arrays and any non-scalar key or value type not supported
+                        continue;
+                    }
+                    // Note: Actual values (unlike keys) are scrubbed later and need no slashing here.
+                    // Restoring config values with slashes is a problem, so instead we use htmlentities() to escape
+                    // single and double quotes, which keeps PHP namespace backslashes.
+                    // @todo may need further inspection.
+                    $cleanedArray[htmlentities($arrayKey)] = $arrayValue;
+                }
+                $value = $cleanedArray;
+            } elseif ($dataType === 'element-list') {
+                // Validate array
+                if (!is_array($value)) {
+                    $value = [];
+                }
+
+                // Iterate list, throw away keys, start off zero-based.
+                $elementList = $value;
+                $value = [];
+                foreach ($elementList as $arrayValue) {
+                    if (is_scalar($arrayValue)) {
+                        // Sub-arrays and any non-scalar key or value type not supported
+                        $value[] = $arrayValue;
+                    }
+                }
+                $oldValueAsJson = json_encode($oldValue);
+                $valueHasChanged = $oldValueAsJson !== json_encode($elementList);
             } elseif ($dataType === 'array') {
                 $oldValueAsString = is_array($oldValue)
                     ? implode(',', $oldValue)
@@ -219,6 +290,9 @@ class LocalConfigurationValueService
 
                 if (is_bool($value)) {
                     $messageBody = 'New value = ' . ($value ? 'true' : 'false');
+                } elseif ($dataType === 'map') {
+                    // "element-list" is covered by the 'is_array()' case.
+                    $messageBody = 'New array value = ' . json_encode($value);
                 } elseif (empty($value)) {
                     $messageBody = 'New value = none';
                 } elseif (is_array($value)) {
