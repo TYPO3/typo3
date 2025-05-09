18dc58b93f75c64521f77f80fa42d1564b6b7eb7
Oliver Krammer <o.k@i-kiu.at>
2025-05-09 09:48:54 +0200

    [TASK] Change comment in EXT:sys_note
    Resolves: #106673
    Releases: main, 13.4
    Change-Id: Ib15f4fa33c3a5775082aa53339f7aa78a7b08ee8
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/89322
    Reviewed-by: Anja Leichsenring <aleichsenring@ab-softlab.de>
    Tested-by: Benjamin Kott <benjamin.kott@outlook.com>
    Tested-by: Anja Leichsenring <aleichsenring@ab-softlab.de>
    Reviewed-by: Benjamin Kott <benjamin.kott@outlook.com>
    Reviewed-by: Georg Ringer <georg.ringer@gmail.com>
    Tested-by: Georg Ringer <georg.ringer@gmail.com>
    Tested-by: core-ci <typo3@b13.com>

--- typo3/sysext/sys_note/ext_localconf.php
+++ typo3/sysext/sys_note/ext_localconf.php
@@ -6,5 +6,5 @@ use TYPO3\CMS\SysNote\Persistence\NoteCreationEnricher;
 
 defined('TYPO3') or die();
 
-// Fill the "owner" field of a dashboard with the user who created it
+// Fill the "owner" field of a sys_note with the user who created it
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = NoteCreationEnricher::class;
