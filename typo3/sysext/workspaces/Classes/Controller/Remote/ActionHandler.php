efeb7b6156fb6d137b42afd21bce63eb7559a20f
Tobias Seczer <set@earlybird.at>
2025-05-13 19:41:09 +0200

    [BUGFIX] Fix returning records to previous stage in workspaces
    Resolves: #106692
    Releases: main, 13.4
    Change-Id: I91c4988c294aabd6d2ef27f64f082216e09ec485
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/89423
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: core-ci <typo3@b13.com>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>

--- typo3/sysext/workspaces/Classes/Controller/Remote/ActionHandler.php
+++ typo3/sysext/workspaces/Classes/Controller/Remote/ActionHandler.php
@@ -539,7 +539,7 @@ class ActionHandler
     public function sendToPrevStageExecute(\stdClass $parameters)
     {
         $cmdArray = [];
-        $setStageId = $parameters->affects->nextStage;
+        $setStageId = (int)$parameters->affects->nextStage;
         $comments = $parameters->comments;
         $table = $parameters->affects->table;
         $uid = $parameters->affects->uid;
