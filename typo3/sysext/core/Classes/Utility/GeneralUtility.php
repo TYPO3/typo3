c2c24f2f324e3d8de8049d7232505888225af6f8
Andreas Kienast <akienast@scripting-base.de>
2025-05-15 08:04:29 +0200

    [BUGFIX] Update `phpstan/phpstan` to 2.1.15
    Executed commands:
    
        composer require --dev phpstan/phpstan:^2.1.15 -W
        ./Build/Scripts/runTests.sh -s phpstanGenerateBaseline
    
    Resolves:
    Releases: main, 13.4, 12.4
    Change-Id: I2af2c4c12f9b3a25ed891ccb5fa91269211c1675
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/89393
    Tested-by: Stefan Bürk <stefan@buerk.tech>
    Tested-by: Andreas Kienast <akienast@scripting-base.de>
    Tested-by: Garvin Hicking <gh@faktor-e.de>
    Reviewed-by: Benni Mack <benni@typo3.org>
    Reviewed-by: Andreas Kienast <akienast@scripting-base.de>
    Reviewed-by: Garvin Hicking <gh@faktor-e.de>
    Tested-by: Benni Mack <benni@typo3.org>
    Reviewed-by: Stefan Bürk <stefan@buerk.tech>
    Tested-by: core-ci <typo3@b13.com>

--- typo3/sysext/core/Classes/Utility/GeneralUtility.php
+++ typo3/sysext/core/Classes/Utility/GeneralUtility.php
@@ -2722,7 +2722,7 @@ class GeneralUtility
     public static function callUserFunction(string|\Closure $funcName, mixed &$params, ?object $ref = null): mixed
     {
         // Check if we're using a closure and invoke it directly.
-        if (is_object($funcName) && is_a($funcName, \Closure::class)) {
+        if (is_a($funcName, \Closure::class)) {
             return call_user_func_array($funcName, [&$params, &$ref]);
         }
         $funcName = trim($funcName);
