bfc0e43108a8e5b7187f8a648008b80d0149f7dd
Stefan Froemken <froemken@gmail.com>
2025-06-06 12:17:54 +0200

    [BUGFIX] Prevent attaching non-object values in ObjectStorageConverter
    Ensure that only objects are attached to ObjectStorage by adding an
    explicit is_object check when iterating over $convertedChildProperties
    in the ObjectStorageConverter. This avoids potential runtime errors
    when $convertedChildProperties may contain non-object entries.
    
    This issue became evident when processing data via
    PersistentObjectConverter, where non-object values could be present in
    the child properties, in contrast to handling via ObjectConverter. To
    ensure both converters behave consistently and handle edge cases
    correctly, extended functional tests have been implemented for both
    PersistentObjectConverter and ObjectConverter.
    
    Resolves: #100797
    Releases: main, 14.3
    Change-Id: I4cab629128d35fd6ab22b42f227abd0ef5121328
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/95567
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Georg Ringer <georg.ringer@gmail.com>
    Reviewed-by: Georg Ringer <georg.ringer@gmail.com>

---
+++ typo3/sysext/extbase/Tests/Functional/Fixtures/Extensions/type_converter_test/Classes/Domain/Model/Horse.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3Tests\TypeConverterTest\Domain\Model;
+
+use TYPO3\CMS\Extbase\DomainObject\AbstractEntity;
+
+/**
+ * Must extend from AbstractEntity, else ObjectConverter will be used instead of PersistentObjectConverter.
+ */
+class Horse extends AbstractEntity
+{
+    protected ?string $color = null;
+
+    public function getColor(): ?string
+    {
+        return $this->color;
+    }
+
+    public function setColor(?string $color): void
+    {
+        $this->color = $color;
+    }
+}
--- typo3/sysext/extbase/Tests/Functional/Property/TypeConverter/ObjectStorageConverterTest.php
+++ typo3/sysext/extbase/Tests/Functional/Property/TypeConverter/ObjectStorageConverterTest.php
@@ -21,33 +21,128 @@ use PHPUnit\Framework\Attributes\Test;
 use TYPO3\CMS\Extbase\Persistence\ObjectStorage;
 use TYPO3\CMS\Extbase\Property\PropertyMapper;
 use TYPO3\CMS\Extbase\Property\PropertyMappingConfiguration;
+use TYPO3\CMS\Extbase\Property\TypeConverter\PersistentObjectConverter;
 use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
 use TYPO3Tests\TypeConverterTest\Domain\Model\Cat;
+use TYPO3Tests\TypeConverterTest\Domain\Model\Home;
+use TYPO3Tests\TypeConverterTest\Domain\Model\Horse;
 
 final class ObjectStorageConverterTest extends FunctionalTestCase
 {
     protected bool $initializeDatabase = false;
 
     #[Test]
-    public function convertReturnsObjectStorage(): void
+    public function convertHomeWithCatsViaObjectConverterWillResultInTwoCatsInObjectStorage(): void
+    {
+        $propertyMapperConfiguration = new PropertyMappingConfiguration();
+        $propertyMapperConfiguration->allowProperties('cats');
+        $propertyMapperConfiguration->forProperty('cats.*')->allowProperties('color');
+
+        $propertyMapper = $this->get(PropertyMapper::class);
+
+        $home = $propertyMapper->convert(
+            [
+                'cats' => [
+                    0 => ['color' => 'black'],
+                    1 => ['color' => 'white'],
+                ],
+            ],
+            Home::class,
+            $propertyMapperConfiguration
+        );
+
+        self::assertInstanceOf(Home::class, $home);
+        self::assertCount(2, $home->getCats());
+
+        $cat = $home->getCats()->current();
+        self::assertInstanceOf(Cat::class, $cat);
+        self::assertSame('black', $cat->getColor());
+    }
+
+    #[Test]
+    public function convertHomeWithHorsesViaPersistenceObjectConverterWillResultInTwoHorsesInObjectStorage(): void
+    {
+        $propertyMapperConfiguration = new PropertyMappingConfiguration();
+        $propertyMapperConfiguration->allowProperties('horses');
+        $propertyMapperConfiguration
+            ->forProperty('horses.*')
+            ->allowProperties('color')
+            ->setTypeConverterOption(PersistentObjectConverter::class, PersistentObjectConverter::CONFIGURATION_CREATION_ALLOWED, true);
+
+        $propertyMapper = $this->get(PropertyMapper::class);
+
+        $home = $propertyMapper->convert(
+            [
+                'horses' => [
+                    0 => ['color' => 'black'],
+                    1 => ['color' => 'white'],
+                ],
+            ],
+            Home::class,
+            $propertyMapperConfiguration
+        );
+
+        self::assertInstanceOf(Home::class, $home);
+        self::assertCount(2, $home->getHorses());
+
+        $cat = $home->getHorses()->current();
+        self::assertInstanceOf(Horse::class, $cat);
+        self::assertSame('black', $cat->getColor());
+    }
+
+    #[Test]
+    public function convertHomeWithOptionalHorsesViaPersistenceObjectConverterWillResultInOneHorseInObjectStorage(): void
+    {
+        $propertyMapperConfiguration = new PropertyMappingConfiguration();
+        $propertyMapperConfiguration->allowProperties('horses');
+        $propertyMapperConfiguration
+            ->forProperty('horses.*')
+            ->allowProperties('color')
+            ->setTypeConverterOption(PersistentObjectConverter::class, PersistentObjectConverter::CONFIGURATION_CREATION_ALLOWED, true);
+
+        $propertyMapper = $this->get(PropertyMapper::class);
+
+        $home = $propertyMapper->convert(
+            [
+                'horses' => [
+                    0 => ['color' => 'black'],
+                    1 => '',
+                ],
+            ],
+            Home::class,
+            $propertyMapperConfiguration
+        );
+
+        self::assertInstanceOf(Home::class, $home);
+        self::assertCount(1, $home->getHorses());
+
+        $cat = $home->getHorses()->current();
+        self::assertInstanceOf(Horse::class, $cat);
+        self::assertSame('black', $cat->getColor());
+    }
+
+    #[Test]
+    public function convertPlainCatsWillReturnCatsInObjectStorage(): void
     {
         $propertyMapperConfiguration = new PropertyMappingConfiguration();
         $propertyMapperConfiguration->allowAllProperties();
-        $propertyMapperConfiguration->forProperty('foo')->allowAllProperties();
+        $propertyMapperConfiguration->forProperty('*')->allowAllProperties();
 
         $propertyMapper = $this->get(PropertyMapper::class);
 
         $objectStorage = $propertyMapper->convert(
             [
-                'foo' => ['color' => 'black'],
+                0 => ['color' => 'black'],
+                1 => ['color' => 'white'],
             ],
             ObjectStorage::class . '<' . Cat::class . '>',
             $propertyMapperConfiguration
         );
 
         self::assertInstanceOf(ObjectStorage::class, $objectStorage);
-        self::assertCount(1, $objectStorage);
+        self::assertCount(2, $objectStorage);
 
+        $objectStorage->rewind();
         $cat = $objectStorage->current();
         self::assertInstanceOf(Cat::class, $cat);
         self::assertSame('black', $cat->getColor());
