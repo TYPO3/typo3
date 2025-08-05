8f0a43ef7db4be0be006fa6b226ce1ae15b095bf
Simon Praetorius <simon@praetorius.me>
2025-08-05 09:57:30 +0200

    [TASK] Auto-configure Fluid ViewHelperResolver delegates
    With Fluid 4.3, the `ViewHelperResolverDelegateInterface` was
    introduced, which allows to "take over" a full ViewHelper
    namespace instead of just one ViewHelper instance. When Fluid
    was updated to 4.3 in the Core, dependency injection was added
    to ViewHelperResolver delegates. For this to work properly,
    these services need to be defined as `public` since Fluid needs
    to access them on-demand based on the namespaces defined
    in the template.
    
    This patch extends the existing auto-configuration for ViewHelpers
    to also auto-configure ViewHelperResolver delegates as `public`
    based on the implemented interface.
    
    Resolves: #107207
    Related: #106951
    Releases: main, 13.4
    Change-Id: I28ecad6f77cc3c29ac5e0030b00f459e3823c899
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/90358
    Reviewed-by: Stefan Bürk <stefan@buerk.tech>
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Stefan Bürk <stefan@buerk.tech>

--- typo3/sysext/fluid/Configuration/Services.php
+++ typo3/sysext/fluid/Configuration/Services.php
@@ -7,11 +7,14 @@ namespace TYPO3\CMS\Fluid;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use TYPO3\CMS\Core\DependencyInjection\PublicServicePass;
 use TYPO3Fluid\Fluid\Core\ViewHelper\ViewHelperInterface;
+use TYPO3Fluid\Fluid\Core\ViewHelper\ViewHelperResolverDelegateInterface;
 
 return static function (ContainerConfigurator $container, ContainerBuilder $containerBuilder) {
+    // Tag ViewHelper services and configure them to be used by Fluid, which currently
+    // cannot inject them properly and doesn't support shared ViewHelper classes
     $containerBuilder->registerForAutoconfiguration(ViewHelperInterface::class)->addTag('fluid.viewhelper');
-
     $containerBuilder->addCompilerPass(new class () implements CompilerPassInterface {
         public function process(ContainerBuilder $container)
         {
@@ -20,4 +23,9 @@ return static function (ContainerConfigurator $container, ContainerBuilder $cont
             }
         }
     });
+
+    // Tag ViewHelperResolver delegate services and configure them to be used by Fluid,
+    // which currently cannot inject them properly
+    $containerBuilder->registerForAutoconfiguration(ViewHelperResolverDelegateInterface::class)->addTag('fluid.resolverdelegate');
+    $containerBuilder->addCompilerPass(new PublicServicePass('fluid.resolverdelegate'));
 };
