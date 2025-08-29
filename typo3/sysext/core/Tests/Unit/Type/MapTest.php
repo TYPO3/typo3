0e8ac34533a1d11aa21fbf4867d111460c280817
Oliver Hader <oliver@typo3.org>
2025-08-29 12:54:28 +0200

    [BUGFIX] Use array accessors inside \TYPO3\CMS\Core\Type\Map
    Use array accessors in \TYPO3\CMS\Core\Type\Map when modifying
    the internal \SplObjectStorage instance to avoid side-effects
    of the PHP implementation.
    
    The invocation of `\SplObjectStorage::current()` is now guarded
    by a corresponding `\SplObjectStorage::valid()` check, to avoid
    `RuntimeException: Called current() on invalid iterator` when
    exceeding the amount of available entries by calling `next()`.
    
    Resolves: #107352
    Releases: main, 13.4, 12.4
    Change-Id: I59a619a2399dee7fb4e27a71104066db1dc1b520
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/90517
    Reviewed-by: Oliver Hader <oliver.hader@typo3.org>
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Oliver Hader <oliver.hader@typo3.org>

--- typo3/sysext/core/Tests/Unit/Type/MapTest.php
+++ typo3/sysext/core/Tests/Unit/Type/MapTest.php
@@ -17,6 +17,7 @@ declare(strict_types=1);
 
 namespace TYPO3\CMS\Core\Tests\Unit\Type;
 
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Test;
 use TYPO3\CMS\Core\Type\Map;
 use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
@@ -136,5 +137,131 @@ final class MapTest extends UnitTestCase
 
         self::assertSame([$aKey, $bKey], $map->keys());
         self::assertSame([$aValue, $bValue], $map->values());
+    }
+
+    public static function mapNextManagesStateDataProvider(): \Generator
+    {
+        $aKey = new \stdClass();
+        $aKey->value = 'aValue';
+        $bKey = new \stdClass();
+        $bKey->value = 'bValue';
+        yield '=ø @ø ?ø' => ['keys' => [], 'index' => 0, 'expectedKey' => null, 'expectedValid' => false];
+        yield '=a @a ?a' => ['keys' => [$aKey], 'index' => 0, 'expectedKey' => $aKey, 'expectedValid' => true];
+        yield '=a @b! ?ø' => ['keys' => [$aKey], 'index' => 1, 'expectedKey' => null, 'expectedValid' => false];
+        yield '=ab @a ?a' => ['keys' => [$aKey, $bKey], 'index' => 0, 'expectedKey' => $aKey, 'expectedValid' => true];
+        yield '=ab @b ?b' => ['keys' => [$aKey, $bKey], 'index' => 1, 'expectedKey' => $bKey, 'expectedValid' => true];
+        yield '=ab @b! ?ø' => ['keys' => [$aKey, $bKey], 'index' => 2, 'expectedKey' => null, 'expectedValid' => false];
+    }
+
+    #[Test]
+    #[DataProvider('mapNextManagesStateDataProvider')]
+    public function mapNextManagesState(array $keys, int $index, mixed $expectedKey, bool $expectedValid): void
+    {
+        $map = new Map();
+        foreach ($keys as $key) {
+            $map[$key] = $key->value;
+        }
+        for ($i = 0; $i < $index; $i++) {
+            $map->next();
+        }
+        $expectedValue = $expectedKey?->value;
+        self::assertSame($expectedValue, $map->current());
+        self::assertSame($expectedKey, $map->key());
+        self::assertSame($expectedValid, $map->valid());
+    }
+
+    public static function mapSetManagesStateDataProvider(): \Generator
+    {
+        $aKey = new \stdClass();
+        $aKey->value = 'aValue';
+        $bKey = new \stdClass();
+        $bKey->value = 'bValue';
+        $cKey = new \stdClass();
+        $cKey->value = 'cValue';
+        yield '=ø @ø +ab ?a' => ['keys' => [], 'index' => 0, 'modifyKeys' => [$aKey, $bKey], 'expectedKey' => $aKey, 'expectedValid' => true];
+        yield '=ø @b! +ab ?a' => ['keys' => [], 'index' => 1, 'modifyKeys' => [$aKey, $bKey], 'expectedKey' => $aKey, 'expectedValid' => true];
+        yield '=a @a +bc ?a' => ['keys' => [$aKey], 'index' => 0, 'modifyKeys' => [$bKey, $cKey], 'expectedKey' => $aKey, 'expectedValid' => true];
+        yield '=a @b! +bc ?b' => ['keys' => [$aKey], 'index' => 1, 'modifyKeys' => [$bKey, $cKey], 'expectedKey' => $bKey, 'expectedValid' => true];
+        yield '=ab @b +c ?b' => ['keys' => [$aKey, $bKey], 'index' => 1, 'modifyKeys' => [$cKey], 'expectedKey' => $bKey, 'expectedValid' => true];
+        yield '=ab @c! +c ?c' => ['keys' => [$aKey, $bKey], 'index' => 2, 'modifyKeys' => [$cKey], 'expectedKey' => $cKey, 'expectedValid' => true];
+        yield '=abc @c +c ?c' => ['keys' => [$aKey, $bKey, $cKey], 'index' => 2, 'modifyKeys' => [$cKey], 'expectedKey' => $cKey, 'expectedValid' => true];
+    }
+
+    #[Test]
+    #[DataProvider('mapSetManagesStateDataProvider')]
+    public function mapSetManagesState(array $keys, int $index, array $modifyKeys, mixed $expectedKey, bool $expectedValid): void
+    {
+        $map = new Map();
+        foreach ($keys as $key) {
+            $map[$key] = $key->value;
+        }
+        for ($i = 0; $i < $index; $i++) {
+            $map->next();
+        }
+        foreach ($modifyKeys as $modifyKey) {
+            $map[$modifyKey] = $modifyKey->value . '!';
+        }
+        $expectedValue = $expectedKey?->value;
+        if ($expectedValue !== null && in_array($expectedKey, $modifyKeys, true)) {
+            $expectedValue .= '!';
+        }
+        self::assertSame($expectedValue, $map->current());
+        self::assertSame($expectedKey, $map->key());
+        self::assertSame($expectedValid, $map->valid());
+    }
+
+    public static function mapUnsetManagesStateDataProvider(): \Generator
+    {
+        // PHP 8.1 behaves different here
+        // * having [0 => A, 1 => B, 2 => C]
+        // * being a position 1 (B)
+        // * removing the same offset 1 (B)
+        // * PHP 8.1 moves the pointer to 0 (A), PHP 8.2+ moves the pointer to 2 (C, which is actually 1 now)
+        $isAtLeastPhp82 = version_compare(PHP_VERSION, '8.2', '>=');
+
+        $aKey = new \stdClass();
+        $aKey->value = 'aValue';
+        $bKey = new \stdClass();
+        $bKey->value = 'bValue';
+        $cKey = new \stdClass();
+        $cKey->value = 'cValue';
+        yield '=abc @a -a ?b' => ['keys' => [$aKey, $bKey, $cKey], 'index' => 0, 'modifyKeys' => [$aKey], 'expectedKey' => $bKey, 'expectedValid' => true];
+        yield '=abc @b -a ?b' => ['keys' => [$aKey, $bKey, $cKey], 'index' => 1, 'modifyKeys' => [$aKey], 'expectedKey' => $bKey, 'expectedValid' => true];
+        if ($isAtLeastPhp82) {
+            yield '=abc @c -a ?c' => ['keys' => [$aKey, $bKey, $cKey], 'index' => 2, 'modifyKeys' => [$aKey], 'expectedKey' => $cKey, 'expectedValid' => true];
+            yield '=abc @c! -a ?c' => ['keys' => [$aKey, $bKey, $cKey], 'index' => 2, 'modifyKeys' => [$aKey], 'expectedKey' => $cKey, 'expectedValid' => true];
+        }
+        yield '=abc @a -b ?a' => ['keys' => [$aKey, $bKey, $cKey], 'index' => 0, 'modifyKeys' => [$bKey], 'expectedKey' => $aKey, 'expectedValid' => true];
+        if ($isAtLeastPhp82) {
+            yield '=abc @b -b ?c' => ['keys' => [$aKey, $bKey, $cKey], 'index' => 1, 'modifyKeys' => [$bKey], 'expectedKey' => $cKey, 'expectedValid' => true];
+            yield '=abc @c -b ?c' => ['keys' => [$aKey, $bKey, $cKey], 'index' => 2, 'modifyKeys' => [$bKey], 'expectedKey' => $cKey, 'expectedValid' => true];
+        }
+        yield '=abc @a -c ?a' => ['keys' => [$aKey, $bKey, $cKey], 'index' => 0, 'modifyKeys' => [$cKey], 'expectedKey' => $aKey, 'expectedValid' => true];
+        if ($isAtLeastPhp82) {
+            yield '=abc @b -c ?b' => ['keys' => [$aKey, $bKey, $cKey], 'index' => 1, 'modifyKeys' => [$cKey], 'expectedKey' => $bKey, 'expectedValid' => true];
+            yield '=abc @c -c ?ø' => ['keys' => [$aKey, $bKey, $cKey], 'index' => 2, 'modifyKeys' => [$cKey], 'expectedKey' => null, 'expectedValid' => false];
+        }
+        yield '=abc @a -abc ?ø' => ['keys' => [$aKey, $bKey, $cKey], 'index' => 0, 'modifyKeys' => [$aKey, $bKey, $cKey], 'expectedKey' => null, 'expectedValid' => false];
+        yield '=ø @ø -abc ?ø' => ['keys' => [$aKey, $bKey, $cKey], 'index' => 0, 'modifyKeys' => [$aKey, $bKey, $cKey], 'expectedKey' => null, 'expectedValid' => false];
+    }
+
+    #[Test]
+    #[DataProvider('mapUnsetManagesStateDataProvider')]
+    public function mapUnsetManagesState(array $keys, int $index, array $modifyKeys, mixed $expectedKey, bool $expectedValid): void
+    {
+        $map = new Map();
+        foreach ($keys as $key) {
+            $map[$key] = $key->value;
+        }
+        for ($i = 0; $i < $index; $i++) {
+            $map->next();
+        }
+        foreach ($modifyKeys as $modifyKey) {
+            unset($map[$modifyKey]);
+        }
+        $expectedValue = $expectedKey?->value;
+        self::assertSame($expectedValue, $map->current());
+        self::assertSame($expectedKey, $map->key());
+        self::assertSame($expectedValid, $map->valid());
     }
 }
