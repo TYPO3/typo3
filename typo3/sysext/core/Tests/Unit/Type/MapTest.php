8adbf2c93ae47b14c8dac83725909fa1e8abadf3
Oliver Hader <oliver@typo3.org>
2025-08-29 13:04:42 +0200

    [TASK] Allow scalar values in \TYPO3\CMS\Core\Type\Map
    This change allows to use scalar in addition to using only
    object values as keys in \TYPO3\CMS\Core\Type\Map.
    
    > $aObject = new \stdClass();
    > $bArray = ['b' => 'Array'];
    > $cString = 'cString';
    >
    > $map = new \TYPO3\CMS\Core\Type\Map();
    > $map[$aObject] = 'a';
    > $map[$bArray] = 'b';
    > $map[$cString] = 'c';
    
    The internal \SplObjectStorage wrapper was dropped, since
    it did only support object values as keys.
    
    Resolves: #107353
    Releases: main, 13.4, 12.4
    Change-Id: I88b10f772173cfdc17ea4f2814baa65a4fe831f3
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/90981
    Reviewed-by: Oliver Hader <oliver.hader@typo3.org>
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Oliver Hader <oliver.hader@typo3.org>

--- typo3/sysext/core/Tests/Unit/Type/MapTest.php
+++ typo3/sysext/core/Tests/Unit/Type/MapTest.php
@@ -24,12 +24,19 @@ use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
 
 final class MapTest extends UnitTestCase
 {
-    #[Test]
-    public function mapIsArrayAccessible(): void
+    public static function mapKeysDataProvider(): \Generator
     {
-        $aKey = new \stdClass();
+        yield 'string keys' => ['aKey', 'bKey'];
+        yield 'array keys' => [['aArrayKey'], ['bArrayKey']];
+        yield 'object keys' => [new \stdClass(), new \stdClass()];
+        yield 'object & string keys' => [new \stdClass(), 'bKey'];
+    }
+
+    #[Test]
+    #[DataProvider('mapKeysDataProvider')]
+    public function mapIsArrayAccessible(mixed $aKey, mixed $bKey): void
+    {
         $aValue = new \stdClass();
-        $bKey = new \stdClass();
         $bValue = new \stdClass();
 
         $map = new Map();
@@ -43,11 +50,10 @@ final class MapTest extends UnitTestCase
     }
 
     #[Test]
-    public function mapKeyCanBeUnset(): void
+    #[DataProvider('mapKeysDataProvider')]
+    public function mapKeyCanBeUnset(mixed $aKey, mixed $bKey): void
     {
-        $aKey = new \stdClass();
         $aValue = new \stdClass();
-        $bKey = new \stdClass();
         $bValue = new \stdClass();
 
         $map = new Map();
@@ -61,11 +67,10 @@ final class MapTest extends UnitTestCase
     }
 
     #[Test]
-    public function mapCanBeIterated(): void
+    #[DataProvider('mapKeysDataProvider')]
+    public function mapCanBeIterated(mixed $aKey, mixed $bKey): void
     {
-        $aKey = new \stdClass();
         $aValue = new \stdClass();
-        $bKey = new \stdClass();
         $bValue = new \stdClass();
 
         $map = new Map();
@@ -85,11 +90,10 @@ final class MapTest extends UnitTestCase
     }
 
     #[Test]
-    public function mapIsCreatedFromEntries(): void
+    #[DataProvider('mapKeysDataProvider')]
+    public function mapIsCreatedFromEntries(mixed $aKey, mixed $bKey): void
     {
-        $aKey = new \stdClass();
         $aValue = new \stdClass();
-        $bKey = new \stdClass();
         $bValue = new \stdClass();
 
         $map = Map::fromEntries(
@@ -103,11 +107,10 @@ final class MapTest extends UnitTestCase
     }
 
     #[Test]
-    public function mapCanBeAssignedToOtherMap(): void
+    #[DataProvider('mapKeysDataProvider')]
+    public function mapCanBeAssignedToOtherMap(mixed $aKey, mixed $bKey): void
     {
-        $aKey = new \stdClass();
         $aValue = new \stdClass();
-        $bKey = new \stdClass();
         $bValue = new \stdClass();
 
         $map = new Map();
@@ -123,11 +126,10 @@ final class MapTest extends UnitTestCase
     }
 
     #[Test]
-    public function mapKeysAndValuesAreFetched(): void
+    #[DataProvider('mapKeysDataProvider')]
+    public function mapKeysAndValuesAreFetched(mixed $aKey, mixed $bKey): void
     {
-        $aKey = new \stdClass();
         $aValue = new \stdClass();
-        $bKey = new \stdClass();
         $bValue = new \stdClass();
 
         $map = Map::fromEntries(
@@ -137,6 +139,7 @@ final class MapTest extends UnitTestCase
 
         self::assertSame([$aKey, $bKey], $map->keys());
         self::assertSame([$aValue, $bValue], $map->values());
+        self::assertSame([[$aKey, $aValue], [$bKey, $bValue]], $map->entries());
     }
 
     public static function mapNextManagesStateDataProvider(): \Generator
