4130a93db2108b55ac0d05bd25975fcac5d3b6d9
Oliver Hader <oliver@typo3.org>
2025-09-27 10:45:20 +0200

    [TASK] Verify cache headers in CSP request handling test
    Resolves: #107554
    Releases: main, 13.4
    Change-Id: I5f326c5c289e7add07e37de66574f52944260c94
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/90819
    Reviewed-by: Oliver Hader <oliver.hader@typo3.org>
    Tested-by: Garvin Hicking <gh@faktor-e.de>
    Reviewed-by: Garvin Hicking <gh@faktor-e.de>
    Tested-by: Simon Schaufelberger <simonschaufi+typo3@gmail.com>
    Reviewed-by: Simon Schaufelberger <simonschaufi+typo3@gmail.com>
    Tested-by: Oliver Hader <oliver.hader@typo3.org>
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Oliver Klee <typo3-coding@oliverklee.de>

--- typo3/sysext/core/Tests/Functional/SiteHandling/SiteBasedTestTrait.php
+++ typo3/sysext/core/Tests/Functional/SiteHandling/SiteBasedTestTrait.php
@@ -17,6 +17,7 @@ declare(strict_types=1);
 
 namespace TYPO3\CMS\Core\Tests\Functional\SiteHandling;
 
+use Symfony\Component\Yaml\Yaml;
 use TYPO3\CMS\Core\Configuration\SiteConfiguration;
 use TYPO3\CMS\Core\Configuration\SiteWriter;
 use TYPO3\CMS\Core\Tests\Functional\Fixtures\Frontend\PhpError;
@@ -53,6 +54,7 @@ trait SiteBasedTestTrait
         array $languages = [],
         array $errorHandling = [],
         array $dependencies = [],
+        ?array $csp = null,
     ): void {
         $configuration = $site;
         if (!empty($languages)) {
@@ -69,6 +71,13 @@ trait SiteBasedTestTrait
             // ensure no previous site configuration influences the test
             GeneralUtility::rmdir($this->instancePath . '/typo3conf/sites/' . $identifier, true);
             $siteWriter->write($identifier, $configuration);
+            if ($csp !== null) {
+                GeneralUtility::writeFile(
+                    $this->instancePath . '/typo3conf/sites/' . $identifier . '/csp.yaml',
+                    Yaml::dump($csp, 99, 2, Yaml::DUMP_EMPTY_ARRAY_AS_SEQUENCE | Yaml::DUMP_OBJECT_AS_MAP),
+                    true
+                );
+            }
         } catch (\Exception $exception) {
             $this->markTestSkipped($exception->getMessage());
         }
