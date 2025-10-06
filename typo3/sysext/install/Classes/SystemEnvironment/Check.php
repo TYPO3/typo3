819ab54337bc5b97d425e3fd3360adef3910a6d5
Garvin Hicking <garvin@hick.ing>
2025-10-06 22:51:53 +0200

    [TASK] Raise required PHP version info in Reports module to PHP 8.2.0
    Patch #102779 missed one major spot to raise the minimum PHP required
    version.
    
    Resolves: #107617
    Related: #102779
    Releases: main, 13.4
    Change-Id: I617dd861d1a7ba1a9a027af83c81842ef116e250
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/90919
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Simon Schaufelberger <simonschaufi+typo3@gmail.com>
    Reviewed-by: Thomas Hohn <tho@gyldendal.dk>
    Tested-by: Sven Liebert <mail@sven-liebert.de>
    Reviewed-by: Garvin Hicking <garvin@hick.ing>
    Tested-by: Garvin Hicking <garvin@hick.ing>
    Reviewed-by: Simon Schaufelberger <simonschaufi+typo3@gmail.com>
    Reviewed-by: Stefan Bürk <stefan@buerk.tech>
    Tested-by: Thomas Hohn <tho@gyldendal.dk>
    Reviewed-by: Sven Liebert <mail@sven-liebert.de>
    Tested-by: Stefan Bürk <stefan@buerk.tech>

--- typo3/sysext/install/Classes/SystemEnvironment/Check.php
+++ typo3/sysext/install/Classes/SystemEnvironment/Check.php
@@ -258,7 +258,7 @@ class Check implements CheckInterface
      */
     protected function checkPhpVersion()
     {
-        $minimumPhpVersion = '8.1.0';
+        $minimumPhpVersion = '8.2.0';
         $currentPhpVersion = PHP_VERSION;
         if (version_compare($currentPhpVersion, $minimumPhpVersion) < 0) {
             $this->messageQueue->enqueue(new FlashMessage(
