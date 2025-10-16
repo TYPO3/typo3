170d6a85073c6d5dd3c1beb0c826a50e0b813573
Elias Häußler <elias@haeussler.dev>
2025-10-16 14:32:24 +0200

    [TASK] Add visual labels to backend login related form input fields
    In order to make the backend login forms accessible, all input fields
    are now covered by visual labels. With this change, all login related
    forms (login, password forget, password reset, otp) are now visually
    aligned.
    
    Resolves: #107718
    Releases: main, 13.4
    Change-Id: Ic66ed69c553dd3e630fb224332dcc066e9bcf948
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/91102
    Reviewed-by: Benjamin Franzke <ben@bnf.dev>
    Reviewed-by: Benjamin Kott <benjamin.kott@outlook.com>
    Tested-by: Benjamin Franzke <ben@bnf.dev>
    Reviewed-by: Josua Vogel <j.vogel97@web.de>
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Benjamin Kott <benjamin.kott@outlook.com>

--- Build/tests/playwright/helper/login.setup.ts
+++ Build/tests/playwright/helper/login.setup.ts
@@ -3,8 +3,8 @@ import config from '../config';
 
 setup('authenticate as admin', async ({ page }) => {
   await page.goto(`${config.baseUrl}`);
-  await page.getByPlaceholder('Username').fill(config.login.admin.username);
-  await page.getByPlaceholder('Password').fill(config.login.admin.password);
+  await page.getByLabel('Username').fill(config.login.admin.username);
+  await page.getByLabel('Password').fill(config.login.admin.password);
   await page.getByRole('button', { name: 'Login' }).click();
   await page.waitForLoadState('networkidle');
   await expect(page.locator('.t3js-topbar-button-modulemenu')).toBeVisible();
