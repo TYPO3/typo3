9da26234439abca38a376ce174f08b7c9bff516b
Oliver Hader <oliver@typo3.org>
2025-10-19 11:05:53 +0200

    [BUGFIX] Ensure CSP header has nonce in partially cached state
    Issue #107613 revealed that the frontend rendering stack created
    two distinct `ConsumableNonce` instances – one in the global request
    (used for building the Content‑Security‑Policy header) and another
    one inside `PageRenderer::updateState()` (used for counting nonces
    when adding assets).
    
    When assets were rendered via an uncached cObject (e.g. `COA_INT`,
    `USER_INT`) the nonce consumption counter was not incremented. As
    a result the generated CSP header did not contain a nonce source and
    browsers rejected inline scripts/styles.
    
    Resolves: #107719
    Releases: main, 13.4
    Change-Id: I209be1ff35795ff70091aa58c3dd97e12eebd897
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/91160
    Tested-by: Sven Liebert <mail@sven-liebert.de>
    Tested-by: Benni Mack <benni@typo3.org>
    Tested-by: Oliver Hader <oliver.hader@typo3.org>
    Reviewed-by: Oliver Hader <oliver.hader@typo3.org>
    Tested-by: core-ci <typo3@b13.com>
    Reviewed-by: Sven Liebert <mail@sven-liebert.de>
    Reviewed-by: Benni Mack <benni@typo3.org>

--- typo3/sysext/core/Classes/Page/PageRenderer.php
+++ typo3/sysext/core/Classes/Page/PageRenderer.php
@@ -155,9 +155,7 @@ class PageRenderer implements SingletonInterface
                 case 'responseFactory':
                 case 'streamFactory':
                 case 'iconRegistry':
-                    break;
                 case 'nonce':
-                    $this->setNonce(new ConsumableNonce($value));
                     break;
                 case 'metaTagRegistry':
                     $this->metaTagRegistry->updateState($value);
@@ -189,11 +187,7 @@ class PageRenderer implements SingletonInterface
                 case 'responseFactory':
                 case 'streamFactory':
                 case 'iconRegistry':
-                    break;
                 case 'nonce':
-                    if ($value instanceof ConsumableNonce) {
-                        $state[$var] = $value->value;
-                    }
                     break;
                 case 'metaTagRegistry':
                     $state[$var] = $this->metaTagRegistry->getState();
