e1a9d15a3b2f03d5ee273b95c8a59fa187e468f4
Markus Klein <markus.klein@typo3.org>
2025-10-31 15:48:07 +0100

    [TASK] Ensure *.action viewhelpers have the same argument descriptions
    The descriptions and default values of
    the `link.action` and `uri.action` viewhelpers
    have diverged over time.
    
    Bring them in sync.
    
    Resolves: #107955
    Releases: main, 13.4
    Change-Id: I965782164c9c9303b9a7f834b81ccded4b69f078
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/91399
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Simon Praetorius <simon@praetorius.me>
    Reviewed-by: Simon Praetorius <simon@praetorius.me>

--- typo3/sysext/fluid/Classes/ViewHelpers/Link/ActionViewHelper.php
+++ typo3/sysext/fluid/Classes/ViewHelpers/Link/ActionViewHelper.php
@@ -50,21 +50,21 @@ final class ActionViewHelper extends AbstractTagBasedViewHelper
     {
         parent::initializeArguments();
         $this->registerArgument('action', 'string', 'Target action');
+        $this->registerArgument('arguments', 'array', 'Arguments for the controller action, associative array (do not use reserved keywords "action", "controller" or "format" if not referring to these internal variables specifically)', false, []);
         $this->registerArgument('controller', 'string', 'Target controller. If NULL current controllerName is used');
         $this->registerArgument('extensionName', 'string', 'Target Extension Name (without `tx_` prefix and no underscores). If NULL the current extension name is used');
         $this->registerArgument('pluginName', 'string', 'Target plugin. If empty, the current plugin name is used');
         $this->registerArgument('pageUid', 'int', 'Target page. See TypoLink destination');
-        $this->registerArgument('pageType', 'int', 'Type of the target page. See typolink.parameter');
+        $this->registerArgument('pageType', 'int', 'Type of the target page. See typolink.parameter', false, 0);
         $this->registerArgument('noCache', 'bool', 'Set this to disable caching for the target page. You should not need this.');
         $this->registerArgument('language', 'string', 'link to a specific language - defaults to the current language, use a language ID or "current" to enforce a specific language');
-        $this->registerArgument('section', 'string', 'The anchor to be added to the URI');
-        $this->registerArgument('format', 'string', 'The requested format, e.g. ".html');
-        $this->registerArgument('linkAccessRestrictedPages', 'bool', 'If set, links pointing to access restricted pages will still link to the page even though the page cannot be accessed.');
-        $this->registerArgument('additionalParams', 'array', 'Additional query parameters that won\'t be prefixed like $arguments (overrule $arguments)');
-        $this->registerArgument('absolute', 'bool', 'If set, the URI of the rendered link is absolute');
+        $this->registerArgument('section', 'string', 'The anchor to be added to the URI', false, '');
+        $this->registerArgument('format', 'string', 'The requested format, e.g. ".html', false, '');
+        $this->registerArgument('linkAccessRestrictedPages', 'bool', 'If set, links pointing to access restricted pages will still link to the page even though the page cannot be accessed.', false, false);
+        $this->registerArgument('additionalParams', 'array', 'Additional query parameters that won\'t be prefixed like $arguments (overrule $arguments)', false, []);
+        $this->registerArgument('absolute', 'bool', 'If set, the URI of the rendered link is absolute', false, false);
         $this->registerArgument('addQueryString', 'string', 'If set, the current query parameters will be kept in the URL. If set to "untrusted", then ALL query parameters will be added. Be aware, that this might lead to problems when the generated link is cached.', false, false);
-        $this->registerArgument('argumentsToBeExcludedFromQueryString', 'array', 'Arguments to be removed from the URI. Only active if $addQueryString = true');
-        $this->registerArgument('arguments', 'array', 'Arguments for the controller action, associative array (do not use reserved keywords "action", "controller" or "format" if not referring to these internal variables specifically)');
+        $this->registerArgument('argumentsToBeExcludedFromQueryString', 'array', 'Arguments to be removed from the URI. Only active if $addQueryString = true', false, []);
     }
 
     public function render(): string
