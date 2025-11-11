d9b6be751069a341589951a7471ac20e1fb84c78
Garvin Hicking <garvin@hick.ing>
2025-11-11 18:57:39 +0100

    [TASK] Add "debug:backend:modules", "debug:backend:routes" commands
    These CLI commands help developers to see:
    
    - the list of registered backend modules and their hierarchy, along
      their extension origin and defined (translated) labels
    
    - the list of backend routes and their targets
    
    Two backend module list metadata items are not exposed through the
    ModuleInterface API (and normally not relevant), so they are fetched
    on a low-level basis.
    
    With these command, developers are able to:
    
    - debug the hierarchy and make choices about possible naming
      conflicts, ill-fated identifiers and proper ordering
    
    - investigate AJAX routes, possible cross-concerns and scopes
    
    Since this is regarded internal usage, it is not exposed
    as a feature and uses a "debug:" CLI scope.
    
    Resolves: #108057
    Resolves: #107830
    Related: #107790
    Related: #108047
    Related: #108045
    Related: #108048
    Releases: main
    Change-Id: If1cabc035fa3c167d8c7a6ee571609ad5ccd1b69
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/91566
    Tested-by: Sven Liebert <mail@sven-liebert.de>
    Tested-by: Georg Ringer <georg.ringer@gmail.com>
    Tested-by: core-ci <typo3@b13.com>
    Reviewed-by: Georg Ringer <georg.ringer@gmail.com>
    Reviewed-by: Sven Liebert <mail@sven-liebert.de>

---
+++ typo3/sysext/backend/Classes/Command/DebugBackendModulesCommand.php
@@ -0,0 +1,210 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3\CMS\Backend\Command;
+
+use Psr\Container\ContainerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Helper\TableSeparator;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use TYPO3\CMS\Backend\Module\ModuleFactory;
+use TYPO3\CMS\Backend\Module\ModuleInterface;
+use TYPO3\CMS\Backend\Module\ModuleRegistry;
+use TYPO3\CMS\Core\Core\BootService;
+use TYPO3\CMS\Core\Localization\LanguageService;
+use TYPO3\CMS\Core\Localization\LanguageServiceFactory;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+/**
+ * Command for showing all backend modules and their associated labels
+ * @internal only for development purposes
+ */
+class DebugBackendModulesCommand extends Command
+{
+    private LanguageService $languageService;
+
+    public function __construct(
+        private readonly ContainerInterface $failsafeContainer,
+        private readonly BootService $bootService,
+        private readonly ModuleFactory $moduleFactory,
+        private readonly LanguageServiceFactory $languageServiceFactory,
+    ) {
+        $this->languageService = $GLOBALS['LANG'] = $this->languageServiceFactory->create('default');
+        // Note: We cannot directly use autowire of 'backend.modules' because that
+        // would only give us the final constructed registry, without access to data
+        // like "packageName" and "labels".
+        // @todo We should expose this data in our registry.
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addOption(
+                'csv-export',
+                'x',
+                InputOption::VALUE_NONE,
+                'Dump data as CSV (instead of CLI table)'
+            )
+            ->addOption(
+                'core-only',
+                'c',
+                InputOption::VALUE_NONE,
+                'Only show core extensions'
+            );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $degraded = false;
+        try {
+            $container = $this->bootService->getContainer();
+        } catch (\Throwable $e) {
+            $container = $this->failsafeContainer;
+            $degraded = true;
+        }
+
+        $coreOnly = $input->getOption('core-only');
+
+        $title = 'Backend Modules';
+        if ($coreOnly) {
+            $title .= ' - Core';
+        }
+        if ($degraded) {
+            $title .= ' (failsafe)';
+        }
+
+        // We need this low-level access because 'packageName' and 'labels' cannot be retrieved by the Module Registry API (yet)
+        $modulesFromPackages = $container->get('backend.modules')->getArrayCopy();
+        $modulesFromPackages = $this->moduleFactory->adaptAliasMappingFromModuleConfiguration($modulesFromPackages);
+
+        $initializedModulesFromPackages = [];
+        foreach ($modulesFromPackages as $identifier => $configuration) {
+            if (!$coreOnly || str_starts_with($configuration['packageName'], 'typo3/cms-')) {
+                $initializedModulesFromPackages[$identifier] = $this->moduleFactory->createModule($identifier, $configuration);
+            }
+        }
+
+        $registry = GeneralUtility::makeInstance(ModuleRegistry::class, $initializedModulesFromPackages);
+        $modules = $registry->getModules();
+
+        $linearTree = [];
+        $headers = [
+            'Pkg',
+            'Main level',
+            'Second level',
+            'Third level',
+            'Position',
+            'Labels',
+            'Path',
+        ];
+        $this->walkTree($modules, $linearTree, $modulesFromPackages);
+
+        if ($input->getOption('csv-export')) {
+            $out = fopen('php://output', 'w');
+            $separator = ';';
+            $enclosure = '"';
+            $escape = '\\';
+            $eol = PHP_EOL;
+
+            fputcsv($out, $headers, $separator, $enclosure, $escape, $eol);
+            foreach ($linearTree as $data) {
+                if ($data instanceof TableSeparator) {
+                    $blankOutput = [];
+                    foreach ($headers as $ignored) {
+                        $blankOutput[] = '';
+                    }
+                    fputcsv($out, $blankOutput, $separator, $enclosure, $escape, $eol);
+                } else {
+                    fputcsv($out, $data, $separator, $enclosure, $escape, $eol);
+                }
+            }
+            fclose($out);
+        } else {
+            $io->title($title);
+            $table = new Table($output);
+            $table->setHeaders($headers);
+
+            foreach ($linearTree as $data) {
+                $table->addRow($data);
+            }
+            $table->render();
+        }
+
+        return Command::SUCCESS;
+    }
+
+    /**
+     * @param ModuleInterface[] $modules
+     */
+    private function walkTree(array $modules, array &$linearTree, array $modulesFromPackages, int $level = 1, array $parentStack = []): void
+    {
+        foreach ($modules as $module) {
+            // Main menus have no "parent". We only iterate these elements on the first level.
+            if ($level === 1 && $module->getParentIdentifier() !== '') {
+                continue;
+            }
+
+            $outputStack = $parentStack;
+            $outputStack[] = $module->getIdentifier();
+
+            $linearTree[] = [
+                $modulesFromPackages[$module->getIdentifier()]['packageName'],
+
+                $outputStack[0] ?? '',
+                $outputStack[1] ?? '',
+                $outputStack[2] ?? '',
+
+                ($module->getPosition() !== [] ? json_encode($module->getPosition()) : ''),
+                $this->languageService->sL($module->getTitle()) . ' [' . $this->parseLabels($modulesFromPackages[$module->getIdentifier()]['labels']) . ']',
+                $module->getPath(),
+            ];
+
+            // Next level
+            if ($module->hasSubModules()) {
+                $this->walkTree($module->getSubModules(), $linearTree, $modulesFromPackages, $level + 1, [...$parentStack, $module->getIdentifier()]);
+            }
+
+            if ($level === 1) {
+                $linearTree[] = new TableSeparator();
+            }
+        }
+
+        if ($level === 1) {
+            // Remove last separator
+            array_pop($linearTree);
+        }
+    }
+
+    private function parseLabels(array|string $labels): string
+    {
+        if (is_string($labels)) {
+            return $labels;
+        }
+
+        $out = "\n";
+        $out .= '  title: ' . ($labels['title'] ?? '-') . "\n";
+        $out .= '  shortDescription: ' . ($labels['shortDescription'] ?? '-') . "\n";
+        $out .= '  description: ' . ($labels['description'] ?? '-') . "\n";
+        return $out;
+    }
+}
---
+++ typo3/sysext/backend/Classes/Command/DebugBackendRoutesCommand.php
@@ -0,0 +1,236 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3\CMS\Backend\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use TYPO3\CMS\Backend\Module\ModuleRegistry;
+use TYPO3\CMS\Backend\Routing\Router;
+
+/**
+ * Debug backend routes, including module routes, AJAX routes and regular routes.
+ * Similar to Symfony's debug:router command.
+ * @internal only for development purposes
+ */
+class DebugBackendRoutesCommand extends Command
+{
+    public function __construct(
+        private readonly Router $router,
+        private readonly ModuleRegistry $moduleRegistry,
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addOption(
+                'json',
+                null,
+                InputOption::VALUE_NONE,
+                'Output routes in JSON format'
+            )
+            ->addOption(
+                'filter',
+                'f',
+                InputOption::VALUE_REQUIRED,
+                'Filter routes by name (supports partial matching)'
+            )
+            ->addOption(
+                'limit',
+                'l',
+                InputOption::VALUE_REQUIRED,
+                'Limit routes by type: ajax, module, or route'
+            );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $jsonOutput = $input->getOption('json');
+        $filter = $input->getOption('filter');
+        $limit = $input->getOption('limit');
+
+        // Validate limit option
+        if ($limit !== null) {
+            $limit = strtolower($limit);
+            if (!in_array($limit, ['ajax', 'module', 'route'], true)) {
+                $io->error('Invalid limit type. Valid options are: ajax, module, route');
+                return Command::FAILURE;
+            }
+        }
+
+        // Collect all routes
+        $routes = $this->collectAllRoutes($filter, $limit);
+
+        if (empty($routes)) {
+            $messages = [];
+            if ($filter) {
+                $messages[] = 'filter: ' . $filter;
+            }
+            if ($limit) {
+                $messages[] = 'type: ' . $limit;
+            }
+            if ($messages) {
+                $io->warning('No routes found matching ' . implode(', ', $messages));
+            } else {
+                $io->warning('No routes found.');
+            }
+            return Command::SUCCESS;
+        }
+
+        // Sort routes by name
+        ksort($routes);
+
+        if ($jsonOutput) {
+            $this->outputJson($routes, $output);
+        } else {
+            $this->outputTable($routes, $io, $output);
+        }
+
+        return Command::SUCCESS;
+    }
+
+    /**
+     * Collect all routes from Router (including AJAX routes) and Module routes
+     *
+     * @return array<string, array{name: string, method: string, path: string, target: string, type: string, options: array}>
+     */
+    private function collectAllRoutes(?string $filter, ?string $limitType): array
+    {
+        $routes = [];
+
+        // Build a set of module identifiers for quick lookup
+        $moduleIdentifiers = [];
+        foreach ($this->moduleRegistry->getModules() as $module) {
+            if ($module->hasParentModule() || $module->isStandalone()) {
+                $moduleIdentifiers[$module->getIdentifier()] = true;
+            }
+        }
+
+        // Get all routes from Router (includes regular routes, AJAX routes, and module routes)
+        // Note: Routes can be either TYPO3 Route or Symfony Route objects
+        foreach ($this->router->getRoutes() as $routeName => $route) {
+            if ($filter && !str_contains((string)$routeName, $filter)) {
+                continue;
+            }
+
+            // Determine route type
+            $type = 'Route';
+            if (str_starts_with((string)$routeName, 'ajax_')) {
+                $type = 'Ajax';
+            } elseif (isset($moduleIdentifiers[(string)$routeName])) {
+                $type = 'Module';
+            }
+
+            // Apply limit filter if specified
+            if ($limitType !== null) {
+                $typeNormalized = strtolower($type);
+                if ($typeNormalized !== $limitType) {
+                    continue;
+                }
+            }
+
+            // Get methods - works for both Symfony and TYPO3 Route objects
+            $methods = $route->getMethods();
+            $methodString = empty($methods) ? 'ANY' : implode('|', $methods);
+
+            // Get options - works for both route types
+            // phpDoc inference is too narrow as Symfony routes are possible. Ignored in the scope of this debug output:
+            // @phpstan-ignore function.alreadyNarrowedType
+            $options = method_exists($route, 'getOptions') ? $route->getOptions() : [];
+            $target = $options['target'] ?? $options['_controller'] ?? '-';
+
+            $routes[$routeName] = [
+                'name' => (string)$routeName,
+                'method' => $methodString,
+                'path' => $route->getPath(),
+                'target' => $target,
+                'type' => $type,
+                'options' => $options,
+            ];
+        }
+
+        return $routes;
+    }
+
+    /**
+     * Output routes as JSON
+     */
+    private function outputJson(array $routes, OutputInterface $output): void
+    {
+        $jsonData = [];
+        foreach ($routes as $route) {
+            $jsonData[] = [
+                'name' => $route['name'],
+                'method' => $route['method'],
+                'path' => $route['path'],
+                'target' => $route['target'],
+                'type' => $route['type'],
+                'options' => $route['options'],
+            ];
+        }
+
+        $output->writeln((string)json_encode($jsonData, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
+    }
+
+    /**
+     * Output routes as formatted table (similar to Symfony's debug:router)
+     */
+    private function outputTable(array $routes, SymfonyStyle $io, OutputInterface $output): void
+    {
+        $table = new Table($output);
+        $table->setHeaders(['Name', 'Method', 'Path', 'Target', 'Type']);
+        $rows = [];
+        foreach ($routes as $route) {
+            $rows[] = [
+                $route['name'],
+                $route['method'],
+                $route['path'],
+                $this->formatTarget($route['target']),
+                $route['type'],
+            ];
+        }
+
+        $table->setRows($rows);
+        $table->render();
+
+        $io->newLine();
+        $io->writeln(sprintf('<info>%d</info> routes found', count($routes)));
+    }
+
+    /**
+     * Format the target for display (shorten class names)
+     */
+    private function formatTarget(string $target): string
+    {
+        // Shorten TYPO3 class names for better readability
+        $target = str_replace('TYPO3\\CMS\\', '', $target);
+
+        // Limit length if too long
+        if (strlen($target) > 80) {
+            return substr($target, 0, 77) . '...';
+        }
+
+        return $target;
+    }
+}
