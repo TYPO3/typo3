00f64970ac0b032753bef04ec30b4dd48ce532bb
Lina Wolf <112@linawolf.de>
2025-11-22 13:08:45 +0100

    [BUGFIX] Remove accidentally added enable field descriptions
    In issue #107947, field descriptions were accidentally added
    to the fe_users table even though these fields are not defined.
    
    In that change, we also decided not to introduce such descriptions,
    but the entries in this table were mistakenly left in place.
    This patch removes them.
    
    Resolves: #108255
    Related: #107947
    Releases: main
    Change-Id: Icb68feb267702a115d0952b7f0d0d60b3f39cf38
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/91814
    Reviewed-by: Josef Glatz <typo3@josefglatz.at>
    Reviewed-by: Jasmina Ließmann <typo3@minapok.eu>
    Tested-by: core-ci <typo3@b13.com>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Jasmina Ließmann <typo3@minapok.eu>

--- typo3/sysext/frontend/Configuration/TCA/Overrides/fe_groups.php
+++ typo3/sysext/frontend/Configuration/TCA/Overrides/fe_groups.php
@@ -3,4 +3,3 @@
 defined('TYPO3') or die();
 
 $GLOBALS['TCA']['fe_groups']['columns']['hidden']['label'] = 'core.db.accounts:group.enabled';
-$GLOBALS['TCA']['fe_groups']['columns']['hidden']['description'] = 'core.db.accounts:group.enabled.description';
