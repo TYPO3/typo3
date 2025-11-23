f62fffa9c90bdc32b6a969bafadcaddc9e737964
Riccardo De Contardi <erredeco@gmail.com>
2025-11-23 16:14:34 +0100

    [TASK] Remove exclamation marks from texts on EXT:backend (MFA) - leftover
    Removes filling words from the string auth.locked;
    
    (leftover of https://review.typo3.org/c/Packages/TYPO3.CMS/+/91753)
    
    Resolves: #108279
    Releases: main
    Change-Id: I66bb974f97856fff707831da191cac784e956d80
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/91838
    Tested-by: core-ci <typo3@b13.com>
    Reviewed-by: Simon Schaufelberger <simonschaufi+typo3@gmail.com>
    Tested-by: Josua Vogel <j.vogel97@web.de>
    Tested-by: Simon Schaufelberger <simonschaufi+typo3@gmail.com>
    Tested-by: Benni Mack <benni@typo3.org>
    Reviewed-by: Benni Mack <benni@typo3.org>
    Reviewed-by: Josua Vogel <j.vogel97@web.de>

--- typo3/sysext/backend/Tests/Functional/Controller/MfaControllerTest.php
+++ typo3/sysext/backend/Tests/Functional/Controller/MfaControllerTest.php
@@ -163,7 +163,7 @@ final class MfaControllerTest extends FunctionalTestCase
         $response = $this->subject->handleRequest($request);
 
         self::assertEquals(200, $response->getStatusCode());
-        self::assertStringContainsString('This provider is temporarily locked!', $response->getBody()->__toString());
+        self::assertStringContainsString('This provider is temporarily locked.', $response->getBody()->__toString());
     }
 
     #[Test]
