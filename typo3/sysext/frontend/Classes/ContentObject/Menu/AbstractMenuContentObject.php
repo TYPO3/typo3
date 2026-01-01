1928ca115580e4f45ea9f4ed084056f1095272e1
Himanshu Ramavat <himanshu.nitsan@gmail.com>
2026-01-01 16:56:15 +0530

    [BUGFIX] Fix undefined array key warning in language menu processing
    When processing language menu items with manually specified language IDs
    that don't exist in the site configuration, the code attempted to access
    `$languages[$sUid]` without checking if the key exists, causing a PHP
    warning.
    
    This fix adds an `isset()` check before accessing the language array to
    gracefully skip invalid language IDs instead of throwing a warning.
    
    Resolves: #108587
    Releases: main, 13.4
    Change-Id: Ib9311543faca55813a2c5d3b50c9b19d848a79b6
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/92450
    Tested-by: Himanshu Nitsan <himanshu.nitsan@gmail.com>
    Reviewed-by: Georg Ringer <georg.ringer@gmail.com>
    Reviewed-by: Garvin Hicking <garvin@hick.ing>
    Tested-by: Georg Ringer <georg.ringer@gmail.com>
    Reviewed-by: Himanshu Nitsan <himanshu.nitsan@gmail.com>
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Garvin Hicking <garvin@hick.ing>

--- typo3/sysext/frontend/Classes/ContentObject/Menu/AbstractMenuContentObject.php
+++ typo3/sysext/frontend/Classes/ContentObject/Menu/AbstractMenuContentObject.php
@@ -577,6 +577,10 @@ abstract class AbstractMenuContentObject
         foreach ($languageItems as $sUid) {
             // Find overlay record:
             if ($sUid) {
+                // Skip if language doesn't exist in site configuration
+                if (!isset($languages[$sUid])) {
+                    continue;
+                }
                 $languageAspect = LanguageAspectFactory::createFromSiteLanguage($languages[$sUid]);
                 $pageRepository = $this->buildPageRepository($languageAspect);
                 $lRecs = $pageRepository->getPageOverlay($currentPageWithNoOverlay, $languageAspect);
