87916314feee670fab299d37225eda7905c9b125
Benjamin Franzke <ben@bnf.dev>
2026-01-22 08:48:06 +0100

    [BUGFIX] Avoid live-search shortcut from opening in all opened tabs
    Avoid broadcasting the live-search trigger to all
    TYPO3 backend instances of the current browser (=all tabs)
    and instead only inform the current top-frame to open
    the live-search modal in the current tab.
    
    Releases: main, 13.4
    Resolves: #108724
    Change-Id: I90f23a3f1f2adfa4e3c306717821f335db409dd7
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/92451
    Tested-by: Garvin Hicking <garvin@hick.ing>
    Reviewed-by: Garvin Hicking <garvin@hick.ing>
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Benjamin Kott <benjamin.kott@outlook.com>
    Tested-by: Benjamin Franzke <ben@bnf.dev>
    Reviewed-by: Benjamin Kott <benjamin.kott@outlook.com>
    Reviewed-by: Benjamin Franzke <ben@bnf.dev>

--- Build/Sources/TypeScript/backend/live-search/live-search-shortcut.ts
+++ Build/Sources/TypeScript/backend/live-search/live-search-shortcut.ts
@@ -1,5 +1,3 @@
-import { BroadcastMessage } from '@typo3/backend/broadcast-message';
-import BroadcastService from '@typo3/backend/broadcast-service';
 import Modal from '../modal';
 import Hotkeys from '@typo3/backend/hotkeys';
 import DocumentService from '@typo3/core/document-service';
@@ -14,13 +12,7 @@ class LiveSearchShortcut {
         }
 
         e.preventDefault();
-
-        document.dispatchEvent(new CustomEvent('typo3:live-search:trigger-open'));
-        BroadcastService.post(new BroadcastMessage(
-          'live-search',
-          'trigger-open',
-          {}
-        ));
+        top.document.dispatchEvent(new CustomEvent('typo3:live-search:trigger-open'));
       }, { allowOnEditables: true /* @todo: bindElement cannot be used at the moment as the suitable element exists twice! */ });
     });
   }
