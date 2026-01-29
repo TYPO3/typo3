c878f1efee231771bc8e54704b161ebdb3673070
Sascha Nowak <sascha.nowak@netlogix.de>
2026-01-29 21:07:53 +0100

    [BUGFIX] Enhance message handler registration to support multiple types
    The following use cases are covered:
    * Single handler with single message with class attributes
    * Single handler with multiple messages with class attributes
    * Single handler with single message with method attributes
    * Single handler with multiple messages with method attributes
    * Multiple handlers for the same message
    * Multiple handlers in a single class with class attributes
    * Multiple handlers in a single class with method attribute
    
    Tests are now added for a message handler bus test.
    
    Also, casual changes to harmonize the ListenerProviderPass
    in types and annotations is performed.
    
    Resolves: #108806
    Releases: main, 13.4
    Change-Id: I4d882e75874afd2bcf5f66826ba64635827c5e9d
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/92644
    Tested-by: Garvin Hicking <garvin@hick.ing>
    Reviewed-by: Garvin Hicking <garvin@hick.ing>
    Tested-by: core-ci <typo3@b13.com>

---
+++ typo3/sysext/core/Tests/Functional/Fixtures/Extensions/test_message_handler/Classes/Message/PrincessesMessageHandler.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3Tests\TestMessageHandler\Message;
+
+use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+
+#[AsMessageHandler]
+final readonly class PrincessesMessageHandler
+{
+    public function __invoke(Princesses $message): void
+    {
+        $message->names[] = 'Peach';
+    }
+}
