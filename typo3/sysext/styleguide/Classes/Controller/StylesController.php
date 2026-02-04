880749eede535ef3e16abdf76d68655be714d829
Benjamin Kott <benjamin.kott@outlook.com>
2026-02-04 17:02:00 +0100

    [TASK] Introduce decorative state color tokens adding lime and indigo
    Adds a complete set of decorative color state tokens for non-semantic
    color usage such as workspace indicators, category tags, or user-assigned
    labels. The palette includes: red, orange, yellow, lime, green, teal,
    blue, indigo, purple, and magenta - sorted by hue.
    
    Resolves: #108860
    Releases: main
    Change-Id: I8a2227b53f4c5749827133c8736e99127bbd61bd
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/92666
    Tested-by: Andreas Kienast <akienast@scripting-base.de>
    Reviewed-by: Andreas Kienast <akienast@scripting-base.de>
    Tested-by: core-ci <typo3@b13.com>
    Reviewed-by: Benjamin Kott <benjamin.kott@outlook.com>
    Reviewed-by: Benjamin Franzke <ben@bnf.dev>
    Tested-by: Benjamin Franzke <ben@bnf.dev>
    Tested-by: Benjamin Kott <benjamin.kott@outlook.com>

--- typo3/sysext/styleguide/Classes/Controller/StylesController.php
+++ typo3/sysext/styleguide/Classes/Controller/StylesController.php
@@ -94,7 +94,7 @@ final class StylesController
             'neutralSteps' => [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100],
             'stateColors' => ['primary', 'secondary', 'info', 'success', 'warning', 'danger', 'notice'],
             'stateSteps' => [1, 2, 3, 4, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 96, 97, 98, 99],
-            'baseColors' => ['blue', 'purple', 'teal', 'green', 'magenta', 'yellow', 'orange', 'red'],
+            'baseColors' => ['red', 'orange', 'yellow', 'lime', 'green', 'teal', 'blue', 'indigo', 'purple', 'magenta'],
             'baseSteps' => [10, 20, 30, 40, 50, 60, 70, 80, 90],
         ]);
         return $view->renderResponse('Backend/Styles/ColorTokens');
@@ -109,6 +109,7 @@ final class StylesController
             'routeIdentifier' => 'styleguide_styles',
             'accentColors' => ['primary', 'secondary'],
             'stateColors' => ['default', 'info', 'success', 'warning', 'danger', 'notice'],
+            'baseStateColors' => ['red', 'orange', 'yellow', 'lime', 'green', 'teal', 'blue', 'indigo', 'purple', 'magenta'],
             'stateVariants' => ['default', 'hover', 'focus', 'disabled'],
             'stateProperties' => ['color', 'bg', 'border-color'],
         ]);
