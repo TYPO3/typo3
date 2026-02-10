a4087b8fc1365a2caba55e4b2aa0d3f8ecf153a7
Oliver Hader <oliver@typo3.org>
2026-02-10 11:05:05 +0100

    [RELEASE] Release of TYPO3 13.4.25
    Change-Id: I4782b9b09161a50505a3b8dec0656e5cd429366f
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/92773
    Reviewed-by: Oliver Hader <oliver.hader@typo3.org>
    Tested-by: Oliver Hader <oliver.hader@typo3.org>

--- typo3/sysext/core/Tests/Functional/Fixtures/Extensions/test_message_handler/ext_emconf.php
+++ typo3/sysext/core/Tests/Functional/Fixtures/Extensions/test_message_handler/ext_emconf.php
@@ -6,14 +6,14 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'This extension contains Symfony Command DI fixtures.',
     'description' => 'This extension contains Symfony Command DI fixtures.',
     'category' => 'example',
-    'version' => '14.2.0',
+    'version' => '13.4.25',
     'state' => 'beta',
     'author' => 'Garvin Hicking',
     'author_email' => 'garvin@hick.ing',
     'author_company' => '',
     'constraints' => [
         'depends' => [
-            'typo3' => '14.2.0',
+            'typo3' => '13.4.25',
         ],
         'conflicts' => [],
         'suggests' => [],
