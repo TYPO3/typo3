ec8679d7f2427bcc3c3895e22650c14b8ccd9758
Oliver Hader <oliver.hader@typo3.org>
2026-02-20 10:23:52 +0100

    [RELEASE] Release of TYPO3 14.1.1
    Change-Id: I3ac2b051c414afdbf7b72b3aad810c42347b157e
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/92917
    Reviewed-by: Oliver Hader <oliver.hader@typo3.org>
    Tested-by: Oliver Hader <oliver.hader@typo3.org>

--- typo3/sysext/core/Classes/Information/Typo3Version.php
+++ typo3/sysext/core/Classes/Information/Typo3Version.php
@@ -19,7 +19,7 @@ namespace TYPO3\CMS\Core\Information;
 
 class Typo3Version
 {
-    protected const VERSION = '14.1.1-dev';
+    protected const VERSION = '14.1.1';
     protected const BRANCH = '14.1';
 
     public function getVersion(): string
