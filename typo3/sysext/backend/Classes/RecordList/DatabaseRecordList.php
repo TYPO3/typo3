553595571474d77874b09782e77c32d54214965e
Ludovic Nogueira <j0r9xy@tx940101.open.etat-de-vaud.ch>
2026-03-04 09:22:37 +0100

    [BUGFIX] Add table name before fieldName for makeSearchString
    Resolves: #107796
    Releases: main, 14.3
    Change-Id: I27ac9b0de1f12654ab2a5078782c209d7a2fc7b1
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/93910
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>

--- typo3/sysext/backend/Classes/RecordList/DatabaseRecordList.php
+++ typo3/sysext/backend/Classes/RecordList/DatabaseRecordList.php
@@ -2733,17 +2733,17 @@ class DatabaseRecordList
         [$subSchemaDivisorFieldName, $fieldsSubSchemaTypes] = $this->searchableSchemaFieldsCollector->getSchemaFieldSubSchemaTypes($table);
         // Get fields from ctrl section of TCA first
         if (MathUtility::canBeInterpretedAsInteger($this->searchString)) {
-            $constraints[] = $expressionBuilder->eq('uid', (int)$this->searchString);
+            $constraints[] = $expressionBuilder->eq($table . '.uid', (int)$this->searchString);
             foreach ($searchableFields as $field) {
                 $searchConstraint = null;
                 if ($field instanceof NumberFieldType || $field instanceof DateTimeFieldType) {
                     $searchConstraint = $expressionBuilder->and(
-                        $expressionBuilder->eq($field->getName(), (int)$this->searchString),
-                        $expressionBuilder->eq($tablePidField, $currentPid)
+                        $expressionBuilder->eq($table . '.' . $field->getName(), (int)$this->searchString),
+                        $expressionBuilder->eq($table . '.' . $tablePidField, $currentPid)
                     );
                 } else {
                     $searchConstraint = $expressionBuilder->like(
-                        $field->getName(),
+                        $table . '.' . $field->getName(),
                         $queryBuilder->quote('%' . $this->searchString . '%')
                     );
                 }
@@ -2760,7 +2760,7 @@ class DatabaseRecordList
                     $searchConstraint = $queryBuilder->expr()->and(
                         $searchConstraint,
                         $queryBuilder->expr()->in(
-                            $subSchemaDivisorFieldName,
+                            $table . '.' . $subSchemaDivisorFieldName,
                             $queryBuilder->quoteArrayBasedValueListToStringList($fieldsSubSchemaTypes[$field->getName()])
                         ),
                     );
@@ -2772,7 +2772,7 @@ class DatabaseRecordList
             $like = $queryBuilder->quote('%' . $queryBuilder->escapeLikeWildcards($this->searchString) . '%');
             foreach ($searchableFields as $field) {
                 $searchConstraint = $expressionBuilder->comparison(
-                    'LOWER(' . $queryBuilder->castFieldToTextType($field->getName()) . ')',
+                    'LOWER(' . $queryBuilder->castFieldToTextType($table . '.' . $field->getName()) . ')',
                     'LIKE',
                     'LOWER(' . $like . ')'
                 );
@@ -2788,7 +2788,7 @@ class DatabaseRecordList
                     $searchConstraint = $queryBuilder->expr()->and(
                         $searchConstraint,
                         $queryBuilder->expr()->in(
-                            $subSchemaDivisorFieldName,
+                            $table . '.' . $subSchemaDivisorFieldName,
                             $queryBuilder->quoteArrayBasedValueListToStringList($fieldsSubSchemaTypes[$field->getName()])
                         ),
                     );
