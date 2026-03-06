808083b02963a0c3cc6d08b1feed6acc245a7709
Benjamin Franzke <ben@bnf.dev>
2026-03-06 10:37:02 +0100

    [TASK] Upgrade to CKEditor 5 v47.6
    CKEditor 5 v47.6 [1] fixes a theoretical XSS issue [2] in the
    General HTML Support (GHS) feature, which could be explouted
    in case CSP is disabled (not allowed in our backend) and if
    CKEditor configuration uses an insecure configuration
    (allows inline JavaScript attributes like `onclick` [3]).
    
    Used commands:
    
      cd Build
      npm --prefix overrides/ckeditor5 version 47.6.0
      npm install --save ./overrides/ckeditor5 $(
        grep @ckeditor/ package.json | \
        grep -v @ckeditor/ckeditor5-dev-utils | \
        grep -v @ckeditor/ckeditor5-inspector | \
        sed 's/^[^"]*"\([^"]*\)".*/\1@47.6.0/' \
      )
      npm run build
    
    [1] https://github.com/ckeditor/ckeditor5/releases/tag/v47.6.0
    [2] https://github.com/ckeditor/ckeditor5/security/advisories/GHSA-jrqm-vmqc-gm93
    [3] https://ckeditor.com/docs/ckeditor5/latest/features/html/general-html-support.html#security
    
    Resolves: #109176
    Releases: main, 13.4
    Change-Id: I6aae8b2583cb475054288cf7fc5f5b0786c6a4e4
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/93100
    Tested-by: Georg Ringer <georg.ringer@gmail.com>
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Benjamin Franzke <ben@bnf.dev>
    Reviewed-by: Georg Ringer <georg.ringer@gmail.com>
    Reviewed-by: Benjamin Franzke <ben@bnf.dev>
    Tested-by: Oliver Hader <oliver.hader@typo3.org>
    Reviewed-by: Oliver Hader <oliver.hader@typo3.org>

--- typo3/sysext/rte_ckeditor/Resources/Public/Contrib/@ckeditor/ckeditor5-select-all.js
+++ typo3/sysext/rte_ckeditor/Resources/Public/Contrib/@ckeditor/ckeditor5-select-all.js
@@ -1,13 +1,13 @@
 import{Command as m,Plugin as o}from"@ckeditor/ckeditor5-core";import{getCode as g,parseKeystroke as f}from"@ckeditor/ckeditor5-utils";import{IconSelectAll as p}from"@ckeditor/ckeditor5-icons";import{ButtonView as A,MenuBarMenuListItemButtonView as h}from"@ckeditor/ckeditor5-ui";/**
-* @license Copyright (c) 2003-2025, CKSource Holding sp. z o.o. All rights reserved.
+* @license Copyright (c) 2003-2026, CKSource Holding sp. z o.o. All rights reserved.
 * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-licensing-options
 */class r extends m{constructor(e){super(e),this.affectsData=!1}execute(){const e=this.editor.model,t=e.document.selection;let i=e.schema.getLimitElement(t);if(t.containsEntireContent(i)||!a(e.schema,i))do if(i=i.parent,!i)return;while(!a(e.schema,i));e.change(n=>{n.setSelection(i,"in")})}}function a(l,e){return l.isLimit(e)&&(l.checkChild(e,"$text")||l.checkChild(e,"paragraph"))}/**
-* @license Copyright (c) 2003-2025, CKSource Holding sp. z o.o. All rights reserved.
+* @license Copyright (c) 2003-2026, CKSource Holding sp. z o.o. All rights reserved.
 * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-licensing-options
 */const S=f("Ctrl+A");class u extends o{static get pluginName(){return"SelectAllEditing"}static get isOfficialPlugin(){return!0}init(){const e=this.editor,t=e.t,n=e.editing.view.document;e.commands.add("selectAll",new r(e)),this.listenTo(n,"keydown",(c,s)=>{g(s)===S&&(e.execute("selectAll"),s.preventDefault())}),e.accessibility.addKeystrokeInfos({keystrokes:[{label:t("Select all"),keystroke:"CTRL+A"}]})}}/**
-* @license Copyright (c) 2003-2025, CKSource Holding sp. z o.o. All rights reserved.
+* @license Copyright (c) 2003-2026, CKSource Holding sp. z o.o. All rights reserved.
 * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-licensing-options
 */class d extends o{static get pluginName(){return"SelectAllUI"}static get isOfficialPlugin(){return!0}init(){const e=this.editor;e.ui.componentFactory.add("selectAll",()=>{const t=this._createButton(A);return t.set({tooltip:!0}),t}),e.ui.componentFactory.add("menuBar:selectAll",()=>this._createButton(h))}_createButton(e){const t=this.editor,i=t.locale,n=t.commands.get("selectAll"),c=new e(t.locale),s=i.t;return c.set({label:s("Select all"),icon:p,keystroke:"Ctrl+A"}),c.bind("isEnabled").to(n,"isEnabled"),this.listenTo(c,"execute",()=>{t.execute("selectAll"),t.editing.view.focus()}),c}}/**
-* @license Copyright (c) 2003-2025, CKSource Holding sp. z o.o. All rights reserved.
+* @license Copyright (c) 2003-2026, CKSource Holding sp. z o.o. All rights reserved.
 * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-licensing-options
 */class w extends o{static get requires(){return[u,d]}static get pluginName(){return"SelectAll"}static get isOfficialPlugin(){return!0}}export{w as SelectAll,r as SelectAllCommand,u as SelectAllEditing,d as SelectAllUI};
