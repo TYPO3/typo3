53aee0cb7194912b3739f427d14d257b946babbe
Ravi Sachaniya <ravi.sachaniya@drcsystems.com>
2026-04-13 16:03:37 +0530

    [BUGFIX] Add index for content_from_pid in pages table
    Resolves: #109561
    Releases: main, 13.4
    Change-Id: If0a2990498bc69eea08ed2e2f12d0dc93dbd7c6e
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/93638
    Tested-by: core-ci <typo3@b13.com>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>

--- typo3/sysext/core/ext_tables.sql
+++ typo3/sysext/core/ext_tables.sql
@@ -44,6 +44,7 @@ CREATE TABLE pages (
 
 	KEY determineSiteRoot (is_siteroot),
 	KEY language_identifier (l10n_parent,sys_language_uid),
+	KEY contentFromPid (content_from_pid),
 	KEY slug (slug(127))
 );
 
