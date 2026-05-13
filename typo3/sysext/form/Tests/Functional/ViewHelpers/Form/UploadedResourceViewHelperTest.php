848a9f54b54648e8d3209c3e75ff8c62b2e99bce
Elias Häußler <elias@haeussler.dev>
2026-05-13 12:45:08 +0200

    [BUGFIX] Use unique ID for resource pointer fields in multi-upload field
    Once a form element of type `FileUpload` is configured with the
    `multiple` attribute and more than one file is uploaded, all resulting
    resource pointer fields contain the same ID. This patch appends the
    current index to the ID to make them unique in the DOM.
    
    Resolves: #109827
    Releases: main, 14.3
    Change-Id: Ie51b3e7d43460baf74d5df57c1c19f020114e5c0
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/94088
    Reviewed-by: Josua Vogel <j.vogel97@web.de>
    Tested-by: Garvin Hicking <garvin@hick.ing>
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Josua Vogel <j.vogel97@web.de>
    Tested-by: Oliver Klee <typo3-coding@oliverklee.de>
    Reviewed-by: Garvin Hicking <garvin@hick.ing>

--- typo3/sysext/form/Tests/Functional/ViewHelpers/Form/UploadedResourceViewHelperTest.php
+++ typo3/sysext/form/Tests/Functional/ViewHelpers/Form/UploadedResourceViewHelperTest.php
@@ -18,6 +18,12 @@ declare(strict_types=1);
 namespace TYPO3\CMS\Form\Tests\Functional\ViewHelpers\Form;
 
 use PHPUnit\Framework\Attributes\Test;
+use TYPO3\CMS\Core\Core\SystemEnvironmentBuilder;
+use TYPO3\CMS\Core\Http\ServerRequest;
+use TYPO3\CMS\Extbase\Domain\Model\FileReference;
+use TYPO3\CMS\Extbase\Mvc\ExtbaseRequestParameters;
+use TYPO3\CMS\Extbase\Mvc\Request;
+use TYPO3\CMS\Extbase\Persistence\ObjectStorage;
 use TYPO3\CMS\Fluid\Core\Rendering\RenderingContextFactory;
 use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
 use TYPO3Fluid\Fluid\View\TemplateView;
@@ -75,5 +81,53 @@ final class UploadedResourceViewHelperTest extends FunctionalTestCase
         self::assertStringContainsString('accept="application/pdf"', $result);
         self::assertStringContainsString('multiple="multiple"', $result);
         self::assertStringContainsString('name="[]"', $result);
+    }
+
+    /*
+     * Regression test for https://forge.typo3.org/issues/109827
+     * When multiple files are uploaded, each hidden resourcePointer input
+     * must receive a unique id attribute instead of all sharing the same id.
+     */
+    #[Test]
+    public function resourcePointerHaveUniqueIdsForMultipleUploadedFiles(): void
+    {
+        $extbaseRequest = $this->buildExtbaseRequest();
+
+        $fileRef1 = $this->createMock(FileReference::class);
+        $fileRef1->method('getUid')->willReturn(1);
+        $fileRef2 = $this->createMock(FileReference::class);
+        $fileRef2->method('getUid')->willReturn(2);
+
+        /** @var ObjectStorage<FileReference> $storage */
+        $storage = new ObjectStorage();
+        $storage->attach($fileRef1);
+        $storage->attach($fileRef2);
+
+        $context = $this->get(RenderingContextFactory::class)->create([], $extbaseRequest);
+        $context->getTemplatePaths()->setTemplateSource(
+            '<formvh:form.uploadedResource name="upload" id="my-field" as="resource" value="{storage}" />'
+        );
+        $context->getVariableProvider()->add('storage', $storage);
+
+        $result = (new TemplateView($context))->render();
+
+        // Each file must produce exactly one id of the form {id}-file-reference-{suffix}
+        preg_match_all('/id="my-field-file-reference-[^"]*"/', $result, $idMatches);
+        $ids = $idMatches[0];
+        self::assertCount(2, array_unique($ids), 'Each resource pointer hidden input must have a unique id');
+
+        // Each resource pointer name attribute must carry a unique index
+        preg_match_all('/name="upload\[__submittedFiles\]\[(\d+)\]\[submittedFile\]\[resourcePointer\]"/', $result, $nameMatches);
+        $names = $nameMatches[1];
+        self::assertCount(2, array_unique($names), 'Each resourcePointer name must have a unique index');
+    }
+
+    private function buildExtbaseRequest(): Request
+    {
+        $extbaseRequestParameters = new ExtbaseRequestParameters();
+        $psr7Request = (new ServerRequest())
+            ->withAttribute('extbase', $extbaseRequestParameters)
+            ->withAttribute('applicationType', SystemEnvironmentBuilder::REQUESTTYPE_BE);
+        return new Request($psr7Request);
     }
 }
