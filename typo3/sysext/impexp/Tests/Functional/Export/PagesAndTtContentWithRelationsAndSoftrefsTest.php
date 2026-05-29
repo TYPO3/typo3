8a003019b2b02684b57a9c98bfd9d66c86e94f2d
Josua Vogel <j.vogel97@web.de>
2026-05-29 09:13:11 +0200

    [TASK] Remove form_definitions-related fixtures from ext:impexp tests
    With the removal of FileMountStorageAdapter in ext:form (#109790) and
    the subsequent removal of FilePersistenceSlot, form definitions are no
    longer stored as FAL files in fileadmin. The 1:/form_definitions/...
    persistence identifier format is therefore no longer in use.
    
    The following test coverage tested exactly this now-removed scenario:
    
    * exportPagesAndRelatedTtContentWithFlexFormSoftrefs – exported a
      tt_content record referencing a FAL-based form definition
    * importPagesAndRelatedTtContentKeepsRelationBetweenImportedFlexForm
      SoftReferenceAndRelatedRecord – verified file restoration on import
    * importPagesAndRelatedTtContentCanKeepOriginalFlexFormSoftReference –
      verified SOFTREF_IMPORT_MODE_EXCLUDE behaviour
    * importPagesAndRelatedTtContentCanEditFlexFormSoftReference –
      verified SOFTREF_IMPORT_MODE_EDITABLE behaviour
    
    Remove these test methods along with all related fixture files:
    form_sys_file.csv, tt_content-with-flexform-softrefs.csv,
    pages-and-ttcontent-with-flexform-softrefs.xml, the three database
    assertion CSVs and the flexFormWithSoftReference.form.yaml fixture
    file. The pathsToLinkInTestInstance entry for fileadmin/form_definitions
    is removed from the export test class accordingly.
    
    Resolves: #109909
    Related: #109790
    Releases: main
    Change-Id: I6f7a0171df3bacfb61d679ab0a185895a5e5b9ab
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/94201
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Stefan Bürk <stefan@buerk.tech>
    Tested-by: core-ci <typo3@b13.com>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Oliver Klee <typo3-coding@oliverklee.de>
    Reviewed-by: Stefan Bürk <stefan@buerk.tech>

--- typo3/sysext/impexp/Tests/Functional/Export/PagesAndTtContentWithRelationsAndSoftrefsTest.php
+++ typo3/sysext/impexp/Tests/Functional/Export/PagesAndTtContentWithRelationsAndSoftrefsTest.php
@@ -26,7 +26,6 @@ final class PagesAndTtContentWithRelationsAndSoftrefsTest extends AbstractImport
 {
     protected array $pathsToLinkInTestInstance = [
         'typo3/sysext/impexp/Tests/Functional/Fixtures/Folders/fileadmin/user_upload' => 'fileadmin/user_upload',
-        'typo3/sysext/impexp/Tests/Functional/Fixtures/Folders/fileadmin/form_definitions' => 'fileadmin/form_definitions',
     ];
 
     protected array $testExtensionsToLoad = [
@@ -118,55 +117,4 @@ final class PagesAndTtContentWithRelationsAndSoftrefsTest extends AbstractImport
         );
     }
 
-    #[Test]
-    public function exportPagesAndRelatedTtContentWithFlexFormSoftrefs(): void
-    {
-        $this->importCSVDataSet(__DIR__ . '/../Fixtures/DatabaseImports/pages.csv');
-        $this->importCSVDataSet(__DIR__ . '/../Fixtures/DatabaseImports/tt_content-with-flexform-softrefs.csv');
-        $this->importCSVDataSet(__DIR__ . '/../Fixtures/DatabaseImports/form_sys_file.csv');
-
-        $GLOBALS['TCA']['tt_content']['columns']['pi_flexform']['config']['ds'] = '
-<T3DataStructure>
-    <sheets>
-        <sDEF>
-            <ROOT>
-                <sheetTitle>LLL:EXT:form/Resources/Private/Language/Database.xlf:tt_content.pi_flexform.formframework.sheet_general</sheetTitle>
-                <type>array</type>
-                <el>
-                    <settings.persistenceIdentifier>
-                        <label>LLL:EXT:form/Resources/Private/Language/Database.xlf:tt_content.pi_flexform.formframework.persistenceIdentifier</label>
-                        <onChange>reload</onChange>
-                        <config>
-                            <type>select</type>
-                            <renderType>selectSingle</renderType>
-                            <items>
-                                <numIndex index="0" type="array">
-                                    <label>LLL:EXT:form/Resources/Private/Language/Database.xlf:tt_content.pi_flexform.formframework.selectPersistenceIdentifier</label>
-                                    <value></value>
-                                </numIndex>
-                            </items>
-                            <softref>formPersistenceIdentifier</softref>
-                        </config>
-                    </settings.persistenceIdentifier>
-                </el>
-            </ROOT>
-        </sDEF>
-    </sheets>
-</T3DataStructure>';
-        $this->get(TcaSchemaFactory::class)->rebuild($GLOBALS['TCA']);
-
-        $subject = $this->get(Export::class);
-        $subject->setPid(1);
-        $subject->setLevels(1);
-        $subject->setTables(['_ALL']);
-        $subject->setRelOnlyTables(['sys_file']);
-        $subject->process();
-
-        $out = $subject->render();
-
-        self::assertXmlStringEqualsXmlFile(
-            __DIR__ . '/../Fixtures/XmlExports/pages-and-ttcontent-with-flexform-softrefs.xml',
-            $out
-        );
-    }
 }
