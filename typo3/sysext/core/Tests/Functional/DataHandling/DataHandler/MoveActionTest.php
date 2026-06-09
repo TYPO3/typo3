c9898d2e67608eda78f8bd1f06ee9cf05a872a56
Torben Hansen <derhansen@gmail.com>
2026-06-09 10:59:03 +0200

    [SECURITY] Prevent unauthorized record move via DataHandler
    With #106220, the DataHandler `moveRecord()` function has
    been refactored and access checks have been simplified. One
    access check has however been forgotten during the process
    leading to unauthorized movement of records.
    
    With this change, the missing check in DataHandler
    `moveRecord()` function has been re-added, so it is now
    checked again, if a user has edit permissions on the source
    page when moving a record to another page.
    
    Resolves: #108758
    Related: #106220
    Releases: main, 14.3, 13.4
    Change-Id: I95a061ab68111b879c34d51bfbe76cfdf5a64be7
    Security-Bulletin: TYPO3-CORE-SA-2026-012
    Security-References: CVE-2026-47350
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/94413
    Tested-by: Oliver Hader <oliver.hader@typo3.org>
    Reviewed-by: Oliver Hader <oliver.hader@typo3.org>

---
+++ typo3/sysext/core/Tests/Functional/DataHandling/DataHandler/MoveActionTest.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3\CMS\Core\Tests\Functional\DataHandling\DataHandler;
+
+use PHPUnit\Framework\Attributes\Test;
+use TYPO3\CMS\Core\Authentication\BackendUserAuthentication;
+use TYPO3\CMS\Core\DataHandling\DataHandler;
+use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
+
+final class MoveActionTest extends FunctionalTestCase
+{
+    private BackendUserAuthentication $backendUser;
+    private DataHandler $subject;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->importCSVDataSet(__DIR__ . '/DataSet/MoveRecord.csv');
+
+        $this->backendUser = $this->setUpBackendUser(9);
+        $this->backendUser->setWebmounts([1]);
+
+        $this->subject = $this->get(DataHandler::class);
+        $this->subject->start([], []);
+    }
+
+    #[Test]
+    public function moveRecordOfAccessibleRecordIsPermitted(): void
+    {
+        $recordUid = 1;
+        $targetPid = 3;
+        $this->subject->moveRecord('tt_content', $recordUid, $targetPid);
+        self::assertEmpty($this->subject->errorLog);
+    }
+
+    #[Test]
+    public function moveRecordOfNonAccessibleRecordIsDenied(): void
+    {
+        $recordUid = 2;
+        $sourcePid = 5;
+        $targetPid = 2;
+        $this->subject->moveRecord('tt_content', $recordUid, $targetPid);
+        self::assertStringContainsString(
+            sprintf(
+                'Attempt to move record tt_content:%d to pid "%d" without having permissions to update the source page (uid=%d)',
+                $recordUid,
+                $targetPid,
+                $sourcePid,
+            ),
+            $this->subject->errorLog[0]
+        );
+    }
+}
