9ebf52e6b5a3adece1a05f6e5dbf124d12c97f19
Benjamin Franzke <ben@bnf.dev>
2026-06-09 10:56:52 +0200

    [SECURITY] Fix open redirection in GeneralUtility::sanitizeLocalUrl
    Resolves: #109694
    Releases: main, 14.3, 13.4
    Change-Id: I8c8ce23c3d6042e5d1afffa6e35237e6a746fd08
    Security-Bulletin: TYPO3-CORE-SA-2026-009
    Security-References: CVE-2026-47347
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/94403
    Tested-by: Oliver Hader <oliver.hader@typo3.org>
    Reviewed-by: Oliver Hader <oliver.hader@typo3.org>

--- typo3/sysext/core/Classes/Utility/GeneralUtility.php
+++ typo3/sysext/core/Classes/Utility/GeneralUtility.php
@@ -2577,8 +2577,28 @@ class GeneralUtility
         }
         $sanitizedUrl = '';
         if (!empty($url)) {
-            if (strpbrk($url, "\n\r\x00") !== false) {
-                static::getLogger()->notice('URL "{url}" contains unexpected whitespace and was denied as local url.', ['url' => $url]);
+            $validUrlCharacters = [
+                // Percent-Encoding: https://datatracker.ietf.org/doc/html/rfc3986#section-2.1
+                '%',
+
+                // Reserved Characters: https://datatracker.ietf.org/doc/html/rfc3986#section-2.2
+                // gen-delims
+                ':', '/', '?', '#', '[', ']', '@',
+                // sub-delims
+                '!', '$', '&', '\'', '(', ')', '*', '+', ',', ';', '=',
+
+                // Unreserved Characters: https://datatracker.ietf.org/doc/html/rfc3986#section-2.3
+                '-', '.', '_', '~',
+                // ALPHA
+                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+                // DIGIT
+                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            ];
+
+            $hasInvalidCharacters = str_replace($validUrlCharacters, '', $url) !== '';
+            if ($hasInvalidCharacters) {
+                static::getLogger()->notice('The URL "{url}" contains unexpected characters and was denied as local url.', ['url' => $url]);
                 return '';
             }
 
