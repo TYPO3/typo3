8207bf4c22249599a82bd522423f8738474ca9a2
Georg Ringer <georg.ringer@gmail.com>
2026-07-10 20:55:29 +0200

    [BUGFIX] Respect required but not rendered fields in Page Wizard
    If a field is required but not shown - e.g. because of a displayCond -
    it must not be shown in the required step.
    
    Resolves: #110143
    Releases: main, 14.3
    Change-Id: I2acd15c1e1675ac0aa5077d77671a7475e36d059
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/94762
    Tested-by: Benjamin Kott <benjamin.kott@outlook.com>
    Tested-by: core-ci <typo3@b13.com>
    Reviewed-by: Benjamin Kott <benjamin.kott@outlook.com>
    Reviewed-by: Lisa Kreitz <lisamariakreitz@web.de>
    Tested-by: Simon Schaufelberger <simonschaufi+typo3@gmail.com>
    Reviewed-by: Simon Schaufelberger <simonschaufi+typo3@gmail.com>
    Tested-by: Lisa Kreitz <lisamariakreitz@web.de>

--- typo3/sysext/backend/Tests/Functional/Wizard/PageWizardStepBuilderTest.php
+++ typo3/sysext/backend/Tests/Functional/Wizard/PageWizardStepBuilderTest.php
@@ -104,4 +104,72 @@ final class PageWizardStepBuilderTest extends FunctionalTestCase
         self::assertEquals('requiredFields', $steps[1]->jsonSerialize()['configurationData']['key']);
         self::assertArrayHasKey('title', $steps[1]->jsonSerialize()['configurationData']['labels']);
     }
+
+    #[Test]
+    public function getStepsForDokTypeSkipsRequiredStepWhenRequiredFieldsAreHiddenViaDisplayCondition(): void
+    {
+        $GLOBALS['TCA']['pages']['columns']['only_in_siteroot'] = [
+            'label' => 'Only in siteroot',
+            'displayCond' => 'FIELD:is_siteroot:=:1',
+            'config' => [
+                'type' => 'input',
+                'required' => true,
+            ],
+        ];
+        $GLOBALS['TCA']['pages']['types']['999'] = [
+            'showitem' => 'title, only_in_siteroot',
+            'wizardSteps' => [
+                'step1' => [
+                    'title' => 'Step 1',
+                    'fields' => ['title'],
+                ],
+            ],
+        ];
+        $this->get(TcaSchemaFactory::class)->rebuild($GLOBALS['TCA']);
+
+        $subject = $this->get(PageWizardStepBuilder::class);
+        // A new page has is_siteroot=0, so the display condition does not match and the
+        // only required field is hidden. Therefore no "requiredFields" step must be added.
+        $steps = $subject->getStepsForDokType('999', 0, $this->getRequest());
+
+        self::assertCount(1, $steps);
+        self::assertEquals('step1', $steps[0]->jsonSerialize()['configurationData']['key']);
+    }
+
+    #[Test]
+    public function getStepsForDokTypeAddsRequiredStepWithOnlyVisibleRequiredFields(): void
+    {
+        // A required field which is always visible ...
+        $GLOBALS['TCA']['pages']['columns']['title']['config']['required'] = true;
+        // ... and a required field which is hidden via display condition for a new page.
+        $GLOBALS['TCA']['pages']['columns']['only_in_siteroot'] = [
+            'label' => 'Only in siteroot',
+            'displayCond' => 'FIELD:is_siteroot:=:1',
+            'config' => [
+                'type' => 'input',
+                'required' => true,
+            ],
+        ];
+        $GLOBALS['TCA']['pages']['types']['999'] = [
+            'showitem' => 'title, nav_title, only_in_siteroot',
+            'wizardSteps' => [
+                'step1' => [
+                    'title' => 'Step 1',
+                    'fields' => ['nav_title'],
+                ],
+            ],
+        ];
+        $this->get(TcaSchemaFactory::class)->rebuild($GLOBALS['TCA']);
+
+        $subject = $this->get(PageWizardStepBuilder::class);
+        $steps = $subject->getStepsForDokType('999', 0, $this->getRequest());
+
+        // The "requiredFields" step must be added since "title" is still required and visible,
+        // but the display-condition-hidden "only_in_siteroot" must not be part of it.
+        self::assertCount(2, $steps);
+        self::assertEquals('step1', $steps[0]->jsonSerialize()['configurationData']['key']);
+        self::assertEquals('requiredFields', $steps[1]->jsonSerialize()['configurationData']['key']);
+        self::assertArrayHasKey('title', $steps[1]->jsonSerialize()['configurationData']['labels']);
+        self::assertArrayNotHasKey('only_in_siteroot', $steps[1]->jsonSerialize()['configurationData']['labels']);
+    }
 }
