2ff2700112b8176da6aa6139ae3f8987d7723085
Garvin Hicking <garvin@hick.ing>
2026-07-29 15:04:10 +0200

    [BUGFIX] Use proper EnvironmentController test transparency generation
    As noted in #97975 in depth, transparency information interpretation
    in GraphicsMagick and ImageMagick acts a bit differently, and using
    a "-background none" parameter allows to prevent applying a white
    background.
    
    However, when the parameter "-flatten" is used, GraphicsMagick
    interprets this background instruction to actually create
    a background, leading to a wrong expectation.
    
    The "-flatten" argument is irrelevant, because the proper "frame"
    is already selected, and no flattening is needed. The "[0] in
    the generation command already takes care of it:
    
    ```
    /usr/bin/gm convert -interlace 'None' -auto-orient \
      -background none '+profile' '*' -auto-orient \
      -geometry 300x225! -quality 85 -colorspace 'RGB' \
      'png:/some.png[0]' 'some.webp'
    ```
    
    The "-flatten" argument was mistakenly copy-pastad into
    the test introduced in #97975 and is now removed to properly
    act on it. Regular frontend image generation never uses
    "-flatten" by default, so having this in a test may lead
    people searching for transparency related CSS generation bugs
    on a wrong route.
    
    Resolves: #110321
    Related: #97975
    Releases: main, 14.3
    Change-Id: I902d1dad6cda502e85bb85e714395b76f7282659
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/94986
    Tested-by: Sven Liebert <mail@sven-liebert.de>
    Reviewed-by: Sven Liebert <mail@sven-liebert.de>
    Reviewed-by: Stefan Bürk <stefan@buerk.tech>
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Stefan Bürk <stefan@buerk.tech>

--- typo3/sysext/install/Classes/Controller/EnvironmentController.php
+++ typo3/sysext/install/Classes/Controller/EnvironmentController.php
@@ -684,7 +684,7 @@ class EnvironmentController extends AbstractController
         $imageService = $this->initializeGraphicalFunctions();
         $inputFile = $imageBasePath . 'TestInput/Transparent.svg';
         $imageService->imageMagickConvert_forceFileNameBody = StringUtility::getUniqueId('transparent-svg-webp');
-        $imResult = $imageService->resize($inputFile, 'webp', '300', '', '-flatten', [], true);
+        $imResult = $imageService->resize($inputFile, 'webp', '300', '', '', [], true);
         if ($imResult !== null && $imResult->isFile()) {
             $result = [
                 'fileExists' => true,
@@ -716,7 +716,7 @@ class EnvironmentController extends AbstractController
         $imageService = $this->initializeGraphicalFunctions();
         $inputFile = $imageBasePath . 'TestInput/Transparent.svg';
         $imageService->imageMagickConvert_forceFileNameBody = StringUtility::getUniqueId('transparent-svg-webp');
-        $imResult = $imageService->resize($inputFile, 'png', '300', '', '-flatten', [], true);
+        $imResult = $imageService->resize($inputFile, 'png', '300', '', '', [], true);
         if ($imResult !== null && $imResult->isFile()) {
             $result = [
                 'fileExists' => true,
