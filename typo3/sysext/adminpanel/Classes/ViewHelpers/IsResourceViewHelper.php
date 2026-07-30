323e808f4c80aea7a7720b35b3f10cad23585263
Maike Kiebele <maike.kiebele@gmail.com>
2026-07-30 09:34:22 +0200

    [BUGFIX] Cast debug value to string before translating in Adminpanel
    TableKeyValue.fluid.html previously treated any value that was not an integer, float, boolean, or array as a string. This included PHP
    resources, which were passed directly as the "key" and "default"
    arguments to <f:translate>, resulting in an uncaught
    InvalidArgumentValueException.
    
    Explicitly detect resources and render them via the existing raw output path instead of attempting a translation. Cast actual string values to string before passing them to <f:translate>.
    
    Resolves: #110299
    Releases: main, 14.3
    Change-Id: Ica62df64ac0262d8cc19a6d584992a9616947aa9
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/95011
    Reviewed-by: Stefan Bürk <stefan@buerk.tech>
    Tested-by: Garvin Hicking <garvin@hick.ing>
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Benni Mack <benni@typo3.org>
    Reviewed-by: Garvin Hicking <garvin@hick.ing>
    Tested-by: Susi Moog <look@susi.dev>
    Reviewed-by: Benni Mack <benni@typo3.org>
    Tested-by: Stefan Bürk <stefan@buerk.tech>
    Reviewed-by: Susi Moog <look@susi.dev>

---
+++ typo3/sysext/adminpanel/Classes/ViewHelpers/IsResourceViewHelper.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3\CMS\Adminpanel\ViewHelpers;
+
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+
+/**
+ * ViewHelper to check whether the given value is a PHP resource. Fluid's
+ * casting expression ("{value as resource}") does not support "resource"
+ * as a target type, so this can't be done with a cast expression.
+ *
+ * ```
+ *   <adm:isResource value="{someVar}" />
+ * ```
+ *
+ * @internal
+ */
+final class IsResourceViewHelper extends AbstractViewHelper
+{
+    public function initializeArguments(): void
+    {
+        $this->registerArgument('value', 'mixed', 'The variable being checked', true);
+    }
+
+    public function render(): bool
+    {
+        return is_resource($this->arguments['value']);
+    }
+}
