a71388230ce4773e09a64829501392f7221199af
Elias Häußler <elias@haeussler.dev>
2026-08-05 09:18:52 +0200

    [BUGFIX] Respect and apply configured CSS classes from file list actions
    When file list actions are configured with custom CSS classes, they were
    previously not applied to the resulting dropdown items in the secondary
    action group. This is now fixed for both generic buttons and link
    buttons.
    
    Resolves: #110377
    Releases: main, 14.3
    Change-Id: Ic16cd05a4b20aec58b4877436194d13a5292dc65
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/95196
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Garvin Hicking <garvin@hick.ing>
    Reviewed-by: Garvin Hicking <garvin@hick.ing>

--- typo3/sysext/filelist/Classes/FileList.php
+++ typo3/sysext/filelist/Classes/FileList.php
@@ -979,20 +979,27 @@ class FileList
         }
         foreach ($event->getActionGroup(ActionGroup::secondary)->getItems() as $action) {
             if ($action instanceof GenericButton) {
+                $attributes = $action->getAttributes();
+                if ($action->getClasses() !== '') {
+                    $attributes['class'] = $action->getClasses();
+                }
                 $action = $this->componentFactory
                     ->createDropDownItem()
                     ->setTag($action->getTag())
                     ->setLabel($action->getLabel() ?: $action->getTitle())
                     ->setIcon($action->getIcon())
                     ->setHref($action->getHref())
-                    ->setAttributes($action->getAttributes());
+                    ->setAttributes($attributes);
                 $cellOutput .= '<li>' . $action->render() . '</li>';
                 continue;
             }
             if ($action instanceof LinkButton) {
-                $attributes = [];
+                $attributes = $action->getAttributes();
                 foreach ($action->getDataAttributes() as $key => $value) {
                     $attributes['data-' . $key] = $value;
+                }
+                if ($action->getClasses() !== '') {
+                    $attributes['class'] = $action->getClasses();
                 }
                 $action = $this->componentFactory
                     ->createDropDownItem()
@@ -1000,7 +1007,6 @@ class FileList
                     ->setIcon($action->getIcon())
                     ->setHref($action->getHref())
                     ->setAttributes([
-                        ...$action->getAttributes(),
                         ...$attributes,
                         'role' => $action->getRole(),
                     ]);
