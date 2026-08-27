31c1887202ffa18defc49d2b328f0e62f7ebfcf5
Georg Ringer <georg.ringer@gmail.com>
2026-08-27 22:16:32 +0200

    [TASK] Allow record objects for record icons
    IconFactory->getIconForRecord() and the row argument of the Fluid
    ViewHelper <core:iconForRecord> only accepted a plain database row
    array. Code already working with record objects had to rebuild an
    array just to render an icon.
    
    IconFactory->getIconForRecordObject() now renders the icon for an
    object implementing RecordInterface. The table is taken from the
    record itself, the raw database row is used, so status overlays for
    disabled or scheduled records are rendered the same way as for a
    plain array.
    
    The ViewHelper gained an interchangeable argument "record": both
    "row" and "record" accept a row array as well as a record object,
    exactly one of them must be given. The argument "table" is only
    required for row arrays and is not evaluated for record objects.
    
    Resolves: #108813
    Releases: main, 14.3
    Signed-off-by: Georg Ringer <georg.ringer@gmail.com>
    Change-Id: I6ec2abfa0e9945805945744ecde027571ba474c6
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/95461
    Tested-by: Benni Mack <benni@typo3.org>
    Reviewed-by: Wouter Wolters <typo3@wouterwolters.nl>
    Reviewed-by: Benni Mack <benni@typo3.org>
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Wouter Wolters <typo3@wouterwolters.nl>

--- typo3/sysext/core/Tests/Functional/ViewHelpers/IconForRecordViewHelperTest.php
+++ typo3/sysext/core/Tests/Functional/ViewHelpers/IconForRecordViewHelperTest.php
@@ -17,15 +17,17 @@ declare(strict_types=1);
 
 namespace TYPO3\CMS\Core\Tests\Functional\ViewHelpers;
 
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Test;
+use TYPO3\CMS\Backend\Utility\BackendUtility;
+use TYPO3\CMS\Core\Domain\RecordFactory;
 use TYPO3\CMS\Fluid\Core\Rendering\RenderingContextFactory;
 use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
+use TYPO3Fluid\Fluid\Core\ViewHelper\InvalidArgumentValueException;
 use TYPO3Fluid\Fluid\View\TemplateView;
 
 final class IconForRecordViewHelperTest extends FunctionalTestCase
 {
-    protected bool $initializeDatabase = false;
-
     #[Test]
     public function renderRendersIconCallingIconFactoryAccordingToGivenArguments(): void
     {
@@ -34,5 +36,61 @@ final class IconForRecordViewHelperTest extends FunctionalTestCase
             '<core:iconForRecord table="tt_content" row="{uid: 123}" size="large" alternativeMarkupIdentifier="inline" />'
         );
         self::assertStringContainsString('<span class="t3js-icon icon icon-size-large icon-state-default icon-mimetypes-x-content-text" data-identifier="mimetypes-x-content-text" aria-hidden="true">', new TemplateView($context)->render());
+    }
+
+    public static function recordObjectTemplateSourceDataProvider(): iterable
+    {
+        yield 'record argument' => ['<core:iconForRecord record="{record}" size="large" />'];
+        yield 'row argument' => ['<core:iconForRecord row="{record}" size="large" />'];
+        yield 'record argument with table' => ['<core:iconForRecord table="tt_content" record="{record}" size="large" />'];
+    }
+
+    #[DataProvider('recordObjectTemplateSourceDataProvider')]
+    #[Test]
+    public function renderRendersIconForRecordObject(string $templateSource): void
+    {
+        $this->importCSVDataSet(__DIR__ . '/../Fixtures/pages.csv');
+        $record = $this->get(RecordFactory::class)->createFromDatabaseRow(
+            'tt_content',
+            BackendUtility::getRecord('tt_content', 5)
+        );
+        $context = $this->get(RenderingContextFactory::class)->create();
+        $context->getTemplatePaths()->setTemplateSource($templateSource);
+        $view = new TemplateView($context);
+        $view->assign('record', $record);
+
+        self::assertStringContainsString('data-identifier="mimetypes-x-content-text-media"', $view->render());
+    }
+
+    #[Test]
+    public function renderThrowsExceptionIfBothRowAndRecordAreGiven(): void
+    {
+        $this->expectException(InvalidArgumentValueException::class);
+        $this->expectExceptionCode(1788361133);
+        $context = $this->get(RenderingContextFactory::class)->create();
+        $context->getTemplatePaths()->setTemplateSource(
+            '<core:iconForRecord table="tt_content" row="{uid: 123}" record="{uid: 123}" />'
+        );
+        new TemplateView($context)->render();
+    }
+
+    #[Test]
+    public function renderThrowsExceptionIfNeitherRowNorRecordIsGiven(): void
+    {
+        $this->expectException(InvalidArgumentValueException::class);
+        $this->expectExceptionCode(1788361134);
+        $context = $this->get(RenderingContextFactory::class)->create();
+        $context->getTemplatePaths()->setTemplateSource('<core:iconForRecord table="tt_content" />');
+        new TemplateView($context)->render();
+    }
+
+    #[Test]
+    public function renderThrowsExceptionIfTableIsMissingForRecordRow(): void
+    {
+        $this->expectException(InvalidArgumentValueException::class);
+        $this->expectExceptionCode(1788361135);
+        $context = $this->get(RenderingContextFactory::class)->create();
+        $context->getTemplatePaths()->setTemplateSource('<core:iconForRecord row="{uid: 123}" />');
+        new TemplateView($context)->render();
     }
 }
