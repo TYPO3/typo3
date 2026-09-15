bfb31adf7a41738813957df3a885d5b53efc4652
Stefan Bürk <stefan@buerk.tech>
2026-09-15 09:54:54 +0200

    [TASK] Isolate global state in SetCookieServiceTest
    SetCookieServiceTest sets the cookie domain in $GLOBALS and does not
    reset it. Since global state is only backed up for test classes which
    opt in, a following test like UserSessionManagerTest resolves a wrong
    cookie scope in randomized unit test runs and fails.
    
    The test class now opts in to the global state backup.
    
    Resolves: #110706
    Related: #110624
    Related: #107895
    Releases: main, 14.3
    Signed-off-by: Stefan Bürk <stefan@buerk.tech>
    Change-Id: I6f87770c5ad0fdd7c97347fd2c36f3352e2cca3a
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/95845
    Tested-by: core-ci <typo3@b13.com>

--- typo3/sysext/core/Tests/Unit/Http/SetCookieServiceTest.php
+++ typo3/sysext/core/Tests/Unit/Http/SetCookieServiceTest.php
@@ -17,6 +17,7 @@ declare(strict_types=1);
 
 namespace TYPO3\CMS\Core\Tests\Unit\Http;
 
+use PHPUnit\Framework\Attributes\BackupGlobals;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Test;
 use Psr\Log\NullLogger;
@@ -28,6 +29,7 @@ use TYPO3\CMS\Core\Session\UserSession;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
 
+#[BackupGlobals(true)]
 final class SetCookieServiceTest extends UnitTestCase
 {
     protected function setUp(): void
