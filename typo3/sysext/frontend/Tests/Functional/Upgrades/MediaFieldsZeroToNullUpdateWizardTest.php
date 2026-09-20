f6739a7d61eb7ba6eb0f1493c3cdf00b410b9889
Wouter Wolters <typo3@wouterwolters.nl>
2026-09-20 16:09:44 +0200

    [BUGFIX] Migrate media schema in upgrade wizard
    Make imagewidth and imageheight nullable with a NULL default before
    replacing zero values. This lets automated upgrades complete without a
    separate database schema analysis step.
    
    Resolves: #110195
    Releases: main, 14.3
    Signed-off-by: Wouter Wolters <typo3@wouterwolters.nl>
    Change-Id: I56a4a30f9d100e57a01378c1df7f0c9a1e6fbdca
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/95978
    Tested-by: Benni Mack <benni@typo3.org>
    Tested-by: core-ci <typo3@b13.com>
    Reviewed-by: Benni Mack <benni@typo3.org>

--- typo3/sysext/frontend/Tests/Functional/Upgrades/MediaFieldsZeroToNullUpdateWizardTest.php
+++ typo3/sysext/frontend/Tests/Functional/Upgrades/MediaFieldsZeroToNullUpdateWizardTest.php
@@ -18,6 +18,7 @@ declare(strict_types=1);
 namespace TYPO3\CMS\Frontend\Tests\Functional\Upgrades;
 
 use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\Attributes\TestWith;
 use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Frontend\Upgrades\MediaFieldsZeroToNullUpdateWizard;
 use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
@@ -48,5 +49,65 @@ final class MediaFieldsZeroToNullUpdateWizardTest extends FunctionalTestCase
         // Just ensure that running the upgrade again does not change anything
         $this->subject->executeUpdate();
         $this->assertCSVDataSet($this->fullMigrationResultDataSet);
+    }
+
+    #[TestWith([true])]
+    #[TestWith([false])]
+    #[Test]
+    public function mediaFieldsAndSchemaAreUpdated(bool $nonNullable): void
+    {
+        $connection = $this->get(ConnectionPool::class)->getConnectionForTable('tt_content');
+        $schemaManager = $connection->createSchemaManager();
+        $oldTable = $schemaManager->introspectTable('tt_content');
+        $newTable = clone $oldTable;
+        foreach (['imagewidth', 'imageheight'] as $fieldName) {
+            $newTable->getColumn($fieldName)->setNotnull($nonNullable)->setDefault(0);
+        }
+        $diff = $schemaManager->createComparator()->compareTables($oldTable, $newTable);
+        foreach ($connection->getDatabasePlatform()->getAlterTableSQL($diff) as $statement) {
+            $connection->executeStatement($statement);
+        }
+        $connection->insert('tt_content', ['uid' => 1, 'imagewidth' => 0, 'imageheight' => 0]);
+        $connection->insert('tt_content', ['uid' => 2, 'imagewidth' => 128, 'imageheight' => 256]);
+
+        self::assertTrue($this->subject->updateNecessary());
+        self::assertTrue($this->subject->executeUpdate());
+
+        self::assertSame(
+            ['imagewidth' => null, 'imageheight' => null],
+            $connection->fetchAssociative('SELECT imagewidth, imageheight FROM tt_content WHERE uid = 1')
+        );
+        self::assertSame(
+            ['imagewidth' => 128, 'imageheight' => 256],
+            $connection->fetchAssociative('SELECT imagewidth, imageheight FROM tt_content WHERE uid = 2')
+        );
+        $updatedTable = $schemaManager->introspectTable('tt_content');
+        self::assertFalse($updatedTable->getColumn('imagewidth')->getNotnull());
+        self::assertFalse($updatedTable->getColumn('imageheight')->getNotnull());
+        self::assertNull($updatedTable->getColumn('imagewidth')->getDefault());
+        self::assertNull($updatedTable->getColumn('imageheight')->getDefault());
+        self::assertFalse($this->subject->updateNecessary());
+    }
+
+    #[Test]
+    public function schemaIsUpdatedWithoutRecordsToMigrate(): void
+    {
+        $connection = $this->get(ConnectionPool::class)->getConnectionForTable('tt_content');
+        $schemaManager = $connection->createSchemaManager();
+        $oldTable = $schemaManager->introspectTable('tt_content');
+        $newTable = clone $oldTable;
+        $newTable->getColumn('imagewidth')->setNotnull(true)->setDefault(0);
+        $diff = $schemaManager->createComparator()->compareTables($oldTable, $newTable);
+        foreach ($connection->getDatabasePlatform()->getAlterTableSQL($diff) as $statement) {
+            $connection->executeStatement($statement);
+        }
+
+        self::assertTrue($this->subject->updateNecessary());
+        self::assertTrue($this->subject->executeUpdate());
+
+        $updatedTable = $schemaManager->introspectTable('tt_content');
+        self::assertFalse($updatedTable->getColumn('imagewidth')->getNotnull());
+        self::assertNull($updatedTable->getColumn('imagewidth')->getDefault());
+        self::assertFalse($this->subject->updateNecessary());
     }
 }
