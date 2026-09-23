2cf43628333f6f59d953f56ffb334222ff460835
Benni Mack <benni@typo3.org>
2026-09-23 19:27:30 +0200

    [BUGFIX] Support comma-separated allowAttributes in RteHtmlParser
    The RTE processing option "allowAttributes" only took effect in its
    array form ("allowAttributes."). A comma-separated list, as accepted
    by sibling options such as "allowTagsOutside", was silently ignored
    and the default attribute list stayed in place.
    
    Both notations are now evaluated. The option restricts the attributes
    of <p> tags only, in contrast to "allowedAttribs" of HTMLparser_db,
    which is configured per tag. The comments in Processing.yaml and the
    property docblock now say so, and no longer refer to the non-existent
    option "keepPDIVattribs".
    
    Resolves: #87314
    Releases: main, 14.3
    Signed-off-by: Benni Mack <benni@typo3.org>
    Change-Id: Ib4ed330e673857597a02f291f41e2caf721df677
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/96011
    Reviewed-by: Wouter Wolters <typo3@wouterwolters.nl>
    Reviewed-by: Georg Ringer <georg.ringer@gmail.com>
    Tested-by: Wouter Wolters <typo3@wouterwolters.nl>
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Georg Ringer <georg.ringer@gmail.com>

--- typo3/sysext/core/Classes/Html/RteHtmlParser.php
+++ typo3/sysext/core/Classes/Html/RteHtmlParser.php
@@ -75,7 +75,7 @@ class RteHtmlParser extends HtmlParser
 
     /**
      * A list of HTML attributes for <p> tags. Because <p> tags are wrapped currently in a special handling,
-     * they have a special place for configuration via 'proc.keepPDIVattribs'
+     * they are configured via the processing option 'allowAttributes'
      */
     protected array $allowedAttributesForParagraphTags = [
         'class',
@@ -144,7 +144,9 @@ class RteHtmlParser extends HtmlParser
 
         // Define which attributes are allowed on <p> tags
         if (isset($this->procOptions['allowAttributes.'])) {
-            $this->allowedAttributesForParagraphTags = $this->procOptions['allowAttributes.'];
+            $this->allowedAttributesForParagraphTags = (array)$this->procOptions['allowAttributes.'];
+        } elseif (!empty($this->procOptions['allowAttributes'])) {
+            $this->allowedAttributesForParagraphTags = GeneralUtility::trimExplode(',', strtolower($this->procOptions['allowAttributes']), true);
         }
         // Override tags which are allowed outside of <p> tags
         if (isset($this->procOptions['allowTagsOutside'])) {
--- typo3/sysext/core/Tests/Unit/Html/RteHtmlParserTest.php
+++ typo3/sysext/core/Tests/Unit/Html/RteHtmlParserTest.php
@@ -752,6 +752,27 @@ final class RteHtmlParserTest extends UnitTestCase
         self::assertEquals('<p><span>Not allowed outside of p-tag</span></p>', $subject->transformTextForRichTextEditor('<span>Not allowed outside of p-tag</span>', $this->procOptions));
     }
 
+    public static function allowAttributesRestrictsAttributesOfParagraphTagsDataProvider(): array
+    {
+        return [
+            'comma separated list' => [['allowAttributes' => 'class,id']],
+            'array' => [['allowAttributes.' => ['class', 'id']]],
+        ];
+    }
+
+    #[DataProvider('allowAttributesRestrictsAttributesOfParagraphTagsDataProvider')]
+    #[Test]
+    public function allowAttributesRestrictsAttributesOfParagraphTags(array $options): void
+    {
+        $eventDispatcher = new NoopEventDispatcher();
+        $subject = new RteHtmlParser($eventDispatcher, new NullLogger(), new LinkService($eventDispatcher));
+        $result = $subject->transformTextForPersistence(
+            '<p class="a" id="b" title="c" dir="ltr">Text</p>',
+            ['mode' => 'default'] + $options
+        );
+        self::assertSame('<p class="a" id="b">Text</p>', $result);
+    }
+
     #[Test]
     public function tableAndFigureApplyCorrectlyOutsideOfParagraphTags(): void
     {
